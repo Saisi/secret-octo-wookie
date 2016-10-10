29653206f8343a5ecb4ed5cff4764d2d72821f68
saisi <saisi@users.noreply.github.com>
2016-10-10 22:40:16 +0000

    1476139216 aerogenesis

---
+++ 1476139216.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1298971780
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 16;
+};
+
+union U1 {
+   const volatile uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-8L);/* VOLATILE GLOBAL g_2 */
