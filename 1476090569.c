ba1794a5ef0bb2aa15daa524c1651b917fca113d
saisi <saisi@users.noreply.github.com>
2016-10-10 09:09:29 +0000

    1476090569 balandrana

---
+++ 1476090569.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3019642931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 4294967294UL;
+static uint32_t g_50 = 0x4D41BD43L;
+static int16_t g_52 = 0xEE35L;
+static int32_t g_56 = 5L;
+static volatile int8_t g_83 = (-6L);/* VOLATILE GLOBAL g_83 */
+static volatile int8_t *g_82 = &g_83;
+static volatile int8_t ** const g_81 = &g_82;
+static volatile int8_t **g_87 = &g_82;
+static volatile int8_t *** const  volatile g_86[6][1][9] = {{{(void*)0,&g_87,&g_87,&g_87,&g_87,&g_87,(void*)0,&g_87,&g_87}},{{&g_87,&g_87,&g_87,(void*)0,(void*)0,&g_87,(void*)0,(void*)0,&g_87}},{{&g_87,&g_87,&g_87,&g_87,&g_87,(void*)0,&g_87,&g_87,&g_87}},{{&g_87,(void*)0,&g_87,&g_87,(void*)0,&g_87,(void*)0,&g_87,&g_87}},{{&g_87,&g_87,&g_87,&g_87,(void*)0,(void*)0,&g_87,&g_87,&g_87}},{{(void*)0,&g_87,(void*)0,&g_87,&g_87,&g_87,(void*)0,(void*)0,&g_87}}};
