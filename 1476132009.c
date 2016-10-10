2cdce448d805d286410144fe5aad4f9512af0134
saisi <saisi@users.noreply.github.com>
2016-10-10 20:40:10 +0000

    1476132009 aponeurotic

---
+++ 1476132009.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1606410557
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const int8_t  f1;
+   const signed f2 : 4;
+   const volatile uint32_t  f3;
+   int16_t  f4;
+   int32_t  f5;
+   uint64_t  f6;
