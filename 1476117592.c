16c0b9d59eb30cc1b60d712eff503de0a329b205
saisi <saisi@users.noreply.github.com>
2016-10-10 16:39:53 +0000

    1476117592 antibureaucratic

---
+++ 1476117592.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1579892274
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int8_t  f1;
+   const int32_t  f2;
+   volatile int32_t  f3;
+   volatile int64_t  f4;
+   const int64_t  f5;
+   const volatile uint8_t  f6;
+   int32_t  f7;
+   const uint16_t  f8;
