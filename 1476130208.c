2f911eddfc854daa3848ab6cd3f11a415ce5e7c5
saisi <saisi@users.noreply.github.com>
2016-10-10 20:10:08 +0000

    1476130208 aerage

---
+++ 1476130208.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2609663077
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 17;
+   const uint32_t  f1;
+   const int64_t  f2;
+   const volatile uint16_t  f3;
+   volatile int32_t  f4;
+   int64_t  f5;
+   unsigned f6 : 28;
+   int8_t  f7;
+};
