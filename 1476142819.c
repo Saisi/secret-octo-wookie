f9000183513e8575e88741c3f56f32554ca09304
saisi <saisi@users.noreply.github.com>
2016-10-10 23:40:19 +0000

    1476142819 banishments

---
+++ 1476142819.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3899531551
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[5][5] = {{4294967295UL,1UL,4294967295UL,4294967295UL,1UL},{4294967287UL,0xBE02189FL,0xB09F5954L,0xA8CF1D65L,0xB09F5954L},{1UL,1UL,0x278C007CL,1UL,1UL},{0xB09F5954L,0xA8CF1D65L,0xB09F5954L,0xBE02189FL,4294967287UL},{1UL,4294967295UL,4294967295UL,1UL,4294967295UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
