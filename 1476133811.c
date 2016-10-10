15e89ee4e156740744531a98b53068d635b0f71a
saisi <saisi@users.noreply.github.com>
2016-10-10 21:10:12 +0000

    1476133811 atelomyelia

---
+++ 1476133811.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1093573119
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 30;
+   signed f1 : 29;
+   signed f2 : 12;
+   signed f3 : 7;
+   const unsigned f4 : 28;
+};
+#pragma pack(pop)
