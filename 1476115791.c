567c4ae8385e36903406fe5440eb422bf7086dab
saisi <saisi@users.noreply.github.com>
2016-10-10 16:09:51 +0000

    1476115791 ampullulae

---
+++ 1476115791.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2907043025
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static int64_t g_8[3] = {(-6L),(-6L),(-6L)};
+static uint32_t g_9[9][2] = {{0x0274952FL,0xDC8DFBA2L},{0xDC8DFBA2L,0x0274952FL},{0xDC8DFBA2L,0xDC8DFBA2L},{0x0274952FL,0xDC8DFBA2L},{0xDC8DFBA2L,0x0274952FL},{0xDC8DFBA2L,0xDC8DFBA2L},{0x0274952FL,0xDC8DFBA2L},{0xDC8DFBA2L,0x0274952FL},{0xDC8DFBA2L,0xDC8DFBA2L}};
+static int64_t g_43 = (-1L);
+static int32_t *g_68 = &g_2;
+static int32_t ** const g_67[5][4][1] = {{{(void*)0},{&g_68},{&g_68},{&g_68}},{{&g_68},{&g_68},{(void*)0},{&g_68}},{{&g_68},{&g_68},{&g_68},{&g_68}},{{(void*)0},{&g_68},{&g_68},{&g_68}},{{&g_68},{&g_68},{(void*)0},{&g_68}}};
+static uint8_t g_104 = 0xC8L;
+static uint8_t g_109 = 255UL;
+static int32_t g_113 = 0xBF60C7DCL;
