be1402996e658f3ada4692b5bf1f6bc96e615298
saisi <saisi@users.noreply.github.com>
2016-10-11 23:11:36 +0000

    1476227496 antipodagron

---
+++ 1476227496.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3072564465
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   volatile signed f1 : 12;
+   volatile signed f2 : 18;
+   volatile signed f3 : 14;
+   const unsigned f4 : 29;
+   signed f5 : 23;
+   volatile unsigned f6 : 30;
+   signed : 0;
+   unsigned f7 : 15;
