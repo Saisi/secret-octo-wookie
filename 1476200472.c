8b5feb487ee7e221ecee7a9c3749f54ed9cc0672
saisi <saisi@users.noreply.github.com>
2016-10-11 15:41:13 +0000

    1476200472 alimonies

---
+++ 1476200472.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2808078320
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+   const int8_t  f2;
+   uint32_t  f3;
+   const volatile int64_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
