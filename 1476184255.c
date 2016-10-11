d41d1b8b6fcfadc7c99b7bae35afa35ec48cac0a
saisi <saisi@users.noreply.github.com>
2016-10-11 11:10:55 +0000

    1476184255 autotrophy

---
+++ 1476184255.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3576925984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 30;
+   const int16_t  f1;
+   const volatile uint32_t  f2;
+};
+#pragma pack(pop)
+
+struct S1 {
