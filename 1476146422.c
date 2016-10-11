215a54750108f16c037c5bbc7196e2ebb8d219cf
saisi <saisi@users.noreply.github.com>
2016-10-11 00:40:23 +0000

    1476146422 angaria

---
+++ 1476146422.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      521614281
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 3;
+   volatile uint32_t  f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[5] = {0L,0L,0L,0L,0L};
+static volatile int32_t g_5 = 0xC4D1454BL;/* VOLATILE GLOBAL g_5 */
