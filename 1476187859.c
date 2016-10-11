6eb47f8a96e86053a09c766c339a83d30c13ff0d
saisi <saisi@users.noreply.github.com>
2016-10-11 12:11:00 +0000

    1476187859 anatomy

---
+++ 1476187859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2736425826
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 31;
+   unsigned f1 : 24;
+   signed f2 : 26;
+   unsigned f3 : 30;
+   unsigned f4 : 21;
+   const signed f5 : 24;
+   volatile signed f6 : 22;
+   unsigned f7 : 19;
+   signed f8 : 11;
