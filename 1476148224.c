cbf8d00765d278ada0bf968c844d88f22c17d355
saisi <saisi@users.noreply.github.com>
2016-10-11 01:10:24 +0000

    1476148224 ammeters

---
+++ 1476148224.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4033945130
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int16_t  f1;
+   uint16_t  f2;
+   uint64_t  f3;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
