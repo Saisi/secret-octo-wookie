27f2854c44a17c2386faa5ef1b04e295a4f1f033
saisi <saisi@users.noreply.github.com>
2016-10-11 13:41:07 +0000

    1476193266 Amphrysian

---
+++ 1476193266.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1678313249
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 15;
+   const volatile signed f1 : 10;
+   unsigned f2 : 5;
+   signed f3 : 28;
+   signed f4 : 29;
+   unsigned f5 : 2;
+   unsigned f6 : 25;
