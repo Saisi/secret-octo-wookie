fec0311592b85a36acd60782720076325aacd473
saisi <saisi@users.noreply.github.com>
2016-10-11 19:11:24 +0000

    1476213084 acate

---
+++ 1476213084.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1545563245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 21;
+   const signed f1 : 24;
+   volatile signed f2 : 27;
+   volatile unsigned f3 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_19 = 0x5B14F4E7L;
+static int32_t g_24 = 0L;
