0a06dc20e7f1aa3b4029e0f385eef17e8bb7d961
saisi <saisi@users.noreply.github.com>
2016-10-11 17:41:20 +0000

    1476207680 Armoracia

---
+++ 1476207680.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1918882971
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U0  func_1(void);
+
