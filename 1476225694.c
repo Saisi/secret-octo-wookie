76629cf330dd07d60fccb351d45208e01fbe4972
saisi <saisi@users.noreply.github.com>
2016-10-11 22:41:35 +0000

    1476225694 argentose

---
+++ 1476225694.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2175276788
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 8;
+   signed f1 : 29;
+   volatile unsigned f2 : 11;
+   unsigned f3 : 11;
+   signed f4 : 14;
+   const signed f5 : 13;
+   signed f6 : 23;
