4b8d1d8f20a64a49333967601396af791e08975b
saisi <saisi@users.noreply.github.com>
2016-10-11 14:11:08 +0000

    1476195068 animation

---
+++ 1476195068.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4080240103
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_16 = 2UL;
+static uint32_t g_47 = 0xE35FE57BL;
+static volatile int16_t g_64 = 1L;/* VOLATILE GLOBAL g_64 */
+static int32_t g_66 = 0xBC92CEBBL;
+static int16_t g_69 = 0x15E3L;
+static uint16_t g_70 = 0x0EF1L;
+static uint8_t g_76 = 248UL;
+static uint32_t g_93 = 8UL;
+static uint32_t g_119 = 4294967292UL;
