2367b6c7064e8abbca9c26e74e25c0d4db34f8e7
saisi <saisi@users.noreply.github.com>
2016-10-11 06:10:38 +0000

    1476166238 aeroplanist

---
+++ 1476166238.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4090156606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 13;
+   unsigned f1 : 14;
+   const unsigned f2 : 24;
+   const signed f3 : 4;
+   const signed f4 : 3;
+   const volatile unsigned f5 : 14;
+   volatile signed f6 : 20;
