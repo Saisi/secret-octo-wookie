9e1f7a77231608f3bb3e7e3c6ae26022e44b1d73
saisi <saisi@users.noreply.github.com>
2016-10-11 13:11:05 +0000

    1476191465 alstonite

---
+++ 1476191465.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3320227696
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 6;
+   signed f1 : 3;
+   unsigned f2 : 25;
+   volatile unsigned f3 : 1;
+   volatile unsigned f4 : 12;
+   unsigned f5 : 19;
+   signed f6 : 5;
+};
+
