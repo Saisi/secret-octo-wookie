de3dfddb58ebb53e659ddd814f95b7f5ff277f54
saisi <saisi@users.noreply.github.com>
2016-10-11 20:11:27 +0000

    1476216686 AUDIX

---
+++ 1476216686.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1611098404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = (-2L);
+static volatile uint32_t g_14 = 18446744073709551613UL;/* VOLATILE GLOBAL g_14 */
+static int16_t g_17 = 0xDCBAL;
+static int32_t g_19 = 0L;
+static int32_t g_22 = (-1L);
