cd4a89a73f95c781afacba0d7109d371ec5e5c8d
saisi <saisi@users.noreply.github.com>
2016-10-11 04:10:32 +0000

    1476159032 balmoral

---
+++ 1476159032.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1397061334
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const int64_t  f1;
+   int8_t  f2;
+   volatile unsigned f3 : 16;
+   const volatile int32_t  f4;
+   int32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
