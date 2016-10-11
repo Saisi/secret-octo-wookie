f09b9b568d6aba9111c9241f7b8415916311baca
saisi <saisi@users.noreply.github.com>
2016-10-11 18:41:23 +0000

    1476211283 Balder

---
+++ 1476211283.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4195183413
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x2A43D953L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 1L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+static int32_t * func_6(int32_t * p_7);
+
+
