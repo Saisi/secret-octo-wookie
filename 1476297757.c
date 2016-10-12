b3329b98aa122557c613dc226850700468390929
saisi <saisi@users.noreply.github.com>
2016-10-12 18:42:38 +0000

    1476297757 anametadromous

---
+++ 1476297757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      396661757
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   int32_t  f1;
+   int64_t  f2;
+   const volatile int8_t  f3;
+};
+
+struct S1 {
+   const volatile uint32_t  f0;
+   int8_t  f1;
