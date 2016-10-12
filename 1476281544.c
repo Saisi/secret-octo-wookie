f5cb230d7cff45337e9be4f2c06b21de0a00cf4a
saisi <saisi@users.noreply.github.com>
2016-10-12 14:12:24 +0000

    1476281544 asthenobiotic

---
+++ 1476281544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1700538059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = 0L;
+static int32_t * const g_9 = &g_10;
+static uint8_t g_15 = 255UL;
+static uint32_t g_24 = 0x8EA5F34BL;
+static int32_t g_34 = 0x2C5D177CL;
