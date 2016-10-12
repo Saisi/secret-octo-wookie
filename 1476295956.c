532c03749c6601e4d1ba0af2582f0328be02f0d9
saisi <saisi@users.noreply.github.com>
2016-10-12 18:12:36 +0000

    1476295956 antiphilosophical

---
+++ 1476295956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2510500309
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 9;
+   signed f1 : 28;
+};
+#pragma pack(pop)
+
+union U1 {
+   const uint8_t  f0;
