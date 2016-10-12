a33c7823e83ee8481c522568c0c6c2ba0ee35142
saisi <saisi@users.noreply.github.com>
2016-10-12 03:11:51 +0000

    1476241911 antenatalitial

---
+++ 1476241911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2434211205
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   signed f1 : 20;
+   unsigned f2 : 7;
+   unsigned f3 : 22;
+   signed f4 : 15;
+   volatile uint32_t  f5;
+   signed : 0;
