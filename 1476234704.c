e148c154d0dd714eda1e7408ffecdad94bdafeb7
saisi <saisi@users.noreply.github.com>
2016-10-12 01:11:45 +0000

    1476234704 bacchanal

---
+++ 1476234704.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2227832017
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 7;
+   signed f1 : 2;
+   unsigned f2 : 29;
+   unsigned f3 : 20;
+   signed f4 : 21;
+   const signed f5 : 26;
+   unsigned f6 : 17;
+   signed : 0;
+   unsigned f7 : 3;
