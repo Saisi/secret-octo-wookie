fe56f32f4399013759da63e3aeaa7c320095451f
saisi <saisi@users.noreply.github.com>
2016-10-12 09:42:11 +0000

    1476265331 amphitene

---
+++ 1476265331.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1763328365
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 0x10E164CAL;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
