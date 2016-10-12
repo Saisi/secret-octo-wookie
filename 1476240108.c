c6012456fbd3ff306141c0464f5d1d43dbab4882
saisi <saisi@users.noreply.github.com>
2016-10-12 02:41:50 +0000

    1476240108 archeal

---
+++ 1476240108.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1827764987
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   unsigned f1 : 3;
+   const uint8_t  f2;
+   unsigned f3 : 11;
+   unsigned f4 : 6;
+   signed f5 : 28;
+   uint8_t  f6;
+};
+
