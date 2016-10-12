9ae80a29cb50d517141ab059827d28869d59b75b
saisi <saisi@users.noreply.github.com>
2016-10-12 12:42:20 +0000

    1476276140 accorporation

---
+++ 1476276140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2392816933
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 17;
+   const signed f1 : 4;
+   const int64_t  f2;
+   unsigned f3 : 24;
+   uint8_t  f4;
+   const unsigned f5 : 23;
+};
+
+/* --- GLOBAL VARIABLES --- */
