31f79130178af9386a5ce95464f03a80b83f34df
saisi <saisi@users.noreply.github.com>
2016-10-12 10:42:13 +0000

    1476268933 ammocoetid

---
+++ 1476268933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1902955250
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const uint64_t  f1;
+};
+
+union U1 {
+   int32_t  f0;
+   int32_t  f1;
+   volatile uint32_t  f2;
+   volatile uint32_t  f3;
