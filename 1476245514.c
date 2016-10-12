0d668ebffaae285f161eae7bb66a8c7b840980ea
saisi <saisi@users.noreply.github.com>
2016-10-12 04:11:55 +0000

    1476245514 affixers

---
+++ 1476245514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3729821595
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 12;
+   signed f1 : 19;
+   int16_t  f2;
+   int16_t  f3;
+   const signed f4 : 23;
+   volatile uint64_t  f5;
+   signed f6 : 18;
+};
+
