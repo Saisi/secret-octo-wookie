59d2f130b17efe4ca8955257610f394300c6b8a2
saisi <saisi@users.noreply.github.com>
2016-10-12 04:41:56 +0000

    1476247315 amitular

---
+++ 1476247315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2904844024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint64_t  f0;
+   uint64_t  f1;
+   int64_t  f2;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
