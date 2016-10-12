92d93ceb67e07d971ab9aa66e5b3500390e90489
saisi <saisi@users.noreply.github.com>
2016-10-12 07:42:06 +0000

    1476258125 ambari

---
+++ 1476258125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      204922451
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 8;
+   int8_t  f1;
+   uint8_t  f2;
+   int8_t * f3;
+};
+
+union U1 {
+   int8_t  f0;
+};
