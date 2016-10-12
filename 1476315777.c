dc92c842326dcf31d82745054e36da68c6c828e1
saisi <saisi@users.noreply.github.com>
2016-10-12 23:42:58 +0000

    1476315777 anlage

---
+++ 1476315777.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2394558559
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+   unsigned f1 : 13;
+   unsigned f2 : 28;
+   unsigned f3 : 23;
+   unsigned f4 : 12;
+};
+
+struct S1 {
+   signed f0 : 25;
