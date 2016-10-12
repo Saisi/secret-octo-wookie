ef18670ab56410042042fdbb510c558c8b2b6830
saisi <saisi@users.noreply.github.com>
2016-10-12 05:11:57 +0000

    1476249117 apolysis

---
+++ 1476249117.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      595460405
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 25;
+   signed f1 : 12;
+   volatile signed f2 : 31;
+   int16_t  f3;
+   volatile unsigned f4 : 7;
+   volatile signed f5 : 4;
+   unsigned f6 : 19;
+   unsigned f7 : 26;
+   const unsigned f8 : 21;
