2733562e5c78c2cf6c30be03c95f19101279cb62
saisi <saisi@users.noreply.github.com>
2016-10-12 20:42:47 +0000

    1476304964 aleuronic

---
+++ 1476304964.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4134833159
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   volatile int8_t * const  f1;
+};
+
+union U2 {
