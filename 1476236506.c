b275501ad781109a717c6160b2899e4802899f1c
saisi <saisi@users.noreply.github.com>
2016-10-12 01:41:46 +0000

    1476236506 balladmongering

---
+++ 1476236506.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1407938015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   signed f1 : 19;
+   signed f2 : 29;
+   signed f3 : 13;
+   signed : 0;
+   signed f4 : 14;
+   volatile uint32_t  f5;
+   volatile unsigned f6 : 21;
+   unsigned f7 : 10;
