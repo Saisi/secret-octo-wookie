f585a77e334344e005ce3aed4836179e3ca3e943
saisi <saisi@users.noreply.github.com>
2016-10-12 15:42:29 +0000

    1476286948 allocryptic

---
+++ 1476286948.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1292206134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-10L);
+static volatile int8_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile uint8_t g_6[8][3] = {{0x4BL,0x4BL,0x4BL},{0x38L,0x38L,0x38L},{0x4BL,0x4BL,0x4BL},{0x38L,0x38L,0x38L},{0x4BL,0x4BL,0x4BL},{0x38L,0x38L,0x38L},{0x4BL,0x4BL,0x4BL},{0x38L,0x38L,0x38L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
