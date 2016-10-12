4c42428748b805c71dba9127561aff739d1d213e
saisi <saisi@users.noreply.github.com>
2016-10-12 17:42:35 +0000

    1476294154 asporulate

---
+++ 1476294154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3758694432
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const uint64_t  f1;
+   const uint16_t  f2;
+   volatile unsigned f3 : 5;
+   volatile uint32_t  f4;
+   uint32_t  f5;
+   uint64_t  f6;
+};
+
