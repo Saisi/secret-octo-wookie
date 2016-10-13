e42a4cd0868369b60a4f4bf1b889635cbcc329be
saisi <saisi@users.noreply.github.com>
2016-10-13 05:43:18 +0000

    1476337397 anucleate

---
+++ 1476337397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3392087443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_8[10][1] = {{0xF1A0734FL},{0x292C5EB2L},{0xF1A0734FL},{0x292C5EB2L},{0xF1A0734FL},{0x292C5EB2L},{0xF1A0734FL},{0x292C5EB2L},{0xF1A0734FL},{0x292C5EB2L}};
+static uint16_t g_10 = 0x15AAL;
+static int16_t g_18 = 0x37E0L;
+static int32_t g_42[8] = {0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L};
+static const int32_t *g_41 = &g_42[6];
+static int32_t *g_69 = (void*)0;
+static int8_t g_82 = 8L;
+static int8_t g_83 = 0x3DL;
+static int8_t g_94 = 0L;
