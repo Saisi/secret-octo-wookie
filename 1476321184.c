6eacea37044e7921b5296071f5ddff4ae5f60af3
saisi <saisi@users.noreply.github.com>
2016-10-13 01:13:04 +0000

    1476321184 asseize

---
+++ 1476321184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4206613402
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x49FEBB67L;
+static int32_t g_4 = 0L;
+static uint8_t g_15 = 255UL;
+static int32_t g_29 = 0x1826FBD1L;
+static int32_t *g_28 = &g_29;
