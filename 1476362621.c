ce545b4c354758bd0f9f89b199e42042fc13b583
saisi <saisi@users.noreply.github.com>
2016-10-13 12:43:42 +0000

    1476362621 amphibole

---
+++ 1476362621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2737244247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint16_t  f1;
+   unsigned f2 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static uint32_t g_10 = 6UL;
+static uint64_t g_21[8] = {0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL,0x5B1510AFBD13D8C5LL};
