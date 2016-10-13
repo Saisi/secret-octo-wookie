04193759a58255deac2edce9a79c918c8dcfb91d
saisi <saisi@users.noreply.github.com>
2016-10-13 04:43:15 +0000

    1476333795 Babby

---
+++ 1476333795.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2692496368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-1L);
+static uint32_t g_7[5] = {4UL,4UL,4UL,4UL,4UL};
+static int32_t g_13 = 0L;
+static int32_t g_50 = 0x83DCDCBDL;
+static int32_t *g_49 = &g_50;
+static uint64_t g_69 = 0x76BF08EAF5359351LL;
+static int32_t ** volatile g_72[8] = {&g_49,&g_49,&g_49,&g_49,&g_49,&g_49,&g_49,&g_49};
+static int32_t *g_76 = &g_50;
+static int32_t g_82 = 0x4E2CD5B9L;
