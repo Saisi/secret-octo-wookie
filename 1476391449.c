c2029a5d1a3d2f6d91a984a731e6b9770dbe24b2
saisi <saisi@users.noreply.github.com>
2016-10-13 20:44:12 +0000

    1476391449 arborvitae

---
+++ 1476391449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2526590137
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   uint32_t  f1;
+   const uint16_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   int16_t  f0;
+   const uint8_t  f1;
