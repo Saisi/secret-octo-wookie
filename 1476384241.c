db84c20db9d90cefe1a821aae57ed57c20c03f97
saisi <saisi@users.noreply.github.com>
2016-10-13 18:44:01 +0000

    1476384241 Agonostomus

---
+++ 1476384241.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2227619745
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   unsigned f1 : 4;
+   volatile int16_t  f2;
+   volatile int32_t  f3;
+   uint32_t  f4;
+   const int8_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
