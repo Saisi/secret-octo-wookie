f4cdc765f9a2421208c5b26e35cd38f1abd4d4c2
saisi <saisi@users.noreply.github.com>
2016-10-13 08:13:28 +0000

    1476346407 asystolic

---
+++ 1476346407.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1538526660
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   const signed f1 : 5;
+   const int32_t  f2;
+   const signed f3 : 11;
+   const signed f4 : 4;
+   volatile unsigned : 0;
+   unsigned f5 : 1;
