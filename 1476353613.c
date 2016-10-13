c107c3ce5eaa78127dc258c66ac847e44f102c8c
saisi <saisi@users.noreply.github.com>
2016-10-13 10:13:33 +0000

    1476353613 Aeschynanthus

---
+++ 1476353613.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3863219301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_17 = 1UL;/* VOLATILE GLOBAL g_17 */
+static union U0 *g_24 = (void*)0;
+static uint32_t g_32 = 0x586FD5D7L;
+static int32_t g_34 = (-1L);
+static int32_t *g_33 = &g_34;
