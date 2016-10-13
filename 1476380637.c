5ac3f2f4f1f34eb9e34f0467b4c003b6db7a5f10
saisi <saisi@users.noreply.github.com>
2016-10-13 17:43:58 +0000

    1476380637 Amilcare

---
+++ 1476380637.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4292346115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_4 = 0xAEEF20FEL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_26 = 0xE819D338L;
+static uint64_t g_32 = 0x08877E06985BA561LL;
+static uint16_t g_40 = 0UL;
+static int32_t g_48 = 5L;
+static uint8_t g_61 = 0x29L;
+static int32_t g_78 = 0xA41183D9L;
+static int32_t g_80 = 0xF2D4E203L;
+static uint16_t g_92 = 65526UL;
