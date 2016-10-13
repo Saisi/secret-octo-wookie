c32d19ba560d47d356f60216775fa71fab6488ff
saisi <saisi@users.noreply.github.com>
2016-10-13 06:43:23 +0000

    1476341000 antipapistical

---
+++ 1476341000.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3883637251
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   int16_t  f1;
+   const signed f2 : 30;
+   volatile signed f3 : 17;
+   signed f4 : 30;
+};
+
+struct S1 {
+   uint32_t  f0;
