47647c4884e36d754327c9b61003fa9a25380992
saisi <saisi@users.noreply.github.com>
2016-10-13 03:43:13 +0000

    1476330192 barberfish

---
+++ 1476330192.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2795539672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+   unsigned f1 : 7;
+   unsigned f2 : 13;
+   int64_t  f3;
+   signed f4 : 22;
+   signed f5 : 10;
+   signed f6 : 3;
+   signed f7 : 16;
+   const volatile unsigned f8 : 1;
