9a200435ab8b33f79bdfe42f397b7f9811b3f4f9
saisi <saisi@users.noreply.github.com>
2016-10-13 22:14:16 +0000

    1476396856 Ausgleiche

---
+++ 1476396856.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3685051342
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_9 = 18446744073709551606UL;
+static int32_t g_39 = 0L;
+static volatile int8_t g_51 = 1L;/* VOLATILE GLOBAL g_51 */
+static int32_t g_92[2][9] = {{0L,0L,0L,0L,0L,0L,0L,0L,0L},{0xEFA4ED9EL,0x1D1BA057L,0xEFA4ED9EL,0x1D1BA057L,0xEFA4ED9EL,0x1D1BA057L,0xEFA4ED9EL,0x1D1BA057L,0xEFA4ED9EL}};
+static int32_t *g_94[4] = {&g_92[1][6],&g_92[1][6],&g_92[1][6],&g_92[1][6]};
+static int32_t ** volatile g_93 = &g_94[3];/* VOLATILE GLOBAL g_93 */
+static int32_t * volatile g_99 = (void*)0;/* VOLATILE GLOBAL g_99 */
+static uint32_t g_105[2][2] = {{0x3E3B667DL,0x3E3B667DL},{0x3E3B667DL,0x3E3B667DL}};
+static const int32_t * const *g_138 = (void*)0;
