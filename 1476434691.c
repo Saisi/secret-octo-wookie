68dfd13ac4fb9caf2acd2bbe2381e2f0089e2cde
saisi <saisi@users.noreply.github.com>
2016-10-14 08:44:51 +0000

    1476434691 Alectrionidae

---
+++ 1476434691.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2043110982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   const volatile uint32_t  f1;
+   unsigned f2 : 9;
+   int32_t  f3;
+};
+#pragma pack(pop)
+
