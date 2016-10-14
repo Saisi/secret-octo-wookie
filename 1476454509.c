3fadcc229bb709f4cdbd9d19a858078e098fbcdd
saisi <saisi@users.noreply.github.com>
2016-10-14 14:15:10 +0000

    1476454509 aventine

---
+++ 1476454509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      661764928
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * volatile  f0;
+   volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 0UL;
+static int8_t g_26 = 0L;
+static uint16_t g_45 = 65529UL;
+static int32_t g_63 = 0x1450187EL;
