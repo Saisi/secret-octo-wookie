e0d68e5ed6ae8a1578f990e143b33a3e445b8e79
saisi <saisi@users.noreply.github.com>
2016-10-14 11:44:59 +0000

    1476445499 angelizing

---
+++ 1476445499.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4132922757
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   signed f1 : 26;
+   int16_t  f2;
+   int8_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   uint16_t  f1;
