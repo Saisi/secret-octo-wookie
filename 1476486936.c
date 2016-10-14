63234dc3a01070425a8ba2c54f7e017b39a0fd96
saisi <saisi@users.noreply.github.com>
2016-10-14 23:15:37 +0000

    1476486936 Babol

---
+++ 1476486936.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      272380915
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+};
+
+struct S1 {
+   volatile int32_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+   int32_t  f3;
+   signed f4 : 23;
