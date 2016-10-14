3a990bf51996c26f8888ce9526e8b04a034e6cce
saisi <saisi@users.noreply.github.com>
2016-10-14 04:44:40 +0000

    1476420279 atmosphereful

---
+++ 1476420279.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2532612271
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[9] = {0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL,0xD5464B5FL};
+static int32_t g_3 = 6L;
+static int32_t g_6 = 7L;
+static int32_t g_10 = 0xA5FEF9D7L;
+static uint32_t g_71 = 1UL;
+static volatile int32_t g_87 = (-1L);/* VOLATILE GLOBAL g_87 */
+static volatile int32_t g_88 = 0L;/* VOLATILE GLOBAL g_88 */
+static volatile int32_t *g_86[8] = {&g_88,(void*)0,&g_88,(void*)0,&g_88,(void*)0,&g_88,(void*)0};
+static volatile int32_t **g_85 = &g_86[1];
