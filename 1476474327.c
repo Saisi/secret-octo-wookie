f541102656b4f22639c414274b48952f0c3af614
saisi <saisi@users.noreply.github.com>
2016-10-14 19:45:28 +0000

    1476474327 adient

---
+++ 1476474327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      117699909
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   signed f1 : 20;
+   const unsigned f2 : 7;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11 = 0x53E58BFCL;
+static int32_t *g_15[1][5][2] = {{{&g_11,&g_11},{&g_11,&g_11},{&g_11,&g_11},{&g_11,&g_11},{&g_11,&g_11}}};
