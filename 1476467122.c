44ba87fa3b7ad145fe3e0df50f67b5ee2b1256ec
saisi <saisi@users.noreply.github.com>
2016-10-14 17:45:22 +0000

    1476467122 abiotic

---
+++ 1476467122.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3424485883
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 28;
+   volatile signed f1 : 3;
+   volatile signed f2 : 26;
+   const unsigned f3 : 29;
+   signed f4 : 26;
+   unsigned f5 : 11;
+   const signed f6 : 30;
