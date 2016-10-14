ae7edf89ccc22b648559d0796bc39a379c55bc60
saisi <saisi@users.noreply.github.com>
2016-10-14 07:14:46 +0000

    1476429286 antipredeterminant

---
+++ 1476429286.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1670536325
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   unsigned f1 : 20;
+   signed f2 : 7;
+   unsigned f3 : 22;
+};
+#pragma pack(pop)
+
