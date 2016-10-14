d34324dd62716726771997af6ee9e55035fd9265
saisi <saisi@users.noreply.github.com>
2016-10-14 05:14:41 +0000

    1476422081 artcraft

---
+++ 1476422081.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1585872032
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_5 = (-5L);/* VOLATILE GLOBAL g_5 */
+static int32_t g_7 = (-10L);
+static int32_t g_10 = 0xBCABEBF3L;
+static int32_t *g_12 = &g_10;
+static int32_t ** volatile g_11 = &g_12;/* VOLATILE GLOBAL g_11 */
+static uint16_t g_16[3][9] = {{0UL,0x1606L,0x1606L,0UL,0UL,0x1606L,0x1606L,0UL,0UL},{0x0B28L,0x9276L,0x0B28L,0x9276L,0x0B28L,0x9276L,0x0B28L,0x9276L,0x0B28L},{0UL,0UL,0x1606L,0x1606L,0UL,0UL,0x1606L,0x1606L,0UL}};
+static uint16_t g_23[1] = {1UL};
+static uint16_t *g_22[10] = {&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0],&g_23[0]};
+static volatile int32_t g_33 = 0x22E7DFE9L;/* VOLATILE GLOBAL g_33 */
