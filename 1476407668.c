1cab4edeebcc86d6aa589a2740dc76d722c5304f
saisi <saisi@users.noreply.github.com>
2016-10-14 01:14:28 +0000

    1476407668 alopecias

---
+++ 1476407668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2422889113
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 11;
+   signed f1 : 22;
+   signed f2 : 1;
+   unsigned f3 : 8;
+   unsigned f4 : 25;
+   unsigned f5 : 14;
+   const volatile signed f6 : 21;
