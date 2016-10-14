dc30facdfed3a1e01a017d56b8231ffe39a2ca82
saisi <saisi@users.noreply.github.com>
2016-10-14 06:44:45 +0000

    1476427485 atabeg

---
+++ 1476427485.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3452096077
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   volatile int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 4294967290UL;
+static int32_t g_6 = 1L;
+static int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static int32_t *g_12 = &g_6;
