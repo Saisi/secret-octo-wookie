5a716ca3cf463874e884c569ceaa315b0e548375
saisi <saisi@users.noreply.github.com>
2016-10-14 03:44:36 +0000

    1476416675 aumbries

---
+++ 1476416675.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3280115195
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   const volatile int8_t  f1;
+   const unsigned f2 : 26;
+};
+
+union U1 {
+   uint32_t  f0;
+   uint16_t  f1;
+   uint32_t  f2;
