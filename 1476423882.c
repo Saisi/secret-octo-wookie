2c5d5287995492fec569750eba62d9d93b4d8649
saisi <saisi@users.noreply.github.com>
2016-10-14 05:44:42 +0000

    1476423882 baggyrinkle

---
+++ 1476423882.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3328749115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 28;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 9L;
+static uint8_t g_48 = 0x8DL;
+static int32_t *g_57 = &g_3;
+static int32_t ** volatile g_56 = &g_57;/* VOLATILE GLOBAL g_56 */
