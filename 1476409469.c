074a7e39b7539a51848304e5977f04cfe2b0ae36
saisi <saisi@users.noreply.github.com>
2016-10-14 01:44:30 +0000

    1476409469 babesias

---
+++ 1476409469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4287529893
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   signed f1 : 4;
+   signed f2 : 23;
+   unsigned f3 : 6;
+};
+#pragma pack(pop)
+
