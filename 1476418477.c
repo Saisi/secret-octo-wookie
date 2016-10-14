40b6bd8f930de3281153357861e40fedd9873cd9
saisi <saisi@users.noreply.github.com>
2016-10-14 04:14:38 +0000

    1476418477 augurship

---
+++ 1476418477.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1781507166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 29;
+   unsigned f1 : 30;
+   volatile signed f2 : 8;
+   signed f3 : 20;
+   signed f4 : 19;
+   volatile unsigned f5 : 30;
+};
+
+#pragma pack(push)
