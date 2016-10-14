115901a505c3afe3bb7cad0bdfcfb53ff3810a9f
saisi <saisi@users.noreply.github.com>
2016-10-14 08:14:49 +0000

    1476432889 aster

---
+++ 1476432889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2414743601
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 23;
+   const signed f1 : 18;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile int8_t g_8 = 0x9BL;/* VOLATILE GLOBAL g_8 */
