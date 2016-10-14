5205270a692cb77f11db71315b6d6e1ba0fff5b4
saisi <saisi@users.noreply.github.com>
2016-10-14 13:15:06 +0000

    1476450906 anteambulation

---
+++ 1476450906.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1279416512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * f0;
+   const int32_t  f1;
+   unsigned f2 : 18;
+};
+
+union U1 {
+   volatile int16_t  f0;
+};
+
