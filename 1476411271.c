976440a332592c957d7d91dec5320d8006820eb0
saisi <saisi@users.noreply.github.com>
2016-10-14 02:14:31 +0000

    1476411271 ABPC

---
+++ 1476411271.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2445778411
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * const  f0;
+   int16_t  f1;
+   uint64_t  f2;
+   volatile uint8_t  f3;
+};
+
+union U1 {
+   uint64_t  f0;
+   const int16_t  f1;
