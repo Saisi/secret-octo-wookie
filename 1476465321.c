1e28e38b64054fbab1560de04b61d2467fa2b075
saisi <saisi@users.noreply.github.com>
2016-10-14 17:15:21 +0000

    1476465321 androcentric

---
+++ 1476465321.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1237340798
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_5 = 4294967289UL;
+static uint32_t g_14[10] = {0xF0720718L,0x1B583DC0L,0x747BB7ABL,0x747BB7ABL,0x1B583DC0L,0xF0720718L,0x1B583DC0L,0x747BB7ABL,0x747BB7ABL,0x1B583DC0L};
+static int32_t g_21 = 0L;
+static uint8_t g_24[2] = {0UL,0UL};
+static int8_t g_26 = 5L;
+static int64_t g_41[2][3] = {{9L,0xCB407CB0BE945D44LL,9L},{9L,0xCB407CB0BE945D44LL,9L}};
+static int64_t *g_42 = &g_41[1][0];
+static uint16_t g_70 = 65529UL;
+static int64_t g_74 = (-7L);
