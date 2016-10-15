eae5d42c10dffa5907c309c656d3454372fe313f
saisi <saisi@users.noreply.github.com>
2016-10-15 08:46:11 +0000

    1476521170 arachnitis

---
+++ 1476521170.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2952577407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const unsigned f1 : 29;
+};
+
+union U1 {
+   const int64_t  f0;
+   uint32_t  f1;
+   volatile int32_t  f2;
+};
