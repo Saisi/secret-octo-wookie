249c5384f71d3b189500b4ea91a2a730e76342ed
saisi <saisi@users.noreply.github.com>
2016-10-15 19:46:43 +0000

    1476560803 adjuvate

---
+++ 1476560803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      903029405
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1] = {(-1L)};
+static int32_t g_5[8] = {0xA9869EC4L,0xA9869EC4L,0xA9869EC4L,0xA9869EC4L,0xA9869EC4L,0xA9869EC4L,0xA9869EC4L,0xA9869EC4L};
+static int32_t * volatile g_4 = &g_5[2];/* VOLATILE GLOBAL g_4 */
+static int32_t g_6 = 0x85A1FA14L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
