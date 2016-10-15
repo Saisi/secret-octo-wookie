0535da2069d49b1e83ccbbc2abc80cc4abef6ebd
saisi <saisi@users.noreply.github.com>
2016-10-15 00:15:41 +0000

    1476490540 acridyl

---
+++ 1476490540.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2756145001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_34 = 0x943E2F7CL;
+static uint8_t g_49 = 0xF7L;
+static const union U0 g_52 = {0x50B7L};/* VOLATILE GLOBAL g_52 */
+static int32_t g_54 = 0L;
+static int32_t *g_53[10] = {&g_54,&g_54,&g_54,&g_54,&g_54,&g_54,&g_54,&g_54,&g_54,&g_54};
