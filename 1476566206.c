b3fad41af6250862eecbd2ceeb0cd3b719688d1d
saisi <saisi@users.noreply.github.com>
2016-10-15 21:16:47 +0000

    1476566206 antefebrile

---
+++ 1476566206.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2781949630
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t * f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7[5] = {0x526D2E85L,0x526D2E85L,0x526D2E85L,0x526D2E85L,0x526D2E85L};
+static int32_t g_22[2][2][9] = {{{0xA2560131L,0xBE945D44L,0x92F9E84EL,0x92F9E84EL,0xBE945D44L,0xA2560131L,0x133ECB40L,0xA2560131L,0xBE945D44L},{(-9L),0x5678D568L,0x5678D568L,(-9L),(-1L),(-6L),(-1L),(-9L),0x5678D568L}},{{0x333DB962L,0x333DB962L,0x133ECB40L,0xBE945D44L,0x3A4B4404L,0xBE945D44L,0x133ECB40L,0x333DB962L,0x333DB962L},{0x5678D568L,(-9L),(-1L),(-6L),(-1L),(-9L),0x5678D568L,0x5678D568L,(-9L)}}};
+static volatile int8_t g_29 = 0x31L;/* VOLATILE GLOBAL g_29 */
