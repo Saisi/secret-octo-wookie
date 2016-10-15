ea413bdb265de0789f1876ca6ae85c3e90541c56
saisi <saisi@users.noreply.github.com>
2016-10-15 09:16:13 +0000

    1476522972 Anisopoda

---
+++ 1476522972.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2786047039
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static uint8_t g_17 = 250UL;
+static uint8_t g_19 = 255UL;
+static int32_t g_25 = 0x189F42C6L;
+static uint8_t g_49 = 0UL;
+static uint8_t *g_48 = &g_49;
+static uint8_t * const *g_75[6] = {&g_48,&g_48,&g_48,&g_48,&g_48,&g_48};
+static uint8_t * const **g_74[8][1] = {{(void*)0},{(void*)0},{(void*)0},{(void*)0},{(void*)0},{(void*)0},{(void*)0},{(void*)0}};
+static int32_t g_107 = 0x6F3C4E31L;
