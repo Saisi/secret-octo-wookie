f4da1aca50c8ba4daaf63885836cb0320853e168
saisi <saisi@users.noreply.github.com>
2016-10-15 11:46:20 +0000

    1476531979 ajaja

---
+++ 1476531979.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3169147061
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 25;
+   int32_t  f1;
+   signed f2 : 31;
+   const unsigned f3 : 14;
+   unsigned f4 : 7;
+   signed f5 : 4;
+   volatile signed f6 : 19;
+   const unsigned f7 : 26;
+   const signed f8 : 21;
