3c47bba0bf309e22caf18c354e81c92d4dd4486a
saisi <saisi@users.noreply.github.com>
2016-10-15 12:46:23 +0000

    1476535583 Altaic

---
+++ 1476535583.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1543900006
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0x89L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0x7A4A12B5L;
+static int64_t g_5[9] = {0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL,0xD4914D2EDC7555B7LL};
+static volatile int32_t g_6 = 0x8872FA4AL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static int32_t g_8[6] = {0x50B5F0B1L,0x9A695768L,0x50B5F0B1L,0x50B5F0B1L,0x9A695768L,0x50B5F0B1L};
+static int32_t g_9 = 0xC6F8B8AEL;
+static int32_t g_30[2] = {0L,0L};
+static int32_t *g_29 = &g_30[0];
