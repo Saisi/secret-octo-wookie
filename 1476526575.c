85538482d20bf4081c12b7dd4fa032e3b5b7bdac
saisi <saisi@users.noreply.github.com>
2016-10-15 10:16:16 +0000

    1476526575 amontillado

---
+++ 1476526575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      376757115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static uint8_t g_8 = 0x6DL;
+static volatile int32_t g_13[5][5][2] = {{{(-5L),(-6L)},{1L,0xBC4AEE50L},{(-6L),(-1L)},{4L,(-5L)},{0xFF446CD5L,2L}},{{2L,0x26128F3BL},{0x6FA66FA4L,0x8451D77FL},{0x612D29EDL,(-5L)},{0x5B3659EAL,0xEC46BC22L},{(-5L),(-5L)}},{{0x26128F3BL,(-10L)},{0xEC46BC22L,(-10L)},{0x26128F3BL,(-5L)},{(-5L),0xEC46BC22L},{0x5B3659EAL,(-5L)}},{{0x612D29EDL,0x8451D77FL},{0x6FA66FA4L,0x26128F3BL},{2L,2L},{0xFF446CD5L,(-5L)},{4L,(-1L)}},{{(-6L),0xBC4AEE50L},{1L,(-6L)},{(-5L),0x7245C2F1L},{0xFF446CD5L,0x5B3659EAL},{0x031C36B5L,(-6L)}}};
+static volatile int32_t g_14 = (-9L);/* VOLATILE GLOBAL g_14 */
+static volatile int32_t g_15 = 4L;/* VOLATILE GLOBAL g_15 */
