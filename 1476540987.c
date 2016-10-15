f8b61a54573f6bf78f758d0923e77b68ac2f0ff4
saisi <saisi@users.noreply.github.com>
2016-10-15 14:16:27 +0000

    1476540987 algodon

---
+++ 1476540987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      597320111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x4C506D44L;
+static int32_t * volatile g_5 = (void*)0;/* VOLATILE GLOBAL g_5 */
+static uint8_t g_6 = 2UL;
+static int16_t g_12 = 0x5925L;
+static volatile int32_t g_27 = 0xB8CEFFB9L;/* VOLATILE GLOBAL g_27 */
+static int32_t g_28 = 0x0E779066L;
+static int16_t g_60 = 0L;
+static int32_t g_72 = 9L;
+static uint8_t g_97 = 1UL;
