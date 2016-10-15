815fea453a92d27c887ea4dc555da50729d97919
saisi <saisi@users.noreply.github.com>
2016-10-15 11:16:18 +0000

    1476530178 armadilla

---
+++ 1476530178.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      669271488
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   const int32_t  f1;
+   volatile uint32_t  f2;
+};
+
+union U1 {
+   signed f0 : 29;
+};
+
