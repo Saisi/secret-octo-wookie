c397d0bd5ac888bc738a3cd59401fe8beabb3cbe
saisi <saisi@users.noreply.github.com>
2016-10-16 12:47:41 +0000

    1476622060 actuary

---
+++ 1476622060.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      937735550
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   int16_t  f1;
+   const volatile uint8_t  f2;
+   int32_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = 0xA983111738AB659DLL;/* VOLATILE GLOBAL g_2 */
