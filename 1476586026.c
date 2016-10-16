eca64fdc102cca14de0c83d761a2dcd34c3eeeb1
saisi <saisi@users.noreply.github.com>
2016-10-16 02:47:08 +0000

    1476586026 archeologian

---
+++ 1476586026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2464822979
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 7;
+   signed f1 : 1;
+   unsigned f2 : 24;
+   signed f3 : 15;
+   volatile uint64_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
