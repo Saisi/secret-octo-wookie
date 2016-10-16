d8e30610af143fb5ed1954a75dc13d2407188d09
saisi <saisi@users.noreply.github.com>
2016-10-16 03:17:09 +0000

    1476587829 archpriestship

---
+++ 1476587829.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4129802910
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x3AE35568L;
+static int32_t g_5 = 0L;
+static uint64_t g_24[5] = {0x4F78D7CBAE8B0121LL,0x4F78D7CBAE8B0121LL,0x4F78D7CBAE8B0121LL,0x4F78D7CBAE8B0121LL,0x4F78D7CBAE8B0121LL};
+static uint64_t g_37 = 0xD20A2E71CC545E21LL;
+static int32_t *g_65 = &g_5;
+static int32_t **g_64 = &g_65;
+static uint32_t g_108 = 4294967286UL;
+static int32_t g_130 = 0xBB11E403L;
+static int32_t * const  volatile g_129 = &g_130;/* VOLATILE GLOBAL g_129 */
