d7654f7e53695ead1c46b80eb9a389ca87093917
saisi <saisi@users.noreply.github.com>
2016-10-16 03:47:10 +0000

    1476589630 anodontia

---
+++ 1476589630.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1536199534
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   int16_t  f1;
+   uint16_t  f2;
+   const volatile uint64_t  f3;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_7[9] = {0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L,0xE8E1L};
