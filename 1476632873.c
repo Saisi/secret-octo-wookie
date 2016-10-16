e14751ca0105004214dfed751d6a544d2b889336
saisi <saisi@users.noreply.github.com>
2016-10-16 15:47:54 +0000

    1476632873 agacant

---
+++ 1476632873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1962273155
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 3;
+   const volatile unsigned f1 : 1;
+   unsigned f2 : 24;
+   signed f3 : 15;
+   unsigned f4 : 14;
+   const volatile unsigned f5 : 30;
+   signed f6 : 20;
+};
+
