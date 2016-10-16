9fc57ef506a2626d48a1662aa902d48af2dcdc8d
saisi <saisi@users.noreply.github.com>
2016-10-16 06:47:20 +0000

    1476600439 allopathies

---
+++ 1476600439.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1131339327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int64_t  f1;
+   const volatile uint32_t  f2;
+   int8_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   const int32_t  f1;
