9da15d390b280ddd4fbf1f78b5127123d5006b6a
saisi <saisi@users.noreply.github.com>
2016-10-16 01:47:03 +0000

    1476582422 anovulant

---
+++ 1476582422.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3925268814
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint32_t  f0;
+   const signed f1 : 18;
+   volatile int32_t  f2;
+   int16_t  f3;
+   int8_t  f4;
+   volatile unsigned f5 : 14;
+   volatile uint64_t  f6;
+   uint64_t  f7;
+   volatile int16_t  f8;
