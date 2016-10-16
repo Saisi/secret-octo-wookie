6305c444c9ce02a794e7d22cb8166078ea2c3519
saisi <saisi@users.noreply.github.com>
2016-10-16 23:18:18 +0000

    1476659897 andamentos

---
+++ 1476659897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1527320040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   const unsigned f1 : 15;
+   const uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[8] = {0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L,0x7DF2F4A8L};
+static volatile int32_t g_3 = (-8L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xF72D4EA6L;/* VOLATILE GLOBAL g_4 */
