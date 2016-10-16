7472913b462022b8fa9f8fbbfb47abec311763ac
saisi <saisi@users.noreply.github.com>
2016-10-16 11:47:38 +0000

    1476618458 azathioprine

---
+++ 1476618458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2020031441
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2 = 0x8EL;
+static int32_t g_4 = (-1L);
+static int8_t g_5 = 0xE1L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
