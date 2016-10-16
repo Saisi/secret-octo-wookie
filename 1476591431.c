709fc38bc7ea950126984c39df5244a66b317c59
saisi <saisi@users.noreply.github.com>
2016-10-16 04:17:11 +0000

    1476591431 amidines

---
+++ 1476591431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      152954237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   volatile int32_t  f1;
+   int8_t  f2;
+   volatile int16_t  f3;
+};
+
+struct S1 {
+   struct S0  f0;
+   const int32_t  f1;
