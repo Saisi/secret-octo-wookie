a83158dc9543be0630aa19a2012a963d68bf7fac
saisi <saisi@users.noreply.github.com>
2016-10-16 07:47:22 +0000

    1476604042 Atacamenan

---
+++ 1476604042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2758274913
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   signed f1 : 10;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_5 = 0x782A1836L;
