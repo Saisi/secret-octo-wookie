3efeda1bb418983852bec6ad2f082c241c415873
saisi <saisi@users.noreply.github.com>
2016-10-16 10:47:35 +0000

    1476614855 antiracer

---
+++ 1476614855.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      512951855
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile uint16_t  f1;
+   volatile uint32_t  f2;
+   volatile unsigned f3 : 31;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 8L;/* VOLATILE GLOBAL g_2 */
