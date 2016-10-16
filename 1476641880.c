a2fc79219a9c27b344c14e4a79a20f5d4e226731
saisi <saisi@users.noreply.github.com>
2016-10-16 18:18:00 +0000

    1476641880 Albruna

---
+++ 1476641880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1094588346
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_9 = 0L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_11 = 0x579253A3L;
+static int32_t * volatile g_10 = &g_11;/* VOLATILE GLOBAL g_10 */
+static int32_t g_16 = (-5L);
+static int32_t * volatile g_15 = &g_16;/* VOLATILE GLOBAL g_15 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
