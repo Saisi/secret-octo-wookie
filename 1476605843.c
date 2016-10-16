1b0b807fb80cc96717af50b4cc936fcfb425ff27
saisi <saisi@users.noreply.github.com>
2016-10-16 08:17:23 +0000

    1476605843 anthroposociology

---
+++ 1476605843.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      848637511
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   unsigned f1 : 7;
+   volatile int32_t  f2;
+};
+
+union U1 {
+   volatile int8_t * volatile  f0;
+   const volatile signed f1 : 25;
+   const uint64_t  f2;
