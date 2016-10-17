a5667fb53b214d21a9952edcc869a382c88ef32d
saisi <saisi@users.noreply.github.com>
2016-10-17 18:19:26 +0000

    1476728365 adnex

---
+++ 1476728365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3269425794
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 15;
+   volatile signed f1 : 8;
+   unsigned f2 : 10;
+   signed f3 : 14;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   volatile uint16_t  f1;
