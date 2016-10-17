5c7f81e4782dad54bfd801e82bc10451b6f76cb5
saisi <saisi@users.noreply.github.com>
2016-10-17 03:18:35 +0000

    1476674315 appledrone

---
+++ 1476674315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2813421287
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   int32_t  f1;
+   volatile unsigned f2 : 25;
+   volatile signed f3 : 23;
+   int16_t  f4;
+   int16_t  f5;
+   uint64_t  f6;
