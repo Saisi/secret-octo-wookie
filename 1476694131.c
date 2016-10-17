e35ffd75f40acb7f3289c57b5cca5e5e56645f98
saisi <saisi@users.noreply.github.com>
2016-10-17 08:48:53 +0000

    1476694131 angledozer

---
+++ 1476694131.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      42861217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   unsigned f2 : 22;
+   volatile uint8_t  f3;
+   uint32_t  f4;
+   signed f5 : 1;
+   int64_t  f6;
