9c7df52dd5f85ddbfe5fc2f0c91ebc1d4b8e4db1
saisi <saisi@users.noreply.github.com>
2016-10-17 20:19:32 +0000

    1476735571 Artesia

---
+++ 1476735571.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1191917228
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * volatile  f0;
+   volatile uint16_t  f1;
+   uint32_t  f2;
+   int64_t  f3;
+   volatile uint32_t  f4;
+};
+
+union U1 {
+   volatile int8_t * const  f0;
