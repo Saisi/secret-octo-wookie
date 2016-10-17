6fa0b265c7011da6652d4e0fd4fd18047b742d57
saisi <saisi@users.noreply.github.com>
2016-10-17 21:49:42 +0000

    1476740981 abstractiveness

---
+++ 1476740981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1091587407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_17 = 0x5769E8A2L;
+static int32_t g_56[4] = {0L,0L,0L,0L};
+static int32_t g_74 = 1L;
+static uint8_t g_76 = 0xABL;
+static int16_t g_89 = 1L;
