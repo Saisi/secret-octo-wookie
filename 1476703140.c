0a02f5392c3eaea3be5c3fd6bff9922c8bdf91d0
saisi <saisi@users.noreply.github.com>
2016-10-17 11:19:00 +0000

    1476703140 algaesthesia

---
+++ 1476703140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3826695137
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_10[1] = {0x17BE9FB1C03809D0LL};
+static uint16_t g_13 = 0xB000L;
+static uint16_t g_36 = 0xBD53L;
+static int32_t g_53 = 0x523830AEL;
+static volatile uint32_t g_73 = 0x1EC3D250L;/* VOLATILE GLOBAL g_73 */
+static uint16_t g_89 = 0UL;
+static volatile int32_t g_96 = (-1L);/* VOLATILE GLOBAL g_96 */
+static int8_t g_97 = 0x34L;
+static volatile int64_t g_98 = (-7L);/* VOLATILE GLOBAL g_98 */
