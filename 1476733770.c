47fac30fcb89d4cdccac652838e80a4e890495bd
saisi <saisi@users.noreply.github.com>
2016-10-17 19:49:30 +0000

    1476733770 algidity

---
+++ 1476733770.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2986355239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 18;
+   volatile uint64_t  f1;
+   signed f2 : 17;
+   volatile int16_t  f3;
+   const uint16_t  f4;
+   int32_t  f5;
+   uint32_t  f6;
+   volatile int16_t  f7;
+   uint64_t  f8;
