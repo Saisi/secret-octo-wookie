2d3c4421e652b521bc77f9d206a89e385991fb2b
saisi <saisi@users.noreply.github.com>
2016-10-17 02:18:32 +0000

    1476670711 attainted

---
+++ 1476670711.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1041706823
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 0x46L;
+static int8_t *g_8 = &g_4;
+static int8_t **g_7 = &g_8;
+static int8_t *** volatile g_6[1] = {&g_7};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
