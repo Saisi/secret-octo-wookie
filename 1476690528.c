e9ed7cc5a08dbb5df54b3c8135f44516fdb7ca9f
saisi <saisi@users.noreply.github.com>
2016-10-17 07:48:49 +0000

    1476690528 agitators

---
+++ 1476690528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3059542016
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 14;
+   int32_t  f1;
+   volatile int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_12 = 0L;
+static const int8_t *g_11 = &g_12;
+static int8_t g_20[8][9] = {{1L,(-2L),(-2L),1L,0x09L,0x7EL,0x09L,1L,(-2L)},{0x11L,0x11L,3L,0x0CL,0xA3L,0x0CL,3L,0x11L,0x11L},{(-2L),1L,0x09L,0x7EL,0x09L,1L,(-2L),(-2L),1L},{0x0CL,0xE4L,3L,0xE4L,0x0CL,0x3CL,0x3CL,0x0CL,0xE4L},{(-2L),0x09L,(-2L),1L,1L,1L,1L,(-2L),0x09L},{0x11L,0x0CL,3L,0x11L,0x11L,3L,0x0CL,0xA3L,0x0CL},{(-2L),1L,1L,1L,1L,(-2L),0x09L,(-2L),1L},{0x39L,0x0CL,0x0CL,0x39L,0xA3L,0x3CL,0xA3L,0x39L,0x0CL}};
