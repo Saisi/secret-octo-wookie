5a9d19bf235d6088efaa26256ab86946d6aa6e44
saisi <saisi@users.noreply.github.com>
2016-10-17 13:19:08 +0000

    1476710348 anastigmat

---
+++ 1476710348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2642127669
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 25;
+   volatile signed f1 : 12;
+   unsigned f2 : 31;
+   unsigned f3 : 14;
+   volatile uint32_t  f4;
+   const unsigned f5 : 25;
+   signed f6 : 24;
+   unsigned f7 : 18;
+};
