dcf8fa88cd672a679338d9d6ae4e47627b8a006e
saisi <saisi@users.noreply.github.com>
2016-10-17 18:49:27 +0000

    1476730167 afterlife

---
+++ 1476730167.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1558859203
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 28;
+   volatile signed f1 : 1;
+   volatile unsigned f2 : 24;
+};
+
+union U1 {
+   int32_t  f0;
+   int8_t  f1;
+   const int32_t  f2;
