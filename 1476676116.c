db59559b4b6cec881ab470c4029a0e51b231d850
saisi <saisi@users.noreply.github.com>
2016-10-17 03:48:36 +0000

    1476676116 appoggiaturas

---
+++ 1476676116.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1533213036
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 4;
+   volatile unsigned f1 : 9;
+   unsigned f2 : 7;
+   signed f3 : 23;
+   const uint16_t  f4;
+   signed f5 : 20;
+   unsigned f6 : 5;
+   unsigned f7 : 10;
+   signed f8 : 19;
