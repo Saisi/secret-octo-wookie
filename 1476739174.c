4c2c75acdd9416d93b4c8b71e89bf618e5a3148f
saisi <saisi@users.noreply.github.com>
2016-10-17 21:19:40 +0000

    1476739174 azophosphore

---
+++ 1476739174.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3310415582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 29;
+   volatile signed f1 : 30;
+   volatile int32_t  f2;
+};
+
+union U1 {
+   const int8_t  f0;
+   int16_t  f1;
+   int16_t  f2;
