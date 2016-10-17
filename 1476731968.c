a902512d679bcaf0b5c7d348a23dd92f7aea40d3
saisi <saisi@users.noreply.github.com>
2016-10-17 19:19:29 +0000

    1476731968 baptistries

---
+++ 1476731968.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      580506542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xE0BCD8C4L;
+static int32_t g_16 = 0x41720348L;
+static volatile uint16_t g_17[4] = {1UL,1UL,1UL,1UL};
+static int32_t **g_56 = (void*)0;
+static uint16_t g_65 = 5UL;
+static int32_t *g_78 = &g_3;
+static int32_t **g_77 = &g_78;
+static uint32_t g_89 = 0x189EFA4EL;
+static uint32_t g_136[4][6] = {{0xF4D98BCDL,0xF5DB90C3L,0xF4D98BCDL,0x225C50F4L,4294967295UL,4294967295UL},{4294967292UL,0UL,0x21EF5C25L,0xF4D98BCDL,0xF4D98BCDL,0x21EF5C25L},{0x28766912L,0x28766912L,0xEF3EF0B8L,0xF4D98BCDL,0UL,0x225C50F4L},{4294967292UL,0xEF3EF0B8L,4294967295UL,0x225C50F4L,4294967295UL,0xEF3EF0B8L}};
