f015cbca189bd56d691b360289883647243095ab
saisi <saisi@users.noreply.github.com>
2016-10-17 12:49:07 +0000

    1476708546 aimlessnesses

---
+++ 1476708546.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      360952558
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x5CDBE8E5L;
+static int32_t g_7 = 0L;
+static int16_t g_22 = (-1L);
+static int32_t g_47[1] = {1L};
