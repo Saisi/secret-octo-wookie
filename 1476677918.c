32fbb2a8495ddf6bba47a015fa7fc2e8a8be71f1
saisi <saisi@users.noreply.github.com>
2016-10-17 04:18:38 +0000

    1476677918 artarine

---
+++ 1476677918.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4080761831
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 4L;
+static int32_t g_4[4] = {0L,0L,0L,0L};
+static int32_t g_7 = (-8L);
+static int32_t g_10 = (-2L);
+static int32_t *g_67 = (void*)0;
+static int32_t ** volatile g_66 = &g_67;/* VOLATILE GLOBAL g_66 */
+static int32_t g_69 = 7L;
+static uint64_t g_75 = 0xC213DB237E2CA82ELL;
+static uint8_t g_83 = 0x45L;
