8ff7a6fc58052e91bbcfd7098bbbe6f02c00e12f
saisi <saisi@users.noreply.github.com>
2016-10-18 07:20:15 +0000

    1476775215 aterian

---
+++ 1476775215.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3823994864
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+   signed f1 : 3;
+   const unsigned f2 : 25;
+   const unsigned f3 : 1;
+   volatile unsigned f4 : 12;
+   unsigned f5 : 19;
+   unsigned f6 : 5;
+};
+
