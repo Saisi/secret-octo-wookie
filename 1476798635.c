864b46cd0499ec6e54924c774a956a0313145fd2
saisi <saisi@users.noreply.github.com>
2016-10-18 13:50:35 +0000

    1476798635 Aleutian

---
+++ 1476798635.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3496221466
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 7UL;
+static uint16_t g_20 = 65535UL;
+static uint16_t g_30 = 0x061AL;
+static int32_t g_57[1] = {9L};
+static volatile int32_t g_81 = (-1L);/* VOLATILE GLOBAL g_81 */
+static volatile int32_t g_82[10] = {0x5B8CCF78L,0x92C9DD34L,(-1L),0x92C9DD34L,0x5B8CCF78L,0x5B8CCF78L,0x92C9DD34L,(-1L),0x92C9DD34L,0x5B8CCF78L};
+static int32_t g_83 = 0x66FE7F9DL;
+static int32_t *g_87 = (void*)0;
+static int32_t ** volatile g_86 = &g_87;/* VOLATILE GLOBAL g_86 */
