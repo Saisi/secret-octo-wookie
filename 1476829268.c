4efa5a3d20fae3b6ffdfff88dd47de3af1d4d65e
saisi <saisi@users.noreply.github.com>
2016-10-18 22:21:09 +0000

    1476829268 arock

---
+++ 1476829268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2503750672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+   volatile unsigned f1 : 19;
+   unsigned f2 : 9;
+   volatile unsigned f3 : 17;
+   volatile signed f4 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_14 = (-1L);
