89f1983bfdeca0fe0e2a4c6ca251c8fa3ea9eeb0
saisi <saisi@users.noreply.github.com>
2016-10-18 02:49:57 +0000

    1476758997 barding

---
+++ 1476758997.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1758103540
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_9 = 2UL;
+static uint8_t g_17 = 1UL;
+static int32_t g_32 = 0x3B0639E6L;
+static int32_t * volatile g_31 = &g_32;/* VOLATILE GLOBAL g_31 */
+static uint8_t g_38 = 0x0FL;
+static uint8_t *g_37 = &g_38;
+static uint64_t g_68 = 0x0A061FA46B4C01F9LL;
+static uint64_t g_70 = 18446744073709551615UL;
+static int16_t g_75 = 1L;
