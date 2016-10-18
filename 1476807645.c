5430638daef30b86ee694109987e762625522641
saisi <saisi@users.noreply.github.com>
2016-10-18 16:20:45 +0000

    1476807645 aglossal

---
+++ 1476807645.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1919308923
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+   int32_t  f2;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 11;
