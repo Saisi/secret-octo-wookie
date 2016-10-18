31d37532f4d5f393810dfe0d0b41f831c9477d07
saisi <saisi@users.noreply.github.com>
2016-10-18 13:20:34 +0000

    1476796833 ageratum

---
+++ 1476796833.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      785874122
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile int8_t  f1;
+   const unsigned f2 : 11;
+   const volatile int32_t  f3;
+   const volatile uint32_t  f4;
+   const volatile int64_t  f5;
+   const volatile uint8_t  f6;
+   int16_t  f7;
+};
