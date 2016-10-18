8b3d66d0d8e96f3450b24a7cbbd2e51c5e203640
saisi <saisi@users.noreply.github.com>
2016-10-18 05:50:11 +0000

    1476769810 agnails

---
+++ 1476769810.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      25904763
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_3 = 0x2AFE01BDL;
+static int32_t g_4 = 0L;
+static uint32_t g_8 = 0UL;
+static int64_t g_60[8] = {0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL,0x32F4BB0F55F5DDA3LL};
