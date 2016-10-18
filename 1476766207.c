baede16888e369b9f77a20b40a5482b634121e64
saisi <saisi@users.noreply.github.com>
2016-10-18 04:50:08 +0000

    1476766207 Anaspides

---
+++ 1476766207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2670231094
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   int8_t  f1;
+   int8_t  f2;
+   uint16_t  f3;
+   volatile uint32_t  f4;
+   volatile uint32_t  f5;
+   uint32_t  f6;
+   volatile uint32_t  f7;
+   volatile int64_t  f8;
