fcec62b62b86d6a39a71835f95fb7b28a3a507c1
saisi <saisi@users.noreply.github.com>
2016-10-18 01:19:52 +0000

    1476753592 archdruid

---
+++ 1476753592.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      767374848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xE90A624CL;
+static int8_t g_6[9][1] = {{(-9L)},{0L},{(-9L)},{0L},{(-9L)},{0L},{(-9L)},{0L},{(-9L)}};
+static volatile int32_t g_7 = 0xE2A8D3A1L;/* VOLATILE GLOBAL g_7 */
+static volatile int16_t g_8[4] = {0xC383L,0xC383L,0xC383L,0xC383L};
+static int32_t g_9[2] = {0x11F3813BL,0x11F3813BL};
+static int16_t g_10 = 0xD996L;
+static uint8_t g_11 = 0xF3L;
+
+
