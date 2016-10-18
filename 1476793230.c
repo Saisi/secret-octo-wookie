cb987652d6b3292776378d0682835666667fd32c
saisi <saisi@users.noreply.github.com>
2016-10-18 12:20:31 +0000

    1476793230 autotroph

---
+++ 1476793230.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3856257717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const volatile int32_t  f1;
+   uint8_t  f2;
+   int32_t  f3;
+   volatile uint32_t  f4;
+   signed f5 : 7;
+   const int8_t  f6;
