beffd1203f63a629a19f892c88eac05ea3955e7f
saisi <saisi@users.noreply.github.com>
2016-10-18 23:51:14 +0000

    1476834674 analogy

---
+++ 1476834674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      841478611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   signed f1 : 31;
+   signed f2 : 6;
+   volatile signed f3 : 25;
+   signed f4 : 6;
+   unsigned f5 : 7;
+   unsigned f6 : 10;
