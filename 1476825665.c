b7a35630c935661142cde243286d6ec5648f3ae8
saisi <saisi@users.noreply.github.com>
2016-10-18 21:21:06 +0000

    1476825665 autotetraploidy

---
+++ 1476825665.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1258119412
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 8;
+   const uint32_t  f1;
+   unsigned f2 : 21;
+   signed f3 : 3;
+   volatile unsigned f4 : 28;
+   const signed f5 : 24;
+   const signed f6 : 25;
+   unsigned f7 : 14;
+   volatile unsigned f8 : 7;
