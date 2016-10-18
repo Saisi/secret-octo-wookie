7c1fe803ceaa4424a79e7b7d614569e25a11a160
saisi <saisi@users.noreply.github.com>
2016-10-18 15:50:43 +0000

    1476805840 acidific

---
+++ 1476805840.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1131984936
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 0xE87DL;
+static const uint16_t g_15 = 1UL;
+static const uint16_t *g_14 = &g_15;
+static uint8_t g_23 = 0x07L;
+static uint8_t g_34 = 1UL;
+static uint8_t *g_33 = &g_34;
+static int16_t g_39 = (-10L);
+static uint16_t *g_85 = (void*)0;
+static uint16_t g_100 = 1UL;
