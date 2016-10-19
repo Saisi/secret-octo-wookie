d9fb629060d75843e84b026b28d4d07e95c76313
saisi <saisi@users.noreply.github.com>
2016-10-19 06:51:42 +0000

    1476859902 Acrothoracica

---
+++ 1476859902.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1250829013
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   unsigned f1 : 30;
+   unsigned f2 : 28;
+   signed f3 : 16;
+   signed f4 : 23;
+   unsigned f5 : 9;
+   unsigned f6 : 8;
+   signed f7 : 2;
+};
