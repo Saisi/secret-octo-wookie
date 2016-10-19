a98429a6af7c871d4cda8d4f776babf2e33d32d5
saisi <saisi@users.noreply.github.com>
2016-10-19 17:52:22 +0000

    1476899542 attunes

---
+++ 1476899542.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3477445257
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   signed f1 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_12 = 0xEEA5L;
+static uint64_t g_19 = 0x1718BC698DC509CALL;
+static int32_t g_26 = 0xE5FEF407L;
+static int32_t * volatile g_25 = &g_26;/* VOLATILE GLOBAL g_25 */
