83b7bfcd92fc178b5889bb7f318deb71f3c929fe
saisi <saisi@users.noreply.github.com>
2016-10-19 12:52:05 +0000

    1476881524 amphilogism

---
+++ 1476881524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3912610011
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = 0xAABECBA2AFE01BD9LL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0L;
+static volatile int32_t g_5 = 0x644ACCB2L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[2][8] = {{(-8L),(-1L),(-1L),(-8L),(-1L),1L,(-1L),(-8L)},{(-1L),(-1L),(-1L),1L,0xDA585283L,0xDA585283L,1L,(-1L)}};
+static volatile int32_t g_7[10] = {0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L,0xF3F7E813L};
+static int32_t g_8 = 0xE700D62EL;
+static int32_t g_11 = 0xFDB7B241L;
+static int32_t g_13[4] = {0L,0L,0L,0L};
+static uint64_t g_14 = 0x8C105A83D87E3BC4LL;
