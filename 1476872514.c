824460ac519165beae366b5d4fa03fb21744ed73
saisi <saisi@users.noreply.github.com>
2016-10-19 10:21:55 +0000

    1476872514 antrectomy

---
+++ 1476872514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      697500915
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 24;
+   signed f1 : 15;
+   signed f2 : 22;
+   signed f3 : 9;
+   unsigned f4 : 22;
+   signed f5 : 23;
+   uint16_t  f6;
