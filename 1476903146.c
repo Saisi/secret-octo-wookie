5ebdd0f523b8438f6813f4c2c01845653979f652
saisi <saisi@users.noreply.github.com>
2016-10-19 18:52:26 +0000

    1476903146 Arctisca

---
+++ 1476903146.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2616222638
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int8_t g_8 = (-7L);
+static uint16_t g_38 = 65528UL;
+static uint32_t g_40 = 4294967288UL;
+static int8_t g_43 = (-10L);
+static int8_t *g_42 = &g_43;
+static int32_t g_57 = 0xB93B3BBBL;
+static int32_t * volatile g_58 = (void*)0;/* VOLATILE GLOBAL g_58 */
+static uint32_t g_60[4] = {0x160C6370L,0x160C6370L,0x160C6370L,0x160C6370L};
