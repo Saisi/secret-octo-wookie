a4e2b4af274eae197300b62687820ab3ece1d40c
saisi <saisi@users.noreply.github.com>
2016-10-19 05:21:37 +0000

    1476854497 anticolonial

---
+++ 1476854497.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      199668951
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   signed f1 : 25;
+   unsigned f2 : 24;
+   const unsigned f3 : 3;
+   const unsigned f4 : 8;
+};
+
+union U1 {
+   const volatile uint64_t  f0;
