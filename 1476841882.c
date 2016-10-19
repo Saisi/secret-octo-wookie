5512d82d4fc65b696cd3b812588acd4eceec6dca
saisi <saisi@users.noreply.github.com>
2016-10-19 01:51:22 +0000

    1476841882 alienness

---
+++ 1476841882.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3783699810
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_14 = 65535UL;
+static uint8_t g_16 = 0x9BL;
+static uint64_t g_22[7] = {0x2E40C18AB9C279A2LL,0xB7C7008B15E314A5LL,0x2E40C18AB9C279A2LL,0x2E40C18AB9C279A2LL,0xB7C7008B15E314A5LL,0x2E40C18AB9C279A2LL,0x2E40C18AB9C279A2LL};
+static uint8_t g_26 = 0x20L;
+static volatile int32_t g_32[9] = {0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL,0xEF874ACCL};
+static volatile int32_t *g_31 = &g_32[1];
+static uint16_t g_35 = 0x550EL;
+static int32_t g_37 = 0L;
+static const int32_t *g_36 = &g_37;
