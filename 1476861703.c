18f5c73c21c18a92c6d72bbc6cd23e7fceaffd38
saisi <saisi@users.noreply.github.com>
2016-10-19 07:21:44 +0000

    1476861703 azophenol

---
+++ 1476861703.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      66715778
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x546158D3L;
+static int32_t g_15 = 1L;
+static int32_t * volatile g_14 = &g_15;/* VOLATILE GLOBAL g_14 */
+static int32_t *g_65 = (void*)0;
+static int32_t ** volatile g_64 = &g_65;/* VOLATILE GLOBAL g_64 */
+static uint64_t g_69[7] = {0x2DE47E5F8F0B53CFLL,0xD4033E270BB1DCEDLL,0x2DE47E5F8F0B53CFLL,0x2DE47E5F8F0B53CFLL,0xD4033E270BB1DCEDLL,0x2DE47E5F8F0B53CFLL,0x2DE47E5F8F0B53CFLL};
+static uint64_t g_71 = 0x1D319D7FCCEA59F7LL;
+static uint64_t *g_70[6][3][1] = {{{&g_71},{&g_71},{&g_71}},{{&g_71},{&g_71},{&g_71}},{{&g_71},{&g_71},{&g_71}},{{&g_71},{&g_71},{&g_71}},{{&g_71},{&g_71},{&g_71}},{{&g_71},{&g_71},{&g_71}}};
+static uint64_t *g_73 = &g_71;
