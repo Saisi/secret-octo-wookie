d49e1782fb3700640c5ac921990c4935a8a0654b
saisi <saisi@users.noreply.github.com>
2016-10-19 01:21:21 +0000

    1476840079 Anglim

---
+++ 1476840079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1715693483
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 20;
+   volatile signed f1 : 27;
+   const volatile unsigned f2 : 18;
+   const unsigned f3 : 21;
+   const volatile unsigned f4 : 18;
+   volatile int64_t  f5;
+   const unsigned f6 : 7;
+   unsigned f7 : 5;
+   signed : 0;
