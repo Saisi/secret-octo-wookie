7dfca39bc19c4fad1ef7b5a4c21fe9eaa0c37b07
saisi <saisi@users.noreply.github.com>
2016-10-19 13:22:08 +0000

    1476883327 anaphases

---
+++ 1476883327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      570356286
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 13;
+   volatile int16_t  f1;
+   volatile uint32_t  f2;
+   int32_t  f3;
+   unsigned f4 : 20;
+   uint8_t  f5;
+};
