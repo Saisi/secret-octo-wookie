4195b0f683c9b43500d519292548a7d75101b79d
saisi <saisi@users.noreply.github.com>
2016-10-19 14:22:11 +0000

    1476886930 adlumin

---
+++ 1476886930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3294578934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+   volatile signed : 0;
+   uint64_t  f3;
+   const signed f4 : 18;
+   uint32_t  f5;
+   uint8_t  f6;
+   volatile int8_t  f7;
