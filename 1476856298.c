945347a129beb5e598c798beeaf1964d2e92c4c6
saisi <saisi@users.noreply.github.com>
2016-10-19 05:51:39 +0000

    1476856298 Achilles

---
+++ 1476856298.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3267489554
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   volatile int64_t  f1;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
