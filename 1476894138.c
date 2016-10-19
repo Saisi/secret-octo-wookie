f20b63bc518acc088217f4bd053b41d79eca4eb4
saisi <saisi@users.noreply.github.com>
2016-10-19 16:22:18 +0000

    1476894138 Arabicism

---
+++ 1476894138.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      500199097
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   signed f0 : 13;
+   const uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2[3] = {0L,0L,0L};
+static volatile int32_t g_3 = 0x87CF2E56L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static uint16_t g_7 = 9UL;
