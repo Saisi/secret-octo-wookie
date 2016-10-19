cf90c4a26b8b6af41e64d00bd2216d6715fd87da
saisi <saisi@users.noreply.github.com>
2016-10-19 23:22:42 +0000

    1476919360 antiburgher

---
+++ 1476919360.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      185377708
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int16_t  f1;
+   volatile uint32_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_17 = 0xFECFL;
+static int8_t g_23 = 0x65L;
