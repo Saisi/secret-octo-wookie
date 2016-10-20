444c3a72009ca62cf3409c9194c15feb35bbd4df
saisi <saisi@users.noreply.github.com>
2016-10-20 10:53:25 +0000

    1476960804 anoplocephalic

---
+++ 1476960804.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3092254422
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 25;
+   volatile unsigned f1 : 10;
+   signed f2 : 4;
+   volatile unsigned : 0;
+   const volatile uint16_t  f3;
+   signed f4 : 9;
+   unsigned f5 : 6;
+   signed f6 : 7;
+   signed f7 : 28;
