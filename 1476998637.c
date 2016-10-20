3a62ecb1dcb1ed8cfcef58ecaed696a225840700
saisi <saisi@users.noreply.github.com>
2016-10-20 21:23:59 +0000

    1476998637 Aldas

---
+++ 1476998637.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3159734772
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const volatile uint64_t  f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_11 = 1UL;
+static int32_t g_13 = 0xF2121F90L;
+static int32_t *g_12 = &g_13;
