7d0efe9b9a3196e46e60776aa19644e8441a87f1
saisi <saisi@users.noreply.github.com>
2016-10-20 18:53:50 +0000

    1476989630 Aquashicola

---
+++ 1476989630.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2127334418
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB39D2057L;
+static int32_t g_6[1] = {(-1L)};
+static int8_t g_31[4] = {(-3L),(-3L),(-3L),(-3L)};
+static int32_t g_58 = 0L;
+static int16_t g_61 = 2L;
+static int8_t g_86 = 0xE2L;
+static int64_t g_96 = 0x9581AD2EC94644A7LL;
+static int32_t *g_101[4][8] = {{&g_58,(void*)0,&g_3,&g_3,(void*)0,&g_58,(void*)0,&g_3},{(void*)0,(void*)0,(void*)0,&g_58,&g_58,(void*)0,(void*)0,(void*)0},{&g_3,&g_58,&g_3,&g_58,&g_3,&g_3,&g_58,&g_3},{&g_3,&g_3,&g_58,&g_3,&g_58,&g_3,&g_3,&g_58}};
+static int32_t ** volatile g_100[5][3] = {{&g_101[2][7],&g_101[3][5],&g_101[2][7]},{&g_101[2][7],&g_101[2][7],&g_101[2][7]},{&g_101[2][7],&g_101[3][5],&g_101[2][7]},{&g_101[2][7],&g_101[2][7],&g_101[2][7]},{&g_101[2][7],&g_101[3][5],&g_101[2][7]}};
