feba0715ea66c1c510bcc1190398878d5cdde77e
saisi <saisi@users.noreply.github.com>
2016-10-20 00:22:44 +0000

    1476922964 baccalaureus

---
+++ 1476922964.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4156010143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   uint8_t  f1;
+   signed f2 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int8_t g_7 = 0x81L;/* VOLATILE GLOBAL g_7 */
+static uint32_t g_10 = 1UL;
