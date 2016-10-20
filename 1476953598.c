1e2eec2b4491330e2f0e3d0977fe6c4a64398ea7
saisi <saisi@users.noreply.github.com>
2016-10-20 08:53:19 +0000

    1476953598 ankerite

---
+++ 1476953598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3331029850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x841B3C69L;
+static int32_t g_24 = 8L;
+static volatile int32_t g_32 = 6L;/* VOLATILE GLOBAL g_32 */
+static int8_t g_35[2] = {0xA2L,0xA2L};
+static volatile uint16_t g_36[5] = {0x3CD3L,0x3CD3L,0x3CD3L,0x3CD3L,0x3CD3L};
+static uint32_t g_99 = 0x102F310EL;
+static int32_t **g_105 = (void*)0;
+static int32_t * volatile g_107 = &g_24;/* VOLATILE GLOBAL g_107 */
+static int32_t * volatile *g_106 = &g_107;
