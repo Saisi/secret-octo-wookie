7245a1350be90a2a0ba998ce4e16042efa2bd30f
saisi <saisi@users.noreply.github.com>
2016-10-20 06:53:09 +0000

    1476946389 Archiplata

---
+++ 1476946389.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3166263891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 8;
+   signed f1 : 31;
+   unsigned f2 : 6;
+   signed f3 : 25;
+   unsigned f4 : 6;
+   unsigned f5 : 7;
+   unsigned f6 : 10;
