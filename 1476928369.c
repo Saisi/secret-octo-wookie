6f8bc4539040120f1af7f4fbf39c61ca3a58be73
saisi <saisi@users.noreply.github.com>
2016-10-20 01:52:50 +0000

    1476928369 aerocolpos

---
+++ 1476928369.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2236397146
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 27;
+   volatile signed f1 : 4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const volatile unsigned f0 : 30;
+   unsigned f1 : 11;
