9f21fb8b4a061d1029eb65f35c65b122c9878cbd
saisi <saisi@users.noreply.github.com>
2016-10-20 17:53:48 +0000

    1476986027 andranatomy

---
+++ 1476986027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      571079555
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile int16_t  f1;
+   int64_t  f2;
+   uint8_t  f3;
+   uint16_t  f4;
+   int8_t  f5;
+   unsigned : 0;
+};
+
