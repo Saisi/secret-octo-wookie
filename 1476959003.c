ea2b42197b2ea61fdcc7254bd9b37f86aa3ba750
saisi <saisi@users.noreply.github.com>
2016-10-20 10:23:23 +0000

    1476959003 aquavalent

---
+++ 1476959003.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      191841340
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   uint64_t  f1;
+   volatile uint32_t  f2;
+   signed f3 : 24;
+   int32_t  f4;
+   int8_t  f5;
+   const uint64_t  f6;
+   volatile uint16_t  f7;
+   volatile unsigned f8 : 18;
