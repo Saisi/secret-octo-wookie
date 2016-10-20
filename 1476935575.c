79717284b902d7ea9c177712c05ce773bf92936d
saisi <saisi@users.noreply.github.com>
2016-10-20 03:52:56 +0000

    1476935575 alifs

---
+++ 1476935575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      455766222
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   volatile uint32_t  f1;
+   uint32_t  f2;
+   const int8_t * const  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xF9B2CFB6L;
+static int8_t g_28 = 0x73L;
