aecf6be89e8b142589c2d0f0f805efd63a206af3
saisi <saisi@users.noreply.github.com>
2016-10-20 03:22:54 +0000

    1476933774 anticonventionalism

---
+++ 1476933774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2151284426
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0xDF98A59CL;
+static volatile int32_t g_7[5][5][7] = {{{(-1L),2L,0xE13A6C9CL,0x12D2C87AL,0x12D2C87AL,0xE13A6C9CL,2L},{0x8E92EDA8L,0L,0L,0x3C15D0D6L,0x8E92EDA8L,0x3C15D0D6L,0L},{0x12D2C87AL,0x405CEFD0L,0xE13A6C9CL,0xADF1ADB2L,(-1L),(-1L),0xADF1ADB2L},{(-1L),0x815F8425L,(-1L),0x3C15D0D6L,(-6L),0x815F8425L,(-6L)},{0x405CEFD0L,0xADF1ADB2L,2L,0x12D2C87AL,(-1L),0x81A82E77L,0x81A82E77L}},{{0x8E92EDA8L,0x49FCFBCEL,(-1L),0x49FCFBCEL,0x8E92EDA8L,0x815F8425L,(-1L)},{0L,0x405CEFD0L,0xADF1ADB2L,2L,0x12D2C87AL,(-1L),0x81A82E77L},{(-6L),0x3C15D0D6L,(-1L),0x815F8425L,(-1L),0x3C15D0D6L,(-6L)},{0L,2L,0x81A82E77L,0x12D2C87AL,0x405CEFD0L,0xE13A6C9CL,0xADF1ADB2L},{0x8E92EDA8L,0x3C15D0D6L,0L,0L,0x8E92EDA8L,0L,0L}},{{0x405CEFD0L,0x405CEFD0L,0x81A82E77L,0xADF1ADB2L,0L,(-1L),2L},{(-1L),0x49FCFBCEL,(-1L),0L,(-6L),0x49FCFBCEL,(-6L)},{0x12D2C87AL,0xADF1ADB2L,0xADF1ADB2L,0x12D2C87AL,0L,0x81A82E77L,0xE13A6C9CL},{0x8E92EDA8L,0x815F8425L,(-1L),0x815F8425L,0x8E92EDA8L,0x49FCFBCEL,(-1L)},{(-1L),0x405CEFD0L,2L,2L,0x405CEFD0L,(-1L),0xE13A6C9CL}},{{(-6L),0L,(-1L),0x49FCFBCEL,(-1L),0L,(-6L)},{(-1L),2L,0xE13A6C9CL,0x12D2C87AL,0x12D2C87AL,0xE13A6C9CL,2L},{0x8E92EDA8L,0L,0L,0x3C15D0D6L,0x8E92EDA8L,0x3C15D0D6L,0L},{0x12D2C87AL,0x405CEFD0L,0xE13A6C9CL,0xADF1ADB2L,(-1L),(-1L),0xADF1ADB2L},{(-1L),0x815F8425L,(-1L),0x3C15D0D6L,(-6L),0x815F8425L,(-6L)}},{{0x405CEFD0L,0xADF1ADB2L,2L,0x12D2C87AL,(-1L),0x81A82E77L,0x81A82E77L},{0x8E92EDA8L,0x49FCFBCEL,(-1L),0x49FCFBCEL,0x8E92EDA8L,0x815F8425L,(-1L)},{0L,0x405CEFD0L,0xADF1ADB2L,2L,0x12D2C87AL,(-1L),0x81A82E77L},{(-6L),0x3C15D0D6L,(-1L),0x815F8425L,(-1L),0x3C15D0D6L,(-6L)},{0L,2L,0x81A82E77L,0x12D2C87AL,0x405CEFD0L,0xE13A6C9CL,0xADF1ADB2L}}};
+static volatile int32_t *g_6 = &g_7[3][3][1];
+static int32_t g_17 = 1L;
+static uint8_t g_25 = 0x03L;
+static int16_t g_67 = 3L;
+static const int32_t *g_69[10][8][3] = {{{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,&g_5,(void*)0}},{{(void*)0,(void*)0,(void*)0},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,(void*)0},{&g_5,(void*)0,&g_5},{&g_5,&g_5,&g_5},{&g_5,(void*)0,&g_5}},{{(void*)0,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,(void*)0,&g_5},{&g_5,(void*)0,(void*)0},{&g_5,&g_5,(void*)0},{&g_5,&g_5,&g_5}},{{(void*)0,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{(void*)0,&g_5,(void*)0},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,(void*)0,&g_5},{&g_5,&g_5,&g_5}},{{&g_5,(void*)0,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5}},{{&g_5,&g_5,&g_5},{&g_5,(void*)0,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5}},{{&g_5,&g_5,&g_5},{(void*)0,&g_5,(void*)0},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,&g_5,(void*)0},{&g_5,&g_5,(void*)0}},{{&g_5,&g_5,&g_5},{&g_5,&g_5,(void*)0},{(void*)0,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,(void*)0,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5}},{{&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,(void*)0,(void*)0},{&g_5,&g_5,(void*)0},{&g_5,(void*)0,&g_5}},{{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,&g_5,(void*)0},{&g_5,&g_5,(void*)0},{&g_5,&g_5,&g_5},{(void*)0,&g_5,&g_5},{&g_5,&g_5,&g_5},{&g_5,&g_5,(void*)0}}};
+static const int32_t **g_68[4] = {&g_69[4][4][0],&g_69[4][4][0],&g_69[4][4][0],&g_69[4][4][0]};
+static uint32_t g_98 = 9UL;
