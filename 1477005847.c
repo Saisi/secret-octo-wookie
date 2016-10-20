0620831797f16024bd16c6bd5a0912d25d154b85
saisi <saisi@users.noreply.github.com>
2016-10-20 23:24:08 +0000

    1477005847 antimiasmatic

---
+++ 1477005847.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2036123886
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const int8_t  f1;
+   volatile uint16_t  f2;
+   const int64_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+};
