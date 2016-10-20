1622609240e6c58f59cb1128e9d29719488c60ba
saisi <saisi@users.noreply.github.com>
2016-10-20 16:53:45 +0000

    1476982425 additionist

---
+++ 1476982425.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      333680873
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 19;
+   volatile unsigned f1 : 30;
+   unsigned f2 : 9;
+   signed f3 : 16;
+   signed f4 : 1;
+   unsigned f5 : 22;
+   unsigned f6 : 29;
