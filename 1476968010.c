8d5c633310d61f463aeead91b742fec90820a6fb
saisi <saisi@users.noreply.github.com>
2016-10-20 12:53:33 +0000

    1476968010 angiocardiographic

---
+++ 1476968010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2576302997
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 9;
+   volatile signed f1 : 28;
+   const unsigned f2 : 15;
+   uint32_t  f3;
+   signed f4 : 23;
+   signed f5 : 9;
+   unsigned f6 : 8;
+   unsigned f7 : 2;
+   unsigned f8 : 9;
