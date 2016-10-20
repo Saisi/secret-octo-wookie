237ea5bc97a2f7e745d4aa2f85c6c38c8abac86b
saisi <saisi@users.noreply.github.com>
2016-10-20 22:24:04 +0000

    1477002243 artificers

---
+++ 1477002243.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2206144387
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   uint8_t  f1;
+   int8_t * const  f2;
+   const volatile int8_t * f3;
+   signed f4 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_5 = 0xB792L;/* VOLATILE GLOBAL g_5 */
