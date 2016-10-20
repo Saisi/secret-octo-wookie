ba3a3c7117a34d07c18a7dbc74a877696edbd5b1
saisi <saisi@users.noreply.github.com>
2016-10-20 19:53:53 +0000

    1476993233 antiphilosophies

---
+++ 1476993233.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3392657744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint16_t g_20 = 0UL;
+static int32_t g_23 = (-1L);
+static int32_t g_30 = (-1L);
+static int32_t * volatile g_29 = &g_30;/* VOLATILE GLOBAL g_29 */
+static int16_t g_52 = (-8L);
+static uint16_t g_54 = 0x206FL;
+static const int32_t *g_63 = &g_30;
+static int32_t g_75 = (-7L);
+static uint8_t g_102[3][1] = {{0xEAL},{0xEAL},{0xEAL}};
