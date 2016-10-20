a56bfe201694673aef1ca76a59f847c413b1f15b
saisi <saisi@users.noreply.github.com>
2016-10-20 22:54:06 +0000

    1477004046 Archimago

---
+++ 1477004046.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3415278113
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   const uint64_t  f0;
+   volatile int16_t  f1;
+};
+
+union U2 {
