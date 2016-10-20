36dcf83fc767b95491426df95b5ccf96d4495b3d
saisi <saisi@users.noreply.github.com>
2016-10-20 11:23:26 +0000

    1476962606 ballones

---
+++ 1476962606.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1909823167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 30;
+   volatile int8_t * f1;
+   volatile int8_t  f2;
+   const int32_t  f3;
+   volatile int8_t * f4;
+};
+
+union U1 {
+   const int8_t  f0;
