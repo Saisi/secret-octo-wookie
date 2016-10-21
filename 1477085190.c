b37dab2aecdf84c5e538f4590f7479b88326c61b
saisi <saisi@users.noreply.github.com>
2016-10-21 21:26:31 +0000

    1477085190 baptizes

---
+++ 1477085190.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1874781748
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 8;
+   const signed f1 : 29;
+   volatile unsigned f2 : 11;
+   unsigned f3 : 11;
+   int32_t  f4;
+   const signed f5 : 24;
+   volatile unsigned f6 : 25;
