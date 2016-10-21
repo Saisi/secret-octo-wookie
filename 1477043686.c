3f7f88e88a36642d4a56946ead676ded4f082759
saisi <saisi@users.noreply.github.com>
2016-10-21 09:54:46 +0000

    1477043686 autocoids

---
+++ 1477043686.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1598695893
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = 0x83C7730F94F37831LL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_5 = 8L;
+static int16_t g_32 = 0x8475L;
+static uint8_t g_79 = 0xC9L;
+static uint8_t g_81 = 1UL;
+static int64_t g_87 = (-9L);
+static int64_t g_92[6][6] = {{1L,(-1L),1L,(-8L),(-1L),7L},{1L,(-1L),(-8L),(-8L),(-1L),1L},{1L,0L,7L,(-8L),0L,(-8L)},{1L,(-1L),1L,(-8L),(-1L),7L},{1L,(-1L),(-8L),(-8L),(-1L),1L},{1L,0L,7L,(-8L),0L,(-8L)}};
+static int16_t g_93[2][6] = {{6L,0L,6L,6L,0L,6L},{6L,0L,6L,6L,6L,0xF7CFL}};
+static uint16_t g_100 = 0x852CL;
