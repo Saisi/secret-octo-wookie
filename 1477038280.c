f4f4120fd4619a95d60590f7b03cec442605f488
saisi <saisi@users.noreply.github.com>
2016-10-21 08:24:41 +0000

    1477038280 backslidden

---
+++ 1477038280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4051870041
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   const uint64_t  f1;
+   uint64_t  f2;
+   volatile int32_t  f3;
+   const volatile unsigned f4 : 26;
+   signed f5 : 13;
+   unsigned f6 : 1;
+   volatile uint64_t  f7;
+};
