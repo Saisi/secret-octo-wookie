9f90b065e516610189564c2b6ca585f34f646037
saisi <saisi@users.noreply.github.com>
2016-10-21 10:24:48 +0000

    1477045487 backpackers

---
+++ 1477045487.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      694261546
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-4L);
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static int8_t g_52 = 0x65L;
+static volatile uint32_t g_61 = 0x10212AEAL;/* VOLATILE GLOBAL g_61 */
+static int64_t g_62[7] = {4L,(-1L),4L,4L,(-1L),4L,4L};
+static int32_t * volatile g_83[10] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t * volatile g_84 = (void*)0;/* VOLATILE GLOBAL g_84 */
+static int64_t g_92[1] = {0L};
+static volatile uint32_t g_96 = 0x0401A71BL;/* VOLATILE GLOBAL g_96 */
