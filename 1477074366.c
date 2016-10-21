3d6ea759870260aeb0970a6653a79b1ea0079980
saisi <saisi@users.noreply.github.com>
2016-10-21 18:26:06 +0000

    1477074366 accoucheur

---
+++ 1477074366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      420318454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = (-5L);
+static int32_t g_7 = 0xA75C19C5L;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
