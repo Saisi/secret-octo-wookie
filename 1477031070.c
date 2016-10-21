c5d92437965c6f739e8738d514a6bef0cf04bab4
saisi <saisi@users.noreply.github.com>
2016-10-21 06:24:31 +0000

    1477031070 androcephalous

---
+++ 1477031070.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3374705709
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   const int32_t  f1;
+   volatile int32_t  f2;
+   int8_t  f3;
+   const uint32_t  f4;
+};
+
+union U1 {
+   const int32_t  f0;
