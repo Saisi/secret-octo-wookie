7999ac00a22ef576558229cd756f165a79ba2e45
saisi <saisi@users.noreply.github.com>
2016-10-21 15:55:49 +0000

    1477065348 augmentative

---
+++ 1477065348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1057285764
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile unsigned f1 : 29;
+   int32_t  f2;
+   int32_t  f3;
+   uint64_t  f4;
+   int64_t  f5;
+   unsigned f6 : 21;
+   uint64_t  f7;
+   int16_t  f8;
