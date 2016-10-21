cafa63a77364c591cc27bf897729a25a1e61cf00
saisi <saisi@users.noreply.github.com>
2016-10-21 20:56:29 +0000

    1477083388 addlins

---
+++ 1477083388.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3916768521
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8[3] = {0xD53BL,0xD53BL,0xD53BL};
+static int32_t g_23 = 0x49B76EEAL;
+static const int32_t g_29[4] = {0x1780EEC2L,0x1780EEC2L,0x1780EEC2L,0x1780EEC2L};
+static int32_t g_35 = 1L;
+static int32_t * volatile g_34 = &g_35;/* VOLATILE GLOBAL g_34 */
+static int32_t * volatile g_36[8] = {&g_35,&g_35,&g_35,&g_35,&g_35,&g_35,&g_35,&g_35};
+static int32_t * volatile g_37[1][2][7] = {{{&g_35,&g_35,&g_35,&g_35,&g_35,&g_35,&g_35},{&g_35,&g_35,&g_35,&g_35,&g_35,&g_35,&g_35}}};
+static uint16_t g_40[9] = {8UL,65530UL,8UL,65530UL,8UL,65530UL,8UL,65530UL,8UL};
+static uint64_t g_74 = 1UL;
