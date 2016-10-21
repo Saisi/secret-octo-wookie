0b6528e98cbac5cefdceac1e10845ef894a252fe
saisi <saisi@users.noreply.github.com>
2016-10-21 14:25:41 +0000

    1477059941 antitintinnabularian

---
+++ 1477059941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2193263017
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 19;
+   int32_t  f1;
+   const uint64_t  f2;
+   volatile signed f3 : 20;
+   uint64_t  f4;
+   signed f5 : 10;
+};
+
+#pragma pack(push)
