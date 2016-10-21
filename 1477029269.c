330595285ac8f2e197d6876c0d9a5184d7398c8e
saisi <saisi@users.noreply.github.com>
2016-10-21 05:54:29 +0000

    1477029269 astrospherecentrosomic

---
+++ 1477029269.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      793098178
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_6 = 9UL;/* VOLATILE GLOBAL g_6 */
+static uint32_t g_13[1][1][6] = {{{4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL}}};
+static int32_t g_39 = (-3L);
+static uint32_t g_43 = 18446744073709551615UL;
+static uint32_t g_70 = 8UL;
+static uint32_t g_72 = 0xBB1DCEDEL;
+static uint32_t *g_71 = &g_72;
+static uint32_t g_77[9] = {1UL,0x12ABB1EFL,1UL,1UL,0x12ABB1EFL,1UL,1UL,0x12ABB1EFL,1UL};
+static volatile uint32_t g_82 = 0xD68CABD0L;/* VOLATILE GLOBAL g_82 */
