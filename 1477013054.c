87a9eab10522281b1c740999982db8d297b85482
saisi <saisi@users.noreply.github.com>
2016-10-21 01:24:14 +0000

    1477013054 arctangent

---
+++ 1477013054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1707271430
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 2L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 4L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 3L;
+static volatile int32_t g_8 = 6L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9[5][1] = {{(-9L)},{0L},{(-9L)},{0L},{(-9L)}};
+static int32_t g_10 = 0xF38C0887L;
+static int32_t g_14 = (-7L);
