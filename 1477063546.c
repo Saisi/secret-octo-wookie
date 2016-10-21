e244b09bef5710d5e69e1f2066d56d97157b55d6
saisi <saisi@users.noreply.github.com>
2016-10-21 15:25:47 +0000

    1477063546 balloen

---
+++ 1477063546.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      991819769
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   signed f1 : 6;
+   uint16_t  f2;
+   const signed f3 : 18;
+   signed f4 : 17;
+   signed f5 : 27;
+   const signed f6 : 18;
+   const signed f7 : 4;
+   unsigned f8 : 3;
