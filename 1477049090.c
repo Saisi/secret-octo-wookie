c3f80734c5bb313c955b0a9a87386aec82a9ca67
saisi <saisi@users.noreply.github.com>
2016-10-21 11:24:51 +0000

    1477049090 auscultating

---
+++ 1477049090.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1840951747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 12;
+   volatile unsigned f1 : 1;
+   unsigned f2 : 24;
+   const unsigned f3 : 15;
+   const volatile unsigned f4 : 14;
+   const volatile signed f5 : 30;
+   unsigned f6 : 20;
+};
+
