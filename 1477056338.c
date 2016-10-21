084adc9fca6980470aaac0f79a8b34af3e96fdb2
saisi <saisi@users.noreply.github.com>
2016-10-21 13:25:38 +0000

    1477056338 aluminish

---
+++ 1477056338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1771185868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 20;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 17;
+   unsigned f1 : 15;
+   signed f2 : 24;
