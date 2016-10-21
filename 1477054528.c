f2525f35c36753ec97cff8618c58cc8940bf8600
saisi <saisi@users.noreply.github.com>
2016-10-21 12:55:28 +0000

    1477054528 autocratic

---
+++ 1477054528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3809563280
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+};
+
+struct S1 {
+   const unsigned f0 : 11;
+   unsigned f1 : 1;
+   signed f2 : 2;
+   unsigned f3 : 6;
+   signed f4 : 15;
