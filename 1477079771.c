316b59f3fbd36ef386f7a5bb50eeed18f9bbbcb8
saisi <saisi@users.noreply.github.com>
2016-10-21 19:56:12 +0000

    1477079771 autophagi

---
+++ 1477079771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3332259584
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_5 = 0x47DC4B4EL;/* VOLATILE GLOBAL g_5 */
+static uint32_t g_15 = 0UL;
+static uint32_t g_17 = 0xD9E2A8D3L;
+static int32_t g_23 = 0x2C9A130DL;
+static int32_t g_26[5] = {0x2894FF54L,0x2894FF54L,0x2894FF54L,0x2894FF54L,0x2894FF54L};
+static int32_t g_31 = (-1L);
+static uint32_t *g_70 = (void*)0;
+static uint32_t g_93 = 0xCEB51834L;
+static uint32_t *g_103 = &g_93;
