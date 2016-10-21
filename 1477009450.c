a99b9ef103ad9d9b2adac9806dfaafcc544cebdd
saisi <saisi@users.noreply.github.com>
2016-10-21 00:24:10 +0000

    1477009450 balancers

---
+++ 1477009450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3776903862
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_11 = 65526UL;
+static int32_t g_29 = 0xA2C6EB81L;
+static int32_t *g_28 = &g_29;
+static int16_t g_36 = 0x036CL;
+static volatile uint32_t g_45 = 0UL;/* VOLATILE GLOBAL g_45 */
+static uint8_t g_70 = 0UL;
+static uint32_t g_88 = 0xD4078237L;
+static uint32_t g_123[4][2][1] = {{{0x3065BCB3L},{0UL}},{{0UL},{0x3065BCB3L}},{{0UL},{0UL}},{{0x3065BCB3L},{0UL}}};
+static uint64_t g_125 = 18446744073709551613UL;
