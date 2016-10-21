9218bbc6eba57612de3a88a54b88858cc328c3ee
saisi <saisi@users.noreply.github.com>
2016-10-21 13:55:40 +0000

    1477058139 Atrax

---
+++ 1477058139.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4039427499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * const  f0;
+   const uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xFE223643L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x7689351FL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 4L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 6L;/* VOLATILE GLOBAL g_5 */
