c6fb1cf9f9905643a02d9fcc009ea6b60eeea782
saisi <saisi@users.noreply.github.com>
2016-10-22 07:57:10 +0000

    1477123029 antigenic

---
+++ 1477123029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4170740302
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   uint32_t  f1;
+   const int64_t  f2;
+   int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 0UL;
+static uint16_t g_7 = 0x7F37L;
