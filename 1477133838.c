dd97db60b6108c70cd38d3aa084d1bed7709f642
saisi <saisi@users.noreply.github.com>
2016-10-22 10:57:19 +0000

    1477133838 acrotrophic

---
+++ 1477133838.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3749683197
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 29;
+   volatile unsigned : 0;
+   signed f1 : 3;
+   unsigned f2 : 2;
+   signed f3 : 11;
+   signed f4 : 24;
+   volatile int32_t  f5;
