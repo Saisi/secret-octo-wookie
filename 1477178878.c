d3f05fcde6c30684e4bb9ae28760384700588b2e
saisi <saisi@users.noreply.github.com>
2016-10-22 23:27:58 +0000

    1477178878 ameliorativ

---
+++ 1477178878.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2450377364
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 24;
+   signed f1 : 13;
+   signed f2 : 27;
+};
+
+struct S1 {
+   struct S0  f0;
+   const signed f1 : 29;
+   const signed f2 : 14;
