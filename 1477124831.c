3ad8601f58039de693b7fa76549c130a52f3afd2
saisi <saisi@users.noreply.github.com>
2016-10-22 08:27:11 +0000

    1477124831 Africander

---
+++ 1477124831.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2461252469
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   const uint8_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static uint8_t g_34 = 255UL;
+static int32_t g_63 = (-8L);
