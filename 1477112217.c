a1480e6b4335f1289e2db544a3ab46c64d396829
saisi <saisi@users.noreply.github.com>
2016-10-22 04:56:58 +0000

    1477112217 Bardwell

---
+++ 1477112217.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      638191896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 26;
+   unsigned f1 : 7;
+   const signed f2 : 13;
+   signed f3 : 5;
+   signed : 0;
+   volatile signed f4 : 23;
+   signed f5 : 25;
