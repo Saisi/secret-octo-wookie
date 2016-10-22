dd275477e43c821658f48fe4396a99b6af89d838
saisi <saisi@users.noreply.github.com>
2016-10-22 10:27:17 +0000

    1477132037 anthranoyl

---
+++ 1477132037.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1963659853
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   volatile uint16_t  f0;
+   volatile unsigned f1 : 5;
+   volatile signed f2 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x96E6A373L;
+static uint16_t g_8 = 0x13CAL;
+static volatile int64_t g_11 = 0L;/* VOLATILE GLOBAL g_11 */
