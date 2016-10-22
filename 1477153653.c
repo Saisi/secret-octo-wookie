4eae35d1047bf07bf7053105526f4da1fd713bd8
saisi <saisi@users.noreply.github.com>
2016-10-22 16:27:34 +0000

    1477153653 amphitropal

---
+++ 1477153653.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2121479931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 28;
+   volatile unsigned f1 : 3;
+   const signed f2 : 26;
+   volatile unsigned f3 : 29;
+   signed f4 : 26;
+   volatile signed f5 : 11;
+   const unsigned f6 : 30;
+};
+
