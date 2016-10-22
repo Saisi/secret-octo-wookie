168b752bb2fabfb118e6be2699500b64993e3233
saisi <saisi@users.noreply.github.com>
2016-10-22 08:57:12 +0000

    1477126632 backlogging

---
+++ 1477126632.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1175268402
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = (-1L);
+static int8_t * const  volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+static uint64_t g_19 = 3UL;
+static uint8_t g_24 = 0x52L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
