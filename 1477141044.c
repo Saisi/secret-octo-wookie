19efb8a41f073e5796e11a7f018aa5f827649f0d
saisi <saisi@users.noreply.github.com>
2016-10-22 12:57:24 +0000

    1477141044 antiphonies

---
+++ 1477141044.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      779985623
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   const int32_t  f0;
+   volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
