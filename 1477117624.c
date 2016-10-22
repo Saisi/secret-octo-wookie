32cbc7e714498cc9a2a5733ea59443ee249bb71f
saisi <saisi@users.noreply.github.com>
2016-10-22 06:27:05 +0000

    1477117624 antimechanism

---
+++ 1477117624.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2911274541
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint64_t  f1;
+   const int64_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+   const int32_t  f1;
+   volatile int64_t  f2;
