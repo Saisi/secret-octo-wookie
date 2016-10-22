687505be6812d078ec25cc7557b0b92c421145a9
saisi <saisi@users.noreply.github.com>
2016-10-22 01:56:48 +0000

    1477101408 autofecundation

---
+++ 1477101408.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      932494318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 0xB023ED15L;
+static int32_t g_13 = 0L;
+static int64_t g_50 = 0xE291920B2455F992LL;
+static int64_t g_52 = (-1L);
+static uint16_t g_68 = 0x6264L;
+static int8_t g_70 = 6L;
+static const int32_t g_73 = 4L;
+static volatile int32_t g_97[1][4] = {{0L,0L,0L,0L}};
+static int32_t g_183 = 7L;
