fe553ae6a3a7d48bd38f78838b3e52d0c1476436
saisi <saisi@users.noreply.github.com>
2016-10-22 09:57:16 +0000

    1477130236 Azygobranchiata

---
+++ 1477130236.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3911172015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+};
+#pragma pack(pop)
+
+struct S1 {
+   const unsigned f0 : 29;
+   const unsigned f1 : 11;
