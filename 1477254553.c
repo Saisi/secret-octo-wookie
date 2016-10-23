1e4f647d6693ea1541dbe9dbc847fec88788dc5b
saisi <saisi@users.noreply.github.com>
2016-10-23 20:29:14 +0000

    1477254553 albuginea

---
+++ 1477254553.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2247320424
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const signed f1 : 15;
+   const volatile signed f2 : 7;
+   signed f3 : 13;
+   unsigned f4 : 30;
+};
+
+union U1 {
+   int64_t  f0;
