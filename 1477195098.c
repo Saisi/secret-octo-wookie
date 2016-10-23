4ff5bf0219550c897e108467f6fe4b9404b34092
saisi <saisi@users.noreply.github.com>
2016-10-23 03:58:18 +0000

    1477195098 alluvions

---
+++ 1477195098.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2946626530
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-3L);
+static int32_t *g_6 = (void*)0;
+static int32_t ** volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
