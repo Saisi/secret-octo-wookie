2870a0f6ce7315aaaee9566afa82f5a97262e517
saisi <saisi@users.noreply.github.com>
2016-10-23 15:28:56 +0000

    1477236536 amygdalotome

---
+++ 1477236536.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2992527774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3] = {0L,0L,0L};
+static volatile int32_t g_3 = 0x3244D513L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
