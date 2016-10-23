350df00247e320aa6f0b2438bb33b564d3e0126a
saisi <saisi@users.noreply.github.com>
2016-10-23 14:28:54 +0000

    1477232933 babished

---
+++ 1477232933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1755192438
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   int32_t  f1;
+   unsigned f2 : 1;
+   unsigned f3 : 1;
+   signed f4 : 16;
+   volatile unsigned f5 : 18;
+   signed f6 : 15;
+   unsigned f7 : 10;
+   const int64_t  f8;
