04b232921fe884e41f2295ec4986e5b7c799863e
saisi <saisi@users.noreply.github.com>
2016-10-23 08:28:33 +0000

    1477211313 asparagyl

---
+++ 1477211313.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1006037871
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 22;
+   signed f1 : 21;
+   int32_t  f2;
+   const signed f3 : 21;
+   volatile signed f4 : 8;
+   volatile unsigned f5 : 22;
+   volatile unsigned f6 : 1;
+   signed f7 : 23;
+   volatile unsigned f8 : 10;
