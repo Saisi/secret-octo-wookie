8d2c98b80857ea00df13faaa51a7c9d94c819bca
saisi <saisi@users.noreply.github.com>
2016-10-23 06:58:28 +0000

    1477205908 aristo

---
+++ 1477205908.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4038769902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2][5] = {{(-8L),(-8L),(-8L),(-8L),(-8L)},{0x3ED15DE0L,0L,0x3ED15DE0L,0L,0x3ED15DE0L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
