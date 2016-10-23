cf8745afcbf50c4aed12f893e2f41b4e7d55cec9
saisi <saisi@users.noreply.github.com>
2016-10-23 21:29:18 +0000

    1477258157 aposafranine

---
+++ 1477258157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2476964840
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   volatile uint8_t  f1;
+   const int8_t  f2;
+   volatile uint32_t  f3;
+   int8_t  f4;
+};
+#pragma pack(pop)
