b1673371048505e0a642fff014e9bddb1a25240a
saisi <saisi@users.noreply.github.com>
2016-10-23 22:59:21 +0000

    1477263561 baldicoot

---
+++ 1477263561.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      533766762
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int16_t  f0;
+   int8_t * f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_22 = 1L;
+static union U0 g_56 = {0x0435L};/* VOLATILE GLOBAL g_56 */
+static int32_t g_59 = (-1L);
