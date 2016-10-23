d645c09d11810c09d91786a1d793ffd81e6af514
saisi <saisi@users.noreply.github.com>
2016-10-23 18:29:07 +0000

    1477247346 apotropaism

---
+++ 1477247346.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2134992094
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 29;
+   signed f1 : 30;
+   volatile signed f2 : 8;
+   signed f3 : 20;
+   volatile int16_t  f4;
+   signed f5 : 13;
+   const uint16_t  f6;
