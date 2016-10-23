e6d03c06e5879a3f9660b90fabac928b1b51c5f8
saisi <saisi@users.noreply.github.com>
2016-10-23 10:58:41 +0000

    1477220320 aziola

---
+++ 1477220320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      924994666
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 3;
+   signed f1 : 12;
+   unsigned f2 : 30;
+   int32_t  f3;
+   const volatile signed f4 : 25;
+   unsigned f5 : 12;
+};
+
+union U1 {
