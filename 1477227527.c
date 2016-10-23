9154e64fa4a7c512342e5b07bf0d0aef25dbe08a
saisi <saisi@users.noreply.github.com>
2016-10-23 12:58:47 +0000

    1477227527 bannerols

---
+++ 1477227527.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1751521065
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const volatile uint64_t  f1;
+   volatile int32_t  f2;
+   unsigned f3 : 20;
+   const int64_t  f4;
+   uint16_t  f5;
+};
+
+union U1 {
