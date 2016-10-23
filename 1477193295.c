2387f52640161bf68ab4c6e30b8599b03949a41d
saisi <saisi@users.noreply.github.com>
2016-10-23 03:28:17 +0000

    1477193295 Aglauros

---
+++ 1477193295.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      691958487
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 10;
+   signed f1 : 25;
+   const volatile unsigned f2 : 24;
+   volatile int16_t  f3;
+   unsigned f4 : 12;
+   unsigned f5 : 10;
+   unsigned f6 : 5;
