7a9867318abe167276cd6901b72dd9eb8ba25397
saisi <saisi@users.noreply.github.com>
2016-10-23 13:58:52 +0000

    1477231129 Adalheid

---
+++ 1477231129.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2214345306
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   int64_t  f1;
+   int32_t  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+   unsigned f1 : 14;
+   volatile int8_t  f2;
