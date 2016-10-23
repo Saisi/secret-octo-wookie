8a661152779284f9fc332f1ea85830a456ea3d02
saisi <saisi@users.noreply.github.com>
2016-10-23 08:58:35 +0000

    1477213114 Apure

---
+++ 1477213114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3175945826
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t *g_18 = (void*)0;
+static int32_t g_33[9] = {0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L,0x28787D63L};
+static int8_t g_44 = 0xADL;
+static const int32_t *g_50 = &g_33[1];
