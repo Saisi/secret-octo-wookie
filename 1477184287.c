c8964eb0d18bb90c3c52cc46749378444591478d
saisi <saisi@users.noreply.github.com>
2016-10-23 00:58:07 +0000

    1477184287 aborigines

---
+++ 1477184287.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1743352197
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   int32_t  f1;
+   const int32_t  f2;
+   volatile unsigned f3 : 15;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_18 = 0x1637C1D6L;
