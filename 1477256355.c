34cc3b1d42b5c0c862083aa6a501c8a5673036d1
saisi <saisi@users.noreply.github.com>
2016-10-23 20:59:16 +0000

    1477256355 anticonservatism

---
+++ 1477256355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1933585015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   volatile signed f1 : 31;
+   int32_t  f2;
+   volatile int8_t  f3;
+   const uint32_t  f4;
+   int8_t  f5;
+   volatile int32_t  f6;
+};
+
