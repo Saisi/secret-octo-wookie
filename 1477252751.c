be4b4b72b3ad65c2d4983393081dec436bc71c06
saisi <saisi@users.noreply.github.com>
2016-10-23 19:59:12 +0000

    1477252751 Axiopoenus

---
+++ 1477252751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3250924148
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int8_t  f0;
+   const int8_t  f1;
+   const volatile int32_t  f2;
+   uint8_t  f3;
+   int8_t  f4;
+};
+#pragma pack(pop)
