b02433decf2578823d0cdd4c8692db2a5daa7acc
saisi <saisi@users.noreply.github.com>
2016-10-23 17:59:05 +0000

    1477245545 anthracitism

---
+++ 1477245545.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3987579012
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   int64_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_11 = 2L;
+static uint16_t g_15 = 65535UL;
+static uint64_t g_18 = 18446744073709551608UL;
