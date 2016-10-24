58b11b0dac6ffa9b48c5ae64ff8cbf0e76668c92
saisi <saisi@users.noreply.github.com>
2016-10-24 06:59:52 +0000

    1477292391 askewness

---
+++ 1477292391.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1333794324
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   signed f1 : 13;
+   int16_t  f2;
+   uint64_t  f3;
+   volatile int8_t * f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7[3][2][4] = {{{(-7L),0L,(-7L),(-7L)},{0L,0L,9L,0L}},{{0L,(-7L),(-7L),0L},{(-7L),0L,(-7L),(-7L)}},{{0L,0L,9L,0L},{0L,(-7L),(-7L),0L}}};
