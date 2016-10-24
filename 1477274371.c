9bdb4101d8828ce9e5b56b01f8fd59ed175efd63
saisi <saisi@users.noreply.github.com>
2016-10-24 01:59:32 +0000

    1477274371 allopolyploid

---
+++ 1477274371.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3296994344
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[6] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint16_t g_14 = 0x8963L;
+static uint64_t g_24 = 0xEB0A117216709E14LL;
+static volatile int32_t g_66 = 0x791B1675L;/* VOLATILE GLOBAL g_66 */
+static volatile int32_t g_67 = 0xA600AC80L;/* VOLATILE GLOBAL g_67 */
+static volatile int32_t g_68 = 0xF554C62BL;/* VOLATILE GLOBAL g_68 */
+static volatile int32_t g_69 = 0x11659018L;/* VOLATILE GLOBAL g_69 */
+static volatile int32_t g_70[3] = {0L,0L,0L};
+static int32_t g_71[4][3][3] = {{{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)}},{{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)}},{{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)}},{{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)},{(-9L),0x3C1DC0ACL,(-9L)}}};
