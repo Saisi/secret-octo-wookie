32ff7424b2374363ad2bca6dae54d4e9bd5653aa
saisi <saisi@users.noreply.github.com>
2016-10-24 16:30:37 +0000

    1477326636 adfreezing

---
+++ 1477326636.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      558827653
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const volatile signed f1 : 22;
+   int32_t  f2;
+   int64_t  f3;
+   const int8_t  f4;
+   volatile int32_t  f5;
+   volatile int32_t  f6;
+   volatile uint64_t  f7;
+   uint8_t  f8;
