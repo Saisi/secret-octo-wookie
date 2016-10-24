9ced3fdaba501324ad91b5656e7b2e7442f62286
saisi <saisi@users.noreply.github.com>
2016-10-24 13:30:27 +0000

    1477315826 amidulin

---
+++ 1477315826.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2003453316
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+   volatile int64_t  f1;
+   uint8_t  f2;
+   int32_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_6 = 0x4DDBL;
