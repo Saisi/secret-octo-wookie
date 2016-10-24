9f51f885831bdc4d378130fbce9576651141c21b
saisi <saisi@users.noreply.github.com>
2016-10-24 07:29:53 +0000

    1477294193 baetuli

---
+++ 1477294193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3676348118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   volatile int64_t  f1;
+};
+
+union U1 {
+   uint32_t  f0;
+   signed f1 : 9;
+   const int32_t  f2;
+};
