f4ffb6ef681555495c557e2041385bee57264c74
saisi <saisi@users.noreply.github.com>
2016-10-24 12:30:20 +0000

    1477312219 alberca

---
+++ 1477312219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3783486987
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+};
+
+union U1 {
+   volatile uint64_t  f0;
+   int32_t  f1;
+};
+
+union U2 {
