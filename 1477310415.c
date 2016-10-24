c1042717935252c65d03a52a5821a71a50bbc992
saisi <saisi@users.noreply.github.com>
2016-10-24 12:00:18 +0000

    1477310415 Akiskemikinik

---
+++ 1477310415.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4243147182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 5;
+   signed f1 : 6;
+   volatile unsigned f2 : 16;
+   const signed f3 : 28;
+   uint64_t  f4;
+   const unsigned f5 : 21;
+   unsigned f6 : 2;
