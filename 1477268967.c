a684b053c8e2787975dcb11ddfb50162837ea89f
saisi <saisi@users.noreply.github.com>
2016-10-24 00:29:28 +0000

    1477268967 Anapterygota

---
+++ 1477268967.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      186492012
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x53AA9AC7L;
+static volatile int32_t g_8 = (-9L);/* VOLATILE GLOBAL g_8 */
+static volatile int32_t *g_7 = &g_8;
+static int32_t g_24[9] = {1L,1L,1L,1L,1L,1L,1L,1L,1L};
