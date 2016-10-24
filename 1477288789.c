7983cc59023e5ae3ef295f3ba9515c0a2692ee06
saisi <saisi@users.noreply.github.com>
2016-10-24 05:59:49 +0000

    1477288789 Algonkin

---
+++ 1477288789.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      348012111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xE87F6FB8L;
+static int16_t g_5 = 0x57D6L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
