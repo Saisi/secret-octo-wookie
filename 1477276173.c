d42db7efc388f7b21aca0be9a7747ad57d9fa2bb
saisi <saisi@users.noreply.github.com>
2016-10-24 02:29:33 +0000

    1477276173 Alcaids

---
+++ 1477276173.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1307322012
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x1A049FE3L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xB0B02971L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-4L);
+static int32_t g_8 = 0x170CD509L;
+static int64_t g_51 = 5L;
+static int64_t g_53 = 0xAE2646899DB2213ALL;
+static int32_t g_69 = (-1L);
+static int32_t *g_68 = &g_69;
+static int64_t *g_75 = &g_51;
