4a01655d9600efdd506d4e261c3688a608dfe35c
saisi <saisi@users.noreply.github.com>
2016-10-24 22:01:01 +0000

    1477346460 archdeanery

---
+++ 1477346460.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1890253362
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 7;
+   volatile unsigned : 0;
+   volatile unsigned f1 : 2;
+   unsigned f2 : 6;
+   signed f3 : 29;
+   const volatile unsigned : 0;
+   signed f4 : 30;
+   const uint8_t  f5;
+   const signed f6 : 2;
