f980707bd4e89158b3c44d4f93b270b28e0e5557
saisi <saisi@users.noreply.github.com>
2016-10-24 20:00:51 +0000

    1477339250 Anethum

---
+++ 1477339250.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1845095109
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int16_t  f1;
+   int64_t  f2;
+   int8_t * volatile  f3;
+   const uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x51DBA339L;
