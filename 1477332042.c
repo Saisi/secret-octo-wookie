1d9ca195547aafca00ba929ff2f798f3bf452967
saisi <saisi@users.noreply.github.com>
2016-10-24 18:00:43 +0000

    1477332042 argillocalcareous

---
+++ 1477332042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3669865292
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_48 = 0xCF5AA05DL;
+static int32_t * const g_47 = &g_48;
+static uint8_t g_75 = 2UL;
+static int16_t g_77 = (-9L);
+static int32_t *g_127 = (void*)0;
+static int32_t ** volatile g_126 = &g_127;/* VOLATILE GLOBAL g_126 */
+static int8_t g_152 = 0x17L;
+static int8_t g_154 = 0xD4L;
+static volatile int16_t g_179 = 8L;/* VOLATILE GLOBAL g_179 */
