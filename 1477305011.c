9562d7795f5afd70a0b7b63af7780744b00c6140
saisi <saisi@users.noreply.github.com>
2016-10-24 10:30:11 +0000

    1477305011 autaesthesy

---
+++ 1477305011.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3644156774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile int8_t  f1;
+   int64_t  f2;
+   const int16_t  f3;
+   const volatile int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 18446744073709551615UL;
