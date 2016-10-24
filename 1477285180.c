37cf66fde87a5bfd2247522607d43653bd63b9e7
saisi <saisi@users.noreply.github.com>
2016-10-24 04:59:43 +0000

    1477285180 antiketogen

---
+++ 1477285180.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2748495691
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile uint16_t  f1;
+   volatile uint8_t  f2;
+   uint32_t  f3;
+   const uint8_t  f4;
+   const int64_t  f5;
+   volatile int32_t  f6;
+};
+
