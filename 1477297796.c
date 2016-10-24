b99f05caa8eaa9383c8c57540ca16a40e4106e25
saisi <saisi@users.noreply.github.com>
2016-10-24 08:29:57 +0000

    1477297796 aflow

---
+++ 1477297796.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2542659160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+   const unsigned f1 : 7;
+   signed f2 : 13;
+   const unsigned f3 : 5;
+   unsigned : 0;
+   volatile unsigned f4 : 23;
+   signed f5 : 25;
+};
+
