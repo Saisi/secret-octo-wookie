fc34566ea11757a6ea358bbaee7e60eff70f5846
saisi <saisi@users.noreply.github.com>
2016-10-24 09:00:00 +0000

    1477299598 adenosarcomas

---
+++ 1477299598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2423032982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   int8_t  f1;
+   volatile unsigned f2 : 18;
+   signed f3 : 12;
+   volatile unsigned f4 : 1;
+   signed f5 : 9;
+   uint16_t  f6;
