e79331f3af8983717299cf7b9b59270ea2e304d3
saisi <saisi@users.noreply.github.com>
2016-10-24 11:00:12 +0000

    1477306812 admedian

---
+++ 1477306812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2635088832
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_12 = 0xA7FE64FD9E2A8D3ALL;
+static int32_t g_14 = (-8L);
+static int32_t g_61 = 0xD0C653DDL;
+static int32_t g_69 = (-9L);
+static uint8_t g_86 = 0x0EL;
+static int16_t g_102 = 5L;
+static int8_t g_129[9][7] = {{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L}};
+static int8_t g_131 = 8L;
+static int32_t * volatile g_132 = (void*)0;/* VOLATILE GLOBAL g_132 */
