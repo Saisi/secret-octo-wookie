20462c1059042626abc84a82c4a47dd14baff5bf
saisi <saisi@users.noreply.github.com>
2016-10-24 21:00:58 +0000

    1477342857 antigigmanic

---
+++ 1477342857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      448388007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 18;
+   unsigned f1 : 1;
+   volatile unsigned : 0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint8_t  f0;
