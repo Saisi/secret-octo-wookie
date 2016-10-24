9deb81030bac642dfa36e144db289ce2ec10a7fc
saisi <saisi@users.noreply.github.com>
2016-10-24 10:00:08 +0000

    1477303207 allelopathy

---
+++ 1477303207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3459893282
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   volatile int8_t  f1;
+   uint8_t  f2;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
