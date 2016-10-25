5d9d16ec85024693b8cb39c51c66fa4fc55f9bfa
saisi <saisi@users.noreply.github.com>
2016-10-25 19:32:13 +0000

    1477423933 Albanese

---
+++ 1477423933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3356777468
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 12;
+   volatile signed f1 : 17;
+   volatile unsigned f2 : 15;
+   const volatile unsigned f3 : 31;
+   unsigned f4 : 18;
+};
+
+union U1 {
+   volatile unsigned f0 : 13;
