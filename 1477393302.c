6298886f99865c4387dc8747e84c66f841065564
saisi <saisi@users.noreply.github.com>
2016-10-25 11:01:43 +0000

    1477393302 angiogram

---
+++ 1477393302.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3052102518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_23 = 0xF98CAFA32FD2D1A4LL;
+static uint32_t g_26[1] = {9UL};
+static uint64_t g_38 = 0x3CEF339200279E56LL;
+static int32_t g_60[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t *g_63 = &g_60[8];
+static int32_t ** volatile g_62 = &g_63;/* VOLATILE GLOBAL g_62 */
+static uint32_t g_66 = 0x121ADF56L;
+static uint32_t g_72 = 18446744073709551614UL;
+static uint32_t g_81 = 1UL;
