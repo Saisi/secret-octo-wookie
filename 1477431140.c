23b9f3915bd981a1a926c99ee63e505638868c75
saisi <saisi@users.noreply.github.com>
2016-10-25 21:32:20 +0000

    1477431140 atherine

---
+++ 1477431140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      246195888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_8 = 0xA9565E08CCCDDD1DLL;
+static uint64_t g_23 = 1UL;
+static int32_t g_26 = 0L;
+static int32_t * const g_25 = &g_26;
+static int32_t g_67[3][1] = {{0x576105F6L},{0x576105F6L},{0x576105F6L}};
+static uint32_t g_103 = 0x3298C059L;
+static int16_t g_115 = 0xABD0L;
+static uint8_t g_117 = 249UL;
+static int32_t g_119[1] = {0x73401CF6L};
