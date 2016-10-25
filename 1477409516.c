ed9d64233eb567a9c964bd931cf96213fa10b9e1
saisi <saisi@users.noreply.github.com>
2016-10-25 15:31:56 +0000

    1477409516 auxanometer

---
+++ 1477409516.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4017696282
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_5 = 0L;
+static int64_t g_10 = 2L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
