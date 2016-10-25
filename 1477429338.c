d36e484151b764f02fa3bf2f7e603f243907d82d
saisi <saisi@users.noreply.github.com>
2016-10-25 21:02:19 +0000

    1477429338 antimonarchal

---
+++ 1477429338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1395777782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t g_4 = 0x9C5A654FL;
+static int32_t g_41 = 1L;
+static int32_t *g_90 = &g_3;
+static int32_t ** volatile g_89 = &g_90;/* VOLATILE GLOBAL g_89 */
+static uint64_t g_91 = 1UL;
+static uint32_t g_95 = 0x1DF95200L;
+static uint8_t g_102 = 8UL;
+static uint64_t g_103 = 0UL;
