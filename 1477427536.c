19337065aa3eea943985bbe3d1c248d79ac8d568
saisi <saisi@users.noreply.github.com>
2016-10-25 20:32:17 +0000

    1477427536 appled

---
+++ 1477427536.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      234372235
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint8_t  f1;
+};
+
+union U1 {
+   volatile int8_t * const  f0;
+   const uint16_t  f1;
+   volatile uint32_t  f2;
+   const volatile unsigned f3 : 2;
