2bfc3378ccae1aa7574f025c38aab1b3e3361412
saisi <saisi@users.noreply.github.com>
2016-10-25 06:31:26 +0000

    1477377086 assiette

---
+++ 1477377086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4197813827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_20 = 0x7183L;
+static int32_t g_44 = 0x9C043CA3L;
+static int32_t *g_51 = &g_44;
+static int64_t g_77 = 1L;
+static volatile uint8_t g_87 = 0UL;/* VOLATILE GLOBAL g_87 */
+static int32_t **g_91 = &g_51;
+static int32_t *** const  volatile g_90[5][6][3] = {{{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91}}};
+static int32_t *** volatile g_92[6][5][2] = {{{&g_91,(void*)0},{(void*)0,&g_91},{&g_91,&g_91},{(void*)0,(void*)0},{&g_91,&g_91}},{{&g_91,&g_91},{&g_91,(void*)0},{&g_91,&g_91},{&g_91,&g_91},{&g_91,&g_91}},{{&g_91,(void*)0},{&g_91,&g_91},{&g_91,&g_91},{&g_91,(void*)0},{(void*)0,&g_91}},{{&g_91,&g_91},{(void*)0,(void*)0},{&g_91,&g_91},{&g_91,&g_91},{&g_91,(void*)0}},{{&g_91,&g_91},{&g_91,&g_91},{&g_91,&g_91},{&g_91,(void*)0},{&g_91,&g_91}},{{&g_91,&g_91},{&g_91,(void*)0},{(void*)0,&g_91},{&g_91,&g_91},{(void*)0,(void*)0}}};
+static uint64_t g_127 = 8UL;
