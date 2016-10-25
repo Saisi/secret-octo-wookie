60927919440470d25c0fecf92954551f7335061c
saisi <saisi@users.noreply.github.com>
2016-10-25 10:01:40 +0000

    1477389699 atabal

---
+++ 1477389699.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      870312360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 2;
+   int8_t  f1;
+   uint32_t  f2;
+   uint32_t  f3;
+   volatile uint64_t  f4;
+};
+
+union U1 {
+   volatile signed f0 : 11;
