c03882849dcaf4b9b5dac03def008a38fda7cd62
saisi <saisi@users.noreply.github.com>
2016-10-25 00:01:09 +0000

    1477353669 Aornum

---
+++ 1477353669.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2309504965
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   volatile signed f1 : 20;
+   signed f2 : 7;
+   unsigned f3 : 22;
+   signed f4 : 15;
+   signed f5 : 12;
+};
+
+#pragma pack(push)
