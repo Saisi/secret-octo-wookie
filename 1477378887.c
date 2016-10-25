08120aeb8de1d49437ea7d2ae8854acbde91b38e
saisi <saisi@users.noreply.github.com>
2016-10-25 07:01:27 +0000

    1477378887 antiscabious

---
+++ 1477378887.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1800325680
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 6;
+   unsigned f1 : 3;
+   const int16_t  f2;
+   uint8_t  f3;
+   volatile uint64_t  f4;
+};
+#pragma pack(pop)
