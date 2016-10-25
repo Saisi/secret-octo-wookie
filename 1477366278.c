742bfc33f4e2474b8a271fff3fdaec96a87c9feb
saisi <saisi@users.noreply.github.com>
2016-10-25 03:31:18 +0000

    1477366278 antipyonin

---
+++ 1477366278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2560581185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   int64_t  f2;
+   volatile int32_t  f3;
+   unsigned f4 : 14;
+   const volatile uint8_t  f5;
+};
