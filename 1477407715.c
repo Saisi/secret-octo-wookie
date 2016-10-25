ae994a73116fc536dd4a54bfeff123035de9c139
saisi <saisi@users.noreply.github.com>
2016-10-25 15:01:55 +0000

    1477407715 anorogenic

---
+++ 1477407715.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1240495795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   uint16_t  f1;
+   volatile int32_t  f2;
+   uint32_t  f3;
+   int64_t  f4;
+};
+#pragma pack(pop)
