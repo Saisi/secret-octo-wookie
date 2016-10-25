7d8b5a167f04293b9ce2d008b7d2582ace7b1730
saisi <saisi@users.noreply.github.com>
2016-10-25 14:31:54 +0000

    1477405914 babuism

---
+++ 1477405914.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3755272443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[10] = {0x76AABECBL,0L,0x76AABECBL,0x76AABECBL,0L,0x76AABECBL,0x76AABECBL,0L,0x76AABECBL,0x76AABECBL};
+static uint16_t g_7 = 0x5E4BL;
+static int32_t g_9 = 0x94B2AB76L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
