9403840caf79c1b147a7f52efb085a7ed44a5ca5
saisi <saisi@users.noreply.github.com>
2016-10-25 04:31:21 +0000

    1477369881 actinoelectricity

---
+++ 1477369881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2976845582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint8_t  f0;
+   volatile signed f1 : 18;
+   unsigned f2 : 30;
+   signed f3 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_32 = (void*)0;
+static volatile int64_t g_37 = 0x53494ABC24891023LL;/* VOLATILE GLOBAL g_37 */
