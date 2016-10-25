b80f6b41749f083cef58a62804073ff8f3a25a10
saisi <saisi@users.noreply.github.com>
2016-10-25 10:31:41 +0000

    1477391501 atlantes

---
+++ 1477391501.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1012626406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   volatile int8_t  f0;
+   signed f1 : 21;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_10 = 0x50B5F0B1L;/* VOLATILE GLOBAL g_10 */
+static union U2 g_28 = {0xEFL};/* VOLATILE GLOBAL g_28 */
+static union U2 g_29[5][6] = {{{0x59L},{-1L},{0x59L},{0x59L},{-1L},{0x59L}},{{0x59L},{-1L},{0x59L},{0x59L},{-1L},{0x59L}},{{0x59L},{-1L},{0x59L},{0x59L},{-1L},{0x59L}},{{0x59L},{-1L},{0x59L},{0x59L},{-1L},{0x59L}},{{0x59L},{-1L},{0x59L},{0x59L},{-1L},{0x59L}}};
+static union U2 g_30[2] = {{0x48L},{0x48L}};
