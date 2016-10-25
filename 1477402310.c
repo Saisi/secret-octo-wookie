9df48723a7ff07f4b3a243054811ab47033dca69
saisi <saisi@users.noreply.github.com>
2016-10-25 13:31:51 +0000

    1477402310 adarticulation

---
+++ 1477402310.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      255652905
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 19;
+   volatile unsigned f1 : 30;
+};
+
+union U1 {
+   volatile unsigned f0 : 10;
+   int32_t  f1;
+};
+
