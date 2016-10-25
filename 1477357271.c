36e4334519c22f14a6f631ec09ef632f36ea875b
saisi <saisi@users.noreply.github.com>
2016-10-25 01:01:11 +0000

    1477357271 angiocarpy

---
+++ 1477357271.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2858218567
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = 0L;
+static int16_t g_12 = 1L;
+static int32_t g_14[10] = {(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L)};
+static int32_t * const  volatile g_13[4][6] = {{&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5]},{&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5]},{&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5]},{&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5],&g_14[5]}};
+static int32_t * volatile g_15[10] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t * volatile g_16[2] = {&g_14[5],&g_14[5]};
+static int32_t * volatile g_17 = &g_14[4];/* VOLATILE GLOBAL g_17 */
+static int8_t g_53 = 1L;
+static int16_t *g_57 = &g_12;
