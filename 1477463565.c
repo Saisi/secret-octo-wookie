7b1949274c9b7bf79866102c62e57ff0c80435ba
saisi <saisi@users.noreply.github.com>
2016-10-26 06:32:45 +0000

    1477463565 aloemodin

---
+++ 1477463565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3226544798
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   int8_t * f0;
+   int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U3 g_2 = {0};
+
+
+/* --- FORWARD DECLARATIONS --- */
