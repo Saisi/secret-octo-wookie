db14bd32b0d0c12d95ccce7e25c8ef9ea806c3cb
saisi <saisi@users.noreply.github.com>
2016-10-26 17:33:24 +0000

    1477503204 bardling

---
+++ 1477503204.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2912621872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 6;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[2] = {0x3C69AED2L,0x3C69AED2L};
+static int32_t g_4 = 0L;
