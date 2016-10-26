9af9e3673f0bc671f60f3c5f276167c0b9b01de1
saisi <saisi@users.noreply.github.com>
2016-10-26 15:03:17 +0000

    1477494197 antialien

---
+++ 1477494197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3978723042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S5 {
+   signed f0 : 24;
+   signed f1 : 28;
+   unsigned f2 : 12;
+   volatile signed f3 : 27;
+};
+
+union U11 {
+   uint64_t  f0;
+};
