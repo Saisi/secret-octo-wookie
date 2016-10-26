c7b95e96c7758a2fb410383d32ca3678fa159b5b
saisi <saisi@users.noreply.github.com>
2016-10-26 07:32:48 +0000

    1477467167 arthritides

---
+++ 1477467167.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3683145155
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-9L);
+static int32_t g_7[9] = {0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL,0x4351CDCBL};
+static int32_t *g_9 = &g_7[0];
+static int32_t * volatile *g_8[4][3] = {{&g_9,&g_9,&g_9},{&g_9,&g_9,&g_9},{&g_9,&g_9,&g_9},{&g_9,&g_9,&g_9}};
+static volatile int32_t g_11 = 0x95196EE4L;/* VOLATILE GLOBAL g_11 */
+static volatile int32_t g_12 = (-6L);/* VOLATILE GLOBAL g_12 */
+static int32_t g_13 = (-7L);
+static const int32_t g_25 = 0x08E2294BL;
+static volatile int32_t g_36 = 6L;/* VOLATILE GLOBAL g_36 */
