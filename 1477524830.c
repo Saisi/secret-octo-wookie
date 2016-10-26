5e3715b5d1b51b602d1939511897d2a79089ebdd
saisi <saisi@users.noreply.github.com>
2016-10-26 23:33:50 +0000

    1477524830 arbota

---
+++ 1477524830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1334222412
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   const int64_t  f2;
+   uint32_t  f3;
+   const uint16_t  f4;
+};
+
+struct S1 {
+   int32_t  f0;
