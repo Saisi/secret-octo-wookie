403698f84ec2c3e9b16bc4cb9b466127b28fd3c4
saisi <saisi@users.noreply.github.com>
2016-10-26 03:02:35 +0000

    1477450955 annihilators

---
+++ 1477450955.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2623450006
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[1][8] = {{0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L}};
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 9L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 1L;
+static uint16_t g_25[8][1][8] = {{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}},{{0x6735L,0UL,0UL,0x6735L,0x6735L,0UL,0UL,0x6735L}}};
+static int32_t g_34[6] = {0x121589AFL,0xA8629342L,0x121589AFL,0x121589AFL,0xA8629342L,0x121589AFL};
+static int32_t g_70 = (-1L);
+static int32_t * volatile g_69 = &g_70;/* VOLATILE GLOBAL g_69 */
+static volatile int32_t g_79 = 0L;/* VOLATILE GLOBAL g_79 */
