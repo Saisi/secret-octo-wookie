4f2531fce1c233ae74803fbfa71f7eb264cbbdf1
saisi <saisi@users.noreply.github.com>
2016-10-26 10:02:56 +0000

    1477476176 Argo

---
+++ 1477476176.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2892846404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint16_t g_4 = 0x0520L;
+static int64_t g_38 = 0x166C1932E3CA3691LL;
+static int64_t g_57[9] = {6L,6L,6L,6L,6L,6L,6L,6L,6L};
+static int32_t g_59 = 7L;
+static uint8_t g_65 = 0xB9L;
+static int32_t * volatile g_69 = (void*)0;/* VOLATILE GLOBAL g_69 */
+static int32_t * volatile * volatile g_71 = &g_69;/* VOLATILE GLOBAL g_71 */
+static int32_t g_75[4] = {2L,2L,2L,2L};
+static int32_t g_94 = 0x76E66819L;
