e49d76ee0f1c6d0f17e9f9f05f70744eaefa699f
saisi <saisi@users.noreply.github.com>
2016-10-26 22:33:47 +0000

    1477521226 Ardoch

---
+++ 1477521226.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2930702101
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 9;
+   const unsigned f1 : 28;
+   unsigned f2 : 15;
+   unsigned f3 : 30;
+   const volatile unsigned f4 : 23;
+   int64_t  f5;
+   const signed f6 : 8;
