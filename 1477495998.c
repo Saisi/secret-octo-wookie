561d6bdb4f25e058921af2613bca4c9e2a34710f
saisi <saisi@users.noreply.github.com>
2016-10-26 15:33:18 +0000

    1477495998 aphodus

---
+++ 1477495998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1600372768
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   const volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-6L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0L;
+static union U0 g_7 = {0xABL};/* VOLATILE GLOBAL g_7 */
