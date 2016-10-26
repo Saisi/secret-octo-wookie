03c8a38059cc7dee73f780b0f56863395291ba8d
saisi <saisi@users.noreply.github.com>
2016-10-26 04:02:38 +0000

    1477454558 andamenta

---
+++ 1477454558.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      439867410
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xF82FF40FL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+static int32_t g_10 = 0x59141A9CL;
+static int32_t * volatile g_9 = &g_10;/* VOLATILE GLOBAL g_9 */
+static uint64_t g_11[3][3] = {{18446744073709551615UL,18446744073709551615UL,18446744073709551615UL},{18446744073709551615UL,18446744073709551615UL,18446744073709551615UL},{18446744073709551615UL,18446744073709551615UL,18446744073709551615UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
