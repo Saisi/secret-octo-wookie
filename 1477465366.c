dd14281ed71f4061c04847fd1a733fa01a04726a
saisi <saisi@users.noreply.github.com>
2016-10-26 07:02:46 +0000

    1477465366 badlands

---
+++ 1477465366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1116027726
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = (-1L);
+static int32_t g_28 = 1L;
+static int32_t *g_27 = &g_28;
+static const int32_t g_68[10] = {0xFDEF05DEL,6L,0xFDEF05DEL,0xFDEF05DEL,6L,0xFDEF05DEL,0xFDEF05DEL,6L,0xFDEF05DEL,0xFDEF05DEL};
+static const int32_t *g_67[9][10] = {{&g_68[8],&g_68[8],&g_68[2],&g_68[7],&g_68[8],&g_68[8],&g_68[8],&g_68[7],&g_68[2],&g_68[8]},{(void*)0,&g_68[8],&g_68[9],(void*)0,(void*)0,(void*)0,(void*)0,&g_68[8],&g_68[8],(void*)0},{&g_68[9],&g_68[8],&g_68[8],(void*)0,&g_68[8],(void*)0,(void*)0,&g_68[8],&g_68[0],&g_68[0]},{(void*)0,&g_68[7],&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[8],(void*)0,&g_68[8],&g_68[8]},{&g_68[9],&g_68[8],&g_68[3],&g_68[8],&g_68[8],&g_68[8],&g_68[9],(void*)0,&g_68[8],&g_68[9]},{&g_68[8],(void*)0,&g_68[3],&g_68[8],(void*)0,(void*)0,&g_68[8],(void*)0,&g_68[8],(void*)0},{&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[9],&g_68[8],&g_68[8]},{&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[3],&g_68[8],(void*)0,&g_68[8],&g_68[8],&g_68[8]},{&g_68[8],&g_68[8],(void*)0,&g_68[3],&g_68[8],&g_68[8],&g_68[8],&g_68[8],&g_68[3],(void*)0}};
+static uint64_t g_80 = 0x88F3C25A1B5826CCLL;
+static volatile int32_t g_86[6] = {0x7BAD6E3FL,0x7BAD6E3FL,0x7BAD6E3FL,0x7BAD6E3FL,0x7BAD6E3FL,0x7BAD6E3FL};
+static int32_t g_105[4][6] = {{1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L}};
+static int32_t * volatile g_106 = (void*)0;/* VOLATILE GLOBAL g_106 */
