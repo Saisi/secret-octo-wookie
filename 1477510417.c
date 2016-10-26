bfd1eb8894125a7ca2c1305bb759e1e91ccb4cc8
saisi <saisi@users.noreply.github.com>
2016-10-26 19:33:38 +0000

    1477510417 Authon

---
+++ 1477510417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1154128475
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int8_t * volatile  f1;
+   int16_t  f2;
+   uint16_t  f3;
+   const unsigned f4 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 1L;
