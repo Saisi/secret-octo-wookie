c2d94211dc3b3064d4dc089a9178fe80222589fc
saisi <saisi@users.noreply.github.com>
2016-10-26 08:02:50 +0000

    1477468969 Amenism

---
+++ 1477468969.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2126400261
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   uint64_t  f1;
+   int8_t * f2;
+   uint16_t  f3;
+   int32_t  f4;
+};
+
+union U1 {
+   volatile uint16_t  f0;
