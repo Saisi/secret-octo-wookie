f82221f7dce200ab8c66572f641e4e73cef6face
saisi <saisi@users.noreply.github.com>
2016-10-26 16:03:20 +0000

    1477497799 adenological

---
+++ 1477497799.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3686607777
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t  f1;
+   uint32_t  f2;
+   int32_t  f3;
+   signed f4 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x9F4288A1L;
