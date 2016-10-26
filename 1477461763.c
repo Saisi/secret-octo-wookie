65972547f332b64ce4a25c1cbef6642608ab61f3
saisi <saisi@users.noreply.github.com>
2016-10-26 06:02:44 +0000

    1477461763 argyrodite

---
+++ 1477461763.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      985542144
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 14;
+   const signed f1 : 11;
+   unsigned f2 : 1;
+   unsigned f3 : 9;
+   volatile unsigned f4 : 20;
+   signed f5 : 27;
+   signed f6 : 13;
