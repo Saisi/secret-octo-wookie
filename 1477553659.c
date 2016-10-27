ddb8604db54cca81963a0fca61b32f3a15f76e67
saisi <saisi@users.noreply.github.com>
2016-10-27 07:34:20 +0000

    1477553659 annulosa

---
+++ 1477553659.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3432420334
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   unsigned f1 : 2;
+};
+
+struct S1 {
+   const signed f0 : 13;
+   uint32_t  f1;
+   int32_t  f2;
+   const int16_t  f3;
