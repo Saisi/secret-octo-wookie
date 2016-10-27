3bd40bcb0a10c5f2b09faadb214b661be382e06b
saisi <saisi@users.noreply.github.com>
2016-10-27 15:04:50 +0000

    1477580690 autocytolysis

---
+++ 1477580690.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2027103304
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * f0;
+   volatile int16_t  f1;
+   const uint32_t  f2;
+   volatile int32_t  f3;
+   volatile uint32_t  f4;
+};
+
+union U1 {
+   const int8_t * f0;
