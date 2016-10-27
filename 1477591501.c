065aba89a86b0ff81fa1edfcb224fdbb6d3cf1cf
saisi <saisi@users.noreply.github.com>
2016-10-27 18:05:02 +0000

    1477591501 Asta

---
+++ 1477591501.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2513795900
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   int16_t  f1;
+   const int8_t * f2;
+   uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = (-9L);
+static const int8_t *g_2[7][7][5] = {{{&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0},{&g_3,(void*)0,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3}},{{&g_3,(void*)0,&g_3,(void*)0,&g_3},{&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0},{&g_3,(void*)0,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3}},{{(void*)0,&g_3,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3},{&g_3,(void*)0,(void*)0,&g_3,&g_3},{&g_3,(void*)0,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3}},{{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3}},{{&g_3,&g_3,&g_3,(void*)0,(void*)0},{(void*)0,(void*)0,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3}},{{&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,(void*)0,&g_3,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0}},{{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,(void*)0,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,(void*)0,&g_3,(void*)0}}};
