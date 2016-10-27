992f2d44ff95a591876667265b3d033ab5186735
saisi <saisi@users.noreply.github.com>
2016-10-27 17:04:58 +0000

    1477587898 alcyone

---
+++ 1477587898.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1109556524
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   volatile uint32_t  f1;
+   uint64_t  f2;
+   volatile int16_t  f3;
+};
+
+union U1 {
+   int32_t  f0;
+   int64_t  f1;
