b26f9498cebf51307163f13a3b4191810f25b097
saisi <saisi@users.noreply.github.com>
2016-10-27 04:34:10 +0000

    1477542849 anginophobia

---
+++ 1477542849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2060153108
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   const int8_t * f1;
+   const volatile uint16_t  f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   unsigned f0 : 29;
+   const int16_t  f1;
