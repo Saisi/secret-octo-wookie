30d0712bd31b3320da40590308d779a955cad2f5
saisi <saisi@users.noreply.github.com>
2016-10-27 13:34:45 +0000

    1477575283 anthropolithic

---
+++ 1477575283.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      833976868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 18;
+   const volatile signed f1 : 30;
+   unsigned f2 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x61F66568L;
+static uint32_t g_32 = 0xD3672969L;
