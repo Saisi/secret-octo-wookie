48e4a81b7673eb65f2f85d133cfabf970689570f
saisi <saisi@users.noreply.github.com>
2016-10-27 12:34:40 +0000

    1477571679 adenoids

---
+++ 1477571679.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1070565494
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   const volatile uint32_t  f1;
+   signed f2 : 2;
+   volatile uint32_t  f3;
+   const volatile uint64_t  f4;
+   const volatile unsigned f5 : 25;
+};
