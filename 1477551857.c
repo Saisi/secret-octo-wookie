a887e4bd4a3331e294e970de1f42202adb38ba5f
saisi <saisi@users.noreply.github.com>
2016-10-27 07:04:18 +0000

    1477551857 aquarians

---
+++ 1477551857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      243054167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   unsigned f1 : 25;
+   unsigned f2 : 24;
+};
+
+union U1 {
+   uint8_t  f0;
+   int32_t  f1;
+};
