8d2186b69c307724a3e08900a6419f0aec83b5e4
saisi <saisi@users.noreply.github.com>
2016-10-27 22:05:27 +0000

    1477605927 attentionality

---
+++ 1477605927.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2724542669
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U4 {
+   int8_t * const  f0;
+   volatile int8_t * f1;
+   const volatile unsigned f2 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static uint64_t g_7[4] = {1UL,1UL,1UL,1UL};
+static union U4 g_10 = {0};/* VOLATILE GLOBAL g_10 */
