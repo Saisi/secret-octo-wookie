012bc10888187f5b8573af0d6abbfbdc177b0da4
saisi <saisi@users.noreply.github.com>
2016-10-27 16:04:54 +0000

    1477584293 amortisable

---
+++ 1477584293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      578981466
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+union U1 {
+   const volatile int8_t * f0;
+   volatile int32_t  f1;
+   int16_t  f2;
+   uint32_t  f3;
+};
