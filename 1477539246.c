65a6bc0adcd10c5ed2e407510186a26aecbad8a9
saisi <saisi@users.noreply.github.com>
2016-10-27 03:34:06 +0000

    1477539246 bachelorship

---
+++ 1477539246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3515339850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   volatile int64_t  f2;
+   volatile uint32_t  f3;
+   uint32_t  f4;
+   int16_t  f5;
+   uint32_t  f6;
+   volatile int32_t  f7;
+   int64_t  f8;
