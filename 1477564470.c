a48dcb20086896e740eb98fd0f077de6d48c4309
saisi <saisi@users.noreply.github.com>
2016-10-27 10:34:30 +0000

    1477564470 alethiology

---
+++ 1477564470.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3031547741
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+};
+
+union U1 {
+   const volatile int8_t  f0;
+   const uint16_t  f1;
+};
+
