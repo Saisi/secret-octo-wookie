b2e8f4919d70ff2c5dcf24bd2c60eec124733d7d
saisi <saisi@users.noreply.github.com>
2016-10-27 17:35:00 +0000

    1477589699 adiposogenital

---
+++ 1477589699.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3928204675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+};
+
+union U1 {
+   int8_t  f0;
+   const int16_t  f1;
+   volatile uint32_t  f2;
+};
+
