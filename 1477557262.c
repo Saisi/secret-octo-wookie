64b5b49c2d54c009331e1219dd354d4cb2064c8c
saisi <saisi@users.noreply.github.com>
2016-10-27 08:34:24 +0000

    1477557262 Atnah

---
+++ 1477557262.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1443074940
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+   const signed f1 : 17;
+   signed f2 : 15;
+   volatile unsigned f3 : 31;
+   int32_t  f4;
+};
+
+struct S1 {
+   signed f0 : 2;
