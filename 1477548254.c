72ed5495da91f83906ee7840b8e61bd5aa89a155
saisi <saisi@users.noreply.github.com>
2016-10-27 06:04:14 +0000

    1477548254 axiolite

---
+++ 1477548254.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2187534516
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   const uint16_t  f1;
+   uint32_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6][3] = {{0x48C66F5AL,8L,0x48C66F5AL},{0x48C66F5AL,8L,0x48C66F5AL},{0x48C66F5AL,8L,0x48C66F5AL},{0x48C66F5AL,8L,0x48C66F5AL},{0x48C66F5AL,8L,0x48C66F5AL},{0x48C66F5AL,8L,0x48C66F5AL}};
