ed64c9ec3e83d4fa0ccc1ebf71441020a6cb92cd
saisi <saisi@users.noreply.github.com>
2016-10-28 18:06:39 +0000

    1477677998 accentuates

---
+++ 1477677998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2899832533
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile int16_t  f1;
+   const uint32_t  f2;
+   uint16_t  f3;
+   const volatile signed f4 : 28;
+   uint32_t  f5;
+   const int32_t  f6;
+   uint8_t  f7;
+   volatile unsigned f8 : 10;
