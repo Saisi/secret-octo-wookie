ee266d4c458b898215f145ef7ca98428421b84d9
saisi <saisi@users.noreply.github.com>
2016-10-28 08:36:00 +0000

    1477643759 antiscia

---
+++ 1477643759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      735654266
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_33 = 1L;
+static uint32_t g_49 = 1UL;
+static uint32_t g_98 = 0x80EFF2B3L;
+static uint32_t *g_97 = &g_98;
+static uint32_t *g_99 = &g_98;
+static uint64_t g_116 = 0UL;
+static int16_t g_120 = 1L;
+static int32_t g_123 = 0x932E4B48L;
+static int32_t * volatile g_122 = &g_123;/* VOLATILE GLOBAL g_122 */
