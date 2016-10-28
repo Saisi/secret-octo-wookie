62cb52a41854d12b21f8cc79d4882d0c54c09020
saisi <saisi@users.noreply.github.com>
2016-10-28 16:36:34 +0000

    1477672594 Ballplatz

---
+++ 1477672594.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3202477980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 2L;
+static uint8_t g_23 = 0x58L;
+static volatile uint8_t * volatile g_31 = (void*)0;/* VOLATILE GLOBAL g_31 */
+static uint8_t g_64[6] = {0x98L,0x98L,0x98L,0x98L,0x98L,0x98L};
+static uint8_t *g_63 = &g_64[2];
+static uint32_t g_90 = 0x08385637L;
+static uint16_t g_99[8] = {0x4D21L,0x4D21L,0x4D21L,0x4D21L,0x4D21L,0x4D21L,0x4D21L,0x4D21L};
+static int32_t g_104 = 0x2871EB1CL;
+static uint32_t g_106 = 18446744073709551615UL;
