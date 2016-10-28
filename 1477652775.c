c5e714142e478b726d7a93ff1b047cb1619eeada
saisi <saisi@users.noreply.github.com>
2016-10-28 11:06:15 +0000

    1477652775 Aubrette

---
+++ 1477652775.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3652272227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 16;
+   unsigned f1 : 7;
+   volatile unsigned f2 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[7] = {0xC0D3L,0xC0D3L,0xC0D3L,0xC0D3L,0xC0D3L,0xC0D3L,0xC0D3L};
+static volatile int32_t g_3 = 0xDCB363F9L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-4L);
