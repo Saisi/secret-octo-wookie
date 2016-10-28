f97c299dd0f85d09cefebcf776f91da186bb3ce8
saisi <saisi@users.noreply.github.com>
2016-10-28 17:36:37 +0000

    1477676197 angioclast

---
+++ 1477676197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      630403499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-7L);
+static int32_t g_7 = 0x83518F4CL;
+static uint8_t g_13[8] = {2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL};
+static int8_t g_41 = 0x6DL;
+static uint32_t g_43 = 0x5830AF36L;
+static int32_t g_79 = 0x25D9175CL;
+static int32_t *g_83 = (void*)0;
+static uint8_t g_86 = 0UL;
