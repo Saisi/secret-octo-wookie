56833191c7006105dbe598b80c2d60be1bd40b8f
saisi <saisi@users.noreply.github.com>
2016-10-28 09:36:08 +0000

    1477647366 acetylcholinic

---
+++ 1477647366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3675617163
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int64_t  f0;
+};
+
+struct S1 {
+   const uint16_t  f0;
+   volatile uint16_t  f1;
+   int32_t  f2;
+   signed f3 : 2;
+};
