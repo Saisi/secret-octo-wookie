83047ef7c69075256735985e347f9b3203674c60
saisi <saisi@users.noreply.github.com>
2016-10-28 15:06:28 +0000

    1477667187 astyanax

---
+++ 1477667187.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1228353960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   unsigned f1 : 15;
+   volatile signed f2 : 21;
+   const unsigned f3 : 13;
+   unsigned f4 : 8;
+};
+
+struct S1 {
+   const int8_t  f0;
