9fac47fa064edd6dfc52c350d9c570379de1f18e
saisi <saisi@users.noreply.github.com>
2016-10-28 21:36:51 +0000

    1477690609 backouts

---
+++ 1477690609.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2723521639
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 18;
+   volatile unsigned f1 : 1;
+   unsigned : 0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   int32_t  f0;
