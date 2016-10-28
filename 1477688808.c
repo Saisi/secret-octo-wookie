a08f2213454a61778320acf5f34b64ecbcea45d8
saisi <saisi@users.noreply.github.com>
2016-10-28 21:06:48 +0000

    1477688808 accursedness

---
+++ 1477688808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      43844360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 18;
+   signed f1 : 31;
+   const int16_t  f2;
+   signed f3 : 29;
+   const signed f4 : 14;
+   volatile signed f5 : 2;
+   signed f6 : 27;
+};
+
