286eb96a5dee1b517463f374168e4fe7a7c88e93
saisi <saisi@users.noreply.github.com>
2016-10-28 18:36:41 +0000

    1477679800 antidotal

---
+++ 1477679800.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3098544851
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+   unsigned f1 : 13;
+   int32_t  f2;
+   volatile uint8_t  f3;
+   uint16_t  f4;
+   uint16_t  f5;
+   const int8_t  f6;
