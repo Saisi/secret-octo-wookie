4e753ec79de97687a8ee8053048e4a7d839bb603
saisi <saisi@users.noreply.github.com>
2016-10-28 22:06:53 +0000

    1477692412 Akerley

---
+++ 1477692412.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      727670773
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 25;
+   const unsigned f1 : 12;
+   volatile signed f2 : 31;
+   const volatile unsigned f3 : 14;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   const volatile int8_t  f1;
