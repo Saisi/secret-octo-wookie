9ca8a6fbe5c3331cbfef2bab10d99fbc41de87ec
saisi <saisi@users.noreply.github.com>
2016-10-28 04:05:45 +0000

    1477627545 angularize

---
+++ 1477627545.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2667373518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 0x5DAE5A60L;
+static uint32_t g_14[6][4] = {{0x77278FA8L,1UL,0x4F545EE2L,0UL},{0xFCF33F23L,4294967295UL,0xFCF33F23L,0UL},{0x4F545EE2L,1UL,0x77278FA8L,0x93D0B548L},{5UL,0x90A3CFCEL,1UL,1UL},{0UL,0UL,1UL,0xFCF33F23L},{5UL,1UL,0x77278FA8L,0x90A3CFCEL}};
+static int32_t g_16[8][1][3] = {{{(-1L),(-1L),(-1L)}},{{(-7L),(-3L),(-7L)}},{{(-1L),(-1L),(-1L)}},{{(-7L),(-3L),(-7L)}},{{(-1L),(-1L),(-1L)}},{{(-7L),(-3L),(-7L)}},{{(-1L),(-1L),(-1L)}},{{(-7L),(-3L),(-7L)}}};
+static int8_t g_18 = 1L;
+static uint32_t g_40 = 4294967288UL;
+static uint32_t g_42 = 0xE46552DEL;
+static int16_t g_44[7][2][6] = {{{0x9AE8L,0x6E82L,(-1L),(-9L),1L,(-3L)},{0xC444L,1L,0x77DAL,0x9B45L,1L,0xC444L}},{{0x643CL,0x6E82L,0x8264L,4L,0x1CD5L,(-9L)},{0x9586L,0x643CL,0x9B45L,0L,0L,(-2L)}},{{0xDA6AL,0x03B5L,0L,0x9AE8L,0x2DFCL,0x9AE8L},{0x4BB9L,0x1CD5L,0x4BB9L,0x77DAL,0L,(-1L)}},{{0xC444L,(-1L),0x643CL,6L,0x03B5L,1L},{1L,0x6E82L,(-3L),6L,0x761BL,0x77DAL}},{{0xC444L,1L,0x9B45L,0x77DAL,1L,0xC444L},{0x4BB9L,(-9L),0x03B5L,0x9AE8L,0x1CD5L,0L}},{{0xDA6AL,0x58A1L,(-3L),0L,0L,(-3L)},{0x9586L,0x9586L,0L,4L,0x9CADL,1L}},{{0x643CL,0x1CD5L,0xA676L,0x9B45L,9L,0L},{0xC444L,0x643CL,0xA676L,(-9L),0x9586L,1L}}};
+static int32_t g_50[6] = {0L,0L,0L,0L,0L,0L};
+static uint8_t g_61 = 0UL;
