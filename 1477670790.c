0f28cea31d86ca51a30c38c4bbd853c08d8e3fcd
saisi <saisi@users.noreply.github.com>
2016-10-28 16:06:33 +0000

    1477670790 alcoholist

---
+++ 1477670790.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4104418095
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+};
+#pragma pack(pop)
+
+struct S1 {
+   volatile unsigned f0 : 5;
+   volatile signed f1 : 21;
