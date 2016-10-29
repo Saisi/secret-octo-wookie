b970d5c479049d7d041a1ec044a6d357f3ecdced
saisi <saisi@users.noreply.github.com>
2016-10-29 13:07:42 +0000

    1477746461 acropodia

---
+++ 1477746461.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      311093943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x8094A188L;
+static uint32_t g_75 = 0xCD4EE3B9L;
+static volatile int32_t g_77 = 0x746B3257L;/* VOLATILE GLOBAL g_77 */
+static int32_t g_78 = 1L;
+static int32_t g_89 = 0xBF5EF7B6L;
+static uint8_t g_105 = 6UL;
+static int16_t g_109 = (-1L);
+static uint16_t g_122 = 65535UL;
+static uint16_t g_126 = 4UL;
