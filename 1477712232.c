37672f5e6fa98b420bbfa6b5cd8e71f30be51d52
saisi <saisi@users.noreply.github.com>
2016-10-29 03:37:13 +0000

    1477712232 affreighter

---
+++ 1477712232.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145961865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   volatile uint16_t  f1;
+   volatile uint16_t  f2;
+   uint16_t  f3;
+   uint16_t  f4;
+   int8_t  f5;
+   const volatile int32_t  f6;
