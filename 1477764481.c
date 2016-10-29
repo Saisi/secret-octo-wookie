8e40a940811d586662cab13c91e3dabf41b0e23b
saisi <saisi@users.noreply.github.com>
2016-10-29 18:08:02 +0000

    1477764481 amusias

---
+++ 1477764481.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2362743230
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 13;
+   const signed f1 : 14;
+   unsigned f2 : 24;
+   const volatile signed f3 : 4;
+   volatile signed f4 : 3;
+   unsigned f5 : 14;
+};
