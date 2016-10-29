c9b455ff8c6df4f299a3f6158a7c3045b57b4caf
saisi <saisi@users.noreply.github.com>
2016-10-29 19:08:06 +0000

    1477768086 alconde

---
+++ 1477768086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      692050845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 13;
+   unsigned f1 : 16;
+   volatile unsigned f2 : 19;
+   unsigned f3 : 18;
+   unsigned f4 : 27;
+   unsigned f5 : 8;
+};
+
+struct S1 {
