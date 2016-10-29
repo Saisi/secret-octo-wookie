a8885e93d9871ce77d5c37bb4a4b97848af4c72c
saisi <saisi@users.noreply.github.com>
2016-10-29 01:07:05 +0000

    1477703225 anaphoras

---
+++ 1477703225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4060854188
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   volatile uint32_t  f1;
+   volatile uint32_t  f2;
+   volatile uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 0xE04822B1L;
+static uint32_t g_5 = 0xC662A2B3L;
