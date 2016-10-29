fd8664ff49f63db704e281efdc969b4e0e2e7325
saisi <saisi@users.noreply.github.com>
2016-10-29 05:37:19 +0000

    1477719439 automotor

---
+++ 1477719439.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1079321902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 5;
+   unsigned f1 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 0x99L;
+static int32_t g_12 = 0xBACC5D9BL;
+static uint32_t g_20 = 8UL;
+static int64_t g_65 = 0L;
