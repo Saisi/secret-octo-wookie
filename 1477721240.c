1b22dcee5ce0780d69ac935b4164c46b78287918
saisi <saisi@users.noreply.github.com>
2016-10-29 06:07:20 +0000

    1477721240 assurgency

---
+++ 1477721240.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3676398462
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xCD453606L;
+static uint32_t g_10 = 0xBC07C207L;
+static int32_t *g_13 = (void*)0;
+static int32_t **g_12[7] = {(void*)0,&g_13,(void*)0,(void*)0,&g_13,(void*)0,(void*)0};
+static int32_t *** volatile g_11 = &g_12[3];/* VOLATILE GLOBAL g_11 */
