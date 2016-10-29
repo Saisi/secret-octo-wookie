14c45513a9e3013824dc6889cd040fee21dd87bb
saisi <saisi@users.noreply.github.com>
2016-10-29 20:08:09 +0000

    1477771689 attornment

---
+++ 1477771689.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1749966504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 2;
+   volatile int16_t  f1;
+   signed f2 : 7;
+   unsigned f3 : 13;
+   volatile int8_t  f4;
+   unsigned f5 : 15;
+   signed f6 : 24;
