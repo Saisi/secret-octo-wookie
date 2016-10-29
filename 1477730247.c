b24bd6a3ae59a329ca56a02e7262d5e37df616cb
saisi <saisi@users.noreply.github.com>
2016-10-29 08:37:28 +0000

    1477730247 automysophobia

---
+++ 1477730247.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4291156489
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   const uint32_t  f1;
+   volatile signed f2 : 25;
+   const uint8_t  f3;
+};
+
+union U1 {
+   volatile int8_t  f0;
+   uint8_t  f1;
