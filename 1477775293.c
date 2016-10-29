ef96260c6f2d9c521105079bd8a8474c17097d63
saisi <saisi@users.noreply.github.com>
2016-10-29 21:08:14 +0000

    1477775293 alumite

---
+++ 1477775293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      861952166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = 0xC196A162C9CC1417LL;
+static int32_t g_9[5] = {0L,0L,0L,0L,0L};
+static volatile int32_t g_12 = 0L;/* VOLATILE GLOBAL g_12 */
+static int32_t g_13 = 7L;
