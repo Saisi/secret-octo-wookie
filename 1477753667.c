4ebeca6ed04d75eab5c026a19852491c028ac4d1
saisi <saisi@users.noreply.github.com>
2016-10-29 15:07:47 +0000

    1477753667 alegars

---
+++ 1477753667.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4057226580
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 0xE0L;
+static uint16_t g_22 = 0xDFADL;
+static const uint16_t g_60 = 0x4559L;
+static const uint16_t *g_59 = &g_60;
+static uint8_t g_76 = 1UL;
+static volatile int32_t *g_85 = (void*)0;
+static volatile int32_t ** volatile g_84[5] = {&g_85,&g_85,&g_85,&g_85,&g_85};
+static int32_t g_88 = 0xCC5119B5L;
+static int32_t *g_87 = &g_88;
