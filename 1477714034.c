988757a1b8fb1df6c025ba91e4c7e7eecc3e4eab
saisi <saisi@users.noreply.github.com>
2016-10-29 04:07:14 +0000

    1477714034 AMLS

---
+++ 1477714034.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      465528846
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xF9B2CFB6L;
+static volatile uint64_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
