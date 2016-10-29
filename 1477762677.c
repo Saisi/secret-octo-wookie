e283b5afa4fc1e863b241431f4357e6eb6998e11
saisi <saisi@users.noreply.github.com>
2016-10-29 17:38:00 +0000

    1477762677 anticonformity

---
+++ 1477762677.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3256769900
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   uint32_t  f1;
+   volatile signed f2 : 28;
+};
+
+struct S1 {
+   const int32_t  f0;
+   struct S0  f1;
+   volatile int32_t  f2;
