237b18973acac92a86e615ac024fd179dae5f7f0
saisi <saisi@users.noreply.github.com>
2016-10-29 17:07:56 +0000

    1477760876 aquadag

---
+++ 1477760876.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      502956822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xF6AF1124L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-3L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x9A3DF603L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0L;
+static uint32_t g_16 = 0x312FBB6BL;
+static uint32_t g_106 = 4294967295UL;
+static int64_t g_109 = 1L;
+static uint16_t g_111[4][1][3] = {{{0xFA72L,0xFA72L,0xFA72L}},{{65531UL,65531UL,65531UL}},{{0xFA72L,0xFA72L,0xFA72L}},{{65531UL,65531UL,65531UL}}};
