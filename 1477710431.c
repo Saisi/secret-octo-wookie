7ef6d76f9dfd70370cd06020acd3ee4f3b813c9e
saisi <saisi@users.noreply.github.com>
2016-10-29 03:07:11 +0000

    1477710431 antaranga

---
+++ 1477710431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3312961960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xF4A8B108L;
+static int32_t *g_5[4] = {&g_3,&g_3,&g_3,&g_3};
+static uint8_t g_31 = 0x57L;
+static int64_t g_42 = 0xE149CFF29C214F16LL;
+static uint8_t g_44 = 0xEDL;
