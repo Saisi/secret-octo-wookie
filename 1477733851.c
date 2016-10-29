2e0d1be093caade44809a1bd3149cf1529841993
saisi <saisi@users.noreply.github.com>
2016-10-29 09:37:31 +0000

    1477733851 aspergilla

---
+++ 1477733851.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2587413177
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 27;
+   unsigned f1 : 6;
+   const unsigned f2 : 17;
+   const volatile unsigned f3 : 24;
+};
+
+struct S1 {
+   uint8_t  f0;
+};
