9245a551a51a90c22be176718c8d28c0def71658
saisi <saisi@users.noreply.github.com>
2016-10-29 18:38:05 +0000

    1477766285 amylopsase

---
+++ 1477766285.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2548062872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6 = (-1L);
+static int32_t g_19 = 1L;
+static volatile uint32_t g_21 = 5UL;/* VOLATILE GLOBAL g_21 */
+static uint16_t g_25[2][1][4] = {{{0x093DL,8UL,0x093DL,0x093DL}},{{8UL,8UL,0UL,8UL}}};
+static uint16_t g_44 = 65526UL;
+static int32_t g_47 = 0L;
+static uint8_t g_50 = 255UL;
+static int32_t g_51 = 0x0E9FB388L;
+static int64_t g_95 = 0xE4F1A1E6DEAED54FLL;
