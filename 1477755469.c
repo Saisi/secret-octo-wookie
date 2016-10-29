ba127fda5ca3d2496f6454574c403a2674c020e6
saisi <saisi@users.noreply.github.com>
2016-10-29 15:37:50 +0000

    1477755469 Anapurna

---
+++ 1477755469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2281971733
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 9;
+   const volatile signed f1 : 28;
+   unsigned f2 : 15;
+   const signed f3 : 30;
+};
+#pragma pack(pop)
+
