13579ae6f9d41be26de80d575b5c1595dbf13d24
saisi <saisi@users.noreply.github.com>
2016-10-29 21:38:18 +0000

    1477777096 ambulation

---
+++ 1477777096.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3150146394
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   volatile int64_t  f1;
+   const int16_t  f2;
+   volatile uint8_t  f3;
+   uint32_t  f4;
+   const uint16_t  f5;
+   volatile uint32_t  f6;
+   const volatile int32_t  f7;
+};
