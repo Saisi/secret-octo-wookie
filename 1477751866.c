4420607e70b81bdd32d44b71989847e371f6280b
saisi <saisi@users.noreply.github.com>
2016-10-29 14:37:46 +0000

    1477751866 arcform

---
+++ 1477751866.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1372749811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_5 = 0x6444696A485BF25CLL;
+static int32_t g_7 = (-3L);
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+static int16_t g_57[4] = {1L,1L,1L,1L};
+static int32_t *g_63 = &g_7;
