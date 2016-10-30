07770c9672b23f29e91f92ab2ddf9b44fc83cd60
saisi <saisi@users.noreply.github.com>
2016-10-30 20:09:36 +0000

    1477858176 barbell

---
+++ 1477858176.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2965896581
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 1;
+   signed f1 : 20;
+   signed f2 : 7;
+   unsigned f3 : 22;
+   signed f4 : 15;
+   volatile unsigned f5 : 12;
+   signed f6 : 27;
+   uint32_t  f7;
+   signed f8 : 1;
