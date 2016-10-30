d3c45a9d171712762b9d8bdbdbe069d2a45f4e45
saisi <saisi@users.noreply.github.com>
2016-10-30 09:38:58 +0000

    1477820338 autoindexing

---
+++ 1477820338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3652542806
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   const int16_t  f1;
+   uint32_t  f2;
+   uint32_t  f3;
+   const int64_t  f4;
+   int32_t  f5;
+   uint64_t  f6;
+   const int32_t  f7;
+};
