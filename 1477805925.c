cc6a5e154912699230bdfadfb678f305f89a19c4
saisi <saisi@users.noreply.github.com>
2016-10-30 05:38:46 +0000

    1477805925 anthracemia

---
+++ 1477805925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2189507451
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   unsigned f1 : 3;
+   const volatile uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1C36B586L;
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 4L;
