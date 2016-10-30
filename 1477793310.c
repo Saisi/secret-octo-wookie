5d9d660b5a80fbfe6a12173ca5ea642f5c17078f
saisi <saisi@users.noreply.github.com>
2016-10-30 02:08:30 +0000

    1477793310 argillocalcite

---
+++ 1477793310.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      233494949
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4[5][6] = {{0x50L,3UL,3UL,0x50L,0x1DL,0x50L},{0x50L,0x50L,0x92L,0x50L,0x50L,5UL},{0x50L,0x50L,5UL,5UL,0x50L,0x50L},{3UL,0x50L,0x1DL,0x50L,0x1DL,0x50L},{0x1DL,3UL,5UL,0x92L,0x92L,5UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
