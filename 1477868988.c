1967da0e91bcc841eaf7944d85e87f108b1284e3
saisi <saisi@users.noreply.github.com>
2016-10-30 23:09:48 +0000

    1477868988 amphicarpic

---
+++ 1477868988.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      730300898
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 31;
+   const unsigned f1 : 24;
+   int8_t  f2;
+   volatile signed f3 : 2;
+   unsigned f4 : 31;
+   unsigned f5 : 15;
+   volatile unsigned f6 : 20;
