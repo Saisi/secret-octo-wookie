ec22e40a47f4aa4b6b37d881b262fd5fd19f0e54
saisi <saisi@users.noreply.github.com>
2016-10-30 01:38:29 +0000

    1477791509 appeal

---
+++ 1477791509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2095604119
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 10;
+   uint64_t  f1;
+   int32_t  f2;
+   volatile int64_t  f3;
+};
+
+union U1 {
+   volatile int32_t  f0;
+};
