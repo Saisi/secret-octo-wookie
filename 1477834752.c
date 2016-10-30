9eb76a2a08df041cc9db97885cc9b350b4305ca9
saisi <saisi@users.noreply.github.com>
2016-10-30 13:39:13 +0000

    1477834752 Apollon

---
+++ 1477834752.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3330566912
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 14;
+   int8_t * volatile  f1;
+   int32_t  f2;
+   int16_t  f3;
+   volatile uint8_t  f4;
+};
+
+union U1 {
+   const uint32_t  f0;
