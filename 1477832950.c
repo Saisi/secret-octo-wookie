c7312eeee45839141101caf0b0ea2e842efc77b0
saisi <saisi@users.noreply.github.com>
2016-10-30 13:09:11 +0000

    1477832950 animalism

---
+++ 1477832950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1940534674
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint32_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint32_t  f0;
+   signed f1 : 6;
