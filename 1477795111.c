ff942fbda56ee344538e2d043136c0e60d4121ef
saisi <saisi@users.noreply.github.com>
2016-10-30 02:38:35 +0000

    1477795111 archisupreme

---
+++ 1477795111.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2502089674
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint64_t  f0;
+   unsigned f1 : 16;
+   signed f2 : 4;
+   uint16_t  f3;
+   const unsigned f4 : 10;
+   const unsigned f5 : 4;
+   signed f6 : 25;
+   const signed f7 : 12;
+};
