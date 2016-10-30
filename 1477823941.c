aa29435cb881b2ef59dad947222b6798eb6ced84
saisi <saisi@users.noreply.github.com>
2016-10-30 10:39:01 +0000

    1477823941 Artair

---
+++ 1477823941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      933985792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x55595070L;
+static struct S0 g_5 = {4UL,-7L,0x18E4L};/* VOLATILE GLOBAL g_5 */
+
