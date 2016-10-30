2e68afe4514067992d93fd5bb61537deb3406e67
saisi <saisi@users.noreply.github.com>
2016-10-30 08:08:53 +0000

    1477814932 aerotherapy

---
+++ 1477814932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2407784197
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1] = {1L};
+static int32_t g_3 = 0x60B4FFB8L;
+static int16_t g_7 = 0x3C4AL;
+static const int16_t g_23[9] = {0x17D0L,0x17D0L,0L,0x17D0L,0x17D0L,0L,0x17D0L,0x17D0L,0L};
+static volatile int32_t g_50 = 6L;/* VOLATILE GLOBAL g_50 */
+static int32_t g_51[1][9][1] = {{{(-1L)},{0x82F744EDL},{(-1L)},{0x82F744EDL},{(-1L)},{0x82F744EDL},{(-1L)},{0x82F744EDL},{(-1L)}}};
+static uint64_t g_70 = 0x8D2125954A437726LL;
+static uint32_t g_79 = 0xA1E5F560L;
+static int32_t **g_100 = (void*)0;
