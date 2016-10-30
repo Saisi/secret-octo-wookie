4665f65f255cdcdd6bf81908cc0081126f5add82
saisi <saisi@users.noreply.github.com>
2016-10-30 21:09:42 +0000

    1477861781 aircrew

---
+++ 1477861781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1253016648
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   volatile int8_t  f1;
+   signed f2 : 25;
+   const uint64_t  f3;
+   const uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_11 = 0UL;
