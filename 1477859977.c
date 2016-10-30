a095b3c6fb56ad45e1f65945abce0caca449a534
saisi <saisi@users.noreply.github.com>
2016-10-30 20:39:40 +0000

    1477859977 Amby

---
+++ 1477859977.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1110539562
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 3;
+   unsigned f1 : 12;
+   uint8_t  f2;
+   volatile unsigned f3 : 6;
+};
+#pragma pack(pop)
+
