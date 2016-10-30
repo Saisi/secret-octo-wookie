2da56b49473d071d6cf115d45d2a785147ac6dd2
saisi <saisi@users.noreply.github.com>
2016-10-30 11:09:03 +0000

    1477825743 abortus

---
+++ 1477825743.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1435609157
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0x860BL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0L;
+static int32_t *g_9[1] = {&g_4};
+static int32_t ** volatile g_8 = &g_9[0];/* VOLATILE GLOBAL g_8 */
+static int32_t g_12[2] = {0x4F27D270L,0x4F27D270L};
