9cfd3ee50f279a0e4b7e9a8d1250a2151e393426
saisi <saisi@users.noreply.github.com>
2016-10-30 04:38:43 +0000

    1477802323 albacore

---
+++ 1477802323.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2097128154
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 27;
+   volatile signed f1 : 4;
+   signed f2 : 22;
+   const volatile unsigned f3 : 10;
+   unsigned f4 : 17;
+   const volatile unsigned f5 : 4;
+   volatile unsigned f6 : 18;
