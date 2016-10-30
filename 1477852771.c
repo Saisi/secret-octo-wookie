56e53c31c86944280e8b30e84f5c51f8b951794c
saisi <saisi@users.noreply.github.com>
2016-10-30 18:39:32 +0000

    1477852771 backwoodsiness

---
+++ 1477852771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1028098504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 18;
+   const int8_t  f1;
+   const unsigned f2 : 23;
+   signed f3 : 29;
+   unsigned f4 : 14;
+   const int16_t  f5;
+   volatile unsigned f6 : 8;
+};
+
