d1074ea516630b8e39f8af1e09e86329bafda4dd
saisi <saisi@users.noreply.github.com>
2016-10-31 16:40:53 +0000

    1477932052 atrichic

---
+++ 1477932052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4234472968
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 18;
+   int64_t  f1;
+   const int8_t  f2;
+   volatile signed f3 : 29;
+   const volatile uint8_t  f4;
+};
+
+union U1 {
+   int16_t  f0;
