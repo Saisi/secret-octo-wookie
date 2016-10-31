f1ffd456fe529de35a068aa2f88622005044a570
saisi <saisi@users.noreply.github.com>
2016-10-31 00:39:53 +0000

    1477874393 adterminal

---
+++ 1477874393.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2797895982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 5;
+   volatile unsigned f1 : 6;
+   volatile unsigned f2 : 16;
+   signed f3 : 28;
+   unsigned f4 : 27;
+   unsigned f5 : 6;
+   unsigned f6 : 12;
