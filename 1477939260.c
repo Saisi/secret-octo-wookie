c008e0203b34aecb810313065f7b359f7b4211c0
saisi <saisi@users.noreply.github.com>
2016-10-31 18:41:00 +0000

    1477939260 algesiometer

---
+++ 1477939260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3677410490
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2 = 9L;
+static uint64_t g_8 = 18446744073709551615UL;
+static uint32_t g_12 = 0x2B483718L;
+static uint16_t g_19 = 0x3CC4L;
+static int32_t g_30 = 0x21FBB9CFL;
+static uint8_t g_32 = 0xB3L;
+static int64_t g_36 = 0xDEDC87A9B65CAB43LL;
+static uint64_t g_54 = 0x227283A38C8A251ALL;
+static int32_t g_95 = (-1L);
