5bec1042068f4261c0bda3320730d1fda48b93d9
saisi <saisi@users.noreply.github.com>
2016-10-31 03:10:00 +0000

    1477883399 alpenstocker

---
+++ 1477883399.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1622588821
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   const uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_14 = 0x7AF5L;
+static int32_t g_43 = 0xAAC89535L;
