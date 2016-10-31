86bb2fbe92ce227716f1380d5167f5b7ae779c19
saisi <saisi@users.noreply.github.com>
2016-10-31 21:41:12 +0000

    1477950071 backstretches

---
+++ 1477950071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      802477232
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_5[1] = {0x18L};
+static volatile int32_t g_33 = 0xC77DCDA7L;/* VOLATILE GLOBAL g_33 */
+static volatile int32_t g_34 = 0x9B37D022L;/* VOLATILE GLOBAL g_34 */
+static volatile int32_t g_35[5][8] = {{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)}};
+static volatile int32_t g_36 = 0x831966C4L;/* VOLATILE GLOBAL g_36 */
+static volatile int32_t g_37 = 0xD44F4CC5L;/* VOLATILE GLOBAL g_37 */
+static volatile int32_t g_38 = (-9L);/* VOLATILE GLOBAL g_38 */
+static volatile int32_t g_39 = (-1L);/* VOLATILE GLOBAL g_39 */
+static volatile int32_t g_40 = 0x3423637BL;/* VOLATILE GLOBAL g_40 */
