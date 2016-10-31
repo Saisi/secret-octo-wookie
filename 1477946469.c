84d548315b0d47209900a0ff4a1a171d52459d10
saisi <saisi@users.noreply.github.com>
2016-10-31 20:41:09 +0000

    1477946469 actinophorous

---
+++ 1477946469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4062832197
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 1;
+   volatile unsigned f1 : 20;
+   const volatile signed f2 : 7;
+   unsigned f3 : 22;
+   volatile unsigned f4 : 15;
+   signed f5 : 12;
+   signed f6 : 27;
