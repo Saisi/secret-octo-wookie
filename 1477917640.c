7476586c4980325f463a100be757044bc8ed05fc
saisi <saisi@users.noreply.github.com>
2016-10-31 12:40:41 +0000

    1477917640 Almon

---
+++ 1477917640.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3079511486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int64_t  f1;
+   volatile unsigned f2 : 24;
+   int32_t  f3;
+   const volatile int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11 = 7L;
