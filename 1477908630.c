f3688b316496cb0a521e25bfd6e265edac93f278
saisi <saisi@users.noreply.github.com>
2016-10-31 10:10:30 +0000

    1477908630 amphigouris

---
+++ 1477908630.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3863433510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[1] = {0x4801897AL};
+static volatile int32_t *g_2 = &g_3[0];
+static volatile int32_t ** volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
