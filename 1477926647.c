050405be092cac9145afb6c922508c99fba3e702
saisi <saisi@users.noreply.github.com>
2016-10-31 15:10:47 +0000

    1477926647 Appaloosa

---
+++ 1477926647.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3512397027
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFEBAEEF2L;
+static int32_t *g_5 = &g_3;
+static int32_t ** volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
