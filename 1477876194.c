1cb441e6ec76aa0716350d1393deaf0380e0a517
saisi <saisi@users.noreply.github.com>
2016-10-31 01:09:54 +0000

    1477876194 aggrandising

---
+++ 1477876194.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      568068301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint16_t  f1;
+   volatile int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xF7F3E76EL;
+static int32_t *g_7 = &g_3;
+static int32_t ** volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
