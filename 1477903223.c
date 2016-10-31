1c7b1fa30eb82443d1bbb3ef8fb4ec6e4827c33f
saisi <saisi@users.noreply.github.com>
2016-10-31 08:40:24 +0000

    1477903223 Axonia

---
+++ 1477903223.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1852246926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int8_t  f1;
+   volatile int16_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 5L;/* VOLATILE GLOBAL g_4 */
