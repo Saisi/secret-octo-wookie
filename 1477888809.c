22ef5d432e321f70872d088da2a64ce163f6a6f0
saisi <saisi@users.noreply.github.com>
2016-10-31 04:40:11 +0000

    1477888809 anisochromatic

---
+++ 1477888809.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4264204029
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 29;
+   signed : 0;
+   volatile unsigned f1 : 3;
+   unsigned f2 : 2;
+   signed f3 : 11;
+   signed f4 : 24;
+   signed f5 : 23;
+   volatile unsigned f6 : 14;
+   unsigned f7 : 25;
