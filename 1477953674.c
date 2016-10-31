1765be69a88ff14a4bd1ffb10cac21214834fb9d
saisi <saisi@users.noreply.github.com>
2016-10-31 22:41:15 +0000

    1477953674 Adiantum

---
+++ 1477953674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2437654949
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 17;
+   volatile signed f1 : 4;
+   uint8_t  f2;
+   signed f3 : 24;
+   uint64_t  f4;
+   const signed f5 : 23;
+   unsigned f6 : 31;
+   unsigned f7 : 28;
+   volatile signed f8 : 21;
