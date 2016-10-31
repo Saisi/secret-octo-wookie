ec0de169aecd2840c56ef6469305bf7236b07983
saisi <saisi@users.noreply.github.com>
2016-10-31 02:09:57 +0000

    1477879797 Acestes

---
+++ 1477879797.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2255239945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   volatile int8_t * volatile  f0;
+   volatile uint32_t  f1;
+   uint64_t  f2;
+   uint8_t  f3;
+   volatile int32_t  f4;
