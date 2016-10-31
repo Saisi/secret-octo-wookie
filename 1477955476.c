e6fcff80478907af8102c7038e1b2ec591ff9fad
saisi <saisi@users.noreply.github.com>
2016-10-31 23:11:17 +0000

    1477955476 avoidment

---
+++ 1477955476.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1545283491
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile unsigned f1 : 21;
+   const volatile unsigned f2 : 23;
+};
+
+struct S1 {
+   signed f0 : 27;
+   uint64_t  f1;
+   int8_t  f2;
