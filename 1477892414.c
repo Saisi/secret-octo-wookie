6e8c46b711a1eb85d84a8e0a84c3141ef58b5b06
saisi <saisi@users.noreply.github.com>
2016-10-31 05:40:14 +0000

    1477892414 amgarn

---
+++ 1477892414.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2053034185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_8 = 8UL;
+static uint64_t g_19 = 0xEA53061ECE3051EBLL;
+static int64_t g_22 = (-6L);
+static const int16_t g_35 = 0x69B7L;
+static uint64_t g_37 = 8UL;
+static uint64_t *g_36 = &g_37;
+static int32_t g_48 = 0x6848298BL;
+static int8_t g_71 = 1L;
+static int8_t g_74 = (-7L);
