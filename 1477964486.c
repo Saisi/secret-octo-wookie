3b1349575be7369b35676f291ac41cbd66051d6d
saisi <saisi@users.noreply.github.com>
2016-11-01 01:41:26 +0000

    1477964486 antiworld

---
+++ 1477964486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3252257877
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 9;
+   uint8_t  f1;
+   signed f2 : 28;
+   signed f3 : 16;
+   unsigned f4 : 23;
+   volatile unsigned f5 : 9;
+};
