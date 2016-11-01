04b3c6abd0845d288507b377c09c8bd71759d3f8
saisi <saisi@users.noreply.github.com>
2016-11-01 00:41:23 +0000

    1477960882 acrotisms

---
+++ 1477960882.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      306303675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint8_t  f0;
+   signed f1 : 3;
+   uint16_t  f2;
+   volatile uint64_t  f3;
+   volatile int64_t  f4;
+   int32_t  f5;
+   volatile unsigned f6 : 26;
