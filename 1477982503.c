0648ed28d069a40edb0e565692c006e4f4de3b0c
saisi <saisi@users.noreply.github.com>
2016-11-01 06:41:43 +0000

    1477982503 Ailsa

---
+++ 1477982503.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1990450239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int64_t  f1;
+   uint16_t  f2;
+   uint8_t  f3;
+   const uint8_t  f4;
+   uint32_t  f5;
+   uint16_t  f6;
+};
+
