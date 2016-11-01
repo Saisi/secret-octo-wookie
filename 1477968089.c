b447937cf0d4e18575768eb9672309c9d754d90e
saisi <saisi@users.noreply.github.com>
2016-11-01 02:41:29 +0000

    1477968089 Amblyrhynchus

---
+++ 1477968089.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      870735686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int8_t  f0;
+   const uint64_t  f1;
+   volatile uint8_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+   int8_t  f5;
+};
