5e9b68e7fa73a304e01b88672e30a2f5c25cbbb3
saisi <saisi@users.noreply.github.com>
2016-11-01 00:11:21 +0000

    1477959080 alternator

---
+++ 1477959080.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      511083863
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 10;
+   unsigned f1 : 25;
+   volatile signed f2 : 24;
+   volatile signed f3 : 3;
+   const volatile unsigned f4 : 8;
+   const unsigned f5 : 1;
+   signed f6 : 28;
