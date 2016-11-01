7337e74e0b8a34ea594c95d5dfb10453022f985c
saisi <saisi@users.noreply.github.com>
2016-11-01 19:12:32 +0000

    1478027552 asomatophyte

---
+++ 1478027552.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3308133390
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_11 = 0L;/* VOLATILE GLOBAL g_11 */
+static volatile uint64_t g_12 = 0x20348F4A8FE24307LL;/* VOLATILE GLOBAL g_12 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
