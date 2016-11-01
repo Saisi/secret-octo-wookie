416bf7b4169667ac7a730757cd2859a46669f5f5
saisi <saisi@users.noreply.github.com>
2016-11-01 02:11:28 +0000

    1477966287 barbiturate

---
+++ 1477966287.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1175213788
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 28;
+   uint16_t  f1;
+   unsigned f2 : 25;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
