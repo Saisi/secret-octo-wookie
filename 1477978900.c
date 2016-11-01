44d87a43b5602d99f9df5f6315e0f7dfc65c2a42
saisi <saisi@users.noreply.github.com>
2016-11-01 05:41:40 +0000

    1477978900 areography

---
+++ 1477978900.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      587597634
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int16_t  f1;
+};
+
+union U1 {
+   uint64_t  f0;
+   int8_t * f1;
+};
+
