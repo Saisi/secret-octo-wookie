8da09a29fec036255bbd2b632fa05910f8033fe4
saisi <saisi@users.noreply.github.com>
2016-11-01 13:12:09 +0000

    1478005929 anglicist

---
+++ 1478005929.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3425832512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   unsigned f1 : 27;
+   uint32_t  f2;
+   volatile int32_t  f3;
+   uint32_t  f4;
+   uint32_t  f5;
+   volatile int32_t  f6;
+};
+
