535db6044504e38dac8e30023b6c446180986045
saisi <saisi@users.noreply.github.com>
2016-11-01 20:12:35 +0000

    1478031154 Aramaic

---
+++ 1478031154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2884994358
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_23 = 2UL;/* VOLATILE GLOBAL g_23 */
+static int32_t g_29 = 0xC2542929L;
+static int32_t g_58 = (-10L);
+static int8_t g_82 = 8L;
+static int16_t g_83 = 0L;
