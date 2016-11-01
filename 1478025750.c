33e465ef2227bbfdb60cefeb6f3e62ff701203f0
saisi <saisi@users.noreply.github.com>
2016-11-01 18:42:30 +0000

    1478025750 antimystic

---
+++ 1478025750.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4238919545
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 27;
+   volatile unsigned f1 : 6;
+   volatile unsigned f2 : 17;
+   signed f3 : 24;
+};
+
+union U1 {
+   signed f0 : 27;
+   int16_t  f1;
