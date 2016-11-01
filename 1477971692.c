19e1e2ed53f7435622c29175e4aad907532fdd4e
saisi <saisi@users.noreply.github.com>
2016-11-01 03:41:33 +0000

    1477971692 acetylglycin

---
+++ 1477971692.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3536487644
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xFE387FB0L;
+static int32_t g_8 = (-1L);
+static uint32_t g_14 = 0x13E19F23L;
+static int8_t g_54 = 8L;
+static int64_t g_77 = 0x34D12EA0FEFE6B3ELL;
+static uint16_t g_97[6][1] = {{0xB9F8L},{0xC006L},{0xC006L},{0xB9F8L},{0xC006L},{0xC006L}};
+static int16_t g_112 = (-7L);
+static volatile int8_t g_113 = 0xAEL;/* VOLATILE GLOBAL g_113 */
