a672d056c576ead3e4a22fddb39e9437fa0a03ef
saisi <saisi@users.noreply.github.com>
2016-11-01 11:42:05 +0000

    1478000520 bagworms

---
+++ 1478000520.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1245180696
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   int16_t  f1;
+   int64_t  f2;
+   const int16_t  f3;
+   const int64_t  f4;
+   volatile uint64_t  f5;
+   volatile int16_t  f6;
