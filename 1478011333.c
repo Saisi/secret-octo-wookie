f55dedeb9385d6252bcf9f94ebbdd7045d63b4a2
saisi <saisi@users.noreply.github.com>
2016-11-01 14:42:14 +0000

    1478011333 archimperialistic

---
+++ 1478011333.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3788763486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 29;
+   uint8_t  f1;
+   uint32_t  f2;
+   volatile int64_t  f3;
+};
+
+union U1 {
+   uint16_t  f0;
+   const volatile int32_t  f1;
