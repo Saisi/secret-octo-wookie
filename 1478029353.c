491d2b07ad335e389e27262ce13fcd07b69c8ae6
saisi <saisi@users.noreply.github.com>
2016-11-01 19:42:33 +0000

    1478029353 anelastic

---
+++ 1478029353.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1019913022
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   int32_t  f0;
+   const int64_t  f1;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
