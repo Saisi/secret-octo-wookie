8133fbe5c516c1c48fd9b024bec494855097bf92
saisi <saisi@users.noreply.github.com>
2016-11-01 06:11:41 +0000

    1477980701 Aggadic

---
+++ 1477980701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3606884192
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = (-1L);
+static int32_t g_24[8] = {1L,2L,1L,1L,2L,1L,1L,2L};
+static uint32_t g_46 = 0xD0BD2F23L;
+static uint32_t *g_87 = &g_46;
+static int32_t g_96[8] = {0x01CF56E0L,0x01CF56E0L,0x01CF56E0L,0x01CF56E0L,0x01CF56E0L,0x01CF56E0L,0x01CF56E0L,0x01CF56E0L};
+static const uint32_t * volatile g_111 = &g_46;/* VOLATILE GLOBAL g_111 */
+static int32_t g_135[5] = {0x60A5FB02L,0x60A5FB02L,0x60A5FB02L,0x60A5FB02L,0x60A5FB02L};
+static uint8_t g_137 = 0x11L;
+static uint32_t g_138 = 1UL;
