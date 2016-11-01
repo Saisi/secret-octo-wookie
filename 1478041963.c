a338268012775c9af30defda68996505b676950f
saisi <saisi@users.noreply.github.com>
2016-11-01 23:12:43 +0000

    1478041963 aldebaranium

---
+++ 1478041963.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2498645030
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 6L;
+static int32_t g_64 = 0x4E69A457L;
+static int32_t g_70[9] = {9L,9L,9L,9L,9L,9L,9L,9L,9L};
+static int32_t * volatile g_69 = &g_70[3];/* VOLATILE GLOBAL g_69 */
+static int32_t *g_78 = &g_70[3];
+static int32_t ** volatile g_77 = &g_78;/* VOLATILE GLOBAL g_77 */
+static int32_t * volatile g_81 = &g_64;/* VOLATILE GLOBAL g_81 */
+static uint64_t g_88 = 0x0822C3E505CE5052LL;
+static uint16_t g_89 = 0UL;
