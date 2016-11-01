3c221a8176fe4c1d6f3959757db87f1955525a80
saisi <saisi@users.noreply.github.com>
2016-11-01 17:12:26 +0000

    1478020345 attendances

---
+++ 1478020345.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3231333100
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   unsigned f1 : 11;
+   const int32_t  f2;
+   volatile uint32_t  f3;
+   volatile unsigned f4 : 20;
