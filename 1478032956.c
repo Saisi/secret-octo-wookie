aaa9439034ee054b8956a46018baaf822aa43643
saisi <saisi@users.noreply.github.com>
2016-11-01 20:42:37 +0000

    1478032956 Agathocles

---
+++ 1478032956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2871661015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 10;
+   volatile unsigned f1 : 25;
+   unsigned f2 : 24;
+   signed f3 : 3;
+   const unsigned f4 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 9L;
