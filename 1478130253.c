159ba82b1a86d8672b1dc771a6fa2a7bc21e0661
saisi <saisi@users.noreply.github.com>
2016-11-02 23:44:13 +0000

    1478130253 banging

---
+++ 1478130253.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      945677023
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int64_t  f1;
+   uint64_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_14 = 0xD711E7BFL;
+static int32_t g_18 = 0xB09F5954L;
