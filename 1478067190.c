7f7fccb89db69d84bd0f2961bd37d9d2b88914a9
saisi <saisi@users.noreply.github.com>
2016-11-02 06:13:11 +0000

    1478067190 abreuvoir

---
+++ 1478067190.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1605933677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   volatile int16_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xFCB50BF3L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 1L;
+static int8_t g_28 = 1L;
