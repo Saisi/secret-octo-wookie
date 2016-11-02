38dabc18bfc37a55c6786496274065b61f62e079
saisi <saisi@users.noreply.github.com>
2016-11-02 22:14:09 +0000

    1478124848 artificiality

---
+++ 1478124848.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1430121990
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   int8_t  f1;
+   volatile unsigned f2 : 7;
+   int8_t * volatile  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_4 = 0x6A162C9CC14173BFLL;/* VOLATILE GLOBAL g_4 */
+static uint8_t g_10 = 0x19L;
