30c8f9f0499a2082d5d832c635b6a639bbebb372
saisi <saisi@users.noreply.github.com>
2016-11-02 06:43:13 +0000

    1478068992 Ambia

---
+++ 1478068992.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      21337824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 30;
+   signed f1 : 27;
+   const signed f2 : 17;
+   volatile unsigned f3 : 25;
+   volatile unsigned f4 : 4;
+   signed f5 : 11;
+   volatile unsigned f6 : 29;
+   signed f7 : 30;
+   volatile signed f8 : 10;
