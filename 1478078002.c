9af63d68bf3b9b999dd1e37cad3f20308286d46f
saisi <saisi@users.noreply.github.com>
2016-11-02 09:13:22 +0000

    1478078002 antipyic

---
+++ 1478078002.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2849126769
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1] = {0x2DB58E46L};
+static int32_t g_8 = 0xC0978C76L;
+static uint64_t g_16[1][4] = {{0x9878FF5F6F594A39LL,0x9878FF5F6F594A39LL,0x9878FF5F6F594A39LL,0x9878FF5F6F594A39LL}};
+static int8_t g_18 = 0xBEL;
+static int32_t ** volatile g_20 = (void*)0;/* VOLATILE GLOBAL g_20 */
+static int32_t *g_22 = (void*)0;
+static int32_t ** volatile g_21[8][3][3] = {{{&g_22,&g_22,(void*)0},{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22}},{{&g_22,&g_22,&g_22},{&g_22,&g_22,(void*)0},{(void*)0,&g_22,&g_22}},{{&g_22,&g_22,&g_22},{&g_22,(void*)0,&g_22},{&g_22,&g_22,&g_22}},{{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22}},{{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22},{&g_22,(void*)0,&g_22}},{{(void*)0,&g_22,(void*)0},{&g_22,&g_22,&g_22},{&g_22,(void*)0,(void*)0}},{{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22}},{{&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22},{(void*)0,&g_22,&g_22}}};
+static int32_t ** const  volatile g_23[4] = {&g_22,&g_22,&g_22,&g_22};
+static int32_t ** volatile g_24 = (void*)0;/* VOLATILE GLOBAL g_24 */
