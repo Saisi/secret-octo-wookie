2a1b7194471638eac175a7a18b0779dbf48e71e8
saisi <saisi@users.noreply.github.com>
2016-11-02 16:43:49 +0000

    1478105028 Angstrom

---
+++ 1478105028.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2537518878
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 29;
+   signed f1 : 30;
+   const unsigned f2 : 8;
+   const int32_t  f3;
+   signed f4 : 21;
+   const unsigned f5 : 13;
+   volatile unsigned f6 : 26;
