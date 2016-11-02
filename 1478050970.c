f594d5f74dbefac24bf8a5fb0b9548ccff22e41a
saisi <saisi@users.noreply.github.com>
2016-11-02 01:42:51 +0000

    1478050970 amphigastrium

---
+++ 1478050970.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2456599941
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
+   const uint64_t  f3;
+   uint32_t  f4;
+   volatile int16_t  f5;
+   const int16_t  f6;
