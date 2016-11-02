626924ab4ba99c60d9b90d1c6d9eb4a21a465ee4
saisi <saisi@users.noreply.github.com>
2016-11-02 07:43:16 +0000

    1478072595 Asclepiad

---
+++ 1478072595.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2797006581
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint8_t  f0;
+   const unsigned f1 : 14;
+   volatile unsigned f2 : 12;
+   uint16_t  f3;
+};
+#pragma pack(pop)
+
