e6341927cbb8bd0cfd03e5a6a373086a4458269a
saisi <saisi@users.noreply.github.com>
2016-11-02 14:43:41 +0000

    1478097821 atticomastoid

---
+++ 1478097821.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1364529694
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   volatile signed f1 : 30;
+   signed f2 : 8;
+   volatile signed f3 : 20;
+   const signed f4 : 19;
+   signed f5 : 30;
+   const signed f6 : 4;
+   unsigned f7 : 9;
+   volatile signed f8 : 9;
