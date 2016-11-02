5cfccaa6e1a74011e748615d2fbbb7f51f717493
saisi <saisi@users.noreply.github.com>
2016-11-02 22:44:10 +0000

    1478126650 Balopticon

---
+++ 1478126650.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      179516383
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4 = (-2L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_6 = 0xA35F394AL;
+static int32_t g_8 = (-1L);
+static int32_t * volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+static int16_t g_26 = 0x5954L;
+static uint16_t g_33 = 0xBF19L;
+static uint16_t g_35 = 6UL;
+static uint8_t g_37 = 0x57L;
+static int32_t *g_44[2] = {&g_8,&g_8};
