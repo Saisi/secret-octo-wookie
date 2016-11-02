2ec4a71c21050fadcc96fa5a5b7317b63fb4a82d
saisi <saisi@users.noreply.github.com>
2016-11-02 12:13:33 +0000

    1478088813 anticularia

---
+++ 1478088813.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3824568892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_18[3] = {(-5L),(-5L),(-5L)};
+static volatile int32_t g_20 = 0x21AFA9B6L;/* VOLATILE GLOBAL g_20 */
+static volatile int32_t * volatile g_19 = &g_20;/* VOLATILE GLOBAL g_19 */
+static int32_t g_22 = 0x3267E6AAL;
+static int16_t g_24 = 0x3E71L;
+static int8_t g_54 = 1L;
+static uint32_t g_55 = 18446744073709551615UL;
+static uint64_t g_57 = 0UL;
+static int32_t * volatile g_58 = &g_22;/* VOLATILE GLOBAL g_58 */
