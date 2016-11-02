247580c4886f9f177190b785ffb376c2731bdde6
saisi <saisi@users.noreply.github.com>
2016-11-02 00:12:46 +0000

    1478045566 antiviolence

---
+++ 1478045566.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2809049825
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-1L);
+static int32_t g_7[9][1] = {{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L},{0xDFE4B283L}};
+static int16_t g_26 = 0xA1BAL;
+static volatile int16_t g_31 = 0x5C08L;/* VOLATILE GLOBAL g_31 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
