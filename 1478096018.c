5a9c7b51fd3964c85fa1efd11b0018db11221b0a
saisi <saisi@users.noreply.github.com>
2016-11-02 14:13:39 +0000

    1478096018 Acroceraunian

---
+++ 1478096018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1517508408
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[10] = {0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL,0x477862FDL};
+static int32_t g_5 = 0x62E08974L;
+static uint8_t g_11 = 0x0BL;
+static int8_t g_47 = 0xA9L;
+static uint16_t g_53 = 0xC821L;
+static int8_t g_55[4][5][3] = {{{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L}},{{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)}},{{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L}},{{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)},{0L,0L,0L},{(-1L),(-1L),(-1L)}}};
+static int64_t g_65 = 0x77E2192FE2CCF58DLL;
+static int32_t g_69 = 0x0EB6BA8AL;
+static int16_t g_94 = (-1L);
