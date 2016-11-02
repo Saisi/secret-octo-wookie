6d7ee2fd816892be2a49b2685f5521836bd8ba19
saisi <saisi@users.noreply.github.com>
2016-11-02 00:42:47 +0000

    1478047367 Acanthocephala

---
+++ 1478047367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1230525795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_4 = 8UL;/* VOLATILE GLOBAL g_4 */
+static int64_t g_12 = 0x88E00F68029989D2LL;
+static int32_t g_58 = 0x4902820AL;
+static int8_t g_71 = 0xD9L;
+static int32_t g_74 = 0x43CA3807L;
+static int8_t g_76 = 1L;
+static int32_t g_78 = 1L;
+static const int32_t *g_77 = &g_78;
+static uint32_t g_115 = 0x9CC38D24L;
