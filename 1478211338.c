944d7423b7f12779de3f69f1f9882b32cad1507e
saisi <saisi@users.noreply.github.com>
2016-11-03 22:15:38 +0000

    1478211338 Bardwell

---
+++ 1478211338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3837674200
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 4L;
+static int32_t g_10 = 3L;
+static volatile int16_t g_11 = 2L;/* VOLATILE GLOBAL g_11 */
+static int8_t g_12 = (-5L);
+static uint64_t g_13 = 0x065C2DD78F4F823ALL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
