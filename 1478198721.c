22aff541b548ca673c3453f8a8a35d8031ca8282
saisi <saisi@users.noreply.github.com>
2016-11-03 18:45:23 +0000

    1478198721 Ablepharus

---
+++ 1478198721.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3921584827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 28;
+   signed f1 : 3;
+   const volatile signed f2 : 26;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
