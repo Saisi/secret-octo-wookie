7da3b325adde2176588706e8bc79f7488782fc89
saisi <saisi@users.noreply.github.com>
2016-11-03 04:44:29 +0000

    1478148269 arsenophenol

---
+++ 1478148269.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1578993509
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 17;
+   uint32_t  f1;
+   volatile int8_t * const  volatile  f2;
+   uint64_t  f3;
+   uint32_t  f4;
+};
+
+union U1 {
+   const volatile uint32_t  f0;
