28c65774bbe0a90d1a4d72d5dbc4cd4e5f2d7e42
saisi <saisi@users.noreply.github.com>
2016-11-03 16:15:11 +0000

    1478189710 aristocrats

---
+++ 1478189710.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3151475774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 13;
+   const int16_t  f1;
+   int8_t  f2;
+   const volatile signed f3 : 21;
+   signed f4 : 20;
+   volatile unsigned f5 : 6;
+};
