c4b41d153219527cf71ae1369ef32ce17d2dd576
saisi <saisi@users.noreply.github.com>
2016-11-03 21:45:36 +0000

    1478209534 Aphthartodocetae

---
+++ 1478209534.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4198172566
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
+   uint8_t  f3;
+   volatile unsigned f4 : 8;
+   signed f5 : 2;
+   int8_t  f6;
+   uint64_t  f7;
+   uint16_t  f8;
