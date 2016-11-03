4727e70e7598796f5e7e3678b68d465eb36b7269
saisi <saisi@users.noreply.github.com>
2016-11-03 05:14:30 +0000

    1478150070 ascomycetes

---
+++ 1478150070.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3623296090
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   unsigned f1 : 4;
+   uint16_t  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+   uint16_t  f1;
+   volatile int8_t * f2;
