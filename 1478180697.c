8a3fb323b9ae7d269197801472264820cf0072ea
saisi <saisi@users.noreply.github.com>
2016-11-03 13:44:58 +0000

    1478180697 amphibryous

---
+++ 1478180697.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1232783878
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   unsigned f1 : 2;
+   const signed f2 : 28;
+   volatile unsigned f3 : 24;
+   volatile signed f4 : 7;
+   int32_t  f5;
+   unsigned f6 : 30;
+   int64_t  f7;
+   unsigned f8 : 17;
