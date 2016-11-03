77795771214092d1e3ff10a10de1c46b82aa657a
saisi <saisi@users.noreply.github.com>
2016-11-03 01:44:20 +0000

    1478137459 asplenium

---
+++ 1478137459.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3677149693
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 29;
+   unsigned : 0;
+   signed f1 : 3;
+   unsigned f2 : 2;
+   unsigned f3 : 11;
+   unsigned f4 : 24;
+   signed f5 : 23;
