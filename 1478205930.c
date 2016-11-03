0eb7ae019ab2b1ed10ab3dcda573b73ffd08f70f
saisi <saisi@users.noreply.github.com>
2016-11-03 20:45:31 +0000

    1478205930 asymbiotic

---
+++ 1478205930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      671456005
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   uint16_t  f1;
+};
+
+struct S1 {
+   unsigned f0 : 31;
+   signed f1 : 27;
+   unsigned f2 : 8;
+   const signed f3 : 11;
