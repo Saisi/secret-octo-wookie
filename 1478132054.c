7506bb1ecee806650cef30320b725abe9a1bded7
saisi <saisi@users.noreply.github.com>
2016-11-03 00:14:15 +0000

    1478132054 Audette

---
+++ 1478132054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3271383624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile unsigned f0 : 18;
+   const uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xD1E1E87DL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x4A8B108FL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-8L);
+static union U0 g_35 = {4UL};/* VOLATILE GLOBAL g_35 */
