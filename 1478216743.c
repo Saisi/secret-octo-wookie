39d75e5f5c01146ff1aa743429234b4730623f58
saisi <saisi@users.noreply.github.com>
2016-11-03 23:45:44 +0000

    1478216743 anthogenesis

---
+++ 1478216743.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      932665093
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int8_t g_29 = 0xE6L;
+static int32_t g_35 = (-5L);
+static volatile int32_t g_44[9] = {(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L)};
+static volatile uint16_t g_45 = 65535UL;/* VOLATILE GLOBAL g_45 */
+static int32_t g_56 = 0x4ADEF089L;
+static uint64_t g_65 = 18446744073709551613UL;
+static uint32_t g_112 = 0xFA11D7DEL;
+static uint32_t g_119 = 0x9694381BL;
