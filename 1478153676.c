804277add728f7a839ec3dffe08df5ff5f46fd54
saisi <saisi@users.noreply.github.com>
2016-11-03 06:14:36 +0000

    1478153676 bandsawing

---
+++ 1478153676.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2992536197
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   uint32_t  f1;
+   const volatile int64_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[4][6] = {{(-1L),0x0B96CA2CL,(-1L),(-1L),0x0B96CA2CL,(-1L)},{(-1L),0x0B96CA2CL,(-1L),(-1L),0x0B96CA2CL,(-1L)},{(-1L),0x0B96CA2CL,(-1L),(-1L),0x0B96CA2CL,(-1L)},{(-1L),0x0B96CA2CL,(-1L),(-1L),0x0B96CA2CL,(-1L)}};
+static int32_t g_3[1][6] = {{(-1L),0x0EDCAD20L,(-1L),(-1L),0x0EDCAD20L,(-1L)}};
