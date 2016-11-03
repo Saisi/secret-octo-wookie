27718f2bbbe8ddd6b55ebab186f10ad05dcdff69
saisi <saisi@users.noreply.github.com>
2016-11-03 12:14:54 +0000

    1478175294 arcading

---
+++ 1478175294.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3990323298
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   uint64_t  f0;
+   int32_t  f1;
+};
+
+union U3 {
+   const struct S1  f0;
+   int32_t  f1;
+   struct S1  f2;
+};
