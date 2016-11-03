29e4897bf547ee55fd906b3a9c3f3731a8862ef3
saisi <saisi@users.noreply.github.com>
2016-11-03 04:14:27 +0000

    1478146467 Bangui

---
+++ 1478146467.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3434779966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   volatile unsigned f0 : 8;
+   signed f1 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x2513244DL;
+static union U2 g_10 = {6UL};/* VOLATILE GLOBAL g_10 */
+
+
