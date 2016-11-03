94d927ed0b414814ced6515a071a5c3f8c99a17c
saisi <saisi@users.noreply.github.com>
2016-11-03 16:45:13 +0000

    1478191512 amperage

---
+++ 1478191512.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1187511246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 21;
+   const signed f1 : 22;
+   volatile signed : 0;
+   unsigned f2 : 12;
+};
+#pragma pack(pop)
+
