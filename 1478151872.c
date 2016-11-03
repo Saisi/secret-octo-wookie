0cabca3d7c30d0022251991a3b97a133bb04122f
saisi <saisi@users.noreply.github.com>
2016-11-03 05:44:32 +0000

    1478151872 bagattini

---
+++ 1478151872.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2934996575
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 14;
+   int8_t  f1;
+   int16_t  f2;
+   uint8_t  f3;
+   const int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_24 = 0xA8CF1D65L;
