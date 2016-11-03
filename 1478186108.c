9a4d07fa65337c780d1f0ad1c1d9cef338e92a7a
saisi <saisi@users.noreply.github.com>
2016-11-03 15:15:08 +0000

    1478186108 arcane

---
+++ 1478186108.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2869231826
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x540D7F82L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xA66FE7D8L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x013DCE9DL;
+static int32_t g_45 = (-4L);
+static uint32_t g_98 = 5UL;
+static uint32_t g_106 = 18446744073709551615UL;
+static uint32_t *g_105[10] = {&g_106,(void*)0,&g_106,&g_106,(void*)0,&g_106,&g_106,(void*)0,&g_106,&g_106};
+static int16_t g_122 = (-10L);
+static int32_t g_125 = 1L;
