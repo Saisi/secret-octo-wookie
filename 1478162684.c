87cda43afafcdd8d755e28eb78f0ebcb72da1f60
saisi <saisi@users.noreply.github.com>
2016-11-03 08:44:44 +0000

    1478162684 aporphin

---
+++ 1478162684.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1603466510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 21;
+   const unsigned f1 : 22;
+};
+#pragma pack(pop)
+
+struct S1 {
+   unsigned f0 : 8;
