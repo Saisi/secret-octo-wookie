77785a0d52fa86ac90d926198c97b14b31004013
saisi <saisi@users.noreply.github.com>
2016-11-03 18:15:20 +0000

    1478196920 autocremation

---
+++ 1478196920.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      628328051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 24;
+   volatile unsigned f1 : 15;
+   const signed f2 : 22;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
