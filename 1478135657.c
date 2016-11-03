aad372e8eb5b7ef393ae0fcfecbbed4bfc5aca0f
saisi <saisi@users.noreply.github.com>
2016-11-03 01:14:18 +0000

    1478135657 anthracomartian

---
+++ 1478135657.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      911745254
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_35 = (-1L);
+static int32_t g_42 = 0xB7E280BFL;
+static int32_t g_63 = 1L;
+static int32_t g_64[2] = {(-2L),(-2L)};
+static uint32_t g_65 = 18446744073709551615UL;
+static int32_t g_70[2] = {0xF37692AFL,0xF37692AFL};
+static uint8_t g_78 = 250UL;
+static int32_t *g_90[6][8] = {{&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42},{&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42},{&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42},{&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42},{&g_42,&g_42,&g_42,&g_42,(void*)0,&g_42,&g_42,&g_42},{&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42,&g_42}};
