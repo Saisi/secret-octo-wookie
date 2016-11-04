1ba77a9e34f35090c3497e2d26b68a7225dfb093
saisi <saisi@users.noreply.github.com>
2016-11-04 11:16:23 +0000

    1478258183 antitobacco

---
+++ 1478258183.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2062599014
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-3L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xC7894801L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[6][7][4] = {{{0xB5C6F8B8L,(-3L),(-9L),0x173BF38CL},{(-5L),(-3L),(-5L),0x2FA4AD74L},{(-3L),(-1L),2L,(-9L)},{(-9L),0x173BF38CL,0xB5C6F8B8L,(-1L)},{1L,0xB5C6F8B8L,0xB5C6F8B8L,1L},{(-9L),0x2FA4AD74L,2L,(-5L)},{(-3L),3L,(-5L),(-6L)}},{{(-5L),(-6L),(-9L),(-6L)},{0xB5C6F8B8L,3L,0x173BF38CL,(-5L)},{(-9L),0x2FA4AD74L,(-6L),1L},{0x173BF38CL,0xB5C6F8B8L,(-1L),(-1L)},{0x173BF38CL,0x173BF38CL,(-3L),0x2FA4AD74L},{(-5L),0xB5C6F8B8L,(-6L),3L},{1L,(-9L),(-9L),(-6L)}},{{(-1L),(-9L),(-1L),3L},{(-9L),0xB5C6F8B8L,(-9L),0x2FA4AD74L},{0x2FA4AD74L,(-6L),1L,0xB5C6F8B8L},{0x173BF38CL,1L,1L,0x173BF38CL},{0x2FA4AD74L,3L,(-9L),(-1L)},{(-9L),2L,(-1L),(-3L)},{(-1L),(-3L),(-9L),(-3L)}},{{1L,2L,(-6L),(-1L)},{(-5L),3L,(-3L),0x173BF38CL},{(-6L),1L,0xB5C6F8B8L,0xB5C6F8B8L},{(-6L),(-6L),(-3L),0x2FA4AD74L},{(-5L),0xB5C6F8B8L,(-6L),3L},{1L,(-9L),(-9L),(-6L)},{(-1L),(-9L),(-1L),3L}},{{(-9L),0xB5C6F8B8L,(-9L),0x2FA4AD74L},{0x2FA4AD74L,(-6L),1L,0xB5C6F8B8L},{0x173BF38CL,1L,1L,0x173BF38CL},{0x2FA4AD74L,3L,(-9L),(-1L)},{(-9L),2L,(-1L),(-3L)},{(-1L),(-3L),(-9L),(-3L)},{1L,2L,(-6L),(-1L)}},{{(-5L),3L,(-3L),0x173BF38CL},{(-6L),1L,0xB5C6F8B8L,0xB5C6F8B8L},{(-6L),(-6L),(-3L),0x2FA4AD74L},{(-5L),0xB5C6F8B8L,(-6L),3L},{1L,(-9L),(-9L),(-6L)},{(-1L),(-9L),(-1L),3L},{(-9L),0xB5C6F8B8L,(-9L),0x2FA4AD74L}}};
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-4L);
+static volatile int32_t g_9[1] = {0x3ECF04B0L};
+static volatile int32_t g_10 = 0xFC36FE5AL;/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = (-1L);
+static int16_t g_17 = (-5L);
