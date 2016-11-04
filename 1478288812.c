08cba11d50a94affc601778a1891dc36cb69fa90
saisi <saisi@users.noreply.github.com>
2016-11-04 19:46:53 +0000

    1478288812 aligners

---
+++ 1478288812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      469139167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xDF75DCECL;
+static int32_t g_14 = 8L;
+static uint64_t g_26[9] = {4UL,4UL,4UL,4UL,4UL,4UL,4UL,4UL,4UL};
+static int16_t g_30 = 1L;
+static int32_t g_34 = 0x0E402095L;
+static int32_t * volatile g_33 = &g_34;/* VOLATILE GLOBAL g_33 */
+static int8_t g_52 = 0x7FL;
+static int32_t g_55 = 0xABDBFAD2L;
+static int8_t g_101 = 0x7CL;
