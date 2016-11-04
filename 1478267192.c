46e755acf0eccc3e738cc68f11c8bec14ec9d124
saisi <saisi@users.noreply.github.com>
2016-11-04 13:46:33 +0000

    1478267192 appleberry

---
+++ 1478267192.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2569583471
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   unsigned f1 : 27;
+   volatile unsigned f2 : 5;
+   volatile int16_t  f3;
+   volatile uint8_t  f4;
+   signed f5 : 16;
+   volatile signed f6 : 20;
+   unsigned f7 : 29;
+   unsigned f8 : 18;
