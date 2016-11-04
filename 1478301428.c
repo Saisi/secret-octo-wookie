f37b34c2ea5ad9bfe96292f5d732152f004068f8
saisi <saisi@users.noreply.github.com>
2016-11-04 23:17:08 +0000

    1478301428 bagatine

---
+++ 1478301428.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3046651754
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[4][10] = {{0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL},{(-10L),(-10L),7L,(-10L),(-10L),7L,(-10L),(-10L),7L,(-10L)},{(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L)},{0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL,(-10L),0x59609C3FL,0x59609C3FL}};
+static int32_t g_6 = (-7L);
+static int32_t g_8 = (-5L);
+static int32_t * volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+static uint8_t g_43 = 1UL;
+static int32_t g_68 = 0x63D476FCL;
+static int8_t g_86[4][1][5] = {{{(-8L),(-8L),(-8L),(-8L),(-8L)}},{{(-8L),(-8L),(-8L),(-8L),(-8L)}},{{(-8L),(-8L),(-8L),(-8L),(-8L)}},{{(-8L),(-8L),(-8L),(-8L),(-8L)}}};
+static int32_t * volatile g_88 = &g_68;/* VOLATILE GLOBAL g_88 */
+static int32_t g_95[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
