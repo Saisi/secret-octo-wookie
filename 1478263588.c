f997b9510264ccdd0d7d25cf71a0045661f6b4f9
saisi <saisi@users.noreply.github.com>
2016-11-04 12:46:29 +0000

    1478263588 Acarina

---
+++ 1478263588.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      516798974
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   const uint8_t  f2;
+   int8_t * const  f3;
+   int8_t * f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 6L;
