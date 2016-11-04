4966913eee0dba0eaa5c0adf1d2ec527cc041cd8
saisi <saisi@users.noreply.github.com>
2016-11-04 08:46:15 +0000

    1478249175 Allana

---
+++ 1478249175.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      876981517
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   int8_t * const  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 2L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-1L);
+static int32_t g_7 = 0L;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
