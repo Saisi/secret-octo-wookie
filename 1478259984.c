a665c480264b0c152eedc321a712bfcc720c9304
saisi <saisi@users.noreply.github.com>
2016-11-04 11:46:26 +0000

    1478259984 Badalona

---
+++ 1478259984.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1368362277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int64_t  f0;
+};
+
+union U1 {
+   uint32_t  f0;
+   unsigned f1 : 24;
+   int32_t  f2;
+   int32_t  f3;
+   volatile int32_t  f4;
