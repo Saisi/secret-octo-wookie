e8677748a7bc7734a714fd4bfa032db367d72e2b
saisi <saisi@users.noreply.github.com>
2016-11-04 15:46:38 +0000

    1478274398 Amblyopsidae

---
+++ 1478274398.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2301027415
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 10;
+   volatile unsigned f1 : 25;
+   const unsigned f2 : 24;
+   const signed f3 : 3;
+   signed f4 : 8;
+   volatile unsigned f5 : 1;
+   unsigned f6 : 28;
