59e1ebc2f6a412d9a7b5d41232dc99ddb707cc8f
saisi <saisi@users.noreply.github.com>
2016-11-04 15:16:37 +0000

    1478272596 angiotonase

---
+++ 1478272596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3181096629
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 25;
+   signed f1 : 12;
+   unsigned f2 : 31;
+   signed f3 : 14;
+   volatile unsigned f4 : 7;
+   unsigned f5 : 4;
+   unsigned f6 : 19;
+   volatile signed f7 : 26;
+};
