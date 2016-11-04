e5f29d4995f56562a17105677946523d4e7adc6b
saisi <saisi@users.noreply.github.com>
2016-11-04 09:46:19 +0000

    1478252778 bacterian

---
+++ 1478252778.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2418074368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   unsigned f1 : 11;
+   signed f2 : 1;
+   volatile signed f3 : 9;
+   volatile unsigned f4 : 20;
+   volatile unsigned f5 : 27;
+   const volatile unsigned f6 : 13;
+   unsigned f7 : 14;
+   unsigned f8 : 26;
