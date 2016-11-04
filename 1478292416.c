f334b652615b39b374de52b025b5fc52f8308c6b
saisi <saisi@users.noreply.github.com>
2016-11-04 20:46:57 +0000

    1478292416 aglucone

---
+++ 1478292416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3694975915
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_8[2] = {65534UL,65534UL};
+static int32_t g_11 = 1L;
+static uint32_t g_20 = 0x4B0AD8EAL;
+static uint8_t g_26 = 0x4CL;
+static int32_t g_53 = 0xF03FA9A1L;
