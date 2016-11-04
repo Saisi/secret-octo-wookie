7a096eea60772acaf3ce5761cc44109870b0bebd
saisi <saisi@users.noreply.github.com>
2016-11-04 01:15:48 +0000

    1478222147 antipleuritic

---
+++ 1478222147.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      802665188
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[9] = {0x52061F66L,0x327CB3F4L,0x52061F66L,0x52061F66L,0x327CB3F4L,0x52061F66L,0x52061F66L,0x327CB3F4L,0x52061F66L};
+static volatile int32_t g_3 = 0xC8FC558BL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static volatile int32_t g_7 = 0x3707822EL;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8[6] = {0x7B8F5F49L,0x7B8F5F49L,0x7B8F5F49L,0x7B8F5F49L,0x7B8F5F49L,0x7B8F5F49L};
