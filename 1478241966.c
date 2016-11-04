bd62285ed01efb09dbb1ecf74273ca6a5ab5b729
saisi <saisi@users.noreply.github.com>
2016-11-04 06:46:09 +0000

    1478241966 bandstring

---
+++ 1478241966.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3095243695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 22;
+   int32_t  f1;
+   unsigned f2 : 5;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   int16_t  f1;
+   int8_t * volatile  f2;
