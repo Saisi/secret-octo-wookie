b031fdc4dbcf934131a4ccf0a858fde1f8d0daef
saisi <saisi@users.noreply.github.com>
2016-11-04 17:46:46 +0000

    1478281605 anthropotheism

---
+++ 1478281605.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2850238251
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 20;
+   signed f1 : 27;
+   const unsigned f2 : 18;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
