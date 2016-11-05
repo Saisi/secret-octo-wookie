a6eb60a7b1c62e0c97769a6e92f045ccbb0a5a0f
saisi <saisi@users.noreply.github.com>
2016-11-05 00:17:10 +0000

    1478305030 barbule

---
+++ 1478305030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2591539265
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int64_t g_4 = 6L;/* VOLATILE GLOBAL g_4 */
+static uint64_t g_5 = 0x968939F5FA318711LL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
