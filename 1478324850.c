d8b552c06881bec8c8efd846e94075576aa812e7
saisi <saisi@users.noreply.github.com>
2016-11-05 05:47:30 +0000

    1478324850 Alcibiades

---
+++ 1478324850.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3023664639
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 30;
+   unsigned f1 : 29;
+   const signed f2 : 12;
+   unsigned f3 : 7;
+   const volatile unsigned f4 : 28;
+};
+#pragma pack(pop)
