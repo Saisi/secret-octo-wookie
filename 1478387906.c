98c54b4e0d1dacf9f0a1b3fae185adc80fd0b2b7
saisi <saisi@users.noreply.github.com>
2016-11-05 23:18:26 +0000

    1478387906 authoritarian

---
+++ 1478387906.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3375723636
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   volatile unsigned f1 : 29;
+   const unsigned f2 : 11;
+   signed f3 : 11;
+   signed f4 : 14;
+   unsigned f5 : 13;
+   signed f6 : 23;
