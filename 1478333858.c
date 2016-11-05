7471fd74e7b199ba7fff2218ee28fac3fb13aa90
saisi <saisi@users.noreply.github.com>
2016-11-05 08:17:38 +0000

    1478333858 autocephaly

---
+++ 1478333858.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1248992037
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   uint32_t  f1;
+   const uint32_t  f2;
+   const volatile int16_t  f3;
+   volatile unsigned f4 : 4;
+   int8_t  f5;
+   uint8_t  f6;
+   signed f7 : 9;
+};
