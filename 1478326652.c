e601751b3468a2d976f59595f4e1c44b9e8d0e6d
saisi <saisi@users.noreply.github.com>
2016-11-05 06:17:32 +0000

    1478326652 assonances

---
+++ 1478326652.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1967552158
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 29;
+   unsigned f1 : 30;
+   const volatile unsigned f2 : 8;
+   unsigned f3 : 20;
+   const int32_t  f4;
+   const volatile signed f5 : 13;
+   signed f6 : 26;
