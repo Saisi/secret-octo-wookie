2821a7136d07bfd8e5dcce8ec76cbe3c41adabf8
saisi <saisi@users.noreply.github.com>
2016-11-05 07:17:35 +0000

    1478330255 anthropophagize

---
+++ 1478330255.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3433476442
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xF69F7FD1L;
+static uint8_t g_4 = 247UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
