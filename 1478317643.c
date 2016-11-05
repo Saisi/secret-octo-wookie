1a780039cd68220ea1a17200f282da2c00a50af8
saisi <saisi@users.noreply.github.com>
2016-11-05 03:47:23 +0000

    1478317643 archpretender

---
+++ 1478317643.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1744714828
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int16_t  f0;
+   signed f1 : 1;
+   const volatile int64_t  f2;
+   volatile uint16_t  f3;
+   volatile uint64_t  f4;
+   volatile uint16_t  f5;
+   volatile uint32_t  f6;
+};
+
