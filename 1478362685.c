5dd79d59480fddf852fc79997766bf941e017a3c
saisi <saisi@users.noreply.github.com>
2016-11-05 16:18:05 +0000

    1478362685 Aspredinidae

---
+++ 1478362685.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2227804046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xB26BDDE0L;
+static uint64_t g_24 = 0UL;
+static int64_t g_30 = 0xE53494ABC2489102LL;
+static int32_t g_59 = 0xB861A716L;
+static int32_t * volatile g_58 = &g_59;/* VOLATILE GLOBAL g_58 */
+static int32_t g_75 = 4L;
+static int32_t * volatile g_74[9] = {&g_75,&g_75,&g_75,&g_75,&g_75,&g_75,&g_75,&g_75,&g_75};
+static int64_t g_88 = 0x63E3B667D15778C4LL;
+static int32_t *g_97 = &g_2;
