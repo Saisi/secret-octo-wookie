316fdb6dfa6fa74804402b00f59b693cc1c0255a
saisi <saisi@users.noreply.github.com>
2016-11-05 15:18:00 +0000

    1478359080 astonishedly

---
+++ 1478359080.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1246792812
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0x822BL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4[9] = {1L,1L,1L,1L,1L,1L,1L,1L,1L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
