f0d9da6130233c421ce6e29a5f04f116e9170dee
saisi <saisi@users.noreply.github.com>
2016-11-05 11:17:49 +0000

    1478344668 Ammodytes

---
+++ 1478344668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3827966843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 28;
+   unsigned f1 : 3;
+   signed f2 : 26;
+};
+
+struct S1 {
+   unsigned f0 : 6;
+   const int16_t  f1;
+   const volatile unsigned f2 : 26;
