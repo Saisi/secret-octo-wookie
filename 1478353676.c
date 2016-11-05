6ccfa12ef5d244555746059e0fe66e23ac65f9ee
saisi <saisi@users.noreply.github.com>
2016-11-05 13:47:56 +0000

    1478353676 aortotomy

---
+++ 1478353676.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2536865912
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   volatile int32_t  f0;
+   volatile int16_t  f1;
+   unsigned f2 : 14;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U3 g_2[7][3][2] = {{{{0x184DCC80L},{0xFEBFB2C4L}},{{0x4C8F0D07L},{0xAAF5BFB6L}},{{0x4C8F0D07L},{0xFEBFB2C4L}}},{{{0x184DCC80L},{0x4C8F0D07L}},{{0xFEBFB2C4L},{0xAAF5BFB6L}},{{0xC3A065C2L},{0xC3A065C2L}}},{{{0x184DCC80L},{0xC3A065C2L}},{{0xC3A065C2L},{0xAAF5BFB6L}},{{0xFEBFB2C4L},{0x4C8F0D07L}}},{{{0x184DCC80L},{0xFEBFB2C4L}},{{0x4C8F0D07L},{0xAAF5BFB6L}},{{0x4C8F0D07L},{0xFEBFB2C4L}}},{{{0x184DCC80L},{0x4C8F0D07L}},{{0xFEBFB2C4L},{0xAAF5BFB6L}},{{0xC3A065C2L},{0xC3A065C2L}}},{{{0x184DCC80L},{0xC3A065C2L}},{{0xC3A065C2L},{0xAAF5BFB6L}},{{0xFEBFB2C4L},{0x4C8F0D07L}}},{{{0x184DCC80L},{0xFEBFB2C4L}},{{0x4C8F0D07L},{0xAAF5BFB6L}},{{0x4C8F0D07L},{0xFEBFB2C4L}}}};
+
