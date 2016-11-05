6dff00e66472cf401e57cec4056c5095e234cb0d
saisi <saisi@users.noreply.github.com>
2016-11-05 14:47:59 +0000

    1478357278 Autarchoglossa

---
+++ 1478357278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2055023804
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7[5] = {0xF84B8114L,0xF84B8114L,0xF84B8114L,0xF84B8114L,0xF84B8114L};
+static int16_t g_12[1][2] = {{0x3F36L,0x3F36L}};
+static uint16_t g_14 = 0xC25BL;
+static int16_t g_20[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int16_t * volatile g_19 = &g_20[3];/* VOLATILE GLOBAL g_19 */
+static int32_t *g_28 = &g_7[3];
+static volatile int32_t g_31[6] = {0x548AAB56L,0xAE215169L,0xAE215169L,0x548AAB56L,0xAE215169L,0xAE215169L};
+static volatile int32_t g_32 = 0xAA5D732AL;/* VOLATILE GLOBAL g_32 */
+static int32_t g_33 = (-1L);
