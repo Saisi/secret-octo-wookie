ba66cefc2d30a8792d4b635df11718b3c984177f
saisi <saisi@users.noreply.github.com>
2016-11-05 02:17:19 +0000

    1478312239 anagyrine

---
+++ 1478312239.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3121631064
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 26;
+   volatile signed f1 : 7;
+   signed f2 : 13;
+   unsigned f3 : 5;
+   signed : 0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0xA3367F5EL;
