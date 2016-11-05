ed384e469e97a2afde7948728ebb0758cfee2e1d
saisi <saisi@users.noreply.github.com>
2016-11-05 04:17:25 +0000

    1478319444 amnesty

---
+++ 1478319444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      403884710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 1;
+   signed f1 : 18;
+   unsigned f2 : 12;
+   signed f3 : 8;
+   volatile unsigned f4 : 23;
+   signed f5 : 18;
+   signed f6 : 8;
