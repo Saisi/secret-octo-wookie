906311a7bf6fcd6b593540de5d930ee043b61c6b
saisi <saisi@users.noreply.github.com>
2016-11-06 00:48:30 +0000

    1478393310 accessors

---
+++ 1478393310.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4035435697
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_5[8][10] = {{(-1L),0x70L,0x0FL,0x0FL,0x70L,(-1L),1L,8L,0x0FL,1L},{0x01L,0x70L,0x6BL,8L,0x70L,0L,0x70L,8L,0x6BL,0x70L},{0x01L,1L,0x0FL,8L,1L,(-1L),0x70L,0x0FL,0x0FL,0x70L},{(-1L),0x70L,0x0FL,0x0FL,0x70L,(-1L),1L,8L,0x0FL,1L},{0x01L,0x70L,0x6BL,8L,0x70L,0L,0L,1L,0x70L,0L},{0x0AL,1L,1L,1L,1L,9L,0L,1L,1L,0L},{9L,0L,1L,1L,0L,9L,1L,1L,1L,1L},{0x0AL,0L,0x70L,1L,0L,(-7L),0L,1L,0x70L,0L}};
+static int8_t *g_4 = &g_5[0][0];
+static int32_t g_7 = 0x1AF9D66DL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
