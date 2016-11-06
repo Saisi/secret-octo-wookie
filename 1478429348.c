b996ed88c7546013897d9cfc9bd3453d6de31ea1
saisi <saisi@users.noreply.github.com>
2016-11-06 10:49:09 +0000

    1478429348 anatox

---
+++ 1478429348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3178734182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 1;
+   const signed f1 : 18;
+   volatile signed f2 : 12;
+   signed f3 : 8;
+   const volatile unsigned f4 : 23;
+   volatile unsigned f5 : 18;
+   unsigned f6 : 8;
