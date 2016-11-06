aabadad0084da248f978ad170c80bdd698c56804
saisi <saisi@users.noreply.github.com>
2016-11-06 14:19:18 +0000

    1478441957 acetylbenzene

---
+++ 1478441957.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145447417
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   unsigned f1 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = (-1L);
+static int32_t *g_6[1][1] = {{&g_7}};
+static int32_t g_46[5][8][6] = {{{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)},{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)}},{{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)},{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)}},{{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)},{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)}},{{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)},{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)}},{{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)},{0xD4E2095EL,2L,0xD4E2095EL,(-1L),0xD4E2095EL,2L},{(-1L),2L,(-4L),2L,(-1L),2L},{0xD4E2095EL,(-1L),0xD4E2095EL,2L,0xD4E2095EL,(-1L)},{(-1L),(-1L),(-4L),(-1L),(-1L),(-1L)}}};
+static uint32_t g_56 = 0xA1D127A4L;
