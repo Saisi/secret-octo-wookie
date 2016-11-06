fa7e94ef32319aadb67c798f24914c25365ed4ac
saisi <saisi@users.noreply.github.com>
2016-11-06 18:49:35 +0000

    1478458175 alcidine

---
+++ 1478458175.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2494458100
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   unsigned f1 : 29;
+   signed f2 : 11;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static uint32_t g_20 = 0xEC3203BDL;
+static int32_t g_40[4][8] = {{4L,0x32D38CA8L,0x32D38CA8L,4L,0x0639E6A4L,(-6L),0x0639E6A4L,4L},{0x32D38CA8L,0x0639E6A4L,0x32D38CA8L,1L,0x674FA72DL,0x674FA72DL,1L,0x32D38CA8L},{0x0639E6A4L,0x0639E6A4L,0x674FA72DL,(-6L),(-6L),(-6L),0x674FA72DL,0x0639E6A4L},{0x0639E6A4L,0x32D38CA8L,1L,0x674FA72DL,0x674FA72DL,1L,0x32D38CA8L,0x0639E6A4L}};
