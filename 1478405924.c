79604077700c46b03581e933394aa683f242934a
saisi <saisi@users.noreply.github.com>
2016-11-06 04:18:44 +0000

    1478405924 Antipascha

---
+++ 1478405924.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1112556965
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   volatile int64_t  f1;
+   uint16_t  f2;
+   const int8_t * volatile  f3;
+};
+
+union U1 {
+   volatile unsigned f0 : 27;
+   int32_t  f1;
