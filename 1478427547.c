bbfd14019f0767299b94b1d5f0dc354e7089ebf9
saisi <saisi@users.noreply.github.com>
2016-11-06 10:19:07 +0000

    1478427547 angina

---
+++ 1478427547.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      707545062
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 7L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * const  volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static int32_t g_39 = (-1L);
+static int32_t *g_38 = &g_39;
+static int32_t g_43 = 0xA6E483BBL;
+static int32_t g_64 = 1L;
+static uint8_t g_73 = 8UL;
+static volatile int32_t g_124 = 0L;/* VOLATILE GLOBAL g_124 */
+static int32_t g_125 = 1L;
