72dbc1977be6962c5b67b72bf41e99c5af83dc4a
saisi <saisi@users.noreply.github.com>
2016-11-06 19:49:39 +0000

    1478461778 bandwidths

---
+++ 1478461778.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      986587611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   uint64_t  f1;
+   unsigned f2 : 19;
+};
+
+struct S1 {
+   signed f0 : 10;
+   unsigned f1 : 27;
+   signed f2 : 18;
