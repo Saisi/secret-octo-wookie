a009853e1f63f8996dfec1d0ce3e914d1aee5b45
saisi <saisi@users.noreply.github.com>
2016-11-06 16:49:26 +0000

    1478450965 amassing

---
+++ 1478450965.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2880738118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   signed f1 : 2;
+   uint32_t  f2;
+   uint8_t  f3;
+};
+
+union U1 {
+   const uint64_t  f0;
+   volatile uint64_t  f1;
