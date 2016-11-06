855d609719f3758bb9e3e2bb39fa5b4226bfadfe
saisi <saisi@users.noreply.github.com>
2016-11-06 03:48:43 +0000

    1478404123 amyotrophia

---
+++ 1478404123.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3247225576
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2][8] = {{0x63C40785L,0x0874B28EL,0x56890C54L,0x56890C54L,0x0874B28EL,0x63C40785L,0x0874B28EL,0x56890C54L},{0x5B054DBBL,0x0874B28EL,0x5B054DBBL,0x63C40785L,0x63C40785L,0x5B054DBBL,0x0874B28EL,0x5B054DBBL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
