8fadabc76841c5e343b9b41218c319a5360b2f0f
saisi <saisi@users.noreply.github.com>
2016-11-06 14:49:19 +0000

    1478443759 aciliate

---
+++ 1478443759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1018257808
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_24 = 1L;/* VOLATILE GLOBAL g_24 */
+static uint32_t g_31 = 1UL;
+static int32_t g_40 = 0xB09B37D0L;
+static uint32_t g_57 = 0xAFEC1E18L;
