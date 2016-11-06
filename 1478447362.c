24a31dd867630fef69c535d3680788d9cd548c29
saisi <saisi@users.noreply.github.com>
2016-11-06 15:49:23 +0000

    1478447362 autoelectrolytic

---
+++ 1478447362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1429942834
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   const unsigned : 0;
+   unsigned f1 : 2;
+   signed f2 : 6;
+   const signed f3 : 29;
+   unsigned : 0;
+   const unsigned f4 : 30;
+   signed f5 : 27;
+};
