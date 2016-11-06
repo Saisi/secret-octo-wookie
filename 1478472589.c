0d7d4af574ddc0aafe55b2657bd1fca7458289b8
saisi <saisi@users.noreply.github.com>
2016-11-06 22:49:49 +0000

    1478472589 anhysteretic

---
+++ 1478472589.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1319398165
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 9;
+   signed f1 : 28;
+   const signed f2 : 15;
+   signed f3 : 30;
+   unsigned f4 : 23;
+   signed f5 : 20;
+};
+
+struct S1 {
