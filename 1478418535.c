70412eabb60881fe08db45213518a38b96d3b488
saisi <saisi@users.noreply.github.com>
2016-11-06 07:48:55 +0000

    1478418535 awacs

---
+++ 1478418535.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1715650031
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 22;
+};
+
+struct S1 {
+   signed f0 : 5;
+   unsigned f1 : 21;
+   unsigned f2 : 8;
+   unsigned f3 : 22;
+};
