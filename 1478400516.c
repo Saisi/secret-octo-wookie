6e49e409faf11e901c041e3d372de2f8d7b32e02
saisi <saisi@users.noreply.github.com>
2016-11-06 02:48:38 +0000

    1478400516 Arcacea

---
+++ 1478400516.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2239002870
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 26;
+   signed f2 : 20;
+   const signed f3 : 16;
+   const unsigned f4 : 31;
+   unsigned f5 : 26;
+   const signed f6 : 16;
+   uint64_t  f7;
+   uint16_t  f8;
