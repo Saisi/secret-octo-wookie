d1e1fb9e44621d032f807554a5b2df0f6a34735d
saisi <saisi@users.noreply.github.com>
2016-11-06 13:19:15 +0000

    1478438355 allegorisation

---
+++ 1478438355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1580317374
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 13;
+   unsigned f1 : 14;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
