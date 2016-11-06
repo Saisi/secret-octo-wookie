dddf9f8c7c0bd1817d5b9c4969d281131f2430fd
saisi <saisi@users.noreply.github.com>
2016-11-06 02:18:35 +0000

    1478398715 appartement

---
+++ 1478398715.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3327672425
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 19;
+   const signed f1 : 30;
+};
+#pragma pack(pop)
+
+struct S1 {
+   struct S0  f0;
