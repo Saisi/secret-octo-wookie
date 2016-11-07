4b884b6f970b53aaa3c41d6285447886ab3b8ec9
saisi <saisi@users.noreply.github.com>
2016-11-07 22:21:18 +0000

    1478557278 anglophilism

---
+++ 1478557278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3533089314
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 31;
+   volatile unsigned f1 : 24;
+   unsigned f2 : 26;
+   const unsigned f3 : 30;
+   signed f4 : 21;
+   unsigned f5 : 24;
+   unsigned f6 : 22;
+   const signed f7 : 19;
+};
