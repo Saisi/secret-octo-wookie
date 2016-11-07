a1663b083a995d09f7b43de310ef2b709d7388c3
saisi <saisi@users.noreply.github.com>
2016-11-07 07:20:25 +0000

    1478503225 balneography

---
+++ 1478503225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1361388012
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_8 = 0xC3938DD44AD37400LL;/* VOLATILE GLOBAL g_8 */
+static uint8_t g_64 = 0x2CL;
+static uint16_t g_90 = 65534UL;
+static int64_t g_143 = (-9L);
+static uint32_t g_146 = 4294967290UL;
+static uint16_t *g_149 = &g_90;
+static int32_t g_151[9] = {0xB199FC80L,(-1L),0xB199FC80L,0xB199FC80L,(-1L),0xB199FC80L,0xB199FC80L,(-1L),0xB199FC80L};
+static int32_t *g_150[2] = {&g_151[8],&g_151[8]};
+static uint32_t g_175[2] = {18446744073709551615UL,18446744073709551615UL};
