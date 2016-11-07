77822db3bc0096517b1c7044b2c4a976255aba05
saisi <saisi@users.noreply.github.com>
2016-11-07 17:21:00 +0000

    1478539259 backtenter

---
+++ 1478539259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3810386926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile unsigned f1 : 6;
+   volatile signed f2 : 16;
+   volatile unsigned f3 : 28;
+   const volatile unsigned f4 : 27;
+   unsigned f5 : 6;
+   signed f6 : 12;
+   const volatile unsigned f7 : 17;
+   int64_t  f8;
