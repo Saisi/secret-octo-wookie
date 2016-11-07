68c5ebb1aa2d45ea1397a89c5f8f2df7115edef7
saisi <saisi@users.noreply.github.com>
2016-11-07 08:50:29 +0000

    1478508629 backstopping

---
+++ 1478508629.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4023006275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[3] = {7L,7L,7L};
+static int8_t g_11 = (-1L);
+static int32_t *g_15 = (void*)0;
+static int32_t ** volatile g_14 = &g_15;/* VOLATILE GLOBAL g_14 */
+static int32_t g_18 = 0xEAB1D6A5L;
+static int32_t * volatile g_17 = &g_18;/* VOLATILE GLOBAL g_17 */
+
+
+/* --- FORWARD DECLARATIONS --- */
