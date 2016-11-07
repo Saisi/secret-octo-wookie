9cbe1f9ef576327ce73b160692d74009876a88c1
saisi <saisi@users.noreply.github.com>
2016-11-07 16:20:56 +0000

    1478535655 alhambresque

---
+++ 1478535655.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      450127453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   volatile uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_14 = 1UL;/* VOLATILE GLOBAL g_14 */
+static const uint8_t g_21 = 0UL;
+static union U0 *g_22 = (void*)0;
+static uint16_t g_28 = 0xDF7EL;
