4ab47ae30dae9f2e076ca281729551baccfc6ea9
saisi <saisi@users.noreply.github.com>
2016-11-07 04:50:17 +0000

    1478494217 aminoaciduria

---
+++ 1478494217.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2505056934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+static int32_t g_41 = (-9L);
+static uint8_t g_78 = 0x40L;
+static int16_t g_83 = 0x2398L;
+static uint8_t g_99[2][3] = {{0UL,0UL,0UL},{0x0DL,0x0DL,0x0DL}};
+static uint8_t *g_126 = &g_99[1][1];
+static uint8_t ** volatile g_125 = &g_126;/* VOLATILE GLOBAL g_125 */
+static uint8_t ** volatile *g_124 = &g_125;
