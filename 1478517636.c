e67afcafdd4362bcab568e9c5d774e018a70d0a8
saisi <saisi@users.noreply.github.com>
2016-11-07 11:20:36 +0000

    1478517636 aspy

---
+++ 1478517636.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4274275245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4] = {(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_35 = 0xC39C9D94L;
+static volatile int32_t g_46 = (-1L);/* VOLATILE GLOBAL g_46 */
+static volatile uint32_t g_47 = 4294967295UL;/* VOLATILE GLOBAL g_47 */
+static volatile int32_t **g_80 = (void*)0;
+static const int32_t g_85 = 0xC9EA21F9L;
+static int64_t g_109 = 0L;
+static int16_t g_111 = 1L;
+static uint32_t g_115 = 0x04B69D3DL;
