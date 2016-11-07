1dbf6222bb11a3895dee4808c59e2acc1b5bb4f6
saisi <saisi@users.noreply.github.com>
2016-11-07 09:20:30 +0000

    1478510430 balkish

---
+++ 1478510430.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1766551263
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   signed f1 : 19;
+   signed f2 : 29;
+};
+
+union U1 {
+   const volatile signed f0 : 9;
+   signed f1 : 8;
+   const volatile uint16_t  f2;
