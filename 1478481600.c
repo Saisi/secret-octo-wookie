c85639cc518da3fc795bd99facb784ed0a4ddb03
saisi <saisi@users.noreply.github.com>
2016-11-07 01:20:00 +0000

    1478481600 abdal

---
+++ 1478481600.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1833741435
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 1UL;/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
