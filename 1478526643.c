c7b467d63eda033b73414d84b9faa5cf7d3321e3
saisi <saisi@users.noreply.github.com>
2016-11-07 13:50:43 +0000

    1478526643 almirah

---
+++ 1478526643.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1471096596
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t  f1;
+   const uint8_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xE97EEDE0L;
+static const int32_t g_32 = 0xE491BA6BL;
