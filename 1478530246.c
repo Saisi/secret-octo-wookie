f26641329b32a362527954dff283752e41ffb7ad
saisi <saisi@users.noreply.github.com>
2016-11-07 14:50:46 +0000

    1478530246 ambary

---
+++ 1478530246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2930438133
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   volatile int64_t  f1;
+   int32_t  f2;
+   const int8_t  f3;
+   int16_t  f4;
+   int8_t  f5;
+   volatile signed f6 : 4;
+   int64_t  f7;
+};
