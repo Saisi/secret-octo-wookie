94fc6bd369757567085ef2bbb3d83f479b4ab79a
saisi <saisi@users.noreply.github.com>
2016-11-07 06:50:24 +0000

    1478501423 Archbp

---
+++ 1478501423.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2265687767
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int8_t  f1;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0xB0CEAFD9L;
+static int8_t g_15[5] = {(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile union U0 g_52 = {0x5FFFF323L};/* VOLATILE GLOBAL g_52 */
