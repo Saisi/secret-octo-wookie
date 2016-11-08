068635d57e384140725f7a74fc735dc6b0bbadbc
saisi <saisi@users.noreply.github.com>
2016-11-08 07:21:52 +0000

    1478589712 antilobium

---
+++ 1478589712.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2633579252
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const uint32_t  f0;
+   uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+
+
+/* --- FORWARD DECLARATIONS --- */
