3b624c083ea7681492d643251f9237cc3692604c
saisi <saisi@users.noreply.github.com>
2016-11-08 21:22:37 +0000

    1478640157 allegresse

---
+++ 1478640157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4190742146
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   volatile uint64_t  f1;
+};
+
+struct S1 {
+   const unsigned f0 : 6;
+   const signed f1 : 7;
+   unsigned f2 : 2;
+   const unsigned f3 : 27;
