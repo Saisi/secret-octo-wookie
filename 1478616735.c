308b7ce62ef55026d6484e60451ab622d9131628
saisi <saisi@users.noreply.github.com>
2016-11-08 14:52:16 +0000

    1478616735 arouser

---
+++ 1478616735.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      71748699
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 12;
+   volatile int64_t  f1;
+   const volatile int32_t  f2;
+   const uint8_t  f3;
+   uint16_t  f4;
+   uint64_t  f5;
+   volatile int8_t  f6;
+   int16_t  f7;
+   uint64_t  f8;
