d00dbe271bdaf1af416372992ae9be962840be9e
saisi <saisi@users.noreply.github.com>
2016-11-08 18:52:30 +0000

    1478631150 aviates

---
+++ 1478631150.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3951972502
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 7L;
+static uint16_t g_27 = 0UL;
+static int32_t * volatile g_45 = &g_7;/* VOLATILE GLOBAL g_45 */
+static int32_t g_46[4][6] = {{(-9L),0xC6EB81F9L,(-9L),(-9L),0xC6EB81F9L,(-9L)},{(-9L),0xC6EB81F9L,(-9L),(-9L),0xC6EB81F9L,(-9L)},{(-9L),0xC6EB81F9L,(-9L),(-9L),0xC6EB81F9L,(-9L)},{(-9L),0xC6EB81F9L,(-9L),(-9L),0xC6EB81F9L,(-9L)}};
+static int32_t g_47 = 0x61302D9CL;
+static uint16_t g_57 = 0xB720L;
+static uint16_t *g_56 = &g_57;
+static int16_t g_64 = (-1L);
+static uint16_t **g_84 = (void*)0;
