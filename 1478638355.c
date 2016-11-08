cf0f16f402f82d6cebcc62e0404c501af6b3a0a8
saisi <saisi@users.noreply.github.com>
2016-11-08 20:52:36 +0000

    1478638355 adenomas

---
+++ 1478638355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1362636990
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 13;
+   unsigned f1 : 14;
+   volatile unsigned f2 : 24;
+   unsigned f3 : 4;
+   int32_t  f4;
+   const unsigned f5 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
