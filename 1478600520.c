5023c0f1ea0c57dbc5c091ee8f56b1c1cd1ca76e
saisi <saisi@users.noreply.github.com>
2016-11-08 10:22:00 +0000

    1478600520 amphioxi

---
+++ 1478600520.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      103747123
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[4] = {(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_3 = 0x7A876E6BL;
+static int32_t g_7[10] = {0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL,0xD94543BEL};
+static uint32_t g_55[8] = {0xBA8D6E85L,8UL,8UL,0xBA8D6E85L,8UL,8UL,0xBA8D6E85L,8UL};
+static int32_t *g_81 = &g_3;
+static int32_t **g_80 = &g_81;
+static int32_t *** volatile g_79[8][3][9] = {{{(void*)0,(void*)0,&g_80,&g_80,(void*)0,&g_80,(void*)0,&g_80,&g_80},{(void*)0,(void*)0,&g_80,&g_80,&g_80,&g_80,(void*)0,(void*)0,&g_80},{(void*)0,(void*)0,(void*)0,&g_80,&g_80,&g_80,&g_80,(void*)0,(void*)0}},{{(void*)0,&g_80,&g_80,&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80},{&g_80,&g_80,(void*)0,(void*)0,(void*)0,&g_80,&g_80,&g_80,&g_80},{&g_80,(void*)0,&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,(void*)0}},{{&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,(void*)0,(void*)0,&g_80},{&g_80,(void*)0,(void*)0,(void*)0,&g_80,&g_80,&g_80,&g_80,&g_80},{&g_80,(void*)0,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80}},{{&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80},{&g_80,(void*)0,(void*)0,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80},{&g_80,(void*)0,&g_80,&g_80,&g_80,&g_80,&g_80,(void*)0,&g_80}},{{&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,(void*)0,&g_80},{&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,&g_80,(void*)0,&g_80},{&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80,&g_80}},{{&g_80,&g_80,(void*)0,&g_80,(void*)0,&g_80,&g_80,&g_80,&g_80},{(void*)0,&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,(void*)0},{(void*)0,&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,(void*)0,&g_80}},{{(void*)0,&g_80,(void*)0,&g_80,&g_80,(void*)0,&g_80,(void*)0,&g_80},{(void*)0,&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80,(void*)0,(void*)0},{(void*)0,&g_80,&g_80,(void*)0,&g_80,&g_80,(void*)0,&g_80,&g_80}},{{&g_80,(void*)0,(void*)0,&g_80,(void*)0,(void*)0,&g_80,&g_80,&g_80},{&g_80,(void*)0,&g_80,&g_80,(void*)0,&g_80,&g_80,&g_80,(void*)0},{&g_80,&g_80,&g_80,(void*)0,&g_80,&g_80,(void*)0,(void*)0,(void*)0}}};
+static int32_t g_91 = 3L;
+static int32_t g_102 = (-1L);
