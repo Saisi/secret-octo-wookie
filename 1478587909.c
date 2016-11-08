f2e459862b92779396da5ff680891e61cee3c654
saisi <saisi@users.noreply.github.com>
2016-11-08 06:51:51 +0000

    1478587909 astrogeology

---
+++ 1478587909.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1949082277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int32_t  f1;
+   volatile signed f2 : 23;
+   uint32_t  f3;
+};
+
+union U1 {
+   uint32_t  f0;
+   volatile int16_t  f1;
