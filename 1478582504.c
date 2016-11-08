4157d515633887b67c214d5f390a4b113119891f
saisi <saisi@users.noreply.github.com>
2016-11-08 05:21:45 +0000

    1478582504 antiquaries

---
+++ 1478582504.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      627085896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 18;
+   const uint32_t  f1;
+};
+
+union U1 {
+   uint64_t  f0;
+   const volatile uint64_t  f1;
+   signed f2 : 2;
+};
