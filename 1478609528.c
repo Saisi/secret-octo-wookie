a60c2db2b8c0f1e5b8794dc60a757be52d814b0e
saisi <saisi@users.noreply.github.com>
2016-11-08 12:52:08 +0000

    1478609528 algal

---
+++ 1478609528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3909704309
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile uint64_t  f1;
+   int32_t  f2;
+   uint64_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
