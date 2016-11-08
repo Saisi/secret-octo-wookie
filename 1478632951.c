6bd20cf8a8b059a288ac51f5b11d57d16e37e8cf
saisi <saisi@users.noreply.github.com>
2016-11-08 19:22:32 +0000

    1478632951 Aulea

---
+++ 1478632951.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2293805096
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCFF0EA75L;
+static int32_t g_5 = 0L;
+static int32_t g_14 = 0xC63C4078L;
+static int32_t * volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
+static volatile int32_t g_62 = (-4L);/* VOLATILE GLOBAL g_62 */
+static volatile int32_t g_63 = (-1L);/* VOLATILE GLOBAL g_63 */
+static int32_t g_64 = 0L;
+static int32_t *g_92 = &g_64;
+static uint16_t g_93 = 0xE7AFL;
