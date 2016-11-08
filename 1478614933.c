5ebdecc7f1cf86427d5c94f7b09fa63871f8edf2
saisi <saisi@users.noreply.github.com>
2016-11-08 14:22:14 +0000

    1478614933 athyrid

---
+++ 1478614933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1148523023
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint8_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+};
+#pragma pack(pop)
+
+struct S1 {
