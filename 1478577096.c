2f79786cf77a8f25f97993541bf7e778048be9fb
saisi <saisi@users.noreply.github.com>
2016-11-08 03:51:37 +0000

    1478577096 allium

---
+++ 1478577096.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      633912607
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_13 = 0xEE1D11023DCDF5EFLL;
+static const int64_t g_27 = 0L;
+static int32_t g_65 = (-1L);
+static int32_t *g_64 = &g_65;
+static uint8_t g_68 = 0xB9L;
