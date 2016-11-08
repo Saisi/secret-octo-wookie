340b0347fe164234adfabe71ceb6a9b609a4b436
saisi <saisi@users.noreply.github.com>
2016-11-08 03:21:35 +0000

    1478575295 abacterial

---
+++ 1478575295.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2055819792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static int64_t g_11 = (-1L);
+static int32_t g_17[10] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint64_t g_50 = 0xAFEC1E183A099773LL;
+static int64_t g_57 = 0L;
+static int64_t g_59 = 0x833AED5EBAE88B14LL;
+static int16_t g_88 = 0L;
+static uint32_t g_99 = 0xF543A9B1L;
+static uint16_t g_100 = 0xAEAAL;
