db0a362fd66a78d1f2094dd2b693ab1b751bdb75
saisi <saisi@users.noreply.github.com>
2016-11-08 21:52:38 +0000

    1478641958 apportioner

---
+++ 1478641958.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2827709229
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5][2] = {{0x3D4210E1L,(-1L)},{(-1L),0x3D4210E1L},{(-1L),(-1L)},{0x3D4210E1L,(-1L)},{(-1L),0x3D4210E1L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
