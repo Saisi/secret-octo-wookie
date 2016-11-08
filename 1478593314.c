a9f744ca9388397246e412128a599578984b295f
saisi <saisi@users.noreply.github.com>
2016-11-08 08:21:55 +0000

    1478593314 apocalyptism

---
+++ 1478593314.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2984269952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_8 = 1L;
+static int8_t g_11 = 9L;
+static uint16_t g_49[6] = {0UL,0UL,0UL,0UL,0UL,0UL};
+static int32_t g_51[10] = {0L,(-1L),(-2L),(-1L),0L,0L,(-1L),(-2L),(-1L),0L};
+static int32_t g_77 = 0xC684CA67L;
+static int32_t *g_76 = &g_77;
+static int8_t g_79 = 6L;
+static uint32_t g_96 = 1UL;
+static int8_t g_134 = 0xBAL;
