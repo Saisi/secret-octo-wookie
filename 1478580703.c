86d12f420adbb1ecf074925c91b0cc16af5a155b
saisi <saisi@users.noreply.github.com>
2016-11-08 04:51:43 +0000

    1478580703 abaculus

---
+++ 1478580703.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1621319351
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+union U1 {
+   unsigned f0 : 3;
+};
+
+union U2 {
+   const int8_t  f0;
