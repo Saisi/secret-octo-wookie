a275dbccd4837ae0c9cfc40062f9a3176ba8c16d
saisi <saisi@users.noreply.github.com>
2016-11-09 00:22:46 +0000

    1478650965 antevocalic

---
+++ 1478650965.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2911305280
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+   uint64_t  f1;
+   int32_t  f2;
+   uint8_t  f3;
+   volatile uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 1L;
