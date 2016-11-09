b897f9ae97b83e05f4a66fa56a08db9c02aa617c
saisi <saisi@users.noreply.github.com>
2016-11-09 02:52:55 +0000

    1478659974 agricole

---
+++ 1478659974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3745193827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 16;
+   int32_t  f1;
+   uint16_t  f2;
+   const int8_t  f3;
+   const int32_t  f4;
+   const int32_t  f5;
+   volatile uint8_t  f6;
