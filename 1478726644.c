8f3d91c8ccc0469fecfd5ea06eb517c3ef9e9ec9
saisi <saisi@users.noreply.github.com>
2016-11-09 21:24:05 +0000

    1478726644 airscape

---
+++ 1478726644.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2200214211
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 11;
+   uint32_t  f1;
+   const uint16_t  f2;
+};
+#pragma pack(pop)
+
+union U1 {
