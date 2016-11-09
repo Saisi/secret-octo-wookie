80f4959d2388012302bb2c8ee32554573e0d3626
saisi <saisi@users.noreply.github.com>
2016-11-09 15:53:41 +0000

    1478706820 baluchitherium

---
+++ 1478706820.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2807539232
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 30;
+   const volatile int8_t  f1;
+   unsigned f2 : 19;
+   unsigned f3 : 9;
+   unsigned f4 : 10;
+   unsigned f5 : 14;
+   unsigned f6 : 23;
+   volatile signed f7 : 20;
+   unsigned f8 : 21;
