f41c49fcfc43c4195a6765cb2deeb335fe5eb507
saisi <saisi@users.noreply.github.com>
2016-11-09 18:53:53 +0000

    1478717633 albedos

---
+++ 1478717633.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      145991155
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x6E6BACD7L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_22 = 0xC92EA21EL;
+static uint64_t g_24 = 0UL;
+static uint64_t g_26 = 1UL;
+static int16_t g_72 = 0x3403L;
