9b20ba49c1df47d886928d0465cbe3d774a618b7
saisi <saisi@users.noreply.github.com>
2016-11-09 16:53:44 +0000

    1478710424 amphanthia

---
+++ 1478710424.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2177498347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2][2] = {{0x60417BE1L,0x60417BE1L},{0x60417BE1L,0x60417BE1L}};
+static int32_t g_4 = 0x4DFBD6C3L;
+static volatile int32_t g_7 = 0x81A7ECC2L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8[3] = {(-2L),(-2L),(-2L)};
+static int8_t g_58 = 0x16L;
