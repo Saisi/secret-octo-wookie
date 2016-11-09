f4ef3efeee7c98f85cecb6250b29e24f4a626733
saisi <saisi@users.noreply.github.com>
2016-11-09 15:23:38 +0000

    1478705018 agroof

---
+++ 1478705018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4177685851
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 12;
+   unsigned f1 : 19;
+   volatile signed f2 : 10;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
