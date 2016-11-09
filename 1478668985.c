9aa531f6d0f6534bfcdbb8156c5f92d064bb0f28
saisi <saisi@users.noreply.github.com>
2016-11-09 05:23:05 +0000

    1478668985 anilopyrine

---
+++ 1478668985.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3378411552
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   uint32_t  f1;
+   const volatile int8_t  f2;
+   volatile uint8_t  f3;
+   volatile signed f4 : 10;
+};
+
+struct S1 {
+   signed f0 : 16;
