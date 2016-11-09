93e61ce2b54b2f3713d3e1bb6ade0c356b8c6340
saisi <saisi@users.noreply.github.com>
2016-11-09 17:53:49 +0000

    1478714029 amphibolite

---
+++ 1478714029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      569445692
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+   int16_t  f1;
+   signed f2 : 9;
+   volatile signed f3 : 7;
+   unsigned f4 : 16;
+   volatile unsigned f5 : 28;
+   const volatile signed f6 : 13;
+};
+
