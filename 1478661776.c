bee407468907e48867a25335d083c562deef3a7f
saisi <saisi@users.noreply.github.com>
2016-11-09 03:22:57 +0000

    1478661776 baboonery

---
+++ 1478661776.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2867648606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   volatile uint16_t  f1;
+   int32_t  f2;
+   int16_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   unsigned f1 : 26;
