248bd6db684a697fc1007579c20f058da153002d
saisi <saisi@users.noreply.github.com>
2016-11-09 10:53:21 +0000

    1478688801 AOPA

---
+++ 1478688801.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4134445989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   unsigned f1 : 4;
+   int32_t  f2;
+   int32_t  f3;
+};
+#pragma pack(pop)
+
