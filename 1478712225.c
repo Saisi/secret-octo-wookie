3ceaa0876721de73c70c057ae565682ac041c17a
saisi <saisi@users.noreply.github.com>
2016-11-09 17:23:48 +0000

    1478712225 ambers

---
+++ 1478712225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1084355072
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile int16_t  f1;
+   const volatile signed f2 : 6;
+   volatile int16_t  f3;
+   volatile uint64_t  f4;
+};
+
+struct S1 {
+   signed f0 : 27;
