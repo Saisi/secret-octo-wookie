9d87b0fdb203a01383da660cfff437f8afd631e7
saisi <saisi@users.noreply.github.com>
2016-11-09 04:53:04 +0000

    1478667183 adorsed

---
+++ 1478667183.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      863109454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_7 = 0xD5816210B999FB02LL;
+static const int32_t g_10 = 0L;
+static int32_t g_12 = 0L;
+static int32_t g_45 = 0L;
+static int32_t *g_52 = &g_45;
+static int32_t *g_53[7][1][6] = {{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}},{{&g_12,(void*)0,&g_12,&g_12,(void*)0,&g_12}}};
+static int8_t g_75 = 0xEBL;
+static int64_t g_76 = (-1L);
+static volatile int8_t g_77 = 0xE2L;/* VOLATILE GLOBAL g_77 */
