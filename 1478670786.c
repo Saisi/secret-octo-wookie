768d36fb248cdc90b38f9779cc620d3ce94fc286
saisi <saisi@users.noreply.github.com>
2016-11-09 05:53:07 +0000

    1478670786 autophone

---
+++ 1478670786.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2151542386
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x540D7F82L;
+static uint32_t g_45 = 0x2A6F9A4DL;
+static uint16_t g_47 = 0xE85FL;
+static int32_t g_49 = 0xC254E7F1L;
+static int8_t g_53[7] = {0x65L,0x65L,0x65L,0x65L,0x65L,0x65L,0x65L};
+static int8_t g_56 = 0x7FL;
+static uint8_t g_59 = 0xEFL;
+static uint8_t g_70 = 0UL;
+static uint32_t g_76 = 0x90CB933DL;
