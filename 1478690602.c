242177cefc8081d3f8ec2853bf41c08cee2944e9
saisi <saisi@users.noreply.github.com>
2016-11-09 11:23:22 +0000

    1478690602 aleurometer

---
+++ 1478690602.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2385705328
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   int8_t  f1;
+   volatile int32_t  f2;
+   volatile uint64_t  f3;
+   const uint32_t  f4;
+};
+
+union U1 {
+   volatile signed f0 : 22;
