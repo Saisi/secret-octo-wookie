75a6cef18c4e3e6a1c4c4d4d716891122331f449
saisi <saisi@users.noreply.github.com>
2016-11-09 19:23:57 +0000

    1478719434 anacoluttha

---
+++ 1478719434.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2598018459
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   signed f1 : 19;
+   volatile signed f2 : 10;
+   unsigned f3 : 13;
+   unsigned f4 : 10;
+};
+
+#pragma pack(push)
+#pragma pack(1)
