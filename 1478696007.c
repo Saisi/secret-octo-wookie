3a2a02e207541bde80b629d8e506e3e7621cbe23
saisi <saisi@users.noreply.github.com>
2016-11-09 12:53:28 +0000

    1478696007 acanthuses

---
+++ 1478696007.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3544947863
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int16_t  f1;
+   uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_22[4] = {0x989B1AB4L,0x989B1AB4L,0x989B1AB4L,0x989B1AB4L};
+static int32_t g_33 = 0L;
+static int32_t *g_32 = &g_33;
