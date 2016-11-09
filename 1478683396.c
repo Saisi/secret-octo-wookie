8a058888b5d5074e7b509dbf6579df97ba234442
saisi <saisi@users.noreply.github.com>
2016-11-09 09:23:17 +0000

    1478683396 autotriploidy

---
+++ 1478683396.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4007695907
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint64_t  f1;
+   int32_t  f2;
+   volatile uint32_t  f3;
+   signed f4 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5[2] = {65530UL,65530UL};
