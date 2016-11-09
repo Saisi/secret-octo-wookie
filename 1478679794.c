9198ce6b9b2bb23aaec2d6591834ba1d6f037441
saisi <saisi@users.noreply.github.com>
2016-11-09 08:23:14 +0000

    1478679794 baaskaaps

---
+++ 1478679794.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2857859153
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int8_t  f0;
+   const uint8_t  f1;
+   uint8_t  f2;
+   uint8_t  f3;
+};
+#pragma pack(pop)
+
