b17bc56c79740b77048ba7140c0e7027c2e2362d
saisi <saisi@users.noreply.github.com>
2016-11-09 20:54:01 +0000

    1478724841 bacteroides

---
+++ 1478724841.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2560597245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 29;
+   unsigned : 0;
+   const unsigned f1 : 3;
+   uint64_t  f2;
+   const signed f3 : 27;
+   unsigned f4 : 29;
+   signed f5 : 12;
