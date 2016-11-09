be82dd3d9aa036487c9f2d917046705f4027dded
saisi <saisi@users.noreply.github.com>
2016-11-09 06:23:08 +0000

    1478672588 atrociousnesses

---
+++ 1478672588.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      245331974
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x7E2799ACL;
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x19AC196AL;
+static volatile int32_t g_8[5] = {0x38C08872L,0x38C08872L,0x38C08872L,0x38C08872L,0x38C08872L};
+static volatile int32_t g_9 = 0x4D0289A6L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x68819570L;
+static uint8_t g_25 = 6UL;
+static int16_t g_44 = (-10L);
+static uint16_t g_47 = 8UL;
