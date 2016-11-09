7e912665ff89e96171a092244696562fc6a3a024
saisi <saisi@users.noreply.github.com>
2016-11-09 21:54:06 +0000

    1478728446 Athiste

---
+++ 1478728446.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      889398805
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const volatile int32_t  f1;
+   const int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 5L;
+static int16_t g_7 = 2L;
+static int16_t g_61 = 0x11C2L;
