c705c080c112ecba2831e3816ab8504b3006a22c
saisi <saisi@users.noreply.github.com>
2016-11-10 18:25:25 +0000

    1478802324 alkylamino

---
+++ 1478802324.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      418748860
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[3] = {0xFB0B0297L,0xFB0B0297L,0xFB0B0297L};
+static const int32_t *g_2 = &g_3[0];
+static const int32_t *g_6 = &g_3[0];
+static const int32_t ** volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
