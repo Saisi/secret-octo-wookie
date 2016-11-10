67365a73ceb9bdd7805e53b6c8c28eba40eef894
saisi <saisi@users.noreply.github.com>
2016-11-10 03:24:28 +0000

    1478748268 bagmaker

---
+++ 1478748268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1953164113
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   const int8_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
+   uint64_t  f4;
+   volatile signed f5 : 21;
+};
