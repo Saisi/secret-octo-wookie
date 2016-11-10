a297817aba30ade8c71bfc8fc973bd2d01d31fc0
saisi <saisi@users.noreply.github.com>
2016-11-10 08:24:45 +0000

    1478766285 antistatism

---
+++ 1478766285.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      948063931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+   volatile uint64_t  f3;
+   uint64_t  f4;
+};
+
+union U1 {
+   volatile int16_t  f0;
