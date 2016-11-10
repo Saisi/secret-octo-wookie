4497d3ebee5680317f966b8096175b5433db58fe
saisi <saisi@users.noreply.github.com>
2016-11-10 02:54:27 +0000

    1478746466 Astur

---
+++ 1478746466.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3070794600
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+   uint16_t  f3;
+   const uint32_t  f4;
+   uint32_t  f5;
+   uint32_t  f6;
+};
+
