b0941e63b07374212edb58da146599bfbfe2b386
saisi <saisi@users.noreply.github.com>
2016-11-10 17:25:22 +0000

    1478798722 Albuca

---
+++ 1478798722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3987285254
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   int64_t  f1;
+   volatile uint16_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = (-1L);
+static int64_t g_11 = (-1L);
