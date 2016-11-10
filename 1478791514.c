114a539c613d19ac6e297e1e3a2418a45d88244e
saisi <saisi@users.noreply.github.com>
2016-11-10 15:25:15 +0000

    1478791514 anthralin

---
+++ 1478791514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1736702262
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   const int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2] = {0x1F2C29A3L,0x1F2C29A3L};
+static int32_t g_3 = 0x75C19C5AL;
+static int32_t g_77 = (-9L);
+static int8_t g_79 = 0x7FL;
