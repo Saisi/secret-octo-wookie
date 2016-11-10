41e0bcceb342270c9886435bbcff733a1257d76a
saisi <saisi@users.noreply.github.com>
2016-11-10 09:24:47 +0000

    1478769887 anticompetitive

---
+++ 1478769887.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1130287951
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   int32_t  f1;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 6L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-2L);
+static volatile union U0 g_18[7][3] = {{{0UL},{0x8D92426F37911B9CLL},{0x8D92426F37911B9CLL}},{{0x55A63A74ADF29855LL},{0x8D92426F37911B9CLL},{18446744073709551615UL}},{{0xE60E7790660D64C9LL},{0UL},{0xF219A6FDD8FB8CEFLL}},{{0x55A63A74ADF29855LL},{0x55A63A74ADF29855LL},{0xF219A6FDD8FB8CEFLL}},{{0UL},{0xE60E7790660D64C9LL},{18446744073709551615UL}},{{0x8D92426F37911B9CLL},{0x55A63A74ADF29855LL},{0x8D92426F37911B9CLL}},{{0x8D92426F37911B9CLL},{0UL},{0x55A63A74ADF29855LL}}};
