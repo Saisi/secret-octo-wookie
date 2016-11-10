0d216edcf4d5e58ff56237958437839c3e921a6d
saisi <saisi@users.noreply.github.com>
2016-11-10 22:25:43 +0000

    1478816742 Alicante

---
+++ 1478816742.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3273166890
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   unsigned : 0;
+   unsigned f1 : 20;
+   volatile uint16_t  f2;
+};
+#pragma pack(pop)
+
