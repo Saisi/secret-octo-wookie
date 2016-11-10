b89c8ceb48076d137e37944e3547f4e037b23755
saisi <saisi@users.noreply.github.com>
2016-11-10 13:55:05 +0000

    1478786105 achromatise

---
+++ 1478786105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3419831723
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x27689351L;
+static int32_t g_5 = 0x60417BE1L;
+static int16_t g_12 = 1L;
+static uint32_t g_16 = 0x7CD95DA2L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
