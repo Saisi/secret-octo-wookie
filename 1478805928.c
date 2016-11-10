9a21c50f7d6c373b484325f848127e28d1b5a1fd
saisi <saisi@users.noreply.github.com>
2016-11-10 19:25:29 +0000

    1478805928 alveoliform

---
+++ 1478805928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2064365692
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+};
+
+union U1 {
+   int32_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+};
+
