62ffdd1c08603f0f8ad28b976f817199f568bc5c
saisi <saisi@users.noreply.github.com>
2016-11-10 01:24:18 +0000

    1478741058 amalgamate

---
+++ 1478741058.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3055806058
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 12;
+   unsigned f2 : 30;
+   const unsigned f3 : 18;
+   volatile unsigned f4 : 25;
+   signed f5 : 12;
+   volatile signed f6 : 26;
