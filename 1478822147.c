02b2407914d6e92f7d6554c0543a4583e7750dc8
saisi <saisi@users.noreply.github.com>
2016-11-10 23:55:47 +0000

    1478822147 autoelectrolysis

---
+++ 1478822147.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2619887182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[10] = {1L,1L,1L,1L,1L,1L,1L,1L,1L,1L};
+static int32_t g_3[4][4] = {{0x8E56F9B2L,0x15DE05DAL,0x0B999FB0L,0x18D58162L},{0x5A60D005L,0x15DE05DAL,0x15DE05DAL,0x5A60D005L},{0x15DE05DAL,0x5A60D005L,0x8E56F9B2L,(-1L)},{0x15DE05DAL,0x8E56F9B2L,0x15DE05DAL,0x0B999FB0L}};
+static int32_t g_46 = 1L;
+static int8_t g_69 = (-8L);
+static int32_t *g_70 = &g_46;
+static uint32_t g_81 = 0xBB5F37D7L;
+static uint32_t g_99 = 0x3A415DBEL;
+static int32_t * volatile g_119[7] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t g_121 = 0x20CFA827L;
