7e0fbf392bd1b5f58381c75ad294d681631864a1
saisi <saisi@users.noreply.github.com>
2016-11-10 07:24:42 +0000

    1478762681 autodrome

---
+++ 1478762681.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2216955695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const int64_t  f1;
+   uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x4C506D44L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x2BD7B1C2L;
+static union U0 g_44 = {248UL};
