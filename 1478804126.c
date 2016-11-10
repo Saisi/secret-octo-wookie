9761842cc2dfc8bc0a700b4c878aac159e7dc7d3
saisi <saisi@users.noreply.github.com>
2016-11-10 18:55:26 +0000

    1478804126 anguishing

---
+++ 1478804126.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2486109402
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 27;
+   volatile int32_t  f1;
+   uint32_t  f2;
+   unsigned f3 : 11;
+   signed f4 : 18;
+   signed f5 : 12;
+   unsigned f6 : 1;
+   signed f7 : 20;
+};
