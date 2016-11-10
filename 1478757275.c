9a1307ae17d4b2b6cd0425d970af589f1866f3d8
saisi <saisi@users.noreply.github.com>
2016-11-10 05:54:36 +0000

    1478757275 antitoxic

---
+++ 1478757275.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2977630727
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 7;
+   int64_t  f2;
+   volatile uint32_t  f3;
+   int16_t  f4;
+   uint32_t  f5;
+   uint32_t  f6;
