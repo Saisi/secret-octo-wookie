cbe64e95c32632617a4f462643f869c42fd36654
saisi <saisi@users.noreply.github.com>
2016-11-10 11:54:56 +0000

    1478778895 archiblastic

---
+++ 1478778895.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      75935872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x85299DADL;
+static uint8_t g_44 = 1UL;
