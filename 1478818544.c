45553f3c707f8e903edec10d51513d75ecfef6a6
saisi <saisi@users.noreply.github.com>
2016-11-10 22:55:44 +0000

    1478818544 amphichromatic

---
+++ 1478818544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1506209725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile signed f0 : 11;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0L;
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
