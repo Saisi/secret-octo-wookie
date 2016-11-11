ecddcd5389971fd9caf9e531f69a26096ef36c55
saisi <saisi@users.noreply.github.com>
2016-11-11 17:26:48 +0000

    1478885208 antisteapsin

---
+++ 1478885208.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3577029208
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint32_t  f1;
+   volatile uint32_t  f2;
+   const volatile int32_t  f3;
+   int8_t  f4;
+   uint16_t  f5;
+   volatile uint64_t  f6;
+};
+
