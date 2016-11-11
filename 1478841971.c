2976e8176e1706389dedbd5a5a2a45824b7a6907
saisi <saisi@users.noreply.github.com>
2016-11-11 05:26:11 +0000

    1478841971 alypine

---
+++ 1478841971.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      499224059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_13 = 0x8A94B2AB76D6092FLL;
+static uint16_t g_15 = 0xCB24L;
+static uint16_t g_25 = 6UL;
+static uint16_t *g_24 = &g_25;
+static uint16_t *g_50 = (void*)0;
+static uint16_t g_51[6] = {0x2908L,0x2908L,0x2908L,0x2908L,0x2908L,0x2908L};
+static uint32_t g_68 = 0UL;
+static uint32_t g_70 = 0x67D7132CL;
+static uint16_t g_75 = 0UL;
