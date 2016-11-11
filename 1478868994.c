b79c1947e31f3d0d8d080034b4e41627a64f6e3b
saisi <saisi@users.noreply.github.com>
2016-11-11 12:56:34 +0000

    1478868994 ascidiozooid

---
+++ 1478868994.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1831086931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 8;
+   volatile unsigned f1 : 31;
+   volatile unsigned f2 : 6;
+   unsigned f3 : 25;
+   signed f4 : 6;
+   volatile signed f5 : 7;
+   signed f6 : 10;
