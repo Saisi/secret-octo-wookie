6a984452b4a1b17d617ef8fe7f2ae239e74a7f07
saisi <saisi@users.noreply.github.com>
2016-11-11 13:56:37 +0000

    1478872596 amniotic

---
+++ 1478872596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2197806433
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x53D06CE1L;
+static int32_t g_5[10] = {0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L,0x137B1891L};
+static volatile union U0 g_38[1][7] = {{{0x1BB000E2L},{0x1BB000E2L},{0x1BB000E2L},{0x1BB000E2L},{0x1BB000E2L},{0x1BB000E2L},{0x1BB000E2L}}};
+static int32_t g_44[7] = {0xCEA2FF3EL,(-1L),(-1L),0xCEA2FF3EL,(-1L),(-1L),0xCEA2FF3EL};
+static uint32_t g_54[2][3][1] = {{{5UL},{4294967287UL},{4294967287UL}},{{5UL},{4294967287UL},{4294967287UL}}};
