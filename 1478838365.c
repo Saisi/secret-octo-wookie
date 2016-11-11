64446624655bb6387b40e13ea59b351bc85d6f8d
saisi <saisi@users.noreply.github.com>
2016-11-11 04:26:05 +0000

    1478838365 attunely

---
+++ 1478838365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1321933381
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 1;
+   signed f1 : 20;
+};
+
+struct S1 {
+   const int32_t  f0;
+};
+
+union U2 {
