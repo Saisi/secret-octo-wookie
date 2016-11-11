ddd604e864354abb684f5feeab82eac8eaa5e3f1
saisi <saisi@users.noreply.github.com>
2016-11-11 23:57:14 +0000

    1478908632 axiology

---
+++ 1478908632.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      404061531
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint64_t  f1;
+   volatile uint8_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   uint32_t  f0;
+   const volatile uint16_t  f1;
