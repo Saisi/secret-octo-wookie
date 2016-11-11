99ca90053a50df513cc4ffef8e9c8a6e8559bcea
saisi <saisi@users.noreply.github.com>
2016-11-11 21:57:06 +0000

    1478901426 avicularium

---
+++ 1478901426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      312655129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 11;
+   const unsigned f1 : 22;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const volatile signed f0 : 2;
+};
