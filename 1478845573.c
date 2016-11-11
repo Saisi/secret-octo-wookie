e9d72ed068cb9957e412da5ae06bdd1eb360cfd7
saisi <saisi@users.noreply.github.com>
2016-11-11 06:26:16 +0000

    1478845573 aswim

---
+++ 1478845573.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1320836216
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   const volatile uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_8[2][4][1] = {{{8UL},{246UL},{8UL},{246UL}},{{8UL},{246UL},{8UL},{246UL}}};
+static const int8_t g_9 = 0x3CL;
+static uint32_t g_11 = 0x95A97963L;
+static uint32_t g_65[5] = {0xF285F402L,0xF285F402L,0xF285F402L,0xF285F402L,0xF285F402L};
