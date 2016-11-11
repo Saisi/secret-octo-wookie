25e1276c17ce9659addede9be15b716e98f96289
saisi <saisi@users.noreply.github.com>
2016-11-11 05:56:12 +0000

    1478843772 acetylaminobenzene

---
+++ 1478843772.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2905397704
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC96DD1E1L;
+static volatile uint16_t g_6 = 0xC3EEL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_18 = 0x96B779C6L;
+static int32_t g_54 = (-8L);
+static int32_t *g_53 = &g_54;
+static int32_t g_66 = 1L;
+static int32_t *g_65[8] = {&g_66,&g_66,&g_66,&g_66,&g_66,&g_66,&g_66,&g_66};
+static int64_t g_67 = 0x2B132DC1B2D863E3LL;
+static uint64_t g_97 = 0xAD2945289E610DA3LL;
