b58b97358d3757bd51b20127925525f5989cc8a9
saisi <saisi@users.noreply.github.com>
2016-11-11 18:56:54 +0000

    1478890613 babyhood

---
+++ 1478890613.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      952653406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   volatile unsigned f1 : 30;
+   unsigned f2 : 8;
+   unsigned f3 : 20;
+   const signed f4 : 19;
+   signed f5 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
