e8c3bd8bcd3e03c862a643648f3e752ae1490bb6
saisi <saisi@users.noreply.github.com>
2016-11-11 21:27:04 +0000

    1478899623 arenicolite

---
+++ 1478899623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3649654417
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   unsigned f1 : 2;
+   signed f2 : 29;
+   signed f3 : 20;
+   volatile uint64_t  f4;
+   unsigned f5 : 7;
+   uint32_t  f6;
+   signed f7 : 26;
+   const signed f8 : 27;
