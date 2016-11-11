3e3d0f31f7e041b022e3675a085b9f890f055d87
saisi <saisi@users.noreply.github.com>
2016-11-11 16:26:46 +0000

    1478881605 bacterially

---
+++ 1478881605.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2773510771
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+struct S1 {
+   const signed f0 : 31;
+   signed f1 : 5;
+   signed f2 : 3;
+   const unsigned f3 : 26;
+};
