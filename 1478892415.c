f4d84be1c64b61d7cf64338fde6a1c61e00e80f4
saisi <saisi@users.noreply.github.com>
2016-11-11 19:26:56 +0000

    1478892415 accusatrix

---
+++ 1478892415.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1082853263
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-4L);
+static uint32_t g_14 = 18446744073709551615UL;
+static int32_t g_24[5] = {0L,0L,0L,0L,0L};
+static uint32_t g_41 = 0xB4743079L;
+static uint8_t g_47 = 0UL;
