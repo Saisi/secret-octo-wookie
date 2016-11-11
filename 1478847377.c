8ab906e7845ded78ead3a770d1adc3f7ec98dca5
saisi <saisi@users.noreply.github.com>
2016-11-11 06:56:18 +0000

    1478847377 aquagreen

---
+++ 1478847377.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4270735416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 10;
+   unsigned f1 : 23;
+   const volatile signed f2 : 29;
+   unsigned f3 : 21;
+   volatile signed f4 : 16;
+   volatile signed f5 : 7;
+   signed f6 : 9;
+   const volatile unsigned f7 : 26;
+   signed f8 : 22;
