5b4ab5f25185a21d20f4d2083085fe5980497f11
saisi <saisi@users.noreply.github.com>
2016-11-11 22:27:08 +0000

    1478903228 antlia

---
+++ 1478903228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      436138067
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_7 = 4294967287UL;/* VOLATILE GLOBAL g_7 */
+static uint32_t g_16 = 4294967290UL;
+static volatile uint32_t g_23 = 0x6AA1C33CL;/* VOLATILE GLOBAL g_23 */
+static volatile uint32_t *g_22 = &g_23;
+static int64_t g_26[4][10][1] = {{{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL}},{{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)}},{{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL}},{{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)},{1L},{0x933BD3E018EBDB88LL},{1L},{(-1L)}}};
+static uint8_t g_30 = 1UL;
+static int32_t g_33[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_47 = 4L;
+static int32_t * volatile g_79 = (void*)0;/* VOLATILE GLOBAL g_79 */
