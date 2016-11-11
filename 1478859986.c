579cf2afc8b5bbe1a71a425cbc6a41a942925eb4
saisi <saisi@users.noreply.github.com>
2016-11-11 10:26:27 +0000

    1478859986 agnathostomatous

---
+++ 1478859986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      811281369
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+   signed f1 : 22;
+   volatile signed f2 : 1;
+   int8_t * const  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2[10][1][5] = {{{0xA0L,0x3DL,(-1L),0xE4L,0L}},{{1L,0xE4L,0x72L,0xB7L,0x85L}},{{0xB7L,0x3DL,(-1L),(-1L),0x3DL}},{{0L,0x72L,7L,(-1L),0xE3L}},{{0xC2L,0xE3L,1L,0xB7L,0xE4L}},{{0x74L,(-1L),0x85L,0xE4L,0xA0L}},{{0xC2L,0x74L,0xA0L,0x74L,0xC2L}},{{0L,(-5L),0xA0L,0x85L,0x72L}},{{0xB7L,0x9EL,0x85L,7L,1L}},{{1L,0x74L,(-5L),(-1L),1L}}};
+static uint16_t g_3[10] = {0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L,0xDC34L};
