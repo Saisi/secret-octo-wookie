2be06f0f12e37453564d80cf07283441cde424a0
saisi <saisi@users.noreply.github.com>
2016-11-12 12:27:51 +0000

    1478953670 Auvil

---
+++ 1478953670.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      120486453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   int8_t  f1;
+   const int8_t  f2;
+   int32_t  f3;
+   volatile int32_t  f4;
+   const int16_t  f5;
+   uint64_t  f6;
+   uint32_t  f7;
+   int32_t  f8;
