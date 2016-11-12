d28386a29d5c7dbb1a7679d49a690bf46a2c9acf
saisi <saisi@users.noreply.github.com>
2016-11-12 23:58:30 +0000

    1478995110 bankrupture

---
+++ 1478995110.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1960846477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 0xF1L;
+static int32_t g_6 = 0x30836B16L;
+static int32_t * volatile g_5[5] = {&g_6,&g_6,&g_6,&g_6,&g_6};
+static int32_t * volatile g_7 = &g_6;/* VOLATILE GLOBAL g_7 */
+static union U3 g_8 = {0UL};
