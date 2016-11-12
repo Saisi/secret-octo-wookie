172487da2d70e98240b36f8cc7409eadff7de978
saisi <saisi@users.noreply.github.com>
2016-11-12 01:57:20 +0000

    1478915840 Asternata

---
+++ 1478915840.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2607121856
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   int8_t * const  f1;
+   const int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = 0x921DL;
+static uint32_t g_10 = 1UL;
+static int8_t g_13 = 0xE4L;
