2a633ad641b3c9627da75bc0e4f834c4a6b2f9a6
saisi <saisi@users.noreply.github.com>
2016-11-12 01:27:19 +0000

    1478914038 Androuet

---
+++ 1478914038.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4200932482
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const int64_t  f1;
+   volatile uint32_t  f2;
+   uint32_t  f3;
+   signed f4 : 6;
+   int64_t  f5;
+   uint32_t  f6;
+   int8_t  f7;
+};
