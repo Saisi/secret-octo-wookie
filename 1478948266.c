2bf6e4322eff3a88d18d58fc3c3e4fc834cfb219
saisi <saisi@users.noreply.github.com>
2016-11-12 10:57:46 +0000

    1478948266 aeolotropism

---
+++ 1478948266.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      44472710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 17;
+   unsigned f1 : 2;
+};
+
+struct S1 {
+   uint16_t  f0;
+   const uint32_t  f1;
+   uint16_t  f2;
+   volatile uint8_t  f3;
