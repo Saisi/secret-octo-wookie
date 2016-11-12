6bb9e488facf3bfbf46e41d06f3140f45d0b4b31
saisi <saisi@users.noreply.github.com>
2016-11-12 02:27:21 +0000

    1478917641 Bach

---
+++ 1478917641.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1012278567
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x000D4E79L;
+static int16_t g_23 = 0x16FEL;
+static volatile int64_t g_24 = 0xEDE048FFD5269945LL;/* VOLATILE GLOBAL g_24 */
+static volatile uint32_t g_26 = 0xA5798870L;/* VOLATILE GLOBAL g_26 */
+static uint8_t g_36[2][2] = {{1UL,1UL},{1UL,1UL}};
+static uint8_t g_45 = 1UL;
+static uint8_t *g_44[4] = {&g_45,&g_45,&g_45,&g_45};
+static uint64_t g_73 = 0xC53E4AB27A36CEB1LL;
+static int32_t g_75 = 2L;
