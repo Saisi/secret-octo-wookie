72811828e507ba9953f8360ab8a22f394132c1fe
saisi <saisi@users.noreply.github.com>
2016-11-12 21:28:23 +0000

    1478986103 Abinoem

---
+++ 1478986103.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      835981020
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 28;
+   int8_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x2971C1ECL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[7][2] = {{0x22124F84L,0x80C170CDL},{0x768C0AA3L,0x22124F84L},{(-1L),(-1L)},{(-1L),0x22124F84L},{0x768C0AA3L,0x80C170CDL},{0x22124F84L,0x80C170CDL},{0x768C0AA3L,0x22124F84L}};
+static uint16_t g_25 = 0x16B5L;
