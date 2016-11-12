71c5b417d5602d80a476e1bcfd127a44c6e5e994
saisi <saisi@users.noreply.github.com>
2016-11-12 05:57:32 +0000

    1478930252 backset

---
+++ 1478930252.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      443417409
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 31;
+   unsigned f1 : 26;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const volatile unsigned f0 : 22;
+};
