552222a900c4b633a8b12401c764dd2c6086368c
saisi <saisi@users.noreply.github.com>
2016-11-12 17:58:11 +0000

    1478973490 asinine

---
+++ 1478973490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1386029086
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_4 = 0x9EA9L;
+static int32_t g_11 = 0xC7747BB7L;
+static int32_t *g_32 = (void*)0;
+static int32_t g_59 = 0L;
+static uint16_t g_75 = 65535UL;
+static uint32_t g_85 = 0xC5EACAA0L;
+static int8_t g_111 = 0xF2L;
+static int16_t g_114 = 0L;
+static uint8_t g_116 = 255UL;
