bde441305cfe8669473708ef28fd61d580e7f96b
saisi <saisi@users.noreply.github.com>
2016-11-12 00:57:17 +0000

    1478912237 backsetting

---
+++ 1478912237.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1061294318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   unsigned f1 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static uint64_t g_15 = 0xB77278FA8F90A3CFLL;
+static int32_t g_16 = 0x486B6FEBL;
+static int32_t g_32 = (-1L);
