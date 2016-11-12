3e7b19917c062a4c58a3ec46b33f5ff898f96312
saisi <saisi@users.noreply.github.com>
2016-11-12 06:57:35 +0000

    1478933855 analgesics

---
+++ 1478933855.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2767446504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0xBEF7D651L;/* VOLATILE GLOBAL g_2 */
+static uint8_t g_4 = 0x2AL;
+static int32_t g_7 = 3L;
+static volatile uint64_t g_8 = 0UL;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
