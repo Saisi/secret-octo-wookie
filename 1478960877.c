81b29063027598972bafbb3965b414c335d2747a
saisi <saisi@users.noreply.github.com>
2016-11-12 14:27:57 +0000

    1478960877 Anchinoe

---
+++ 1478960877.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      982827215
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   int8_t * const  f0;
+   int16_t  f1;
+   volatile unsigned f2 : 6;
+   const uint16_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[4] = {65535UL,65535UL,65535UL,65535UL};
