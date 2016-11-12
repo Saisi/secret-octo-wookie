15e48bdce9359039f87e8c0633b2a7eb51da9a6c
saisi <saisi@users.noreply.github.com>
2016-11-12 22:28:26 +0000

    1478989706 adenocarcinomata

---
+++ 1478989706.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1491556347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   const volatile int8_t  f1;
+   int8_t * f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   const int16_t  f0;
+   int8_t * const  volatile  f1;
