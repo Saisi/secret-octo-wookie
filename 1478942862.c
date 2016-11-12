cf23ed7b006f11444a58cfb88264d1237432eee8
saisi <saisi@users.noreply.github.com>
2016-11-12 09:27:42 +0000

    1478942862 analemma

---
+++ 1478942862.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1419395503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xF09CC25FL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xEF9D7865L;
+static volatile int32_t * volatile g_6[5] = {&g_2,&g_2,&g_2,&g_2,&g_2};
+static int32_t g_10[4] = {0x8AB5457DL,0x8AB5457DL,0x8AB5457DL,0x8AB5457DL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int8_t  func_1(void);
+
