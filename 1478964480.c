56dd2539e703fb718f11c7237943dd5f1c36f5ee
saisi <saisi@users.noreply.github.com>
2016-11-12 15:28:00 +0000

    1478964480 antifungin

---
+++ 1478964480.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      980371574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   uint32_t  f1;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 0x12491E29L;
+static int32_t g_18[8] = {4L,4L,4L,4L,4L,4L,4L,4L};
+static int32_t *g_24 = &g_18[6];
