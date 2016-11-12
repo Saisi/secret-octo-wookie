b46c55bb734e98d9dde999b84aa580d7c966c6d3
saisi <saisi@users.noreply.github.com>
2016-11-12 13:27:55 +0000

    1478957274 adventists

---
+++ 1478957274.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3429909258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_22 = 1UL;/* VOLATILE GLOBAL g_22 */
+static int16_t g_34 = 0x8A60L;
+static int32_t g_70 = (-9L);
+static uint32_t g_89 = 0x01101210L;
+static union U0 g_90 = {0x476FL};
