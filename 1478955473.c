6a02723d69c343b096e71baffee2cdda8e8499ab
saisi <saisi@users.noreply.github.com>
2016-11-12 12:57:53 +0000

    1478955473 antiracism

---
+++ 1478955473.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      878429212
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_10[3] = {0x3FL,0x3FL,0x3FL};
+static uint16_t g_54 = 0x0C19L;
+static int16_t g_59 = 0L;
+static int32_t g_61 = 0L;
+static int32_t *g_66 = &g_61;
+static int32_t ** volatile g_65 = &g_66;/* VOLATILE GLOBAL g_65 */
+static int32_t g_69 = 0x42895483L;
+static int32_t * volatile g_70 = &g_61;/* VOLATILE GLOBAL g_70 */
+static uint32_t g_83 = 4294967288UL;
