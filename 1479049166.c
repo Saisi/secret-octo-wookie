5c532ea3305f2bd7557b90e0c2356135aed04a22
saisi <saisi@users.noreply.github.com>
2016-11-13 14:59:26 +0000

    1479049166 arbitrational

---
+++ 1479049166.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1153970546
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   int64_t  f0;
+   volatile uint64_t  f1;
+   int32_t  f2;
+   volatile int64_t  f3;
+   unsigned f4 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x059141A9L;
