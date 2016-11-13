508dadecd51a0a2d446fa95a532b8408c9076ebb
saisi <saisi@users.noreply.github.com>
2016-11-13 23:59:55 +0000

    1479081595 aplacental

---
+++ 1479081595.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1355620381
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 13;
+   unsigned f1 : 16;
+   signed f2 : 19;
+   const volatile signed f3 : 18;
+   volatile unsigned f4 : 27;
+   unsigned f5 : 8;
+   const signed f6 : 7;
+   volatile signed f7 : 30;
+   const int16_t  f8;
