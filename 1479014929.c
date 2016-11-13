74802d9819813923dfaed8967accb5c79b7a65ea
saisi <saisi@users.noreply.github.com>
2016-11-13 05:28:49 +0000

    1479014929 audiologist

---
+++ 1479014929.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      262677128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 18;
+   signed f1 : 31;
+   volatile unsigned f2 : 5;
+   const unsigned f3 : 29;
+   signed f4 : 24;
+};
+#pragma pack(pop)
