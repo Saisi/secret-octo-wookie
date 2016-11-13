613ee1215d40e6c9cb51633725c60dd18eb421e4
saisi <saisi@users.noreply.github.com>
2016-11-13 02:28:40 +0000

    1479004120 baning

---
+++ 1479004120.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4278906667
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[9][10][2] = {{{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)}},{{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)}},{{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)}},{{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)}},{{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)}},{{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)}},{{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)}},{{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)}},{{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)},{(-1L),0xB9B55BB2L},{(-1L),(-1L)},{1L,0xB9B55BB2L},{1L,(-1L)}}};
+static volatile int32_t g_3 = 0x2443AC46L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xD16652D0L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xBDBDF7CDL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xA2282D56L;
+static int32_t *g_12 = &g_6;
+static int32_t ** volatile g_11 = &g_12;/* VOLATILE GLOBAL g_11 */
+static int32_t g_14 = 0xDAC842F8L;
+
