a1277d2d9a9fec0e21553a1aa32a6bf9b59d907a
saisi <saisi@users.noreply.github.com>
2016-11-13 15:29:27 +0000

    1479050967 Ashangos

---
+++ 1479050967.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3105467416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint16_t g_2[8][2] = {{65532UL,65535UL},{0xE559L,65532UL},{0xA7C2L,0xA7C2L},{0xA7C2L,65532UL},{0xE559L,65535UL},{65532UL,65535UL},{0xE559L,65532UL},{0xA7C2L,0xA7C2L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
