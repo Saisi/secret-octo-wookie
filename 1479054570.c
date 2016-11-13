b43e3778e73dd59bb5f0c739dd22bbb7cd04688e
saisi <saisi@users.noreply.github.com>
2016-11-13 16:29:30 +0000

    1479054570 Actinopoda

---
+++ 1479054570.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4222055818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int16_t g_10[7] = {0x15B4L,0L,0x15B4L,0x15B4L,0L,0x15B4L,0x15B4L};
+static volatile uint16_t g_11 = 65535UL;/* VOLATILE GLOBAL g_11 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
