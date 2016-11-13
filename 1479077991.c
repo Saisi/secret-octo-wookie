cfb7c0f752a5429b1d34bacf27c86271df1f5fa5
saisi <saisi@users.noreply.github.com>
2016-11-13 22:59:52 +0000

    1479077991 aywhere

---
+++ 1479077991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3149356532
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xD8DA8E97L;
+static int32_t g_5 = 4L;
+static int8_t g_17 = (-5L);
