42d08a46f76f7e8d58887c24feb11135532f30a1
saisi <saisi@users.noreply.github.com>
2016-11-13 18:59:39 +0000

    1479063578 autotetraploid

---
+++ 1479063578.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2757194794
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_2[9][8][1] = {{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}},{{3UL},{3UL},{3UL},{9UL},{3UL},{3UL},{3UL},{9UL}}};
+static int32_t g_47 = 0xC3EFC167L;
+static int32_t *g_46 = &g_47;
+static int64_t g_65 = (-7L);
+static int16_t g_76 = 1L;
+static uint64_t g_90 = 0x79B6842A457AFC49LL;
+static uint32_t g_93 = 5UL;
+static int8_t g_111 = 6L;
+static uint64_t g_116 = 0UL;
