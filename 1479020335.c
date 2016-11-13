973c67ba1763675031b329162cc9350902bed2af
saisi <saisi@users.noreply.github.com>
2016-11-13 06:58:56 +0000

    1479020335 accentuations

---
+++ 1479020335.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3533135640
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_6 = (-1L);
+static int32_t g_15 = 0xF4F823AAL;
+static uint32_t g_43[1] = {4294967287UL};
+static int32_t g_44[6] = {0xD97867A7L,0xD97867A7L,0xD97867A7L,0xD97867A7L,0xD97867A7L,0xD97867A7L};
+static int16_t g_67 = 0x7412L;
+static uint64_t g_84 = 0UL;
+static uint64_t g_92 = 5UL;
+static uint8_t g_94[10] = {0x33L,247UL,0x33L,247UL,0x33L,247UL,0x33L,247UL,0x33L,247UL};
+static int32_t g_95 = 0xDEEAF792L;
