167e52f5ede5297455976a5228b04ff7935b73e1
saisi <saisi@users.noreply.github.com>
2016-11-13 04:28:47 +0000

    1479011326 anthroposociology

---
+++ 1479011326.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2184363096
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   int16_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = 0x0714L;
+static int32_t g_48 = 2L;
+static int32_t *g_47[10][5] = {{&g_48,&g_48,&g_48,(void*)0,&g_48},{&g_48,&g_48,&g_48,&g_48,(void*)0},{&g_48,(void*)0,(void*)0,&g_48,&g_48},{&g_48,&g_48,&g_48,(void*)0,&g_48},{(void*)0,(void*)0,&g_48,&g_48,&g_48},{&g_48,&g_48,&g_48,&g_48,&g_48},{&g_48,&g_48,&g_48,&g_48,(void*)0},{&g_48,&g_48,&g_48,&g_48,&g_48},{&g_48,&g_48,(void*)0,&g_48,&g_48},{(void*)0,&g_48,&g_48,&g_48,&g_48}};
