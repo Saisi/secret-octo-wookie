eabb406dc1fb16003e1031e8e9e28297e757a162
saisi <saisi@users.noreply.github.com>
2016-11-13 10:29:11 +0000

    1479032951 ammino

---
+++ 1479032951.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2397813846
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_3 = 0xC29A3DF6L;/* VOLATILE GLOBAL g_3 */
+static int8_t g_27 = 0xD8L;
+static int32_t g_45 = 0x993DD61FL;
+static int32_t g_56 = 0x947F9F41L;
+static uint32_t g_61 = 0xDBF60700L;
+static uint64_t g_64 = 0x1ADF561F6604C38DLL;
+static uint64_t g_75 = 1UL;
+static int64_t g_81 = 0L;
+static uint16_t g_82[1] = {0x44E3L};
