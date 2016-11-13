7a6cdb7bec72c4872cf3bc979fe484518094d19a
saisi <saisi@users.noreply.github.com>
2016-11-13 23:29:53 +0000

    1479079793 adenohypersthenia

---
+++ 1479079793.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2601561363
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   const unsigned f1 : 31;
+   int8_t  f2;
+   const int8_t  f3;
+   const int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_32[1] = {0xBFL};
