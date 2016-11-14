35c098dff5ed69a76171eae66a9eeda051886a44
saisi <saisi@users.noreply.github.com>
2016-11-14 17:31:04 +0000

    1479144664 antigrammaticalness

---
+++ 1479144664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3738896700
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   volatile unsigned f1 : 17;
+   signed f2 : 15;
+   unsigned f3 : 31;
+   signed f4 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
