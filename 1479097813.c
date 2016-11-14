2ed5c53d8d28e2607ffba0b57f3d0cdba272bb20
saisi <saisi@users.noreply.github.com>
2016-11-14 04:30:14 +0000

    1479097813 Bacchuslike

---
+++ 1479097813.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1116141718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile int32_t  f0;
+   volatile signed f1 : 22;
+};
+#pragma pack(pop)
+
+union U1 {
+   uint32_t  f0;
