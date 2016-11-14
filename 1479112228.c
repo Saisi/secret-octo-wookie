ec6c7a6791b3f9247e2db7db8a530c9389f35931
saisi <saisi@users.noreply.github.com>
2016-11-14 08:30:28 +0000

    1479112228 anachorism

---
+++ 1479112228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      78568642
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 2L;
+static int32_t g_31 = (-10L);
+static int32_t g_37 = 5L;
+static volatile int64_t g_45 = 0xD5DF5B04CE9D0615LL;/* VOLATILE GLOBAL g_45 */
+static uint32_t g_46 = 18446744073709551615UL;
+static int32_t *g_59 = &g_37;
+static const int32_t *g_65 = &g_31;
+static const int32_t **g_64 = &g_65;
+static int16_t g_85 = (-8L);
