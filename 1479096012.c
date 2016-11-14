4f381a2e7d784fff7bbeecfbfff71fb1e0348cfc
saisi <saisi@users.noreply.github.com>
2016-11-14 04:00:12 +0000

    1479096012 Anisodactyla

---
+++ 1479096012.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2742737547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int32_t  f0;
+   int64_t  f1;
+   uint8_t  f2;
+   volatile uint32_t  f3;
+   volatile uint64_t  f4;
+   const int8_t  f5;
+   const int32_t  f6;
