9fcae8098e8ec2b750f01af60646145255e7a47a
saisi <saisi@users.noreply.github.com>
2016-11-14 12:30:42 +0000

    1479126642 anaerobism

---
+++ 1479126642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2528605571
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile signed f1 : 23;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint8_t g_2[10][9][2] = {{{255UL,0xA1L},{0xCEL,1UL},{0xCEL,0xA1L},{255UL,1UL},{0xA1L,1UL},{0x8EL,0xF9L},{0x1EL,0UL},{0UL,255UL},{252UL,0x51L}},{{1UL,0UL},{0x92L,1UL},{0x8EL,251UL},{8UL,1UL},{0x51L,8UL},{0xCEL,0x99L},{0xD4L,0xA1L},{0x51L,0UL},{0xA1L,251UL}},{{0UL,0xF9L},{0x92L,1UL},{0UL,0x51L},{255UL,0x51L},{0UL,1UL},{1UL,0UL},{0x6CL,255UL},{0x1EL,0x92L},{255UL,0x1EL}},{{0x0FL,8UL},{1UL,1UL},{255UL,0x3AL},{1UL,255UL},{0xD4L,1UL},{1UL,1UL},{0xCCL,255UL},{1UL,0x79L},{1UL,1UL}},{{0x4DL,0UL},{0xD4L,0x8FL},{0x1EL,0x3AL},{0x79L,0x1EL},{1UL,0x95L},{1UL,0x1EL},{0x79L,0x3AL},{0x1EL,0x8FL},{0xD4L,0UL}},{{0x4DL,1UL},{1UL,0x79L},{1UL,255UL},{0xCCL,1UL},{1UL,1UL},{0xD4L,255UL},{1UL,0x3AL},{255UL,1UL},{1UL,8UL}},{{0x0FL,0x1EL},{255UL,0x92L},{0x1EL,255UL},{0x6CL,0UL},{1UL,0xCCL},{1UL,255UL},{0UL,255UL},{1UL,0xCCL},{1UL,0UL}},{{0x6CL,255UL},{0x1EL,0x92L},{255UL,0x1EL},{0x0FL,8UL},{1UL,1UL},{255UL,0x3AL},{1UL,255UL},{0xD4L,1UL},{1UL,1UL}},{{0xCCL,255UL},{1UL,0x79L},{1UL,1UL},{0x4DL,0UL},{0xD4L,0x8FL},{0x1EL,0x3AL},{0x79L,0x1EL},{1UL,0x95L},{1UL,0x1EL}},{{0x79L,0x3AL},{0x1EL,0x8FL},{0xD4L,0UL},{0x4DL,1UL},{1UL,0x79L},{1UL,255UL},{0xCCL,1UL},{1UL,1UL},{0xD4L,255UL}}};
+static volatile int32_t g_6 = 0xA07C694AL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static int32_t g_11[1][5][8] = {{{6L,6L,6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L,6L,6L}}};
