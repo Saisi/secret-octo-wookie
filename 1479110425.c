c8a326610bc4eaf4c0b30e1d37d43934a33efcee
saisi <saisi@users.noreply.github.com>
2016-11-14 08:00:27 +0000

    1479110425 aberrating

---
+++ 1479110425.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1582259200
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+union U1 {
+   volatile uint64_t  f0;
+   signed f1 : 9;
+   uint64_t  f2;
+};
+
