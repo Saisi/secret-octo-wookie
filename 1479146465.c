85ac663cf3a17000ea2c9152a4884a254427049d
saisi <saisi@users.noreply.github.com>
2016-11-14 18:01:06 +0000

    1479146465 aloetical

---
+++ 1479146465.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2471316778
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   signed f3 : 11;
+   uint32_t  f4;
+};
+
+union U1 {
+   const uint64_t  f0;
