4779dcbf74357e118c5fdeec1cfb4daf04e55d44
saisi <saisi@users.noreply.github.com>
2016-11-14 22:01:21 +0000

    1479160881 bagios

---
+++ 1479160881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      162375483
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[10] = {1L,(-7L),1L,1L,(-7L),1L,1L,(-7L),1L,1L};
+static int32_t *g_4[3] = {&g_3[6],&g_3[6],&g_3[6]};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
