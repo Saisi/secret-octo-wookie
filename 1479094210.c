1d9c71697f7df47e51d4840f7f8d510570e69e09
saisi <saisi@users.noreply.github.com>
2016-11-14 03:30:11 +0000

    1479094210 backlashers

---
+++ 1479094210.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3511646024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 18;
+   signed f1 : 31;
+   signed f2 : 5;
+   signed f3 : 29;
+   volatile signed f4 : 24;
+   const volatile unsigned f5 : 15;
+   unsigned f6 : 17;
+   volatile signed f7 : 2;
+   uint32_t  f8;
