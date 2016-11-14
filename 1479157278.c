225aaf8f81af39508af6ab55b58b60c9d747ff3e
saisi <saisi@users.noreply.github.com>
2016-11-14 21:01:18 +0000

    1479157278 atlantoaxial

---
+++ 1479157278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2068907383
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_4 = 1UL;/* VOLATILE GLOBAL g_4 */
+static uint64_t g_11 = 0UL;
+static int16_t *g_43 = (void*)0;
+static int16_t g_69 = 1L;
+static int8_t g_71[6] = {0xDAL,0xDAL,0xDAL,0xDAL,0xDAL,0xDAL};
+static int16_t g_89 = (-5L);
+static uint8_t g_91 = 254UL;
+static int64_t g_92[1] = {0x8187147D86C3CE99LL};
+static int8_t g_94 = 0L;
