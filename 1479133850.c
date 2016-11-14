1672a32a384a549f9fd4d501cb3916c92c5916b1
saisi <saisi@users.noreply.github.com>
2016-11-14 14:30:50 +0000

    1479133850 albitite

---
+++ 1479133850.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      149955096
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB4C8F0D0L;
+static int16_t g_18 = (-3L);
+static int8_t g_19 = 1L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
