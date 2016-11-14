5b1cc46894e48293afb3f7620ebe461d363831fb
saisi <saisi@users.noreply.github.com>
2016-11-14 07:00:22 +0000

    1479106821 Alba

---
+++ 1479106821.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3086522279
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   uint16_t  f1;
+   uint32_t  f2;
+};
+
+struct S1 {
+   signed f0 : 24;
+   unsigned f1 : 17;
+   unsigned f2 : 16;
