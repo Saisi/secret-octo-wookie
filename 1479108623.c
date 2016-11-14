9c6c18b227849b4834b399e6d1c3c39e989400ed
saisi <saisi@users.noreply.github.com>
2016-11-14 07:30:23 +0000

    1479108623 backplanes

---
+++ 1479108623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3003205830
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   int64_t  f1;
+   volatile signed f2 : 10;
+   int16_t  f3;
+   volatile signed f4 : 24;
+   signed f5 : 26;
+};
