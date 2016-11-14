40397d425d468ec80292bf9cb75943157ec5aed2
saisi <saisi@users.noreply.github.com>
2016-11-14 20:01:14 +0000

    1479153673 aminoacetophenetidine

---
+++ 1479153673.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      110812637
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint32_t  f1;
+   volatile uint32_t  f2;
+   volatile int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCA2961C0L;
+static uint32_t g_5 = 1UL;
