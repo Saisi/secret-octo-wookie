8ccd5ed166e7086cbc1837e5685e86a2798471c6
saisi <saisi@users.noreply.github.com>
2016-11-14 19:01:09 +0000

    1479150069 archineuron

---
+++ 1479150069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1074727701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[2][8] = {{65535UL,0xC2E0L,0xC2E0L,65535UL,65535UL,0xC2E0L,0xC2E0L,65535UL},{65535UL,0xC2E0L,0xC2E0L,65535UL,65535UL,0xC2E0L,0xC2E0L,65535UL}};
+static int32_t g_3 = (-9L);
+static int32_t g_4[2] = {0x4C23AFC3L,0x4C23AFC3L};
+static volatile int32_t g_5[1] = {0x0374C7AFL};
+static volatile int32_t g_6 = 0x131D8B9DL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x5223FA3AL;
+static int16_t g_32 = (-3L);
+static uint64_t g_48 = 0xDBD3D38E20B45845LL;
+static const volatile int16_t g_53 = 0x1330L;/* VOLATILE GLOBAL g_53 */
