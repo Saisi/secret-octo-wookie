674b663e4d7e9f0f0a82fe7a306caea6505db12d
saisi <saisi@users.noreply.github.com>
2016-11-14 15:00:52 +0000

    1479135651 aristocratic

---
+++ 1479135651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2213953493
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   int32_t  f1;
+   const uint32_t  f2;
+   int64_t  f3;
+   volatile uint16_t  f4;
+   const unsigned f5 : 19;
+   int32_t  f6;
+   volatile uint64_t  f7;
+};
