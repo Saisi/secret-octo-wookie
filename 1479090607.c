62b0dbc4f90700923226cd4eeffef9d050a7ae8a
saisi <saisi@users.noreply.github.com>
2016-11-14 02:30:08 +0000

    1479090607 amyrol

---
+++ 1479090607.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2003947997
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   volatile int32_t  f1;
+   int16_t  f2;
+   const int64_t  f3;
+   int8_t  f4;
+   uint64_t  f5;
+   volatile int32_t  f6;
