b28a9aa6591b73ae0acf13bca2656349a9a22390
saisi <saisi@users.noreply.github.com>
2016-11-14 14:00:49 +0000

    1479132048 Acanthocereus

---
+++ 1479132048.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1480234772
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 24;
+   signed f1 : 13;
+   signed f2 : 27;
+   const unsigned f3 : 27;
+   unsigned f4 : 30;
+   unsigned f5 : 29;
+   signed f6 : 7;
+};
+
