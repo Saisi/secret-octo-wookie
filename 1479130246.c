ac73c7e01dc172d5fc849fd675ded2262ed7b5cd
saisi <saisi@users.noreply.github.com>
2016-11-14 13:30:47 +0000

    1479130246 anodynes

---
+++ 1479130246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1538128027
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint16_t  f0;
+   volatile uint32_t  f1;
+   const int64_t  f2;
+   int32_t  f3;
+   uint32_t  f4;
+   signed f5 : 10;
+   unsigned f6 : 27;
