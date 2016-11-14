10a80af23b517ecb52c6c9a10b7ba57cca871904
saisi <saisi@users.noreply.github.com>
2016-11-14 15:30:53 +0000

    1479137453 anticlinorium

---
+++ 1479137453.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2237372496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[1][2][10] = {{{0xF23BL,3L,0xCCCDL,0xCCCDL,3L,0xF23BL,3L,0xCCCDL,0xCCCDL,3L},{0xF23BL,3L,0xCCCDL,0xCCCDL,3L,0xF23BL,3L,0xCCCDL,0xCCCDL,3L}}};
+static int32_t g_4 = 0x76EC7516L;
+static int32_t * const  volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
