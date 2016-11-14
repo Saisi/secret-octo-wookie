e8a6f262147a7ff927f6ab3b4c111a880aafa773
saisi <saisi@users.noreply.github.com>
2016-11-14 22:31:23 +0000

    1479162682 antrin

---
+++ 1479162682.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2688527123
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_4 = 1UL;
+static int8_t g_9[1] = {5L};
+static uint64_t g_10 = 18446744073709551612UL;
+static int32_t g_18 = 0x0915BB00L;
+static const int32_t * volatile g_17[3][8] = {{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0}};
+static volatile int32_t g_33 = (-6L);/* VOLATILE GLOBAL g_33 */
+static uint16_t g_34 = 0xE85CL;
+static uint8_t g_83 = 0x0CL;
+static uint8_t g_107[10] = {0xECL,0xECL,249UL,0xECL,0xECL,249UL,0xECL,0xECL,249UL,0xECL};
