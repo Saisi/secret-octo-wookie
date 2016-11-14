a67a16307483951ffadf2d9001e086cabfc38ce2
saisi <saisi@users.noreply.github.com>
2016-11-14 21:31:20 +0000

    1479159079 antiparliamenteer

---
+++ 1479159079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      284911856
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 1L;
+static uint64_t g_32 = 0x68F62C4DF8E3BF11LL;
+static uint64_t g_34 = 18446744073709551606UL;
+static int32_t g_59[4][8] = {{0xD734895CL,1L,(-5L),(-5L),1L,0xD734895CL,1L,(-5L)},{0x6DAE1DC6L,1L,0x6DAE1DC6L,0xD734895CL,0xD734895CL,0x6DAE1DC6L,1L,0x6DAE1DC6L},{0x0D296407L,0xD734895CL,(-5L),0xD734895CL,0x0D296407L,0x0D296407L,0xD734895CL,(-5L)},{0x0D296407L,0x0D296407L,0xD734895CL,(-5L),0xD734895CL,0x0D296407L,0x0D296407L,0xD734895CL}};
+static volatile int32_t g_64 = 0L;/* VOLATILE GLOBAL g_64 */
+static volatile int32_t g_65 = (-9L);/* VOLATILE GLOBAL g_65 */
+static volatile int32_t g_66 = 0xB95DFEA4L;/* VOLATILE GLOBAL g_66 */
+static int32_t g_67 = 6L;
+static volatile int32_t g_68 = (-5L);/* VOLATILE GLOBAL g_68 */
