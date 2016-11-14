ae6982800286056431e4a32ab4df698273cf3b04
saisi <saisi@users.noreply.github.com>
2016-11-14 16:30:57 +0000

    1479141055 airtightness

---
+++ 1479141055.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2356961046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int32_t  f1;
+   const uint32_t  f2;
+   const int64_t  f3;
+   const uint32_t  f4;
+   int32_t  f5;
+   volatile uint32_t  f6;
+   const uint32_t  f7;
+   int8_t  f8;
