63e8c79becfbecf1b261d438edbff48bbd365bdb
saisi <saisi@users.noreply.github.com>
2016-11-15 23:32:50 +0000

    1479252769 astrolithology

---
+++ 1479252769.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1526085206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint32_t  f1;
+   volatile unsigned f2 : 4;
+   int16_t  f3;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   const int16_t  f1;
