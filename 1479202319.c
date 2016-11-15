8a56970ad74848063ecf3e1809903a32b4c960b7
saisi <saisi@users.noreply.github.com>
2016-11-15 09:32:01 +0000

    1479202319 alginates

---
+++ 1479202319.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1483687151
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * const  f0;
+   volatile unsigned f1 : 7;
+   int8_t * volatile  f2;
+   uint32_t  f3;
+   const uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 1L;
