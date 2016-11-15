6308a5fbc003da3b2a54956e9b55f20b221ed939
saisi <saisi@users.noreply.github.com>
2016-11-15 08:31:56 +0000

    1479198716 adipocele

---
+++ 1479198716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4179869502
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   const uint64_t  f1;
+   uint16_t  f2;
+   unsigned f3 : 4;
+   const volatile int64_t  f4;
+   signed f5 : 6;
+   volatile uint16_t  f6;
