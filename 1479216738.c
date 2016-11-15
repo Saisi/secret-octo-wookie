6b20efa123b0fd9c9b99bd4570dbcdc493c5370a
saisi <saisi@users.noreply.github.com>
2016-11-15 13:32:18 +0000

    1479216738 Bandor

---
+++ 1479216738.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1341631272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   unsigned f1 : 15;
+   const int64_t  f2;
+   volatile uint32_t  f3;
+   const int32_t  f4;
+   uint32_t  f5;
+   uint8_t  f6;
+   volatile uint64_t  f7;
+   const volatile int64_t  f8;
