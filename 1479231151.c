8b066556cbe1472767f2b123df0bce63a72bf2fa
saisi <saisi@users.noreply.github.com>
2016-11-15 17:32:31 +0000

    1479231151 arsenophenylglycin

---
+++ 1479231151.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2006385295
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   int8_t * const  volatile  f0;
+   volatile int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 4L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x5FEF9D78L;
+static union U3 g_6 = {0};/* VOLATILE GLOBAL g_6 */
+
