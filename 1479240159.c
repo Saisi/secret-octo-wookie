f235502ef0c28c1d287ef8227bd960b1ced4ef2d
saisi <saisi@users.noreply.github.com>
2016-11-15 20:02:39 +0000

    1479240159 arbutus

---
+++ 1479240159.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      165033214
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 13;
+   const volatile unsigned f1 : 14;
+   volatile unsigned f2 : 24;
+   volatile unsigned f3 : 4;
+   volatile signed f4 : 3;
+   volatile signed f5 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
