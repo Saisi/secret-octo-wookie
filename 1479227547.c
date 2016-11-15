02526c71302d304e6d402ae78518454a548d1ce0
saisi <saisi@users.noreply.github.com>
2016-11-15 16:32:28 +0000

    1479227547 acousmata

---
+++ 1479227547.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3090902000
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 6;
+   const int8_t  f1;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   int8_t  f1;
+   int32_t  f2;
+};
