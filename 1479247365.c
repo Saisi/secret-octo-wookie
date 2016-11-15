bc1b7d986c79568703680abcaf7f95fd919f45fd
saisi <saisi@users.noreply.github.com>
2016-11-15 22:02:45 +0000

    1479247365 Armalda

---
+++ 1479247365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3340416727
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 17;
+   const volatile unsigned f1 : 15;
+   volatile unsigned f2 : 5;
