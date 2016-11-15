1c6c04780a96f391874fc5113431d0d252b56544
saisi <saisi@users.noreply.github.com>
2016-11-15 00:01:28 +0000

    1479168088 Barabra

---
+++ 1479168088.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      127685846
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint32_t  f1;
+   volatile uint32_t  f2;
+   const uint64_t  f3;
+   const int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = (-1L);
