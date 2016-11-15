9dfe48b2de4187ec15af24683e08e4f951209069
saisi <saisi@users.noreply.github.com>
2016-11-15 08:01:55 +0000

    1479196915 australoid

---
+++ 1479196915.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1652387779
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   volatile int16_t  f1;
+   unsigned f2 : 7;
+   signed f3 : 13;
+   int8_t  f4;
+   const volatile uint32_t  f5;
+   volatile uint8_t  f6;
+};
+
