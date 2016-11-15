78ca6857f51ac9befc0bac91a7be9f42f0c8cfdb
saisi <saisi@users.noreply.github.com>
2016-11-15 18:02:33 +0000

    1479232952 antiwhitism

---
+++ 1479232952.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4124947713
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   int32_t  f1;
+   volatile int32_t  f2;
+   volatile uint32_t  f3;
+   const uint32_t  f4;
+   uint16_t  f5;
+};
+
+struct S1 {
