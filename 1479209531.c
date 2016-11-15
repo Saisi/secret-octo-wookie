7072ca492d54ba75b8930e61d39912a87b288cde
saisi <saisi@users.noreply.github.com>
2016-11-15 11:32:12 +0000

    1479209531 ayelp

---
+++ 1479209531.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1180032496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8[7] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL};
+static uint32_t g_16 = 1UL;
+static int8_t g_25[8] = {0L,0L,0L,0L,0L,0L,0L,0L};
+static uint64_t g_27 = 0UL;
+static int64_t g_30[10][2] = {{(-1L),1L},{1L,(-1L)},{0x258D0336D065835ALL,(-5L)},{0x258D0336D065835ALL,(-1L)},{1L,1L},{(-1L),0x258D0336D065835ALL},{(-5L),0x258D0336D065835ALL},{(-1L),1L},{1L,(-1L)},{0x258D0336D065835ALL,(-5L)}};
+static uint32_t g_61 = 0x49CF94A2L;
+static int32_t g_67 = 0xE42BF625L;
+static int32_t * volatile g_66 = &g_67;/* VOLATILE GLOBAL g_66 */
+static int32_t g_79[2] = {0x08375F08L,0x08375F08L};
