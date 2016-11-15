8f55dd4353a5bb42e4000e5afd1699ce64abd5e2
saisi <saisi@users.noreply.github.com>
2016-11-15 03:01:39 +0000

    1479178899 aerodromics

---
+++ 1479178899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1686461616
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const unsigned f0 : 30;
+   const int32_t  f1;
+   unsigned f2 : 25;
+   unsigned f3 : 19;
+   const signed f4 : 18;
+   unsigned f5 : 21;
+};
