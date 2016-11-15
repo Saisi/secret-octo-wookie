996bf56674b97d19191c58e2bc9fbe3342dbc4b4
saisi <saisi@users.noreply.github.com>
2016-11-15 07:31:54 +0000

    1479195113 ablatival

---
+++ 1479195113.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3530616750
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static int32_t g_10 = 8L;
+static int32_t g_11 = 0xCE2949F4L;
+static uint8_t g_22 = 0x85L;
+static int32_t g_25 = 1L;
+static int32_t *g_52 = &g_25;
+static int32_t **g_51 = &g_52;
+static int32_t ***g_50 = &g_51;
