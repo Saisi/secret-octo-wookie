e7086c2af7e814dac7aeb25e57c8b4247df777d5
saisi <saisi@users.noreply.github.com>
2016-11-15 09:01:58 +0000

    1479200518 antihuff

---
+++ 1479200518.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2833367710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * f0;
+   int32_t  f1;
+   uint64_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0xD2E8588DL;
+static int64_t g_9 = 0L;
