1e95a76a52d1229784497a73a5c32e5eebd5315e
saisi <saisi@users.noreply.github.com>
2016-11-15 16:02:26 +0000

    1479225746 abri

---
+++ 1479225746.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      704173326
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int16_t g_2 = 0xBDDEL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
