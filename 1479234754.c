14c867d6966e98fdea8c389dff9db92c5eaa22eb
saisi <saisi@users.noreply.github.com>
2016-11-15 18:32:35 +0000

    1479234754 ataghan

---
+++ 1479234754.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4094523256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static uint8_t g_58 = 9UL;
+static uint32_t g_68 = 0x39BA5493L;
+static uint32_t g_70 = 18446744073709551608UL;
+static int8_t g_73 = 0xD1L;
+static int32_t g_75 = (-4L);
+static int8_t g_88 = (-1L);
+static uint16_t g_102[2][3][9] = {{{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL}},{{65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL,0x0392L,65531UL}}};
+static uint32_t g_112 = 0x0ADFF402L;
