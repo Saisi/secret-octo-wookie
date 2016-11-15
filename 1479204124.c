eafa218e8d6ffddcc5a4bd6c604aebf97c0213a9
saisi <saisi@users.noreply.github.com>
2016-11-15 10:02:04 +0000

    1479204124 avolation

---
+++ 1479204124.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3348042293
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int64_t  f1;
+   unsigned f2 : 31;
+   volatile uint16_t  f3;
+   volatile int64_t  f4;
+};
+
+union U1 {
+   int32_t  f0;
