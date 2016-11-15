771a65a8d004270e93860e2ec2a003c4e31a912e
saisi <saisi@users.noreply.github.com>
2016-11-15 05:01:45 +0000

    1479186105 aphetism

---
+++ 1479186105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4142982930
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   const volatile signed f1 : 16;
+   const signed f2 : 18;
+   unsigned f3 : 22;
+   unsigned f4 : 13;
+   const volatile signed f5 : 16;
+   const signed f6 : 14;
