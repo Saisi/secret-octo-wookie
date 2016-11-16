dcc4ae281542e44708f2cdf3332127d01189ad21
saisi <saisi@users.noreply.github.com>
2016-11-16 05:03:12 +0000

    1479272589 aldohexose

---
+++ 1479272589.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3626661165
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   uint16_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 19;
+   volatile unsigned f1 : 31;
