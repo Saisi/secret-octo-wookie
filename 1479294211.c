1b1500d242aceb8cbf7dcff50738e2b7d652d78a
saisi <saisi@users.noreply.github.com>
2016-11-16 11:03:32 +0000

    1479294211 Akkadist

---
+++ 1479294211.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3282765758
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+   volatile uint8_t  f2;
+   volatile int32_t  f3;
+   volatile int32_t  f4;
