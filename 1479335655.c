6e7f34d1c8ae0d1ef4159d607693d975f4151c27
saisi <saisi@users.noreply.github.com>
2016-11-16 22:34:16 +0000

    1479335655 abducting

---
+++ 1479335655.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2113494120
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint64_t  f0;
+   signed f1 : 15;
+   const unsigned f2 : 7;
+   volatile unsigned f3 : 13;
+   unsigned f4 : 30;
+   signed f5 : 18;
+   signed f6 : 11;
+};
+
