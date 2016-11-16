1841945201448eb3ca78306f37da5e9335d006e5
saisi <saisi@users.noreply.github.com>
2016-11-16 19:34:05 +0000

    1479324845 aetiology

---
+++ 1479324845.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4139899136
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 14;
+   signed f1 : 11;
+   signed f2 : 1;
+   const volatile int16_t  f3;
+   signed f4 : 26;
+   volatile uint32_t  f5;
+   volatile unsigned f6 : 4;
+};
+
