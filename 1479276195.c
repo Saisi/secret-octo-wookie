b1cc9e63f98bdb9fb60f647cad78e807d312190d
saisi <saisi@users.noreply.github.com>
2016-11-16 06:03:15 +0000

    1479276195 ballam

---
+++ 1479276195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1760684389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   int32_t  f1;
+   uint8_t  f2;
+};
+
+union U1 {
+   int64_t  f0;
+   int8_t * const  f1;
+   signed f2 : 16;
