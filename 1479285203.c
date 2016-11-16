31e12124d2d3ef4b5fd94328cb0a35286548027c
saisi <saisi@users.noreply.github.com>
2016-11-16 08:33:23 +0000

    1479285203 Arkabutla

---
+++ 1479285203.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      560575138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static struct S1 g_2 = {-10L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static struct S1  func_1(void);
