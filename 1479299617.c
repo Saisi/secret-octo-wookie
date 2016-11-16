8fdd64c8fdc1c1ee3142dfe5da3449ba42393cab
saisi <saisi@users.noreply.github.com>
2016-11-16 12:33:38 +0000

    1479299617 algaeology

---
+++ 1479299617.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1990058618
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const volatile uint32_t  f1;
+   volatile unsigned f2 : 2;
+   int8_t  f3;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   const int8_t  f1;
