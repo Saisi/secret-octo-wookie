b356eb763c5a808b87290fb7dd0499ea7d47d4e7
saisi <saisi@users.noreply.github.com>
2016-11-16 03:03:02 +0000

    1479265381 archtraitor

---
+++ 1479265381.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3476032115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const signed f1 : 29;
+   volatile signed f2 : 31;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint16_t  f0;
