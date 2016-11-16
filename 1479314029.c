8062b91c889b34fc172e2f2b3aa47214edfdc510
saisi <saisi@users.noreply.github.com>
2016-11-16 16:33:49 +0000

    1479314029 Aindrea

---
+++ 1479314029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3916265641
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 19;
+   const signed f1 : 30;
+   signed f2 : 9;
+   signed f3 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_4 = 0x7227L;
+static struct S0 g_22 = {2,24882,-7,6};
