2d7d9454712d8bf30807e95b72e414af8b21f13a
saisi <saisi@users.noreply.github.com>
2016-11-16 23:34:19 +0000

    1479339259 amovability

---
+++ 1479339259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1748230388
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   uint64_t  f1;
+   unsigned : 0;
+   int32_t  f2;
+   uint32_t  f3;
+   volatile int16_t  f4;
+   uint16_t  f5;
