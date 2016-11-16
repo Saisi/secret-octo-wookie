e2c52b34612c46069765acb9090da32ee6356a66
saisi <saisi@users.noreply.github.com>
2016-11-16 15:33:46 +0000

    1479310426 balks

---
+++ 1479310426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3444557998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 5;
+   unsigned f1 : 6;
+   unsigned f2 : 16;
+   signed f3 : 28;
+   volatile signed f4 : 27;
+   unsigned f5 : 6;
+   const volatile signed f6 : 12;
