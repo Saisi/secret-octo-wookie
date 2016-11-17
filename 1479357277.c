8f7682c195201919184258c16152a503b3be33fb
saisi <saisi@users.noreply.github.com>
2016-11-17 04:34:37 +0000

    1479357277 anisocoria

---
+++ 1479357277.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2272465505
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_4 = 0xD16DL;/* VOLATILE GLOBAL g_4 */
+static uint32_t g_10[8] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL};
+static int32_t g_13[9] = {0x283066C9L,(-1L),(-1L),0x283066C9L,(-1L),(-1L),0x283066C9L,(-1L),(-1L)};
+static int32_t *g_22 = &g_13[4];
+static int32_t **g_73 = &g_22;
+static int32_t ***g_86 = (void*)0;
+static uint16_t g_118 = 1UL;
+static int32_t g_123 = 7L;
+static uint32_t g_165 = 0x2DABF79CL;
