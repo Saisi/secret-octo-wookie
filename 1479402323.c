d0c5d0c93267313b871d1fd4d487c3fdabff677a
saisi <saisi@users.noreply.github.com>
2016-11-17 17:05:24 +0000

    1479402323 acacatechol

---
+++ 1479402323.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      521607123
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 8;
+   signed f1 : 31;
+   unsigned f2 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_9 = 0x2C5EB200L;/* VOLATILE GLOBAL g_9 */
+static uint16_t g_10 = 0xAA96L;
+static uint8_t g_13[2] = {0UL,0UL};
