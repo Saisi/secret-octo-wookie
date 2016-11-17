2566eeba309edbcc0b356faa8d63667895ff289a
saisi <saisi@users.noreply.github.com>
2016-11-17 22:35:46 +0000

    1479422146 Aeolic

---
+++ 1479422146.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      802217213
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const unsigned f0 : 5;
+   uint64_t  f1;
+   uint8_t  f2;
+   uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U1 g_2 = {0UL};
+
