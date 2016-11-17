7d74bc4603886a29077dcc68f7968684a154b099
saisi <saisi@users.noreply.github.com>
2016-11-17 02:04:28 +0000

    1479348267 archchampion

---
+++ 1479348267.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4218899787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   uint16_t  f1;
+   volatile int8_t  f2;
+   uint64_t  f3;
+   int16_t  f4;
+};
+
+struct S1 {
+   signed f0 : 4;
