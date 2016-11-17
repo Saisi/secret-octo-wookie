54aaa040aad4361b840dff7f2da5cc10b90b1dd6
saisi <saisi@users.noreply.github.com>
2016-11-17 15:05:15 +0000

    1479395115 ataxinomic

---
+++ 1479395115.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      775501607
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   int64_t  f1;
+   uint32_t  f2;
+   const volatile uint16_t  f3;
+};
+
+union U1 {
+   signed f0 : 17;
+   volatile int32_t  f1;
