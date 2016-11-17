1539863e7b6ac8c287766f62d3d57c0896c493c7
saisi <saisi@users.noreply.github.com>
2016-11-17 12:35:04 +0000

    1479386104 antilogies

---
+++ 1479386104.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3713951884
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   const int8_t * f1;
+   int8_t  f2;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   const int32_t  f1;
+   const uint32_t  f2;
