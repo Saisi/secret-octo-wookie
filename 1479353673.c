ce866f5517352d4f8efb21bf70dc6b114c52d15a
saisi <saisi@users.noreply.github.com>
2016-11-17 03:34:34 +0000

    1479353673 antimethodic

---
+++ 1479353673.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2890811073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t * f1;
+   int64_t  f2;
+   const volatile uint32_t  f3;
+   uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_19 = 1L;
