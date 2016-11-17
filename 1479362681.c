9811e6304a79dd7974fa902100d9c08d99683439
saisi <saisi@users.noreply.github.com>
2016-11-17 06:04:42 +0000

    1479362681 Anetta

---
+++ 1479362681.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2456052963
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 16;
+   unsigned f1 : 7;
+   const volatile signed f2 : 14;
+   volatile unsigned f3 : 1;
+   unsigned f4 : 14;
+   signed f5 : 15;
+   int16_t  f6;
