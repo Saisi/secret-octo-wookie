66aff0eda1f0a3fd43afc94acfa178c2f13ab7ce
saisi <saisi@users.noreply.github.com>
2016-11-17 19:35:34 +0000

    1479411334 Alleghanian

---
+++ 1479411334.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2786738423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_3 = 0xB0L;/* VOLATILE GLOBAL g_3 */
+static volatile int16_t g_4 = (-2L);/* VOLATILE GLOBAL g_4 */
+static uint16_t g_5 = 65535UL;
+static struct S0 g_8 = {6507};
+
