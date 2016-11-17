5c16c83ec834ac4440aec8fe268e2b9e9da316b2
saisi <saisi@users.noreply.github.com>
2016-11-17 17:35:28 +0000

    1479404127 bacharach

---
+++ 1479404127.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1104470214
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x7E2799ACL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-1L);
+static uint8_t g_88 = 0x45L;
+static uint32_t g_96 = 0xE0E3F56FL;
+static int16_t g_106 = 0x4ECBL;
+static const uint8_t *g_111 = (void*)0;
+static const uint8_t **g_110 = &g_111;
