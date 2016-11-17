3783bfee62ee95ed704dcc67800be21cf1676864
saisi <saisi@users.noreply.github.com>
2016-11-17 04:04:36 +0000

    1479355475 banish

---
+++ 1479355475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2892824446
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int64_t  f0;
+   const uint16_t  f1;
+   int8_t  f2;
+};
+
+union U1 {
+   const int64_t  f0;
+   uint16_t  f1;
+   uint64_t  f2;
