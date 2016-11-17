56704fd71ed3de892de6bf24a8bf7b9f3f639b82
saisi <saisi@users.noreply.github.com>
2016-11-17 23:05:50 +0000

    1479423948 achates

---
+++ 1479423948.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3269796718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 5;
+   unsigned f1 : 6;
+   signed f2 : 16;
+   signed f3 : 28;
+   volatile signed f4 : 27;
+   volatile int32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
