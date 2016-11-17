e2d33462cc9a93309591dac29009f0fbe77de181
saisi <saisi@users.noreply.github.com>
2016-11-17 10:34:57 +0000

    1479378896 allophones

---
+++ 1479378896.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2001116492
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10[10] = {0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L,0xB1658084L};
+static volatile int32_t *g_43 = (void*)0;
+static volatile int32_t ** volatile g_44 = &g_43;/* VOLATILE GLOBAL g_44 */
+static int32_t *g_69[6] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t g_81 = 0xBF624E19L;
+static int32_t ** volatile g_82 = &g_69[5];/* VOLATILE GLOBAL g_82 */
+static int32_t ** volatile g_90 = &g_69[1];/* VOLATILE GLOBAL g_90 */
+static volatile uint8_t g_112 = 0x4AL;/* VOLATILE GLOBAL g_112 */
+static uint32_t g_116 = 4294967294UL;
