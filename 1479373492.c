365880be5cf581eae31abe9555fbabad21666b06
saisi <saisi@users.noreply.github.com>
2016-11-17 09:04:52 +0000

    1479373492 Azalea

---
+++ 1479373492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2071014340
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-2L);
+static struct S0 g_6 = {0x2E6EL,18446744073709551615UL,0x7A9A9718L};/* VOLATILE GLOBAL g_6 */
+
