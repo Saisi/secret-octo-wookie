d2fa4554a6edd6cd66161b977edbf7ab4cd9db71
saisi <saisi@users.noreply.github.com>
2016-11-17 13:35:09 +0000

    1479389709 appendiculate

---
+++ 1479389709.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1248621614
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 5;
+   const int8_t  f1;
+   signed f2 : 14;
+   unsigned f3 : 24;
+};
+#pragma pack(pop)
+
