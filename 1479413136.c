a86a2fad9266c180294f700df1f449cd44e46479
saisi <saisi@users.noreply.github.com>
2016-11-17 20:05:39 +0000

    1479413136 Advent

---
+++ 1479413136.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1482172539
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 28;
+   volatile int32_t  f1;
+   volatile unsigned f2 : 19;
+   signed f3 : 11;
+   volatile unsigned f4 : 6;
+   signed f5 : 28;
+   signed f6 : 15;
