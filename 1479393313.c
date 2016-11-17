028815ab5a34f48b3bfe646176edde94c1c8f3c0
saisi <saisi@users.noreply.github.com>
2016-11-17 14:35:13 +0000

    1479393313 antichreses

---
+++ 1479393313.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1178385751
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[2][9] = {{0x60B0CEAFL,0L,0L,0x60B0CEAFL,(-1L),0x85C6F1BFL,(-1L),0x60B0CEAFL,0L},{(-1L),(-1L),(-6L),0x85C6F1BFL,(-1L),0x85C6F1BFL,(-6L),(-1L),(-1L)}};
+static int32_t g_20 = (-8L);
+static int32_t *g_19 = &g_20;
+static int32_t *g_22[1][10][7] = {{{&g_2[1][0],&g_20,&g_20,&g_2[1][0],&g_20,&g_20,&g_20},{&g_2[1][0],&g_20,&g_20,&g_2[1][0],&g_20,&g_2[1][0],&g_20},{(void*)0,&g_2[1][0],&g_20,&g_20,&g_20,&g_20,&g_2[1][0]},{&g_20,(void*)0,&g_20,&g_20,&g_20,&g_20,&g_20},{&g_20,&g_20,&g_20,&g_20,&g_20,&g_2[1][0],&g_20},{&g_20,&g_20,&g_20,&g_2[1][0],&g_2[1][0],&g_2[1][0],&g_20},{&g_20,&g_20,&g_20,&g_20,&g_20,(void*)0,&g_20},{&g_20,&g_2[1][0],&g_20,&g_20,&g_2[1][0],&g_20,&g_2[1][0]},{&g_20,&g_20,&g_20,&g_2[1][0],&g_20,&g_20,&g_2[1][0]},{&g_20,&g_2[1][0],&g_20,(void*)0,&g_2[1][0],&g_20,&g_20}}};
