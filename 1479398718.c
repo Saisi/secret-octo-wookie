0fe5d0611e5db541c792310ca12cabb1837d273e
saisi <saisi@users.noreply.github.com>
2016-11-17 16:05:19 +0000

    1479398718 anorthoclase

---
+++ 1479398718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4018409174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 25;
+   const signed f1 : 10;
+   signed f2 : 4;
+   signed : 0;
+   volatile signed f3 : 15;
+   signed f4 : 10;
+   unsigned : 0;
