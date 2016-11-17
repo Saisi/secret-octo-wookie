7c1bf8c05f0f38b15e2f85eba0314dba394b510c
saisi <saisi@users.noreply.github.com>
2016-11-17 15:35:17 +0000

    1479396917 archdeaconries

---
+++ 1479396917.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      99670311
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint16_t g_15 = 65528UL;
+static int32_t g_43 = 0xD7777BABL;
+static int32_t *g_42 = &g_43;
+static int32_t ** volatile g_47 = &g_42;/* VOLATILE GLOBAL g_47 */
