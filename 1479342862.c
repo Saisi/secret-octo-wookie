d01a662e2a0b7555844d83eedf187193f041f1de
saisi <saisi@users.noreply.github.com>
2016-11-17 00:34:23 +0000

    1479342862 amli

---
+++ 1479342862.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4185670040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+   int8_t  f2;
+   const int32_t  f3;
+   int32_t  f4;
+};
+#pragma pack(pop)
