28671edfb62f5e5a2130b249bd8167409c57f8a3
saisi <saisi@users.noreply.github.com>
2016-11-17 11:34:59 +0000

    1479382499 apex

---
+++ 1479382499.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3104359136
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 30;
+   const signed f1 : 27;
+   volatile int8_t  f2;
+   signed f3 : 9;
+   int32_t  f4;
+   uint8_t  f5;
+   uint16_t  f6;
+   volatile int32_t  f7;
+   uint32_t  f8;
