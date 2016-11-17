c7628eca6f8942d238c4207044d0b648d8dd4ccf
saisi <saisi@users.noreply.github.com>
2016-11-17 00:04:21 +0000

    1479341061 barbule

---
+++ 1479341061.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1867547316
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const uint64_t  f0;
+   const int32_t  f1;
+   const uint16_t  f2;
+   signed f3 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x34BF8F0AL;
+
