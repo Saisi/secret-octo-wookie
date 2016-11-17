6430d0bd5ef04ddfd2005a0401e51aa24abb1606
saisi <saisi@users.noreply.github.com>
2016-11-17 05:04:39 +0000

    1479359078 albuminous

---
+++ 1479359078.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1077985761
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const uint64_t  f1;
+   const uint32_t  f2;
+   uint32_t  f3;
+   volatile uint16_t  f4;
+   uint16_t  f5;
+   int64_t  f6;
+   volatile uint16_t  f7;
+};
