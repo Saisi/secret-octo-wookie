d6349ed3516aed867777bf0b2d05c9615e5815bc
saisi <saisi@users.noreply.github.com>
2016-11-17 02:34:30 +0000

    1479350069 Artair

---
+++ 1479350069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3705235579
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   volatile int64_t  f1;
+   signed f2 : 3;
+   const uint32_t  f3;
+   volatile int32_t  f4;
+};
+#pragma pack(pop)
