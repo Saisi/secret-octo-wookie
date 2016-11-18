02c2aa5b741a02a773dfa62b4cb8f05666c26a76
saisi <saisi@users.noreply.github.com>
2016-11-18 10:36:36 +0000

    1479465395 apogeny

---
+++ 1479465395.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1576728981
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 9;
+   unsigned f1 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x730F94F3L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x1E42A43DL;
+static int32_t g_7 = (-1L);
+static int32_t g_29 = 0L;
