7e141251f97f9e5bfccde1edfa4e5d33a9199eb8
saisi <saisi@users.noreply.github.com>
2016-11-18 19:37:09 +0000

    1479497828 Bankston

---
+++ 1479497828.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3160568586
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 28;
+   signed f2 : 14;
+   signed f3 : 2;
+   signed f4 : 9;
+   unsigned f5 : 28;
+   volatile unsigned f6 : 10;
+   unsigned f7 : 23;
+   const unsigned f8 : 31;
