32ca95366e054c5a1e9ea834f1a6932ddad3ab3c
saisi <saisi@users.noreply.github.com>
2016-11-18 15:06:51 +0000

    1479481610 alkanna

---
+++ 1479481610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      701237132
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
+union U2 {
+   volatile uint64_t  f0;
