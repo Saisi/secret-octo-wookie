ba73403e346a4b278d46a5d769c9b75b44f5cda4
saisi <saisi@users.noreply.github.com>
2016-11-18 00:35:55 +0000

    1479429355 adductor

---
+++ 1479429355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      833710560
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 30;
+   const volatile unsigned f1 : 27;
+   unsigned f2 : 17;
+};
+#pragma pack(pop)
+
+struct S1 {
