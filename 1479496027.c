4a8a148c69cc34b2264ab0da5d40fb62accbf50c
saisi <saisi@users.noreply.github.com>
2016-11-18 19:07:07 +0000

    1479496027 answerlessly

---
+++ 1479496027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      47472997
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[9] = {0x43C4AB27L,(-1L),0x43C4AB27L,0x43C4AB27L,(-1L),0x43C4AB27L,0x43C4AB27L,(-1L),0x43C4AB27L};
+static volatile int32_t * volatile g_2[8][10] = {{(void*)0,(void*)0,&g_3[3],&g_3[0],&g_3[0],&g_3[0],&g_3[3],(void*)0,(void*)0,&g_3[8]},{&g_3[4],&g_3[4],&g_3[4],&g_3[0],(void*)0,&g_3[4],&g_3[0],(void*)0,&g_3[0],&g_3[4]},{&g_3[4],&g_3[0],(void*)0,&g_3[0],(void*)0,&g_3[4],&g_3[4],&g_3[4],(void*)0,&g_3[2]},{&g_3[8],(void*)0,&g_3[4],&g_3[0],&g_3[4],(void*)0,(void*)0,(void*)0,&g_3[0],&g_3[8]},{(void*)0,(void*)0,&g_3[4],&g_3[0],&g_3[4],(void*)0,&g_3[8],&g_3[3],&g_3[4],&g_3[0]},{&g_3[0],(void*)0,&g_3[8],&g_3[3],&g_3[4],&g_3[8],&g_3[8],&g_3[4],&g_3[8],&g_3[4]},{&g_3[5],(void*)0,(void*)0,&g_3[5],&g_3[0],&g_3[3],&g_3[4],&g_3[8],&g_3[4],&g_3[3]},{(void*)0,(void*)0,&g_3[2],(void*)0,(void*)0,&g_3[4],(void*)0,&g_3[3],(void*)0,(void*)0}};
+static volatile int32_t * volatile * volatile g_4[2][6] = {{&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0]},{&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0],&g_2[4][0]}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
