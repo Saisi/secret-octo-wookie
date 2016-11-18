741f764c3c23c52bb84dae2d2747aedb94ee0a26
saisi <saisi@users.noreply.github.com>
2016-11-18 17:06:57 +0000

    1479488816 augmenter

---
+++ 1479488816.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      118059412
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   uint8_t  f1;
+   uint32_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_15 = 1UL;
+static int64_t g_26[9][6][1] = {{{(-7L)},{6L},{8L},{(-7L)},{8L},{6L}},{{(-7L)},{1L},{1L},{(-7L)},{6L},{8L}},{{(-7L)},{8L},{6L},{(-7L)},{1L},{1L}},{{(-7L)},{6L},{8L},{(-7L)},{8L},{6L}},{{(-7L)},{1L},{1L},{(-7L)},{6L},{8L}},{{(-7L)},{8L},{6L},{(-7L)},{1L},{1L}},{{(-7L)},{6L},{8L},{(-7L)},{8L},{6L}},{{(-7L)},{1L},{1L},{(-7L)},{(-7L)},{(-9L)}},{{(-9L)},{(-9L)},{(-7L)},{(-9L)},{0xD7494DFD99643747LL},{0xD7494DFD99643747LL}}};
