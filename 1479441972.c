decd3c025e6b20282506814f74c1f6bcc316c18e
saisi <saisi@users.noreply.github.com>
2016-11-18 04:06:12 +0000

    1479441972 azotising

---
+++ 1479441972.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3060737013
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   int32_t  f1;
+   const volatile uint64_t  f2;
+   const signed f3 : 23;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xE3580D41L;
