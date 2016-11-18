1ec93064c09a0e293f5ede342256a5899fb082a9
saisi <saisi@users.noreply.github.com>
2016-11-18 18:37:05 +0000

    1479494225 amazed

---
+++ 1479494225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      637707864
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_23[3][4] = {{65527UL,0xCC80L,65527UL,65527UL},{0xCC80L,0xCC80L,65531UL,0xCC80L},{0xCC80L,65527UL,65527UL,0xCC80L}};
+static int32_t g_30 = 0xB0BEFCDFL;
+static uint16_t g_38 = 0UL;
+static volatile union U0 g_40 = {-4L};/* VOLATILE GLOBAL g_40 */
+static uint32_t g_60[7][2] = {{0xFC71010FL,0xA7E8B7DDL},{0xA7E8B7DDL,0xFC71010FL},{0xA7E8B7DDL,0xA7E8B7DDL},{0xFC71010FL,0xA7E8B7DDL},{0xA7E8B7DDL,0xFC71010FL},{0xA7E8B7DDL,0xA7E8B7DDL},{0xFC71010FL,0xA7E8B7DDL}};
