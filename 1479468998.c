678a28dd8c0fb218c9c65aca9c6545134dc2ec18
saisi <saisi@users.noreply.github.com>
2016-11-18 11:36:39 +0000

    1479468998 antiprostitution

---
+++ 1479468998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2950807692
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 20;
+};
+
+struct S1 {
+   unsigned f0 : 17;
+   unsigned f1 : 15;
+   signed f2 : 24;
+   unsigned f3 : 4;
+   volatile signed f4 : 21;
