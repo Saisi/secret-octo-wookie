8ef1d6de391e67f9a105c1c24ff58e684942ed25
saisi <saisi@users.noreply.github.com>
2016-11-18 18:07:03 +0000

    1479492423 archegonial

---
+++ 1479492423.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1485467666
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   volatile signed f1 : 16;
+   signed f2 : 18;
+   uint64_t  f3;
+   signed f4 : 23;
+   volatile signed f5 : 7;
+   volatile unsigned f6 : 12;
+   const volatile unsigned f7 : 21;
+   unsigned f8 : 18;
