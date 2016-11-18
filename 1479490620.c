75badee4f1a2747770cf1d65eb4e8938f3f40733
saisi <saisi@users.noreply.github.com>
2016-11-18 17:37:01 +0000

    1479490620 Baalitical

---
+++ 1479490620.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4208144972
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint8_t  f0;
+   volatile int8_t  f1;
+   int16_t  f2;
+   int64_t  f3;
+   int32_t  f4;
+   int8_t  f5;
+   uint8_t  f6;
