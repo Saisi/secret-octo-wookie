8edac6ded87a566557cd4f8f282e9e5cca6e1e6f
saisi <saisi@users.noreply.github.com>
2016-11-18 03:36:11 +0000

    1479440171 aflat

---
+++ 1479440171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      568820317
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 13;
+   const unsigned f1 : 16;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   int64_t  f1;
+   signed f2 : 13;
+   int16_t  f3;
