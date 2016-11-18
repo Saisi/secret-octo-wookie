610d74e8a8b9a978849f1841db20b562ce57a254
saisi <saisi@users.noreply.github.com>
2016-11-18 11:06:37 +0000

    1479467197 adultoid

---
+++ 1479467197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      422505649
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 23;
+   unsigned f1 : 18;
+   signed f2 : 13;
+   signed f3 : 4;
+   volatile unsigned f4 : 5;
+   unsigned f5 : 10;
+   unsigned f6 : 1;
+   unsigned f7 : 16;
+   unsigned f8 : 19;
