a3c30258b75c2439ea31c26f1759b6911a218ea5
saisi <saisi@users.noreply.github.com>
2016-11-18 16:36:55 +0000

    1479487015 arterioverter

---
+++ 1479487015.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1913124006
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   const uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4[6] = {0x14D2EDC7L,0x14D2EDC7L,0x14D2EDC7L,0x14D2EDC7L,0x14D2EDC7L,0x14D2EDC7L};
+static uint32_t g_5 = 4294967286UL;
+static const union U0 g_6 = {0xF0B101AAE4B9A733LL};
+static int16_t g_8[4][2][5] = {{{0L,0L,(-2L),0L,0L},{0x641BL,8L,0x12AAL,8L,0x641BL}},{{0L,0L,(-2L),0L,0L},{0x641BL,8L,0x12AAL,8L,0x641BL}},{{0L,0L,(-2L),0L,0L},{0x641BL,8L,0x12AAL,8L,0x641BL}},{{0L,0L,(-2L),0L,0L},{0x641BL,8L,0x12AAL,8L,0x641BL}}};
