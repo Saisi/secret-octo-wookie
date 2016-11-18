b29cdf2726b253827889a43559219c6f1da71951
saisi <saisi@users.noreply.github.com>
2016-11-18 23:07:25 +0000

    1479510445 amphictyon

---
+++ 1479510445.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3045745083
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 28;
+   signed f1 : 3;
+   signed f2 : 26;
+   signed f3 : 29;
+   uint16_t  f4;
+   volatile int32_t  f5;
+   signed f6 : 26;
