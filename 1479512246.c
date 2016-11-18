00353a479688c5b8a77a1621eb6ffdfbc8a05f76
saisi <saisi@users.noreply.github.com>
2016-11-18 23:37:27 +0000

    1479512246 bagwoman

---
+++ 1479512246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2032290148
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11[5] = {0x4DDBC370L,0x4DDBC370L,0x4DDBC370L,0x4DDBC370L,0x4DDBC370L};
+static uint8_t g_22 = 0x27L;
+static volatile uint8_t * volatile g_34 = (void*)0;/* VOLATILE GLOBAL g_34 */
+static int32_t g_43 = (-4L);
+static uint16_t g_51[10] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL};
+static int32_t g_60 = 0xA737B78FL;
+static uint32_t g_75 = 0UL;
+static uint16_t g_77[4][3] = {{65527UL,0UL,65527UL},{65527UL,0UL,65527UL},{65527UL,0UL,65527UL},{65527UL,0UL,65527UL}};
+static uint16_t *g_76 = &g_77[3][0];
