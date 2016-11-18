32d01b29c04ed005ff5760b0e45599c5eb5ee9ad
saisi <saisi@users.noreply.github.com>
2016-11-18 14:36:49 +0000

    1479479808 arsenillo

---
+++ 1479479808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1815592168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int32_t  f1;
+};
+
+union U1 {
+   uint8_t  f0;
+   volatile unsigned f1 : 30;
+   const int8_t  f2;
+};
