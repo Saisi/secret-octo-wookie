efa701001d91d1161b70d67bad44d7ed2736206d
saisi <saisi@users.noreply.github.com>
2016-11-18 16:06:54 +0000

    1479485213 antiempiricism

---
+++ 1479485213.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3137209448
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xF0EA75C9L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x1E1E87DFL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-10L);
+static volatile int32_t *g_8[10] = {&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3};
+static volatile int32_t g_9 = (-9L);/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x4B13281DL;
+static int32_t g_69 = 0x9CDE89C6L;
+static int32_t **g_75 = (void*)0;
+static uint8_t g_80 = 0xC8L;
