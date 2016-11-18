5a84c784479287fc88aa316e01e6d06ee3303d53
saisi <saisi@users.noreply.github.com>
2016-11-18 09:06:30 +0000

    1479459987 absorptiometric

---
+++ 1479459987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2316982387
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   int64_t  f1;
+   const volatile int16_t  f2;
+   int8_t  f3;
+   uint8_t  f4;
+   volatile int32_t  f5;
+   int32_t  f6;
+};
+
