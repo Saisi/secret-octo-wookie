703a5b752496d2430ecaacb41028da77be7926ab
saisi <saisi@users.noreply.github.com>
2016-11-19 23:38:50 +0000

    1479598729 apotheosising

---
+++ 1479598729.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2794989152
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 30;
+   volatile unsigned f1 : 27;
+   volatile unsigned f2 : 17;
+   signed f3 : 25;
+   signed f4 : 4;
+};
+#pragma pack(pop)
