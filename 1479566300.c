fbbb8ad230ed3ac1940468eb71fc44d4d5cd0aff
saisi <saisi@users.noreply.github.com>
2016-11-19 14:38:21 +0000

    1479566300 Barajas

---
+++ 1479566300.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1343983074
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x76F5020FL;
+static uint8_t g_15 = 0UL;
+static volatile int32_t g_50 = (-2L);/* VOLATILE GLOBAL g_50 */
+static int32_t g_51 = (-6L);
+static volatile int32_t *g_54 = &g_50;
+static volatile int32_t ** volatile g_53 = &g_54;/* VOLATILE GLOBAL g_53 */
+static volatile int32_t g_55 = 0xE18BD2E8L;/* VOLATILE GLOBAL g_55 */
+static volatile int32_t g_56 = (-7L);/* VOLATILE GLOBAL g_56 */
+static int32_t g_57 = (-10L);
