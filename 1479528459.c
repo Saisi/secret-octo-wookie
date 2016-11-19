b1666e70870636033a39e7daeda14ed420413411
saisi <saisi@users.noreply.github.com>
2016-11-19 04:07:39 +0000

    1479528459 Aquasco

---
+++ 1479528459.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      298659775
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 30;
+   volatile unsigned f1 : 29;
+   const signed f2 : 12;
+   signed f3 : 7;
+   const signed f4 : 28;
+   unsigned f5 : 5;
+   signed f6 : 16;
