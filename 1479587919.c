157059c39d73bd357615c203e9a48c0d829f8e73
saisi <saisi@users.noreply.github.com>
2016-11-19 20:38:40 +0000

    1479587919 Atman

---
+++ 1479587919.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4264252129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 15;
+   volatile signed f1 : 10;
+   uint32_t  f2;
+   int16_t  f3;
+};
+
+union U1 {
+   int8_t * f0;
+   volatile int32_t  f1;
