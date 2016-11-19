2a75171692a3ea8ce232ee1bef8a30f4e56846d2
saisi <saisi@users.noreply.github.com>
2016-11-19 03:37:38 +0000

    1479526658 Aretino

---
+++ 1479526658.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1980966771
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 24;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[10] = {0x96A485BFL,(-1L),6L,6L,(-1L),0x96A485BFL,(-1L),6L,6L,(-1L)};
+static volatile int32_t g_16 = 0L;/* VOLATILE GLOBAL g_16 */
