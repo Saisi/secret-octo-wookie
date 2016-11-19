509e2d8621a170be1102b4ebb715b69407b4e5cc
saisi <saisi@users.noreply.github.com>
2016-11-19 02:37:35 +0000

    1479523055 Attacus

---
+++ 1479523055.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1858833198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+};
+
+union U1 {
+   int64_t  f0;
+   int8_t  f1;
+   volatile int64_t  f2;
+   int32_t  f3;
+   int32_t  f4;
