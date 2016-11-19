57bf53fd0f0d5485afbb6f8b3e0f57b48e5d0158
saisi <saisi@users.noreply.github.com>
2016-11-19 04:37:41 +0000

    1479530260 acupunctured

---
+++ 1479530260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2769619176
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 2;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_45[4] = {0x709E149CL,0x709E149CL,0x709E149CL,0x709E149CL};
+static int32_t g_47 = 9L;
