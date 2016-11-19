6991c6aa50a1513e6ea9df5bae134a1ce65056ae
saisi <saisi@users.noreply.github.com>
2016-11-19 10:38:07 +0000

    1479551887 aerodynamicist

---
+++ 1479551887.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      675276989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x837BC752L;
+static int8_t g_16 = 3L;
+static volatile int16_t g_17 = 0x3C1DL;/* VOLATILE GLOBAL g_17 */
+static int16_t g_18[3][1] = {{0x77EBL},{0x77EBL},{0x77EBL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
