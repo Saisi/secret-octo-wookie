814ae0ff8eea5275636ab9e0871ce2c8960f0583
saisi <saisi@users.noreply.github.com>
2016-11-19 16:38:27 +0000

    1479573506 axiologies

---
+++ 1479573506.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      548053706
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 19;
+   volatile unsigned f1 : 28;
+   const signed f2 : 14;
+   signed f3 : 2;
+};
+#pragma pack(pop)
+
