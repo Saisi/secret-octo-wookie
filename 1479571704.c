06047d00338c5debcfece5018d744f3015e2eb24
saisi <saisi@users.noreply.github.com>
2016-11-19 16:08:25 +0000

    1479571704 bails

---
+++ 1479571704.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      958012120
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 26;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   int8_t * f1;
+   const uint8_t  f2;
+};
+
