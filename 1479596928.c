4922bfc6a2b31ea9557da4756942285563772850
saisi <saisi@users.noreply.github.com>
2016-11-19 23:08:48 +0000

    1479596928 avaremotemo

---
+++ 1479596928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4096831933
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 29;
+   signed : 0;
+   signed f1 : 3;
+   signed f2 : 2;
+   volatile signed f3 : 11;
+   unsigned f4 : 24;
+};
