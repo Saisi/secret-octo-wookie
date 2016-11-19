709333d31063bf41b6de7c662ded717011c56953
saisi <saisi@users.noreply.github.com>
2016-11-19 10:08:05 +0000

    1479550085 aggrievement

---
+++ 1479550085.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1334507785
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static int32_t *g_2 = &g_3;
+static int32_t ** volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+static const int32_t g_45 = 1L;
+static const int32_t *g_44 = &g_45;
+static int32_t g_69[9] = {0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL,0x7762969FL};
+static uint16_t g_70 = 0xFCA4L;
+static uint32_t g_106 = 0UL;
+static volatile int32_t g_107 = (-1L);/* VOLATILE GLOBAL g_107 */
