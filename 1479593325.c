566862fa7e166ad80b156a7499aed7f1b8e84de3
saisi <saisi@users.noreply.github.com>
2016-11-19 22:08:45 +0000

    1479593325 balls

---
+++ 1479593325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2080074790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 1;
+   int64_t  f1;
+   unsigned f2 : 26;
+   volatile signed f3 : 20;
+   unsigned f4 : 9;
+   signed f5 : 17;
+   volatile unsigned f6 : 14;
+   unsigned f7 : 15;
+   const volatile signed f8 : 4;
