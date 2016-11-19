4412ab7f24ba184adf5b22dbc93a288fbcec39fe
saisi <saisi@users.noreply.github.com>
2016-11-19 18:08:32 +0000

    1479578912 antinomist

---
+++ 1479578912.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2662030047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile int32_t  f1;
+   volatile int16_t  f2;
+   volatile signed f3 : 28;
+   volatile int8_t  f4;
+   const volatile unsigned f5 : 4;
+   const int32_t  f6;
+   uint16_t  f7;
+   int8_t  f8;
