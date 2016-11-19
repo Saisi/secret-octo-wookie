802e6caafaca4df3c2d87809d5843d77cac36d20
saisi <saisi@users.noreply.github.com>
2016-11-19 06:07:46 +0000

    1479535666 adventure

---
+++ 1479535666.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      240835479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   volatile unsigned f0 : 5;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x69393DDAL;
+static volatile int64_t g_4 = 0x48CC3837ACED45B0LL;/* VOLATILE GLOBAL g_4 */
+static uint8_t g_5 = 0x68L;
+static volatile int32_t g_8 = 0xBD4AA41AL;/* VOLATILE GLOBAL g_8 */
+static int16_t g_11[3][6] = {{0L,(-1L),(-1L),0L,0x319BL,1L},{1L,0L,0xD925L,0L,1L,0x3C09L},{0L,1L,0x3C09L,0x3C09L,1L,0L}};
