6a297ae5033cfc7f028d0968a2d31a879ad86f39
saisi <saisi@users.noreply.github.com>
2016-11-20 05:09:12 +0000

    1479618550 awakenable

---
+++ 1479618550.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      668949755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 28;
+   unsigned f1 : 3;
+   volatile unsigned f2 : 26;
+   volatile unsigned f3 : 29;
+   unsigned f4 : 26;
+   const unsigned f5 : 11;
+   signed f6 : 30;
+};
+
