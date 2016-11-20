5aea892778020e50490f3499b8005584f8f386d2
saisi <saisi@users.noreply.github.com>
2016-11-20 21:40:12 +0000

    1479678010 allodelphite

---
+++ 1479678010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1044067456
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 14;
+   signed f1 : 11;
+   volatile signed f2 : 1;
+   volatile signed f3 : 9;
+   uint16_t  f4;
+   unsigned f5 : 30;
+   volatile unsigned f6 : 7;
+};
+
