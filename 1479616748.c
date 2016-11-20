76ce5fcacb46dc6546cbdd07c8c8d59ef74eca15
saisi <saisi@users.noreply.github.com>
2016-11-20 04:39:09 +0000

    1479616748 Apolista

---
+++ 1479616748.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1982843305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_4 = 0xF47AL;/* VOLATILE GLOBAL g_4 */
