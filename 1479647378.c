d2fc10248757dc29582d5c73562aa2d6d2e7cdb5
saisi <saisi@users.noreply.github.com>
2016-11-20 13:09:39 +0000

    1479647378 bards

---
+++ 1479647378.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1120804092
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   int64_t  f1;
+   const int8_t  f2;
+   const int32_t  f3;
+   uint16_t  f4;
+   const int16_t  f5;
+   volatile uint8_t  f6;
