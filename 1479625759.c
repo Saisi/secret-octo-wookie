ec230fd8d0407b161f8afccbb093be2a70cfe764
saisi <saisi@users.noreply.github.com>
2016-11-20 07:09:19 +0000

    1479625759 adenoncus

---
+++ 1479625759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1894178121
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 3;
+   signed f1 : 14;
+   signed f2 : 25;
+   unsigned : 0;
+   volatile signed f3 : 17;
+   unsigned f4 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
