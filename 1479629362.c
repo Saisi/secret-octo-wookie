f8c02c43083053c12c7b56308c51569adeaa4038
saisi <saisi@users.noreply.github.com>
2016-11-20 08:09:22 +0000

    1479629362 adagy

---
+++ 1479629362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      29774462
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 13;
+   const volatile signed f1 : 14;
+   unsigned f2 : 24;
+   volatile signed f3 : 4;
+   volatile unsigned f4 : 3;
+   signed f5 : 14;
+};
+
+union U1 {
