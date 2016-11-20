0ed1178c8597317078d25b500fde5aab81cc7dab
saisi <saisi@users.noreply.github.com>
2016-11-20 01:38:58 +0000

    1479605937 babyishness

---
+++ 1479605937.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1273868592
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+   uint32_t  f1;
+   volatile int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_6 = (-9L);/* VOLATILE GLOBAL g_6 */
+static const int16_t g_28 = 0xDA72L;
+static int32_t g_47 = 0x64071C19L;
