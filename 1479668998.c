f289e8fc41b970d1b129cbfdce95935f37319b81
saisi <saisi@users.noreply.github.com>
2016-11-20 19:09:59 +0000

    1479668998 anabolism

---
+++ 1479668998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1907236053
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   uint16_t  f1;
+   int32_t  f2;
+   int16_t  f3;
+   volatile uint16_t  f4;
+   const int8_t  f5;
+   signed f6 : 20;
