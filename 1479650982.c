52c316430aa309f9f9ed44fdbb51d8222de32683
saisi <saisi@users.noreply.github.com>
2016-11-20 14:09:43 +0000

    1479650982 Aegina

---
+++ 1479650982.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      815850005
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 9;
+   volatile int32_t  f1;
+};
+#pragma pack(pop)
+
+struct S1 {
+   signed f0 : 18;
