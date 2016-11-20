88b62ebf5365983ea0c68c69ed2b8539d0fbe473
saisi <saisi@users.noreply.github.com>
2016-11-20 11:39:34 +0000

    1479641974 amphiblestritis

---
+++ 1479641974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      945093974
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   volatile int8_t  f0;
+   const uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xFA72C25FL;
+static union U3 g_5 = {4L};/* VOLATILE GLOBAL g_5 */
+static union U3 g_6[6][3] = {{{0L},{0L},{0L}},{{0L},{0L},{0L}},{{0L},{0L},{0L}},{{0L},{0L},{0L}},{{0L},{0L},{0L}},{{0L},{0L},{0L}}};
+
