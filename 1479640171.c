d39e1ece8d5845a91c3a0ee2c793e4bdfe7e33de
saisi <saisi@users.noreply.github.com>
2016-11-20 11:09:32 +0000

    1479640171 angiosclerotic

---
+++ 1479640171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      456168271
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint16_t  f0;
+   const signed f1 : 11;
+   int32_t  f2;
+   uint16_t  f3;
+   uint8_t  f4;
+};
+#pragma pack(pop)
