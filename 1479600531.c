bc43c7305a0261301e0b066c05c789d192d8fb61
saisi <saisi@users.noreply.github.com>
2016-11-20 00:08:51 +0000

    1479600531 armaturing

---
+++ 1479600531.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1398819966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile signed f1 : 10;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 21;
+   signed f1 : 20;
