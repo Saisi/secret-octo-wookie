89b80419c5f64629ba20fee3bd1142bcf3af2b7b
saisi <saisi@users.noreply.github.com>
2016-11-20 08:39:24 +0000

    1479631164 aromatics

---
+++ 1479631164.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2614169872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 22;
+   uint16_t  f1;
+   uint32_t  f2;
+   const volatile uint8_t  f3;
+   const int16_t  f4;
+};
+#pragma pack(pop)
