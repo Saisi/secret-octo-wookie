5f81d791676a70ff3a80a3afaa67f04095b04b19
saisi <saisi@users.noreply.github.com>
2016-11-20 16:09:48 +0000

    1479658188 acarine

---
+++ 1479658188.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2172319442
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   signed f1 : 16;
+   unsigned f2 : 18;
+   unsigned f3 : 22;
+   const int64_t  f4;
+   unsigned f5 : 7;
+   signed f6 : 12;
