b2c2f8fdf9e3fb98cd342679fbccc62db693a6fb
saisi <saisi@users.noreply.github.com>
2016-11-20 23:40:19 +0000

    1479685219 aphacia

---
+++ 1479685219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1149030428
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_10 = 1L;
+static int32_t g_35 = (-4L);
+static const int32_t *g_34 = &g_35;
+static int16_t g_45 = 0xA237L;
+static int8_t g_73 = 0x59L;
+static uint32_t g_75[1] = {9UL};
+static uint8_t g_77 = 0x3BL;
+static int32_t g_80 = 1L;
+static const int16_t g_96 = 0x75E7L;
