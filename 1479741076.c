7f2a50802cad5cfde352e365c632c0f2d3fb10a4
saisi <saisi@users.noreply.github.com>
2016-11-21 15:11:18 +0000

    1479741076 apoplectoid

---
+++ 1479741076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      757030843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t  f1;
+   signed f2 : 26;
+   volatile unsigned f3 : 29;
+};
+
+union U1 {
+   volatile unsigned f0 : 11;
+   volatile uint64_t  f1;
