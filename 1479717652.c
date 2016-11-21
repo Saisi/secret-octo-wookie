86a75f537603c05467e0a83ec54a4ef6defcef63
saisi <saisi@users.noreply.github.com>
2016-11-21 08:40:54 +0000

    1479717652 angulates

---
+++ 1479717652.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2573735969
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 15;
+   volatile signed f1 : 10;
+   signed f2 : 5;
+   const unsigned f3 : 28;
+   signed f4 : 29;
+   signed f5 : 2;
+   volatile signed f6 : 25;
+   volatile unsigned f7 : 8;
+};
