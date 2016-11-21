273ffe205b8098877b79bd3ee4e00c3da3f7dd0c
saisi <saisi@users.noreply.github.com>
2016-11-21 09:10:56 +0000

    1479719455 Aeroflot

---
+++ 1479719455.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2168870288
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static volatile uint32_t g_16 = 0x18E9934BL;/* VOLATILE GLOBAL g_16 */
+static uint16_t g_19 = 0UL;
+static uint16_t g_62 = 0UL;
+static int8_t g_63 = 0x00L;
+static int32_t *g_66 = &g_4;
+static int32_t * volatile *g_65 = &g_66;
+static int64_t g_73 = (-2L);
+static int16_t g_90 = (-1L);
