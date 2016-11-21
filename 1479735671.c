2007f1e61849f679e7a4e82442f4e11a68dddda4
saisi <saisi@users.noreply.github.com>
2016-11-21 13:41:11 +0000

    1479735671 antiresonator

---
+++ 1479735671.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2167484587
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   volatile unsigned f1 : 27;
+   const unsigned f2 : 18;
+   signed f3 : 21;
+   volatile unsigned f4 : 18;
+   const volatile signed f5 : 3;
+   signed f6 : 22;
+   unsigned f7 : 17;
+   signed f8 : 16;
