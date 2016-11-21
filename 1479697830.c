90d21490fdac2d66896696f2e5188bd822561589
saisi <saisi@users.noreply.github.com>
2016-11-21 03:10:30 +0000

    1479697830 antitechnological

---
+++ 1479697830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1013573585
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   signed f1 : 2;
+   volatile unsigned f2 : 29;
+   volatile unsigned f3 : 20;
+   volatile signed f4 : 21;
+   signed f5 : 26;
+   uint64_t  f6;
+   volatile uint16_t  f7;
+   signed f8 : 27;
