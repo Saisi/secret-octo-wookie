dc14eecd899ae6fe5623cf9411ed28e191bc4507
saisi <saisi@users.noreply.github.com>
2016-11-21 02:40:29 +0000

    1479696029 arthroclasia

---
+++ 1479696029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2998478731
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB2C21353L;
+static uint16_t g_4 = 0x43ACL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
