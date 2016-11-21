bd0a18dd048e71fd8d6247e279a402923bfdaada
saisi <saisi@users.noreply.github.com>
2016-11-21 03:40:32 +0000

    1479699631 Amomales

---
+++ 1479699631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3963585067
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   uint64_t  f0;
+   const int8_t * volatile  f1;
+   volatile uint32_t  f2;
+   uint64_t  f3;
