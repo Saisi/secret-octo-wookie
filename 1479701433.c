d72c4cf5221bbfdeb790c36595779a80b049ef21
saisi <saisi@users.noreply.github.com>
2016-11-21 04:10:34 +0000

    1479701433 admarginate

---
+++ 1479701433.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3108807425
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   volatile int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0x91F782A1L;
+static uint16_t g_18 = 0UL;
+static uint16_t g_65 = 3UL;
+static int32_t g_71[10][6] = {{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,1L,0L},{0L,1L,0L,0L,0L,5L},{5L,0L,5L,5L,0L,5L},{5L,0L,5L,5L,0L,5L}};
