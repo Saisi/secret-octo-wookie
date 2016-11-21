24b4794b508fc66931dfe68fedba17efb52273d3
saisi <saisi@users.noreply.github.com>
2016-11-21 19:41:35 +0000

    1479757295 anaesthyl

---
+++ 1479757295.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4146352427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint32_t  f0;
+   const volatile uint64_t  f1;
+   volatile uint16_t  f2;
+   const volatile int8_t  f3;
+   int64_t  f4;
+};
+#pragma pack(pop)
