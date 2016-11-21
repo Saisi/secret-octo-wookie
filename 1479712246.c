f15c411a8c543a05d7a0deb2cf2a1028d45fa660
saisi <saisi@users.noreply.github.com>
2016-11-21 07:10:47 +0000

    1479712246 atlantomastoid

---
+++ 1479712246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3321828845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   const int32_t  f1;
+   signed f2 : 19;
+};
+
+union U1 {
+   uint8_t  f0;
+};
+
