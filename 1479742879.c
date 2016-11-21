b5a891f6c4115af6b2244045c788ac572b059552
saisi <saisi@users.noreply.github.com>
2016-11-21 15:41:20 +0000

    1479742879 backhanding

---
+++ 1479742879.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3035100506
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x3C692BEAL;
+static int32_t g_7[4] = {2L,2L,2L,2L};
+static int32_t *g_13 = &g_7[1];
+static int8_t g_21 = 0x26L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
