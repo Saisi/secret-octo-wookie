8ddb25958d93be7f4a0bc395b80b6ba72cdee736
saisi <saisi@users.noreply.github.com>
2016-11-21 06:10:43 +0000

    1479708643 acuesthesia

---
+++ 1479708643.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2679237440
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xBCA63185L;
+static uint16_t g_9 = 65535UL;
+static int32_t g_64 = (-9L);
+static uint64_t g_77 = 0xECEB2CFB11589A45LL;
+static volatile int32_t g_92 = 1L;/* VOLATILE GLOBAL g_92 */
+static volatile int32_t *g_91 = &g_92;
+static volatile int32_t ** volatile g_90 = &g_91;/* VOLATILE GLOBAL g_90 */
+static volatile int32_t ** const  volatile *g_89 = &g_90;
+static volatile int64_t g_113[5] = {0xA1EA5693CDB7CFF1LL,0xA1EA5693CDB7CFF1LL,0xA1EA5693CDB7CFF1LL,0xA1EA5693CDB7CFF1LL,0xA1EA5693CDB7CFF1LL};
