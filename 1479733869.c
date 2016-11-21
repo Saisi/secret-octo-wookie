bc85ebfd3b51e4bac3e700776927cf3ae4e13797
saisi <saisi@users.noreply.github.com>
2016-11-21 13:11:10 +0000

    1479733869 Anosia

---
+++ 1479733869.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2833997136
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+   unsigned f1 : 19;
+   const unsigned f2 : 9;
+   unsigned f3 : 17;
+   unsigned f4 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x6FF23B1AL;/* VOLATILE GLOBAL g_2 */
