7eb7e776528da45e509425c3acdbbf110864a5ce
saisi <saisi@users.noreply.github.com>
2016-11-21 04:40:36 +0000

    1479703235 auride

---
+++ 1479703235.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3821342574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3] = {0x9B2CFB63L,0x9B2CFB63L,0x9B2CFB63L};
+static volatile int32_t g_3 = 0x210B999FL;/* VOLATILE GLOBAL g_3 */
