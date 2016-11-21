c65f5dcff32e5a92566e54bcd05976725b9845d5
saisi <saisi@users.noreply.github.com>
2016-11-21 10:41:00 +0000

    1479724860 algocyan

---
+++ 1479724860.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2237116437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 9;
+   volatile unsigned f1 : 28;
+   const unsigned f2 : 15;
+   volatile signed f3 : 30;
+   const signed f4 : 23;
+   signed f5 : 20;
+   volatile signed f6 : 3;
