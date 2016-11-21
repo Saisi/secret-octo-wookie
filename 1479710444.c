93960058beb5db863ef22074b7390ac5793bd36f
saisi <saisi@users.noreply.github.com>
2016-11-21 06:40:45 +0000

    1479710444 ablauts

---
+++ 1479710444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      898398153
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x915A1369L;
+static uint32_t g_4 = 18446744073709551615UL;
+static uint8_t g_8[2] = {0xEEL,0xEEL};
+static int32_t *g_32 = &g_3;
+static int16_t g_41 = 0x2115L;
+static int64_t g_91 = 0x336FDE0027488F57LL;
+static int8_t g_112 = (-10L);
+static int16_t g_117[3] = {0xCECCL,0xCECCL,0xCECCL};
+static uint32_t g_145 = 1UL;
