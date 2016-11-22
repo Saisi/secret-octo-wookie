9c093cbac286ae5f7b03b33571389db2461395f2
saisi <saisi@users.noreply.github.com>
2016-11-22 09:12:27 +0000

    1479805946 Arabophil

---
+++ 1479805946.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2920425341
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   unsigned : 0;
+   unsigned f1 : 3;
+   const int16_t  f2;
+   signed f3 : 27;
+   unsigned f4 : 29;
+};
+
+#pragma pack(push)
