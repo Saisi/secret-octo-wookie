8f9a0fe6c772488000f178f6103f3d568cccb573
saisi <saisi@users.noreply.github.com>
2016-11-22 22:43:12 +0000

    1479854591 arteriomotor

---
+++ 1479854591.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2474689784
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+};
+
+struct S1 {
+   unsigned f0 : 2;
+   volatile unsigned f1 : 30;
+   volatile signed f2 : 25;
+   signed f3 : 23;
+   signed : 0;
