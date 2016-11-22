c75f1fe3da8d751991cb9abe8b95d8ed66a3e051
saisi <saisi@users.noreply.github.com>
2016-11-22 11:12:33 +0000

    1479813153 Adrenalin

---
+++ 1479813153.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4143287785
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[7][7] = {{(-1L),0xEC0AE971L,(-1L),(-1L),0xEC0AE971L,(-1L),(-1L)},{0xEC0AE971L,0xEC0AE971L,0x6915A136L,0xEC0AE971L,0xEC0AE971L,0x6915A136L,0xEC0AE971L},{0xEC0AE971L,(-1L),(-1L),0xEC0AE971L,(-1L),(-1L),0xEC0AE971L},{(-1L),0xEC0AE971L,(-1L),(-1L),0xEC0AE971L,(-1L),(-1L)},{0xEC0AE971L,0xEC0AE971L,0x6915A136L,0xEC0AE971L,0xEC0AE971L,0x6915A136L,0xEC0AE971L},{0xEC0AE971L,(-1L),(-1L),0xEC0AE971L,(-1L),(-1L),0xEC0AE971L},{(-1L),0xEC0AE971L,(-1L),(-1L),(-1L),0x6915A136L,0x6915A136L}};
+static int32_t g_3 = 0x6CCE57BFL;
+static int32_t * volatile g_4[4][9][4] = {{{&g_2[1][3],&g_3,&g_3,&g_2[1][2]},{&g_3,&g_2[6][4],&g_3,&g_3},{&g_3,&g_3,&g_2[4][3],&g_3},{&g_3,&g_3,&g_3,&g_2[6][4]},{&g_3,&g_3,&g_2[1][2],&g_2[5][0]},{&g_3,&g_3,&g_3,&g_2[0][0]},{&g_3,(void*)0,(void*)0,&g_3},{&g_3,&g_2[1][3],&g_3,&g_3},{&g_2[2][4],&g_3,&g_3,&g_2[2][2]}},{{(void*)0,&g_3,&g_3,&g_2[2][2]},{&g_2[0][6],&g_3,&g_2[2][5],&g_3},{&g_3,&g_2[1][3],&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_2[0][0]},{&g_2[6][4],&g_3,&g_3,&g_2[5][0]},{(void*)0,&g_3,&g_3,&g_2[6][4]},{&g_3,&g_3,(void*)0,&g_3},{&g_2[2][2],&g_3,&g_2[5][0],&g_3},{&g_2[0][1],&g_2[6][4],&g_3,&g_2[1][2]}},{{&g_2[1][2],&g_3,&g_2[0][0],&g_3},{&g_3,&g_2[6][6],&g_2[1][3],&g_2[6][1]},{&g_3,&g_2[2][1],&g_2[0][0],&g_3},{&g_2[1][2],&g_2[6][1],&g_3,&g_3},{&g_2[0][1],&g_2[0][0],&g_2[5][0],(void*)0},{&g_2[2][2],&g_3,(void*)0,&g_2[2][1]},{&g_3,(void*)0,&g_3,&g_2[0][1]},{(void*)0,&g_2[4][6],&g_3,(void*)0},{&g_2[6][4],&g_3,&g_3,&g_3}},{{&g_3,&g_3,&g_3,&g_3},{&g_3,&g_2[1][3],&g_2[2][5],&g_2[1][3]},{&g_2[0][6],&g_2[2][4],&g_3,&g_2[2][5]},{(void*)0,&g_2[2][4],&g_3,&g_2[1][3]},{&g_2[2][4],&g_2[1][3],&g_3,&g_3},{&g_3,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,(void*)0},{&g_3,&g_2[4][6],&g_2[1][2],&g_2[0][1]},{&g_3,(void*)0,&g_3,&g_2[2][1]}}};
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0xEE61780EL;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 0x99E885DCL;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9 = 0xEAB3CF81L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0L;
+static uint8_t g_52 = 246UL;
