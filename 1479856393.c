7f87140c62bcd17087235107f1754720f26bf078
saisi <saisi@users.noreply.github.com>
2016-11-22 23:13:13 +0000

    1479856393 antigraft

---
+++ 1479856393.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1563363750
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   const uint64_t  f1;
+   unsigned f2 : 26;
+   int8_t  f3;
+   const unsigned f4 : 8;
+   signed f5 : 10;
+   unsigned f6 : 7;
+   unsigned f7 : 2;
+};
