3da0bf1c2e996f6d51a77aa09ba92e58358dedf5
saisi <saisi@users.noreply.github.com>
2016-11-22 04:12:06 +0000

    1479787925 antimonyl

---
+++ 1479787925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      21870432
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x2811FFBEL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static int32_t g_5 = (-1L);
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static int32_t * volatile g_7 = (void*)0;/* VOLATILE GLOBAL g_7 */
+static uint64_t g_20 = 0x589A458230B54543LL;
+static uint64_t g_33[10] = {0x558B047C1471C2AALL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,0x558B047C1471C2AALL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
+static int32_t g_59 = (-9L);
+static uint64_t *g_66 = &g_33[6];
