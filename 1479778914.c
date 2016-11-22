facd9d15eed305c7ca163d4371b023ac1009204b
saisi <saisi@users.noreply.github.com>
2016-11-22 01:41:55 +0000

    1479778914 balloonflower

---
+++ 1479778914.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3415577150
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_8 = 254UL;/* VOLATILE GLOBAL g_8 */
+static int32_t g_13 = 0L;
+static int32_t *g_12 = &g_13;
+static int32_t g_39 = 1L;
+static int8_t g_52[3] = {0xEFL,0xEFL,0xEFL};
+static volatile int64_t g_53 = (-1L);/* VOLATILE GLOBAL g_53 */
+static int64_t g_54 = (-4L);
+static volatile uint32_t g_55 = 1UL;/* VOLATILE GLOBAL g_55 */
+static int32_t **g_87[6][1] = {{(void*)0},{&g_12},{(void*)0},{&g_12},{(void*)0},{&g_12}};
