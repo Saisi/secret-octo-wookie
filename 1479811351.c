6ca021d689022c072009ec59b73d7b305f5b3d8b
saisi <saisi@users.noreply.github.com>
2016-11-22 10:42:32 +0000

    1479811351 antalgesic

---
+++ 1479811351.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1519670570
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_29 = 0UL;
+static int16_t g_36 = 0L;
+static int16_t *g_71 = (void*)0;
+static int8_t g_76 = 2L;
