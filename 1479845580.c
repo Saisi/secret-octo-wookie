cbba8267e034e6db32a72b1dd86e3edff53c8c53
saisi <saisi@users.noreply.github.com>
2016-11-22 20:13:01 +0000

    1479845580 Apodes

---
+++ 1479845580.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1134106236
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+   int32_t  f1;
+   uint32_t  f2;
+   volatile uint8_t  f3;
+   int32_t  f4;
+   unsigned f5 : 18;
+   uint16_t  f6;
