31cff91e45207d40db18e8587487a62ba778a1e8
saisi <saisi@users.noreply.github.com>
2016-11-22 00:11:50 +0000

    1479773509 Anacortes

---
+++ 1479773509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2108343982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   unsigned f1 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint16_t  f0;
+   int8_t  f1;
