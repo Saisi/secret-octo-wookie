2b6addbfab84d7d11d6e7e1e36f54e17ea9bf46c
saisi <saisi@users.noreply.github.com>
2016-11-22 12:12:36 +0000

    1479816756 archdivine

---
+++ 1479816756.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2024505444
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_10 = 0xD2L;
+static int16_t g_71[6][10] = {{(-1L),0x4ACAL,0x4ACAL,(-1L),0L,0x95C1L,0L,(-1L),0x4ACAL,0x4ACAL},{0L,0x4ACAL,0L,0x5AF8L,0x5AF8L,0L,0x4ACAL,0L,0x4ACAL,0L},{0x95C1L,(-1L),0x5AF8L,(-1L),0x95C1L,0L,0L,0x95C1L,(-1L),0x5AF8L},{0L,0L,0x5AF8L,0x95C1L,0L,0x95C1L,0x5AF8L,0L,0L,0x5AF8L},{(-1L),0x95C1L,0L,0L,0x95C1L,(-1L),0L,0x4ACAL,0L,0x5AF8L},{0x95C1L,0L,0x95C1L,0x5AF8L,0L,0L,0x5AF8L,0x95C1L,0L,0x95C1L}};
+static uint16_t g_83 = 0x786CL;
+static int32_t g_85 = 0L;
+static int32_t * volatile g_84[7][3][1] = {{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}},{{&g_85},{&g_85},{&g_85}}};
+static uint16_t g_100 = 65533UL;
+static int32_t g_103 = 0xB820FDEBL;
+static int32_t * const  volatile g_102 = &g_103;/* VOLATILE GLOBAL g_102 */
+static int8_t g_112 = (-7L);
