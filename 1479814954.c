775cf11a9b371f7311992b2dea1b890a40f89c80
saisi <saisi@users.noreply.github.com>
2016-11-22 11:42:35 +0000

    1479814954 ardentness

---
+++ 1479814954.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3082322642
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 23;
+   unsigned f1 : 16;
+   unsigned f2 : 18;
+   unsigned f3 : 22;
+   const volatile signed f4 : 13;
+   uint16_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
