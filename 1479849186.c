c0ed14df32d8c59e5253a7e8976b5dbfa2eab59c
saisi <saisi@users.noreply.github.com>
2016-11-22 21:13:06 +0000

    1479849186 alloy

---
+++ 1479849186.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3940021238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_6[6] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint32_t g_15 = 0xFFFF9752L;
+static int32_t * volatile g_83[10][2] = {{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0},{(void*)0,(void*)0}};
+static int32_t * volatile g_84 = &g_6[5];/* VOLATILE GLOBAL g_84 */
+static volatile int32_t g_95 = 0xFF20F69DL;/* VOLATILE GLOBAL g_95 */
+static int32_t g_96 = 0L;
+static uint8_t g_111 = 4UL;
+static int32_t * volatile * const  volatile *g_118 = (void*)0;
