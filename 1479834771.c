3782cd0a4777c16c0b569d90d7fc697cbc98311f
saisi <saisi@users.noreply.github.com>
2016-11-22 17:12:52 +0000

    1479834771 aphacia

---
+++ 1479834771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1264635820
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+};
+
+struct S1 {
+   uint64_t  f0;
+   const struct S0  f1;
+   const struct S0  f2;
+   const struct S0  f3;
+   struct S0  f4;
