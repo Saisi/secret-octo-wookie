0f6fd4cbf6a7228896458c8cdece272032876ff6
saisi <saisi@users.noreply.github.com>
2016-11-22 14:12:43 +0000

    1479823962 aerodonetics

---
+++ 1479823962.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      96008717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   volatile uint32_t  f2;
+   int64_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
