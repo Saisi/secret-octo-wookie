73a2483a893ed8897f37282cfc3650b09de880a8
saisi <saisi@users.noreply.github.com>
2016-11-22 15:42:47 +0000

    1479829367 arthroendoscopy

---
+++ 1479829367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      709832460
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_10[8] = {(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L)};
+static uint8_t g_16 = 0x6EL;
+static int32_t g_35 = 0x76270E7AL;
+static uint16_t g_37 = 65527UL;
+static int16_t g_58 = (-7L);
+static int32_t g_70[4] = {0L,0L,0L,0L};
+static int32_t *g_97 = &g_70[3];
+static uint64_t g_112 = 0xF18804D593052EA8LL;
+static uint32_t g_122 = 4294967295UL;
