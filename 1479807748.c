066831ee29c835d19e2cca330970f43a4809a696
saisi <saisi@users.noreply.github.com>
2016-11-22 09:42:29 +0000

    1479807748 americanumancestors

---
+++ 1479807748.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3705459435
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   signed f1 : 27;
+   volatile signed f2 : 18;
+   unsigned f3 : 21;
+   signed f4 : 18;
+   signed f5 : 3;
+   volatile signed f6 : 22;
+};
+
