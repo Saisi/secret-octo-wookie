f8b1dcb9f769e8b8f915583992052b751167afa3
saisi <saisi@users.noreply.github.com>
2016-11-22 15:12:46 +0000

    1479827565 antheriform

---
+++ 1479827565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2203894202
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   const int16_t  f0;
+   unsigned f1 : 19;
+   unsigned f2 : 24;
+   unsigned f3 : 6;
+   signed f4 : 6;
+   const volatile signed f5 : 9;
+   volatile unsigned f6 : 26;
+   volatile unsigned f7 : 4;
+};
