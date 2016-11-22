d06257383597de2e938bb52d9b5927a23afc72b7
saisi <saisi@users.noreply.github.com>
2016-11-22 08:12:23 +0000

    1479802340 archeunuch

---
+++ 1479802340.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      332728182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * volatile  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   signed f3 : 24;
+};
+
+union U1 {
+   signed f0 : 18;
+};
