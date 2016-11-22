f72a6d82d563274bdd801a14ded09a8b860526a9
saisi <saisi@users.noreply.github.com>
2016-11-22 06:12:15 +0000

    1479795134 antecurvature

---
+++ 1479795134.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4016646104
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 26;
+   int16_t  f1;
+   volatile unsigned f2 : 31;
+   const volatile signed f3 : 5;
+   const signed f4 : 22;
+   volatile int8_t  f5;
+   volatile signed f6 : 16;
+   const unsigned f7 : 7;
+   unsigned f8 : 18;
