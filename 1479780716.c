db0ac2b2ba901b4e325d49dc9c5a032e660481a2
saisi <saisi@users.noreply.github.com>
2016-11-22 02:11:58 +0000

    1479780716 autumnally

---
+++ 1479780716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2926657758
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+   uint16_t  f3;
+   const volatile int32_t  f4;
+   uint16_t  f5;
+   volatile unsigned f6 : 13;
+   uint8_t  f7;
+};
