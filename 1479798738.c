04de9a78092ec0e2d7f7886947ca7e938da297bd
saisi <saisi@users.noreply.github.com>
2016-11-22 07:12:18 +0000

    1479798738 arreptitious

---
+++ 1479798738.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3460961770
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 12;
+   unsigned f2 : 30;
+   volatile unsigned f3 : 18;
+   unsigned f4 : 25;
+   volatile signed f5 : 12;
+   volatile signed f6 : 26;
+   int64_t  f7;
+};
