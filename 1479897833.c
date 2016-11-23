94d0b8884dcf0058152bccce65a9b280f65bf1a4
saisi <saisi@users.noreply.github.com>
2016-11-23 10:43:53 +0000

    1479897833 alchera

---
+++ 1479897833.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3510082795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_10 = 0xB2B534DEL;
+static uint8_t g_15 = 8UL;
+static uint16_t g_17[4] = {0x66D8L,0x66D8L,0x66D8L,0x66D8L};
+static uint32_t g_18 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
