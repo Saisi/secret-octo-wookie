90f61fbee558250a878bc2043ded1151923a08c8
saisi <saisi@users.noreply.github.com>
2016-11-23 18:44:25 +0000

    1479926664 balsamaceous

---
+++ 1479926664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1903349283
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 16;
+   uint32_t  f1;
+   volatile uint32_t  f2;
+};
+#pragma pack(pop)
+
+union U1 {
