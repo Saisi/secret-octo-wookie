550635b218c5998f162f20a68e89d71a7bc2e11a
saisi <saisi@users.noreply.github.com>
2016-11-23 00:43:18 +0000

    1479861797 azophenetole

---
+++ 1479861797.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4274497829
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   signed f1 : 4;
+   unsigned f2 : 23;
+   signed f3 : 6;
+   unsigned f4 : 31;
+   unsigned f5 : 28;
+};
