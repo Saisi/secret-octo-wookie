dbf6da12f9d6ac0207789aaed24a986712187c45
saisi <saisi@users.noreply.github.com>
2016-11-23 09:43:51 +0000

    1479894230 aggravator

---
+++ 1479894230.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1862021728
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   int8_t  f1;
+   const unsigned f2 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-5L);
+static int32_t g_4[10] = {0L,6L,0xAB3921DEL,0xAB3921DEL,6L,0L,6L,0xAB3921DEL,0xAB3921DEL,6L};
+static union U0 g_26[6][9] = {{{0x57L},{3L},{0x57L},{-10L},{0L},{0x77L},{3L},{1L},{0x81L}},{{0xF3L},{0x4CL},{0L},{0x4FL},{1L},{-1L},{3L},{0xB1L},{-1L}},{{0x77L},{1L},{9L},{-10L},{-10L},{9L},{1L},{0x77L},{0x57L}},{{0xA4L},{1L},{-2L},{0x4CL},{-3L},{-1L},{0L},{0x57L},{0x6BL}},{{-10L},{0x77L},{0xA4L},{3L},{0x6BL},{-2L},{0xF3L},{-2L},{0x6BL}},{{-1L},{7L},{7L},{-1L},{3L},{9L},{0xF3L},{0x57L},{1L}}};
