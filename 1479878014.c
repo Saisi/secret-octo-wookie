4d5bde1dbf1632a1f9daceb0c6d11c43e745beb2
saisi <saisi@users.noreply.github.com>
2016-11-23 05:13:34 +0000

    1479878014 antivitalistic

---
+++ 1479878014.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2913685011
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 8;
+   volatile int32_t  f1;
+   const int64_t  f2;
+   int32_t  f3;
+   uint32_t  f4;
+};
+
+union U1 {
+   volatile uint64_t  f0;
