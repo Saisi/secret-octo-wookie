b7b98317419ce90991480513c7b0a2869616575b
saisi <saisi@users.noreply.github.com>
2016-11-23 13:44:06 +0000

    1479908646 archae

---
+++ 1479908646.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2760775198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   const volatile signed f1 : 30;
+   volatile unsigned f2 : 8;
+   unsigned f3 : 20;
+   signed f4 : 19;
+   unsigned f5 : 30;
+   const signed f6 : 4;
+   unsigned f7 : 9;
+};
