27508c4728b81d48e7277ca6ce68897c84015e34
saisi <saisi@users.noreply.github.com>
2016-11-23 11:43:58 +0000

    1479901437 antisialagogue

---
+++ 1479901437.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3218287224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 10;
+};
+
+struct S1 {
+   int8_t  f0;
+   volatile int8_t  f1;
+   int32_t  f2;
+   unsigned f3 : 5;
+   int16_t  f4;
