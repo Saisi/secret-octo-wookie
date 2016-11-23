3cfffb7d1466e79cb817744edb4ae0d760ac1ec8
saisi <saisi@users.noreply.github.com>
2016-11-23 00:13:16 +0000

    1479859996 Baccio

---
+++ 1479859996.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2028932892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8][2] = {{1L,0x39ED080CL},{(-6L),1L},{0x70CD5099L,0x70CD5099L},{0x70CD5099L,1L},{(-6L),0x39ED080CL},{1L,0x39ED080CL},{(-6L),1L},{0x70CD5099L,0x70CD5099L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
