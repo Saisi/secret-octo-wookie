0df3ef1496e0f9fdf6a61be1c8bc17214e7056c8
saisi <saisi@users.noreply.github.com>
2016-11-23 03:13:27 +0000

    1479870807 balteus

---
+++ 1479870807.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2271502219
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int16_t  f0;
+   uint16_t  f1;
+   uint16_t  f2;
+   const int8_t  f3;
+   volatile int32_t  f4;
+   volatile signed f5 : 2;
+   const signed f6 : 19;
