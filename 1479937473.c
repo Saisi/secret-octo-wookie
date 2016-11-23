ea2aaa642075d15f3e5ae60b54f9b887c5eb52d5
saisi <saisi@users.noreply.github.com>
2016-11-23 21:44:33 +0000

    1479937473 Agaricus

---
+++ 1479937473.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1137190856
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = (-8L);
+static int32_t *g_6 = &g_7;
+static int64_t g_16 = (-9L);
+static int64_t g_18 = (-1L);
+static int32_t g_73 = (-7L);
+static int32_t g_75[10] = {0L,0xF9898791L,0L,0L,0xF9898791L,0L,0L,0xF9898791L,0L,0L};
+static int32_t *g_79 = &g_73;
+static int32_t ** volatile g_78 = &g_79;/* VOLATILE GLOBAL g_78 */
+static uint16_t g_83 = 0x1D00L;
