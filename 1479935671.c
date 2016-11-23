0fcef4973b05c9031498e812ddf1c9918483d056
saisi <saisi@users.noreply.github.com>
2016-11-23 21:14:32 +0000

    1479935671 backbitten

---
+++ 1479935671.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2318784040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_5 = 0L;
+static int32_t g_20 = (-8L);
+static int32_t *g_19 = &g_20;
+static int32_t ** volatile g_22[8] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static uint64_t g_29[4] = {1UL,1UL,1UL,1UL};
+static int32_t g_73 = 0xC62B4191L;
+static int32_t * volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static uint64_t g_79 = 0x47B1D005767A065BLL;
+static uint64_t g_96[4] = {5UL,5UL,5UL,5UL};
