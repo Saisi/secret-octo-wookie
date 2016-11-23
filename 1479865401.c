c4e4d1f2655e93e298ed0494c012185e959cdd36
saisi <saisi@users.noreply.github.com>
2016-11-23 01:43:22 +0000

    1479865401 Aulacomniaceae

---
+++ 1479865401.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3619338472
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   int64_t  f1;
+   volatile int32_t  f2;
+   volatile int16_t  f3;
+};
+
+union U1 {
+   int8_t  f0;
+   volatile int32_t  f1;
