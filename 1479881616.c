de51b412383b517de57b772aacd4f3e235402e5f
saisi <saisi@users.noreply.github.com>
2016-11-23 06:13:37 +0000

    1479881616 aurifying

---
+++ 1479881616.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3994931883
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   uint32_t  f1;
+   int16_t  f2;
+   volatile int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-8L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
