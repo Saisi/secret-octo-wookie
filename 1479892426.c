bc5e3203ee1490764cae6fb3fe8f50ba5b02e403
saisi <saisi@users.noreply.github.com>
2016-11-23 09:13:48 +0000

    1479892426 achromophilous

---
+++ 1479892426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2327881780
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile int64_t  f0;
+   const uint64_t  f1;
+   uint64_t  f2;
+   int32_t  f3;
+   int16_t  f4;
+   int32_t  f5;
+   int8_t  f6;
