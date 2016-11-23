27fd09d4df5ff7fd2c1ff6656472cbd93d910ab1
saisi <saisi@users.noreply.github.com>
2016-11-23 16:14:16 +0000

    1479917656 amiantus

---
+++ 1479917656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2648612721
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x2DB58E46L;
+static volatile int64_t g_17 = 0x8DFBA22E974A5202LL;/* VOLATILE GLOBAL g_17 */
+static int16_t g_18 = 0xAE7BL;
+static volatile int16_t g_19 = 0x6872L;/* VOLATILE GLOBAL g_19 */
+static volatile int64_t g_20[2] = {0L,0L};
+static volatile int32_t g_21 = (-6L);/* VOLATILE GLOBAL g_21 */
+static int32_t g_22 = 0x0AEC51ACL;
+static int16_t g_23 = (-1L);
+static uint16_t g_24 = 0xFD6BL;
