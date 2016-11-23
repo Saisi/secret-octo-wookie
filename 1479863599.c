4472c159f79020e56d62153bcf327228910236a2
saisi <saisi@users.noreply.github.com>
2016-11-23 01:13:20 +0000

    1479863599 bailpiece

---
+++ 1479863599.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3843021327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int64_t  f0;
+   volatile unsigned f1 : 5;
+   int32_t  f2;
+   const uint32_t  f3;
+};
+
+union U1 {
+   int32_t  f0;
+   const int32_t  f1;
