99f3123437b59bc47f77d03bc5a254f40565ea22
saisi <saisi@users.noreply.github.com>
2016-11-23 03:43:29 +0000

    1479872609 astroscopy

---
+++ 1479872609.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1865252756
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-3L);
+static int32_t g_38[1][6] = {{0x42BE197EL,0x17F4039EL,0x42BE197EL,0x42BE197EL,0x17F4039EL,0x42BE197EL}};
