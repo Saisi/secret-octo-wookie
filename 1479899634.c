83450eba9f24c07dd270f4cb5d70800560453002
saisi <saisi@users.noreply.github.com>
2016-11-23 11:13:56 +0000

    1479899634 astaticism

---
+++ 1479899634.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1334998229
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 9;
+   const signed f1 : 28;
+   int8_t  f2;
+   const volatile unsigned f3 : 16;
+   uint32_t  f4;
+   signed f5 : 15;
+   const signed f6 : 23;
