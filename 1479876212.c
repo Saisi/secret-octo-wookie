d02d720746c8ff60207c1feb2bee9c206e2fbfe5
saisi <saisi@users.noreply.github.com>
2016-11-23 04:43:32 +0000

    1479876212 archangelical

---
+++ 1479876212.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3518118671
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint16_t  f1;
+   uint32_t  f2;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+   signed f1 : 13;
+   const volatile unsigned f2 : 24;
