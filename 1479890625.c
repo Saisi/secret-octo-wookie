f13d7764980e002e772e0196f6be733266ea6b98
saisi <saisi@users.noreply.github.com>
2016-11-23 08:43:45 +0000

    1479890625 Asperges

---
+++ 1479890625.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3889078830
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   int8_t * f1;
+   unsigned f2 : 11;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5816210BL;
+static int32_t g_5 = 0xACC5D9B4L;
+static uint32_t g_13 = 4294967295UL;
