574e541a337c158fb81bf7864ed8ddbfcf9a558a
saisi <saisi@users.noreply.github.com>
2016-11-23 02:43:25 +0000

    1479869005 allothimorphic

---
+++ 1479869005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3162093276
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   signed f1 : 1;
+   const unsigned f2 : 31;
+   signed f3 : 15;
+   signed f4 : 2;
+   signed f5 : 17;
+   signed f6 : 11;
+   unsigned f7 : 20;
+   const unsigned f8 : 8;
