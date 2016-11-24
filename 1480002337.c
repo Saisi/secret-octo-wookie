ffa31dc6fe62ba0000e3b5b74ee08b2e5c921f56
saisi <saisi@users.noreply.github.com>
2016-11-24 15:45:38 +0000

    1480002337 antiblack

---
+++ 1480002337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      171392453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+   int16_t  f2;
+   int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xBF0B96CAL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-9L);
