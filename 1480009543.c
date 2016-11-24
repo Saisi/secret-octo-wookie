3d2783a7de7e902cf8b5334ed65f1b6f886d1908
saisi <saisi@users.noreply.github.com>
2016-11-24 17:45:43 +0000

    1480009543 Alvina

---
+++ 1480009543.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145277928
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 2;
+   signed f1 : 15;
+   signed f2 : 21;
+   signed f3 : 13;
+   unsigned f4 : 8;
+   const volatile signed f5 : 31;
+   int32_t  f6;
