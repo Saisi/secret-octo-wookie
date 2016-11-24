1a70366e67a8ff6731a4c141aecdd3689d953ed8
saisi <saisi@users.noreply.github.com>
2016-11-24 10:15:21 +0000

    1479982520 aquatile

---
+++ 1479982520.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1386734103
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 10;
+   volatile int8_t  f1;
+   volatile unsigned f2 : 9;
+   signed f3 : 9;
+   signed f4 : 12;
+};
+#pragma pack(pop)
