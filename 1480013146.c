04b3fd4868c5612da42769bc50ba5dd939526c1a
saisi <saisi@users.noreply.github.com>
2016-11-24 18:45:47 +0000

    1480013146 Acalyptratae

---
+++ 1480013146.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4264572191
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   unsigned f1 : 13;
+   volatile signed f2 : 28;
+   signed f3 : 23;
+   volatile unsigned f4 : 12;
+   const signed f5 : 21;
+   unsigned : 0;
+};
+
