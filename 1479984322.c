205544898539a91ee11a9d9cdea1189d99e4b7a7
saisi <saisi@users.noreply.github.com>
2016-11-24 10:45:22 +0000

    1479984322 Amorgos

---
+++ 1479984322.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1061064454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xAC789480L;
+static int32_t g_12 = 0xF9C71042L;
+static volatile uint32_t g_13 = 18446744073709551609UL;/* VOLATILE GLOBAL g_13 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
