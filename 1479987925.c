b1be67aa9d55b58a15d8beb7720a4ac19b3ea524
saisi <saisi@users.noreply.github.com>
2016-11-24 11:45:25 +0000

    1479987925 anthropobiology

---
+++ 1479987925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1560833272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[1] = {0L};
+static volatile int32_t g_3 = 0x41E55969L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x57A7C203L;
+static int32_t g_10 = 0xD9A924C0L;
+static int32_t g_54 = 0xD01CD43AL;
+static uint64_t g_74 = 1UL;
+static int16_t g_76 = 0L;
+static int64_t g_84 = 0xB98F8D6FFBA4BDACLL;
+static volatile int32_t g_103[3] = {0L,0L,0L};
