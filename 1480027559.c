d275e61870e9bdd612b70b4bedf915411c65e12c
saisi <saisi@users.noreply.github.com>
2016-11-24 22:45:59 +0000

    1480027559 antifertilizer

---
+++ 1480027559.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1621979424
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const uint64_t  f1;
+   volatile uint64_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+   volatile int8_t * volatile  f1;
+};
