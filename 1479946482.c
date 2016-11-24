bcdb94724710fed8447c6809b94b8af15272ee7c
saisi <saisi@users.noreply.github.com>
2016-11-24 00:14:42 +0000

    1479946482 archegoniate

---
+++ 1479946482.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1947699335
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile int32_t  f0;
+   const uint8_t  f1;
+   volatile int16_t  f2;
+   int64_t  f3;
+   int8_t  f4;
+   volatile int32_t  f5;
+   uint64_t  f6;
