04b557689c36f26b5d883d4923d74214304b5b4c
saisi <saisi@users.noreply.github.com>
2016-11-24 02:44:52 +0000

    1479955492 anisogeny

---
+++ 1479955492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4000998131
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+   int8_t  f1;
+   const int16_t  f2;
+   volatile int32_t  f3;
+   uint32_t  f4;
+   volatile uint64_t  f5;
+   const volatile uint32_t  f6;
+   volatile int32_t  f7;
+   int32_t  f8;
