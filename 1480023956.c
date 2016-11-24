8dd251a27f59c01316e8a0457e2656480658c0a9
saisi <saisi@users.noreply.github.com>
2016-11-24 21:45:56 +0000

    1480023956 amendatory

---
+++ 1480023956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      761210040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xE2167FA5L;
+static int32_t * volatile g_10 = (void*)0;/* VOLATILE GLOBAL g_10 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+static int32_t * func_4(int32_t  p_5);
+
+
