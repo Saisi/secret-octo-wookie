772a0339a7f504731da31029af7d0a5275e28d50
saisi <saisi@users.noreply.github.com>
2016-11-24 03:14:54 +0000

    1479957294 Adelric

---
+++ 1479957294.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3486874551
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x9F9B3198L;
+static uint16_t g_16 = 65535UL;
+static uint32_t g_19 = 4294967289UL;
+static int64_t g_51 = 0L;
+static int32_t g_58 = (-3L);
+static int32_t *g_62 = &g_2;
+static int32_t ** const  volatile g_61[4] = {&g_62,&g_62,&g_62,&g_62};
+static int8_t g_72 = (-1L);
+static int64_t g_98 = (-3L);
