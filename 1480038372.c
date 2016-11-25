b0292ca38b629c08893d90d8fcd4dfe8baa4d815
saisi <saisi@users.noreply.github.com>
2016-11-25 01:46:13 +0000

    1480038372 alexandrite

---
+++ 1480038372.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3967023831
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 10;
+   volatile unsigned f1 : 25;
+   unsigned f2 : 24;
+};
+
+union U1 {
+   volatile int8_t * const  f0;
+};
+
