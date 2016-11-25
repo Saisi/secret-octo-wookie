7696d2f4a1cb12c9e738a0db2f1432ca615f2f7d
saisi <saisi@users.noreply.github.com>
2016-11-25 19:47:15 +0000

    1480103235 analkalinity

---
+++ 1480103235.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3502713325
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 21;
+   unsigned f1 : 24;
+   volatile unsigned f2 : 27;
+   signed f3 : 26;
+   signed f4 : 3;
+   volatile unsigned f5 : 16;
+   volatile signed f6 : 15;
