a0a453bd71fafcfe8d7361a728e4e71c10c8b5d2
saisi <saisi@users.noreply.github.com>
2016-11-25 15:16:58 +0000

    1480087018 antihierarchy

---
+++ 1480087018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2939644038
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_11 = 65535UL;
+static uint32_t g_12 = 0xE4B9A733L;
+static int32_t g_16 = (-1L);
+static int64_t g_56 = 0L;
+static int32_t g_69 = 0xD0C9AD53L;
+static uint16_t g_70 = 0x8485L;
+static int32_t g_79 = 1L;
+static int64_t * const *g_87 = (void*)0;
+static int32_t g_89[5][6] = {{(-9L),0x7DDBA47AL,0L,0xE0EE4D50L,0L,0x7DDBA47AL},{(-1L),(-9L),0L,0x9E4C0C5DL,0xCEE7E287L,0xE0EE4D50L},{0x835292FDL,0x9E4C0C5DL,0x7DDBA47AL,0x7DDBA47AL,0x9E4C0C5DL,0x835292FDL},{0x7DDBA47AL,0x9E4C0C5DL,0x835292FDL,(-4L),0xCEE7E287L,0L},{0L,(-9L),(-1L),(-9L),0L,0x9E4C0C5DL}};
