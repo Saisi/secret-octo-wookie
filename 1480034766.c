b08168ee227db5eb5e56e74fa4ace8fc26369776
saisi <saisi@users.noreply.github.com>
2016-11-25 00:46:08 +0000

    1480034766 ambulacra

---
+++ 1480034766.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      256537114
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+   volatile int64_t  f1;
+   uint32_t  f2;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   int8_t * const  f1;
+   int32_t  f2;
