e563dcb81fc153cbedc6ccb1f196bdc25c6b4357
saisi <saisi@users.noreply.github.com>
2016-11-25 09:16:37 +0000

    1480065397 appellate

---
+++ 1480065397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      424461990
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 1;
+   unsigned f1 : 18;
+   signed f2 : 12;
+   signed f3 : 8;
+   const signed f4 : 23;
+   unsigned f5 : 18;
+   int32_t  f6;
+   signed f7 : 15;
+};
