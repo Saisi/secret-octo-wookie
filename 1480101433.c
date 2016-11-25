22edc36491f04750787f4ad5afb15a30ee57e301
saisi <saisi@users.noreply.github.com>
2016-11-25 19:17:14 +0000

    1480101433 alcyonoid

---
+++ 1480101433.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3494000865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 15;
+   unsigned f1 : 10;
+   const signed f2 : 5;
+   unsigned f3 : 28;
+   unsigned f4 : 29;
+   const uint8_t  f5;
+   const unsigned f6 : 22;
+   signed f7 : 24;
+};
