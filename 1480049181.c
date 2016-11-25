5947fe2eda756f4477690b91aa42fce040e73573
saisi <saisi@users.noreply.github.com>
2016-11-25 04:46:21 +0000

    1480049181 Araucanian

---
+++ 1480049181.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2742363740
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   const int8_t  f1;
+   volatile int16_t  f2;
+};
+
+union U1 {
+   uint32_t  f0;
+   const volatile int64_t  f1;
+};
