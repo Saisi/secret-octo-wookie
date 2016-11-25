00bcb007f1551d78d91fa9ae0538f4e79b1a393a
saisi <saisi@users.noreply.github.com>
2016-11-25 02:16:14 +0000

    1480040174 averia

---
+++ 1480040174.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3093377782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   const volatile int64_t  f1;
+   const int32_t  f2;
+   int64_t  f3;
+   const volatile int32_t  f4;
+   volatile uint64_t  f5;
+   const uint64_t  f6;
