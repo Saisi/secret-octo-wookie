5b0dc618e17c6004976051ca3ead0330119d633d
saisi <saisi@users.noreply.github.com>
2016-11-25 13:46:53 +0000

    1480081613 Andaman

---
+++ 1480081613.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1910630318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6 = (-10L);
+static int32_t g_7 = (-1L);
+static volatile int32_t g_8 = 0xACC5D9B4L;/* VOLATILE GLOBAL g_8 */
+static uint16_t g_9 = 0x5EE2L;
+static int32_t *g_33 = (void*)0;
+static int32_t g_43 = 0x66B3883FL;
+static uint8_t g_49 = 0UL;
+static uint32_t g_70 = 0x513E67DCL;
+static uint32_t g_78 = 0x1C2B46DFL;
