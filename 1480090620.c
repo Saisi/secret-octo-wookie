cb7d87912eefbc8b9c0cbaa29c9cbbf61ab7ecaf
saisi <saisi@users.noreply.github.com>
2016-11-25 16:17:01 +0000

    1480090620 autocrats

---
+++ 1480090620.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3627360678
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2] = {(-1L),(-1L)};
+static int32_t g_3 = 0x94801897L;
+static uint16_t g_9 = 65531UL;
+static uint8_t g_19 = 4UL;
+static int32_t g_21 = 0x6F8B8AE5L;
+static uint8_t g_32 = 0x65L;
+static uint64_t g_47 = 7UL;
+static int32_t g_88 = 0x09DF947BL;
+static int32_t g_92 = (-1L);
