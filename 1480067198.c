9ad4cce979035b4266ade765bf2ef3d7a0271d3b
saisi <saisi@users.noreply.github.com>
2016-11-25 09:46:39 +0000

    1480067198 archtreasurer

---
+++ 1480067198.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3411073040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 22;
+   signed f1 : 19;
+   const volatile unsigned f2 : 9;
+   signed f3 : 17;
+   volatile signed f4 : 28;
+};
+
+struct S1 {
+   volatile uint32_t  f0;
