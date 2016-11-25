76df501e778ab91340520db235521b2928fd1d02
saisi <saisi@users.noreply.github.com>
2016-11-25 04:16:20 +0000

    1480047379 allosome

---
+++ 1480047379.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      428887491
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile uint32_t  f1;
+   uint16_t  f2;
+   const volatile signed f3 : 17;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   int32_t  f1;
