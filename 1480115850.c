4d932443efd38d8c44a282a09fe09d9fc55f049b
saisi <saisi@users.noreply.github.com>
2016-11-25 23:17:30 +0000

    1480115850 ascogonia

---
+++ 1480115850.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3454357544
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x99333C7DL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t *g_2[9] = {&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3};
+static uint32_t g_6 = 9UL;
+static int32_t g_8 = 0x4856CD83L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
