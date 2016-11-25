79d03ddb853a1e88bfbbe603b7ccd215c5438917
saisi <saisi@users.noreply.github.com>
2016-11-25 16:47:04 +0000

    1480092423 Alectoria

---
+++ 1480092423.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1215595062
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 9;
+   const unsigned f1 : 26;
+   volatile signed f2 : 20;
+   unsigned f3 : 16;
+   const unsigned f4 : 31;
+   const signed f5 : 26;
+   volatile signed f6 : 16;
+   const unsigned f7 : 5;
+   signed f8 : 21;
