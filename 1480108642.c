99dc3807536adba2b545f43a17ba5494723d05c4
saisi <saisi@users.noreply.github.com>
2016-11-25 21:17:23 +0000

    1480108642 Audion

---
+++ 1480108642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1154317551
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-9L);
+static uint8_t g_12 = 249UL;
+static volatile uint8_t g_14[9] = {255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL};
+static volatile uint8_t *g_13[5][6] = {{&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6]},{&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6]},{&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6]},{&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6]},{&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6],&g_14[6]}};
