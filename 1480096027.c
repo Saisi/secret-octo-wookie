237076f892fef2a213a501e81b5c4dd428171e6b
saisi <saisi@users.noreply.github.com>
2016-11-25 17:47:07 +0000

    1480096027 archegonium

---
+++ 1480096027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3928317813
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-10L);
+static int32_t g_6 = 0xD0374C7AL;
+static volatile int32_t g_7 = 0x2BD42CA8L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_21 = 1L;
+static int32_t g_29[3] = {(-1L),(-1L),(-1L)};
+static int32_t * volatile g_31 = &g_29[1];/* VOLATILE GLOBAL g_31 */
+static int32_t * volatile * const g_30 = &g_31;
+static int32_t *g_58 = &g_29[0];
+static int32_t **g_57[9] = {&g_58,&g_58,&g_58,&g_58,&g_58,&g_58,&g_58,&g_58,&g_58};
