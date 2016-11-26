40e62ad8229821eb01e049df1f299adb679f0f78
saisi <saisi@users.noreply.github.com>
2016-11-26 05:47:50 +0000

    1480139270 argentometer

---
+++ 1480139270.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      657131598
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   volatile unsigned f1 : 22;
+   signed : 0;
+   signed f2 : 12;
+   signed f3 : 11;
+   signed f4 : 22;
+};
+
+#pragma pack(push)
