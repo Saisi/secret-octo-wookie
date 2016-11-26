e6db4cbcde027762e894f7adc613a616396b6bab
saisi <saisi@users.noreply.github.com>
2016-11-26 23:18:50 +0000

    1480202329 affector

---
+++ 1480202329.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      507061735
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   volatile unsigned f1 : 23;
+   int32_t  f2;
+   const volatile unsigned f3 : 23;
+   unsigned f4 : 13;
+   const signed f5 : 28;
+   signed f6 : 16;
