da8e93ca358df0ff9066ec3f1135c33cd032aa61
saisi <saisi@users.noreply.github.com>
2016-11-26 16:48:31 +0000

    1480178910 bagnet

---
+++ 1480178910.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3089349815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+};
+
+union U1 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xC8094A18L;/* VOLATILE GLOBAL g_2 */
