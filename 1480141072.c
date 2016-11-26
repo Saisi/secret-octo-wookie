b174da49bb2df3c9a8381e5d1e998ca250ce5dd1
saisi <saisi@users.noreply.github.com>
2016-11-26 06:17:52 +0000

    1480141072 allonomous

---
+++ 1480141072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2969322527
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint16_t  f0;
+   unsigned f1 : 3;
+   unsigned f2 : 9;
