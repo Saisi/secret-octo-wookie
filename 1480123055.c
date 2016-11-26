c7ed450bae200e89a50491e29e8f2203c31e23e2
saisi <saisi@users.noreply.github.com>
2016-11-26 01:17:35 +0000

    1480123055 arthrozoan

---
+++ 1480123055.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1285445395
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x92EA21E1L;/* VOLATILE GLOBAL g_2 */
+static uint8_t g_4[2][8] = {{255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL},{255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
