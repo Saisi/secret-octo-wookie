19b146b87b71f28ed46b897d5110b0d389625765
saisi <saisi@users.noreply.github.com>
2016-11-26 00:47:34 +0000

    1480121254 assortments

---
+++ 1480121254.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3247642980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile uint16_t  f1;
+   int16_t  f2;
+   signed f3 : 3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint32_t g_6 = 1UL;/* VOLATILE GLOBAL g_6 */
+static const int64_t g_9[7] = {(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L)};
