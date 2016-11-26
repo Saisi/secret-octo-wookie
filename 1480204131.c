f7d9cb520393720e6dee3b27a973face8be59199
saisi <saisi@users.noreply.github.com>
2016-11-26 23:48:51 +0000

    1480204131 amyelencephalia

---
+++ 1480204131.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3599675073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = (-5L);
+static volatile uint64_t g_67 = 0UL;/* VOLATILE GLOBAL g_67 */
+static volatile uint64_t * volatile g_66 = &g_67;/* VOLATILE GLOBAL g_66 */
+static uint64_t *g_68[3][1] = {{(void*)0},{(void*)0},{(void*)0}};
+static int32_t g_71 = 0x04FECCDFL;
+static int32_t g_76[5] = {(-6L),(-6L),(-6L),(-6L),(-6L)};
+static int32_t g_126 = (-1L);
+static int16_t g_137 = 0x7708L;
+static int16_t g_144 = (-3L);
