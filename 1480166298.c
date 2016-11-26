794f141682ed9f841fab1ce5335cc032c7ff24f5
saisi <saisi@users.noreply.github.com>
2016-11-26 13:18:19 +0000

    1480166298 badan

---
+++ 1480166298.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      696960041
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint64_t  f1;
+   const volatile int64_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_5 = 0x0FL;
+static int16_t g_56 = 0xE002L;
