6fc9f6bcb2d24a0819668ea8b6c6329e8570b2ab
saisi <saisi@users.noreply.github.com>
2016-11-26 19:18:37 +0000

    1480187917 bankeress

---
+++ 1480187917.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2386402713
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x37D45DB3L;
+static int16_t g_110 = 0x1EE1L;
+static int8_t g_112 = 0L;
+static uint32_t g_117 = 0UL;
+static int32_t g_121 = 1L;
+static int32_t *g_120[9] = {&g_121,&g_121,&g_121,&g_121,&g_121,&g_121,&g_121,&g_121,&g_121};
+static int16_t g_124 = 3L;
+static uint8_t g_140 = 247UL;
+static int32_t * volatile g_142[3] = {(void*)0,(void*)0,(void*)0};
