316a60786ae2c0c2d7b96f039a42211df1c1e04c
saisi <saisi@users.noreply.github.com>
2016-11-26 22:48:48 +0000

    1480200528 antiparalytical

---
+++ 1480200528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2480554110
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 13;
+   uint16_t  f1;
+   volatile signed f2 : 24;
+   uint32_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_6 = {0L};/* VOLATILE GLOBAL g_6 */
