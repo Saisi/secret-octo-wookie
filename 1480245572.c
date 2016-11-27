dfd041dff48e013b4f4c4935981053816228afb2
saisi <saisi@users.noreply.github.com>
2016-11-27 11:19:32 +0000

    1480245572 Abell

---
+++ 1480245572.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2393763024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+   signed f1 : 19;
+   int32_t  f2;
+   volatile unsigned f3 : 1;
+   volatile signed f4 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_23 = 0x8C84L;
