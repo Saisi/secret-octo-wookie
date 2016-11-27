2ef85be29cbdd16e65ab81f6085f03b588693826
saisi <saisi@users.noreply.github.com>
2016-11-27 00:18:52 +0000

    1480205932 antialien

---
+++ 1480205932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1899230941
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[10] = {1L,0x976FF55EL,0x5286E871L,0x5286E871L,0x976FF55EL,1L,0x976FF55EL,0x5286E871L,0x5286E871L,0x976FF55EL};
+static const int32_t * const g_2 = &g_3[0];
+static const int32_t *g_5 = &g_3[9];
+static const int32_t ** volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
