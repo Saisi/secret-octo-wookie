cff7a8d546f09c3ef9124c07b457dd9f504f721d
saisi <saisi@users.noreply.github.com>
2016-11-27 02:19:00 +0000

    1480213138 aggrandising

---
+++ 1480213138.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4109075368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   uint64_t  f1;
+   uint8_t  f2;
+   uint32_t  f3;
+   volatile uint32_t  f4;
+   signed f5 : 7;
+   uint32_t  f6;
+};
+
