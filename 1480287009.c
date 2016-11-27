0abba2d9be3280e8613501c5330cca3d594a718a
saisi <saisi@users.noreply.github.com>
2016-11-27 22:50:09 +0000

    1480287009 actinozoal

---
+++ 1480287009.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1888435403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_2 = 0UL;
+static uint8_t g_3 = 0x53L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
