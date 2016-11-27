22edd708f34be4a91777c2cf9729d430253b6e63
saisi <saisi@users.noreply.github.com>
2016-11-27 06:49:17 +0000

    1480229356 abjection

---
+++ 1480229356.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2851476184
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xC2039807L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static int32_t g_24 = 7L;
+static int32_t g_74 = 0x4A3A207BL;
