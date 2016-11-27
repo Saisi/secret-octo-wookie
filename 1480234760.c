a7e19d2775d1576d96ccd2aa2a3f07697d164dd9
saisi <saisi@users.noreply.github.com>
2016-11-27 08:19:20 +0000

    1480234760 astelic

---
+++ 1480234760.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1594636436
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_2[2] = {(void*)0,(void*)0};
+static int32_t g_6 = 0xDE0B4074L;
+static int32_t *g_5[2] = {&g_6,&g_6};
+static int32_t ** volatile g_4 = &g_5[1];/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
