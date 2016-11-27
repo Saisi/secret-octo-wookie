b6d19b533fe528f614fe0ade1665264950a02e39
saisi <saisi@users.noreply.github.com>
2016-11-27 08:49:22 +0000

    1480236562 agorot

---
+++ 1480236562.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      475715427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-7L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x4351CDCBL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 7L;
+static uint64_t g_14 = 0xE9C96C7979B38951LL;
+static uint64_t g_18 = 0x373B2C57E71F09A2LL;
+static uint64_t g_35 = 18446744073709551615UL;
+static uint64_t *g_58 = &g_35;
+static int32_t g_66 = 0xC36C1D66L;
+static uint8_t g_69 = 1UL;
