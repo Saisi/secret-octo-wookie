ab1a330af6517688c6c8f86b829371637169977c
saisi <saisi@users.noreply.github.com>
2016-11-27 01:48:57 +0000

    1480211336 anadicrotism

---
+++ 1480211336.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      450289199
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   const volatile signed f1 : 27;
+   unsigned f2 : 5;
+};
+
+struct S1 {
+   const volatile struct S0  f0;
+   uint32_t  f1;
+   signed f2 : 4;
