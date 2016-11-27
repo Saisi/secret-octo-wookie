20a5a9a5fececf0ea0ae7a35406fd178f4e02d27
saisi <saisi@users.noreply.github.com>
2016-11-27 19:49:57 +0000

    1480276197 arbitraments

---
+++ 1480276197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3395202293
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_7 = 0x31L;
+static int8_t g_12 = 0xCDL;
+static int8_t *g_11 = &g_12;
+static uint8_t g_22 = 0UL;
+static int64_t g_23[6][1][3] = {{{1L,1L,1L}},{{0x3A79CC099A56718ELL,0x3A79CC099A56718ELL,0x3A79CC099A56718ELL}},{{1L,1L,1L}},{{0x3A79CC099A56718ELL,0x3A79CC099A56718ELL,0x3A79CC099A56718ELL}},{{1L,1L,1L}},{{0x3A79CC099A56718ELL,0x3A79CC099A56718ELL,0x3A79CC099A56718ELL}}};
