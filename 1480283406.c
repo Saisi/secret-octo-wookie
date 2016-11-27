17612d53a30dceab6960e2b108daafe5b1c0886a
saisi <saisi@users.noreply.github.com>
2016-11-27 21:50:06 +0000

    1480283406 adriatic

---
+++ 1480283406.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1789181198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[5][10][1] = {{{0x15DE05DAL},{0x5A60D005L},{(-9L)},{(-1L)},{(-9L)},{0x5A60D005L},{0x15DE05DAL},{1L},{0x63E8E1B8L},{(-1L)}},{{0xFDEB7727L},{(-1L)},{0x15DE05DAL},{(-1L)},{0xFDEB7727L},{(-1L)},{0x63E8E1B8L},{1L},{0x15DE05DAL},{0x5A60D005L}},{{(-9L)},{(-1L)},{(-9L)},{0x5A60D005L},{0x15DE05DAL},{1L},{0x63E8E1B8L},{(-1L)},{0xFDEB7727L},{(-1L)}},{{0x15DE05DAL},{(-1L)},{0xFDEB7727L},{(-1L)},{0x63E8E1B8L},{1L},{0x15DE05DAL},{0x5A60D005L},{(-9L)},{(-1L)}},{{(-9L)},{0x5A60D005L},{0x15DE05DAL},{1L},{0x63E8E1B8L},{(-1L)},{0x15DE05DAL},{0xF23E93D0L},{0xF934F545L},{0xF23E93D0L}}};
+static volatile int64_t g_8 = 0xF1D91A4AC155EA0DLL;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
