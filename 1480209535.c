9a662656c3e6e5ae86a4428420c3833dc730d8b4
saisi <saisi@users.noreply.github.com>
2016-11-27 01:18:55 +0000

    1480209535 alastrim

---
+++ 1480209535.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1757152237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 21;
+   const signed f1 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6 = 0x70L;
+static int32_t g_20 = 0x0031AA08L;
+static uint8_t g_33[7] = {4UL,4UL,4UL,4UL,4UL,4UL,4UL};
+static uint32_t g_49[4][1][3] = {{{4294967291UL,8UL,8UL}},{{8UL,0UL,0xC63B8821L}},{{4294967291UL,0UL,4294967291UL}},{{0x694A2199L,8UL,0xC63B8821L}}};
