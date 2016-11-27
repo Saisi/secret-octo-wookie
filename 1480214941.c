83a15ecb558644c1c3551d02b71cc32787f48d04
saisi <saisi@users.noreply.github.com>
2016-11-27 02:49:01 +0000

    1480214941 backiebird

---
+++ 1480214941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1329688870
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   signed f1 : 18;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_4 = (-7L);
+static uint8_t g_14[5][1] = {{0x4BL},{0xE0L},{0x4BL},{0xE0L},{0x4BL}};
+static uint8_t g_39[1] = {0x94L};
+static uint8_t *g_38 = &g_39[0];
