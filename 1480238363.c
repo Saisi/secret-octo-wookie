e036f594c8cccbfc6c54ed1492aff0de088eae25
saisi <saisi@users.noreply.github.com>
2016-11-27 09:19:24 +0000

    1480238363 architecturalist

---
+++ 1480238363.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2766599090
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+   int16_t  f1;
+   uint32_t  f2;
+   signed f3 : 30;
+   volatile int32_t  f4;
+   const volatile uint8_t  f5;
+   const volatile int8_t  f6;
