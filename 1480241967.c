ff21d74a7cdfac1b030d01793a85d1a2859bfd43
saisi <saisi@users.noreply.github.com>
2016-11-27 10:19:28 +0000

    1480241967 asexualization

---
+++ 1480241967.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2656142074
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB69B2E41L;
+static uint32_t g_7 = 0x2065C829L;
+static int32_t g_15 = (-1L);
+static uint16_t g_73 = 65535UL;
+static uint16_t g_79[3][3] = {{1UL,1UL,1UL},{1UL,1UL,1UL},{1UL,1UL,1UL}};
+static int32_t g_82 = 0L;
+static int32_t * volatile g_81 = &g_82;/* VOLATILE GLOBAL g_81 */
+static int8_t g_90[9] = {(-1L),0x71L,(-1L),0x71L,(-1L),0x71L,(-1L),0x71L,(-1L)};
+static uint32_t g_105[7][7][1] = {{{4294967294UL},{1UL},{1UL},{1UL},{4294967294UL},{4294967295UL},{0xABB8780EL}},{{0x7D236808L},{0xABB8780EL},{4294967295UL},{4294967294UL},{1UL},{1UL},{1UL}},{{4294967294UL},{4294967295UL},{0xABB8780EL},{0x7D236808L},{0xABB8780EL},{4294967295UL},{4294967294UL}},{{1UL},{1UL},{1UL},{4294967294UL},{4294967295UL},{0xABB8780EL},{0x7D236808L}},{{1UL},{0x99732ED8L},{1UL},{0x7D236808L},{4294967294UL},{0x7D236808L},{1UL}},{{0x99732ED8L},{1UL},{4294967295UL},{1UL},{0x99732ED8L},{1UL},{0x7D236808L}},{{4294967294UL},{0x7D236808L},{1UL},{0x99732ED8L},{1UL},{4294967295UL},{1UL}}};
