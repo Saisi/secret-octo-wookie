5033a555e248d23c73aa941c510d1ff6e5e63528
saisi <saisi@users.noreply.github.com>
2016-11-27 07:19:18 +0000

    1480231158 antibasilican

---
+++ 1480231158.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1086523712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   signed f1 : 11;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_10 = 0x2A8DL;
+static int64_t g_26 = (-1L);
+static int8_t g_36 = 0x7CL;
+static int16_t g_65 = 1L;
