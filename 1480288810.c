1fd0ec9d8ca7e1c2615c2a48cca3b1a77a061189
saisi <saisi@users.noreply.github.com>
2016-11-27 23:20:10 +0000

    1480288810 arugola

---
+++ 1480288810.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3776410024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   volatile int64_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
+   int32_t  f4;
+   const int64_t  f5;
+   int32_t  f6;
