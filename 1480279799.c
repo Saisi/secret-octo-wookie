10019138ca147992eee499df1a1add3feb1fe02f
saisi <saisi@users.noreply.github.com>
2016-11-27 20:50:00 +0000

    1480279799 antichristian

---
+++ 1480279799.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3331392116
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 8;
+   volatile unsigned f1 : 29;
+   const volatile unsigned f2 : 11;
+   signed f3 : 11;
+   signed f4 : 14;
+};
+#pragma pack(pop)
