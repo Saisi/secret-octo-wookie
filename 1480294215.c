6647b5aff85a734da25227cb5fcb3424e3ad81f0
saisi <saisi@users.noreply.github.com>
2016-11-28 00:50:16 +0000

    1480294215 antipuritan

---
+++ 1480294215.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1351838437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   unsigned f1 : 4;
+   volatile int32_t  f2;
+   const uint32_t  f3;
+};
+
+union U1 {
+   volatile int8_t * f0;
+};
