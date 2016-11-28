5632c56496c2a23049c3c56247442ff74cf680cb
saisi <saisi@users.noreply.github.com>
2016-11-28 03:50:28 +0000

    1480305027 appanage

---
+++ 1480305027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3220409201
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   const volatile signed f1 : 18;
+   const uint8_t  f2;
+   volatile uint32_t  f3;
+   int32_t  f4;
+   const volatile uint32_t  f5;
+   int16_t  f6;
+   int16_t  f7;
+   volatile int32_t  f8;
