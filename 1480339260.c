f4b6ba0cb1a4ffcc15088c0038e2a08a016d8f93
saisi <saisi@users.noreply.github.com>
2016-11-28 13:21:03 +0000

    1480339260 backchat

---
+++ 1480339260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1207671282
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   uint32_t  f1;
+   const volatile signed f2 : 31;
+   int64_t  f3;
+   volatile uint8_t  f4;
+   uint32_t  f5;
+   int32_t  f6;
+   const int16_t  f7;
+   const int8_t  f8;
