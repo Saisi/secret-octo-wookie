ab27667db638ac365f1f87af2b6a2154638c4992
saisi <saisi@users.noreply.github.com>
2016-11-28 22:51:49 +0000

    1480373508 angiostomize

---
+++ 1480373508.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3395133450
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 28;
+   unsigned f2 : 14;
+   unsigned f3 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_9 = 0x15L;
+static uint16_t g_51 = 0x1E34L;
