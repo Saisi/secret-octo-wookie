8b622050bc4b2f37f3477bb5bc23177fc6dad0c9
saisi <saisi@users.noreply.github.com>
2016-11-28 20:51:39 +0000

    1480366297 aporias

---
+++ 1480366297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1283072736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint32_t  f1;
+   uint32_t  f2;
+   volatile int8_t  f3;
+   volatile int32_t  f4;
+   uint32_t  f5;
+   signed f6 : 22;
+   unsigned f7 : 20;
+   const int8_t  f8;
