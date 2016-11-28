06370396d6aa2f9b62efdc19709b002332acb9b6
saisi <saisi@users.noreply.github.com>
2016-11-28 10:20:49 +0000

    1480328449 abrasax

---
+++ 1480328449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3694533822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 13;
+   unsigned f1 : 14;
+   volatile unsigned f2 : 24;
+   volatile unsigned f3 : 4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
