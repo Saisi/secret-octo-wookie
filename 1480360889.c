8525aa66cee5d9f8621e41e6125d79e24c189cec
saisi <saisi@users.noreply.github.com>
2016-11-28 19:21:30 +0000

    1480360889 amphibalus

---
+++ 1480360889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1686418117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 1;
+   unsigned f1 : 20;
+   volatile uint16_t  f2;
+   const signed f3 : 22;
+   unsigned f4 : 15;
+   volatile signed f5 : 12;
+   signed f6 : 27;
+   const volatile signed f7 : 2;
+};
