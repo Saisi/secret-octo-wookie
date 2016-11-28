2b42c060d5aeffbbabd5e79405039a768f2fe4b0
saisi <saisi@users.noreply.github.com>
2016-11-28 04:20:29 +0000

    1480306829 amoraic

---
+++ 1480306829.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3196773573
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_3[5] = {0x3C4AB274BF90EDCALL,0x3C4AB274BF90EDCALL,0x3C4AB274BF90EDCALL,0x3C4AB274BF90EDCALL,0x3C4AB274BF90EDCALL};
+static int8_t g_5 = 0x53L;
+static uint8_t g_35 = 0x04L;
+static int8_t g_50 = 5L;
+static uint32_t g_69 = 0UL;
+static const uint8_t *g_75 = &g_35;
+static const uint8_t ** const g_74 = &g_75;
+static int32_t g_80 = 0L;
+static uint8_t g_86 = 0x31L;
