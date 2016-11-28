80fdd12cc5e2ea485189a1e389b191a1b96dfebf
saisi <saisi@users.noreply.github.com>
2016-11-28 21:21:40 +0000

    1480368100 ambulators

---
+++ 1480368100.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1958652166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+};
+
+union U1 {
+   volatile struct S0  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
