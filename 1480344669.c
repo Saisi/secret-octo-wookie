88e92357080dfed8e2ae509a23b1ebd6f91a2028
saisi <saisi@users.noreply.github.com>
2016-11-28 14:51:10 +0000

    1480344669 arbusta

---
+++ 1480344669.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3030874007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 10;
+   signed f1 : 25;
+   signed f2 : 24;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 12;
