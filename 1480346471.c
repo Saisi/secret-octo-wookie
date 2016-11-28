e9c032b65efbc31fa2a22c18f05cb30dea2b518f
saisi <saisi@users.noreply.github.com>
2016-11-28 15:21:11 +0000

    1480346471 Alarbus

---
+++ 1480346471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2013817716
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   int8_t  f2;
+   uint16_t  f3;
+   const int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0xBF8F0AA6L;
