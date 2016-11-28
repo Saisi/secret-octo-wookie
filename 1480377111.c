0790d6a6cb5ccb936d284a95e01f8ccc1baf5073
saisi <saisi@users.noreply.github.com>
2016-11-28 23:51:52 +0000

    1480377111 adrenolysis

---
+++ 1480377111.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4226847492
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_31[6] = {1L,1L,1L,1L,1L,1L};
+static int32_t g_37 = (-5L);
+static int32_t g_40[6][9] = {{1L,0L,0x1940C9BAL,0xF5E4D8D9L,0xEB16E2C9L,0xEB16E2C9L,0xF5E4D8D9L,0x1940C9BAL,0L},{(-1L),0xEFC72FA5L,(-3L),(-1L),0x249687FDL,0xEB16E2C9L,(-1L),0xBFCF419CL,1L},{(-1L),0xF5E4D8D9L,0xEFC72FA5L,(-1L),0x1940C9BAL,0xBFCF419CL,0x1940C9BAL,(-1L),0xEFC72FA5L},{0xEFC72FA5L,0xEFC72FA5L,0x8720725FL,1L,(-4L),0xB76E1EE6L,0x1940C9BAL,2L,(-1L)},{(-1L),0L,8L,(-3L),0x249687FDL,0xB0420410L,0xB0420410L,0x249687FDL,0xEFC72FA5L},{(-1L),0L,(-1L),0x8720725FL,0xF5E4D8D9L,(-1L),2L,0x249687FDL,8L}};
+static volatile int32_t g_41[8][2] = {{3L,0L},{0L,3L},{0L,0L},{3L,0L},{0L,3L},{0L,0L},{3L,0L},{0L,3L}};
+static int32_t g_42 = 0x8A87EA82L;
