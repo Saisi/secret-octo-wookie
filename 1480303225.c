3ba3d76081afd9fa6978c09437ff8f7d84897949
saisi <saisi@users.noreply.github.com>
2016-11-28 03:20:26 +0000

    1480303225 Aundrea

---
+++ 1480303225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4280708095
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_21[2][2][1] = {{{0xA278C007L},{0xA278C007L}},{{0xA278C007L},{0xA278C007L}}};
+static int8_t g_46[4][6][3] = {{{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL}},{{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL}},{{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL}},{{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL},{(-1L),(-1L),(-1L)},{0x9AL,0x9AL,0x9AL}}};
+static int8_t g_73 = 1L;
+static uint64_t g_76 = 1UL;
+static int8_t g_88 = 0xCAL;
+static const int8_t *g_87 = &g_88;
+static uint64_t g_90 = 0UL;
+static uint64_t *g_89 = &g_90;
+static int32_t g_92 = 1L;
