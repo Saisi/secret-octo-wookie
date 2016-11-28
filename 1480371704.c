552857060d26d7dc2f2b181735ff07d0e284c5db
saisi <saisi@users.noreply.github.com>
2016-11-28 22:21:46 +0000

    1480371704 bandle

---
+++ 1480371704.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3229454373
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 17;
+   int32_t  f1;
+   const unsigned f2 : 4;
+   signed f3 : 24;
+   unsigned f4 : 31;
+   const signed f5 : 17;
+   signed f6 : 16;
