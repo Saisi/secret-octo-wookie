7f4f182e408686cd20889d30bd175359f504d9a2
saisi <saisi@users.noreply.github.com>
2016-11-28 00:20:14 +0000

    1480292413 apartment

---
+++ 1480292413.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2168837319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   signed f1 : 17;
+   volatile unsigned f2 : 16;
+   const signed f3 : 27;
+   signed : 0;
+   unsigned f4 : 25;
+   signed f5 : 20;
+   const signed f6 : 23;
+   unsigned f7 : 30;
