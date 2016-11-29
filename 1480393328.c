f643eb0ae94f95fa59700a1613502560ea4ee643
saisi <saisi@users.noreply.github.com>
2016-11-29 04:22:08 +0000

    1480393328 balteus

---
+++ 1480393328.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3525139792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB2906C90L;
+static int16_t g_9[8] = {0x89B5L,1L,0x89B5L,0x89B5L,1L,0x89B5L,0x89B5L,1L};
+static int32_t g_11 = (-1L);
+static int8_t g_20[2] = {(-1L),(-1L)};
+static uint32_t g_43[10][2] = {{0UL,0UL},{0x618D7C58L,0UL},{0UL,0x618D7C58L},{0UL,0UL},{0x618D7C58L,0UL},{0UL,0x618D7C58L},{0UL,0UL},{0x618D7C58L,0UL},{0UL,0x618D7C58L},{0UL,0UL}};
+static uint8_t g_75 = 0xD3L;
+static volatile int32_t g_85 = 9L;/* VOLATILE GLOBAL g_85 */
+static int32_t g_86 = 0x2CBD0316L;
+static int32_t g_95 = (-2L);
