ae474f7b9321df67a6ee1d988cf642d390c997fd
saisi <saisi@users.noreply.github.com>
2016-11-29 13:52:41 +0000

    1480427561 banns

---
+++ 1480427561.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3338135247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_4 = 0UL;
+static int32_t g_21 = 0xAE49A2F3L;
+static volatile uint8_t g_27 = 255UL;/* VOLATILE GLOBAL g_27 */
+static int32_t g_68 = (-1L);
+static int32_t g_111 = 0x92B64398L;
+static int32_t g_118 = 0xD104A743L;
+static uint8_t g_128 = 0x2FL;
+static volatile uint64_t g_139[1] = {0x53EA2BDD4A0C5CC1LL};
+static int32_t *g_161[8][2][2] = {{{&g_118,&g_21},{&g_68,&g_21}},{{&g_118,&g_118},{&g_21,(void*)0}},{{&g_21,(void*)0},{&g_21,&g_118}},{{&g_21,&g_21},{&g_21,&g_118}},{{&g_21,&g_21},{&g_21,&g_118}},{{&g_21,(void*)0},{&g_21,(void*)0}},{{&g_21,&g_118},{&g_118,&g_21}},{{&g_68,&g_21},{&g_118,&g_118}}};
