5eb991b03b874a2cb1d8748d3d6750e754d3c1d9
saisi <saisi@users.noreply.github.com>
2016-11-29 00:21:53 +0000

    1480378913 acquisitive

---
+++ 1480378913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2692084864
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S0 g_2 = {0x43L};/* VOLATILE GLOBAL g_2 */
+static volatile struct S0 * volatile g_3 = &g_2;/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
