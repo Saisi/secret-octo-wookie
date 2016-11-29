72655fad13039bdd17f917ca0e262c4de3b0b8d9
saisi <saisi@users.noreply.github.com>
2016-11-29 12:52:38 +0000

    1480423957 bandelet

---
+++ 1480423957.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      404472795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   uint8_t  f1;
+   int8_t * const  volatile  f2;
+};
+
+union U1 {
+   unsigned f0 : 23;
+   const volatile int16_t  f1;
+   uint32_t  f2;
