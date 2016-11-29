c0c41872faf3ffc874476300ed7ffb59b92221b5
saisi <saisi@users.noreply.github.com>
2016-11-29 12:22:36 +0000

    1480422156 antieducationalist

---
+++ 1480422156.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2008826294
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 26;
+   signed f2 : 20;
+   const signed f3 : 16;
+   signed f4 : 31;
+   volatile unsigned f5 : 26;
+   signed f6 : 16;
