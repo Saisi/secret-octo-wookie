61e45f11e7ab823424514fce492498d9c52e25de
saisi <saisi@users.noreply.github.com>
2016-11-29 18:53:05 +0000

    1480445584 aurocyanide

---
+++ 1480445584.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1585512068
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_8[1][5] = {{6UL,6UL,6UL,6UL,6UL}};
+static uint8_t g_75[8][5] = {{0x1FL,0xA8L,1UL,0xEEL,0x87L},{255UL,0xB9L,0xE2L,0xE2L,0xB9L},{0x1FL,0xA8L,1UL,0xEEL,0x87L},{255UL,0xB9L,0xE2L,0xE2L,0xB9L},{0x1FL,0xA8L,1UL,0xEEL,0x87L},{255UL,0xB9L,0xE2L,0xE2L,0xB9L},{0x1FL,0xA8L,1UL,0xEEL,0x87L},{255UL,0xB9L,0xE2L,0xE2L,0xB9L}};
+static int8_t g_77 = 4L;
+static volatile uint8_t g_87 = 0xEDL;/* VOLATILE GLOBAL g_87 */
+static volatile uint8_t *g_86 = &g_87;
