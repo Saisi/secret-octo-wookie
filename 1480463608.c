e99778bf383392cb72c824f829099abb21df36aa
saisi <saisi@users.noreply.github.com>
2016-11-29 23:53:28 +0000

    1480463608 archeology

---
+++ 1480463608.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3878582168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+   unsigned f1 : 7;
+   signed f2 : 13;
+   const signed f3 : 5;
+   unsigned : 0;
+   unsigned f4 : 23;
+   volatile unsigned f5 : 25;
+};
+
