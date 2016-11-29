66153ab0e3cd50f4decee517e6bc442b9d8436df
saisi <saisi@users.noreply.github.com>
2016-11-29 22:53:25 +0000

    1480460005 astigmatizer

---
+++ 1480460005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1347629438
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_19 = 1UL;/* VOLATILE GLOBAL g_19 */
+static volatile int32_t g_25 = 1L;/* VOLATILE GLOBAL g_25 */
+static volatile int32_t *g_24[2][8] = {{&g_25,&g_25,&g_25,&g_25,&g_25,&g_25,&g_25,&g_25},{&g_25,&g_25,&g_25,&g_25,&g_25,&g_25,&g_25,&g_25}};
+static uint32_t g_34 = 1UL;
+static int32_t g_37 = (-8L);
+static uint16_t g_44 = 0x9B4CL;
+static uint32_t g_77 = 0x2B4B20F9L;
+static uint64_t g_84 = 0xFCB68B63C96AADB6LL;
+static uint8_t g_86 = 5UL;
