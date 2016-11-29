01411f3238a4ad0454221b47db0a4da30720b706
saisi <saisi@users.noreply.github.com>
2016-11-29 04:52:10 +0000

    1480395129 arithmograph

---
+++ 1480395129.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1997098799
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static const uint32_t g_16[2] = {5UL,5UL};
+static int32_t g_20 = 0L;
+static uint32_t g_31 = 1UL;
+static uint32_t g_69[3][5] = {{18446744073709551613UL,0xC33196ADL,0xC33196ADL,18446744073709551613UL,0xC33196ADL},{18446744073709551613UL,18446744073709551613UL,0x9EF32124L,18446744073709551613UL,18446744073709551613UL},{0xC33196ADL,18446744073709551613UL,0xC33196ADL,0xC33196ADL,18446744073709551613UL}};
+static uint32_t *g_68[7] = {&g_69[2][1],&g_69[2][1],&g_69[2][1],&g_69[2][1],&g_69[2][1],&g_69[2][1],&g_69[2][1]};
+static int32_t *g_74 = (void*)0;
+static uint8_t g_80 = 0x9EL;
+static uint32_t g_99 = 0xADAB70C6L;
