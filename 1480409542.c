cc0a1920882c3cdad4e5e9d666fa77bb27b5beca
saisi <saisi@users.noreply.github.com>
2016-11-29 08:52:23 +0000

    1480409542 athetizing

---
+++ 1480409542.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1360281486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 21;
+   unsigned f1 : 22;
+};
+#pragma pack(pop)
+
+union U1 {
+   volatile signed f0 : 8;
