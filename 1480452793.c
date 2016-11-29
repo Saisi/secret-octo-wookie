262e266836aa02eafbf285a26de84be4c628518f
saisi <saisi@users.noreply.github.com>
2016-11-29 20:53:13 +0000

    1480452793 baramin

---
+++ 1480452793.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      786536028
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 7L;
+static int32_t g_6 = (-2L);
+static int16_t g_28 = 1L;
+static volatile int32_t g_44 = 0xEF66F2FCL;/* VOLATILE GLOBAL g_44 */
+static int32_t g_45 = (-1L);
+static int32_t g_49 = (-1L);
+static int32_t g_52 = 0x58079396L;
+static int16_t g_81 = (-1L);
+static int32_t g_86 = 4L;
