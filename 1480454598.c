570d7bdea2e81db02a009c7529668a5d097b7a19
saisi <saisi@users.noreply.github.com>
2016-11-29 21:23:18 +0000

    1480454598 affusions

---
+++ 1480454598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4282515381
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   signed f1 : 12;
+   signed f2 : 31;
+   signed f3 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = (-7L);
+static int8_t g_24 = 0L;
