679bf3a72c5a6c38ddf3122820bc1fc7be5cdb90
saisi <saisi@users.noreply.github.com>
2016-11-29 13:22:39 +0000

    1480425759 bacilluria

---
+++ 1480425759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      292150825
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * volatile  f0;
+   volatile int8_t * f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 0x54L;
+static uint8_t g_27[10] = {246UL,0xA0L,246UL,246UL,0xA0L,246UL,246UL,0xA0L,246UL,246UL};
+static int8_t * const g_34 = &g_9;
