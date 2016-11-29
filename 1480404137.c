c1b2956e204aa2469967cd70f1b0c6caae589e59
saisi <saisi@users.noreply.github.com>
2016-11-29 07:22:18 +0000

    1480404137 apophyses

---
+++ 1480404137.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      424487843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   unsigned f1 : 7;
+   unsigned f2 : 14;
+   uint64_t  f3;
+   unsigned f4 : 10;
+   const volatile unsigned f5 : 16;
+   volatile unsigned f6 : 19;
