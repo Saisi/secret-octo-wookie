f22894f82c4b841026ddbf67fddfdc83fde579ed
saisi <saisi@users.noreply.github.com>
2016-11-29 21:53:20 +0000

    1480456400 analemmatic

---
+++ 1480456400.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3270874138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_10 = 0x42L;
+static uint8_t g_38 = 255UL;
+static uint8_t g_40 = 0x88L;
+static int32_t g_51 = (-1L);
+static int8_t g_53 = (-4L);
+static int32_t *g_86 = (void*)0;
+static int32_t **g_85 = &g_86;
+static int32_t g_105[2] = {0xBB371DABL,0xBB371DABL};
+static int32_t * volatile g_104 = &g_105[1];/* VOLATILE GLOBAL g_104 */
