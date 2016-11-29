388dd51a979953d366910b4788f98348f09ae75b
saisi <saisi@users.noreply.github.com>
2016-11-29 06:52:16 +0000

    1480402336 amphistomoid

---
+++ 1480402336.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2290679875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[2][2] = {{0x351CDCB3L,0x351CDCB3L},{0x351CDCB3L,0x351CDCB3L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
