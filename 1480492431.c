d8233f4da4bcb224f68f8d79e6dd9fd67095cb98
saisi <saisi@users.noreply.github.com>
2016-11-30 07:53:52 +0000

    1480492431 alienisms

---
+++ 1480492431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      167621042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   int8_t * volatile  f1;
+   uint32_t  f2;
+   volatile int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_9 = 18446744073709551615UL;/* VOLATILE GLOBAL g_9 */
+static uint32_t g_12 = 0xA9C21324L;
