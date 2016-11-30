4c9f7232ab1f432754c2b610cc61a39fe42788f0
saisi <saisi@users.noreply.github.com>
2016-11-30 21:24:46 +0000

    1480541086 alowe

---
+++ 1480541086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      70230246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-10L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[6][7][3] = {{{(-7L),(-7L),(-1L)},{0xF050678EL,1L,(-7L)},{(-7L),0x0B101AAEL,0xB064BAE0L},{0xBB8BFFBEL,0xA5F9C710L,0xB064BAE0L},{(-5L),(-10L),(-7L)},{4L,0x45DCEFBDL,(-1L)},{0xB064BAE0L,(-10L),4L}},{{(-10L),0xA5F9C710L,(-4L)},{(-10L),0x0B101AAEL,4L},{0xB064BAE0L,1L,(-1L)},{4L,(-7L),4L},{(-5L),(-9L),(-4L)},{0xBB8BFFBEL,(-9L),4L},{(-7L),(-7L),(-1L)}},{{0xF050678EL,1L,(-7L)},{(-7L),(-4L),(-1L)},{(-1L),(-7L),(-1L)},{0xFE5AB06CL,4L,5L},{0x44ADCA1AL,(-10L),0x744D0289L},{(-1L),4L,1L},{(-6L),(-7L),0xB9A733E3L}},{{(-6L),(-4L),0x44ADCA1AL},{(-1L),0xF050678EL,0xF0D3ECF0L},{0x44ADCA1AL,(-5L),0x44ADCA1AL},{0xFE5AB06CL,0xBC1B0FE2L,0xB9A733E3L},{(-1L),0xBC1B0FE2L,1L},{5L,(-5L),0x744D0289L},{9L,0xF050678EL,5L}},{{5L,(-4L),(-1L)},{(-1L),(-7L),(-1L)},{0xFE5AB06CL,4L,5L},{0x44ADCA1AL,(-10L),0x744D0289L},{(-1L),4L,1L},{(-6L),(-7L),0xB9A733E3L},{(-6L),(-4L),0x44ADCA1AL}},{{(-1L),0xF050678EL,0xF0D3ECF0L},{0x44ADCA1AL,(-5L),0x44ADCA1AL},{0xFE5AB06CL,0xBC1B0FE2L,0xB9A733E3L},{(-1L),0xBC1B0FE2L,1L},{5L,(-5L),0x744D0289L},{9L,0xF050678EL,5L},{5L,(-4L),(-1L)}}};
+static int32_t g_5 = (-8L);
+static int32_t g_6 = 0x280BFE73L;
+static int32_t g_35 = 9L;
+static int16_t g_76 = 0xFEC8L;
+static int32_t *g_92 = &g_5;
+static int32_t **g_91 = &g_92;
+static uint32_t g_109 = 4294967295UL;
