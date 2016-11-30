ed7d74daa3a4195fb80b9db666cc60191f43e608
saisi <saisi@users.noreply.github.com>
2016-11-30 15:24:20 +0000

    1480519459 archeologist

---
+++ 1480519459.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3909889115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const int8_t * volatile  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x7045B365L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x4D59FF44L;
+static int32_t g_5 = 1L;
+static int16_t g_12 = 0xA928L;
