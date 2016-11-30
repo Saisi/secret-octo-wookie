44452bfd699fc2ece2c59b196c2931ca1f78babd
saisi <saisi@users.noreply.github.com>
2016-11-30 02:53:37 +0000

    1480474416 airlifting

---
+++ 1480474416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1021496227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[2][2] = {{0x2C0D37ADL,0x2C0D37ADL},{0x2C0D37ADL,0x2C0D37ADL}};
+static int32_t g_27 = 3L;
+static int32_t g_30 = 0x3388A019L;
+static int32_t *g_76[10][3] = {{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30},{&g_27,&g_27,&g_30}};
+static uint64_t g_81 = 7UL;
