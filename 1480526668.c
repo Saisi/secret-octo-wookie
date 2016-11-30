2b02e6ef92dbac374f575e8a12cf5f27cba24a37
saisi <saisi@users.noreply.github.com>
2016-11-30 17:24:28 +0000

    1480526668 adiposeness

---
+++ 1480526668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1055470931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint64_t  f0;
+   const volatile int32_t  f1;
+   volatile uint64_t  f2;
+   volatile int16_t  f3;
+   const uint8_t  f4;
+};
+#pragma pack(pop)
