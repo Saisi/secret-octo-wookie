3b4bc1cee24cce63d0b675a61889a5ca147cf118
saisi <saisi@users.noreply.github.com>
2016-11-30 05:23:44 +0000

    1480483424 bams

---
+++ 1480483424.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2321706176
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   uint8_t  f1;
+   int64_t  f2;
+   volatile int16_t  f3;
+};
+
+union U1 {
+   int16_t  f0;
+   int64_t  f1;
