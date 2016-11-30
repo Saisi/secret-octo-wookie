ecb90f3b262e1ce25915a68a556f347ffe0645e1
saisi <saisi@users.noreply.github.com>
2016-11-30 14:54:18 +0000

    1480517657 ammonic

---
+++ 1480517657.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      876218958
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t g_11[3][8] = {{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)}};
+static uint64_t g_13 = 0x93D0B5486B6FEB9ELL;
+static int16_t g_32 = 0L;
+static int32_t g_37[9][1][2] = {{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}},{{0xE5A82A6FL,0xE5A82A6FL}}};
