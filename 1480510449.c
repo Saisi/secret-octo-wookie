7b14dd07445eff5a91fbb0bbecf2fda4858e5854
saisi <saisi@users.noreply.github.com>
2016-11-30 12:54:09 +0000

    1480510449 aureoles

---
+++ 1480510449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      549771326
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 13;
+   unsigned f1 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t * volatile g_2 = (void*)0;/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
