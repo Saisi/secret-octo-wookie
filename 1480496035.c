40f899d181c3e35533357d1e27896a966a86908f
saisi <saisi@users.noreply.github.com>
2016-11-30 08:53:55 +0000

    1480496035 babbools

---
+++ 1480496035.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3105868583
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_7 = 0L;/* VOLATILE GLOBAL g_7 */
+static uint8_t g_13 = 0x1AL;
+static int32_t g_15 = 0L;
+static uint16_t g_24 = 0x4B0BL;
+static uint16_t g_44 = 0x6EA1L;
+static uint16_t *g_43[7] = {&g_44,&g_44,&g_44,&g_44,&g_44,&g_44,&g_44};
+static int32_t *g_55 = &g_15;
+static int32_t **g_54 = &g_55;
+static uint64_t g_76 = 0x47684A5798870B37LL;
