32e42d4dd37dcf1bf4c2b28f69e662b8ed08bba3
saisi <saisi@users.noreply.github.com>
2016-11-30 22:24:56 +0000

    1480544694 airparks

---
+++ 1480544694.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1256664708
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   int8_t  f3;
+   uint8_t  f4;
+};
+#pragma pack(pop)
