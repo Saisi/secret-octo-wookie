549d0b11c5e0f222705a66d0648fc28bc2370bc1
saisi <saisi@users.noreply.github.com>
2016-11-30 06:23:47 +0000

    1480487027 ambodexter

---
+++ 1480487027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3910788008
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 2;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1E87DF2FL;
+static int8_t g_11 = (-1L);
