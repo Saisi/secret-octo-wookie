2c7f4f82dece5ddbb6e61336c751930a0e96aa34
saisi <saisi@users.noreply.github.com>
2016-11-30 17:54:30 +0000

    1480528470 antisexist

---
+++ 1480528470.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3954445354
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_45 = (-5L);
+static uint16_t g_70[10] = {65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL};
+static int16_t g_76 = 1L;
+static int16_t g_78 = 0x4DCCL;
+static int16_t g_80 = 1L;
+static uint16_t *g_87[1] = {&g_70[0]};
+static uint16_t **g_86 = &g_87[0];
