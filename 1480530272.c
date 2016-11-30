714f15de9ffc3ec2df38475c50d373b2a8e34a09
saisi <saisi@users.noreply.github.com>
2016-11-30 18:24:32 +0000

    1480530272 adreno

---
+++ 1480530272.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      458494567
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int32_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+   const volatile uint64_t  f3;
+   const uint64_t  f4;
+};
+#pragma pack(pop)
