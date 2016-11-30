6931be44ca5a82eabd224f5b9a5a3f0c15088e20
saisi <saisi@users.noreply.github.com>
2016-11-30 23:55:07 +0000

    1480550106 AngloCatholicism

---
+++ 1480550106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1032437241
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_7 = 0x7D45DB34E6D4F288LL;
+static int32_t g_13 = 5L;
+static int32_t *g_14 = (void*)0;
+static const int32_t **g_25 = (void*)0;
+static int32_t *g_27 = &g_13;
+static int32_t * const *g_26 = &g_27;
+static int64_t g_37 = 0x72737AE9F9034E10LL;
+static uint8_t g_38 = 1UL;
+static int32_t g_51[5] = {0x169B41DBL,0x169B41DBL,0x169B41DBL,0x169B41DBL,0x169B41DBL};
