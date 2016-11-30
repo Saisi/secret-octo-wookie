5310f572d05c852602a88bb563c2737d7ca30929
saisi <saisi@users.noreply.github.com>
2016-11-30 16:54:27 +0000

    1480524866 archicerebrum

---
+++ 1480524866.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      321571732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_7 = (-1L);
+static int32_t g_9 = 0x4BF8F0AAL;
+static int32_t g_12[6] = {(-5L),(-5L),(-5L),(-5L),(-5L),(-5L)};
+static int8_t g_24 = 1L;
+static int32_t ***g_39 = (void*)0;
+static int8_t g_49 = (-8L);
+static volatile int8_t g_60 = 0L;/* VOLATILE GLOBAL g_60 */
+static volatile int8_t * volatile g_59 = &g_60;/* VOLATILE GLOBAL g_59 */
+static uint32_t g_62[1][4] = {{8UL,8UL,8UL,8UL}};
