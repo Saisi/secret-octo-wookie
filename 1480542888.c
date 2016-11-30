ad1ede4b4e3420472e5d3813ea591ccd6ecd771d
saisi <saisi@users.noreply.github.com>
2016-11-30 21:54:52 +0000

    1480542888 alsifilm

---
+++ 1480542888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2579318855
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   uint16_t  f1;
+   volatile signed f2 : 9;
+   int32_t  f3;
+   const int32_t  f4;
+   volatile unsigned f5 : 8;
+   uint64_t  f6;
+   int32_t  f7;
+   const int32_t  f8;
