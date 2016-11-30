df81faa03e87f98223bbbb6226714ee2ec92bc61
saisi <saisi@users.noreply.github.com>
2016-11-30 11:24:04 +0000

    1480505044 accademia

---
+++ 1480505044.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3677366087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_2 = (void*)0;
+static int32_t g_9 = (-4L);
+static int64_t g_25 = 8L;
+static uint16_t g_46[4][6] = {{0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L},{0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L},{0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L},{0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L,0x0A83L}};
+static uint64_t g_67 = 1UL;
+static uint64_t g_76 = 8UL;
+static uint32_t g_82 = 0UL;
+static uint64_t g_85 = 1UL;
+static uint16_t g_87 = 0xC00EL;
