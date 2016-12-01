ecb8e39c94f414774e7b5d797d33633e2bb83293
saisi <saisi@users.noreply.github.com>
2016-12-01 01:25:11 +0000

    1480555511 anerotic

---
+++ 1480555511.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2447308552
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = 0xD4EAL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
