1a40fd564993f2e48e802fb9df795d8257a906bc
saisi <saisi@users.noreply.github.com>
2016-12-01 15:56:07 +0000

    1480607765 apostrophus

---
+++ 1480607765.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3395740426
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+union U1 {
+   uint32_t  f0;
+   uint32_t  f1;
+   uint32_t  f2;
+};
+
