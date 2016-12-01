70a564fa0556033183f0be6e3e44e6e05385964a
saisi <saisi@users.noreply.github.com>
2016-12-01 14:56:00 +0000

    1480604159 alderling

---
+++ 1480604159.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2366264835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 0UL;
+static uint8_t g_18 = 0x4AL;
+static uint8_t g_32 = 252UL;
+static int32_t g_61[2] = {1L,1L};
+static int32_t g_65 = 1L;
+static const uint8_t *g_88 = &g_32;
+static const uint8_t * volatile *g_87 = &g_88;
+static int8_t g_94 = 4L;
+static uint32_t g_99 = 0x1ABBC910L;
