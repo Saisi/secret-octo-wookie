5a62465e68e58a3394073a6647432989650d5b49
saisi <saisi@users.noreply.github.com>
2016-12-01 21:26:30 +0000

    1480627590 antibank

---
+++ 1480627590.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2746872134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile int8_t * const  volatile  f1;
+   int8_t * f2;
+};
+
+union U1 {
+   const int32_t  f0;
+   uint32_t  f1;
+   volatile uint8_t  f2;
