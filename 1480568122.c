e9f177ea1256485bce7a3644bc6b7894039cfcf8
saisi <saisi@users.noreply.github.com>
2016-12-01 04:55:22 +0000

    1480568122 alastrim

---
+++ 1480568122.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      685620622
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   int64_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static volatile int32_t g_5 = 2L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 1L;/* VOLATILE GLOBAL g_6 */
