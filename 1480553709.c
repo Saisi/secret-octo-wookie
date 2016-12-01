132ab6d9edb10b183c9350ebe68ca5292ca67433
saisi <saisi@users.noreply.github.com>
2016-12-01 00:55:10 +0000

    1480553709 antipapism

---
+++ 1480553709.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3194688507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 6L;
+static uint16_t g_22 = 0xDEC4L;
+static int32_t g_42 = 1L;
+static int8_t g_43 = 0x1DL;
+static uint64_t g_47 = 0x2C9DBE4FCD58F28DLL;
+static int32_t g_78 = 4L;
+static uint8_t g_84 = 250UL;
+static uint64_t g_102 = 0xD67785DC571EBB70LL;
+static int16_t g_112[3][9][2] = {{{(-1L),0xD644L},{0xF839L,(-1L)},{0L,0L},{(-5L),0L},{0L,(-1L)},{0xF839L,0xD644L},{(-1L),6L},{0xA18FL,0xA18FL},{0x0AC9L,1L}},{{(-1L),(-5L)},{0x8044L,1L},{1L,0x8044L},{0xA1D0L,0xA1FFL},{0xA1D0L,0x8044L},{1L,1L},{0x8044L,(-5L)},{(-1L),1L},{0x0AC9L,0xA18FL}},{{0xA18FL,6L},{(-1L),0xD644L},{0xF839L,(-1L)},{0L,0L},{(-5L),0L},{(-1L),0x0AC9L},{0xD5CEL,0xA1FFL},{(-1L),0xA18FL},{0xA1D0L,0xA1D0L}}};
