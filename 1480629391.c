56f098c5b4a903a8081a467e7483c487ff869680
saisi <saisi@users.noreply.github.com>
2016-12-01 21:56:32 +0000

    1480629391 ankerhold

---
+++ 1480629391.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1463683794
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   signed f1 : 16;
+   unsigned f2 : 18;
+   unsigned f3 : 22;
+   volatile signed f4 : 13;
+   signed f5 : 16;
+};
