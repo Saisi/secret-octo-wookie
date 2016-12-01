4ae65c85b0def2a0f001a46bdaa2d171bc748067
saisi <saisi@users.noreply.github.com>
2016-12-01 14:25:58 +0000

    1480602357 achievability

---
+++ 1480602357.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3718878098
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xB9DA4D07L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x82FF40FCL;
+static struct S0 g_64 = {0xD5L,0x57D38C46L};/* VOLATILE GLOBAL g_64 */
+static int32_t *g_67 = &g_64.f1;
