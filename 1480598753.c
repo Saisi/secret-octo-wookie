45547eef492533aac134d6e175f61d3446e44066
saisi <saisi@users.noreply.github.com>
2016-12-01 13:25:53 +0000

    1480598753 acerbityacerose

---
+++ 1480598753.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3799903701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_5 = 0x32BBL;/* VOLATILE GLOBAL g_5 */
+static int8_t g_14 = 0xB6L;
+static int32_t g_30 = 0x9BB7A140L;
+static int8_t g_35 = 1L;
+static const int32_t g_53 = 0x4B6369DDL;
+static const int32_t g_55 = (-1L);
+static const int32_t *g_54 = &g_55;
+static const int32_t * volatile g_79 = (void*)0;/* VOLATILE GLOBAL g_79 */
+static const int32_t * volatile *g_78 = &g_79;
