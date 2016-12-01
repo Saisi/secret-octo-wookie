5b034b56ecbd4b7c40dfc96efb21efd6df2dbb32
saisi <saisi@users.noreply.github.com>
2016-12-01 08:55:36 +0000

    1480582536 amberous

---
+++ 1480582536.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3343264403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_7 = 0x696AL;
+static const int32_t g_11 = 0x7161AB6EL;
+static const int32_t *g_10 = &g_11;
+static int32_t g_23 = 0x1E1D82C3L;
+static int32_t *g_22 = &g_23;
+static uint16_t g_33 = 65535UL;
+static int32_t g_67 = 0xCC772A30L;
+static int32_t g_76 = 0x81B38C87L;
+static uint8_t g_78 = 255UL;
