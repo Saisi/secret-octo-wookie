6804c71cb6808fe7f90f69d4cb1638f6ad0ea7b2
saisi <saisi@users.noreply.github.com>
2016-12-01 05:25:24 +0000

    1480569923 achiotes

---
+++ 1480569923.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4083506643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 31;
+   signed f2 : 6;
+   signed f3 : 25;
+   signed f4 : 6;
+   volatile signed f5 : 7;
+   unsigned f6 : 10;
+};
+
