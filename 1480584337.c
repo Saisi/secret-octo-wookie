a9a441541fd592838efdd151fc4792b076536bce
saisi <saisi@users.noreply.github.com>
2016-12-01 09:25:39 +0000

    1480584337 Bakewell

---
+++ 1480584337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2212882686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   int32_t  f1;
+   int16_t  f2;
+};
+
+union U1 {
+   int8_t  f0;
+   const uint16_t  f1;
+   uint64_t  f2;
