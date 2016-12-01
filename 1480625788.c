b2acb2533452dac47882a4b45cd67177de4f6adc
saisi <saisi@users.noreply.github.com>
2016-12-01 20:56:29 +0000

    1480625788 achondroplasia

---
+++ 1480625788.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4180826944
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 14;
+   signed f1 : 11;
+   signed f2 : 1;
+   unsigned f3 : 9;
+   volatile signed f4 : 20;
+   volatile signed f5 : 27;
+   signed f6 : 13;
+   signed f7 : 14;
+   const volatile unsigned f8 : 26;
