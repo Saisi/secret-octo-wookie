e56de50e658e2e8ec03324049b42059b4007734e
saisi <saisi@users.noreply.github.com>
2016-12-01 19:26:22 +0000

    1480620379 amygdalolith

---
+++ 1480620379.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2090304926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   int16_t  f1;
+   volatile uint64_t  f2;
+   volatile uint64_t  f3;
+   uint32_t  f4;
+   unsigned f5 : 22;
+   volatile int64_t  f6;
+   uint64_t  f7;
+   volatile uint64_t  f8;
