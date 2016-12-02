ec1917fc15e5e7dc0fa460e7c52ca53402fef585
saisi <saisi@users.noreply.github.com>
2016-12-02 15:27:33 +0000

    1480692453 Aramu

---
+++ 1480692453.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2267094186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2[6][4] = {{8UL,8UL,8UL,8UL},{8UL,8UL,8UL,8UL},{8UL,8UL,8UL,8UL},{8UL,8UL,8UL,8UL},{8UL,8UL,8UL,8UL},{8UL,8UL,8UL,8UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
