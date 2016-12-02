8c0a22d7c6e54d9e1cda9779009685663a77706c
saisi <saisi@users.noreply.github.com>
2016-12-02 08:57:13 +0000

    1480669032 achree

---
+++ 1480669032.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      447949198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 21;
+   const uint8_t  f1;
+   int32_t  f2;
+   volatile int8_t  f3;
+   volatile uint64_t  f4;
+   const int8_t  f5;
+   int32_t  f6;
