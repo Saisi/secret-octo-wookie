8f9ab8de3162a06222d11605522287388ab101fa
saisi <saisi@users.noreply.github.com>
2016-12-02 15:57:35 +0000

    1480694254 bangtails

---
+++ 1480694254.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4155005858
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 31;
+   const unsigned f1 : 24;
+   unsigned f2 : 26;
+   unsigned f3 : 30;
+};
+
+struct S1 {
+   int8_t  f0;
+   signed f1 : 5;
