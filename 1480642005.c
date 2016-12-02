525cfc00ac3e806404bb85e3516fa04e873f3e3a
saisi <saisi@users.noreply.github.com>
2016-12-02 01:26:45 +0000

    1480642005 afterbirths

---
+++ 1480642005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2723490479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 1L;
+static int32_t g_15 = 0xF2C36544L;
+static int32_t g_16 = (-1L);
+static int16_t g_43 = 0xA966L;
+static int32_t g_46 = 0x17F95E19L;
+static int32_t g_76 = 0L;
+static int32_t *g_86 = &g_46;
+static int32_t ** volatile g_85 = &g_86;/* VOLATILE GLOBAL g_85 */
+static int32_t ** volatile g_87 = &g_86;/* VOLATILE GLOBAL g_87 */
