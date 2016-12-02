4010b0cf95c1445352c42d219bc003e9b88ca57b
saisi <saisi@users.noreply.github.com>
2016-12-02 20:27:51 +0000

    1480710471 aribine

---
+++ 1480710471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3444122589
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int16_t  f1;
+   volatile unsigned f2 : 19;
+   const uint8_t  f3;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   volatile uint8_t  f1;
