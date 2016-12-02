0778f25868d9c2b788bd9e1a0e4cb4d9491fa291
saisi <saisi@users.noreply.github.com>
2016-12-02 03:56:53 +0000

    1480651013 acenesthesia

---
+++ 1480651013.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1550740691
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   unsigned f1 : 31;
+   unsigned f2 : 6;
+   const signed f3 : 25;
+   volatile unsigned f4 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5][4] = {{(-5L),0xD7953644L,(-5L),(-5L)},{0xD7953644L,0xD7953644L,0x485BF25CL,0xD7953644L},{0xD7953644L,(-5L),(-5L),0xD7953644L},{(-5L),0xD7953644L,(-5L),(-5L)},{0xD7953644L,0xD7953644L,0x485BF25CL,0xD7953644L}};
