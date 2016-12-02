9618abcaf19eeee0679de85271222bab51df83f2
saisi <saisi@users.noreply.github.com>
2016-12-02 11:57:23 +0000

    1480679842 anticancer

---
+++ 1480679842.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      181140971
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_15 = 5UL;
+static int16_t g_20 = 5L;
+static int64_t g_35[3] = {1L,1L,1L};
+static int16_t g_50 = (-1L);
+static int64_t g_70 = 0xDA978833E6FCB758LL;
+static volatile int32_t g_78 = 1L;/* VOLATILE GLOBAL g_78 */
+static volatile int32_t * volatile g_77[1][9] = {{&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78}};
+static int64_t g_81 = 0x57CEC448071C3330LL;
+static int8_t g_121 = 0x61L;
