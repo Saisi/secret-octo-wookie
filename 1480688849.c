c25d24695610ebcac44bc993e338bb7dfe771f03
saisi <saisi@users.noreply.github.com>
2016-12-02 14:27:30 +0000

    1480688849 allopelagic

---
+++ 1480688849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      388639179
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8[5] = {0x0F7A73D2L,0x0F7A73D2L,0x0F7A73D2L,0x0F7A73D2L,0x0F7A73D2L};
+static struct S0 g_14 = {0x3AC4L};
