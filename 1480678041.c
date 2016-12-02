29628d9063a250f0876d395b9d6bfd8da13fffe2
saisi <saisi@users.noreply.github.com>
2016-12-02 11:27:21 +0000

    1480678041 aura

---
+++ 1480678041.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      735564331
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 20;
+   signed f1 : 27;
+   const volatile signed f2 : 18;
+   unsigned f3 : 21;
+   signed f4 : 18;
+};
+
+struct S1 {
+   uint32_t  f0;
