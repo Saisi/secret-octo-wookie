a6b87e16c2d98f9cef12924f38cd6292f5ac35ac
saisi <saisi@users.noreply.github.com>
2016-12-02 07:27:08 +0000

    1480663628 alfaqui

---
+++ 1480663628.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1554501810
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0x58D020BDL;
+static int32_t *g_8[5] = {&g_9,&g_9,&g_9,&g_9,&g_9};
+static int32_t g_31[5][3][1] = {{{0xDC86695CL},{6L},{0xDC86695CL}},{{6L},{0xDC86695CL},{6L}},{{0xDC86695CL},{6L},{0xDC86695CL}},{{6L},{0xDC86695CL},{6L}},{{0xDC86695CL},{6L},{0xDC86695CL}}};
+static uint32_t g_44[5][4] = {{4294967295UL,4294967295UL,4294967295UL,4294967295UL},{4294967295UL,4294967295UL,4294967295UL,4294967295UL},{4294967295UL,4294967295UL,4294967295UL,4294967295UL},{4294967295UL,4294967295UL,4294967295UL,4294967295UL},{4294967295UL,4294967295UL,4294967295UL,4294967295UL}};
+static uint32_t g_46 = 1UL;
