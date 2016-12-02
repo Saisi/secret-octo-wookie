c6f600e30ed0f53bd03e656234817ade406ca60e
saisi <saisi@users.noreply.github.com>
2016-12-02 08:27:11 +0000

    1480667231 Annunciata

---
+++ 1480667231.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2516440128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U1  func_1(void);
+
