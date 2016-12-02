7de8decee797d9c48e521d2e6a1bcbab98fbe134
saisi <saisi@users.noreply.github.com>
2016-12-02 21:57:56 +0000

    1480715875 anthracitic

---
+++ 1480715875.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3981207838
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static volatile int32_t g_6 = 0x83111738L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 1L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 0x8D526D2EL;/* VOLATILE GLOBAL g_8 */
+static int32_t g_9 = 7L;
+static int32_t g_25 = (-1L);
+static int16_t g_28 = 0x8D02L;
+static int16_t g_29 = 0x7CB0L;
+static int8_t g_30[4] = {0L,0L,0L,0L};
