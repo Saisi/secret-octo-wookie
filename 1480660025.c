cf1a31e96d56cac4b6f4ae716fbe40f3af33f710
saisi <saisi@users.noreply.github.com>
2016-12-02 06:27:05 +0000

    1480660025 aphony

---
+++ 1480660025.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      933969627
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 4294967290UL;
+static uint16_t g_25 = 0UL;
+static uint16_t * const g_35[1][1] = {{(void*)0}};
+static int32_t g_43 = (-3L);
+static int32_t g_45 = 7L;
+static int32_t g_67 = 0x32C2674DL;
+static uint32_t g_89 = 0x0CE4AC03L;
+static int32_t g_107 = 0xE98E2CF0L;
+static int8_t g_113 = (-6L);
