b4dd4be986a07083e580ee29f61807656bc5277e
saisi <saisi@users.noreply.github.com>
2016-12-02 10:57:19 +0000

    1480676239 aves

---
+++ 1480676239.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1475308638
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[9] = {0x6834L,0x6834L,0x6834L,0x6834L,0x6834L,0x6834L,0x6834L,0x6834L,0x6834L};
+static int32_t g_52 = 2L;
+static int32_t * volatile g_51 = &g_52;/* VOLATILE GLOBAL g_51 */
+static int32_t *g_64 = &g_52;
+static int16_t g_66 = 0x945DL;
+static int32_t g_83[3] = {0x8378C3A3L,0x8378C3A3L,0x8378C3A3L};
+static int32_t * volatile g_88 = (void*)0;/* VOLATILE GLOBAL g_88 */
+static uint32_t g_90[10] = {0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L,0x5F1E6118L};
+static uint64_t g_93 = 2UL;
