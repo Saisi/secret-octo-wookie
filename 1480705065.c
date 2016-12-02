3af186972d65d4204e28ef69753f4829a41687fd
saisi <saisi@users.noreply.github.com>
2016-12-02 18:57:45 +0000

    1480705065 astringence

---
+++ 1480705065.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      385132670
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[9][1] = {{6L},{6L},{6L},{6L},{6L},{6L},{6L},{6L},{6L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
