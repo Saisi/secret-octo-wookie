b6d481c2f78106af8f612b0bf922c45673a61873
saisi <saisi@users.noreply.github.com>
2016-12-02 12:57:25 +0000

    1480683445 adulteries

---
+++ 1480683445.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      395908367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S2 {
+   uint8_t  f0;
+   uint64_t  f1;
+   signed f2 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint16_t g_2 = 0x403CL;/* VOLATILE GLOBAL g_2 */
+static volatile int64_t g_3 = 0x63A74ADF29855EECLL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_5 = 0x8FB8CEFFL;
