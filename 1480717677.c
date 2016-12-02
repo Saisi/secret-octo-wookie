679fc1a7404c2c13b62b6d4c04e0e5014f0de4b9
saisi <saisi@users.noreply.github.com>
2016-12-02 22:27:58 +0000

    1480717677 autojigger

---
+++ 1480717677.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3553936091
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   int8_t * f1;
+   const volatile uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 5L;
+static volatile int32_t g_4[6] = {0xEFA20BD0L,0xEFA20BD0L,0xEFA20BD0L,0xEFA20BD0L,0xEFA20BD0L,0xEFA20BD0L};
+static int32_t g_5 = 1L;
