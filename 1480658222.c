08e8c72dde1443de252b284b152093029b9c1074
saisi <saisi@users.noreply.github.com>
2016-12-02 05:57:03 +0000

    1480658222 andiroba

---
+++ 1480658222.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3686900613
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 1;
+   signed f1 : 20;
+   volatile signed f2 : 7;
+   const volatile uint32_t  f3;
+   const signed f4 : 15;
+   int64_t  f5;
+   unsigned f6 : 15;
+   signed f7 : 12;
+};
