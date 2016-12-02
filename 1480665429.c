0df164072d886c0efad7f3dc80b07d0b31186b3b
saisi <saisi@users.noreply.github.com>
2016-12-02 07:57:09 +0000

    1480665429 arrogancy

---
+++ 1480665429.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      294995272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0L;
+static uint8_t g_29 = 1UL;
+static int32_t *g_32 = &g_9;
+static uint8_t *g_54 = (void*)0;
+static uint64_t g_58 = 0UL;
+static uint8_t g_60 = 0x98L;
+static const uint8_t *g_59[6] = {&g_60,&g_60,&g_60,&g_60,&g_60,&g_60};
+static volatile uint32_t g_64 = 1UL;/* VOLATILE GLOBAL g_64 */
+static int32_t g_66 = 1L;
