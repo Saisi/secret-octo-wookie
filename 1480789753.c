8e75cdd9dff68285f1f65f6b666a3ece7464481e
saisi <saisi@users.noreply.github.com>
2016-12-03 18:29:13 +0000

    1480789753 balsamum

---
+++ 1480789753.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      254316817
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 28;
+   uint32_t  f2;
+   const uint64_t  f3;
+   const volatile int8_t  f4;
+   uint32_t  f5;
+   const int32_t  f6;
