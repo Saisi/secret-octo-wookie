871a75e824bd760666716ce1532bddf0354488f2
saisi <saisi@users.noreply.github.com>
2016-12-03 02:58:17 +0000

    1480733896 Atfalati

---
+++ 1480733896.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2459858783
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+};
+
+union U1 {
+   int8_t * const  f0;
+   int32_t  f1;
+   volatile uint32_t  f2;
+   uint8_t  f3;
+};
