6e2a5475d1af707fdaf5f2262f83f21a1a2bac02
saisi <saisi@users.noreply.github.com>
2016-12-03 13:58:58 +0000

    1480773538 astrobiologist

---
+++ 1480773538.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1213667516
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 18446744073709551615UL;
+static int32_t g_4[9][7] = {{1L,(-6L),1L,1L,(-6L),1L,1L},{(-6L),(-6L),4L,(-6L),(-6L),4L,(-6L)},{(-6L),1L,1L,(-6L),1L,1L,(-6L)},{1L,(-6L),1L,1L,(-6L),1L,1L},{(-6L),(-6L),4L,(-6L),(-6L),4L,(-6L)},{(-6L),1L,1L,(-6L),1L,1L,(-6L)},{1L,(-6L),1L,1L,1L,4L,4L},{1L,1L,(-6L),1L,1L,(-6L),1L},{1L,4L,4L,1L,4L,4L,1L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
