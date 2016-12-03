615e18ed32a375db0de22d6fa6f88af26ee73088
saisi <saisi@users.noreply.github.com>
2016-12-03 14:59:01 +0000

    1480777141 amebiform

---
+++ 1480777141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3544230278
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_33 = 0x53L;
+static int32_t g_34 = 0x4B064BAEL;
+static int16_t g_38 = 0x678EL;
+static int32_t g_65 = 0L;
+static uint16_t g_74 = 0x35DFL;
