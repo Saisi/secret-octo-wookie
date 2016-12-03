a575745b8164973c2acb7fb7a578b8d12bef4ccc
saisi <saisi@users.noreply.github.com>
2016-12-03 08:58:44 +0000

    1480755521 androconia

---
+++ 1480755521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3864573737
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 19;
+   const unsigned f1 : 30;
+   const signed f2 : 9;
+   int64_t  f3;
+   const unsigned f4 : 9;
+   signed f5 : 19;
+   unsigned f6 : 10;
+   const signed f7 : 28;
+};
