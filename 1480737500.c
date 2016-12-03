c167c9b29d843574b66112d5a0f6cd607fac04b3
saisi <saisi@users.noreply.github.com>
2016-12-03 03:58:21 +0000

    1480737500 bandsaw

---
+++ 1480737500.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3223352041
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 19;
+   volatile unsigned f1 : 30;
+   unsigned f2 : 9;
+   volatile signed f3 : 16;
+};
+#pragma pack(pop)
+
