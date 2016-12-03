dfe492ce2cc0d3f7b4758ec188946ea1e0ca8552
saisi <saisi@users.noreply.github.com>
2016-12-03 01:58:12 +0000

    1480730292 appenzell

---
+++ 1480730292.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3424065714
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_12 = 0x456160CBEB969161LL;
+static int8_t g_16 = (-1L);
+static volatile int32_t g_33 = 3L;/* VOLATILE GLOBAL g_33 */
+static int32_t g_34 = 0xBBBC254EL;
+static int32_t g_59 = (-1L);
