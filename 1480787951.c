a0bc20fda5634954502aa083cfe28d7908a0a898
saisi <saisi@users.noreply.github.com>
2016-12-03 17:59:11 +0000

    1480787951 Arunta

---
+++ 1480787951.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1378001403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+};
+#pragma pack(pop)
+
+struct S1 {
+   volatile unsigned f0 : 3;
+   unsigned f1 : 25;
