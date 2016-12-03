c867345a5a7295f5eb67d4354ed3589d1604fef1
saisi <saisi@users.noreply.github.com>
2016-12-03 04:58:24 +0000

    1480741103 archhypocrisy

---
+++ 1480741103.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1111220025
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint64_t  f1;
+   const uint32_t  f2;
+   int32_t  f3;
+   int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-6L);/* VOLATILE GLOBAL g_3 */
