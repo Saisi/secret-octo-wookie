d9466df3f5d3c76367e3f181cce52b2d2e70077f
saisi <saisi@users.noreply.github.com>
2016-12-03 16:29:07 +0000

    1480782547 Anthomyia

---
+++ 1480782547.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1501763204
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_12 = 1L;
+static uint16_t g_23 = 0x725DL;
+static int32_t g_28 = 0xEF9A8551L;
+static uint64_t g_52 = 0UL;
+static int16_t g_56 = 0x50ECL;
+static int32_t * volatile g_61 = &g_28;/* VOLATILE GLOBAL g_61 */
+static uint16_t g_64 = 0x0A09L;
+static int64_t g_78[1] = {7L};
+static uint32_t g_109 = 4294967294UL;
