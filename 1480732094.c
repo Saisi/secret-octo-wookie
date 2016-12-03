785d8600c39b7f551ade0b90e4fa247d34e5c0b5
saisi <saisi@users.noreply.github.com>
2016-12-03 02:28:15 +0000

    1480732094 Afrikanerize

---
+++ 1480732094.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2637163945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 8;
+};
+
+struct S1 {
+   const signed f0 : 20;
+   signed f1 : 22;
+   const unsigned f2 : 1;
+   signed f3 : 1;
