152a33e796f7a8f412ef6c5123681ca923f12140
saisi <saisi@users.noreply.github.com>
2016-12-03 10:28:48 +0000

    1480760928 anterospinal

---
+++ 1480760928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1109655577
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_2 = {0L};/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U0  func_1(void);
