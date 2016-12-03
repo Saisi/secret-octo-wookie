6bc6d4ac3db65844d10c70c48c763e4b7d3476f7
saisi <saisi@users.noreply.github.com>
2016-12-03 07:58:38 +0000

    1480751917 antipriestcraft

---
+++ 1480751917.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2238787347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 8;
+   volatile signed f1 : 31;
+   volatile signed f2 : 6;
+   unsigned f3 : 25;
+   const signed f4 : 6;
+   unsigned f5 : 7;
+   int64_t  f6;
