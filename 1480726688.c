3ed96247f63fd95b5476cf149763a2e229912b46
saisi <saisi@users.noreply.github.com>
2016-12-03 00:58:08 +0000

    1480726688 aplustra

---
+++ 1480726688.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3836993587
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+};
+
+struct S1 {
+   uint8_t  f0;
+   int16_t  f1;
+};
+
+union U2 {
