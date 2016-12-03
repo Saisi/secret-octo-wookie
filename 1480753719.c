0ecae81b7bb835cc3d2ebdb4418f1546d3af9a59
saisi <saisi@users.noreply.github.com>
2016-12-03 08:28:40 +0000

    1480753719 analgias

---
+++ 1480753719.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4180490227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 24;
+   signed f1 : 15;
+   volatile unsigned f2 : 22;
+   unsigned f3 : 9;
+   unsigned f4 : 22;
+};
+
+#pragma pack(push)
+#pragma pack(1)
