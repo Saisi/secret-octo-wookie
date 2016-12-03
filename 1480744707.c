66974d7adef82544d661b38a823c42d572c4b6e6
saisi <saisi@users.noreply.github.com>
2016-12-03 05:58:29 +0000

    1480744707 aphorists

---
+++ 1480744707.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3137579889
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 23;
+   volatile uint32_t  f1;
+   signed f2 : 18;
+   const unsigned f3 : 14;
+   const volatile uint32_t  f4;
+   signed f5 : 21;
+   const unsigned f6 : 5;
