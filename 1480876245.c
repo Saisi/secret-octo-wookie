8da3b0492b0a25fdbe61299f54920b5c31baf0e1
saisi <saisi@users.noreply.github.com>
2016-12-04 18:30:45 +0000

    1480876245 assonances

---
+++ 1480876245.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      680263995
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x313DD33AL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 3L;
+static uint16_t g_66 = 65530UL;
+static uint16_t g_69 = 0x9EFEL;
+static uint16_t *g_68 = &g_69;
+static int32_t g_80 = 0x4BB0F55FL;
+static const int32_t *** volatile g_92 = (void*)0;/* VOLATILE GLOBAL g_92 */
+static const int32_t *g_95 = (void*)0;
+static const int32_t **g_94 = &g_95;
