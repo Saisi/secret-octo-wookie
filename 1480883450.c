071b89b12ad7a2c553875f7819410799d5d77d06
saisi <saisi@users.noreply.github.com>
2016-12-04 20:30:52 +0000

    1480883450 anionics

---
+++ 1480883450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2942108160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 14;
+};
+
+struct S1 {
+   volatile unsigned f0 : 3;
+   int64_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
+   const uint16_t  f4;
