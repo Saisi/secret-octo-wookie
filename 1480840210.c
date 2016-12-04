0c158a76405b3db6b58ac4aa63997595633091b4
saisi <saisi@users.noreply.github.com>
2016-12-04 08:30:11 +0000

    1480840210 apritif

---
+++ 1480840210.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2385184824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 10;
+   uint64_t  f1;
+   int64_t  f2;
+   uint32_t  f3;
+   volatile int32_t  f4;
+   uint64_t  f5;
+   uint8_t  f6;
+   volatile uint16_t  f7;
+   volatile int32_t  f8;
