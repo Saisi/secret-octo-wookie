5a79947b3d0da72f595f3e3607cf83dbaba18588
saisi <saisi@users.noreply.github.com>
2016-12-04 09:00:12 +0000

    1480842012 algaesthesis

---
+++ 1480842012.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1147409845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   signed f1 : 12;
+   unsigned f2 : 31;
+   unsigned f3 : 14;
+   const unsigned f4 : 7;
+   unsigned f5 : 4;
+};
