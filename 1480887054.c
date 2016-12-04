0756b794577fb47bff3f2702cbcbe4866313b7bd
saisi <saisi@users.noreply.github.com>
2016-12-04 21:30:54 +0000

    1480887054 antiselene

---
+++ 1480887054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1907146847
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0x4F402E31L;
+static uint16_t g_37 = 0x6E86L;
+static uint64_t g_40 = 0xFE3D2F55126ED4BCLL;
+static uint8_t g_56 = 1UL;
+static uint32_t g_78 = 1UL;
+static uint32_t g_89 = 18446744073709551610UL;
+static uint32_t g_95 = 1UL;
+static uint8_t g_100 = 0x02L;
+static uint8_t *g_99 = &g_100;
