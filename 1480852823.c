ccfcdb96c9c770b318990ea236e60a4ada7a9689
saisi <saisi@users.noreply.github.com>
2016-12-04 12:00:24 +0000

    1480852823 apatetic

---
+++ 1480852823.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1614673877
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int8_t  f0;
+   int64_t  f1;
+   int16_t  f2;
+   const uint32_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
