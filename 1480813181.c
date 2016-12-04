c841cf40f11599c376e3c19f0d1c69dc82614de6
saisi <saisi@users.noreply.github.com>
2016-12-04 00:59:42 +0000

    1480813181 anthemia

---
+++ 1480813181.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3655834937
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint64_t  f0;
+   uint32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[3][5][2] = {{{1L,(-1L)},{0x1D925B0EL,0L},{0x1D925B0EL,(-1L)},{1L,0x1D925B0EL},{(-1L),0L}},{{0x34E6D4F2L,0x34E6D4F2L},{1L,0x34E6D4F2L},{0x34E6D4F2L,0L},{(-1L),0x1D925B0EL},{1L,(-1L)}},{{0x1D925B0EL,0L},{0x1D925B0EL,(-1L)},{1L,0x1D925B0EL},{(-1L),0L},{0x34E6D4F2L,0x34E6D4F2L}}};
