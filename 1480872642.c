1e0e5fd91fb82d0e71cb7de49430d9106109c81f
saisi <saisi@users.noreply.github.com>
2016-12-04 17:30:42 +0000

    1480872642 acromioclavicular

---
+++ 1480872642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3412258219
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 20;
+   volatile uint32_t  f1;
+   volatile unsigned f2 : 18;
+   volatile uint64_t  f3;
+   const int64_t  f4;
+};
+
+union U1 {
+   int32_t  f0;
