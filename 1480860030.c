2576a92c4fc6e27b61c6358730a218ee6e92cfe6
saisi <saisi@users.noreply.github.com>
2016-12-04 14:00:30 +0000

    1480860030 Amalthea

---
+++ 1480860030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2102159494
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int64_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   int8_t  f3;
+   const volatile int8_t  f4;
+   int32_t  f5;
+};
