a301b503ed0993542c7986949e8cf097adae3b57
saisi <saisi@users.noreply.github.com>
2016-12-04 05:29:58 +0000

    1480829397 animotheism

---
+++ 1480829397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      697651515
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 28;
+   signed f1 : 3;
+   const volatile signed f2 : 26;
+   const unsigned f3 : 29;
+   unsigned f4 : 26;
+   unsigned f5 : 11;
+   const unsigned f6 : 30;
