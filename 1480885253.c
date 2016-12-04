c1ab2475163f502d4eae6f2051147403818c4f12
saisi <saisi@users.noreply.github.com>
2016-12-04 21:00:53 +0000

    1480885253 atticism

---
+++ 1480885253.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3999171499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   int32_t  f1;
+   int64_t  f2;
+   const volatile int64_t  f3;
+};
+
+union U1 {
+   uint64_t  f0;
+   volatile uint16_t  f1;
