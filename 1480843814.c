f5b8c199285962e236a5a17ee67aeaf355a13568
saisi <saisi@users.noreply.github.com>
2016-12-04 09:30:15 +0000

    1480843814 adenochondrosarcoma

---
+++ 1480843814.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4254291834
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 11;
+   volatile unsigned f1 : 20;
+   signed f2 : 6;
+   int64_t  f3;
+   unsigned f4 : 27;
+   const int16_t  f5;
+};
+
+union U1 {
