da3a8b57187c021cdadab86dd0642da0556fa9c5
saisi <saisi@users.noreply.github.com>
2016-12-04 19:00:46 +0000

    1480878046 alogically

---
+++ 1480878046.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2947145486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 21;
+   volatile unsigned f1 : 22;
+   volatile unsigned : 0;
+   volatile signed f2 : 12;
+   const signed f3 : 11;
+   signed f4 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
