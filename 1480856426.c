6679a7258c87be47ea3c4170fbe4424f380f91be
saisi <saisi@users.noreply.github.com>
2016-12-04 13:00:27 +0000

    1480856426 bandicoot

---
+++ 1480856426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3813755930
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x841B3C69L;
+static int32_t g_5[3] = {0x2D149FEBL,0x2D149FEBL,0x2D149FEBL};
+static int32_t g_18[9] = {0x9B69B2E4L,7L,0x9B69B2E4L,7L,0x9B69B2E4L,7L,0x9B69B2E4L,7L,0x9B69B2E4L};
+static int64_t g_47 = (-1L);
+static int32_t *g_55 = (void*)0;
+static uint16_t g_100 = 0x9518L;
+static volatile uint16_t g_109[4] = {0xBB1CL,0xBB1CL,0xBB1CL,0xBB1CL};
+static const volatile uint16_t * volatile g_108 = &g_109[2];/* VOLATILE GLOBAL g_108 */
+static uint16_t *g_123 = &g_100;
