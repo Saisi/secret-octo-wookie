eb8b516db972ba50dc1517cfd810bf8b2b1479a3
saisi <saisi@users.noreply.github.com>
2016-12-04 04:59:56 +0000

    1480827596 atrophic

---
+++ 1480827596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2397570680
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   unsigned f1 : 23;
+   const volatile signed f2 : 29;
+   const unsigned f3 : 21;
+   const signed f4 : 16;
+   unsigned f5 : 7;
+   uint32_t  f6;
+   const signed : 0;
+   const unsigned f7 : 17;
