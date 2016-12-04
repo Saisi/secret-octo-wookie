273e547f5b1bbde19c9456cdc91cd116ce5bf7c4
saisi <saisi@users.noreply.github.com>
2016-12-04 00:29:40 +0000

    1480811378 aerophagist

---
+++ 1480811378.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4124494415
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 6;
+   const volatile signed f1 : 5;
+   signed f2 : 20;
+   unsigned f3 : 15;
+   unsigned f4 : 4;
+   volatile int8_t  f5;
+   const int8_t  f6;
+   uint32_t  f7;
+   volatile signed f8 : 2;
