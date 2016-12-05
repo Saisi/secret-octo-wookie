30dd472004494745e9fad1e097dc508c34ca1e60
saisi <saisi@users.noreply.github.com>
2016-12-05 21:02:28 +0000

    1480971748 alveolars

---
+++ 1480971748.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2699683996
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_19 = 0L;
+static union U0 g_33 = {0xAF8716B5L};
+static uint16_t g_59[8] = {5UL,65531UL,5UL,5UL,65531UL,5UL,5UL,65531UL};
+static uint16_t *g_66[4][7] = {{(void*)0,&g_59[1],&g_59[1],(void*)0,&g_59[1],(void*)0,(void*)0},{&g_59[2],&g_59[1],(void*)0,&g_59[1],&g_59[2],&g_59[0],(void*)0},{(void*)0,&g_59[1],(void*)0,&g_59[0],&g_59[2],(void*)0,&g_59[2]},{&g_59[1],(void*)0,(void*)0,&g_59[1],&g_59[1],&g_59[0],(void*)0}};
