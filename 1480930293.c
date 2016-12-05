2bf3d6c9b2f6c98f03b653bf43fe2b36f3e394de
saisi <saisi@users.noreply.github.com>
2016-12-05 09:31:33 +0000

    1480930293 apothecarcaries

---
+++ 1480930293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3633468327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 18;
+   volatile signed f1 : 1;
+   signed : 0;
+   volatile unsigned f2 : 11;
+   volatile signed f3 : 16;
+   volatile unsigned f4 : 9;
+   const volatile signed f5 : 4;
