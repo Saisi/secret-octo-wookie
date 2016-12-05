30cc9e57aba8946c117d7c1664de1879a28f8c83
saisi <saisi@users.noreply.github.com>
2016-12-05 03:01:13 +0000

    1480906873 agenetic

---
+++ 1480906873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4137283498
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 3;
+   volatile signed f1 : 12;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_24 = 65535UL;
+static int64_t g_63[1] = {0L};
+static int64_t g_65 = (-1L);
+static uint16_t g_102[4][3][1] = {{{0xFF9BL},{0x3FE7L},{65535UL}},{{0x3FE7L},{0xFF9BL},{1UL}},{{0xFF9BL},{0x3FE7L},{65535UL}},{{0x3FE7L},{0xFF9BL},{1UL}}};
