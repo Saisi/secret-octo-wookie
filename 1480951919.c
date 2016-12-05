a8835535a3675f8c853c90dfbda921a8eee1e856
saisi <saisi@users.noreply.github.com>
2016-12-05 15:31:59 +0000

    1480951919 apportioning

---
+++ 1480951919.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1991983997
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint16_t  f1;
+   const uint32_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+   volatile int64_t  f5;
+   int32_t  f6;
+   int16_t  f7;
+};
