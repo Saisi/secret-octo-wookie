d72d5beaab6007d28efa4584d2d4d083b67e811c
saisi <saisi@users.noreply.github.com>
2016-12-05 08:01:28 +0000

    1480924888 antipatriarchy

---
+++ 1480924888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3787766333
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   const int8_t * volatile  f0;
+   int64_t  f1;
+   const int8_t * f2;
+   uint32_t  f3;
