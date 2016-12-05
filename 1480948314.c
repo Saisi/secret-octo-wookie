7191b1c4b103e79125f46d06d6d7007dc041cf17
saisi <saisi@users.noreply.github.com>
2016-12-05 14:31:56 +0000

    1480948314 airplay

---
+++ 1480948314.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      426947298
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint64_t  f0;
+   const volatile uint8_t  f1;
+   const volatile int32_t  f2;
+   int64_t  f3;
+   const uint16_t  f4;
+   uint32_t  f5;
+   int8_t  f6;
+   const uint32_t  f7;
+   volatile uint64_t  f8;
