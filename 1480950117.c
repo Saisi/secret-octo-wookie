e7b888bcb8bb1da72b57f3e05c7b5d13ef1276f8
saisi <saisi@users.noreply.github.com>
2016-12-05 15:01:58 +0000

    1480950117 arcanist

---
+++ 1480950117.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3009750211
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = 0xF147909BL;
+static uint8_t g_44 = 7UL;
+static int32_t g_48 = 0x7E06985BL;
+static uint8_t * const g_63 = (void*)0;
+static int32_t g_70 = 0x1D4B4EA6L;
+static volatile int16_t g_77 = (-9L);/* VOLATILE GLOBAL g_77 */
+static volatile uint64_t g_80 = 0xC15A2D128B415AEELL;/* VOLATILE GLOBAL g_80 */
+static uint8_t g_92 = 0x82L;
+static uint8_t *g_93 = &g_92;
