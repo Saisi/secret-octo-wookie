f4cf711ac5eaf824728c762f0af21b7b49709a46
saisi <saisi@users.noreply.github.com>
2016-12-05 18:02:16 +0000

    1480960934 airmass

---
+++ 1480960934.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3776452613
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const uint8_t  f1;
+   volatile int16_t  f2;
+   const uint8_t  f3;
+   uint64_t  f4;
+};
+
+union U1 {
+   const int32_t  f0;
