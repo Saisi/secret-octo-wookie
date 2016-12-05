21c56a4d1f41047ef27a9bec210aed5588340f24
saisi <saisi@users.noreply.github.com>
2016-12-05 05:31:21 +0000

    1480915881 Achaean

---
+++ 1480915881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2730650044
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+};
+
+union U1 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_5 = 0xED08L;/* VOLATILE GLOBAL g_5 */
