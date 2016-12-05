d6f1f32856cc0bb8e614dd8141ee36f66231341e
saisi <saisi@users.noreply.github.com>
2016-12-05 02:31:12 +0000

    1480905071 angiospermatous

---
+++ 1480905071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      696434339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   const signed f1 : 7;
+   unsigned f2 : 14;
+   volatile int64_t  f3;
+   signed f4 : 30;
+   volatile uint16_t  f5;
+   volatile uint64_t  f6;
