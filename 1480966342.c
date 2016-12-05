f6f22bdbe16e5a6a6f94c0a1aa3dcb7b799c76df
saisi <saisi@users.noreply.github.com>
2016-12-05 19:32:22 +0000

    1480966342 Atalan

---
+++ 1480966342.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3555393520
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-7L);
+static int32_t g_4 = 1L;
+static uint8_t g_18 = 246UL;
+static uint16_t g_58 = 0x7944L;
+static uint64_t g_64 = 0xB87B6FF578636BC8LL;
+static int8_t g_73 = (-4L);
+static uint32_t g_88 = 1UL;
+static int32_t g_90 = 5L;
+static int32_t * volatile g_89 = &g_90;/* VOLATILE GLOBAL g_89 */
