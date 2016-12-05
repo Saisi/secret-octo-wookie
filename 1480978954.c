aa868247cf4a90af9a7981fbcad712d3bcdbd70b
saisi <saisi@users.noreply.github.com>
2016-12-05 23:02:35 +0000

    1480978954 anaplastic

---
+++ 1480978954.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1853382651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
+   volatile int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_5 = 1L;
