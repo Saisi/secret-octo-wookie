21894de04ddf5de757d8f6a8bae5d84b16c14b1c
saisi <saisi@users.noreply.github.com>
2016-12-05 19:02:20 +0000

    1480964539 autocomplexes

---
+++ 1480964539.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2301462498
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   uint32_t  f1;
+   uint16_t  f2;
+   int16_t  f3;
+   uint64_t  f4;
+   const volatile int16_t  f5;
+   volatile int64_t  f6;
+   volatile int32_t  f7;
+   volatile int8_t  f8;
