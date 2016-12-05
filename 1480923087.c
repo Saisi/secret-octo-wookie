80dad7772518d3d29d79a88378cbae36dd6393de
saisi <saisi@users.noreply.github.com>
2016-12-05 07:31:27 +0000

    1480923087 azoimide

---
+++ 1480923087.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1579291866
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 27;
+   const volatile uint8_t  f1;
+   unsigned f2 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x49FEBB67L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-9L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xD1E814B7L;/* VOLATILE GLOBAL g_5 */
