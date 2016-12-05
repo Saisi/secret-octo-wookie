f7a1ac0ccbf9979248da6d3fac898bc01f69735b
saisi <saisi@users.noreply.github.com>
2016-12-05 23:32:36 +0000

    1480980756 Akra

---
+++ 1480980756.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2328748234
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile uint16_t  f1;
+   volatile int32_t  f2;
+   volatile int32_t  f3;
+   volatile uint64_t  f4;
+   const int32_t  f5;
+   const volatile int32_t  f6;
+   volatile uint32_t  f7;
+};
