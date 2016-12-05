7bb1ac5e2f823559c9a453c2b8127e6687adf4ba
saisi <saisi@users.noreply.github.com>
2016-12-05 12:31:46 +0000

    1480941105 abigails

---
+++ 1480941105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3359249325
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   volatile unsigned f1 : 24;
+   volatile unsigned f2 : 27;
+   const signed f3 : 26;
+   unsigned f4 : 3;
+   unsigned f5 : 16;
+   unsigned f6 : 15;
