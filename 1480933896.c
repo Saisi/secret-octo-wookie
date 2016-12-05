fd630a62ff186249bf8623fdd98dabefee729b62
saisi <saisi@users.noreply.github.com>
2016-12-05 10:31:38 +0000

    1480933896 Anastrophia

---
+++ 1480933896.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2606241080
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 10;
+   signed f1 : 23;
+   signed f2 : 29;
+   signed f3 : 21;
+   volatile signed f4 : 16;
+   volatile unsigned f5 : 7;
+   signed f6 : 9;
