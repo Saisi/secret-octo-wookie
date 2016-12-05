40b5e946ab9cf6133a98d8777a29cc60fea8f773
saisi <saisi@users.noreply.github.com>
2016-12-05 09:01:32 +0000

    1480928491 ampelitic

---
+++ 1480928491.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4212438245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_12 = 18446744073709551607UL;
+static uint8_t g_14[6] = {1UL,1UL,1UL,1UL,1UL,1UL};
+static uint8_t g_73 = 0x6CL;
+static int16_t g_94 = (-1L);
+static uint64_t g_119 = 6UL;
+static int32_t g_125 = 0L;
+static int32_t *g_124[6] = {&g_125,&g_125,&g_125,&g_125,&g_125,&g_125};
+static int32_t ** volatile g_126[5][8] = {{(void*)0,&g_124[2],(void*)0,(void*)0,&g_124[2],(void*)0,(void*)0,&g_124[2]},{&g_124[2],(void*)0,(void*)0,&g_124[2],(void*)0,(void*)0,&g_124[2],(void*)0},{&g_124[2],&g_124[2],&g_124[3],&g_124[2],&g_124[2],&g_124[3],&g_124[2],&g_124[2]},{(void*)0,&g_124[2],(void*)0,(void*)0,&g_124[2],(void*)0,(void*)0,&g_124[2]},{&g_124[2],(void*)0,&g_124[3],(void*)0,&g_124[3],&g_124[3],(void*)0,&g_124[3]}};
+static int32_t ** volatile g_127[5] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
