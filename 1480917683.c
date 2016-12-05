30936b01a1f018e3a23430b10f286a2dbea4993d
saisi <saisi@users.noreply.github.com>
2016-12-05 06:01:23 +0000

    1480917683 astroscopy

---
+++ 1480917683.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1854576416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x8BBCA631L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x99DADA43L;
+static volatile uint64_t g_7[10] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
+static uint32_t g_82[2] = {1UL,1UL};
+static uint32_t g_91 = 0UL;
+static uint32_t *g_90 = &g_91;
+static uint8_t g_100 = 0xE3L;
+static int32_t g_105 = 0x5642E063L;
+static int32_t g_127 = 0xF097794FL;
