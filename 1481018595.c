36c4e57eb3cdf2540294667bba6549dd1648fd87
saisi <saisi@users.noreply.github.com>
2016-12-06 10:03:15 +0000

    1481018595 Argyrosomus

---
+++ 1481018595.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2641900504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint64_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2 = 1UL;
+static union U0 g_9 = {18446744073709551615UL};/* VOLATILE GLOBAL g_9 */
+static union U0 g_72 = {0xC4505AF75FBD26DALL};/* VOLATILE GLOBAL g_72 */
