e73546c11dedb2982a0306600c24e24275e959a8
saisi <saisi@users.noreply.github.com>
2016-12-06 22:34:03 +0000

    1481063642 baloneys

---
+++ 1481063642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2198532800
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 14;
+   const volatile uint16_t  f1;
+   volatile uint32_t  f2;
+   int8_t * volatile  f3;
+   const volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_14 = 4294967286UL;
