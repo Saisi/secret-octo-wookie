80ee79a05a06e64b97cea41aba19b216ab3e88ae
saisi <saisi@users.noreply.github.com>
2016-12-06 08:03:08 +0000

    1481011388 averin

---
+++ 1481011388.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1772165891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   unsigned f1 : 5;
+   signed f2 : 7;
+};
+
+struct S1 {
+   signed f0 : 11;
+   signed f1 : 2;
+   const volatile signed f2 : 30;
