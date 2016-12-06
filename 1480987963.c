842de2facfc1a33146a3ea2e371ebcff15f5a50c
saisi <saisi@users.noreply.github.com>
2016-12-06 01:32:44 +0000

    1480987963 antivolition

---
+++ 1480987963.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3629040930
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_17 = 1UL;
+static uint16_t g_19 = 0x80CAL;
+static uint16_t g_41 = 1UL;
+static uint16_t * const g_40[9] = {&g_41,&g_41,&g_41,&g_41,&g_41,&g_41,&g_41,&g_41,&g_41};
+static int32_t g_60 = 1L;
+static int32_t * volatile g_59 = &g_60;/* VOLATILE GLOBAL g_59 */
+static uint8_t g_70 = 4UL;
+static int32_t * volatile g_103[3] = {(void*)0,(void*)0,(void*)0};
+static int32_t g_105 = 1L;
