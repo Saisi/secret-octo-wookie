516b10b9a1c0453109c3f704bb0b4312ba14e974
saisi <saisi@users.noreply.github.com>
2016-12-06 06:03:00 +0000

    1481004179 balladmonger

---
+++ 1481004179.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1087206568
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   volatile int32_t  f1;
+   const int32_t  f2;
+   const unsigned f3 : 13;
+};
+
+union U1 {
+   const volatile int8_t * const  f0;
+   int64_t  f1;
