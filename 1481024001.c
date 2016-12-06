6435504c43588dc002d16b84a3872d663b2b37b2
saisi <saisi@users.noreply.github.com>
2016-12-06 11:33:22 +0000

    1481024001 aneurins

---
+++ 1481024001.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2721491413
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 9;
+   unsigned f1 : 28;
+   volatile unsigned f2 : 15;
+   unsigned f3 : 30;
+};
+#pragma pack(pop)
+
