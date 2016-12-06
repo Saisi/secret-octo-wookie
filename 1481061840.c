5f01758a495a5f113e682b0b26db5f5ba78aafc9
saisi <saisi@users.noreply.github.com>
2016-12-06 22:04:00 +0000

    1481061840 Avilion

---
+++ 1481061840.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2325734992
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile uint32_t  f1;
+   uint16_t  f2;
+   int8_t * f3;
+};
+
+union U1 {
+   uint16_t  f0;
+   int8_t  f1;
