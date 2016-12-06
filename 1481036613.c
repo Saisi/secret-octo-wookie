694272d036c69c4fb30ce0ba0c1aa0cde387795b
saisi <saisi@users.noreply.github.com>
2016-12-06 15:03:33 +0000

    1481036613 adhesives

---
+++ 1481036613.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2889699889
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 23;
+   signed f1 : 18;
+   const volatile signed f2 : 13;
+   const volatile signed f3 : 4;
+   unsigned f4 : 5;
+   signed f5 : 10;
+   signed f6 : 1;
+   volatile signed f7 : 16;
+   signed f8 : 19;
