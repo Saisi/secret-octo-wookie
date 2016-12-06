265753a51e5be02f2ebd35b3b4a08368bd923587
saisi <saisi@users.noreply.github.com>
2016-12-06 09:03:12 +0000

    1481014991 Achras

---
+++ 1481014991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4024711412
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   volatile uint32_t  f1;
+   volatile int64_t  f2;
+   const signed f3 : 23;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 255UL;
