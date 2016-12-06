5231fca3024c8b7e844b52259b8083f73ca998b6
saisi <saisi@users.noreply.github.com>
2016-12-06 09:33:14 +0000

    1481016793 antisepticist

---
+++ 1481016793.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3606039438
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 21;
+   uint32_t  f1;
+   volatile signed f2 : 30;
+   int32_t  f3;
+};
+
+union U1 {
+   int16_t  f0;
+   const volatile int32_t  f1;
