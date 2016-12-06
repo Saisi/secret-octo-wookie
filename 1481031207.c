7631e55bae12d46aafbb7bc52744d602d8424edd
saisi <saisi@users.noreply.github.com>
2016-12-06 13:33:28 +0000

    1481031207 aminolysis

---
+++ 1481031207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      675130248
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_19 = 0x5CL;
+static uint8_t g_23 = 0xBEL;
+static volatile uint8_t *g_40 = (void*)0;
+static volatile uint8_t ** volatile g_39 = &g_40;/* VOLATILE GLOBAL g_39 */
+static int32_t g_71 = (-1L);
+static uint16_t g_75 = 65528UL;
+static int16_t g_97 = (-10L);
+static int8_t g_101[10] = {5L,5L,5L,5L,5L,5L,5L,5L,5L,5L};
+static int16_t g_103 = 2L;
