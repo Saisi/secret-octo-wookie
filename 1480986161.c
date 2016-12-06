3bccc99c09af8e232ddb26429d08839f201d349a
saisi <saisi@users.noreply.github.com>
2016-12-06 01:02:42 +0000

    1480986161 antiatonement

---
+++ 1480986161.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2090513045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 9;
+   uint64_t  f1;
+   volatile signed f2 : 28;
+   signed f3 : 16;
+   unsigned f4 : 23;
+   signed f5 : 9;
+   unsigned f6 : 8;
+   volatile signed f7 : 2;
+};
