5e112a1e281108dd14ce6f8cd61cb4ec4b7f713d
saisi <saisi@users.noreply.github.com>
2016-12-06 04:32:55 +0000

    1480998775 Aschim

---
+++ 1480998775.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1584702123
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x2C213532L;
+static volatile uint64_t g_4 = 0xD672A8358BD4DFBDLL;/* VOLATILE GLOBAL g_4 */
+static uint32_t g_56 = 0x8FAA83FBL;
+static uint8_t g_59[5][4] = {{0x8AL,0x53L,0x8AL,0x5DL},{0x18L,0x53L,0x49L,0x67L},{0x53L,0xA4L,0xA4L,0x53L},{0x8AL,0x67L,0xA4L,0x5DL},{0x53L,0x18L,0x49L,0x18L}};
+static int32_t g_61[9] = {0xC74600EBL,(-1L),(-1L),0xC74600EBL,(-1L),(-1L),0xC74600EBL,(-1L),(-1L)};
+static int32_t g_107 = 0x07242BEBL;
+static uint8_t g_112 = 6UL;
+static uint64_t g_127 = 0x687EAF6650442C13LL;
+static uint64_t g_129 = 18446744073709551615UL;
