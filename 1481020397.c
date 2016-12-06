56fd0dbd1bf36973f263a47c2314099636b9f237
saisi <saisi@users.noreply.github.com>
2016-12-06 10:33:18 +0000

    1481020397 abortionists

---
+++ 1481020397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1398744827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   const volatile int32_t  f1;
+   int16_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xAE8C9F36L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[10] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
