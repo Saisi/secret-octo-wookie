7d4ce0659030e3a9f4520cdd8468bf64ded705ff
saisi <saisi@users.noreply.github.com>
2016-12-06 21:03:57 +0000

    1481058234 arrogation

---
+++ 1481058234.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2999188452
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const uint32_t  f1;
+   int16_t  f2;
+   uint64_t  f3;
+   const int32_t  f4;
+};
+
+struct S1 {
+   struct S0  f0;
