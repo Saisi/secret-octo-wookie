d7dcf2de41ccbf89a910dd7abf840b29ec9d090e
saisi <saisi@users.noreply.github.com>
2016-12-06 15:33:34 +0000

    1481038414 acridonium

---
+++ 1481038414.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1800268172
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x099CFE04L;
+static uint8_t g_11[8][1][6] = {{{0x56L,0x3EL,0x3EL,0x56L,0x06L,0xD1L}},{{0xD1L,0x56L,0xAFL,0x56L,0xD1L,5UL}},{{0x56L,0xD1L,5UL,5UL,0xD1L,0x56L}},{{0x3EL,0x56L,0x06L,0xD1L,0x06L,0x56L}},{{0x06L,0x3EL,5UL,0xAFL,0xAFL,5UL}},{{0x06L,0x06L,0xAFL,0xD1L,255UL,0xD1L}},{{0x3EL,0x06L,0x3EL,5UL,0xAFL,0xAFL}},{{0x56L,0x3EL,0x3EL,0x56L,255UL,5UL}}};
+static uint8_t g_21 = 0x3DL;
+static uint8_t *g_20 = &g_21;
+static int32_t g_38 = 0xD840DFE9L;
+static volatile uint64_t g_46[3] = {0xB4763523E24B13B1LL,0xB4763523E24B13B1LL,0xB4763523E24B13B1LL};
+static int32_t g_63 = 9L;
+static volatile int16_t g_84 = 0xA6F2L;/* VOLATILE GLOBAL g_84 */
+static int32_t g_87[6][2] = {{0L,0x08E27FB6L},{1L,0xC5DA298BL},{0x08E27FB6L,0xC5DA298BL},{1L,0x08E27FB6L},{0L,0L},{0L,0x08E27FB6L}};
