c7763c30d454c16019c7c028130ab0eeb92e6824
saisi <saisi@users.noreply.github.com>
2016-12-06 05:02:57 +0000

    1481000577 angulose

---
+++ 1481000577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      98947411
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   volatile uint8_t  f1;
+   const volatile int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_8 = (void*)0;
+static int32_t g_9 = 4L;
+static int16_t g_68 = 4L;
