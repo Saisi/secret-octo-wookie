c8f0db3cbfa5149a6b049afd339ed6fbcfe16d1c
saisi <saisi@users.noreply.github.com>
2016-12-07 03:04:24 +0000

    1481079863 anhidrosis

---
+++ 1481079863.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3660064240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+   volatile int32_t  f3;
+   int16_t  f4;
+};
+
+struct S1 {
+   struct S0  f0;
