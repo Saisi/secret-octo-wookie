f4d31f7f92f20ecfe34789ab0f5b1125374ed71c
saisi <saisi@users.noreply.github.com>
2016-12-07 16:35:12 +0000

    1481128512 ardours

---
+++ 1481128512.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2332418975
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint8_t  f0;
+   volatile int16_t  f1;
+   volatile uint8_t  f2;
+   int64_t  f3;
+   uint64_t  f4;
+   int32_t  f5;
+   int16_t  f6;
