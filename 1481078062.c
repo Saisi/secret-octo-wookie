209dc32df5e307ccfab7f9faa50b81d0483753c2
saisi <saisi@users.noreply.github.com>
2016-12-07 02:34:22 +0000

    1481078062 aurichlorohydric

---
+++ 1481078062.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1142242643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint16_t  f1;
+   signed f2 : 6;
+   uint32_t  f3;
+};
+
+union U1 {
+   const int16_t  f0;
+   const int32_t  f1;
