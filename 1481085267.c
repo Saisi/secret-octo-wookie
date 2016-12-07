e0552da71ecbb308674b2e7c26ffb977da3ce3a0
saisi <saisi@users.noreply.github.com>
2016-12-07 04:34:27 +0000

    1481085267 absorbents

---
+++ 1481085267.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2941052984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[6] = {1L,1L,1L,1L,1L,1L};
+static volatile int32_t g_4[1][9] = {{0L,0L,0L,0L,0L,0L,0L,0L,0L}};
+static volatile int32_t g_5[10] = {9L,(-7L),9L,9L,(-7L),9L,9L,(-7L),9L,9L};
+static int32_t g_6 = 0xAA08BBD1L;
+static int32_t *g_7 = (void*)0;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
