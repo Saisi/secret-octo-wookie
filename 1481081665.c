475485ebb3b81881c4ffe3f5f95fb9a159e298ce
saisi <saisi@users.noreply.github.com>
2016-12-07 03:34:25 +0000

    1481081665 Alcaics

---
+++ 1481081665.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2880880910
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1] = {5L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
