fe2ed744f5dfd834cbf47c33d9789e348f65ec62
saisi <saisi@users.noreply.github.com>
2016-12-07 10:34:50 +0000

    1481106889 algific

---
+++ 1481106889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2681977660
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+};
+
+struct S1 {
+   volatile int16_t  f0;
+   volatile uint16_t  f1;
+   volatile uint16_t  f2;
+   volatile uint16_t  f3;
+   int32_t  f4;
