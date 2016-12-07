33448a48e33918b9b7b9c4973ffae84a24395868
saisi <saisi@users.noreply.github.com>
2016-12-07 05:34:31 +0000

    1481088870 Babine

---
+++ 1481088870.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3487997237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x7730F94FL;
+static uint16_t g_71 = 0x3D38L;
+static int32_t g_79 = 1L;
+static int32_t * volatile g_78 = &g_79;/* VOLATILE GLOBAL g_78 */
+static int16_t g_105 = 0x07F4L;
+static int32_t g_107 = 0x54113837L;
+static uint8_t g_120[9] = {0x49L,0x8EL,0x49L,0x49L,0x8EL,0x49L,0x49L,0x8EL,0x49L};
+static volatile int16_t g_131 = 0x872AL;/* VOLATILE GLOBAL g_131 */
+static volatile uint8_t g_133 = 1UL;/* VOLATILE GLOBAL g_133 */
