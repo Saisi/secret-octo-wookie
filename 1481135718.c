8ee97192e55eff494ffc5ddbd4dd725b55a75741
saisi <saisi@users.noreply.github.com>
2016-12-07 18:35:18 +0000

    1481135718 apiole

---
+++ 1481135718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      255779284
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   int8_t  f1;
+   volatile int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCE53D8DAL;
+static int32_t g_5 = 0x248C66F5L;
+static int32_t *g_17 = &g_2;
