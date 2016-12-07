97225bd7e2f997a3d2e85fd6b4e86bf4200762a4
saisi <saisi@users.noreply.github.com>
2016-12-07 18:05:17 +0000

    1481133916 armigeros

---
+++ 1481133916.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1763436432
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 22;
+   const unsigned f1 : 19;
+   unsigned f2 : 9;
+   const signed f3 : 17;
+   const volatile signed f4 : 28;
+   const signed f5 : 3;
+   signed f6 : 21;
