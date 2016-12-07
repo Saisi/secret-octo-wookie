1aa1456a259ed6b13944ea8ba593a2031fd30852
saisi <saisi@users.noreply.github.com>
2016-12-07 11:34:53 +0000

    1481110492 acalephes

---
+++ 1481110492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      296319558
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 17;
+   signed f1 : 2;
+   int32_t  f2;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   const signed f1 : 20;
+   int8_t  f2;
