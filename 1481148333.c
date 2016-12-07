d4b204ed6e29770ed38a28f594bacfd8ea9fb9db
saisi <saisi@users.noreply.github.com>
2016-12-07 22:05:33 +0000

    1481148333 alluviate

---
+++ 1481148333.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      996246284
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-6L);/* VOLATILE GLOBAL g_2 */
+static volatile int16_t g_3 = (-4L);/* VOLATILE GLOBAL g_3 */
+static uint32_t g_16 = 0xC41FD9D0L;
+static int32_t g_22[5] = {0xAD53A38DL,0xAD53A38DL,0xAD53A38DL,0xAD53A38DL,0xAD53A38DL};
+static union U0 g_55 = {-2L};
