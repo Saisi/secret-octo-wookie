984dad1d63fb108c274a872132004c1417d1b947
saisi <saisi@users.noreply.github.com>
2016-12-07 08:34:43 +0000

    1481099683 apostatising

---
+++ 1481099683.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3888819290
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+};
+
+union U1 {
+   uint16_t  f0;
+   const int32_t  f1;
+   uint64_t  f2;
+   int64_t  f3;
+   uint8_t  f4;
