76c621aef1e1a4d75d619eb257622f11462f9a07
saisi <saisi@users.noreply.github.com>
2016-12-07 20:05:25 +0000

    1481141125 Achromycin

---
+++ 1481141125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1140389608
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 15;
+   const uint32_t  f2;
+};
+
+struct S1 {
+   signed f0 : 30;
+};
+
