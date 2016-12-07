82fbdc7ba0ec219e641f3caa7d2f83d10ff012ed
saisi <saisi@users.noreply.github.com>
2016-12-07 00:04:10 +0000

    1481069049 allocrotonic

---
+++ 1481069049.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4058743952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x6182EAC4L;
+static volatile int32_t g_21 = 0x29B85755L;/* VOLATILE GLOBAL g_21 */
+static volatile int32_t g_22 = (-1L);/* VOLATILE GLOBAL g_22 */
+static volatile int32_t g_23 = (-9L);/* VOLATILE GLOBAL g_23 */
+static volatile int32_t g_24 = 0xA01C77DCL;/* VOLATILE GLOBAL g_24 */
+static volatile int32_t g_25[3] = {0L,0L,0L};
+static volatile int32_t g_26 = (-1L);/* VOLATILE GLOBAL g_26 */
+static int32_t g_27 = 0xD02268F6L;
+static int32_t g_30 = 0x66C4779DL;
