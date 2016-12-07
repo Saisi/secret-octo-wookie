9c48a2f371e610a27a2bb5ebca8afa051a106e02
saisi <saisi@users.noreply.github.com>
2016-12-07 12:04:54 +0000

    1481112294 Andria

---
+++ 1481112294.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3569048207
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_4[7] = {0x184C506DL,0x184C506DL,0x184C506DL,0x184C506DL,0x184C506DL,0x184C506DL,0x184C506DL};
+static const int32_t g_6 = 0x865925EFL;
+static const int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_8 = (-10L);/* VOLATILE GLOBAL g_8 */
+static int32_t g_10 = (-6L);
+static volatile uint8_t g_13 = 1UL;/* VOLATILE GLOBAL g_13 */
+static int32_t *g_51 = &g_10;
+static int32_t **g_50 = &g_51;
+static int32_t g_71 = 5L;
