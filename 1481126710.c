5a58ca8f848020058583ffa9f3158901e7497c59
saisi <saisi@users.noreply.github.com>
2016-12-07 16:05:11 +0000

    1481126710 anemologic

---
+++ 1481126710.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2773546606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 5;
+   int32_t  f1;
+   unsigned f2 : 16;
+   uint64_t  f3;
+   int64_t  f4;
+   volatile uint64_t  f5;
+};
+
+#pragma pack(push)
