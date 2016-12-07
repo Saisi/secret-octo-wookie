94df31a497418b18b691ccc72c0aff8651830688
saisi <saisi@users.noreply.github.com>
2016-12-07 07:34:40 +0000

    1481096078 awoke

---
+++ 1481096078.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2267737818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   const int16_t  f1;
+   uint32_t  f2;
+   int64_t  f3;
+   const volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[5][10] = {{7L,0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L},{0x2D149FEBL,0x2D149FEBL,(-1L),0x2D149FEBL,0x2D149FEBL,(-1L),0x2D149FEBL,0x2D149FEBL,(-1L),0x2D149FEBL},{0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L,0x2D149FEBL},{7L,0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L,0x2D149FEBL,7L,7L},{0x2D149FEBL,0x2D149FEBL,(-1L),0x2D149FEBL,0x2D149FEBL,(-1L),7L,7L,0x2D149FEBL,7L}};
