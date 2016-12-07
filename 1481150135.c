e2f711c35ee0848f655fb30795214d2870681650
saisi <saisi@users.noreply.github.com>
2016-12-07 22:35:35 +0000

    1481150135 Acerata

---
+++ 1481150135.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1099176735
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   unsigned f0 : 28;
+   volatile int8_t  f1;
+   signed f2 : 14;
+};
