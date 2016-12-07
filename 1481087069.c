ca1742386c9350ab8c8dae605d891d4ceeaaa245
saisi <saisi@users.noreply.github.com>
2016-12-07 05:04:29 +0000

    1481087069 archaeocyte

---
+++ 1481087069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      671786904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+   int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x57A7C203L;
+static volatile int32_t g_5 = 0x4B666A75L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[2][4][5] = {{{0xEB4C8F0DL,1L,1L,0xEB4C8F0DL,0L},{0x97963BA2L,0L,1L,1L,0L},{0L,1L,0x1151C3C3L,(-5L),(-5L)},{(-9L),(-4L),(-9L),1L,0xA924C000L}},{{0xA0D461E3L,0xEB4C8F0DL,(-5L),0xEB4C8F0DL,0xA0D461E3L},{(-9L),0x97963BA2L,(-4L),0L,(-4L)},{0L,0L,(-5L),0xA0D461E3L,3L},{0x97963BA2L,(-9L),(-9L),0x97963BA2L,(-4L)}}};
