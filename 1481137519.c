c04cb63dd716a0eec80094a1c4e3ad33b0536190
saisi <saisi@users.noreply.github.com>
2016-12-07 19:05:21 +0000

    1481137519 antimethodicalness

---
+++ 1481137519.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3244009656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const int32_t  f1;
+   const int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4[2][9] = {{0x1151C3C3L,0x756A3367L,0x1151C3C3L,0x1151C3C3L,0x756A3367L,0x1151C3C3L,0x1151C3C3L,0x756A3367L,0x1151C3C3L},{0x1151C3C3L,0x756A3367L,0x1151C3C3L,0x1151C3C3L,0x756A3367L,0x1151C3C3L,0x1151C3C3L,0x756A3367L,0x1151C3C3L}};
+static int32_t g_6 = 0x0B95AE64L;
+static volatile int64_t g_7 = 0xD78F4F823AAF5BFBLL;/* VOLATILE GLOBAL g_7 */
