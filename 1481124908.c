232ca3bec28e149115c39e37c1900eb31e9510ca
saisi <saisi@users.noreply.github.com>
2016-12-07 15:35:09 +0000

    1481124908 antifederalist

---
+++ 1481124908.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1389469875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+   const signed f1 : 15;
+   signed f2 : 22;
+   uint32_t  f3;
+   signed f4 : 18;
+   signed f5 : 24;
+   volatile unsigned f6 : 27;
+   int8_t  f7;
+   unsigned f8 : 4;
