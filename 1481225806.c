e54d50a4b906d12b4fa428c6b0b2d3de7ea87f8c
saisi <saisi@users.noreply.github.com>
2016-12-08 19:36:46 +0000

    1481225806 astrologaster

---
+++ 1481225806.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2844803752
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 2;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_23 = 0x54DBBE56890C5440LL;
+static int32_t g_69 = 1L;
