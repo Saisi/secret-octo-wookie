b7c9ee638382705e11755db6d9d44981c82e4c93
saisi <saisi@users.noreply.github.com>
2016-12-08 11:36:20 +0000

    1481196980 algic

---
+++ 1481196980.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3396785582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[2] = {0xDE0BCD8CL,0xDE0BCD8CL};
+static volatile int64_t g_10 = 0x60A2215256EE9734LL;/* VOLATILE GLOBAL g_10 */
+static volatile uint64_t g_12 = 0xCE35A12A2EF7E291LL;/* VOLATILE GLOBAL g_12 */
+static uint32_t g_41 = 0x4D89BB64L;
+static int16_t g_85 = 0x4B54L;
+static int8_t g_89 = 1L;
+static uint16_t g_92 = 0xC082L;
+static uint32_t g_97 = 4294967295UL;
