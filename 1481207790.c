3236993d88d51de4273745c894297a2fbf5fe2e0
saisi <saisi@users.noreply.github.com>
2016-12-08 14:36:30 +0000

    1481207790 anticor

---
+++ 1481207790.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1880195787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = (-1L);
+static int32_t *g_6 = &g_7;
