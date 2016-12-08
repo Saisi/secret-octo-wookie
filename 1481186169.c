f3472d3a993ad83245a5e3bf830c85f666be3362
saisi <saisi@users.noreply.github.com>
2016-12-08 08:36:09 +0000

    1481186169 antivaccinator

---
+++ 1481186169.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3415375006
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint16_t  f0;
+   uint32_t  f1;
+   int16_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+   uint64_t  f5;
+};
