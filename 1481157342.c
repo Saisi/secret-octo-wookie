8d0d4aa64753a98ce23a659e3a468bc076e95a79
saisi <saisi@users.noreply.github.com>
2016-12-08 00:35:42 +0000

    1481157342 acclimations

---
+++ 1481157342.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      915694880
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 5L;
+static volatile int32_t g_6[2][6][3] = {{{4L,(-1L),4L},{4L,1L,0x7DFE4D9EL},{4L,0L,0x2A8D3A18L},{4L,(-1L),4L},{4L,1L,0x7DFE4D9EL},{4L,0L,0x2A8D3A18L}},{{4L,(-1L),4L},{4L,1L,0x7DFE4D9EL},{4L,0L,0x2A8D3A18L},{4L,(-1L),4L},{4L,1L,0x7DFE4D9EL},{4L,0L,0x2A8D3A18L}}};
+static int32_t g_7 = (-1L);
+static int32_t g_9[5] = {0L,0L,0L,0L,0L};
+static int32_t g_16 = 0L;
+static int32_t *g_42 = (void*)0;
+static int32_t *g_63 = (void*)0;
+static int32_t **g_62[3][7] = {{&g_63,(void*)0,&g_63,(void*)0,&g_63,(void*)0,(void*)0},{&g_63,&g_63,&g_63,(void*)0,(void*)0,&g_63,&g_63},{(void*)0,&g_63,&g_63,&g_63,&g_63,(void*)0,&g_63}};
+static int8_t g_85[7] = {0x04L,1L,1L,0x04L,1L,1L,0x04L};
