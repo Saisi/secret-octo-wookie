be016eb81a3c0c4c470447d65d7f87480f84cab9
saisi <saisi@users.noreply.github.com>
2016-12-08 14:06:29 +0000

    1481205989 aburst

---
+++ 1481205989.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4288060787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_9 = 0x94543BEB7161AB6ELL;
+static int32_t g_11 = 0x813F1A07L;
+static int32_t *g_10[7][2][3] = {{{&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11}},{{&g_11,&g_11,(void*)0},{&g_11,(void*)0,(void*)0}},{{&g_11,(void*)0,&g_11},{&g_11,&g_11,&g_11}},{{(void*)0,(void*)0,&g_11},{&g_11,(void*)0,&g_11}},{{&g_11,&g_11,&g_11},{(void*)0,&g_11,(void*)0}},{{&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11}},{{&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11}}};
+static int32_t *g_13 = &g_11;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+static int32_t * func_2(uint32_t  p_3, int32_t * p_4, int32_t * p_5);
