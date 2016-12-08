5d8f9a5b2228e7b9b4329f1a0881220226318bfb
saisi <saisi@users.noreply.github.com>
2016-12-08 17:06:39 +0000

    1481216799 anthracotic

---
+++ 1481216799.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2776259574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 9;
+   const signed f1 : 26;
+};
+
+union U1 {
+   const int8_t  f0;
+   unsigned f1 : 17;
+   uint32_t  f2;
+   volatile int8_t  f3;
