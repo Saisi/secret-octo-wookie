2a17c3a0046411f9a542c3e1417135c8a9db4536
saisi <saisi@users.noreply.github.com>
2016-12-08 00:05:40 +0000

    1481155540 arsmetrik

---
+++ 1481155540.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2030398222
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   uint8_t  f1;
+   uint32_t  f2;
+   const int16_t  f3;
+   volatile int64_t  f4;
+};
+
+union U1 {
+   volatile uint8_t  f0;
