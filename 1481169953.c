78d55a7c9287b59a1175e65cac03e46486f980ba
saisi <saisi@users.noreply.github.com>
2016-12-08 04:05:54 +0000

    1481169953 Anomoeanism

---
+++ 1481169953.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2026537253
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const signed f1 : 6;
+   signed f2 : 4;
+   const volatile unsigned f3 : 24;
+   unsigned f4 : 31;
+   signed f5 : 17;
+};
