4d09242a03ceff77422f19285cc7331d21867abe
saisi <saisi@users.noreply.github.com>
2016-12-08 23:37:04 +0000

    1481240223 appeasers

---
+++ 1481240223.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3180472506
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 11;
+   signed f1 : 20;
+   unsigned f2 : 6;
+   volatile unsigned f3 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-10L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x49FEBB67L;/* VOLATILE GLOBAL g_3 */
