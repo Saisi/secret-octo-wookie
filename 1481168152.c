d8f6c2d43d186625654dc21f316c26b54eaf3c39
saisi <saisi@users.noreply.github.com>
2016-12-08 03:35:52 +0000

    1481168152 Bacova

---
+++ 1481168152.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3138732454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_25 = (-1L);
+static union U0 g_41[10][3] = {{{0x7029L},{0x7029L},{0x7029L}},{{0xF577L},{0xF577L},{0xF577L}},{{0x7029L},{0x7029L},{0x7029L}},{{0xF577L},{0xF577L},{0xF577L}},{{0x7029L},{0x7029L},{0x7029L}},{{0xF577L},{0xF577L},{0xF577L}},{{0x7029L},{0x7029L},{0x7029L}},{{0xF577L},{0xF577L},{0xF577L}},{{0x7029L},{0x7029L},{0x7029L}},{{0xF577L},{0xF577L},{0xF577L}}};
+static int8_t g_44 = (-4L);
+static volatile int64_t g_52 = 0L;/* VOLATILE GLOBAL g_52 */
