61421c6de1af9eef2afe97199077a65df6d34303
saisi <saisi@users.noreply.github.com>
2016-12-08 04:35:56 +0000

    1481171755 adenophlegmon

---
+++ 1481171755.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2610722583
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   signed f1 : 15;
+   volatile uint64_t  f2;
+   uint16_t  f3;
+   uint32_t  f4;
+   volatile signed f5 : 9;
+   uint8_t  f6;
