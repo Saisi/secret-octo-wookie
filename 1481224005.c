9ee219f5336913e6524889f6f9d16adde8ecc1f8
saisi <saisi@users.noreply.github.com>
2016-12-08 19:06:45 +0000

    1481224005 bankfull

---
+++ 1481224005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      809557961
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_2[1][10] = {{0x91L,0x86L,255UL,255UL,0x86L,0x91L,0x86L,255UL,255UL,0x86L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
