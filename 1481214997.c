4384aa7f8f1cfdce847604e93cab8726fb9f1ea9
saisi <saisi@users.noreply.github.com>
2016-12-08 16:36:38 +0000

    1481214997 athyroidism

---
+++ 1481214997.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3153823151
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 22;
+   unsigned f1 : 21;
+   volatile signed f2 : 4;
+};
+#pragma pack(pop)
+
+struct S1 {
