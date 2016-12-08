5bcb261179087bf951de726760bf1a3fa12be1a0
saisi <saisi@users.noreply.github.com>
2016-12-08 13:36:28 +0000

    1481204188 Alisma

---
+++ 1481204188.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1738436757
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_11 = 1L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_15 = 1L;
+static int32_t g_24 = 9L;
+static int32_t *g_23 = &g_24;
