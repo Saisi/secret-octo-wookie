24c096da06f6f7ea84cca2d49e11d1de2881d1cb
saisi <saisi@users.noreply.github.com>
2016-12-08 12:06:22 +0000

    1481198781 abattoir

---
+++ 1481198781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2158977957
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   signed f1 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0x1C7BF0B9L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 5L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[6] = {1L,1L,1L,1L,1L,1L};
