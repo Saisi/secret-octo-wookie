fbd6534b1d0aa3a2814320d293a49b3680704623
saisi <saisi@users.noreply.github.com>
2016-12-08 06:36:03 +0000

    1481178962 Barbera

---
+++ 1481178962.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2852124185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   const int32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11[9][1][4] = {{{(-5L),0x7A5B7D8CL,(-5L),(-5L)}},{{0x7A5B7D8CL,0x7A5B7D8CL,0xC8959310L,0x7A5B7D8CL}},{{0x7A5B7D8CL,(-5L),(-5L),0x7A5B7D8CL}},{{(-5L),0x7A5B7D8CL,(-5L),(-5L)}},{{0x7A5B7D8CL,0x7A5B7D8CL,0xC8959310L,0x7A5B7D8CL}},{{0x7A5B7D8CL,(-5L),(-5L),0x7A5B7D8CL}},{{(-5L),0x7A5B7D8CL,(-5L),(-5L)}},{{0x7A5B7D8CL,0x7A5B7D8CL,0xC8959310L,0x7A5B7D8CL}},{{0x7A5B7D8CL,(-5L),(-5L),0x7A5B7D8CL}}};
