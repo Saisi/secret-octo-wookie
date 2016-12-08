02a59c431eacf9c6c999e6698fc9948bda5bb850
saisi <saisi@users.noreply.github.com>
2016-12-08 05:05:57 +0000

    1481173557 Atticizing

---
+++ 1481173557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1041726616
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   uint8_t  f0;
+   volatile uint8_t  f1;
+};
+
