809ed1c215e62ee3c4c9a8c7944dfbc6996c5b7e
saisi <saisi@users.noreply.github.com>
2016-12-08 23:07:01 +0000

    1481238420 bagpiper

---
+++ 1481238420.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2228599676
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_12 = 0L;/* VOLATILE GLOBAL g_12 */
+static uint32_t g_13 = 0x64FDA7EFL;
+static int32_t g_58 = (-4L);
+static const int32_t g_93 = 0x1CA8FD5FL;
+static int32_t g_105 = 1L;
+static uint16_t g_130 = 65532UL;
+static uint32_t g_139 = 0x5B7CA3B7L;
+static uint64_t g_158 = 0UL;
+static uint16_t g_160 = 4UL;
