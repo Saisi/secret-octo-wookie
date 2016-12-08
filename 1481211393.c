daf51434d4787b370c99764f5ab77c846bef561e
saisi <saisi@users.noreply.github.com>
2016-12-08 15:36:34 +0000

    1481211393 barbituism

---
+++ 1481211393.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2726120811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 20;
+   volatile signed f1 : 27;
+   const volatile signed f2 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile signed f0 : 30;
