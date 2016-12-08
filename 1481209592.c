5f9a630ebea5a505ecd6d575da4c8c13c1879ab3
saisi <saisi@users.noreply.github.com>
2016-12-08 15:06:32 +0000

    1481209592 arrogated

---
+++ 1481209592.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      595745899
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3[5][3][6] = {{{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L}},{{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L}},{{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L}},{{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L}},{{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L},{5L,5L,5L,5L,5L,5L}}};
+static int32_t g_5 = 0x00025260L;
+static volatile uint32_t g_8 = 0xDBDF7CD9L;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
