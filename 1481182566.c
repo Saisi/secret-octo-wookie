89e159638098aea3c14718e654a75a16a0dab364
saisi <saisi@users.noreply.github.com>
2016-12-08 07:36:06 +0000

    1481182566 astigmism

---
+++ 1481182566.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1416517927
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-6L);
+static volatile int32_t g_4 = 0xD8E6E838L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x751C76D7L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-9L);
+static uint16_t g_34[2] = {65535UL,65535UL};
+static int16_t g_42 = 0x7B6AL;
+static uint8_t g_44 = 0x08L;
+static uint16_t g_45 = 1UL;
+static int32_t * volatile g_49 = (void*)0;/* VOLATILE GLOBAL g_49 */
