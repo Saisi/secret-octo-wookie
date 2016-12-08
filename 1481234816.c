a61318bb06729f0c015e1f5a39d91ec76df719d7
saisi <saisi@users.noreply.github.com>
2016-12-08 22:06:56 +0000

    1481234816 axonemes

---
+++ 1481234816.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1711354047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 13;
+   signed f1 : 29;
+   signed f2 : 16;
