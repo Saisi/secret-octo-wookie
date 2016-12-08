b87656a53915d51721b1ce893b5bfd0457766f64
saisi <saisi@users.noreply.github.com>
2016-12-08 09:06:11 +0000

    1481187970 baldachini

---
+++ 1481187970.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1530463667
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+   const volatile unsigned f1 : 15;
+   unsigned f2 : 22;
+   unsigned f3 : 9;
+   const unsigned f4 : 22;
+   volatile int8_t  f5;
+   signed f6 : 27;
+};
+
