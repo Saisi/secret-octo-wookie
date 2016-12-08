66ac0b4d7aef88a951d23b5e21704dbdb64d10aa
saisi <saisi@users.noreply.github.com>
2016-12-08 18:36:44 +0000

    1481222203 amaryllis

---
+++ 1481222203.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2479147543
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-8L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0L;
+static int8_t g_67 = (-7L);
+static int32_t g_79 = 0x7B06F861L;
+static int16_t g_81 = 0xDAA9L;
