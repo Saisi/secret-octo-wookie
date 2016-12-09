1664fd7e2268daef81c6e1bf510e992aab6433d3
saisi <saisi@users.noreply.github.com>
2016-12-09 03:07:16 +0000

    1481252835 antipopery

---
+++ 1481252835.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      506546810
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   const volatile signed f1 : 20;
+   volatile unsigned f2 : 6;
+   volatile unsigned f3 : 26;
+   signed f4 : 1;
+   volatile int32_t  f5;
+   int32_t  f6;
+   volatile unsigned f7 : 9;
+   signed f8 : 22;
