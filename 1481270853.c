aa37690078a73059cae1a98c1a289279b186c3d8
saisi <saisi@users.noreply.github.com>
2016-12-09 08:07:33 +0000

    1481270853 anticonstitutionalist

---
+++ 1481270853.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2817654446
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[7] = {0L,0xB26BDDE0L,0L,0L,0xB26BDDE0L,0L,0L};
+static int32_t g_3 = 0L;
+static int32_t g_7 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
