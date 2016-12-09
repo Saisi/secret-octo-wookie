b7c70f53fe21d8438feff5cdfbce021cebb5e5c2
saisi <saisi@users.noreply.github.com>
2016-12-09 05:07:22 +0000

    1481260042 Arachnomorphae

---
+++ 1481260042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2111531128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[10] = {0x1EL,0x1EL,0x1EL,0x1EL,0x1EL,0x1EL,0x1EL,0x1EL,0x1EL,0x1EL};
+static volatile int32_t g_3 = (-2L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x98070ADEL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x7FA5C62EL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x74B666A7L;
+static int32_t g_12 = 0xAE649F6DL;
+static uint32_t g_14 = 4294967292UL;
+static int32_t * volatile g_17 = (void*)0;/* VOLATILE GLOBAL g_17 */
+static int32_t * volatile g_18 = &g_12;/* VOLATILE GLOBAL g_18 */
