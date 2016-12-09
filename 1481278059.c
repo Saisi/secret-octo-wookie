3d16b21b17dbe9a2b8062bbc2dadbdec7a14042e
saisi <saisi@users.noreply.github.com>
2016-12-09 10:07:40 +0000

    1481278059 Annuloida

---
+++ 1481278059.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2212577815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   int32_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+   int8_t * volatile  f3;
+   const uint32_t  f4;
