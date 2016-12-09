ccec4675ffa85d57a026fcec275d41c82528cf2f
saisi <saisi@users.noreply.github.com>
2016-12-09 22:38:25 +0000

    1481323105 anaglypton

---
+++ 1481323105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1928626389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   const int8_t  f1;
+};
+#pragma pack(pop)
+
+struct S1 {
+   unsigned f0 : 7;
