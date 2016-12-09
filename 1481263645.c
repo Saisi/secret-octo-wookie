66c526acb23dc47c30c0aadc2975565a6946bab7
saisi <saisi@users.noreply.github.com>
2016-12-09 06:07:26 +0000

    1481263645 Archelochus

---
+++ 1481263645.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      6738837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-8L);
+static int32_t g_51 = 5L;
+static int32_t *g_61[8][9][3] = {{{&g_51,&g_51,&g_51},{&g_2,&g_2,(void*)0},{&g_51,&g_2,&g_51},{&g_51,&g_2,(void*)0},{&g_51,&g_51,&g_51},{&g_2,&g_51,&g_51},{&g_2,(void*)0,&g_51},{&g_51,(void*)0,&g_2},{&g_2,&g_51,&g_2}},{{&g_51,&g_51,&g_51},{(void*)0,&g_2,&g_51},{&g_2,(void*)0,&g_2},{&g_51,&g_51,&g_51},{&g_51,&g_51,(void*)0},{&g_2,&g_51,&g_51},{(void*)0,&g_51,(void*)0},{&g_51,&g_2,&g_51},{&g_2,&g_2,(void*)0}},{{&g_51,&g_2,&g_2},{&g_2,&g_51,&g_51},{&g_51,&g_2,&g_2},{(void*)0,&g_2,&g_2},{&g_2,&g_51,(void*)0},{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_51},{&g_51,&g_51,(void*)0},{&g_51,&g_2,&g_51}},{{&g_51,&g_2,&g_51},{&g_2,&g_2,&g_51},{&g_51,&g_51,&g_51},{&g_2,&g_51,&g_2},{&g_2,(void*)0,&g_2},{&g_2,&g_51,&g_2},{&g_51,&g_2,&g_2},{(void*)0,&g_51,&g_51},{&g_51,&g_51,&g_51}},{{&g_2,&g_2,&g_51},{&g_2,(void*)0,&g_51},{(void*)0,&g_51,(void*)0},{&g_51,(void*)0,&g_51},{&g_51,&g_51,&g_2},{&g_51,&g_2,(void*)0},{&g_51,&g_51,&g_2},{&g_51,&g_51,&g_2},{&g_51,&g_2,&g_51}},{{&g_51,&g_51,&g_2},{(void*)0,&g_51,(void*)0},{&g_2,&g_2,&g_51},{&g_2,&g_51,(void*)0},{&g_51,&g_2,(void*)0},{(void*)0,&g_51,&g_2},{&g_51,&g_51,&g_51},{&g_2,&g_51,&g_2},{&g_2,&g_51,&g_2}},{{&g_2,&g_2,&g_51},{&g_51,&g_51,&g_2},{&g_2,&g_2,&g_2},{&g_51,&g_51,&g_2},{&g_51,&g_51,&g_51},{&g_51,&g_2,&g_51},{&g_2,&g_51,&g_2},{(void*)0,&g_51,(void*)0},{&g_2,&g_2,&g_2}},{{(void*)0,&g_51,&g_51},{&g_51,(void*)0,&g_51},{&g_2,&g_51,&g_2},{&g_51,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_51,&g_51,&g_51},{&g_2,&g_51,&g_2},{&g_51,&g_2,&g_2},{&g_2,&g_51,&g_51}}};
+static int32_t g_63[3] = {0xC7592E0CL,0xC7592E0CL,0xC7592E0CL};
+static const uint64_t g_70 = 0x1792F5726B06574FLL;
+static const int32_t g_95 = 4L;
+static const int32_t g_97[1] = {0x634890B8L};
+static uint16_t g_100 = 0x049CL;
+static volatile uint32_t g_110 = 18446744073709551608UL;/* VOLATILE GLOBAL g_110 */
