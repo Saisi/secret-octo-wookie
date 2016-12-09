7f91671b26ac103633510b1fb28ab4594c7a1271
saisi <saisi@users.noreply.github.com>
2016-12-09 16:08:02 +0000

    1481299681 ancoral

---
+++ 1481299681.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3286719750
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[9][1] = {{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L},{0x2799AC78L}};
+static volatile int32_t g_3 = 0xA4A12B57L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x914D2EDCL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[4] = {0xC196A162L,0xC196A162L,0xC196A162L,0xC196A162L};
+static int32_t g_7 = 0x14173BF3L;
+static volatile int32_t g_18 = 1L;/* VOLATILE GLOBAL g_18 */
+static volatile int32_t *g_17 = &g_18;
+static int32_t g_20 = (-10L);
