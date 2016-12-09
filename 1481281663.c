8122388304310163b7da126618cd86f3718cb461
saisi <saisi@users.noreply.github.com>
2016-12-09 11:07:44 +0000

    1481281663 atmophile

---
+++ 1481281663.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      438366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   volatile signed f1 : 26;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 5;
+   signed f1 : 4;
