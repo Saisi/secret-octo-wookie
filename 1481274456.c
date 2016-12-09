8b8f9f85c4a9ba18e7ccbb96814dcdc85635a7e1
saisi <saisi@users.noreply.github.com>
2016-12-09 09:07:36 +0000

    1481274456 adulteration

---
+++ 1481274456.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3651841406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   volatile int8_t  f1;
+   const uint16_t  f2;
+   int16_t  f3;
+   volatile signed f4 : 28;
+   volatile int32_t  f5;
+   const volatile int16_t  f6;
+   volatile int64_t  f7;
+};
