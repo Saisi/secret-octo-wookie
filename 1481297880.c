84bc5ba2a28aeadd658427e5cb0969ed5f778a1e
saisi <saisi@users.noreply.github.com>
2016-12-09 15:38:00 +0000

    1481297880 acetometric

---
+++ 1481297880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      141873823
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   unsigned f1 : 13;
+   signed f2 : 28;
+   const unsigned f3 : 23;
+   signed f4 : 12;
+   volatile unsigned f5 : 21;
+   volatile unsigned : 0;
+   unsigned f6 : 19;
+   volatile unsigned f7 : 10;
