83bff580074a8177a25ecbdc13ad8db9a120befa
saisi <saisi@users.noreply.github.com>
2016-12-09 15:07:59 +0000

    1481296078 antipodist

---
+++ 1481296078.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1554542223
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5B57FD54L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x5F5C184CL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-2L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0xB1C2A2C7L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0xBA1BF2B2L;
+static uint16_t g_46[9] = {65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL};
+static int32_t g_49[2][6][10] = {{{1L,0L,0x4FD57441L,0L,0x23279329L,0L,0x4FD57441L,0L,1L,(-1L)},{3L,(-4L),0x4FD57441L,0x23279329L,0L,0L,0x23279329L,0x4FD57441L,(-4L),3L},{(-4L),0L,1L,0x23279329L,(-1L),3L,(-1L),0x23279329L,1L,0L},{(-1L),0x4FD57441L,3L,0L,(-1L),(-10L),(-10L),(-10L),(-4L),1L},{(-10L),(-10L),(-1L),0L,3L,0x4FD57441L,(-1L),0x4FD57441L,3L,0L},{0L,0x23279329L,0L,0x4FD57441L,0L,1L,(-1L),(-1L),1L,0L}},{{(-1L),(-10L),(-10L),(-1L),0L,3L,0x4FD57441L,(-1L),0x4FD57441L,3L},{1L,3L,0L,3L,1L,(-4L),(-10L),0x4FD57441L,0x4FD57441L,(-10L)},{(-1L),(-4L),(-1L),(-1L),(-4L),(-1L),0L,(-10L),1L,(-10L)},{3L,(-1L),1L,0x4FD57441L,1L,(-1L),3L,0L,3L,3L},{3L,3L,(-1L),0L,0L,(-1L),3L,3L,(-4L),0L},{(-1L),3L,3L,(-4L),0L,(-4L),3L,3L,(-1L),0L}}};
