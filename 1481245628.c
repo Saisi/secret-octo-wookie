a8c06457e24dd4cf47c6ade1f9b1236b13e17650
saisi <saisi@users.noreply.github.com>
2016-12-09 01:07:08 +0000

    1481245628 AMPAS

---
+++ 1481245628.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      941774814
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static uint64_t g_4 = 0x67C28CAE2FE81720LL;
+static int32_t g_8 = 9L;
+static uint64_t g_16 = 0UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
