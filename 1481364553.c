0d0807dd1e6f34e4f4e47aad7294a461a2f7efea
saisi <saisi@users.noreply.github.com>
2016-12-10 10:09:14 +0000

    1481364553 Actinopterygii

---
+++ 1481364553.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3241128122
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int16_t  f0;
+   volatile int32_t  f1;
+   uint32_t  f2;
+   volatile int16_t  f3;
+   const int32_t  f4;
+   uint16_t  f5;
+   uint16_t  f6;
