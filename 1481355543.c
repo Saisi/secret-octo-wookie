6c2d83497b97e1474169011648a8d917ddb043dd
saisi <saisi@users.noreply.github.com>
2016-12-10 07:39:03 +0000

    1481355543 Armorel

---
+++ 1481355543.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3025487364
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 0x40L;
+static uint16_t g_19 = 0x72C4L;
+static uint16_t g_32 = 65527UL;
+static uint16_t * volatile g_31 = &g_32;/* VOLATILE GLOBAL g_31 */
+static int32_t g_48 = (-1L);
+static uint32_t g_61[1][3][2] = {{{18446744073709551615UL,18446744073709551615UL},{18446744073709551615UL,18446744073709551615UL},{18446744073709551615UL,18446744073709551615UL}}};
+static uint8_t g_76 = 250UL;
+static uint16_t g_85 = 65534UL;
+static uint16_t *g_84 = &g_85;
