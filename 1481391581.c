559d54f039674d08a049223befd28ec5cea0f27a
saisi <saisi@users.noreply.github.com>
2016-12-10 17:39:41 +0000

    1481391581 acanthin

---
+++ 1481391581.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2375519779
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 16;
+   unsigned f1 : 7;
+   unsigned f2 : 14;
+   const unsigned f3 : 1;
+   int64_t  f4;
+   const volatile signed f5 : 16;
+   unsigned f6 : 19;
+   signed f7 : 1;
+   signed f8 : 12;
