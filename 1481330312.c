fa123dff563a90980493ef110a4636c7d8b08ac3
saisi <saisi@users.noreply.github.com>
2016-12-10 00:38:33 +0000

    1481330312 antitussive

---
+++ 1481330312.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      599658625
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   signed f1 : 26;
+   const volatile int8_t * volatile  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+};
+
