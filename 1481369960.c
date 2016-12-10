c81db1947dead82832120b98efa38fcb4346d0eb
saisi <saisi@users.noreply.github.com>
2016-12-10 11:39:20 +0000

    1481369960 Agnella

---
+++ 1481369960.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2878817495
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_8 = 0xFD94FE5F36F6DD8DLL;
+static int32_t g_32 = 1L;
+static int8_t g_34 = 0x28L;
+static int32_t g_59 = 6L;
+static int32_t *g_58 = &g_59;
