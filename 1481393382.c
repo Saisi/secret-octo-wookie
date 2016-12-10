73f61de1b747f7d753a001405333d938800e4587
saisi <saisi@users.noreply.github.com>
2016-12-10 18:09:42 +0000

    1481393382 aerodontic

---
+++ 1481393382.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      537031655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[10] = {(-1L),0x8E1B61BAL,(-1L),(-1L),0x8E1B61BAL,(-1L),(-1L),0x8E1B61BAL,(-1L),(-1L)};
+static uint8_t g_56 = 0xB2L;
+static int32_t *g_61 = &g_3[2];
+static int32_t g_70 = 0xB40075A6L;
+static int32_t g_78 = 2L;
+static volatile int32_t g_82 = 0xF9D625AEL;/* VOLATILE GLOBAL g_82 */
+static volatile uint32_t g_83[3] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
+static uint64_t g_93 = 18446744073709551615UL;
+static int8_t g_94 = (-1L);
