a3e332168ea18012c6a0d3f1445df44b43bb5a5d
saisi <saisi@users.noreply.github.com>
2016-12-10 11:09:18 +0000

    1481368157 albuminate

---
+++ 1481368157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1455926350
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 21;
+   signed f1 : 22;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   const int32_t  f1;
+   const uint8_t  f2;
+   int16_t  f3;
