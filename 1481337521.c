a796117f0adb9dee11671da2f347068c51f36841
saisi <saisi@users.noreply.github.com>
2016-12-10 02:38:42 +0000

    1481337521 Aaronite

---
+++ 1481337521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1312324322
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const uint16_t  f1;
+   int32_t  f2;
+   volatile int8_t  f3;
+   const volatile uint16_t  f4;
+   uint32_t  f5;
+   volatile unsigned f6 : 24;
