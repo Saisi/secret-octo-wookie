5c5641ce47094cd9bbe39ade4cd5f27843005309
saisi <saisi@users.noreply.github.com>
2016-12-10 12:09:22 +0000

    1481371761 anthrachrysone

---
+++ 1481371761.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1264825698
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 31;
+   signed f1 : 24;
+};
+#pragma pack(pop)
+
+struct S1 {
+   unsigned f0 : 2;
