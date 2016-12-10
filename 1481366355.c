d8590562e8467c83d01ee7e86fadda17b1b4e7e0
saisi <saisi@users.noreply.github.com>
2016-12-10 10:39:16 +0000

    1481366355 abecedarium

---
+++ 1481366355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2360988255
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+};
+
+union U1 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+   int8_t  f2;
+   signed f3 : 14;
+   int8_t  f4;
