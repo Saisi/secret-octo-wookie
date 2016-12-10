f7b16fe9b93ae3a6ece1d7e6e8660e6cd05eaffb
saisi <saisi@users.noreply.github.com>
2016-12-10 13:09:25 +0000

    1481375365 antiattrition

---
+++ 1481375365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4044722139
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_6 = 0x8C9F3690L;
+static volatile uint64_t g_38 = 0x7245C2F1FCC1031CLL;/* VOLATILE GLOBAL g_38 */
+static int32_t *g_42 = (void*)0;
+static int32_t ** const  volatile g_41 = &g_42;/* VOLATILE GLOBAL g_41 */
