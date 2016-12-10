df1e3c8b13e86e8665d697cb1c68344e5b779dd6
saisi <saisi@users.noreply.github.com>
2016-12-10 09:09:11 +0000

    1481360949 amphitheatric

---
+++ 1481360949.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3243545691
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 12;
+   signed f1 : 19;
+   const signed f2 : 10;
+   signed f3 : 13;
+   const volatile unsigned f4 : 10;
+};
+#pragma pack(pop)
