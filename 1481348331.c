ca4ee72d8011a48c013efbdd174a8b6d494ff47d
saisi <saisi@users.noreply.github.com>
2016-12-10 05:38:53 +0000

    1481348331 apprehended

---
+++ 1481348331.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3332353039
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+};
+
+struct S1 {
+   volatile uint8_t  f0;
+   volatile uint8_t  f1;
+   struct S0  f2;
+   int8_t  f3;
+};
