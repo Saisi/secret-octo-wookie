ce1107db349e96634d8084fc24b775f39f06d4ca
saisi <saisi@users.noreply.github.com>
2016-12-10 19:39:47 +0000

    1481398787 adenomalacia

---
+++ 1481398787.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      765380847
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   int8_t  f1;
+   uint16_t  f2;
+   volatile int32_t  f3;
+};
+
+union U1 {
+   uint32_t  f0;
+   uint8_t  f1;
