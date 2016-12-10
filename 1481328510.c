958e3570b52d2498f78dcd3e624ec9e0a5f265df
saisi <saisi@users.noreply.github.com>
2016-12-10 00:08:31 +0000

    1481328510 Bagpuize

---
+++ 1481328510.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      66197355
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   signed f1 : 27;
+   signed f2 : 18;
+   volatile unsigned f3 : 21;
+   const unsigned f4 : 18;
+   unsigned f5 : 3;
+   signed f6 : 22;
+   int64_t  f7;
+   unsigned : 0;
