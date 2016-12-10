855a99cd3af73a45ad155a81fef3f8a160b2db60
saisi <saisi@users.noreply.github.com>
2016-12-10 05:08:50 +0000

    1481346529 balaenoidean

---
+++ 1481346529.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      321682514
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x540D7F82L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xA66FE7D8L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x013DCE9DL;
+static volatile int32_t g_7[2][10] = {{0xF40FC027L,(-1L),0xF40FC027L,0xF40FC027L,(-1L),0xF40FC027L,0xF40FC027L,(-1L),0xF40FC027L,0xF40FC027L},{(-1L),(-1L),0x759951EAL,(-1L),(-1L),0x759951EAL,(-1L),(-1L),0x759951EAL,(-1L)}};
+static int32_t g_8[2] = {0x0BD2AB60L,0x0BD2AB60L};
+static uint8_t g_14 = 0x1AL;
+static uint32_t g_32 = 0x18670FC2L;
+static uint32_t g_41 = 0UL;
+static uint32_t g_43 = 0x7FFEAC8EL;
