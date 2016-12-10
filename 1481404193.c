1e086a9345032c4f05840ba6fa49493ef2a98e54
saisi <saisi@users.noreply.github.com>
2016-12-10 21:09:53 +0000

    1481404193 Areus

---
+++ 1481404193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      271531626
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 3;
+   const uint32_t  f1;
+   signed f2 : 20;
+   unsigned f3 : 6;
+};
+
+union U1 {
+   uint64_t  f0;
+   volatile int16_t  f1;
