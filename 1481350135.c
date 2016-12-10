4c7b7249a541b533cfdce7abde04a1a10c875c73
saisi <saisi@users.noreply.github.com>
2016-12-10 06:08:56 +0000

    1481350135 Arguedas

---
+++ 1481350135.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2825995723
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int64_t  f1;
+   int8_t * const  f2;
+   const uint16_t  f3;
+};
+
+union U1 {
+   signed f0 : 22;
+   uint64_t  f1;
