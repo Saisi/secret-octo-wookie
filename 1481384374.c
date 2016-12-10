98862d1e821fe8a8e3b4f9bfb14b078a2299eefa
saisi <saisi@users.noreply.github.com>
2016-12-10 15:39:34 +0000

    1481384374 bardy

---
+++ 1481384374.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1294715282
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 23;
+   const unsigned f1 : 16;
+   const volatile unsigned f2 : 18;
+   const signed f3 : 22;
+   unsigned f4 : 13;
+   unsigned f5 : 16;
+};
+
+union U1 {
