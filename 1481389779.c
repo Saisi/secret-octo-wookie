bb332d142958a8ead09951a9a17893c55cbb5ccd
saisi <saisi@users.noreply.github.com>
2016-12-10 17:09:40 +0000

    1481389779 amphigastrula

---
+++ 1481389779.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2772898980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 0L;
+static int32_t *g_12 = &g_13;
+static int32_t g_55 = 0xA61940C9L;
+static int32_t * volatile g_54[3] = {&g_55,&g_55,&g_55};
+static uint8_t g_62 = 0xF6L;
+static int8_t g_70[2][9] = {{0xE7L,(-4L),0xE7L,1L,1L,0xE7L,(-4L),0xE7L,1L},{0xE7L,1L,1L,0xE7L,(-4L),0xE7L,1L,1L,0xE7L}};
+static uint8_t g_74 = 0x4DL;
+static uint64_t g_88[5] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
+static int32_t g_121 = 0x1BE15C8DL;
