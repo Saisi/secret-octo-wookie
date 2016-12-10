26bca35652c2676f9b040acb994dd7d0b54e3a44
saisi <saisi@users.noreply.github.com>
2016-12-10 04:08:47 +0000

    1481342926 apteral

---
+++ 1481342926.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3132479436
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 20;
+   unsigned f1 : 25;
+   unsigned f2 : 23;
+   volatile unsigned f3 : 7;
+   int8_t  f4;
+   volatile unsigned f5 : 4;
+   int16_t  f6;
+   volatile signed f7 : 25;
+   volatile signed f8 : 28;
