4480b66d06927ed3e3a9cf2ab3436ceea19bf587
saisi <saisi@users.noreply.github.com>
2016-12-10 23:40:01 +0000

    1481413201 amylic

---
+++ 1481413201.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2062971262
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[7][2] = {{0L,0L},{0L,0L},{0L,0L},{0L,0L},{0L,0L},{0L,0L},{0L,0L}};
+static int16_t g_30[8] = {1L,1L,1L,1L,1L,1L,1L,1L};
+static int32_t g_54[1] = {0x1260E2EFL};
+static int32_t g_70 = (-1L);
+static int32_t g_71 = 0x7B9FFBDDL;
+static uint64_t g_86 = 0xC28D2166A3C8FCBDLL;
+static int32_t *g_89 = &g_54[0];
+static int32_t **g_88 = &g_89;
+static int32_t **g_91 = &g_89;
