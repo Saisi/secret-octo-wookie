e9b78d0a455e80bcb8ffc16ec41325a237abb275
saisi <saisi@users.noreply.github.com>
2016-12-11 20:11:14 +0000

    1481487074 Amyraldist

---
+++ 1481487074.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2169376504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11 = 1L;
+static uint32_t g_15 = 0x61E3FF14L;
+static uint32_t g_20 = 4294967295UL;
+static const uint32_t *g_19 = &g_20;
+static int32_t *g_26 = &g_11;
+static int32_t **g_25 = &g_26;
+static int16_t g_60 = 0x8240L;
+static int32_t * volatile g_72 = (void*)0;/* VOLATILE GLOBAL g_72 */
+static uint16_t g_78 = 0xE27DL;
