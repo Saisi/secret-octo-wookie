1768d7bc8cdb28981dd2a9952e7c6cf9e516e548
saisi <saisi@users.noreply.github.com>
2016-12-11 08:10:32 +0000

    1481443832 bankera

---
+++ 1481443832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      310255385
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile uint32_t  f1;
+};
+
+union U1 {
+   int8_t * f0;
+   volatile unsigned f1 : 25;
+   signed f2 : 14;
+   const int32_t  f3;
