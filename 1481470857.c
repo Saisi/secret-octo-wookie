30a5273752a901fb50463032e9109a34a9df9398
saisi <saisi@users.noreply.github.com>
2016-12-11 15:40:58 +0000

    1481470857 backheel

---
+++ 1481470857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2230507416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 1UL;
+static int16_t g_6 = 1L;
+static int8_t g_52 = (-7L);
+static int8_t *g_51 = &g_52;
+static int16_t g_62 = 0xEF9FL;
+static uint32_t g_65 = 0x0EB6BA8AL;
+static int16_t g_70 = 0xF193L;
+static const int16_t *g_69 = &g_70;
+static int32_t g_72[4][5] = {{0x4954824FL,0x199ED189L,0x4954824FL,0x4954824FL,0x199ED189L},{0x24264A3AL,0x6206CBC8L,0x6206CBC8L,0x24264A3AL,0x6206CBC8L},{0x199ED189L,0x199ED189L,0x07B8A5F0L,0x199ED189L,0x199ED189L},{0x6206CBC8L,0x24264A3AL,0x6206CBC8L,0x6206CBC8L,0x24264A3AL}};
