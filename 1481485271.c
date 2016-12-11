233014d0ea97dbb1343277986ed5ae804078a535
saisi <saisi@users.noreply.github.com>
2016-12-11 19:41:13 +0000

    1481485271 amphibolies

---
+++ 1481485271.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      597933150
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 29;
+   volatile int8_t * f1;
+};
+
+union U1 {
+   volatile int8_t * const  f0;
+   volatile uint8_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
