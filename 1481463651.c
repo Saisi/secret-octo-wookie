1636b9cb96536c29bcd5072da7112a62ca37061a
saisi <saisi@users.noreply.github.com>
2016-12-11 13:40:51 +0000

    1481463651 aggies

---
+++ 1481463651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3076590511
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 22;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   uint64_t  f1;
+   volatile int8_t  f2;
+   int32_t  f3;
+};
