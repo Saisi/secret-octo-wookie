323bfc0057e7256b3f57c1e392eb030e3ac2c3e6
saisi <saisi@users.noreply.github.com>
2016-12-11 11:40:45 +0000

    1481456444 arachnological

---
+++ 1481456444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2727042107
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int32_t  f0;
+   int16_t  f1;
+   uint32_t  f2;
+};
+#pragma pack(pop)
+
+union U1 {
