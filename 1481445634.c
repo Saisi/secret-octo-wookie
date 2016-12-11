8b811452be37fdba8408f24d6bbd2294dcb48867
saisi <saisi@users.noreply.github.com>
2016-12-11 08:40:34 +0000

    1481445634 aprosopia

---
+++ 1481445634.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4039509328
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x0DA11525L;
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7[2] = {0xB6182EACL,0xB6182EACL};
+static volatile int32_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9 = 0x6FF23B1AL;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 8L;
+static uint16_t g_57 = 1UL;
+static int32_t **g_71 = (void*)0;
