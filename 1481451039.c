acb19fc6e2a2303715bc38c6378d332dc633684e
saisi <saisi@users.noreply.github.com>
2016-12-11 10:10:39 +0000

    1481451039 banshies

---
+++ 1481451039.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1327332499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   const int32_t  f1;
+   int64_t  f2;
+};
+
+union U1 {
+   unsigned f0 : 22;
+   uint64_t  f1;
+   int32_t  f2;
