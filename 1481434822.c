700e2db6e5daf3ae59e01ba77f65753808f36654
saisi <saisi@users.noreply.github.com>
2016-12-11 05:40:24 +0000

    1481434822 anarchists

---
+++ 1481434822.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2370606678
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_8 = 8L;/* VOLATILE GLOBAL g_8 */
+static uint32_t g_16 = 0x326874BEL;
+static int16_t g_22 = 0x8CD5L;
+static int32_t g_23 = (-1L);
+static int32_t g_25 = 0x3B26D5D6L;
+static int32_t * const g_24 = &g_25;
+static uint32_t g_51[10] = {0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL,0x203FCA5EL};
+static int16_t g_78 = 0xAB10L;
+static uint32_t g_101 = 0x2F852669L;
