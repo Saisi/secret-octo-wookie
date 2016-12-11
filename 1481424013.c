9586119ad67e0e917ff8214041b9d6d24e511662
saisi <saisi@users.noreply.github.com>
2016-12-11 02:40:14 +0000

    1481424013 Antonella

---
+++ 1481424013.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2379754747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   uint8_t  f3;
+   volatile uint8_t  f4;
+   int16_t  f5;
+   uint32_t  f6;
+   int8_t  f7;
+   int64_t  f8;
