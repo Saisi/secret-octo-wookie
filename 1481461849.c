11dd1e61de33c75b54b74b05f2f270f66c3d1fd4
saisi <saisi@users.noreply.github.com>
2016-12-11 13:10:50 +0000

    1481461849 adjective

---
+++ 1481461849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2759446791
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   unsigned f1 : 17;
+   volatile signed f2 : 16;
+   volatile unsigned f3 : 27;
+   const signed : 0;
+   signed f4 : 25;
+   const uint64_t  f5;
+   uint8_t  f6;
+   unsigned f7 : 30;
