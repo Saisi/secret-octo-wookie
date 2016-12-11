b98b82a9b22f9c720b0e68800cf34c60e690e945
saisi <saisi@users.noreply.github.com>
2016-12-11 12:40:48 +0000

    1481460047 angelicness

---
+++ 1481460047.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4225186838
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0L;
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static uint32_t g_11 = 0UL;
+static uint64_t g_67 = 0UL;
+static uint64_t g_69 = 18446744073709551613UL;
+static int16_t g_76 = 0x814CL;
+static int32_t g_78 = 5L;
+static volatile uint32_t g_84 = 0xED226F84L;/* VOLATILE GLOBAL g_84 */
+static volatile uint32_t *g_83 = &g_84;
