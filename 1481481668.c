08fc0782afa873f540885a9d0f14a7e8b676ceb4
saisi <saisi@users.noreply.github.com>
2016-12-11 18:41:09 +0000

    1481481668 anophthalmia

---
+++ 1481481668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2895143341
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_16 = 0UL;
+static int32_t g_38 = 0x42B4CF22L;
+static int32_t * volatile g_37[8] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t * volatile g_39 = &g_38;/* VOLATILE GLOBAL g_39 */
