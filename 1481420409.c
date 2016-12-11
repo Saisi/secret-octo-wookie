c78593857a44c8aed659d096c9d1f2f1a10ead02
saisi <saisi@users.noreply.github.com>
2016-12-11 01:40:10 +0000

    1481420409 Areca

---
+++ 1481420409.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3215369945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 0xB0L;
+static int32_t g_45 = 1L;
+static uint16_t g_76 = 1UL;
+static uint16_t g_83 = 0xC826L;
+static uint16_t *g_82[4][7] = {{&g_83,&g_83,&g_83,&g_83,&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83,&g_83,&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83,&g_83,&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83,&g_83,&g_83,&g_83,&g_83}};
+static uint8_t g_87 = 0xC0L;
+static int8_t g_100[10][4] = {{0L,0L,7L,0x5DL},{0L,(-1L),0x05L,0L},{(-1L),0x5DL,(-1L),0x05L},{(-1L),0x5DL,7L,0L},{0x5DL,(-1L),7L,(-1L)},{0x7EL,(-1L),7L,(-1L)},{(-1L),0x05L,0L,0x05L},{0x05L,7L,0x7EL,0x05L},{0x7EL,0x05L,(-1L),(-1L)},{(-1L),(-1L),0L,(-1L)}};
+static int32_t g_102 = 0xCED4B89DL;
+static int32_t * volatile g_101 = &g_102;/* VOLATILE GLOBAL g_101 */
