9ba0e2533c4c5e6bd35fbd68608ad6f09f204d53
saisi <saisi@users.noreply.github.com>
2016-12-11 09:40:38 +0000

    1481449237 argon

---
+++ 1481449237.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2103242862
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile signed f1 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const volatile signed f0 : 24;
+   volatile signed f1 : 29;
