a0572b0b7db5c0f4ede62a096c682c35fea85dcf
saisi <saisi@users.noreply.github.com>
2016-12-11 06:10:26 +0000

    1481436625 atlantosaurus

---
+++ 1481436625.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4053908296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 18;
+   const signed f1 : 31;
+   const signed f2 : 5;
+   signed f3 : 29;
+   const unsigned f4 : 24;
+   signed f5 : 15;
+   volatile signed f6 : 17;
