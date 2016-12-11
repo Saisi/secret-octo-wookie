45f854d589f4c0a6394f3092d01952aac3a7d8c7
saisi <saisi@users.noreply.github.com>
2016-12-11 17:11:03 +0000

    1481476263 antiradiating

---
+++ 1481476263.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1377125691
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 28;
+   signed f1 : 3;
+   signed f2 : 26;
+   unsigned f3 : 29;
+   unsigned f4 : 26;
+   int16_t  f5;
+   volatile unsigned f6 : 15;
+};
+
