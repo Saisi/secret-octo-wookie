8beabf40c9479347415dc58d32af12cfa82212e7
saisi <saisi@users.noreply.github.com>
2016-12-11 17:41:06 +0000

    1481478065 acetophenin

---
+++ 1481478065.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      766614010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint8_t  f0;
+   unsigned f1 : 8;
+   unsigned f2 : 28;
+   signed f3 : 14;
+   volatile unsigned f4 : 27;
+   const signed f5 : 27;
+   volatile signed f6 : 16;
+   signed f7 : 9;
+};
