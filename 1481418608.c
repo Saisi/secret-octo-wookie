17d1478eb3735e5464ec458c34f8cca5e299ae46
saisi <saisi@users.noreply.github.com>
2016-12-11 01:10:08 +0000

    1481418608 alcoholometrical

---
+++ 1481418608.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      869825973
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_8 = 0x0E3580D4L;
+static int32_t *g_64 = &g_8;
+static uint32_t g_66 = 4294967293UL;
+static uint32_t g_73 = 0x92F5726BL;
+static uint32_t * volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static int16_t g_77 = 0x8BC8L;
+static uint8_t g_92 = 255UL;
+static uint8_t g_108 = 0x01L;
