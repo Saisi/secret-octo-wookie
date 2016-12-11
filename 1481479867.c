6175eabb9f6acdf1de55e1d5e160cd41ffb8fd08
saisi <saisi@users.noreply.github.com>
2016-12-11 18:11:07 +0000

    1481479867 Arthrodonteae

---
+++ 1481479867.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      11340563
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   signed f1 : 31;
+   const signed f2 : 6;
+   signed f3 : 25;
+   unsigned f4 : 6;
+   unsigned f5 : 7;
+   volatile unsigned f6 : 10;
+   volatile signed f7 : 21;
+   volatile unsigned f8 : 4;
