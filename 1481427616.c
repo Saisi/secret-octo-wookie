61258ffcbf2624688151a0085cc81117c2599da2
saisi <saisi@users.noreply.github.com>
2016-12-11 03:40:17 +0000

    1481427616 aunthood

---
+++ 1481427616.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3954269788
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint64_t  f0;
+   uint16_t  f1;
+   int16_t  f2;
+   signed f3 : 3;
+   int64_t  f4;
+   volatile unsigned f5 : 25;
+   int32_t  f6;
+};
+
