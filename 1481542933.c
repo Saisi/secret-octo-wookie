1170a249f6a71f3d915dd130dd445056e8492ff1
saisi <saisi@users.noreply.github.com>
2016-12-12 11:42:13 +0000

    1481542933 alinasal

---
+++ 1481542933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      7936073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[5] = {(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile int32_t g_3 = 0x915A1369L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[10][6] = {{1L,0L,0L,1L,0L,0xDEEC5416L},{0x66941FC9L,0xDEEC5416L,0L,1L,0xA7B445ACL,0xA7B445ACL},{(-1L),0xDEEC5416L,0xDEEC5416L,(-1L),0L,0xF5047C4DL},{(-1L),0L,0xF5047C4DL,1L,0xDEEC5416L,0xF5047C4DL},{0x66941FC9L,0xA7B445ACL,0xDEEC5416L,1L,0xDEEC5416L,0xA7B445ACL},{1L,0L,0L,1L,0L,0xDEEC5416L},{0x66941FC9L,0xDEEC5416L,0L,1L,0xA7B445ACL,0L},{0L,3L,3L,0L,0x3061ECE3L,0xAA0D7227L},{0L,0x3061ECE3L,0xAA0D7227L,0xF5047C4DL,3L,0xAA0D7227L},{0xDEEC5416L,0L,3L,0L,3L,0L}};
+static int32_t g_7 = 0xEAB3CF81L;
+static volatile int32_t g_11 = 0L;/* VOLATILE GLOBAL g_11 */
