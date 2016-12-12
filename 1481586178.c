cbb3dc0b6c6857e325b85982b3ad6ed79d2dc948
saisi <saisi@users.noreply.github.com>
2016-12-12 23:42:58 +0000

    1481586178 amphipodan

---
+++ 1481586178.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      719981672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 18446744073709551607UL;
+static uint32_t g_4[2] = {0UL,0UL};
+static int32_t g_17 = 0x07858F9CL;
+static int32_t *g_16[8][4][5] = {{{&g_17,&g_17,&g_17,&g_17,&g_17},{(void*)0,&g_17,&g_17,(void*)0,&g_17},{&g_17,&g_17,&g_17,&g_17,(void*)0},{(void*)0,&g_17,&g_17,&g_17,&g_17}},{{&g_17,&g_17,&g_17,&g_17,(void*)0},{&g_17,&g_17,&g_17,(void*)0,&g_17},{&g_17,(void*)0,&g_17,&g_17,&g_17},{(void*)0,&g_17,&g_17,&g_17,&g_17}},{{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17}},{{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,(void*)0},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,(void*)0}},{{&g_17,&g_17,&g_17,(void*)0,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,(void*)0,&g_17},{&g_17,&g_17,(void*)0,&g_17,&g_17}},{{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,(void*)0,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17}},{{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,(void*)0}},{{&g_17,&g_17,&g_17,&g_17,&g_17},{&g_17,&g_17,&g_17,&g_17,(void*)0},{&g_17,&g_17,&g_17,(void*)0,&g_17},{&g_17,&g_17,&g_17,&g_17,&g_17}}};
+static int32_t g_42 = 1L;
+static uint16_t g_60 = 0x91A1L;
+static uint32_t g_111 = 0UL;
+static int32_t g_131 = 0xDB1C9D1CL;
+static uint8_t g_133[10] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL};
