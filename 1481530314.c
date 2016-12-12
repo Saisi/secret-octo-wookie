f39aaa511c059eb880d7e0e1e9d209a589a35d2c
saisi <saisi@users.noreply.github.com>
2016-12-12 08:11:54 +0000

    1481530314 amphibological

---
+++ 1481530314.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2087168341
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 0x371865425A80E358LL;/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
