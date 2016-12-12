c8e97c876160c521b057de8001ac4a5fc489cd8e
saisi <saisi@users.noreply.github.com>
2016-12-12 16:42:35 +0000

    1481560953 aosmic

---
+++ 1481560953.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3186208306
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_31 = 0x357257EAL;
+static uint32_t g_39 = 0x92F1E5C8L;
+static uint8_t g_62 = 0x7AL;
+static uint8_t g_68[10] = {0UL,246UL,0UL,0UL,246UL,0UL,0UL,246UL,0UL,0UL};
+static uint8_t *g_67[5][5] = {{&g_68[4],(void*)0,(void*)0,&g_68[7],&g_68[7]},{&g_68[7],&g_68[2],&g_68[7],&g_68[4],(void*)0},{(void*)0,(void*)0,&g_68[4],&g_68[7],(void*)0},{(void*)0,&g_68[7],&g_68[7],(void*)0,&g_68[7]},{&g_68[7],(void*)0,&g_68[4],(void*)0,(void*)0}};
+static int8_t g_71 = 1L;
+static uint16_t g_73 = 65535UL;
+static uint16_t g_75 = 65535UL;
+static uint32_t *g_87 = (void*)0;
