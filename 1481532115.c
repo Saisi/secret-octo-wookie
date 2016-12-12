525b1faaf57f9a9776040569c38f3516ced98e54
saisi <saisi@users.noreply.github.com>
2016-12-12 08:41:55 +0000

    1481532115 apatosaurus

---
+++ 1481532115.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      107045813
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   volatile signed f1 : 30;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   int8_t  f1;
+};
+
