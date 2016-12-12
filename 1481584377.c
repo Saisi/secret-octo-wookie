f0e34c739c155ee64d6907525cd4ef5d37afc276
saisi <saisi@users.noreply.github.com>
2016-12-12 23:12:57 +0000

    1481584377 axisymmetric

---
+++ 1481584377.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2694422584
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 10;
+   volatile signed f1 : 23;
+   const signed f2 : 29;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
