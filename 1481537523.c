0bd8c96a89154f62c94736b6f69c3ea5c0fa7a88
saisi <saisi@users.noreply.github.com>
2016-12-12 10:12:03 +0000

    1481537523 Aglaspis

---
+++ 1481537523.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4219747256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 10;
+   signed f1 : 23;
+   volatile int32_t  f2;
+   const uint8_t  f3;
+   volatile uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
