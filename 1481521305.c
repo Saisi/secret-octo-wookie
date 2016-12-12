7c0ce22e395287915ab988ee8c29859f6556e799
saisi <saisi@users.noreply.github.com>
2016-12-12 05:41:45 +0000

    1481521305 Asplenieae

---
+++ 1481521305.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      874814848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 14;
+   unsigned f1 : 11;
+   unsigned f2 : 1;
+   const signed f3 : 9;
+   unsigned f4 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x4B4E1265L;
