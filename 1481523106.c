f2ba942a874e3ed46694f81d3c7415bd3d3984c6
saisi <saisi@users.noreply.github.com>
2016-12-12 06:11:48 +0000

    1481523106 baddishness

---
+++ 1481523106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4205567175
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 2;
+   volatile signed f1 : 17;
+   signed f2 : 16;
+   volatile signed f3 : 27;
+   const unsigned : 0;
+};
+
+union U1 {
+   int64_t  f0;
