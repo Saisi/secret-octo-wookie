d8d53fa38ce7ebbc0496f7ac1b87194ced1e1ef4
saisi <saisi@users.noreply.github.com>
2016-12-12 07:11:51 +0000

    1481526711 alloisomeric

---
+++ 1481526711.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1039760569
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   unsigned f1 : 16;
+   signed f2 : 6;
+   const volatile unsigned f3 : 18;
+   volatile unsigned f4 : 17;
+   signed f5 : 27;
+   unsigned f6 : 18;
+   unsigned f7 : 4;
+   volatile signed f8 : 3;
