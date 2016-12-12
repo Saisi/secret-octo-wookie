8df3b451d7a4471d185f107a6b1c4d86f2c6c4bd
saisi <saisi@users.noreply.github.com>
2016-12-12 14:12:22 +0000

    1481551941 allactite

---
+++ 1481551941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3130773297
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 5L;
+static uint32_t g_13 = 8UL;
+static uint8_t g_75 = 1UL;
+static int16_t g_77 = 0x457CL;
+static int32_t *g_96 = &g_8;
+static uint16_t g_101 = 0xCDAFL;
+static int16_t g_106 = 0x1EDAL;
+static uint16_t g_107[7] = {0x3862L,0x6F81L,0x3862L,0x3862L,0x6F81L,0x3862L,0x3862L};
+static int64_t g_110 = 0x137E2FFC3EBA449FLL;
