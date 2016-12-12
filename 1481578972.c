689316c19ece9321c3f51ff51277d4dfb6c34fd4
saisi <saisi@users.noreply.github.com>
2016-12-12 21:42:52 +0000

    1481578972 araban

---
+++ 1481578972.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1625506500
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xBC370782L;
+static int32_t g_5 = 0x069F30CCL;
+static uint16_t g_6 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
