0512e1a89b1a09e9a9d759ba2ec89b6316d45800
saisi <saisi@users.noreply.github.com>
2016-12-12 13:42:20 +0000

    1481550139 backslider

---
+++ 1481550139.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3683159654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t g_25[9][3] = {{(-5L),0x7F141C5AL,(-5L)},{0x5AA7F794L,(-3L),0x5AA7F794L},{(-5L),0x7F141C5AL,(-5L)},{0x5AA7F794L,(-3L),0x5AA7F794L},{(-5L),0x7F141C5AL,(-5L)},{0x5AA7F794L,(-3L),0x5AA7F794L},{(-5L),0x7F141C5AL,(-5L)},{0x5AA7F794L,(-3L),0x5AA7F794L},{(-5L),0x7F141C5AL,(-5L)}};
+static uint16_t g_31 = 1UL;
+static int32_t g_35 = 0xCF37692AL;
+static uint8_t g_54[8][6] = {{0x87L,255UL,0x7EL,1UL,0xBAL,0xAEL},{0xE4L,255UL,3UL,3UL,255UL,0xE4L},{255UL,0x09L,6UL,0UL,1UL,0xFDL},{3UL,0xAEL,1UL,0x09L,0xBAL,3UL},{6UL,1UL,0x09L,1UL,6UL,255UL},{255UL,255UL,0xBAL,6UL,3UL,0x7EL},{0UL,0xE4L,3UL,255UL,0x7EL,0x7EL},{0xC7L,0xBAL,0xBAL,0xC7L,0x50L,255UL}};
+static int32_t *g_73 = &g_35;
+static int32_t ** volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static int32_t g_74 = 0x5449CDFCL;
+static uint64_t g_85 = 6UL;
