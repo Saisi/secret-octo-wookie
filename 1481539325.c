d9f24f17b098da863670272a89a5a9a15d605979
saisi <saisi@users.noreply.github.com>
2016-12-12 10:42:05 +0000

    1481539325 balkanization

---
+++ 1481539325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      346594697
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0UL;/* VOLATILE GLOBAL g_2 */
+static volatile uint8_t g_3 = 1UL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_5 = (-1L);
+static int32_t g_8 = 0x702E6337L;
+static volatile uint32_t g_9 = 0xE44FA3FDL;/* VOLATILE GLOBAL g_9 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
