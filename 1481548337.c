56e34a0f5d9d5cdfb8da8dbe3ec59d1f0ff744ed
saisi <saisi@users.noreply.github.com>
2016-12-12 13:12:18 +0000

    1481548337 Bahner

---
+++ 1481548337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3974206369
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static volatile int64_t g_6 = 7L;/* VOLATILE GLOBAL g_6 */
+static int16_t g_7 = 0x61D2L;
+static volatile uint64_t g_8 = 0xA7BC99958900FDB6LL;/* VOLATILE GLOBAL g_8 */
+static volatile uint64_t g_11 = 0xE92E3F9150FAEB38LL;/* VOLATILE GLOBAL g_11 */
+static volatile int32_t g_68 = 0x1EC3D250L;/* VOLATILE GLOBAL g_68 */
+static int32_t g_69 = 3L;
+static uint16_t g_74 = 0x4064L;
+static int8_t g_77 = 0x2FL;
