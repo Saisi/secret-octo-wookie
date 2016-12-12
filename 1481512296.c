7bf3258119d889996863c26fe1329e4a477a57a4
saisi <saisi@users.noreply.github.com>
2016-12-12 03:11:37 +0000

    1481512296 Atherurus

---
+++ 1481512296.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2427965605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 17;
+   signed f1 : 4;
+   signed f2 : 23;
+   const unsigned f3 : 6;
+   unsigned f4 : 31;
+   volatile signed f5 : 28;
+   signed f6 : 11;
