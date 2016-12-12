4da79b62d3e1bce9d46d452a4f6eb9614d624ae3
saisi <saisi@users.noreply.github.com>
2016-12-12 09:11:59 +0000

    1481533917 Babita

---
+++ 1481533917.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2963414812
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint64_t  f0;
+   const unsigned f1 : 9;
+   const int16_t  f2;
+};
+#pragma pack(pop)
+
+struct S1 {
