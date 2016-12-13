391e6cf629c60043108bcce6ee49c3fa8dbdb0f9
saisi <saisi@users.noreply.github.com>
2016-12-13 17:14:10 +0000

    1481649249 antilogistic

---
+++ 1481649249.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3777755499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int16_t  f0;
+   int8_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_4 = 0x7A73D2D672A8358BLL;/* VOLATILE GLOBAL g_4 */
+static int16_t g_48[7] = {0x9A18L,0x9A18L,0x9A18L,0x9A18L,0x9A18L,0x9A18L,0x9A18L};
+static int16_t g_70 = 0xACD6L;
