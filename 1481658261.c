f0a5d9af100dbacc6f80e4f457bb92342eaee01f
saisi <saisi@users.noreply.github.com>
2016-12-13 19:44:21 +0000

    1481658261 antidisestablishmentarian

---
+++ 1481658261.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1457786683
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   const unsigned f0 : 11;
+   volatile int8_t  f1;
+   volatile signed f2 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U2 g_2[9] = {{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL},{0xA5EFA20BL}};
+
+
