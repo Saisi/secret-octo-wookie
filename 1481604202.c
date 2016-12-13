13fafe48b6d95ef3fb2d81b121a354cf9310b748
saisi <saisi@users.noreply.github.com>
2016-12-13 04:43:22 +0000

    1481604202 acutest

---
+++ 1481604202.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2314672196
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 16;
+   unsigned f1 : 5;
+   signed f2 : 19;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
