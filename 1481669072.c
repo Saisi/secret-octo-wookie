c16a0bdbc7089ed2bdf5d422584df3e15ddc30d9
saisi <saisi@users.noreply.github.com>
2016-12-13 22:44:32 +0000

    1481669072 Antipodes

---
+++ 1481669072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2621584592
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 22;
+   const volatile signed f1 : 19;
+   volatile unsigned f2 : 9;
+   const volatile unsigned f3 : 17;
+   volatile unsigned f4 : 28;
+   unsigned f5 : 3;
+   unsigned f6 : 21;
+};
+
