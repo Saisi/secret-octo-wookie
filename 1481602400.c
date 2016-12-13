1389d0e6fc83b702c3d6989bbf4b4f94837f1966
saisi <saisi@users.noreply.github.com>
2016-12-13 04:13:20 +0000

    1481602400 alcoves

---
+++ 1481602400.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2549784859
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 12;
+   signed f1 : 19;
+   const volatile unsigned f2 : 10;
+   signed f3 : 13;
+   signed f4 : 10;
+   signed f5 : 27;
+   unsigned f6 : 14;
