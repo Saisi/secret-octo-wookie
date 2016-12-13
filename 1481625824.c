58bb7c252205271783ab29fad6302884d3917bd2
saisi <saisi@users.noreply.github.com>
2016-12-13 10:43:45 +0000

    1481625824 acantholysis

---
+++ 1481625824.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3185979601
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 7;
+   volatile signed f1 : 2;
+   signed f2 : 29;
+   uint16_t  f3;
+};
+#pragma pack(pop)
+
