3ebac98304a15b3fb835d91ebfa241dc63c1356e
saisi <saisi@users.noreply.github.com>
2016-12-13 12:13:49 +0000

    1481631229 Aeria

---
+++ 1481631229.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4152294179
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[3][7][1] = {{{(-1L)},{(-3L)},{(-1L)},{(-3L)},{(-1L)},{(-3L)},{(-1L)}},{{(-3L)},{(-1L)},{(-3L)},{(-1L)},{(-3L)},{(-1L)},{(-3L)}},{{(-1L)},{(-3L)},{(-1L)},{(-3L)},{(-1L)},{(-3L)},{(-1L)}}};
+static int32_t g_4 = (-1L);
+static int32_t g_5 = 1L;
+static volatile int32_t g_6 = 0x23E64F14L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x9B7928FBL;
+static uint64_t g_11 = 0x1BF0796840EB4495LL;
+static uint64_t g_37 = 8UL;
+static uint64_t * const  volatile *g_46 = (void*)0;
+static uint8_t g_50 = 0x2FL;
