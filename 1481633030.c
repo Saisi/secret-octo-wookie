d224142eca864ed02daed03a3cd95c0997941093
saisi <saisi@users.noreply.github.com>
2016-12-13 12:43:51 +0000

    1481633030 ateloglossia

---
+++ 1481633030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3204876627
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   volatile int8_t  f1;
+   uint16_t  f2;
+   const volatile signed f3 : 25;
+   uint8_t  f4;
+};
+
+union U1 {
+   const unsigned f0 : 30;
