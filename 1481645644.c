50388154a2985c192aaab519f8cf12a174a10ab6
saisi <saisi@users.noreply.github.com>
2016-12-13 16:14:06 +0000

    1481645644 babesiasis

---
+++ 1481645644.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1263268126
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 29;
+   const volatile unsigned f1 : 30;
+   unsigned f2 : 8;
+   volatile signed f3 : 20;
+   unsigned f4 : 19;
+   signed f5 : 30;
+};
