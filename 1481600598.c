71b80a36db7bb426d94adc989ba16f98029de800
saisi <saisi@users.noreply.github.com>
2016-12-13 03:43:19 +0000

    1481600598 backadation

---
+++ 1481600598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      415699265
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 31;
+   unsigned f1 : 26;
+   int64_t  f2;
+   int16_t  f3;
+   uint64_t  f4;
+   const signed f5 : 22;
+   signed f6 : 1;
+   volatile uint32_t  f7;
+   volatile signed f8 : 1;
