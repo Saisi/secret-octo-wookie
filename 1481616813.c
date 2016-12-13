9ac736ec0223dffbfc5920621c5c3de924189f30
saisi <saisi@users.noreply.github.com>
2016-12-13 08:13:34 +0000

    1481616813 antipepsin

---
+++ 1481616813.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3944554847
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_7 = 0x2A4F402EL;
+static int32_t g_25 = 9L;
+static int32_t *g_24 = &g_25;
+static int32_t * volatile * volatile g_55 = (void*)0;/* VOLATILE GLOBAL g_55 */
+static int32_t * const g_62 = (void*)0;
+static int32_t * const *g_61 = &g_62;
+static uint8_t g_68 = 0xD6L;
+static uint8_t g_70 = 1UL;
+static const int32_t *g_85 = &g_25;
