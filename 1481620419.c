387e08e78beafda0a08298ed883c33653be38229
saisi <saisi@users.noreply.github.com>
2016-12-13 09:13:39 +0000

    1481620419 anopisthograph

---
+++ 1481620419.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3110440332
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   signed f1 : 1;
+   const signed f2 : 17;
+};
+
+struct S1 {
+   int32_t  f0;
+   int8_t  f1;
+   volatile unsigned f2 : 22;
