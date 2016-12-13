1811adb442bea6a4d652e6697a66ce859e890f03
saisi <saisi@users.noreply.github.com>
2016-12-13 15:14:00 +0000

    1481642039 autolytic

---
+++ 1481642039.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3399328422
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   int8_t  f1;
+   volatile uint16_t  f2;
+   volatile uint32_t  f3;
+};
+#pragma pack(pop)
+
