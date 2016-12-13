04c713eb430eb008c780789c6bfbb3f05449939a
saisi <saisi@users.noreply.github.com>
2016-12-13 00:13:00 +0000

    1481587980 actinobacillus

---
+++ 1481587980.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3558047968
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 30;
+   uint64_t  f1;
+   const signed f2 : 19;
+   volatile signed f3 : 9;
+   unsigned f4 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_6[2] = {0xB9AEL,0xB9AEL};
