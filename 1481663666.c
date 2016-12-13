43bdb466b527702b4d68521c991d4011f46dbb8d
saisi <saisi@users.noreply.github.com>
2016-12-13 21:14:27 +0000

    1481663666 antiromance

---
+++ 1481663666.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2384839661
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile signed f1 : 10;
+   signed f2 : 24;
+   const volatile signed f3 : 28;
+   signed f4 : 19;
+   const unsigned f5 : 21;
+   unsigned f6 : 4;
+   volatile uint32_t  f7;
+   unsigned f8 : 25;
