d34ac57dad38d865450e137a66ff38d650d62fca
saisi <saisi@users.noreply.github.com>
2016-12-13 14:43:58 +0000

    1481640237 autotoxication

---
+++ 1481640237.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3745295093
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 25;
+   const signed f1 : 12;
+   volatile unsigned f2 : 31;
+   unsigned f3 : 14;
+   int32_t  f4;
+   signed f5 : 25;
+};
+
+/* --- GLOBAL VARIABLES --- */
