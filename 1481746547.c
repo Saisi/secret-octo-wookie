e5aadf08131ddb7b7e85704bd60c2a602dd12958
saisi <saisi@users.noreply.github.com>
2016-12-14 20:15:49 +0000

    1481746547 articulator

---
+++ 1481746547.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1222116293
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static uint8_t g_21 = 0x2FL;
+static int64_t g_74 = 0L;
+static uint32_t g_77 = 0x392E18E6L;
+static uint8_t g_92 = 0x8AL;
+static uint64_t g_96 = 0xF1997376B242CFF4LL;
+static uint16_t g_110[4][4][3] = {{{1UL,65535UL,0x9AB9L},{65533UL,0x02D3L,0UL},{65535UL,0x9AB9L,1UL},{65533UL,1UL,0xAB14L}},{{1UL,0UL,0UL},{0xE9F7L,0UL,0UL},{0x46DAL,65535UL,0xAB14L},{65535UL,0x46DAL,1UL}},{{0x6B07L,0xB762L,0UL},{0x5F64L,0x46DAL,0x9AB9L},{0x02D3L,65535UL,1UL},{0UL,0UL,0x46DAL}},{{0UL,0UL,0x5F64L},{0x02D3L,1UL,0x476FL},{0x5F64L,0x9AB9L,0x02D3L},{0x6B07L,65535UL,65533UL}}};
+static uint16_t g_132 = 0UL;
+static uint64_t *** volatile g_136 = (void*)0;/* VOLATILE GLOBAL g_136 */
