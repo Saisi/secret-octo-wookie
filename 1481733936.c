9c28451280b1c90e88936dc3eda99caebce59ab5
saisi <saisi@users.noreply.github.com>
2016-12-14 16:45:36 +0000

    1481733936 asperse

---
+++ 1481733936.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      484726655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   const int64_t  f1;
+   int32_t  f2;
+   uint64_t  f3;
+   const int32_t  f4;
+   uint64_t  f5;
+   const int16_t  f6;
+};
+
