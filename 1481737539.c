01057577563af7eb7ff33cd18f313e894abfd8cf
saisi <saisi@users.noreply.github.com>
2016-12-14 17:45:39 +0000

    1481737539 afterhold

---
+++ 1481737539.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1185431219
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   const uint16_t  f2;
+   uint64_t  f3;
+   int32_t  f4;
+   const uint16_t  f5;
+   volatile uint64_t  f6;
+};
+
