359c00125b3d45786127e2e1e22b4c9d59867511
saisi <saisi@users.noreply.github.com>
2016-12-14 04:14:49 +0000

    1481688889 anticlassicalism

---
+++ 1481688889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      646351819
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int16_t  f1;
+   int16_t  f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   const uint32_t  f0;
+   uint16_t  f1;
