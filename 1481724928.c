9d529c0c2de4cb8990309229a562fb4ec941f09a
saisi <saisi@users.noreply.github.com>
2016-12-14 14:15:29 +0000

    1481724928 arachnoidea

---
+++ 1481724928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1511897627
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   uint32_t  f1;
+   volatile uint32_t  f2;
+   uint8_t  f3;
+   volatile int8_t  f4;
+   int8_t  f5;
+   uint16_t  f6;
