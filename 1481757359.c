470b353e8431aebb4e25102c371e0b61f60bba5c
saisi <saisi@users.noreply.github.com>
2016-12-14 23:16:00 +0000

    1481757359 Astronautarum

---
+++ 1481757359.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3264563844
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 16;
+   signed f1 : 5;
+   int32_t  f2;
+   int8_t * f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xD214C405L;
