62d9cd3ea4cd709483e37b64c60675430ea69769
saisi <saisi@users.noreply.github.com>
2016-12-14 11:45:20 +0000

    1481715920 Alana

---
+++ 1481715920.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3510753836
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 4;
+   const signed f1 : 9;
+   signed f2 : 7;
+   unsigned f3 : 23;
+   const unsigned f4 : 10;
+   signed f5 : 25;
+   unsigned f6 : 19;
+   unsigned f7 : 28;
+   volatile unsigned f8 : 16;
