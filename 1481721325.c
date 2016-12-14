83ec41584f8ea251efe44654b330889cc1fbab12
saisi <saisi@users.noreply.github.com>
2016-12-14 13:15:25 +0000

    1481721325 arete

---
+++ 1481721325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      194889815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 10;
+   int16_t  f1;
+   const unsigned f2 : 9;
+   const volatile unsigned f3 : 9;
+   volatile signed f4 : 12;
+   signed f5 : 10;
+   const volatile unsigned f6 : 5;
+   unsigned f7 : 11;
+   uint64_t  f8;
