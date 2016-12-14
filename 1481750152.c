ddadea58f3a33268b262692d9b83828e191ede4d
saisi <saisi@users.noreply.github.com>
2016-12-14 21:15:53 +0000

    1481750152 approachless

---
+++ 1481750152.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3455083744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint16_t  f0;
+   int32_t  f1;
+   const uint64_t  f2;
