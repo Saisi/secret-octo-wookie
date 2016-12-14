f139976b39af9694d17474059144fbc927dc0737
saisi <saisi@users.noreply.github.com>
2016-12-14 22:45:58 +0000

    1481755558 addlepated

---
+++ 1481755558.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      724113625
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   int32_t  f1;
+   int16_t  f2;
+   const volatile int64_t  f3;
+};
+#pragma pack(pop)
+
