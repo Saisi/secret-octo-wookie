af0200ab41a8b89bd170347d2b6d60342d8766c0
saisi <saisi@users.noreply.github.com>
2016-12-14 05:14:52 +0000

    1481692491 antanagoge

---
+++ 1481692491.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1240254859
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   volatile int32_t  f1;
+   const unsigned f2 : 2;
+   uint32_t  f3;
+};
+
+union U1 {
+   const volatile uint16_t  f0;
+   const int64_t  f1;
