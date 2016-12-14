0680f1ab78e148d9c72ee355e1279a968d11cdc4
saisi <saisi@users.noreply.github.com>
2016-12-14 06:45:00 +0000

    1481697899 aphanites

---
+++ 1481697899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3636369361
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_11 = 0x438BL;
+static int16_t g_13[3] = {1L,1L,1L};
+static uint32_t g_23 = 0x88753E1EL;
+static int32_t g_55 = 0xB1F356D9L;
+static int32_t *g_54[3] = {&g_55,&g_55,&g_55};
+static int16_t g_84 = 0x7E7BL;
+static uint64_t g_90 = 0xE630EE3E797ADA24LL;
+static uint8_t g_93 = 0x11L;
+static uint64_t *g_114 = &g_90;
