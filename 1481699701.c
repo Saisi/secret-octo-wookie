f9a1aeebd1b9c3299d5ef884f875653c87f95614
saisi <saisi@users.noreply.github.com>
2016-12-14 07:15:01 +0000

    1481699701 antipolice

---
+++ 1481699701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3201555602
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2[3] = {0x3B9DL,0x3B9DL,0x3B9DL};
+static volatile int32_t g_5 = (-8L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t * volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_6 = 0x59951EA6L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x8D020BD2L;
+static volatile int32_t g_13[4] = {(-7L),(-7L),(-7L),(-7L)};
+static volatile int32_t g_14 = 0x0D018D30L;/* VOLATILE GLOBAL g_14 */
+static volatile int32_t g_15 = 0x244C3F4FL;/* VOLATILE GLOBAL g_15 */
+static int32_t g_16[3] = {7L,7L,7L};
