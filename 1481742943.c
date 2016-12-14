725d5c7008c592a8d2c8fbce3507f48aa791ae2b
saisi <saisi@users.noreply.github.com>
2016-12-14 19:15:44 +0000

    1481742943 Antilochus

---
+++ 1481742943.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2054862555
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   unsigned f1 : 19;
+   volatile signed f2 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xAFE01BD9L;
+static int32_t g_7 = 0x8A94B2ABL;
+static uint16_t g_11[10] = {0xEC4FL,0x56E7L,65535UL,65535UL,0x56E7L,0xEC4FL,0x56E7L,65535UL,65535UL,0x56E7L};
