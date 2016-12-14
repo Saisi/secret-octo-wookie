40a9876c0e595c9d85d198fc7a0ca918023f7642
saisi <saisi@users.noreply.github.com>
2016-12-14 12:15:22 +0000

    1481717722 automysophobia

---
+++ 1481717722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3388222191
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static int32_t g_16 = 0L;
+static volatile uint16_t g_17 = 0x19A6L;/* VOLATILE GLOBAL g_17 */
+static int8_t g_40 = 0x92L;
+static int8_t g_53[5][2][4] = {{{0x19L,4L,4L,0x19L},{4L,0x19L,0L,0x45L}},{{4L,0L,4L,0x77L},{0x19L,0x45L,0x77L,0x77L}},{{0L,0L,(-1L),0x45L},{0x45L,0x19L,(-1L),0x19L}},{{0L,4L,0x77L,(-1L)},{0x19L,4L,4L,0x19L}},{{4L,0x19L,0L,0x45L},{4L,0L,4L,0x77L}}};
+static int32_t g_88 = 1L;
+static int32_t g_91 = 6L;
+static uint16_t g_102 = 0UL;
+static uint16_t g_108 = 0x07BDL;
