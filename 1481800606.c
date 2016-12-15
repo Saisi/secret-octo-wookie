4240f608dfb24a2500bfccea33ed8e87ed030845
saisi <saisi@users.noreply.github.com>
2016-12-15 11:16:47 +0000

    1481800606 abdicable

---
+++ 1481800606.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      613887433
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint16_t  f0;
+   int8_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 1;
+   unsigned f1 : 21;
