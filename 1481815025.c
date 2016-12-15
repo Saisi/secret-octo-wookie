9ed1bff9d520058c1335bfe00e93f00522b61c7d
saisi <saisi@users.noreply.github.com>
2016-12-15 15:17:05 +0000

    1481815025 anabaptize

---
+++ 1481815025.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4133869839
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 5L;
+static volatile int32_t g_5 = 0xC7C8A6B9L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xA1BF2B29L;
+static int32_t g_11 = 0x44D1878AL;
+static uint8_t g_15[8] = {255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL};
