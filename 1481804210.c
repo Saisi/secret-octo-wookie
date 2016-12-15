f49d453b0d6b24deeaded3cd1967dceaac47d9e0
saisi <saisi@users.noreply.github.com>
2016-12-15 12:16:51 +0000

    1481804210 arrogances

---
+++ 1481804210.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2624800678
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 1;
+   const unsigned f1 : 18;
+   volatile signed f2 : 12;
+   uint32_t  f3;
+   const volatile signed f4 : 9;
+   unsigned f5 : 17;
+   unsigned f6 : 14;
+   unsigned f7 : 15;
+};
