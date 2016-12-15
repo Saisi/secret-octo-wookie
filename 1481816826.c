7ad04ef5f169c38938a1afc5f8991aa37bb660e9
saisi <saisi@users.noreply.github.com>
2016-12-15 15:47:06 +0000

    1481816826 adjunction

---
+++ 1481816826.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2059354216
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xCFF0EA75L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xDD1E1E87L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-6L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0x108F8256L;
+static volatile int32_t g_9 = 2L;/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10 = (-9L);/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = (-1L);
+static int32_t *g_29[7][7][4] = {{{&g_11,&g_5,(void*)0,&g_5},{(void*)0,(void*)0,&g_11,&g_11},{&g_5,(void*)0,&g_11,&g_11},{&g_5,&g_11,&g_11,&g_5},{&g_11,&g_5,(void*)0,&g_11},{&g_11,&g_5,&g_11,(void*)0},{&g_11,&g_5,&g_5,(void*)0}},{{(void*)0,&g_5,(void*)0,&g_11},{&g_11,&g_5,&g_5,&g_5},{&g_11,&g_11,&g_11,&g_11},{&g_11,(void*)0,&g_5,&g_11},{&g_11,(void*)0,&g_5,&g_5},{&g_5,&g_5,(void*)0,&g_5},{&g_5,&g_11,&g_11,&g_11}},{{&g_5,&g_11,(void*)0,&g_11},{&g_11,&g_11,&g_11,(void*)0},{&g_11,&g_5,(void*)0,&g_5},{&g_5,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_5,(void*)0},{&g_5,&g_11,&g_5,&g_11},{&g_5,&g_11,&g_5,(void*)0}},{{&g_11,&g_5,(void*)0,(void*)0},{&g_11,&g_11,&g_5,&g_11},{&g_11,&g_11,&g_11,&g_11},{&g_5,(void*)0,(void*)0,&g_11},{&g_5,&g_11,&g_11,(void*)0},{&g_5,&g_11,(void*)0,(void*)0},{&g_5,(void*)0,&g_5,&g_11}},{{&g_5,&g_5,&g_5,&g_5},{&g_11,&g_11,(void*)0,&g_5},{&g_11,&g_11,(void*)0,&g_5},{&g_11,&g_11,&g_5,(void*)0},{&g_5,&g_11,&g_5,&g_11},{&g_5,&g_11,(void*)0,(void*)0},{&g_5,&g_11,&g_11,(void*)0}},{{&g_5,&g_11,(void*)0,&g_5},{&g_5,&g_11,&g_11,&g_5},{&g_11,&g_11,&g_5,&g_5},{&g_11,&g_11,(void*)0,&g_11},{&g_11,(void*)0,&g_5,&g_5},{&g_5,&g_11,&g_5,(void*)0},{&g_5,&g_5,&g_5,(void*)0}},{{&g_11,(void*)0,&g_11,&g_5},{&g_5,(void*)0,(void*)0,&g_5},{&g_11,&g_11,&g_11,&g_11},{&g_11,&g_5,(void*)0,(void*)0},{&g_5,&g_5,&g_11,(void*)0},{&g_5,&g_11,(void*)0,(void*)0},{&g_5,&g_11,&g_5,&g_5}}};
+static int32_t ** volatile g_28[4][4] = {{&g_29[1][6][2],&g_29[3][1][2],(void*)0,&g_29[3][1][2]},{&g_29[3][1][2],&g_29[1][6][2],(void*)0,(void*)0},{&g_29[1][6][2],&g_29[1][6][2],&g_29[3][1][2],(void*)0},{&g_29[1][6][2],&g_29[1][6][2],&g_29[1][6][2],&g_29[3][1][2]}};
