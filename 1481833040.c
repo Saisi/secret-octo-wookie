80c8cda115893b0ec043d5aea3da222ae5bde450
saisi <saisi@users.noreply.github.com>
2016-12-15 20:17:20 +0000

    1481833040 Avivah

---
+++ 1481833040.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2848374529
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1][9] = {{1L,(-2L),1L,1L,(-2L),1L,1L,(-2L),1L}};
+static int32_t g_7 = 0x810D21DFL;
+static int64_t g_32[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_35 = 1L;
+static int16_t g_68 = (-4L);
+static int8_t g_71 = (-3L);
+static volatile int32_t g_77[7] = {(-10L),(-10L),(-10L),(-10L),(-10L),(-10L),(-10L)};
+static volatile int32_t * volatile g_76[1] = {&g_77[3]};
+static int64_t g_80 = 0xE525B78C6AEE34D1LL;
