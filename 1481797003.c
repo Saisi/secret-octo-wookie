6f1ffeb1b85203c15030269c7a73e5419e8f8cc1
saisi <saisi@users.noreply.github.com>
2016-12-15 10:16:44 +0000

    1481797003 apehood

---
+++ 1481797003.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2934982904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 10;
+   volatile uint64_t  f1;
+   const unsigned f2 : 15;
+   const uint64_t  f3;
+   signed f4 : 25;
+   unsigned f5 : 23;
+   volatile unsigned : 0;
+};
+
