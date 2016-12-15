1abbdd93abb17af469cdb08544f663c55a4ced63
saisi <saisi@users.noreply.github.com>
2016-12-15 17:47:11 +0000

    1481824031 anthocephalous

---
+++ 1481824031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3714233173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_2[1] = {0x57L};
+static int8_t g_25 = 0xC4L;
+static int16_t g_27 = 0x7C2CL;
+static int32_t g_69[6][2][3] = {{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}},{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}},{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}},{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}},{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}},{{0xF12AFBCAL,(-8L),0xF12AFBCAL},{1L,1L,1L}}};
+static int32_t g_92 = (-7L);
+static int32_t *g_102 = &g_69[0][0][2];
+static int32_t *g_103[4] = {&g_69[3][1][2],&g_69[3][1][2],&g_69[3][1][2],&g_69[3][1][2]};
+static uint32_t g_110 = 0UL;
+static int32_t g_113 = (-9L);
