70b95522af8370c552357fd9fb469fa7bfa25283
saisi <saisi@users.noreply.github.com>
2016-12-15 03:16:15 +0000

    1481771775 authigenic

---
+++ 1481771775.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      407783078
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x801897A4L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[3][1] = {{(-1L)},{(-1L)},{(-1L)}};
+static volatile int32_t g_4[8][10] = {{0xA5F9C710L,0x744D0289L,0x744D0289L,0xA5F9C710L,1L,0L,3L,0L,3L,0L},{(-5L),0x7CEFA9D1L,1L,0x7CEFA9D1L,(-5L),7L,0x744D0289L,3L,3L,0x744D0289L},{0L,7L,0xA5F9C710L,0xA5F9C710L,7L,0L,(-3L),0x744D0289L,(-5L),0x744D0289L},{0x7CEFA9D1L,0xA5F9C710L,(-5L),3L,(-5L),0xA5F9C710L,0x7CEFA9D1L,(-3L),0L,0L},{0x7CEFA9D1L,0L,0L,7L,7L,1L,(-5L),0x76881957L,0xA5F9C710L,0L},{1L,(-5L),0x76881957L,0xA5F9C710L,0L,0xA5F9C710L,0x76881957L,(-5L),1L,7L},{0L,0x744D0289L,0x76881957L,0L,(-5L),(-5L),0L,0x76881957L,0x744D0289L,0L},{0x744D0289L,0xA5F9C710L,1L,0L,3L,0L,3L,0L,1L,0xA5F9C710L}};
+static volatile int32_t g_5 = 5L;/* VOLATILE GLOBAL g_5 */
