c672f40140cf9ce1b4017bf5eb2343fca86c456b
saisi <saisi@users.noreply.github.com>
2016-12-15 13:16:54 +0000

    1481807813 ancylopod

---
+++ 1481807813.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1717577058
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_16 = 0L;
+static int32_t * volatile g_15 = &g_16;/* VOLATILE GLOBAL g_15 */
+static uint32_t g_57 = 0x84CBF776L;
+static int16_t g_60[7][6] = {{(-2L),0x5250L,(-5L),0x5250L,(-2L),(-1L)},{0x8224L,(-1L),0x695EL,(-2L),1L,1L},{4L,1L,1L,(-1L),1L,1L},{0xD06DL,0x695EL,0x695EL,0xD06DL,1L,(-1L)},{1L,0x6B6CL,(-5L),0xCA72L,0x5250L,1L},{(-5L),4L,1L,1L,0x5250L,1L},{0L,0x6B6CL,0L,5L,1L,4L}};
+static uint8_t g_65 = 248UL;
+static uint32_t g_79 = 4294967294UL;
+static uint32_t g_81 = 0xCDCE3FF5L;
+static int64_t g_86 = 0x7422E233AD2D1E56LL;
+static int32_t * volatile g_91 = (void*)0;/* VOLATILE GLOBAL g_91 */
