e2dcdd28fa635f546f5dfee6d4cce51c49a600fb
saisi <saisi@users.noreply.github.com>
2016-12-15 21:47:24 +0000

    1481838444 armbands

---
+++ 1481838444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2699736410
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[9] = {(-5L),(-5L),(-5L),(-5L),(-5L),(-5L),(-5L),(-5L),(-5L)};
+static volatile int32_t g_4 = 0x6748F69FL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x1E814B73L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7[4] = {6L,6L,6L,6L};
+static int32_t g_8[2] = {0L,0L};
+static volatile int32_t g_10 = 0xF2579253L;/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = 0xC865B49DL;
+static uint32_t g_19 = 1UL;
