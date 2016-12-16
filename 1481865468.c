0a08784135857db3b9b84b7ec414b46eb8caca88
saisi <saisi@users.noreply.github.com>
2016-12-16 05:17:49 +0000

    1481865468 anabiotic

---
+++ 1481865468.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1035786636
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_23[8] = {(-1L),1L,(-1L),(-1L),1L,(-1L),(-1L),1L};
+static int32_t g_44 = 3L;
+static uint64_t g_46[6] = {0x8EB7BFBC35CF455DLL,0x8EB7BFBC35CF455DLL,0x8EB7BFBC35CF455DLL,0x8EB7BFBC35CF455DLL,0x8EB7BFBC35CF455DLL,0x8EB7BFBC35CF455DLL};
+static uint16_t g_50[9] = {0UL,1UL,1UL,0UL,1UL,1UL,0UL,1UL,1UL};
+static int32_t g_58 = 0x7B1ECBC5L;
+static volatile int8_t g_65 = (-1L);/* VOLATILE GLOBAL g_65 */
+static volatile uint32_t g_67 = 0x1BFD6FFBL;/* VOLATILE GLOBAL g_67 */
+static int32_t g_86 = 0xE5145DB5L;
+static int32_t g_97 = (-1L);
