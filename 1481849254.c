c5d7f9290ac77325c2b3257d671b7d55d2d7246d
saisi <saisi@users.noreply.github.com>
2016-12-16 00:47:35 +0000

    1481849254 ampalea

---
+++ 1481849254.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1520607966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = (-6L);
+static int8_t g_5 = 0x5DL;
+static int32_t g_41 = 0x04F77722L;
+static int64_t g_43 = 0xAA4807C1F4B9EFECLL;
+static int64_t g_44 = 2L;
