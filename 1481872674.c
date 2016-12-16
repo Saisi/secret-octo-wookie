9808bb1f8aac57a822fb8793eb6514238cadc952
saisi <saisi@users.noreply.github.com>
2016-12-16 07:17:54 +0000

    1481872674 acetosalicylic

---
+++ 1481872674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4270668042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-3L);
+static uint8_t g_28 = 1UL;
+static uint16_t g_35[7][5] = {{0xCC9DL,65534UL,65534UL,0xCC9DL,0UL},{0xCC9DL,0x6461L,0x86B6L,0x86B6L,0x6461L},{0UL,65534UL,0x86B6L,1UL,1UL},{65534UL,0UL,65534UL,0x86B6L,1UL},{0x6461L,0xCC9DL,1UL,0xCC9DL,0x6461L},{65534UL,0xCC9DL,0UL,0x6461L,0UL},{0UL,0UL,1UL,0x6461L,65530UL}};
+static volatile int8_t g_39 = 0xCAL;/* VOLATILE GLOBAL g_39 */
+static int16_t g_41 = 0x5B79L;
+static int32_t g_44 = 0L;
+static int32_t * volatile g_43[2] = {&g_44,&g_44};
+static uint8_t *g_56 = &g_28;
+static int32_t g_88 = 0L;
