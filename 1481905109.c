dcc8ff96edf376de7ec717cc5cb37856973d2fdd
saisi <saisi@users.noreply.github.com>
2016-12-16 16:18:29 +0000

    1481905109 Archaean

---
+++ 1481905109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1832705416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 4UL;
+static int32_t g_26 = 0L;
+static const uint64_t *g_46 = (void*)0;
+static uint32_t g_66 = 0x9C674F98L;
+static uint16_t g_87 = 65535UL;
+static volatile uint16_t g_91[8][4] = {{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL},{4UL,4UL,4UL,4UL}};
+static volatile uint16_t * volatile g_90[1][9] = {{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0}};
+static int8_t g_95 = 0xA2L;
+static uint64_t g_107 = 0x4372BB1342F27840LL;
