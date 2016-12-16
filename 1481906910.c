216030222e09f2ff21d1ac7951eb45192b188056
saisi <saisi@users.noreply.github.com>
2016-12-16 16:48:31 +0000

    1481906910 amebula

---
+++ 1481906910.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      585174503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int16_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+};
+
+struct S1 {
+   const volatile unsigned f0 : 1;
+   unsigned f1 : 24;
+   volatile signed : 0;
