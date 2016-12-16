fd983a2cff6714ef0ab9fc811687f326a2dd6751
saisi <saisi@users.noreply.github.com>
2016-12-16 21:48:51 +0000

    1481924930 Balthazar

---
+++ 1481924930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1460791438
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile signed f1 : 18;
+   uint16_t  f2;
+   volatile int64_t  f3;
+   int64_t  f4;
+   int32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
