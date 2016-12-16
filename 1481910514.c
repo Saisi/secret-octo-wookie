e069d7a704b2c3219b9715bfdae4aa223316726d
saisi <saisi@users.noreply.github.com>
2016-12-16 17:48:34 +0000

    1481910514 acoustoelectric

---
+++ 1481910514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3332160143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   uint32_t  f1;
+   int32_t  f2;
+   const uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_15 = 0x78ABC106L;
+static int8_t g_22 = (-2L);
