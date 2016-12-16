496dd3b016452a5a1a968919f96f6f981194839d
saisi <saisi@users.noreply.github.com>
2016-12-16 07:47:56 +0000

    1481874475 baconize

---
+++ 1481874475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2426991445
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   signed f0 : 7;
+   int64_t  f1;
+   const int64_t  f2;
+};
