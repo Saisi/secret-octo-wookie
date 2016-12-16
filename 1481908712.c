0117bc663e75a11678e3813fa10ae5e6fc456083
saisi <saisi@users.noreply.github.com>
2016-12-16 17:18:33 +0000

    1481908712 Agaristidae

---
+++ 1481908712.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4249651192
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-10L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x596960F5L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[10] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile int32_t g_5 = 0xADE2167FL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[1][5] = {{0x62E08974L,0x62E08974L,0x62E08974L,0x62E08974L,0x62E08974L}};
