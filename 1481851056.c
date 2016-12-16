6351cb67e9e793cae85b0b3c89aff530a0bae212
saisi <saisi@users.noreply.github.com>
2016-12-16 01:17:37 +0000

    1481851056 admonitorial

---
+++ 1481851056.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      65939998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   const volatile int8_t  f0;
+   volatile int8_t  f1;
+   signed : 0;
+};
