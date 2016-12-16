65b350770a2736f16d0dfed4090a3376589059cf
saisi <saisi@users.noreply.github.com>
2016-12-16 06:47:53 +0000

    1481870873 anaesthesiologist

---
+++ 1481870873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2119002304
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x4CDB8FAAL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
