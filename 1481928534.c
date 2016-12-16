c7eebb1ac095e125bc65a2ff2f22554ac3886eef
saisi <saisi@users.noreply.github.com>
2016-12-16 22:48:54 +0000

    1481928534 anthropomorphize

---
+++ 1481928534.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4117119718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_16 = (void*)0;
+static int32_t g_32 = 0L;
+static uint64_t g_39 = 18446744073709551615UL;
+static volatile int32_t g_42 = (-8L);/* VOLATILE GLOBAL g_42 */
+static int64_t g_43 = 0x657CF7A0211E9A9DLL;
+static uint32_t g_45 = 0x25701832L;
+static int32_t g_50[4][1][4] = {{{0xDBC1552CL,0xDBC1552CL,0xDBC1552CL,0xDBC1552CL}},{{0xDBC1552CL,0xDBC1552CL,0xDBC1552CL,0xDBC1552CL}},{{0xDBC1552CL,0xDBC1552CL,0xDBC1552CL,0xDBC1552CL}},{{0xDBC1552CL,0xDBC1552CL,0xDBC1552CL,0xDBC1552CL}}};
+static uint16_t g_70 = 0x8A3DL;
+static uint16_t g_86 = 7UL;
