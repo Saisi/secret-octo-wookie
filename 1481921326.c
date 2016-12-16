bbbd7d664823e6816e66bd0d07c384f6ae611624
saisi <saisi@users.noreply.github.com>
2016-12-16 20:48:46 +0000

    1481921326 anticosmetic

---
+++ 1481921326.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1868823800
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   const unsigned f1 : 23;
+   unsigned f2 : 15;
+   volatile unsigned f3 : 21;
+   unsigned f4 : 6;
+   unsigned f5 : 26;
+   signed f6 : 19;
+   const signed : 0;
+   signed f7 : 17;
