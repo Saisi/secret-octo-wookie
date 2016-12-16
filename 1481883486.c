4f3e3fd6701938305fcd4af8b00f9824158fbcd5
saisi <saisi@users.noreply.github.com>
2016-12-16 10:18:07 +0000

    1481883486 Aventine

---
+++ 1481883486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3708665559
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0x4D00693EL;
+static int16_t g_60 = 6L;
+static int16_t g_62 = 0x6953L;
+static uint64_t g_73 = 0UL;
+static int32_t g_80 = 8L;
+static int32_t * volatile g_79 = &g_80;/* VOLATILE GLOBAL g_79 */
+static uint32_t g_101[7] = {0x749941B8L,0x749941B8L,0x749941B8L,0x749941B8L,0x749941B8L,0x749941B8L,0x749941B8L};
+static uint8_t g_105 = 255UL;
+static uint64_t g_108 = 3UL;
