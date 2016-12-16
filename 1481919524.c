e057f4e9253411f6c1f07364e5102e3bbf8a3b09
saisi <saisi@users.noreply.github.com>
2016-12-16 20:18:45 +0000

    1481919524 archanthropine

---
+++ 1481919524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1161887158
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 9;
+   const uint32_t  f1;
+   unsigned f2 : 2;
+   const signed f3 : 28;
+};
+#pragma pack(pop)
+
