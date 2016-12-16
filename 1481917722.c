fb8061c282a963601e1d059ef314122f49c9c3b6
saisi <saisi@users.noreply.github.com>
2016-12-16 19:48:42 +0000

    1481917722 Achaemenes

---
+++ 1481917722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1759920750
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 5;
+};
+
+union U1 {
+   volatile uint64_t  f0;
+   volatile int8_t  f1;
+   const int16_t  f2;
+   const uint8_t  f3;
+   const volatile int32_t  f4;
