a2b474d22f794b425821fbdc3fc56a606c738709
saisi <saisi@users.noreply.github.com>
2016-12-16 05:47:50 +0000

    1481867270 apodan

---
+++ 1481867270.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1354466083
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int32_t  f1;
+   volatile unsigned f2 : 23;
+   int8_t  f3;
+   signed f4 : 30;
+};
+
+union U1 {
+   const unsigned f0 : 14;
