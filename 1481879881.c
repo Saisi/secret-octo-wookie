5d4423a9fc6d6250deb33c0315c6d441d791b9da
saisi <saisi@users.noreply.github.com>
2016-12-16 09:18:02 +0000

    1481879881 Amon

---
+++ 1481879881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1493610382
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile uint16_t  f1;
+};
+
+union U1 {
+   const uint32_t  f0;
+   int64_t  f1;
+   volatile int32_t  f2;
+   uint64_t  f3;
