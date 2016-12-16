a6daf13e2c054d4eb3b7d1738873414b52255b84
saisi <saisi@users.noreply.github.com>
2016-12-16 08:48:00 +0000

    1481878079 anthroposomatology

---
+++ 1481878079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2126485227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6] = {(-5L),(-5L),(-5L),(-5L),(-5L),(-5L)};
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 1L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8[9][4] = {{0x609B2B53L,0L,0xDFBD6C3FL,0xC3A23FE5L},{0L,1L,1L,1L},{0xDDCD1665L,0x609B2B53L,0xC3A23FE5L,1L},{0xF7CD95DAL,1L,0xF7CD95DAL,0xC3A23FE5L},{0x81A7ECC2L,0L,0xD0DA0BDBL,0x81A7ECC2L},{0xDDCD1665L,0xC3A23FE5L,0xDFBD6C3FL,0L},{0xC3A23FE5L,1L,0xDFBD6C3FL,0xDFBD6C3FL},{0xDDCD1665L,0xDDCD1665L,0xD0DA0BDBL,1L},{0x81A7ECC2L,6L,0xF7CD95DAL,0L}};
+static volatile int32_t g_9 = 6L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x6EDAD94EL;
+static int32_t g_11 = 0x5F34BDACL;
+static int32_t g_13 = (-1L);
+static int16_t g_78 = 0x25D9L;
