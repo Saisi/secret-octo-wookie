d4a23818abccb964907877cd1330c02bbf1bdce3
saisi <saisi@users.noreply.github.com>
2016-12-16 13:18:20 +0000

    1481894300 alveolae

---
+++ 1481894300.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2274601400
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5ED9A924L;
+static int32_t *g_5 = (void*)0;
+static int32_t ** volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
