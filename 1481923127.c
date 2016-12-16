879c0edc41a0d62d07da3ac1c00ddec27a84c6af
saisi <saisi@users.noreply.github.com>
2016-12-16 21:18:49 +0000

    1481923127 baboot

---
+++ 1481923127.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      93955536
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 22;
+   int8_t * f1;
+   uint64_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   volatile int8_t * const  volatile  f0;
+   uint16_t  f1;
