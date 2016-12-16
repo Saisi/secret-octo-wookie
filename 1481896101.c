f8436764bbc74b075ff68ffa59ccc415055452ac
saisi <saisi@users.noreply.github.com>
2016-12-16 13:48:22 +0000

    1481896101 Abroms

---
+++ 1481896101.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      259048458
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 28;
+   const unsigned f2 : 14;
+   volatile signed f3 : 2;
+   unsigned f4 : 9;
+   const unsigned f5 : 28;
+   signed f6 : 10;
+   const int64_t  f7;
+};
