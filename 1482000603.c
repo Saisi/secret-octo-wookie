d3a29726e862c573dff6d1619bad47dae36953b1
saisi <saisi@users.noreply.github.com>
2016-12-17 18:50:04 +0000

    1482000603 Apoxyomenos

---
+++ 1482000603.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      136729395
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_8 = 0x4696A485BF25CBD9LL;
+static uint16_t g_13[2] = {0x07E6L,0x07E6L};
+static int8_t g_36 = 0L;
+static uint8_t g_81 = 0xB0L;
+static uint32_t g_83[2] = {0xA6E4C1C1L,0xA6E4C1C1L};
+static int32_t g_85 = 8L;
+static int32_t *g_87 = (void*)0;
+static int32_t ** volatile g_86 = &g_87;/* VOLATILE GLOBAL g_86 */
+static int16_t g_89 = 0x7F2DL;
