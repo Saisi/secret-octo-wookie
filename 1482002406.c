fef0cf847ef5a73c75ed1ea2d686be623d8dfda7
saisi <saisi@users.noreply.github.com>
2016-12-17 19:20:07 +0000

    1482002406 automanipulative

---
+++ 1482002406.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2792490331
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 12;
+   const unsigned f1 : 19;
+   unsigned f2 : 10;
+   volatile signed f3 : 13;
+   const volatile unsigned f4 : 10;
+};
+#pragma pack(pop)
