e583df15ddfe887aae117c026865072a759ef346
saisi <saisi@users.noreply.github.com>
2016-12-17 04:19:15 +0000

    1481948354 avenses

---
+++ 1481948354.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2392716418
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 15;
+   volatile unsigned f1 : 8;
+   unsigned f2 : 10;
+   signed f3 : 14;
+};
+#pragma pack(pop)
+
