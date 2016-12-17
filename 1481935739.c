abbc2fc13414ede461d9937b582b984e0a9b68d2
saisi <saisi@users.noreply.github.com>
2016-12-17 00:49:00 +0000

    1481935739 azygoses

---
+++ 1481935739.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1734381456
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_27 = 18446744073709551615UL;
+static int32_t g_41 = 0x95A52772L;
+static uint16_t g_42 = 0x1C19L;
+static uint8_t g_44 = 0x1DL;
+static uint8_t g_48 = 0xD1L;
