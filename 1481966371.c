49ceb3f610fa846eca334c245b73f796e0f71362
saisi <saisi@users.noreply.github.com>
2016-12-17 09:19:32 +0000

    1481966371 Antiochene

---
+++ 1481966371.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      92343118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const uint8_t  f1;
+   uint64_t  f2;
+   signed f3 : 12;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 18446744073709551611UL;
+static int32_t g_26[9] = {0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L,0x720348F4L};
