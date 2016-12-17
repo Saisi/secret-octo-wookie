8c51b163af21870bb735150205c7e84f7e1db835
saisi <saisi@users.noreply.github.com>
2016-12-17 05:49:20 +0000

    1481953760 antergic

---
+++ 1481953760.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      758148778
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   signed f1 : 12;
+   const volatile uint8_t  f2;
+   volatile signed f3 : 6;
+   volatile signed f4 : 19;
+   unsigned f5 : 19;
+   volatile uint16_t  f6;
+   unsigned f7 : 28;
+   unsigned f8 : 19;
