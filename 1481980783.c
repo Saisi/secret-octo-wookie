fad45ec10fe733a6ab9c1825f11c9cb17705e516
saisi <saisi@users.noreply.github.com>
2016-12-17 13:19:45 +0000

    1481980783 Apul

---
+++ 1481980783.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3733670931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   const volatile int8_t * f1;
+   int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_18 = (-1L);
+static int8_t *g_17 = &g_18;
+static int32_t g_21[7] = {1L,1L,1L,1L,1L,1L,1L};
