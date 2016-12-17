805515decd9f57faaa33840d782764cb4dc76b18
saisi <saisi@users.noreply.github.com>
2016-12-17 12:49:42 +0000

    1481978982 aspermatism

---
+++ 1481978982.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1409794300
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 18446744073709551609UL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_21 = 1L;
+static int32_t g_40 = 0xAE215169L;
+static uint32_t g_69 = 0xE95372A7L;
+static int16_t g_87 = 0x31FDL;
+static int32_t g_89 = 0L;
+static uint8_t g_105 = 0xEDL;
+static uint64_t g_122 = 0xFA49BFE1A8FA0B35LL;
+static volatile int16_t g_124 = 0xAD57L;/* VOLATILE GLOBAL g_124 */
