ded0dce491fdab232175c97217f55c9720805661
saisi <saisi@users.noreply.github.com>
2016-12-17 03:19:12 +0000

    1481944751 Alpheratz

---
+++ 1481944751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4238295830
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint64_t  f0;
+   volatile uint32_t  f1;
+   int64_t  f2;
+   int8_t  f3;
+   volatile int64_t  f4;
+   const volatile int64_t  f5;
+   uint32_t  f6;
+   uint32_t  f7;
+   volatile uint32_t  f8;
