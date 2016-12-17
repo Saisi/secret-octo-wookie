451814c0442e7f3df3243384ce1439c6ef3bf4d0
saisi <saisi@users.noreply.github.com>
2016-12-17 13:49:46 +0000

    1481982586 ascogonidia

---
+++ 1481982586.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      177595404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   volatile unsigned f1 : 25;
+   signed f2 : 23;
+   unsigned f3 : 7;
+   signed f4 : 26;
+   volatile unsigned f5 : 9;
+   unsigned f6 : 3;
+   unsigned f7 : 12;
+   uint64_t  f8;
