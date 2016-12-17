e78d9d150291b4947a144bc0173c2858b952d8a3
saisi <saisi@users.noreply.github.com>
2016-12-17 17:19:58 +0000

    1481995197 Arde

---
+++ 1481995197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      493846338
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+union U1 {
+   int8_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
