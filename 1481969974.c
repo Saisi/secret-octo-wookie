4a57c3688afdaa7599fcceec0a43aa2aebf1aa53
saisi <saisi@users.noreply.github.com>
2016-12-17 10:19:35 +0000

    1481969974 alliancer

---
+++ 1481969974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3412768720
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   const int8_t * const  f1;
+   int16_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 6L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-9L);/* VOLATILE GLOBAL g_3 */
