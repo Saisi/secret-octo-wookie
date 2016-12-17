acb94afac0e2b5ac4df12144482112681f81fcf9
saisi <saisi@users.noreply.github.com>
2016-12-17 23:20:20 +0000

    1482016820 asarabacca

---
+++ 1482016820.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3527867798
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t * const g_2 = (void*)0;
+static volatile int32_t g_5[4][4] = {{0L,3L,3L,0L},{0L,3L,3L,0L},{0L,3L,3L,0L},{0L,3L,3L,0L}};
+static volatile int32_t *g_4[2][6][6] = {{{&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3]},{&g_5[1][3],&g_5[0][0],&g_5[1][3],(void*)0,&g_5[3][2],(void*)0},{&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][1],&g_5[0][1]},{&g_5[1][3],(void*)0,&g_5[1][3],(void*)0,&g_5[1][3],&g_5[3][2]},{&g_5[1][3],&g_5[3][2],&g_5[1][1],&g_5[1][3],&g_5[0][1],&g_5[1][3]},{(void*)0,&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[0][0],&g_5[0][0]}},{{&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],(void*)0,&g_5[1][3]},{&g_5[0][0],(void*)0,&g_5[0][0],&g_5[1][3],&g_5[3][2],&g_5[1][3]},{&g_5[0][0],(void*)0,&g_5[0][1],&g_5[1][3],&g_5[3][2],&g_5[1][3]},{(void*)0,(void*)0,(void*)0,&g_5[1][3],(void*)0,(void*)0},{&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[0][0],(void*)0},{&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3],&g_5[1][3]}}};
+static volatile int32_t ** volatile g_3 = &g_4[1][4][1];/* VOLATILE GLOBAL g_3 */
+static int32_t g_11[7][3] = {{(-8L),0x38E486AEL,0xDB785B64L},{1L,1L,0xDB785B64L},{0x38E486AEL,(-8L),0xDB785B64L},{(-8L),0x38E486AEL,0xDB785B64L},{1L,1L,0xDB785B64L},{0x38E486AEL,(-8L),0xDB785B64L},{(-8L),0x38E486AEL,0xDB785B64L}};
+static int32_t g_24 = 0L;
+static int32_t * volatile g_23 = &g_24;/* VOLATILE GLOBAL g_23 */
+
+
