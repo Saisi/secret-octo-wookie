bb37530230bdcbf4e05ba79bfd1bd6d0e970733c
saisi <saisi@users.noreply.github.com>
2016-12-17 06:49:23 +0000

    1481957362 apses

---
+++ 1481957362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1439504878
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   const uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_7 = 65527UL;
+static int16_t g_29 = 0L;
+static int64_t g_31 = (-4L);
+static int32_t g_43 = 0x3090707EL;
