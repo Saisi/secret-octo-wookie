7ef23cc5c2e773eb2817e2f9bfc3c7e3a17a8c28
saisi <saisi@users.noreply.github.com>
2016-12-17 18:20:01 +0000

    1481998800 argentol

---
+++ 1481998800.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2539559166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xA473EB90L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x3111738AL;
+static int32_t g_7 = 0x8D0D3F7BL;
+static uint64_t g_14 = 1UL;
+static int32_t g_16 = 0xB583DC0EL;
+static int32_t *g_24 = &g_16;
+static int32_t ** volatile g_23 = &g_24;/* VOLATILE GLOBAL g_23 */
