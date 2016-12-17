1d853ba79813adb30e031edc12028fbbaf60ac16
saisi <saisi@users.noreply.github.com>
2016-12-17 20:20:10 +0000

    1482006010 antipathies

---
+++ 1482006010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      192816369
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   const volatile uint8_t  f1;
+   uint32_t  f2;
+   const int8_t * const  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = 7L;
+static uint8_t g_6 = 1UL;
