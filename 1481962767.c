d7266c3d5f239ca1124ce0a32497c5ceb096ae94
saisi <saisi@users.noreply.github.com>
2016-12-17 08:19:27 +0000

    1481962767 allocthonous

---
+++ 1481962767.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1643224121
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+union U1 {
+   int8_t * f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
