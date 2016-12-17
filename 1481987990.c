af9f23d6df3fddbe5efc873535afe0e5fbb28867
saisi <saisi@users.noreply.github.com>
2016-12-17 15:19:51 +0000

    1481987990 Amberly

---
+++ 1481987990.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      81989023
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   uint64_t  f1;
+};
+
+union U1 {
+   const volatile uint32_t  f0;
+   volatile int8_t * f1;
+   uint32_t  f2;
+   const volatile int8_t  f3;
