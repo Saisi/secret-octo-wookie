e0e4bd7abc6d0e43e8f6fd82e0bc048acaf1677e
saisi <saisi@users.noreply.github.com>
2016-12-17 12:19:41 +0000

    1481977180 Adrell

---
+++ 1481977180.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2229205656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_25 = (-9L);
+static int32_t *g_24 = &g_25;
+static uint16_t g_53 = 0UL;
+static uint16_t g_57 = 0x093DL;
+static int16_t g_61 = 0xA21BL;
+static uint16_t g_126 = 65535UL;
+static int8_t g_138 = 0x0AL;
+static uint16_t g_139 = 0x85E3L;
+static volatile uint8_t g_159 = 253UL;/* VOLATILE GLOBAL g_159 */
