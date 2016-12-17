5478bf86699aa400b3d9189c2f876b64ff6b8890
saisi <saisi@users.noreply.github.com>
2016-12-17 23:50:21 +0000

    1482018621 allopurinol

---
+++ 1482018621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1160505750
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 25;
+   const unsigned f1 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_15 = 0xE0L;
+static int32_t g_20 = 0x8D383303L;
+static int32_t g_22 = 0x8CAFA32FL;
+static int32_t * const  volatile g_21[9] = {&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22};
