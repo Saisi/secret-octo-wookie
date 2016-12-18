faf9dafa0c21b991c91d6d5a778124aa3207d30f
saisi <saisi@users.noreply.github.com>
2016-12-18 14:21:12 +0000

    1482070872 apparentements

---
+++ 1482070872.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3169625159
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_9 = 0x0AL;
+static uint32_t g_22 = 7UL;
+static const uint32_t g_26 = 0xCBED562BL;
+static uint32_t g_28 = 0x5DA2CA33L;
+static int16_t g_30 = (-5L);
+static uint32_t *g_51 = (void*)0;
+static uint32_t ** const g_50 = &g_51;
+static int16_t g_53[9][4] = {{0L,(-9L),0x25A3L,8L},{4L,6L,4L,8L},{0x25A3L,(-9L),0L,1L},{0x6E49L,(-1L),(-9L),(-9L)},{8L,8L,(-9L),4L},{0x6E49L,(-1L),0L,(-1L)},{0x25A3L,0L,4L,0L},{4L,0L,0x25A3L,(-1L)},{0L,(-1L),0x6E49L,4L}};
+static int32_t g_81[10] = {0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L,0x684A5798L};
