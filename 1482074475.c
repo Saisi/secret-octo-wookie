7ca17775b109e96151f76dbecddbacfc8113e60e
saisi <saisi@users.noreply.github.com>
2016-12-18 15:21:16 +0000

    1482074475 antegarden

---
+++ 1482074475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      489299623
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile int64_t  f1;
+};
+
+union U1 {
+   const volatile int8_t * volatile  f0;
+   uint8_t  f1;
+};
+
