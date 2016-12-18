e10a0548cad22effb094aa36577cacca544a436a
saisi <saisi@users.noreply.github.com>
2016-12-18 06:50:45 +0000

    1482043843 arkansans

---
+++ 1482043843.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      24037548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile int16_t  f1;
+   uint32_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 0x9AC70C3099CFE048LL;
+static volatile union U0 g_6 = {4UL};/* VOLATILE GLOBAL g_6 */
