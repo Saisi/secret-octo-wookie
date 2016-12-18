c85b7067703aa791c7802408ca75bcb1ad506832
saisi <saisi@users.noreply.github.com>
2016-12-18 22:21:38 +0000

    1482099698 Anteros

---
+++ 1482099698.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1931939809
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   const uint16_t  f1;
+   const int64_t  f2;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   uint64_t  f1;
+   volatile int32_t  f2;
