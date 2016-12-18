54736417e955c21974d620d770cadc62cb36cde4
saisi <saisi@users.noreply.github.com>
2016-12-18 07:50:49 +0000

    1482047449 aristorepublicanism

---
+++ 1482047449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3556782713
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 8L;
+static volatile int32_t g_4 = 9L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-5L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
