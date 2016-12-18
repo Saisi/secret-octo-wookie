ae1500dbb075697efced293b7648487acb685b56
saisi <saisi@users.noreply.github.com>
2016-12-18 01:20:28 +0000

    1482024026 achromatosis

---
+++ 1482024026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1807295572
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 24;
+   signed f1 : 13;
+   unsigned f2 : 27;
+   unsigned f3 : 27;
+   unsigned f4 : 30;
+   unsigned f5 : 29;
+   unsigned f6 : 7;
