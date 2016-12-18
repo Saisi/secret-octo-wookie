141147a71b5191ed0ce305892b2ea98ed2a8c519
saisi <saisi@users.noreply.github.com>
2016-12-18 19:21:27 +0000

    1482088887 baller

---
+++ 1482088887.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1905724518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   uint32_t  f1;
+   const unsigned f2 : 26;
+   int64_t  f3;
+   int16_t  f4;
+   signed f5 : 4;
+};
+
+#pragma pack(push)
