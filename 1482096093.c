06f5613f648ce37af27d6b932b026e74de1413c9
saisi <saisi@users.noreply.github.com>
2016-12-18 21:21:34 +0000

    1482096093 artillerist

---
+++ 1482096093.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      822723806
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_16 = 18446744073709551614UL;
+static uint64_t g_18 = 18446744073709551606UL;
+static int64_t g_29 = 0x631615E82AA9DADELL;
+static int32_t g_38 = 0L;
+static int32_t g_40 = (-8L);
+static volatile int32_t g_52 = (-1L);/* VOLATILE GLOBAL g_52 */
+static volatile int64_t g_53 = 1L;/* VOLATILE GLOBAL g_53 */
+static volatile uint16_t g_54 = 65534UL;/* VOLATILE GLOBAL g_54 */
+static int32_t *g_58 = &g_40;
