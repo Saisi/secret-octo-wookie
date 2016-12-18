a449e883d781ca2bc857db4366d94d783209f9ec
saisi <saisi@users.noreply.github.com>
2016-12-18 20:51:32 +0000

    1482094292 arumin

---
+++ 1482094292.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1812807154
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   signed f1 : 4;
+   uint32_t  f2;
+   unsigned f3 : 7;
+   volatile unsigned f4 : 30;
+   volatile signed f5 : 8;
+};
