14a5157eccb91d9b91f5bb5dc336e9a35679fe5f
saisi <saisi@users.noreply.github.com>
2016-12-18 03:20:34 +0000

    1482031233 anesthetist

---
+++ 1482031233.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3811589682
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   const uint16_t  f1;
+   uint64_t  f2;
+   uint8_t  f3;
+   uint64_t  f4;
+   const int16_t  f5;
+   int32_t  f6;
+   uint16_t  f7;
+   volatile uint64_t  f8;
