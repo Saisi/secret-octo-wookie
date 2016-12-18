2d6789e31ba8ab1b5ba58cdd0df75c145492501a
saisi <saisi@users.noreply.github.com>
2016-12-18 16:51:20 +0000

    1482079880 Androcles

---
+++ 1482079880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      87010745
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint16_t  f1;
+   int8_t * const  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6] = {0x5B0E446DL,0x5B0E446DL,0x5B0E446DL,0x5B0E446DL,0x5B0E446DL,0x5B0E446DL};
+static int32_t g_3 = 0x4E6D4F28L;
