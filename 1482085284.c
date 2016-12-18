7892a1bb2bbfe5f9d16ae5adca0344f2f6854837
saisi <saisi@users.noreply.github.com>
2016-12-18 18:21:24 +0000

    1482085284 abdominoscopy

---
+++ 1482085284.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      418477998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   volatile int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = 0xED15L;
+static uint8_t g_23 = 0xE2L;
