0498d3a96ce0d066ac347c7378f2508ade2a7c29
saisi <saisi@users.noreply.github.com>
2016-12-18 21:51:37 +0000

    1482097895 bagwork

---
+++ 1482097895.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      890051220
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   signed f1 : 5;
+   int64_t  f2;
+   int32_t  f3;
+   const int64_t  f4;
+   uint32_t  f5;
+   uint64_t  f6;
