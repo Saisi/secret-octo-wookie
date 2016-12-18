85802252610e186e538b77cb5b823d04a632ff32
saisi <saisi@users.noreply.github.com>
2016-12-18 13:51:11 +0000

    1482069071 aquiculture

---
+++ 1482069071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      587212461
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * volatile  f0;
+   uint8_t  f1;
+   int16_t  f2;
+};
+
+union U1 {
+   const volatile uint32_t  f0;
+   const int32_t  f1;
+   volatile int32_t  f2;
