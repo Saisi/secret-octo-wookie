7714e782bba0e10ec87ee15d5f0483d0f709b45b
saisi <saisi@users.noreply.github.com>
2016-12-18 10:20:58 +0000

    1482056458 Bardesanist

---
+++ 1482056458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      728726896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 6;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x9BA5F27AL;
+static int32_t g_6 = 0xA7209C39L;
+static int8_t g_12 = (-3L);
+static uint32_t g_13 = 0x5BF7C295L;
