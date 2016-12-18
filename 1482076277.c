61391542064dbc9824ce26e0dd5082550a2c7111
saisi <saisi@users.noreply.github.com>
2016-12-18 15:51:17 +0000

    1482076277 ayurveda

---
+++ 1482076277.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4025653479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 18;
+   signed f1 : 1;
+   signed : 0;
+   unsigned f2 : 11;
+   unsigned f3 : 16;
+   uint64_t  f4;
+   volatile signed f5 : 4;
