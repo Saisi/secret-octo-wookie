815f3fa9e972f9cf618f0514c9f2bf9664789dbd
saisi <saisi@users.noreply.github.com>
2016-12-18 18:51:26 +0000

    1482087085 albuminocholia

---
+++ 1482087085.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2691386843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const int32_t  f1;
+   uint8_t  f2;
+   const int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1BD97A88L;
+static volatile int32_t g_8[1] = {0x2D7BC352L};
