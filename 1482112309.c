c157f5311c65ec79f1de928691592a7c4f5edee9
saisi <saisi@users.noreply.github.com>
2016-12-19 01:51:49 +0000

    1482112309 acromial

---
+++ 1482112309.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1151949772
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 20;
+   int32_t  f1;
+   volatile int64_t  f2;
+   int32_t  f3;
+   uint16_t  f4;
+};
+
+union U1 {
+   const int8_t  f0;
