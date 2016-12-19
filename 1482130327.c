ef19e48a448ae04834482f468644d344348de9c4
saisi <saisi@users.noreply.github.com>
2016-12-19 06:52:07 +0000

    1482130327 antiradicalism

---
+++ 1482130327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1934857387
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 0xDFBD6C3FL;
+static int32_t g_21 = 0x9B2B534DL;
+static int32_t *g_20 = &g_21;
+static uint32_t g_82[9][2] = {{0UL,4UL},{4UL,0UL},{4UL,4UL},{0UL,4UL},{4UL,0UL},{4UL,4UL},{0UL,4UL},{4UL,0UL},{4UL,4UL}};
+static volatile int32_t g_83 = 1L;/* VOLATILE GLOBAL g_83 */
