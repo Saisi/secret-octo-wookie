7c8350e718232cd12433d9d30226c1744f40d8f1
saisi <saisi@users.noreply.github.com>
2016-12-19 21:22:58 +0000

    1482182578 atmidometer

---
+++ 1482182578.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      400456835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   volatile signed f1 : 5;
+   volatile unsigned f2 : 7;
+   uint32_t  f3;
+   const volatile int16_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
