fa20fe263cdd5321e2c0fd5692a6852ac16d02b6
saisi <saisi@users.noreply.github.com>
2016-12-19 08:52:13 +0000

    1482137533 adrenalectomy

---
+++ 1482137533.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      683206512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 6;
+   volatile signed f1 : 3;
+   unsigned f2 : 25;
+   const int16_t  f3;
+   volatile unsigned f4 : 18;
+};
+#pragma pack(pop)
