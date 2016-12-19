20db7742a6dca959d0f3f8804582df9e05aa0dc7
saisi <saisi@users.noreply.github.com>
2016-12-19 05:22:01 +0000

    1482124920 antiscolic

---
+++ 1482124920.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1091265232
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_10 = 0xCCDDL;
+static int32_t g_21 = (-9L);
+static volatile int32_t g_23[4] = {0x27A4129BL,0x27A4129BL,0x27A4129BL,0x27A4129BL};
+static volatile int32_t * volatile g_22[10] = {&g_23[0],&g_23[0],&g_23[2],(void*)0,&g_23[2],&g_23[0],&g_23[0],&g_23[2],(void*)0,&g_23[2]};
+static int32_t *g_36[4][3][10] = {{{(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0},{&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21},{(void*)0,(void*)0,&g_21,(void*)0,(void*)0,&g_21,(void*)0,(void*)0,&g_21,(void*)0}},{{(void*)0,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21}},{{&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21}},{{&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21,&g_21},{&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21,&g_21,(void*)0,&g_21}}};
