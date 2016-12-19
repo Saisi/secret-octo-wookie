47719cf0ebc9746a0660256896fdbf5d00eb197d
saisi <saisi@users.noreply.github.com>
2016-12-19 00:51:46 +0000

    1482108706 bahera

---
+++ 1482108706.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3851138978
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_4 = 0x2FL;/* VOLATILE GLOBAL g_4 */
+static int64_t g_7 = 0xE86F8C7B5C6EFF3DLL;
+static uint32_t g_25 = 0xC3D21462L;
+static uint8_t g_28[9] = {0x54L,0UL,0x54L,0x54L,0UL,0x54L,0x54L,0UL,0x54L};
+static int64_t g_33[8] = {0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL,0x36EF6F0C3DA550E8LL};
+static uint16_t g_53 = 0x2EFAL;
+static int32_t g_56 = 0xBB57CF3DL;
+static volatile int32_t g_93 = 0x73E42CDCL;/* VOLATILE GLOBAL g_93 */
+static volatile int32_t *g_92[3] = {&g_93,&g_93,&g_93};
