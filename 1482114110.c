8a74a4d326ea17cf5dd53cd7897f23654c2ded4e
saisi <saisi@users.noreply.github.com>
2016-12-19 02:21:51 +0000

    1482114110 Aponogetonaceae

---
+++ 1482114110.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3679113186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[8][1][3] = {{{0xE76FL,1UL,0xE76FL}},{{65535UL,0UL,65535UL}},{{0xE76FL,1UL,0xE76FL}},{{65535UL,0UL,65535UL}},{{0xE76FL,1UL,0xE76FL}},{{65535UL,0UL,65535UL}},{{0xE76FL,1UL,0xE76FL}},{{65535UL,0UL,65535UL}}};
+static volatile int32_t g_3[2] = {9L,9L};
+static int32_t g_4 = 0L;
+static int32_t g_8[1] = {0x5A23E86FL};
+static int32_t g_12[7] = {0x8D19C921L,0x8D19C921L,0x8D19C921L,0x8D19C921L,0x8D19C921L,0x8D19C921L,0x8D19C921L};
+static int32_t *g_80 = &g_8[0];
+static int32_t g_83[6] = {5L,5L,0L,5L,5L,0L};
+static int32_t g_84 = (-1L);
+static int32_t g_85 = 0x24202D95L;
