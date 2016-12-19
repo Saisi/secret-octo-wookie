9c0be19db47fcdd85c76c1e0c5dbad0db71dbe5a
saisi <saisi@users.noreply.github.com>
2016-12-19 19:52:48 +0000

    1482177168 babesia

---
+++ 1482177168.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1339792879
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x2F3817D1L;
+static int64_t g_4 = 0xFDD8FB8CEFFB91E6LL;
