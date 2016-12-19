8f9197cf1ccfd5bde8b5f3b67bd5ee494357fff6
saisi <saisi@users.noreply.github.com>
2016-12-19 12:52:27 +0000

    1482151946 androgynies

---
+++ 1482151946.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1904877859
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 16;
+};
+
+struct S1 {
+   int32_t  f0;
+   const uint64_t  f1;
+   volatile uint16_t  f2;
+   struct S0  f3;
+};
