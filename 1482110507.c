c04980447b4ad79a61436886efa4eaca24aa20ed
saisi <saisi@users.noreply.github.com>
2016-12-19 01:21:48 +0000

    1482110507 Annelides

---
+++ 1482110507.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2775376941
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_28 = 18446744073709551608UL;
+static volatile uint16_t g_58 = 6UL;/* VOLATILE GLOBAL g_58 */
+static const volatile int32_t g_67 = 0x5EE87909L;/* VOLATILE GLOBAL g_67 */
+static const volatile int32_t *g_66 = &g_67;
+static int32_t g_75 = 0xC37F5E17L;
+static int8_t g_88 = (-1L);
+static int16_t g_101 = 9L;
+static int32_t g_102 = 0x99CC0F79L;
+static int32_t g_103 = 0x15FFE8FEL;
