5330a62e47bed6b0e3ced082e9e1ff02e1c677f0
saisi <saisi@users.noreply.github.com>
2016-12-19 11:22:22 +0000

    1482146541 annelidian

---
+++ 1482146541.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1052833296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 22;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[2] = {1L,1L};
+static int32_t g_56[5] = {(-1L),(-1L),(-1L),(-1L),(-1L)};
