eda4de34258747b36bde344555dba3546ecae81b
saisi <saisi@users.noreply.github.com>
2016-12-19 04:21:57 +0000

    1482121317 arnicas

---
+++ 1482121317.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3795488915
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   unsigned f1 : 31;
+   uint8_t  f2;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const uint8_t  f0;
