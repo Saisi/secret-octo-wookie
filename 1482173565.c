e59d59588a5678184630d0dc5d48db38e105c38a
saisi <saisi@users.noreply.github.com>
2016-12-19 18:52:46 +0000

    1482173565 arthriticine

---
+++ 1482173565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3705753995
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_14 = 1UL;/* VOLATILE GLOBAL g_14 */
+static int32_t g_16 = (-4L);
+static uint16_t g_25 = 1UL;
+static int16_t g_27 = 0x0CD9L;
+static int32_t g_33 = 0xB8DB370CL;
+static int32_t *g_32[4] = {&g_33,&g_33,&g_33,&g_33};
+static int16_t g_52 = 0xA415L;
+static int32_t g_63[3] = {0x77C83BE7L,0x77C83BE7L,0x77C83BE7L};
+static int16_t g_83 = 0L;
