23c4d7c4efab1bb760a74902c6b34777b7eb5a3a
saisi <saisi@users.noreply.github.com>
2016-12-19 21:53:00 +0000

    1482184379 arthrometry

---
+++ 1482184379.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3135762139
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   volatile signed f1 : 19;
+   signed f2 : 10;
+   unsigned f3 : 13;
+   const volatile signed f4 : 10;
+};
+
+union U1 {
+   const uint8_t  f0;
