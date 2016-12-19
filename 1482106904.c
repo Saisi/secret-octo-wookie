35bcca36b3f9fa101aa8a2ad32a93500653d5ec6
saisi <saisi@users.noreply.github.com>
2016-12-19 00:21:44 +0000

    1482106904 azulene

---
+++ 1482106904.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      222939807
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 14;
+   const unsigned f1 : 13;
+   signed f2 : 28;
+   const unsigned f3 : 23;
+   const int8_t  f4;
+   const signed f5 : 14;
+   volatile signed f6 : 25;
