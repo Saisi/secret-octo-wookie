fad4d593c62ab4f0908e66361fecd4ecd667273d
saisi <saisi@users.noreply.github.com>
2016-12-19 16:22:38 +0000

    1482164558 abrico

---
+++ 1482164558.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2883021712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 22;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_27 = 9UL;/* VOLATILE GLOBAL g_27 */
+static volatile int32_t g_29 = 0x85589B58L;/* VOLATILE GLOBAL g_29 */
