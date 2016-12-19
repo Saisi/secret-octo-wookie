21e36c27b90026c2bbfd8c465d8954413f074fee
saisi <saisi@users.noreply.github.com>
2016-12-19 06:22:05 +0000

    1482128525 agrarians

---
+++ 1482128525.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4041223017
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const int64_t  f1;
+   int64_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_3 = 9L;
+static volatile int32_t g_6[6] = {0x651328F6L,0x651328F6L,0x651328F6L,0x651328F6L,0x651328F6L,0x651328F6L};
