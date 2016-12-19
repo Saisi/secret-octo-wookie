f5c8f8c0708e98e60556250412823dfd100db22b
saisi <saisi@users.noreply.github.com>
2016-12-19 14:52:33 +0000

    1482159152 afghans

---
+++ 1482159152.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      557556717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 21;
+   const unsigned f1 : 24;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x34FDA0E6L;/* VOLATILE GLOBAL g_3 */
