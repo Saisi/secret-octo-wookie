3f6034666fc690e568f3237344e05983ab13fa4b
saisi <saisi@users.noreply.github.com>
2016-12-19 11:52:23 +0000

    1482148343 aleuromancy

---
+++ 1482148343.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3532866335
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 14;
+};
+#pragma pack(pop)
+
+struct S1 {
+   uint16_t  f0;
+   unsigned f1 : 3;
