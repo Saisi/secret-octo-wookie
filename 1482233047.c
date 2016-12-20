b99e08f0101dbebc730b0a77edc611549cfc2757
saisi <saisi@users.noreply.github.com>
2016-12-20 11:24:08 +0000

    1482233047 apodyterium

---
+++ 1482233047.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      222169791
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   volatile unsigned f1 : 29;
+   const unsigned f2 : 12;
+   volatile signed f3 : 7;
+   signed f4 : 28;
+   unsigned f5 : 5;
+   int32_t  f6;
