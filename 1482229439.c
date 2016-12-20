f7c79918512898e6a22057df55acfa12e4b2ec59
saisi <saisi@users.noreply.github.com>
2016-12-20 10:24:01 +0000

    1482229439 baetylic

---
+++ 1482229439.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2609406931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   int32_t  f1;
+   uint32_t  f2;
+   const int32_t  f3;
+   volatile uint16_t  f4;
+   volatile int16_t  f5;
+   unsigned f6 : 8;
