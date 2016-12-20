78f4ade5b3c8b8398c8060a0350bb34e5e0bb98b
saisi <saisi@users.noreply.github.com>
2016-12-20 16:24:37 +0000

    1482251076 atheneum

---
+++ 1482251076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1025467320
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[8] = {0x1E559696L,0x1E559696L,0x1E559696L,0x1E559696L,0x1E559696L,0x1E559696L,0x1E559696L,0x1E559696L};
+static int32_t g_5 = (-1L);
