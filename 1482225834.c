86f9273e34258dcf825b50fe8ff95cfa638e928a
saisi <saisi@users.noreply.github.com>
2016-12-20 09:23:55 +0000

    1482225834 Arlina

---
+++ 1482225834.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2457504836
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 16;
+   signed f1 : 5;
+   volatile unsigned f2 : 19;
+   signed f3 : 19;
+   signed f4 : 22;
+   volatile signed f5 : 21;
+   const volatile signed f6 : 31;
+};
+
