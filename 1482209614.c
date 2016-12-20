7ad6c0c64552ae7cccb783dbb47fc1f64619577d
saisi <saisi@users.noreply.github.com>
2016-12-20 04:53:34 +0000

    1482209614 autoclaves

---
+++ 1482209614.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3138066588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   volatile unsigned f1 : 1;
+   unsigned f2 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-2L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x768C0AA3L;
+static int16_t g_31 = 4L;
