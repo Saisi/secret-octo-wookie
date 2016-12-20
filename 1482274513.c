4dacd9b8fb2a502d9073efe08118f2c6c12dd730
saisi <saisi@users.noreply.github.com>
2016-12-20 22:55:13 +0000

    1482274513 apercus

---
+++ 1482274513.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3905188865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_4 = (-1L);
+static int32_t g_7 = 0x000CD9D7L;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
