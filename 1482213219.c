1ecb5936ecd0dd163b84460fc3dbaf71509ea646
saisi <saisi@users.noreply.github.com>
2016-12-20 05:53:39 +0000

    1482213219 albumoscope

---
+++ 1482213219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      328799611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 28;
+   volatile unsigned f1 : 3;
+   const signed f2 : 26;
+   signed f3 : 29;
+   volatile signed f4 : 26;
+   volatile signed f5 : 11;
+   unsigned f6 : 30;
+   unsigned f7 : 25;
+   int32_t  f8;
