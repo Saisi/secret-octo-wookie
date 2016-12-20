1ba645e7fc250edce3efae7a1afb1fc29df03c1b
saisi <saisi@users.noreply.github.com>
2016-12-20 01:53:19 +0000

    1482198799 backtrack

---
+++ 1482198799.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      717967552
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 14;
+   volatile int8_t * f1;
+   volatile uint32_t  f2;
+   uint32_t  f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-9L);
