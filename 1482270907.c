f914b4c263c71891139fa2494a2f4693b2a5f158
saisi <saisi@users.noreply.github.com>
2016-12-20 21:55:08 +0000

    1482270907 Arola

---
+++ 1482270907.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3272385070
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile signed f1 : 6;
+   uint64_t  f2;
+   signed f3 : 24;
+   signed f4 : 29;
+   volatile signed f5 : 21;
+   const unsigned f6 : 2;
+   const unsigned f7 : 19;
+};
