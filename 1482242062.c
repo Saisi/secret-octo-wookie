04fcb3fc0e4d751e0152791318e31c87e2cceefc
saisi <saisi@users.noreply.github.com>
2016-12-20 13:54:22 +0000

    1482242062 adipopexis

---
+++ 1482242062.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1012507854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 255UL;
+static int32_t g_19[8] = {0xA3CFCE29L,0xFDEB7727L,0xA3CFCE29L,0xA3CFCE29L,0xFDEB7727L,0xA3CFCE29L,0xA3CFCE29L,0xFDEB7727L};
+static int32_t *g_18 = &g_19[4];
+static uint32_t g_35 = 0x4F32B7B8L;
+static uint64_t g_43 = 18446744073709551615UL;
+static int64_t g_51 = (-1L);
+static uint8_t g_52 = 0xCEL;
+static volatile uint16_t g_56 = 65534UL;/* VOLATILE GLOBAL g_56 */
+static volatile uint16_t * volatile g_55[4][1][1] = {{{&g_56}},{{&g_56}},{{&g_56}},{{&g_56}}};
