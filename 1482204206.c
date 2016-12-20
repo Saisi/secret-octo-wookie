db3c1ef5c68ce107af1f0b12e01c704609fe37d5
saisi <saisi@users.noreply.github.com>
2016-12-20 03:23:26 +0000

    1482204206 anisaldoxime

---
+++ 1482204206.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2553663391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-7L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0L;
+static int32_t g_7 = (-5L);
+static int32_t g_73[3][10][2] = {{{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)}},{{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)}},{{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)},{(-6L),(-6L)}}};
+static int32_t *g_81 = &g_73[1][5][0];
+static int32_t ** volatile g_80 = &g_81;/* VOLATILE GLOBAL g_80 */
+static uint32_t g_99 = 0xBF6E6797L;
+static int64_t g_112 = 0x1E39B249F0F192DBLL;
+static const volatile uint32_t * volatile *g_113 = (void*)0;
