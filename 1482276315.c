af79eb320e9453814a3cee3686f9a7e58bdb6355
saisi <saisi@users.noreply.github.com>
2016-12-20 23:25:16 +0000

    1482276315 amyxorrhea

---
+++ 1482276315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3890220624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_19 = 7L;
+static int32_t *g_18 = &g_19;
+static int16_t g_43[3][8] = {{0x77AEL,0xF953L,0x77AEL,0x77AEL,0xF953L,0x77AEL,0x77AEL,0xF953L},{0xF953L,0x77AEL,0x77AEL,0xF953L,0x77AEL,0x77AEL,0xF953L,0x77AEL},{0xF953L,0xF953L,0x7B30L,0xF953L,0xF953L,0x7B30L,0xF953L,0xF953L}};
+static int64_t g_58 = 0L;
+static int64_t g_60 = 7L;
+static int8_t g_62[10] = {1L,1L,1L,1L,1L,1L,1L,1L,1L,1L};
+static int16_t g_64 = 0x54C3L;
+static int16_t g_66 = 1L;
+static int32_t g_99 = (-9L);
