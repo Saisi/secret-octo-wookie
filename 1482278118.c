b7e661e10f9c6be72b939e7c9b56e39f3ad711d1
saisi <saisi@users.noreply.github.com>
2016-12-20 23:55:18 +0000

    1482278118 airstrips

---
+++ 1482278118.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      224712732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 28;
+   int8_t  f1;
+   signed f2 : 25;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint16_t  f0;
