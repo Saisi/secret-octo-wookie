159af360c557cd5035dd23d351a3136496481320
saisi <saisi@users.noreply.github.com>
2016-12-20 06:23:41 +0000

    1482215021 Asterella

---
+++ 1482215021.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      325446224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[6] = {7L,7L,7L,7L,7L,7L};
+static volatile uint32_t g_7 = 9UL;/* VOLATILE GLOBAL g_7 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
