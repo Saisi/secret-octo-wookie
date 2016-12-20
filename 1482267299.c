ec213f1b8b7d8c65cd0b9284d1c52eea41493155
saisi <saisi@users.noreply.github.com>
2016-12-20 20:54:59 +0000

    1482267299 aliyos

---
+++ 1482267299.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2759125333
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_4 = 65535UL;
+static int16_t g_7 = 0x3A92L;
+static volatile uint16_t g_18 = 1UL;/* VOLATILE GLOBAL g_18 */
+static uint16_t g_19 = 65533UL;
+static int32_t g_21 = 3L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
