b1afe19a7e08111b4243c7808c3b17e8637c98b7
saisi <saisi@users.noreply.github.com>
2016-12-20 08:23:50 +0000

    1482222230 adjoined

---
+++ 1482222230.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2739302019
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[7][5] = {{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L},{6L,0L,0xFBA1A4D6L,0xFBA1A4D6L,0L}};
+static int8_t g_3 = 3L;
+static volatile uint32_t g_5 = 0xE4E91208L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_9 = (-1L);
+static int32_t * volatile g_8 = &g_9;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
