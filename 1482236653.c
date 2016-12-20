70e19a0b86da51cc8a702f71ae548e7e0c92225d
saisi <saisi@users.noreply.github.com>
2016-12-20 12:24:14 +0000

    1482236653 astroid

---
+++ 1482236653.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      355521844
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   volatile signed f1 : 29;
+   volatile unsigned f2 : 11;
+   volatile unsigned f3 : 11;
+   unsigned f4 : 14;
+};
+
+#pragma pack(push)
+#pragma pack(1)
