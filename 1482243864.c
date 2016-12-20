0da92390f137c6e682222bed79a29d3988e409f3
saisi <saisi@users.noreply.github.com>
2016-12-20 14:24:24 +0000

    1482243864 anobing

---
+++ 1482243864.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      759603967
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const unsigned f1 : 23;
+   const uint32_t  f2;
+   int16_t  f3;
+   volatile uint64_t  f4;
+};
+
+union U1 {
+   volatile int64_t  f0;
