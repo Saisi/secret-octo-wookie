3e0ef3f565570a7ec537fc8a09368a99b611f3b3
saisi <saisi@users.noreply.github.com>
2016-12-20 07:53:48 +0000

    1482220427 angiopressure

---
+++ 1482220427.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      243532406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 9;
+   signed f1 : 26;
+   unsigned f2 : 20;
+   volatile signed f3 : 16;
+   signed f4 : 31;
+   unsigned f5 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
