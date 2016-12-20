5201f530bb915110d0a4f8e0b59c3194e95d3373
saisi <saisi@users.noreply.github.com>
2016-12-20 22:25:11 +0000

    1482272710 anticorn

---
+++ 1482272710.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145577633
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 15;
+   volatile signed f1 : 10;
+   signed f2 : 5;
+   volatile signed f3 : 28;
+   volatile unsigned f4 : 29;
+   volatile signed f5 : 2;
+   const signed f6 : 25;
+   volatile unsigned f7 : 8;
+   signed f8 : 11;
