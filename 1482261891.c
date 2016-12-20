e361ff84df606a169568b8a1c45232e2c3fec2e3
saisi <saisi@users.noreply.github.com>
2016-12-20 19:24:52 +0000

    1482261891 autolyse

---
+++ 1482261891.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4287510521
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 0UL;
+static int32_t g_40 = 0xF8A6EBBFL;
+static int32_t * volatile g_39[6] = {&g_40,&g_40,&g_40,&g_40,&g_40,&g_40};
+static int32_t g_53 = 0xA1B4415EL;
