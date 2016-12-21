3f1a0a59dfdf2a4e8af375f78d920d4a81cc57a5
saisi <saisi@users.noreply.github.com>
2016-12-21 00:25:20 +0000

    1482279920 Amenia

---
+++ 1482279920.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1073293945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 27;
+   unsigned f1 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   struct S0  f0;
+};
