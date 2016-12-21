e5262fb908a0d8ce3c8a006bc60c6e5a448d7bb2
saisi <saisi@users.noreply.github.com>
2016-12-21 07:25:57 +0000

    1482305156 anthryl

---
+++ 1482305156.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      773165882
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 0x48F69F7FL;
+static int32_t g_12 = (-1L);
+static int32_t *g_15 = &g_12;
+static int8_t g_29 = (-6L);
+static uint8_t g_104 = 0x97L;
+static int32_t g_106 = (-1L);
+static uint64_t g_131 = 0xE8981CC384641B43LL;
+static uint32_t g_137[4] = {1UL,1UL,1UL,1UL};
+static int16_t g_140 = 2L;
