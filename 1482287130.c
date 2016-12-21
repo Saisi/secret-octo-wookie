8e0f938097ccc85cff4ab98df5b92e6440b19d6e
saisi <saisi@users.noreply.github.com>
2016-12-21 02:25:31 +0000

    1482287130 abutilons

---
+++ 1482287130.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      853449640
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 2;
+   volatile uint16_t  f1;
+   const signed f2 : 7;
+   const volatile signed f3 : 13;
+   volatile signed f4 : 30;
+   unsigned f5 : 18;
+   signed f6 : 11;
+};
+
