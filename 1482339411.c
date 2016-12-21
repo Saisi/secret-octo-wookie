d0278b6239a9734f114c89a99661e327baa9a285
saisi <saisi@users.noreply.github.com>
2016-12-21 16:56:52 +0000

    1482339411 Appleton

---
+++ 1482339411.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2106701305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   unsigned f1 : 17;
+   uint64_t  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+};
+
