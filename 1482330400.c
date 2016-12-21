3d277f157f3b2999eb4cfd1527eae5b6a5e65379
saisi <saisi@users.noreply.github.com>
2016-12-21 14:26:40 +0000

    1482330400 barbarised

---
+++ 1482330400.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4032167088
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   uint8_t  f2;
+   uint32_t  f3;
+   volatile int64_t  f4;
+   const int8_t  f5;
+   volatile int64_t  f6;
+};
+
