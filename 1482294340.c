9881f06e342b7f1436a7e1e864302622514606cb
saisi <saisi@users.noreply.github.com>
2016-12-21 04:25:41 +0000

    1482294340 alcoholizable

---
+++ 1482294340.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      583976143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x57FD5464L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x5C184C50L;
+static uint16_t g_36 = 5UL;
+static volatile uint16_t g_48 = 65530UL;/* VOLATILE GLOBAL g_48 */
+static volatile uint16_t *g_47 = &g_48;
+static uint16_t g_53 = 0xF321L;
+static int32_t g_57 = 5L;
+static uint8_t g_59 = 0UL;
+static int32_t g_107 = (-1L);
