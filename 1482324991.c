189b17d810908fa0a07f2af7c8134477c4cf8cf2
saisi <saisi@users.noreply.github.com>
2016-12-21 12:56:32 +0000

    1482324991 alterableness

---
+++ 1482324991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3738275521
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[10] = {0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L,0x25D16DF2L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
