a6516c68da4b2b638bbe575d8c22f40366aaa54a
saisi <saisi@users.noreply.github.com>
2016-12-21 06:55:54 +0000

    1482303353 ampery

---
+++ 1482303353.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2134197407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint64_t  f1;
+};
+
+union U1 {
+   signed f0 : 28;
+   int32_t  f1;
+   int8_t * const  f2;
+   int8_t  f3;
