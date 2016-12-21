36f7b7fa2a23139e69137d167f601777e9415b89
saisi <saisi@users.noreply.github.com>
2016-12-21 06:25:51 +0000

    1482301551 Baldwin

---
+++ 1482301551.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1888779217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   int16_t  f1;
+};
+#pragma pack(pop)
+
+union U1 {
+   int8_t * volatile  f0;
