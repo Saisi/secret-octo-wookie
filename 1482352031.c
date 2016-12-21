395d9f27dd29ac81aeb351677be3c7bc6b6ca6e9
saisi <saisi@users.noreply.github.com>
2016-12-21 20:27:11 +0000

    1482352031 addlement

---
+++ 1482352031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1853897072
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+   volatile uint8_t  f3;
+   volatile uint8_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
