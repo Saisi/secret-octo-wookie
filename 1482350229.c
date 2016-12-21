f32b9f6656f994ebd4aca87513e3d17c0ca59a3c
saisi <saisi@users.noreply.github.com>
2016-12-21 19:57:09 +0000

    1482350229 Arianize

---
+++ 1482350229.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1420495591
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 18;
+   signed f1 : 1;
+   signed : 0;
+};
+#pragma pack(pop)
+
+struct S1 {
