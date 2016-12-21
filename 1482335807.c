ffd02ecb0090c367eabe2c7e5fadbe07e4651175
saisi <saisi@users.noreply.github.com>
2016-12-21 15:56:47 +0000

    1482335807 airparks

---
+++ 1482335807.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1110100070
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2 = 0x38L;
+static int32_t g_4 = 0L;
+static volatile int64_t g_10 = (-1L);/* VOLATILE GLOBAL g_10 */
+static int64_t g_23 = 0x1B44ADCA1A1F0506LL;
+static volatile int32_t g_24 = 0L;/* VOLATILE GLOBAL g_24 */
+static volatile int32_t g_25[6][1][8] = {{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}},{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}},{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}},{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}},{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}},{{0x172DDB74L,1L,1L,0x172DDB74L,0x172DDB74L,1L,1L,0x172DDB74L}}};
+static uint16_t g_26 = 65535UL;
+
+
