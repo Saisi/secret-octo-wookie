45d28dad472a0f04c35e15233cef69ee32edb005
saisi <saisi@users.noreply.github.com>
2016-12-21 13:56:37 +0000

    1482328597 Alcyonium

---
+++ 1482328597.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145406766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_21 = 0x72L;
+static int32_t g_39[7][2] = {{0x56B8642EL,7L},{(-1L),(-1L)},{7L,0x56B8642EL},{(-1L),0x56B8642EL},{7L,(-1L)},{(-1L),7L},{0x56B8642EL,(-1L)}};
+static int32_t * volatile g_38 = &g_39[5][0];/* VOLATILE GLOBAL g_38 */
+static uint32_t g_51 = 4294967288UL;
+static int32_t *g_76 = &g_39[5][1];
+static uint8_t g_100 = 0UL;
+static uint16_t g_132 = 1UL;
+static uint8_t *g_141[6][1] = {{&g_21},{&g_21},{&g_21},{&g_21},{&g_21},{&g_21}};
+static uint8_t **g_140 = &g_141[3][0];
