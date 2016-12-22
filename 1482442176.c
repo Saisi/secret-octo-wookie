7f9c41f1333c11cac56bdb30064f95aec280cc27
saisi <saisi@users.noreply.github.com>
2016-12-22 21:29:36 +0000

    1482442176 babylonian

---
+++ 1482442176.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2422671477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 25;
+   volatile signed f1 : 12;
+};
+
+union U1 {
+   signed f0 : 25;
+   int16_t  f1;
+};
+
