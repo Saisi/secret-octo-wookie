40f0d5b858092439e7152b1f250822d0d4b3d987
saisi <saisi@users.noreply.github.com>
2016-12-22 04:58:02 +0000

    1482382681 bannermen

---
+++ 1482382681.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1743036224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   int64_t  f1;
+};
+
+union U1 {
+   uint8_t  f0;
+   volatile uint64_t  f1;
+   uint64_t  f2;
+   int32_t  f3;
