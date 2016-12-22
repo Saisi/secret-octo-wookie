8b26c9349a68186c49a48d23963b1e8d77ca91ee
saisi <saisi@users.noreply.github.com>
2016-12-22 20:29:30 +0000

    1482438568 aneroidograph

---
+++ 1482438568.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2801086350
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   const signed f1 : 22;
+   const volatile int16_t  f2;
+   unsigned f3 : 8;
+   signed f4 : 13;
+   unsigned f5 : 5;
+   uint16_t  f6;
