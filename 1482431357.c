fa1017fb29279c9c9235eaaa78f006a5093c98bf
saisi <saisi@users.noreply.github.com>
2016-12-22 18:29:18 +0000

    1482431357 amblosis

---
+++ 1482431357.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1965938766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_2 = (-10L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0x1B88146CL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
