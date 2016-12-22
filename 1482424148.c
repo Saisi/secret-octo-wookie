b2e5a52fa2a34769bffc27d16f26f33c34525e12
saisi <saisi@users.noreply.github.com>
2016-12-22 16:29:08 +0000

    1482424148 affusions

---
+++ 1482424148.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4161622260
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xD210EC32L;
+static volatile uint32_t g_4 = 0xD49598AAL;/* VOLATILE GLOBAL g_4 */
