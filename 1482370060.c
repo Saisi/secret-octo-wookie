290e02ebe92c4a73f8ed98e8780eec8a6c80b59c
saisi <saisi@users.noreply.github.com>
2016-12-22 01:27:41 +0000

    1482370060 adamances

---
+++ 1482370060.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3406659365
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 17;
+   signed f1 : 4;
+};
+
+struct S1 {
+   const signed f0 : 24;
+   volatile signed f1 : 31;
+   volatile unsigned f2 : 17;
+   int32_t  f3;
