c5d8c77b2201c21424651283c32244641b79f39c
saisi <saisi@users.noreply.github.com>
2016-12-22 12:58:49 +0000

    1482411528 autoimmunizing

---
+++ 1482411528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2762265131
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[5][4] = {{0x0417BE18L,0xF2000252L,0xF2000252L,0x0417BE18L},{0xF2000252L,0x0417BE18L,0xBB2C2135L,7L},{0xF2000252L,0xBB2C2135L,0xF2000252L,0x432A2768L},{0x0417BE18L,7L,0x432A2768L,0x432A2768L},{0xBB2C2135L,0xBB2C2135L,(-3L),7L}};
+static volatile int32_t g_4 = (-7L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0xD672A835L;
+static uint8_t g_37 = 0UL;
+static uint8_t g_57 = 0xF2L;
+static uint64_t g_61 = 0UL;
+static int32_t g_78 = 0x9B61F286L;
+static int8_t g_118 = 0L;
