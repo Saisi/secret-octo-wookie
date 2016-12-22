e4d81df48eedeb6896e25b30b9ad9f18ddd9905a
saisi <saisi@users.noreply.github.com>
2016-12-22 19:29:23 +0000

    1482434962 Alosa

---
+++ 1482434962.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3164173832
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t g_4 = 1L;
+static int32_t *g_6 = &g_4;
+static int32_t g_17 = 0L;
+static uint32_t g_49[6] = {0xACD6BA8DL,0xACD6BA8DL,0xACD6BA8DL,0xACD6BA8DL,0xACD6BA8DL,0xACD6BA8DL};
