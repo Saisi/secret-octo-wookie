d0496c1d6c4915710e6c5880ae357b54f29c63b1
saisi <saisi@users.noreply.github.com>
2016-12-22 03:57:57 +0000

    1482379076 balsawood

---
+++ 1482379076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2972303815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 2;
+   const int8_t  f1;
+   const signed f2 : 1;
+   signed f3 : 1;
+   volatile int64_t  f4;
+   unsigned f5 : 2;
+   signed f6 : 29;
+   volatile signed f7 : 3;
+   const signed f8 : 6;
