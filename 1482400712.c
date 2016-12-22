f38c62104c2761c460f49925b5f293e278968f88
saisi <saisi@users.noreply.github.com>
2016-12-22 09:58:33 +0000

    1482400712 banisters

---
+++ 1482400712.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      212744754
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   const unsigned : 0;
+   volatile signed f1 : 2;
+   volatile int32_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_25[5] = {0xB9C2L,0xB9C2L,0xB9C2L,0xB9C2L,0xB9C2L};
