880eeaae53a0eb3800cac988559258f5cbf8a5b6
saisi <saisi@users.noreply.github.com>
2016-12-22 16:59:11 +0000

    1482425950 Bahai

---
+++ 1482425950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      219913242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * const g_2[4][10] = {{&g_3,(void*)0,(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0,(void*)0},{&g_3,(void*)0,(void*)0,&g_3,&g_3,(void*)0,(void*)0,&g_3,(void*)0,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0,(void*)0,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3,&g_3}};
+static volatile int32_t *g_5 = (void*)0;
+static volatile int32_t ** volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
