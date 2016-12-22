dfe1b0756a0958d820911fbbe838981464ff6a0d
saisi <saisi@users.noreply.github.com>
2016-12-22 06:28:11 +0000

    1482388090 BANCS

---
+++ 1482388090.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4222086376
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 2;
+   unsigned f1 : 15;
+   volatile unsigned f2 : 21;
+   unsigned f3 : 13;
+   volatile signed f4 : 8;
+   signed f5 : 31;
+   const unsigned f6 : 1;
