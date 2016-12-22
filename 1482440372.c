ff1fa0076caa8df70f157afb52c0ac294c4cadcb
saisi <saisi@users.noreply.github.com>
2016-12-22 20:59:33 +0000

    1482440372 Athamas

---
+++ 1482440372.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2616107118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int16_t  f0;
+   uint8_t  f1;
+   uint16_t  f2;
+   volatile uint64_t  f3;
+   volatile uint8_t  f4;
+   uint16_t  f5;
+   const uint8_t  f6;
+   uint16_t  f7;
+};
