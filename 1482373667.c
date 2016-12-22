aa71d207447e3f0c87da4cd4151c27e4e365daeb
saisi <saisi@users.noreply.github.com>
2016-12-22 02:27:49 +0000

    1482373667 antiradicalism

---
+++ 1482373667.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1825253783
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint32_t  f0;
+   const uint32_t  f1;
+   const int32_t  f2;
+   uint64_t  f3;
+   signed f4 : 16;
+};
+#pragma pack(pop)
