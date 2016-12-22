ca1d22f64c4566b62a0d11f7a957a50511c9a569
saisi <saisi@users.noreply.github.com>
2016-12-22 17:29:13 +0000

    1482427753 abacinate

---
+++ 1482427753.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      200514064
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 22;
+   volatile signed f1 : 19;
+   unsigned f2 : 9;
+   signed f3 : 17;
+   signed f4 : 28;
+   signed f5 : 3;
+   uint32_t  f6;
