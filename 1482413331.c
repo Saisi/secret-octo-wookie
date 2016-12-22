86a4ccee3384257780bd7d5a610ded9143777cfe
saisi <saisi@users.noreply.github.com>
2016-12-22 13:28:51 +0000

    1482413331 attenuative

---
+++ 1482413331.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1231071809
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1][8][4] = {{{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L}}};
+static int32_t g_5 = (-1L);
+static int32_t **g_46 = (void*)0;
+static int32_t * volatile g_51 = (void*)0;/* VOLATILE GLOBAL g_51 */
+static int32_t g_53 = 7L;
+static int32_t * const  volatile g_52 = &g_53;/* VOLATILE GLOBAL g_52 */
+static int16_t g_84[5] = {0x9C1FL,0x9C1FL,0x9C1FL,0x9C1FL,0x9C1FL};
+static int16_t g_86 = 0x2E7FL;
+static int16_t g_88 = 0L;
