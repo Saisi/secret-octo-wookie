8d691a81eca4c99ef034666a7cae29e328d73687
saisi <saisi@users.noreply.github.com>
2016-12-22 18:59:20 +0000

    1482433160 azygospore

---
+++ 1482433160.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2459522467
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   signed f1 : 7;
+   const signed f2 : 14;
+   signed f3 : 1;
+   volatile signed f4 : 14;
+   int32_t  f5;
+   signed f6 : 19;
