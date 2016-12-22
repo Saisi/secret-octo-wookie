bdd66ab37bcb4da54f9b5a8e40a8aba6a7adc7c9
saisi <saisi@users.noreply.github.com>
2016-12-22 12:28:46 +0000

    1482409725 atonable

---
+++ 1482409725.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2794718182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint32_t  f1;
+   const int64_t  f2;
+   int32_t  f3;
+   int32_t  f4;
+   const signed f5 : 23;
+};
+
+#pragma pack(push)
