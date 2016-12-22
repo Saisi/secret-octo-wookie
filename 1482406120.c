e3308f591d95d6740be7cf93b46efe7f582ffa29
saisi <saisi@users.noreply.github.com>
2016-12-22 11:28:40 +0000

    1482406120 autophony

---
+++ 1482406120.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3004512861
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[9] = {8L,8L,8L,8L,8L,8L,8L,8L,8L};
+static int32_t g_3 = (-1L);
+static int32_t g_4[6] = {0x00FE1CA2L,0x00FE1CA2L,0x00FE1CA2L,0x00FE1CA2L,0x00FE1CA2L,0x00FE1CA2L};
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xC4185A1FL;
+static uint16_t g_17 = 0x0D2FL;
+static int64_t g_22[6] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t * volatile g_26 = &g_2[3];/* VOLATILE GLOBAL g_26 */
