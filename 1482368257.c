6aa942d07dc9545e1f9aa03b05858d8d6f875eb2
saisi <saisi@users.noreply.github.com>
2016-12-22 00:57:38 +0000

    1482368257 autotelic

---
+++ 1482368257.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      805840801
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static uint8_t g_17 = 255UL;
+static int32_t g_22 = (-9L);
+static int64_t g_72[3] = {9L,9L,9L};
