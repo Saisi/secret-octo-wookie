869fb31ca6081db784c5fb1cf877402740d308de
saisi <saisi@users.noreply.github.com>
2016-12-23 11:00:52 +0000

    1482490852 adulterine

---
+++ 1482490852.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1618972339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2[4] = {18446744073709551610UL,18446744073709551610UL,18446744073709551610UL,18446744073709551610UL};
+static int32_t g_4 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
