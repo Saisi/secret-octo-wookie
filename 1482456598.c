1cfeb98b34c7e1496af4cf0378a3056a0e3c302b
saisi <saisi@users.noreply.github.com>
2016-12-23 01:29:58 +0000

    1482456598 antapex

---
+++ 1482456598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2444972339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile int64_t  f0;
+   signed f1 : 29;
+   volatile signed f2 : 31;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
