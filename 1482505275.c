6d34484b19e0031ca69050f448184ef1e0742abd
saisi <saisi@users.noreply.github.com>
2016-12-23 15:01:16 +0000

    1482505275 achievable

---
+++ 1482505275.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      68644886
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   int64_t  f1;
+   uint16_t  f2;
+   uint64_t  f3;
+   volatile int64_t  f4;
+   int8_t  f5;
+   uint16_t  f6;
