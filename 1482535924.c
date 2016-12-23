da74e6c9d455d110d8991832fa1ec4b8badf6cd6
saisi <saisi@users.noreply.github.com>
2016-12-23 23:32:05 +0000

    1482535924 acuductor

---
+++ 1482535924.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1282685793
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_29[3] = {0x00E2E4ED383DE467LL,0x00E2E4ED383DE467LL,0x00E2E4ED383DE467LL};
+static int64_t g_30 = 0xBE2D3D478254981CLL;
+static uint32_t g_34 = 0x1C443818L;
+static int32_t g_41 = 8L;
+static uint64_t g_45 = 0UL;
