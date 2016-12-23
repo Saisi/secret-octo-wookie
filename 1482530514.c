d289ef897897b690dcf96324fe8da1f57da6b6a0
saisi <saisi@users.noreply.github.com>
2016-12-23 22:01:54 +0000

    1482530514 Averno

---
+++ 1482530514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2000341709
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t *g_2 = (void*)0;
+static volatile int32_t g_3 = 5L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-5L);
+static const int32_t g_20 = 0x6F65E790L;
+static const int32_t *g_19 = &g_20;
+static uint32_t g_22[9][2] = {{0xB263D188L,0x76E5B14FL},{0x038E85A7L,0xB263D188L},{0xE7975358L,0xE7975358L},{0xE7975358L,0xB263D188L},{0x038E85A7L,0x76E5B14FL},{0xB263D188L,0x76E5B14FL},{0x038E85A7L,0xB263D188L},{0xE7975358L,0xE7975358L},{0xE7975358L,0xB263D188L}};
+static int32_t *g_37 = &g_4;
+static int32_t **g_36 = &g_37;
+static int32_t *** volatile g_35[9] = {&g_36,&g_36,&g_36,&g_36,&g_36,&g_36,&g_36,&g_36,&g_36};
