90305046c8c7534aeaf8d70ee55caed6193febc8
saisi <saisi@users.noreply.github.com>
2016-12-23 12:00:57 +0000

    1482494457 anticatalyzer

---
+++ 1482494457.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3698129943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+};
+
+struct S1 {
+   unsigned f0 : 9;
+   const unsigned f1 : 9;
+   volatile signed f2 : 12;
+   unsigned f3 : 10;
+   unsigned f4 : 5;
