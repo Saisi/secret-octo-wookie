b2d6dd18fc924b591a3f247b992bc079aa2862bd
saisi <saisi@users.noreply.github.com>
2016-12-23 12:31:00 +0000

    1482496259 ammocoetiform

---
+++ 1482496259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      606814452
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_14 = 0x90EF307DL;
+static int32_t g_18 = 0x20E32CF4L;
+static int32_t *g_17 = &g_18;
+static int32_t ** volatile g_19[5] = {&g_17,&g_17,&g_17,&g_17,&g_17};
+static int32_t ** volatile g_20 = &g_17;/* VOLATILE GLOBAL g_20 */
+static int16_t g_31 = 1L;
+static uint32_t *g_56 = &g_14;
+static uint32_t *g_60 = &g_14;
+static uint32_t *g_61 = &g_14;
