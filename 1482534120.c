ce0f693964f3841c0762d1eb4fbb3fd93ed2467d
saisi <saisi@users.noreply.github.com>
2016-12-23 23:02:01 +0000

    1482534120 allactite

---
+++ 1482534120.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1086426567
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 2;
+   unsigned f1 : 17;
+   volatile unsigned f2 : 16;
+};
+
+union U1 {
+   int32_t  f0;
+   uint64_t  f1;
+   uint64_t  f2;
