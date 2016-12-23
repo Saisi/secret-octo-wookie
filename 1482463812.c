2232a271dd80c9027ad5ccd78403a88b88990fcf
saisi <saisi@users.noreply.github.com>
2016-12-23 03:30:13 +0000

    1482463812 Banares

---
+++ 1482463812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3850224564
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 4294967295UL;
+static uint32_t g_21 = 0xC6FE23A2L;
+static int32_t g_24 = 1L;
+static volatile uint64_t g_31 = 18446744073709551615UL;/* VOLATILE GLOBAL g_31 */
+static uint8_t g_45 = 0xC8L;
+static uint64_t g_47 = 0xD253D975123020F6LL;
+static int32_t g_64 = (-1L);
+static int8_t g_70 = 8L;
+static uint8_t g_71 = 0xA8L;
