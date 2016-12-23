7c5a5307ced1a75d779f1cab35663392102606ac
saisi <saisi@users.noreply.github.com>
2016-12-23 09:30:44 +0000

    1482485444 agogics

---
+++ 1482485444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2809011008
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x63185299L;
+static int32_t g_7 = 0x5AB3E90AL;
+static int64_t g_12 = 0x1BC383C67EE39F3FLL;
+static uint32_t g_13 = 0xB94D0F4FL;
+static int32_t g_73 = (-10L);
+static uint8_t g_75 = 0x82L;
+static int16_t g_78 = 0xCE93L;
+static int32_t g_97[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
