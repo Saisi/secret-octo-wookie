0851a75b11d8dfb253edfda1d3710dde10b66c25
saisi <saisi@users.noreply.github.com>
2016-12-23 17:01:27 +0000

    1482512486 Bairoil

---
+++ 1482512486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3426694199
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 26;
+   int8_t  f1;
+   int32_t  f2;
+   volatile uint64_t  f3;
+   volatile int8_t * f4;
+};
+
+union U1 {
+   const uint64_t  f0;
