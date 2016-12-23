806c74f105875151369747b15f0e3296e4435f9e
saisi <saisi@users.noreply.github.com>
2016-12-23 13:31:07 +0000

    1482499867 agglomerates

---
+++ 1482499867.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3641561896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const volatile int32_t  f1;
+   const volatile int64_t  f2;
+};
+
+struct S1 {
+   volatile signed f0 : 17;
+   volatile int32_t  f1;
+   volatile struct S0  f2;
