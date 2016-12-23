7ed3d90223059cd3e2031a43edbc37074fde3d9b
saisi <saisi@users.noreply.github.com>
2016-12-23 04:30:18 +0000

    1482467418 Achariaceae

---
+++ 1482467418.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3472147087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int16_t  f0;
+   const int64_t  f1;
+   const volatile unsigned f2 : 20;
+   uint16_t  f3;
+   int16_t  f4;
+};
+
+union U1 {
+   uint8_t  f0;
