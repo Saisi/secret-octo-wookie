1b2bef5ae89fd5e5a73a9222cc782657550f1da9
saisi <saisi@users.noreply.github.com>
2016-12-23 08:30:39 +0000

    1482481839 arboricoline

---
+++ 1482481839.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      67005237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   uint64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_19[1][7] = {{(-2L),(-2L),(-2L),(-2L),(-2L),(-2L),(-2L)}};
+static int32_t g_21 = 0xE75D5F8DL;
