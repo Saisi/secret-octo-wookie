9b52b584a8bbc9b00a334c663dfb0e7648d7bc5e
saisi <saisi@users.noreply.github.com>
2016-12-23 20:31:46 +0000

    1482525106 Bakemeier

---
+++ 1482525106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2416777950
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 7L;
+static int16_t g_6 = 1L;
+static uint32_t g_7 = 0x61B583DCL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
