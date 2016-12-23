109ffb7dfa258ec946b15c3f122cca231a15863a
saisi <saisi@users.noreply.github.com>
2016-12-23 04:00:16 +0000

    1482465615 angelim

---
+++ 1482465615.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      566971413
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 9;
+   volatile unsigned f1 : 28;
+   signed f2 : 15;
+   unsigned f3 : 30;
+   unsigned f4 : 23;
+   volatile signed f5 : 20;
+   signed f6 : 3;
