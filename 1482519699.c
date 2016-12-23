6cf552e753d734bca4cc5ae0c983f2c15045933d
saisi <saisi@users.noreply.github.com>
2016-12-23 19:01:39 +0000

    1482519699 Alius

---
+++ 1482519699.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      262315968
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U5 {
+   const uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x1355FFF8L;
+static const volatile int8_t *g_11[3] = {(void*)0,(void*)0,(void*)0};
+static int32_t g_13 = 1L;
+static int32_t * const  volatile g_12 = &g_13;/* VOLATILE GLOBAL g_12 */
