64f2d29713c6ec249636b906ee5c43e569384113
saisi <saisi@users.noreply.github.com>
2016-12-24 23:34:18 +0000

    1482622458 Atikokania

---
+++ 1482622458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3694429146
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xBBF9ACF2L;
+static int32_t g_7 = 0L;
+static int64_t g_8 = (-1L);
+static uint32_t g_9 = 0x0CAAD901L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
