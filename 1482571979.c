5648ea18c63e0179feb573ae9742c4c662b63f1b
saisi <saisi@users.noreply.github.com>
2016-12-24 09:32:59 +0000

    1482571979 acetophenone

---
+++ 1482571979.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3621480010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 0xEFC07E17F759609CLL;
+static uint64_t g_12 = 18446744073709551615UL;
+static int16_t g_21[10] = {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L};
+static int16_t *g_49[6] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static uint8_t g_64 = 0x1EL;
