440053342add38f127ef33d9586c1ff504a4a0f4
saisi <saisi@users.noreply.github.com>
2016-12-24 16:03:38 +0000

    1482595417 agglomerator

---
+++ 1482595417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3931516064
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5299DADAL;
+static int32_t g_11[4] = {0x4A90611BL,0x4A90611BL,0x4A90611BL,0x4A90611BL};
+static int32_t g_33[1] = {(-6L)};
+static int32_t g_35 = 0L;
+static int32_t * const g_75 = (void*)0;
+static int32_t * const *g_74 = &g_75;
+static volatile int32_t g_79 = 0L;/* VOLATILE GLOBAL g_79 */
+static int32_t g_81 = 0x7F16C3B1L;
+static uint16_t g_82 = 1UL;
