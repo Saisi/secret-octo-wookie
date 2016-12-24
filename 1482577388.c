4e4bf26b0c8576cdf74e17e7ce42b7b64236c2e7
saisi <saisi@users.noreply.github.com>
2016-12-24 11:03:09 +0000

    1482577388 arresting

---
+++ 1482577388.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      392562494
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   int32_t  f1;
+   const int16_t  f2;
+   int16_t  f3;
+   int64_t  f4;
+   const volatile uint32_t  f5;
+   uint32_t  f6;
+   int8_t  f7;
+};
