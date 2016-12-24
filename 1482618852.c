7fd9fb313d902b9e13e738482f05d095fad0ceb3
saisi <saisi@users.noreply.github.com>
2016-12-24 22:34:13 +0000

    1482618852 antigraphy

---
+++ 1482618852.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1222713865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 14;
+   unsigned f2 : 25;
+   signed : 0;
+};
+#pragma pack(pop)
+
