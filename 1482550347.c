abddcf4e90f0d7eb479ff55d0038f6595d6263f6
saisi <saisi@users.noreply.github.com>
2016-12-24 03:32:27 +0000

    1482550347 bacteriopsonic

---
+++ 1482550347.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3839949134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t * f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6[10] = {3L,3L,3L,3L,3L,3L,3L,3L,3L,3L};
+static int8_t *g_5 = &g_6[5];
+static uint64_t g_8[5] = {0UL,0UL,0UL,0UL,0UL};
