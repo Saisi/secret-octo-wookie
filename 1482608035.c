82b4be64e94e56e389662e537a3b549a1c645806
saisi <saisi@users.noreply.github.com>
2016-12-24 19:33:55 +0000

    1482608035 aliform

---
+++ 1482608035.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3134854333
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   const int16_t  f1;
+   const int32_t  f2;
+   const volatile uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0x69L;/* VOLATILE GLOBAL g_2 */
+static uint64_t g_8 = 0x8CF5A8B060D2F38DLL;
