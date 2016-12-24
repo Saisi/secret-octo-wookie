3b8ec96d9a02e5b9c6d6a34b58b1f0d3f5f41215
saisi <saisi@users.noreply.github.com>
2016-12-24 15:03:32 +0000

    1482591811 archisperm

---
+++ 1482591811.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4062868951
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   unsigned f1 : 15;
+   const int64_t  f2;
+   const volatile uint32_t  f3;
+   uint16_t  f4;
+   uint32_t  f5;
+   volatile int32_t  f6;
+   const int64_t  f7;
+   const int32_t  f8;
