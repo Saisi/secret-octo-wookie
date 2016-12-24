be94acde19ab281c64cee55d9d495c646b7f72a3
saisi <saisi@users.noreply.github.com>
2016-12-24 01:32:16 +0000

    1482543135 BACH

---
+++ 1482543135.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      439994018
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile int8_t  f1;
+   int64_t  f2;
+   int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xAD3B3152L;
+static int32_t g_6 = 1L;
