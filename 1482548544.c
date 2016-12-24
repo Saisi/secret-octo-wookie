fc3d3512e59ac21f811f622680e242c219997e8f
saisi <saisi@users.noreply.github.com>
2016-12-24 03:02:25 +0000

    1482548544 acrogamy

---
+++ 1482548544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1873456822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint32_t  f0;
+   uint8_t  f1;
+   const int64_t  f2;
+   uint32_t  f3;
+   int64_t  f4;
+   int8_t  f5;
+};
+
+union U1 {
