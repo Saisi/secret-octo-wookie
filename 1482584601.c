465e37fd6e84ce64a238ca897186561bc730d160
saisi <saisi@users.noreply.github.com>
2016-12-24 13:03:22 +0000

    1482584601 appositions

---
+++ 1482584601.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3862578706
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int16_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xD05BA772L;
+static uint32_t g_5 = 0x4D073FF8L;
+static int32_t g_10 = (-1L);
