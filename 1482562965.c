473f9c89d762a017a8d75d99a9d1d1f2bbf9db58
saisi <saisi@users.noreply.github.com>
2016-12-24 07:02:45 +0000

    1482562965 arcose

---
+++ 1482562965.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2759676538
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   int64_t  f1;
+   int8_t * f2;
+   volatile int32_t  f3;
+   volatile unsigned f4 : 28;
+};
+
+union U1 {
+   int8_t  f0;
