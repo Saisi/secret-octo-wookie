88d4544fa9406c1cb0b878450b6b7d2ef76fd2a5
saisi <saisi@users.noreply.github.com>
2016-12-24 14:03:27 +0000

    1482588207 autosuggestibility

---
+++ 1482588207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3017607362
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x58D32FFAL;
+static int32_t g_7[4][1][5] = {{{(-1L),(-1L),(-1L),(-1L),(-1L)}},{{0x962EE6D0L,0L,0x962EE6D0L,0L,0x962EE6D0L}},{{(-1L),(-1L),(-1L),(-1L),(-1L)}},{{0xB5C6EFF3L,0L,0xB5C6EFF3L,0L,0xB5C6EFF3L}}};
+static int16_t g_45 = 1L;
+static int32_t * volatile g_46 = &g_7[0][0][2];/* VOLATILE GLOBAL g_46 */
+static int32_t g_67 = 0x422E233AL;
+static volatile int64_t g_81 = 0x1B5959469FD68CABLL;/* VOLATILE GLOBAL g_81 */
+static volatile int16_t g_82 = 0xDAE0L;/* VOLATILE GLOBAL g_82 */
+static int8_t g_83 = 0xF3L;
+static int32_t g_84 = 1L;
