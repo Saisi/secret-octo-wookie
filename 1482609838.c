1ddb6a2258da6249c755c605de8face686363614
saisi <saisi@users.noreply.github.com>
2016-12-24 20:03:59 +0000

    1482609838 Bardolphian

---
+++ 1482609838.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3988883093
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   const uint16_t  f1;
+   int32_t  f2;
+   signed f3 : 7;
+   const volatile int64_t  f4;
+   volatile uint8_t  f5;
+   volatile unsigned f6 : 20;
