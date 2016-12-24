945a757c80262006382311ce77da60763a104332
saisi <saisi@users.noreply.github.com>
2016-12-24 18:33:51 +0000

    1482604430 axopodium

---
+++ 1482604430.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1129897568
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+   volatile unsigned f1 : 27;
+   volatile int16_t  f2;
+   const signed f3 : 9;
+   const signed f4 : 10;
+   unsigned f5 : 14;
+   unsigned f6 : 23;
+   const unsigned f7 : 20;
+   volatile unsigned f8 : 21;
