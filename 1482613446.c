c018bf54ab107f2cedd6ed5286be9c8e334f3996
saisi <saisi@users.noreply.github.com>
2016-12-24 21:04:06 +0000

    1482613446 ampullulae

---
+++ 1482613446.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      106719574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2] = {0x75C19C5AL,0x75C19C5AL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
