0e1f7784ae9b5446278fa1e37472e9b0aa79662b
saisi <saisi@users.noreply.github.com>
2016-12-24 06:32:42 +0000

    1482561161 abiogenesist

---
+++ 1482561161.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4003591267
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+   uint64_t  f1;
+   int8_t  f2;
+   volatile signed f3 : 1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xAD4351CDL;
+static int32_t g_31 = 0xB6890635L;
