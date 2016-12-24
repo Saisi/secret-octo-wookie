cfe67141bdb55f2da37c273ff2f810025dae9365
saisi <saisi@users.noreply.github.com>
2016-12-24 04:32:32 +0000

    1482553952 amphophil

---
+++ 1482553952.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1349189616
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 6;
+   const uint8_t  f1;
+   volatile int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x3638ECF5L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[9][1] = {{0L},{(-1L)},{0L},{(-1L)},{0L},{(-1L)},{0L},{(-1L)},{0L}};
+static int8_t g_28[1][3][2] = {{{(-10L),(-10L)},{(-10L),(-10L)},{(-10L),(-10L)}}};
