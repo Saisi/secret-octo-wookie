4c2b8736822fc863aa1dbb300bb839e146a55053
saisi <saisi@users.noreply.github.com>
2016-12-24 18:03:48 +0000

    1482602627 antiepithelial

---
+++ 1482602627.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2642122634
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 6L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[2][9] = {{(-3L),(-1L),(-3L),0xC3FBB4EBL,(-1L),(-1L),(-1L),(-1L),0xC3FBB4EBL},{(-3L),(-1L),(-3L),0xC3FBB4EBL,(-1L),(-1L),(-1L),(-1L),0xC3FBB4EBL}};
+static volatile int32_t g_4 = 0xCE3E63E3L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x1DA622A1L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-10L);
+static int32_t g_10 = 0x7ADF1ADBL;
+static int32_t g_47 = (-2L);
+static int32_t g_48 = 0x75C875FFL;
+static int16_t g_70 = (-9L);
