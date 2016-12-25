6a3cde5774033a9d3d980af40027cf3135e1e2a9
saisi <saisi@users.noreply.github.com>
2016-12-25 10:35:15 +0000

    1482662115 Bains

---
+++ 1482662115.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      732477381
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 5L;
+static int8_t g_7 = 0x86L;
+static int32_t g_9 = (-8L);
+static int64_t g_30 = 0xA1434A938F738F58LL;
+static uint8_t g_31 = 0x04L;
+static uint16_t g_58 = 65534UL;
+static int32_t g_71 = 0L;
+static int32_t g_78 = 0x55EF1C8DL;
+static int32_t * volatile g_77[8] = {&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78};
