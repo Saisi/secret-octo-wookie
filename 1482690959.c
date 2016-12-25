73361c3b46d4d2f76dc096cf85c68407000bc227
saisi <saisi@users.noreply.github.com>
2016-12-25 18:35:59 +0000

    1482690959 azotine

---
+++ 1482690959.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      150335794
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 7;
+   const volatile unsigned : 0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_10 = {2};/* VOLATILE GLOBAL g_10 */
