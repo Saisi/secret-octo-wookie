8ae1e0face98993cb7fd1829791a5744b4fdc1cc
saisi <saisi@users.noreply.github.com>
2016-12-25 00:04:21 +0000

    1482624260 alimentariness

---
+++ 1482624260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4088629468
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   volatile uint32_t  f1;
+   int16_t  f2;
+   uint32_t  f3;
+   const volatile uint32_t  f4;
+};
+
+struct S1 {
+   uint32_t  f0;
