a9dd04387395f977c9fd21740154ed95f90b589e
saisi <saisi@users.noreply.github.com>
2016-12-25 17:05:52 +0000

    1482685551 antiknock

---
+++ 1482685551.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1036339148
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_17 = 7UL;
+static uint32_t g_22 = 0xD0890515L;
+static int64_t g_53 = 0x55DBFE6B87C2657ALL;
+static int32_t g_57[5][10] = {{0xC51F7EE0L,1L,0x35309F33L,0x56BBB307L,0x35309F33L,1L,0xC51F7EE0L,(-1L),1L,(-1L)},{(-1L),0L,0x702B6816L,(-1L),0xC51F7EE0L,0xC51F7EE0L,(-1L),0x702B6816L,0L,(-1L)},{0L,0L,1L,0x48A24E22L,0x56BBB307L,(-1L),0xC51F7EE0L,(-1L),0x56BBB307L,0x48A24E22L},{0x48A24E22L,1L,0x48A24E22L,0L,0x56BBB307L,0xC2C102D1L,(-1L),0x35309F33L,0x35309F33L,(-1L)},{0x56BBB307L,0xC51F7EE0L,0xC2C102D1L,0xC2C102D1L,0xC51F7EE0L,0x56BBB307L,0L,(-1L),0x56BBB307L,1L}};
+static int64_t *g_81 = (void*)0;
+static int64_t **g_80[3] = {&g_81,&g_81,&g_81};
+static int8_t g_85 = 0x60L;
+static int32_t g_94 = 0xA9186717L;
+static int32_t *g_96 = (void*)0;
