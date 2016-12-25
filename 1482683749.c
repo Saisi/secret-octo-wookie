a6726dd2f8bc582a03a6089b460d48843ee9e85d
saisi <saisi@users.noreply.github.com>
2016-12-25 16:35:49 +0000

    1482683749 ametabolic

---
+++ 1482683749.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      484241008
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_7 = 8UL;
+static uint8_t g_17 = 0UL;
+static int32_t g_23 = 0L;
+static volatile int8_t g_34 = 0L;/* VOLATILE GLOBAL g_34 */
+static volatile uint64_t g_35 = 0xC3997B09B37D0226LL;/* VOLATILE GLOBAL g_35 */
+static uint8_t g_39 = 250UL;
+static uint8_t *g_38[6][2] = {{(void*)0,&g_39},{&g_39,(void*)0},{&g_39,&g_39},{(void*)0,&g_39},{&g_39,(void*)0},{&g_39,&g_39}};
+static int16_t g_107[7] = {0x1CA2L,0x1CA2L,0x1CA2L,0x1CA2L,0x1CA2L,0x1CA2L,0x1CA2L};
+static uint64_t g_110[8] = {0xF408375F08A7CF5ELL,0x5B7D07A12536DE7ELL,0xF408375F08A7CF5ELL,0x5B7D07A12536DE7ELL,0xF408375F08A7CF5ELL,0x5B7D07A12536DE7ELL,0xF408375F08A7CF5ELL,0x5B7D07A12536DE7ELL};
