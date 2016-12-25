5322a327e292466dbda54149d4447dd1aa2e9958
saisi <saisi@users.noreply.github.com>
2016-12-25 14:05:34 +0000

    1482674734 Bairnsfather

---
+++ 1482674734.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2308819364
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0L;
+static volatile uint32_t g_19[8] = {0x6BDAE894L,0x6BDAE894L,0x6BDAE894L,0x6BDAE894L,0x6BDAE894L,0x6BDAE894L,0x6BDAE894L,0x6BDAE894L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
