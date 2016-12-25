cf81b76448628ef56361ce3b80746f4783815cf6
saisi <saisi@users.noreply.github.com>
2016-12-25 02:04:32 +0000

    1482631471 amortizations

---
+++ 1482631471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2449022076
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   unsigned f1 : 17;
+   volatile uint16_t  f2;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint16_t  f0;
