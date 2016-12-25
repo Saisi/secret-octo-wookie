bee8e49d61fa368355a36c92dc18983d458b3f27
saisi <saisi@users.noreply.github.com>
2016-12-25 23:06:27 +0000

    1482707187 antiperspirants

---
+++ 1482707187.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1003289357
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint16_t  f1;
+   uint64_t  f2;
+   volatile int32_t  f3;
+   signed f4 : 19;
+};
+
+union U1 {
+   const int32_t  f0;
