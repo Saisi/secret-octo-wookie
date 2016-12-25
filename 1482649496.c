83a79cf9902c04b71f09c4435f0ddb9c05f4ec45
saisi <saisi@users.noreply.github.com>
2016-12-25 07:04:56 +0000

    1482649496 antiflattering

---
+++ 1482649496.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3081644723
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 24;
+   uint8_t  f1;
+   signed f2 : 22;
+   signed f3 : 9;
+   const signed f4 : 22;
+};
+#pragma pack(pop)
