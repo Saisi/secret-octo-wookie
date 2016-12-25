6334c4a0f4dc8a953fb859a0ffea4b6334907c27
saisi <saisi@users.noreply.github.com>
2016-12-25 17:35:54 +0000

    1482687354 aeroplane

---
+++ 1482687354.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2193781989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xDFA860B4L;
+static int32_t g_7 = 0xA26F3F17L;
+static int32_t g_8[6] = {0x039ECF4AL,0x039ECF4AL,0x039ECF4AL,0x039ECF4AL,0x039ECF4AL,0x039ECF4AL};
+static volatile uint64_t g_22 = 0xFE924F63ABEFA025LL;/* VOLATILE GLOBAL g_22 */
+static volatile uint64_t * const  volatile g_21[3][4][9] = {{{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,(void*)0,(void*)0,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,(void*)0}},{{(void*)0,(void*)0,&g_22,&g_22,(void*)0,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,(void*)0,&g_22,(void*)0,&g_22,(void*)0,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22}},{{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,(void*)0,&g_22,(void*)0},{&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22},{(void*)0,&g_22,&g_22,(void*)0,&g_22,&g_22,(void*)0,&g_22,&g_22},{&g_22,&g_22,(void*)0,&g_22,&g_22,&g_22,&g_22,&g_22,&g_22}}};
+static volatile uint64_t g_24 = 7UL;/* VOLATILE GLOBAL g_24 */
+static const volatile uint64_t *g_23 = &g_24;
+static uint8_t g_53[6] = {9UL,7UL,9UL,9UL,7UL,9UL};
+static uint16_t g_77[6] = {0x6354L,1UL,0x6354L,0x6354L,1UL,0x6354L};
