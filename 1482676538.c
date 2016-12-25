cf8a9f4f5ac4024e17e80c9bd758bc54ed24879e
saisi <saisi@users.noreply.github.com>
2016-12-25 14:35:39 +0000

    1482676538 ballistophobia

---
+++ 1482676538.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3406485821
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_10 = 0x85L;
+static uint8_t g_26[10] = {7UL,246UL,0xFBL,0xFBL,246UL,7UL,246UL,0xFBL,0xFBL,246UL};
+static int16_t g_28 = 0x796AL;
+static int8_t *g_30 = &g_10;
