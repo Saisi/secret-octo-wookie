cac5800b993b4235fa78be3294a2d5af7a6bbc80
saisi <saisi@users.noreply.github.com>
2016-12-25 02:34:34 +0000

    1482633274 autocracies

---
+++ 1482633274.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3710992324
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   const volatile unsigned f1 : 5;
+   unsigned f2 : 19;
+};
+#pragma pack(pop)
+
+struct S1 {
