be8ecc6a1a1755b5b4b64a1d62254af91879a139
saisi <saisi@users.noreply.github.com>
2016-12-26 02:36:49 +0000

    1482719808 aperitif

---
+++ 1482719808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3832660842
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile uint16_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static int16_t g_20 = 0x8B15L;
+static int16_t g_22[2] = {5L,5L};
