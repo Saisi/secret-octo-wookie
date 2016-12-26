c75cd408666914c34e1017d3f510d99b8982445e
saisi <saisi@users.noreply.github.com>
2016-12-26 09:37:27 +0000

    1482745047 Actinia

---
+++ 1482745047.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3984827667
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+   const int8_t  f3;
+   int64_t  f4;
+   int8_t  f5;
+   const uint8_t  f6;
+};
+
