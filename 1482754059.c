ea8346d3c8ac8be48415c46c7cbceddbd958925a
saisi <saisi@users.noreply.github.com>
2016-12-26 12:07:40 +0000

    1482754059 anacardic

---
+++ 1482754059.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3708652450
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   unsigned f1 : 28;
+   const int8_t  f2;
+   int64_t  f3;
+   const int16_t  f4;
+   int32_t  f5;
+   const uint32_t  f6;
+   volatile int8_t  f7;
+   volatile int16_t  f8;
