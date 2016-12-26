3d87da06deb941342e7949b6851904e8f8903dab
saisi <saisi@users.noreply.github.com>
2016-12-26 04:06:58 +0000

    1482725217 affectation

---
+++ 1482725217.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1691496649
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int64_t  f0;
+   uint32_t  f1;
+   const signed f2 : 10;
+   volatile int64_t  f3;
+};
+#pragma pack(pop)
+
