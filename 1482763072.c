1ca4b3270fcfc9487ebbab8b89fc184738194d28
saisi <saisi@users.noreply.github.com>
2016-12-26 14:37:52 +0000

    1482763072 aphacia

---
+++ 1482763072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4246133360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile uint8_t  f1;
+   signed f2 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[7] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static volatile int32_t g_4 = 0x6182EAC4L;/* VOLATILE GLOBAL g_4 */
