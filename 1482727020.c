54c68fa1080a62472567b2c8d491cfa17745ac02
saisi <saisi@users.noreply.github.com>
2016-12-26 04:37:01 +0000

    1482727020 aerator

---
+++ 1482727020.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1995408020
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+   unsigned f1 : 13;
+   const signed f2 : 27;
+   unsigned f3 : 27;
+   volatile unsigned f4 : 30;
+   volatile unsigned f5 : 29;
+   const unsigned f6 : 7;
+};
+
