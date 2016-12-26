29ab1aad9646d0f4f34d921ab38d30bf0aa496e2
saisi <saisi@users.noreply.github.com>
2016-12-26 13:07:45 +0000

    1482757665 barbecued

---
+++ 1482757665.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3240661604
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 3;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x6E7F737AL;
+static uint8_t g_10 = 251UL;
