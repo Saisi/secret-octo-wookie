2c413806e0a4727cd22172b6f8d6c6096f9f7a84
saisi <saisi@users.noreply.github.com>
2016-12-26 19:08:18 +0000

    1482779297 adephaga

---
+++ 1482779297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3767051557
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   const signed f1 : 4;
+};
+
+union U1 {
+   int16_t  f0;
+   int64_t  f1;
+   const uint32_t  f2;
+   volatile int64_t  f3;
