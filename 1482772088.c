2403ced0a57dcaf06b82bf4b9c5c47cb6552de01
saisi <saisi@users.noreply.github.com>
2016-12-26 17:08:08 +0000

    1482772088 amylums

---
+++ 1482772088.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      808339554
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 31;
+   signed f1 : 24;
+};
+
+union U1 {
+   unsigned f0 : 2;
+   unsigned f1 : 31;
+};
+
