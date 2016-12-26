b9419054edc68d22f8bb0a55b9fc8a7798484f61
saisi <saisi@users.noreply.github.com>
2016-12-26 15:07:56 +0000

    1482764874 aquocarbonic

---
+++ 1482764874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      701329721
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   signed f1 : 6;
+   unsigned f2 : 17;
+   unsigned f3 : 24;
+   volatile unsigned f4 : 9;
+   signed f5 : 30;
+   unsigned f6 : 5;
+   signed f7 : 4;
+   volatile unsigned f8 : 23;
