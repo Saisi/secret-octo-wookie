c077f68d7d22b5bba5bc9ba6d3cb83a2c85f6852
saisi <saisi@users.noreply.github.com>
2016-12-26 01:06:39 +0000

    1482714397 alarmist

---
+++ 1482714397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      598223672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   uint64_t  f1;
+   volatile int32_t  f2;
+};
+
+union U1 {
+   const volatile uint8_t  f0;
+   int8_t  f1;
+   const int16_t  f2;
