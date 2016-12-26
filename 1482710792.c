b6cd2ec3973e54366f0798f6b6a5479ab7233299
saisi <saisi@users.noreply.github.com>
2016-12-26 00:06:33 +0000

    1482710792 abutment

---
+++ 1482710792.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3456806191
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static volatile uint64_t g_16 = 18446744073709551608UL;/* VOLATILE GLOBAL g_16 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
