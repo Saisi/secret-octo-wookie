514ae82f4689a4a5811c0969ef0e08d014679180
saisi <saisi@users.noreply.github.com>
2016-12-26 23:38:43 +0000

    1482795523 Adighe

---
+++ 1482795523.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3492779046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 1;
+   const volatile signed f1 : 18;
+   volatile signed f2 : 12;
+   volatile signed f3 : 8;
+};
+
+struct S1 {
+   volatile signed f0 : 17;
+   volatile signed f1 : 14;
