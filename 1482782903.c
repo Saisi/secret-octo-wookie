d8046d680f729af16d1f131725452574ffe49266
saisi <saisi@users.noreply.github.com>
2016-12-26 20:08:23 +0000

    1482782903 antihumanist

---
+++ 1482782903.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3081959810
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int16_t g_7[4] = {1L,1L,1L,1L};
+static volatile uint64_t g_8 = 0UL;/* VOLATILE GLOBAL g_8 */
+static int8_t g_14 = 0x88L;
+static uint16_t g_16[2] = {0xFDAFL,0xFDAFL};
+static int8_t g_18 = 0xA7L;
+
+
+/* --- FORWARD DECLARATIONS --- */
