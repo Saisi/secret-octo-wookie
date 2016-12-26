c26ae10ffabcbff02d6434fb159078f95f3ed538
saisi <saisi@users.noreply.github.com>
2016-12-26 02:06:46 +0000

    1482718004 angiopressure

---
+++ 1482718004.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145030225
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   int8_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+   volatile uint64_t  f5;
+   const int8_t  f6;
+   volatile int64_t  f7;
+};
