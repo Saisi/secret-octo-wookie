5adcbc606d19b22289e9517acbe4a94cb390f285
saisi <saisi@users.noreply.github.com>
2016-12-27 09:09:32 +0000

    1482829772 acesodyne

---
+++ 1482829772.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2073838502
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const uint32_t  f1;
+   int16_t  f2;
+   uint64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 9UL;
+static int8_t g_5[8] = {8L,8L,8L,8L,8L,8L,8L,8L};
