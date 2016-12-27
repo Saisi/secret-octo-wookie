64df9e0610f9409a936e9f055e3d2b4a6e56e1c2
saisi <saisi@users.noreply.github.com>
2016-12-27 16:40:20 +0000

    1482856819 alodia

---
+++ 1482856819.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1922516222
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int32_t  f1;
+   const volatile signed f2 : 22;
+   volatile signed : 0;
+   signed f3 : 5;
+   signed f4 : 29;
+   signed f5 : 10;
+   int16_t  f6;
+};
