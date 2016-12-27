33f120f0d99dc6287183689abebb5d3f8a11d790
saisi <saisi@users.noreply.github.com>
2016-12-27 18:10:27 +0000

    1482862227 axiological

---
+++ 1482862227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      702160204
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 9L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xA1F94964L;
+static int32_t *g_8[4] = {&g_3,&g_3,&g_3,&g_3};
+static int32_t ** volatile g_7 = &g_8[1];/* VOLATILE GLOBAL g_7 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
