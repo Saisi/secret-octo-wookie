6abcfd79a5c89fed8de0ff843cf16ec2cf3b1e12
saisi <saisi@users.noreply.github.com>
2016-12-27 01:08:51 +0000

    1482800930 armseye

---
+++ 1482800930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1172352143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const union U0 g_2[1][7][3] = {{{{1L},{0x06L},{1L}},{{-1L},{0L},{-1L}},{{1L},{0x06L},{1L}},{{-1L},{0L},{-1L}},{{1L},{0x06L},{1L}},{{-1L},{0L},{-1L}},{{1L},{0x06L},{1L}}}};
+static uint64_t g_80 = 0UL;
+static uint32_t g_93 = 0xFE9DE559L;
+static int32_t g_104 = 0xEE7B07E0L;
+static uint8_t g_108[4] = {7UL,7UL,7UL,7UL};
