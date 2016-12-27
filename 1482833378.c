68b3f7f0fb9d0a2e1dc656c4b18726259a9375bb
saisi <saisi@users.noreply.github.com>
2016-12-27 10:09:40 +0000

    1482833378 aliesterase

---
+++ 1482833378.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2062349367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int32_t  f1;
+   volatile uint8_t  f2;
+   volatile int8_t  f3;
+   const uint8_t  f4;
+};
+
+union U1 {
+   unsigned f0 : 24;
