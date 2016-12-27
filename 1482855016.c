9811a15d0778fed1af70f2d0b91e92d42ca13fff
saisi <saisi@users.noreply.github.com>
2016-12-27 16:10:17 +0000

    1482855016 ambleocarpus

---
+++ 1482855016.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      192068677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   uint64_t  f1;
+   unsigned f2 : 7;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
