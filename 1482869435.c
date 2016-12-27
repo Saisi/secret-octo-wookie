b2c8127edfe2750ba707436f2cf4f71cfa1a2b91
saisi <saisi@users.noreply.github.com>
2016-12-27 20:10:36 +0000

    1482869435 antimedical

---
+++ 1482869435.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1085613110
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_17 = 0x59L;
+static int32_t * volatile g_19 = (void*)0;/* VOLATILE GLOBAL g_19 */
+static int32_t * volatile g_20 = (void*)0;/* VOLATILE GLOBAL g_20 */
+static int32_t g_22 = 0xE486AEBAL;
+static union U0 g_38[3][1][1] = {{{{0xEF33L}}},{{{0xEF33L}}},{{{0xEF33L}}}};
