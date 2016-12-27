f6f0a8a7afae3d9a19244ff771111f5cb4cde449
saisi <saisi@users.noreply.github.com>
2016-12-27 06:39:20 +0000

    1482820759 aspersory

---
+++ 1482820759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1699597594
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int16_t  f0;
+   volatile uint8_t  f1;
+   volatile uint16_t  f2;
+   volatile uint64_t  f3;
+};
+
+struct S1 {
+   const uint16_t  f0;
+   const volatile int64_t  f1;
