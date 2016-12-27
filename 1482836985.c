cb8b145c203e9ca8bf0100a409f6f5451dd84432
saisi <saisi@users.noreply.github.com>
2016-12-27 11:09:46 +0000

    1482836985 ABEPP

---
+++ 1482836985.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1888962972
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   int32_t  f1;
+   const volatile uint8_t  f2;
+   const volatile uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_23 = 18446744073709551607UL;
+static uint8_t g_37 = 0x54L;
