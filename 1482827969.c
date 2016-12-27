e6e9aab5ecdce0b0ee2534297e449585f42dd275
saisi <saisi@users.noreply.github.com>
2016-12-27 08:39:29 +0000

    1482827969 ambassadorships

---
+++ 1482827969.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      460243423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-7L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x5DCEC3D7L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x0C4D8E5CL;
+static int32_t g_8 = (-7L);
+static volatile int8_t g_20 = (-1L);/* VOLATILE GLOBAL g_20 */
+static int16_t g_22 = (-1L);
+static int32_t g_25 = 0x6E836EA0L;
+static volatile int64_t g_26 = 0x1543F36FC75DFCC9LL;/* VOLATILE GLOBAL g_26 */
+static int32_t g_27 = 0x32D49ACBL;
