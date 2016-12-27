87e8dc43fb82f39f8497858a683064ceefce038e
saisi <saisi@users.noreply.github.com>
2016-12-27 21:10:40 +0000

    1482873040 archership

---
+++ 1482873040.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1840434262
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x3F467215L;
+static int32_t g_7[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int64_t g_46 = 1L;
+static uint32_t g_59 = 18446744073709551610UL;
+static int64_t g_67 = 0x91F28F68172882C6LL;
+static int16_t g_77 = 0x950CL;
+static uint32_t g_102[8] = {0x3D079042L,0x3F68C368L,0x3D079042L,0x3D079042L,0x3F68C368L,0x3D079042L,0x3D079042L,0x3F68C368L};
+static uint64_t g_105 = 0xC253094EEE57E67FLL;
+static int32_t *g_110 = (void*)0;
