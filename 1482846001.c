75c26f16faa177796654bda8804157fe2be27663
saisi <saisi@users.noreply.github.com>
2016-12-27 13:40:02 +0000

    1482846001 arrhizous

---
+++ 1482846001.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1797581153
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+   uint16_t  f1;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   unsigned f1 : 17;
+   int32_t  f2;
+   uint64_t  f3;
