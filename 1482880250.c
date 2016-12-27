10e7f64efd384ca3a1a7bb3f1d5c1587a3fb8b24
saisi <saisi@users.noreply.github.com>
2016-12-27 23:10:51 +0000

    1482880250 anomalocephalus

---
+++ 1482880250.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1721157724
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_20 = 0xB64FDA7EL;
+static volatile int32_t g_51[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile int32_t g_52 = 1L;/* VOLATILE GLOBAL g_52 */
