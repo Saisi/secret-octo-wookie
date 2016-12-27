c95faed52da259930f50f01ee72e30312e2e8433
saisi <saisi@users.noreply.github.com>
2016-12-27 06:09:17 +0000

    1482818956 Adelanto

---
+++ 1482818956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4125052879
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   uint64_t  f1;
+   volatile uint32_t  f2;
+   uint8_t  f3;
+   volatile unsigned f4 : 21;
+   signed f5 : 28;
+   uint32_t  f6;
