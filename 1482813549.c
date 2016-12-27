c995384111efae4011a7cd8e5f0c0a8b9eedb54d
saisi <saisi@users.noreply.github.com>
2016-12-27 04:39:09 +0000

    1482813549 Atropos

---
+++ 1482813549.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1176585835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   volatile int16_t  f1;
+   unsigned f2 : 27;
+   const volatile uint16_t  f3;
+   volatile int64_t  f4;
+   unsigned f5 : 18;
+   unsigned f6 : 18;
