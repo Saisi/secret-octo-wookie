b7469c9bf74c57b503e3a108c5fb737c93799011
saisi <saisi@users.noreply.github.com>
2016-12-27 13:09:59 +0000

    1482844198 abducent

---
+++ 1482844198.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      175109518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   volatile signed f1 : 18;
+   uint8_t  f2;
+   uint8_t  f3;
+};
+#pragma pack(pop)
+
