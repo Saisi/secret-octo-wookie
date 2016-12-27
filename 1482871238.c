e3312428f3bafd98d6190d017b66133c22b39e30
saisi <saisi@users.noreply.github.com>
2016-12-27 20:40:38 +0000

    1482871238 aeroplankton

---
+++ 1482871238.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1964594427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   const unsigned f0 : 15;
+   signed f1 : 13;
+   unsigned f2 : 8;
+   volatile uint64_t  f3;
+   signed f4 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const struct S1 g_2 = {116,4,2,0x644ACCB24CE55459LL,82};/* VOLATILE GLOBAL g_2 */
