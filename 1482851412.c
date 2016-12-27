06dc7ea8a33b80601300ee4363ab2639c5277617
saisi <saisi@users.noreply.github.com>
2016-12-27 15:10:12 +0000

    1482851412 antemeridian

---
+++ 1482851412.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3218907921
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile signed f1 : 28;
+   const volatile unsigned f2 : 2;
+   signed f3 : 30;
+   const signed f4 : 13;
+   signed f5 : 7;
+};
+
+/* --- GLOBAL VARIABLES --- */
