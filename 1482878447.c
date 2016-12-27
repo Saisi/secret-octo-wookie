8fbbb7e0d4df47ec9e82df71266fd653aaac0fd5
saisi <saisi@users.noreply.github.com>
2016-12-27 22:40:48 +0000

    1482878447 arbor

---
+++ 1482878447.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      677293686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_10 = 0xE2L;/* VOLATILE GLOBAL g_10 */
+static uint32_t g_11[7] = {0x58A012E1L,0x04800FA7L,0x58A012E1L,0x58A012E1L,0x04800FA7L,0x58A012E1L,0x58A012E1L};
+static int16_t g_13 = 7L;
+static uint16_t g_19 = 65530UL;
+static int32_t g_21 = 0x5D64F689L;
+static int32_t *g_20 = &g_21;
+static int32_t ** volatile g_22 = (void*)0;/* VOLATILE GLOBAL g_22 */
+static uint32_t g_72[9] = {0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L,0xE8591F28L};
+static int32_t g_74 = 0xFA727FAAL;
