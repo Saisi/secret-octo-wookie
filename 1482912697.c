b828f5a9325e1c563087a92f6958e42ab4588a1c
saisi <saisi@users.noreply.github.com>
2016-12-28 08:11:38 +0000

    1482912697 Affrica

---
+++ 1482912697.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3486842129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-8L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[10] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static volatile int32_t g_6 = 1L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7[5] = {0x20985A95L,0x20985A95L,0x20985A95L,0x20985A95L,0x20985A95L};
+static int32_t g_8 = 0L;
+static int32_t g_9[10][2] = {{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L},{0L,7L}};
+static volatile int32_t g_10 = 0xC8DFBA22L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t g_11 = 0L;/* VOLATILE GLOBAL g_11 */
