cabc6f4466df4a44e94943d0fca2927782394c50
saisi <saisi@users.noreply.github.com>
2016-12-28 14:42:15 +0000

    1482936135 actuarially

---
+++ 1482936135.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2567467536
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+union U1 {
+   const int32_t  f0;
+   int32_t  f1;
+   int16_t  f2;
+   uint8_t  f3;
+   signed f4 : 15;
