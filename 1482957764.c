bd0a823087f19cb861b30807945e284a4fa0f81a
saisi <saisi@users.noreply.github.com>
2016-12-28 20:42:44 +0000

    1482957764 baculums

---
+++ 1482957764.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2991421174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[7] = {0x9A3DF603L,0x24F01C08L,0x9A3DF603L,0x9A3DF603L,0x24F01C08L,0x9A3DF603L,0x9A3DF603L};
+static uint32_t g_20 = 0xE8F5349DL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
