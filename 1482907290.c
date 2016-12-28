98cdb1a877b7392e6e518311d3f229972dc99c5d
saisi <saisi@users.noreply.github.com>
2016-12-28 06:41:30 +0000

    1482907290 airships

---
+++ 1482907290.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      818278766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_17 = 1UL;
+static int8_t g_20 = 0x5DL;
+static int32_t g_51 = 9L;
+static int32_t g_53 = 0xC39D5B61L;
+static int32_t * volatile g_52 = &g_53;/* VOLATILE GLOBAL g_52 */
+static int8_t g_59 = 0x65L;
+static int8_t *g_58 = &g_59;
+static int32_t ** const  volatile g_62[6] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t *g_64 = &g_53;
