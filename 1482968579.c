e79baaf16f137a229e6e3640fc30a4142fb0a99c
saisi <saisi@users.noreply.github.com>
2016-12-28 23:42:59 +0000

    1482968579 antigrowth

---
+++ 1482968579.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4028521461
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 25;
+   const unsigned f1 : 12;
+   unsigned f2 : 31;
+   signed f3 : 14;
+   const unsigned f4 : 7;
+   const unsigned f5 : 4;
+};
+
+struct S1 {
