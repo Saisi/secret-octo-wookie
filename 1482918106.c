794d0aaac6d3ceb2208e19562705664403520929
saisi <saisi@users.noreply.github.com>
2016-12-28 09:41:47 +0000

    1482918106 arythmia

---
+++ 1482918106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1033536204
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 20;
+};
+
+union U1 {
+   const uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0xA53AA9ACL;
