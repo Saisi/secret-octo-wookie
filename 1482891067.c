18d1f16d25586d842570b213c61ecf681f837e2d
saisi <saisi@users.noreply.github.com>
2016-12-28 02:11:08 +0000

    1482891067 anogenital

---
+++ 1482891067.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      651292528
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint32_t  f0;
+   unsigned f1 : 3;
+   unsigned f2 : 25;
+   const unsigned f3 : 1;
+   volatile int32_t  f4;
+};
+#pragma pack(pop)
