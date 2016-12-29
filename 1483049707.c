7527e5917a932c1776de0675d8000f464babc06a
saisi <saisi@users.noreply.github.com>
2016-12-29 22:15:08 +0000

    1483049707 assimilationist

---
+++ 1483049707.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4292390519
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int8_t  f0;
+   uint8_t  f1;
+   volatile uint8_t  f2;
+   int64_t  f3;
+   uint8_t  f4;
+   signed f5 : 24;
+   const int32_t  f6;
