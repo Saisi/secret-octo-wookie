d0645ab3847704f92b2512cddfc4aa434acaf61c
saisi <saisi@users.noreply.github.com>
2016-12-29 23:45:16 +0000

    1483055115 Arkansawyer

---
+++ 1483055115.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3724138081
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 15;
+};
+
+union U1 {
+   const int32_t  f0;
+   int32_t  f1;
+};
+
+union U2 {
