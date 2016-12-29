261b9d5f76108bb7db3f5c36016350149cb38bb9
saisi <saisi@users.noreply.github.com>
2016-12-29 17:44:43 +0000

    1483033482 avalon

---
+++ 1483033482.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1107043787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   volatile int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_3 = {18446744073709551615UL};/* VOLATILE GLOBAL g_3 */
+static int32_t g_6[8] = {0xBC5021C3L,0x358BD4DFL,0xBC5021C3L,0xBC5021C3L,0x358BD4DFL,0xBC5021C3L,0xBC5021C3L,0x358BD4DFL};
+static const int32_t *g_7 = &g_6[2];
