8f350e5b08d37a5c904a56deba9fb22479d2bc74
saisi <saisi@users.noreply.github.com>
2016-12-29 08:43:47 +0000

    1483001026 aciform

---
+++ 1483001026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1649813853
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 13;
+   unsigned f1 : 16;
+   const volatile unsigned f2 : 19;
+   unsigned f3 : 18;
+   volatile signed f4 : 27;
+   const signed f5 : 8;
+};
