a2cbe027b39233b99054e24d62b3974ad7855440
saisi <saisi@users.noreply.github.com>
2016-12-29 10:14:00 +0000

    1483006440 agrologies

---
+++ 1483006440.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2652960098
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   signed f1 : 24;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5] = {0x5020F89BL,0x5020F89BL,0x5020F89BL,0x5020F89BL,0x5020F89BL};
+static int32_t g_5 = 0x52C58FB7L;
+static int8_t g_8[7] = {0x05L,0x05L,0x05L,0x05L,0x05L,0x05L,0x05L};
