26a7e06ec400065b4129a0a2f93bd734f621f01c
saisi <saisi@users.noreply.github.com>
2016-12-29 13:44:21 +0000

    1483019061 Amphissa

---
+++ 1483019061.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4136720316
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[7][4] = {{0xE34EDF6BL,1L,0xC54C9751L,(-1L)},{0x8018D9DCL,1L,0x8018D9DCL,9L},{1L,0xEF5D870AL,0x591B68DEL,1L},{1L,(-1L),0xE34EDF6BL,0xEF5D870AL},{0L,0xE34EDF6BL,0xE34EDF6BL,0L},{1L,9L,0x591B68DEL,0x8018D9DCL},{1L,0x47F1A969L,0x8018D9DCL,0xE6AA1654L}};
+static int8_t g_15 = 0x13L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
