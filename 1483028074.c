61fcbf7885d48fe0c879c8887befb951567606d0
saisi <saisi@users.noreply.github.com>
2016-12-29 16:14:35 +0000

    1483028074 ambicolorate

---
+++ 1483028074.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3138132963
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   volatile unsigned f1 : 7;
+   const signed f2 : 14;
+   volatile signed f3 : 1;
+   const volatile signed f4 : 14;
+   unsigned f5 : 15;
+   volatile unsigned f6 : 18;
