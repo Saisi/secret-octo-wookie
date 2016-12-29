96292aa260ce178c2522e4bb31e09a41b32f6c7f
saisi <saisi@users.noreply.github.com>
2016-12-29 21:45:03 +0000

    1483047903 arachnidium

---
+++ 1483047903.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2815632677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 8L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[7] = {0xFFB871C7L,(-1L),0xFFB871C7L,0xFFB871C7L,(-1L),0xFFB871C7L,0xFFB871C7L};
+static uint32_t g_13[4] = {18446744073709551613UL,18446744073709551613UL,18446744073709551613UL,18446744073709551613UL};
+static int32_t g_19 = 9L;
+static volatile uint8_t g_21 = 3UL;/* VOLATILE GLOBAL g_21 */
+static int32_t g_50 = 0x39AEA1DCL;
+static int32_t g_63 = 0x3DC28BF2L;
+static int32_t * volatile g_62 = &g_63;/* VOLATILE GLOBAL g_62 */
+static int32_t g_70[10] = {0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL,0x679F1A7DL};
