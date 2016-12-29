63269dce9c17091f148351d5dadf37993ca18862
saisi <saisi@users.noreply.github.com>
2016-12-29 07:43:42 +0000

    1482997421 amateurship

---
+++ 1482997421.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3639883742
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 29;
+   unsigned f1 : 30;
+   unsigned f2 : 8;
+   const volatile unsigned f3 : 20;
+};
+#pragma pack(pop)
+
