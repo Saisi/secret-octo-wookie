3f8d5d1b00ac7239ece4c69a635335a7102488ea
saisi <saisi@users.noreply.github.com>
2016-12-29 08:13:44 +0000

    1482999224 barbierite

---
+++ 1482999224.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1286485298
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 7;
+   volatile signed : 0;
+   volatile int8_t  f1;
+   unsigned f2 : 10;
+   signed f3 : 7;
+   signed f4 : 23;
+   const signed f5 : 28;
