92c56917c59db116d85db653f1c6a3cf534e32a2
saisi <saisi@users.noreply.github.com>
2016-12-29 20:44:57 +0000

    1483044297 accordionists

---
+++ 1483044297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2457927922
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 7;
+   const unsigned : 0;
+   volatile uint32_t  f1;
+   signed f2 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 0UL;
+static uint32_t g_17 = 0x14AF12A8L;
