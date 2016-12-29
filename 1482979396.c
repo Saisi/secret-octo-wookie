7eedca4e695cc1d715814e74e1cc398363040e30
saisi <saisi@users.noreply.github.com>
2016-12-29 02:43:16 +0000

    1482979396 aphorists

---
+++ 1482979396.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1824587628
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const uint64_t  f1;
+   volatile int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_6[5] = {0x07L,0x07L,0x07L,0x07L,0x07L};
+static uint8_t g_11[10] = {0x56L,0x56L,0x56L,0x56L,0x56L,0x56L,0x56L,0x56L,0x56L,0x56L};
+static uint8_t *g_21 = (void*)0;
