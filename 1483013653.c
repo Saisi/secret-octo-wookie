6232b180d21baecd1071ba3fbc7542ff746c1d46
saisi <saisi@users.noreply.github.com>
2016-12-29 12:14:14 +0000

    1483013653 baffler

---
+++ 1483013653.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3848369761
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 15;
+   signed f1 : 10;
+   signed f2 : 5;
+   volatile signed f3 : 28;
+   volatile signed f4 : 29;
+   int32_t  f5;
+};
+
+struct S1 {
