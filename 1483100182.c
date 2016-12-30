51b3b74ee07d08b472352e51f07f9066c6289f53
saisi <saisi@users.noreply.github.com>
2016-12-30 12:16:22 +0000

    1483100182 arterialising

---
+++ 1483100182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3942039603
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_7 = 0UL;
+static int32_t g_20 = (-4L);
+static int16_t g_25 = 0L;
+static volatile uint32_t g_26 = 0x002891BAL;/* VOLATILE GLOBAL g_26 */
+static int64_t g_44[7] = {0xF314D41BD43D9C8CLL,7L,0xF314D41BD43D9C8CLL,0xF314D41BD43D9C8CLL,7L,0xF314D41BD43D9C8CLL,0xF314D41BD43D9C8CLL};
+static volatile int32_t g_45 = 0xEC73F5CFL;/* VOLATILE GLOBAL g_45 */
+static int32_t g_46 = 0xE52275DDL;
+static volatile uint64_t g_64 = 0xED416A781B38C87CLL;/* VOLATILE GLOBAL g_64 */
+static uint64_t g_99 = 0x9C50E9BCC685A4B5LL;
