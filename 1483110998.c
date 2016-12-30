e23a57414072b84ec25fda95d2fe7c5c48d50192
saisi <saisi@users.noreply.github.com>
2016-12-30 15:16:38 +0000

    1483110998 amble

---
+++ 1483110998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2884856614
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   signed f1 : 18;
+   signed f2 : 12;
+   const unsigned f3 : 8;
+   volatile signed f4 : 23;
+   volatile signed f5 : 18;
+   signed f6 : 8;
