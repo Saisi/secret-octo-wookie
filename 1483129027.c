f1e905ec667fc1342fb652612c9d4645255e33c3
saisi <saisi@users.noreply.github.com>
2016-12-30 20:17:07 +0000

    1483129027 albitic

---
+++ 1483129027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      717092787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+};
+
+union U1 {
+   uint16_t  f0;
+   int16_t  f1;
+   volatile signed f2 : 6;
+};
+
