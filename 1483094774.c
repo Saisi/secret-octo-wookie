6029324fcbda868b3facc5b30ad3ddb84c697403
saisi <saisi@users.noreply.github.com>
2016-12-30 10:46:16 +0000

    1483094774 abscondence

---
+++ 1483094774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      437038173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   int8_t  f1;
+   volatile uint64_t  f2;
+   uint32_t  f3;
+   uint8_t  f4;
+   int8_t  f5;
+};
+
+union U1 {
