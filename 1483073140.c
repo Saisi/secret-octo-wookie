d7336b632135f08aeafc0213ca4c141e79e8eaf2
saisi <saisi@users.noreply.github.com>
2016-12-30 04:45:40 +0000

    1483073140 Babelise

---
+++ 1483073140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      588283871
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-9L);
+static uint64_t g_50 = 0xD857289C089CED66LL;
+static int16_t g_83 = 0xFF01L;
+static uint32_t g_98[1] = {1UL};
+static int32_t g_103 = 3L;
+static volatile int32_t * volatile * volatile g_139 = (void*)0;/* VOLATILE GLOBAL g_139 */
+static volatile int32_t g_145 = 0xCFD0E8CDL;/* VOLATILE GLOBAL g_145 */
+static volatile int32_t *g_144 = &g_145;
+static uint64_t g_163[2] = {18446744073709551614UL,18446744073709551614UL};
