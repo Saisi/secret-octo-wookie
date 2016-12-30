fd4187a871c9a72968f7788c4335beccf6ef66fb
saisi <saisi@users.noreply.github.com>
2016-12-30 16:16:43 +0000

    1483114602 anhydride

---
+++ 1483114602.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3735410628
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint64_t  f1;
+   signed f2 : 8;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 20;
