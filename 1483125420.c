baee9a99801027e91afc21842d4855eec1292df1
saisi <saisi@users.noreply.github.com>
2016-12-30 19:17:00 +0000

    1483125420 acidoses

---
+++ 1483125420.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2224367433
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 3;
+   signed f1 : 14;
+   const volatile signed f2 : 25;
+   signed : 0;
+};
+#pragma pack(pop)
+
