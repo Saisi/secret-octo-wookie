83af3f54046f1f808e91cbc1a45a8f36647dd48a
saisi <saisi@users.noreply.github.com>
2016-12-30 01:45:25 +0000

    1483062325 argalas

---
+++ 1483062325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2516390602
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint64_t  f1;
+};
+
+union U1 {
+   const signed f0 : 10;
+   uint64_t  f1;
+   int16_t  f2;
+   int32_t  f3;
