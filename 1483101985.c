95d1c1a4d7888f590f05fe287eb44e3475d3aacd
saisi <saisi@users.noreply.github.com>
2016-12-30 12:46:25 +0000

    1483101985 Amorete

---
+++ 1483101985.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      803060163
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint8_t  f0;
+   volatile int16_t  f1;
+   unsigned f2 : 7;
+   const signed f3 : 13;
+   unsigned f4 : 11;
+   unsigned f5 : 2;
+   const unsigned f6 : 30;
+   int32_t  f7;
+   unsigned f8 : 30;
