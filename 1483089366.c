a45500fcae1f91b08d32760a02649a364f32e694
saisi <saisi@users.noreply.github.com>
2016-12-30 09:16:06 +0000

    1483089366 abridgement

---
+++ 1483089366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3769119326
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = 0x1738AB659D568D52LL;
+static uint64_t g_17 = 1UL;
+static uint16_t g_26 = 65535UL;
+static uint16_t g_34 = 0xEB81L;
+static uint16_t *g_33 = &g_34;
+static uint32_t g_59[4] = {7UL,7UL,7UL,7UL};
+static int16_t g_79 = 1L;
+static uint32_t g_98 = 1UL;
+static uint32_t g_102 = 5UL;
