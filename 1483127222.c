49394b8f710892ebdb282610bcf012aab69b80e3
saisi <saisi@users.noreply.github.com>
2016-12-30 19:47:04 +0000

    1483127222 Balopticon

---
+++ 1483127222.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3127670584
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   const int8_t * const  f1;
+   volatile int64_t  f2;
+};
+
+union U1 {
+   const volatile unsigned f0 : 16;
+   const uint16_t  f1;
+   int64_t  f2;
