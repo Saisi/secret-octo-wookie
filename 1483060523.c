daeb3899b2f392eede4d2bb605065f6caf2a8a9a
saisi <saisi@users.noreply.github.com>
2016-12-30 01:15:23 +0000

    1483060523 baccare

---
+++ 1483060523.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2163967305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   int32_t  f1;
+   int8_t  f2;
+   volatile int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[5] = {0x4AA0D722L,0x4AA0D722L,0x4AA0D722L,0x4AA0D722L,0x4AA0D722L};
+static union U0 g_17 = {0};/* VOLATILE GLOBAL g_17 */
