927402a08951e35365620ce0d29858e346e41f13
saisi <saisi@users.noreply.github.com>
2016-12-30 14:46:35 +0000

    1483109195 accumb

---
+++ 1483109195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2109146007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_17 = 0xD6923607L;
+static uint16_t g_44 = 0x22A1L;
+static uint8_t g_52[6][4] = {{250UL,0x88L,250UL,250UL},{0x88L,0x88L,253UL,0x88L},{0x88L,250UL,250UL,0x88L},{250UL,0x88L,250UL,250UL},{0x88L,0x88L,253UL,0x88L},{0x88L,250UL,250UL,0x88L}};
+static uint64_t g_57 = 0x177FD8B26DE0D615LL;
+static uint16_t *g_66[3] = {&g_44,&g_44,&g_44};
+static uint16_t *g_68 = &g_44;
+static int8_t g_70 = 0xCDL;
+static int32_t g_74 = 0L;
+static uint32_t g_75 = 3UL;
