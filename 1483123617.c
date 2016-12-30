64b364123a7f6db3b96aa507463e8d56398433c6
saisi <saisi@users.noreply.github.com>
2016-12-30 18:46:57 +0000

    1483123617 archurger

---
+++ 1483123617.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2042772164
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x3707822EL;
+static int32_t g_10 = (-5L);
+static int32_t g_11 = (-10L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
