02ad01d53e53d624cfc1b929952cee91983da3fe
saisi <saisi@users.noreply.github.com>
2016-12-30 09:46:09 +0000

    1483091169 antidynastic

---
+++ 1483091169.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1728809498
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int64_t  f0;
+   uint16_t  f1;
+   int8_t  f2;
+   const volatile uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = (-9L);
+static int32_t g_24 = 0xD5A519D8L;
