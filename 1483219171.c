beb65e9f1e01ca999f7af315df52135f6c5c8a61
saisi <saisi@users.noreply.github.com>
2016-12-31 21:19:31 +0000

    1483219171 Addia

---
+++ 1483219171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      479287510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[7] = {0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L,0xF6AF1124L};
+static int32_t g_3 = 3L;
+static uint32_t g_17 = 0x091DF82DL;
+static int32_t g_23[4][10][1] = {{{(-1L)},{(-1L)},{(-4L)},{0xA22DB785L},{0xCAFA32FDL},{(-4L)},{0xCAFA32FDL},{0xA22DB785L},{(-4L)},{(-1L)}},{{(-1L)},{(-4L)},{0xA22DB785L},{0xCAFA32FDL},{(-4L)},{0xCAFA32FDL},{0xA22DB785L},{(-4L)},{(-1L)},{(-1L)}},{{(-4L)},{0xA22DB785L},{0xCAFA32FDL},{(-4L)},{0xCAFA32FDL},{0xA22DB785L},{(-4L)},{(-1L)},{(-1L)},{(-4L)}},{{0xA22DB785L},{0xCAFA32FDL},{(-4L)},{0xCAFA32FDL},{0xA22DB785L},{(-4L)},{(-1L)},{(-1L)},{(-4L)},{(-4L)}}};
+static int32_t * volatile g_22 = &g_23[2][9][0];/* VOLATILE GLOBAL g_22 */
+static uint32_t g_81 = 2UL;
+static int32_t *g_126 = (void*)0;
+static int32_t ** volatile g_125 = &g_126;/* VOLATILE GLOBAL g_125 */
+static uint16_t g_137 = 65535UL;
