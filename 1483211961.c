a954e349c229e9685eb12035a5d6172d0d04438a
saisi <saisi@users.noreply.github.com>
2016-12-31 19:19:21 +0000

    1483211961 astrophel

---
+++ 1483211961.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1336519783
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t * const  volatile g_2 = (void*)0;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4 = 0x6D7BE7E5L;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
