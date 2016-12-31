f540e3e4260572688cd5468c8db74013103caefa
saisi <saisi@users.noreply.github.com>
2016-12-31 00:17:35 +0000

    1483143455 bacchanalization

---
+++ 1483143455.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2915593007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int64_t g_17[2][3] = {{0x55A63A74ADF29855LL,(-1L),0x55A63A74ADF29855LL},{0x55A63A74ADF29855LL,(-1L),0x55A63A74ADF29855LL}};
+static volatile uint16_t g_18[4][9] = {{1UL,0x9A6FL,0UL,0UL,0x9A6FL,1UL,3UL,0x9A6FL,3UL},{65528UL,65533UL,65527UL,65527UL,65533UL,65528UL,0x660DL,65533UL,0x660DL},{1UL,0x9A6FL,0UL,0UL,0x9A6FL,1UL,3UL,0x9A6FL,3UL},{65528UL,65533UL,65527UL,65527UL,65533UL,65528UL,0x660DL,65533UL,0x660DL}};
+static int8_t g_21 = 0x42L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
