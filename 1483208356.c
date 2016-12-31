7a84d43ee89261a0e4df3923c2f077a46c75b50e
saisi <saisi@users.noreply.github.com>
2016-12-31 18:19:17 +0000

    1483208356 annection

---
+++ 1483208356.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2799271194
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int32_t  f1;
+   uint32_t  f2;
+   int32_t  f3;
+   int16_t  f4;
+   int32_t  f5;
+   int8_t  f6;
+   int64_t  f7;
+};
