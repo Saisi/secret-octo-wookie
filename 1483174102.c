f6b8d3686c15c7345c28d85cbb3775baa9ee2076
saisi <saisi@users.noreply.github.com>
2016-12-31 08:48:24 +0000

    1483174102 adenoidism

---
+++ 1483174102.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2953190119
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int32_t  f0;
+   volatile unsigned f1 : 23;
+   const signed f2 : 17;
+   unsigned f3 : 17;
+   signed f4 : 20;
+};
+
+union U1 {
+   uint32_t  f0;
