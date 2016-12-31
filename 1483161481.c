dee0565bdfc90e71feeb73bee047baa169e5a52b
saisi <saisi@users.noreply.github.com>
2016-12-31 05:18:02 +0000

    1483161481 ambulatory

---
+++ 1483161481.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1555806162
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_8 = 0xBD2AL;/* VOLATILE GLOBAL g_8 */
+static int32_t g_10 = 0L;
+static int32_t * volatile g_9[9][7][4] = {{{&g_10,(void*)0,&g_10,&g_10},{(void*)0,(void*)0,&g_10,&g_10},{&g_10,&g_10,&g_10,(void*)0},{&g_10,&g_10,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{&g_10,(void*)0,&g_10,&g_10},{&g_10,(void*)0,&g_10,&g_10}},{{&g_10,(void*)0,(void*)0,&g_10},{&g_10,(void*)0,(void*)0,&g_10},{(void*)0,&g_10,(void*)0,(void*)0},{&g_10,&g_10,&g_10,(void*)0},{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,(void*)0,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{(void*)0,&g_10,&g_10,&g_10},{&g_10,&g_10,(void*)0,(void*)0},{(void*)0,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{(void*)0,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,(void*)0,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{(void*)0,&g_10,&g_10,&g_10},{&g_10,&g_10,(void*)0,(void*)0},{(void*)0,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{(void*)0,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,(void*)0,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{(void*)0,&g_10,&g_10,&g_10},{&g_10,&g_10,(void*)0,(void*)0},{(void*)0,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{(void*)0,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,(void*)0,(void*)0},{&g_10,&g_10,&g_10,&g_10}},{{&g_10,&g_10,&g_10,(void*)0},{&g_10,(void*)0,&g_10,(void*)0},{&g_10,&g_10,&g_10,&g_10},{(void*)0,&g_10,&g_10,&g_10},{&g_10,(void*)0,&g_10,&g_10},{&g_10,&g_10,&g_10,&g_10},{&g_10,&g_10,(void*)0,&g_10}}};
+static uint32_t g_13 = 0xCA69919CL;
+static uint32_t g_42 = 0x96DAD8BEL;
+static int32_t g_64 = 0x81002A95L;
+static int32_t g_66 = (-4L);
+static uint32_t g_83[3][9][1] = {{{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL}},{{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL}},{{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL}}};
+static int32_t g_98[3][3][10] = {{{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L}},{{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L}},{{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L},{3L,0xB81C74F2L,0x072C4636L,(-1L),0x072C4636L,0xB81C74F2L,3L,0xF7977D32L,(-5L),0xF7977D32L}}};
