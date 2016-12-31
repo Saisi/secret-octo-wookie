6531e772deeaa2728a9eeee77d0e62fee345eb75
saisi <saisi@users.noreply.github.com>
2016-12-31 04:47:59 +0000

    1483159678 arteriomalacia

---
+++ 1483159678.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      632848451
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   signed f1 : 1;
+   volatile unsigned f2 : 24;
+   const volatile signed f3 : 15;
+   const signed f4 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC96C7979L;
