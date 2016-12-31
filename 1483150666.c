33f5a64c8e5de4ca65cbe28d34d4a879ba53ffa4
saisi <saisi@users.noreply.github.com>
2016-12-31 02:17:46 +0000

    1483150666 balks

---
+++ 1483150666.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4202173976
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 26;
+   uint64_t  f1;
+   int8_t  f2;
+   uint64_t  f3;
+   uint8_t  f4;
+};
+#pragma pack(pop)
