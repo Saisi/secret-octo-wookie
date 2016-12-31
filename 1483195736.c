7c575f0e6514b8681be15d5e715db75186bfa39d
saisi <saisi@users.noreply.github.com>
2016-12-31 14:48:57 +0000

    1483195736 Alces

---
+++ 1483195736.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      498401689
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile uint32_t  f1;
+   volatile int64_t  f2;
+   const uint32_t  f3;
+   unsigned f4 : 25;
+};
+
+union U1 {
+   const uint16_t  f0;
