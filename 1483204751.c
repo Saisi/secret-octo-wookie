13d278e86b06bbc3b5cdbb76430a98e1205fa51d
saisi <saisi@users.noreply.github.com>
2016-12-31 17:19:11 +0000

    1483204751 atophan

---
+++ 1483204751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      721899181
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint8_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x3FCB50BFL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xDA0E60F9L;
+static volatile int32_t g_61 = 0xA9994938L;/* VOLATILE GLOBAL g_61 */
