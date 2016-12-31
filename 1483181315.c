ce125454e22efdbc9aac234f6f0cde9e7d6b10fe
saisi <saisi@users.noreply.github.com>
2016-12-31 10:48:35 +0000

    1483181315 axonophorous

---
+++ 1483181315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1102651904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+   volatile signed f1 : 11;
+   const volatile signed f2 : 1;
+   signed f3 : 9;
+   const int32_t  f4;
+   signed f5 : 30;
+   signed f6 : 7;
+};
+
