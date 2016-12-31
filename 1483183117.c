951c22b4faf78713d04ed7ca176a41c41880aad4
saisi <saisi@users.noreply.github.com>
2016-12-31 11:18:38 +0000

    1483183117 aurigal

---
+++ 1483183117.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2300537256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   volatile uint32_t  f1;
+   uint32_t  f2;
+   int16_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[8] = {2L,2L,2L,2L,2L,2L,2L,2L};
