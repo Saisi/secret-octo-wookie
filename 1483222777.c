841878ef456bfc460c556879c1d534fd30296a9f
saisi <saisi@users.noreply.github.com>
2016-12-31 22:19:37 +0000

    1483222777 Acroceridae

---
+++ 1483222777.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      359454227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   uint32_t  f1;
+   int64_t  f2;
+   volatile uint16_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xD94543BEL;/* VOLATILE GLOBAL g_3 */
