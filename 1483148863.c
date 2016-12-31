630828f22f0440be7878575485836a1b636d7bfc
saisi <saisi@users.noreply.github.com>
2016-12-31 01:47:44 +0000

    1483148863 alburnum

---
+++ 1483148863.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3136567186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_3[1] = {18446744073709551610UL};
+static int32_t g_22 = 0x160CBEB9L;
+static int32_t *g_21 = &g_22;
+static int32_t * volatile g_31 = &g_22;/* VOLATILE GLOBAL g_31 */
+static int32_t * volatile *g_30[2][9] = {{&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31},{&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31}};
+static uint32_t g_59 = 0x70AACC70L;
+static uint8_t g_71 = 0x10L;
+static int16_t g_73 = (-1L);
+static int32_t *g_124 = (void*)0;
