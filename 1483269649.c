6525ba0c7d0e76392aeed7cd954f3b5d179d0f2a
saisi <saisi@users.noreply.github.com>
2017-01-01 11:20:50 +0000

    1483269649 arbusterin

---
+++ 1483269649.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1624559334
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 1;
+   unsigned f1 : 18;
+   const unsigned f2 : 12;
+   const signed f3 : 8;
+   int32_t  f4;
+   signed f5 : 17;
+   signed f6 : 14;
