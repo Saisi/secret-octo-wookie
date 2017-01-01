21f53281d73fe21f5a48956dd8b6dccfd6bc271b
saisi <saisi@users.noreply.github.com>
2017-01-01 05:50:19 +0000

    1483249818 Auburta

---
+++ 1483249818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      179572607
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   signed f1 : 29;
+   signed f2 : 12;
+   volatile unsigned f3 : 7;
+   volatile unsigned f4 : 28;
+   unsigned f5 : 5;
+   const volatile signed f6 : 16;
+   unsigned f7 : 3;
+   signed f8 : 26;
