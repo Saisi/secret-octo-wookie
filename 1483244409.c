41a5beb3f00b348c6671f26c144847ed6298598e
saisi <saisi@users.noreply.github.com>
2017-01-01 04:20:09 +0000

    1483244409 airlessness

---
+++ 1483244409.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1526309617
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   signed f1 : 18;
+   unsigned f2 : 13;
+   volatile unsigned f3 : 4;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+   signed f1 : 1;
