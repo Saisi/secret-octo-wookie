5ebc86f7d2537991b5c5b0227e3fc3effaf9d7ef
saisi <saisi@users.noreply.github.com>
2017-01-01 13:21:00 +0000

    1483276860 anisopodal

---
+++ 1483276860.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4118117190
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   const unsigned f1 : 2;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
