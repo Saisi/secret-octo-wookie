132dc89243a75e4b1a0b5aa015ce62f01cbf8f5a
saisi <saisi@users.noreply.github.com>
2017-01-01 19:21:33 +0000

    1483298492 antiexpansion

---
+++ 1483298492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2992919732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+   volatile int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
