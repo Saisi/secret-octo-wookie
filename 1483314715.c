6467e8f4f030cf24256b6cb2ea9acb45d045c155
saisi <saisi@users.noreply.github.com>
2017-01-01 23:51:55 +0000

    1483314715 airbills

---
+++ 1483314715.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      822571808
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+};
+
+union U1 {
+   uint32_t  f0;
+   int8_t * const  f1;
+   uint32_t  f2;
+};
+
