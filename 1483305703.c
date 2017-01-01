a1995bb4f6d48a462f4795d43ada86e060793f18
saisi <saisi@users.noreply.github.com>
2017-01-01 21:21:43 +0000

    1483305703 Attical

---
+++ 1483305703.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      210531979
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+};
+
+struct S1 {
+   const volatile signed f0 : 27;
+   signed f1 : 19;
+   signed f2 : 14;
+   unsigned f3 : 4;
+};
