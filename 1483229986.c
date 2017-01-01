38d42a1b57f216550cceadb238e15a00bf453ac1
saisi <saisi@users.noreply.github.com>
2017-01-01 00:19:47 +0000

    1483229986 arteriometer

---
+++ 1483229986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1063248125
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[6][4] = {{1L,0x871D976FL,0x871D976FL,1L},{0x871D976FL,1L,0L,0x7BC75286L},{0x871D976FL,0L,0x871D976FL,0x0BB743D8L},{1L,0x7BC75286L,0x0BB743D8L,0x0BB743D8L},{0L,0L,(-10L),0x7BC75286L},{0x7BC75286L,1L,(-10L),1L}};
+static int32_t g_12 = 0xF5A8B060L;
+static int32_t g_13 = 0x39C7C6F1L;
+static int32_t g_14 = 0x9193C1DEL;
+static uint16_t g_15 = 0x77EBL;
+static const uint32_t g_41 = 4UL;
+static uint32_t g_48 = 0x587697ABL;
+static int32_t *g_50[6][9] = {{&g_13,&g_13,&g_13,(void*)0,&g_3[3][2],(void*)0,&g_13,&g_13,&g_13},{(void*)0,(void*)0,&g_3[3][2],&g_3[3][2],&g_13,&g_13,&g_3[0][0],&g_13,&g_13},{&g_13,&g_13,&g_13,&g_13,&g_13,&g_13,&g_13,(void*)0,&g_3[3][2]},{(void*)0,&g_13,&g_3[3][3],&g_3[0][0],&g_3[0][0],&g_3[3][3],&g_13,(void*)0,&g_13},{&g_13,&g_13,&g_3[3][2],&g_3[3][2],&g_13,(void*)0,(void*)0,&g_13,&g_3[3][2]},{(void*)0,(void*)0,(void*)0,&g_13,&g_13,&g_3[0][0],&g_3[3][2],&g_13,&g_13}};
