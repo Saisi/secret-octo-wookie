0122af9ec0f04fb342a845a6c8c9b2d7389087f5
saisi <saisi@users.noreply.github.com>
2017-01-01 16:21:16 +0000

    1483287675 antiburglar

---
+++ 1483287675.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2080774762
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 3;
+   signed f1 : 12;
+   const unsigned f2 : 30;
+   unsigned f3 : 18;
+};
+#pragma pack(pop)
+
