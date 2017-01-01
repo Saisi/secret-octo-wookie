897ea1e2898c0ad7e9ed819fe5803ee0deffdbab
saisi <saisi@users.noreply.github.com>
2017-01-01 14:51:09 +0000

    1483282269 antistatist

---
+++ 1483282269.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1236172741
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x7BF0B96CL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 3L;
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0xC4AB274BL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xEDCAD208L;
+static int32_t g_21 = 0x8BC0D133L;
+static int32_t g_29 = (-1L);
+static int32_t * volatile g_28 = &g_29;/* VOLATILE GLOBAL g_28 */
+static uint32_t g_85 = 0x69233C9EL;
