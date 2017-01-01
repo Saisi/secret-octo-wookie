a07f645ccf515779d93764c56d4c49f7b905eb66
saisi <saisi@users.noreply.github.com>
2017-01-01 12:50:58 +0000

    1483275058 aspca

---
+++ 1483275058.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3505445999
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5B57FD54L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[9] = {0xB1C2A2C7L,0x5F5C184CL,0xB1C2A2C7L,0xB1C2A2C7L,0x5F5C184CL,0xB1C2A2C7L,0xB1C2A2C7L,0x5F5C184CL,0xB1C2A2C7L};
+static int32_t g_6 = 0x9DFCF09CL;
+static int32_t g_10 = 4L;
+static int32_t g_50 = 0xD2214C25L;
+static int32_t g_62 = 6L;
+static int32_t g_73 = 0L;
+static int32_t *g_77 = &g_6;
+static int32_t ** volatile g_76 = &g_77;/* VOLATILE GLOBAL g_76 */
