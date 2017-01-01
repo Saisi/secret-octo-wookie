a418336edbb3b3a3e01c55db5c0a0bd1b1afe302
saisi <saisi@users.noreply.github.com>
2017-01-01 19:51:36 +0000

    1483300295 arsenic

---
+++ 1483300295.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      314714642
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+union U1 {
+   const int8_t  f0;
+   volatile uint64_t  f1;
+   volatile uint32_t  f2;
+};
+
