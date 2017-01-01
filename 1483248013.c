ddfa764bfbf0a9e0764f12aba57e280f4cc4c524
saisi <saisi@users.noreply.github.com>
2017-01-01 05:20:16 +0000

    1483248013 apostrophus

---
+++ 1483248013.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2490057261
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   signed f1 : 24;
+   signed f2 : 27;
+   unsigned f3 : 26;
+   signed f4 : 3;
+   signed f5 : 16;
+};
