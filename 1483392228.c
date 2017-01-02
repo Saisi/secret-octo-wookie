0beb94e4b6ff250373acef5f4182ad1b82e78b64
saisi <saisi@users.noreply.github.com>
2017-01-02 21:23:48 +0000

    1483392228 Amblyopsis

---
+++ 1483392228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2991010047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x3DF75DCEL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0L;
+static int32_t g_21 = 0xF8EE1D11L;
+static int64_t g_35[3] = {0x2FC257A10DD66408LL,0x2FC257A10DD66408LL,0x2FC257A10DD66408LL};
+static int32_t g_75 = (-3L);
+static int8_t g_77 = 0xFFL;
+static int64_t g_80 = 9L;
+static uint64_t g_92 = 0x35443C7F34FA629ALL;
