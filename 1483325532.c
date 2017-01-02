cef91583f54f6097f78777ff3cabe7540f584e58
saisi <saisi@users.noreply.github.com>
2017-01-02 02:52:13 +0000

    1483325532 anandria

---
+++ 1483325532.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3154485879
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 0xE5F3L;
