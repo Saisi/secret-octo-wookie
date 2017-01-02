7b98538d16bfa951c77f91fe5aa37c6d5f2b21eb
saisi <saisi@users.noreply.github.com>
2017-01-02 13:53:12 +0000

    1483365192 arsenophagy

---
+++ 1483365192.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      403624031
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile unsigned f1 : 13;
+   volatile int8_t * const  f2;
+   unsigned f3 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6[6][5] = {{(-1L),0x4BEDCC59L,(-1L),(-1L),0x4BEDCC59L},{(-3L),0x7CAD7D71L,0xEF889A27L,1L,0xEF889A27L},{0x4BEDCC59L,0x4BEDCC59L,0xE7308F47L,0x4BEDCC59L,0x4BEDCC59L},{0xEF889A27L,1L,0xEF889A27L,0x7CAD7D71L,(-3L)},{0x4BEDCC59L,(-1L),(-1L),0x4BEDCC59L,(-1L)},{(-3L),1L,0xFF44CACFL,1L,(-3L)}};
+static int8_t g_37 = 0x04L;
