98be743b6612a56d8425ef06b953c133b67058c5
saisi <saisi@users.noreply.github.com>
2017-01-02 11:22:58 +0000

    1483356178 anagrams

---
+++ 1483356178.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      958814936
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[1] = {0x0ADE2167L};
+static volatile int32_t g_4 = 6L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xA756A336L;
+static uint32_t g_75 = 0x39EFD913L;
+static int32_t g_78 = 0x4954824FL;
+static int16_t g_99 = (-1L);
+static int8_t g_101 = 0xDEL;
+static int32_t g_103[8] = {0xC049D9BBL,8L,0xC049D9BBL,8L,0xC049D9BBL,8L,0xC049D9BBL,8L};
