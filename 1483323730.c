d6ab3c7e026e2c50688319b9c7395cf52a92e6d7
saisi <saisi@users.noreply.github.com>
2017-01-02 02:22:10 +0000

    1483323730 acromyotonus

---
+++ 1483323730.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1420926632
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint16_t  f0;
+   unsigned f1 : 15;
+   volatile signed f2 : 7;
+   unsigned f3 : 13;
+   signed f4 : 30;
+   volatile unsigned f5 : 18;
+   volatile unsigned f6 : 11;
