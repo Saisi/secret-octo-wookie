5d20741ac06d882492e9698f6a50c5b62d9f3dce
saisi <saisi@users.noreply.github.com>
2017-01-02 16:53:26 +0000

    1483376005 autotrophic

---
+++ 1483376005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3156532208
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const signed f1 : 3;
+   const volatile signed f2 : 25;
+};
+
+union U1 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   uint8_t  f2;
