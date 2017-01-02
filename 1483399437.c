fd832f34e8cfa48e4b64e08f7bc2e281686336b8
saisi <saisi@users.noreply.github.com>
2017-01-02 23:23:57 +0000

    1483399437 atrophy

---
+++ 1483399437.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      203389206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const uint8_t  f1;
+   volatile int32_t  f2;
+   unsigned f3 : 16;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
