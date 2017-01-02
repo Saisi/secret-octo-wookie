ed155e1f7c5e6ff78e9239259ea657631d1a8f6e
saisi <saisi@users.noreply.github.com>
2017-01-02 18:23:34 +0000

    1483381413 anaglyphical

---
+++ 1483381413.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2531779399
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   const signed f1 : 17;
+   const volatile unsigned f2 : 16;
+   volatile signed f3 : 27;
+   volatile unsigned : 0;
+};
+
+struct S1 {
+   const signed f0 : 29;
