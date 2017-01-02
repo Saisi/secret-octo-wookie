25663fb7d2fdee473c17455add0219d800274c22
saisi <saisi@users.noreply.github.com>
2017-01-02 21:53:50 +0000

    1483394030 Astylospongia

---
+++ 1483394030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3815949787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x8987F2FBL;
+static int32_t *g_2 = &g_3;
+static int32_t ** volatile g_4 = (void*)0;/* VOLATILE GLOBAL g_4 */
+static int32_t ** volatile g_5 = &g_2;/* VOLATILE GLOBAL g_5 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
