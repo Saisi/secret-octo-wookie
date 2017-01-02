4a621a87cd3ca7dcae16ca51b30b532aef015920
saisi <saisi@users.noreply.github.com>
2017-01-02 09:52:50 +0000

    1483350769 anticapitalistic

---
+++ 1483350769.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3380528938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 3;
+   signed f1 : 12;
+   unsigned f2 : 30;
+   const signed f3 : 18;
+   signed f4 : 25;
+   const unsigned f5 : 12;
+   signed f6 : 26;
