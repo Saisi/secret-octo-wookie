7c3e0eefe6a07a7315a5b50d65abca047b4b0258
saisi <saisi@users.noreply.github.com>
2017-01-02 00:52:01 +0000

    1483318320 bambini

---
+++ 1483318320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2982906679
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint8_t  f0;
+   signed f1 : 31;
+   const unsigned f2 : 25;
+   signed f3 : 25;
+   signed f4 : 28;
+};
+#pragma pack(pop)
