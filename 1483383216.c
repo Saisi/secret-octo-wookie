42e12f9bd7d34b2344ef688889224e88624094e1
saisi <saisi@users.noreply.github.com>
2017-01-02 18:53:36 +0000

    1483383216 arpeggios

---
+++ 1483383216.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      118908703
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   unsigned f1 : 13;
+   signed f2 : 28;
+   const int8_t  f3;
+   signed : 0;
+   signed f4 : 14;
+   signed f5 : 25;
