abb00268ae33106ac2fd14d5b73ff80cadf766b6
saisi <saisi@users.noreply.github.com>
2017-01-02 17:53:31 +0000

    1483379610 animalivore

---
+++ 1483379610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      226958886
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   const unsigned f1 : 30;
+   uint8_t  f2;
+   uint64_t  f3;
+   const volatile uint16_t  f4;
+   int32_t  f5;
+   uint8_t  f6;
+   const int32_t  f7;
+};
