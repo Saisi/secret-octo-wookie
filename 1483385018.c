dedbd20f5a37ed0ddd1c21c992a16493a921da0f
saisi <saisi@users.noreply.github.com>
2017-01-02 19:23:39 +0000

    1483385018 armigero

---
+++ 1483385018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      679760063
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+};
+
+struct S1 {
+   volatile unsigned f0 : 13;
+   int16_t  f1;
+   struct S0  f2;
+   const signed f3 : 29;
+   int16_t  f4;
