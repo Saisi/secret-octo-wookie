a14a05c079c250a60b5e8d08528fdace5bef8d27
saisi <saisi@users.noreply.github.com>
2017-01-03 18:55:48 +0000

    1483469748 autaesthesy

---
+++ 1483469748.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      800646524
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint64_t  f0;
+   uint16_t  f1;
+   uint64_t  f2;
+   volatile uint32_t  f3;
+   volatile uint8_t  f4;
+   uint32_t  f5;
+   uint64_t  f6;
