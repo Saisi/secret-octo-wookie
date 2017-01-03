c77cceab42401035ef68cbce1ef59f9f0d5e3702
saisi <saisi@users.noreply.github.com>
2017-01-03 16:25:33 +0000

    1483460733 amicronucleate

---
+++ 1483460733.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2838103449
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 11;
+   volatile signed f1 : 22;
+   signed f2 : 1;
+   volatile signed f3 : 8;
+   const unsigned f4 : 25;
+   signed f5 : 14;
+   signed f6 : 21;
+   const unsigned f7 : 20;
+   volatile unsigned f8 : 7;
