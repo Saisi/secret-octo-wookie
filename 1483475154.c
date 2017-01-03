5e34331e60c013ba32b385e10c3e517616c743d2
saisi <saisi@users.noreply.github.com>
2017-01-03 20:25:56 +0000

    1483475154 admiralships

---
+++ 1483475154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2086167872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_7 = 1UL;/* VOLATILE GLOBAL g_7 */
+static int16_t g_8 = 0x77ABL;
+static int32_t g_28 = 0L;
+static int32_t g_29 = 0xA7790412L;
+static int16_t g_42[2] = {(-8L),(-8L)};
+static uint32_t g_79[1] = {0x347A64A0L};
+static int32_t g_83 = (-10L);
+static uint8_t g_95 = 249UL;
+static uint64_t g_96 = 0UL;
