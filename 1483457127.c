8f8ece82cbe5c199c307575620745bacd9a59e1b
saisi <saisi@users.noreply.github.com>
2017-01-03 15:25:28 +0000

    1483457127 alliterates

---
+++ 1483457127.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3242566535
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static int32_t g_10 = 0xBF2465CDL;
+static int16_t g_47 = 1L;
+static int16_t g_48 = (-1L);
+static uint8_t g_49[8] = {0xE1L,0xE1L,0xE1L,0xE1L,0xE1L,0xE1L,0xE1L,0xE1L};
