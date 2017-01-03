e6d54506582b52c7dae02d9e765b6215ef0e2669
saisi <saisi@users.noreply.github.com>
2017-01-03 12:55:15 +0000

    1483448114 Autobasisii

---
+++ 1483448114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3074780938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x43CDF98AL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static int32_t g_13 = 7L;
+static uint16_t g_52 = 0x7A33L;
