7f3b98b2ed502d25450171800d8fd60270b1446e
saisi <saisi@users.noreply.github.com>
2017-01-03 14:25:22 +0000

    1483453522 arylate

---
+++ 1483453522.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1888579398
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[2] = {0x97A4A12BL,0x97A4A12BL};
+static int32_t * volatile g_3 = &g_4[0];/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
