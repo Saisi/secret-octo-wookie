1cb372a287f988beac3058ce460e08963fd6c541
saisi <saisi@users.noreply.github.com>
2017-01-03 03:54:24 +0000

    1483415663 atwirl

---
+++ 1483415663.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1168010897
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   volatile uint64_t  f1;
+   volatile int32_t  f2;
+   const int64_t  f3;
+   uint64_t  f4;
+   const volatile uint8_t  f5;
+   int8_t  f6;
