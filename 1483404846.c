bd2f0c53491e9f51a3704c4fa1aa03fa9d1a7180
saisi <saisi@users.noreply.github.com>
2017-01-03 00:54:08 +0000

    1483404846 ablepharon

---
+++ 1483404846.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2247884903
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 18;
+   int32_t  f1;
+   uint8_t  f2;
+   int8_t * f3;
+};
+
+union U1 {
+   int64_t  f0;
+   volatile signed f1 : 4;
