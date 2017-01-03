f1cabe6a83c84a57505bf55465936e960af6990d
saisi <saisi@users.noreply.github.com>
2017-01-03 15:55:31 +0000

    1483458930 azlactone

---
+++ 1483458930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2004931226
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 27;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_3[2] = {0xD149FEBB6748F69FLL,0xD149FEBB6748F69FLL};
+static int16_t g_26 = (-8L);
+static int32_t g_27 = (-1L);
+static int32_t g_37 = 0xFBB9CF7DL;
