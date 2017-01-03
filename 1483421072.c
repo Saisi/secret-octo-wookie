f97e9284d83f0055c3685a939a5d558c44231bb7
saisi <saisi@users.noreply.github.com>
2017-01-03 05:24:32 +0000

    1483421072 backhandedness

---
+++ 1483421072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2491086073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   uint8_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 1UL;
+static int8_t g_9 = 0xCAL;
+static int32_t g_11 = (-1L);
+
