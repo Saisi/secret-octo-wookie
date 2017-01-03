1e69e90237f60dba6286fc79f84fd10aef0f54a4
saisi <saisi@users.noreply.github.com>
2017-01-03 02:54:18 +0000

    1483412058 aeroplaner

---
+++ 1483412058.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2933753443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile int16_t  f1;
+   volatile signed f2 : 23;
+   volatile uint16_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0L;
