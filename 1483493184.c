9a7661a3deca39dd56ace8d391f5c363913c4fcd
saisi <saisi@users.noreply.github.com>
2017-01-04 01:26:24 +0000

    1483493184 Ataentsic

---
+++ 1483493184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      407996998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint8_t  f1;
+   uint8_t  f2;
+};
+
+union U1 {
+   volatile signed f0 : 1;
+   int8_t * volatile  f1;
+   uint16_t  f2;
