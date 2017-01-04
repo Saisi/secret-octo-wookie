ba805bf919cd575b0709da8c3bee6b54dc60a062
saisi <saisi@users.noreply.github.com>
2017-01-04 09:57:10 +0000

    1483523830 Audubon

---
+++ 1483523830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      12988512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+};
+
+union U1 {
+   int32_t  f0;
+   const uint16_t  f1;
+   volatile signed f2 : 10;
+   volatile uint16_t  f3;
+   const volatile uint64_t  f4;
