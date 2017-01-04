2552f6c04521b4c4935f72a91b9c6691a575079c
saisi <saisi@users.noreply.github.com>
2017-01-04 07:56:58 +0000

    1483516618 autoignition

---
+++ 1483516618.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4032891437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const signed f1 : 10;
+   volatile signed f2 : 24;
+   const uint32_t  f3;
+   volatile int16_t  f4;
+   uint64_t  f5;
+   int64_t  f6;
+   unsigned f7 : 15;
+   uint64_t  f8;
