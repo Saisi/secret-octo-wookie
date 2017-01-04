db84dda5d336b7bb2a5cffbd3ab1d74d163161c9
saisi <saisi@users.noreply.github.com>
2017-01-04 02:26:29 +0000

    1483496789 Aire

---
+++ 1483496789.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2642590620
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int16_t  f0;
+   signed f1 : 9;
+   const int32_t  f2;
+   signed f3 : 8;
+   unsigned f4 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xAA39ED08L;
