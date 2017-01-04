cb02a97922ad8369b4a0614f4b2e8e7dd6a31f79
saisi <saisi@users.noreply.github.com>
2017-01-04 11:27:18 +0000

    1483529237 antherozooid

---
+++ 1483529237.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2190105285
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x4A6847DFL;
+static volatile int32_t g_5 = 0xCA2CB51DL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[1][3] = {{0L,0L,0L}};
+static volatile int32_t g_7 = 1L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = (-3L);
+static uint32_t g_25 = 4294967289UL;
+static uint32_t g_49[2][5] = {{0xC3F90802L,4294967295UL,0xC3F90802L,4294967295UL,0xC3F90802L},{2UL,2UL,2UL,2UL,2UL}};
+static int16_t g_78 = 1L;
+static uint16_t g_79 = 0x2392L;
