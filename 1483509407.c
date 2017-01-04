037f9db13cb24748661a5f548f64a0237bf37902
saisi <saisi@users.noreply.github.com>
2017-01-04 05:56:47 +0000

    1483509407 backrests

---
+++ 1483509407.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2016314294
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2 = 1L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
