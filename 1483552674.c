581dee96abc81883ed30e4c5ec598605852746e5
saisi <saisi@users.noreply.github.com>
2017-01-04 17:57:54 +0000

    1483552674 Aria

---
+++ 1483552674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3352508342
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static volatile uint32_t g_9 = 4294967289UL;/* VOLATILE GLOBAL g_9 */
+static uint16_t g_12 = 0x4673L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
