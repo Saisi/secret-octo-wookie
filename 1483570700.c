f2c33929ab0254820a93bdce90703a1529fba419
saisi <saisi@users.noreply.github.com>
2017-01-04 22:58:21 +0000

    1483570700 bamboche

---
+++ 1483570700.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2334211728
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int16_t  f0;
+   int32_t  f1;
+   int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_11 = (-9L);
+static uint32_t g_43 = 4294967295UL;
+static int8_t g_46[2] = {0x95L,0x95L};
