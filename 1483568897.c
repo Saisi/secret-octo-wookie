250858ad09ad7e5a35def2d3d7c2030bca521050
saisi <saisi@users.noreply.github.com>
2017-01-04 22:28:18 +0000

    1483568897 agamogenetic

---
+++ 1483568897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1219428815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   signed f0 : 20;
+   int32_t  f1;
+   volatile int64_t  f2;
+   int32_t  f3;
