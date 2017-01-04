9619949bc460050bc07b4717c5e4f92bccb0bafa
saisi <saisi@users.noreply.github.com>
2017-01-04 20:58:09 +0000

    1483563489 aviculture

---
+++ 1483563489.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      893432739
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+   volatile int32_t  f2;
+   const int8_t  f3;
+   int16_t  f4;
+   volatile int8_t  f5;
+   uint32_t  f6;
+   const volatile int16_t  f7;
+   volatile int16_t  f8;
