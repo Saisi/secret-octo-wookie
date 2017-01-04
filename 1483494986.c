5deffc7298167df7e9add8408c7f36038bc41c32
saisi <saisi@users.noreply.github.com>
2017-01-04 01:56:26 +0000

    1483494986 bandspreading

---
+++ 1483494986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1099795820
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 4;
+   unsigned f1 : 9;
+   unsigned f2 : 7;
+   volatile signed f3 : 23;
+   const unsigned f4 : 10;
+   unsigned f5 : 25;
+   volatile unsigned f6 : 19;
