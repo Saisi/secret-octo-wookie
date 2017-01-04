ed9ae11658618ce5fd06ace185e6dc10397df03f
saisi <saisi@users.noreply.github.com>
2017-01-04 08:27:02 +0000

    1483518421 adipopexic

---
+++ 1483518421.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1625402102
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 9;
+   const uint64_t  f1;
+   const int8_t * f2;
+   int32_t  f3;
+};
+
+union U1 {
+   int32_t  f0;
+};
