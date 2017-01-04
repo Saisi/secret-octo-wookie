3c2b1e2dc74e33eabbc725c90bed8230e1504f0a
saisi <saisi@users.noreply.github.com>
2017-01-04 15:57:41 +0000

    1483545461 antiphrases

---
+++ 1483545461.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      526212591
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = 0x5925EF2CL;
+static uint64_t g_24 = 1UL;
+static uint64_t *g_29 = (void*)0;
+static int32_t g_31 = 0x855EEC80L;
+static int32_t g_44 = (-1L);
+static uint64_t g_48 = 0x9AE65C3421AD7BA9LL;
+static volatile uint32_t g_57 = 18446744073709551615UL;/* VOLATILE GLOBAL g_57 */
+static uint16_t g_72[10] = {0x248CL,0x248CL,0x248CL,0x248CL,0x248CL,0x248CL,0x248CL,0x248CL,0x248CL,0x248CL};
+static int8_t g_76 = 1L;
