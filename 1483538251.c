cf6beb41347ec58542bbb7c2b974c846a696adf4
saisi <saisi@users.noreply.github.com>
2017-01-04 13:57:31 +0000

    1483538251 assertor

---
+++ 1483538251.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      536117730
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x6F2EE76FL;
+static int8_t g_18 = 0x14L;
+static int8_t g_58 = 0x8AL;
+static int32_t *g_62 = (void*)0;
+static int32_t g_67[1][6] = {{8L,8L,8L,8L,8L,8L}};
