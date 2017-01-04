d0182cd1d115136f76c6eb84f6c303986a78f594
saisi <saisi@users.noreply.github.com>
2017-01-04 04:56:42 +0000

    1483505802 alcaide

---
+++ 1483505802.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      996699006
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x834504CDL;
+static int32_t * const  volatile g_3 = &g_2;/* VOLATILE GLOBAL g_3 */
+static volatile uint16_t g_6 = 65535UL;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
