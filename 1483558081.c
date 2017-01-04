5be3850a608bfb6be35e83dd3ac84f7a61678b8e
saisi <saisi@users.noreply.github.com>
2017-01-04 19:28:01 +0000

    1483558081 academicism

---
+++ 1483558081.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      583390444
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x9AC70C30L;
+static int64_t g_6 = (-1L);
+static int32_t g_7 = 0x3B7BEDEDL;
+static uint8_t g_8 = 0x3EL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
