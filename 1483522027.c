2aacf0beab344eb142a1f18742576f6f0e51eb1f
saisi <saisi@users.noreply.github.com>
2017-01-04 09:27:08 +0000

    1483522027 antilapsarian

---
+++ 1483522027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2896579170
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 31;
+   signed f1 : 24;
+   const signed f2 : 26;
+   int16_t  f3;
+   const unsigned f4 : 31;
+   unsigned f5 : 15;
+   signed f6 : 20;
