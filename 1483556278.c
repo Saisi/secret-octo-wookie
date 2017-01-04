77c168abd95c33a4ea6d75bd7776660619e2df41
saisi <saisi@users.noreply.github.com>
2017-01-04 18:57:58 +0000

    1483556278 auspices

---
+++ 1483556278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4019309644
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_6 = 7UL;
+static int64_t g_10 = 1L;
+static uint64_t * const g_11 = (void*)0;
+static int32_t g_14 = (-4L);
+static int32_t * volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
