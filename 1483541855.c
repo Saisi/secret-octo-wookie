8a7d3b1acc60e763dc07c35339c0640299c34ef0
saisi <saisi@users.noreply.github.com>
2017-01-04 14:57:36 +0000

    1483541855 Bailly

---
+++ 1483541855.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2296661237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   const int32_t  f1;
+};
+
+union U1 {
+   uint16_t  f0;
+   const int16_t  f1;
+   uint16_t  f2;
+   uint32_t  f3;
