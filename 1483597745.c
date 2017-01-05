86d9e8b3aaeea0fc28976d20a3136535dbc6481f
saisi <saisi@users.noreply.github.com>
2017-01-05 06:29:05 +0000

    1483597745 autorotate

---
+++ 1483597745.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1596172729
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+   uint32_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
