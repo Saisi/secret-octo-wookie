b8da55c2de765baac163fb8dc21432c22352c06a
saisi <saisi@users.noreply.github.com>
2017-01-05 02:28:44 +0000

    1483583321 Abama

---
+++ 1483583321.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4155012979
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint16_t  f0;
+   uint64_t  f1;
+   const volatile int32_t  f2;
+   uint32_t  f3;
+   volatile int32_t  f4;
+   int32_t  f5;
+   volatile uint32_t  f6;
