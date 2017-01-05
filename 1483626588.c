fb52f592a786feb13dc170efce138fe29c611094
saisi <saisi@users.noreply.github.com>
2017-01-05 14:29:48 +0000

    1483626588 anaplasmoses

---
+++ 1483626588.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2372172180
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2[2][4][3] = {{{0x121FL,0xC553L,0xC553L},{65526UL,65532UL,65526UL},{0x121FL,0x121FL,0xC553L},{0x7FBFL,65532UL,0x7FBFL}},{{0x121FL,0xC553L,0xC553L},{65526UL,65532UL,65526UL},{0x121FL,0x121FL,0xC553L},{0x7FBFL,65532UL,0x7FBFL}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
