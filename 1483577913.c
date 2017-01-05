753781330518b253710b872cf32f0e617cab5083
saisi <saisi@users.noreply.github.com>
2017-01-05 00:58:33 +0000

    1483577913 aglee

---
+++ 1483577913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      600156880
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[2] = {0xCB13638EL,0xCB13638EL};
+static uint32_t g_5 = 0x7770B168L;
+static uint64_t g_11[3][1][5] = {{{0x2C4DF8E3BF118C84LL,18446744073709551615UL,18446744073709551615UL,0x2C4DF8E3BF118C84LL,1UL}},{{0x2C4DF8E3BF118C84LL,18446744073709551615UL,18446744073709551615UL,0x2C4DF8E3BF118C84LL,1UL}},{{0x2C4DF8E3BF118C84LL,18446744073709551615UL,18446744073709551615UL,0x2C4DF8E3BF118C84LL,1UL}}};
+static int32_t g_48 = 1L;
+static int32_t g_64[4] = {(-8L),(-8L),(-8L),(-8L)};
+static int32_t g_66 = 1L;
+static int32_t *g_82 = &g_66;
+static int32_t ** volatile g_81 = &g_82;/* VOLATILE GLOBAL g_81 */
+static int32_t ** volatile g_84 = &g_82;/* VOLATILE GLOBAL g_84 */
