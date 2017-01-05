2a965cfec9b4d54bb3c0d597f343b6641c4d556d
saisi <saisi@users.noreply.github.com>
2017-01-05 11:59:36 +0000

    1483617575 acclimations

---
+++ 1483617575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3658579479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x3905C809L;
+static volatile int32_t g_33 = (-4L);/* VOLATILE GLOBAL g_33 */
+static volatile int32_t *g_32 = &g_33;
+static union U0 g_34[9] = {{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL},{0xDF3E2E16658069D1LL}};
