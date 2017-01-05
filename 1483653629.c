8140614138e6ceb4e73845d112c8f1b31d0344f3
saisi <saisi@users.noreply.github.com>
2017-01-05 22:00:29 +0000

    1483653629 arcform

---
+++ 1483653629.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3429920806
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   unsigned f1 : 18;
+   unsigned f2 : 12;
+   int16_t  f3;
+   signed f4 : 9;
+   signed f5 : 17;
+   signed f6 : 14;
