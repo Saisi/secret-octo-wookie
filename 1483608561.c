397ec327b2fee7f07470905db3ecf12fdc4abbfe
saisi <saisi@users.noreply.github.com>
2017-01-05 09:29:21 +0000

    1483608561 apesthesia

---
+++ 1483608561.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4032653736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   const int8_t  f1;
+   const uint8_t  f2;
+   uint64_t  f3;
+   const volatile int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint64_t g_2 = 0UL;/* VOLATILE GLOBAL g_2 */
