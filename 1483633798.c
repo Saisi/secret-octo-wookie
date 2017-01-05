eaec22b23b53982b8dac38265643af77995d652d
saisi <saisi@users.noreply.github.com>
2017-01-05 16:29:58 +0000

    1483633798 Amtrak

---
+++ 1483633798.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      767060787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_42 = 3L;
+static int32_t g_45[4][4] = {{0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L},{0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L},{0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L},{0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L,0xD32DE9E5L}};
+static volatile int32_t g_48 = (-1L);/* VOLATILE GLOBAL g_48 */
+static volatile int32_t g_49 = 0xAD88CA0EL;/* VOLATILE GLOBAL g_49 */
+static volatile int32_t g_50 = 1L;/* VOLATILE GLOBAL g_50 */
+static int32_t g_51 = (-1L);
+static int8_t g_57[2][9] = {{0x73L,0x73L,0x73L,0x73L,0x73L,0x73L,0x73L,0x73L,0x73L},{1L,1L,1L,1L,1L,1L,1L,1L,1L}};
+static int8_t g_59 = 0x36L;
+static int8_t g_75 = 1L;
