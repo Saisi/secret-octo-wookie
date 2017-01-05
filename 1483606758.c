0fb19fd6983b7aed5bd46af24fd22453fbb34a77
saisi <saisi@users.noreply.github.com>
2017-01-05 08:59:18 +0000

    1483606758 acantholysis

---
+++ 1483606758.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3417501722
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   uint32_t  f1;
+   const volatile int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_8[9] = {5UL,5UL,5UL,5UL,5UL,5UL,5UL,5UL,5UL};
+static uint64_t g_16 = 18446744073709551610UL;
+static int8_t g_17 = 0x51L;
