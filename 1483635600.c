6adbfee908227d55f717b22869d2df02d97363f9
saisi <saisi@users.noreply.github.com>
2017-01-05 17:00:01 +0000

    1483635600 agynic

---
+++ 1483635600.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2014166728
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int32_t  f1;
+   uint8_t  f2;
+};
+
+union U1 {
+   volatile int8_t * f0;
+   int8_t * const  f1;
+   const uint64_t  f2;
