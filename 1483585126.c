7315aea777c612715404684680efd805fd93f83e
saisi <saisi@users.noreply.github.com>
2017-01-05 02:58:47 +0000

    1483585126 anaerobium

---
+++ 1483585126.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      157830338
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 15;
+   signed f1 : 8;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   const uint16_t  f1;
+   volatile int8_t * f2;
+};
