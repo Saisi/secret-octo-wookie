4fc10b98538c5e75666eeb9736e14c3516f37e07
saisi <saisi@users.noreply.github.com>
2017-01-05 13:59:46 +0000

    1483624785 Assassin

---
+++ 1483624785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      204116270
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x8C45CDBEL;
+static int32_t g_6[7] = {(-1L),0x04CCB63EL,(-1L),(-1L),0x04CCB63EL,(-1L),(-1L)};
+static uint64_t g_32 = 5UL;
+static int32_t g_41 = 0x58FE4DE5L;
+static volatile int16_t g_51 = 0xC8F4L;/* VOLATILE GLOBAL g_51 */
