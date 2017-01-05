e39daff74f096191c023c4e7595ac932b1de2f00
saisi <saisi@users.noreply.github.com>
2017-01-05 19:00:14 +0000

    1483642814 acrocoracoid

---
+++ 1483642814.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1310864531
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = 0xE6BAL;
+static uint16_t g_28[6] = {0xE1C2L,0xE1C2L,0xE1C2L,0xE1C2L,0xE1C2L,0xE1C2L};
+static int32_t g_36[10][10] = {{5L,0x2275DD36L,8L,0xBA80278EL,0L,1L,4L,(-10L),1L,0x018EBDB8L},{0x3F2F34E4L,1L,0xC8C74C3EL,0x5BB00289L,5L,0L,0x390DFC1FL,0x390DFC1FL,0L,5L},{(-8L),3L,3L,(-8L),0x3312EF31L,0xBC795C38L,4L,(-1L),1L,(-1L)},{3L,5L,0xC73F5CFEL,0x390DFC1FL,1L,1L,3L,4L,1L,0xC8C74C3EL},{0x5C48968FL,0x0FF68E10L,0x3312EF31L,1L,5L,4L,0xBC795C38L,1L,1L,(-1L)},{0x3F2F34E4L,6L,5L,0L,5L,1L,7L,8L,5L,0L},{0xBA80278EL,0x2275DD36L,0L,5L,(-8L),0L,(-8L),5L,0L,0x2275DD36L},{0x2275DD36L,(-6L),(-10L),0xBA80278EL,0x0FF68E10L,1L,0x3312EF31L,0xC8C74C3EL,0xC73F5CFEL,7L},{(-1L),(-1L),(-3L),0x3312EF31L,0x17716E31L,1L,3L,5L,0x2275DD36L,8L},{0x2275DD36L,(-3L),(-1L),5L,0xC8C74C3EL,0L,0x0CB4E347L,(-6L),0x3312EF31L,1L}};
+static int32_t g_54 = 0x42B08F18L;
+static int32_t *g_59 = &g_36[3][2];
+static int32_t ** volatile g_58[9][9][1] = {{{&g_59},{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59}},{{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{&g_59},{&g_59},{&g_59}},{{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59}},{{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0}},{{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0}},{{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59}},{{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0}},{{&g_59},{&g_59},{&g_59},{(void*)0},{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0}},{{&g_59},{&g_59},{&g_59},{(void*)0},{(void*)0},{&g_59},{&g_59},{&g_59},{&g_59}}};
+static int32_t g_71 = 0xADEB0CCCL;
+static volatile int32_t g_72 = 9L;/* VOLATILE GLOBAL g_72 */
+static volatile int32_t g_73 = 0xC022AD61L;/* VOLATILE GLOBAL g_73 */
