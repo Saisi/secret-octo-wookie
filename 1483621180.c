1605e39145c3b55e5f481b34ba027f54a79cd1e0
saisi <saisi@users.noreply.github.com>
2017-01-05 12:59:40 +0000

    1483621180 Anser

---
+++ 1483621180.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1998056917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 9;
+   signed f1 : 28;
+};
+
+union U1 {
+   uint16_t  f0;
+   int16_t  f1;
+   int8_t  f2;
+   int32_t  f3;
