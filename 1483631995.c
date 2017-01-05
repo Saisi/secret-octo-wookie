c2d64f59ffd9fd8c9c5435d06af41cd51404d683
saisi <saisi@users.noreply.github.com>
2017-01-05 15:59:55 +0000

    1483631995 azygosperm

---
+++ 1483631995.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3715087065
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6][1][8] = {{{(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L}},{{0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L)}},{{0x8ADE19D2L,0x8ADE19D2L,(-3L),0x8ADE19D2L,0x8ADE19D2L,(-3L),0x8ADE19D2L,0x8ADE19D2L}},{{(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L}},{{0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L),(-1L),0x8ADE19D2L,(-1L)}},{{0x8ADE19D2L,0x8ADE19D2L,(-3L),0x8ADE19D2L,0x8ADE19D2L,(-3L),(-1L),(-1L)}}};
+static int32_t g_3 = 0x5664DC9EL;
+static int64_t g_66 = 0x6AEF2D6A391DE2FCLL;
+static int32_t g_71 = 0L;
