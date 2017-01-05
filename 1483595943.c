e39f53bfab3a8750dc7531388c0082a9d195b3fb
saisi <saisi@users.noreply.github.com>
2017-01-05 05:59:03 +0000

    1483595943 badminton

---
+++ 1483595943.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      87022626
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 31;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = 1L;
+static int32_t g_5 = 0L;
+static int32_t g_10 = 0x1D962EE6L;
+static volatile int32_t g_13 = 0x7B5C6EFFL;/* VOLATILE GLOBAL g_13 */
