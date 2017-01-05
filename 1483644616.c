4f7a0ce579231f4224c288ca7d848b566d112539
saisi <saisi@users.noreply.github.com>
2017-01-05 19:30:16 +0000

    1483644616 ajugas

---
+++ 1483644616.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2281765140
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile signed f1 : 13;
+   uint16_t  f2;
+   volatile uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 2L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0xB844A754L;
