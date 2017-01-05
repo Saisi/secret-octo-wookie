8c77d7c0954039bfc856648c62d81e2a41d15aff
saisi <saisi@users.noreply.github.com>
2017-01-05 08:29:16 +0000

    1483604955 allylthiourea

---
+++ 1483604955.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1708097242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x9841B3C6L;
+static volatile int32_t g_5[8] = {0x69F7FD1EL,0x02D149FEL,0x69F7FD1EL,0x69F7FD1EL,0x02D149FEL,0x69F7FD1EL,0x69F7FD1EL,0x02D149FEL};
+static volatile int32_t g_6 = 1L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 1L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 2L;/* VOLATILE GLOBAL g_8 */
+static int32_t g_9 = 5L;
+static int32_t g_12[4][5] = {{0x7182ABBFL,0x3A34C865L,0x7182ABBFL,0L,0L},{0x7182ABBFL,0x3A34C865L,0x7182ABBFL,0L,0L},{0x7182ABBFL,0x3A34C865L,0x7182ABBFL,0L,0L},{0x7182ABBFL,0x3A34C865L,0x7182ABBFL,0L,0L}};
+static int32_t * volatile g_18 = (void*)0;/* VOLATILE GLOBAL g_18 */
+static int32_t g_20 = 9L;
