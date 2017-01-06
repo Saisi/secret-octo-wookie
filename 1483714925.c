fabdb358a1322bf29ecc691f02406089c01d8292
saisi <saisi@users.noreply.github.com>
2017-01-06 15:02:06 +0000

    1483714925 ankylodactylia

---
+++ 1483714925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2925454368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const volatile uint32_t  f1;
+   uint64_t  f2;
+   int8_t  f3;
+   volatile uint8_t  f4;
+   int32_t  f5;
+   uint8_t  f6;
+   volatile uint16_t  f7;
+   uint32_t  f8;
