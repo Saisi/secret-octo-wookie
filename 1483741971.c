ced50a83f3be6848681fc4db4034c100e0b221a3
saisi <saisi@users.noreply.github.com>
2017-01-06 22:32:51 +0000

    1483741971 Alwyn

---
+++ 1483741971.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2288975686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const int16_t  f1;
+   const uint16_t  f2;
+   uint16_t  f3;
+   uint32_t  f4;
+   int32_t  f5;
+};
+
+union U1 {
