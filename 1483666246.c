2544ede75fb48399a3c04e20bfd4cb8268600fde
saisi <saisi@users.noreply.github.com>
2017-01-06 01:30:46 +0000

    1483666246 Bahner

---
+++ 1483666246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      103757472
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   unsigned f1 : 27;
+   volatile unsigned f2 : 17;
+   const volatile signed f3 : 25;
+   const signed f4 : 4;
+   const int32_t  f5;
+   unsigned f6 : 23;
