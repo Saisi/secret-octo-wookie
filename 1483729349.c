f17bd6def6e7e7d4c7eced29cfe68ecc67735f03
saisi <saisi@users.noreply.github.com>
2017-01-06 19:02:29 +0000

    1483729349 archmystagogue

---
+++ 1483729349.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1804005388
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 20;
+   volatile int16_t  f1;
+   signed f2 : 17;
+};
+#pragma pack(pop)
+
+struct S1 {
