f837b88a3cf5417710b111d871ba8ac44615b623
saisi <saisi@users.noreply.github.com>
2017-01-06 03:30:55 +0000

    1483673455 ammocoetiform

---
+++ 1483673455.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1822973533
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   uint32_t  f2;
+   int8_t  f3;
+};
+
+struct S1 {
+   unsigned f0 : 7;
+};
