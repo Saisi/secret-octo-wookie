20a388b71fbf07ca23d8e530f3947bbd1f297b25
saisi <saisi@users.noreply.github.com>
2017-01-06 08:31:24 +0000

    1483691484 antinationalist

---
+++ 1483691484.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3201278923
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xB9B55BB2L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-5L);
+static int64_t g_18 = (-1L);
+static int64_t g_21 = (-9L);
+static int32_t g_32[5][9] = {{1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L},{(-1L),1L,(-1L),(-1L),1L,(-1L),(-1L),1L,(-1L)},{1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L},{(-1L),1L,(-1L),(-1L),1L,(-1L),(-1L),1L,(-1L)},{1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L,1L,0xCD95FF79L,1L}};
+static int32_t *g_31 = &g_32[3][6];
+static int16_t g_49 = 0x6C64L;
+static volatile int64_t g_50 = 0x3387DD7D552B8909LL;/* VOLATILE GLOBAL g_50 */
+static uint32_t g_55 = 0x7680DD33L;
