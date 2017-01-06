bacb00664d90c183ad3f3c1f962214f754866f78
saisi <saisi@users.noreply.github.com>
2017-01-06 04:31:00 +0000

    1483677060 agrestal

---
+++ 1483677060.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4192080054
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 9;
+   unsigned f1 : 26;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
