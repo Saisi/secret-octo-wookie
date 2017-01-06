cc6711db35e87d663e88a5f9c49c54b5672804d2
saisi <saisi@users.noreply.github.com>
2017-01-06 11:31:43 +0000

    1483702300 assaut

---
+++ 1483702300.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3990137282
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const uint64_t  f1;
+   signed f2 : 10;
+   volatile int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 0xABL;
+static int8_t g_7 = 7L;
