f8f84bae7a8ffc90020b077d862c58ff4882a4a0
saisi <saisi@users.noreply.github.com>
2017-01-06 09:31:30 +0000

    1483695089 aphydrotropic

---
+++ 1483695089.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2057100482
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   const uint64_t  f1;
+   const int8_t  f2;
+   int32_t  f3;
+   int8_t * volatile  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
