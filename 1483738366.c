d42454f2458865720b7acda05ec5db4d84d3d6ba
saisi <saisi@users.noreply.github.com>
2017-01-06 21:32:46 +0000

    1483738366 acuminous

---
+++ 1483738366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2066134391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x93E9323DL;
+static volatile uint16_t g_6[3] = {0x9683L,0x9683L,0x9683L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
