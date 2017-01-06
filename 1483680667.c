731257558a73524ef30b8969bee451065046ea76
saisi <saisi@users.noreply.github.com>
2017-01-06 05:31:07 +0000

    1483680667 banghy

---
+++ 1483680667.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2102035350
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   const uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 0UL;
+static uint64_t g_46 = 0x8A996121589AFCA8LL;
+static int32_t g_57[4][1][8] = {{{4L,(-4L),0xEB5A40A7L,2L,0xEB5A40A7L,(-4L),4L,0xA5019003L}},{{(-4L),0x2349124CL,(-1L),0xEE8B7203L,0x7FFFF975L,2L,2L,0x7FFFF975L}},{{0xE12CDFD7L,4L,4L,0xE12CDFD7L,0x7FFFF975L,0xDBF60700L,0xA5019003L,2L}},{{(-4L),(-8L),2L,0x7FFFF975L,0xEB5A40A7L,0x7FFFF975L,2L,(-8L)}}};
+static struct S0 g_90 = {0xBDFBL,0xBE831782E4D72E8FLL};
