18cc908f620de67f987b881808c1aa50c4310630
saisi <saisi@users.noreply.github.com>
2017-01-06 06:31:12 +0000

    1483684271 backstabbed

---
+++ 1483684271.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2399976791
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+};
+
+union U1 {
+   const volatile struct S0  f0;
+   volatile uint32_t  f1;
+   int8_t  f2;
+};
+
