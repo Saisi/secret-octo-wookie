819e06477810b615825a576adceef294ebfc9300
saisi <saisi@users.noreply.github.com>
2017-01-06 23:33:02 +0000

    1483745582 anapes

---
+++ 1483745582.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1272940718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 5;
+   unsigned f1 : 6;
+   volatile unsigned f2 : 16;
+   unsigned f3 : 28;
+};
+#pragma pack(pop)
+
