d6a4f795a16f6e45cd8fc2c66bab1b4694183d53
saisi <saisi@users.noreply.github.com>
2017-01-06 11:01:38 +0000

    1483700498 Anamnia

---
+++ 1483700498.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4007027434
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int16_t  f1;
+   int8_t  f2;
+   int64_t  f3;
+   uint32_t  f4;
+   const int32_t  f5;
+   uint64_t  f6;
+   uint32_t  f7;
+};
