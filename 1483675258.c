d7f8b2277fa940842392038d263e36d50d490443
saisi <saisi@users.noreply.github.com>
2017-01-06 04:00:58 +0000

    1483675258 assert

---
+++ 1483675258.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2963839025
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 23;
+   const uint32_t  f1;
+   uint32_t  f2;
+   int8_t * f3;
+   int64_t  f4;
+};
+
+union U1 {
+   const uint32_t  f0;
