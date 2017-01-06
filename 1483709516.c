3012b51b9164e56b411aff0f37b9b6a2267d32ee
saisi <saisi@users.noreply.github.com>
2017-01-06 13:31:57 +0000

    1483709516 areometry

---
+++ 1483709516.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1170714445
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x0F951FE7L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0L;
+static int32_t g_5 = (-1L);
+static volatile int32_t g_6 = 0xD29729CFL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7[6][6] = {{(-1L),0x36B16B47L,(-4L),4L,4L,(-4L)},{4L,4L,(-4L),0x36B16B47L,(-1L),(-1L)},{4L,0x36B16B47L,0x36B16B47L,4L,(-1L),(-4L)},{(-1L),4L,0x36B16B47L,0x36B16B47L,4L,(-1L)},{(-1L),0x36B16B47L,(-4L),4L,4L,(-4L)},{4L,4L,(-4L),0x36B16B47L,(-1L),(-1L)}};
+static int32_t g_15 = (-4L);
+static int32_t *g_14 = &g_15;
+static uint16_t g_36 = 8UL;
+static int16_t g_38[1][10] = {{0xE181L,8L,0xE181L,0xE181L,8L,0xE181L,0xE181L,8L,0xE181L,0xE181L}};
