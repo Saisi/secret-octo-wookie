a7e6a7d97dddab3ea207bccedfda4929a69864c8
saisi <saisi@users.noreply.github.com>
2017-01-06 19:32:31 +0000

    1483731151 aquacade

---
+++ 1483731151.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2410702715
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 28;
+   uint32_t  f1;
+};
+
+union U1 {
+   const int8_t * volatile  f0;
+   int64_t  f1;
+   volatile int16_t  f2;
+};
