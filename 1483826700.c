d34dfb4ffd6a90adfc24fdb8cc4cbedad1be4b9a
saisi <saisi@users.noreply.github.com>
2017-01-07 22:05:00 +0000

    1483826700 Altis

---
+++ 1483826700.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2893031659
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 27;
+   volatile signed f1 : 17;
+   volatile signed f2 : 31;
