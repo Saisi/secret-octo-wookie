9b01c4f0ecad411555c9e103b3c4e951d17ef20a
saisi <saisi@users.noreply.github.com>
2017-01-07 00:33:07 +0000

    1483749187 banjulele

---
+++ 1483749187.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      194159292
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_20 = (-6L);
+static uint8_t g_30 = 0xDBL;
+static uint32_t g_34 = 4294967295UL;
+static uint8_t g_57[3] = {0x90L,0x90L,0x90L};
+static uint32_t g_80 = 0x72A7181CL;
+static int32_t g_88 = 0xECA2A428L;
+static int32_t g_89 = 0x0DB1B793L;
+static int16_t g_104 = 0xC327L;
+static uint8_t g_125 = 0x04L;
