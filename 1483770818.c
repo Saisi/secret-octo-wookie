881120ec57308dc9ea1220747c7f6e2eea094a9c
saisi <saisi@users.noreply.github.com>
2017-01-07 06:33:38 +0000

    1483770818 backcasts

---
+++ 1483770818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      435978315
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint8_t  f0;
+   uint8_t  f1;
+   const int16_t  f2;
+   const volatile unsigned f3 : 5;
+   int32_t  f4;
+   uint8_t  f5;
+   int64_t  f6;
+};
+
