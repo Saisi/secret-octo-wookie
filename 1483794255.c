c36190dfd5ce3075dd17f8a50e2ca66d6a81dc16
saisi <saisi@users.noreply.github.com>
2017-01-07 13:04:16 +0000

    1483794255 balant

---
+++ 1483794255.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3029341550
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   int32_t  f1;
+   volatile int8_t  f2;
+   volatile int8_t * const  f3;
+   volatile int8_t * volatile  f4;
+};
+
+union U1 {
+   volatile unsigned f0 : 12;
