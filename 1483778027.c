05a5ca7dfd9a0f25cbda92ef5d03203e16b0b4c9
saisi <saisi@users.noreply.github.com>
2017-01-07 08:33:48 +0000

    1483778027 aerotonometer

---
+++ 1483778027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3476019301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 17;
+   volatile signed f1 : 4;
+   volatile unsigned f2 : 23;
+   signed f3 : 6;
+   unsigned f4 : 31;
+   volatile signed f5 : 28;
+   const volatile unsigned f6 : 11;
