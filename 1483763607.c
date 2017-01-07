619d25a048ece17e1d3107dfa6c2f59e9cd85df5
saisi <saisi@users.noreply.github.com>
2017-01-07 04:33:28 +0000

    1483763607 acrindoline

---
+++ 1483763607.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2589618127
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 6;
+   const int64_t  f1;
+};
+
+union U1 {
+   volatile unsigned f0 : 20;
+   volatile unsigned f1 : 28;
+   int8_t * f2;
+   int8_t * volatile  f3;
