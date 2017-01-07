fbd11b13c10c66776563489f6569b3d1592178ea
saisi <saisi@users.noreply.github.com>
2017-01-07 04:03:25 +0000

    1483761804 antimediaevally

---
+++ 1483761804.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1097354307
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 19;
+   volatile signed f1 : 1;
+   signed f2 : 24;
+   uint8_t  f3;
+   unsigned f4 : 30;
+   unsigned f5 : 31;
+   volatile unsigned f6 : 2;
+};
+
