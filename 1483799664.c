378897419edd4c01be7c1d36fe83e698f3921022
saisi <saisi@users.noreply.github.com>
2017-01-07 14:34:25 +0000

    1483799664 ankyloproctia

---
+++ 1483799664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1053027081
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x951AD386L;
+static uint8_t g_5 = 1UL;
+static int32_t g_16[5] = {0L,0L,0L,0L,0L};
+static uint64_t g_20 = 0x5CB5542AFA017779LL;
+static int32_t ** volatile g_39 = (void*)0;/* VOLATILE GLOBAL g_39 */
+static int32_t *g_41 = &g_4;
+static int16_t g_92 = 0L;
+static uint64_t g_93 = 0x6FDE0027488F5754LL;
+static uint64_t g_94 = 0x643E267A5E2B1956LL;
