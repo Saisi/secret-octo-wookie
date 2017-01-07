23dd897ac3c076faada4a0b2abff3fdd0176c237
saisi <saisi@users.noreply.github.com>
2017-01-07 14:04:22 +0000

    1483797861 Apidae

---
+++ 1483797861.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1857488385
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile uint8_t  f1;
+   signed f2 : 21;
+};
+
+union U1 {
+   const unsigned f0 : 1;
+   int16_t  f1;
+   const int8_t * volatile  f2;
