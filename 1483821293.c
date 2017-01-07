29fd2f9af0eab2b53741227362bb3d6e2380eb3a
saisi <saisi@users.noreply.github.com>
2017-01-07 20:34:54 +0000

    1483821293 assubjugate

---
+++ 1483821293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1587416245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_18 = 0xE33A79CCL;
+static uint32_t g_28 = 4294967295UL;
+static int8_t g_30 = 0xE8L;
+static int32_t g_32 = 0x11BD77DFL;
