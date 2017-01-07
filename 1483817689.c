8f38095e2b25cccb2901ecf6aa22bd7f8eea18c6
saisi <saisi@users.noreply.github.com>
2017-01-07 19:34:49 +0000

    1483817689 Amblystoma

---
+++ 1483817689.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1043583503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+};
+
+struct S1 {
+   uint8_t  f0;
+   int32_t  f1;
+   int32_t  f2;
