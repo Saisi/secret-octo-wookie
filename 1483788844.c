4383bece71173325a604b8041938c5004784e2a4
saisi <saisi@users.noreply.github.com>
2017-01-07 11:34:07 +0000

    1483788844 Amphisile

---
+++ 1483788844.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4257644213
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int8_t  f1;
+   const int16_t  f2;
+   uint64_t  f3;
+};
+
+union U1 {
+   const uint32_t  f0;
+   volatile int8_t * const  f1;
