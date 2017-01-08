b1ffd4d07e99dba21a50d99eb47d76b2ae4d650c
saisi <saisi@users.noreply.github.com>
2017-01-08 17:37:36 +0000

    1483897054 Armatoles

---
+++ 1483897054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4124885562
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   int32_t  f1;
+   volatile uint64_t  f2;
+   volatile int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static uint8_t g_11[2] = {247UL,247UL};
