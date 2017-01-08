afd57e4d986d5acc2d496e9080fc46d8959740b5
saisi <saisi@users.noreply.github.com>
2017-01-08 11:06:51 +0000

    1483873611 ascogonia

---
+++ 1483873611.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1027116943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 6;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 7L;
+static int32_t g_4[9] = {0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L,0x50BA1BF2L};
