a9fa132c205aa09d2f6a0c003c6f0b03b92e145c
saisi <saisi@users.noreply.github.com>
2017-01-08 06:36:16 +0000

    1483857376 americium

---
+++ 1483857376.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1042582588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x0CD50997L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-3L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x653D4237L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
