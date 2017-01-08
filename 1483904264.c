97901b60fabd70b8179b40a0e65255897e762532
saisi <saisi@users.noreply.github.com>
2017-01-08 19:37:46 +0000

    1483904264 arthropterous

---
+++ 1483904264.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3525012672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint64_t  f0;
+   signed f1 : 27;
+   const unsigned f2 : 3;
+   int32_t  f3;
+   uint32_t  f4;
+   unsigned f5 : 9;
+   signed f6 : 6;
