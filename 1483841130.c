3a84cc7b0b69f1dd380ef00294921cd04bc0b532
saisi <saisi@users.noreply.github.com>
2017-01-08 02:05:31 +0000

    1483841130 abelite

---
+++ 1483841130.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      719710069
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 25;
+   signed f1 : 12;
+   unsigned f2 : 31;
+   unsigned f3 : 14;
+   volatile signed f4 : 7;
+   const signed f5 : 4;
+   signed f6 : 19;
