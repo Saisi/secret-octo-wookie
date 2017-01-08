08e1e2034926a74a0341658705fec834116eebb8
saisi <saisi@users.noreply.github.com>
2017-01-08 04:05:55 +0000

    1483848354 Banna

---
+++ 1483848354.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4172013465
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 3L;
+static int32_t g_65 = (-6L);
+static uint8_t g_70 = 0xD5L;
+static uint8_t g_76 = 0x29L;
+static uint8_t g_80 = 3UL;
+static uint32_t g_97 = 4294967295UL;
+static uint32_t g_98 = 2UL;
+static const int32_t g_101 = 0L;
