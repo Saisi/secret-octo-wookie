02eb2b79ad803b96a096b71c1b3ba8fa0877c5af
saisi <saisi@users.noreply.github.com>
2017-01-08 14:37:21 +0000

    1483886240 Alvito

---
+++ 1483886240.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1272918174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 29;
+   signed f1 : 30;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
