da73b78c1ade5493f6b521b717fa94e8e777a8c8
saisi <saisi@users.noreply.github.com>
2017-01-08 07:36:21 +0000

    1483860980 appressor

---
+++ 1483860980.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      155172510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   int32_t  f1;
+   const uint8_t  f2;
+   const unsigned f3 : 20;
+   int64_t  f4;
+   const unsigned f5 : 22;
+   uint8_t  f6;
