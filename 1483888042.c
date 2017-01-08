debd3c337bbd8c8ac02afa70c3fec35940a4870e
saisi <saisi@users.noreply.github.com>
2017-01-08 15:07:22 +0000

    1483888042 acetphenetidin

---
+++ 1483888042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      502322608
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 6;
+   signed f1 : 3;
+   signed f2 : 25;
+   volatile unsigned f3 : 1;
+   volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
