cd27b2a79ad7ea9cbed896d37c4daaf6f429925f
saisi <saisi@users.noreply.github.com>
2017-01-08 19:07:42 +0000

    1483902462 absorbent

---
+++ 1483902462.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3012293702
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   uint32_t  f1;
+   volatile unsigned f2 : 10;
+   signed f3 : 4;
+   const volatile unsigned f4 : 25;
+   const unsigned f5 : 1;
+   signed f6 : 30;
