d1961b0fb8d1f383ca1a9a728a3c6f889f8ac840
saisi <saisi@users.noreply.github.com>
2017-01-08 18:07:39 +0000

    1483898857 adenyls

---
+++ 1483898857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3355757913
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * volatile  f0;
+   uint32_t  f1;
+   volatile signed f2 : 12;
+   int16_t  f3;
+   unsigned f4 : 25;
+};
+
+union U1 {
+   volatile int32_t  f0;
