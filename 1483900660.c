3b0ae81442804c0342c141b37d900c2aa862f140
saisi <saisi@users.noreply.github.com>
2017-01-08 18:37:40 +0000

    1483900660 baniwa

---
+++ 1483900660.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      494605803
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static uint64_t g_5 = 0x6DDCD16652D0DA0BLL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
