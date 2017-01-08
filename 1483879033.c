925e25d259358e5d0e68aa6d2e721815dbfe4330
saisi <saisi@users.noreply.github.com>
2017-01-08 12:37:13 +0000

    1483879033 aberroscope

---
+++ 1483879033.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3675435602
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 23;
+   unsigned f1 : 16;
+   unsigned f2 : 18;
+   const signed f3 : 22;
+   unsigned f4 : 13;
+   volatile unsigned f5 : 16;
+};
+
+union U1 {
