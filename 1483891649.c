a836ec396b6d10c5b957aa149d5b1ce30df1c6ef
saisi <saisi@users.noreply.github.com>
2017-01-08 16:07:30 +0000

    1483891649 Alismales

---
+++ 1483891649.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4124113358
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   int16_t  f1;
+   signed : 0;
+   uint32_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
