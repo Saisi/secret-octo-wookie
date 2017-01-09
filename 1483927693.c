b96ff4f7e5b20cd8613e960c96c81ce30342731e
saisi <saisi@users.noreply.github.com>
2017-01-09 02:08:13 +0000

    1483927693 assaulted

---
+++ 1483927693.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2759657346
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static uint16_t g_30[3] = {65535UL,65535UL,65535UL};
+static int32_t g_38 = 1L;
+static int32_t g_40 = 1L;
+static int64_t g_81 = 0xDE47E5F8F0B53CF5LL;
+static volatile uint64_t g_92[7] = {9UL,9UL,9UL,9UL,9UL,9UL,9UL};
+static uint64_t g_94 = 0x2B81468F30040642LL;
+static uint8_t g_102 = 0UL;
+static int8_t g_112 = 0x23L;
