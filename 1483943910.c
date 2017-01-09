da5e1df5f9015c56b01fa1e1a03555770dc6e13a
saisi <saisi@users.noreply.github.com>
2017-01-09 06:38:30 +0000

    1483943910 aquatic

---
+++ 1483943910.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2682881869
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0xE77DD688L;
+static int32_t *g_5 = &g_6;
+static uint32_t g_84 = 5UL;
+static int8_t g_88 = 5L;
