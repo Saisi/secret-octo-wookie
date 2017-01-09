49889d2427e2d794b02fde47717702a7006d9c5b
saisi <saisi@users.noreply.github.com>
2017-01-09 12:39:04 +0000

    1483965543 Arjuna

---
+++ 1483965543.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4201446138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * volatile  f0;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   uint8_t  f1;
+   signed f2 : 2;
+   const uint32_t  f3;
+   volatile uint8_t  f4;
