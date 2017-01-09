09ed509228f1140b8817450aa731a75e0bb12906
saisi <saisi@users.noreply.github.com>
2017-01-09 00:08:06 +0000

    1483920486 balletically

---
+++ 1483920486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2140441648
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xDD1D8F8FL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[2] = {0xC5165352L,0xC5165352L};
+static int32_t g_5 = 0L;
+static uint16_t g_8[8][1][10] = {{{0x70B1L,0x68F9L,5UL,0UL,65535UL,0xBDD7L,0x7AB8L,0x7AB8L,0xBDD7L,65535UL}},{{0x8F6CL,0UL,0UL,0x8F6CL,0x9934L,0x17B7L,0x5589L,0x70B1L,65530UL,0x68F9L}},{{1UL,0x9934L,0x7E80L,0xBDD7L,65530UL,0x5589L,0UL,0x5589L,65530UL,0xBDD7L}},{{0x68F9L,1UL,0x68F9L,0x8F6CL,0x7AB8L,0x2BA7L,65530UL,65530UL,0xBDD7L,0xC587L}},{{0xC587L,0x2BA7L,0x9934L,0UL,0x17B7L,0xEF66L,0xEF66L,0x17B7L,0x7E80L,0x5833L}},{{65535UL,65535UL,65530UL,0UL,0x9934L,0x2BA7L,0xC587L,1UL,0x17B7L,0xBDD7L}},{{0xEF66L,0x5589L,65535UL,5UL,0xC587L,65530UL,0xC587L,5UL,65535UL,0x5589L}},{{0x17B7L,65535UL,0x7E80L,0x8F6CL,0xBDD7L,0x68F9L,0xEF66L,65530UL,0UL,1UL}}};
+static volatile int32_t * volatile g_11 = &g_4[0];/* VOLATILE GLOBAL g_11 */
+static uint64_t g_20 = 1UL;
+static volatile uint64_t g_25 = 0x353EA3A7221EBC5FLL;/* VOLATILE GLOBAL g_25 */
+static volatile uint64_t *g_24[9][6][1] = {{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}},{{&g_25},{&g_25},{&g_25},{&g_25},{&g_25},{&g_25}}};
+static int32_t *g_27 = (void*)0;
