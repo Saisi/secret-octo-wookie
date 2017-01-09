693b888acfaef7288079629bf3595fc96caba17d
saisi <saisi@users.noreply.github.com>
2017-01-09 14:09:11 +0000

    1483970951 aceturic

---
+++ 1483970951.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      265686115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   volatile unsigned f1 : 21;
+   unsigned f2 : 23;
+   volatile unsigned f3 : 31;
+   signed f4 : 10;
+};
+
+#pragma pack(push)
+#pragma pack(1)
