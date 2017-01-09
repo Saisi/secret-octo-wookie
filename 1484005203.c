d50086af25c793c5754d9e0eeca152950d3b4038
saisi <saisi@users.noreply.github.com>
2017-01-09 23:40:04 +0000

    1484005203 actinoscopy

---
+++ 1484005203.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      706700920
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   const signed f0 : 2;
+   struct S0  f1;
+   uint64_t  f2;
+};
+
