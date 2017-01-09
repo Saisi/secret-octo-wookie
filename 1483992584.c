8469b85057341da178a8bca006e68d9209f15440
saisi <saisi@users.noreply.github.com>
2017-01-09 20:09:44 +0000

    1483992584 balneotherapy

---
+++ 1483992584.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1192547904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_8[5][8][2] = {{{1UL,3UL},{3UL,1UL},{0xFD9EL,0x67EEL},{0xFD9EL,1UL},{3UL,3UL},{1UL,0UL},{3UL,65535UL},{0xFD9EL,0xFD9EL}},{{0xFD9EL,65535UL},{3UL,0UL},{1UL,3UL},{3UL,1UL},{0xFD9EL,0x67EEL},{0xFD9EL,1UL},{3UL,3UL},{1UL,0UL}},{{3UL,65535UL},{0xFD9EL,0xFD9EL},{0xFD9EL,65535UL},{3UL,0UL},{1UL,3UL},{3UL,1UL},{0xFD9EL,0x67EEL},{1UL,3UL}},{{0x414BL,0x67EEL},{3UL,0xFD9EL},{0x414BL,65532UL},{1UL,1UL},{1UL,65532UL},{0x414BL,0xFD9EL},{3UL,0x67EEL},{0x414BL,3UL}},{{1UL,65535UL},{1UL,3UL},{0x414BL,0x67EEL},{3UL,0xFD9EL},{0x414BL,65532UL},{1UL,1UL},{1UL,65532UL},{0x414BL,0xFD9EL}}};
+static int16_t g_35[7][9] = {{0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL},{(-1L),(-10L),(-10L),(-1L),(-10L),(-10L),(-1L),(-10L),(-10L)},{0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL},{(-1L),(-10L),(-10L),(-1L),(-10L),(-10L),(-1L),(-10L),(-10L)},{0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL},{(-1L),(-10L),(-10L),(-1L),(-10L),(-10L),(-1L),(-10L),(-10L)},{0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL,0x4060L,0xDC4BL,0xDC4BL}};
+static int32_t g_38 = 0L;
+static const int32_t g_85 = 1L;
+static const int32_t *g_89 = (void*)0;
+static const int32_t ** volatile g_88 = &g_89;/* VOLATILE GLOBAL g_88 */
+static int32_t g_102 = (-1L);
+static uint32_t g_103 = 1UL;
+static int32_t *g_105 = &g_38;
