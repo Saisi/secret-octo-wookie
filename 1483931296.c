870a7172e2ea3f5a839037966fd0123100a417d7
saisi <saisi@users.noreply.github.com>
2017-01-09 03:08:16 +0000

    1483931296 anneloid

---
+++ 1483931296.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1271779091
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int16_t g_2 = 0L;
+static int32_t g_4 = (-7L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
