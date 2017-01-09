8c0521eb3086fef0e1895bf9a308fe01028ce4e3
saisi <saisi@users.noreply.github.com>
2017-01-09 22:09:54 +0000

    1483999794 averroist

---
+++ 1483999794.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1384027938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xC85F70ADL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xEE76F502L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[9] = {8L,(-7L),8L,8L,(-7L),8L,8L,(-7L),8L};
+static volatile int32_t g_5 = 0x8C3AD3B3L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xC58FB770L;
+static int32_t g_9 = 0x62EE6D05L;
+static volatile int32_t g_12[2][3][9] = {{{(-9L),1L,(-9L),(-9L),1L,(-9L),(-9L),1L,(-9L)},{(-9L),1L,(-9L),(-9L),1L,(-9L),(-9L),1L,(-9L)},{(-9L),1L,(-9L),(-9L),1L,(-9L),(-9L),1L,(-9L)}},{{(-9L),1L,(-9L),(-9L),1L,(-9L),(-9L),1L,(-9L)},{(-9L),(-9L),0L,0L,(-9L),0L,0L,(-9L),0L},{0L,(-9L),0L,0L,(-9L),0L,0L,(-9L),0L}}};
+static int32_t g_13 = (-5L);
+static int32_t g_36 = (-9L);
