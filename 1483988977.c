ed70370e2165654b2b0159b0511eb6ae41fa5964
saisi <saisi@users.noreply.github.com>
2017-01-09 19:09:37 +0000

    1483988977 aniconism

---
+++ 1483988977.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3047682975
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_10 = 0x57L;
+static int32_t g_20 = (-2L);
+static uint16_t g_26 = 0x278CL;
+static int16_t g_64 = 0x1241L;
+static volatile int32_t g_91 = 0x72677139L;/* VOLATILE GLOBAL g_91 */
+static int8_t g_93 = 0x18L;
+static uint16_t g_98 = 65535UL;
+static volatile uint32_t g_105 = 0x8E619AEFL;/* VOLATILE GLOBAL g_105 */
+static const int32_t g_109[4][6] = {{0x8A746704L,0x63D6F3B0L,0x8A746704L,(-1L),0x63D6F3B0L,0xCAB38427L},{0x8A746704L,0xA52A621CL,(-1L),(-1L),0xA52A621CL,0x8A746704L},{0x8A746704L,1L,0xCAB38427L,(-1L),1L,(-1L)},{0x8A746704L,0x63D6F3B0L,0x8A746704L,(-1L),0x63D6F3B0L,0xCAB38427L}};
