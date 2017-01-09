49ad96ed65095673206dd19475ad8efc6e354464
saisi <saisi@users.noreply.github.com>
2017-01-09 11:38:58 +0000

    1483961937 alkyl

---
+++ 1483961937.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1725098389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   int8_t  f1;
+   signed f2 : 7;
+   uint32_t  f3;
+   uint16_t  f4;
+};
+
+union U1 {
+   uint16_t  f0;
