37ab0b95c374fe3d3c81a73852897e619974c5cb
saisi <saisi@users.noreply.github.com>
2017-01-09 12:09:00 +0000

    1483963740 animose

---
+++ 1483963740.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3348605876
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x52568108L;
+static int8_t g_8 = 0x7BL;
+static volatile int16_t g_9 = 0x4039L;/* VOLATILE GLOBAL g_9 */
+static volatile int64_t g_10 = (-1L);/* VOLATILE GLOBAL g_10 */
+static uint32_t g_12 = 0UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
