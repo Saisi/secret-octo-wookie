d63c697e4556cdd2a81a35214945c84f0fff567f
saisi <saisi@users.noreply.github.com>
2017-01-09 20:39:46 +0000

    1483994386 Anthribidae

---
+++ 1483994386.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1831227808
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   signed f1 : 27;
+   signed f2 : 17;
+   signed f3 : 25;
+   unsigned f4 : 4;
+   const unsigned f5 : 11;
+   volatile signed f6 : 29;
