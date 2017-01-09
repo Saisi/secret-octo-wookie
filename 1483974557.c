99025fd655febee2ae5c0774afc0701b85dee6a4
saisi <saisi@users.noreply.github.com>
2017-01-09 15:09:17 +0000

    1483974557 axone

---
+++ 1483974557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4127420511
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x4A2A4F40L;
+static volatile int16_t g_4[8] = {0x7A3AL,0x7A3AL,0x7A3AL,0x7A3AL,0x7A3AL,0x7A3AL,0x7A3AL,0x7A3AL};
+static int64_t g_10 = (-1L);
+static uint16_t g_11 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
