9071cd90c91706c15927a54fb26e49136db2cc8f
saisi <saisi@users.noreply.github.com>
2017-01-09 17:39:30 +0000

    1483983569 Americanize

---
+++ 1483983569.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      243447672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   volatile int8_t  f2;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
