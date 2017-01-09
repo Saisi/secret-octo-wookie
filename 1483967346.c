2b9afb3398dd834b8ffb047012141c04a9715476
saisi <saisi@users.noreply.github.com>
2017-01-09 13:09:06 +0000

    1483967346 acridane

---
+++ 1483967346.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3424027092
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x4A754320L;
+static volatile int32_t g_12 = 0L;/* VOLATILE GLOBAL g_12 */
+static uint32_t g_13 = 0x1C5A3D7FL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
