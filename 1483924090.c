bdba09e709e5f6a90fc352d3378ee13adf5b540a
saisi <saisi@users.noreply.github.com>
2017-01-09 01:08:10 +0000

    1483924090 archlute

---
+++ 1483924090.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1989257955
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_2 = 0x1177E8EDF1FEBAEELL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
