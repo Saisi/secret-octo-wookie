82271e2e7e5b25d92069ec4a9fda5fcc7022553b
saisi <saisi@users.noreply.github.com>
2017-01-10 09:40:57 +0000

    1484041257 angioleucitis

---
+++ 1484041257.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3074225676
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 0xBFC3938DL;
+static int32_t *g_13 = &g_14;
+static int32_t g_51 = 0x314B8123L;
+static int32_t g_74 = 0xDEBF624EL;
+static int32_t g_80[1] = {0L};
+static int32_t *g_88 = &g_74;
+static int32_t * volatile g_90 = &g_74;/* VOLATILE GLOBAL g_90 */
+static uint16_t g_117 = 0xBC5AL;
+static int32_t ** const  volatile g_146 = &g_88;/* VOLATILE GLOBAL g_146 */
