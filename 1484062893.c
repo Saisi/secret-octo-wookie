76431b2dda1bd0981bd9c21cf037371bd6228430
saisi <saisi@users.noreply.github.com>
2017-01-10 15:41:33 +0000

    1484062893 algological

---
+++ 1484062893.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2129787822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 5;
+   signed f1 : 6;
+};
+#pragma pack(pop)
+
+struct S1 {
+   int32_t  f0;
