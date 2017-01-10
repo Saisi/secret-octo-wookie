8fdb6d6e9d7d6126b802931996c4bc1b07874b04
saisi <saisi@users.noreply.github.com>
2017-01-10 23:42:19 +0000

    1484091737 Adamically

---
+++ 1484091737.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3282075481
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   unsigned f1 : 22;
+   const unsigned f2 : 1;
+   signed f3 : 8;
+   signed f4 : 25;
+   const unsigned f5 : 14;
+};
+
+struct S1 {
