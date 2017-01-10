38bc732044bba5c61d2a551beedcc23d1fdc643f
saisi <saisi@users.noreply.github.com>
2017-01-10 06:10:39 +0000

    1484028638 Anaxibia

---
+++ 1484028638.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2218918624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static uint64_t g_24[4] = {0x83C67EE39F3FAA32LL,0x83C67EE39F3FAA32LL,0x83C67EE39F3FAA32LL,0x83C67EE39F3FAA32LL};
+static uint32_t g_66[2] = {0x97E65FBCL,0x97E65FBCL};
+static uint16_t g_89 = 65527UL;
+static int32_t g_105[7] = {0xD542D3F7L,0xD542D3F7L,0xD542D3F7L,0xD542D3F7L,0xD542D3F7L,0xD542D3F7L,0xD542D3F7L};
+static const uint16_t * volatile * const g_152 = (void*)0;
+static uint8_t g_157 = 250UL;
+static int64_t g_159[4][2][2] = {{{4L,4L},{0xB9FEB662F6D3EBDDLL,4L}},{{4L,0xB9FEB662F6D3EBDDLL},{4L,4L}},{{0xB9FEB662F6D3EBDDLL,4L},{4L,0xB9FEB662F6D3EBDDLL}},{{4L,4L},{0xB9FEB662F6D3EBDDLL,4L}}};
+static int64_t g_189 = 0xE694C051F803E926LL;
