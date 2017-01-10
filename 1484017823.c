7f54cef4588baf4a0ce2d2b20f53ebaaef4e8e0c
saisi <saisi@users.noreply.github.com>
2017-01-10 03:10:23 +0000

    1484017823 Asiatize

---
+++ 1484017823.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4028640912
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t *g_2 = &g_3;
+static uint8_t g_31 = 255UL;
+static uint16_t g_39 = 0xCDA7L;
+static int32_t * volatile g_58[10] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static uint32_t g_65[10] = {0x997736EAL,0x584EDB55L,9UL,9UL,0x584EDB55L,0x997736EAL,0x584EDB55L,9UL,9UL,0x584EDB55L};
+static int32_t g_75[2] = {0x9BABE4A9L,0x9BABE4A9L};
+static uint8_t g_83[2][10] = {{0x64L,9UL,9UL,0x64L,255UL,0x64L,9UL,9UL,0x64L,255UL},{0x64L,9UL,9UL,0x64L,255UL,0x64L,9UL,9UL,0x64L,255UL}};
+static uint64_t g_100 = 0x9801C16B31B79E29LL;
