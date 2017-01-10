a7c97976d039721abfcdd0b43dbd69120b4e294c
saisi <saisi@users.noreply.github.com>
2017-01-10 21:12:04 +0000

    1484082723 armillas

---
+++ 1484082723.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2681327563
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   const volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0UL;
+static const volatile union U0 g_10 = {2UL};/* VOLATILE GLOBAL g_10 */
+static int32_t g_29 = 0x34C266D8L;
+static int32_t *g_28 = &g_29;
