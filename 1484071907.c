f03c494847ef845175e1e807b286b2b6bf786fea
saisi <saisi@users.noreply.github.com>
2017-01-10 18:11:48 +0000

    1484071907 Aphaniptera

---
+++ 1484071907.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2461889634
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+   uint64_t  f1;
+   volatile int32_t  f2;
+   volatile int64_t  f3;
+   volatile uint16_t  f4;
+   uint32_t  f5;
+   const uint8_t  f6;
