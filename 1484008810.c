cd1f2ece34eb475bc227988333a82e65e5633bac
saisi <saisi@users.noreply.github.com>
2017-01-10 00:40:10 +0000

    1484008810 antiproteolysis

---
+++ 1484008810.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1277581868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_25 = 65526UL;
+static int64_t g_39 = 0L;
+static uint16_t g_40 = 0x1D99L;
+static volatile int32_t g_49 = (-5L);/* VOLATILE GLOBAL g_49 */
+static volatile int32_t * volatile g_48 = &g_49;/* VOLATILE GLOBAL g_48 */
+static int32_t g_63 = 0L;
+static uint8_t g_75 = 0x9DL;
+static int32_t **g_77 = (void*)0;
+static int32_t *g_83 = &g_63;
