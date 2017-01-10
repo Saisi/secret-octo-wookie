9a06c303bb595bd4513e9766eaad0458760691d8
saisi <saisi@users.noreply.github.com>
2017-01-10 22:12:10 +0000

    1484086329 airlifting

---
+++ 1484086329.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3022518854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const uint64_t  f1;
+   const uint64_t  f2;
+   int32_t  f3;
+};
+
+struct S1 {
+   int64_t  f0;
+   const signed f1 : 2;
