a5e663120e47848bb7d7a7a848de01c8efc2ea2e
saisi <saisi@users.noreply.github.com>
2017-01-10 01:40:15 +0000

    1484012415 artistries

---
+++ 1484012415.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3084505417
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int8_t  f1;
+   uint8_t  f2;
+   volatile int32_t  f3;
+   int32_t  f4;
+   const int64_t  f5;
+   volatile int8_t  f6;
+   volatile int8_t  f7;
+};
