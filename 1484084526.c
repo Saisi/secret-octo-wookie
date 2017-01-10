4546e48283c465069fc56edc93147b6ac39bd5e4
saisi <saisi@users.noreply.github.com>
2017-01-10 21:42:07 +0000

    1484084526 awheel

---
+++ 1484084526.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      994755739
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_15 = 0x28L;
+static int8_t *g_14 = &g_15;
+static union U0 g_45 = {0};
+static const int8_t g_49[4] = {0x51L,0x51L,0x51L,0x51L};
