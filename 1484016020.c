35e5571344ef5130c4fb86ae5655f474125085c6
saisi <saisi@users.noreply.github.com>
2017-01-10 02:40:21 +0000

    1484016020 adrenalectomizing

---
+++ 1484016020.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2155110392
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3][6] = {{6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L},{6L,6L,6L,6L,6L,6L}};
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x2E08974BL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xA756A336L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xED9A924CL;
