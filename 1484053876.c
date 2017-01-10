bb3b445d90330124c5b953503a126f2040e3aecd
saisi <saisi@users.noreply.github.com>
2017-01-10 13:11:17 +0000

    1484053876 antigene

---
+++ 1484053876.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      941731393
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4] = {1L,1L,1L,1L};
+static int32_t *g_7 = (void*)0;
+static int32_t g_30[10] = {(-7L),(-7L),8L,(-7L),(-7L),8L,(-7L),(-7L),8L,(-7L)};
+static uint32_t g_31 = 4294967295UL;
+static int32_t g_60 = 0L;
+static int8_t g_64 = 0xC2L;
+static int8_t g_66[3][1] = {{(-3L)},{(-3L)},{(-3L)}};
+static int8_t *g_65 = &g_66[1][0];
+static uint16_t g_83 = 0x0558L;
