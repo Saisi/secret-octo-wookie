e432396473f53575dacb40d1b5ff2c7bf9e70342
saisi <saisi@users.noreply.github.com>
2017-01-10 05:40:36 +0000

    1484026836 ascertain

---
+++ 1484026836.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1055220885
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_28 = 0UL;
+static uint16_t g_37 = 65533UL;
+static uint8_t g_74 = 0x19L;
+static uint32_t g_78 = 0x22CBD4DAL;
+static int8_t g_80[1] = {0x95L};
+static int32_t g_84 = 6L;
+static uint16_t g_110 = 0xEEBDL;
+static int32_t g_112 = 0xD331F862L;
+static int32_t * volatile g_111[8] = {&g_112,&g_112,(void*)0,&g_112,&g_112,(void*)0,&g_112,&g_112};
