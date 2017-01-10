469e1720d05fd09ee9294a5eb09c63d5b28b8b83
saisi <saisi@users.noreply.github.com>
2017-01-10 01:10:12 +0000

    1484010612 anaglyptics

---
+++ 1484010612.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1793232351
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_10 = 0x0F023498E64E6DC6LL;
+static int32_t g_22[4][10] = {{1L,(-6L),0xEB2FC257L,0x12E75507L,0xEB2FC257L,(-6L),1L,0x26C12A7BL,0xC7A8CF1DL,(-1L)},{0x26C12A7BL,0x773196E8L,(-10L),0x1571C097L,1L,1L,0x1571C097L,(-10L),0x773196E8L,0x26C12A7BL},{(-8L),0x773196E8L,0xC7A8CF1DL,0x10DD6640L,0x12E75507L,(-1L),1L,(-1L),0x12E75507L,0x10DD6640L},{0x10DD6640L,(-6L),0x10DD6640L,0x773196E8L,0x12E75507L,0x9F5954E3L,0x26C12A7BL,0xEB2FC257L,0xEB2FC257L,0x26C12A7BL}};
+static int32_t * const g_21[8][7][1] = {{{&g_22[0][8]},{&g_22[2][5]},{&g_22[3][0]},{(void*)0},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]}},{{&g_22[0][6]},{&g_22[2][5]},{(void*)0},{(void*)0},{&g_22[2][5]},{&g_22[2][5]},{&g_22[1][4]}},{{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[1][4]}},{{&g_22[2][5]},{&g_22[2][5]},{(void*)0},{(void*)0},{&g_22[2][5]},{&g_22[0][6]},{&g_22[2][5]}},{{&g_22[2][5]},{&g_22[2][5]},{&g_22[0][6]},{&g_22[2][5]},{(void*)0},{(void*)0},{&g_22[2][5]}},{{&g_22[2][5]},{&g_22[1][4]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]}},{{&g_22[2][5]},{&g_22[1][4]},{&g_22[2][5]},{&g_22[2][5]},{(void*)0},{(void*)0},{&g_22[2][5]}},{{&g_22[0][6]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[2][5]},{&g_22[0][6]},{&g_22[2][5]},{(void*)0}}};
+static const int32_t g_64 = 0xB13F4B1AL;
+static int32_t g_78 = 0x983C1E2AL;
+static int32_t * volatile g_77[8] = {&g_78,&g_22[2][5],&g_78,&g_78,&g_22[2][5],&g_78,&g_78,&g_22[2][5]};
+static int32_t * volatile g_79 = &g_78;/* VOLATILE GLOBAL g_79 */
+static int64_t g_84[3] = {0xA6511C0E03A8461ALL,0xA6511C0E03A8461ALL,0xA6511C0E03A8461ALL};
+static uint32_t g_91 = 4294967295UL;
