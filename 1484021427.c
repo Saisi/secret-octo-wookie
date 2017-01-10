2d502d322038a5f5abf26d4c55a1bca8ac4f70b5
saisi <saisi@users.noreply.github.com>
2017-01-10 04:10:28 +0000

    1484021427 anisoleucocytosis

---
+++ 1484021427.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4246877267
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 8;
+   signed f1 : 31;
+   volatile signed f2 : 6;
+   const int32_t  f3;
+   const signed f4 : 2;
+};
+#pragma pack(pop)
