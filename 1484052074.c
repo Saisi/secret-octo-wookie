3e7cd060d644976042d265cae19752ab03890b64
saisi <saisi@users.noreply.github.com>
2017-01-10 12:41:14 +0000

    1484052074 androphagous

---
+++ 1484052074.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4110538312
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_7[1][4][10] = {{{0xB13281D2L,0x5F07C70AL,0x3C407858L,0L,(-9L),0x227D373EL,0x4B28ED4AL,0x90C54408L,0x90C54408L,0x4B28ED4AL},{0L,0x5F07C70AL,0x4B28ED4AL,0x4B28ED4AL,0x5F07C70AL,0L,0x5121AC48L,0xB32A831DL,(-1L),0x227D373EL},{0xB32A831DL,2L,8L,0L,0L,0x5B054DBBL,(-9L),0x3C407858L,(-9L),0x5B054DBBL},{0xB32A831DL,0x90C54408L,0xB13281D2L,0x90C54408L,0xB32A831DL,0L,0x5B054DBBL,0x6EC17E5CL,0x8999D94BL,0xC3EEE2FDL}}};
+static uint32_t g_36 = 0x140A9711L;
+static int32_t **g_64 = (void*)0;
+static uint16_t g_77 = 0x7BC4L;
+static uint8_t g_79 = 255UL;
+static int8_t g_87 = 0x83L;
+static uint32_t g_89 = 0x986173B0L;
+static uint8_t g_92 = 0xE3L;
