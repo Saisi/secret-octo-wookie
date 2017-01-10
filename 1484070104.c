f94fd93710b7a7cad490627604b0d0926a640e8b
saisi <saisi@users.noreply.github.com>
2017-01-10 17:41:45 +0000

    1484070104 ambivalence

---
+++ 1484070104.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1803263300
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   uint64_t  f1;
+   unsigned f2 : 23;
+   int32_t  f3;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   const uint8_t  f1;
