070810ced2b2aa7b893aacf852c267eeedc89e99
saisi <saisi@users.noreply.github.com>
2017-01-10 07:10:44 +0000

    1484032244 affectations

---
+++ 1484032244.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4257829275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+};
+
+struct S1 {
+   unsigned f0 : 3;
+   unsigned f1 : 27;
+   signed f2 : 22;
+   signed f3 : 12;
+   int16_t  f4;
