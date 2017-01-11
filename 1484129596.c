84bcec431d80ac7c8c2090dcd4a39f14342848d1
saisi <saisi@users.noreply.github.com>
2017-01-11 10:13:17 +0000

    1484129596 ACW

---
+++ 1484129596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      645871072
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   const int8_t * const  f1;
+   uint32_t  f2;
+   volatile int64_t  f3;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = 0x82F80C34L;
