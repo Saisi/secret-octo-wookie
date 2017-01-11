f443f7e3500c2f83c2f504940407d5d07f75b23a
saisi <saisi@users.noreply.github.com>
2017-01-11 17:13:54 +0000

    1484154834 acapnias

---
+++ 1484154834.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3110976812
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 4;
+   signed f1 : 9;
+   signed f2 : 7;
+   unsigned f3 : 23;
+   uint32_t  f4;
+   unsigned f5 : 20;
+   unsigned f6 : 5;
