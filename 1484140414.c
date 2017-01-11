7b84e6010f043d2864b794880fece4851f1c7c28
saisi <saisi@users.noreply.github.com>
2017-01-11 13:13:34 +0000

    1484140414 arval

---
+++ 1484140414.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3330961949
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5CA9869EL;
+static volatile uint8_t g_7 = 0x60L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_10 = 0xE9AB5C29L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
