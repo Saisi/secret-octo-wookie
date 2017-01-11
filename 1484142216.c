a4b15f105eaf102b4f0ba5a212a5c6a8ee278f30
saisi <saisi@users.noreply.github.com>
2017-01-11 13:43:36 +0000

    1484142216 allodialist

---
+++ 1484142216.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3748218795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   uint64_t  f0;
+   int32_t  f1;
+   int8_t * volatile  f2;
+   int8_t * volatile  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC5021C3AL;
+static union U1 g_7 = {0x3AC46DDCD16652D0LL};/* VOLATILE GLOBAL g_7 */
