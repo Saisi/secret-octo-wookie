b919bf5903682da430c45f8e0ee2ad0abb7470df
saisi <saisi@users.noreply.github.com>
2017-01-11 19:14:05 +0000

    1484162045 antianopheline

---
+++ 1484162045.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      692894665
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint64_t  f0;
+   signed f1 : 16;
+   volatile uint16_t  f2;
+};
+
+union U1 {
+   const uint16_t  f0;
+   const int32_t  f1;
+};
