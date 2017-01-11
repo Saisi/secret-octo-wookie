9edbb7f8c80efc6a892b0f6304cdf20f13bafeb5
saisi <saisi@users.noreply.github.com>
2017-01-11 02:42:36 +0000

    1484102556 apologer

---
+++ 1484102556.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      162172548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   uint32_t  f3;
+   const int32_t  f4;
+};
+
+union U1 {
+   const uint64_t  f0;
