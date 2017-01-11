74f33adf6c73000421553eb872c744617a2b9623
saisi <saisi@users.noreply.github.com>
2017-01-11 15:13:43 +0000

    1484147623 alfilaria

---
+++ 1484147623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1418738633
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   uint8_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xCE57BF5CL;
