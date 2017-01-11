d90cfeb21ba44d3f528d392400def8add76e0333
saisi <saisi@users.noreply.github.com>
2017-01-11 16:43:52 +0000

    1484153031 Aistopodes

---
+++ 1484153031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1518320257
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_39[6] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint32_t g_61 = 0x5ADF851FL;
+static uint32_t g_84[8] = {0xF0912742L,0xF0912742L,0xF0912742L,0xF0912742L,0xF0912742L,0xF0912742L,0xF0912742L,0xF0912742L};
+static uint64_t g_113[10] = {0x47550418440AC715LL,0x47550418440AC715LL,0x5CEC10C1A6FE1600LL,1UL,0x5CEC10C1A6FE1600LL,0x47550418440AC715LL,0x47550418440AC715LL,0x5CEC10C1A6FE1600LL,1UL,0x5CEC10C1A6FE1600LL};
+static uint32_t g_116 = 1UL;
+static uint16_t g_123 = 65535UL;
+static int16_t g_125 = 5L;
+static uint8_t g_155[1] = {0x53L};
+static int32_t g_158 = 0x9491B29FL;
