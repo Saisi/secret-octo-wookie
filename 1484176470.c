8653b95975929fb5121a4d674bba6367aa44a576
saisi <saisi@users.noreply.github.com>
2017-01-11 23:14:31 +0000

    1484176470 anoura

---
+++ 1484176470.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3573025217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile uint8_t  f1;
+   uint64_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   uint8_t  f0;
+   volatile int16_t  f1;
