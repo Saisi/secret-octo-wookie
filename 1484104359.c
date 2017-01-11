413dc91fa037ca2df4cca64c3887c2bb26299f53
saisi <saisi@users.noreply.github.com>
2017-01-11 03:12:39 +0000

    1484104359 assumptious

---
+++ 1484104359.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1961283109
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[8][1] = {{0xC64A6847L},{0xC64A6847L},{0xC64A6847L},{0xC64A6847L},{0xC64A6847L},{0xC64A6847L},{0xC64A6847L},{0xC64A6847L}};
+static uint8_t g_15 = 0xA3L;
+static uint8_t g_18 = 0xD4L;
+static const uint8_t **g_46[4] = {(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t g_50 = 0xEA885C82L;
+static uint8_t g_63 = 0x96L;
+static int8_t g_65 = 0x50L;
+static uint8_t *g_113 = &g_15;
+static uint8_t **g_112[10][4] = {{&g_113,&g_113,&g_113,&g_113},{&g_113,&g_113,&g_113,&g_113},{&g_113,(void*)0,&g_113,&g_113},{&g_113,&g_113,&g_113,&g_113},{&g_113,(void*)0,&g_113,&g_113},{&g_113,&g_113,&g_113,&g_113},{&g_113,&g_113,&g_113,&g_113},{&g_113,(void*)0,&g_113,&g_113},{&g_113,&g_113,&g_113,(void*)0},{&g_113,&g_113,&g_113,&g_113}};
