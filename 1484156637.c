6b84a052b8a9c1af4874f64e37e649e782504b54
saisi <saisi@users.noreply.github.com>
2017-01-11 17:43:57 +0000

    1484156637 Atikokania

---
+++ 1484156637.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      128628458
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   int32_t  f1;
+   unsigned f2 : 27;
+   int32_t  f3;
+   uint32_t  f4;
+   const int16_t  f5;
+   volatile int16_t  f6;
