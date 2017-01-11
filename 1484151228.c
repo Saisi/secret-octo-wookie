339a1e5cfcf00d13cb3cec1262c750672ede7257
saisi <saisi@users.noreply.github.com>
2017-01-11 16:13:49 +0000

    1484151228 Aldric

---
+++ 1484151228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3872246704
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 6;
+   unsigned f1 : 3;
+   const unsigned f2 : 25;
+   signed f3 : 1;
+   volatile signed f4 : 12;
+   signed f5 : 19;
+   unsigned f6 : 5;
+   unsigned f7 : 6;
+   unsigned f8 : 18;
