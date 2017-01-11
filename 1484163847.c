44ce25657796341a9e9806fb089f53aeb57ad7dd
saisi <saisi@users.noreply.github.com>
2017-01-11 19:44:09 +0000

    1484163847 arbutes

---
+++ 1484163847.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1250488892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+   unsigned f1 : 17;
+   unsigned f2 : 15;
+   signed f3 : 31;
+   signed f4 : 18;
+};
+
+struct S1 {
+   volatile unsigned f0 : 13;
