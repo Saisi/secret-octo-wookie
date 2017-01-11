f522f3e5ebdbeab0db27d6cc0b920104ec74b1b8
saisi <saisi@users.noreply.github.com>
2017-01-11 03:42:42 +0000

    1484106161 antianaphylaxis

---
+++ 1484106161.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2471416484
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   const volatile uint8_t  f1;
+   uint16_t  f2;
+   uint16_t  f3;
+   int32_t  f4;
+   uint32_t  f5;
+   volatile int8_t  f6;
+   uint8_t  f7;
+   uint64_t  f8;
