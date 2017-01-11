64e444a3a54ef3449c3a49b6ae38ab654c506001
saisi <saisi@users.noreply.github.com>
2017-01-11 09:13:12 +0000

    1484125991 baggy

---
+++ 1484125991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2906183754
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   uint16_t  f1;
+   int32_t  f2;
+   const int32_t  f3;
+   int32_t  f4;
+   int64_t  f5;
+   uint8_t  f6;
+   volatile int64_t  f7;
+   signed f8 : 12;
