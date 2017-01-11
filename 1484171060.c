c90381f4b0044ce89c24b6dce54c8baa140e2a83
saisi <saisi@users.noreply.github.com>
2017-01-11 21:44:20 +0000

    1484171060 autotelism

---
+++ 1484171060.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1846505194
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int64_t g_4 = 1L;
+static uint32_t g_5[5] = {18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint64_t  func_1(void);
+
+
