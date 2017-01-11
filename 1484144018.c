45b5af4c866282e5ff4bb05ebac832256fcef76a
saisi <saisi@users.noreply.github.com>
2017-01-11 14:13:38 +0000

    1484144018 annuli

---
+++ 1484144018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3790527184
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 22;
+};
+
+union U1 {
+   int32_t  f0;
+   uint32_t  f1;
+   const volatile int32_t  f2;
+   int16_t  f3;
+   int32_t  f4;
