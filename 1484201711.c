58956a2fac487466da7d1e79dfe29e3e082b611f
saisi <saisi@users.noreply.github.com>
2017-01-12 06:15:12 +0000

    1484201711 aerologies

---
+++ 1484201711.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2050823721
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[7] = {8UL,1UL,8UL,8UL,1UL,8UL,8UL};
+static int32_t g_14 = 2L;
+static uint64_t g_42 = 0x6A37D7C869B7D6B5LL;
+static uint64_t *g_50 = &g_42;
+static uint64_t **g_49[7][8][1] = {{{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50}},{{&g_50},{&g_50},{&g_50},{&g_50},{(void*)0},{&g_50},{&g_50},{&g_50}},{{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50}},{{(void*)0},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50}},{{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{(void*)0},{&g_50},{(void*)0}},{{(void*)0},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{(void*)0},{(void*)0}},{{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50},{&g_50}}};
+static int16_t g_68[3] = {0L,0L,0L};
+static int16_t g_70 = 0L;
+static int8_t g_81 = 0x98L;
+static uint64_t *g_82 = &g_42;
