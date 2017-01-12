cd7612492f1d0253b3535f3cf4158a03787d8b75
saisi <saisi@users.noreply.github.com>
2017-01-12 05:15:07 +0000

    1484198106 axine

---
+++ 1484198106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4156618094
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 5;
+   uint8_t  f1;
+   uint64_t  f2;
+   uint16_t  f3;
+};
+#pragma pack(pop)
+
