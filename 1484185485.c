de202091051037a3033c26ed0f3fd9ecdda64904
saisi <saisi@users.noreply.github.com>
2017-01-12 01:44:45 +0000

    1484185485 amoved

---
+++ 1484185485.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      355039402
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint16_t  f1;
+   int8_t * const  volatile  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static int8_t g_5 = (-2L);
+static int8_t *g_4[10][10][2] = {{{&g_5,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{&g_5,(void*)0}},{{&g_5,(void*)0},{&g_5,&g_5},{&g_5,(void*)0},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,(void*)0},{(void*)0,&g_5},{&g_5,&g_5}},{{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{(void*)0,&g_5},{(void*)0,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{&g_5,&g_5}},{{(void*)0,&g_5},{&g_5,&g_5},{(void*)0,(void*)0},{&g_5,&g_5},{(void*)0,(void*)0},{&g_5,(void*)0},{&g_5,(void*)0},{&g_5,&g_5},{&g_5,&g_5},{&g_5,(void*)0}},{{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5}},{{&g_5,(void*)0},{&g_5,(void*)0},{&g_5,(void*)0},{&g_5,(void*)0},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5}},{{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,(void*)0},{&g_5,&g_5},{&g_5,&g_5},{&g_5,(void*)0},{&g_5,(void*)0},{&g_5,(void*)0}},{{(void*)0,&g_5},{&g_5,(void*)0},{(void*)0,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{(void*)0,&g_5},{(void*)0,&g_5}},{{(void*)0,&g_5},{&g_5,&g_5},{&g_5,&g_5},{(void*)0,&g_5},{&g_5,&g_5},{(void*)0,(void*)0},{&g_5,&g_5},{(void*)0,(void*)0},{&g_5,(void*)0},{&g_5,(void*)0}},{{&g_5,&g_5},{&g_5,&g_5},{&g_5,(void*)0},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5},{&g_5,&g_5}}};
