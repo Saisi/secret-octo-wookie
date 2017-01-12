7cdc98fdb53051f744548f2fe5b741783a51e6e9
saisi <saisi@users.noreply.github.com>
2017-01-12 23:46:50 +0000

    1484264810 additivity

---
+++ 1484264810.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2196944340
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = 0x1F90EF30L;
+static int32_t *g_9[10] = {&g_10,(void*)0,&g_10,&g_10,(void*)0,&g_10,&g_10,(void*)0,&g_10,&g_10};
+static uint32_t g_63 = 0x610563ABL;
