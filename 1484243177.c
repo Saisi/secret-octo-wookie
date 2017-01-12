e5bf2f98087cd9558324fb60eced0fa2c02b50de
saisi <saisi@users.noreply.github.com>
2017-01-12 17:46:20 +0000

    1484243177 antacids

---
+++ 1484243177.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1094057986
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   uint16_t  f1;
+   int32_t  f2;
+};
+
+union U1 {
+   const uint16_t  f0;
+   int8_t * f1;
+   volatile signed f2 : 6;
