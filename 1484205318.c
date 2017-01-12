61f9f9df5f92286c0e056085ac8ac7fb39d62304
saisi <saisi@users.noreply.github.com>
2017-01-12 07:15:18 +0000

    1484205318 AUTOVON

---
+++ 1484205318.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2372964310
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_5 = 0x19C5A654FDDD3F46LL;/* VOLATILE GLOBAL g_5 */
+static int16_t g_7 = 0x9892L;
+static volatile uint64_t g_8[10] = {0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL,0xC3EC3D4E703026D8LL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
