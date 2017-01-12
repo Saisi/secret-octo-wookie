f41e57acb51f008619bc99d69e3d5fd17795f0e5
saisi <saisi@users.noreply.github.com>
2017-01-12 10:15:33 +0000

    1484216133 Acanthocephali

---
+++ 1484216133.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      153184280
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 26;
+   volatile signed f1 : 7;
+   signed f2 : 13;
+   const int16_t  f3;
+   unsigned f4 : 22;
+   volatile unsigned f5 : 10;
+   unsigned f6 : 3;
+};
+
