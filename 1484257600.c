f9733a4ed4102bf85adf59e9733d6e68817a4226
saisi <saisi@users.noreply.github.com>
2017-01-12 21:46:41 +0000

    1484257600 airsheds

---
+++ 1484257600.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2698259310
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 5;
+   signed f1 : 6;
+   unsigned f2 : 16;
+   int32_t  f3;
+   volatile unsigned f4 : 29;
+   const signed f5 : 21;
+};
+
+#pragma pack(push)
