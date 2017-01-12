d70b9af784d7e94e2a652043476bdf22a390df53
saisi <saisi@users.noreply.github.com>
2017-01-12 10:45:36 +0000

    1484217935 allurements

---
+++ 1484217935.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      937636701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x82062403L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[10] = {(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L),(-8L)};
+static int32_t g_4 = 0x3D837BC7L;
+static int32_t g_8[3] = {0x2961C0F8L,0x2961C0F8L,0x2961C0F8L};
+static int16_t g_47 = 1L;
+static int16_t g_53 = 0L;
+static int16_t *g_52 = &g_53;
+static int8_t g_57 = (-1L);
+static const int32_t *g_65 = (void*)0;
