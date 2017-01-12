093ec195e3effe97f0cb9abe1a65cce38e8cd234
saisi <saisi@users.noreply.github.com>
2017-01-12 16:46:13 +0000

    1484239571 artifacts

---
+++ 1484239571.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      749237076
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 24;
+   volatile unsigned f1 : 13;
+   uint32_t  f2;
+   signed f3 : 19;
+   signed f4 : 12;
+   unsigned f5 : 8;
+   signed f6 : 9;
