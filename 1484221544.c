5324063d1b3673ea57ceab0b5e8c75e2c4f072a4
saisi <saisi@users.noreply.github.com>
2017-01-12 11:45:44 +0000

    1484221544 areche

---
+++ 1484221544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3428990736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   int32_t  f1;
+   const volatile signed f2 : 9;
+   unsigned f3 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_14 = 0x6C5165352F76EC75LL;
+static int32_t g_72 = 3L;
