030bb8b136ccd0dfc7b04ecaa419b18ae66ebc65
saisi <saisi@users.noreply.github.com>
2017-01-12 13:45:54 +0000

    1484228754 alsatian

---
+++ 1484228754.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3250534443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   const unsigned f0 : 2;
+};
+
+union U2 {
