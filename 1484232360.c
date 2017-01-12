9c9a6b310a86de2aa79072147353945de0877b63
saisi <saisi@users.noreply.github.com>
2017-01-12 14:46:00 +0000

    1484232360 agriologist

---
+++ 1484232360.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2839671952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   volatile uint32_t  f2;
+   volatile signed f3 : 17;
+   volatile int8_t * volatile  f4;
+};
+
+union U1 {
+   int32_t  f0;
