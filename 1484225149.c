a07700aeac60c7396ad9bbd2d6c03645dd51a2ff
saisi <saisi@users.noreply.github.com>
2017-01-12 12:45:49 +0000

    1484225149 azurous

---
+++ 1484225149.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4210984253
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_3[5] = {0x00L,0x00L,0x00L,0x00L,0x00L};
+static int64_t g_27 = 0x6E40424B969B42C5LL;
+static int8_t g_36 = 0x49L;
+static uint8_t g_43 = 1UL;
+static uint8_t *g_42 = &g_43;
+static int32_t g_45 = 0L;
+static int32_t g_72 = (-1L);
+static int32_t g_73 = (-1L);
+static volatile uint8_t g_74 = 0xC0L;/* VOLATILE GLOBAL g_74 */
