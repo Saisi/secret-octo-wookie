cb9920acd6f849dccab1e566ac969006a875ce47
saisi <saisi@users.noreply.github.com>
2017-01-12 15:46:06 +0000

    1484235966 aeolian

---
+++ 1484235966.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3415198016
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_4 = 0x0647DC4B4E1265ABLL;
+static int32_t g_39 = 0xABEBF3B8L;
+static int32_t g_40 = 0L;
+static uint32_t g_49 = 1UL;
+static int32_t g_56[8][5][1] = {{{(-1L)},{0xC3F121F2L},{0x653DDB20L},{(-4L)},{0x653DDB20L}},{{0xC3F121F2L},{(-1L)},{0xC3F121F2L},{0x653DDB20L},{(-4L)}},{{0x653DDB20L},{0xC3F121F2L},{(-1L)},{0xC3F121F2L},{0x653DDB20L}},{{(-4L)},{0x653DDB20L},{0xC3F121F2L},{(-1L)},{0xC3F121F2L}},{{0x653DDB20L},{(-4L)},{0x653DDB20L},{0xC3F121F2L},{(-1L)}},{{0xC3F121F2L},{0x653DDB20L},{(-4L)},{0x653DDB20L},{0xC3F121F2L}},{{(-1L)},{0xC3F121F2L},{0x653DDB20L},{(-4L)},{0x653DDB20L}},{{0xC3F121F2L},{(-1L)},{0xC3F121F2L},{0x653DDB20L},{(-4L)}}};
+static uint32_t g_59 = 0UL;
+static uint32_t *g_58[9][3] = {{&g_59,&g_59,&g_59},{&g_59,&g_59,&g_59},{&g_59,&g_59,(void*)0},{&g_59,&g_59,&g_59},{(void*)0,&g_59,&g_59},{&g_59,&g_59,&g_59},{(void*)0,(void*)0,&g_59},{&g_59,&g_59,&g_59},{&g_59,(void*)0,&g_59}};
+static uint32_t **g_57 = &g_58[4][1];
+static uint32_t **g_61 = &g_58[4][1];
