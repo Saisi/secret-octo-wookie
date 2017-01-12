69385cee14bd3adb7ddbd60437bdbfb325f41192
saisi <saisi@users.noreply.github.com>
2017-01-12 03:44:56 +0000

    1484192696 amniote

---
+++ 1484192696.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3567725818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_6[3] = {1UL,1UL,1UL};
+static int32_t g_8 = 0x837182ABL;
+static uint64_t g_19[10][1][2] = {{{8UL,18446744073709551615UL}},{{18446744073709551615UL,8UL}},{{18446744073709551615UL,18446744073709551615UL}},{{8UL,18446744073709551615UL}},{{18446744073709551615UL,8UL}},{{18446744073709551615UL,18446744073709551615UL}},{{8UL,18446744073709551615UL}},{{18446744073709551615UL,8UL}},{{18446744073709551615UL,18446744073709551615UL}},{{8UL,18446744073709551615UL}}};
+static uint32_t g_68 = 0x9C99C4DBL;
+static uint8_t g_75 = 0xBBL;
+static int32_t g_79 = 0L;
+static int64_t g_80[3] = {0xBD3396AC63FEC725LL,0xBD3396AC63FEC725LL,0xBD3396AC63FEC725LL};
+static int16_t g_99 = 0x821FL;
+static int16_t *g_98 = &g_99;
