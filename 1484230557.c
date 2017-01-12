ff3a5d9383ff9eb46598766b4196286aae5e3988
saisi <saisi@users.noreply.github.com>
2017-01-12 14:15:58 +0000

    1484230557 autooxidation

---
+++ 1484230557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      291952632
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4[3][9] = {{0x167FA5C6L,0x6960F57AL,0x167FA5C6L,0x167FA5C6L,0x6960F57AL,0x167FA5C6L,0x167FA5C6L,0x6960F57AL,0x167FA5C6L},{18446744073709551612UL,2UL,18446744073709551612UL,18446744073709551612UL,2UL,18446744073709551612UL,18446744073709551612UL,2UL,18446744073709551612UL},{0x167FA5C6L,0x6960F57AL,0x167FA5C6L,0x167FA5C6L,0x6960F57AL,0x167FA5C6L,0x167FA5C6L,0x6960F57AL,0x167FA5C6L}};
+static volatile int32_t g_50 = (-1L);/* VOLATILE GLOBAL g_50 */
+static volatile int32_t * volatile g_49 = &g_50;/* VOLATILE GLOBAL g_49 */
+static const int32_t *g_51 = (void*)0;
+static int32_t g_53 = 8L;
+static int32_t g_55 = 0x510A7412L;
+static int32_t * volatile g_54 = &g_55;/* VOLATILE GLOBAL g_54 */
+static int16_t g_79 = 6L;
+static int8_t g_82 = 1L;
