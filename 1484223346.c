c8cdd4cef47387728d1ce328f375bcd4033a0e6a
saisi <saisi@users.noreply.github.com>
2017-01-12 12:15:47 +0000

    1484223346 aetiologist

---
+++ 1484223346.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4038990406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   volatile uint8_t  f1;
+};
+
+union U1 {
+   const signed f0 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
