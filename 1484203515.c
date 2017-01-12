337b1aa426dc5cebc135c876e402a9c1c0094ea1
saisi <saisi@users.noreply.github.com>
2017-01-12 06:45:15 +0000

    1484203515 antisialic

---
+++ 1484203515.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      457035174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   unsigned f1 : 18;
+   unsigned f2 : 12;
+   volatile signed f3 : 8;
+   uint8_t  f4;
+   const unsigned f5 : 17;
+   uint32_t  f6;
+   volatile unsigned f7 : 2;
+   signed f8 : 1;
