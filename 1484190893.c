0d15100a3a8be18ec5d46ce508fc84a76c1ad4d0
saisi <saisi@users.noreply.github.com>
2017-01-12 03:14:54 +0000

    1484190893 antivenin

---
+++ 1484190893.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1791254976
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   const signed f1 : 27;
+   volatile int8_t  f2;
+   const unsigned f3 : 18;
+   unsigned f4 : 29;
+   unsigned f5 : 11;
+   unsigned f6 : 4;
