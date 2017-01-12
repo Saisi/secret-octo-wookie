3bf0b0467bd506b2d58adeba91a6d72db3cf1cae
saisi <saisi@users.noreply.github.com>
2017-01-12 23:16:48 +0000

    1484263008 androgenic

---
+++ 1484263008.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2075016101
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint32_t  f1;
+   uint16_t  f2;
+   uint8_t  f3;
+   volatile uint32_t  f4;
+};
+
+union U1 {
+   int64_t  f0;
