67af328f8e71d5c4ffbebc627de37c402505ab7e
saisi <saisi@users.noreply.github.com>
2017-01-12 11:15:40 +0000

    1484219738 Asherite

---
+++ 1484219738.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2123627299
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const volatile int32_t  f1;
+   uint32_t  f2;
+   const uint64_t  f3;
+   volatile uint32_t  f4;
+};
+
+union U1 {
+   uint16_t  f0;
