f5e53bada63053506685761d10cc203e1491e39b
saisi <saisi@users.noreply.github.com>
2017-01-13 04:17:18 +0000

    1484281038 accumulator

---
+++ 1484281038.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2457177502
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile unsigned f1 : 26;
+   int8_t  f2;
+   signed f3 : 5;
+   const int64_t  f4;
+   volatile uint64_t  f5;
+};
+
+struct S1 {
