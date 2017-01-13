0e4c7734c7f6a4d6f72ef220107edd9fd1f60b25
saisi <saisi@users.noreply.github.com>
2017-01-13 02:17:08 +0000

    1484273828 animisms

---
+++ 1484273828.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1906526245
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile int32_t  f1;
+   signed f2 : 24;
+   int32_t  f3;
+   volatile int32_t  f4;
+   volatile signed f5 : 27;
+   const unsigned f6 : 23;
+   volatile uint8_t  f7;
+};
