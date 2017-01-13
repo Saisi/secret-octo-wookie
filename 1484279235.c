84e4d109fa134fe6238263ea0c93fb2123894ba3
saisi <saisi@users.noreply.github.com>
2017-01-13 03:47:16 +0000

    1484279235 azoturia

---
+++ 1484279235.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      391499019
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   uint16_t  f2;
+};
+
+struct S1 {
+   struct S0  f0;
+   struct S0  f1;
+   signed f2 : 19;
