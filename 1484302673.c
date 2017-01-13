badb7995293d0fab4a11966042240ab4da51bc42
saisi <saisi@users.noreply.github.com>
2017-01-13 10:17:53 +0000

    1484302673 balds

---
+++ 1484302673.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      744687727
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S3 {
+   volatile signed f0 : 15;
+   unsigned f1 : 28;
+   signed f2 : 2;
+   const volatile signed f3 : 25;
+   volatile unsigned f4 : 23;
+};
+#pragma pack(pop)
