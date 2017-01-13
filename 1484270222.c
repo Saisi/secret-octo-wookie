3d9f2f4a559bc7711ee5a6d1273ac240b469f6b2
saisi <saisi@users.noreply.github.com>
2017-01-13 01:17:03 +0000

    1484270222 bakeshop

---
+++ 1484270222.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2966331411
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 8;
+   unsigned f1 : 31;
+   signed f2 : 6;
+   volatile signed f3 : 25;
+   const volatile unsigned f4 : 6;
+   uint32_t  f5;
+   signed f6 : 11;
