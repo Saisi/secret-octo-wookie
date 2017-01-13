dddfdf8c3e4181fd9f736dbdfd7f3f9538cecc81
saisi <saisi@users.noreply.github.com>
2017-01-13 10:47:56 +0000

    1484304476 bandoleros

---
+++ 1484304476.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1247329391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+   volatile uint32_t  f3;
+   int64_t  f4;
+   const uint32_t  f5;
+   uint16_t  f6;
