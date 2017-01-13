0fa134f1523eef402cf5a4c51acc54de5e4da6e3
saisi <saisi@users.noreply.github.com>
2017-01-13 11:17:58 +0000

    1484306278 autosepticemia

---
+++ 1484306278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2169927102
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = (-1L);
+static uint8_t g_43 = 9UL;
+static uint64_t g_44 = 18446744073709551608UL;
+static int8_t g_46 = (-1L);
+static int32_t g_48 = (-1L);
+static uint32_t g_50 = 1UL;
+static uint32_t g_84 = 4294967291UL;
+static volatile uint8_t g_134 = 3UL;/* VOLATILE GLOBAL g_134 */
+static uint64_t g_139 = 0UL;
