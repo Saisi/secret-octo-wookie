311da9476109e84d8642e5b712ea50f6b0be2d61
saisi <saisi@users.noreply.github.com>
2017-01-13 00:16:53 +0000

    1484266612 ambrotype

---
+++ 1484266612.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2918175160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int32_t  f0;
+   volatile uint32_t  f1;
+   uint32_t  f2;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
