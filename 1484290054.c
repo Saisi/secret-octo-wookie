cf661d5e971eca9c8e309e58d43d3015ae34e8a7
saisi <saisi@users.noreply.github.com>
2017-01-13 06:47:35 +0000

    1484290054 alabastos

---
+++ 1484290054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3634980171
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 1UL;
+static int32_t g_44 = 0x2C34CCBBL;
+static uint32_t g_45 = 0x90D790F9L;
+static int32_t *g_77[1][4] = {{(void*)0,(void*)0,(void*)0,(void*)0}};
+static const int32_t g_84 = (-4L);
+static const int32_t g_87[1] = {0x6106A478L};
+static int8_t g_90 = 0xF0L;
+static uint8_t g_96 = 0x91L;
+static int32_t g_98[10] = {0L,0x10A26324L,(-4L),(-4L),0x10A26324L,0L,0x10A26324L,(-4L),(-4L),0x10A26324L};
