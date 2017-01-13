76344fdb546bc81605d0fdb885f5d6456ada9c31
saisi <saisi@users.noreply.github.com>
2017-01-13 20:19:23 +0000

    1484338763 ballon

---
+++ 1484338763.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4125066370
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   volatile int8_t * const  f1;
+};
+
+union U1 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
