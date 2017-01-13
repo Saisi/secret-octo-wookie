95472b414a00bb91e5edee14f2f585e6cc198df6
saisi <saisi@users.noreply.github.com>
2017-01-13 12:18:04 +0000

    1484309884 archetypical

---
+++ 1484309884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1317151660
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int64_t g_9 = 0xD44AD374006310FELL;
