4342e077f4c2f494b83a69449d849be3ebf11b2d
saisi <saisi@users.noreply.github.com>
2017-01-13 15:18:22 +0000

    1484320701 ACAA

---
+++ 1484320701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3253433960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+};
+
+struct S1 {
+   volatile unsigned f0 : 26;
+   unsigned f1 : 22;
+   int32_t  f2;
+   signed f3 : 29;
+   volatile signed f4 : 10;
