5cb8c860c182a4679f06ab8bc7daca4c531155c8
saisi <saisi@users.noreply.github.com>
2017-01-13 18:49:16 +0000

    1484333355 airplanes

---
+++ 1484333355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3856846562
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint32_t  f1;
+   uint32_t  f2;
+   uint64_t  f3;
+};
+
+union U1 {
+   volatile int8_t * volatile  f0;
+   int32_t  f1;
