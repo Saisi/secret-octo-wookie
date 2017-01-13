8fdf6f71be60077f2d1cba21080f40dfc9459736
saisi <saisi@users.noreply.github.com>
2017-01-13 22:49:37 +0000

    1484347776 antislip

---
+++ 1484347776.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1852292827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int32_t  f1;
+   signed f2 : 20;
+   uint16_t  f3;
+   const int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 0x5AL;
