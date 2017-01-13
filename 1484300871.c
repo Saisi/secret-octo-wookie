cf8485fb7ffaa59ff86eef381f634395f03a3cc9
saisi <saisi@users.noreply.github.com>
2017-01-13 09:47:51 +0000

    1484300871 acidimetry

---
+++ 1484300871.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      283442136
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xF57A7C20L;
+static int32_t *g_78 = &g_4;
+static int32_t **g_77[6][3][7] = {{{&g_78,(void*)0,&g_78,&g_78,&g_78,&g_78,&g_78},{&g_78,&g_78,&g_78,&g_78,(void*)0,&g_78,(void*)0},{(void*)0,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78}},{{&g_78,&g_78,&g_78,&g_78,(void*)0,&g_78,&g_78},{&g_78,(void*)0,&g_78,(void*)0,&g_78,&g_78,(void*)0},{&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78}},{{&g_78,(void*)0,&g_78,&g_78,&g_78,&g_78,&g_78},{&g_78,&g_78,(void*)0,&g_78,&g_78,&g_78,&g_78},{&g_78,(void*)0,&g_78,&g_78,&g_78,(void*)0,&g_78}},{{(void*)0,&g_78,&g_78,&g_78,(void*)0,&g_78,&g_78},{&g_78,&g_78,(void*)0,&g_78,&g_78,(void*)0,&g_78},{&g_78,&g_78,(void*)0,&g_78,(void*)0,(void*)0,&g_78}},{{(void*)0,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78},{&g_78,&g_78,&g_78,&g_78,(void*)0,&g_78,&g_78},{&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78}},{{&g_78,&g_78,(void*)0,(void*)0,&g_78,&g_78,&g_78},{&g_78,&g_78,&g_78,&g_78,&g_78,(void*)0,&g_78},{&g_78,&g_78,&g_78,&g_78,&g_78,&g_78,&g_78}}};
+static uint64_t g_81[3] = {0xEB6BA8A7E8B7DD14LL,0xEB6BA8A7E8B7DD14LL,0xEB6BA8A7E8B7DD14LL};
+static uint64_t g_83 = 1UL;
+static int32_t g_91 = (-8L);
+static int32_t g_98 = 0xA847E08AL;
+static volatile int32_t g_114 = 0xD91D694BL;/* VOLATILE GLOBAL g_114 */
+static volatile int32_t * volatile g_113 = &g_114;/* VOLATILE GLOBAL g_113 */
