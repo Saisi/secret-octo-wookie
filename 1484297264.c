228bd9f5c6ae004883bda65edd0e900eaff530cb
saisi <saisi@users.noreply.github.com>
2017-01-13 08:47:45 +0000

    1484297264 archdeacon

---
+++ 1484297264.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2424252687
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   volatile int64_t  f0;
+   uint32_t  f1;
+};
+
