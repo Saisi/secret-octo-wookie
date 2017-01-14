eb9e62951ad00e7077521cdc8d40d45d5e395d6f
saisi <saisi@users.noreply.github.com>
2017-01-14 12:20:50 +0000

    1484396449 addibility

---
+++ 1484396449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2207269744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   int64_t  f0;
+   const int32_t  f1;
+   volatile uint8_t  f2;
