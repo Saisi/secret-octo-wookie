444cdbd2d803a0ef71e16b5dc167f47459bf4fbc
saisi <saisi@users.noreply.github.com>
2017-01-14 13:50:59 +0000

    1484401857 affich

---
+++ 1484401857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2668433790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0x111738ABL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0xD568D526L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0x8588D0D3L;
+static uint32_t g_10 = 1UL;
+static int32_t g_35 = (-8L);
+static volatile int32_t g_41 = 5L;/* VOLATILE GLOBAL g_41 */
