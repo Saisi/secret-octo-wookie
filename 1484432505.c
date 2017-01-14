cf4f9e079e953f58309bfc73f052f46d111d6e0d
saisi <saisi@users.noreply.github.com>
2017-01-14 22:21:49 +0000

    1484432505 anxiety

---
+++ 1484432505.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1040004358
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 17;
+   const volatile unsigned f1 : 2;
+   unsigned f2 : 28;
+   unsigned f3 : 24;
+   unsigned f4 : 7;
+   unsigned f5 : 2;
+   volatile unsigned f6 : 24;
+   signed f7 : 13;
+};
