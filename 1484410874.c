171fb146091651cd7bbfddffdd910cc2a97da663
saisi <saisi@users.noreply.github.com>
2017-01-14 16:21:15 +0000

    1484410874 aspherical

---
+++ 1484410874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      408162367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 30;
+   signed f1 : 29;
+   const signed f2 : 12;
+   unsigned f3 : 7;
+   volatile unsigned f4 : 28;
+   const signed f5 : 5;
+   const signed f6 : 16;
