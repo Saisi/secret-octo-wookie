2b9913939096af582f40e4e83c810382700b1097
saisi <saisi@users.noreply.github.com>
2017-01-14 15:21:09 +0000

    1484407268 banjara

---
+++ 1484407268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      779831082
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   int32_t  f1;
+   signed f2 : 20;
+   volatile signed f3 : 6;
+   int32_t  f4;
+   unsigned f5 : 20;
+   uint8_t  f6;
+   const signed f7 : 25;
+   unsigned f8 : 19;
