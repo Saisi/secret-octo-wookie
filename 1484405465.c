32ccf9ae3ce88f9980babf20cba57ea13d9b6ef4
saisi <saisi@users.noreply.github.com>
2017-01-14 14:51:06 +0000

    1484405465 anorectal

---
+++ 1484405465.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2054894961
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 23;
+   int8_t  f1;
+};
+
+union U1 {
+   const volatile int8_t  f0;
+   const signed f1 : 29;
+};
+
