78debd00b67af2c8d89844a8748b05e02fe7c8bc
saisi <saisi@users.noreply.github.com>
2017-01-14 11:20:45 +0000

    1484392844 Alcatraz

---
+++ 1484392844.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      458718527
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint32_t  f1;
+   int64_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_6 = 4UL;/* VOLATILE GLOBAL g_6 */
+static uint32_t g_12 = 1UL;
