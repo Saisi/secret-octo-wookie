35f369aaae828c39191f6ed08c88375950792134
saisi <saisi@users.noreply.github.com>
2017-01-14 10:50:42 +0000

    1484391042 Arvind

---
+++ 1484391042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3747394892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 20;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_5 = 0x9CFE04822B165808LL;
+static const uint32_t g_8 = 0xBC662A2BL;
