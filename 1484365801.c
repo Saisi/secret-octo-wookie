894c3ff50b0c97dd17fee5882f49126f3d72571b
saisi <saisi@users.noreply.github.com>
2017-01-14 03:50:02 +0000

    1484365801 Alydar

---
+++ 1484365801.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3287054145
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint64_t  f1;
+};
+
+union U1 {
+   uint8_t  f0;
+   int64_t  f1;
+   const int32_t  f2;
+   const int16_t  f3;
