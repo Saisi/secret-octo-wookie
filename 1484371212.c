4c29a7e0b986153bc5a2d45e38d68b5723454408
saisi <saisi@users.noreply.github.com>
2017-01-14 05:20:13 +0000

    1484371212 alfresco

---
+++ 1484371212.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1805418135
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const uint32_t  f0;
+   const volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U1 g_3 = {18446744073709551615UL};/* VOLATILE GLOBAL g_3 */
+static union U1 ** volatile g_5 = (void*)0;/* VOLATILE GLOBAL g_5 */
+static union U1 *g_7 = &g_3;
+static union U1 ** volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
