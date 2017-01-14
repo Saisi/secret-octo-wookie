0c4a7d50e3e048b9bd1f9e343eb0553de267af5f
saisi <saisi@users.noreply.github.com>
2017-01-14 04:50:10 +0000

    1484369410 adorner

---
+++ 1484369410.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1333909907
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 8;
+   const uint32_t  f1;
+   volatile int16_t  f2;
+   const volatile unsigned f3 : 23;
+   int32_t  f4;
+   const volatile int32_t  f5;
+   int16_t  f6;
