07b23fc4a729470642359e2aa9398250c135536f
saisi <saisi@users.noreply.github.com>
2017-01-14 17:21:21 +0000

    1484414480 Archaeocyathidae

---
+++ 1484414480.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1089688832
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   uint64_t  f1;
+   uint16_t  f2;
+   const uint16_t  f3;
+   signed f4 : 29;
+   unsigned f5 : 11;
+   volatile int8_t  f6;
+};
+
