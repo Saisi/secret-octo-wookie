4017a90461333fec55d1d3d10bf9f7e01d22e4ec
saisi <saisi@users.noreply.github.com>
2017-01-14 05:50:15 +0000

    1484373015 baccae

---
+++ 1484373015.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2076221107
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   const uint8_t  f1;
+   volatile unsigned f2 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_7 = 1L;
+static int32_t g_23 = (-8L);
+static volatile int16_t g_24 = 5L;/* VOLATILE GLOBAL g_24 */
