a27b53da3abbc04026c182162d03204fe35984d9
saisi <saisi@users.noreply.github.com>
2017-01-14 08:50:31 +0000

    1484383831 Ardelia

---
+++ 1484383831.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1867147631
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7[4] = {0x7C8A6B95L,0x7C8A6B95L,0x7C8A6B95L,0x7C8A6B95L};
+static uint8_t g_61[3] = {248UL,248UL,248UL};
+static int16_t g_73 = 1L;
+static int16_t g_76 = 0x5372L;
+static int32_t g_88 = 0xE80F9606L;
+static int32_t g_111 = 1L;
+static int64_t g_125 = 9L;
+static int32_t g_126 = 0xE340D7ECL;
+static int8_t g_133 = (-1L);
