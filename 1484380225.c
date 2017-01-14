765ddabae0aff618ee8c75805fed489149f7f209
saisi <saisi@users.noreply.github.com>
2017-01-14 07:50:26 +0000

    1484380225 adrop

---
+++ 1484380225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2205382492
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-7L);
+static uint64_t g_29 = 0UL;
+static int8_t g_31 = 0L;
+static uint8_t g_34[2] = {0x23L,0x23L};
+static int32_t g_38[4][6] = {{(-6L),(-4L),(-4L),(-6L),(-4L),(-4L)},{(-6L),(-4L),(-4L),(-6L),(-4L),(-4L)},{(-6L),(-4L),(-4L),(-6L),(-4L),(-4L)},{(-6L),(-4L),(-4L),(-6L),(-4L),(-4L)}};
