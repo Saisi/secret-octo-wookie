a41fbace9cbf6a88f1a41d4b716cb467fb976934
saisi <saisi@users.noreply.github.com>
2017-01-15 06:22:38 +0000

    1484461355 Abrahamite

---
+++ 1484461355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1562540309
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 28;
+   volatile signed f2 : 15;
+   signed f3 : 30;
+   const unsigned f4 : 23;
+   volatile signed f5 : 20;
+   signed f6 : 3;
