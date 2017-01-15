dae937e4a9c32cb2e198ba762a6d7df3b144cd17
saisi <saisi@users.noreply.github.com>
2017-01-15 03:22:15 +0000

    1484450535 arterialising

---
+++ 1484450535.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2062138998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_3[2][1] = {{0x08901F2CL},{0x08901F2CL}};
+static const int32_t *g_2 = &g_3[1][0];
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
