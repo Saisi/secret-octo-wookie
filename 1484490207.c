9225e55b5b101b3e0c51c326f663fa05f430510e
saisi <saisi@users.noreply.github.com>
2017-01-15 14:23:27 +0000

    1484490207 alacrities

---
+++ 1484490207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3591784982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[8] = {0L,(-9L),0L,0L,(-9L),0L,0L,(-9L)};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
