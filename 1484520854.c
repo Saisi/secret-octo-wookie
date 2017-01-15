056d41798e5d5c71d7f557084c4e6e3b45b38dc2
saisi <saisi@users.noreply.github.com>
2017-01-15 22:54:15 +0000

    1484520854 backsplice

---
+++ 1484520854.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1092408837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   uint32_t  f1;
+   const uint8_t  f2;
+   const int16_t  f3;
+   volatile int16_t  f4;
+   const signed f5 : 15;
+   volatile uint8_t  f6;
+   uint16_t  f7;
+   uint64_t  f8;
