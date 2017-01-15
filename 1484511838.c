299cfaebd147e20c55b79d4edff16bb9e086ec8b
saisi <saisi@users.noreply.github.com>
2017-01-15 20:23:58 +0000

    1484511838 abnormity

---
+++ 1484511838.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      362958640
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 6;
+   const signed f1 : 3;
+   volatile signed f2 : 25;
+   volatile signed f3 : 1;
+   uint32_t  f4;
+};
+#pragma pack(pop)
