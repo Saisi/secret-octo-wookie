49790f09daca9238329d28d3118e3885b99c5524
saisi <saisi@users.noreply.github.com>
2017-01-15 06:52:41 +0000

    1484463160 ayurvedas

---
+++ 1484463160.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3821574239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCCC90A35L;
+static uint8_t g_4 = 0UL;
+static int32_t g_8 = 0xAD67A4BEL;
+static uint64_t g_14 = 0xEE1D11023DCDF5EFLL;
+static int32_t ** volatile g_82 = (void*)0;/* VOLATILE GLOBAL g_82 */
