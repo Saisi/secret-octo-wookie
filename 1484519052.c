23e60596e183ad1cf8694a744743867b4d13d520
saisi <saisi@users.noreply.github.com>
2017-01-15 22:24:12 +0000

    1484519052 Arsinous

---
+++ 1484519052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      691929527
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int32_t  f1;
+   volatile int16_t  f2;
+   uint8_t  f3;
+   int16_t  f4;
+};
+
+union U1 {
+   int32_t  f0;
