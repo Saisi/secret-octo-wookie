28a6d53b68cd33090bcf52cf126e58fa58348f48
saisi <saisi@users.noreply.github.com>
2017-01-15 02:22:10 +0000

    1484446930 Asclepieion

---
+++ 1484446930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3347614868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x91C5534BL;
+static uint32_t g_5 = 0x14CD4959L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
