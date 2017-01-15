d9a0a79b3063686f482c2f08df7bee095c188f71
saisi <saisi@users.noreply.github.com>
2017-01-15 07:52:46 +0000

    1484466766 barbiturism

---
+++ 1484466766.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3949406595
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+};
+
+union U1 {
+   uint16_t  f0;
+   const unsigned f1 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
