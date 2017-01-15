e3b57bfc1492e718de35b06d473ff0cc384de164
saisi <saisi@users.noreply.github.com>
2017-01-15 11:23:11 +0000

    1484479390 aircheck

---
+++ 1484479390.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1366093528
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 26;
+   volatile uint64_t  f1;
+   int64_t  f2;
+   volatile int8_t  f3;
+   int8_t * const  volatile  f4;
+};
+
+union U1 {
+   volatile signed f0 : 16;
