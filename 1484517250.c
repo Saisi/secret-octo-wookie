a3a4ae51268bf24ca74822ca5b4fca77e2a11b13
saisi <saisi@users.noreply.github.com>
2017-01-15 21:54:10 +0000

    1484517250 Anthropomorpha

---
+++ 1484517250.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      203287447
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const unsigned f1 : 15;
+   int8_t  f2;
+   int32_t  f3;
+   volatile unsigned f4 : 5;
+   unsigned f5 : 20;
+   const signed f6 : 8;
+   int16_t  f7;
+   signed f8 : 6;
