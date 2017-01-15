bdd5b80e8c34dc7476996daad7f9bcdd5415c7f2
saisi <saisi@users.noreply.github.com>
2017-01-15 02:52:13 +0000

    1484448732 Aba

---
+++ 1484448732.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3661222719
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   volatile int32_t  f1;
+   volatile int8_t  f2;
+};
+
+union U1 {
+   const volatile unsigned f0 : 13;
+   const int64_t  f1;
+   volatile int64_t  f2;
