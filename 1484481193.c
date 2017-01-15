2eb4eb5a4517284eabba443fc99ea9ee0700c998
saisi <saisi@users.noreply.github.com>
2017-01-15 11:53:14 +0000

    1484481193 atmolyze

---
+++ 1484481193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4190348264
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB108F825L;
+static int64_t g_32 = 0xE809B4AF7BDD4840LL;
+static uint64_t g_34 = 3UL;
+static int32_t g_45 = 1L;
+static volatile uint32_t g_46 = 0x3C5A4A10L;/* VOLATILE GLOBAL g_46 */
+static uint32_t g_49 = 2UL;
+static int64_t g_55 = 0x763964E48C0C84A4LL;
+static int64_t g_58 = 0xCC114FF6A91A76A4LL;
+static int32_t g_60[10] = {0x662B132DL,(-1L),0x662B132DL,0x662B132DL,(-1L),0x662B132DL,0x662B132DL,(-1L),0x662B132DL,0x662B132DL};
