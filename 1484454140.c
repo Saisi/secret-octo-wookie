61a851adf589066de1232df72b0c7b15fc4c4efe
saisi <saisi@users.noreply.github.com>
2017-01-15 04:22:25 +0000

    1484454140 Bambuba

---
+++ 1484454140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3599218520
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 26;
+   volatile unsigned f1 : 7;
+   unsigned f2 : 13;
+   signed f3 : 5;
+   signed : 0;
+   unsigned f4 : 23;
+   volatile unsigned f5 : 25;
