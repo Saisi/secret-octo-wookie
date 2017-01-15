e92686f86c3a697692933d6d5d02290d9848e93d
saisi <saisi@users.noreply.github.com>
2017-01-15 04:52:27 +0000

    1484455947 bankbook

---
+++ 1484455947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2953473039
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   unsigned f1 : 5;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 8L;
+static int32_t *g_12 = &g_13;
+static int32_t g_44[1][2][2] = {{{0x9F8C5172L,0x9F8C5172L},{0x9F8C5172L,0x9F8C5172L}}};
