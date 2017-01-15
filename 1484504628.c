6fb3a4db78ed33f00417501459fab5f13d56edfd
saisi <saisi@users.noreply.github.com>
2017-01-15 18:23:48 +0000

    1484504628 Ballentine

---
+++ 1484504628.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      241519078
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_4[4] = {0x55L,0x55L,0x55L,0x55L};
+static uint16_t g_24 = 0xF456L;
+static int32_t g_28[3][1] = {{0xDDB74A71L},{0xDDB74A71L},{0xDDB74A71L}};
+static uint16_t g_31 = 0x280BL;
+static uint8_t g_44 = 255UL;
+static int32_t g_72 = 3L;
+static volatile uint32_t g_73 = 4294967295UL;/* VOLATILE GLOBAL g_73 */
+static uint32_t g_110[4] = {7UL,7UL,7UL,7UL};
+static int32_t g_144[2][3] = {{0xBFABC748L,0L,0L},{0xBFABC748L,0L,0L}};
