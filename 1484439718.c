4c10c57f48ec70b4165bdc1d0f25fdf0d7735bea
saisi <saisi@users.noreply.github.com>
2017-01-15 00:21:59 +0000

    1484439718 armlike

---
+++ 1484439718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      261203261
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   int8_t  f1;
+   volatile uint16_t  f2;
+   signed f3 : 22;
+   int32_t  f4;
+   const volatile uint32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
