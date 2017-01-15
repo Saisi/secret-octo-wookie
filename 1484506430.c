13612f4bd5115ea154ed73d0d6d4d25d37aac752
saisi <saisi@users.noreply.github.com>
2017-01-15 18:53:51 +0000

    1484506430 aheap

---
+++ 1484506430.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      970898732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   const volatile int32_t  f1;
+   int32_t  f2;
+   int32_t  f3;
+   signed f4 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xA9AC70C3L;/* VOLATILE GLOBAL g_2 */
