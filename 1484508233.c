0254c971724027ad9085b615de9c16389d68173e
saisi <saisi@users.noreply.github.com>
2017-01-15 19:23:54 +0000

    1484508233 arthrogenous

---
+++ 1484508233.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2277052404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   const int64_t  f1;
+   unsigned f2 : 4;
+};
+
+union U1 {
+   uint64_t  f0;
+};
+
