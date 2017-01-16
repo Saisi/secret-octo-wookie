ed071cec6ba9aa5637270166dbd2350ad5bc6298
saisi <saisi@users.noreply.github.com>
2017-01-16 09:25:16 +0000

    1484558716 Alic

---
+++ 1484558716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4008102675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 31;
+   unsigned f2 : 6;
+   unsigned f3 : 25;
+   signed f4 : 6;
+   unsigned f5 : 7;
+   volatile unsigned f6 : 10;
