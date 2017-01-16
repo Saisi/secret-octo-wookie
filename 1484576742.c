3a04084b129b782eae806fe77472fc89634a607b
saisi <saisi@users.noreply.github.com>
2017-01-16 14:25:43 +0000

    1484576742 awhir

---
+++ 1484576742.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      553170441
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 3;
+   signed f1 : 14;
+   signed f2 : 25;
+   const unsigned : 0;
+   unsigned f3 : 17;
+   const signed f4 : 6;
+   const signed f5 : 13;
