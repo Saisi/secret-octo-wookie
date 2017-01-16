8a7bb6df55ee5d3de2458d1a0149af3f437f3a1a
saisi <saisi@users.noreply.github.com>
2017-01-16 04:24:47 +0000

    1484540686 Barbarea

---
+++ 1484540686.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1317897501
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   unsigned f1 : 2;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_15 = 0x6673E7EDL;
+static uint32_t g_19 = 0xBD0176DFL;
+static uint8_t g_20 = 0xEBL;
