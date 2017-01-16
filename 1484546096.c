81fa9017080b41da514d878fd364882bdb405dbc
saisi <saisi@users.noreply.github.com>
2017-01-16 05:54:56 +0000

    1484546096 acanthad

---
+++ 1484546096.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1155648199
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_13 = 18446744073709551615UL;
+static int32_t g_68[1] = {1L};
+static int32_t g_70 = 1L;
+static uint16_t g_73 = 0UL;
+static uint32_t g_76 = 0xE691445AL;
+static int32_t * volatile g_81 = (void*)0;/* VOLATILE GLOBAL g_81 */
+static volatile int32_t g_92[7][1][10] = {{{0L,0L,0x3AC0F783L,0L,0L,0x3AC0F783L,0L,0L,0x3AC0F783L,0L}},{{0L,9L,9L,0L,9L,9L,0L,9L,9L,0L}},{{9L,0L,9L,9L,0L,9L,9L,0L,9L,9L}},{{0L,0L,0x3AC0F783L,0L,0L,0x3AC0F783L,0L,0L,0x3AC0F783L,0L}},{{0L,9L,9L,0L,9L,0x3AC0F783L,9L,0x3AC0F783L,0x3AC0F783L,9L}},{{0x3AC0F783L,9L,0x3AC0F783L,0x3AC0F783L,9L,0x3AC0F783L,0x3AC0F783L,9L,0x3AC0F783L,0x3AC0F783L}},{{9L,9L,0L,9L,9L,0L,9L,9L,0L,9L}}};
+static uint8_t g_96 = 0x86L;
+static int8_t g_122 = 0x4CL;
