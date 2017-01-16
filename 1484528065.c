519048e73c21abdcd85a0c3e8093b2fbca4bbddc
saisi <saisi@users.noreply.github.com>
2017-01-16 00:54:26 +0000

    1484528065 asynergia

---
+++ 1484528065.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3152213751
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int16_t  f1;
+   const volatile uint16_t  f2;
+};
+
+union U1 {
+   const uint16_t  f0;
+   unsigned f1 : 25;
+   int8_t * volatile  f2;
