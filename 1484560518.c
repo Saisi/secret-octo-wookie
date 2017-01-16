8cdb91480e856ca13412679e7fde9ac47e72291f
saisi <saisi@users.noreply.github.com>
2017-01-16 09:55:19 +0000

    1484560518 Ajanta

---
+++ 1484560518.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1090780020
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   const volatile int8_t * f3;
+   int32_t  f4;
+};
+
+union U1 {
+   volatile int16_t  f0;
