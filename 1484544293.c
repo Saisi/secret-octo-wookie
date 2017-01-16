6128b3d2201e932085b2ba39b88a85b132d67d1b
saisi <saisi@users.noreply.github.com>
2017-01-16 05:24:54 +0000

    1484544293 amniotic

---
+++ 1484544293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2447660242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   unsigned f1 : 16;
+   const unsigned f2 : 18;
+   unsigned f3 : 22;
+};
+#pragma pack(pop)
+
