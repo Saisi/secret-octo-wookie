dcd861162df847eae29980a21fdb7a984234eb93
saisi <saisi@users.noreply.github.com>
2017-01-16 18:56:09 +0000

    1484592969 antiricin

---
+++ 1484592969.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1944221324
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static int16_t g_16 = 0x7921L;
+static int32_t g_17 = 0x6F93BD5DL;
+static uint16_t g_18 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
