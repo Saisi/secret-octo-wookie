e9c4ae9079b872dfb8a91f4fe89a8b5660c1ce15
saisi <saisi@users.noreply.github.com>
2017-01-16 17:26:00 +0000

    1484587559 agtbasic

---
+++ 1484587559.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1443182704
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint32_t  f1;
+   const volatile int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4] = {0L,0L,0L,0L};
+static int32_t g_4 = 1L;
+static int32_t g_7 = 1L;
