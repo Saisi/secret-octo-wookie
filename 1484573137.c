8797d9a7cb7c7f88f844dc9ff8bc21b376e6b08f
saisi <saisi@users.noreply.github.com>
2017-01-16 13:25:38 +0000

    1484573137 akroteria

---
+++ 1484573137.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2558152766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-4L);
+static int32_t g_5 = (-5L);
+static int32_t g_24 = 1L;
+static volatile uint32_t g_38 = 0x377D6446L;/* VOLATILE GLOBAL g_38 */
+static uint32_t g_47 = 4UL;
+static int32_t g_49 = 0xC53D8B3DL;
+static int32_t *g_109 = &g_24;
+static volatile int32_t g_117[3] = {0x6C5B50A7L,0x6C5B50A7L,0x6C5B50A7L};
+static int32_t g_118[2][6] = {{(-1L),(-2L),(-1L),(-1L),(-2L),(-1L)},{(-1L),(-2L),(-1L),(-1L),(-2L),(-1L)}};
