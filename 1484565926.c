7ebd251df3603656f078155c3087991ee65e44f9
saisi <saisi@users.noreply.github.com>
2017-01-16 11:25:26 +0000

    1484565926 bandstand

---
+++ 1484565926.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3539389689
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile uint8_t  f1;
+   int32_t  f2;
+   signed f3 : 24;
+   unsigned f4 : 9;
+};
+
+union U1 {
+   uint16_t  f0;
