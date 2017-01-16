b644e2eb427d4821b3864f624feb3d65a0c6a202
saisi <saisi@users.noreply.github.com>
2017-01-16 15:55:51 +0000

    1484582151 anthraquinone

---
+++ 1484582151.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1326828098
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   volatile unsigned f1 : 12;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_10 = {0x97E780CA9F8D19C9LL,2};/* VOLATILE GLOBAL g_10 */
