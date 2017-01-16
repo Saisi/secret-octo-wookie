8379d8a60c50c081f6b9b1a11bd8ac2a2bb315a1
saisi <saisi@users.noreply.github.com>
2017-01-16 07:55:09 +0000

    1484553308 amaroid

---
+++ 1484553308.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1845942276
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_7 = 6UL;
+static volatile int32_t g_16 = 0xB4E76AB2L;/* VOLATILE GLOBAL g_16 */
+static int32_t g_18 = 4L;
+static uint16_t g_21 = 0x6C19L;
+static int16_t g_35 = (-1L);
+static int32_t g_38 = 0xC9063832L;
+static int32_t * volatile g_37 = &g_38;/* VOLATILE GLOBAL g_37 */
+static uint8_t g_68 = 253UL;
+static int32_t **g_78 = (void*)0;
