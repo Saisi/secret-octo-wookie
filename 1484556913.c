70ec7bc9756d14a3c219da2e4e1dfd164ba5a421
saisi <saisi@users.noreply.github.com>
2017-01-16 08:55:13 +0000

    1484556913 Alain

---
+++ 1484556913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3226260239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x7B1C2A2CL;
+static int32_t g_13 = (-1L);
+static volatile int64_t g_24[3] = {0xC80F219A6FDD8FB8LL,0xC80F219A6FDD8FB8LL,0xC80F219A6FDD8FB8LL};
+static volatile int8_t g_26 = 1L;/* VOLATILE GLOBAL g_26 */
+static uint64_t g_27 = 3UL;
+static int32_t g_83 = 3L;
+static int32_t *g_91[10][6][4] = {{{&g_6,(void*)0,(void*)0,&g_6},{(void*)0,&g_13,(void*)0,&g_13},{&g_13,&g_83,&g_13,(void*)0},{&g_13,(void*)0,&g_83,(void*)0},{(void*)0,&g_83,&g_13,&g_13},{&g_83,&g_13,(void*)0,&g_6}},{{&g_13,(void*)0,&g_6,&g_6},{&g_13,&g_13,(void*)0,(void*)0},{&g_83,&g_6,&g_13,&g_13},{(void*)0,&g_13,&g_83,&g_13},{&g_13,&g_13,&g_13,&g_83},{(void*)0,(void*)0,&g_83,&g_13}},{{&g_13,(void*)0,&g_6,(void*)0},{&g_13,&g_6,&g_6,&g_13},{&g_13,&g_83,&g_83,&g_6},{(void*)0,(void*)0,&g_6,&g_13},{&g_6,&g_13,&g_83,&g_13},{&g_6,(void*)0,(void*)0,&g_6}},{{&g_13,&g_83,&g_13,&g_13},{(void*)0,&g_6,(void*)0,(void*)0},{(void*)0,(void*)0,&g_13,&g_13},{&g_13,(void*)0,(void*)0,&g_83},{&g_6,(void*)0,&g_83,(void*)0},{&g_6,(void*)0,&g_6,&g_83}},{{(void*)0,(void*)0,&g_83,&g_13},{&g_13,(void*)0,&g_6,(void*)0},{&g_13,&g_6,&g_6,&g_13},{&g_13,&g_83,&g_83,&g_6},{(void*)0,(void*)0,&g_6,&g_13},{&g_6,&g_13,&g_83,&g_13}},{{&g_6,(void*)0,(void*)0,&g_6},{&g_13,&g_83,&g_13,&g_13},{(void*)0,&g_6,(void*)0,(void*)0},{(void*)0,(void*)0,&g_13,&g_13},{&g_13,(void*)0,(void*)0,&g_83},{&g_6,(void*)0,&g_83,(void*)0}},{{&g_6,(void*)0,&g_6,&g_83},{(void*)0,(void*)0,&g_83,&g_13},{&g_13,(void*)0,&g_6,(void*)0},{&g_13,&g_6,&g_6,&g_13},{&g_13,&g_83,&g_83,&g_6},{(void*)0,(void*)0,&g_6,&g_13}},{{&g_6,&g_13,&g_83,&g_13},{&g_6,(void*)0,(void*)0,&g_6},{&g_13,&g_83,&g_13,&g_13},{(void*)0,&g_6,(void*)0,(void*)0},{(void*)0,(void*)0,&g_13,&g_13},{&g_13,(void*)0,(void*)0,&g_83}},{{&g_6,(void*)0,&g_83,(void*)0},{&g_6,(void*)0,&g_6,&g_83},{(void*)0,(void*)0,&g_83,&g_13},{&g_13,(void*)0,&g_6,(void*)0},{&g_13,&g_6,&g_6,&g_13},{&g_13,&g_83,&g_83,&g_6}},{{(void*)0,(void*)0,&g_6,&g_13},{&g_6,&g_13,&g_83,&g_13},{&g_6,(void*)0,(void*)0,&g_6},{&g_13,&g_83,&g_13,&g_13},{(void*)0,&g_6,&g_6,&g_6},{&g_13,&g_13,(void*)0,&g_83}}};
+static uint16_t g_123 = 0x3C15L;
+static volatile int8_t g_129 = 0x14L;/* VOLATILE GLOBAL g_129 */
