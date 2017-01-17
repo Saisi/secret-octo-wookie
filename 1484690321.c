666d69a6d7a8bf6dc301466fc3dbe730de687347
saisi <saisi@users.noreply.github.com>
2017-01-17 21:58:42 +0000

    1484690321 adequate

---
+++ 1484690321.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1376965747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   unsigned f1 : 15;
+   uint64_t  f2;
+   volatile int8_t * volatile  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_15 = 0x8A11015AL;
+static uint64_t g_19 = 0x6F97812A3CC92EA2LL;
