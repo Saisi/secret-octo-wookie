88af88b129ba41af6e9d4f02aea4a7c1096cb191
saisi <saisi@users.noreply.github.com>
2017-01-17 09:27:29 +0000

    1484645247 aspergill

---
+++ 1484645247.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      266782866
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   volatile int8_t * f1;
+   const uint32_t  f2;
+   uint64_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   int32_t  f1;
