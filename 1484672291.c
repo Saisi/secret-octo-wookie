073c63e9cb648adf2c40790c0272aaf850a47008
saisi <saisi@users.noreply.github.com>
2017-01-17 16:58:11 +0000

    1484672291 abidingness

---
+++ 1484672291.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3972707760
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 0x2631L;
+static int32_t g_4 = (-1L);
+static int32_t g_7 = (-7L);
+static int32_t * volatile g_6[6] = {&g_7,&g_7,&g_7,&g_7,&g_7,&g_7};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
