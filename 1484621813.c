b18c7addbd74ba5f9b29abc0f023b11e47f5c46b
saisi <saisi@users.noreply.github.com>
2017-01-17 02:56:54 +0000

    1484621813 apophylaxis

---
+++ 1484621813.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      657907926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0xF2C29A3DL;
+static uint8_t g_34[3][1] = {{0xF6L},{0xF6L},{0xF6L}};
+static uint8_t g_37 = 251UL;
+static uint8_t *g_36 = &g_37;
+static volatile int32_t g_90 = (-4L);/* VOLATILE GLOBAL g_90 */
+static int32_t g_91 = 0xD22F7F1CL;
+static uint8_t **g_95[7] = {&g_36,&g_36,&g_36,&g_36,&g_36,&g_36,&g_36};
+static uint8_t ***g_94 = &g_95[4];
