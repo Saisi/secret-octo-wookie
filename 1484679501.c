b2e964d0de919b0fa7caa4dd4e123754059bdc2a
saisi <saisi@users.noreply.github.com>
2017-01-17 18:58:23 +0000

    1484679501 alectoromorphous

---
+++ 1484679501.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      662391962
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+};
+
+union U1 {
+   unsigned f0 : 19;
+   int32_t  f1;
+   signed f2 : 18;
+   unsigned f3 : 12;
+   int8_t * f4;
