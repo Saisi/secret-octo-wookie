50ae7b42b5c9689ae367680b735f9dada0adbcb4
saisi <saisi@users.noreply.github.com>
2017-01-17 17:58:17 +0000

    1484675896 argillocalcite

---
+++ 1484675896.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2234716462
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_9 = (-7L);
+static const int16_t g_26[1][8][10] = {{{0x28C7L,1L,0L,1L,0x28C7L,0x26C7L,0xC39DL,(-1L),8L,0xB8EAL},{0xC39DL,4L,0xB8EAL,0x26C7L,9L,(-1L),(-1L),9L,0x26C7L,0xB8EAL},{0x26C7L,0x26C7L,(-8L),0xB8EAL,0x28C7L,0x389AL,9L,0L,4L,0xB8FAL},{0L,0x05F4L,9L,8L,1L,8L,9L,0x05F4L,0L,0x26C7L},{4L,0x26C7L,4L,0x28C7L,0xB8FAL,0xABC2L,(-8L),0x26C7L,0x26C7L,(-8L)},{1L,4L,0x389AL,0x389AL,4L,1L,0xABC2L,0xB8EAL,(-1L),(-1L)},{(-1L),0x26C7L,0L,0x05F4L,9L,8L,1L,8L,9L,0x05F4L},{(-1L),8L,(-1L),4L,(-8L),1L,0x05F4L,0x28C7L,0L,0xB8EAL}}};
+static const int16_t *g_29[9][1][7] = {{{&g_26[0][1][1],(void*)0,(void*)0,&g_26[0][1][1],(void*)0,(void*)0,&g_26[0][1][1]}},{{&g_26[0][6][7],&g_26[0][3][9],&g_26[0][6][7],&g_26[0][3][1],&g_26[0][2][6],&g_26[0][3][1],&g_26[0][6][7]}},{{&g_26[0][1][1],&g_26[0][1][1],(void*)0,&g_26[0][1][1],&g_26[0][1][1],(void*)0,&g_26[0][1][1]}},{{&g_26[0][2][6],&g_26[0][3][1],&g_26[0][6][7],&g_26[0][3][9],&g_26[0][6][7],&g_26[0][3][1],&g_26[0][2][6]}},{{(void*)0,&g_26[0][1][1],(void*)0,(void*)0,&g_26[0][1][1],(void*)0,(void*)0}},{{&g_26[0][2][6],&g_26[0][3][9],(void*)0,&g_26[0][3][9],&g_26[0][2][6],&g_26[0][3][6],&g_26[0][2][6]}},{{&g_26[0][1][1],(void*)0,(void*)0,&g_26[0][1][1],(void*)0,(void*)0,&g_26[0][1][1]}},{{&g_26[0][6][7],&g_26[0][3][9],&g_26[0][6][7],&g_26[0][3][1],&g_26[0][2][6],&g_26[0][3][1],&g_26[0][6][7]}},{{&g_26[0][1][1],&g_26[0][1][1],(void*)0,&g_26[0][1][1],&g_26[0][1][1],(void*)0,&g_26[0][1][1]}}};
+static const int16_t **g_28 = &g_29[4][0][2];
+static const int16_t *** volatile g_27 = &g_28;/* VOLATILE GLOBAL g_27 */
+static uint64_t g_38 = 18446744073709551615UL;
+static uint16_t g_40[10] = {65535UL,0UL,65535UL,0x9AE8L,0x9AE8L,65535UL,0UL,65535UL,0x9AE8L,0x9AE8L};
+static int16_t g_46 = 0x83FAL;
+static int32_t g_48 = 0xCDA6A44FL;
