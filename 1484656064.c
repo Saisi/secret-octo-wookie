2e2d9ae5e661e78b2244fc6e2a9802e156ad6c4c
saisi <saisi@users.noreply.github.com>
2017-01-17 12:27:45 +0000

    1484656064 amyloidoses

---
+++ 1484656064.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3216040441
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   volatile uint32_t  f0;
+   signed f1 : 7;
+   int8_t * f2;
+   int32_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x9CDC345FL;
