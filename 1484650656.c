ef6f87cd46ff7551dac7efe087b1e788a118e759
saisi <saisi@users.noreply.github.com>
2017-01-17 10:57:37 +0000

    1484650656 Barclay

---
+++ 1484650656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3375400910
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 21;
+   unsigned f1 : 22;
+   volatile signed : 0;
+   volatile unsigned f2 : 12;
+};
+
+struct S1 {
+   volatile int16_t  f0;
+   uint32_t  f1;
