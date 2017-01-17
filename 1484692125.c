3db7170ff0e27fa10f5b4fff57ddd05f32dc85f7
saisi <saisi@users.noreply.github.com>
2017-01-17 22:28:45 +0000

    1484692125 abscoulomb

---
+++ 1484692125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4116765503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   const signed f1 : 29;
+   unsigned f2 : 12;
+   const unsigned f3 : 7;
+   signed f4 : 28;
+   signed f5 : 5;
+   unsigned f6 : 16;
+};
+
