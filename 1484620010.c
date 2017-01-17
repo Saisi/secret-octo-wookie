2d83a1d96364d3a09983186f8a35d5f81c088746
saisi <saisi@users.noreply.github.com>
2017-01-17 02:26:51 +0000

    1484620010 anthracites

---
+++ 1484620010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3074684466
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   volatile signed : 0;
+   unsigned f1 : 2;
+   signed f2 : 6;
+   unsigned f3 : 29;
+   unsigned : 0;
+};
+
+union U1 {
