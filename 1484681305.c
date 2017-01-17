c4ff0c0cc2729165626b24e11a988da984f542cd
saisi <saisi@users.noreply.github.com>
2017-01-17 19:28:25 +0000

    1484681305 arytenoidal

---
+++ 1484681305.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3959111001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 11;
+   const volatile unsigned f1 : 22;
+   const signed f2 : 1;
+   signed f3 : 8;
+   volatile signed f4 : 25;
+   signed f5 : 14;
+   volatile signed f6 : 21;
