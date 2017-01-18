43b16427b51854ac4528803b00b192e5a82905a3
saisi <saisi@users.noreply.github.com>
2017-01-18 19:00:42 +0000

    1484766041 alightment

---
+++ 1484766041.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      285719227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile struct S0  f0;
+   const uint16_t  f1;
+   volatile uint8_t  f2;
