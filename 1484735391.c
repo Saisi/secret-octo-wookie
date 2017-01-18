1d3b29162932dc1a1577cc6a00115c7c3b92235b
saisi <saisi@users.noreply.github.com>
2017-01-18 10:29:51 +0000

    1484735391 ascension

---
+++ 1484735391.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3399752993
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 15;
+   signed f1 : 10;
+   unsigned f2 : 5;
+   volatile signed f3 : 28;
+   volatile unsigned f4 : 29;
+   const volatile unsigned f5 : 2;
+   signed f6 : 25;
