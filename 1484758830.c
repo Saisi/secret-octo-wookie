a1950e1512ecb9d093fe9a4bc33de38f89138045
saisi <saisi@users.noreply.github.com>
2017-01-18 17:00:31 +0000

    1484758830 bacilliform

---
+++ 1484758830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2625995717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x6CA2CB51L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[8] = {(-2L),0L,(-2L),(-2L),0L,(-2L),(-2L),0L};
+static volatile int32_t g_5 = 0xAB274BF9L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0xCAD2086BL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0x5C9E53E5L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0L;
+static uint32_t g_39 = 0x284868FDL;
+static uint64_t g_98 = 0UL;
+static uint16_t g_102 = 0x8AD7L;
