a9610ef549f1b8344d7bb7638c7733c729acb607
saisi <saisi@users.noreply.github.com>
2017-01-18 23:01:05 +0000

    1484780464 attornare

---
+++ 1484780464.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2505744519
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC044B0BFL;
+static int32_t *g_5 = (void*)0;
+static int32_t g_8 = 0xDD7582BAL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint8_t  func_1(void);
+
+
