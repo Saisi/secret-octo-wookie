bde3d12c9bca12739098ca7fdc65205d9160cf59
saisi <saisi@users.noreply.github.com>
2017-01-18 20:00:47 +0000

    1484769647 arcuses

---
+++ 1484769647.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2972401461
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const volatile signed f1 : 12;
+   int64_t  f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   volatile int16_t  f0;
+};
