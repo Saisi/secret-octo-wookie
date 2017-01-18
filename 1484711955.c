06532352a51921169717a20f9d3302f997c51ef3
saisi <saisi@users.noreply.github.com>
2017-01-18 03:59:15 +0000

    1484711955 Ammonea

---
+++ 1484711955.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      109539374
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[8][1] = {{0xDE0BCD8CL},{0xE8E56F9BL},{0xDE0BCD8CL},{0xE8E56F9BL},{0xDE0BCD8CL},{0xE8E56F9BL},{0xDE0BCD8CL},{0xE8E56F9BL}};
+static int16_t g_11 = (-1L);
+static uint32_t g_22 = 1UL;
+static int32_t g_42 = 0xCDF8ABFFL;
+static uint8_t g_45[7] = {6UL,6UL,6UL,6UL,6UL,6UL,6UL};
+static uint8_t g_61 = 0xAFL;
+static volatile int32_t g_80 = (-1L);/* VOLATILE GLOBAL g_80 */
+static volatile int32_t * volatile g_79 = &g_80;/* VOLATILE GLOBAL g_79 */
