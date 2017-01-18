fbee8708b43a1a90ec48bd19e32b3e513136629f
saisi <saisi@users.noreply.github.com>
2017-01-18 04:29:18 +0000

    1484713757 appledrone

---
+++ 1484713757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      845954493
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   uint32_t  f1;
+   const volatile int16_t  f2;
+   volatile signed f3 : 22;
+   signed f4 : 16;
+   unsigned f5 : 19;
+};
+
+/* --- GLOBAL VARIABLES --- */
