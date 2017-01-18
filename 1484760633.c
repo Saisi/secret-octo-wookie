c85d88094588f54e6752b38574a140a10c7ef120
saisi <saisi@users.noreply.github.com>
2017-01-18 17:30:33 +0000

    1484760633 Aphareus

---
+++ 1484760633.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      190167845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   int32_t  f0;
+   const int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const union U1 g_2 = {0x43C4AB27L};
+
+
+/* --- FORWARD DECLARATIONS --- */
