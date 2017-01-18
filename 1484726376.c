135f5e7d1b4593ea2ae63409decada037cbd2ebf
saisi <saisi@users.noreply.github.com>
2017-01-18 07:59:36 +0000

    1484726376 babloh

---
+++ 1484726376.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      431877542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int32_t  f1;
+   volatile int8_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_3 = 0xC7L;
+static int32_t g_16 = (-1L);
