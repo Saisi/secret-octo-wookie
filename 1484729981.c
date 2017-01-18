32a8db4155d3a33c6f081ced143bfe15486448ef
saisi <saisi@users.noreply.github.com>
2017-01-18 08:59:41 +0000

    1484729981 axialities

---
+++ 1484729981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2366029963
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int16_t  f0;
+   volatile uint32_t  f1;
+   volatile int64_t  f2;
+};
+#pragma pack(pop)
+
+struct S1 {
