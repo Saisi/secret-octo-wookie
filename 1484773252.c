8f018353c5e6e83dd97da17825fa59728104db6d
saisi <saisi@users.noreply.github.com>
2017-01-18 21:00:52 +0000

    1484773252 atria

---
+++ 1484773252.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      460469686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_19[2] = {(-5L),(-5L)};
+static int8_t g_50[7] = {0L,0L,0L,0L,0L,0L,0L};
+static int16_t g_84 = 5L;
+static int16_t g_87 = 8L;
+static int32_t g_89 = 0L;
+static int32_t g_94 = 0x279A1AFFL;
+static int32_t * volatile g_93 = &g_94;/* VOLATILE GLOBAL g_93 */
+static int32_t g_131 = 1L;
+static uint32_t g_134 = 1UL;
