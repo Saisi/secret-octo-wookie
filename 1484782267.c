f07b39d7d895f4cbf817efb34a7043b314af0f14
saisi <saisi@users.noreply.github.com>
2017-01-18 23:31:07 +0000

    1484782267 aeolic

---
+++ 1484782267.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2764267009
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 31;
+   signed f1 : 26;
+   unsigned f2 : 21;
+   const unsigned f3 : 12;
+   unsigned f4 : 13;
+   unsigned f5 : 18;
+};
