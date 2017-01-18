c4201812e339dc224b389a9b2f13eab479dc12e5
saisi <saisi@users.noreply.github.com>
2017-01-18 10:59:54 +0000

    1484737194 auganite

---
+++ 1484737194.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      331238573
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   const uint16_t  f1;
+   const uint8_t  f2;
+   int16_t  f3;
+   uint8_t  f4;
+   int64_t  f5;
+   uint8_t  f6;
+   uint64_t  f7;
+};
