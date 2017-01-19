2abb085421d09a46e4c6c9ca0f2d84414a53974f
saisi <saisi@users.noreply.github.com>
2017-01-19 01:01:14 +0000

    1484787674 antipyretic

---
+++ 1484787674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      898243446
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * volatile  f0;
+   uint64_t  f1;
+   int8_t * const  f2;
+   volatile uint16_t  f3;
+   unsigned f4 : 12;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_13 = 1UL;
