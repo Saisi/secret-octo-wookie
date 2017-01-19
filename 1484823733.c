2b6ce353a298823b5957b3b6bf72ab61032df855
saisi <saisi@users.noreply.github.com>
2017-01-19 11:02:14 +0000

    1484823733 Argyra

---
+++ 1484823733.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4073748221
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-7L);
+static int32_t g_44 = (-1L);
+static int32_t g_67[3] = {(-5L),(-5L),(-5L)};
+static volatile uint64_t g_70 = 9UL;/* VOLATILE GLOBAL g_70 */
+static int32_t g_83 = (-1L);
+static const int32_t *g_82 = &g_83;
+static uint8_t g_101 = 0UL;
+static int16_t g_108 = (-6L);
+static int16_t g_111[5] = {(-3L),(-3L),(-3L),(-3L),(-3L)};
