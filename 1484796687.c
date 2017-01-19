0395827e32a50132ae579411805bc0bda1bf4b3c
saisi <saisi@users.noreply.github.com>
2017-01-19 03:31:27 +0000

    1484796687 Aggappora

---
+++ 1484796687.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2848075477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 28;
+   volatile unsigned f2 : 15;
+   unsigned f3 : 30;
+   signed f4 : 23;
+   signed f5 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
