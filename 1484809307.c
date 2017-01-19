79764a9c20d14e2ac072667721f1914994c3852d
saisi <saisi@users.noreply.github.com>
2017-01-19 07:01:48 +0000

    1484809307 azodicarboxylic

---
+++ 1484809307.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1706426675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = (-1L);
+static int32_t g_39 = (-3L);
+static volatile int32_t g_44 = 0L;/* VOLATILE GLOBAL g_44 */
+static uint64_t g_45[8] = {5UL,5UL,5UL,5UL,5UL,5UL,5UL,5UL};
+static uint32_t g_50 = 0x51ECDE47L;
+static uint16_t g_72 = 0xFF46L;
+static uint16_t g_74 = 65535UL;
+static uint16_t g_76[3] = {0x3981L,0x3981L,0x3981L};
+static int64_t g_97 = 2L;
