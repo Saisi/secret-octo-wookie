c322088478510031a0b2292f7455c11b3a0bb47b
saisi <saisi@users.noreply.github.com>
2017-01-19 04:31:35 +0000

    1484800294 anaglyphics

---
+++ 1484800294.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      293413858
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+};
+
+union U1 {
+   const volatile uint32_t  f0;
+   const int16_t  f1;
+};
+
+union U2 {
