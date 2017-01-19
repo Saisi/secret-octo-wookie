17e6b741ba004729c42f1e088c833ffd643f9d54
saisi <saisi@users.noreply.github.com>
2017-01-19 13:02:24 +0000

    1484830944 ballpoints

---
+++ 1484830944.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      113411584
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   const int8_t  f1;
+   const signed f2 : 3;
+   unsigned f3 : 25;
+   signed f4 : 26;
+};
+#pragma pack(pop)
