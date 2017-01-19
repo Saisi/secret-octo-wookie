ac5838d7bd6dc490e80e1305a17371ce7e8efe5d
saisi <saisi@users.noreply.github.com>
2017-01-19 01:31:17 +0000

    1484789477 Anamoose

---
+++ 1484789477.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1995562306
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-3L);
+static int32_t g_5[6] = {1L,(-10L),1L,1L,(-10L),1L};
+static int32_t g_25 = 1L;
+static int32_t * volatile g_24 = &g_25;/* VOLATILE GLOBAL g_24 */
+static volatile uint32_t g_29 = 0x321210CBL;/* VOLATILE GLOBAL g_29 */
