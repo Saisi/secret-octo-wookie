87b41c14b352a7a0fbc6f987e4b2cac898ab89db
saisi <saisi@users.noreply.github.com>
2017-01-19 18:02:51 +0000

    1484848970 aestuary

---
+++ 1484848970.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3104143128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int8_t  f1;
+   int8_t  f2;
+   volatile unsigned f3 : 5;
+   int8_t * f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2 = 255UL;
