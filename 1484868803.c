859ed396055deca4601eeace25977123b074d1c7
saisi <saisi@users.noreply.github.com>
2017-01-19 23:33:24 +0000

    1484868803 angelfishes

---
+++ 1484868803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1970957984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   unsigned f1 : 27;
+   unsigned f2 : 17;
+   signed f3 : 25;
+   signed f4 : 4;
+};
+#pragma pack(pop)
