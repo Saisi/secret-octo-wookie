31401d775ab3198dcdc6f5380950673949e4e744
saisi <saisi@users.noreply.github.com>
2017-01-19 11:32:17 +0000

    1484825536 acinose

---
+++ 1484825536.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2090138609
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 23;
+   unsigned f1 : 18;
+};
+#pragma pack(pop)
+
+struct S1 {
+   signed f0 : 14;
