39031d9db62dcbe6a8191de915963ec82896fcb4
saisi <saisi@users.noreply.github.com>
2017-01-19 15:32:37 +0000

    1484839956 babysitter

---
+++ 1484839956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      889679930
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   signed f1 : 20;
+   unsigned f2 : 6;
+   const volatile unsigned f3 : 26;
+   unsigned f4 : 1;
+   signed f5 : 20;
+};
+
+union U1 {
