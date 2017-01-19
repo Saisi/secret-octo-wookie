d0ba7ba2fa6cca58aaa97317f18ab85ad8ef7608
saisi <saisi@users.noreply.github.com>
2017-01-19 00:31:12 +0000

    1484785872 AGS

---
+++ 1484785872.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      599121482
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 2L;
+static int32_t *g_6[10][6][4] = {{{(void*)0,&g_7,(void*)0,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,(void*)0,&g_7,&g_7},{&g_7,&g_7,&g_7,&g_7},{(void*)0,(void*)0,&g_7,&g_7}},{{(void*)0,(void*)0,(void*)0,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,&g_7,(void*)0,(void*)0},{(void*)0,&g_7,&g_7,&g_7},{(void*)0,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,&g_7}},{{&g_7,(void*)0,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7},{(void*)0,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0},{&g_7,(void*)0,&g_7,&g_7},{(void*)0,&g_7,(void*)0,&g_7}},{{&g_7,&g_7,&g_7,&g_7},{&g_7,(void*)0,&g_7,(void*)0},{(void*)0,&g_7,&g_7,(void*)0},{&g_7,&g_7,&g_7,(void*)0},{(void*)0,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0}},{{&g_7,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7},{(void*)0,&g_7,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7}},{{&g_7,(void*)0,&g_7,(void*)0},{&g_7,(void*)0,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0},{(void*)0,&g_7,&g_7,(void*)0},{(void*)0,&g_7,&g_7,(void*)0}},{{&g_7,(void*)0,&g_7,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,(void*)0,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7},{(void*)0,&g_7,&g_7,&g_7}},{{&g_7,&g_7,&g_7,(void*)0},{&g_7,(void*)0,&g_7,&g_7},{(void*)0,&g_7,(void*)0,&g_7},{&g_7,&g_7,&g_7,&g_7},{&g_7,(void*)0,&g_7,(void*)0},{(void*)0,&g_7,&g_7,(void*)0}},{{&g_7,&g_7,&g_7,(void*)0},{(void*)0,&g_7,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0},{&g_7,(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,&g_7,&g_7},{&g_7,&g_7,&g_7,(void*)0}},{{&g_7,&g_7,&g_7,&g_7},{(void*)0,&g_7,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7},{&g_7,(void*)0,&g_7,(void*)0},{&g_7,(void*)0,&g_7,(void*)0},{&g_7,&g_7,&g_7,&g_7}}};
+static uint8_t g_10[9] = {0xE1L,2UL,0xE1L,0xE1L,2UL,0xE1L,0xE1L,2UL,0xE1L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+static int32_t * func_2(int32_t * p_3, uint8_t  p_4, int32_t * p_5);
+
