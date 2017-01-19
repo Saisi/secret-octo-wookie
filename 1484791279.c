e7f968a15e770a42109a4f14bb6eebe956cdfc2d
saisi <saisi@users.noreply.github.com>
2017-01-19 02:01:20 +0000

    1484791279 alhenna

---
+++ 1484791279.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2491507202
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 15;
+   unsigned f1 : 8;
+   volatile unsigned f2 : 10;
+   signed f3 : 14;
+};
+#pragma pack(pop)
+
