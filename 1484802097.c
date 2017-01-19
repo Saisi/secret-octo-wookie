f1ce16521e7f4f295450012a5601c54b534efdb2
saisi <saisi@users.noreply.github.com>
2017-01-19 05:01:37 +0000

    1484802097 arcanist

---
+++ 1484802097.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2397276464
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   int64_t  f1;
+   const uint32_t  f2;
+   const signed f3 : 10;
+   const uint32_t  f4;
+   int32_t  f5;
+   uint32_t  f6;
+};
+
