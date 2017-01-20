816c8038f998a301de299aa7b9ec5d8a2b1d25e2
saisi <saisi@users.noreply.github.com>
2017-01-20 14:04:47 +0000

    1484921086 anacletica

---
+++ 1484921086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2743028464
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x252CB136L;
+static int8_t g_10 = 0x05L;
+static volatile int32_t g_11 = 0x6C218E99L;/* VOLATILE GLOBAL g_11 */
+static uint32_t g_17 = 7UL;
+static int8_t g_26[4][2] = {{6L,6L},{6L,6L},{6L,6L},{6L,6L}};
