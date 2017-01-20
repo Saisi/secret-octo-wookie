2df763f6e6096961120f437022e731dc0101c28f
saisi <saisi@users.noreply.github.com>
2017-01-20 06:04:02 +0000

    1484892240 arthrodial

---
+++ 1484892240.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      521149968
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint16_t g_22 = 8UL;
+static uint16_t g_26 = 0x168FL;
+static int32_t g_41[4] = {1L,1L,1L,1L};
+static uint8_t g_65[9][5] = {{0x5EL,0x5EL,1UL,0x5EL,0x5EL},{0xAEL,0x91L,0xAEL,0xAEL,0x91L},{0x5EL,0x8BL,0x8BL,0x5EL,0x8BL},{0x91L,0x91L,255UL,0x91L,0x91L},{0x8BL,0x5EL,0x8BL,0x8BL,0x5EL},{0x91L,0xAEL,0xAEL,0x91L,0xAEL},{0x5EL,0x5EL,1UL,0x8BL,0x8BL},{255UL,0xAEL,255UL,255UL,0xAEL},{0x8BL,1UL,1UL,0x8BL,1UL}};
