a387d3e269057265afa2f53c6de77ccb889b70d4
saisi <saisi@users.noreply.github.com>
2017-01-20 09:34:20 +0000

    1484904859 adrenolytic

---
+++ 1484904859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2525188064
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_4[6] = {1UL,1UL,1UL,1UL,1UL,1UL};
+static int32_t g_7 = 1L;
+static int32_t *g_24[8] = {(void*)0,&g_7,(void*)0,(void*)0,&g_7,(void*)0,(void*)0,&g_7};
+static int32_t **g_23 = &g_24[4];
+static const int32_t *g_47 = &g_7;
+static const int32_t **g_46 = &g_47;
+static int16_t g_56 = 0x6954L;
+static int16_t g_58 = 0x759FL;
+static int32_t g_79 = 0xEAADAF8DL;
