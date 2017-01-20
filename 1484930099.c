76c146f77468240f92fd1d5b216993fb5243fb17
saisi <saisi@users.noreply.github.com>
2017-01-20 16:35:00 +0000

    1484930099 Apepi

---
+++ 1484930099.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1473858016
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0x0A624CDBL;
+static int32_t *g_8 = &g_9;
+static volatile uint8_t g_24 = 0xF3L;/* VOLATILE GLOBAL g_24 */
+static int32_t g_62 = 0x7B11082EL;
+static volatile int32_t g_63 = 0xADAF8D98L;/* VOLATILE GLOBAL g_63 */
+static uint16_t g_64 = 0UL;
+static int32_t *g_75[1] = {&g_9};
+static const int32_t *g_124 = &g_9;
+static const int32_t **g_123 = &g_124;
