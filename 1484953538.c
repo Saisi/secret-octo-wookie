d3b8f764902f1f07c7065f0bc69a09779058dd49
saisi <saisi@users.noreply.github.com>
2017-01-20 23:05:39 +0000

    1484953538 autoconvection

---
+++ 1484953538.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3752019344
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile signed f1 : 3;
+   int16_t  f2;
+   volatile unsigned f3 : 1;
+   uint32_t  f4;
+   uint16_t  f5;
+   const unsigned f6 : 14;
+};
+
