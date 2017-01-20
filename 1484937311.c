f3c382f6a37072dbdb622687129576956a003c46
saisi <saisi@users.noreply.github.com>
2017-01-20 18:35:13 +0000

    1484937311 amphistylar

---
+++ 1484937311.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2441755983
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
+   int64_t  f3;
+   int16_t  f4;
+   int16_t  f5;
+   const int32_t  f6;
+};
+
