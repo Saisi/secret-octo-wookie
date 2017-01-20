a8120280ab6e1077024e87ee881e7f1de67328f4
saisi <saisi@users.noreply.github.com>
2017-01-20 15:34:54 +0000

    1484926494 Aphelandra

---
+++ 1484926494.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3961782630
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint32_t  f1;
+   signed f2 : 24;
+   int32_t  f3;
+};
+
+union U1 {
+   int8_t * f0;
+   volatile int32_t  f1;
