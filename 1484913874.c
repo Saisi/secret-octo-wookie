a0bd42b8325cc594605395880322c306b4ecd087
saisi <saisi@users.noreply.github.com>
2017-01-20 12:04:34 +0000

    1484913874 baldachino

---
+++ 1484913874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3488692640
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 30;
+   signed f1 : 27;
+   signed f2 : 17;
+   signed f3 : 25;
+   signed f4 : 4;
+};
+#pragma pack(pop)
