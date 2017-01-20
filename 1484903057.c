5e78b83ffb3ead1bc0a945fe04f57ef28b4c3ca1
saisi <saisi@users.noreply.github.com>
2017-01-20 09:04:17 +0000

    1484903057 aphototaxis

---
+++ 1484903057.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1857598051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3] = {0xCED8F6EFL,0xCED8F6EFL,0xCED8F6EFL};
+static int32_t g_3 = 0L;
+static int32_t g_6 = 0x96C7979BL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
