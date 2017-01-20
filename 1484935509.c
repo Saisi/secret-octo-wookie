35bdac045cd47df35cd9e847118f0d8d8b445b2e
saisi <saisi@users.noreply.github.com>
2017-01-20 18:05:09 +0000

    1484935509 alpinist

---
+++ 1484935509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1436600351
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   unsigned f1 : 13;
+   volatile uint64_t  f2;
+};
+
+struct S1 {
+   volatile unsigned f0 : 9;
+   volatile int64_t  f1;
+   signed f2 : 28;
