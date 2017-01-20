5b3c01212a8d70eed777f1af82421885237c5995
saisi <saisi@users.noreply.github.com>
2017-01-20 08:04:13 +0000

    1484899452 andalusite

---
+++ 1484899452.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      510718160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   const uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 0UL;
+static uint32_t g_36 = 0UL;
+static uint32_t *g_35 = &g_36;
