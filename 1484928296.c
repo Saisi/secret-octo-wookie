d05a1004b2bc7825765207a7313ef648fec3fc31
saisi <saisi@users.noreply.github.com>
2017-01-20 16:04:57 +0000

    1484928296 aspiratory

---
+++ 1484928296.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      242335662
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 5;
+   const uint32_t  f1;
+   volatile int8_t  f2;
+   int8_t * volatile  f3;
+};
+
+union U1 {
+   volatile int16_t  f0;
+};
