f3a999b2dd3ebb19bfb1c9c7179c907ff339562a
saisi <saisi@users.noreply.github.com>
2017-01-20 06:34:05 +0000

    1484894045 ballium

---
+++ 1484894045.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2968120512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[7][10] = {{1L,0xDA7FL,0x5950L,0xD9E2L,(-1L),1L,0xD9EDL,(-1L),0x3921L,0L},{4L,0L,1L,0x80C3L,0xED68L,1L,0xED68L,0x80C3L,1L,0L},{0L,1L,0L,1L,(-1L),0xD9E2L,1L,1L,0x3582L,0L},{0L,4L,1L,(-1L),0L,0xD9E2L,0xD9EDL,4L,0x5950L,0x5950L},{0L,0x80C3L,1L,4L,4L,1L,0x3582L,(-1L),5L,0x3921L},{0L,0L,(-6L),1L,0x14B5L,1L,0L,0L,0x90A6L,0L},{0x90A6L,0xD9E2L,(-6L),0x14B5L,0xD9E2L,0L,1L,(-1L),1L,0L}};
+static int32_t g_5 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
