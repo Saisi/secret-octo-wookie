a8be18130a9df91efb5b675024ffce6345a9571d
saisi <saisi@users.noreply.github.com>
2017-01-20 04:03:48 +0000

    1484885027 admarginate

---
+++ 1484885027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2863801934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   const volatile signed f1 : 18;
+   volatile int32_t  f2;
+   uint8_t  f3;
+   const uint8_t  f4;
+   const volatile int32_t  f5;
+};
+
+struct S1 {
