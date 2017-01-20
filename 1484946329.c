0d536517bf7c36929ce495be1bfe4e2229a0e16d
saisi <saisi@users.noreply.github.com>
2017-01-20 21:05:29 +0000

    1484946329 abstrude

---
+++ 1484946329.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2295848215
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x93DDA37BL;
+static int32_t g_12 = 0x921B0AE2L;
+static volatile int64_t g_15 = 0x8C5378470ED99EA7LL;/* VOLATILE GLOBAL g_15 */
+static volatile int32_t g_24 = (-8L);/* VOLATILE GLOBAL g_24 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
