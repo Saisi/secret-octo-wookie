5c389bd2e9eb3f84665c5e8898a777fed1358e08
saisi <saisi@users.noreply.github.com>
2017-01-21 18:07:28 +0000

    1485022047 Aigialosaurus

---
+++ 1485022047.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1193065682
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 23;
+   uint8_t  f1;
+   volatile int16_t  f2;
+   const volatile int8_t  f3;
+   uint64_t  f4;
+   const volatile int32_t  f5;
+   int32_t  f6;
+   volatile int64_t  f7;
+};
