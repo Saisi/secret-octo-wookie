e55ebf25f3383ff2219d374f6c31b300aae045d9
saisi <saisi@users.noreply.github.com>
2017-01-21 02:05:56 +0000

    1484964355 alkalifies

---
+++ 1484964355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2487067427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 7L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[3][3][4] = {{{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L}},{{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L}},{{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L},{4L,(-1L),(-1L),4L}}};
+static volatile int32_t g_6 = (-2L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7[2] = {0x7909B792L,0x7909B792L};
+static int32_t g_14 = 0L;
+static int32_t * volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
+static int32_t * volatile g_15[4] = {&g_14,&g_14,&g_14,&g_14};
+static int32_t * volatile g_16 = (void*)0;/* VOLATILE GLOBAL g_16 */
+static int32_t * volatile g_17 = (void*)0;/* VOLATILE GLOBAL g_17 */
