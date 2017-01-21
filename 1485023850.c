8e7f9e07afd1307a25ce47f9e2e8c38d69f7f89f
saisi <saisi@users.noreply.github.com>
2017-01-21 18:37:31 +0000

    1485023850 arterioarctia

---
+++ 1485023850.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3014667765
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 25;
+   const signed f1 : 12;
+   unsigned f2 : 31;
+   const unsigned f3 : 14;
+};
+#pragma pack(pop)
+
