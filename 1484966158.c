bd03d331b2dfcff1b87788e5b7d3ff785442381f
saisi <saisi@users.noreply.github.com>
2017-01-21 02:35:59 +0000

    1484966158 autopathic

---
+++ 1484966158.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3528861582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   signed f1 : 22;
+   unsigned : 0;
+   signed f2 : 12;
+};
+#pragma pack(pop)
+
