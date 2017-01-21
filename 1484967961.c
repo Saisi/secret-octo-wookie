46f5a69c164c99e983dee647e934d3b8378ee06f
saisi <saisi@users.noreply.github.com>
2017-01-21 03:06:02 +0000

    1484967961 Barca

---
+++ 1484967961.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2464078203
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   uint32_t  f1;
+   uint64_t  f2;
+};
+
+struct S1 {
+   int32_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
