3a80974e68c0d5b475d265472c0efd18f4920794
saisi <saisi@users.noreply.github.com>
2017-01-21 20:37:40 +0000

    1485031060 Agkistrodon

---
+++ 1485031060.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2530278990
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   unsigned f1 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_4 = {1055,57};
+static volatile int32_t g_5 = 0xEB77278FL;/* VOLATILE GLOBAL g_5 */
+
+
