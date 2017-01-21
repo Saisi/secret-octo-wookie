1e3d574c8a139215111242c499503575227a988c
saisi <saisi@users.noreply.github.com>
2017-01-21 10:36:43 +0000

    1484995002 Arafura

---
+++ 1484995002.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3550512178
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-10L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_6 = (-1L);
+static int32_t *g_7[1] = {&g_6};
+static int8_t g_47 = 0x86L;
+static uint64_t g_69 = 0xB5881F6C987F9BD8LL;
+static int32_t g_72 = 0L;
+static uint8_t g_73 = 255UL;
+static int32_t g_74 = 0xDCAE3649L;
+static int64_t g_92 = 0x994F54D63FF5A50ELL;
