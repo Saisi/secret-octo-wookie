4a38e3218820c7339462183bdf5808bc827511e6
saisi <saisi@users.noreply.github.com>
2017-01-21 11:06:46 +0000

    1484996806 Achromatium

---
+++ 1484996806.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3224932603
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x33A4A9BDL;
+static uint8_t g_45 = 0xD4L;
+static int32_t g_76 = 1L;
+static int32_t *g_80 = (void*)0;
+static int32_t **g_79 = &g_80;
+static const int32_t g_86 = 0x4C3FC6C0L;
+static const int32_t * const g_85 = &g_86;
+static const int32_t * const *g_84 = &g_85;
