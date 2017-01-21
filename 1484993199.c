3b548259e3c76d2e691531bb14556ced296fde3e
saisi <saisi@users.noreply.github.com>
2017-01-21 10:06:40 +0000

    1484993199 adiaphonon

---
+++ 1484993199.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1697426934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   unsigned f1 : 26;
+};
+
+union U1 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
