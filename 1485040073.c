354921965efd73456650506c7c41534d69b169ea
saisi <saisi@users.noreply.github.com>
2017-01-21 23:07:54 +0000

    1485040073 anticlactic

---
+++ 1485040073.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      673602066
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 23;
+   unsigned f1 : 16;
+   signed f2 : 18;
+   unsigned f3 : 22;
+   unsigned f4 : 13;
+   unsigned f5 : 16;
+   unsigned f6 : 14;
