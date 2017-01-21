352bba90c42c4c75f0a5c240d670d52eeb284ae0
saisi <saisi@users.noreply.github.com>
2017-01-21 00:35:48 +0000

    1484958947 autokinesis

---
+++ 1484958947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2186782098
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+union U1 {
+   int8_t  f0;
+   volatile int16_t  f1;
+   const unsigned f2 : 15;
+   volatile int32_t  f3;
+   uint32_t  f4;
