c0fbe1b818d4d15231e1e417d21624b1ffcafe55
saisi <saisi@users.noreply.github.com>
2017-01-21 07:36:26 +0000

    1484984185 aborting

---
+++ 1484984185.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1458990762
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   uint32_t  f1;
+   uint64_t  f2;
+   volatile signed f3 : 18;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x59C17633L;/* VOLATILE GLOBAL g_3 */
