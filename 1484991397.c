a3271c8b2ec0ff9e3aa81fd9927da0eaea8ffe4c
saisi <saisi@users.noreply.github.com>
2017-01-21 09:36:37 +0000

    1484991397 aloetical

---
+++ 1484991397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      732216407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 7L;
+static int32_t g_7 = 2L;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+static uint16_t g_24 = 0x3765L;
+static union U0 g_38 = {0xFEL};
