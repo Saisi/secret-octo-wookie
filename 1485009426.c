888603323f452ce5a6d1ff7a38da489ed7352f53
saisi <saisi@users.noreply.github.com>
2017-01-21 14:37:06 +0000

    1485009426 Alactaga

---
+++ 1485009426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2185612778
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static int32_t *g_17 = &g_4;
+static const int32_t g_52 = 7L;
+static uint8_t g_78 = 0UL;
+static uint8_t g_83 = 0x6CL;
+static int32_t g_90 = (-1L);
+static uint16_t g_96[7][4] = {{0x7969L,0x22C9L,0x7969L,0x7969L},{0x22C9L,0x22C9L,65527UL,0x22C9L},{0x22C9L,0x7969L,0x7969L,0x22C9L},{0x7969L,0x22C9L,0x7969L,0x7969L},{0x22C9L,0x22C9L,65527UL,0x22C9L},{0x22C9L,0x7969L,0x7969L,0x22C9L},{0x7969L,0x22C9L,0x7969L,0x7969L}};
+static int32_t g_106 = 0x40E9B690L;
+static int32_t * volatile g_105 = &g_106;/* VOLATILE GLOBAL g_105 */
