c4815c884c5d9a4d3f9e965d08f6b815bd172708
saisi <saisi@users.noreply.github.com>
2017-01-22 05:08:27 +0000

    1485061707 armies

---
+++ 1485061707.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3749074238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_13 = 18446744073709551615UL;/* VOLATILE GLOBAL g_13 */
+static int16_t g_17 = (-1L);
+static int16_t g_19[8][4] = {{0xC9FAL,0x661FL,0x661FL,0xC9FAL},{0x661FL,0xC9FAL,0L,0L},{0x661FL,0L,0x661FL,0x2AA9L},{0xC9FAL,0L,0x2AA9L,0x2AA9L},{0L,0L,4L,0L},{0L,0xC9FAL,4L,0xC9FAL},{0L,0x661FL,0x2AA9L,4L},{0xC9FAL,0x661FL,0x661FL,0xC9FAL}};
+static uint32_t g_30 = 0x4C7BA3F3L;
+static uint64_t g_33 = 0x3ACDD711A53FDE75LL;
+static const uint32_t g_34 = 0xB818F8F6L;
+static int64_t g_52 = 0L;
+static int32_t g_101 = 0L;
+static uint16_t g_131 = 0x3AD3L;
