ddf5ee158e055d84cc94b8ac502defd0d74d36d4
saisi <saisi@users.noreply.github.com>
2017-01-22 13:09:10 +0000

    1485090550 antarthritic

---
+++ 1485090550.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      54451845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 1;
+   volatile signed f1 : 20;
+   unsigned f2 : 7;
+   signed f3 : 22;
+   unsigned f4 : 15;
+   const signed f5 : 12;
+};
+
+#pragma pack(push)
