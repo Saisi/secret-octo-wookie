10085c25bd089c3dc9d10d096d27a4825951c587
saisi <saisi@users.noreply.github.com>
2017-01-22 08:08:43 +0000

    1485072522 adeptnesses

---
+++ 1485072522.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1676573001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 14;
+   int16_t  f2;
+   signed : 0;
+   signed f3 : 17;
+   signed f4 : 6;
+   const volatile unsigned f5 : 13;
