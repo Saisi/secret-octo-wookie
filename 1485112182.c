8744809d5e318a4f9bdab747bcaea317bc963f2d
saisi <saisi@users.noreply.github.com>
2017-01-22 19:09:43 +0000

    1485112182 Amandus

---
+++ 1485112182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      285238523
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+};
+
+struct S1 {
+   const volatile uint8_t  f0;
+   int16_t  f1;
+   struct S0  f2;
+   volatile signed f3 : 26;
+   uint32_t  f4;
