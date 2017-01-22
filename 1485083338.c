bfebcac69f7b5d4cc12e178fc119ae1615617b5b
saisi <saisi@users.noreply.github.com>
2017-01-22 11:08:59 +0000

    1485083338 actualizes

---
+++ 1485083338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      831157057
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 31;
+   const uint16_t  f1;
+   int16_t  f2;
+   int64_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   uint8_t  f1;
