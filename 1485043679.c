6ceddc5dc409aa53264fbf4f374df00a71629be4
saisi <saisi@users.noreply.github.com>
2017-01-22 00:08:00 +0000

    1485043679 antipathogen

---
+++ 1485043679.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3961355396
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint16_t  f1;
+   uint32_t  f2;
+   const int64_t  f3;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   int8_t  f1;
