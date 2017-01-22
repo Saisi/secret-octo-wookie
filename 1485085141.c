3bad8cad25c0dd75a8b01183c49a8db35afed884
saisi <saisi@users.noreply.github.com>
2017-01-22 11:39:01 +0000

    1485085141 Achaemenid

---
+++ 1485085141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2420790278
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 17;
+   const unsigned f1 : 2;
+};
+
+union U1 {
+   volatile signed f0 : 4;
+   uint8_t  f1;
+   uint16_t  f2;
+   uint32_t  f3;
