d6aa2ca60c1fdd86e5945fde8845303daf388897
saisi <saisi@users.noreply.github.com>
2017-01-22 22:40:05 +0000

    1485124803 Badalona

---
+++ 1485124803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1646656779
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile signed f0 : 4;
+   volatile int8_t * volatile  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x27689351L;
+static int32_t g_8 = 1L;
+static int32_t g_11 = (-1L);
+static int32_t *g_20 = &g_3;
