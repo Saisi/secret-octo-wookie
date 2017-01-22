73dc29c03d7d9136a450a805599e7555bffc8f25
saisi <saisi@users.noreply.github.com>
2017-01-22 14:09:16 +0000

    1485094155 arterialization

---
+++ 1485094155.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2854165112
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   unsigned f1 : 23;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_7[4] = {0x9BA0L,0x9BA0L,0x9BA0L,0x9BA0L};
+static int16_t g_10 = 0xA0D4L;
+static int64_t g_12[1][9][3] = {{{0x9B996A53086335C0LL,(-1L),0x9B996A53086335C0LL},{0xB0BEFCDF11BBB4F5LL,0xEB64D07DDBF4DF7BLL,0xAD38DC5336DE0184LL},{(-7L),0xEB64D07DDBF4DF7BLL,0xC3DC99CDF9C77509LL},{2L,(-1L),0x6C1DFE936BE856DFLL},{0xF5BFB66FEBFB2C41LL,2L,2L},{2L,0L,(-7L)},{(-7L),0x065C2DD78F4F823ALL,(-7L)},{0xB0BEFCDF11BBB4F5LL,(-7L),2L},{0x9B996A53086335C0LL,0xC3DC99CDF9C77509LL,0x6C1DFE936BE856DFLL}}};
