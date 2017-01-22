9641afeb2cd47bfc04364e4ce39fc2d964e5afd8
saisi <saisi@users.noreply.github.com>
2017-01-22 20:39:51 +0000

    1485117590 Amintor

---
+++ 1485117590.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      487969591
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 26;
+   signed f1 : 9;
+   unsigned f2 : 8;
+   unsigned f3 : 19;
+   volatile signed f4 : 24;
+   volatile signed f5 : 17;
+   unsigned f6 : 12;
+   volatile signed f7 : 15;
+   unsigned f8 : 22;
