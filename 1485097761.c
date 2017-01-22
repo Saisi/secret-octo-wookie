4c93cf92a78eb4ea46c332714dbc84c1b1e182ce
saisi <saisi@users.noreply.github.com>
2017-01-22 15:09:22 +0000

    1485097761 atmas

---
+++ 1485097761.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2488515519
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[10] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_13 = (-6L);
+static int32_t g_47 = (-1L);
+static int32_t *g_46 = &g_47;
+static int32_t g_53 = 0x36F28A7DL;
+static volatile uint32_t g_59 = 1UL;/* VOLATILE GLOBAL g_59 */
+static uint64_t g_68 = 18446744073709551611UL;
+static uint32_t g_70 = 1UL;
+static int16_t g_75 = (-2L);
