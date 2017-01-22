d4b48b4dec9496785acaff7f19670f4fee5dbd39
saisi <saisi@users.noreply.github.com>
2017-01-22 12:39:08 +0000

    1485088747 autocorrelation

---
+++ 1485088747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3569563353
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 11;
+   uint8_t  f1;
+   int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xD45DB34EL;
+static volatile int32_t g_6[10][5][1] = {{{(-5L)},{9L},{(-5L)},{7L},{9L}},{{0x7E4FC228L},{0xB8E9646BL},{(-5L)},{(-5L)},{0xB8E9646BL}},{{0x7E4FC228L},{9L},{7L},{(-5L)},{9L}},{{(-5L)},{9L},{(-5L)},{7L},{9L}},{{0x7E4FC228L},{0xB8E9646BL},{(-5L)},{(-5L)},{0xB8E9646BL}},{{0x7E4FC228L},{9L},{7L},{(-5L)},{9L}},{{(-5L)},{9L},{(-5L)},{7L},{9L}},{{0x7E4FC228L},{0xB8E9646BL},{(-5L)},{(-5L)},{0xB8E9646BL}},{{0x7E4FC228L},{9L},{7L},{(-5L)},{9L}},{{(-5L)},{(-5L)},{(-10L)},{0xCF4C59CDL},{(-5L)}}};
+static const volatile int32_t * volatile g_5 = &g_6[6][4][0];/* VOLATILE GLOBAL g_5 */
