9a6ec7d14036c7259c983a1095689bedb42dddd2
saisi <saisi@users.noreply.github.com>
2017-01-22 04:08:22 +0000

    1485058101 acetabuliform

---
+++ 1485058101.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3500465479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   const volatile int32_t  f1;
+   const int16_t  f2;
+   volatile uint8_t  f3;
+   int16_t  f4;
+};
+
+union U1 {
+   const volatile uint32_t  f0;
