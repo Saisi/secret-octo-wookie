bd115832f57732ce238bdac57a9aa4306f501fd7
saisi <saisi@users.noreply.github.com>
2017-01-22 18:09:38 +0000

    1485108577 antemask

---
+++ 1485108577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1787195503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = (-2L);
+static int32_t g_4 = 0xBD7B1C2AL;
+static int8_t g_21 = (-1L);
+static int8_t g_47 = 0L;
+static int8_t *g_50[9][10][2] = {{{(void*)0,&g_47},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,&g_47},{(void*)0,&g_47},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,&g_47},{&g_47,(void*)0},{&g_47,(void*)0}},{{&g_47,(void*)0},{(void*)0,(void*)0},{&g_47,&g_47},{(void*)0,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,&g_47},{(void*)0,&g_47},{&g_47,&g_47},{&g_47,(void*)0}},{{&g_47,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{(void*)0,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{(void*)0,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{&g_47,(void*)0}},{{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47}},{{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,(void*)0},{&g_47,&g_47}},{{&g_47,(void*)0},{&g_47,&g_47},{(void*)0,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{(void*)0,(void*)0},{&g_47,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,&g_47}},{{&g_47,&g_47},{&g_47,&g_47},{(void*)0,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47}},{{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,&g_47},{&g_47,&g_47},{&g_47,&g_47},{(void*)0,(void*)0},{&g_47,&g_47},{&g_47,&g_47}},{{&g_47,&g_47},{&g_47,&g_47},{&g_47,(void*)0},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,&g_47},{&g_47,(void*)0},{(void*)0,&g_47},{&g_47,&g_47}}};
+static int8_t **g_49 = &g_50[0][4][0];
+static int8_t *** volatile g_48 = &g_49;/* VOLATILE GLOBAL g_48 */
+static int32_t g_66[2] = {(-1L),(-1L)};
+static int32_t g_73 = (-4L);
