90d7d3045b37eb415ce07e865886adf6130f77e6
saisi <saisi@users.noreply.github.com>
2017-01-23 20:12:03 +0000

    1485202321 Barcot

---
+++ 1485202321.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3953055739
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int32_t  f1;
+   const int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[6] = {0x97A8L,0xCBA2L,0x97A8L,0x97A8L,0xCBA2L,0x97A8L};
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
