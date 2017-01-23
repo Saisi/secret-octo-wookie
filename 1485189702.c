131e65050eb83aef9066faaad3a383afd9c83bb9
saisi <saisi@users.noreply.github.com>
2017-01-23 16:41:43 +0000

    1485189702 Anchistea

---
+++ 1485189702.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2282758079
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   volatile uint8_t  f1;
+   volatile uint8_t  f2;
+   unsigned f3 : 30;
+   int8_t * f4;
