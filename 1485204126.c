a18e46d4777096c8aad6e36edf14c6efa316b4b6
saisi <saisi@users.noreply.github.com>
2017-01-23 20:42:06 +0000

    1485204126 askewness

---
+++ 1485204126.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2233156173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 9UL;
+static int32_t g_46 = 3L;
+static int64_t g_50[6][2] = {{0x16928694A219909DLL,0x16928694A219909DLL},{0x16928694A219909DLL,0x16928694A219909DLL},{0x16928694A219909DLL,0x16928694A219909DLL},{0x16928694A219909DLL,0x16928694A219909DLL},{0x16928694A219909DLL,0x16928694A219909DLL},{0x16928694A219909DLL,0x16928694A219909DLL}};
+static uint8_t g_83 = 0xAFL;
+static int32_t *g_86 = &g_46;
+static int32_t **g_85 = &g_86;
+static uint32_t g_90 = 0x70D28CDDL;
+static int16_t g_120 = (-1L);
+static int64_t g_122[4] = {0L,0L,0L,0L};
