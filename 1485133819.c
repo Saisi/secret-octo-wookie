3e49e699ca031bc8d42653c673c8a0377c491b20
saisi <saisi@users.noreply.github.com>
2017-01-23 01:10:19 +0000

    1485133819 audiogenic

---
+++ 1485133819.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2403841592
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x7862FD41L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x96960F57L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 2L;
+static int16_t g_73 = 0x1D44L;
+static uint8_t g_81 = 1UL;
+static uint32_t g_84 = 0x92179B10L;
+static uint64_t g_86[4] = {2UL,2UL,2UL,2UL};
+static int64_t g_87 = 0L;
+static uint16_t g_103 = 0xE3EEL;
