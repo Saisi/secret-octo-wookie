f37b29bfca35a6fce66ac02b46c3e9c502626cb3
saisi <saisi@users.noreply.github.com>
2017-01-23 14:41:32 +0000

    1485182491 Abeokuta

---
+++ 1485182491.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4266011895
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 26;
+   int32_t  f1;
+   unsigned f2 : 25;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
