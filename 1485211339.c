a372f9ff9fea4de844ff033026d1a49aca63b019
saisi <saisi@users.noreply.github.com>
2017-01-23 22:42:20 +0000

    1485211339 Acre

---
+++ 1485211339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      677658961
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 7;
+   signed f1 : 2;
+   int64_t  f2;
+   volatile signed f3 : 30;
+   unsigned f4 : 13;
+   unsigned f5 : 7;
+};
