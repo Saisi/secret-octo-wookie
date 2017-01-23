42937e48f726208cf36e7806e785d7e1ec054ab3
saisi <saisi@users.noreply.github.com>
2017-01-23 03:40:32 +0000

    1485142832 Adorl

---
+++ 1485142832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3008320035
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_9 = 0xCCCE1E2C0D37AD43LL;
+static int32_t g_25 = 0x7C694A61L;
+static uint16_t g_60 = 0x638FL;
+static int8_t g_63 = 1L;
+static uint16_t g_73 = 65528UL;
+static int32_t *g_82 = &g_25;
+static int16_t g_88[9] = {0x5C70L,0x5C70L,0x5C70L,0x5C70L,0x5C70L,0x5C70L,0x5C70L,0x5C70L,0x5C70L};
+static uint8_t g_109 = 0x42L;
+static uint8_t g_110 = 0x20L;
