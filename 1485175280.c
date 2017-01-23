f4d2e61d401426a79467a5a21aa6456d80ca0f1b
saisi <saisi@users.noreply.github.com>
2017-01-23 12:41:21 +0000

    1485175280 agism

---
+++ 1485175280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3200849190
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int32_t  f1;
+   const volatile int64_t  f2;
+   uint16_t  f3;
+   volatile unsigned f4 : 23;
+};
+
+union U1 {
+   const int32_t  f0;
