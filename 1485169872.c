ded6cda9dffba7eb2d1e9cf5c292aac61ce116ef
saisi <saisi@users.noreply.github.com>
2017-01-23 11:11:13 +0000

    1485169872 archilithic

---
+++ 1485169872.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3098183833
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x0648261FL;
+static uint32_t g_25 = 0x5245F5B1L;
+static int32_t * volatile g_60 = (void*)0;/* VOLATILE GLOBAL g_60 */
+static int32_t g_62 = 0x0C88307BL;
+static int32_t * volatile g_61 = &g_62;/* VOLATILE GLOBAL g_61 */
+static uint32_t g_64 = 0x5B167D3DL;
+static int64_t g_81[3] = {(-1L),(-1L),(-1L)};
+static int32_t g_89[4] = {(-6L),(-6L),(-6L),(-6L)};
+static int32_t g_91 = (-5L);
