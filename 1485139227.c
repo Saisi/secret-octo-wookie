824b25ed5b7ea538033fbbf7bb9b89d8717d88ea
saisi <saisi@users.noreply.github.com>
2017-01-23 02:40:27 +0000

    1485139227 antlerless

---
+++ 1485139227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1189888684
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 4;
+   volatile unsigned f1 : 9;
+   int8_t  f2;
+   signed f3 : 31;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_6 = 0x80849F845D811F07LL;
