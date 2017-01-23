bbe6b037c8e3bde4ccc35708d64cf9c1d8270b19
saisi <saisi@users.noreply.github.com>
2017-01-23 23:12:23 +0000

    1485213142 adephagia

---
+++ 1485213142.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3272193509
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7[6] = {0x6BD085C9L,0x6BD085C9L,0x6BD085C9L,0x6BD085C9L,0x6BD085C9L,0x6BD085C9L};
+static int32_t g_10 = (-1L);
+static int32_t g_11 = 0x26F3F17EL;
+static int32_t g_12 = 0xC1D6B3BFL;
+static volatile int32_t g_13 = 3L;/* VOLATILE GLOBAL g_13 */
+static volatile int32_t g_14 = 0x88586FD5L;/* VOLATILE GLOBAL g_14 */
+static int32_t g_15 = 0x512A30A4L;
+static int8_t g_33 = 0x01L;
+static int32_t g_53 = (-4L);
