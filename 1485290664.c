300e9d6144e1198c9e3fe8d959b77cf648b466bf
saisi <saisi@users.noreply.github.com>
2017-01-24 20:44:24 +0000

    1485290664 Antiphates

---
+++ 1485290664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1749961920
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_10 = 0x4CL;
+static int32_t g_44 = 0L;
+static int32_t *g_43[2][10] = {{&g_44,(void*)0,(void*)0,&g_44,&g_44,(void*)0,(void*)0,&g_44,&g_44,(void*)0},{&g_44,&g_44,(void*)0,(void*)0,&g_44,&g_44,(void*)0,(void*)0,&g_44,&g_44}};
+static int32_t ** const  volatile g_47 = &g_43[1][1];/* VOLATILE GLOBAL g_47 */
+static uint16_t g_95 = 0UL;
+static uint16_t g_101[6][3][4] = {{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}},{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}},{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}},{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}},{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}},{{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL},{7UL,7UL,7UL,7UL}}};
+static int8_t g_103[8] = {0x9DL,0x9DL,0x9DL,0x9DL,0x9DL,0x9DL,0x9DL,0x9DL};
+static volatile int32_t g_112 = 0xFC28A9D4L;/* VOLATILE GLOBAL g_112 */
+static uint64_t g_140 = 0xC1874894D46530D6LL;
