07e663a22e6018eee649984d7c6c2dbabe727f1e
saisi <saisi@users.noreply.github.com>
2017-01-24 06:13:02 +0000

    1485238382 analogicalness

---
+++ 1485238382.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2822724805
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   signed f1 : 20;
+   volatile signed f2 : 7;
+   const int8_t  f3;
+   volatile signed f4 : 15;
+   signed f5 : 1;
+};
