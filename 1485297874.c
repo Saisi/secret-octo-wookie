f9e058a98892f12cd1ac3f563ad0f7c676b95b33
saisi <saisi@users.noreply.github.com>
2017-01-24 22:44:35 +0000

    1485297874 acetothienone

---
+++ 1485297874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1739208108
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_8 = 3UL;
+static const struct S0 g_16 = {4294967289UL};
