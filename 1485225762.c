ed8cade81ac0907eb26886456b62a842f25af5b6
saisi <saisi@users.noreply.github.com>
2017-01-24 02:42:42 +0000

    1485225762 almemor

---
+++ 1485225762.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2964110229
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   int32_t  f0;
+   int8_t  f1;
+   int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = (-8L);
+static uint32_t g_6 = 0x9A2B82C4L;
+static uint32_t g_8 = 0xC20F746EL;
