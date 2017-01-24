6004df3ffa3bff55576ef60b45813c794429b198
saisi <saisi@users.noreply.github.com>
2017-01-24 03:12:45 +0000

    1485227565 amniomancy

---
+++ 1485227565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3749926981
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 1;
+   unsigned f1 : 20;
+   const volatile signed f2 : 7;
+   unsigned f3 : 22;
+   unsigned f4 : 15;
+   const signed f5 : 12;
+   unsigned f6 : 27;
