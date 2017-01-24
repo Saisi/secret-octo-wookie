c030966f6b584fbd7048a980b210bd5deb05b629
saisi <saisi@users.noreply.github.com>
2017-01-24 23:44:40 +0000

    1485301479 antimonarchicalness

---
+++ 1485301479.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4202411732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 24;
+   signed f1 : 13;
+   signed f2 : 27;
+};
+
+union U1 {
+   const volatile int32_t  f0;
+};
+
