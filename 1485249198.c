aa28485c0a9c9ccba0395b1dec553ceccae38c32
saisi <saisi@users.noreply.github.com>
2017-01-24 09:13:18 +0000

    1485249198 antibilious

---
+++ 1485249198.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1794356492
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   uint64_t  f1;
+   const volatile uint16_t  f2;
+   uint16_t  f3;
+   unsigned f4 : 3;
+};
+
+struct S1 {
+   unsigned f0 : 9;
