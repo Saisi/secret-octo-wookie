a7f8ea85bd0e73e8c93e63c2ded5dd7e1ae16c49
saisi <saisi@users.noreply.github.com>
2017-01-24 10:43:28 +0000

    1485254607 AEGIS

---
+++ 1485254607.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      148685259
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 4;
+   unsigned f1 : 11;
+   unsigned f2 : 2;
+};
+
+union U1 {
+   int8_t * f0;
+   int32_t  f1;
+   volatile uint16_t  f2;
