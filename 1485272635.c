fd674e4487a750f595a2a4ed166727378f4db691
saisi <saisi@users.noreply.github.com>
2017-01-24 15:43:55 +0000

    1485272635 atmological

---
+++ 1485272635.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      252639261
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC70BB743L;
+static int32_t g_8 = 1L;
+static int32_t * volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+static int32_t g_34 = 0x6E3F0E26L;
+static int32_t *g_55 = (void*)0;
+static int32_t ** volatile g_54 = &g_55;/* VOLATILE GLOBAL g_54 */
+static int32_t g_70 = 0x4DDAF140L;
+static int32_t *g_71 = &g_70;
+static int32_t g_84 = 0x111CD9E9L;
