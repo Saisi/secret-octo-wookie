5e64eb02738dba39ac0dccf44be8410e9b988927
saisi <saisi@users.noreply.github.com>
2017-01-24 22:14:32 +0000

    1485296072 androclinia

---
+++ 1485296072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      536894768
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 1L;
+static int32_t g_16 = 0xDD7770B1L;
+static int32_t *g_52 = (void*)0;
+static int32_t g_63 = 0L;
+static int8_t g_71 = 0xC0L;
+static int8_t g_73 = 0x0DL;
+static uint64_t g_79 = 0x5EE2CBD03168F1D3LL;
+static uint64_t g_83 = 18446744073709551615UL;
+static uint64_t *g_82 = &g_83;
