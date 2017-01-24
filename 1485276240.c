ce59567e344c5c79febe6579e8a47c220477e807
saisi <saisi@users.noreply.github.com>
2017-01-24 16:44:01 +0000

    1485276240 adventitiousness

---
+++ 1485276240.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2250849842
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 7;
+   signed : 0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const uint8_t  f0;
+   uint64_t  f1;
