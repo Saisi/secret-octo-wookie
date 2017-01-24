9a52a87945ce2648e40290bdc0f992ac825c115a
saisi <saisi@users.noreply.github.com>
2017-01-24 21:14:27 +0000

    1485292466 Aeschynanthus

---
+++ 1485292466.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2568377248
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static struct S0 g_72 = {0x082E70D5L};
