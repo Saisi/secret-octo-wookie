64e372f1baadbcb622175136d5ec12da4f659719
saisi <saisi@users.noreply.github.com>
2017-01-24 05:12:57 +0000

    1485234777 Archencephala

---
+++ 1485234777.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3787181604
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_2 = 3L;
+static int32_t g_15[9] = {0xA86BA47CL,6L,0xA86BA47CL,0xA86BA47CL,6L,0xA86BA47CL,0xA86BA47CL,6L,0xA86BA47CL};
+static int32_t *g_14 = &g_15[2];
+static uint64_t g_40 = 0x2777FC3FE767E792LL;
+static uint16_t g_44 = 0xAEA0L;
+static int32_t g_46 = 0xC50EC319L;
+static volatile uint64_t g_58 = 1UL;/* VOLATILE GLOBAL g_58 */
+static int32_t g_92 = (-9L);
+static uint16_t g_95 = 65528UL;
