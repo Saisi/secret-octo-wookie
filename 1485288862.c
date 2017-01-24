676e0b67475205ba0345e43df857e70c09dbd6b8
saisi <saisi@users.noreply.github.com>
2017-01-24 20:14:22 +0000

    1485288862 abmhos

---
+++ 1485288862.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1042070717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 7L;
+static uint16_t g_11 = 0x8067L;
+static int8_t g_50[1][4][3] = {{{(-8L),(-8L),(-8L)},{(-8L),(-8L),(-8L)},{(-8L),(-8L),(-8L)},{(-8L),(-8L),(-8L)}}};
+static int8_t g_52 = (-9L);
+static uint8_t g_63 = 246UL;
