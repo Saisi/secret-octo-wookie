4def2d08bdf15d9be471ed7272a04f7d7a714470
saisi <saisi@users.noreply.github.com>
2017-01-24 14:13:47 +0000

    1485267227 Anchiale

---
+++ 1485267227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3176722826
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static uint64_t g_12 = 18446744073709551609UL;
+static uint32_t g_14[2][7] = {{0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL},{0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL,0x49CEC79EL}};
+static int32_t g_25 = (-8L);
+static int32_t g_54 = 1L;
