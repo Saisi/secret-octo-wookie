068e3d7999d66c3801276683b5a37c95c76cab5a
saisi <saisi@users.noreply.github.com>
2017-01-25 03:14:59 +0000

    1485314098 agarita

---
+++ 1485314098.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1585928946
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 7;
+   unsigned : 0;
+   signed f1 : 2;
+   signed f2 : 6;
+   volatile unsigned f3 : 29;
+   unsigned : 0;
+   const unsigned f4 : 30;
+   unsigned f5 : 27;
+   unsigned f6 : 19;
