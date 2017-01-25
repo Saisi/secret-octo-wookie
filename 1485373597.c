6143852b0f3b9a81534b6e4c6e58d45179a38130
saisi <saisi@users.noreply.github.com>
2017-01-25 19:46:37 +0000

    1485373597 Asterolepidae

---
+++ 1485373597.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2482188893
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_8[7] = {(-1L),0x5CA9869EL,(-1L),(-1L),0x5CA9869EL,(-1L),(-1L)};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
