e740dcc704974fb27b4aadf34ec3493265728d50
saisi <saisi@users.noreply.github.com>
2017-01-25 05:15:14 +0000

    1485321313 antisacerdotal

---
+++ 1485321313.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      441830409
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[9][5][1] = {{{1L},{0x1651328FL},{(-1L)},{0xE5C7A00FL},{2L}},{{0x57BF5CD5L},{(-5L)},{0x57BF5CD5L},{2L},{0xE5C7A00FL}},{{(-1L)},{0x1651328FL},{1L},{0x6951AD38L},{1L}},{{0x1651328FL},{(-1L)},{0xE5C7A00FL},{2L},{0x57BF5CD5L}},{{(-5L)},{0x57BF5CD5L},{2L},{0xE5C7A00FL},{(-1L)}},{{0x1651328FL},{1L},{0x6951AD38L},{1L},{0x1651328FL}},{{(-1L)},{0xE5C7A00FL},{2L},{0x57BF5CD5L},{(-5L)}},{{0x57BF5CD5L},{2L},{0xE5C7A00FL},{(-1L)},{0x1651328FL}},{{1L},{0x6951AD38L},{1L},{0x1651328FL},{(-1L)}}};
+static int32_t g_16 = 0xF42965CCL;
+static int32_t * volatile g_15 = &g_16;/* VOLATILE GLOBAL g_15 */
+static volatile int32_t g_19 = 1L;/* VOLATILE GLOBAL g_19 */
+static int32_t g_20 = (-10L);
+static int32_t g_21 = 0x52772FB5L;
+static int32_t g_42 = 1L;
+static volatile int16_t g_49 = 0L;/* VOLATILE GLOBAL g_49 */
+static volatile int8_t g_50[1] = {0xB3L};
