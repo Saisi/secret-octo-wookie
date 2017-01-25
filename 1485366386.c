7dd4085438c019894b80ad5ef6b49b66104c9187
saisi <saisi@users.noreply.github.com>
2017-01-25 17:46:26 +0000

    1485366386 adenophyllous

---
+++ 1485366386.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      749227559
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 18;
+   int64_t  f1;
+   const volatile int32_t  f2;
+   const int32_t  f3;
+   uint16_t  f4;
+   uint16_t  f5;
+   int32_t  f6;
