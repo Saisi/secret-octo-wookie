0819abb7db0d357e1f4e7bdf9df0af6c9272759d
saisi <saisi@users.noreply.github.com>
2017-01-25 07:45:28 +0000

    1485330327 aerophotography

---
+++ 1485330327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3915263956
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2][3][6] = {{{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL},{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL},{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL}},{{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL},{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL},{0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL,0xA5D6C05BL}}};
+static int32_t g_3 = 0xEEDE0B40L;
+static int32_t g_19 = 1L;
+static int8_t g_51 = 0x76L;
+static int32_t g_65 = 0xD8E2DC66L;
+static int32_t * volatile g_64 = &g_65;/* VOLATILE GLOBAL g_64 */
+static int16_t g_75 = 0x26B3L;
+static int8_t g_81 = 1L;
+static int32_t *g_90 = &g_19;
