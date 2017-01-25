58f9afdf343d65f3123188d8faf643ae83afa9e7
saisi <saisi@users.noreply.github.com>
2017-01-25 13:16:01 +0000

    1485350161 barbitos

---
+++ 1485350161.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3285070716
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_15 = 1L;
+static uint32_t g_26 = 0x69AC54C9L;
