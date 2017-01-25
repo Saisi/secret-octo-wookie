25f606a095abbd69972bb7002833b850dd9c4f28
saisi <saisi@users.noreply.github.com>
2017-01-25 17:16:24 +0000

    1485364583 arbuscule

---
+++ 1485364583.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4241475359
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   unsigned f1 : 13;
+   unsigned f2 : 28;
+   signed f3 : 23;
+   volatile signed f4 : 12;
+   const int32_t  f5;
+   unsigned f6 : 25;
