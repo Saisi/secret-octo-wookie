91b3219638f28c248f8b1e4953bfafc1c17a9a7e
saisi <saisi@users.noreply.github.com>
2017-01-25 16:16:19 +0000

    1485360978 antimonium

---
+++ 1485360978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2565271542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xF6AF1124L;
+static uint16_t g_8 = 0x37B0L;
+static int32_t g_14 = 0x0091DF82L;
+static int32_t * volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
+static int8_t g_21 = 0x2CL;
+static uint8_t g_41 = 253UL;
+static int16_t g_53 = 0xF605L;
+static int64_t g_55 = (-10L);
+static int8_t g_85 = 0x72L;
