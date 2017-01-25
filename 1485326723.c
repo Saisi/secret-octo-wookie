c92c0d8cc848a15d9e603039665de35c7698dc2c
saisi <saisi@users.noreply.github.com>
2017-01-25 06:45:23 +0000

    1485326723 albuminometer

---
+++ 1485326723.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3034224605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   volatile int16_t  f0;
+   unsigned f1 : 8;
+   const volatile unsigned f2 : 4;
+   int32_t  f3;
+   const uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
