d61744cc75cc4ed7bc62ee653e84ce163c97bde4
saisi <saisi@users.noreply.github.com>
2017-01-25 09:15:37 +0000

    1485335735 ablauts

---
+++ 1485335735.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2663498313
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[4][3][3] = {{{0x3061ECE3L,0x66941FC9L,0x66941FC9L},{0x47C4D145L,0x49B76EEAL,0x6F44401DL},{(-1L),0x3061ECE3L,0xBCD76CE4L}},{{0x47C4D145L,0x47C4D145L,0xC74AA0D7L},{0x3061ECE3L,(-1L),0x276B87D0L},{0x49B76EEAL,0x47C4D145L,0x49B76EEAL}},{{0x66941FC9L,0x3061ECE3L,0xEEC54165L},{(-3L),0x49B76EEAL,0x49B76EEAL},{0xEEC54165L,0x66941FC9L,0x276B87D0L}},{{1L,(-3L),0xC74AA0D7L},{0xEEC54165L,0xEEC54165L,0xBCD76CE4L},{(-3L),1L,0x6F44401DL}}};
+static int32_t g_8 = 1L;
+static int32_t * const  volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+static uint16_t g_57 = 0x05D3L;
+static uint64_t g_60 = 18446744073709551612UL;
+static int8_t g_100 = 4L;
+static uint32_t g_101 = 0x8C93045DL;
+static int64_t g_103 = 0xBC7639B44AAF27CELL;
+static int16_t g_133 = 0x2056L;
