032b3a2be5f25ce5c456e08a4007a07d84b13f08
saisi <saisi@users.noreply.github.com>
2017-01-25 16:46:21 +0000

    1485362781 agenes

---
+++ 1485362781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4060622404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2[2][5][5] = {{{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)}},{{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,(-10L),0xBCL,0xBCL,(-10L)},{0L,0L,(-10L),(-10L),0L}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
