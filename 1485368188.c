413133eb51c2f907b091118a6e3ff342ae55ab4e
saisi <saisi@users.noreply.github.com>
2017-01-25 18:16:29 +0000

    1485368188 apraxia

---
+++ 1485368188.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2161927800
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 10;
+   signed f1 : 23;
+   const int16_t  f2;
+   const volatile signed f3 : 21;
+   signed f4 : 6;
+   volatile unsigned f5 : 26;
+   signed f6 : 19;
