fc27487f5a037ea8f34ab517cafe8c76f97b87af
saisi <saisi@users.noreply.github.com>
2017-01-26 21:19:09 +0000

    1485465549 antiabrasion

---
+++ 1485465549.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3811711660
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   signed f1 : 17;
+   const int16_t  f2;
+   signed f3 : 16;
+   volatile uint32_t  f4;
+   uint32_t  f5;
+   unsigned f6 : 6;
