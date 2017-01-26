1ff935b54ae17b986a80436eb6289963dc5e9a6b
saisi <saisi@users.noreply.github.com>
2017-01-26 20:19:04 +0000

    1485461944 Alejandrina

---
+++ 1485461944.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2259236543
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[5] = {0xC90A35F3L,0xC90A35F3L,0xC90A35F3L,0xC90A35F3L,0xC90A35F3L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
