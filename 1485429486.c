e8d3e7f37dcac37831cbb148c1789245723d504f
saisi <saisi@users.noreply.github.com>
2017-01-26 11:18:06 +0000

    1485429486 amoeba

---
+++ 1485429486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      386554024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+};
+
+struct S1 {
+   unsigned f0 : 7;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+   const volatile uint64_t  f3;
+   uint16_t  f4;
