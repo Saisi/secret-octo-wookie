4b39a4c9e40bb07cd176785fc3b9bc0380f793a3
saisi <saisi@users.noreply.github.com>
2017-01-26 12:48:22 +0000

    1485434901 Aquarii

---
+++ 1485434901.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      350661554
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 7;
+   volatile signed : 0;
+};
+
+struct S1 {
+   volatile signed f0 : 10;
+   volatile uint64_t  f1;
+   const volatile int32_t  f2;
+   volatile int32_t  f3;
