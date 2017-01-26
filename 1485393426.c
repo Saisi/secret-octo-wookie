0ea7fdbe937c6bd24c3c027cf092bd7194f14832
saisi <saisi@users.noreply.github.com>
2017-01-26 01:17:07 +0000

    1485393426 amphitheatre

---
+++ 1485393426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1048631655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int8_t * f1;
+};
+
+union U1 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
