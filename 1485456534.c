9f1b67dc8bde0714c306d16240adee32e4bc17df
saisi <saisi@users.noreply.github.com>
2017-01-26 18:48:54 +0000

    1485456534 Altaloma

---
+++ 1485456534.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1750357129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2] = {0xBF2D3F50L,0xBF2D3F50L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
