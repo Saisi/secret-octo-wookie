c2dc18a97411cc7b2a42ecaf5983dacbe011dca8
saisi <saisi@users.noreply.github.com>
2017-01-26 14:18:29 +0000

    1485440309 abraum

---
+++ 1485440309.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      388387138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static uint16_t g_6[8] = {65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL};
+static const int32_t g_57 = 0x7DF2DDFDL;
+static uint16_t g_60 = 0xE84CL;
+static int16_t g_83[3] = {0x3098L,0x3098L,0x3098L};
+static uint16_t g_85 = 65535UL;
+static int16_t *g_90 = &g_83[1];
+static int16_t *g_91 = &g_83[2];
+static uint32_t g_96 = 0xF8785D54L;
