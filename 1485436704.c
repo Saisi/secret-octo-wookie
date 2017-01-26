1b993219952961f23649d4f69ad5484541d4b5f4
saisi <saisi@users.noreply.github.com>
2017-01-26 13:18:24 +0000

    1485436704 accentors

---
+++ 1485436704.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1936269822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 13;
+   unsigned f1 : 14;
+   volatile unsigned f2 : 24;
+   unsigned f3 : 4;
+   const signed f4 : 3;
+   signed f5 : 14;
+   const unsigned f6 : 20;
