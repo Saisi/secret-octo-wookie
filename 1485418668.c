e1cab8f21d3fb72405115676d9cfdd7ac1bf9ee9
saisi <saisi@users.noreply.github.com>
2017-01-26 08:17:49 +0000

    1485418668 bafflers

---
+++ 1485418668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1786719409
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = (-9L);
+static int32_t g_7[1][9][4] = {{{0xBAFCB143L,(-1L),0xBAFCB143L,(-1L)},{(-1L),(-1L),0xAD8530DEL,(-9L)},{(-1L),0xBF9F3617L,0xBF9F3617L,(-1L)},{0xBAFCB143L,(-9L),0xBF9F3617L,(-1L)},{(-1L),(-1L),0xAD8530DEL,(-1L)},{(-1L),0xBF9F3617L,0xBAFCB143L,(-1L)},{0xBAFCB143L,(-1L),(-1L),(-1L)},{(-9L),(-9L),0xAD8530DEL,(-1L)},{(-9L),0xBF9F3617L,(-1L),(-9L)}}};
+static int32_t * volatile g_6 = &g_7[0][3][0];/* VOLATILE GLOBAL g_6 */
+static uint16_t g_51 = 0x5276L;
+static int32_t g_77 = 5L;
+static int32_t g_78[5][10] = {{(-6L),0x522227AEL,0x40E8EEE9L,8L,0x29C85ED0L,5L,0xDCD109C4L,0L,0x0A24D0A3L,0xF3C20DDCL},{(-6L),8L,0x0A24D0A3L,0xDCD109C4L,0L,(-2L),0L,(-2L),0L,0xDCD109C4L},{0L,0xFD0F0649L,0L,0xC4EEBD0FL,(-8L),0x40E8EEE9L,0xC83E95E5L,0x0A24D0A3L,0xE10E1137L,(-1L)},{0xFD0F0649L,(-8L),0L,(-6L),0xC83E95E5L,0xFB4F1CBCL,0x2BCF3F1EL,0x0A24D0A3L,0x0A24D0A3L,0x2BCF3F1EL},{0x29C85ED0L,0xE10E1137L,0L,0L,0xE10E1137L,0x29C85ED0L,0x0A24D0A3L,(-2L),0xF3C20DDCL,0L}};
+static int32_t **g_102 = (void*)0;
+static int8_t g_107 = 1L;
+static int32_t g_121 = 0x3EF0F463L;
