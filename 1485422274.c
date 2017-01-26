608ef0118b14c3b4de79fb5ece6320685fef4579
saisi <saisi@users.noreply.github.com>
2017-01-26 09:17:55 +0000

    1485422274 archegone

---
+++ 1485422274.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      80136857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile unsigned f0 : 11;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5] = {5L,5L,5L,5L,5L};
+static volatile int32_t ** const  volatile g_40 = (void*)0;/* VOLATILE GLOBAL g_40 */
+static uint8_t g_63[9][4] = {{255UL,0xAFL,0xC8L,3UL},{0xCCL,0x9DL,0xE1L,0xC8L},{0x34L,0UL,0UL,255UL},{0x34L,0xE1L,0xE1L,0x34L},{0xCCL,255UL,0xC8L,0xE1L},{255UL,0UL,8UL,3UL},{0x9DL,0xCCL,0xE1L,3UL},{0xC5L,0UL,0xC5L,0xE1L},{0x34L,255UL,2UL,0x34L}};
+static int32_t g_64 = (-4L);
+static int32_t g_68[9][5] = {{0xF781BE2FL,9L,0xF781BE2FL,9L,0xF781BE2FL},{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xF781BE2FL,9L,0xF781BE2FL,9L,0xF781BE2FL},{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xF781BE2FL,9L,0xF781BE2FL,9L,0xF781BE2FL},{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xF781BE2FL,9L,0xF781BE2FL,9L,0xF781BE2FL},{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xF781BE2FL,9L,0xF781BE2FL,9L,0xF781BE2FL}};
