bc0e2c56a6e2c9bbf2224df003be55bdda879379
saisi <saisi@users.noreply.github.com>
2017-01-26 19:49:01 +0000

    1485460139 aithochroi

---
+++ 1485460139.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3159215733
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static uint16_t g_10[1][6][1] = {{{65535UL},{0x0E35L},{0x0E35L},{65535UL},{0x0E35L},{0x0E35L}}};
+static int32_t g_18[2][3] = {{0xA715CC48L,0xA715CC48L,0xA715CC48L},{0xA715CC48L,0xA715CC48L,0xA715CC48L}};
+static int32_t *g_17 = &g_18[1][0];
+static const int32_t *g_28[3][1][7] = {{{&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0]}},{{&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0]}},{{&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0],&g_18[1][0]}}};
+static int32_t g_47 = 0x511C25C5L;
+static int16_t g_58 = 0xDB12L;
+static int16_t g_60 = 0x618EL;
+static int16_t g_62 = 6L;
