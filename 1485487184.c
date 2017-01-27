c6ea52f4025d4a4c6b9a50cb9c2bd92951bede58
saisi <saisi@users.noreply.github.com>
2017-01-27 03:19:45 +0000

    1485487184 antimorality

---
+++ 1485487184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      988836619
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0x260417BEL;
+static volatile int32_t g_24 = 0x9A07B304L;/* VOLATILE GLOBAL g_24 */
+static volatile int32_t g_25 = 0x20B50F6BL;/* VOLATILE GLOBAL g_25 */
+static volatile int32_t g_26 = 0x266D8FF8L;/* VOLATILE GLOBAL g_26 */
+static int32_t g_27 = 0xBED162C5L;
