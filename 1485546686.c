4315d2de4dc77ae1ded91bf5700dff5586c988e1
saisi <saisi@users.noreply.github.com>
2017-01-27 19:51:27 +0000

    1485546686 bagpipes

---
+++ 1485546686.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2785255966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   uint32_t  f1;
+   const signed f2 : 8;
+   const uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 18446744073709551615UL;/* VOLATILE GLOBAL g_4 */
+static uint8_t g_6 = 0xD2L;
