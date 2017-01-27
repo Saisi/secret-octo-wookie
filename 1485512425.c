82cd99d43c5d9314a28df7e118a5ee9dce841de3
saisi <saisi@users.noreply.github.com>
2017-01-27 10:20:25 +0000

    1485512425 avowry

---
+++ 1485512425.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      126606572
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint16_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S1 g_2 = {0xFC39L};/* VOLATILE GLOBAL g_2 */
+static volatile struct S1 * volatile g_3 = &g_2;/* VOLATILE GLOBAL g_3 */
