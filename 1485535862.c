f0fd2dfac541cab76f8345527359ac8972e3a1cd
saisi <saisi@users.noreply.github.com>
2017-01-27 16:51:02 +0000

    1485535862 automobilists

---
+++ 1485535862.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      963758611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 8;
+};
+
+struct S1 {
+   volatile uint8_t  f0;
+   unsigned f1 : 21;
+};
+
+struct S2 {
