babc13fd6392a02357105f8b0c522cb927983d02
saisi <saisi@users.noreply.github.com>
2017-01-27 23:51:49 +0000

    1485561109 altarlet

---
+++ 1485561109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      501816107
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 20;
+   const signed f1 : 27;
+   uint64_t  f2;
+};
+#pragma pack(pop)
+
+struct S1 {
