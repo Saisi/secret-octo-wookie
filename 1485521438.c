c7924c86616c919d2eb3d49dc16ec43e978a01e3
saisi <saisi@users.noreply.github.com>
2017-01-27 12:50:39 +0000

    1485521438 adenophyma

---
+++ 1485521438.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2527358655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_16 = 0xED1AD67AL;
+static int16_t g_29 = 0x9A27L;
+static uint32_t g_33 = 4294967295UL;
+static uint16_t g_48[1][1][6] = {{{5UL,5UL,5UL,5UL,5UL,5UL}}};
+static uint64_t g_50[2] = {0xF1BA1372625AA9C4LL,0xF1BA1372625AA9C4LL};
+static uint8_t g_58 = 0x83L;
+static uint32_t g_76 = 1UL;
+static uint8_t g_80 = 0x01L;
+static uint64_t g_82 = 0x85B90C327405EF11LL;
