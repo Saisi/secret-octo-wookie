c5c324965204f9805f408fc9a25bb10d5406471a
saisi <saisi@users.noreply.github.com>
2017-01-27 03:49:47 +0000

    1485488987 asclepius

---
+++ 1485488987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3407908379
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 18446744073709551609UL;
+static int32_t g_5 = 0L;
+static uint32_t g_28[6] = {0x55F5DDA3L,0x55F5DDA3L,0x55F5DDA3L,0x55F5DDA3L,0x55F5DDA3L,0x55F5DDA3L};
+static uint64_t g_52 = 0x0BB6E743FD0416CBLL;
+static uint64_t g_81[9] = {18446744073709551615UL,2UL,18446744073709551615UL,18446744073709551615UL,2UL,18446744073709551615UL,18446744073709551615UL,2UL,18446744073709551615UL};
+static int32_t g_82[1] = {8L};
+static uint64_t g_83 = 18446744073709551615UL;
+static uint32_t g_86[9] = {0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL,0x47D5CE9AL};
+static volatile int32_t *g_98 = (void*)0;
