b62c04bb3bc3c6bd1371dca487de2cedcc98bc51
saisi <saisi@users.noreply.github.com>
2017-01-27 17:51:16 +0000

    1485539475 astringe

---
+++ 1485539475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2326329493
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * f0;
+   int8_t  f1;
+   uint64_t  f2;
+   const uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x42A43D95L;/* VOLATILE GLOBAL g_3 */
