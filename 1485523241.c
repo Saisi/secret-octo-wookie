85bcd49f56bb459d8231fcd3ddd4cf1bf72db86a
saisi <saisi@users.noreply.github.com>
2017-01-27 13:20:44 +0000

    1485523241 Alcyonium

---
+++ 1485523241.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      335349246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 13;
+   unsigned f1 : 14;
+   signed f2 : 24;
+   unsigned f3 : 4;
+   const volatile signed f4 : 3;
+   const signed f5 : 14;
+};
