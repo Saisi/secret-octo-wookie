89d84860b517dc1d1aadddb055e24ae89b20b2e2
saisi <saisi@users.noreply.github.com>
2017-01-27 21:51:37 +0000

    1485553897 aikona

---
+++ 1485553897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1831881612
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x3838A1F9L;
+static uint8_t g_4 = 0x53L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
