f3505ac17f8008976eb3484c14be2a9685d3dc16
saisi <saisi@users.noreply.github.com>
2017-01-27 06:50:04 +0000

    1485499804 antitypes

---
+++ 1485499804.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2027831657
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 19;
+   unsigned f1 : 30;
+   unsigned f2 : 9;
+   signed f3 : 16;
+   volatile signed f4 : 1;
+   signed f5 : 22;
+   unsigned f6 : 29;
+   volatile unsigned f7 : 28;
+};
