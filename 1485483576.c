bccb0a6ff5d21c6e373dfd4250bff1565fc441db
saisi <saisi@users.noreply.github.com>
2017-01-27 02:19:37 +0000

    1485483576 anchorets

---
+++ 1485483576.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      919972939
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   uint64_t  f1;
+   signed f2 : 2;
+};
+
+union U1 {
+   int64_t  f0;
+   volatile unsigned f1 : 19;
+   int32_t  f2;
