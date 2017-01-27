7a46fefd30238eea79cc91d20bb5bc7a1bc88550
saisi <saisi@users.noreply.github.com>
2017-01-27 22:51:43 +0000

    1485557502 Akhlame

---
+++ 1485557502.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3744458462
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_27 = 18446744073709551615UL;
+static int64_t g_28 = 1L;
+static uint64_t *g_46 = &g_27;
+static int32_t g_65 = 0L;
+static int32_t * const g_64 = &g_65;
