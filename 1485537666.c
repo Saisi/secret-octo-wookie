5b94bad9465a6b6d4537b991ec8852e78f69823c
saisi <saisi@users.noreply.github.com>
2017-01-27 17:21:13 +0000

    1485537666 Baraboo

---
+++ 1485537666.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1913779033
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   signed : 0;
+   unsigned f1 : 22;
+   signed f2 : 2;
+};
+#pragma pack(pop)
+
