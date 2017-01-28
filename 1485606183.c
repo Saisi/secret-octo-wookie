88b202becf9b95bcdff6b0e7be5a06a6458cb11d
saisi <saisi@users.noreply.github.com>
2017-01-28 12:23:04 +0000

    1485606183 accelerometers

---
+++ 1485606183.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      864782223
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_6 = 65535UL;
+static volatile int32_t g_9 = (-1L);/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10 = 0x7CFAF425L;/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = (-5L);
+static int32_t g_13 = 0x457D6700L;
+static uint16_t g_66 = 1UL;
+static uint8_t g_101 = 255UL;
+static int32_t g_107[1] = {0xD994868BL};
+static int32_t *g_127 = &g_107[0];
