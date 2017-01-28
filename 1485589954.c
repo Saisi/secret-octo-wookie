6f663fb96cd681652dae0f82ab0fb4e14f886315
saisi <saisi@users.noreply.github.com>
2017-01-28 07:52:35 +0000

    1485589954 Amita

---
+++ 1485589954.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2186816850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   unsigned f1 : 16;
+   unsigned f2 : 18;
+   signed f3 : 22;
+   unsigned f4 : 13;
+   const unsigned f5 : 16;
+};
