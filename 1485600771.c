58c1d9c26836afc00ea5826efbce216b354eb488
saisi <saisi@users.noreply.github.com>
2017-01-28 10:52:52 +0000

    1485600771 anotropia

---
+++ 1485600771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3361470163
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   volatile unsigned f1 : 31;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+};
+
+union U2 {
