a4e4a1cb75956f64171cbc8921b1c84438867ffb
saisi <saisi@users.noreply.github.com>
2017-01-28 10:22:48 +0000

    1485598968 abayah

---
+++ 1485598968.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2645477427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_11 = 18446744073709551615UL;
+static uint64_t g_17 = 0xD91E66C619A80BFBLL;
+static union U0 g_49 = {0xD8L};
+static int32_t g_63 = (-1L);
+static uint32_t g_99 = 0x2A59A03CL;
