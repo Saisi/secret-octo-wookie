3de9c7b854e094ef3a00f1589f15a0fe8258b744
saisi <saisi@users.noreply.github.com>
2017-01-28 16:53:29 +0000

    1485622408 Antares

---
+++ 1485622408.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2678626026
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   int8_t  f1;
+};
+
+union U1 {
+   int16_t  f0;
+   const int16_t  f1;
+   volatile int8_t * f2;
+};
