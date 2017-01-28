cfa16bdd10262c91c4b66cd6594ef271fe0abc00
saisi <saisi@users.noreply.github.com>
2017-01-28 23:24:10 +0000

    1485645850 asthenopic

---
+++ 1485645850.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2917028258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S2 {
+   signed f0 : 20;
+   const unsigned f1 : 2;
+   volatile uint16_t  f2;
+   unsigned f3 : 14;
+   const unsigned f4 : 21;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[3][2][7] = {{{1L,0x15E314A5L,0x15E314A5L,1L,1L,0x15E314A5L,0x15E314A5L},{(-10L),1L,(-10L),1L,(-10L),1L,(-10L)}},{{1L,1L,0x15E314A5L,0x15E314A5L,1L,1L,0x15E314A5L},{(-10L),1L,(-10L),1L,(-10L),1L,(-10L)}},{{1L,0x15E314A5L,0x15E314A5L,1L,1L,0x15E314A5L,0x15E314A5L},{(-10L),1L,(-10L),1L,(-10L),1L,(-10L)}}};
