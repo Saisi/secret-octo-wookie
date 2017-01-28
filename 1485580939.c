3dae8a0c4e116242d561e83697b6ccc8bacf2b81
saisi <saisi@users.noreply.github.com>
2017-01-28 05:22:20 +0000

    1485580939 arborise

---
+++ 1485580939.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1194464712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2 = 18446744073709551615UL;
+static uint64_t g_5 = 0x4EA6801FC3EEE2FDLL;
+static uint16_t g_24 = 65533UL;
+static uint32_t g_34 = 0xDC0F1FA6L;
+static uint16_t g_58 = 0x84A4L;
+static uint16_t *g_57 = &g_58;
+static int32_t g_87 = 0x56627F1AL;
+static int8_t g_97 = (-10L);
+static volatile uint16_t g_98 = 0x6683L;/* VOLATILE GLOBAL g_98 */
