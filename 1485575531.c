2f6eda5f648fd63e5d4860ac93956c931d758282
saisi <saisi@users.noreply.github.com>
2017-01-28 03:52:12 +0000

    1485575531 anaerobiont

---
+++ 1485575531.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1584146434
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+   uint64_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x8D32FFABL;/* VOLATILE GLOBAL g_2 */
