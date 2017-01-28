8c29741ff9e56960f986e338726119d073de1682
saisi <saisi@users.noreply.github.com>
2017-01-28 01:52:00 +0000

    1485568320 agribusiness

---
+++ 1485568320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1804264355
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   uint8_t  f1;
+   const int8_t * volatile  f2;
+   const volatile uint8_t  f3;
+   const volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[10] = {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L};
