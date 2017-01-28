f5eb68c7bbeea85bc4bd3d017e63e5d605067da0
saisi <saisi@users.noreply.github.com>
2017-01-28 08:22:38 +0000

    1485591757 argosy

---
+++ 1485591757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      594440250
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 11;
+   const unsigned f1 : 20;
+   const unsigned f2 : 6;
+   unsigned f3 : 26;
+   volatile unsigned f4 : 1;
+   const unsigned f5 : 20;
+   volatile signed f6 : 2;
+   const signed f7 : 15;
+   const signed f8 : 23;
