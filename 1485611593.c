1ef59747d29d7323ea6e367ff691f3047b0605ae
saisi <saisi@users.noreply.github.com>
2017-01-28 13:53:13 +0000

    1485611593 astigmometry

---
+++ 1485611593.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1672164598
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4][2][3] = {{{(-7L),0x1449C9FBL,0x1449C9FBL},{0L,0x1449C9FBL,1L}},{{(-1L),(-7L),0x21591249L},{0L,0L,0x21591249L}},{{(-7L),(-1L),1L},{0x1449C9FBL,0L,0x1449C9FBL}},{{0x1449C9FBL,(-7L),0L},{(-7L),0x1449C9FBL,0x1449C9FBL}}};
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
