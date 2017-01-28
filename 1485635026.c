0fce27c1d82eb659457450bde0304083ef2a840d
saisi <saisi@users.noreply.github.com>
2017-01-28 20:23:53 +0000

    1485635026 ampongue

---
+++ 1485635026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2268527249
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   int16_t  f1;
+};
+
+struct S1 {
+   unsigned f0 : 5;
+   volatile unsigned f1 : 9;
+   volatile signed f2 : 30;
+   unsigned f3 : 9;
