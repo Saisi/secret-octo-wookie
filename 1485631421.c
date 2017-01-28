0b47c5823224e2564c7ce717cf5e159463685248
saisi <saisi@users.noreply.github.com>
2017-01-28 19:23:41 +0000

    1485631421 Antisana

---
+++ 1485631421.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      422479275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_6 = 0x51F200025260417BLL;
+static uint8_t g_57 = 0xE5L;
+static int64_t g_83 = 0L;
+static int8_t g_90 = 0x24L;
+static int16_t g_109 = 6L;
+static int32_t g_118 = 0L;
+static int32_t *g_117 = &g_118;
+static int8_t g_135 = 0xAEL;
+static int32_t * volatile g_136[5] = {&g_118,&g_118,&g_118,&g_118,&g_118};
