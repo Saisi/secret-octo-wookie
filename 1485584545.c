6959e34a2efd177fba40b5a464f0906961ba449b
saisi <saisi@users.noreply.github.com>
2017-01-28 06:22:26 +0000

    1485584545 Akka

---
+++ 1485584545.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      170357269
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 9;
+   volatile unsigned f1 : 28;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 16;
+   signed f1 : 23;
