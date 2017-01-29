b303f40027e49eefc60168415f9b030b5a1d6cfd
saisi <saisi@users.noreply.github.com>
2017-01-29 12:25:22 +0000

    1485692722 bandage

---
+++ 1485692722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2354082933
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   const volatile int32_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const volatile signed f0 : 23;
+   volatile signed f1 : 19;
