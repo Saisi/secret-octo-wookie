71b43f78d28f16c72bbce71e77be7ac4aba437b5
saisi <saisi@users.noreply.github.com>
2017-01-29 16:25:45 +0000

    1485707144 ableptical

---
+++ 1485707144.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3363453242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_16 = 0UL;
+static int16_t g_20 = 0xA5A4L;
+static int32_t g_56[9] = {0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L,0xE404D464L};
+static int32_t * const g_55 = &g_56[0];
+static int32_t * const *g_54 = &g_55;
+static int8_t g_57[7] = {0xC7L,0xC7L,0xC7L,0xC7L,0xC7L,0xC7L,0xC7L};
+static uint32_t g_79 = 0x830F13E0L;
+static uint32_t g_81 = 0UL;
+static uint16_t g_85 = 65535UL;
