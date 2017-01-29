4b8c9838e0227678dbaac5c8e6d89997dfe453b1
saisi <saisi@users.noreply.github.com>
2017-01-29 06:54:50 +0000

    1485672889 academy

---
+++ 1485672889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1029432993
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 15;
+   uint64_t  f1;
+};
+
+union U1 {
+   uint32_t  f0;
+   volatile uint64_t  f1;
+   int8_t  f2;
+   int32_t  f3;
