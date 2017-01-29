fa36e8d45d51391ac79a710dc364cf86dc89509b
saisi <saisi@users.noreply.github.com>
2017-01-29 23:26:23 +0000

    1485732383 atmos

---
+++ 1485732383.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2985876338
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   volatile int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[9] = {0xA8L,0xA8L,0xA8L,0xA8L,0xA8L,0xA8L,0xA8L,0xA8L,0xA8L};
+static volatile int32_t g_3[3][2] = {{0xA4D073FFL,0xBA772304L},{0xBA772304L,0xA4D073FFL},{0xBA772304L,0xBA772304L}};
+static volatile int32_t g_4 = (-7L);/* VOLATILE GLOBAL g_4 */
