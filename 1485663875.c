531e51948ff18a4c6c243de0c3841b7b398ecb1b
saisi <saisi@users.noreply.github.com>
2017-01-29 04:24:35 +0000

    1485663875 aquicultural

---
+++ 1485663875.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      654204861
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint32_t  f1;
+   int8_t * const  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8][2] = {{(-1L),(-1L)},{7L,7L},{(-1L),(-1L)},{0L,(-1L)},{(-1L),7L},{7L,(-1L)},{(-1L),0L},{(-1L),(-1L)}};
+static int8_t g_14 = 6L;
+static int8_t *g_13 = &g_14;
