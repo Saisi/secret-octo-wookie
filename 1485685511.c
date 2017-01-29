73005628dda84491ce99c017296bccaaa563f158
saisi <saisi@users.noreply.github.com>
2017-01-29 10:25:11 +0000

    1485685511 amphisbaenic

---
+++ 1485685511.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      479773437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   volatile signed : 0;
+   unsigned f1 : 3;
+   signed f2 : 2;
+   signed f3 : 11;
+   signed f4 : 24;
+   unsigned f5 : 23;
+   unsigned f6 : 14;
+};
