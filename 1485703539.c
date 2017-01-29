d6d956d55a0e4c3f920641df2ff0a0a4ad95dfb0
saisi <saisi@users.noreply.github.com>
2017-01-29 15:25:40 +0000

    1485703539 append

---
+++ 1485703539.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1168716507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   const volatile uint32_t  f1;
+   int32_t  f2;
+   uint8_t  f3;
+   const volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xAE8C9F36L;/* VOLATILE GLOBAL g_3 */
