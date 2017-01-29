2c6a00106febb9f1a3e4c77f0e34380ff95dda79
saisi <saisi@users.noreply.github.com>
2017-01-29 20:26:06 +0000

    1485721566 aluminite

---
+++ 1485721566.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1433015938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[5][2][3] = {{{0x9B546158L,0xB770A77CL,0x9B546158L},{0x9B546158L,0L,0xB3152C58L}},{{0x9B546158L,0L,1L},{0x9B546158L,0xB770A77CL,0x9B546158L}},{{0x9B546158L,0L,0xB3152C58L},{0x9B546158L,0L,1L}},{{0x9B546158L,0xB770A77CL,0x9B546158L},{0x9B546158L,0L,0xB3152C58L}},{{0x9B546158L,0L,1L},{0x9B546158L,0xB770A77CL,0x9B546158L}}};
+static uint16_t g_30 = 0x4033L;
+static uint16_t *g_29 = &g_30;
+static volatile int32_t **g_40 = (void*)0;
+static int32_t *g_44 = (void*)0;
+static int32_t **g_43 = &g_44;
+static int16_t g_45 = 0x0988L;
+static uint16_t **g_48 = (void*)0;
+static uint16_t *** volatile g_47 = &g_48;/* VOLATILE GLOBAL g_47 */
