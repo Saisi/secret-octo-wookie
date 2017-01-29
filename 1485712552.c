a29cf6b2b389cf1c121d849be153e6393f539902
saisi <saisi@users.noreply.github.com>
2017-01-29 17:55:52 +0000

    1485712552 acidophilus

---
+++ 1485712552.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2460721651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   int8_t  f1;
+   uint64_t  f2;
+   int64_t  f3;
+   uint32_t  f4;
+   volatile signed f5 : 22;
+   int64_t  f6;
