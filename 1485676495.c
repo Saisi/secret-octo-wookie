456f29d7bc51d302ceaa195f798db4bb694f6cb4
saisi <saisi@users.noreply.github.com>
2017-01-29 07:54:56 +0000

    1485676495 axolemma

---
+++ 1485676495.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4078479143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 18;
+   volatile int32_t  f1;
+   signed f2 : 17;
+   volatile signed f3 : 17;
+   unsigned f4 : 20;
+   volatile signed f5 : 18;
+   volatile unsigned f6 : 13;
