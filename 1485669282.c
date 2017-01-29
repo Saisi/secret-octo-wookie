86ce3d8bce2ba3225f6fcff89edb05ba607d98a4
saisi <saisi@users.noreply.github.com>
2017-01-29 05:54:44 +0000

    1485669282 backheel

---
+++ 1485669282.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3208663797
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 25;
+   const unsigned f1 : 12;
+   const unsigned f2 : 31;
+   volatile signed f3 : 14;
+   unsigned f4 : 7;
+   const unsigned f5 : 4;
+   signed f6 : 19;
+   const signed f7 : 26;
+   signed f8 : 21;
