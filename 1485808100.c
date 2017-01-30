d41ac148bd83f3c5147cbc3e635ef322982ce850
saisi <saisi@users.noreply.github.com>
2017-01-30 20:28:20 +0000

    1485808100 alpenstocks

---
+++ 1485808100.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      90501687
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static int32_t g_5[8][2] = {{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL},{0x6E60B0CEL,0x6E60B0CEL}};
+static int32_t * const  volatile g_4 = &g_5[5][1];/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
