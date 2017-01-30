1c67d66bd49d104201f964c7a13f27a002ac0eaa
saisi <saisi@users.noreply.github.com>
2017-01-30 11:27:32 +0000

    1485775651 ballyragging

---
+++ 1485775651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3373622707
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   uint8_t  f1;
+   uint8_t  f2;
+   volatile uint16_t  f3;
+   int16_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
