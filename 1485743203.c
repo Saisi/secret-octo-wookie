6574f8a6cdbacec0e9c68cd1214c1e7c94aea3ce
saisi <saisi@users.noreply.github.com>
2017-01-30 02:26:45 +0000

    1485743203 antitype

---
+++ 1485743203.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1471312323
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+   int8_t  f1;
+   uint32_t  f2;
+   int8_t * f3;
+};
+
+union U1 {
+   const volatile int16_t  f0;
+   signed f1 : 30;
