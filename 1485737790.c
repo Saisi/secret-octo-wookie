df2fbe732acd2b187a0a2a1f41441c4688db9123
saisi <saisi@users.noreply.github.com>
2017-01-30 00:56:35 +0000

    1485737790 aportlast

---
+++ 1485737790.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2473783546
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 11;
+   signed f1 : 20;
+   signed f2 : 6;
+   const volatile signed f3 : 26;
+   signed f4 : 1;
+   volatile unsigned f5 : 20;
+   signed f6 : 2;
