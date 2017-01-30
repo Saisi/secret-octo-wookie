27dd3288fc57ae26a72d7e12929e0108057b3ea2
saisi <saisi@users.noreply.github.com>
2017-01-30 18:28:10 +0000

    1485800889 azelaic

---
+++ 1485800889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2372844925
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 29;
+   volatile unsigned : 0;
+   signed f1 : 3;
+   volatile signed f2 : 2;
+   signed f3 : 11;
+   unsigned f4 : 24;
+   volatile signed f5 : 23;
