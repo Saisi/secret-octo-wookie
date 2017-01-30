8a456c5470c258dc5dfc8d1a6991e1e6945d387f
saisi <saisi@users.noreply.github.com>
2017-01-30 06:27:06 +0000

    1485757625 bacquet

---
+++ 1485757625.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      565915703
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+   volatile signed f1 : 9;
+   unsigned f2 : 8;
+   const unsigned f3 : 19;
+   const uint64_t  f4;
+   signed f5 : 26;
+   signed f6 : 21;
+   unsigned f7 : 27;
+   signed f8 : 30;
