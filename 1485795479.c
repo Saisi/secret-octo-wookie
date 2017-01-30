063d1bc15e8dd19e9985cae4691f0ae834ef4e3a
saisi <saisi@users.noreply.github.com>
2017-01-30 16:58:00 +0000

    1485795479 anthologist

---
+++ 1485795479.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3438873015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+   volatile signed f1 : 9;
+   const unsigned f2 : 8;
+   signed f3 : 19;
+   unsigned f4 : 24;
+   volatile unsigned f5 : 17;
+   const volatile unsigned f6 : 12;
+};
+
