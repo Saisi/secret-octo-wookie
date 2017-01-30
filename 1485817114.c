d75a4a81d4447ddf26e17974ed6b8a136569d717
saisi <saisi@users.noreply.github.com>
2017-01-30 22:58:36 +0000

    1485817114 Bakelite

---
+++ 1485817114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      57304746
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x4E1A3622L;
+static int32_t g_6 = 0x33EFC07EL;
+static volatile int32_t g_10 = 0xE672CA3CL;/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = 1L;
+static int32_t g_75 = 0x28B293C3L;
+static int64_t g_83 = (-5L);
+static int16_t g_84 = 0x29C6L;
+static uint16_t g_142 = 0x5736L;
