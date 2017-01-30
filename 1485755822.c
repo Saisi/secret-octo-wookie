91a1bf5cc8df79b1903d7c15fa98ef2c91217c16
saisi <saisi@users.noreply.github.com>
2017-01-30 05:57:02 +0000

    1485755822 atticomastoid

---
+++ 1485755822.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2277517475
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 2L;
+static int32_t g_6 = 1L;
+static volatile uint64_t g_9 = 0x9F7A9D2E819D3388LL;/* VOLATILE GLOBAL g_9 */
+static uint64_t g_15 = 0x890635300547507ALL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
