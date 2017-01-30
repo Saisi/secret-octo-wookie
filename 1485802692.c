df1003cbb87ed57702c2fd4c8a3eb153313d466f
saisi <saisi@users.noreply.github.com>
2017-01-30 18:58:12 +0000

    1485802692 antagonism

---
+++ 1485802692.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      541507189
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 25;
+   unsigned f1 : 12;
+   signed f2 : 31;
+   signed f3 : 14;
+   const signed f4 : 7;
+   const unsigned f5 : 4;
+   unsigned f6 : 19;
