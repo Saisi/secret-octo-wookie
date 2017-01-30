ea665c76b59392d7eb1700c2e39ddb13f7f28ccf
saisi <saisi@users.noreply.github.com>
2017-01-30 17:58:07 +0000

    1485799085 astricting

---
+++ 1485799085.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3496930183
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const int64_t  f1;
+   uint64_t  f2;
+   int16_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   uint8_t  f1;
