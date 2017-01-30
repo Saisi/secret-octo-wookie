7f7ac176e77d7480a8b16353424c9877b98d3b02
saisi <saisi@users.noreply.github.com>
2017-01-30 04:26:55 +0000

    1485750414 Abasgi

---
+++ 1485750414.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3100115232
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_13[4] = {(-1L),(-1L),(-1L),(-1L)};
+static uint64_t g_23 = 1UL;
+static int32_t g_48 = 8L;
+static uint32_t g_61 = 0x6DC06892L;
+static uint64_t g_81[5][4] = {{18446744073709551606UL,18446744073709551615UL,1UL,0UL},{18446744073709551615UL,1UL,1UL,18446744073709551615UL},{0UL,0UL,1UL,0x0795B172C66C3C34LL},{18446744073709551615UL,18446744073709551606UL,1UL,18446744073709551606UL},{18446744073709551606UL,1UL,0UL,18446744073709551606UL}};
+static int16_t g_108 = (-6L);
+static uint16_t g_116 = 0xF9B9L;
+static uint16_t g_122[2] = {0xCE2CL,0xCE2CL};
+static uint16_t g_125 = 0x7DF3L;
