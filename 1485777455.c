4bc6309a3ff15bbd6ba0d878c4b1d002faf7d535
saisi <saisi@users.noreply.github.com>
2017-01-30 11:57:35 +0000

    1485777455 aldolizing

---
+++ 1485777455.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2536825336
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 6L;
+static int32_t g_53 = 0xEF8169C1L;
+static uint8_t g_66 = 0x48L;
+static int16_t g_83 = (-1L);
