0a9ecf251bd0376d7500c6ebdf5a845991dc8f1d
saisi <saisi@users.noreply.github.com>
2017-01-30 22:28:32 +0000

    1485815311 acrochordon

---
+++ 1485815311.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2232250917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int64_t  f1;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   volatile int32_t  f1;
+   const int64_t  f2;
+   int8_t * const  f3;
