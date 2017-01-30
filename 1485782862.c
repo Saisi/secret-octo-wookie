fef5e66164630acf9842b827320ff3406d8f85a9
saisi <saisi@users.noreply.github.com>
2017-01-30 13:27:42 +0000

    1485782862 apozem

---
+++ 1485782862.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      459458736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_10[2][1][1] = {{{0x95L}},{{0x95L}}};
+static int64_t g_11 = 1L;
+static int64_t g_19[3] = {0L,0L,0L};
+static int64_t g_21 = 0x934B0B4EF66B17B7LL;
+static const int64_t g_61 = 0x21EBC5F93BE41B0CLL;
+static int64_t g_68 = 0x9CD890F6DAE1DC63LL;
+static uint16_t g_72 = 0UL;
+static int32_t g_84 = 1L;
+static int32_t g_95 = (-1L);
