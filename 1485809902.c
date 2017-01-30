6a889b14b02f2825b4d1819db7b12dd38929b3a4
saisi <saisi@users.noreply.github.com>
2017-01-30 20:58:22 +0000

    1485809902 antiliquor

---
+++ 1485809902.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      276473605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xC64A6847L;
+static volatile int32_t g_6 = (-3L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xC9B780F8L;
+static int32_t g_39 = 0xD407B094L;
+static uint64_t g_54 = 8UL;
+static uint8_t g_57 = 0x70L;
+static uint64_t g_81[10] = {0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL,0xA0476F2A9E5F641DLL};
+static int32_t g_84 = 1L;
+static volatile uint32_t g_87 = 0xFC6D38C1L;/* VOLATILE GLOBAL g_87 */
