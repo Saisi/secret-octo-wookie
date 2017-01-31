ff2c9e8359947449beecfd66ea14025d299cb384
saisi <saisi@users.noreply.github.com>
2017-01-31 19:00:37 +0000

    1485889236 Antilocapridae

---
+++ 1485889236.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2450694886
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   uint16_t  f1;
+   signed f2 : 25;
+   uint64_t  f3;
+   volatile uint8_t  f4;
+   int8_t  f5;
+   int32_t  f6;
