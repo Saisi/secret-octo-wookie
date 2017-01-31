8a51e46914c75d5d923e9a4d65841cacf350571c
saisi <saisi@users.noreply.github.com>
2017-01-31 10:29:37 +0000

    1485858577 astigmatoscopies

---
+++ 1485858577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      230485683
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+   signed f1 : 15;
+   volatile signed f2 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile int64_t g_8 = 1L;/* VOLATILE GLOBAL g_8 */
+static uint16_t g_13 = 0x1D91L;
+static uint16_t g_17 = 0UL;
