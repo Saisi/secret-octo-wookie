2ab5e0fadf78f724a0921ce40c70badbd9dabb27
saisi <saisi@users.noreply.github.com>
2017-01-31 07:59:24 +0000

    1485849563 balderdashes

---
+++ 1485849563.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2323346178
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xEE76F502L;
+static volatile int32_t g_9 = 0x697E780CL;/* VOLATILE GLOBAL g_9 */
+static volatile int8_t g_10 = 3L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t g_11[3][10] = {{(-1L),(-6L),(-6L),(-1L),0L,(-6L),0L,(-1L),(-6L),(-6L)},{0L,(-6L),0x9642E40CL,0x4D64EA33L,0x4D64EA33L,0x9642E40CL,(-6L),0L,(-6L),0x9642E40CL},{(-6L),(-1L),0x4D64EA33L,(-1L),(-6L),0x9642E40CL,0x9642E40CL,(-6L),(-1L),0x4D64EA33L}};
+static volatile uint8_t g_12 = 0x2CL;/* VOLATILE GLOBAL g_12 */
+static int32_t g_17 = 0xE11914CCL;
+static uint8_t g_65[3] = {0xF3L,0xF3L,0xF3L};
+static int32_t g_79 = 3L;
+static int32_t *g_78 = &g_79;
