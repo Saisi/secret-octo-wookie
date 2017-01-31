0ecb2685233f328a48f0d9f0e847805124036a6f
saisi <saisi@users.noreply.github.com>
2017-01-31 11:29:43 +0000

    1485862182 arcatures

---
+++ 1485862182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3326224907
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 4;
+   volatile unsigned f1 : 11;
+   volatile signed f2 : 2;
+   signed f3 : 5;
+   signed f4 : 2;
+   const unsigned f5 : 19;
+   volatile signed f6 : 6;
+   volatile unsigned f7 : 1;
+   volatile signed : 0;
