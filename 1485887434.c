9d1fd8aeda0282924b25ad65a016d1125f91d9b2
saisi <saisi@users.noreply.github.com>
2017-01-31 18:30:35 +0000

    1485887434 backbiter

---
+++ 1485887434.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2523842907
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 0x3A4A9BDBA76AABECLL;
+static int32_t g_4 = (-1L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
