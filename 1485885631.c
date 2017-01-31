dc8704c81aa37ff8a0fe360828daccc70dcc0179
saisi <saisi@users.noreply.github.com>
2017-01-31 18:00:32 +0000

    1485885631 aerobe

---
+++ 1485885631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      155589668
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   volatile int32_t  f1;
+   volatile uint32_t  f2;
+   const uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x52061F66L;
+static union U0 g_22 = {0x9694D2B5L};/* VOLATILE GLOBAL g_22 */
