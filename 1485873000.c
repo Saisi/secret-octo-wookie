1e822b20c06e6036ddd1d706e4e5a0b394750ba7
saisi <saisi@users.noreply.github.com>
2017-01-31 14:30:01 +0000

    1485873000 apposers

---
+++ 1485873000.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3267441104
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 22;
+   volatile int8_t * f1;
+   uint16_t  f2;
+   int16_t  f3;
+   volatile int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = (-1L);
