ef748c9f9530f9847bfe7c32fc062692b3623386
saisi <saisi@users.noreply.github.com>
2017-01-31 08:59:29 +0000

    1485853169 adenodiastasis

---
+++ 1485853169.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1128387277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_17 = 0x64896E6A3735B263LL;
+static volatile int32_t g_18 = (-2L);/* VOLATILE GLOBAL g_18 */
+static const int64_t g_26 = 0xC9CA3191B4956E74LL;
+static const int64_t *g_25 = &g_26;
