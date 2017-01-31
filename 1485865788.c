b37df9202504cd859042eec1c601cf54cd5904e0
saisi <saisi@users.noreply.github.com>
2017-01-31 12:29:50 +0000

    1485865788 audivision

---
+++ 1485865788.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3726994329
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 11;
+   const unsigned f1 : 22;
+   signed f2 : 1;
+   unsigned f3 : 8;
+   volatile signed f4 : 25;
+   signed f5 : 14;
+};
