f8f602cf76c72b2e79a3576f971862535a9d297e
saisi <saisi@users.noreply.github.com>
2017-02-01 12:02:16 +0000

    1485950536 aerophyte

---
+++ 1485950536.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      700270339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+   unsigned f1 : 1;
+   int8_t  f2;
+   volatile int16_t  f3;
+   volatile unsigned f4 : 26;
+   unsigned : 0;
+   unsigned f5 : 3;
+   unsigned f6 : 17;
+   volatile signed f7 : 28;
