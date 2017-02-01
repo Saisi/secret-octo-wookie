4286881baf7cc2249836954000ee065dea1a922e
saisi <saisi@users.noreply.github.com>
2017-02-01 03:31:25 +0000

    1485919885 antispiritual

---
+++ 1485919885.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2567595149
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t  f0;
+   int64_t  f1;
+   volatile int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_5 = 0xE77DD688L;
+static int8_t g_24[6] = {0xA3L,0xA3L,0xA3L,0xA3L,0xA3L,0xA3L};
