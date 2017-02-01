6d0879fa5ef40eba73a2c448b8d4fdb8a0d41f6f
saisi <saisi@users.noreply.github.com>
2017-02-01 04:01:29 +0000

    1485921687 alcaydes

---
+++ 1485921687.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3537218970
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   const int8_t * const  f1;
+   uint64_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 1L;
+static uint16_t g_15 = 0xD065L;
