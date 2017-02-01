4da9c34984fe3eccea1e251d16008c041806cab2
saisi <saisi@users.noreply.github.com>
2017-02-01 10:32:06 +0000

    1485945126 aperitifs

---
+++ 1485945126.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      29757789
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x70BB743DL;
+static volatile int32_t g_8 = (-10L);/* VOLATILE GLOBAL g_8 */
+static int16_t g_9 = 0xBE39L;
+static int64_t g_10 = 0x1F2E96673E7ED6F1LL;
+static volatile uint32_t g_12 = 0xE0CD9796L;/* VOLATILE GLOBAL g_12 */
+static volatile uint32_t g_17 = 0UL;/* VOLATILE GLOBAL g_17 */
+
+
+/* --- FORWARD DECLARATIONS --- */
