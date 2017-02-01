bd13e4c6a111b958e3740713fcc28823a42ca414
saisi <saisi@users.noreply.github.com>
2017-02-01 07:31:48 +0000

    1485934307 arcuale

---
+++ 1485934307.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3617373709
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   uint16_t  f1;
+   const volatile signed f2 : 8;
+   signed f3 : 12;
+   unsigned f4 : 3;
+   signed f5 : 5;
+   volatile signed f6 : 20;
+   signed f7 : 30;
+};
