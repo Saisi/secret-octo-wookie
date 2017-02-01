b471cca83037bc4e2841098ba0002e5e07b947e2
saisi <saisi@users.noreply.github.com>
2017-02-01 05:01:33 +0000

    1485925293 audiometer

---
+++ 1485925293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2939488258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t  f1;
+   signed f2 : 10;
+   uint16_t  f3;
+};
+
+union U1 {
+   int64_t  f0;
+   uint64_t  f1;
