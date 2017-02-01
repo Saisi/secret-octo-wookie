6b6185ad64c6af6a1905823e43a71b7b2aeb73f4
saisi <saisi@users.noreply.github.com>
2017-02-01 12:32:19 +0000

    1485952338 antidynastic

---
+++ 1485952338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1712224802
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static uint64_t g_6 = 18446744073709551615UL;
+static int32_t g_13 = 0xFF3DE036L;
+static int64_t g_23 = 0x5E314A5EA964A90CLL;
+static int32_t g_26[9][1][4] = {{{0x9C279A20L,(-4L),0xA33A4D1BL,0xA33A4D1BL}},{{0x1A796D40L,0x1A796D40L,0x9C279A20L,0xA33A4D1BL}},{{0xD6364D64L,(-4L),0xD6364D64L,0x9C279A20L}},{{0xD6364D64L,0x9C279A20L,0x9C279A20L,0xD6364D64L}},{{0x1A796D40L,0x9C279A20L,0xA33A4D1BL,0x9C279A20L}},{{0x9C279A20L,(-4L),0xA33A4D1BL,0xA33A4D1BL}},{{0x1A796D40L,0x1A796D40L,0x9C279A20L,0xA33A4D1BL}},{{0xD6364D64L,(-4L),0xD6364D64L,0x9C279A20L}},{{0xD6364D64L,0x9C279A20L,0x9C279A20L,0xD6364D64L}}};
+static int64_t g_90 = 3L;
+static int64_t * volatile *g_106 = (void*)0;
+static int64_t *g_113[8] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t g_123 = (-8L);
