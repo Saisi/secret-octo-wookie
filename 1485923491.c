c811247b81839e270ac6216183304f502a81c6d3
saisi <saisi@users.noreply.github.com>
2017-02-01 04:31:31 +0000

    1485923491 Arivaca

---
+++ 1485923491.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2201895072
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_15 = 0x0C3414B5L;
+static uint64_t g_22 = 0UL;
+static uint64_t *g_28 = &g_22;
+static uint64_t *g_31 = &g_22;
+static int32_t g_72 = (-8L);
+static int8_t g_90[10] = {0x13L,0xECL,0x24L,0x24L,0xECL,0x13L,0xECL,0x24L,0x24L,0xECL};
+static int8_t g_112 = 1L;
+static uint8_t g_116 = 0x49L;
