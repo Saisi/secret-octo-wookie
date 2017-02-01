1b4f6098540e9af70753c45c95b12156736f468a
saisi <saisi@users.noreply.github.com>
2017-02-01 13:02:22 +0000

    1485954141 atmo

---
+++ 1485954141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3464065233
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 7;
+   signed f1 : 2;
+   volatile signed f2 : 29;
+   volatile unsigned f3 : 20;
+   uint64_t  f4;
+   signed f5 : 7;
+   signed f6 : 14;
+   const unsigned f7 : 16;
+   volatile uint16_t  f8;
