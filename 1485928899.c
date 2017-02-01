f285956465a81666dd7a0a980f0feb449ea2d197
saisi <saisi@users.noreply.github.com>
2017-02-01 06:01:40 +0000

    1485928899 aloneness

---
+++ 1485928899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2880863387
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 12;
+   signed f1 : 19;
+   volatile signed f2 : 10;
+   volatile unsigned f3 : 13;
+   unsigned f4 : 10;
+   unsigned f5 : 27;
+   unsigned f6 : 14;
+   unsigned f7 : 9;
+   unsigned f8 : 8;
