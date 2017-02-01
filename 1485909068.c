57b562d98a602307655a71cf20d08bfa8f3377de
saisi <saisi@users.noreply.github.com>
2017-02-01 00:31:08 +0000

    1485909068 actinology

---
+++ 1485909068.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      559574431
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xDF75DCECL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xD490C4D8L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[7] = {2L,(-6L),2L,2L,(-6L),2L,2L};
+static int32_t g_5 = 0xF402E31DL;
+static int32_t g_8[9] = {0x90F02349L,0x5F0ED1ADL,0x90F02349L,0x5F0ED1ADL,0x90F02349L,0x5F0ED1ADL,0x90F02349L,0x5F0ED1ADL,0x90F02349L};
+static uint16_t g_35[4][3] = {{0xF7CDL,0xF7CDL,0xF7CDL},{4UL,4UL,4UL},{0xF7CDL,0xF7CDL,0xF7CDL},{4UL,4UL,4UL}};
+static int32_t g_83 = 0x42FB88B7L;
+static int32_t *g_96 = &g_83;
+static int32_t **g_95[2] = {&g_96,&g_96};
