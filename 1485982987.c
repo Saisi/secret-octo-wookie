69cecebffd4c5b115e546b1b9100a7e6168cafff
saisi <saisi@users.noreply.github.com>
2017-02-01 21:03:07 +0000

    1485982987 acrobats

---
+++ 1485982987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2643332175
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0xD5L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0x5C184C50L;
+static uint32_t g_14 = 0x25EF2C36L;
+static int32_t g_16 = 0x6EC6E54EL;
+static uint32_t g_26 = 0x17CA4E86L;
+static uint8_t g_27[4] = {0x9AL,0x9AL,0x9AL,0x9AL};
+static volatile int64_t g_28[10] = {0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL,0x0E7790660D64C9D8LL};
+static int32_t g_31 = (-9L);
+static int32_t g_63 = (-1L);
