ce64fd38d58a80c60564aadeb7c61432d2bf0b37
saisi <saisi@users.noreply.github.com>
2017-02-01 20:33:05 +0000

    1485981182 azophenylene

---
+++ 1485981182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1192761174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 25;
+   volatile signed f1 : 10;
+   unsigned f2 : 4;
+   unsigned : 0;
+   const signed f3 : 15;
+   const signed f4 : 10;
+   signed : 0;
