15827c7647cf1dc83595e5d14880a8682761218a
saisi <saisi@users.noreply.github.com>
2017-02-01 22:03:14 +0000

    1485986593 aphotic

---
+++ 1485986593.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3292818148
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 18446744073709551615UL;/* VOLATILE GLOBAL g_2 */
+static uint32_t g_5 = 0xAC04EE7DL;
+static int32_t g_7 = 0xDDBC3707L;
+static int8_t g_43[6] = {3L,3L,3L,3L,3L,3L};
+static volatile int32_t g_49[6] = {1L,0xBD55ADB5L,1L,1L,0xBD55ADB5L,1L};
+static int32_t g_50 = (-3L);
+static int32_t g_53 = 0xC83B9135L;
+static uint8_t g_153[3][1][6] = {{{0UL,0xC2L,250UL,9UL,0xC2L,9UL}},{{0UL,0xD7L,0UL,9UL,0xD7L,250UL}},{{0UL,0x47L,9UL,9UL,0x47L,0UL}}};
+static volatile uint32_t g_168[8] = {18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL,18446744073709551612UL};
