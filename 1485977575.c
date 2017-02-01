3acd14ab7b31a3a2d8cef15f7d77f0fb877d7a74
saisi <saisi@users.noreply.github.com>
2017-02-01 19:32:56 +0000

    1485977575 Antilope

---
+++ 1485977575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3333673011
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 24;
+   const unsigned f1 : 15;
+   const signed f2 : 22;
+   volatile uint64_t  f3;
+   signed f4 : 18;
+   volatile signed f5 : 24;
+   unsigned f6 : 27;
