c989264b41b6965cae20df553636278003b163ee
saisi <saisi@users.noreply.github.com>
2017-02-01 08:31:54 +0000

    1485937913 Balkanization

---
+++ 1485937913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3233542994
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   unsigned f1 : 16;
+   signed f2 : 18;
+   unsigned f3 : 22;
+   const signed f4 : 13;
+   volatile unsigned f5 : 16;
+   unsigned f6 : 14;
+   signed f7 : 11;
+};
