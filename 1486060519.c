82430c958e6fa99e2aa80b1a0df25321e969f53c
saisi <saisi@users.noreply.github.com>
2017-02-02 18:35:20 +0000

    1486060519 antihemoglobin

---
+++ 1486060519.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      701493379
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   uint32_t  f1;
+   signed f2 : 7;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
