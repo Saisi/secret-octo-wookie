8e20379c528830b2fe3a97e200638fcc8b55ac0d
saisi <saisi@users.noreply.github.com>
2017-02-02 15:34:58 +0000

    1486049698 ardencies

---
+++ 1486049698.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      964659885
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[4][3] = {{0L,0L,0L},{0xE948708EL,0xDD688769L,0xE948708EL},{0L,0L,0L},{0xE948708EL,0xDD688769L,0xE948708EL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
