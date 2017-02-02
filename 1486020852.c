8dc6dfd8c944d3accf8c8ef0a343e9eaef01d4ed
saisi <saisi@users.noreply.github.com>
2017-02-02 07:34:12 +0000

    1486020852 amidoacetic

---
+++ 1486020852.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      811650603
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const volatile int32_t  f1;
+   const int32_t  f2;
+   unsigned f3 : 18;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2 = 0x93L;
+static int32_t g_4 = 0x5010F7A7L;
