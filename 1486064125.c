d63599c89d0db2971d5998496deb249c1f5348b4
saisi <saisi@users.noreply.github.com>
2017-02-02 19:35:26 +0000

    1486064125 agapanthus

---
+++ 1486064125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4267446837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_2 = 0x94F3L;/* VOLATILE GLOBAL g_2 */
+static uint32_t g_8[3] = {7UL,7UL,7UL};
+static uint32_t g_20 = 0x3A00D0E7L;
+static uint8_t g_23 = 9UL;
+static uint8_t *g_38 = (void*)0;
+static int32_t g_43[9] = {8L,(-8L),8L,(-8L),8L,(-8L),8L,(-8L),8L};
+static uint64_t g_63 = 0x0FD7A8D5DA263B4ALL;
+static uint8_t g_76 = 1UL;
+static int64_t g_97 = 0x1E1CB09366CA3FDCLL;
