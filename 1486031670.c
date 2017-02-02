c606480ec84da39a6583f3d86940b278d7f7be9e
saisi <saisi@users.noreply.github.com>
2017-02-02 10:34:31 +0000

    1486031670 athena

---
+++ 1486031670.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2946173163
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x534DE6F8L;
+static int32_t g_5 = (-8L);
+static volatile int32_t g_8 = 0x82467468L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9 = 0xEA9177C8L;/* VOLATILE GLOBAL g_9 */
+static uint16_t g_10[5] = {0x433CL,0x433CL,0x433CL,0x433CL,0x433CL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
