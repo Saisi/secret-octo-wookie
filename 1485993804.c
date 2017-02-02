9ecad6ed50958f4b052eceb75c6c31d86c932c6e
saisi <saisi@users.noreply.github.com>
2017-02-02 00:03:25 +0000

    1485993804 alkenna

---
+++ 1485993804.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1290794883
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   signed f1 : 23;
+   signed f2 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_5 = 0xCCL;
+static volatile union U0 g_34 = {0x93F9BEAB1D6A56C3LL};/* VOLATILE GLOBAL g_34 */
+static union U0 g_39 = {0x19D3388A0193208ALL};/* VOLATILE GLOBAL g_39 */
