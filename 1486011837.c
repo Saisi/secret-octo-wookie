276b2062f06fdb0dda1215ce6e92ec43b95c0fef
saisi <saisi@users.noreply.github.com>
2017-02-02 05:03:57 +0000

    1486011837 asteria

---
+++ 1486011837.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3984881671
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x02071B18L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[3] = {1L,1L,1L};
+static int32_t g_5 = 0L;
+static uint64_t g_58 = 1UL;
+static int64_t g_62 = 6L;
+static volatile int64_t g_64 = 0L;/* VOLATILE GLOBAL g_64 */
+static volatile int64_t *g_63 = &g_64;
+static uint8_t g_65 = 0xFDL;
