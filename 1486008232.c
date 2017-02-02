90b16969d3460cafbece0b84c07f931c3334fdc9
saisi <saisi@users.noreply.github.com>
2017-02-02 04:03:52 +0000

    1486008232 anorectic

---
+++ 1486008232.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      779064293
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   const volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_17[9] = {0x4163L,0x4163L,0x4163L,0x4163L,0x4163L,0x4163L,0x4163L,0x4163L,0x4163L};
+static uint8_t g_19 = 0xA1L;
+static uint32_t g_23 = 1UL;
+static int32_t g_26 = 0L;
