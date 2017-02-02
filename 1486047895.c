048a4579ec82560edd96f7242dbf57ce7039e47d
saisi <saisi@users.noreply.github.com>
2017-02-02 15:04:55 +0000

    1486047895 ambidexterity

---
+++ 1486047895.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2651169987
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int8_t  f0;
+   uint8_t  f1;
+   const volatile int16_t  f2;
+   int64_t  f3;
+   int32_t  f4;
+   int8_t  f5;
+   int8_t  f6;
+   uint8_t  f7;
+   uint16_t  f8;
