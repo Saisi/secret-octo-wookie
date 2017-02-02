c0425ae31cf0461dc8a6c6dc289cbab4d3d8e183
saisi <saisi@users.noreply.github.com>
2017-02-02 12:34:41 +0000

    1486038880 amethyst

---
+++ 1486038880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      886857708
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint64_t  f1;
+   volatile uint32_t  f2;
+};
+
+union U1 {
+   uint32_t  f0;
+   volatile uint8_t  f1;
+   const volatile struct S0  f2;
