728fc6d2e05a9d97c9698a0d3b486e6f43c125b5
saisi <saisi@users.noreply.github.com>
2017-02-02 09:34:23 +0000

    1486028063 arf

---
+++ 1486028063.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3179196312
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int8_t  f1;
+   signed f2 : 13;
+   const int16_t  f3;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   unsigned f1 : 2;
