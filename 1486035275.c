75fb51f597e1665a0be344ae3b763f262e8d477e
saisi <saisi@users.noreply.github.com>
2017-02-02 11:34:35 +0000

    1486035275 alants

---
+++ 1486035275.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4040637302
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xDD3F4672L;
+static volatile int8_t g_6 = (-5L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xE12CDFD7L;
+static uint32_t g_8[8] = {4294967293UL,4294967293UL,4294967293UL,4294967293UL,4294967293UL,4294967293UL,4294967293UL,4294967293UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
