769f60c3c07adbc4ee8bbfb6894f4fe9275d4067
saisi <saisi@users.noreply.github.com>
2017-02-03 05:36:24 +0000

    1486100184 acetates

---
+++ 1486100184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1737217426
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 23;
+   unsigned f1 : 16;
+   unsigned f2 : 18;
+   signed f3 : 22;
+   volatile uint8_t  f4;
+   const volatile unsigned f5 : 7;
+   signed f6 : 12;
