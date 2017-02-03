a8ada448a5f360b83b6439cbdfcd7858bfe5d2b0
saisi <saisi@users.noreply.github.com>
2017-02-03 14:07:16 +0000

    1486130835 Balkhash

---
+++ 1486130835.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1038913557
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4] = {(-3L),(-3L),(-3L),(-3L)};
+static int32_t *g_5 = &g_3[3];
+static int32_t ** volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_6 = 0xB11ACCBAL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 3L;
+static int32_t g_10 = (-1L);
+static int32_t g_13[2][8] = {{1L,1L,1L,1L,1L,1L,1L,1L},{0x131D8B9DL,1L,0x131D8B9DL,0x131D8B9DL,1L,0x131D8B9DL,0x131D8B9DL,1L}};
+static int64_t g_69 = (-1L);
+static int16_t g_72[1] = {9L};
