986467cf761a631a6305f621a557cc0286413707
saisi <saisi@users.noreply.github.com>
2017-02-03 13:37:13 +0000

    1486129032 Angostura

---
+++ 1486129032.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2243406696
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1E1E87DFL;
+static int32_t g_9[5][10] = {{0x7E5C285CL,1L,0x8F9C25B0L,0x7E5C285CL,(-1L),(-1L),0x440874B2L,(-1L),0x8F9C25B0L,0x8F9C25B0L},{(-1L),1L,(-9L),(-1L),(-1L),(-9L),1L,(-1L),2L,6L},{0x32A831D2L,0x7E5C285CL,(-1L),(-1L),(-1L),1L,6L,(-1L),0x7E5C285CL,(-1L)},{0x440874B2L,(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),0x440874B2L,(-1L)},{(-1L),0x32A831D2L,(-9L),6L,0x32A831D2L,(-1L),0x440874B2L,0x8F9C25B0L,6L,(-1L)}};
+static uint16_t g_10 = 0UL;
+static int32_t *g_17 = (void*)0;
+static int32_t **g_16 = &g_17;
