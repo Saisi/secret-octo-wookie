af5bedf2377d355605d2ade6d2b881642ff86e9a
saisi <saisi@users.noreply.github.com>
2017-02-03 20:07:51 +0000

    1486152471 avenge

---
+++ 1486152471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3913358926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   volatile int64_t  f0;
+   uint16_t  f1;
+   int8_t * f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1] = {0L};
+static const union U2 g_12 = {0xB611FB8EB5EA28C7LL};/* VOLATILE GLOBAL g_12 */
