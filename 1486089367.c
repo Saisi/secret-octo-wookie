6842a28cb4d9cdee216660d5e0f08744e70e221d
saisi <saisi@users.noreply.github.com>
2017-02-03 02:36:07 +0000

    1486089367 archruler

---
+++ 1486089367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4056736846
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x6EE97341L;
+static volatile int64_t g_6 = 0xA2EF7E291920B245LL;/* VOLATILE GLOBAL g_6 */
+static volatile uint32_t g_7 = 2UL;/* VOLATILE GLOBAL g_7 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
