d1377a4f2125713dc1378a64d4d93571e8fc90cb
saisi <saisi@users.noreply.github.com>
2017-02-03 10:36:52 +0000

    1486118211 anthine

---
+++ 1486118211.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      863594599
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const uint16_t  f0;
+   uint32_t  f1;
+};
