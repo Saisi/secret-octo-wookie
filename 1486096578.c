cc24497b45344afadd64fcef3c5c5104d14e0161
saisi <saisi@users.noreply.github.com>
2017-02-03 04:36:19 +0000

    1486096578 analogic

---
+++ 1486096578.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3383606109
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const volatile int32_t  f1;
+   volatile int32_t  f2;
+   const signed f3 : 11;
+};
+
+struct S1 {
+   volatile unsigned f0 : 7;
+   int32_t  f1;
