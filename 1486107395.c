2ec9248c1679faf0b4f1d24cb5574275ea31e032
saisi <saisi@users.noreply.github.com>
2017-02-03 07:36:35 +0000

    1486107395 afterstretch

---
+++ 1486107395.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3431425837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   volatile uint64_t  f1;
+   signed f2 : 24;
+   volatile signed f3 : 28;
+   unsigned f4 : 19;
+   unsigned f5 : 21;
+   unsigned f6 : 4;
+   unsigned f7 : 14;
+};
