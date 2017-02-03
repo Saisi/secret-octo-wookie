939eb089ce7a7c839bf1f45d479ba7bfec77eb1a
saisi <saisi@users.noreply.github.com>
2017-02-03 09:06:43 +0000

    1486112803 Archegetes

---
+++ 1486112803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      248275685
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-5L);
+static uint16_t g_5 = 0x26F3L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
