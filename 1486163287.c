49130ce6c113b28a7eeccf70efeedba4b1beb853
saisi <saisi@users.noreply.github.com>
2017-02-03 23:08:07 +0000

    1486163287 backwasher

---
+++ 1486163287.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4021659203
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-7L);
+static volatile uint32_t g_4 = 0x388A0193L;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
