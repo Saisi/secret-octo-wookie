95952d212074484a088de74672207c03c2028730
saisi <saisi@users.noreply.github.com>
2017-02-03 11:06:54 +0000

    1486120014 arabine

---
+++ 1486120014.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2564108618
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t *g_3[2] = {(void*)0,(void*)0};
+static int8_t * volatile * volatile g_2 = &g_3[1];/* VOLATILE GLOBAL g_2 */
+static int32_t g_5 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
