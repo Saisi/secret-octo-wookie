137a46841a9f7316c226e2245486810ef3433d35
saisi <saisi@users.noreply.github.com>
2017-02-04 04:08:45 +0000

    1486181325 arables

---
+++ 1486181325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1772882276
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 13;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_15 = (-1L);
+static uint32_t g_17 = 0UL;
+static uint32_t g_28 = 0x25D36729L;
+static uint32_t *g_27 = &g_28;
+static int32_t g_30 = 1L;
