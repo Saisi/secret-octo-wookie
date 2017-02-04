d04bbb28adb24326293ea35b3c00d241a4c2f2a4
saisi <saisi@users.noreply.github.com>
2017-02-04 02:08:26 +0000

    1486174105 Atherinidae

---
+++ 1486174105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4121329862
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x7E2799ACL;
+static int32_t g_6 = 0L;
+static volatile int32_t g_7 = 0xB742D63DL;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = (-3L);
+static int32_t g_9 = 0x485BDF60L;
+static uint16_t g_78[2][4] = {{0x3119L,0x3119L,0x3119L,0x3119L},{0x3119L,0x3119L,0x3119L,0x3119L}};
+static uint16_t *g_77 = &g_78[1][0];
+static int16_t g_98 = 0L;
+static int32_t g_107 = (-2L);
