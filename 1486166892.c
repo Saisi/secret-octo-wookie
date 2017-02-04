b13f3a7dfa708fa66d40ff6bbb1f82cebee8cfa3
saisi <saisi@users.noreply.github.com>
2017-02-04 00:08:12 +0000

    1486166892 Baptornis

---
+++ 1486166892.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1355804736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint64_t  f0;
+   int8_t  f1;
+   const int64_t  f2;
+   uint16_t  f3;
+   int64_t  f4;
+   uint64_t  f5;
+   volatile int32_t  f6;
