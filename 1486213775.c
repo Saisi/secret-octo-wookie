a84b6f8b8c9c95acfb82d853a4e98d9286aabf82
saisi <saisi@users.noreply.github.com>
2017-02-04 13:09:40 +0000

    1486213775 alderfly

---
+++ 1486213775.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2583130039
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   uint64_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 9L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-6L);
