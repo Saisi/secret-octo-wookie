9d0dbfcbf09c77678e8eb7b59f7676648efaca64
saisi <saisi@users.noreply.github.com>
2017-02-04 11:39:27 +0000

    1486208367 affronter

---
+++ 1486208367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1588128896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const volatile uint32_t  f1;
+   int8_t  f2;
+   volatile uint16_t  f3;
+   const uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-6L);/* VOLATILE GLOBAL g_2 */
