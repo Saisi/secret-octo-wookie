6806ae3dfd241ca47087b2353d5cf3c1f3d83d82
saisi <saisi@users.noreply.github.com>
2017-02-04 18:40:10 +0000

    1486233610 Abrahamic

---
+++ 1486233610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      541605503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static uint32_t g_6 = 0x5F394A2AL;
+static volatile int32_t g_14[8] = {0xE02189F4L,0xDCDF5EF8L,0xE02189F4L,0xE02189F4L,0xDCDF5EF8L,0xE02189F4L,0xE02189F4L,0xDCDF5EF8L};
+static int32_t g_15 = 0L;
+static uint16_t g_45[8][1][6] = {{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0x8A77L,0xDB1AL,0x8A77L,0x8A77L}},{{0xDB1AL,0x8A77L,0UL,0x8A77L,0UL,0UL}},{{0x8A77L,0UL,0UL,0x8A77L,0UL,0UL}}};
+static const int32_t *g_56 = (void*)0;
+static const int32_t g_58 = 0x4DFB4810L;
+static const int32_t *g_57 = &g_58;
+static uint16_t g_66 = 65531UL;
