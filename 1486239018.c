3db6cd8d7eedee19ce1519a8c0d8dabcf654d218
saisi <saisi@users.noreply.github.com>
2017-02-04 20:10:18 +0000

    1486239018 Acarida

---
+++ 1486239018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4111074215
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   signed f1 : 1;
+   int16_t  f2;
+   volatile unsigned f3 : 11;
+   uint32_t  f4;
+};
+
+union U1 {
+   const int8_t * f0;
