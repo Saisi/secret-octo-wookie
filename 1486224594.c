cd5c7a76eb6524e6c9e0510ae92fb447631c8471
saisi <saisi@users.noreply.github.com>
2017-02-04 16:09:54 +0000

    1486224594 alizarate

---
+++ 1486224594.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3602275897
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_6 = 0xE6D4F2887CF2E566LL;
+static int8_t g_14 = 7L;
+static int32_t g_62 = 0x4EF21DDBL;
+static int32_t * volatile g_61 = &g_62;/* VOLATILE GLOBAL g_61 */
+static int8_t g_67 = 0xC6L;
