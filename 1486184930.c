bd110952c48c707b7d8f7db02ca35bdfe0a507d3
saisi <saisi@users.noreply.github.com>
2017-02-04 05:08:50 +0000

    1486184930 arsenicism

---
+++ 1486184930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4103245051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 18446744073709551615UL;
+static int32_t g_5 = 0x451D52AAL;
+static int32_t *g_4 = &g_5;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
