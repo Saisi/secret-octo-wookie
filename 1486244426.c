5f32a0cc5f0619e04ca1f90a947f6ae1fc8f71fc
saisi <saisi@users.noreply.github.com>
2017-02-04 21:40:26 +0000

    1486244426 ambrosine

---
+++ 1486244426.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3805611703
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static volatile uint8_t g_5 = 254UL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_63[7] = {0x031B10CDL,0L,0x031B10CDL,0x031B10CDL,0L,0x031B10CDL,0x031B10CDL};
+static uint16_t g_64 = 0UL;
+static volatile int32_t g_105 = 0x3D706802L;/* VOLATILE GLOBAL g_105 */
+static volatile int32_t *g_104 = &g_105;
+static volatile int32_t * volatile *g_103[1] = {&g_104};
+static int32_t **g_110 = (void*)0;
+static uint32_t g_146 = 0UL;
