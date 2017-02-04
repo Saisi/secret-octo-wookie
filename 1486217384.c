4af3deee48f683deb35a11c2a78844bde19bc219
saisi <saisi@users.noreply.github.com>
2017-02-04 14:09:45 +0000

    1486217384 Aegialitis

---
+++ 1486217384.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1107904512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1][6] = {{0xA439BEB0L,0xA439BEB0L,0xA439BEB0L,0xA439BEB0L,0xA439BEB0L,0xA439BEB0L}};
+static int32_t g_19 = (-1L);
+static uint16_t g_48 = 0xF205L;
+static int16_t g_53 = (-7L);
+static uint8_t g_59 = 4UL;
+static int32_t g_62 = (-5L);
+static int32_t *g_73 = &g_3[0][0];
+static int32_t ** volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static const uint16_t g_94[3] = {0xCF56L,0xCF56L,0xCF56L};
