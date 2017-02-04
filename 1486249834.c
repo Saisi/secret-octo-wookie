59fe077c41d5ed75261745adcd4869b35edb7e0e
saisi <saisi@users.noreply.github.com>
2017-02-04 23:10:34 +0000

    1486249834 avigate

---
+++ 1486249834.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3629968740
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_13 = 7UL;
+static uint64_t g_21[2][7] = {{0UL,18446744073709551614UL,18446744073709551614UL,0UL,18446744073709551615UL,0x80DB959C45C1D102LL,18446744073709551615UL},{0UL,18446744073709551614UL,18446744073709551614UL,0UL,18446744073709551615UL,0x80DB959C45C1D102LL,18446744073709551615UL}};
+static int32_t g_34[2][3] = {{0xEF9A8551L,0xEF9A8551L,0xEF9A8551L},{0xEF9A8551L,0xEF9A8551L,0xEF9A8551L}};
+static const int32_t g_81[4] = {0x4D257E7EL,0x4D257E7EL,0x4D257E7EL,0x4D257E7EL};
+static int8_t g_85 = 1L;
