33bbf5afef4e3d833d10c8f8b8f635a5ff8aef9f
saisi <saisi@users.noreply.github.com>
2017-02-04 08:09:07 +0000

    1486195746 Altaloma

---
+++ 1486195746.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3972829586
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x825E7A66L;
+static int16_t g_7[3][4] = {{0L,0L,0L,0L},{0L,0L,0L,0L},{0L,0L,0L,0L}};
+static int32_t *g_43[5] = {&g_3,&g_3,&g_3,&g_3,&g_3};
+static int32_t **g_42 = &g_43[3];
+static int32_t g_56 = 3L;
+static volatile int8_t g_65[9][8][3] = {{{(-5L),0x95L,0L},{(-9L),(-8L),0xDDL},{(-6L),(-8L),(-6L)},{0L,(-9L),0xDDL},{0x9BL,1L,0L},{0x45L,0x44L,0x44L},{0xB5L,(-8L),1L},{0x45L,(-2L),0x81L}},{{0x9BL,0x95L,0xBDL},{0L,0L,0x81L},{1L,0L,(-6L)},{0x45L,0L,(-8L)},{0xBDL,1L,0xBDL},{(-9L),0x81L,(-8L)},{0xB5L,0x95L,(-6L)},{0xCBL,0x45L,0x81L}},{{(-5L),1L,0x72L},{0xCBL,0L,0xDDL},{0xB5L,0L,1L},{(-9L),(-9L),0x81L},{0xBDL,0L,(-5L)},{0x45L,0L,(-8L)},{1L,1L,1L},{(-9L),0x45L,(-8L)}},{{0L,0x95L,(-5L)},{0xCBL,0x81L,0x81L},{(-6L),1L,1L},{0xCBL,0L,0xDDL},{0L,0L,0x72L},{(-9L),0x44L,0x81L},{1L,0L,(-6L)},{0x45L,0L,(-8L)}},{{0xBDL,1L,0xBDL},{(-9L),0x81L,(-8L)},{0xB5L,0x95L,(-6L)},{0xCBL,0x45L,0x81L},{(-5L),1L,0x72L},{0xCBL,0L,0xDDL},{0xB5L,0L,1L},{(-9L),(-9L),0x81L}},{{0xBDL,0L,(-5L)},{0x45L,0L,(-8L)},{1L,1L,1L},{(-9L),0x45L,(-8L)},{0L,0x95L,(-5L)},{0xCBL,0x81L,0x81L},{(-6L),1L,1L},{0xCBL,0L,0xDDL}},{{0L,0L,0x72L},{(-9L),0x44L,0x81L},{1L,0L,(-6L)},{0x45L,0L,(-8L)},{0xBDL,1L,0xBDL},{(-9L),0x81L,(-8L)},{0xB5L,0x95L,(-6L)},{0xCBL,0x45L,0x81L}},{{(-5L),1L,0x72L},{0xCBL,0L,0xDDL},{0xB5L,0L,1L},{(-9L),(-9L),0x81L},{0xBDL,0L,(-5L)},{0x45L,0L,(-8L)},{1L,1L,1L},{(-9L),0x45L,(-8L)}},{{0L,0x95L,(-5L)},{0xCBL,0x81L,0x81L},{(-6L),1L,1L},{0xCBL,0L,0xDDL},{0L,0L,0x72L},{(-9L),0x44L,0x81L},{1L,0L,(-6L)},{0x45L,0L,(-8L)}}};
+static int32_t **g_77[7] = {&g_43[1],(void*)0,(void*)0,&g_43[1],&g_43[3],&g_43[3],(void*)0};
+static int32_t g_98 = 0x46132A9EL;
+static uint32_t g_102 = 0x73B1A022L;
