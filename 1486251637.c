dfaed38bc69bd4c38f51aa2dc4a0d4a995ac9d34
saisi <saisi@users.noreply.github.com>
2017-02-04 23:40:37 +0000

    1486251637 alkalinization

---
+++ 1486251637.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      974872290
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-9L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[2] = {(-4L),(-4L)};
+static int32_t g_5 = 0xAD3B3152L;
+static int32_t g_8 = 4L;
