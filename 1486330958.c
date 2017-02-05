30e3c9ddd2df7b6e530910b36126cb89df0d9f81
saisi <saisi@users.noreply.github.com>
2017-02-05 21:42:39 +0000

    1486330958 aerocyst

---
+++ 1486330958.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2284775482
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 11;
+   const volatile int16_t  f1;
+   volatile int16_t  f2;
+};
+
+union U1 {
+   uint32_t  f0;
+   int8_t * volatile  f1;
+   const uint16_t  f2;
