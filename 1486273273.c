b0e1443f2c67edef82bd471862fc6e1edb9468a1
saisi <saisi@users.noreply.github.com>
2017-02-05 05:41:13 +0000

    1486273273 altigraph

---
+++ 1486273273.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1960473651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_11 = 1UL;
+static const volatile int32_t g_47 = 9L;/* VOLATILE GLOBAL g_47 */
+static const volatile int32_t * volatile g_46[8][8] = {{&g_47,&g_47,&g_47,&g_47,&g_47,&g_47,(void*)0,(void*)0},{&g_47,(void*)0,&g_47,&g_47,&g_47,&g_47,&g_47,&g_47},{&g_47,&g_47,(void*)0,&g_47,(void*)0,&g_47,&g_47,&g_47},{(void*)0,&g_47,&g_47,&g_47,&g_47,&g_47,&g_47,&g_47},{&g_47,&g_47,(void*)0,&g_47,(void*)0,&g_47,&g_47,&g_47},{(void*)0,&g_47,&g_47,&g_47,&g_47,&g_47,(void*)0,&g_47},{&g_47,&g_47,(void*)0,&g_47,&g_47,(void*)0,(void*)0,&g_47},{&g_47,&g_47,&g_47,&g_47,&g_47,&g_47,&g_47,&g_47}};
+static int32_t g_49[1][7][4] = {{{0x0DFC1F7BL,0x0DFC1F7BL,0xE4BC795CL,(-6L)},{0x80A3888AL,6L,0x80A3888AL,0xE4BC795CL},{0x80A3888AL,0xE4BC795CL,0xE4BC795CL,0x80A3888AL},{0x0DFC1F7BL,0xE4BC795CL,(-6L),0xE4BC795CL},{0xE4BC795CL,6L,(-6L),(-6L)},{0x0DFC1F7BL,0x0DFC1F7BL,0xE4BC795CL,(-6L)},{0x80A3888AL,6L,0x80A3888AL,0xE4BC795CL}}};
