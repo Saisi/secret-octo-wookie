3e1eefd00eede21d14953d72f24a24faa183c884
saisi <saisi@users.noreply.github.com>
2017-02-05 19:12:26 +0000

    1486321945 Ailuropoda

---
+++ 1486321945.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4109134237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_14 = 0x5751CFD1F2E96673LL;
+static int32_t g_15[10] = {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L};
+static int64_t g_17 = 0xEB59008CF5A8B060LL;
+static int64_t g_19 = 0x927F7B6FE6D39C7CLL;
+static int16_t g_25 = (-2L);
+static int32_t g_45 = 0x7684ABF0L;
+static volatile int8_t g_50 = 1L;/* VOLATILE GLOBAL g_50 */
+static volatile int8_t g_51 = 0x90L;/* VOLATILE GLOBAL g_51 */
+static int8_t g_52 = 0x94L;
