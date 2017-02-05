00fd0da6224ad0d5bf6ba0ef7932653bb6c97159
saisi <saisi@users.noreply.github.com>
2017-02-05 17:12:15 +0000

    1486314735 antagonization

---
+++ 1486314735.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      511571679
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+   const volatile int16_t  f2;
+   signed f3 : 13;
+   int8_t  f4;
+};
+
+struct S1 {
+   int32_t  f0;
