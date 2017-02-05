3e1e478f24460b37af4c3550c8bfeb8bea7c705b
saisi <saisi@users.noreply.github.com>
2017-02-05 15:12:05 +0000

    1486307524 autopotent

---
+++ 1486307524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      35401254
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   signed f1 : 30;
+   unsigned f2 : 26;
+   volatile unsigned f3 : 20;
+   const signed f4 : 9;
+   unsigned f5 : 17;
+   unsigned f6 : 14;
+   signed f7 : 15;
+   const signed f8 : 4;
