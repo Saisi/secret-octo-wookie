a7fe35eb4287d3155e40d5d04480b72175091f34
saisi <saisi@users.noreply.github.com>
2017-02-05 08:11:27 +0000

    1486282286 acestoma

---
+++ 1486282286.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      348704073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 3;
+   volatile unsigned f1 : 14;
+   signed f2 : 25;
+   int32_t  f3;
+   volatile unsigned f4 : 17;
+   volatile unsigned f5 : 6;
+   uint16_t  f6;
