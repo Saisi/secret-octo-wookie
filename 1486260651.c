a0e19e28863e5b9b3ef220d4bd197818816506f0
saisi <saisi@users.noreply.github.com>
2017-02-05 02:10:51 +0000

    1486260651 ampule

---
+++ 1486260651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      792608643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 16;
+   volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 2L;
+static int32_t *g_17 = (void*)0;
+static union U0 g_46[10] = {{-1L},{-1L},{-1L},{-1L},{-1L},{-1L},{-1L},{-1L},{-1L},{-1L}};
+static int32_t g_53 = 1L;
