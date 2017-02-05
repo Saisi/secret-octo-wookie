9312acd69ce40e36e2da4be2db422a34a3c53952
saisi <saisi@users.noreply.github.com>
2017-02-05 03:10:58 +0000

    1486264256 averager

---
+++ 1486264256.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3722513906
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 7;
+   signed : 0;
+   signed f1 : 2;
+   signed f2 : 6;
+   const signed f3 : 29;
+   int32_t  f4;
+};
