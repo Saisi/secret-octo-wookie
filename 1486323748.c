c1d79eb8e73b74f0acbaed36f26c015efd4c59dc
saisi <saisi@users.noreply.github.com>
2017-02-05 19:42:28 +0000

    1486323748 adagios

---
+++ 1486323748.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2704386400
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0xE90A624CL;
+static int8_t g_8 = (-3L);
+static int32_t g_11 = 1L;
+static volatile int8_t g_13[8] = {0L,0L,0L,0L,0L,0L,0L,0L};
+static volatile int32_t g_14 = 0x11F3813BL;/* VOLATILE GLOBAL g_14 */
+static volatile int32_t g_15 = 0x07EA4C57L;/* VOLATILE GLOBAL g_15 */
+static volatile uint16_t g_16[9][3] = {{65535UL,0x44D9L,65535UL},{65535UL,0UL,0x44D9L},{0UL,65535UL,65535UL},{0x44D9L,65535UL,0x1DBCL},{0UL,0UL,0x4402L},{0x44D9L,0x44D9L,0x4402L},{0UL,0UL,0x1DBCL},{65535UL,0x44D9L,65535UL},{65535UL,0UL,0x44D9L}};
+static int32_t *g_20 = &g_6;
+static int32_t ** volatile g_19 = &g_20;/* VOLATILE GLOBAL g_19 */
