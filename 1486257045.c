c7613bb1bf891656f7fdf01fca1c9969fe806b3a
saisi <saisi@users.noreply.github.com>
2017-02-05 01:10:46 +0000

    1486257045 Androclea

---
+++ 1486257045.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2015332388
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_5 = 0x558BL;/* VOLATILE GLOBAL g_5 */
+static uint64_t g_11 = 2UL;
+static int32_t g_80 = 0x9FB95C1FL;
+static int64_t g_82 = 0x7EEF10DEC53312A7LL;
+static volatile int32_t g_93 = 1L;/* VOLATILE GLOBAL g_93 */
+static volatile int32_t * const g_92 = &g_93;
+static volatile int32_t * const *g_91 = &g_92;
+static int32_t g_94 = 0x78FD71F6L;
+static int64_t g_101 = 0xC6596AAD9F1AA496LL;
