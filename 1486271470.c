edd6e2b65872114eb70141c21454106547931157
saisi <saisi@users.noreply.github.com>
2017-02-05 05:11:11 +0000

    1486271470 adagial

---
+++ 1486271470.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1174268300
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   int16_t  f1;
+   uint8_t  f2;
+   volatile unsigned f3 : 7;
+   volatile int32_t  f4;
+};
+
+union U1 {
+   const volatile uint8_t  f0;
