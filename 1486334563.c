e3f9be2b38cbdaf7445a0fb68c543c4c2f3e324a
saisi <saisi@users.noreply.github.com>
2017-02-05 22:42:44 +0000

    1486334563 ADC

---
+++ 1486334563.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4283808288
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x3185299DL;
+static uint8_t g_8[3] = {7UL,7UL,7UL};
+static int32_t g_32 = 0xE80A7C6DL;
+static uint32_t g_55 = 0x65FBC01DL;
+static int8_t g_71 = 0x2EL;
+static int32_t *g_74 = &g_3;
+static int32_t ** volatile g_73[4][1][7] = {{{&g_74,&g_74,&g_74,&g_74,&g_74,&g_74,&g_74}},{{&g_74,&g_74,&g_74,&g_74,&g_74,&g_74,&g_74}},{{&g_74,&g_74,&g_74,&g_74,&g_74,&g_74,&g_74}},{{&g_74,&g_74,&g_74,&g_74,&g_74,&g_74,&g_74}}};
+static int32_t ** volatile g_75 = &g_74;/* VOLATILE GLOBAL g_75 */
+static int32_t g_94 = 0xB7C80BFDL;
