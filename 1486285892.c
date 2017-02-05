c5f4a6569849110be05f5b4f6e2f44f3aca78f3b
saisi <saisi@users.noreply.github.com>
2017-02-05 09:11:32 +0000

    1486285892 astronautic

---
+++ 1486285892.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3709380617
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_9 = 1UL;
+static int8_t g_12[8][3][2] = {{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{0xA0L,0xA0L}},{{(-1L),0xA0L},{0xA0L,(-1L)},{(-1L),(-1L)}}};
+static uint64_t g_59 = 0x4E2B783560277ED4LL;
+static uint64_t *g_62 = &g_59;
+static uint64_t *g_63 = (void*)0;
