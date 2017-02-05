8816b4845a53e26d04624304f1aabb1b089e887a
saisi <saisi@users.noreply.github.com>
2017-02-05 10:11:38 +0000

    1486289497 bannerols

---
+++ 1486289497.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2211925998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_7 = 0xFB6318D5816210B9LL;
+static uint32_t g_8 = 0UL;
+static int32_t g_53 = 1L;
+static int8_t g_65 = 1L;
+static int8_t g_77 = 0x6FL;
+static int8_t *g_76 = &g_77;
+static int32_t *g_80 = &g_53;
+static int16_t g_97[1] = {0xA827L};
+static int32_t g_101 = 0L;
