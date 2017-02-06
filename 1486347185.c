ac9ed1e8295658c0ab4f5e5848f37464add911f6
saisi <saisi@users.noreply.github.com>
2017-02-06 02:13:05 +0000

    1486347185 Abhorson

---
+++ 1486347185.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3842791092
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 8;
+   signed f1 : 29;
+   unsigned f2 : 11;
+   unsigned f3 : 11;
+   const volatile unsigned f4 : 14;
+   volatile signed f5 : 13;
+   unsigned f6 : 23;
