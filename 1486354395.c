f1fe9a058cfc3c1cc2ee8cd441f3905c43faae94
saisi <saisi@users.noreply.github.com>
2017-02-06 04:13:15 +0000

    1486354395 Anakes

---
+++ 1486354395.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3197736876
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 4;
+   const signed f1 : 9;
+   signed f2 : 7;
+   uint8_t  f3;
+   unsigned f4 : 8;
+   signed f5 : 20;
+   const signed f6 : 5;
