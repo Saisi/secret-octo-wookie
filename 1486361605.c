206ad26fb2883d8db6003fbffd100e07e4a2455e
saisi <saisi@users.noreply.github.com>
2017-02-06 06:13:26 +0000

    1486361605 argos

---
+++ 1486361605.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      397263044
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x7CB3F4F4L;
+static volatile int32_t g_6 = 0x9BDD86BDL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 2L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9[7] = {0xDB51FD9AL,0xDB51FD9AL,0xDB51FD9AL,0xDB51FD9AL,0xDB51FD9AL,0xDB51FD9AL,0xDB51FD9AL};
