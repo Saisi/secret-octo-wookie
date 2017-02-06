fbe25361205f090a561361b712505e76b54bb3f5
saisi <saisi@users.noreply.github.com>
2017-02-06 23:45:08 +0000

    1486424708 antilogic

---
+++ 1486424708.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      705702411
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint8_t  f1;
+};
+
+union U1 {
+   int16_t  f0;
+   const int8_t * f1;
+   const volatile int16_t  f2;
+   signed f3 : 2;
