2b7c49e2a88ebf1fe44b23c7bd285c0032c12f60
saisi <saisi@users.noreply.github.com>
2017-02-06 17:44:30 +0000

    1486403069 antenna

---
+++ 1486403069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2889941327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0L;
+static uint16_t g_24 = 0xF340L;
+static uint16_t g_61 = 65533UL;
+static int8_t g_63 = 1L;
+static int32_t g_66 = 0xFEE61394L;
+static uint32_t g_68 = 5UL;
+static int8_t g_74[10][9][1] = {{{(-1L)},{0L},{0L},{0L},{0L},{(-1L)},{0L},{0L},{0L}},{{0L},{(-1L)},{0L},{0L},{0L},{0L},{(-1L)},{0L},{0L}},{{0L},{0L},{(-1L)},{0L},{0L},{0L},{0L},{(-1L)},{0L}},{{0L},{0L},{0L},{(-1L)},{0L},{0L},{0L},{0L},{(-1L)}},{{0L},{0L},{0L},{0L},{(-1L)},{0L},{0L},{0L},{0L}},{{(-1L)},{(-1L)},{0x27L},{0x27L},{(-1L)},{0L},{(-1L)},{0x27L},{0x27L}},{{(-1L)},{0L},{(-1L)},{0x27L},{0x27L},{(-1L)},{0L},{(-1L)},{0x27L}},{{0x27L},{(-1L)},{0L},{(-1L)},{0x27L},{0x27L},{(-1L)},{0L},{(-1L)}},{{0x27L},{0x27L},{(-1L)},{0L},{(-1L)},{0x27L},{0x27L},{(-1L)},{0L}},{{(-1L)},{0x27L},{0x27L},{(-1L)},{0L},{(-1L)},{0x27L},{0x27L},{(-1L)}}};
+static int32_t g_77 = 0x10B496A2L;
+static int32_t * volatile g_76[10] = {&g_77,&g_77,&g_66,&g_66,&g_77,&g_77,&g_77,&g_66,&g_66,&g_77};
