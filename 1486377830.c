dc7926b4946d07e077dd78a4c9338cc7058a16bb
saisi <saisi@users.noreply.github.com>
2017-02-06 10:43:50 +0000

    1486377830 antihierarchist

---
+++ 1486377830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      261720152
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_17 = 0xFF14EB4CL;
+static int64_t g_51[6] = {9L,9L,9L,9L,9L,9L};
+static volatile int16_t g_56 = 1L;/* VOLATILE GLOBAL g_56 */
+static int64_t g_57 = 0x189098934ECA6696LL;
+static volatile uint8_t g_58 = 255UL;/* VOLATILE GLOBAL g_58 */
+static int32_t *** volatile g_61 = (void*)0;/* VOLATILE GLOBAL g_61 */
+static int32_t *** volatile g_62 = (void*)0;/* VOLATILE GLOBAL g_62 */
+static int32_t *** volatile g_63 = (void*)0;/* VOLATILE GLOBAL g_63 */
+static int8_t g_66[6][10] = {{0x38L,0x5BL,0x8BL,1L,(-1L),0x38L,1L,0xB2L,1L,0x38L},{(-1L),0x5BL,0xACL,0x5BL,(-1L),0L,0x5BL,0xB2L,0x69L,(-1L)},{(-1L),1L,0x8BL,0x5BL,0x38L,0x38L,0x5BL,0x8BL,1L,(-1L)},{0x38L,0x5BL,0x8BL,1L,(-1L),0x38L,1L,0xB2L,1L,0x38L},{1L,1L,(-1L),1L,1L,0x5BL,1L,0xB1L,(-1L),1L},{1L,0x93L,0xB7L,1L,0x69L,0x69L,1L,0xB7L,0x93L,1L}};
