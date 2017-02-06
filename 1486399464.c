eb5af0c805ed92dca455a2d86271805ce7689632
saisi <saisi@users.noreply.github.com>
2017-02-06 16:44:24 +0000

    1486399464 antiangular

---
+++ 1486399464.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      560081568
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_30[4] = {0x71A0L,0x71A0L,0x71A0L,0x71A0L};
+static const uint16_t g_39 = 3UL;
+static const uint16_t *g_38[3][9] = {{&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39},{&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39},{&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39,&g_39}};
+static uint16_t g_41 = 0xB91EL;
+static uint16_t *g_40 = &g_41;
+static int16_t g_45 = 0xC6DDL;
+static uint16_t g_57 = 0x53DDL;
+static int32_t g_78 = 0x2E70D5DDL;
+static int32_t *g_77 = &g_78;
