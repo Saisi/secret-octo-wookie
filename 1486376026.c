dcae0b10daca34f10c33fe069b71c09c6a9ca87e
saisi <saisi@users.noreply.github.com>
2017-02-06 10:13:47 +0000

    1486376026 backbiter

---
+++ 1486376026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1410715277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-1L);
+static int32_t g_6 = 0xF951FE77L;
+static int32_t g_40 = 0x1459F451L;
+static volatile int32_t g_44 = (-1L);/* VOLATILE GLOBAL g_44 */
