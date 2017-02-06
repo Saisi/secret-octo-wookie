ac7ca4804119e2b1b24ca4698517185d851b2334
saisi <saisi@users.noreply.github.com>
2017-02-06 12:44:01 +0000

    1486385041 Arcadic

---
+++ 1486385041.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1970117291
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_7 = 0x81A7ECC2L;
+static int32_t g_10 = 0x0DA0BDBDL;
+static uint16_t g_12 = 4UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
