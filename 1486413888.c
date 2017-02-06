74eecda408e2aee2a584064f2121bfd830b951f8
saisi <saisi@users.noreply.github.com>
2017-02-06 20:44:48 +0000

    1486413888 acyloins

---
+++ 1486413888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      487923856
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x42EB90DAL;
+static uint16_t g_6 = 0xDD1DL;
+static int8_t g_8[7] = {0xBDL,0xBDL,0xBDL,0xBDL,0xBDL,0xBDL,0xBDL};
+static int8_t g_12 = (-1L);
+static uint32_t g_14 = 0x7B722BA7L;
+static int32_t g_41 = 3L;
+static int32_t g_48 = 1L;
+static uint32_t g_49 = 0x5DD68647L;
+static uint16_t g_68 = 0UL;
