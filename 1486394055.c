f8dab555aeb8ae8b404cbdd040ab3a53a1c05fb5
saisi <saisi@users.noreply.github.com>
2017-02-06 15:14:17 +0000

    1486394055 animists

---
+++ 1486394055.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1738844216
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_7[10] = {0xE0L,0x16L,0xE0L,0xE0L,0x16L,0xE0L,0xE0L,0x16L,0xE0L,0xE0L};
+static uint8_t g_9 = 0UL;
+static int32_t g_16[10] = {0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L,0x095A9796L};
+static int32_t g_17 = 0L;
+static volatile int32_t g_20[1] = {(-1L)};
+static volatile int32_t g_21 = 1L;/* VOLATILE GLOBAL g_21 */
+static int32_t g_22 = 0xFC3A065CL;
+static const uint8_t g_29 = 0UL;
+static const uint8_t * volatile g_28 = &g_29;/* VOLATILE GLOBAL g_28 */
