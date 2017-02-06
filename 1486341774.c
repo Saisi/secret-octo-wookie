d317c867c4240f63990ec46097a98a63cf55eeac
saisi <saisi@users.noreply.github.com>
2017-02-06 00:42:55 +0000

    1486341774 antimediaeval

---
+++ 1486341774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2203220165
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xFA860B4FL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 6L;
+static uint16_t g_10 = 0xE53EL;
+static volatile uint16_t g_21 = 65530UL;/* VOLATILE GLOBAL g_21 */
