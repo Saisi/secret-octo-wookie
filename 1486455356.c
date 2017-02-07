aa1d8ae6faf706a71431d4fe7e570eb8d9394047
saisi <saisi@users.noreply.github.com>
2017-02-07 08:15:57 +0000

    1486455356 animalculine

---
+++ 1486455356.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1884077725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 6L;
+static int32_t *g_5[8] = {&g_6,&g_6,&g_6,&g_6,&g_6,&g_6,&g_6,&g_6};
+static int32_t ** volatile g_10 = &g_5[0];/* VOLATILE GLOBAL g_10 */
+static uint32_t g_31 = 1UL;
+static int16_t g_50 = 0xB49FL;
+static uint32_t g_70 = 0xD05B3B6AL;
+static int16_t g_77 = 0xF33EL;
+static int16_t *g_76[10][4][2] = {{{(void*)0,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{(void*)0,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{(void*)0,&g_77},{&g_77,&g_77}},{{(void*)0,&g_77},{&g_77,&g_77},{&g_77,&g_77},{(void*)0,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}},{{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77},{&g_77,&g_77}}};
+static int32_t g_80 = 0x5ED53545L;
