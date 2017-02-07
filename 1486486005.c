7852aff086ee5de96d1df09c303b68e64f33d6e0
saisi <saisi@users.noreply.github.com>
2017-02-07 16:46:45 +0000

    1486486005 bachelorhoods

---
+++ 1486486005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1918460774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+   volatile int16_t  f2;
+   const int8_t  f3;
+};
+
+struct S1 {
+   int64_t  f0;
+   volatile int64_t  f1;
