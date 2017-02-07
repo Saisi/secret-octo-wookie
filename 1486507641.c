220b405edf667578c97e1b86b4a3a20bb78dc427
saisi <saisi@users.noreply.github.com>
2017-02-07 22:47:21 +0000

    1486507641 Archegoniata

---
+++ 1486507641.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2773403411
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[7] = {(-8L),0xFDF99FF6L,(-8L),(-8L),0xFDF99FF6L,(-8L),(-8L)};
+static int32_t g_6 = 0xF25CBD94L;
+static int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static int32_t *g_13 = &g_2[2];
+static int32_t ** volatile g_12 = &g_13;/* VOLATILE GLOBAL g_12 */
+static int32_t g_72 = 0x5F6E5699L;
+static uint32_t g_74[9][4] = {{0UL,0x03C9D266L,0x86EC4F4DL,0x03C9D266L},{0x03C9D266L,0xC8A068CFL,0x02EE3608L,0x03C9D266L},{0x02EE3608L,0x03C9D266L,0xEEA3ADEBL,0xEEA3ADEBL},{0UL,0UL,0x86EC4F4DL,0UL},{0UL,0xC8A068CFL,0xEEA3ADEBL,0UL},{0x02EE3608L,0UL,0x02EE3608L,0xEEA3ADEBL},{0x03C9D266L,0UL,0x86EC4F4DL,0UL},{0UL,0xC8A068CFL,0xC8A068CFL,0UL},{0x02EE3608L,0UL,0xC8A068CFL,0xEEA3ADEBL}};
+static uint32_t g_83 = 0x8086ADB6L;
