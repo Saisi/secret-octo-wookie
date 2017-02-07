90639e60a7474b9ae0efe19e4110ed907c9f43aa
saisi <saisi@users.noreply.github.com>
2017-02-07 19:47:03 +0000

    1486496820 aerobium

---
+++ 1486496820.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1012985499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 12;
+   unsigned f1 : 19;
+   unsigned f2 : 10;
+   volatile unsigned f3 : 13;
+   signed f4 : 10;
+   volatile signed f5 : 27;
+   unsigned f6 : 14;
+};
+
