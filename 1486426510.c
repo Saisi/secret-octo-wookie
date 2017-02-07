6d9d9fe20caad6b1182d87985ff326fc1938a131
saisi <saisi@users.noreply.github.com>
2017-02-07 00:15:11 +0000

    1486426510 Abatua

---
+++ 1486426510.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1208530702
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   signed f1 : 22;
+   volatile signed : 0;
+   unsigned f2 : 12;
+   signed f3 : 11;
+   const int32_t  f4;
+   signed f5 : 18;
+   signed f6 : 3;
+   int64_t  f7;
