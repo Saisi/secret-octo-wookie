39553fe0127ec942f377e5844fc661dac3a77570
saisi <saisi@users.noreply.github.com>
2017-02-07 01:45:19 +0000

    1486431918 Aletha

---
+++ 1486431918.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      871880226
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 0x8748AFA2L;
+static int32_t g_13 = (-1L);
+static int32_t *g_12 = &g_13;
+static uint64_t g_14[9] = {2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL};
+static int32_t ** volatile g_35 = &g_12;/* VOLATILE GLOBAL g_35 */
+static uint8_t g_59 = 8UL;
+static int8_t g_84[10][1][4] = {{{3L,3L,0L,0xDEL}},{{0xDEL,(-3L),0L,(-3L)}},{{3L,0x30L,0x81L,0L}},{{(-3L),0x30L,0x30L,(-3L)}},{{0x30L,(-3L),3L,0xDEL}},{{0x30L,3L,0x30L,0x81L}},{{(-3L),0xDEL,0x81L,0x81L}},{{3L,3L,0L,0xDEL}},{{0xDEL,(-3L),0L,(-3L)}},{{3L,0xDEL,0L,3L}}};
+static int32_t ** const  volatile g_91 = &g_12;/* VOLATILE GLOBAL g_91 */
+static uint32_t g_109 = 0xAA128409L;
