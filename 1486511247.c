2b89bb052f6fe36242c4dba12e21bad43563a7d4
saisi <saisi@users.noreply.github.com>
2017-02-07 23:47:27 +0000

    1486511247 Bardulph

---
+++ 1486511247.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2867242359
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S0 g_2 = {4294967289UL};/* VOLATILE GLOBAL g_2 */
+static volatile struct S0 * volatile g_4[3][2] = {{&g_2,&g_2},{&g_2,&g_2},{&g_2,&g_2}};
+static volatile struct S0 g_7 = {0xCD4EE3B9L};/* VOLATILE GLOBAL g_7 */
+
+
