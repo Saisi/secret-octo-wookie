39c5dd711e0e338a46666fb1afccf4b14fd36a2a
saisi <saisi@users.noreply.github.com>
2017-02-07 02:45:24 +0000

    1486435523 anticommerciality

---
+++ 1486435523.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3064512206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 21;
+   unsigned f1 : 22;
+   signed : 0;
+   volatile unsigned f2 : 12;
+   signed f3 : 11;
+   signed f4 : 22;
+   volatile unsigned f5 : 28;
+   const unsigned f6 : 1;
+   const unsigned f7 : 1;
