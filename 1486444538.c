aa1b5f22233233c87f76e58443f38ecde27c068e
saisi <saisi@users.noreply.github.com>
2017-02-07 05:15:40 +0000

    1486444538 astringencies

---
+++ 1486444538.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1966002319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 6;
+   unsigned f1 : 5;
+   unsigned f2 : 20;
+   unsigned f3 : 15;
+   volatile signed f4 : 4;
+};
+#pragma pack(pop)
