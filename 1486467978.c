9fd29f506555a5d16a473dcea9bfb5031e8b5d74
saisi <saisi@users.noreply.github.com>
2017-02-07 11:46:18 +0000

    1486467978 anlet

---
+++ 1486467978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1411785292
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 20;
+   const unsigned f1 : 25;
+   signed f2 : 23;
+   volatile unsigned f3 : 7;
+   unsigned f4 : 26;
+   signed f5 : 9;
+   signed f6 : 3;
+};
+
