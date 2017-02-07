af8f717a06801bd02655dc51a20992d47a37beb4
saisi <saisi@users.noreply.github.com>
2017-02-07 15:16:37 +0000

    1486480596 antidemocratical

---
+++ 1486480596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      68619648
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_15 = 8UL;/* VOLATILE GLOBAL g_15 */
+static int32_t g_18 = 0L;
+static uint64_t g_20 = 0xCC7C44D996497440LL;
+static int16_t g_82 = 0x771CL;
+static uint8_t g_84 = 251UL;
+static int32_t g_101 = 0L;
+static int64_t g_103 = 0x5D98765FAFB16363LL;
+static uint32_t g_107 = 0x652714D0L;
+static int16_t g_111 = 0x1C05L;
