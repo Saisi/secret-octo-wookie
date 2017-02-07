d9a1cfbb1b05a98ac1a99b0307c12f3cff947863
saisi <saisi@users.noreply.github.com>
2017-02-07 10:16:09 +0000

    1486462569 aliamenta

---
+++ 1486462569.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2567298189
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_2[7] = {0x76L,0x76L,0x76L,0x76L,0x76L,0x76L,0x76L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
