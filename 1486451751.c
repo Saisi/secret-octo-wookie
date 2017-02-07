08edd02539c89885390c5681234fc392d4a6e54d
saisi <saisi@users.noreply.github.com>
2017-02-07 07:15:51 +0000

    1486451751 Anaces

---
+++ 1486451751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2854120452
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   const int16_t  f1;
+   uint8_t  f2;
+   uint16_t  f3;
+   const uint32_t  f4;
+   int16_t  f5;
+   const int64_t  f6;
