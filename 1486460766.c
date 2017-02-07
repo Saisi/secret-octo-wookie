9ec768cd152bdc2da05a4a7037d8aa84168a72c7
saisi <saisi@users.noreply.github.com>
2017-02-07 09:46:06 +0000

    1486460766 baal

---
+++ 1486460766.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      849891989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int64_t  f0;
+   unsigned f1 : 30;
+   const volatile unsigned f2 : 28;
+   volatile signed f3 : 16;
+   unsigned f4 : 23;
+   volatile signed f5 : 9;
+   unsigned f6 : 8;
+   const volatile signed f7 : 2;
+};
