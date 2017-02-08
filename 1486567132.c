8c32621f35866a3e126b67fdc4b677e6e98dcdbe
saisi <saisi@users.noreply.github.com>
2017-02-08 15:18:52 +0000

    1486567132 armhoop

---
+++ 1486567132.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4254345671
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xD4081C29L;
+static volatile int32_t g_4 = (-6L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x51C76D7BL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static volatile int32_t g_7 = 1L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0xBAB59342L;
+static volatile int32_t g_9 = 0x4678DC2AL;/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10 = (-10L);/* VOLATILE GLOBAL g_10 */
+static volatile int32_t g_11 = 0x3B2B74C3L;/* VOLATILE GLOBAL g_11 */
