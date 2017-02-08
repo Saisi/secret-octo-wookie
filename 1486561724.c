bf8c5d9c1ee97ebbb2006c12ece86b18da7c9574
saisi <saisi@users.noreply.github.com>
2017-02-08 13:48:45 +0000

    1486561724 backfields

---
+++ 1486561724.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4277230404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   signed f1 : 29;
+   const uint32_t  f2;
+   const signed f3 : 12;
+   volatile unsigned f4 : 15;
+};
+#pragma pack(pop)
