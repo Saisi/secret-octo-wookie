519cb8452f49c1cc915ec3e8e98ae290681953c8
saisi <saisi@users.noreply.github.com>
2017-02-08 21:49:32 +0000

    1486590569 abdomen

---
+++ 1486590569.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1036324425
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   const unsigned f1 : 14;
+   volatile signed f2 : 25;
+   uint16_t  f3;
+   const volatile unsigned f4 : 25;
+   volatile unsigned f5 : 3;
+   const volatile signed f6 : 26;
