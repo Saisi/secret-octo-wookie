e386047d31ee732d98f0d87cb3e2cd39ad03482d
saisi <saisi@users.noreply.github.com>
2017-02-08 03:17:46 +0000

    1486523865 aliening

---
+++ 1486523865.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1787112722
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 23;
+   signed f1 : 16;
+   volatile signed f2 : 18;
+   signed f3 : 22;
+   unsigned f4 : 13;
+   signed f5 : 16;
+   signed f6 : 14;
