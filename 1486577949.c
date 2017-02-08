82e65b536513ab63baa63e686a46210ddfac7eec
saisi <saisi@users.noreply.github.com>
2017-02-08 18:19:09 +0000

    1486577949 Auscultoscope

---
+++ 1486577949.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      278517701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint64_t  f1;
+   uint16_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xDBA33992L;
+static int64_t g_5[3][10] = {{0x44C039ECF4A41637LL,(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL,(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL,(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL},{(-9L),(-9L),(-1L),(-9L),(-9L),(-1L),(-9L),(-9L),(-1L),(-9L)},{(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL,(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL,(-9L),0x44C039ECF4A41637LL,0x44C039ECF4A41637LL,(-9L)}};
