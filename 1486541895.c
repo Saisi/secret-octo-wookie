074f65a12cca31809e35e0277d2e139885c54c74
saisi <saisi@users.noreply.github.com>
2017-02-08 08:18:15 +0000

    1486541895 Ardhanari

---
+++ 1486541895.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2573149222
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0L;
+static int32_t g_10 = 0L;
+static int32_t g_51 = 7L;
+static int32_t g_54[3] = {1L,1L,1L};
