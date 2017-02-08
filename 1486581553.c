d7f0cc6350b42a21fd547e2d0fca05dbcbbde038
saisi <saisi@users.noreply.github.com>
2017-02-08 19:19:14 +0000

    1486581553 alpinisms

---
+++ 1486581553.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1849021384
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const unsigned f1 : 31;
+   uint32_t  f2;
+   const signed f3 : 29;
+};
+
+union U1 {
+   uint16_t  f0;
+};
