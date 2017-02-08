630c5dbcf2a65aa05ce8a4acab84c9428370bb01
saisi <saisi@users.noreply.github.com>
2017-02-08 05:48:00 +0000

    1486532880 Aruba

---
+++ 1486532880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4670968
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x477862FDL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0L;
+static int32_t *g_45[1] = {(void*)0};
+static int32_t * const * const g_44 = &g_45[0];
+static uint8_t g_60 = 0x2CL;
+static int16_t g_68 = (-9L);
+static uint32_t g_91 = 18446744073709551615UL;
+static int8_t g_93[1] = {(-1L)};
