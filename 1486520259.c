8f94110a4862ff29876da8f5092f985c9535d9cc
saisi <saisi@users.noreply.github.com>
2017-02-08 02:17:39 +0000

    1486520259 arterioplasty

---
+++ 1486520259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1823243674
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   uint64_t  f0;
+   unsigned f1 : 11;
+   int8_t * const  volatile  f2;
+};
