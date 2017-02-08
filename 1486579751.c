ae84da10659d9ec5b23bdf74cef9373e9c48fb63
saisi <saisi@users.noreply.github.com>
2017-02-08 18:49:11 +0000

    1486579751 Amphicarpaea

---
+++ 1486579751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      917822724
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 0xF665683CL;
+static uint32_t g_28 = 1UL;
+static uint32_t g_67 = 0x4A737B78L;
+static int32_t g_83 = (-4L);
+static int32_t *g_82 = &g_83;
