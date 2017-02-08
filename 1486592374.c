eb670518bc2ed30037b8888c706f30a5703b507e
saisi <saisi@users.noreply.github.com>
2017-02-08 22:19:37 +0000

    1486592374 accreditations

---
+++ 1486592374.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      585568976
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 22;
+   const signed f1 : 19;
+   volatile signed f2 : 9;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
