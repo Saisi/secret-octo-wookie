bb32b7e926e029d267b979d274dba3e1c60aa0f6
saisi <saisi@users.noreply.github.com>
2017-02-08 19:49:19 +0000

    1486583358 acridin

---
+++ 1486583358.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2920533634
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+};
+
+union U1 {
+   signed f0 : 15;
+   volatile uint32_t  f1;
+   uint64_t  f2;
+};
+
