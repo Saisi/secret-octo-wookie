1f1181822c2b1ca7b4e1284bd0d2b9f1edfd804c
saisi <saisi@users.noreply.github.com>
2017-02-08 08:48:18 +0000

    1486543697 Alledonia

---
+++ 1486543697.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3104078921
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 3;
+   signed f1 : 14;
+   unsigned f2 : 25;
+   unsigned : 0;
+   const volatile unsigned f3 : 17;
+   volatile signed f4 : 6;
+   volatile signed f5 : 13;
+   signed f6 : 12;
+};
