4e283bdf4e8b5087f3f9215114643c9f57db2e8e
saisi <saisi@users.noreply.github.com>
2017-02-08 06:18:04 +0000

    1486534684 Bagheera

---
+++ 1486534684.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      20644327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[1][2][10] = {{{3L,0xB910L,0xB910L,3L,3L,0xB910L,0xB910L,3L,3L,0xB910L},{3L,3L,0xB910L,0xB910L,3L,3L,0xB910L,0xB910L,3L,3L}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
