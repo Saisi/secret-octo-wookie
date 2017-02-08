08899d0e0edd75c12d645241011205197bda689e
saisi <saisi@users.noreply.github.com>
2017-02-08 17:49:06 +0000

    1486576146 astereognosis

---
+++ 1486576146.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3331117154
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[7] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL};
+static volatile int32_t g_3 = 0x76F5020FL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+static int32_t g_25 = (-1L);
+static uint8_t g_75 = 247UL;
+static uint32_t g_82[3] = {0xFAF1BECDL,0xFAF1BECDL,0xFAF1BECDL};
+static int32_t * volatile g_86[3] = {&g_4,&g_4,&g_4};
+static int32_t * volatile * volatile g_85 = &g_86[1];/* VOLATILE GLOBAL g_85 */
+static int32_t g_91[4][4] = {{1L,0x8CABD046L,1L,0x5F8785D5L},{1L,0x5F8785D5L,0x5F8785D5L,1L},{0x7E6FF2F0L,0x5F8785D5L,0x959469FDL,0x5F8785D5L},{0x5F8785D5L,0x8CABD046L,0x959469FDL,0x959469FDL}};
