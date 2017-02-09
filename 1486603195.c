55c7391fe82b7d908eb0c09abf2ec421dc161224
saisi <saisi@users.noreply.github.com>
2017-02-09 01:19:55 +0000

    1486603195 actress

---
+++ 1486603195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2611223131
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   unsigned f1 : 19;
+   unsigned f2 : 10;
+};
+
+struct S1 {
+   const uint16_t  f0;
+   int16_t  f1;
+   int64_t  f2;
