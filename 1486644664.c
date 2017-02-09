11bab9de9c865fe2587d50d73c231757ff974d0f
saisi <saisi@users.noreply.github.com>
2017-02-09 12:51:06 +0000

    1486644664 abdominocystic

---
+++ 1486644664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1844899674
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   int32_t  f1;
+   uint32_t  f2;
+   uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[8] = {0xBB6748F6L,0xBB6748F6L,0xBB6748F6L,0xBB6748F6L,0xBB6748F6L,0xBB6748F6L,0xBB6748F6L,0xBB6748F6L};
+static int64_t g_49 = 0x99B96BC712BAD479LL;
