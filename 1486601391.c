14e70862dffe0cc9a5f8d6d0c635c0fe0b82387c
saisi <saisi@users.noreply.github.com>
2017-02-09 00:49:53 +0000

    1486601391 aeolopantalon

---
+++ 1486601391.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2719834450
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 23;
+   int32_t  f1;
+   int8_t * f2;
+   int8_t  f3;
+   uint64_t  f4;
+};
+
+union U1 {
+   unsigned f0 : 29;
