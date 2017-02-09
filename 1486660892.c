c6b6393fabcc18843ed7d27030f0cf34079efd4e
saisi <saisi@users.noreply.github.com>
2017-02-09 17:21:33 +0000

    1486660892 agave

---
+++ 1486660892.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1973410960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 22;
+};
+
+struct S1 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x9AED23B6L;/* VOLATILE GLOBAL g_3 */
