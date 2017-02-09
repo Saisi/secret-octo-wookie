c64c5812270f787ca22d1fc0c73db832944cf86c
saisi <saisi@users.noreply.github.com>
2017-02-09 22:22:02 +0000

    1486678922 asymbolic

---
+++ 1486678922.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1034212557
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   const int8_t * f1;
+   int8_t * f2;
+   const volatile uint64_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 0x70L;
