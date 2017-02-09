aae21cbf504f0798cd0b9407d1063e9bbf0e3432
saisi <saisi@users.noreply.github.com>
2017-02-09 01:49:58 +0000

    1486604998 astrobiologies

---
+++ 1486604998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3596189699
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 28;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   unsigned f0 : 30;
+   int16_t  f1;
+   signed f2 : 26;
