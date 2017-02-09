6685bcdd55f2cf1d7de9a41c29f83314e9920a46
saisi <saisi@users.noreply.github.com>
2017-02-09 04:20:14 +0000

    1486614014 amylase

---
+++ 1486614014.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3911672710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   const signed f1 : 2;
+   const volatile unsigned f2 : 28;
+   const signed f3 : 24;
+   volatile unsigned f4 : 7;
+   volatile signed f5 : 2;
+   volatile unsigned f6 : 24;
+   const unsigned f7 : 13;
+};
