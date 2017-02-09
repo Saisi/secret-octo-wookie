58dc023bbc5d676ef29aa21046c047f059d084df
saisi <saisi@users.noreply.github.com>
2017-02-09 16:21:27 +0000

    1486657286 anemias

---
+++ 1486657286.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1494601164
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   int32_t  f1;
+   uint64_t  f2;
+   int64_t  f3;
+   int64_t  f4;
+   int16_t  f5;
+   const int64_t  f6;
+   uint32_t  f7;
+   uint64_t  f8;
