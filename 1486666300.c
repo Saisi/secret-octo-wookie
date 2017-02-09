4cdfa41fe65f616c682ea766985d5b6c89b96205
saisi <saisi@users.noreply.github.com>
2017-02-09 18:51:41 +0000

    1486666300 autoionization

---
+++ 1486666300.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1150019001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[1] = {0x9CL};
+static int32_t g_6 = 0x46DF37D4L;
+static int32_t * volatile g_7 = (void*)0;/* VOLATILE GLOBAL g_7 */
+static uint32_t g_9 = 5UL;
+static int32_t *g_55 = &g_6;
+static int32_t **g_54 = &g_55;
+static int64_t g_78 = 5L;
+static int16_t g_87 = 0xE30EL;
+static uint64_t g_96 = 18446744073709551615UL;
