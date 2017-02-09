20d756d8bb84ab040637f024ad064bccb718484d
saisi <saisi@users.noreply.github.com>
2017-02-09 20:21:50 +0000

    1486671710 bagnet

---
+++ 1486671710.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      628182709
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const signed f1 : 12;
+   uint64_t  f2;
+};
+
+union U1 {
+   int32_t  f0;
+   int8_t * f1;
+};
