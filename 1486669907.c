d927a02801f8d77e07a631077138714918892b54
saisi <saisi@users.noreply.github.com>
2017-02-09 19:51:47 +0000

    1486669907 aphoria

---
+++ 1486669907.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4004394046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 13;
+   signed f1 : 14;
+   signed f2 : 24;
+   const uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_15 = 3UL;
+static uint64_t g_30 = 0xCAE2FE81720D7006LL;
