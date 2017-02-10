25ea4c7e6c731ac93b01d7530ae03548a53ed740
saisi <saisi@users.noreply.github.com>
2017-02-10 07:52:58 +0000

    1486713176 arsenide

---
+++ 1486713176.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1771023596
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   uint64_t  f1;
+   uint8_t  f2;
+   int64_t  f3;
+   volatile uint32_t  f4;
+};
+
+struct S1 {
+   signed f0 : 25;
