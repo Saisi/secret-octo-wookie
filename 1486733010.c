e37ab3c31f3ffa62c9610ad42a299078b9517c20
saisi <saisi@users.noreply.github.com>
2017-02-10 13:23:30 +0000

    1486733010 Aranha

---
+++ 1486733010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1199728756
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 8;
+};
+
+struct S1 {
+   int16_t  f0;
+   uint16_t  f1;
+   signed f2 : 12;
+};
+
