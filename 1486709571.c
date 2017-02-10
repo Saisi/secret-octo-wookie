1a31b78a8840f90cb5105d3cc351728cca55363b
saisi <saisi@users.noreply.github.com>
2017-02-10 06:52:52 +0000

    1486709571 amberoids

---
+++ 1486709571.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2647879471
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 22;
+   const signed f1 : 21;
+   signed f2 : 4;
+   volatile unsigned f3 : 31;
+   volatile signed f4 : 20;
+   volatile unsigned f5 : 29;
+   const signed f6 : 8;
+   volatile unsigned f7 : 27;
+   const unsigned f8 : 18;
