e83569323141feef4c45515e78fac61d7e8a221e
saisi <saisi@users.noreply.github.com>
2017-02-10 19:54:09 +0000

    1486756447 acupunctuate

---
+++ 1486756447.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2874109815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+   unsigned f1 : 9;
+   unsigned f2 : 8;
+   const unsigned f3 : 19;
+   volatile signed f4 : 24;
+   volatile signed f5 : 17;
+   signed f6 : 12;
+};
+
