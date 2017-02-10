80d98367db4ff65d29c26b0b6489d1634387a55d
saisi <saisi@users.noreply.github.com>
2017-02-10 06:22:49 +0000

    1486707768 Alecto

---
+++ 1486707768.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1612152366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_37 = 65535UL;/* VOLATILE GLOBAL g_37 */
+static int8_t g_52 = 2L;
+static uint32_t g_68 = 0x503B5B0DL;
+static int64_t g_75 = 0x82ADE89B93B3BBBBLL;
+static uint16_t g_89 = 65535UL;
+static int32_t g_97 = 0xEA0D0A06L;
+static int32_t *g_96[6] = {&g_97,&g_97,&g_97,&g_97,&g_97,&g_97};
+static int32_t g_122 = 0x34EA093EL;
+static uint32_t g_158 = 1UL;
