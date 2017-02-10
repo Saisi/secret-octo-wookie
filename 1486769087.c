94ba0c1d29255a91f469ae0c5474f99a252175f0
saisi <saisi@users.noreply.github.com>
2017-02-10 23:24:47 +0000

    1486769087 alkalises

---
+++ 1486769087.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3007243319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+   unsigned f1 : 9;
+   unsigned f2 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static struct S0 g_15 = {4828,1,8};
+static int64_t g_61 = (-3L);
