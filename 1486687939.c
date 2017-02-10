c1627ecf55c83610d21d9d9f15dfc06eda347653
saisi <saisi@users.noreply.github.com>
2017-02-10 00:52:19 +0000

    1486687939 astroite

---
+++ 1486687939.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3152102936
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x596960F5L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 8L;
+static int32_t g_6 = 0xC62E0897L;
+static int32_t g_10 = 0xC39BA095L;
+static int32_t g_12 = 0x0B95AE64L;
