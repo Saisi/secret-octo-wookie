a60391687365b89b849731eb21313a013c31be3c
saisi <saisi@users.noreply.github.com>
2017-02-10 04:52:40 +0000

    1486702360 ambash

---
+++ 1486702360.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      199996414
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 13;
+   int32_t  f1;
+   const uint8_t  f2;
+   unsigned f3 : 21;
+   const unsigned f4 : 20;
+   const int8_t  f5;
+   volatile int8_t  f6;
+   int8_t  f7;
+   int32_t  f8;
