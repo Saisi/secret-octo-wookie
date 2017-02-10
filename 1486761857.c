3034d5e12b46ddc59c650ba799885d0166ad5cf0
saisi <saisi@users.noreply.github.com>
2017-02-10 21:24:17 +0000

    1486761857 bacilli

---
+++ 1486761857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3760532248
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_11 = 0x01L;/* VOLATILE GLOBAL g_11 */
+static uint64_t g_23[4] = {1UL,1UL,1UL,1UL};
+static uint16_t g_39 = 0xC75DL;
+static int16_t g_97 = 0x38D5L;
+static uint8_t g_98[7] = {0xF1L,0xF1L,0xF1L,0xF1L,0xF1L,0xF1L,0xF1L};
