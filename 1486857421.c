926d5913eb541d59173d47bccddb636711c602d4
saisi <saisi@users.noreply.github.com>
2017-02-11 23:57:02 +0000

    1486857421 antisophism

---
+++ 1486857421.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2094746775
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_4 = 0x1888L;
+static uint16_t g_25 = 0x3648L;
+static int8_t g_64 = 0x53L;
+static int32_t g_73 = 0L;
+static int32_t g_77 = 7L;
+static volatile uint8_t g_78 = 4UL;/* VOLATILE GLOBAL g_78 */
+static int32_t *g_82[10] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t ** volatile g_81 = &g_82[4];/* VOLATILE GLOBAL g_81 */
+static int32_t g_135 = (-9L);
