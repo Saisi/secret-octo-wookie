8b1f2af1406e700265633c1e19273a8a347cce91
saisi <saisi@users.noreply.github.com>
2017-02-11 21:56:51 +0000

    1486850209 amaranthine

---
+++ 1486850209.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2115190111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+   volatile unsigned f1 : 13;
+   unsigned f2 : 28;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile uint8_t  f0;
