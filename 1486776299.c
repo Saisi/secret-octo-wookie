6c24ba65997a8919aed9518a649aa59c17c77170
saisi <saisi@users.noreply.github.com>
2017-02-11 01:24:59 +0000

    1486776299 alterableness

---
+++ 1486776299.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3915480505
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   int64_t  f1;
+   int16_t  f2;
+   volatile int64_t  f3;
+   volatile unsigned f4 : 10;
+   uint32_t  f5;
+   int32_t  f6;
+   const signed f7 : 18;
+};
