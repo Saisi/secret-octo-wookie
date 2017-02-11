deedd1424eaa07a275a994d472446b06cd4dddf5
saisi <saisi@users.noreply.github.com>
2017-02-11 07:25:32 +0000

    1486797932 aspide

---
+++ 1486797932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      613103051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_4 = 18446744073709551615UL;/* VOLATILE GLOBAL g_4 */
+static uint16_t g_12 = 0xAC46L;
+static uint8_t g_14 = 1UL;
+static int32_t g_15 = (-5L);
+static const uint8_t g_26[2] = {0x53L,0x53L};
+static const uint8_t *g_27 = (void*)0;
+static uint8_t g_55[5] = {255UL,255UL,255UL,255UL,255UL};
+static uint16_t g_57 = 1UL;
+static int16_t g_65 = (-1L);
