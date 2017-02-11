17c406a21ad997f045fbdf1e80056cab07a85feb
saisi <saisi@users.noreply.github.com>
2017-02-11 19:56:40 +0000

    1486842999 Apharsathacites

---
+++ 1486842999.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2103555712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint8_t  f1;
+   volatile uint32_t  f2;
+   uint8_t  f3;
+   volatile uint64_t  f4;
+   uint16_t  f5;
+   uint64_t  f6;
+   uint16_t  f7;
+   int16_t  f8;
