83ee39960196ac875dfde68af75e764ecfc1a03d
saisi <saisi@users.noreply.github.com>
2017-02-11 08:25:38 +0000

    1486801537 agitates

---
+++ 1486801537.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2072527672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_10 = (-8L);/* VOLATILE GLOBAL g_10 */
+static int8_t g_21 = (-1L);
+static int32_t g_49 = 0x3A4C6382L;
+static int32_t *g_51 = &g_49;
+static uint16_t g_68 = 1UL;
+static int64_t g_80 = 0xE2CCF58D911ACA8BLL;
+static int16_t g_90 = 0L;
+static uint32_t g_114 = 0UL;
+static int16_t g_143[10] = {0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L,0x33D8L};
