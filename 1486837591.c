9a7c4a361ec7b000c9dc106c9325ef7805fb29d1
saisi <saisi@users.noreply.github.com>
2017-02-11 18:26:32 +0000

    1486837591 bankroller

---
+++ 1486837591.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2737718192
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+};
+
+union U1 {
+   struct S0  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_20 = 0L;
