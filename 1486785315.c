d696e5492e7ea298c7b5fa8e57cc4a529dfe6f0a
saisi <saisi@users.noreply.github.com>
2017-02-11 03:55:15 +0000

    1486785315 anorthitic

---
+++ 1486785315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2982362550
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int8_t  f1;
+   uint8_t  f2;
+   int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8][1] = {{(-4L)},{(-4L)},{(-4L)},{(-4L)},{(-4L)},{(-4L)},{(-4L)},{(-4L)}};
+static uint16_t g_5 = 4UL;
