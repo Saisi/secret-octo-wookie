f88ae78846aa37b7f49d34bd481c3c2ec89df924
saisi <saisi@users.noreply.github.com>
2017-02-11 19:26:37 +0000

    1486841196 Balsameaceae

---
+++ 1486841196.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      151547013
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const volatile uint8_t  f1;
+   volatile uint8_t  f2;
+   signed f3 : 10;
+   uint16_t  f4;
+   int32_t  f5;
+   unsigned f6 : 15;
