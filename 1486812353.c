0041ba941ae072dac9a37f8c715fe4a3922f3097
saisi <saisi@users.noreply.github.com>
2017-02-11 11:25:53 +0000

    1486812353 barbicans

---
+++ 1486812353.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1840088435
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int16_t  f0;
+   const int32_t  f1;
+   uint64_t  f2;
+   int16_t  f3;
+   int64_t  f4;
+   int8_t  f5;
+   uint8_t  f6;
