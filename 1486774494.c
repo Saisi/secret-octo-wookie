32dcf4c1e40ea6850303c22cc63222ab7706c270
saisi <saisi@users.noreply.github.com>
2017-02-11 00:54:57 +0000

    1486774494 arabizing

---
+++ 1486774494.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1805290429
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   unsigned : 0;
+   signed f1 : 3;
+   volatile signed f2 : 2;
+   volatile signed f3 : 11;
+   volatile signed f4 : 24;
+   signed f5 : 23;
+   unsigned f6 : 14;
+   signed f7 : 25;
