5a40c02b6d66eb58220d48f16210cfd5ef25b0a4
saisi <saisi@users.noreply.github.com>
2017-02-11 05:25:22 +0000

    1486790722 anastomos

---
+++ 1486790722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      970991654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int8_t  f1;
+   const volatile int8_t * volatile  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_4[8] = {{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL},{1UL}};
+static int8_t *g_11[4][5] = {{&g_4[0].f1,&g_4[0].f1,&g_4[0].f1,&g_4[0].f1,&g_4[0].f1},{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0},{&g_4[0].f1,&g_4[0].f1,&g_4[0].f1,&g_4[0].f1,&g_4[0].f1},{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0}};
+static uint8_t g_14 = 0UL;
