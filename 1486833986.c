4c5c9268e2b13efe4720b874aad1bbc9540078df
saisi <saisi@users.noreply.github.com>
2017-02-11 17:26:27 +0000

    1486833986 bagging

---
+++ 1486833986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      957042357
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 14;
+   unsigned f2 : 12;
+   unsigned : 0;
+   signed f3 : 7;
+   const volatile signed f4 : 25;
+   volatile unsigned f5 : 24;
+   signed f6 : 18;
+};
