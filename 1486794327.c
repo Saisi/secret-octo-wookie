b64223ed388f38a54481ed14bff6ad83ddda9512
saisi <saisi@users.noreply.github.com>
2017-02-11 06:25:27 +0000

    1486794327 Ampyx

---
+++ 1486794327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3051504151
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[6] = {3L,0xAFD94FE5L,3L,3L,0xAFD94FE5L,3L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
