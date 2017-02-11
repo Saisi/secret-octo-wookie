3fcd166f728b0b4b2b38f506bb6ae790e5b11ff8
saisi <saisi@users.noreply.github.com>
2017-02-11 07:55:34 +0000

    1486799734 adon

---
+++ 1486799734.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1431476406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1C08901FL;
+static uint16_t g_6 = 65529UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
