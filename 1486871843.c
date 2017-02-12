136d4b785f37739affadf6a8d6d1c34379a96f4a
saisi <saisi@users.noreply.github.com>
2017-02-12 03:57:23 +0000

    1486871843 acromiohumeral

---
+++ 1486871843.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      643013588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[5][1] = {{(-9L)},{(-9L)},{(-9L)},{(-9L)},{(-9L)}};
+static int32_t g_3 = 0x3B7C7CE5L;
+static int32_t g_6[3] = {0L,0L,0L};
+static volatile int32_t g_9 = 1L;/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10[4] = {0x0AA6E6F7L,0x0AA6E6F7L,0x0AA6E6F7L,0x0AA6E6F7L};
+static int32_t g_11 = 0x7C1188E1L;
+static int32_t g_46 = 2L;
+static uint16_t g_104[2] = {65535UL,65535UL};
+static uint16_t g_106[10] = {0x567FL,0x567FL,0x567FL,0x567FL,0x567FL,0x567FL,0x567FL,0x567FL,0x567FL,0x567FL};
