2efbe06fed1ce017276347392c7a194e0b990f7c
saisi <saisi@users.noreply.github.com>
2017-02-12 21:59:05 +0000

    1486936745 affluents

---
+++ 1486936745.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4264844265
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 3L;
+static int32_t g_7 = 0xA6DEEC54L;
+static int16_t g_12 = 0x243EL;
+static int16_t g_13 = 0L;
+static uint8_t g_14 = 0UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
