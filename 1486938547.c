b8c618489ac131ae551cebe8d72166ce79f5de75
saisi <saisi@users.noreply.github.com>
2017-02-12 22:29:08 +0000

    1486938547 Angora

---
+++ 1486938547.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      146719311
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5B57FD54L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x5F5C184CL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xD444182BL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-2L);
+static int16_t g_12 = 0x5457L;
+static int16_t g_16 = 0x017CL;
+static uint64_t g_19 = 18446744073709551609UL;
+static uint64_t g_55 = 0x1AB372C247AFAE86LL;
+static int16_t *g_62[9] = {&g_16,&g_16,&g_16,&g_16,&g_16,&g_16,&g_16,&g_16,&g_16};
