43b9519cd7542ad311e642ee9d0e8f1de3f77f36
saisi <saisi@users.noreply.github.com>
2017-02-12 13:28:21 +0000

    1486906099 arachin

---
+++ 1486906099.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2072979339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int8_t * volatile  f1;
+   const int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[9][4][1] = {{{1L},{(-10L)},{0x17BE1879L},{(-1L)}},{{0x17BE1879L},{(-10L)},{1L},{(-1L)}},{{0xC5021C3AL},{(-1L)},{(-4L)},{4L}},{{1L},{4L},{(-4L)},{(-1L)}},{{0xC5021C3AL},{(-1L)},{1L},{(-10L)}},{{0x17BE1879L},{(-1L)},{0x17BE1879L},{(-10L)}},{{1L},{(-1L)},{0xC5021C3AL},{(-1L)}},{{(-4L)},{4L},{1L},{4L}},{{(-4L)},{(-1L)},{0xC5021C3AL},{(-1L)}}};
+static int32_t g_6 = 0x34DE6F88L;
+static uint8_t g_71 = 253UL;
