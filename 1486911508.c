d79ab4476c4016cfc87dd6cf8d79aeecfa676097
saisi <saisi@users.noreply.github.com>
2017-02-12 14:58:29 +0000

    1486911508 avariciousness

---
+++ 1486911508.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3266322962
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile int64_t  f1;
+   signed f2 : 18;
+   signed f3 : 22;
+   volatile uint64_t  f4;
+};
+
+union U1 {
+   volatile int32_t  f0;
