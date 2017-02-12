337d5e6e108f9a6e6c6c44f6310e355fd0d0b131
saisi <saisi@users.noreply.github.com>
2017-02-12 03:27:21 +0000

    1486870040 antiphons

---
+++ 1486870040.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3664007615
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 8L;
+static int32_t *g_6[6] = {&g_7,(void*)0,&g_7,&g_7,(void*)0,&g_7};
+static uint16_t g_10 = 0x1AD6L;
+static uint8_t g_16 = 0UL;
+static int8_t g_50 = 0xC4L;
+static int32_t ** volatile g_67 = &g_6[5];/* VOLATILE GLOBAL g_67 */
+static int32_t g_89 = 0xDA23AE4BL;
+static uint32_t g_98 = 4294967287UL;
+static uint16_t g_100[3] = {1UL,1UL,1UL};
