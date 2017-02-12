fb0740105c5f01eee863bb0c5b240d4fe8b7e748
saisi <saisi@users.noreply.github.com>
2017-02-12 00:57:08 +0000

    1486861027 aquatics

---
+++ 1486861027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1877220928
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   int8_t  f1;
+   unsigned f2 : 3;
+   const volatile unsigned f3 : 25;
+   signed f4 : 26;
+};
+#pragma pack(pop)
