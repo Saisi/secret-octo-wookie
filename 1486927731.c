26be3fc7f7666cb538ec15dc40fc192f21dffcb5
saisi <saisi@users.noreply.github.com>
2017-02-12 19:28:51 +0000

    1486927731 acanthophorous

---
+++ 1486927731.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3308621682
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x21705914L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xC2132456L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0xCBEB9691L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
