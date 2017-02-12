7288fb510dfe66669f0aa351251fb8d8fd2a58ae
saisi <saisi@users.noreply.github.com>
2017-02-12 16:28:36 +0000

    1486916916 Balanidae

---
+++ 1486916916.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3669102619
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static int8_t g_6 = 8L;
+static uint32_t g_7 = 0x451D52AAL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
