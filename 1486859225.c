3664403cdbc1b40fa20155782f48c8eadd5fdbf0
saisi <saisi@users.noreply.github.com>
2017-02-12 00:27:05 +0000

    1486859225 alligations

---
+++ 1486859225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1635597747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 24;
+   unsigned f1 : 15;
+   const signed f2 : 22;
+   unsigned f3 : 9;
+   volatile signed f4 : 22;
+   volatile signed f5 : 23;
+   volatile unsigned f6 : 26;
+};
+
