26312cf91ea01c0d780d4d70231e67a49ae4daa4
saisi <saisi@users.noreply.github.com>
2017-02-12 14:28:26 +0000

    1486909705 Avictor

---
+++ 1486909705.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2154107200
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 14;
+   signed f1 : 11;
+   const unsigned f2 : 1;
+   volatile signed f3 : 9;
+   unsigned f4 : 20;
+};
+
+union U1 {
+   unsigned f0 : 7;
