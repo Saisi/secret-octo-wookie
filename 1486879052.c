96a9fedf5a2481bfc14c034700bdc574fb404757
saisi <saisi@users.noreply.github.com>
2017-02-12 05:57:37 +0000

    1486879052 arterialisation

---
+++ 1486879052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2970344580
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   const int8_t  f1;
+   uint16_t  f2;
+   const uint32_t  f3;
+   const signed f4 : 20;
+};
+
+union U1 {
+   volatile uint64_t  f0;
