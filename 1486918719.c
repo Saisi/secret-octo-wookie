d7e06dc833f4dc872886077a8c3334398c945019
saisi <saisi@users.noreply.github.com>
2017-02-12 16:58:39 +0000

    1486918719 axialities

---
+++ 1486918719.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4092606917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+   signed f2 : 8;
+   uint32_t  f3;
+   uint16_t  f4;
+   uint8_t  f5;
+};
+
+union U1 {
