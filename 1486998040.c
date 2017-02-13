17699884156ad96b6443cf27df823f4a7167c324
saisi <saisi@users.noreply.github.com>
2017-02-13 15:00:41 +0000

    1486998040 autoscience

---
+++ 1486998040.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1614402554
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xCECB4571L;
+static volatile int32_t g_4 = 0xF7FD1E81L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0x33C63384L;
+static int32_t g_7 = 4L;
+static volatile int32_t g_12 = 0L;/* VOLATILE GLOBAL g_12 */
+static uint32_t g_13[3][8] = {{0x0920A6FFL,1UL,18446744073709551615UL,18446744073709551615UL,1UL,0x0920A6FFL,1UL,18446744073709551615UL},{18446744073709551613UL,1UL,18446744073709551613UL,0x0920A6FFL,0x0920A6FFL,18446744073709551613UL,1UL,18446744073709551613UL},{4UL,0x0920A6FFL,18446744073709551615UL,0x0920A6FFL,4UL,4UL,0x0920A6FFL,18446744073709551615UL}};
+static volatile int32_t *g_18 = &g_4;
+static uint8_t g_34 = 0UL;
+static int32_t g_36 = 7L;
