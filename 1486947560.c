fcfb77c1d5fd80a47590f528c78592a340bca8a7
saisi <saisi@users.noreply.github.com>
2017-02-13 00:59:21 +0000

    1486947560 aracanga

---
+++ 1486947560.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1373706511
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   uint64_t  f1;
+   int64_t  f2;
+   uint8_t  f3;
+   uint16_t  f4;
+};
+
+union U1 {
+   int64_t  f0;
