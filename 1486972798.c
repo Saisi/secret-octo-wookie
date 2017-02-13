7eb4d3c500e62e20443c9f739050c4e193bd1031
saisi <saisi@users.noreply.github.com>
2017-02-13 07:59:58 +0000

    1486972798 Averno

---
+++ 1486972798.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4102111454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_13[9][4] = {{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL},{6UL,6UL,6UL,6UL}};
+static int32_t g_42[7][4] = {{(-6L),0x3063D62AL,(-6L),0xABAC89D7L},{1L,0x7F6A191AL,0xABAC89D7L,0xABAC89D7L},{0x3063D62AL,0x3063D62AL,0x3063D62AL,0xABAC89D7L},{0xABAC89D7L,(-6L),0x3063D62AL,(-6L)},{(-4L),0x7F6A191AL,0x6446BD29L,0x3063D62AL},{(-6L),0x7F6A191AL,0x7F6A191AL,(-6L)},{0x7F6A191AL,(-6L),(-4L),0xABAC89D7L}};
+static int32_t * const g_41 = &g_42[1][1];
+static int8_t g_47 = 0x53L;
+static int32_t *g_54 = (void*)0;
+static int32_t g_95[10][2][6] = {{{(-4L),(-4L),(-1L),0x4BCF40E1L,3L,(-1L)},{3L,0xBD07DBC3L,0xB00C388AL,0x4BCF40E1L,0xBD07DBC3L,(-1L)}},{{(-4L),3L,0xB00C388AL,3L,(-4L),(-1L)},{7L,3L,(-1L),7L,0xBD07DBC3L,(-8L)}},{{7L,0xBD07DBC3L,(-8L),3L,3L,(-8L)},{(-4L),(-4L),(-1L),0x4BCF40E1L,3L,(-1L)}},{{3L,0xBD07DBC3L,0xB00C388AL,0x4BCF40E1L,0xBD07DBC3L,(-1L)},{(-4L),3L,0xB00C388AL,3L,(-4L),(-1L)}},{{7L,3L,(-1L),7L,0xBD07DBC3L,(-8L)},{7L,0xBD07DBC3L,(-8L),3L,3L,(-8L)}},{{(-4L),(-4L),(-1L),0x4BCF40E1L,3L,(-1L)},{3L,0xBD07DBC3L,0xB00C388AL,0x4BCF40E1L,0xBD07DBC3L,(-1L)}},{{(-4L),3L,0xB00C388AL,3L,(-4L),(-1L)},{7L,3L,(-1L),7L,0xBD07DBC3L,(-8L)}},{{7L,0xBD07DBC3L,(-8L),3L,3L,(-8L)},{(-4L),(-4L),(-1L),0x4BCF40E1L,3L,(-1L)}},{{3L,0xBD07DBC3L,0xB00C388AL,0x4BCF40E1L,0xBD07DBC3L,(-1L)},{(-4L),3L,0xB00C388AL,3L,(-4L),(-1L)}},{{7L,3L,(-1L),7L,0xBD07DBC3L,(-8L)},{7L,0xBD07DBC3L,(-8L),3L,3L,(-8L)}}};
+static uint64_t g_148 = 1UL;
+static int8_t g_153 = 0xFEL;
+static int32_t * const *g_156 = &g_41;
