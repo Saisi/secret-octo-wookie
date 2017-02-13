e7962f4529316ccbb914935cef39e04e1effd5fd
saisi <saisi@users.noreply.github.com>
2017-02-13 06:29:50 +0000

    1486967390 apostasy

---
+++ 1486967390.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      299476289
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 31;
+   volatile unsigned f1 : 26;
+};
+
+struct S1 {
+   signed f0 : 22;
+   const unsigned f1 : 5;
+   volatile int16_t  f2;
+   volatile unsigned f3 : 6;
