e015ee797f0633bab78a6d7512b359468fa67453
saisi <saisi@users.noreply.github.com>
2017-02-13 04:29:40 +0000

    1486960179 amidoxyl

---
+++ 1486960179.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3068894238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int8_t  f0;
+   signed f1 : 26;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
