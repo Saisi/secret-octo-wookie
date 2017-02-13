046b92854c3ecde7b00c6d4288f006961be7911b
saisi <saisi@users.noreply.github.com>
2017-02-13 18:01:00 +0000

    1487008859 aimer

---
+++ 1487008859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2879312048
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 6;
+   volatile uint8_t  f1;
+   volatile uint32_t  f2;
+   const unsigned f3 : 1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_6 = 0L;
+static int32_t g_43 = 5L;
