fd66463e2f1cfd884cca41a2674694a126434c9f
saisi <saisi@users.noreply.github.com>
2017-02-13 20:01:13 +0000

    1487016072 Arianne

---
+++ 1487016072.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2560900008
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_14 = 0x1AL;
+static int16_t g_23 = 0xB054L;
+static int32_t g_28 = 0xE76373A4L;
+static int32_t *g_27 = &g_28;
+static uint32_t g_41 = 4294967295UL;
+static uint64_t g_54 = 0x9D95921BA52B3395LL;
+static const uint64_t g_76[1] = {4UL};
+static int64_t g_90 = 0x86E6AF5756627F1ALL;
+static int64_t g_97 = 0x6A5C54CD8D27FD73LL;
