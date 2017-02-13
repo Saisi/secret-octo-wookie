593ddbc0b5bcce1d0c6974dc87cdad30c3445655
saisi <saisi@users.noreply.github.com>
2017-02-13 01:29:24 +0000

    1486949363 appetibility

---
+++ 1486949363.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3716227358
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const uint16_t  f1;
+   uint16_t  f2;
+   int64_t  f3;
+   const uint8_t  f4;
+   const uint16_t  f5;
+   const uint64_t  f6;
+   uint64_t  f7;
+   volatile uint8_t  f8;
