3c1fb7fdadb1baf513233b4472086e7de331f93a
saisi <saisi@users.noreply.github.com>
2017-02-13 22:31:28 +0000

    1487025087 Arimaspian

---
+++ 1487025087.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4007146938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x3C692BEAL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xB1CECB45L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 2L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static int32_t g_9 = 0L;
+static int32_t g_46 = 0x982B357FL;
