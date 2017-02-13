1a97f7a2d0b3dc264696f7ecb0d7b9253c367c1a
saisi <saisi@users.noreply.github.com>
2017-02-13 13:00:31 +0000

    1486990831 astrospectroscopic

---
+++ 1486990831.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2758597841
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 7;
+   unsigned f1 : 2;
+   unsigned f2 : 29;
+   volatile signed f3 : 20;
+   signed f4 : 21;
+   unsigned f5 : 26;
+   const volatile signed f6 : 17;
