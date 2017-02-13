54d02f9069196fa2d9852404f92afdc63e336368
saisi <saisi@users.noreply.github.com>
2017-02-13 16:30:51 +0000

    1487003450 bairnteem

---
+++ 1487003450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      280268790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int64_t  f1;
+   signed f2 : 20;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   const signed f1 : 12;
+   uint8_t  f2;
