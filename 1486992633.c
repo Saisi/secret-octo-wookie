e0ca269dbfd5daeaa3fef9e8c44782a0d5d2b4d5
saisi <saisi@users.noreply.github.com>
2017-02-13 13:30:33 +0000

    1486992633 autoimmunization

---
+++ 1486992633.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3528174234
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+};
+
+union U1 {
+   const int8_t  f0;
+   const uint32_t  f1;
+   uint64_t  f2;
+   uint32_t  f3;
+   uint16_t  f4;
