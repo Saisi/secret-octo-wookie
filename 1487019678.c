553cef7325e210970bfdadd83c77c52e4177e091
saisi <saisi@users.noreply.github.com>
2017-02-13 21:01:18 +0000

    1487019678 Armentieres

---
+++ 1487019678.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1304335744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_18 = (-4L);
+static int32_t g_29 = (-1L);
+static volatile uint32_t g_54 = 5UL;/* VOLATILE GLOBAL g_54 */
+static uint16_t g_60[1] = {0x2F52L};
+static int64_t g_66 = 0xFE98D7E942FAFECELL;
+static int64_t g_77 = 1L;
+static int8_t g_80 = 0xF0L;
+static int8_t g_81 = 0x1CL;
+static volatile int8_t g_82 = 0x58L;/* VOLATILE GLOBAL g_82 */
