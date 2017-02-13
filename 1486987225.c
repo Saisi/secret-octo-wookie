9fe080454f5d2489287c9beee1706c1dc2d3a12c
saisi <saisi@users.noreply.github.com>
2017-02-13 12:00:26 +0000

    1486987225 autopista

---
+++ 1486987225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      390119238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 17;
+   uint16_t  f1;
+   uint8_t  f2;
+   uint32_t  f3;
+   volatile uint16_t  f4;
+   uint8_t  f5;
+   const uint64_t  f6;
+   int32_t  f7;
+   volatile int32_t  f8;
