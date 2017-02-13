061a5df28cd17d5ec69909f88a5e8a71f9a2699a
saisi <saisi@users.noreply.github.com>
2017-02-13 23:01:31 +0000

    1487026890 aspheterism

---
+++ 1487026890.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2366038233
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-10L);
+static int32_t g_16 = 0x45FE46C2L;
+static int8_t g_39 = 0xBFL;
+static const int8_t g_48 = (-1L);
+static volatile int8_t g_72 = 1L;/* VOLATILE GLOBAL g_72 */
+static volatile int8_t *g_71 = &g_72;
+static volatile int8_t **g_70 = &g_71;
+static uint32_t g_84 = 0x75688AFCL;
+static int32_t g_90 = 0xFAAB93B5L;
