c3df87a38ab2986e6edc55642de61103c953f783
saisi <saisi@users.noreply.github.com>
2017-02-13 00:29:18 +0000

    1486945757 alluvions

---
+++ 1486945757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      433487241
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_7 = 0x386CCE57L;
+static int16_t g_15 = 4L;
+static int16_t g_34[5][6][4] = {{{0L,3L,0x509CL,0xB84CL},{3L,(-7L),0L,(-1L)},{0x28ABL,0x6702L,0L,0x6702L},{0x718BL,0x3521L,0xB84CL,(-1L)},{0x2628L,0x5088L,0x9A27L,1L},{0L,7L,(-1L),0L}},{{0L,0x509CL,0x9A27L,3L},{0x2628L,0L,0xB84CL,0x28ABL},{0x718BL,0L,0L,0x718BL},{0x28ABL,0xB84CL,0L,0x2628L},{3L,0x9A27L,0x509CL,0L},{0L,(-1L),0x3521L,0x7D6BL}},{{0L,3L,(-7L),0L},{0x9A27L,0L,(-1L),0xC7A8L},{0L,0x7D6BL,0L,0x5088L},{0x6702L,0x28ABL,7L,0x2628L},{0L,(-1L),0x2628L,0x28ABL},{(-7L),0x3521L,0x2628L,0L}},{{0L,(-7L),7L,0x9A27L},{0x6702L,(-1L),0L,0L},{0L,0L,(-1L),0x6702L},{0x9A27L,7L,(-7L),0L},{0L,0x2628L,0x3521L,(-7L)},{0x28ABL,0x2628L,(-1L),0L}},{{0x2628L,7L,0x28ABL,0x6702L},{0x5088L,0L,0x7D6BL,0L},{0xC7A8L,(-1L),0L,0x9A27L},{0L,(-7L),3L,0L},{0x7D6BL,0x3521L,1L,0x28ABL},{0x7D6BL,(-1L),3L,0x2628L}}};
+static int16_t *g_33 = &g_34[1][0][1];
+static int64_t g_47 = 4L;
+static int32_t g_49[9][3] = {{0x83560277L,0xD469F41AL,1L},{0x83560277L,(-1L),1L},{0x83560277L,0x832A49E8L,0x83560277L},{0x83560277L,0xD469F41AL,1L},{0x83560277L,(-1L),1L},{0x83560277L,0x832A49E8L,0x83560277L},{0x83560277L,0xD469F41AL,1L},{0x83560277L,(-1L),1L},{0x83560277L,0x832A49E8L,0x83560277L}};
+static int32_t *g_93 = &g_49[8][0];
+static int32_t ** volatile g_92[8][3][1] = {{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}},{{(void*)0},{&g_93},{&g_93}}};
+static int32_t ** volatile g_95 = &g_93;/* VOLATILE GLOBAL g_95 */
