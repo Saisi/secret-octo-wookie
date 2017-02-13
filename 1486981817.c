89942cf1108f99cf143290034cb653913f352a94
saisi <saisi@users.noreply.github.com>
2017-02-13 10:30:18 +0000

    1486981817 andiron

---
+++ 1486981817.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      517029370
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 11;
+   unsigned f1 : 20;
+   unsigned f2 : 6;
+   signed f3 : 26;
+};
+
+struct S1 {
+   uint32_t  f0;
+   int64_t  f1;
