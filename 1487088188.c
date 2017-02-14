f61edf22ebb66ffe205397609865ed4e9f380f1d
saisi <saisi@users.noreply.github.com>
2017-02-14 16:03:08 +0000

    1487088188 abacus

---
+++ 1487088188.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1721036755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5] = {0xFDF99FF6L,0xFDF99FF6L,0xFDF99FF6L,0xFDF99FF6L,0xFDF99FF6L};
+static int32_t g_6 = (-1L);
+static int32_t g_8[6] = {0x1A0734F0L,0x1A0734F0L,0x1A0734F0L,0x1A0734F0L,0x1A0734F0L,0x1A0734F0L};
+static int32_t * volatile g_7 = &g_8[3];/* VOLATILE GLOBAL g_7 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
