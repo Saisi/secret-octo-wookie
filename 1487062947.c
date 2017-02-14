5b65cc55ca1bb3551ab223d3a4a91a497cbd87a5
saisi <saisi@users.noreply.github.com>
2017-02-14 09:02:27 +0000

    1487062947 atmosphereful

---
+++ 1487062947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2199875898
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   unsigned f1 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_21 = 0L;
+static int32_t * volatile g_20 = &g_21;/* VOLATILE GLOBAL g_20 */
+static int32_t g_57 = 0L;
+static uint8_t g_59 = 0xE8L;
