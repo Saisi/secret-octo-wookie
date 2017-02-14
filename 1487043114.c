d9f412c20bd5d2551dce83604355ea13f0b5f4f7
saisi <saisi@users.noreply.github.com>
2017-02-14 03:31:54 +0000

    1487043114 amentias

---
+++ 1487043114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      563676087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x885C6F1BL;
+static int16_t g_24 = 0xAD9DL;
+static int8_t g_47[2][8] = {{0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL},{0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL}};
+static const int32_t g_54 = 0x4177FD8BL;
+static const int32_t *g_53 = &g_54;
+static const int32_t **g_52 = &g_53;
+static int64_t g_91 = (-5L);
+static int64_t g_98 = 0xB74084DAEA480D8ALL;
+static int32_t **g_100 = (void*)0;
