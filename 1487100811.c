98ca19266550a69129c10a158d27c63670ac31a5
saisi <saisi@users.noreply.github.com>
2017-02-14 19:33:31 +0000

    1487100811 bafflement

---
+++ 1487100811.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3739181029
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 0x4BF90EDCL;
+static int32_t g_54 = (-4L);
+static int16_t g_60 = 0x85DCL;
+static uint8_t g_64 = 2UL;
+static int16_t g_66 = 0x8A21L;
+static int64_t g_83 = 0xA6EDB5B292197989LL;
+static int64_t *g_82[7] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static const uint8_t g_98[9][10] = {{0xABL,7UL,1UL,1UL,1UL,1UL,1UL,7UL,0xABL,248UL},{0xABL,7UL,1UL,1UL,1UL,1UL,1UL,7UL,0xABL,248UL},{0xABL,7UL,1UL,1UL,1UL,1UL,1UL,7UL,0xABL,248UL},{0xABL,7UL,1UL,1UL,1UL,1UL,1UL,7UL,0xABL,0x1CL},{0UL,1UL,0xABL,0x9FL,1UL,0x9FL,0xABL,1UL,0UL,0x1CL},{0UL,1UL,0xABL,0x9FL,1UL,0x9FL,0xABL,1UL,0UL,0x1CL},{0UL,1UL,0xABL,0x9FL,1UL,0x9FL,0xABL,1UL,0UL,0x1CL},{0UL,1UL,0xABL,0x9FL,1UL,0x9FL,0xABL,1UL,0UL,0x1CL},{0UL,1UL,0xABL,0x9FL,1UL,0x9FL,0xABL,1UL,0UL,0x1CL}};
+static int32_t g_101 = 0x1D611CF5L;
