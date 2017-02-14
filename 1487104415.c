154ed62bd677f7f149d5a02f8031dcbd9c7370f4
saisi <saisi@users.noreply.github.com>
2017-02-14 20:33:36 +0000

    1487104415 algazel

---
+++ 1487104415.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      280902656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6] = {1L,0xBCA63185L,1L,1L,0xBCA63185L,1L};
+static volatile int32_t g_3 = (-2L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[1] = {0x265AB3E9L};
+static int32_t g_50 = (-5L);
+static uint64_t g_57 = 0x048C84060A3B7DC4LL;
+static int32_t *g_60 = &g_5[0];
+static int32_t * volatile *g_59 = &g_60;
+static uint16_t g_62 = 65529UL;
