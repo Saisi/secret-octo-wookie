a9f75fd953a347f12cfe325e350b633e896e708a
saisi <saisi@users.noreply.github.com>
2017-02-14 10:32:37 +0000

    1487068354 alarms

---
+++ 1487068354.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1630502657
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 31;
+   volatile unsigned f1 : 26;
+   unsigned f2 : 21;
+   volatile signed f3 : 12;
+   const unsigned f4 : 13;
+   volatile signed f5 : 18;
+   unsigned f6 : 9;
+   unsigned f7 : 24;
+};
