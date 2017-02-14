0f7a5b9caa2e83573c6534eecadc622606cb5bbf
saisi <saisi@users.noreply.github.com>
2017-02-14 19:03:29 +0000

    1487099008 apostacy

---
+++ 1487099008.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1942532211
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int8_t * volatile  f1;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x7A876E6BL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[10][2] = {{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L},{0x444696A4L,0x444696A4L}};
