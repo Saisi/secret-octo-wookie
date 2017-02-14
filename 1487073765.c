69d68b614834a13f6f504d06e7dc4e24f1fd90d8
saisi <saisi@users.noreply.github.com>
2017-02-14 12:02:46 +0000

    1487073765 Avicenna

---
+++ 1487073765.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3286968051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 24;
+   signed f1 : 15;
+   signed f2 : 22;
+   unsigned f3 : 9;
+   unsigned f4 : 22;
+   unsigned f5 : 23;
+   signed f6 : 26;
+};
+
