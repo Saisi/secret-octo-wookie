808378c7cdde9efd76f757775fb52b5be59e0d75
saisi <saisi@users.noreply.github.com>
2017-02-14 00:01:36 +0000

    1487030495 agrobiologically

---
+++ 1487030495.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4250743304
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 18;
+   volatile unsigned f1 : 31;
+   const volatile unsigned f2 : 5;
+   volatile signed f3 : 29;
+   volatile unsigned f4 : 24;
+   uint32_t  f5;
+   volatile unsigned f6 : 27;
+   const signed f7 : 8;
+   signed f8 : 25;
