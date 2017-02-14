90a3d6c7edd0c165d92306a18767f6dfcf96f921
saisi <saisi@users.noreply.github.com>
2017-02-14 14:02:56 +0000

    1487080975 authority

---
+++ 1487080975.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3760999221
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int8_t  f1;
+   uint64_t  f2;
+   int8_t  f3;
+   int32_t  f4;
+   unsigned : 0;
+};
+
+union U1 {
