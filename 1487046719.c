53e0ff84e00689929c8d14238b6ee16118fcad5a
saisi <saisi@users.noreply.github.com>
2017-02-14 04:32:00 +0000

    1487046719 ajava

---
+++ 1487046719.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2577726854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   int32_t  f1;
+   signed f2 : 8;
+   signed f3 : 1;
+};
+
+union U1 {
+   int32_t  f0;
+   const unsigned f1 : 30;
