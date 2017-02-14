f65a6d65baad720f14b80a956c7db81a47bfbcf3
saisi <saisi@users.noreply.github.com>
2017-02-14 03:01:52 +0000

    1487041311 asterin

---
+++ 1487041311.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3037939332
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 29;
+   unsigned f2 : 29;
+   unsigned f3 : 11;
+   signed f4 : 4;
+   const volatile signed : 0;
+   uint8_t  f5;
