5178e9241ee5b54b1137e579b6919ec7b67f1ff1
saisi <saisi@users.noreply.github.com>
2017-02-14 21:33:42 +0000

    1487108021 actory

---
+++ 1487108021.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3991127212
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 4;
+   const volatile unsigned f1 : 9;
+   unsigned f2 : 7;
+   signed f3 : 23;
+   volatile unsigned f4 : 10;
+   signed f5 : 25;
+   const volatile uint16_t  f6;
