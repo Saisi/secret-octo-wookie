607975fa4e2c8dfa38cdf3aeefc818aee7f09099
saisi <saisi@users.noreply.github.com>
2017-02-14 14:32:58 +0000

    1487082778 Arkwright

---
+++ 1487082778.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      673373394
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int32_t g_6[2][7] = {{0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L},{0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L,0x4E677B56L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
