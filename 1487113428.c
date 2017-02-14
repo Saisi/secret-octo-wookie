ce3614e4bfa067e485c2f1a5e143f850994eabd2
saisi <saisi@users.noreply.github.com>
2017-02-14 23:03:48 +0000

    1487113428 archpiece

---
+++ 1487113428.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1657753179
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_2 = 4294967293UL;
+static int32_t g_4 = 0xA76AABECL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
