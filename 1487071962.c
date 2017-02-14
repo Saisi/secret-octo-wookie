f5b86a0435e2ad53784c09d35ee3836a29c5a3e4
saisi <saisi@users.noreply.github.com>
2017-02-14 11:32:43 +0000

    1487071962 auxilium

---
+++ 1487071962.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2268855059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 4L;
+static int32_t *g_4 = &g_5;
+static uint8_t g_23 = 0xE4L;
+static struct S0 g_34 = {-8};
+static int32_t g_52 = 1L;
