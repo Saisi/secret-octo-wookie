165e4f5b91340c6f2a65dd02f786092c5762d910
saisi <saisi@users.noreply.github.com>
2017-02-14 04:01:57 +0000

    1487044916 anabaenas

---
+++ 1487044916.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1782275014
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint16_t  f0;
+   unsigned f1 : 2;
+   volatile unsigned f2 : 28;
+   signed f3 : 24;
+   const volatile unsigned f4 : 7;
+   signed f5 : 2;
+   signed f6 : 24;
