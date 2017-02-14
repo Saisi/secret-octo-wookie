c96656a803ed9025269dec2775161a2cc8497c09
saisi <saisi@users.noreply.github.com>
2017-02-14 13:02:51 +0000

    1487077371 alkyds

---
+++ 1487077371.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2489621548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static uint8_t g_22[10][3][2] = {{{0x6FL,0x70L},{0UL,0x6FL},{0x65L,6UL}},{{0x65L,0x6FL},{0UL,0x70L},{0x6FL,255UL}},{{0xBDL,1UL},{0x70L,0x71L},{0x71L,0x71L}},{{0x70L,1UL},{0xBDL,255UL},{0x6FL,0x70L}},{{0UL,0x6FL},{0x65L,6UL},{0x65L,0x6FL}},{{0UL,0x70L},{0x6FL,255UL},{0xBDL,1UL}},{{0x70L,0x71L},{0x71L,0x71L},{0x70L,1UL}},{{0xBDL,255UL},{0x6FL,0x70L},{0xBDL,6UL}},{{1UL,248UL},{1UL,6UL},{0xBDL,0x71L}},{{6UL,0UL},{0x65L,0x70L},{0x71L,0x1FL}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+static int16_t  func_7(uint16_t  p_8, int32_t  p_9, int64_t  p_10);
+
+
