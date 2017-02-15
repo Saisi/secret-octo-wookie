df102f63fc9db4bf49f8bca6a257b56992807250
saisi <saisi@users.noreply.github.com>
2017-02-15 08:34:43 +0000

    1487147683 alulet

---
+++ 1487147683.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3078486974
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_3 = 0xEB905D09L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_11 = 0x13244D51L;
+static int32_t g_15 = 6L;
+static int32_t g_17 = 0x6D6DBC1CL;
+static uint8_t g_18 = 3UL;
+static const int32_t *g_34 = &g_15;
+static const int32_t **g_33 = &g_34;
+static int32_t g_60[5] = {1L,1L,1L,1L,1L};
+static uint64_t g_69 = 0xE6007B4AD293D0AELL;
