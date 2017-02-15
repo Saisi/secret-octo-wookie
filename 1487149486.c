f5fd4c2e68992446d96afa6a56a006484f8f7f35
saisi <saisi@users.noreply.github.com>
2017-02-15 09:04:47 +0000

    1487149486 asinegoes

---
+++ 1487149486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4072022571
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   const int32_t  f1;
+   const unsigned f2 : 11;
+   signed f3 : 3;
+   const unsigned f4 : 30;
+};
+
+union U1 {
+   uint32_t  f0;
