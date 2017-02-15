dbaed662a1940992a08b579e15dc14c1e244b417
saisi <saisi@users.noreply.github.com>
2017-02-15 04:34:21 +0000

    1487133261 Alaria

---
+++ 1487133261.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3426543708
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 8;
+   signed f1 : 22;
+   const volatile unsigned f2 : 26;
+   signed : 0;
+   unsigned f3 : 23;
+   int8_t  f4;
+   const signed f5 : 8;
