211c14e8f0b8f0e851de00de1953eedc9e82bbb8
saisi <saisi@users.noreply.github.com>
2017-02-15 19:05:43 +0000

    1487185541 antiperistatical

---
+++ 1487185541.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3086940624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 22;
+   const uint32_t  f1;
+   unsigned f2 : 11;
+   signed f3 : 1;
+   int32_t  f4;
+   const signed f5 : 19;
+   volatile unsigned f6 : 12;
+   const volatile signed f7 : 19;
+   volatile signed f8 : 14;
