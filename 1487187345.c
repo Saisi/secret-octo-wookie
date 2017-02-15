78d15178e6ccf4b91151857ce828fd9a5d655732
saisi <saisi@users.noreply.github.com>
2017-02-15 19:35:46 +0000

    1487187345 armets

---
+++ 1487187345.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1548103557
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * volatile  f0;
+   int8_t * f1;
+   const volatile int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_10 = 1L;/* VOLATILE GLOBAL g_10 */
+static int8_t g_28 = 2L;
+static int32_t g_78[1][9] = {{0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL,0xAE38EDFCL}};
