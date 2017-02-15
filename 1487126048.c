9cd4e30d9cc6926e950e17b266af3d19ab093df8
saisi <saisi@users.noreply.github.com>
2017-02-15 02:34:09 +0000

    1487126048 acromyodian

---
+++ 1487126048.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1752989107
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xFF606579L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 8L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 5L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xBACD7953L;
+static int8_t g_36 = 0x3CL;
+static int32_t g_44 = 0xDE9E5A93L;
+static uint64_t g_60 = 0UL;
+static const uint32_t g_65 = 0xCB11B199L;
