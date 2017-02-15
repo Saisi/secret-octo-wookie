c32600cc0b8b51491cffc3d205304f63875f9050
saisi <saisi@users.noreply.github.com>
2017-02-15 21:05:53 +0000

    1487192753 Altair

---
+++ 1487192753.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2117520902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_3[10] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static uint32_t g_5[1] = {4294967288UL};
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7[3] = {0xBF38C088L,0xBF38C088L,0xBF38C088L};
+static volatile int32_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9 = (-1L);/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x95768819L;
+static int32_t *g_37[10] = {&g_10,&g_10,&g_10,&g_10,&g_10,&g_10,&g_10,&g_10,&g_10,&g_10};
+static int32_t **g_36[7][5] = {{&g_37[1],&g_37[1],(void*)0,&g_37[1],(void*)0},{&g_37[1],&g_37[1],(void*)0,(void*)0,(void*)0},{&g_37[1],&g_37[1],&g_37[1],(void*)0,(void*)0},{&g_37[1],&g_37[1],(void*)0,&g_37[1],(void*)0},{&g_37[1],&g_37[1],(void*)0,&g_37[1],(void*)0},{(void*)0,(void*)0,&g_37[4],&g_37[1],&g_37[1]},{&g_37[1],(void*)0,(void*)0,(void*)0,&g_37[1]}};
