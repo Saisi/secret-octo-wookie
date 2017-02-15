b42105c2ea89d5b8292f28ebd214f7b31047cd65
saisi <saisi@users.noreply.github.com>
2017-02-15 09:34:50 +0000

    1487151289 Airel

---
+++ 1487151289.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4083120960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_15 = 9UL;/* VOLATILE GLOBAL g_15 */
+static uint32_t g_20 = 0xC3414B58L;
+static int16_t g_56 = (-10L);
+static int32_t g_58 = 0xD37F4A1DL;
+static int32_t *g_57 = &g_58;
+static int8_t g_64[4] = {0x63L,0x63L,0x63L,0x63L};
+static int8_t g_66 = (-5L);
+static uint64_t g_82 = 0xDFE98D7E942FAFECLL;
+static int16_t g_87 = 0xBFCEL;
