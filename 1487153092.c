243617f4960d56a3692f537cc2ebe9a70c74778d
saisi <saisi@users.noreply.github.com>
2017-02-15 10:04:52 +0000

    1487153092 antiemetic

---
+++ 1487153092.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      835725348
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 0x58BAC04EL;
+static int32_t g_23 = 0xA8B35EDAL;
+static int64_t g_35 = (-4L);
+static uint32_t g_44 = 0xF419C2B2L;
+static int32_t g_57 = 0L;
+static uint32_t g_83 = 18446744073709551615UL;
+static int32_t g_84 = 0xB52F506AL;
+static int32_t g_105 = (-1L);
+static int8_t g_112 = 0xDFL;
