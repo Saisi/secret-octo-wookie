34d8dfb64f73f174f11739f58ac5a71c3c7091ce
saisi <saisi@users.noreply.github.com>
2017-02-15 18:05:37 +0000

    1487181936 aphototropic

---
+++ 1487181936.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1208133052
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   uint32_t  f1;
+   uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_10[3] = {0x653D42374B1B055CLL,0x653D42374B1B055CLL,0x653D42374B1B055CLL};
+static uint8_t g_24 = 0xFEL;
+static int32_t g_51 = (-1L);
