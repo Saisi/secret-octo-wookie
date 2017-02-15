bd9235e173b1cde8441f88abf0848fdae1759c10
saisi <saisi@users.noreply.github.com>
2017-02-15 06:34:34 +0000

    1487140473 applenut

---
+++ 1487140473.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3227249663
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+};
+
+struct S1 {
+   volatile int32_t  f0;
+   const signed f1 : 19;
+   volatile unsigned f2 : 25;
+   volatile unsigned f3 : 24;
+   volatile signed f4 : 28;
