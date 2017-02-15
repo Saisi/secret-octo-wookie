c6f8a3f5d09b531338d87303c759ceb7bb296715
saisi <saisi@users.noreply.github.com>
2017-02-15 15:35:23 +0000

    1487172923 avenary

---
+++ 1487172923.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1542309766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x01897A4AL;
+static int32_t g_6[9] = {0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L,0xCEFA9D19L};
+static volatile struct S0 g_24 = {0xE5AB06C0L,255UL};/* VOLATILE GLOBAL g_24 */
+static volatile struct S0 * const  volatile g_25 = &g_24;/* VOLATILE GLOBAL g_25 */
