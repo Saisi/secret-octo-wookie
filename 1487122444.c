a73f747183c81cce0785fad510ec7e4d363e8104
saisi <saisi@users.noreply.github.com>
2017-02-15 01:34:04 +0000

    1487122444 angiomyosarcoma

---
+++ 1487122444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      55046000
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 1L;
+static volatile int8_t g_25 = 0xBDL;/* VOLATILE GLOBAL g_25 */
+static volatile int8_t *g_24 = &g_25;
+static int32_t g_35 = 0x209C3997L;
+static uint16_t g_42 = 65535UL;
+static int8_t g_52 = 0xDBL;
+static int32_t * volatile *g_111 = (void*)0;
+static volatile int32_t g_113 = 0x127326B3L;/* VOLATILE GLOBAL g_113 */
+static volatile int32_t * const g_112 = &g_113;
