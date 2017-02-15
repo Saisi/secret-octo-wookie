fad1c036163ad5889fec9feb3e417221bcd47164
saisi <saisi@users.noreply.github.com>
2017-02-15 16:05:25 +0000

    1487174725 archdean

---
+++ 1487174725.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1965006734
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_4 = 0x023ED15DE05DAE5ALL;
+static uint16_t g_5 = 65535UL;
+static uint64_t g_14 = 0xF4A8FE24307A329ELL;
+static int16_t g_16 = 0x26A7L;
+static uint16_t g_19[10] = {0xF126L,0xF126L,0xF126L,0xF126L,0xF126L,0xF126L,0xF126L,0xF126L,0xF126L,0xF126L};
+static uint32_t g_21 = 4294967288UL;
+static int32_t * volatile g_25 = (void*)0;/* VOLATILE GLOBAL g_25 */
+static int32_t * const  volatile g_26 = (void*)0;/* VOLATILE GLOBAL g_26 */
+static int32_t * const  volatile g_27 = (void*)0;/* VOLATILE GLOBAL g_27 */
