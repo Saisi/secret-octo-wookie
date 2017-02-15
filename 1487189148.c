6a477d9bc2034b1227c2b5e67dcf17e17c0eaa09
saisi <saisi@users.noreply.github.com>
2017-02-15 20:05:48 +0000

    1487189148 appendicectomies

---
+++ 1487189148.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      918596898
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_2 = 1L;
+static volatile int32_t g_14 = 1L;/* VOLATILE GLOBAL g_14 */
+static int32_t g_15[6] = {0x97E780CAL,0x97E780CAL,0x97E780CAL,0x97E780CAL,0x97E780CAL,0x97E780CAL};
+static int32_t g_19 = 0x309642E4L;
+static uint8_t g_78 = 255UL;
+static int32_t g_82 = (-1L);
+static int32_t g_100 = 0x0C4F586FL;
+static int64_t g_101 = 0x8D320CD53B18A503LL;
+static volatile int32_t g_102 = 0xABB1EF36L;/* VOLATILE GLOBAL g_102 */
