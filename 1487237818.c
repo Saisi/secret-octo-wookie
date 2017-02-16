28b3026935e9b341ced4cc857a4dca39f2ad2575
saisi <saisi@users.noreply.github.com>
2017-02-16 09:36:58 +0000

    1487237818 apesthetic

---
+++ 1487237818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1882442843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 12;
+   volatile signed f1 : 19;
+   unsigned f2 : 10;
+   signed f3 : 13;
+   signed f4 : 10;
+   const unsigned f5 : 27;
+   signed f6 : 14;
