29518cdc61ac1fde15f83ced622d0048b9253e87
saisi <saisi@users.noreply.github.com>
2017-02-16 06:06:41 +0000

    1487225201 agaroid

---
+++ 1487225201.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      635154659
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 16;
+   unsigned f1 : 7;
+   signed f2 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 1UL;
+static uint8_t g_7 = 0xCEL;
+static int16_t g_11[8][6][4] = {{{0x7E59L,0x2A96L,0x09A2L,0x4B08L},{0xA31EL,1L,3L,0x0054L},{(-8L),2L,2L,(-8L)},{(-1L),1L,0x2C57L,1L},{(-1L),2L,(-10L),0x3F9BL},{0xE8F0L,(-8L),0L,0x3F9BL}},{{1L,2L,(-4L),1L},{1L,1L,0x2A96L,(-8L)},{3L,2L,9L,0x0054L},{4L,1L,2L,0x4B08L},{9L,0x2A96L,(-10L),0x09A2L},{1L,2L,0xBA56L,0x09A2L}},{{0x2C57L,(-8L),0x6890L,2L},{0xF0BBL,0x0250L,(-10L),0x7E59L},{0x7E59L,0L,2L,0x8E22L},{0x7E59L,0x4B08L,(-10L),0xF9E6L},{0xF0BBL,0x8E22L,0x6890L,(-4L)},{0x2C57L,0x79B3L,0xBA56L,1L}},{{(-10L),2L,0x7E59L,1L},{(-8L),2L,0xA31EL,0x9CA3L},{0xBA56L,(-1L),(-8L),(-8L)},{4L,4L,(-1L),0L},{2L,0xF9E6L,(-1L),(-10L)},{0x0250L,1L,0xE8F0L,(-1L)}},{{1L,1L,1L,(-10L)},{1L,0xF9E6L,1L,0L},{2L,4L,3L,(-8L)},{0x2A96L,(-1L),4L,0x9CA3L},{0x4B08L,2L,9L,1L},{0x6890L,2L,1L,1L}},{{0x8E22L,0x79B3L,0x8E22L,(-4L)},{0x231FL,0x8E22L,0x3F9BL,0xF9E6L},{1L,0x4B08L,(-10L),0x8E22L},{0xA31EL,0L,(-10L),0x7E59L},{1L,0x0250L,0x3F9BL,2L},{0x231FL,(-8L),0x8E22L,0x09A2L}},{{0x8E22L,0x09A2L,1L,9L},{0x6890L,(-1L),9L,(-10L)},{0x4B08L,1L,4L,0x3F9BL},{0x2A96L,3L,3L,0x2A96L},{2L,(-10L),1L,0x79B3L},{1L,0xA31EL,1L,0xF0BBL}},{{1L,0x2A96L,0xE8F0L,0xF0BBL},{0x0250L,0xA31EL,(-1L),0x79B3L},{2L,(-10L),(-1L),0x2A96L},{4L,3L,(-8L),0x3F9BL},{0xBA56L,1L,0xA31EL,(-10L)},{(-8L),(-1L),0x7E59L,9L}}};
