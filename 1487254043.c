9afc18c158fa794c37f037d7609057ebc01141b4
saisi <saisi@users.noreply.github.com>
2017-02-16 14:07:24 +0000

    1487254043 assumingness

---
+++ 1487254043.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3671170763
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 4;
+   const volatile int16_t  f1;
+   uint32_t  f2;
+   const volatile uint8_t  f3;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_2[3][5][4] = {{{0L,1L,0L,0xD6L},{0x20L,1L,0L,(-1L)},{1L,0x10L,0x10L,1L},{0L,(-1L),0x10L,0xD6L},{1L,0x20L,0L,0x20L}},{{0x20L,0x10L,0L,0x20L},{0L,0x20L,0xD6L,0xD6L},{(-1L),(-1L),0L,1L},{(-1L),0x10L,0xD6L,(-1L)},{0L,1L,0L,0xD6L}},{{0x20L,1L,0L,(-1L)},{1L,0x10L,0L,0x10L},{(-1L),0L,0L,0x3DL},{0x10L,0xD6L,(-1L),0xD6L},{0xD6L,0L,(-1L),0xD6L}}};
