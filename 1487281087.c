7341041b19a3bb32ed4a3bfb60a4c6652762cc32
saisi <saisi@users.noreply.github.com>
2017-02-16 21:38:08 +0000

    1487281087 affying

---
+++ 1487281087.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4154512744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 2;
+   unsigned f1 : 15;
+   const unsigned f2 : 21;
+   signed f3 : 13;
+   volatile signed f4 : 8;
+   const volatile signed f5 : 31;
+   signed f6 : 1;
