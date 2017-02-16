3d19e23d02ef997220da12e90b2601765d5ebb07
saisi <saisi@users.noreply.github.com>
2017-02-16 10:07:01 +0000

    1487239621 afflict

---
+++ 1487239621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3447479121
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int8_t g_7 = 0x76L;
+static int32_t g_8 = (-6L);
+static int32_t g_34 = 3L;
+static int32_t g_73 = 0x26FBC79AL;
+static volatile int32_t g_76[4] = {0x0F3ADCFEL,0x0F3ADCFEL,0x0F3ADCFEL,0x0F3ADCFEL};
+static volatile int32_t g_77 = 0L;/* VOLATILE GLOBAL g_77 */
+static volatile int32_t g_78 = 0L;/* VOLATILE GLOBAL g_78 */
+static int32_t g_79 = 0x44FA7555L;
