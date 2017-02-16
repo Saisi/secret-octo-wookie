f50e63101c46c7294a29a2d4e7c320c50fe44f8f
saisi <saisi@users.noreply.github.com>
2017-02-16 00:36:13 +0000

    1487205372 badges

---
+++ 1487205372.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      372760200
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xCFF0EA75L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xDD1E1E87L;
+static int32_t g_7 = 0L;
+static uint32_t g_14 = 4294967288UL;
+static int32_t *g_43 = (void*)0;
+static int32_t ** const g_42 = &g_43;
+static int32_t g_46[6] = {0L,0L,0L,0L,0L,0L};
+static const int32_t *g_48 = &g_7;
+static const int32_t **g_47 = &g_48;
