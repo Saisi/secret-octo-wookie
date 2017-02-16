fda3be3f7a438c54ccad53e5516dcfff8df929e4
saisi <saisi@users.noreply.github.com>
2017-02-16 15:07:30 +0000

    1487257650 abhor

---
+++ 1487257650.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      810447875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[5][8] = {{0xC0D37AD4L,(-1L),(-1L),0x363F9E92L,(-1L),(-1L),0xC0D37AD4L,(-3L)},{(-1L),(-1L),0xC0D37AD4L,(-3L),(-2L),(-2L),(-3L),0xC0D37AD4L},{0L,0L,(-1L),0xE64F1479L,(-2L),0x363F9E92L,(-7L),0x363F9E92L},{(-1L),0xC0D37AD4L,0xE64F1479L,0xC0D37AD4L,(-1L),(-1L),0L,0x363F9E92L},{0xC0D37AD4L,(-2L),(-7L),0xE64F1479L,0xE64F1479L,(-7L),(-2L),0xC0D37AD4L}};
+static int32_t g_7 = 0x4D63A88EL;
+static uint8_t g_92 = 0x80L;
+static uint8_t *g_99 = &g_92;
