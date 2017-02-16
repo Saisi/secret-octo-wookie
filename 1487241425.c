840c7d0b704f7e0beb8f7f5ee2d96641c769801b
saisi <saisi@users.noreply.github.com>
2017-02-16 10:37:05 +0000

    1487241425 apathia

---
+++ 1487241425.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3370398366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 29;
+   const signed f1 : 30;
+   unsigned f2 : 8;
+   volatile unsigned f3 : 20;
+};
+
+struct S1 {
+   volatile signed f0 : 13;
+   const int32_t  f1;
