f8d9822645415e83e23a2b66d355ebeee2dbb97b
saisi <saisi@users.noreply.github.com>
2017-02-16 18:37:51 +0000

    1487270269 alcaid

---
+++ 1487270269.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      289085247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t  f1;
+   const signed f2 : 12;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   const int32_t  f1;
+   const volatile int16_t  f2;
