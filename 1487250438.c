c487a9db6d00215f87bdfe4f1d856f73b9c04c44
saisi <saisi@users.noreply.github.com>
2017-02-16 13:07:18 +0000

    1487250438 antidiuretic

---
+++ 1487250438.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1667847558
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 17;
+   signed f1 : 2;
+   signed f2 : 28;
+   const volatile signed f3 : 24;
+   volatile signed f4 : 7;
+   signed f5 : 2;
+   const signed f6 : 24;
+   signed f7 : 13;
+   uint32_t  f8;
