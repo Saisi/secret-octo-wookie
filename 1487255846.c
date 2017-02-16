fc120db69dbea5a3108c3141950c4afe30c40f2f
saisi <saisi@users.noreply.github.com>
2017-02-16 14:37:27 +0000

    1487255846 anticovenanting

---
+++ 1487255846.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1866965654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 5L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-8L);
+static uint64_t g_64 = 0x781CDFFA870C387CLL;
+static int32_t g_68 = 0xEE8B7203L;
+static volatile uint64_t g_82 = 18446744073709551611UL;/* VOLATILE GLOBAL g_82 */
