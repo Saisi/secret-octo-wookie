dfb4f9ed0d4dd442c001a1452a4886b9bfbde5a9
saisi <saisi@users.noreply.github.com>
2017-02-16 19:07:53 +0000

    1487272073 arachnidism

---
+++ 1487272073.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3702587530
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   const unsigned f1 : 28;
+   unsigned f2 : 14;
+   unsigned f3 : 2;
+   signed f4 : 9;
+   const volatile signed f5 : 28;
+   const volatile signed f6 : 10;
+   volatile unsigned f7 : 23;
+   volatile unsigned f8 : 31;
