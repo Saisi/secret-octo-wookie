ad5a4fe5ff2954add92c7b5b3fb1553150869ab5
saisi <saisi@users.noreply.github.com>
2017-02-16 04:36:34 +0000

    1487219793 animates

---
+++ 1487219793.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1741896210
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 23;
+   volatile unsigned f1 : 16;
+   volatile unsigned f2 : 18;
+   const uint8_t  f3;
+   volatile signed f4 : 23;
+   signed f5 : 7;
+};
