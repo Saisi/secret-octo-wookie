bbe24313ce198aa499ae8f73ef6ee35798f5c1f7
saisi <saisi@users.noreply.github.com>
2017-02-16 20:38:01 +0000

    1487277481 autonegation

---
+++ 1487277481.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2843900667
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int8_t * const  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_4 = (-9L);
+static volatile uint8_t g_5[10][4] = {{0UL,0x35L,252UL,251UL},{251UL,0x35L,0x35L,251UL},{0UL,0x35L,252UL,251UL},{251UL,0x35L,0x35L,251UL},{0UL,0x35L,252UL,251UL},{251UL,0x35L,0x35L,251UL},{0UL,0x35L,252UL,251UL},{251UL,0x35L,0x35L,251UL},{0UL,0x35L,252UL,251UL},{251UL,0x35L,0x35L,251UL}};
+static int64_t g_32 = 1L;
