a46e4c97fb56ccbbf249829c88ca4a2636222518
saisi <saisi@users.noreply.github.com>
2017-02-16 19:37:55 +0000

    1487273875 bacillophobia

---
+++ 1487273875.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3794718227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_32 = 0xB96CL;
+static uint64_t g_37[7] = {0x7F215A60915BB002LL,1UL,0x7F215A60915BB002LL,0x7F215A60915BB002LL,1UL,0x7F215A60915BB002LL,0x7F215A60915BB002LL};
+static const int32_t *g_53 = (void*)0;
+static uint16_t g_80 = 1UL;
+static int64_t g_82[1] = {0x8E0DA53D1EDBCC77LL};
+static int32_t g_84 = 1L;
+static int32_t g_86[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int8_t g_106 = 0xAFL;
+static uint16_t g_132 = 0x1642L;
