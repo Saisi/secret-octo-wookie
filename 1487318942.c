e6d3518992721602a817502de276830035e4cbff
saisi <saisi@users.noreply.github.com>
2017-02-17 08:09:02 +0000

    1487318942 abrachia

---
+++ 1487318942.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3936670888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x96DD1E1EL;
+static int8_t g_7 = 0x0CL;
+static volatile int8_t g_8 = 1L;/* VOLATILE GLOBAL g_8 */
+static int16_t g_9 = 0xE3D4L;
+static uint32_t g_10 = 4294967286UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
