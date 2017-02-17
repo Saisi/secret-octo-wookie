030e99fda4aba46a787593233a309af3b9de98d7
saisi <saisi@users.noreply.github.com>
2017-02-17 10:09:15 +0000

    1487326154 baptism

---
+++ 1487326154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      332515901
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 29;
+   const signed : 0;
+   unsigned f1 : 3;
+   unsigned f2 : 2;
+   unsigned f3 : 11;
+   signed f4 : 24;
+   signed f5 : 23;
+   unsigned f6 : 14;
+};
