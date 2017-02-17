7d6cbf058ce5e918fe83a86f3e11bb5c570aa332
saisi <saisi@users.noreply.github.com>
2017-02-17 20:40:15 +0000

    1487364014 Bahima

---
+++ 1487364014.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      742215548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const int8_t * f1;
+   unsigned f2 : 24;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
