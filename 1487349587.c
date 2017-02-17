36cf429d4ee83c2d5e7052a9ded46439849752b6
saisi <saisi@users.noreply.github.com>
2017-02-17 16:39:48 +0000

    1487349587 albinuria

---
+++ 1487349587.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1615357476
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 22;
+   volatile unsigned f1 : 30;
+   const signed f2 : 2;
+   volatile unsigned f3 : 15;
+   volatile signed f4 : 13;
+   unsigned f5 : 2;
+   unsigned f6 : 6;
