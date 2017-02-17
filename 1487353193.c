12e2da224bb4c3883a252fcf9c97eedba01c6e4e
saisi <saisi@users.noreply.github.com>
2017-02-17 17:39:54 +0000

    1487353193 audiometer

---
+++ 1487353193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      515473309
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 3L;
+static uint32_t g_63[1][4][1] = {{{0x3CBFB41CL},{0UL},{0x3CBFB41CL},{0UL}}};
+static uint32_t *g_79 = (void*)0;
+static int32_t *g_95 = &g_3;
+static int32_t **g_94 = &g_95;
+static int32_t **g_100 = &g_95;
+static uint8_t g_105 = 1UL;
+static uint32_t g_107 = 0UL;
+static uint16_t g_115 = 65535UL;
