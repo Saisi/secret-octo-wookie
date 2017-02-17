dceaddf66e317266b2e7e2ec2a78f38278fe0e77
saisi <saisi@users.noreply.github.com>
2017-02-17 07:39:00 +0000

    1487317140 amidoazobenzene

---
+++ 1487317140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3490980339
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 3L;
+static uint8_t g_5 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
