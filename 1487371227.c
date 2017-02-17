ee642b35aa7b44bfd9e6c838f14ed0bf24e86546
saisi <saisi@users.noreply.github.com>
2017-02-17 22:40:27 +0000

    1487371227 anteverting

---
+++ 1487371227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1637901409
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 4;
+   volatile int64_t  f2;
+   volatile uint64_t  f3;
+};
+
+union U1 {
+   const volatile int16_t  f0;
+   int8_t  f1;
