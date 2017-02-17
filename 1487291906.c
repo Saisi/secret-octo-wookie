e93307cce362a31055b0ef2d8dfd8e9bae329c0c
saisi <saisi@users.noreply.github.com>
2017-02-17 00:38:26 +0000

    1487291906 airbusses

---
+++ 1487291906.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1252528902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_9 = 0x2CF45657L;/* VOLATILE GLOBAL g_9 */
+static int64_t g_13[8] = {4L,(-8L),4L,4L,(-8L),4L,4L,(-8L)};
+static int16_t g_14 = 0x7374L;
+static uint8_t g_16 = 1UL;
+static uint8_t g_34 = 0x11L;
+static const uint8_t *g_33 = &g_34;
+static uint8_t g_45 = 0x86L;
+static uint8_t *g_44 = &g_45;
+static volatile int32_t g_52 = (-3L);/* VOLATILE GLOBAL g_52 */
