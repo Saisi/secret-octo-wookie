293e3668bb65d8d1e3da68fc5324ed45bbd73a91
saisi <saisi@users.noreply.github.com>
2017-02-17 13:09:30 +0000

    1487336970 Adigranth

---
+++ 1487336970.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      307163217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int8_t  f0;
+   const int64_t  f1;
+   int64_t  f2;
+   uint32_t  f3;
+   int8_t  f4;
+   volatile uint32_t  f5;
+   unsigned f6 : 7;
