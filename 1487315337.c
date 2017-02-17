a768b2ea34875867074c67b6da9a7fff427a2751
saisi <saisi@users.noreply.github.com>
2017-02-17 07:08:58 +0000

    1487315337 abdominocentesis

---
+++ 1487315337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2600612684
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 20;
+   uint8_t  f1;
+   const volatile uint32_t  f2;
+   volatile int16_t  f3;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   const uint32_t  f1;
