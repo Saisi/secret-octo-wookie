940fc7e0ad1d66dd8f5738c614deafb795093b6b
saisi <saisi@users.noreply.github.com>
2017-02-17 01:08:28 +0000

    1487293708 acheat

---
+++ 1487293708.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2821245574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S3 {
+   unsigned f0 : 23;
+   unsigned f1 : 2;
+   volatile unsigned f2 : 25;
+   int32_t  f3;
+   volatile unsigned f4 : 7;
+   const signed f5 : 11;
+   int64_t  f6;
