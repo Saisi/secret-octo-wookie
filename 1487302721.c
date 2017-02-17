ff3c3e5d9439c2d116c56c0e97be1169d411acda
saisi <saisi@users.noreply.github.com>
2017-02-17 03:38:41 +0000

    1487302721 asphaltus

---
+++ 1487302721.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3250606835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 24;
+   volatile unsigned f1 : 15;
+   signed f2 : 22;
+   const unsigned f3 : 9;
+   const unsigned f4 : 22;
+   signed f5 : 23;
+   int32_t  f6;
+};
+
