a3083afe8b97e12882ed35a768644d23f12f9ed3
saisi <saisi@users.noreply.github.com>
2017-02-17 12:39:28 +0000

    1487335168 backslashes

---
+++ 1487335168.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3595568283
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 12;
+   signed f1 : 19;
+   signed f2 : 10;
+   signed f3 : 13;
+   const int64_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
