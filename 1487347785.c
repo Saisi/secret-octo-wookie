4b480b55f55029f4c26464d55d0e446448939574
saisi <saisi@users.noreply.github.com>
2017-02-17 16:09:45 +0000

    1487347785 axebreaker

---
+++ 1487347785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      557917367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 26;
+   unsigned f1 : 9;
+   unsigned f2 : 8;
+   volatile signed f3 : 19;
+   volatile unsigned f4 : 24;
+   signed f5 : 17;
+   signed f6 : 12;
+   volatile unsigned f7 : 15;
+   unsigned f8 : 22;
