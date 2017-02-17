335c042df3a744b03eb503de10c2ad01ed83679a
saisi <saisi@users.noreply.github.com>
2017-02-17 06:38:55 +0000

    1487313535 anthracyl

---
+++ 1487313535.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2309280778
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   const unsigned f0 : 3;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[10][1] = {{0x658EDFE0L},{0L},{0L},{0x658EDFE0L},{0L},{0L},{0x658EDFE0L},{0L},{0L},{0x658EDFE0L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
