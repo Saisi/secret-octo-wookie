2c3dfdfe9cfc74f0086ef93933c8e0e5ba8ad7bd
saisi <saisi@users.noreply.github.com>
2017-02-17 03:08:39 +0000

    1487300918 acetomorphin

---
+++ 1487300918.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1437150283
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint64_t  f1;
+   const int32_t  f2;
+};
+
+union U1 {
+   int32_t  f0;
+   signed f1 : 19;
+   uint32_t  f2;
