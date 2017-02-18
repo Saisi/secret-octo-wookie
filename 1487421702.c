767b08459faa9bd1cde2644671cd286cd47a6754
saisi <saisi@users.noreply.github.com>
2017-02-18 12:41:43 +0000

    1487421702 alitrunk

---
+++ 1487421702.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2941090100
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 29;
+   const volatile signed f2 : 11;
+   volatile signed f3 : 11;
+   signed f4 : 14;
+   signed f5 : 13;
+   signed f6 : 23;
