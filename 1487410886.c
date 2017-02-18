9b5811a46c38e5b9bab87b3697155fe5d42642a1
saisi <saisi@users.noreply.github.com>
2017-02-18 09:41:26 +0000

    1487410886 arborway

---
+++ 1487410886.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2145864877
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 2L;
+static union U0 g_23 = {247UL};
+static int32_t g_40 = 0x31E13CA4L;
+static int32_t *g_39[2][3] = {{&g_40,&g_40,&g_40},{&g_8,&g_8,&g_8}};
+static union U0 *** volatile g_50 = (void*)0;/* VOLATILE GLOBAL g_50 */
