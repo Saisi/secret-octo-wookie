e204e0e266d81cd98c4c0f1c82aac39c36d5ca0b
saisi <saisi@users.noreply.github.com>
2017-02-18 09:11:24 +0000

    1487409083 auxospore

---
+++ 1487409083.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      559941510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   const unsigned f1 : 2;
+   signed f2 : 28;
+   volatile unsigned f3 : 24;
+};
+
+union U1 {
+   int8_t * f0;
+   uint16_t  f1;
