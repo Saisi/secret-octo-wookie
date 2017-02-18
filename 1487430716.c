d821ec682a84d225d1433b612fa748d9218f10f0
saisi <saisi@users.noreply.github.com>
2017-02-18 15:11:56 +0000

    1487430716 antivirus

---
+++ 1487430716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2510015654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   uint16_t  f1;
+   int32_t  f2;
+   const uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_19 = 1UL;
+static int32_t g_30 = 0L;
