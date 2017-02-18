d52fb4911df55e6ff771ec08c40587a51e005b26
saisi <saisi@users.noreply.github.com>
2017-02-18 21:42:36 +0000

    1487454155 apple

---
+++ 1487454155.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1505468129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x9FB1C038L;
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
