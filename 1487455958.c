407f5a99f6e91a1be19f64721e354cfc72ca9dec
saisi <saisi@users.noreply.github.com>
2017-02-18 22:12:38 +0000

    1487455958 Aristotelianism

---
+++ 1487455958.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1836083346
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int16_t  f1;
+   signed f2 : 15;
+   uint8_t  f3;
+   uint8_t  f4;
+   int32_t  f5;
+   uint16_t  f6;
+   int8_t  f7;
+};
