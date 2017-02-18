153ec0744730b7bd9896ecbefecccbf7274c37cb
saisi <saisi@users.noreply.github.com>
2017-02-18 21:12:33 +0000

    1487452353 armipotent

---
+++ 1487452353.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      398377861
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 1;
+   signed f1 : 20;
+   volatile signed f2 : 7;
+   unsigned f3 : 22;
+   volatile unsigned f4 : 15;
+   const volatile signed f5 : 12;
+};
+
+struct S1 {
