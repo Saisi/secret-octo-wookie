6227f7561f59c38717115daaba238b72b74c8cd9
saisi <saisi@users.noreply.github.com>
2017-02-19 07:13:29 +0000

    1487488408 antifeudal

---
+++ 1487488408.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2794103198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8[10][2] = {{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)}};
+static volatile int32_t g_11 = 0L;/* VOLATILE GLOBAL g_11 */
+static volatile int16_t g_12 = 0x5A3DL;/* VOLATILE GLOBAL g_12 */
+static int32_t * volatile g_21 = &g_8[0][0];/* VOLATILE GLOBAL g_21 */
+static int16_t g_31 = 1L;
+static const uint16_t *g_76 = (void*)0;
+static uint16_t g_78 = 0xDE23L;
+static int16_t g_80[3] = {(-5L),(-5L),(-5L)};
+static uint64_t g_96[3][9] = {{0x99CA764965E8A440LL,1UL,0xBE11D5C9785EC66ALL,0xD446BF52A38B0DACLL,0xBE11D5C9785EC66ALL,1UL,0x99CA764965E8A440LL,1UL,0xBE11D5C9785EC66ALL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{0x99CA764965E8A440LL,1UL,0xBE11D5C9785EC66ALL,0xD446BF52A38B0DACLL,0xBE11D5C9785EC66ALL,0xFC2D246E6FC7E2C9LL,0xBE11D5C9785EC66ALL,0xFC2D246E6FC7E2C9LL,4UL}};
