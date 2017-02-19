f363c2d949341792b827f558f07a6dd71912cf6e
saisi <saisi@users.noreply.github.com>
2017-02-19 03:13:07 +0000

    1487473986 argali

---
+++ 1487473986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2978546706
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[3] = {0x825E7A66L,0x825E7A66L,0x825E7A66L};
+static int32_t g_32 = (-9L);
+static int16_t g_34 = 0xE20BL;
+static uint64_t g_42 = 0xE89BE308DDD7AD25LL;
+static uint64_t * volatile g_41[1] = {&g_42};
+static int16_t g_80 = 0xB0F0L;
+static const int16_t *g_79 = &g_80;
+static int32_t *g_108 = &g_32;
+static int32_t * volatile * const g_107 = &g_108;
