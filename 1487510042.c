e899f38767942fde1f77f50c31d7b61a7c7ebbc8
saisi <saisi@users.noreply.github.com>
2017-02-19 13:14:03 +0000

    1487510042 accretion

---
+++ 1487510042.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2332478397
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static volatile int16_t g_8 = 0L;/* VOLATILE GLOBAL g_8 */
+static uint64_t g_9 = 0xBD0176DF7E44142BLL;
+static uint8_t g_39 = 248UL;
+static uint32_t *g_86 = (void*)0;
+static int32_t g_94[3][9] = {{0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL},{(-5L),0xC032BE65L,(-5L),0xC032BE65L,(-5L),0xC032BE65L,(-5L),0xC032BE65L,(-5L)},{0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL,0x4567548BL}};
+static int32_t g_99 = 0x23B973E4L;
+static uint32_t g_106 = 0xE27DD120L;
+static int32_t *g_131 = (void*)0;
