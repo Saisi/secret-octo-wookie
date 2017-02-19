04f6ce383f6e47420bedbaed090e6128641e2719
saisi <saisi@users.noreply.github.com>
2017-02-19 02:43:04 +0000

    1487472184 baldric

---
+++ 1487472184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1995504317
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   unsigned : 0;
+   volatile unsigned f1 : 3;
+   uint64_t  f2;
+   volatile signed f3 : 27;
+   const signed f4 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
