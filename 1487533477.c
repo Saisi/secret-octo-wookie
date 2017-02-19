0056c07a49e295e8159694bd56017af973665181
saisi <saisi@users.noreply.github.com>
2017-02-19 19:44:37 +0000

    1487533477 amour

---
+++ 1487533477.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1988776965
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   const signed f1 : 22;
+   const signed f2 : 20;
+   volatile unsigned f3 : 8;
+};
+#pragma pack(pop)
+
