6feaa7bbd0ece9de4b50ec1eb1a0e09e87490d90
saisi <saisi@users.noreply.github.com>
2017-02-19 10:43:50 +0000

    1487501030 arabinoside

---
+++ 1487501030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1766072420
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[3] = {0x61F66568L,0x61F66568L,0x61F66568L};
+static volatile uint8_t g_14[1] = {0x96L};
+static volatile int32_t g_31 = 0x2902D4ECL;/* VOLATILE GLOBAL g_31 */
+static int32_t g_32 = 0x87CCB484L;
+static int32_t *g_75 = &g_4[0];
+static int32_t **g_74 = &g_75;
+static int16_t g_108[2][9] = {{(-8L),(-8L),0x733EL,(-10L),0x733EL,(-8L),(-8L),0x733EL,(-10L)},{0xD931L,0xA8D7L,0xD931L,0x733EL,0x733EL,0xD931L,0xA8D7L,0xD931L,0x733EL}};
+static int16_t g_143 = 0L;
+static uint16_t g_158 = 0xF83BL;
