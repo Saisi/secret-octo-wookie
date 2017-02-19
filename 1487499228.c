5bdeed5ebd8ae9e6f4b9b520e6baa756048aaf62
saisi <saisi@users.noreply.github.com>
2017-02-19 10:13:48 +0000

    1487499228 antimere

---
+++ 1487499228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1636643962
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint8_t  f1;
+   volatile signed f2 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x14B733C6L;
+static volatile int16_t g_8[4] = {0x2ABBL,0x2ABBL,0x2ABBL,0x2ABBL};
+static uint32_t g_9 = 0x17473468L;
