73d239e4acc496524fae31ec45b650b417baeca4
saisi <saisi@users.noreply.github.com>
2017-02-19 12:44:00 +0000

    1487508239 Adelarthrosomata

---
+++ 1487508239.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      524579237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   signed f1 : 4;
+   signed f2 : 23;
+   unsigned f3 : 6;
+   const unsigned f4 : 31;
+   signed f5 : 28;
+};
+
+union U1 {
