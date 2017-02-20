3f7207794dae5b588d7495e0ae57c8aeac820261
saisi <saisi@users.noreply.github.com>
2017-02-20 18:46:51 +0000

    1487616411 abib

---
+++ 1487616411.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2914150303
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[2] = {1L,1L};
+static int32_t g_6[9][5] = {{9L,0x02E31D83L,0x02E31D83L,9L,0x02E31D83L},{9L,9L,(-1L),9L,9L},{0x02E31D83L,9L,0x02E31D83L,0x02E31D83L,9L},{9L,0x02E31D83L,0x02E31D83L,9L,0x02E31D83L},{9L,9L,(-1L),9L,9L},{0x02E31D83L,9L,0x02E31D83L,0x02E31D83L,9L},{9L,0x02E31D83L,0x02E31D83L,9L,0x02E31D83L},{9L,9L,(-1L),9L,9L},{0x02E31D83L,9L,0x02E31D83L,0x02E31D83L,0x02E31D83L}};
+static int32_t * volatile g_57 = (void*)0;/* VOLATILE GLOBAL g_57 */
+static const int32_t g_87 = 0x4E38B13FL;
+static uint8_t g_89 = 0x66L;
+static int32_t *g_92 = &g_3[1];
+static int32_t ** volatile g_91[8][5][6] = {{{(void*)0,&g_92,(void*)0,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,(void*)0,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,(void*)0,(void*)0,&g_92},{&g_92,&g_92,(void*)0,(void*)0,&g_92,&g_92}},{{&g_92,(void*)0,&g_92,&g_92,&g_92,(void*)0},{&g_92,&g_92,(void*)0,&g_92,(void*)0,&g_92},{&g_92,&g_92,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,(void*)0,(void*)0,&g_92},{&g_92,&g_92,&g_92,(void*)0,&g_92,&g_92}},{{&g_92,(void*)0,&g_92,&g_92,(void*)0,&g_92},{&g_92,&g_92,(void*)0,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,&g_92,&g_92},{(void*)0,&g_92,(void*)0,&g_92,(void*)0,&g_92},{(void*)0,&g_92,&g_92,&g_92,&g_92,&g_92}},{{(void*)0,(void*)0,(void*)0,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,(void*)0,&g_92},{&g_92,&g_92,&g_92,(void*)0,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,(void*)0,&g_92},{(void*)0,(void*)0,&g_92,(void*)0,&g_92,&g_92}},{{&g_92,&g_92,(void*)0,&g_92,&g_92,(void*)0},{&g_92,(void*)0,&g_92,&g_92,(void*)0,&g_92},{&g_92,&g_92,(void*)0,(void*)0,&g_92,&g_92},{(void*)0,&g_92,&g_92,(void*)0,(void*)0,&g_92},{&g_92,&g_92,(void*)0,&g_92,&g_92,&g_92}},{{&g_92,(void*)0,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,(void*)0,&g_92,(void*)0,(void*)0},{&g_92,&g_92,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,&g_92,&g_92,(void*)0,(void*)0}},{{&g_92,(void*)0,&g_92,(void*)0,&g_92,&g_92},{(void*)0,&g_92,&g_92,(void*)0,&g_92,&g_92},{(void*)0,(void*)0,&g_92,&g_92,&g_92,&g_92},{&g_92,&g_92,(void*)0,&g_92,(void*)0,&g_92},{&g_92,&g_92,(void*)0,&g_92,(void*)0,&g_92}},{{&g_92,&g_92,&g_92,&g_92,(void*)0,&g_92},{&g_92,(void*)0,&g_92,&g_92,&g_92,(void*)0},{&g_92,(void*)0,(void*)0,(void*)0,&g_92,(void*)0},{&g_92,(void*)0,&g_92,&g_92,&g_92,(void*)0},{&g_92,(void*)0,&g_92,&g_92,(void*)0,&g_92}}};
+static int32_t ** volatile g_93 = (void*)0;/* VOLATILE GLOBAL g_93 */
