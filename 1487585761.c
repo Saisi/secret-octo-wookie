6e94be3af3a14a73a433a6587673211f181b9d1a
saisi <saisi@users.noreply.github.com>
2017-02-20 10:16:01 +0000

    1487585761 acetamids

---
+++ 1487585761.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2283200950
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-1L);
+static uint64_t g_20 = 18446744073709551608UL;
+static int32_t g_24 = 0xBA4D3EC9L;
+static int8_t g_34 = 1L;
+static uint32_t g_36 = 18446744073709551609UL;
+static uint32_t g_41[3][9][3] = {{{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL}},{{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL}},{{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL},{7UL,7UL,7UL}}};
+static int32_t *g_47 = &g_24;
+static int32_t * const  volatile *g_46 = &g_47;
+
