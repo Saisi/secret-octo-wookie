21a97f56876d1c6b2e0305f82df32f9f7abcc918
saisi <saisi@users.noreply.github.com>
2017-02-20 04:45:29 +0000

    1487565928 acylates

---
+++ 1487565928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4136068007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   uint64_t  f1;
+   uint8_t  f2;
+   volatile int16_t  f3;
+   volatile uint64_t  f4;
+   const volatile uint32_t  f5;
+   int16_t  f6;
+   volatile int16_t  f7;
+   int16_t  f8;
