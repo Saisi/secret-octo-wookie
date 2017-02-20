200b9b72d74a0881c29f5c9c99a20d6a290cd3e2
saisi <saisi@users.noreply.github.com>
2017-02-20 19:16:54 +0000

    1487618214 anticks

---
+++ 1487618214.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3714184247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   int32_t  f1;
+   signed f2 : 8;
+   uint32_t  f3;
+};
+
+union U1 {
+   signed f0 : 26;
+   uint64_t  f1;
