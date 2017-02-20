8be294a65719517248208f8d8f25733bcc664933
saisi <saisi@users.noreply.github.com>
2017-02-20 04:15:26 +0000

    1487564125 archconfraternity

---
+++ 1487564125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2681642755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 16;
+   const unsigned f1 : 1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x4F147909L;
+static int32_t g_4 = 0x6EFBA1A4L;
+static int32_t g_36 = 0x0D1F2830L;
+static uint32_t g_46 = 0x2DBE4212L;
