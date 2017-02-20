b18d9ce9530af30290d4e8e1027248cb78131d1d
saisi <saisi@users.noreply.github.com>
2017-02-20 10:46:08 +0000

    1487587567 auriphrygia

---
+++ 1487587567.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3355547226
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xBEAA32B1L;
+static int32_t g_7 = 0x733C6338L;
+static const union U0 g_63 = {1UL};/* VOLATILE GLOBAL g_63 */
+static uint32_t g_73[8] = {4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL};
+static int64_t g_76 = 0xE57B790EB5CFE74DLL;
