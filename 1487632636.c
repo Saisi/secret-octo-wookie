820a682c52fc312f480a34686c447964a55cb71d
saisi <saisi@users.noreply.github.com>
2017-02-20 23:17:17 +0000

    1487632636 aerosat

---
+++ 1487632636.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      707443980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+   volatile int32_t  f1;
+   int8_t * volatile  f2;
+   int8_t * f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x3AA9AC70L;
+static int32_t g_8[2] = {0L,0L};
