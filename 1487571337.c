6cf9f75bd77095cb0c9cc42908dabc526c72d31b
saisi <saisi@users.noreply.github.com>
2017-02-20 06:15:39 +0000

    1487571337 albiness

---
+++ 1487571337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1814458230
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 9;
+   unsigned f1 : 26;
+   const volatile int32_t  f2;
+   volatile signed f3 : 28;
+   const signed f4 : 17;
+   volatile unsigned f5 : 25;
+   const volatile unsigned f6 : 22;
+   volatile unsigned f7 : 23;
+   unsigned f8 : 12;
