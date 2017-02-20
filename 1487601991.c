4bb62a6931eef727edc2ec73b0600a969d3daa95
saisi <saisi@users.noreply.github.com>
2017-02-20 14:46:31 +0000

    1487601991 Adoula

---
+++ 1487601991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2994819259
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_8 = 5UL;
+static uint8_t g_12 = 0xEDL;
+static int32_t g_30 = 0x12D29ED7L;
+static int32_t * volatile g_29 = &g_30;/* VOLATILE GLOBAL g_29 */
+static int8_t g_35 = 0xB5L;
+static int32_t g_48 = 0L;
+static uint8_t g_109[2] = {1UL,1UL};
+static uint8_t *g_108 = &g_109[1];
+static int32_t g_143 = 0L;
