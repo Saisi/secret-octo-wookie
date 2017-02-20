4d1021645513f91c9d0796d1dc0c80f2bd33e8d1
saisi <saisi@users.noreply.github.com>
2017-02-20 09:15:54 +0000

    1487582154 Anisomeles

---
+++ 1487582154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3880568011
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0x3D2D672AL;
+static int32_t g_37[7][3] = {{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L},{0xC6F44469L,0xC6F44469L,0xC6F44469L}};
