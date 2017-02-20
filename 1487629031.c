c9c8dd67270dcd9733e4d59d77bd31046a9aa8f1
saisi <saisi@users.noreply.github.com>
2017-02-20 22:17:11 +0000

    1487629031 Agbogla

---
+++ 1487629031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2609283398
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static int32_t * volatile * volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
