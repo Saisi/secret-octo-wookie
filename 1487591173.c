21d564d53742d0024fec3a5999b6ac2823d38141
saisi <saisi@users.noreply.github.com>
2017-02-20 11:46:13 +0000

    1487591173 anticoagulants

---
+++ 1487591173.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2963911630
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   int64_t  f1;
+   volatile unsigned f2 : 30;
+   unsigned f3 : 8;
+   const unsigned f4 : 13;
+   signed f5 : 5;
+   signed f6 : 18;
