6b3644a1487834aa5d92a3b90ffd3c7400489574
saisi <saisi@users.noreply.github.com>
2017-02-21 15:48:48 +0000

    1487692128 amakebe

---
+++ 1487692128.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2382895542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1] = {0x24F01C08L};
+static volatile int32_t g_24 = 0x76403EC6L;/* VOLATILE GLOBAL g_24 */
+static volatile int32_t *g_23[10][9][2] = {{{&g_24,(void*)0},{(void*)0,&g_24},{(void*)0,&g_24},{(void*)0,(void*)0},{(void*)0,(void*)0},{&g_24,(void*)0},{(void*)0,&g_24},{(void*)0,&g_24},{(void*)0,(void*)0}},{{&g_24,(void*)0},{&g_24,(void*)0},{(void*)0,&g_24},{(void*)0,&g_24},{(void*)0,(void*)0},{&g_24,(void*)0},{(void*)0,(void*)0},{(void*)0,&g_24},{(void*)0,&g_24}},{{(void*)0,(void*)0},{&g_24,&g_24},{(void*)0,(void*)0},{&g_24,&g_24},{(void*)0,(void*)0},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,&g_24},{&g_24,&g_24}},{{&g_24,&g_24},{&g_24,&g_24},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24}},{{&g_24,(void*)0},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,&g_24},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,(void*)0},{(void*)0,&g_24}},{{(void*)0,(void*)0},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,&g_24},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24}},{{&g_24,(void*)0},{&g_24,&g_24},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,&g_24},{&g_24,&g_24},{&g_24,&g_24}},{{&g_24,&g_24},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,(void*)0}},{{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,&g_24},{&g_24,&g_24},{(void*)0,&g_24},{&g_24,(void*)0},{(void*)0,&g_24},{(void*)0,(void*)0}},{{&g_24,&g_24},{(void*)0,&g_24},{&g_24,&g_24},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,(void*)0},{&g_24,&g_24},{&g_24,(void*)0}}};
+static volatile int32_t ** volatile g_22 = &g_23[1][8][1];/* VOLATILE GLOBAL g_22 */
+static int64_t g_85 = (-5L);
+static int16_t g_91 = (-5L);
+static uint32_t g_92 = 0x6D2A347AL;
+static uint8_t g_100 = 0xAAL;
+static uint32_t g_119 = 6UL;
