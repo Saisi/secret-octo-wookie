d7b1ee9f2c322b21c30a9cbfabdf66133a1e5075
saisi <saisi@users.noreply.github.com>
2017-02-21 01:47:29 +0000

    1487641648 archispore

---
+++ 1487641648.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      233544655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5464B5F5L;
+static volatile int64_t g_13 = 0xC6E54EE87F6FB807LL;/* VOLATILE GLOBAL g_13 */
+static uint16_t g_14 = 0x6695L;
+static int32_t *g_22 = (void*)0;
+static int32_t g_71 = (-7L);
+static int32_t g_84 = 1L;
+static int64_t g_92[6] = {0xCD4C676C23079319LL,0xCD4C676C23079319LL,0xCD4C676C23079319LL,0xCD4C676C23079319LL,0xCD4C676C23079319LL,0xCD4C676C23079319LL};
+static int64_t *g_95 = &g_92[2];
+static uint8_t g_106 = 248UL;
