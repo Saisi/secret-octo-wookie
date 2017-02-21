c544cefe1a4fbaa785b8eacbce38f5d5ef4a643a
saisi <saisi@users.noreply.github.com>
2017-02-21 20:49:17 +0000

    1487710157 abnormals

---
+++ 1487710157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      928147781
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 1;
+   unsigned f1 : 20;
+   unsigned f2 : 7;
+   signed f3 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-6L);
+static int64_t g_31 = 0xC4A87FCE6BB2F144LL;
