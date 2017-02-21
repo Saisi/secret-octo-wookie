51b6774bef3306a444ffaa21fdb72032de50f241
saisi <saisi@users.noreply.github.com>
2017-02-21 02:47:34 +0000

    1487645253 aristocraticism

---
+++ 1487645253.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1914050220
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_23 = 9L;
+static int32_t g_27 = 0x41FD9D08L;
+static uint64_t g_63 = 18446744073709551615UL;
+static int16_t g_81[3][6][6] = {{{0x6BBBL,0L,1L,1L,0L,0x6BBBL},{6L,0x6BBBL,1L,0x6BBBL,6L,6L},{1L,0x6BBBL,0x6BBBL,1L,0L,1L},{1L,0L,1L,0x6BBBL,0x6BBBL,1L},{6L,6L,0x6BBBL,1L,0x6BBBL,6L},{0x6BBBL,0L,1L,1L,0L,0x6BBBL}},{{6L,0x6BBBL,1L,0x6BBBL,6L,6L},{1L,0x6BBBL,0x6BBBL,1L,6L,1L},{1L,6L,1L,1L,1L,1L},{0x6BBBL,0x6BBBL,1L,0L,1L,0x6BBBL},{1L,6L,0L,0L,6L,1L},{0x6BBBL,1L,0L,1L,0x6BBBL,0x6BBBL}},{{1L,1L,1L,1L,6L,1L},{1L,6L,1L,1L,1L,1L},{0x6BBBL,0x6BBBL,1L,0L,1L,0x6BBBL},{1L,6L,0L,0L,6L,1L},{0x6BBBL,1L,0L,1L,0x6BBBL,0x6BBBL},{1L,1L,1L,1L,6L,1L}}};
+static int16_t g_85[6] = {7L,7L,7L,7L,7L,7L};
+static int32_t g_90 = 8L;
+static int8_t g_98 = (-1L);
+static int8_t g_102[1] = {(-1L)};
+static volatile uint32_t g_126 = 0x540135D8L;/* VOLATILE GLOBAL g_126 */
