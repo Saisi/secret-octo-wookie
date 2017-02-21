86fcd79ef3744410211efef0dcf220178a6fd465
saisi <saisi@users.noreply.github.com>
2017-02-21 10:18:17 +0000

    1487672297 arakawaite

---
+++ 1487672297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      471210701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 5;
+   volatile signed f1 : 8;
+   volatile signed f2 : 11;
+   unsigned f3 : 10;
+   const volatile signed f4 : 19;
+   volatile unsigned : 0;
+};
