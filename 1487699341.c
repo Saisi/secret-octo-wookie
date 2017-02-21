d935edcae847aa22169cf2e933a32a746b7288c1
saisi <saisi@users.noreply.github.com>
2017-02-21 17:49:01 +0000

    1487699341 antiscii

---
+++ 1487699341.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      124815775
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   int32_t  f1;
+   int32_t  f2;
+   uint16_t  f3;
+   uint64_t  f4;
+   volatile signed f5 : 13;
+   volatile signed f6 : 28;
