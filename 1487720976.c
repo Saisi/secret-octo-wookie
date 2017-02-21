506a726c73162fa91f0da8f1e32b763c6b3699e4
saisi <saisi@users.noreply.github.com>
2017-02-21 23:49:38 +0000

    1487720976 antimoniferous

---
+++ 1487720976.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2521772081
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 23;
+   unsigned f1 : 18;
+   unsigned f2 : 13;
+   uint32_t  f3;
+   const volatile signed f4 : 29;
+   signed f5 : 23;
+};
+
+union U1 {
