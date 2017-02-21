65b14b4da45b5d13c888f4d48db27dadcf52a54c
saisi <saisi@users.noreply.github.com>
2017-02-21 05:47:49 +0000

    1487656069 aldopentose

---
+++ 1487656069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1102870399
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static int32_t g_8 = 1L;
+static int8_t g_12 = 0x32L;
+static volatile uint16_t g_14 = 0x5BB3L;/* VOLATILE GLOBAL g_14 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
