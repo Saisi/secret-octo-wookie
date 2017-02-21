8ec01c890be3ab366b94b47de09302e28ad08cfd
saisi <saisi@users.noreply.github.com>
2017-02-21 04:47:44 +0000

    1487652464 abrasing

---
+++ 1487652464.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3193420773
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xAD2086BDL;
+static int32_t ** volatile g_4 = (void*)0;/* VOLATILE GLOBAL g_4 */
+static int32_t ** const  volatile g_6 = (void*)0;/* VOLATILE GLOBAL g_6 */
+static int32_t g_8 = 0x2A26F3F1L;
+static int32_t * volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
