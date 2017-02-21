2227a9600843fc30c32b5d810d17f48f36982b35
saisi <saisi@users.noreply.github.com>
2017-02-21 07:17:59 +0000

    1487661479 ampelidaceous

---
+++ 1487661479.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      429663791
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 22;
+   volatile signed f1 : 21;
+   unsigned f2 : 4;
+   volatile unsigned f3 : 31;
+   signed f4 : 20;
+   const unsigned f5 : 29;
+   volatile signed f6 : 8;
+};
+
