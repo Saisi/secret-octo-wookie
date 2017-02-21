5356440af4e8e2d38682cd3295aabb74d408252b
saisi <saisi@users.noreply.github.com>
2017-02-21 14:18:40 +0000

    1487686719 atheism

---
+++ 1487686719.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1575190984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 2UL;
+static int32_t g_12 = 0x88999D94L;
+static uint16_t g_28 = 0x881DL;
+static uint16_t g_32 = 1UL;
+static uint16_t * const g_31 = &g_32;
+static uint32_t g_37 = 0x9C214F16L;
+static int8_t g_41 = 0xC3L;
+static uint16_t g_80 = 0x3887L;
+static int16_t g_100 = 1L;
