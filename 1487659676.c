464f2175ef68668a42c1baa945ac25c5a8e2bf1d
saisi <saisi@users.noreply.github.com>
2017-02-21 06:47:57 +0000

    1487659676 antidromal

---
+++ 1487659676.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4118592335
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = (-1L);
+static int8_t g_4 = 0x18L;
+static int8_t g_5 = 0xFEL;
+static int32_t g_6 = 0xF6FB807CL;
+static volatile uint16_t g_7 = 0x5998L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_69 = 1L;
+static int16_t g_87[4][7] = {{(-5L),2L,(-7L),0x7543L,(-7L),2L,(-5L)},{1L,(-9L),1L,1L,0x7543L,9L,(-5L)},{0x7543L,(-5L),6L,6L,(-5L),0x7543L,1L},{2L,6L,1L,1L,0x91DAL,0x7543L,0x7543L}};
+static int32_t *g_93 = (void*)0;
+static int32_t *g_101 = &g_69;
