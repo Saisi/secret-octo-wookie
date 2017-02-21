e90b4ed5aeb5c8b180f7a2d86d2aabca87da1f3a
saisi <saisi@users.noreply.github.com>
2017-02-21 18:49:08 +0000

    1487702948 Ainus

---
+++ 1487702948.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3539477984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 30;
+   volatile uint32_t  f1;
+   unsigned f2 : 19;
+   const volatile uint8_t  f3;
+   volatile uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x7DC4B4E1L;
