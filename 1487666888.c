d5bd9f1316f578b31ad0dc1e73cb7129f5469a5b
saisi <saisi@users.noreply.github.com>
2017-02-21 08:48:09 +0000

    1487666888 arthroneuralgia

---
+++ 1487666888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3945494857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   volatile uint16_t  f1;
+   uint32_t  f2;
+   const int8_t  f3;
+   uint64_t  f4;
+   volatile int16_t  f5;
+};
+
+#pragma pack(push)
