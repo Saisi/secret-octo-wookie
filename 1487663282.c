0bc3a0d2f813a5b39e417ed740d3cdefe9a69d39
saisi <saisi@users.noreply.github.com>
2017-02-21 07:48:02 +0000

    1487663282 activator

---
+++ 1487663282.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2588529555
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_12 = 6UL;
+static int32_t * volatile g_16 = (void*)0;/* VOLATILE GLOBAL g_16 */
