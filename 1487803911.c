db243ec8228c1bc15ad16d483d68afea09a33434
saisi <saisi@users.noreply.github.com>
2017-02-22 22:51:52 +0000

    1487803911 anaesthetization

---
+++ 1487803911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2265096005
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int64_t  f0;
+   const uint8_t  f1;
+   int32_t  f2;
+   unsigned f3 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint16_t g_18 = 0x9ECFL;/* VOLATILE GLOBAL g_18 */
+static uint16_t g_23 = 9UL;
