1cfe3defd8e7db9ab3a3522d2910b3df6e479315
saisi <saisi@users.noreply.github.com>
2017-02-22 04:19:59 +0000

    1487737199 anarchisms

---
+++ 1487737199.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      250199564
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xB7BEDEDBL;
+static int32_t g_6[4] = {0x38DD44ADL,0x38DD44ADL,0x38DD44ADL,0x38DD44ADL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
