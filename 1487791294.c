535eec67f1812675020144882c2828310c814801
saisi <saisi@users.noreply.github.com>
2017-02-22 19:21:35 +0000

    1487791294 antidactyl

---
+++ 1487791294.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      589946348
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   volatile uint32_t  f1;
+   volatile int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[6] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static int32_t g_4[7][5] = {{(-1L),(-1L),(-6L),(-1L),0xB0D193E0L},{0x213EC752L,0xD44AD374L,0L,0xD44AD374L,0x213EC752L},{0xB0D193E0L,(-1L),(-6L),(-1L),(-1L)},{0L,0xD44AD374L,0L,0x6580849FL,0L},{0xB0D193E0L,(-1L),(-1L),(-1L),(-1L)},{0x213EC752L,0x6580849FL,0L,0x6580849FL,0x213EC752L},{(-1L),(-1L),(-1L),(-1L),0xB0D193E0L}};
+static uint8_t g_11 = 255UL;
