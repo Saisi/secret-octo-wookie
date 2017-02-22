00964d384fb7025df31e7fb385d2edd4ca1391c2
saisi <saisi@users.noreply.github.com>
2017-02-22 00:19:40 +0000

    1487722780 achromatise

---
+++ 1487722780.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2613506459
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 19;
+   uint32_t  f1;
+   volatile unsigned f2 : 10;
