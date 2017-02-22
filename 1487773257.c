c04e283a765ce5c34f285a1e6fb8aab5c9dea11e
saisi <saisi@users.noreply.github.com>
2017-02-22 14:20:58 +0000

    1487773257 afghanets

---
+++ 1487773257.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      214489501
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static uint32_t g_15 = 0xFD1F2E96L;
+static int8_t g_21 = 0xB9L;
+static int16_t g_36[3][2] = {{8L,8L},{8L,8L},{8L,8L}};
+static uint32_t g_44 = 4294967295UL;
+static int8_t g_56 = (-6L);
+static int8_t *g_55[1] = {&g_56};
+static int32_t g_85 = 0L;
+static int16_t g_87[3] = {1L,1L,1L};
