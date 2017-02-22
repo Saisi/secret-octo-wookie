c9d1853594ba75b972b89a11fa8a08b886ae97ba
saisi <saisi@users.noreply.github.com>
2017-02-22 08:20:23 +0000

    1487751623 alantolactone

---
+++ 1487751623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3746617507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 16;
+   const volatile unsigned f1 : 7;
+   const unsigned f2 : 14;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const signed f0 : 10;
