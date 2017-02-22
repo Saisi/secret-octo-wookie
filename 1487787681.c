6370e8759563d4c0559d42079926cb5e9619e374
saisi <saisi@users.noreply.github.com>
2017-02-22 18:21:29 +0000

    1487787681 ardor

---
+++ 1487787681.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3822862305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 15;
+   signed f1 : 10;
+   const unsigned f2 : 5;
+   signed f3 : 28;
+   volatile unsigned f4 : 29;
+   volatile signed f5 : 2;
+};
+
+struct S1 {
