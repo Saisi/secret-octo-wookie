78b52987aa5550094d3c864814c13276c75a9d9a
saisi <saisi@users.noreply.github.com>
2017-02-22 10:50:34 +0000

    1487760634 Baptlsta

---
+++ 1487760634.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1544884874
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint8_t  f1;
+   int16_t  f2;
+   const uint32_t  f3;
+   const volatile int64_t  f4;
+};
+
+union U1 {
+   volatile unsigned f0 : 24;
