6adfd59723250f0a652fab139eb15927e8f55aa0
saisi <saisi@users.noreply.github.com>
2017-02-22 19:51:37 +0000

    1487793097 amphikaryon

---
+++ 1487793097.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1679649258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x8A59C176L;
+static volatile int32_t g_6[1][3] = {{1L,1L,1L}};
+static volatile int32_t *g_5 = &g_6[0][2];
+static uint64_t g_56 = 0x9FC5C2DA50E28AB8LL;
