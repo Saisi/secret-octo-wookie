f7fffceea1aceb7734a52c365df7011695729ad4
saisi <saisi@users.noreply.github.com>
2017-02-22 06:20:13 +0000

    1487744413 antifideism

---
+++ 1487744413.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      261754689
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 31;
+};
+
+union U1 {
+   volatile signed f0 : 28;
+};
+
+union U2 {
+   int16_t  f0;
