68e488f342ad792774b86ebe1f610032f9dc0104
saisi <saisi@users.noreply.github.com>
2017-02-22 11:50:39 +0000

    1487764239 anticultural

---
+++ 1487764239.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3216769051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 12;
+   volatile int32_t  f1;
+   uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[6] = {2UL,4294967292UL,2UL,2UL,4294967292UL,2UL};
+static volatile int32_t g_3 = (-6L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[6] = {0x690F1D89L,0x690F1D89L,0x690F1D89L,0x690F1D89L,0x690F1D89L,0x690F1D89L};
