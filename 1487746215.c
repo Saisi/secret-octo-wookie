b7f2ccfb94ad09e61b61216f807b2bfd83d97b69
saisi <saisi@users.noreply.github.com>
2017-02-22 06:50:15 +0000

    1487746215 anviltops

---
+++ 1487746215.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      978454579
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-5L);
+static volatile uint8_t g_16 = 0x2BL;/* VOLATILE GLOBAL g_16 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
