3de27ce57a3d53b3ab29baa8b122e9aa43fd68ca
saisi <saisi@users.noreply.github.com>
2017-02-23 06:22:31 +0000

    1487830951 addlepated

---
+++ 1487830951.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      432164718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2][3] = {{(-1L),9L,(-1L)},{(-1L),9L,(-1L)}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
