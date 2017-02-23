a20ea6fe2bd805f5f2bb0277fac5f7b26278732c
saisi <saisi@users.noreply.github.com>
2017-02-23 18:23:33 +0000

    1487874210 Bainter

---
+++ 1487874210.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4002651107
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile uint8_t  f1;
+   volatile uint32_t  f2;
+   volatile uint16_t  f3;
+   int32_t  f4;
+   volatile uint16_t  f5;
+   uint32_t  f6;
+   int32_t  f7;
+   volatile uint32_t  f8;
