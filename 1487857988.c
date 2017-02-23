899824ab004eef5a2fec1f4253ed1e7196468919
saisi <saisi@users.noreply.github.com>
2017-02-23 13:53:09 +0000

    1487857988 apron

---
+++ 1487857988.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1399671240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 18;
+   const uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-7L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-4L);
+static int32_t g_5 = (-1L);
+static volatile int32_t g_6 = 0xE5C285CEL;/* VOLATILE GLOBAL g_6 */
