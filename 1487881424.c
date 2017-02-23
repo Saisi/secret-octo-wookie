39498948664127520e319ad658a6362a696082ac
saisi <saisi@users.noreply.github.com>
2017-02-23 20:23:44 +0000

    1487881424 afterswell

---
+++ 1487881424.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3282829185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[5][3] = {{(-1L),(-1L),(-1L)},{(-8L),0x125D16DFL,(-8L)},{(-1L),(-1L),(-1L)},{(-8L),0x125D16DFL,(-8L)},{(-1L),(-1L),(-1L)}};
+static volatile int32_t g_3 = 0x891F782AL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x6CCB0362L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x810D21DFL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0x283066C9L;/* VOLATILE GLOBAL g_6 */
