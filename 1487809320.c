d71607108ca4364f047afad314acb15be90097b0
saisi <saisi@users.noreply.github.com>
2017-02-23 00:22:00 +0000

    1487809320 Andra

---
+++ 1487809320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2778135256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 26;
+   signed f1 : 7;
+   volatile unsigned f2 : 13;
+   const unsigned f3 : 5;
+   const unsigned : 0;
+   const volatile unsigned f4 : 23;
+   volatile signed f5 : 25;
+   unsigned f6 : 10;
+   const volatile signed f7 : 6;
