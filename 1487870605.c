bc1d7d4e82c268b136109fab9839a254d4c78196
saisi <saisi@users.noreply.github.com>
2017-02-23 17:23:26 +0000

    1487870605 awayness

---
+++ 1487870605.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1775865876
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+   volatile unsigned f1 : 13;
+   volatile unsigned f2 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_16 = 0x0EL;/* VOLATILE GLOBAL g_16 */
+static int64_t g_20 = 0xD49598AAF0A9B27FLL;
+static uint32_t g_23 = 0xFA1E1C5AL;
