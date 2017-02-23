fd9a1666390984f72973cf3d18bcd0e667ee207e
saisi <saisi@users.noreply.github.com>
2017-02-23 14:53:13 +0000

    1487861593 appet

---
+++ 1487861593.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2904781811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 24;
+   int8_t * const  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x7632667AL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[2] = {0xE6BACD79L,0xE6BACD79L};
+static volatile int32_t g_6 = 0x543BEB71L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xB6EC79CFL;
