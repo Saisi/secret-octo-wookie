b3e9f1b54253e9dc4c640a8a5d9eee4c9cd5dcd2
saisi <saisi@users.noreply.github.com>
2017-02-23 05:52:29 +0000

    1487829149 antemarginal

---
+++ 1487829149.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      144519059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   uint8_t  f1;
+   int32_t  f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   const uint64_t  f0;
+};
