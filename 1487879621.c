cfbc6dec5cf285be990b8f11f688877a5451a3eb
saisi <saisi@users.noreply.github.com>
2017-02-23 19:53:42 +0000

    1487879621 antitropy

---
+++ 1487879621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2403812240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 22;
+   int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_14 = (-5L);
+static const int8_t * volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
+static int32_t g_23 = 7L;
+static int32_t * const  volatile g_22 = &g_23;/* VOLATILE GLOBAL g_22 */
