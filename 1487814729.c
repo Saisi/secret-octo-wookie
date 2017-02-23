2c6163c0b0b9a84f7bac4c82ef7662046e4c0982
saisi <saisi@users.noreply.github.com>
2017-02-23 01:52:10 +0000

    1487814729 amaryllid

---
+++ 1487814729.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      601625765
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 17;
+   unsigned f1 : 4;
+   volatile signed f2 : 23;
+   const int32_t  f3;
+   volatile unsigned f4 : 31;
+   unsigned f5 : 17;
+   volatile signed f6 : 16;
+   const int32_t  f7;
+};
