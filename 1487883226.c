b9087a5578b6742b45100e96b8ca495cf4f26675
saisi <saisi@users.noreply.github.com>
2017-02-23 20:53:48 +0000

    1487883226 arboraceous

---
+++ 1487883226.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3814916221
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 29;
+   unsigned : 0;
+   unsigned f1 : 3;
+   volatile uint64_t  f2;
+};
+#pragma pack(pop)
+
