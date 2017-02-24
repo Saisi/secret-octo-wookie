86d716f0bd3eb82d688c68d5ea744c66a0c2006b
saisi <saisi@users.noreply.github.com>
2017-02-24 16:25:37 +0000

    1487953537 ampulla

---
+++ 1487953537.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      650998493
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 13;
+   signed f1 : 16;
+   const volatile unsigned f2 : 19;
+   volatile unsigned f3 : 18;
+   volatile unsigned f4 : 27;
+   unsigned f5 : 8;
+   const uint64_t  f6;
