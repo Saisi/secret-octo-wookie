edfc00dae34d472a82794c37783736d27ed084e0
saisi <saisi@users.noreply.github.com>
2017-02-24 23:26:14 +0000

    1487978774 adulates

---
+++ 1487978774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1761332601
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   int32_t  f1;
+};
+
+union U1 {
+   int32_t  f0;
+   volatile unsigned f1 : 17;
+   uint64_t  f2;
+   signed f3 : 18;
