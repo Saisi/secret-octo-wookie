544e93cc071bf7a7a7c5b39c478b1bb190f82f70
saisi <saisi@users.noreply.github.com>
2017-02-24 16:55:39 +0000

    1487955339 adjutor

---
+++ 1487955339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      789173325
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 5;
+   volatile int32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_12 = 0x7D2038E8L;/* VOLATILE GLOBAL g_12 */
+static const int16_t g_13 = 0x1AF7L;
+static int64_t g_46[1][5] = {{0x28694A219909D073LL,0x28694A219909D073LL,0x28694A219909D073LL,0x28694A219909D073LL,0x28694A219909D073LL}};
