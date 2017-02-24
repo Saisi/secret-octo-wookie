fb0ea8ac800e06daac4d934e1be2649a33c854cd
saisi <saisi@users.noreply.github.com>
2017-02-24 02:24:22 +0000

    1487903061 alisma

---
+++ 1487903061.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3544842392
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint16_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_15 = 0x11151C3C39BA095ALL;
+static int64_t g_31 = 0xF9C77509CB0BEFCDLL;
