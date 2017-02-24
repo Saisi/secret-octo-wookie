016621fde8fcf901ae835879feba5a91e3855114
saisi <saisi@users.noreply.github.com>
2017-02-24 01:54:19 +0000

    1487901259 ashcake

---
+++ 1487901259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1906826677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 4L;
+static uint8_t g_9 = 0xD4L;
+static volatile int64_t g_17 = 0x82C4475EE4C20F74LL;/* VOLATILE GLOBAL g_17 */
+static volatile int8_t g_19[3] = {(-1L),(-1L),(-1L)};
+static uint64_t g_31 = 18446744073709551615UL;
+static uint16_t g_37[2][8] = {{0xF0DCL,65535UL,0xF0DCL,0UL,0UL,0xF0DCL,65535UL,0xF0DCL},{0x3143L,0UL,0xB636L,0UL,0x3143L,0x3143L,0UL,0xB636L}};
+static int32_t *g_70[2][1] = {{&g_8},{&g_8}};
+static int32_t **g_69[7] = {&g_70[0][0],&g_70[0][0],&g_70[0][0],&g_70[0][0],&g_70[0][0],&g_70[0][0],&g_70[0][0]};
+static int32_t g_74 = 0xAD1DC2CBL;
