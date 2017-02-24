e0433893d8b0fb1de9055c5b40f77327604d627e
saisi <saisi@users.noreply.github.com>
2017-02-24 08:54:54 +0000

    1487926494 advocatrix

---
+++ 1487926494.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3379831035
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_8 = 0x0FL;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_10 = 0x035C5735L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t *g_9 = &g_10;
+static int32_t g_12 = 0x2AB76D60L;
+static uint16_t *g_52 = (void*)0;
+static int32_t g_58 = (-1L);
+static int32_t g_60 = (-1L);
+static const uint16_t g_95[6] = {0xB57DL,0xB57DL,0xB57DL,0xB57DL,0xB57DL,0xB57DL};
+static int32_t g_106 = 5L;
