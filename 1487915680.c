df79f21958c5e2c5509944068c458fb72da41c63
saisi <saisi@users.noreply.github.com>
2017-02-24 05:54:41 +0000

    1487915680 arditi

---
+++ 1487915680.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      497548047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6] = {(-7L),(-7L),(-7L),(-7L),(-7L),(-7L)};
+static int32_t g_17 = 0L;
+static int64_t g_27 = 0x4307976B5A7DD9AELL;
+static uint8_t g_63 = 0x48L;
+static int64_t g_66 = 0x1AC573C098B3027FLL;
+static int16_t g_70 = 0xA128L;
+static uint64_t g_84 = 18446744073709551615UL;
+static int32_t g_125[2] = {0xD1E45324L,0xD1E45324L};
+static int32_t *g_127 = &g_125[1];
