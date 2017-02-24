c3ca0d1cc8705bf0e243ace8e563f678b7e30860
saisi <saisi@users.noreply.github.com>
2017-02-24 05:24:38 +0000

    1487913878 amounters

---
+++ 1487913878.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3698747334
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_6 = (void*)0;
+static uint8_t g_20 = 0x9CL;
+static uint32_t g_29 = 3UL;
+static int64_t g_73 = 0xC1552C25470E9879LL;
+static uint8_t g_93 = 0x8DL;
+static uint32_t g_96 = 0UL;
+static int64_t g_98 = 0x4EDB9D72FAF72239LL;
+static int32_t * const  volatile * const g_101 = &g_6;
+static int32_t * const  volatile * const *g_100 = &g_101;
