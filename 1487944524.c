358a4dff10095f302a40630d080502af1c7807b2
saisi <saisi@users.noreply.github.com>
2017-02-24 13:55:24 +0000

    1487944524 Bantingism

---
+++ 1487944524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2892908879
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = 0x5EF2C365L;
+static struct S0 g_19 = {-6};
+static uint16_t g_29 = 65534UL;
+static uint32_t g_61[1] = {0xBAC609E8L};
+static uint16_t g_65[10] = {65535UL,8UL,0x45A3L,8UL,65535UL,65535UL,8UL,0x45A3L,8UL,65535UL};
