733b859b6cf344909176612d059b212917b8053e
saisi <saisi@users.noreply.github.com>
2017-02-24 15:25:32 +0000

    1487949932 annulosa

---
+++ 1487949932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3982331620
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S2 {
+   unsigned f0 : 11;
+   volatile unsigned : 0;
+   const volatile signed f1 : 22;
+   unsigned f2 : 28;
+   volatile int16_t  f3;
+   volatile unsigned f4 : 28;
+   unsigned f5 : 23;
