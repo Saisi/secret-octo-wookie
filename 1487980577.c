f29808e4e3d2275e891b3179a8d856e70f27692a
saisi <saisi@users.noreply.github.com>
2017-02-24 23:56:17 +0000

    1487980577 arcature

---
+++ 1487980577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2715889606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static uint16_t g_22[1] = {0xCF04L};
+static volatile uint8_t g_25 = 255UL;/* VOLATILE GLOBAL g_25 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
