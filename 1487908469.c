6ada1b930aa71d0aadb182a8e1252c55d1fc4d53
saisi <saisi@users.noreply.github.com>
2017-02-24 03:54:30 +0000

    1487908469 axolemma

---
+++ 1487908469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3248862313
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   signed f1 : 30;
+   signed f2 : 9;
+   signed f3 : 16;
+   const signed f4 : 1;
+   signed f5 : 22;
+   unsigned f6 : 29;
+   unsigned f7 : 28;
+   unsigned f8 : 15;
