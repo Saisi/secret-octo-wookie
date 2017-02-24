fce74f439af2b9b5ab4a1d3baeb748ef3861c877
saisi <saisi@users.noreply.github.com>
2017-02-24 19:55:53 +0000

    1487966153 agenesis

---
+++ 1487966153.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3341376258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   signed f0 : 16;
+   uint8_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
