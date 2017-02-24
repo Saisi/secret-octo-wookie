8b2e0c1493edaaad9522bb2502fdc8c54a10580b
saisi <saisi@users.noreply.github.com>
2017-02-24 00:24:11 +0000

    1487895849 Almerian

---
+++ 1487895849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3042349932
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 4;
+   const unsigned f1 : 9;
+   signed f2 : 7;
+   const signed f3 : 23;
+   unsigned f4 : 10;
+   const volatile signed f5 : 25;
+   const signed f6 : 19;
+   signed f7 : 28;
+   unsigned f8 : 16;
