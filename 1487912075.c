b9353bb2fbf1b264d48c98c7b641f63536b219b8
saisi <saisi@users.noreply.github.com>
2017-02-24 04:54:36 +0000

    1487912075 Almyra

---
+++ 1487912075.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2744429135
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x57FD5464L;
+static int64_t g_16[9][4] = {{0xA56D104A74307F07LL,0L,0L,(-6L)},{0L,0L,0L,0xB07E043BC77BB9FELL},{0L,0xF67138D59A4365C1LL,0L,(-1L)},{(-1L),(-6L),0xA56D104A74307F07LL,0xF67138D59A4365C1LL},{(-4L),0xA56D104A74307F07LL,0xA56D104A74307F07LL,(-4L)},{(-1L),0xB07E043BC77BB9FELL,0L,0L},{0L,0x969097B3892EBBAELL,0L,0xE52AB26C3A74F570LL},{0L,0xE52AB26C3A74F570LL,0L,0xE52AB26C3A74F570LL},{0xA56D104A74307F07LL,0x969097B3892EBBAELL,(-6L),0L}};
+static uint32_t g_25 = 0xD1175432L;
+static int64_t g_27 = 0x0A27EA102817ACDDLL;
+static int64_t *g_26 = &g_27;
+static int16_t g_60 = 0xED1FL;
+static int32_t g_69 = 0L;
+static uint8_t g_96 = 0x87L;
+static int16_t g_99 = (-1L);
