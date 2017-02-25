55bbb26ab56a48b3503dfdead48a2e34a520c424
saisi <saisi@users.noreply.github.com>
2017-02-25 15:27:58 +0000

    1488036477 Azarria

---
+++ 1488036477.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2716677240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint32_t  f1;
+   const unsigned f2 : 17;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = (-1L);
+static int32_t g_63 = 1L;
