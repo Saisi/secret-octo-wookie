f57c838b0a480ccdc1b53da5ddf7820d70587cab
saisi <saisi@users.noreply.github.com>
2017-02-25 14:57:56 +0000

    1488034675 Alvordton

---
+++ 1488034675.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2198352104
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int8_t g_6 = 4L;
+static int32_t g_8 = 0x01FC3EEEL;
+static int16_t g_17 = 1L;
+static int8_t g_53 = 0x3EL;
+static uint8_t g_60 = 0x7BL;
+static uint64_t g_64 = 4UL;
+static uint32_t g_71[3] = {18446744073709551612UL,18446744073709551612UL,18446744073709551612UL};
+static int32_t *g_81[5] = {&g_8,&g_8,&g_8,&g_8,&g_8};
