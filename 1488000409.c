9a80d28e7067df4308c4577e31f6fbff180b0c31
saisi <saisi@users.noreply.github.com>
2017-02-25 05:26:50 +0000

    1488000409 atheist

---
+++ 1488000409.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3550220980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 8;
+   volatile unsigned f1 : 29;
+   signed f2 : 11;
+   volatile unsigned f3 : 11;
+   unsigned f4 : 14;
+   volatile signed f5 : 13;
+   signed f6 : 23;
