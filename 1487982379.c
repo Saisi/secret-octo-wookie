f5c69ed1bec1308c31570894e7d5afbab56ae32c
saisi <saisi@users.noreply.github.com>
2017-02-25 00:26:19 +0000

    1487982379 baning

---
+++ 1487982379.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3559966403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x1E9C96C7L;
+static int16_t g_22 = 0x9CA3L;
+static const int32_t * volatile g_67 = &g_8;/* VOLATILE GLOBAL g_67 */
+static const int32_t * volatile * volatile g_66 = &g_67;/* VOLATILE GLOBAL g_66 */
+static int16_t *g_81[1] = {&g_22};
+static int16_t **g_80 = &g_81[0];
+static int8_t g_83[1][10] = {{0L,(-1L),0xFBL,(-1L),0L,0L,(-1L),0xFBL,(-1L),0L}};
+static uint16_t g_109 = 0xD7ACL;
+static uint16_t g_111 = 1UL;
