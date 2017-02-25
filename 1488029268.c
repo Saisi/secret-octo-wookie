0ab7cc5d79c4c9387fdb2984249a882ca123a167
saisi <saisi@users.noreply.github.com>
2017-02-25 13:27:48 +0000

    1488029268 Balearian

---
+++ 1488029268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1161647171
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-3L);
+static uint64_t g_5 = 0x3F9E923E64F14790LL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
