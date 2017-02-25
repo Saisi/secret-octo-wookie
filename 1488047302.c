e9c89de7bf2238c124042c36910e9bc21477e06b
saisi <saisi@users.noreply.github.com>
2017-02-25 18:28:23 +0000

    1488047302 Alope

---
+++ 1488047302.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1159803038
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_9 = 0x59L;
+static int32_t g_43[6] = {7L,7L,7L,7L,7L,7L};
+static int32_t g_68 = 0x8C32619CL;
+static volatile uint64_t g_75 = 0x807C1F4B9EFECF47LL;/* VOLATILE GLOBAL g_75 */
+static int64_t g_102[3] = {0xB711A46C428827C8LL,0xB711A46C428827C8LL,0xB711A46C428827C8LL};
