a416a8936f4c14bbafd3bfdb950c07c38b4764a1
saisi <saisi@users.noreply.github.com>
2017-02-25 10:57:33 +0000

    1488020251 asthenobiosis

---
+++ 1488020251.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1070226298
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int8_t  f1;
+   uint64_t  f2;
+   int64_t  f3;
+   int8_t  f4;
+   int64_t  f5;
+   unsigned f6 : 27;
+   int8_t  f7;
+};
