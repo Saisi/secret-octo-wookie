65beeeba7f8dead2baa3436d2d7d69d0376b21f5
saisi <saisi@users.noreply.github.com>
2017-02-25 08:27:13 +0000

    1488011232 artels

---
+++ 1488011232.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3310207885
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 5;
+   const signed f1 : 8;
+   const unsigned f2 : 11;
+   const signed f3 : 10;
+   int64_t  f4;
+   signed f5 : 5;
+};
+
+/* --- GLOBAL VARIABLES --- */
