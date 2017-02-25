7732e665ddce2764a9b873354b38a228b0129207
saisi <saisi@users.noreply.github.com>
2017-02-25 05:56:52 +0000

    1488002212 Aecidiomycetes

---
+++ 1488002212.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      772210921
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6] = {(-5L),(-4L),(-5L),(-5L),(-4L),(-5L)};
+static int32_t g_3 = (-4L);
+static uint64_t g_41 = 0xC869B7D6B59229A2LL;
+static uint32_t g_63 = 0xC786C7A8L;
