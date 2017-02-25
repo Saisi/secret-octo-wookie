deb45e3bdaba8af08a8891e1fb1d92761dc623e5
saisi <saisi@users.noreply.github.com>
2017-02-25 11:57:38 +0000

    1488023857 adatis

---
+++ 1488023857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2568104642
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 15;
+   unsigned f1 : 8;
+   unsigned f2 : 10;
+   const unsigned f3 : 14;
+   const volatile signed f4 : 5;
+   const unsigned f5 : 8;
+};
+
+#pragma pack(push)
