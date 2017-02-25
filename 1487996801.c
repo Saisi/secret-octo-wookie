1f7eb21cc235e40164bc9275d802c0b6e2b05c57
saisi <saisi@users.noreply.github.com>
2017-02-25 04:26:44 +0000

    1487996801 anagramming

---
+++ 1487996801.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2765515477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int32_t  f1;
+   volatile int8_t * const  volatile  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-2L);
+static int8_t *g_5 = (void*)0;
+static volatile union U0 g_10 = {0x223FA3A92BD42CA8LL};/* VOLATILE GLOBAL g_10 */
