e8bf0266eb0a200e43849a94c3646d123abb256f
saisi <saisi@users.noreply.github.com>
2017-02-25 14:27:53 +0000

    1488032873 adaptor

---
+++ 1488032873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1152585379
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 4294967295UL;
+static uint32_t g_52 = 0x1BB35B68L;
+static int64_t g_67 = 0x3D4F39AB1B57606ALL;
+static int32_t * volatile g_91 = (void*)0;/* VOLATILE GLOBAL g_91 */
+static int32_t * volatile g_92 = (void*)0;/* VOLATILE GLOBAL g_92 */
+static int32_t g_94 = 9L;
+static int32_t *g_99 = (void*)0;
+static const int64_t *g_101 = (void*)0;
+static int64_t g_161 = 5L;
