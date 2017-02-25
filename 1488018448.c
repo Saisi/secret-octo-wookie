0a8f83edb9363606a84d5e6d05a61624ba8e9b44
saisi <saisi@users.noreply.github.com>
2017-02-25 10:27:29 +0000

    1488018448 amplexicauline

---
+++ 1488018448.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3354405343
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+   volatile unsigned f1 : 13;
+   volatile unsigned f2 : 28;
+   signed f3 : 23;
+   unsigned f4 : 12;
+   signed f5 : 21;
+   volatile unsigned : 0;
+   const unsigned f6 : 19;
+   volatile signed f7 : 10;
