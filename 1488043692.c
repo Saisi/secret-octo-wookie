0ca31803f98d9a124e9f9278bb4bf2f1c51093eb
saisi <saisi@users.noreply.github.com>
2017-02-25 17:28:12 +0000

    1488043692 angiostomize

---
+++ 1488043692.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3912931057
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1] = {0x59E57AD9L};
+static volatile uint8_t g_4 = 1UL;/* VOLATILE GLOBAL g_4 */
+static int8_t g_24 = 1L;
+static int8_t g_35 = 0L;
+static int8_t *g_34[10][1] = {{(void*)0},{&g_35},{(void*)0},{&g_35},{(void*)0},{&g_35},{(void*)0},{&g_35},{(void*)0},{&g_35}};
+static uint16_t g_57 = 0xF2FDL;
+static uint16_t g_61 = 0xD978L;
+static uint16_t * const  volatile g_60 = &g_61;/* VOLATILE GLOBAL g_60 */
+static int32_t g_98 = 0x9E278474L;
