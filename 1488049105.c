bc841daf0f15f27dca6232a2a869eac32c4fffd6
saisi <saisi@users.noreply.github.com>
2017-02-25 18:58:25 +0000

    1488049105 Avigdor

---
+++ 1488049105.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2549177133
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   unsigned f1 : 24;
+   const volatile unsigned f2 : 27;
+   signed f3 : 26;
+   const unsigned f4 : 3;
+   signed f5 : 16;
+   const unsigned f6 : 15;
