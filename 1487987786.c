63eb13be3091801bbdf4eb3858247002550c0054
saisi <saisi@users.noreply.github.com>
2017-02-25 01:56:27 +0000

    1487987786 archgenethliac

---
+++ 1487987786.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1633966471
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile int16_t  f1;
+   const unsigned f2 : 9;
+   int32_t  f3;
+   const signed f4 : 8;
+   unsigned f5 : 1;
+   uint8_t  f6;
+};
+
