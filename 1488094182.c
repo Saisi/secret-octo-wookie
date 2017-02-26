8b21966a8f05601457ee56bbaf250c94f4894a7c
saisi <saisi@users.noreply.github.com>
2017-02-26 07:29:43 +0000

    1488094182 acapnias

---
+++ 1488094182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3359269844
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0x3248C66FL;
+static int32_t g_17 = 0x52568108L;
+static volatile int32_t g_21 = 0x912530A9L;/* VOLATILE GLOBAL g_21 */
+static volatile int32_t * volatile g_20 = &g_21;/* VOLATILE GLOBAL g_20 */
+static uint32_t g_30 = 4294967294UL;
