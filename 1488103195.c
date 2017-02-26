1e5a6e1ed356522452fdbfc912d9329191ba3206
saisi <saisi@users.noreply.github.com>
2017-02-26 09:59:55 +0000

    1488103195 bacons

---
+++ 1488103195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3171520857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 11;
+   const volatile signed f1 : 22;
+   const signed f2 : 1;
+   volatile signed f3 : 8;
+   unsigned f4 : 25;
+   const volatile uint64_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
