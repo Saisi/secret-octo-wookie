b156626f6aeadddcf398430ce8662fbc498b609b
saisi <saisi@users.noreply.github.com>
2017-02-26 21:30:59 +0000

    1488144658 anarcotin

---
+++ 1488144658.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1858825166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   uint64_t  f1;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_8 = 4L;
+static uint8_t g_17 = 0x94L;
+static uint32_t g_20 = 0xB5486B6FL;
