944d83a3c5bf926f2abaed7fb31b0e4e8082d29f
saisi <saisi@users.noreply.github.com>
2017-02-26 20:00:51 +0000

    1488139251 affidavits

---
+++ 1488139251.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3442864512
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-2L);
+static uint16_t g_9 = 65535UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
