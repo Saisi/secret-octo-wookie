031148cfce3cb9aed3f90d6da1d5fdbce735b590
saisi <saisi@users.noreply.github.com>
2017-02-26 19:00:47 +0000

    1488135646 aerobacteriologically

---
+++ 1488135646.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1017360007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_13 = 0xAB593421L;/* VOLATILE GLOBAL g_13 */
+static uint8_t g_27 = 0x35L;
+static uint16_t g_35 = 1UL;
+static uint8_t g_45 = 0x1AL;
+static uint8_t *g_44 = &g_45;
+static uint16_t g_47 = 65535UL;
+static uint16_t * const g_46 = &g_47;
+static int8_t g_53 = 8L;
+static int64_t g_56 = 0xEFD52276C1F5E2D0LL;
