72ab26c32788d048d3f14122391841de7b3bb1e0
saisi <saisi@users.noreply.github.com>
2017-02-26 09:29:53 +0000

    1488101392 amphilogism

---
+++ 1488101392.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2409825842
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile uint64_t  f1;
+   int16_t  f2;
+   int32_t  f3;
+   const volatile int32_t  f4;
+   signed f5 : 29;
+   const uint64_t  f6;
+   uint16_t  f7;
+   unsigned f8 : 16;
