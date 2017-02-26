3ca53e11aee799ec24ece417545329c395063b8b
saisi <saisi@users.noreply.github.com>
2017-02-26 14:00:20 +0000

    1488117619 ballotist

---
+++ 1488117619.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      424961340
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 12;
+   signed f1 : 17;
+   signed f2 : 15;
+   unsigned f3 : 31;
+   signed f4 : 18;
+   const unsigned f5 : 1;
+   unsigned f6 : 27;
