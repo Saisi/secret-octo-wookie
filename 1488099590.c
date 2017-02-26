4fc87dd69f5ca91c6261b1a507eb74f69dd32296
saisi <saisi@users.noreply.github.com>
2017-02-26 08:59:50 +0000

    1488099590 azorubine

---
+++ 1488099590.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      908741746
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 7;
+   unsigned : 0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_17 = 250UL;
+static int32_t g_19 = 0L;
+static int32_t g_32 = 0xCCEDB775L;
+static int32_t *g_31 = &g_32;
