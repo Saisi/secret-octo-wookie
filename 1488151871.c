7d023acf87acb6551ffb2aa8e2fcd25e292008ba
saisi <saisi@users.noreply.github.com>
2017-02-26 23:31:12 +0000

    1488151871 agriology

---
+++ 1488151871.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3546676003
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 16;
+   uint16_t  f1;
+   int64_t  f2;
+   signed f3 : 29;
+   uint32_t  f4;
+   int32_t  f5;
+   uint32_t  f6;
