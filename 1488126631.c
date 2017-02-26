ca90fc19eaffc673b180cce3587e46ad5c3b5859
saisi <saisi@users.noreply.github.com>
2017-02-26 16:30:32 +0000

    1488126631 algedo

---
+++ 1488126631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      9266416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 6;
+   volatile unsigned f1 : 3;
+   signed f2 : 25;
+   signed f3 : 1;
+   signed f4 : 12;
+   signed f5 : 19;
+   signed f6 : 5;
