f5558f3066a53d6448cda6c14cda9d12d474d2a2
saisi <saisi@users.noreply.github.com>
2017-02-26 22:01:02 +0000

    1488146461 Ancyrean

---
+++ 1488146461.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2299091334
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 17;
+   const signed f1 : 2;
+   unsigned f2 : 28;
+   signed f3 : 24;
+   const int32_t  f4;
+   int8_t  f5;
+   volatile signed f6 : 23;
+   volatile uint64_t  f7;
+   signed f8 : 9;
