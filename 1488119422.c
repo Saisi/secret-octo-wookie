6146fa79d2f975440caf5e00b008ff38b9bf8481
saisi <saisi@users.noreply.github.com>
2017-02-26 14:30:22 +0000

    1488119422 barberish

---
+++ 1488119422.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1060140238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 21;
+   unsigned f1 : 22;
+   unsigned : 0;
+   unsigned f2 : 12;
+   signed f3 : 11;
+   signed f4 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
