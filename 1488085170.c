565d2e92203ab04d533c7524ed14bd8d8d93edba
saisi <saisi@users.noreply.github.com>
2017-02-26 04:59:30 +0000

    1488085170 accusal

---
+++ 1488085170.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3074722252
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_2 = (-5L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
