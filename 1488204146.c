476c3fc81c55d85f5aa69c6e4e5f01467a0c0df0
saisi <saisi@users.noreply.github.com>
2017-02-27 14:02:26 +0000

    1488204146 azogrenadine

---
+++ 1488204146.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2180493523
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 7L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[9][5] = {{(-1L),9L,(-1L),(-1L),9L},{0L,0L,0L,0L,0L},{9L,9L,0xACD79536L,9L,9L},{0L,0L,0L,0L,0L},{9L,(-1L),(-1L),9L,(-1L)},{0L,0L,0x97632667L,0L,0L},{(-1L),9L,(-1L),(-1L),9L},{0L,0L,0L,0L,0L},{9L,9L,0xACD79536L,9L,9L}};
+static int32_t g_7 = 1L;
+static int8_t g_30 = 3L;
+static volatile int8_t g_31 = (-1L);/* VOLATILE GLOBAL g_31 */
+static uint8_t g_43 = 255UL;
+static uint8_t g_81 = 0x08L;
+static int32_t g_107 = 0L;
+static int64_t g_127 = 0x33B2D9701DEE0D32LL;
