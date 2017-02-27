06f11fac53175c41b8b2950cbb4ed80f6e25db9a
saisi <saisi@users.noreply.github.com>
2017-02-27 07:31:53 +0000

    1488180713 angiorrhaphy

---
+++ 1488180713.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2893529913
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   uint16_t  f1;
+   const int8_t  f2;
+   const uint64_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 5L;
