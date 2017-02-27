5612e93b93c814b6d7751c54b279ebf33c48b258
saisi <saisi@users.noreply.github.com>
2017-02-27 12:02:16 +0000

    1488196936 Alvadore

---
+++ 1488196936.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3653923984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   uint8_t  f1;
+   uint32_t  f2;
+   unsigned f3 : 17;
+};
+
+union U1 {
+   volatile signed f0 : 17;
+   uint32_t  f1;
