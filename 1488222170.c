0510b4dbaccd5be5274620b91949562adfaa7858
saisi <saisi@users.noreply.github.com>
2017-02-27 19:02:50 +0000

    1488222170 astrogated

---
+++ 1488222170.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      819740211
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint8_t  f1;
+   signed f2 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = (-1L);
+static int16_t g_12[5][5] = {{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xB200L,1L,0xB200L,1L,0xB200L},{(-6L),(-6L),(-6L),(-6L),(-6L)},{0xB200L,1L,0xB200L,1L,0xB200L},{(-6L),(-6L),(-6L),(-6L),(-6L)}};
+static int16_t g_25 = 0x017FL;
