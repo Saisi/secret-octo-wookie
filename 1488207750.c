e708cb03bc187cf00af292ac3b01d310a120b648
saisi <saisi@users.noreply.github.com>
2017-02-27 15:02:31 +0000

    1488207750 acanthotic

---
+++ 1488207750.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3030052167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2 = 0UL;
+static uint64_t g_18 = 18446744073709551606UL;
+static uint64_t g_41 = 0xA335C22C92085949LL;
+static uint64_t *g_40 = &g_41;
+static const struct S0 g_67 = {1};
