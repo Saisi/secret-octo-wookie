dd18d824dc3af3b42dbb940e4318148847e59c4c
saisi <saisi@users.noreply.github.com>
2017-02-27 08:01:56 +0000

    1488182516 Archbp

---
+++ 1488182516.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2927238730
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 19;
+   signed f1 : 28;
+   volatile unsigned f2 : 14;
+   unsigned f3 : 2;
+   signed f4 : 9;
+   signed f5 : 28;
+   unsigned f6 : 10;
