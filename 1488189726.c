0c5febb30f8c1a940febcab5a3b7b4a752c83610
saisi <saisi@users.noreply.github.com>
2017-02-27 10:02:07 +0000

    1488189726 autonephrotoxin

---
+++ 1488189726.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      353601168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 1L;
+static int32_t g_14 = 0x409884CBL;
+static int16_t g_17 = 0L;
+static uint16_t g_18 = 65532UL;
+static uint64_t g_22[6] = {2UL,2UL,2UL,2UL,2UL,2UL};
+static int32_t * volatile g_32 = &g_14;/* VOLATILE GLOBAL g_32 */
+static uint32_t g_36 = 1UL;
+static int32_t g_41 = 0x77D900D2L;
+static uint16_t g_79[7] = {65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL};
