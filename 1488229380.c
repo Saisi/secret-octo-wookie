72c7148e6ca82eaeb22045310e2e299925d1e9d2
saisi <saisi@users.noreply.github.com>
2017-02-27 21:03:02 +0000

    1488229380 apnoea

---
+++ 1488229380.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1574222244
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_2 = 0xF6L;
+static uint16_t g_41 = 6UL;
+static int32_t g_54 = 0xE89405DBL;
+static int8_t g_64 = 0x62L;
