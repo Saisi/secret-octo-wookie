bd2a6d382feeba2e660c8d71a985e7c49696095e
saisi <saisi@users.noreply.github.com>
2017-02-27 13:32:24 +0000

    1488202344 advoke

---
+++ 1488202344.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1853793150
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_22[4] = {0xE3E7L,0xE3E7L,0xE3E7L,0xE3E7L};
+static uint8_t g_23 = 0xDEL;
+static int32_t g_33 = 0x9973063DL;
+static uint16_t g_35 = 0x73AEL;
+static int8_t g_59 = 0xD0L;
+static uint8_t g_79 = 0xE2L;
+static uint32_t g_82 = 4294967293UL;
+static uint64_t g_85 = 0x5B50A70091F4D940LL;
+static uint64_t g_87 = 1UL;
