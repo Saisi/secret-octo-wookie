44e8ace5d40d7b9c0f1ea108a96c81449fc07027
saisi <saisi@users.noreply.github.com>
2017-02-27 02:31:27 +0000

    1488162687 Aethalides

---
+++ 1488162687.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1765081868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+   uint64_t  f3;
+   signed f4 : 31;
+};
+
+union U1 {
+   signed f0 : 21;
