4264d780da9961270d477443a5ba6b754cc9d6a9
saisi <saisi@users.noreply.github.com>
2017-02-27 01:31:22 +0000

    1488159082 arcticized

---
+++ 1488159082.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      510384726
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x24F01C08L;
+static int32_t g_25 = 0x3EC93CC2L;
+static int16_t g_27 = 5L;
+static int64_t g_28 = 0L;
+static int8_t g_29 = 0xAFL;
+static uint32_t g_30 = 0xDF890BA5L;
+static uint8_t g_56 = 0x48L;
+static int32_t g_76 = (-6L);
+static int32_t * volatile g_75 = &g_76;/* VOLATILE GLOBAL g_75 */
