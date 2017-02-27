ce605a9bea2b3f1f285496a36b07298700954410
saisi <saisi@users.noreply.github.com>
2017-02-27 20:02:56 +0000

    1488225775 Arcesius

---
+++ 1488225775.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3329972810
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   volatile signed f1 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_14 = 0x0A23L;
+static uint16_t g_19 = 0x1509L;
+static int32_t g_33[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t *g_39 = &g_33[5];
