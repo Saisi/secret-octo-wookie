e0e41a8a8c1e1eeed9e5e678957cfab2fb502d3e
saisi <saisi@users.noreply.github.com>
2017-02-27 22:33:10 +0000

    1488234790 AUTODIN

---
+++ 1488234790.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1159793134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x9B2CFB63L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x5816210BL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
