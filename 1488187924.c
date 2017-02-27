fcf22ef741713034a938443729098379576c12b4
saisi <saisi@users.noreply.github.com>
2017-02-27 09:32:04 +0000

    1488187924 acupunctures

---
+++ 1488187924.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3354179466
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x0C93B4E1L;
+static volatile int32_t g_5 = 0x8A59C176L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x17F75960L;
+static int32_t g_10 = 0L;
+static int32_t g_24 = 0xCE008FB0L;
+static int64_t g_29 = 0xDA18461E7071658ALL;
+static volatile uint32_t g_30 = 0x47F59EC1L;/* VOLATILE GLOBAL g_30 */
+static uint8_t g_56[10][5] = {{0x71L,255UL,255UL,0x71L,0x4AL},{0x84L,0x49L,1UL,1UL,0x49L},{0x4AL,255UL,1UL,0xF5L,0xF5L},{253UL,0xB4L,253UL,1UL,1UL},{0x2AL,0x71L,0xF5L,0x71L,0x2AL},{253UL,0x84L,0xB4L,0x49L,0xB4L},{0x4AL,0x4AL,0xF5L,0x2AL,0x72L},{0x84L,253UL,253UL,0x84L,0xB4L},{0x71L,0x2AL,1UL,1UL,0x2AL},{0xB4L,0x49L,1UL,0xB4L,0xB4L}};
