b9d3395f2ca82059c1f69edf093314f9f6098345
saisi <saisi@users.noreply.github.com>
2017-02-27 18:02:45 +0000

    1488218564 balloonist

---
+++ 1488218564.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2136213986
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+   volatile uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_10 = 0x6EFF3DE0L;/* VOLATILE GLOBAL g_10 */
+static uint32_t g_48[3] = {0x7A68FAB8L,0x7A68FAB8L,0x7A68FAB8L};
