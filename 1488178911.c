d9d9d2cdd6870dbe6169eb858cb60ec743daed4a
saisi <saisi@users.noreply.github.com>
2017-02-27 07:01:51 +0000

    1488178911 anatomising

---
+++ 1488178911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2152392635
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   uint8_t  f1;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 5L;
+static int32_t g_16[2] = {(-1L),(-1L)};
+static uint8_t g_28 = 0x36L;
