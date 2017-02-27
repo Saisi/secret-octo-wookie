ec16ec63a76316a5cccd6c76ada6e56634e186a3
saisi <saisi@users.noreply.github.com>
2017-02-27 23:03:13 +0000

    1488236593 Bahima

---
+++ 1488236593.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3557556713
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_19 = 0x2D3FL;
+static uint16_t g_26 = 65535UL;
+static int32_t g_36 = (-1L);
+static int32_t *g_35 = &g_36;
+static int8_t g_39 = 5L;
+static int32_t g_54 = 0x37B187B3L;
+static volatile int32_t g_56 = 2L;/* VOLATILE GLOBAL g_56 */
+static uint8_t g_64 = 0xD8L;
+static volatile int64_t g_69 = 7L;/* VOLATILE GLOBAL g_69 */
