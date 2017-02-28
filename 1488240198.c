c1a9560e4dcb7996458831f15c4173cfa5617a49
saisi <saisi@users.noreply.github.com>
2017-02-28 00:03:19 +0000

    1488240198 aspalax

---
+++ 1488240198.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      314274129
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   unsigned f1 : 2;
+   unsigned f2 : 29;
+   volatile unsigned f3 : 20;
+   signed f4 : 21;
+   int32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
