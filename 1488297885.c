eefa0c03f36d99ea4c9dee93185ee405404f6375
saisi <saisi@users.noreply.github.com>
2017-02-28 16:04:45 +0000

    1488297885 anatropous

---
+++ 1488297885.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1643957166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   signed f1 : 6;
+   signed f2 : 16;
+   unsigned f3 : 28;
+   unsigned f4 : 27;
+   signed f5 : 6;
+   volatile unsigned f6 : 12;
+   const signed f7 : 17;
+   uint8_t  f8;
