ed177339503beb5c6954c2a1fbf5684a4047f1ca
saisi <saisi@users.noreply.github.com>
2017-02-28 19:35:08 +0000

    1488310507 Acantholimon

---
+++ 1488310507.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1695174138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 11;
+   signed f1 : 20;
+   unsigned f2 : 6;
+   int32_t  f3;
+   volatile int32_t  f4;
+   const unsigned f5 : 28;
+};
