4e725a27c8c2d27876131dcd84b48ea085f67fc6
saisi <saisi@users.noreply.github.com>
2017-02-28 05:33:48 +0000

    1488260028 anthracaemia

---
+++ 1488260028.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3778306115
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xE8EDF1FEL;
+static int32_t g_5 = 0xDBECCCE1L;
+static int32_t g_8[7] = {0x928FBC78L,0x363F9E92L,0x928FBC78L,0x928FBC78L,0x363F9E92L,0x928FBC78L,0x928FBC78L};
+static int32_t g_22 = 0xC0549EC7L;
+static int8_t g_49 = 0xDAL;
