6f706f4da35b08d1c03e92898e4e424f3c2ad3c1
saisi <saisi@users.noreply.github.com>
2017-02-28 09:04:09 +0000

    1488272648 allotypies

---
+++ 1488272648.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      188231477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 25;
+   const unsigned f1 : 12;
+   volatile unsigned f2 : 31;
+   unsigned f3 : 14;
+   const signed f4 : 7;
+   volatile int32_t  f5;
+   unsigned f6 : 24;
+   signed f7 : 18;
+};
