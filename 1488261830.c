24438d68bed2524cbefa3a4163fa67f33e65c193
saisi <saisi@users.noreply.github.com>
2017-02-28 06:03:50 +0000

    1488261830 akazga

---
+++ 1488261830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      157105145
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const volatile int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[8] = {0x5B0E446DL,0x8ADE19D2L,0x5B0E446DL,0x5B0E446DL,0x8ADE19D2L,0x5B0E446DL,0x5B0E446DL,0x8ADE19D2L};
+static volatile int32_t g_3[8][4] = {{(-9L),1L,(-9L),(-5L)},{(-9L),(-5L),(-5L),(-9L)},{1L,(-5L),0xF2887CF2L,(-5L)},{(-5L),1L,0xF2887CF2L,0xF2887CF2L},{1L,1L,(-5L),0xF2887CF2L},{(-9L),1L,(-9L),(-5L)},{(-9L),(-5L),(-5L),(-9L)},{1L,(-5L),0xF2887CF2L,(-5L)}};
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0x4FC2285FL;
