415bc0619dc803759408f7eb2678711c4805e0c3
saisi <saisi@users.noreply.github.com>
2017-02-28 21:05:14 +0000

    1488315914 Balistidae

---
+++ 1488315914.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3514554864
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 6;
+   unsigned f1 : 3;
+   const signed f2 : 25;
+   signed f3 : 1;
+   signed f4 : 12;
+   uint16_t  f5;
+   signed f6 : 31;
