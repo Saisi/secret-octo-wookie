bb43b50e8695afbf487600276647d772b57a83c0
saisi <saisi@users.noreply.github.com>
2017-02-28 23:05:25 +0000

    1488323124 amphigonia

---
+++ 1488323124.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3682655160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = 0x7F5ED9A9L;
+static uint32_t g_25 = 0xB4F5AC3DL;
+static uint64_t g_34 = 0x4A9C79EBEF9F3B06LL;
+static uint16_t g_39 = 0xB7A2L;
+static int32_t *g_43 = &g_10;
+static int32_t **g_42[10][9] = {{(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0},{(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0},{(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0},{(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0},{(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0,(void*)0,&g_43,(void*)0},{(void*)0,(void*)0,&g_43,&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43},{&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43},{&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43},{&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43},{&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43,&g_43,(void*)0,&g_43}};
+static int16_t g_60[6] = {0xECA6L,0x96B4L,0xECA6L,0xECA6L,0x96B4L,0xECA6L};
+static uint64_t g_99 = 0xBBD88186F270ADFFLL;
+static uint64_t g_115 = 0UL;
