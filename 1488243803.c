2f2724ffdea649c4cf7c882a5e2762302dc7223b
saisi <saisi@users.noreply.github.com>
2017-02-28 01:03:24 +0000

    1488243803 bacterin

---
+++ 1488243803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1868814155
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 4;
+   unsigned f1 : 11;
+   uint32_t  f2;
+   unsigned f3 : 19;
+   const int16_t  f4;
+};
+
+struct S1 {
+   volatile unsigned f0 : 10;
