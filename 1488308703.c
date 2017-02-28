b466ab45399eaf61933e337667f99e9893e26573
saisi <saisi@users.noreply.github.com>
2017-02-28 19:05:03 +0000

    1488308703 Arctogea

---
+++ 1488308703.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1942095705
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_10 = 0xC9ED9BA0L;
+static uint8_t g_25 = 0x5CL;
+static int32_t g_30 = (-1L);
+static int32_t g_35[2] = {0xF390F3BDL,0xF390F3BDL};
+static int32_t g_45 = (-1L);
+static int16_t g_47 = 1L;
+static uint32_t g_58 = 18446744073709551615UL;
+static const int32_t *g_66 = (void*)0;
+static const int32_t **g_65 = &g_66;
