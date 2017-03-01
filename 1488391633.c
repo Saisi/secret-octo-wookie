f743ef1f5a02fd8d1e5169e026509b7dde818747
saisi <saisi@users.noreply.github.com>
2017-03-01 18:07:14 +0000

    1488391633 bannat

---
+++ 1488391633.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1189947102
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   volatile unsigned f1 : 30;
+   volatile signed f2 : 8;
+   volatile signed f3 : 20;
+};
+
+struct S1 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
