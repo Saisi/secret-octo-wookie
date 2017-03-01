c21d5c3809abfc1257f8b394fdf0d000818df455
saisi <saisi@users.noreply.github.com>
2017-03-01 15:36:59 +0000

    1488382619 antihierarchist

---
+++ 1488382619.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4139898952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   volatile unsigned f1 : 31;
+   volatile unsigned f2 : 23;
+   int8_t  f3;
+   uint16_t  f4;
+   volatile int32_t  f5;
+   int64_t  f6;
+};
+
