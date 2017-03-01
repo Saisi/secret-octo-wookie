bac379d1411c2bd6fe88e36e41102ce761ea46ba
saisi <saisi@users.noreply.github.com>
2017-03-01 18:37:17 +0000

    1488393436 anotia

---
+++ 1488393436.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3314983966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x3A473EB9L;
+static uint32_t g_18 = 4UL;
+static uint32_t g_20 = 0x76ACB8F2L;
+static int32_t g_22[3] = {0x9AE4F78DL,0x9AE4F78DL,0x9AE4F78DL};
