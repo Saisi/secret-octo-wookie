fd12028c7d7b37ed5387b6a0f9e560ec01c60f1d
saisi <saisi@users.noreply.github.com>
2017-03-01 09:36:26 +0000

    1488360986 autokinesis

---
+++ 1488360986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3134618606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0x8EB5EA28L;
+static int32_t g_4 = 1L;
+static int32_t g_6 = 9L;
+static uint32_t g_7[7] = {2UL,0xFE4DE5A8L,2UL,2UL,0xFE4DE5A8L,2UL,2UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
