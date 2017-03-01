e3d9d64fbd86e5a1c61a0e0d7a692e249e0b89b1
saisi <saisi@users.noreply.github.com>
2017-03-01 11:06:34 +0000

    1488366393 autobahnen

---
+++ 1488366393.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      80334586
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int8_t  f1;
+   signed f2 : 6;
+   const volatile int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8[6][2][4] = {{{0L,(-1L),0L,(-3L)},{0L,(-1L),(-4L),0xC865B49DL}},{{(-1L),6L,6L,(-1L)},{0L,0xC865B49DL,6L,(-3L)}},{{(-1L),0L,(-4L),0L},{0L,6L,0L,0L}},{{0L,0L,(-3L),(-3L)},{0xC865B49DL,0xC865B49DL,(-4L),(-1L)}},{{0xC865B49DL,6L,(-3L),0xC865B49DL},{0L,(-1L),0L,(-3L)}},{{0L,(-1L),(-4L),0xC865B49DL},{(-1L),6L,(-4L),6L}}};
+static int16_t g_18 = 1L;
