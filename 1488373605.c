8730854720eb42b735fb47dd4ab873a9aa0c041d
saisi <saisi@users.noreply.github.com>
2017-03-01 13:06:47 +0000

    1488373605 angularities

---
+++ 1488373605.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1392015345
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   volatile int16_t  f1;
+   const uint64_t  f2;
+   int64_t  f3;
+   const int64_t  f4;
+   int32_t  f5;
+   const int16_t  f6;
+   const int16_t  f7;
+   volatile int32_t  f8;
