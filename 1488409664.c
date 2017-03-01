412ef816a88ef55da7dbd3d9001c329bba2d10a6
saisi <saisi@users.noreply.github.com>
2017-03-01 23:07:44 +0000

    1488409664 astonies

---
+++ 1488409664.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1826554559
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCEC3D7D4L;
+static int32_t g_21[3] = {(-1L),(-1L),(-1L)};
+static uint32_t g_50 = 0xC8167BA9L;
+static int64_t g_75 = 1L;
+static int32_t *g_78 = &g_2;
