ada98324f85024707c3162da0284a725d5c6caef
saisi <saisi@users.noreply.github.com>
2017-03-01 14:36:54 +0000

    1488379013 baldpatedness

---
+++ 1488379013.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      805788857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   const signed f1 : 16;
+   volatile unsigned f2 : 6;
+   unsigned f3 : 18;
+   unsigned f4 : 17;
+   const unsigned f5 : 27;
+   unsigned f6 : 18;
+   signed f7 : 4;
+   int8_t  f8;
