81a2bbf2d6f843495f1384667ca725e50901718e
saisi <saisi@users.noreply.github.com>
2017-03-01 22:37:41 +0000

    1488407861 abolishing

---
+++ 1488407861.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3658672373
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_15 = (-1L);
+static int32_t g_55 = 0xE5232406L;
+static int8_t g_66 = 0L;
+static int16_t g_70 = (-1L);
+static int32_t *g_79 = &g_55;
+static int32_t ** volatile g_78 = &g_79;/* VOLATILE GLOBAL g_78 */
+static volatile uint8_t g_96 = 1UL;/* VOLATILE GLOBAL g_96 */
+static uint32_t g_113 = 4294967287UL;
+static int32_t g_146[1] = {(-10L)};
