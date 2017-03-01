015ffaa20f2dd2b20baebe478c578303feecd6bb
saisi <saisi@users.noreply.github.com>
2017-03-01 06:06:08 +0000

    1488348367 allanturic

---
+++ 1488348367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1709234643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   signed f1 : 31;
+   const signed f2 : 6;
+   unsigned f3 : 25;
+   unsigned f4 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
