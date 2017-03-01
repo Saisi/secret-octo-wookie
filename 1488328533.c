756bf197addfadc004c6acffe7b69ae1cbf06256
saisi <saisi@users.noreply.github.com>
2017-03-01 00:35:36 +0000

    1488328533 aestheticism

---
+++ 1488328533.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      223043650
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 15;
+   const unsigned f1 : 8;
+   signed f2 : 10;
+   int8_t  f3;
+};
+
+union U1 {
+   const volatile uint8_t  f0;
+   int8_t * f1;
