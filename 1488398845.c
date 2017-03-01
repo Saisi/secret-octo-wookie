eab963b77783e10cab26ea04091f6b1470775f9c
saisi <saisi@users.noreply.github.com>
2017-03-01 20:07:25 +0000

    1488398845 allodialist

---
+++ 1488398845.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3495631425
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * const  volatile g_3[9] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t g_5[5][7] = {{(-3L),(-9L),1L,0xB1C03809L,0x93EE3274L,1L,0x810D21DFL},{0x810D21DFL,0x75405732L,0xA062B000L,0xA062B000L,0x75405732L,0x810D21DFL,0x61D2250DL},{0x75405732L,1L,0x93EE3274L,0xA062B000L,8L,0xF782A183L,0x01078932L},{0x85613717L,0x810D21DFL,(-9L),0xB1C03809L,(-9L),0xB1C03809L,(-9L)},{1L,1L,1L,2L,0xA062B000L,(-3L),(-9L)}};
+static int32_t * volatile g_4[6] = {&g_5[1][5],&g_5[1][5],&g_5[1][5],&g_5[1][5],&g_5[1][5],&g_5[1][5]};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
