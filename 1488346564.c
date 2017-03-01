29da833eb585b55064c979ebe702292ca10bcf4c
saisi <saisi@users.noreply.github.com>
2017-03-01 05:36:05 +0000

    1488346564 Athecae

---
+++ 1488346564.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      318452702
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   const volatile int8_t * volatile  f2;
+   volatile uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_29 = 0L;
+static int32_t g_57 = 3L;
