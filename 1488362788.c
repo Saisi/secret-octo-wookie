d1cc97c95cf94569168d513abf3b305e23b96dd8
saisi <saisi@users.noreply.github.com>
2017-03-01 10:06:29 +0000

    1488362788 Allvar

---
+++ 1488362788.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3304471305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x68EC0AE9L;
+static volatile int32_t g_5 = 0xAD386CCEL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6[2] = {(-1L),(-1L)};
+static volatile int32_t g_7[2] = {0xE5C7A00FL,0xE5C7A00FL};
+static int32_t g_8 = (-5L);
+static volatile int32_t g_11 = 0x72276B87L;/* VOLATILE GLOBAL g_11 */
+static volatile int32_t g_12 = 0xA5166941L;/* VOLATILE GLOBAL g_12 */
+static volatile int32_t g_13 = 0xA57B5BF2L;/* VOLATILE GLOBAL g_13 */
+static volatile int32_t g_14 = 0x5047C4D1L;/* VOLATILE GLOBAL g_14 */
