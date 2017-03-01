53d2dbca6baef93c7cb1d8e4bac8ca06cf70ffdc
saisi <saisi@users.noreply.github.com>
2017-03-01 16:37:06 +0000

    1488386225 backtracking

---
+++ 1488386225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1175673043
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 8;
+   const volatile unsigned f1 : 31;
+   unsigned f2 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 2;
