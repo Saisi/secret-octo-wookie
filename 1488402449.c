b90dd1b0f8c98a8101e09130ceb2a9e71ea0da79
saisi <saisi@users.noreply.github.com>
2017-03-01 21:07:30 +0000

    1488402449 alcade

---
+++ 1488402449.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3964000262
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 0x2FD4914D2EDC7555LL;/* VOLATILE GLOBAL g_4 */
+static const uint32_t g_10 = 0UL;
+static uint32_t g_23 = 0x50678E04L;
+static uint8_t g_36 = 1UL;
+static uint64_t g_55 = 0x2A4E630971CF717CLL;
+static uint8_t g_65 = 250UL;
+static uint8_t *g_67 = (void*)0;
+static int16_t g_88[7] = {8L,1L,8L,8L,1L,8L,8L};
+static int32_t g_91 = 0xE8D9F6C8L;
