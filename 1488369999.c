0d82563de8e2bb3380d83d92ff808928bc41c2fc
saisi <saisi@users.noreply.github.com>
2017-03-01 12:06:41 +0000

    1488369999 afterburners

---
+++ 1488369999.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3916572849
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint16_t  f1;
+   int32_t  f2;
+   uint64_t  f3;
+   int16_t  f4;
+   const uint8_t  f5;
+   int8_t  f6;
+   const int64_t  f7;
+   uint32_t  f8;
