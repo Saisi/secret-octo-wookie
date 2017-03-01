d04f4b0e16fafa9a672b7f03a8283ff1665fa4c7
saisi <saisi@users.noreply.github.com>
2017-03-01 13:36:49 +0000

    1488375409 ankylodontia

---
+++ 1488375409.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      679425891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2 = 8L;
+static uint32_t g_4[4] = {4294967294UL,4294967294UL,4294967294UL,4294967294UL};
+static int32_t g_6 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
