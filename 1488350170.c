ec562ce89346101f3cc106d08de7927f05b3740c
saisi <saisi@users.noreply.github.com>
2017-03-01 06:36:10 +0000

    1488350170 albumenizer

---
+++ 1488350170.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3126436240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_7 = 0x2EACL;/* VOLATILE GLOBAL g_7 */
+static uint8_t g_9 = 255UL;
+static int8_t g_49[1] = {0xBFL};
+static int8_t g_63 = 0xC5L;
+static int8_t *g_62 = &g_63;
+static int32_t g_65 = 0x7A8D1D9DL;
+static int16_t g_74 = 0x1356L;
+static int64_t g_86 = 0x77CCB2DC50754B49LL;
+static uint8_t *g_90[3][6][4] = {{{&g_9,&g_9,(void*)0,&g_9},{&g_9,&g_9,(void*)0,&g_9},{(void*)0,&g_9,&g_9,(void*)0},{(void*)0,&g_9,(void*)0,&g_9},{&g_9,&g_9,&g_9,(void*)0},{&g_9,&g_9,&g_9,&g_9}},{{&g_9,(void*)0,&g_9,(void*)0},{&g_9,&g_9,&g_9,(void*)0},{&g_9,(void*)0,(void*)0,&g_9},{(void*)0,&g_9,&g_9,(void*)0},{(void*)0,&g_9,(void*)0,&g_9},{&g_9,&g_9,&g_9,(void*)0}},{{&g_9,&g_9,(void*)0,(void*)0},{(void*)0,&g_9,&g_9,&g_9},{(void*)0,&g_9,&g_9,&g_9},{&g_9,(void*)0,&g_9,(void*)0},{&g_9,(void*)0,&g_9,(void*)0},{&g_9,&g_9,&g_9,&g_9}}};
