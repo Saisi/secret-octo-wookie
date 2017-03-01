2f21e9a6e994b61ad31a9668c9c20b015b00b299
saisi <saisi@users.noreply.github.com>
2017-03-01 03:35:52 +0000

    1488339351 Aspidiotus

---
+++ 1488339351.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1785216643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile int8_t * f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xE3245F1DL;
+static uint16_t g_11 = 0x31FBL;
+static int8_t g_44 = 0x56L;
