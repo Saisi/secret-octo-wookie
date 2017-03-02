e98d93009e6a4938e45f143fa269ee4a54551f6f
saisi <saisi@users.noreply.github.com>
2017-03-02 19:39:34 +0000

    1488483574 anemogram

---
+++ 1488483574.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3738187623
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_8[8] = {4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL};
+static int64_t g_9 = 0xE8F21009D434678DLL;
+static int32_t g_25 = 0x75DA2CA3L;
+static uint32_t g_26 = 0xDD7582BAL;
+static int8_t g_37 = 0L;
