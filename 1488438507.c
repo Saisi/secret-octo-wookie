2cf3cc3d06c25d812dfa3a2dd4769375cf3e5954
saisi <saisi@users.noreply.github.com>
2017-03-02 07:08:28 +0000

    1488438507 abominableness

---
+++ 1488438507.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      463507689
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 19;
+   uint32_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 20;
+   signed f1 : 22;
