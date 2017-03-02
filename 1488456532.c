a1c9e47048031a7774e43027b98a14f643787b69
saisi <saisi@users.noreply.github.com>
2017-03-02 12:08:52 +0000

    1488456532 asexualise

---
+++ 1488456532.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      210784196
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_22 = 0xC9D5FAC1L;
+static int32_t g_44 = 7L;
+static int32_t g_47 = 0x1940C9BAL;
+static uint32_t g_49 = 0x02249687L;
+static uint32_t g_51 = 1UL;
+static int8_t g_53[4] = {2L,2L,2L,2L};
+static int16_t g_62 = 1L;
+static int32_t *g_103 = (void*)0;
+static int32_t **g_102 = &g_103;
