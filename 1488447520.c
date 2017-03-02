94439164bc86db781d8c59103d96f51407e7c2e2
saisi <saisi@users.noreply.github.com>
2017-03-02 09:38:40 +0000

    1488447520 Amapondo

---
+++ 1488447520.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4219481774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x10B999FBL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0xED15DE05L;
+static int32_t g_5 = 0xB63E8E1BL;
+static uint8_t g_51 = 0xC7L;
+static int32_t g_54 = 0xD4826422L;
+static uint16_t g_71 = 0xAE91L;
+static int32_t *g_80 = &g_54;
+static int32_t g_81[10] = {0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL,0x2C5ACC8EL};
+static uint64_t g_88 = 7UL;
