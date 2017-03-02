32826a8548161e83479c418b1069d0da56b38adf
saisi <saisi@users.noreply.github.com>
2017-03-02 15:09:10 +0000

    1488467350 anapophysial

---
+++ 1488467350.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1148192447
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0x5DL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_11 = 0x4BEDCC59L;
+static uint8_t g_26[1][2] = {{4UL,4UL}};
+static uint8_t g_35 = 1UL;
+static uint8_t *g_34 = &g_35;
+static int32_t g_43 = 0xA9C4AD15L;
+static int32_t g_84 = 1L;
+static int32_t g_87[1] = {(-2L)};
+static uint32_t g_88 = 0x5D5A10D5L;
