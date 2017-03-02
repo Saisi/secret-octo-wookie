a7899f65cd888511b515a1fd49c8e4e13fb834f5
saisi <saisi@users.noreply.github.com>
2017-03-02 04:38:14 +0000

    1488429493 Aryanizing

---
+++ 1488429493.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      749443719
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x8462751CL;
+static int32_t * volatile g_7 = &g_6;/* VOLATILE GLOBAL g_7 */
+static int32_t g_11 = 0x21009D43L;
+static int32_t * volatile g_10 = &g_11;/* VOLATILE GLOBAL g_10 */
+static uint32_t g_33 = 0xAA84CBEDL;
+static uint32_t *g_65 = (void*)0;
+static int16_t g_69 = (-1L);
+static uint16_t g_71 = 0x9EE1L;
+static int32_t g_74 = 3L;
