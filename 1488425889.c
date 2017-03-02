354c5ceb239e553db8c9c1328f519318f2491b02
saisi <saisi@users.noreply.github.com>
2017-03-02 03:38:09 +0000

    1488425889 alabandite

---
+++ 1488425889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3203047439
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 6;
+   volatile unsigned f1 : 5;
+   volatile uint16_t  f2;
+   unsigned f3 : 15;
+   volatile unsigned f4 : 4;
+};
+
+struct S1 {
+   const uint32_t  f0;
