f1b12b319db6ac4369b1bc70277ff6dcaf206af8
saisi <saisi@users.noreply.github.com>
2017-03-02 05:08:16 +0000

    1488431296 attestative

---
+++ 1488431296.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2231484214
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[9] = {1L,1L,1L,1L,1L,1L,1L,1L,1L};
+static int32_t *g_2 = &g_3[1];
+static int32_t *g_6 = &g_3[1];
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
