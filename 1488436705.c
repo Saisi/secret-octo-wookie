c169691a71ea8ddceda2078de6e46d38a23d08e8
saisi <saisi@users.noreply.github.com>
2017-03-02 06:38:25 +0000

    1488436705 ambassadorial

---
+++ 1488436705.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3725932581
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 2L;
+static int8_t g_9 = 0L;
+static int16_t g_41[8][6][5] = {{{0xD55CL,1L,1L,0xD55CL,0x1AA4L},{1L,5L,1L,(-1L),0L},{0x1AA4L,0x6DABL,0xF8D3L,0x2F74L,5L},{0xD8F3L,(-5L),0xD8F3L,(-1L),(-1L)},{8L,1L,0x2F74L,0xD55CL,8L},{(-1L),0xAE69L,0xD4DBL,5L,0xD4DBL}},{{0x1AA4L,0x1AA4L,0x2F74L,0L,(-1L)},{0x06A8L,1L,0xD8F3L,0xAE69L,0x885CL},{0xD55CL,0L,0xF8D3L,(-6L),8L},{0xD4DBL,5L,0L,(-5L),0xD4DBL},{8L,(-1L),0L,0x2F74L,0x1AA4L},{0x1552L,1L,0x885CL,1L,0x1552L}},{{8L,1L,3L,(-6L),(-1L)},{5L,0x31FCL,0xD4DBL,(-1L),1L},{0x6DABL,8L,0L,1L,(-1L)},{0xD8F3L,(-1L),(-1L),(-10L),0x1552L},{(-1L),0L,0x2F74L,0x1AA4L,0x1AA4L},{0L,0x31FCL,0L,(-10L),0xD4DBL}},{{(-6L),0x6DABL,(-1L),1L,(-6L)},{0x9A27L,1L,0L,(-1L),0L},{(-1L),3L,(-1L),(-6L),0xD55CL},{(-1L),5L,0L,1L,5L},{0x6DABL,(-6L),0x2F74L,0x2F74L,(-6L)},{0L,5L,(-1L),(-5L),0x885CL}},{{8L,3L,0L,5L,0x1AA4L},{1L,1L,0xD4DBL,1L,1L},{8L,0x6DABL,3L,0xF8D3L,(-1L)},{0L,0x31FCL,0x885CL,(-1L),0x06A8L},{0x6DABL,0L,0L,0x6DABL,(-1L)},{(-1L),(-1L),0L,(-10L),1L}},{{(-1L),8L,0x2F74L,(-1L),0x1AA4L},{0x9A27L,0x31FCL,0x9A27L,(-10L),0x885CL},{(-6L),1L,(-1L),0x6DABL,(-6L)},{0L,1L,5L,(-1L),5L},{(-1L),(-1L),(-1L),0xF8D3L,0xD55CL},{0xD8F3L,5L,0x9A27L,1L,0L}},{{0x6DABL,0xF8D3L,0x2F74L,5L,(-6L)},{5L,5L,0L,(-5L),0xD4DBL},{8L,(-1L),0L,0x2F74L,0x1AA4L},{0x1552L,1L,0x885CL,1L,0x1552L},{8L,1L,3L,(-6L),(-1L)},{5L,0x31FCL,0xD4DBL,(-1L),1L}},{{0x6DABL,8L,0L,1L,(-1L)},{0xD8F3L,(-1L),(-1L),(-10L),0x1552L},{(-1L),0L,0x2F74L,0x1AA4L,0x1AA4L},{0L,0x31FCL,0L,(-10L),0xD4DBL},{(-6L),0x6DABL,(-1L),1L,(-6L)},{0x9A27L,1L,0x06A8L,(-10L),0x06A8L}}};
+static int32_t g_46 = 1L;
+static int16_t g_47 = (-1L);
