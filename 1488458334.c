760524085dea9f2eb9c52bc7650bfd580a1ea566
saisi <saisi@users.noreply.github.com>
2017-03-02 12:38:55 +0000

    1488458334 absoluter

---
+++ 1488458334.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      511653210
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint8_t  f1;
+   const unsigned f2 : 22;
+   const uint16_t  f3;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+};
