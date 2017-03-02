24b63b149cd201bf1139a45e26e3ecbed72d125c
saisi <saisi@users.noreply.github.com>
2017-03-02 09:08:37 +0000

    1488445717 Arkab

---
+++ 1488445717.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3352476907
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_26 = 0xEA5F34BDL;
+static uint8_t g_27[7] = {0xC2L,0xC2L,0xC2L,0xC2L,0xC2L,0xC2L,0xC2L};
+static int32_t g_30 = 1L;
+static int16_t g_76 = (-2L);
+static volatile int16_t g_77 = 1L;/* VOLATILE GLOBAL g_77 */
+static uint32_t g_79 = 4UL;
+static int32_t g_81 = 0x8C043881L;
+static uint8_t *g_83 = &g_27[1];
+static uint8_t *g_85 = &g_27[5];
