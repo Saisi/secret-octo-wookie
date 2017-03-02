23d3259c15275e542017cbe608c90f89a0b8665f
saisi <saisi@users.noreply.github.com>
2017-03-02 11:38:50 +0000

    1488454730 Aegates

---
+++ 1488454730.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4198649543
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   volatile signed f1 : 17;
+   const volatile unsigned f2 : 16;
+   signed f3 : 27;
+   const volatile unsigned : 0;
+};
+
+struct S1 {
+   const volatile int64_t  f0;
