f39ef84368cfe82eda06b150331974c66aa073b2
saisi <saisi@users.noreply.github.com>
2017-03-02 16:09:16 +0000

    1488470955 autobiographies

---
+++ 1488470955.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1801104169
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t * f1;
+};
+
+union U1 {
+   uint16_t  f0;
+   uint64_t  f1;
+   volatile uint8_t  f2;
+   uint8_t  f3;
