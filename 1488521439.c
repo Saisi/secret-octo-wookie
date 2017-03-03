61833d24fe8a39dfd289747524386e9dc7906d15
saisi <saisi@users.noreply.github.com>
2017-03-03 06:10:39 +0000

    1488521439 aspidospermine

---
+++ 1488521439.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2362839144
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_5[8] = {(-10L),(-10L),(-10L),(-10L),(-10L),(-10L),(-10L),(-10L)};
+static uint64_t g_43[5] = {0xFC7DF39F3C5A4A10LL,0xFC7DF39F3C5A4A10LL,0xFC7DF39F3C5A4A10LL,0xFC7DF39F3C5A4A10LL,0xFC7DF39F3C5A4A10LL};
+static int64_t g_52[8] = {(-6L),(-6L),0x20C3D8D58945CB29LL,(-6L),(-6L),0x20C3D8D58945CB29LL,(-6L),(-6L)};
+static uint16_t g_66 = 0xCC99L;
+static int32_t g_74 = 0L;
+static uint8_t g_75 = 0xC4L;
+static int32_t *g_82 = &g_74;
+static int32_t ** volatile g_81 = &g_82;/* VOLATILE GLOBAL g_81 */
+static volatile uint32_t g_85 = 0x10705E77L;/* VOLATILE GLOBAL g_85 */
