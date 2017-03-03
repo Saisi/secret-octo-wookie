c3fa69c0079435066d7e58a3829028ee9039fa79
saisi <saisi@users.noreply.github.com>
2017-03-03 21:42:07 +0000

    1488577326 amiantuses

---
+++ 1488577326.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      609058332
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 28;
+   signed f1 : 1;
+   int32_t  f2;
+};
+#pragma pack(pop)
+
+union U1 {
