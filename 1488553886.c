cfc51770a74ade1b0cc0f79f2bb1192c86fa579b
saisi <saisi@users.noreply.github.com>
2017-03-03 15:11:27 +0000

    1488553886 adenoviral

---
+++ 1488553886.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      62420301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const uint32_t  f1;
+   uint64_t  f2;
+   int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x51FE77DDL;
+static volatile int32_t g_7[3] = {1L,1L,1L};
