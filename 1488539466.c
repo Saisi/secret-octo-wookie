26eb3a33235a23300c3bb48607d29255930ddc11
saisi <saisi@users.noreply.github.com>
2017-03-03 11:11:07 +0000

    1488539466 Acoela

---
+++ 1488539466.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      862289007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = 0xA6B9L;
+static int32_t g_16 = 6L;
+static int32_t * const g_15 = &g_16;
+static int32_t g_27 = 5L;
+static int32_t g_31 = 0x4F204795L;
+static int8_t g_59 = 0x6BL;
+static uint16_t g_62 = 1UL;
+static int32_t g_107 = (-7L);
+static uint32_t g_109 = 0xE0AB02DAL;
