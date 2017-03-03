e1fe74031eced396ff15c4141ce54638475935d0
saisi <saisi@users.noreply.github.com>
2017-03-03 18:11:42 +0000

    1488564701 angularities

---
+++ 1488564701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1175651051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 20;
+   unsigned f1 : 27;
+   const unsigned f2 : 18;
+   const volatile int64_t  f3;
+   signed f4 : 30;
+   signed f5 : 20;
+   signed f6 : 7;
