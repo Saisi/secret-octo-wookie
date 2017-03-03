8f26fd6058baa8551b6a42e8cbf1dcbb4c800aff
saisi <saisi@users.noreply.github.com>
2017-03-03 02:10:17 +0000

    1488507015 antileft

---
+++ 1488507015.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3772095947
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   int32_t  f1;
+   const uint64_t  f2;
+   uint8_t  f3;
+   volatile int32_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
