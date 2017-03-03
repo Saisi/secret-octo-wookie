938553919ae49daeb2411f409024349ccbe03646
saisi <saisi@users.noreply.github.com>
2017-03-03 13:11:16 +0000

    1488546676 agitability

---
+++ 1488546676.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3589440967
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static int64_t g_18[3] = {0x8B6B0E1B1B552BF3LL,0x8B6B0E1B1B552BF3LL,0x8B6B0E1B1B552BF3LL};
+static int32_t g_25 = (-3L);
+static int32_t g_69 = 0xCC0D6813L;
+static volatile uint8_t g_70[1] = {0x47L};
