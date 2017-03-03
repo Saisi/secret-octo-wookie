d717e5949bdd97965f1af9a873fb65e40e13c05c
saisi <saisi@users.noreply.github.com>
2017-03-03 02:40:19 +0000

    1488508819 acerbates

---
+++ 1488508819.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2260701005
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 5;
+   const unsigned f1 : 8;
+   signed f2 : 11;
+   unsigned f3 : 10;
+   volatile signed f4 : 19;
+   unsigned : 0;
+};
+
+#pragma pack(push)
