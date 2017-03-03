026f433d0f12a51cf963307c23a150b85a9a201a
saisi <saisi@users.noreply.github.com>
2017-03-03 07:40:47 +0000

    1488526847 autocephaly

---
+++ 1488526847.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2771923793
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7[1] = {0x6C3A0B8EL};
+static int32_t * volatile g_6 = &g_7[0];/* VOLATILE GLOBAL g_6 */
+static int32_t *g_8 = &g_7[0];
+static int32_t *g_11 = &g_7[0];
+static int32_t *g_12 = (void*)0;
+static int8_t g_14[4] = {0x2EL,0x2EL,0x2EL,0x2EL};
+static int32_t * volatile g_16[4] = {(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t * volatile *g_15 = &g_16[0];
+static volatile int8_t g_21 = (-7L);/* VOLATILE GLOBAL g_21 */
