79da1cf405b646fb758583bd8849346aaf7cea58
saisi <saisi@users.noreply.github.com>
2017-03-03 08:10:50 +0000

    1488528650 anorexias

---
+++ 1488528650.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3998125206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_33 = 0xF92EL;
+static int32_t g_38 = 0x23B26D5DL;
+static volatile int16_t g_50 = 0x0461L;/* VOLATILE GLOBAL g_50 */
+static volatile uint16_t g_51 = 65526UL;/* VOLATILE GLOBAL g_51 */
+static int64_t g_89 = 8L;
+static int32_t g_102 = 0x0E46F67AL;
+static uint32_t g_105 = 0xDF0D7C7EL;
+static int32_t g_140[9] = {0x95A077DDL,0xDCAF5201L,0x95A077DDL,0x95A077DDL,0xDCAF5201L,0x95A077DDL,0x95A077DDL,0xDCAF5201L,0x95A077DDL};
+static int16_t g_144 = 0xEB9BL;
