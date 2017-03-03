80ff31aae230965b7d805c36dc8ee76cf78c29db
saisi <saisi@users.noreply.github.com>
2017-03-03 05:10:33 +0000

    1488517832 affination

---
+++ 1488517832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      8547104
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 30;
+   uint8_t  f1;
+   uint32_t  f2;
+   uint32_t  f3;
+   volatile uint16_t  f4;
+   const int8_t  f5;
+   int32_t  f6;
+   unsigned f7 : 29;
+   volatile uint32_t  f8;
