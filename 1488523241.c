49f8a6c73d536f2cdf7f8ad9f280d1885821f8b3
saisi <saisi@users.noreply.github.com>
2017-03-03 06:40:42 +0000

    1488523241 angiolymphitis

---
+++ 1488523241.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3195772756
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int32_t  f1;
+   int8_t * const  f2;
+};
+
+union U1 {
+   signed f0 : 3;
+   volatile int32_t  f1;
+   uint32_t  f2;
