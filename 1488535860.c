6bec1697b65e2cf741a9384298cadcf613a2c2e0
saisi <saisi@users.noreply.github.com>
2017-03-03 10:11:01 +0000

    1488535860 aposporogony

---
+++ 1488535860.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1118233703
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_20 = 0x82A10AA4L;
+static uint8_t g_22[10][2] = {{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL},{0x0EL,0x0EL}};
+static uint8_t g_37[5] = {0UL,0UL,0UL,0UL,0UL};
+static volatile int32_t g_38 = 0x9B504DBFL;/* VOLATILE GLOBAL g_38 */
