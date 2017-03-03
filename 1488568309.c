2341cc4a071487118c7cd0118bf1e1a4cad9e124
saisi <saisi@users.noreply.github.com>
2017-03-03 19:11:50 +0000

    1488568309 aggro

---
+++ 1488568309.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2348180408
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x167FA5C6L;
+static volatile int8_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
+static volatile uint64_t g_6 = 0xB66FEBFB2C41AC3DLL;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int64_t  func_1(void);
+
+
