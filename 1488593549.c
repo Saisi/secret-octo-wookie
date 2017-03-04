3312d86705c7d72a45a66e15adda507f58df9b8b
saisi <saisi@users.noreply.github.com>
2017-03-04 02:12:30 +0000

    1488593549 alloxuremia

---
+++ 1488593549.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1895890510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x56F9B2CFL;
+static int32_t g_49 = 0x6C4F32B7L;
+static int32_t *g_48 = &g_49;
+static int16_t g_59 = 0xBBB5L;
+static uint8_t g_84[5][5] = {{0x26L,255UL,255UL,255UL,255UL},{253UL,1UL,0x46L,0x46L,1UL},{0x26L,255UL,255UL,255UL,255UL},{253UL,1UL,0x46L,0x46L,1UL},{0x26L,255UL,255UL,255UL,255UL}};
+static uint8_t g_88 = 0x96L;
+static uint32_t g_103 = 0x19455490L;
+static uint8_t *g_115[1][3][1] = {{{&g_84[2][0]},{&g_84[2][0]},{&g_84[2][0]}}};
+static int64_t g_117 = 0xEAD792BF48DF1171LL;
