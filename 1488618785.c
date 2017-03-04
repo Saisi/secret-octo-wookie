863c9cc8481b206ef22e560c0c8a3d55587cd234
saisi <saisi@users.noreply.github.com>
2017-03-04 09:13:05 +0000

    1488618785 authentications

---
+++ 1488618785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2957116677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   unsigned f1 : 20;
+   int64_t  f2;
+   const uint8_t  f3;
+   uint32_t  f4;
+   uint64_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
