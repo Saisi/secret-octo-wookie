9273108b17b24a9eb91dc49b148f20ec52b1cf98
saisi <saisi@users.noreply.github.com>
2017-03-04 20:13:58 +0000

    1488658437 Atridae

---
+++ 1488658437.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1566580677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int32_t g_22 = 7L;/* VOLATILE GLOBAL g_22 */
+static int32_t g_23 = 0x50CFDFE9L;
+static int32_t g_24 = (-1L);
