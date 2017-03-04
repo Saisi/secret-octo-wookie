4bee61c510c1433c1e02ed64c414216c1cc5c407
saisi <saisi@users.noreply.github.com>
2017-03-04 03:12:35 +0000

    1488597154 Ardelia

---
+++ 1488597154.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3329487984
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 6;
+   signed f1 : 3;
+   const volatile signed f2 : 25;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_16 = 1UL;
+static uint32_t g_17 = 0xB168F928L;
+static int8_t g_24[1][9][10] = {{{(-9L),(-2L),(-1L),0xB3L,0xDAL,(-1L),(-9L),(-4L),0L,0L},{0xB7L,0xB3L,1L,(-7L),(-7L),1L,0xB3L,0xB7L,1L,(-4L)},{0xB9L,0L,(-9L),0x42L,0L,2L,0xB7L,0L,6L,(-7L)},{(-1L),(-2L),(-9L),0L,0xB3L,0L,0L,0xB7L,(-1L),(-1L)},{(-2L),0x42L,1L,(-4L),(-2L),1L,0xB3L,0L,(-7L),(-1L)},{2L,0x28L,1L,(-1L),0xD0L,0x99L,(-9L),0xD0L,0L,6L},{0xD0L,0xB3L,6L,1L,(-1L),0x99L,0x79L,(-9L),1L,1L},{2L,0L,0x42L,(-9L),0L,0xB9L,0x26L,0x26L,0xB9L,0L},{1L,0xD0L,0xD0L,1L,1L,1L,(-9L),0x79L,0x99L,(-1L)}}};
