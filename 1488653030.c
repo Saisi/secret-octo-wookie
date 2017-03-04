a5eaabfdae1d4eb7075f2822080183ef486ac43d
saisi <saisi@users.noreply.github.com>
2017-03-04 18:43:50 +0000

    1488653030 androcephalum

---
+++ 1488653030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2940052150
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 9;
+   unsigned f1 : 26;
+   unsigned f2 : 20;
+   volatile int32_t  f3;
+   volatile unsigned f4 : 31;
+   const unsigned f5 : 26;
+};
+
+struct S1 {
