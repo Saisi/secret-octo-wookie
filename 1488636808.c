4e3301b32b43b159a5cb012c46fbb21387dad2f0
saisi <saisi@users.noreply.github.com>
2017-03-04 14:13:29 +0000

    1488636808 attestator

---
+++ 1488636808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      740294423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int32_t  f0;
+   const int16_t  f1;
+   int32_t  f2;
+   signed f3 : 15;
+   unsigned f4 : 5;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3][4][3] = {{{(-10L),0x8D13034DL,(-10L)},{(-10L),1L,(-5L)},{(-10L),0xF1BF506EL,0xD1BCA9D6L},{(-10L),0x8D13034DL,(-10L)}},{{(-10L),1L,(-5L)},{(-10L),0xF1BF506EL,0xD1BCA9D6L},{(-10L),0x8D13034DL,(-10L)},{(-10L),1L,(-5L)}},{{(-10L),0xF1BF506EL,0xD1BCA9D6L},{(-10L),0x8D13034DL,(-10L)},{(-10L),1L,(-5L)},{(-10L),0xF1BF506EL,0xD1BCA9D6L}}};
