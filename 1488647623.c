a54aab9a77b1fe67a7c9606bea604c6598166a89
saisi <saisi@users.noreply.github.com>
2017-03-04 17:13:43 +0000

    1488647623 acetose

---
+++ 1488647623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      609312820
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   uint64_t  f1;
+   int16_t  f2;
+   uint32_t  f3;
+   int64_t  f4;
+   const uint8_t  f5;
+   const int32_t  f6;
+};
+
