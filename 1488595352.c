22cac171cab3f7f1d9fbe88c3efef62712077724
saisi <saisi@users.noreply.github.com>
2017-03-04 02:42:32 +0000

    1488595352 ataps

---
+++ 1488595352.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3390226517
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2[1][1][1] = {{{1L}}};
+static int32_t g_3 = 0xC3718654L;
+static int32_t g_4[10][4] = {{0x2BD42CA8L,1L,1L,0x2BD42CA8L},{(-1L),0xD416DB6DL,(-9L),0xF6A715CCL},{(-9L),0xF6A715CCL,9L,0x8B1647C2L},{0xDA2AE7B7L,0xC7AF5AF8L,0x2BA29F58L,0x8B1647C2L},{0xF495E3B3L,0xF6A715CCL,0xDA2AE7B7L,0xF6A715CCL},{2L,0xD416DB6DL,3L,0x2BD42CA8L},{9L,1L,0xF495E3B3L,1L},{0xC7AF5AF8L,0x8B1647C2L,1L,9L},{0xC7AF5AF8L,(-1L),0xF495E3B3L,0xF495E3B3L},{9L,9L,3L,0x5223FA3AL}};
+static int32_t g_5[5] = {0x9CD329B3L,0x9CD329B3L,0x9CD329B3L,0x9CD329B3L,0x9CD329B3L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
