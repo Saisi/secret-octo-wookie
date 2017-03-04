5d9626f45ba31505227f4f14595b7f384719d9b5
saisi <saisi@users.noreply.github.com>
2017-03-04 12:43:21 +0000

    1488631401 apicilar

---
+++ 1488631401.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3766128360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x6801FC3EL;
+static struct S0 g_6 = {1};
+
+
+/* --- FORWARD DECLARATIONS --- */
