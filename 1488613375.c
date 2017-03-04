987fd9ea4bfdee77f61ea2c2582469b9aa8146d4
saisi <saisi@users.noreply.github.com>
2017-03-04 07:42:56 +0000

    1488613375 bandrol

---
+++ 1488613375.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4016902916
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   uint8_t  f1;
+   int32_t  f2;
+   volatile int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_43[4][8] = {{0x1B76E1EEL,0x55A61940L,0x1B76E1EEL,0x41022496L,0x55A61940L,0L,0L,0x55A61940L},{0x55A61940L,0L,0L,0x55A61940L,0x41022496L,0x1B76E1EEL,0x55A61940L,0x1B76E1EEL},{0x55A61940L,(-1L),0x2C9BF5E4L,(-1L),0x55A61940L,0x2C9BF5E4L,0x7B38EB16L,0x7B38EB16L},{0x1B76E1EEL,(-1L),0x41022496L,0x41022496L,(-1L),0x1B76E1EEL,0L,(-1L)}};
+static uint8_t g_45 = 1UL;
