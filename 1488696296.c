46295770882224072a6042c59ac084aaa284281f
saisi <saisi@users.noreply.github.com>
2017-03-05 06:44:57 +0000

    1488696296 Baluchitherium

---
+++ 1488696296.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2319397513
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 0L;
+static uint32_t g_17 = 2UL;
+static const int32_t g_19 = 1L;
+static const int32_t *g_18 = &g_19;
+static int32_t g_44 = 0xF8B243E5L;
+static int32_t *g_43 = &g_44;
+static int32_t ** volatile g_42 = &g_43;/* VOLATILE GLOBAL g_42 */
+static uint16_t g_76 = 65528UL;
+static uint32_t *g_79 = (void*)0;
