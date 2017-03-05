76c9c6b6fdd00ee72bb4039ee2d7094d7dd4a98c
saisi <saisi@users.noreply.github.com>
2017-03-05 20:46:13 +0000

    1488746773 autechoscope

---
+++ 1488746773.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3286652233
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   volatile signed f1 : 14;
+   volatile unsigned f2 : 25;
+   volatile unsigned : 0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
