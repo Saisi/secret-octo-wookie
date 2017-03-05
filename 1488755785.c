56d6fad67a0c437fa745228add30e10c8fb86fc9
saisi <saisi@users.noreply.github.com>
2017-03-05 23:16:26 +0000

    1488755785 balsamiferous

---
+++ 1488755785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2009492264
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 2;
+   unsigned f1 : 15;
+   signed f2 : 21;
+   uint32_t  f3;
+   signed f4 : 8;
+   unsigned f5 : 31;
+   volatile unsigned f6 : 1;
