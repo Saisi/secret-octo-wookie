2923b539168f5faadd4b541e1bc86a22efeebbb3
saisi <saisi@users.noreply.github.com>
2017-03-05 04:44:45 +0000

    1488689084 anthological

---
+++ 1488689084.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3443030288
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 2;
+   signed f1 : 17;
+   unsigned f2 : 14;
+   volatile unsigned f3 : 12;
+   signed f4 : 26;
+};
+#pragma pack(pop)
