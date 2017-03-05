43571167649900f41241e392000ade2aa4f99915
saisi <saisi@users.noreply.github.com>
2017-03-05 18:15:59 +0000

    1488737759 Antipasch

---
+++ 1488737759.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2972270437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_4 = (void*)0;/* VOLATILE GLOBAL g_4 */
+static int32_t g_6 = (-1L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
