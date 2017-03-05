380145763ed1f7ec251c3cea665fb12a0e824ff7
saisi <saisi@users.noreply.github.com>
2017-03-05 22:16:20 +0000

    1488752180 anticolonial

---
+++ 1488752180.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1345920663
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   int8_t * f0;
+   int8_t * const  volatile  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[1] = {0x054A20A2L};
+static int64_t g_11 = 0xAFA15260EE2FB047LL;
+static union U2 g_15 = {0};/* VOLATILE GLOBAL g_15 */
