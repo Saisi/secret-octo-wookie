672df3e18b61488b85685bc9d5a0481ce1dc957f
saisi <saisi@users.noreply.github.com>
2017-03-05 18:46:01 +0000

    1488739561 autoanticomplement

---
+++ 1488739561.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3176258792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+union U1 {
+   uint16_t  f0;
+   volatile int32_t  f1;
+   signed f2 : 30;
+   int32_t  f3;
+   uint32_t  f4;
