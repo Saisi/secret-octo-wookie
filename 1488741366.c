71c493c56d48b82408aa395aa49fb3f96df462b8
saisi <saisi@users.noreply.github.com>
2017-03-05 19:16:06 +0000

    1488741366 Ateles

---
+++ 1488741366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      516723247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_10 = 5UL;
+static int32_t g_16 = 0xAF425F5FL;
+static int32_t * volatile g_15 = &g_16;/* VOLATILE GLOBAL g_15 */
+static int32_t g_20[10] = {0xAF0488F4L,0x795ED7C6L,0x86524F20L,0x86524F20L,0x795ED7C6L,0xAF0488F4L,0x795ED7C6L,0x86524F20L,0x86524F20L,0x795ED7C6L};
+static int32_t * volatile g_19 = &g_20[5];/* VOLATILE GLOBAL g_19 */
+static volatile int32_t g_21 = 0x55A63A74L;/* VOLATILE GLOBAL g_21 */
+static int32_t g_22[5] = {0x29855EECL,0x29855EECL,0x29855EECL,0x29855EECL,0x29855EECL};
+static int16_t g_28 = 6L;
+static int8_t g_30 = 4L;
