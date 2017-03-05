1dffe24685827d35dfbc5a601001d8b36d072edb
saisi <saisi@users.noreply.github.com>
2017-03-05 12:15:30 +0000

    1488716129 anthropographic

---
+++ 1488716129.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1354485168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_7 = 0x182EAC45L;
+static int8_t g_48 = 0x27L;
+static uint8_t g_63[7][3][1] = {{{0UL},{0x3DL},{0x04L}},{{0UL},{0x04L},{0x3DL}},{{0UL},{5UL},{5UL}},{{0UL},{0x3DL},{0x04L}},{{0UL},{0x04L},{0x3DL}},{{0UL},{5UL},{5UL}},{{0UL},{0x3DL},{0x04L}}};
+static uint16_t g_67 = 0x980AL;
+static int64_t g_99 = (-2L);
+static uint8_t g_100 = 3UL;
+static int16_t g_128 = 0L;
+static int16_t *g_127 = &g_128;
+static int16_t *g_131 = &g_128;
