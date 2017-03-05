9adb69d3cb5c1ffdc04a32a81b35f2f9951e98a9
saisi <saisi@users.noreply.github.com>
2017-03-05 16:15:49 +0000

    1488730549 antapodosis

---
+++ 1488730549.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2655654931
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile unsigned f1 : 9;
+   uint64_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_4[7] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL};
+static int8_t g_7[2] = {0L,0L};
