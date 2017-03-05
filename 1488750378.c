cbddf225c363ec5e697d72a8b81d6aacc639259e
saisi <saisi@users.noreply.github.com>
2017-03-05 21:46:18 +0000

    1488750378 ambiences

---
+++ 1488750378.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4280969466
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int16_t  f1;
+};
+
+struct S1 {
+   const int8_t  f0;
+   uint16_t  f1;
+};
+
