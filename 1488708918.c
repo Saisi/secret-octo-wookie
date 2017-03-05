dee1c85d9a73977523b96500bd7c060187d142fe
saisi <saisi@users.noreply.github.com>
2017-03-05 10:15:18 +0000

    1488708918 aldane

---
+++ 1488708918.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3691707544
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   volatile uint16_t  f0;
+   const volatile int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x49F6D4A0L;
+static const union U1 g_5 = {9UL};/* VOLATILE GLOBAL g_5 */
+
+
