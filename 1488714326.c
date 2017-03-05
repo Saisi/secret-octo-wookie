78af9b0f3eefe9218477335ffe15c8fbaabad521
saisi <saisi@users.noreply.github.com>
2017-03-05 11:45:27 +0000

    1488714326 asteriskos

---
+++ 1488714326.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1922217942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 25;
+   int64_t  f1;
+   volatile signed f2 : 18;
+   volatile signed f3 : 12;
+   volatile signed f4 : 1;
+   signed f5 : 9;
+};
+
+#pragma pack(push)
