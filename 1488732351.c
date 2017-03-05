70ecc0a59e8ed40df9cab96f8e6ad669f93140f8
saisi <saisi@users.noreply.github.com>
2017-03-05 16:45:51 +0000

    1488732351 AMBA

---
+++ 1488732351.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3950303672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+static volatile int32_t g_5[8] = {0x4F823AAFL,0x84DCC80FL,0x4F823AAFL,0x4F823AAFL,0x84DCC80FL,0x4F823AAFL,0x4F823AAFL,0x84DCC80FL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
