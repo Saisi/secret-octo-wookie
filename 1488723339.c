bb0cd1e807e5fdc3250ce9b88e5e2bdb7736a990
saisi <saisi@users.noreply.github.com>
2017-03-05 14:15:39 +0000

    1488723339 bandmasters

---
+++ 1488723339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3944584042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_18 = 0x1247ADF1ADB2BC40LL;
+static uint16_t g_24 = 0x8FB0L;
+static uint16_t g_28 = 0x59ECL;
+static uint16_t g_46 = 65532UL;
+static uint16_t *g_45 = &g_46;
+static uint64_t g_66[6] = {18446744073709551613UL,18446744073709551613UL,18446744073709551613UL,18446744073709551613UL,18446744073709551613UL,18446744073709551613UL};
+static int16_t g_70[2][2] = {{4L,4L},{4L,4L}};
+static uint32_t g_73[4] = {1UL,1UL,1UL,1UL};
+static const int16_t g_106 = 0x9198L;
