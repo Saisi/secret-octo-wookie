138b43260db8ead98763d6beab3154b5b8242055
saisi <saisi@users.noreply.github.com>
2017-03-06 15:47:58 +0000

    1488815278 ailanthic

---
+++ 1488815278.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3256889824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   const int64_t  f1;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_25 = 65527UL;
+static int16_t g_27 = (-1L);
+static uint32_t g_52[4][4] = {{0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L},{0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L},{0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L},{0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L,0x5FBC01D4L}};
