ac315674c467bdcbbd2984bd41455ec35b0e60bf
saisi <saisi@users.noreply.github.com>
2017-03-06 21:18:30 +0000

    1488835109 Autolycus

---
+++ 1488835109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1356880606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_20[4][4] = {{0x1D0EL,0x1D0EL,0x1D0EL,0x1D0EL},{0x1D0EL,0x1D0EL,0x1D0EL,0x1D0EL},{0x1D0EL,0x1D0EL,0x1D0EL,0x1D0EL},{0x1D0EL,0x1D0EL,0x1D0EL,0x1D0EL}};
+static int16_t g_72 = 1L;
+static int32_t g_77[9] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static uint32_t g_84 = 0x8517817FL;
