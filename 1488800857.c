2c9bff49ee22191b42cdea1c43d088add7a23566
saisi <saisi@users.noreply.github.com>
2017-03-06 11:47:38 +0000

    1488800857 Aviva

---
+++ 1488800857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3217724368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1A9565E0L;
+static int32_t g_5 = 0x8F8F576CL;
+static int32_t * volatile g_4[10] = {&g_5,&g_5,&g_5,&g_5,&g_5,&g_5,&g_5,&g_5,&g_5,&g_5};
+static int32_t * volatile g_6 = &g_5;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint16_t  func_1(void);
+
