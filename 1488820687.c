7d744ec4db53bafdf9778c7cb60e0c023c4d525d
saisi <saisi@users.noreply.github.com>
2017-03-06 17:18:07 +0000

    1488820687 anemotactic

---
+++ 1488820687.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2386156415
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_3 = 4L;
+static int32_t g_4 = 1L;
+static int8_t g_5 = 0L;
+static uint32_t g_6 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
