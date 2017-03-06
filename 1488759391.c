ccc60f43383b50085a2c1f33080e1c135179dae3
saisi <saisi@users.noreply.github.com>
2017-03-06 00:16:33 +0000

    1488759391 abranchiate

---
+++ 1488759391.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2496672372
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile uint32_t  f1;
+   const unsigned f2 : 11;
+   volatile int8_t * f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0xC5534BF8L;
+static int32_t * volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
