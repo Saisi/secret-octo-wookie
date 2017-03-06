acd0d4da62a11fca2d78bddf774684ed7d6bb596
saisi <saisi@users.noreply.github.com>
2017-03-06 06:17:10 +0000

    1488781028 anomorhomboid

---
+++ 1488781028.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1845555068
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   unsigned f1 : 17;
+   const volatile unsigned f2 : 15;
+   const unsigned f3 : 31;
+   unsigned f4 : 18;
+   const signed f5 : 1;
+   volatile unsigned f6 : 27;
+   uint8_t  f7;
+   signed f8 : 27;
