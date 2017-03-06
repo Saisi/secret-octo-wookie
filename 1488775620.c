9f4d05d0aad56c0248c38c81d0c987739b1d1c4b
saisi <saisi@users.noreply.github.com>
2017-03-06 04:47:01 +0000

    1488775620 archeunuch

---
+++ 1488775620.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1222623567
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static uint32_t g_11 = 0x29949DFCL;
+static uint64_t g_28 = 0x79DF9481F3403CA9LL;
+static uint16_t g_56[8] = {0x5C12L,0x5C12L,0x5C12L,0x5C12L,0x5C12L,0x5C12L,0x5C12L,0x5C12L};
+static int32_t g_57 = 0x63446B7DL;
+static int32_t g_59[5][1] = {{0L},{0x69E8A28DL},{0L},{0x69E8A28DL},{0L}};
+static uint32_t g_81 = 8UL;
+static uint32_t g_121 = 0xEF243A45L;
+static uint64_t g_145 = 3UL;
