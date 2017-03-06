028c8a873caa19e7e588436886cd808dcbc84367
saisi <saisi@users.noreply.github.com>
2017-03-06 16:48:04 +0000

    1488818884 antiproductivity

---
+++ 1488818884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4270228598
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[8][4][1] = {{{0x491E29EAL},{1L},{0L},{(-1L)}},{{0L},{1L},{0x491E29EAL},{(-1L)}},{{0x9C9FB37BL},{1L},{1L},{0x9C9FB37BL}},{{(-1L)},{0x491E29EAL},{1L},{0L}},{{(-1L)},{0L},{1L},{0x491E29EAL}},{{(-1L)},{0x9C9FB37BL},{1L},{1L}},{{0x9C9FB37BL},{(-1L)},{0x491E29EAL},{1L}},{{0L},{(-1L)},{0L},{1L}}};
+static volatile int32_t *g_2 = &g_3[4][2][0];
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
