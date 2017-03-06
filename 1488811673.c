39e6f93354d8777d679412bbd905a920da5b0780
saisi <saisi@users.noreply.github.com>
2017-03-06 14:47:53 +0000

    1488811673 appendicularian

---
+++ 1488811673.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1780093004
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 20;
+   volatile unsigned f1 : 25;
+   signed f2 : 23;
+   const unsigned f3 : 7;
+   unsigned f4 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_21[3] = {0L,0L,0L};
