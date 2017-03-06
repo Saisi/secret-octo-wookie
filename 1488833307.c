2e7d0105005522ac91ead196ee95e2c02fb8b59b
saisi <saisi@users.noreply.github.com>
2017-03-06 20:48:27 +0000

    1488833307 americium

---
+++ 1488833307.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      708294939
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 19;
+   unsigned f1 : 9;
+   unsigned f2 : 23;
