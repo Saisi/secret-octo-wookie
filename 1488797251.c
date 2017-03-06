bc35354ce63aaa015c6c3895ba6eda86c9fed5bc
saisi <saisi@users.noreply.github.com>
2017-03-06 10:47:32 +0000

    1488797251 amula

---
+++ 1488797251.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2982944970
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
