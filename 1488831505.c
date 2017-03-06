3712474a4e9a4f4839243a4dd84834553a819e2b
saisi <saisi@users.noreply.github.com>
2017-03-06 20:18:25 +0000

    1488831505 automanipulation

---
+++ 1488831505.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      583467056
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xCCCDDD1DL;/* VOLATILE GLOBAL g_2 */
+static const int32_t g_7 = (-1L);
+static const int32_t *g_6 = &g_7;
+static const int32_t ** volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static int32_t g_9 = 3L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
