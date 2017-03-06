9c4baab84d56ac013d973f2884f9325f9d24f944
saisi <saisi@users.noreply.github.com>
2017-03-06 13:47:49 +0000

    1488808069 armoniac

---
+++ 1488808069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1994193141
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[5] = {0x83C7730FL,0x83C7730FL,0x83C7730FL,0x83C7730FL,0x83C7730FL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
