7030a7f6439e776a7de1fe6a622280c4ee9e525d
saisi <saisi@users.noreply.github.com>
2017-03-06 11:17:34 +0000

    1488799054 azulejo

---
+++ 1488799054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3838323370
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-3L);
+static int32_t g_4 = 0xC17633EFL;
+static int8_t g_43 = 0xC1L;
+static int8_t g_47 = 0L;
+static int8_t *g_60 = &g_43;
+static int8_t **g_59[3][7] = {{&g_60,&g_60,&g_60,&g_60,&g_60,&g_60,&g_60},{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0},{&g_60,&g_60,&g_60,&g_60,&g_60,&g_60,&g_60}};
+static uint16_t g_73[10] = {0xF510L,0xF510L,0xF510L,0xF510L,0xF510L,0xF510L,0xF510L,0xF510L,0xF510L,0xF510L};
+static uint64_t g_76 = 0x802EEDA7DA9FC5C2LL;
+static int32_t g_97 = 7L;
