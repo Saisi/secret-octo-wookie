1348e3052df2e4b52b72b33e28908631debaec83
saisi <saisi@users.noreply.github.com>
2017-03-06 01:16:37 +0000

    1488762997 anglophobes

---
+++ 1488762997.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2042195725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   volatile int32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_3 = {-1L,0L};/* VOLATILE GLOBAL g_3 */
