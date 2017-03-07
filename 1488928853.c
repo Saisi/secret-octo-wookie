1fee1e3c2fba05da5e17d9946ecd7f9303fa74a9
saisi <saisi@users.noreply.github.com>
2017-03-07 23:20:53 +0000

    1488928853 Ashmead

---
+++ 1488928853.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2168963413
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+};
+
+union U1 {
+   volatile int8_t * const  volatile  f0;
+   volatile uint16_t  f1;
+   const int32_t  f2;
+   const int32_t  f3;
+};
