585e03a0e599ea98c7319a0ec0e1c90a132eefc8
saisi <saisi@users.noreply.github.com>
2017-03-07 18:50:29 +0000

    1488912628 antimodernism

---
+++ 1488912628.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1924323646
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_5 = 65535UL;/* VOLATILE GLOBAL g_5 */
+static int8_t g_25 = (-9L);
+static uint8_t g_27 = 0x7DL;
+static uint64_t g_29 = 0x07182B4746A1C17CLL;
+static volatile uint8_t g_37[3] = {0x06L,0x06L,0x06L};
+static volatile uint8_t *g_36 = &g_37[0];
+static uint64_t g_42 = 4UL;
+static uint64_t *g_41 = &g_42;
+static int64_t g_47 = 1L;
