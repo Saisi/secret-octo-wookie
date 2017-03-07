3dcf264a9220d6a1206f3e158e11d6516c747c61
saisi <saisi@users.noreply.github.com>
2017-03-07 19:20:31 +0000

    1488914431 anatomicomedical

---
+++ 1488914431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3501718904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x862FD41EL;
+static int32_t g_6 = 0x7FA5C62EL;
+static int32_t * volatile g_5[5][2][6] = {{{&g_2,&g_6,&g_6,(void*)0,&g_2,&g_6},{&g_2,&g_6,&g_6,(void*)0,&g_2,&g_2}},{{&g_2,&g_6,&g_6,&g_2,&g_6,(void*)0},{&g_2,&g_2,&g_6,&g_2,&g_6,&g_2}},{{&g_6,(void*)0,&g_2,(void*)0,&g_6,&g_6},{&g_6,&g_2,&g_2,&g_2,&g_6,(void*)0}},{{&g_2,&g_6,&g_6,(void*)0,&g_2,&g_6},{&g_6,&g_6,&g_6,&g_6,(void*)0,(void*)0}},{{&g_6,(void*)0,&g_2,&g_2,(void*)0,&g_6},{(void*)0,&g_6,&g_2,&g_6,&g_6,(void*)0}}};
+static int32_t * volatile g_7 = &g_6;/* VOLATILE GLOBAL g_7 */
+static volatile uint8_t g_17 = 1UL;/* VOLATILE GLOBAL g_17 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int64_t  func_1(void);
