0d4060399909954212c500fcc014b3f2841a5259
saisi <saisi@users.noreply.github.com>
2017-03-07 09:49:37 +0000

    1488880177 Achromatium

---
+++ 1488880177.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3824241231
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 6;
+   unsigned f1 : 5;
+   signed f2 : 20;
+   const volatile unsigned f3 : 15;
+   unsigned f4 : 4;
+   unsigned f5 : 13;
+   volatile unsigned f6 : 24;
