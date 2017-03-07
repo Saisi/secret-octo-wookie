778cd2b904bff7a35034b778e130ccff83f7aa44
saisi <saisi@users.noreply.github.com>
2017-03-07 04:49:11 +0000

    1488862151 Baralipton

---
+++ 1488862151.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4264121867
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1][8] = {{0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L,0xB9B55BB2L}};
+static volatile int32_t g_5 = (-8L);/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0x2D672A83L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xD4DFBD6CL;
+static int32_t g_13[2] = {(-1L),(-1L)};
+static uint16_t g_40 = 0UL;
+static uint16_t g_72 = 0UL;
+static uint16_t *g_71 = &g_72;
+static int8_t g_81[3][3][3] = {{{0x80L,0x80L,0x8FL},{0xE7L,0x77L,0xE7L},{0x80L,0x8FL,0x8FL}},{{1L,0x77L,1L},{0x80L,0x80L,0x8FL},{0xE7L,0x77L,0xE7L}},{{0x80L,0x8FL,0x8FL},{1L,0x77L,1L},{0x80L,0x80L,0x8FL}}};
