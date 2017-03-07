b8accfdafcdace2e13d0040eb629c8487d60fab0
saisi <saisi@users.noreply.github.com>
2017-03-07 03:49:04 +0000

    1488858543 barcella

---
+++ 1488858543.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      570138220
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   unsigned f1 : 9;
+   unsigned f2 : 7;
+   volatile signed f3 : 23;
+   unsigned f4 : 10;
+   volatile unsigned f5 : 25;
+   volatile signed f6 : 19;
+};
+
