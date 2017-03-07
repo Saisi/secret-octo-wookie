9acdffa8a626bce0920edabfc80571bb4826b012
saisi <saisi@users.noreply.github.com>
2017-03-07 05:19:14 +0000

    1488863953 archplunderer

---
+++ 1488863953.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1672390124
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-9L);
+static int8_t g_7[7] = {4L,(-10L),4L,4L,(-10L),4L,4L};
+static volatile int16_t g_9[10] = {6L,6L,6L,6L,6L,6L,6L,6L,6L,6L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
