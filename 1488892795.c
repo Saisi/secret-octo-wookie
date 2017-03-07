fd4462cf7225f05bb098f95e35c93cea741457aa
saisi <saisi@users.noreply.github.com>
2017-03-07 13:19:55 +0000

    1488892795 Ardea

---
+++ 1488892795.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2604883403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xB9B55BB2L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x3532FE22L;
+static int32_t g_7 = 0x0417BE18L;
+static int64_t g_20 = 0xE8FE7528ACE4B0ADLL;
+static int64_t g_51 = 0x18BA70A1EE3199F2LL;
+static int64_t *g_50 = &g_51;
+static int16_t g_76 = 1L;
+static int16_t g_78 = 0x29B6L;
+static int16_t g_80[1] = {0x176AL};
