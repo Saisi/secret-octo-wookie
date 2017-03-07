52cf2416f7e06fc7af19f876f05312002422c0ae
saisi <saisi@users.noreply.github.com>
2017-03-07 22:50:50 +0000

    1488927050 adsorption

---
+++ 1488927050.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1158136671
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+};
+
+union U1 {
+   const volatile uint8_t  f0;
+   int8_t * f1;
+   uint8_t  f2;
+   volatile unsigned f3 : 25;
+};
