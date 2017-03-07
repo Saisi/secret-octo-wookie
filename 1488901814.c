7dec3b606c8ff7f428db7ea87aeac74ea6f1ba3d
saisi <saisi@users.noreply.github.com>
2017-03-07 15:50:14 +0000

    1488901814 asylum

---
+++ 1488901814.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3060339170
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2][2][3] = {{{(-1L),0L,(-1L)},{(-1L),0L,(-1L)}},{{(-1L),0L,(-1L)},{(-1L),0L,(-1L)}}};
+static uint64_t g_7 = 0x279A20B28787D636LL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
