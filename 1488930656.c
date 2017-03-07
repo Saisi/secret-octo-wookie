13980c018703759e75e6a721bb514695a178621d
saisi <saisi@users.noreply.github.com>
2017-03-07 23:50:57 +0000

    1488930656 Bander

---
+++ 1488930656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1220909896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint8_t  f0;
+};
+
+struct S1 {
+   const volatile signed f0 : 10;
+   volatile int32_t  f1;
+   signed f2 : 14;
+   volatile uint64_t  f3;
+   volatile unsigned f4 : 4;
