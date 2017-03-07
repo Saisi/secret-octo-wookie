d0a43094958885db1ca61ab3e412c7aff06e6c2a
saisi <saisi@users.noreply.github.com>
2017-03-07 00:18:45 +0000

    1488845925 Ankeny

---
+++ 1488845925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1368227198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_5 = 0x73EB905DL;
+static const int32_t *g_4 = &g_5;
+static int32_t g_7 = 0x0D3F7BF4L;
+static const int32_t *g_6 = &g_7;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
