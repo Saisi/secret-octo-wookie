de156ea98997007a5203e230f37a17a5be446fee
saisi <saisi@users.noreply.github.com>
2017-03-07 21:50:44 +0000

    1488923443 admittable

---
+++ 1488923443.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3407851872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xEB0647DCL;
+static int32_t * volatile g_3[8][7][3] = {{{(void*)0,&g_4,&g_4},{&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4},{(void*)0,&g_4,(void*)0},{&g_4,&g_4,&g_4},{&g_4,(void*)0,&g_4},{&g_4,(void*)0,&g_4}},{{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4},{&g_4,&g_4,(void*)0},{(void*)0,(void*)0,&g_4},{&g_4,(void*)0,&g_4},{&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4}},{{&g_4,&g_4,&g_4},{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4},{&g_4,&g_4,(void*)0},{&g_4,(void*)0,&g_4},{&g_4,&g_4,&g_4},{(void*)0,&g_4,(void*)0}},{{(void*)0,&g_4,(void*)0},{(void*)0,&g_4,&g_4},{(void*)0,&g_4,&g_4},{&g_4,(void*)0,&g_4},{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4},{(void*)0,(void*)0,&g_4}},{{(void*)0,(void*)0,&g_4},{(void*)0,&g_4,&g_4},{(void*)0,&g_4,&g_4},{&g_4,(void*)0,(void*)0},{&g_4,&g_4,&g_4},{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4}},{{&g_4,(void*)0,&g_4},{&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4},{(void*)0,(void*)0,(void*)0},{(void*)0,(void*)0,&g_4},{(void*)0,&g_4,(void*)0},{&g_4,&g_4,(void*)0}},{{&g_4,(void*)0,(void*)0},{&g_4,&g_4,&g_4},{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4},{(void*)0,&g_4,&g_4},{&g_4,(void*)0,&g_4}},{{&g_4,&g_4,(void*)0},{(void*)0,&g_4,&g_4},{&g_4,&g_4,(void*)0},{&g_4,&g_4,&g_4},{(void*)0,&g_4,&g_4},{&g_4,&g_4,&g_4},{&g_4,(void*)0,&g_4}}};
+static int32_t *g_8 = (void*)0;
+static int32_t ** volatile g_7[4] = {&g_8,&g_8,&g_8,&g_8};
+static int32_t ** volatile g_9 = &g_8;/* VOLATILE GLOBAL g_9 */
+static int64_t g_45 = 0x9690B2811FFBE626LL;
+static int32_t ** const g_47 = &g_8;
+static uint8_t g_71[4] = {0x50L,0x50L,0x50L,0x50L};
+static uint64_t g_74[1] = {18446744073709551607UL};
