4359ea7abc384757108d0fc4f80261f28ec82a4a
saisi <saisi@users.noreply.github.com>
2017-03-07 07:49:26 +0000

    1488872965 antiorgastic

---
+++ 1488872965.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3737976373
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 7L;
+static int32_t g_15[6] = {0x7B79F6A7L,0x7B79F6A7L,0x7B79F6A7L,0x7B79F6A7L,0x7B79F6A7L,0x7B79F6A7L};
+static int32_t *g_30 = &g_5;
+static int32_t ** volatile g_29 = &g_30;/* VOLATILE GLOBAL g_29 */
