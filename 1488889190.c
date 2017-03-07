3ee4764957b6b22f81012ae31b88f1cd64319aee
saisi <saisi@users.noreply.github.com>
2017-03-07 12:19:50 +0000

    1488889190 bankroller

---
+++ 1488889190.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1260935277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S3 {
+   const uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[10][2][4] = {{{1L,3L,1L,1L},{0x31AAL,0x31AAL,1L,1L}},{{0L,3L,0L,1L},{0L,1L,1L,0L}},{{0x31AAL,1L,1L,1L},{1L,3L,1L,1L}},{{0x31AAL,0x31AAL,1L,1L},{0L,3L,0L,1L}},{{0L,1L,1L,0L},{0x31AAL,1L,1L,1L}},{{1L,3L,1L,1L},{0x31AAL,0x31AAL,0L,3L}},{{1L,0x31AAL,1L,0L},{1L,0L,0L,1L}},{{1L,0L,3L,0L},{0L,0x31AAL,3L,3L}},{{1L,1L,0L,3L},{1L,0x31AAL,1L,0L}},{{1L,0L,0L,1L},{1L,0L,3L,0L}}};
+static int32_t g_4 = 0x04157736L;
+static struct S3 g_5 = {3UL};
+
+
