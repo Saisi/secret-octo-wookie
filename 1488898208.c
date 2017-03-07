edc91b0f425112cb01dd3e6fd9b39f777a34181b
saisi <saisi@users.noreply.github.com>
2017-03-07 14:50:09 +0000

    1488898208 abscession

---
+++ 1488898208.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3507020032
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 1UL;/* VOLATILE GLOBAL g_2 */
+static const int32_t g_10 = 0L;
+static int32_t g_29 = 0x0412894FL;
+static uint32_t g_102 = 0xA51E29AFL;
+static int32_t *g_105 = &g_29;
+static int32_t ** const  volatile g_104 = &g_105;/* VOLATILE GLOBAL g_104 */
+static int16_t g_109[6][4][8] = {{{(-1L),(-6L),0x236BL,0x7C9AL,1L,0x236BL,0x68C8L,(-2L)},{(-3L),0x68C8L,0L,1L,0xFF66L,0x236BL,5L,(-1L)},{(-2L),(-6L),(-2L),1L,1L,(-2L),(-6L),(-2L)},{(-6L),(-6L),0L,0x7C9AL,0x7879L,0x236BL,0xA2DCL,(-2L)}},{{(-2L),0x68C8L,0x236BL,1L,0x7C9AL,0x236BL,(-6L),(-1L)},{(-3L),(-6L),1L,1L,0x7879L,(-2L),0xA2DCL,(-1L)},{1L,0x68C8L,(-2L),0x7562L,0x7C9AL,(-2L),0xB285L,(-1L)},{(-6L),0xB285L,1L,0x7C9AL,0xE219L,(-2L),0xA2DCL,1L}},{{(-1L),0x68C8L,1L,0x7C9AL,0x7C9AL,1L,0x68C8L,(-1L)},{1L,0x68C8L,1L,0x7562L,0xFF66L,(-2L),0x19B8L,(-1L)},{(-1L),0xB285L,(-2L),0x7C9AL,0x7562L,(-2L),0x68C8L,1L},{(-6L),0x68C8L,0xC480L,0x7C9AL,0xFF66L,1L,0xA2DCL,(-1L)}},{{1L,0x68C8L,(-2L),0x7562L,0x7C9AL,(-2L),0xB285L,(-1L)},{(-6L),0xB285L,1L,0x7C9AL,0xE219L,(-2L),0xA2DCL,1L},{(-1L),0x68C8L,1L,0x7C9AL,0x7C9AL,1L,0x68C8L,(-1L)},{1L,0x68C8L,1L,0x7562L,0xFF66L,(-2L),0x19B8L,(-1L)}},{{(-1L),0xB285L,(-2L),0x7C9AL,0x7562L,(-2L),0x68C8L,1L},{(-6L),0x68C8L,0xC480L,0x7C9AL,0xFF66L,1L,0xA2DCL,(-1L)},{1L,0x68C8L,(-2L),0x7562L,0x7C9AL,(-2L),0xB285L,(-1L)},{(-6L),0xB285L,1L,0x7C9AL,0xE219L,(-2L),0xA2DCL,1L}},{{(-1L),0x68C8L,1L,0x7C9AL,0x7C9AL,1L,0x68C8L,(-1L)},{1L,0x68C8L,1L,0x7562L,0xFF66L,(-2L),0x19B8L,(-1L)},{(-1L),0xB285L,(-2L),0x7C9AL,0x7562L,(-2L),0x68C8L,1L},{(-6L),0x68C8L,0xC480L,0x7C9AL,0xFF66L,1L,0xA2DCL,(-1L)}}};
+static uint8_t g_129 = 0xA8L;
+static uint64_t g_139 = 18446744073709551607UL;
