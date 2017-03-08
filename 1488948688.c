969824eae25086daf6abd1696c61c2ff43b690e8
saisi <saisi@users.noreply.github.com>
2017-03-08 04:51:28 +0000

    1488948688 autosomes

---
+++ 1488948688.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1022509688
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1] = {0x8070ADE2L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
