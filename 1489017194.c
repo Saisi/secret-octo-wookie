b0f27de9253d07bd1b7bf070bf89dd8dc1ecbda7
saisi <saisi@users.noreply.github.com>
2017-03-08 23:53:14 +0000

    1489017194 anachronize

---
+++ 1489017194.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      919448305
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U4 {
+   uint64_t  f0;
+   volatile int8_t * const  f1;
+   int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
