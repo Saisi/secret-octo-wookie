90ebc01106e6dfef852f06e2380b83bdee476004
saisi <saisi@users.noreply.github.com>
2017-03-08 04:21:26 +0000

    1488946884 alength

---
+++ 1488946884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      949920116
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+};
+
+struct S1 {
+   uint32_t  f0;
+   signed f1 : 4;
+   unsigned f2 : 7;
+};
+
