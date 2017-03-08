61649d1a4c12f3aaca88cf395ac3ec7518ca2ad8
saisi <saisi@users.noreply.github.com>
2017-03-08 06:51:39 +0000

    1488955897 bantling

---
+++ 1488955897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2973927087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 22;
+   unsigned f1 : 21;
+   volatile unsigned f2 : 4;
+   unsigned f3 : 31;
+   const signed f4 : 20;
+   unsigned f5 : 29;
+   volatile signed f6 : 8;
+   unsigned f7 : 27;
+   signed f8 : 18;
