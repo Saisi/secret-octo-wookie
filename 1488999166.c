82dcad9601fcf2b2c5ee4b300beb9c5eda127013
saisi <saisi@users.noreply.github.com>
2017-03-08 18:52:47 +0000

    1488999166 allogene

---
+++ 1488999166.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1613261695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static uint8_t g_11 = 1UL;
+static uint32_t g_13 = 1UL;
+static int32_t g_27 = (-4L);
+static int32_t g_66 = 2L;
+static int32_t *g_65 = &g_66;
+static uint64_t g_72 = 0UL;
+static int32_t g_77 = (-6L);
+static int8_t g_84[8][8] = {{(-1L),(-5L),(-1L),(-1L),(-5L),(-1L),(-1L),(-5L)},{(-5L),(-1L),(-1L),(-5L),(-1L),(-1L),(-5L),(-1L)},{(-5L),(-5L),1L,(-5L),(-5L),1L,(-5L),(-5L)},{(-1L),(-5L),(-1L),(-1L),(-5L),(-1L),(-1L),(-5L)},{(-5L),(-1L),(-1L),(-5L),(-1L),(-1L),(-5L),(-1L)},{(-5L),(-5L),1L,(-5L),(-5L),1L,(-1L),(-1L)},{1L,(-1L),1L,1L,(-1L),1L,1L,(-1L)},{(-1L),1L,1L,(-1L),1L,1L,(-1L),1L}};
