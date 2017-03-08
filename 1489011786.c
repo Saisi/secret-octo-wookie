7f666db5ffb3f1bc02bb9cf5b9ebd7ef3e303e9f
saisi <saisi@users.noreply.github.com>
2017-03-08 22:23:07 +0000

    1489011786 alienate

---
+++ 1489011786.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1284511547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   int8_t * volatile  f1;
+   const int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_6 = 0x0F1D8987L;
+static const int32_t *g_5 = &g_6;
+static int8_t g_8[3] = {0xEFL,0xEFL,0xEFL};
