c563c12c405a11c422d33cfb1d41508c4438800d
saisi <saisi@users.noreply.github.com>
2017-03-08 06:21:35 +0000

    1488954095 anomalure

---
+++ 1488954095.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2734538273
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x25D16DF2L;/* VOLATILE GLOBAL g_2 */
+static const uint32_t g_39 = 1UL;
+static int32_t g_41 = 0x61C44381L;
+static uint16_t g_68 = 0x3AB7L;
+static int32_t g_72 = 1L;
+static int64_t g_82 = 0xEEFD7F9B64A47447LL;
+static uint8_t g_108 = 0xFCL;
+static uint32_t g_115[9] = {8UL,8UL,8UL,8UL,8UL,8UL,8UL,8UL,8UL};
+static int64_t g_118 = 0x8F66F91BAA844380LL;
