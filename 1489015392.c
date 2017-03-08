c27505bcbd3b02b08619687232886d6f888e3d24
saisi <saisi@users.noreply.github.com>
2017-03-08 23:23:12 +0000

    1489015392 agnosia

---
+++ 1489015392.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3870201457
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   int32_t  f1;
+   const int32_t  f2;
+   const unsigned f3 : 21;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 4UL;
+static int64_t g_13 = 0L;
