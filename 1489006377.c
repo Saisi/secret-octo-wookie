43974b31d5815300372832d0b43e3b26ec19a572
saisi <saisi@users.noreply.github.com>
2017-03-08 20:52:57 +0000

    1489006377 adaptionism

---
+++ 1489006377.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2290513987
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   int8_t  f1;
+   volatile uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = 0xB792L;
+static int64_t g_15 = 1L;
+static int16_t g_20 = 0x1F09L;
