f3d5867dad0209e4bf875bce41aefc0c8589fce7
saisi <saisi@users.noreply.github.com>
2017-03-08 07:21:41 +0000

    1488957701 Achaenodon

---
+++ 1488957701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3470251890
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   const volatile int16_t  f1;
+   volatile int32_t  f2;
+   volatile int32_t  f3;
+   volatile unsigned f4 : 22;
+   const uint8_t  f5;
+};
