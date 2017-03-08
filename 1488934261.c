9ca63ff24296dea53efc19a6c50ce371e4db21cd
saisi <saisi@users.noreply.github.com>
2017-03-08 00:51:02 +0000

    1488934261 backbrand

---
+++ 1488934261.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4128313554
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint16_t  f1;
+   const uint8_t  f2;
+   uint16_t  f3;
+   volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = 0x6E58D020BD2AB60ELL;
