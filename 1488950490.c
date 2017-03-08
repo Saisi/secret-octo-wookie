ac576b78e58b544fc4770368f617a5d6a15d8b0c
saisi <saisi@users.noreply.github.com>
2017-03-08 05:21:30 +0000

    1488950490 atrophies

---
+++ 1488950490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      879551625
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x57BF5CD5L;
+static uint64_t g_48 = 6UL;
+static uint8_t g_82 = 0xB5L;
+static uint32_t g_95 = 0x56B862B3L;
+static int32_t g_98 = 0x2B1506F5L;
