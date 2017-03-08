4b073fd2c345adc0e82252bef39c7d1f4f8825eb
saisi <saisi@users.noreply.github.com>
2017-03-08 18:22:44 +0000

    1488997364 azimine

---
+++ 1488997364.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1891423995
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_7 = 0x092F644ACCB24CE5LL;
+static int32_t g_9 = 0x58528353L;
+static int32_t * volatile g_8 = &g_9;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_11 = 0xFF446CD5L;/* VOLATILE GLOBAL g_11 */
+static uint64_t g_12 = 0x45C2F1FCC1031C36LL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
