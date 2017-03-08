ab82f996c27253197ef8d0cbd962f3b05fb4841e
saisi <saisi@users.noreply.github.com>
2017-03-08 21:22:59 +0000

    1489008179 antigenicity

---
+++ 1489008179.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2836116827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_22 = 0x0A5DEC4FL;
+static uint32_t g_30 = 0xA83D87E3L;
+static uint16_t g_65 = 0xED5EL;
+static uint8_t g_78 = 1UL;
+static volatile int32_t g_84 = 0L;/* VOLATILE GLOBAL g_84 */
+static volatile int32_t *g_83 = &g_84;
+static volatile int32_t **g_82 = &g_83;
+static int8_t g_91 = 0xFBL;
+static int32_t g_92 = 5L;
