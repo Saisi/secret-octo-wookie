3e27ca9395b3460ebcb5aea47724640f40d5bd5b
saisi <saisi@users.noreply.github.com>
2017-03-08 16:22:35 +0000

    1488990155 apilary

---
+++ 1488990155.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3601667193
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x48261F9CL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 8L;
+static uint8_t g_17 = 255UL;
+static uint8_t g_33 = 0xBAL;
+static int8_t g_59 = 0x9BL;
+static volatile int32_t *g_82 = &g_2;
+static int32_t g_90 = 0x140DA8F9L;
+static const int8_t g_94 = 0xCBL;
+static int8_t g_116[2][5] = {{(-9L),(-6L),(-10L),(-10L),(-6L)},{(-9L),(-6L),(-10L),(-10L),(-6L)}};
