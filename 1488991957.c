48f57c0cea246232772eed8c8dc8c544dba34167
saisi <saisi@users.noreply.github.com>
2017-03-08 16:52:38 +0000

    1488991957 anisogeny

---
+++ 1488991957.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3992845048
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_34 = 0xDF11BBB4L;
+static int32_t **g_54 = (void*)0;
+static int8_t g_71 = 0x97L;
+static uint32_t g_72 = 0x21B78CD9L;
+static int16_t g_79 = 0xFE2CL;
+static int32_t *g_86 = &g_34;
+static int32_t ** const  volatile g_85 = &g_86;/* VOLATILE GLOBAL g_85 */
+static int32_t g_133 = 5L;
+static uint32_t g_134 = 1UL;
