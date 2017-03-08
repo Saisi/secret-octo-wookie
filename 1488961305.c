c3601c6c1c0a9c2a92f718e6914a1a2c48520a30
saisi <saisi@users.noreply.github.com>
2017-03-08 08:21:46 +0000

    1488961305 Antimachus

---
+++ 1488961305.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4193004722
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[7] = {0x2456160CL,0x2456160CL,0x2456160CL,0x2456160CL,0x2456160CL,0x2456160CL,0x2456160CL};
+static volatile int32_t g_4[8] = {0xD303F024L,0L,0xD303F024L,0L,0xD303F024L,0L,0xD303F024L,0L};
+static int32_t g_5 = (-4L);
+static uint64_t g_24 = 18446744073709551615UL;
+static uint32_t g_45 = 0x8E487928L;
+static uint16_t g_61 = 65533UL;
+static uint8_t g_67 = 0xC1L;
+static uint8_t g_71 = 1UL;
+static uint8_t g_111 = 250UL;
