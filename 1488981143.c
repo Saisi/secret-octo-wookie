ea5d20be760be2d39321316d18a7471d4edab9fa
saisi <saisi@users.noreply.github.com>
2017-03-08 13:52:23 +0000

    1488981143 Adele

---
+++ 1488981143.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      226923136
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+   volatile unsigned f1 : 11;
+   unsigned f2 : 1;
+   volatile unsigned f3 : 9;
+   volatile unsigned f4 : 20;
+};
+
+struct S1 {
+   signed f0 : 7;
