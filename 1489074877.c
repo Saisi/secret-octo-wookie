0d4a8b48afccf7c43781d94e5b1b2fa8c689b04f
saisi <saisi@users.noreply.github.com>
2017-03-09 15:54:40 +0000

    1489074877 baikie

---
+++ 1489074877.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      523346874
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 11;
+   unsigned f1 : 20;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
