d414b078bc562998b6f46b11f1062dee04901596
saisi <saisi@users.noreply.github.com>
2017-03-09 05:23:42 +0000

    1489037022 allelvia

---
+++ 1489037022.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3968768170
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x2BA39464L;
+static uint64_t g_6 = 18446744073709551612UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
