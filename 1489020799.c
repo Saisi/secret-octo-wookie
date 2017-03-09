a982d9e352feb987887e2228067a2daaea85f01e
saisi <saisi@users.noreply.github.com>
2017-03-09 00:53:20 +0000

    1489020799 Anastrophia

---
+++ 1489020799.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3109221303
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[6][1] = {{(-10L)},{(-10L)},{(-10L)},{(-10L)},{(-10L)},{(-10L)}};
+static uint32_t g_21 = 0xD921B0AEL;
+static int32_t g_32 = 0x7A9413E4L;
+static int32_t g_33 = 0x22A19820L;
+static int32_t g_55 = 0x76953C53L;
+static int32_t * volatile g_54 = &g_55;/* VOLATILE GLOBAL g_54 */
+static int8_t g_72 = (-1L);
+static int8_t *g_80 = (void*)0;
