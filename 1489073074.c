7c5adabc5a2d2ca96fa6ca8186142ffb1b0776ac
saisi <saisi@users.noreply.github.com>
2017-03-09 15:24:35 +0000

    1489073074 adelomorphous

---
+++ 1489073074.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3036375702
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int16_t  f0;
+   int8_t * f1;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   int64_t  f1;
+   const uint32_t  f2;
+   uint16_t  f3;
