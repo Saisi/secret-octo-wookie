4f3fa63d8f9ec737795ecbee83063d6c51303f5f
saisi <saisi@users.noreply.github.com>
2017-03-09 13:24:24 +0000

    1489065863 backtracks

---
+++ 1489065863.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1135333743
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xC184C506L;
+static volatile uint8_t g_6 = 0x98L;/* VOLATILE GLOBAL g_6 */
+static int64_t g_26[6][5] = {{1L,(-10L),0x07976B5A7DD9AE65LL,0x07976B5A7DD9AE65LL,(-10L)},{(-4L),0xD5F49DCB7BE74FD5LL,0x07976B5A7DD9AE65LL,(-1L),0xD5F49DCB7BE74FD5LL},{(-4L),(-10L),0L,(-1L),(-10L)},{1L,(-10L),0x07976B5A7DD9AE65LL,0x07976B5A7DD9AE65LL,(-10L)},{(-4L),0xD5F49DCB7BE74FD5LL,0x07976B5A7DD9AE65LL,(-1L),0xD5F49DCB7BE74FD5LL},{(-4L),(-10L),0L,(-1L),(-10L)}};
+static volatile int32_t g_35 = (-5L);/* VOLATILE GLOBAL g_35 */
+static int32_t g_36 = 1L;
