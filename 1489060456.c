bc1c9c99acea289d418ce0f963929cb0b9d0f89e
saisi <saisi@users.noreply.github.com>
2017-03-09 11:54:17 +0000

    1489060456 amphithere

---
+++ 1489060456.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3630921892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile int64_t g_2 = 0x5B40194DDBC37078LL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4[10] = {(-1L),1L,(-1L),(-1L),1L,(-1L),(-1L),1L,(-1L),(-1L)};
+static volatile int32_t * volatile g_3[6] = {&g_4[3],&g_4[3],&g_4[3],&g_4[3],&g_4[3],&g_4[3]};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
