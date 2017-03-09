e4f1ef5b0e9a6fec628c9ec8642b04445ca498db
saisi <saisi@users.noreply.github.com>
2017-03-09 21:25:11 +0000

    1489094711 albites

---
+++ 1489094711.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3648151272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 2L;
+static volatile int32_t g_12 = (-9L);/* VOLATILE GLOBAL g_12 */
+static int32_t g_19 = 0L;
+static int64_t g_20 = (-3L);
+static volatile uint64_t g_21 = 0x5799333C7D24B5E4LL;/* VOLATILE GLOBAL g_21 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
