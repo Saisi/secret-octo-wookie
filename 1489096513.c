d188e3846d279e2d6ac0e9cb94829342360b96cb
saisi <saisi@users.noreply.github.com>
2017-03-09 21:55:14 +0000

    1489096513 apl

---
+++ 1489096513.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      644310393
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5B0E446DL;
+static int32_t g_28[5][8][1] = {{{1L},{(-8L)},{0x1CB6A7F4L},{1L},{0x1CB6A7F4L},{(-8L)},{1L},{1L}},{{1L},{1L},{(-8L)},{0x1CB6A7F4L},{1L},{0x1CB6A7F4L},{(-8L)},{1L}},{{1L},{1L},{1L},{(-8L)},{0x1CB6A7F4L},{1L},{0x1CB6A7F4L},{(-8L)}},{{1L},{1L},{1L},{1L},{(-8L)},{0x1CB6A7F4L},{1L},{0x1CB6A7F4L}},{{(-8L)},{1L},{1L},{1L},{1L},{(-8L)},{0x1CB6A7F4L},{1L}}};
+static int32_t *g_49 = &g_3;
+static int32_t **g_48 = &g_49;
