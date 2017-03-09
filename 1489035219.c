73db28e4326d22c2708db0eedf2958c2c7a3e32c
saisi <saisi@users.noreply.github.com>
2017-03-09 04:53:40 +0000

    1489035219 animis

---
+++ 1489035219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      796585239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   volatile unsigned f1 : 25;
+   signed f2 : 24;
+   unsigned f3 : 3;
+   const unsigned f4 : 8;
+   int8_t  f5;
+   volatile unsigned f6 : 5;
+   signed f7 : 11;
+   const signed f8 : 14;
