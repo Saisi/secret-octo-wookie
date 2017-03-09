81845a856781e7d664dc524fa0abd929e3e113ed
saisi <saisi@users.noreply.github.com>
2017-03-09 02:23:27 +0000

    1489026207 Adventism

---
+++ 1489026207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1869649133
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_14 = 0xB1L;
+static int64_t g_16[9] = {(-3L),(-3L),(-3L),(-3L),(-3L),(-3L),(-3L),(-3L),(-3L)};
+static uint32_t g_17 = 0x263D1880L;
+static volatile uint16_t g_33 = 65535UL;/* VOLATILE GLOBAL g_33 */
+static volatile int32_t g_44[10][1] = {{0x3A67D625L},{(-3L)},{0x3A67D625L},{(-3L)},{0x3A67D625L},{(-3L)},{0x3A67D625L},{(-3L)},{0x3A67D625L},{(-3L)}};
+static int32_t g_46 = 0x77106CF3L;
+static int8_t g_64 = (-1L);
+static int64_t g_76 = 0xBF9D45AA775BEB37LL;
+static uint32_t g_86 = 0x36032EE7L;
