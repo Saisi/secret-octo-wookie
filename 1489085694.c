ddbf796d16e46174aab7ee56f932c6a1efd5b516
saisi <saisi@users.noreply.github.com>
2017-03-09 18:54:54 +0000

    1489085694 attorneydom

---
+++ 1489085694.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2510221837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x0A8EAC8BL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0x50BF34FDL;
+static volatile int32_t * volatile g_8 = &g_4;/* VOLATILE GLOBAL g_8 */
+static int32_t g_10 = (-4L);
+static uint32_t g_43 = 1UL;
+static int32_t g_54 = (-2L);
+static volatile uint32_t *g_67 = (void*)0;
