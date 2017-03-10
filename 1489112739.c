e4a5d92af62899ed87748195a7c3051e2f9d1cda
saisi <saisi@users.noreply.github.com>
2017-03-10 02:25:39 +0000

    1489112739 Anyang

---
+++ 1489112739.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3435041040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+union U1 {
+   unsigned f0 : 18;
+   int32_t  f1;
+   uint8_t  f2;
+   signed f3 : 17;
+   const int32_t  f4;
