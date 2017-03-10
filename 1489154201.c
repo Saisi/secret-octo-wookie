38d53c1714b2d331c17e35c7c1dc6d1c12c23117
saisi <saisi@users.noreply.github.com>
2017-03-10 13:56:42 +0000

    1489154201 bagle

---
+++ 1489154201.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3522154260
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = (-2L);
+static int32_t g_23 = (-10L);
+static int64_t g_28 = (-2L);
+static int32_t g_31 = 0x601C8D53L;
+static int32_t * volatile g_30 = &g_31;/* VOLATILE GLOBAL g_30 */
