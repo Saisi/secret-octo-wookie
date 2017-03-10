5fe393c2be3bbbfa16cf07689ed1eccfeb5e795d
saisi <saisi@users.noreply.github.com>
2017-03-10 23:57:39 +0000

    1489190257 bacteriostat

---
+++ 1489190257.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2608068588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 4;
+   unsigned f1 : 9;
+   int64_t  f2;
+   volatile unsigned f3 : 31;
+   volatile signed f4 : 8;
+   volatile signed f5 : 20;
+   const signed f6 : 5;
