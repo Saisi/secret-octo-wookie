7e202047e8b36b284a4c82af86b0dd22c12f0df9
saisi <saisi@users.noreply.github.com>
2017-03-10 20:27:19 +0000

    1489177639 Babylonian

---
+++ 1489177639.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2887556410
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_2[4] = {(void*)0,(void*)0,(void*)0,(void*)0};
+static int32_t ** volatile g_3 = &g_2[2];/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-4L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0x9FEBB674L;
+static uint64_t g_13 = 0xACF2579253A34C86LL;
+static uint16_t g_67 = 4UL;
+static int64_t g_69 = 0x4BC71F8B108A6652LL;
+static int32_t g_76 = 7L;
+static uint16_t **g_156 = (void*)0;
