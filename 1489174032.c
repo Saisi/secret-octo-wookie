081ee9d5c8a56b36783fbf581b77179f2279f362
saisi <saisi@users.noreply.github.com>
2017-03-10 19:27:13 +0000

    1489174032 agraphia

---
+++ 1489174032.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      960583173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int16_t  f0;
+   uint64_t  f1;
+   volatile uint8_t  f2;
+   int64_t  f3;
+   const volatile int64_t  f4;
+   const int32_t  f5;
+};
+
+struct S1 {
