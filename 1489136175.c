8452009ee93d5ecb976d56f3f56bdee6d28317b1
saisi <saisi@users.noreply.github.com>
2017-03-10 08:56:15 +0000

    1489136175 antalkali

---
+++ 1489136175.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3236193884
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   const volatile uint32_t  f1;
+   signed f2 : 20;
+   int32_t  f3;
+   int8_t  f4;
+};
+
+union U1 {
+   uint32_t  f0;
