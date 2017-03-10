e04ee66046992902586533844ec850d6b2d63064
saisi <saisi@users.noreply.github.com>
2017-03-10 17:27:01 +0000

    1489166821 antimedievalist

---
+++ 1489166821.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2454235774
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[3][5][4] = {{{1L,0L,1L,1L},{1L,0L,0L,1L},{1L,0L,1L,1L},{1L,0L,0L,1L},{1L,0L,1L,1L}},{{1L,0L,0L,1L},{1L,0L,1L,1L},{1L,0L,0L,1L},{1L,0L,1L,1L},{1L,0L,0L,1L}},{{1L,0L,1L,1L},{1L,0L,0L,1L},{1L,0L,1L,1L},{1L,0L,0L,1L},{1L,0L,1L,1L}}};
+static uint32_t g_9 = 0xB61B583DL;
+static uint32_t g_10 = 0x1615E82AL;
+static uint16_t g_35 = 0x6446L;
+static uint16_t g_38 = 0xD20AL;
+static int32_t g_59 = (-1L);
+static uint32_t g_60 = 3UL;
+static int8_t g_95 = (-2L);
+static int8_t * const g_94 = &g_95;
