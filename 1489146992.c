74d97d3d6b880d782830b823345b1524682c447d
saisi <saisi@users.noreply.github.com>
2017-03-10 11:56:32 +0000

    1489146992 abnormalised

---
+++ 1489146992.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      184644365
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0xF34FDA0EL;
+static volatile int32_t g_48 = (-4L);/* VOLATILE GLOBAL g_48 */
+static volatile int32_t *g_47 = &g_48;
+static uint8_t g_50 = 1UL;
+static uint8_t *g_49 = &g_50;
+static uint64_t g_77 = 0x240190438315EAC8LL;
+static uint8_t *g_82 = (void*)0;
+static uint8_t g_84 = 0UL;
+static uint8_t *g_83 = &g_84;
