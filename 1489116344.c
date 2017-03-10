c06c24b356c4b9ae2293b4102ab90a8722d327f5
saisi <saisi@users.noreply.github.com>
2017-03-10 03:25:44 +0000

    1489116344 anesthetic

---
+++ 1489116344.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1380149320
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-3L);
+static volatile int8_t g_12 = 0L;/* VOLATILE GLOBAL g_12 */
+static volatile int8_t g_13 = (-1L);/* VOLATILE GLOBAL g_13 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
