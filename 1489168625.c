33a730cc4329606492777f6ab3fb578c94b139f5
saisi <saisi@users.noreply.github.com>
2017-03-10 17:57:06 +0000

    1489168625 Anzanite

---
+++ 1489168625.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3466469917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static int32_t *g_15[2][1] = {{&g_3},{&g_3}};
+static int8_t g_28[10] = {2L,2L,2L,2L,2L,2L,2L,2L,2L,2L};
+static uint64_t g_61 = 9UL;
+static uint16_t g_68 = 4UL;
+static int32_t g_72 = 0x6FF1AB12L;
+static int32_t g_93[1][3] = {{(-1L),(-1L),(-1L)}};
+static int32_t g_95 = 0x0DD4D5FFL;
