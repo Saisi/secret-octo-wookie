9fa2df36a6116afe4093e1661e523823fd17f081
saisi <saisi@users.noreply.github.com>
2017-03-10 10:56:25 +0000

    1489143385 ambage

---
+++ 1489143385.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2311004444
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xE91A049FL;
+static volatile int32_t g_5 = 0xDB22124FL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x8114768CL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
