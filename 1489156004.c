256672481236be460199218c34f964c2e6a365c1
saisi <saisi@users.noreply.github.com>
2017-03-10 14:26:44 +0000

    1489156004 Apaches

---
+++ 1489156004.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      407158359
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[10] = {1L,1L,(-3L),(-3L),1L,1L,1L,(-3L),(-3L),1L};
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[6][6][2] = {{{0L,0L},{(-2L),0L},{(-10L),0L},{0L,0L},{0L,(-10L)},{0L,(-2L)}},{{0L,0L},{0x3FC4B03EL,0L},{(-10L),0xB00868BBL},{(-10L),0L},{0x3FC4B03EL,0L},{0L,(-2L)}},{{0L,(-10L)},{0L,0L},{0L,0L},{(-10L),0L},{(-2L),0L},{0L,0x3FC4B03EL}},{{0xB00868BBL,0L},{0x393DDA37L,0L},{0xB00868BBL,0x29822CB3L},{0xB1AB4456L,0xB00868BBL},{0xD8D13034L,0xD94FE5F3L},{0L,0x815CBCEFL}},{{0xB1AB4456L,0xB1AB4456L},{0x815CBCEFL,0L},{0xD94FE5F3L,0xD8D13034L},{0xB00868BBL,0xB1AB4456L},{0x29822CB3L,0xB00868BBL},{0L,0x393DDA37L}},{{0L,0xB00868BBL},{0x29822CB3L,0xB1AB4456L},{0xB00868BBL,0xD8D13034L},{0xD94FE5F3L,0L},{0x815CBCEFL,0xB1AB4456L},{0xB1AB4456L,0x815CBCEFL}}};
+static volatile int32_t g_7 = 0x2CA96836L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = (-1L);/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9[10][1] = {{0x0AE28DE6L},{0xC5378470L},{0x0AE28DE6L},{0xC5378470L},{0x0AE28DE6L},{0xC5378470L},{0x0AE28DE6L},{0xC5378470L},{0x0AE28DE6L},{0xC5378470L}};
+static volatile int32_t g_10 = 0xA7EB8E13L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t g_11[4] = {0x99220C43L,0x99220C43L,0x99220C43L,0x99220C43L};
+static volatile int32_t g_12 = 0xFD52A42BL;/* VOLATILE GLOBAL g_12 */
