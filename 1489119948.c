240710b33906ca8c01f89abe2ea85df564d2e493
saisi <saisi@users.noreply.github.com>
2017-03-10 04:25:48 +0000

    1489119948 agrosterol

---
+++ 1489119948.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1519868343
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-3L);
+static int32_t g_8[8] = {0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL,0xEAFD94FEL};
+static volatile int32_t g_9[8] = {(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L)};
+static int32_t g_10 = 0x98815CBCL;
+static int32_t g_13 = (-3L);
+static int32_t *g_40 = (void*)0;
+static int64_t g_42 = 0xCAC2457718DA91E5LL;
+static uint32_t g_44 = 0x18B25769L;
+static uint32_t g_46 = 4294967289UL;
