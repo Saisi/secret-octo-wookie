4472b1225846eb79036cc187023117b59ed98d3e
saisi <saisi@users.noreply.github.com>
2017-03-10 09:56:20 +0000

    1489139779 acoustician

---
+++ 1489139779.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3306926989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   uint64_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 3;
+   volatile signed f1 : 15;
