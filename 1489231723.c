0b02c4934a42bd8196595cfe3e8b4f343542db48
saisi <saisi@users.noreply.github.com>
2017-03-11 11:28:46 +0000

    1489231723 adaptivity

---
+++ 1489231723.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1481402407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   int16_t  f2;
+   volatile signed : 0;
+   const volatile uint64_t  f3;
+};
+
+struct S1 {
+   volatile signed f0 : 26;
