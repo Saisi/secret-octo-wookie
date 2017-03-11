5f0f51ccb50bf699708a88301023153ea692b198
saisi <saisi@users.noreply.github.com>
2017-03-11 22:29:46 +0000

    1489271386 appetizers

---
+++ 1489271386.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2623632812
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   uint32_t  f1;
+   signed f2 : 28;
+   volatile signed f3 : 16;
+   volatile uint32_t  f4;
+   int8_t  f5;
+   volatile uint32_t  f6;
+   uint32_t  f7;
+   const volatile uint32_t  f8;
