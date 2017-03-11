fd9f59f14f8e4c89f614dbaa765ceb60f5e8e861
saisi <saisi@users.noreply.github.com>
2017-03-11 22:59:48 +0000

    1489273188 adrenaline

---
+++ 1489273188.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3239978538
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-1L);
+static volatile int8_t g_11 = 1L;/* VOLATILE GLOBAL g_11 */
+static uint32_t g_12 = 0x72CA3C0BL;
+static int32_t g_23 = 1L;
+static int16_t g_30 = 0xA2AAL;
+static int32_t *g_38 = &g_4;
+static int32_t **g_37 = &g_38;
+static uint32_t g_120 = 4294967290UL;
+static int32_t g_121 = (-6L);
