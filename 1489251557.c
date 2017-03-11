cda7828c7dea1daaa9cd25b5043b01fdb33ef371
saisi <saisi@users.noreply.github.com>
2017-03-11 16:59:18 +0000

    1489251557 bandcutter

---
+++ 1489251557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2097766416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 22;
+   volatile uint8_t  f1;
+   int64_t  f2;
+   int32_t  f3;
+   int8_t  f4;
+};
+
+union U1 {
+   volatile uint8_t  f0;
