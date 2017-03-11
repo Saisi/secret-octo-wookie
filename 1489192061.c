fcea62e54f2b8a03922984aec6b68cba913a75d6
saisi <saisi@users.noreply.github.com>
2017-03-11 00:27:41 +0000

    1489192061 antenatalitial

---
+++ 1489192061.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1957499955
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 255UL;
+static int32_t g_8[5] = {0x1D82C32BL,0x1D82C32BL,0x1D82C32BL,0x1D82C32BL,0x1D82C32BL};
+static const int32_t *g_13 = &g_8[4];
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint8_t  func_1(void);
+static const int32_t * func_5(int32_t * p_6);
+
