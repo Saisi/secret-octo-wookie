3e552d39529f4d5f875628c06dd7a7783a7dce78
saisi <saisi@users.noreply.github.com>
2017-03-11 07:58:26 +0000

    1489219106 autodestruction

---
+++ 1489219106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      148330258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile uint64_t  f1;
+   const volatile int8_t * f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t *g_11 = (void*)0;
+static uint16_t g_12 = 65535UL;
