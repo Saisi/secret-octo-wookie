7a921a813ccb4555203af34c5df8ccb21bced27a
saisi <saisi@users.noreply.github.com>
2017-03-11 09:58:36 +0000

    1489226315 banderilleros

---
+++ 1489226315.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2409333479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   volatile int8_t * const  volatile  f1;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
