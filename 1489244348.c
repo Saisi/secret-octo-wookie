af6d09ef3af0b7148bc97098a006621b6c7c80ba
saisi <saisi@users.noreply.github.com>
2017-03-11 14:59:08 +0000

    1489244348 ablactating

---
+++ 1489244348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4254802224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 6;
+   volatile unsigned f1 : 3;
+   volatile unsigned f2 : 25;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
