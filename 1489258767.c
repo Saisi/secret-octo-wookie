c3e64ee0f748a1a47dbc26c204c32b2ef9a1ee3a
saisi <saisi@users.noreply.github.com>
2017-03-11 18:59:29 +0000

    1489258767 amylodextrin

---
+++ 1489258767.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1692388128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   volatile signed f1 : 27;
+   unsigned f2 : 17;
+   const volatile signed f3 : 25;
+   volatile unsigned f4 : 4;
+   int16_t  f5;
+   unsigned f6 : 23;
