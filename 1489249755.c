4deea17742fa4f405eee2e4e0a5d507dc3c778de
saisi <saisi@users.noreply.github.com>
2017-03-11 16:29:15 +0000

    1489249755 bagge

---
+++ 1489249755.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1636186898
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xF825E7A6L;
+static uint16_t g_6 = 1UL;
+static int32_t g_14 = 2L;
+static int32_t g_16 = 0xCEA445A8L;
+static int8_t g_39 = (-2L);
+static int32_t g_50 = 0x89BE308DL;
+static uint32_t g_51[2] = {4294967295UL,4294967295UL};
+static volatile uint8_t g_55 = 0xCAL;/* VOLATILE GLOBAL g_55 */
+static int32_t g_78 = 0xFBA83E1AL;
