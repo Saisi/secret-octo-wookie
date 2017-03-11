8673ac7752a1476b9941eb68444b195596f841fc
saisi <saisi@users.noreply.github.com>
2017-03-11 14:29:06 +0000

    1489242544 aggro

---
+++ 1489242544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1418237117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+   const unsigned f3 : 27;
+   int16_t  f4;
+   volatile int64_t  f5;
+   volatile uint64_t  f6;
+   const int16_t  f7;
+};
