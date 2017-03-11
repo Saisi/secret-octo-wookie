22ee427225eb8cb5757e72487faf18c2b13cb51d
saisi <saisi@users.noreply.github.com>
2017-03-11 13:59:00 +0000

    1489240739 assertorial

---
+++ 1489240739.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1953024042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   int8_t * f1;
+   uint16_t  f2;
+   const int16_t  f3;
+   const signed f4 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
