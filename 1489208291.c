ad4614989327ee58931fcf871fa39a429cac21ac
saisi <saisi@users.noreply.github.com>
2017-03-11 04:58:11 +0000

    1489208291 baguette

---
+++ 1489208291.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3026026323
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 31;
+   signed f2 : 6;
+   const signed f3 : 25;
+   const unsigned f4 : 6;
+   const volatile unsigned f5 : 7;
+   signed f6 : 10;
