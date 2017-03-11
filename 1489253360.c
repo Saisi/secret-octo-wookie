a9866a4c8e1f4af6452cec3baeec52e719e82fe6
saisi <saisi@users.noreply.github.com>
2017-03-11 17:29:20 +0000

    1489253360 ambivalences

---
+++ 1489253360.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4089853966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 8L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xDE0BCD8CL;
+static uint8_t g_83 = 0UL;
+static int32_t g_85 = 0x6DDF936BL;
+static uint32_t g_101 = 1UL;
+static uint8_t g_121 = 255UL;
+static int64_t g_124 = 0x6B1ECDBA6A349BDALL;
+static const int64_t g_138 = 3L;
+static const int64_t *g_137 = &g_138;
