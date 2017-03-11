9695f4be5fd57bb51578e8fe9b5a1dd89361a909
saisi <saisi@users.noreply.github.com>
2017-03-11 20:59:38 +0000

    1489265978 barbascoes

---
+++ 1489265978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4052061657
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   int8_t  f0;
+   signed f1 : 6;
+   volatile int32_t  f2;
+   const volatile uint64_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xEE1C9741L;
