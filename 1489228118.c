545f4e50f8db1665055db6becf8457488d673955
saisi <saisi@users.noreply.github.com>
2017-03-11 10:28:38 +0000

    1489228118 awanyu

---
+++ 1489228118.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3203180460
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static uint16_t g_20 = 0xBC65L;
+static int8_t g_34 = (-7L);
+static int8_t g_52[4] = {0x73L,0x73L,0x73L,0x73L};
+static int16_t g_54 = 0xFFABL;
+static int32_t g_66[10][3] = {{1L,1L,0xE1D35EE8L},{(-1L),(-1L),1L},{1L,1L,0xE1D35EE8L},{(-1L),(-1L),1L},{1L,1L,0xE1D35EE8L},{(-1L),(-1L),1L},{1L,1L,0xE1D35EE8L},{(-1L),(-1L),1L},{1L,1L,0xE1D35EE8L},{(-1L),(-1L),1L}};
+static volatile int16_t * volatile * volatile g_67 = (void*)0;/* VOLATILE GLOBAL g_67 */
+static volatile int16_t * volatile * volatile * volatile g_68 = &g_67;/* VOLATILE GLOBAL g_68 */
+static uint32_t g_81 = 0x941AE2D2L;
