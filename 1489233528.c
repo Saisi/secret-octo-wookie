7878f6e87ea5b6e52a79f16a66d49cb47d293620
saisi <saisi@users.noreply.github.com>
2017-03-11 11:58:49 +0000

    1489233528 anisomyodous

---
+++ 1489233528.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1210355994
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0UL;
+static volatile int8_t g_11 = (-7L);/* VOLATILE GLOBAL g_11 */
+static volatile int8_t *g_10 = &g_11;
+static int64_t g_12[3] = {0x57689A5A40F2FDADLL,0x57689A5A40F2FDADLL,0x57689A5A40F2FDADLL};
+static union U0 g_13 = {0x1479B69BL};/* VOLATILE GLOBAL g_13 */
