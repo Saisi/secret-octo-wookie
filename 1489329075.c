d3a703e55c5ed066089b31f06b94afd32e6069a5
saisi <saisi@users.noreply.github.com>
2017-03-12 14:31:16 +0000

    1489329075 angia

---
+++ 1489329075.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1201246239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0xD832937AL;
+static uint16_t g_39 = 0x7649L;
+static int32_t g_54 = 1L;
+static struct S0 g_86 = {124};
+static int8_t g_105 = 0L;
