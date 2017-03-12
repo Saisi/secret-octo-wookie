2ea618108ef65add0dde25e7d91d142b4621cb66
saisi <saisi@users.noreply.github.com>
2017-03-12 19:31:40 +0000

    1489347099 aerostatics

---
+++ 1489347099.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4008141975
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+   volatile uint32_t  f1;
+   int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_24 = 0xAFBDL;/* VOLATILE GLOBAL g_24 */
+static int32_t *g_32 = (void*)0;
+static uint32_t g_38 = 4UL;
