a88f1d22c8e3226b4f5cf796b989c6842cbc1b63
saisi <saisi@users.noreply.github.com>
2017-03-12 19:01:37 +0000

    1489345297 anthropocentrism

---
+++ 1489345297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2824169447
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   uint64_t  f1;
+   int32_t  f2;
+   int64_t  f3;
+   const volatile int32_t  f4;
+   const volatile uint32_t  f5;
+   const volatile uint32_t  f6;
+   int8_t  f7;
+   volatile unsigned f8 : 7;
