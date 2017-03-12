61cee9d1cb1b8386adfd96fb4197c5fe80c4a95c
saisi <saisi@users.noreply.github.com>
2017-03-12 15:31:20 +0000

    1489332680 acetazolamide

---
+++ 1489332680.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1762542047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0L;
+static int32_t *g_8 = &g_9;
+static uint32_t g_49 = 0x7BF19931L;
+static uint32_t g_57 = 0xCA925F32L;
+static uint32_t g_67 = 7UL;
+static uint32_t g_68 = 18446744073709551607UL;
+static int64_t g_74 = 0L;
+static int32_t g_80 = 0xF34FA629L;
+static int32_t * volatile g_79 = &g_80;/* VOLATILE GLOBAL g_79 */
