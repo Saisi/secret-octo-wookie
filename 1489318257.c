a06e54d44fc487d9ca34387708f18af46dde96d4
saisi <saisi@users.noreply.github.com>
2017-03-12 11:30:58 +0000

    1489318257 anilino

---
+++ 1489318257.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      344641443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 16;
+   int32_t  f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xF20FE6E3L;
+static int32_t g_5 = 7L;
+static int32_t g_8 = (-2L);
