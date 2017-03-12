0630460e308314a39d1faf6c4105530047047648
saisi <saisi@users.noreply.github.com>
2017-03-12 06:30:31 +0000

    1489300231 aryballos

---
+++ 1489300231.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      672548120
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x477862FDL;
+static int32_t *g_16 = (void*)0;
+static int32_t g_56[2] = {0xEF9F3B06L,0xEF9F3B06L};
+static int32_t *g_55[2] = {&g_56[1],&g_56[1]};
+static int32_t *g_57[6][7] = {{&g_56[1],&g_56[1],&g_2,&g_2,&g_56[1],&g_2,&g_56[1]},{&g_56[1],&g_56[1],&g_2,&g_56[1],&g_56[0],&g_56[0],&g_56[1]},{(void*)0,&g_2,(void*)0,&g_56[1],&g_56[1],(void*)0,&g_2},{&g_56[1],&g_2,&g_56[1],&g_2,&g_2,&g_2,&g_2},{(void*)0,&g_56[1],&g_56[1],(void*)0,&g_2,(void*)0,&g_56[1]},{&g_56[0],&g_56[1],&g_56[1],&g_2,&g_56[1],&g_56[0],&g_56[0]}};
+static int16_t g_60 = (-8L);
+static int16_t *g_72 = &g_60;
+static int16_t *g_73 = &g_60;
+static int64_t g_82 = (-1L);
