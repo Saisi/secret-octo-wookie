ff66f2fb2e1782b96e437db7e85d2e08bdb99549
saisi <saisi@users.noreply.github.com>
2017-03-12 10:30:53 +0000

    1489314653 ataraxy

---
+++ 1489314653.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4283912363
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile int32_t  f1;
+   uint32_t  f2;
+   int32_t  f3;
+   volatile signed f4 : 18;
+};
+
+union U1 {
+   volatile int8_t * volatile  f0;
