584cc9480cfa7c235debd75de7a7a6b8548e740a
saisi <saisi@users.noreply.github.com>
2017-03-12 17:01:27 +0000

    1489338087 aeolomelodicon

---
+++ 1489338087.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2766183818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   const volatile unsigned f1 : 28;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S0 g_4 = {270,15593};/* VOLATILE GLOBAL g_4 */
+static volatile struct S0 *g_3[5][3][8] = {{{&g_4,&g_4,&g_4,(void*)0,&g_4,&g_4,&g_4,(void*)0},{(void*)0,&g_4,&g_4,&g_4,(void*)0,(void*)0,&g_4,&g_4},{&g_4,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0}},{{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,(void*)0,&g_4},{(void*)0,&g_4,(void*)0,(void*)0,&g_4,(void*)0,&g_4,&g_4},{&g_4,&g_4,&g_4,(void*)0,&g_4,&g_4,(void*)0,&g_4}},{{&g_4,(void*)0,&g_4,&g_4,(void*)0,&g_4,&g_4,&g_4},{&g_4,(void*)0,(void*)0,&g_4,&g_4,&g_4,(void*)0,&g_4},{&g_4,&g_4,(void*)0,&g_4,(void*)0,&g_4,&g_4,&g_4}},{{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,(void*)0,&g_4,&g_4,&g_4,(void*)0,&g_4,&g_4}},{{&g_4,&g_4,&g_4,&g_4,(void*)0,(void*)0,&g_4,&g_4},{&g_4,&g_4,(void*)0,&g_4,(void*)0,&g_4,(void*)0,&g_4},{&g_4,&g_4,(void*)0,(void*)0,&g_4,&g_4,&g_4,&g_4}}};
+static int32_t g_8[4][3] = {{0xA8ED40A1L,0x405CEFD0L,0x874DA862L},{1L,0x405CEFD0L,1L},{0xCF592758L,0xA8ED40A1L,0x874DA862L},{0xCF592758L,0xCF592758L,0xA8ED40A1L}};
+static uint16_t g_19 = 0x28ABL;
