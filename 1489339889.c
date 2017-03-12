e4de6940cbc9e847166a9888c056b62a2b79c89d
saisi <saisi@users.noreply.github.com>
2017-03-12 17:31:30 +0000

    1489339889 abducing

---
+++ 1489339889.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3922565517
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6 = 0UL;
+static uint32_t g_7 = 18446744073709551613UL;
+static uint8_t g_46 = 0x21L;
+static uint64_t g_62 = 0UL;
+static int32_t g_68 = 3L;
+static int32_t *g_94[9] = {(void*)0,&g_68,(void*)0,(void*)0,&g_68,(void*)0,(void*)0,&g_68,(void*)0};
+static int32_t ** volatile g_93 = &g_94[3];/* VOLATILE GLOBAL g_93 */
+static volatile uint16_t g_99[3] = {0UL,0UL,0UL};
+static volatile int32_t g_101 = 0x939A49A9L;/* VOLATILE GLOBAL g_101 */
