29535bd59a3c5597821a214b95df5d7e27358b11
saisi <saisi@users.noreply.github.com>
2017-03-12 04:30:22 +0000

    1489293021 argol

---
+++ 1489293021.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      850158401
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 31;
+   signed f1 : 26;
+   unsigned f2 : 21;
+   const unsigned f3 : 12;
+   uint16_t  f4;
+   unsigned f5 : 31;
+   signed f6 : 6;
