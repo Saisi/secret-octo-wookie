7074bd8334d40e517bce58fd7eb5671837ffc8ac
saisi <saisi@users.noreply.github.com>
2017-03-12 10:00:51 +0000

    1489312851 amberous

---
+++ 1489312851.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3841604335
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x5B57FD54L;
+static uint32_t g_30[9][1][8] = {{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}},{{0UL,1UL,1UL,0UL,0UL,1UL,1UL,0UL}}};
+static uint32_t g_62 = 3UL;
+static uint32_t *g_61 = &g_62;
+static int64_t g_68[8] = {0x2FEE613944077424LL,0x70207789B64D43D0LL,0x2FEE613944077424LL,0x2FEE613944077424LL,0x70207789B64D43D0LL,0x2FEE613944077424LL,0x2FEE613944077424LL,0x70207789B64D43D0LL};
+static int8_t g_83 = 6L;
+static uint8_t g_85 = 0x70L;
+static uint8_t g_103 = 0xAEL;
+static int32_t ** volatile g_105 = (void*)0;/* VOLATILE GLOBAL g_105 */
