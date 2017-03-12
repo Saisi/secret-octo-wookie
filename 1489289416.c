62acfc94f4ca2a128a6d8976bbe4c5499a397074
saisi <saisi@users.noreply.github.com>
2017-03-12 03:30:17 +0000

    1489289416 angletouch

---
+++ 1489289416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3746900956
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 28;
+   signed f1 : 1;
+   const signed f2 : 31;
+   volatile signed f3 : 15;
+   uint16_t  f4;
+   volatile signed f5 : 12;
+   unsigned f6 : 29;
