99f1d2dad78b2c44c1d095b7101e2c699431d6b2
saisi <saisi@users.noreply.github.com>
2017-03-12 03:00:14 +0000

    1489287614 aldehydase

---
+++ 1489287614.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3219152865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 15;
+   int32_t  f1;
+   const signed f2 : 10;
+   unsigned f3 : 6;
+   signed f4 : 21;
+   unsigned f5 : 15;
+   volatile unsigned f6 : 22;
+   int64_t  f7;
+};
