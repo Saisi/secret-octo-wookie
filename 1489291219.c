8cb321f11e8b79c9bdea69710a153edcecae8e48
saisi <saisi@users.noreply.github.com>
2017-03-12 04:00:19 +0000

    1489291219 antepreterit

---
+++ 1489291219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      327029833
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int16_t  f1;
+};
+
+struct S1 {
+   volatile int32_t  f0;
+};
+
+union U2 {
