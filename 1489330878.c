1f1ec5462e640d05beeaf8d675c637176a0ec61e
saisi <saisi@users.noreply.github.com>
2017-03-12 15:01:18 +0000

    1489330878 ageings

---
+++ 1489330878.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1681460800
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[2][9] = {{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),0x7AB3921DL,(-1L),0x7AB3921DL,(-1L),0x7AB3921DL,(-1L),0x7AB3921DL,(-1L)}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
