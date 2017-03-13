a7c1d1c5237d31d3a786f3455017fa6d38337c0c
saisi <saisi@users.noreply.github.com>
2017-03-13 11:02:59 +0000

    1489402978 amazements

---
+++ 1489402978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3259865876
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_10 = 0x63ABL;
+static int8_t g_13 = 4L;
+static int8_t g_15 = (-4L);
+static uint8_t g_37[7] = {0x91L,0x91L,0x91L,0x91L,0x91L,0x91L,0x91L};
+static uint8_t g_39 = 0x26L;
+static uint8_t g_41 = 0UL;
+static int32_t g_43 = 0x4B1C2FBFL;
+static volatile int32_t g_44[5] = {0x8564DBB9L,0x8564DBB9L,0x8564DBB9L,0x8564DBB9L,0x8564DBB9L};
+static uint32_t g_139 = 0x35C7AEBAL;
