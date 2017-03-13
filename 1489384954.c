0dd478bfb62f25a71891f0e25378ed5bf217887e
saisi <saisi@users.noreply.github.com>
2017-03-13 06:02:35 +0000

    1489384954 avertin

---
+++ 1489384954.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      28688840
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 0UL;
+static uint8_t g_26 = 255UL;
+static uint16_t g_40 = 65535UL;
+static uint16_t g_44 = 0x7A0DL;
+static uint16_t g_46 = 0xDF39L;
+static uint8_t g_88 = 0xB0L;
+static uint8_t *g_87 = &g_88;
+static uint16_t g_90 = 0x2577L;
+static int32_t g_94 = 0x6445A0AEL;
