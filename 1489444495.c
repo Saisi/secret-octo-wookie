822b23fb88e0ffd8e0815b5d265d7d97747a9dab
saisi <saisi@users.noreply.github.com>
2017-03-13 22:34:55 +0000

    1489444495 asynchrony

---
+++ 1489444495.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4083100595
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   const signed f1 : 15;
+   signed f2 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 0x6887E08AL;
+static int16_t g_15 = 0xA4DFL;
+static int16_t g_22 = 1L;
