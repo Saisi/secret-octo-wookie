66868d8956810607477c7cefab43d71e98ac5dad
saisi <saisi@users.noreply.github.com>
2017-03-13 18:34:35 +0000

    1489430074 antihierarchy

---
+++ 1489430074.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2846367848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile int64_t  f1;
+   int32_t  f2;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   struct S0  f1;
+};
