7d56e7065fe955324431980444a8a192555c7932
saisi <saisi@users.noreply.github.com>
2017-03-13 12:03:06 +0000

    1489406586 acrotisms

---
+++ 1489406586.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1854432422
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 1;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x897A4A12L;
+static uint32_t g_82 = 0UL;
+static uint64_t g_84 = 0x6E0E3F56F60D197ALL;
+static uint32_t g_88 = 0xE4C0C5DCL;
