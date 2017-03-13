b1fad2c5df6d71d0cefe1fa72d6ac28dbbbc5326
saisi <saisi@users.noreply.github.com>
2017-03-13 19:04:37 +0000

    1489431876 Aethusa

---
+++ 1489431876.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2737179351
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   signed f1 : 25;
+   volatile uint32_t  f2;
+   signed f3 : 9;
+   volatile signed f4 : 12;
+   signed f5 : 10;
+   unsigned f6 : 5;
+};
+
