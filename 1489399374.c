1fabb9ecd156c0048a4a3f44aeb40a0c5f14be0c
saisi <saisi@users.noreply.github.com>
2017-03-13 10:02:54 +0000

    1489399374 Arisbe

---
+++ 1489399374.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1899818253
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 5;
+   unsigned f1 : 8;
+   volatile signed f2 : 11;
+   unsigned f3 : 10;
+   const signed f4 : 19;
+   signed : 0;
+   unsigned f5 : 31;
+   const unsigned f6 : 22;
+};
