279805493adc44c434f4efe8500fb40d0d086efa
saisi <saisi@users.noreply.github.com>
2017-03-13 08:32:47 +0000

    1489393966 Alfeo

---
+++ 1489393966.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3588569275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_14[7] = {8UL,8UL,8UL,8UL,8UL,8UL,8UL};
+static volatile int32_t g_15 = (-1L);/* VOLATILE GLOBAL g_15 */
+static int32_t g_16 = 0x5459EEDAL;
+static int32_t g_18 = 0xDEC4F3F7L;
+static int64_t g_27 = 8L;
+static volatile uint32_t g_28[6] = {0x2105ADB5L,0x2105ADB5L,0x2105ADB5L,0x2105ADB5L,0x2105ADB5L,0x2105ADB5L};
+static int32_t g_49 = 0L;
+static int32_t *g_62[1][6] = {{&g_49,&g_49,&g_18,&g_49,&g_49,&g_18}};
+static int32_t * volatile *g_61 = &g_62[0][1];
