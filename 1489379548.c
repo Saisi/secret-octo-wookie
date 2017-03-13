c285026772451c320ed6cbd709b40a11ca8ab9b8
saisi <saisi@users.noreply.github.com>
2017-03-13 04:32:28 +0000

    1489379548 animallike

---
+++ 1489379548.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3170754420
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x6F7FBF21L;
+static int8_t g_7 = 0xEFL;
+static int32_t g_27 = 0x8FAD7494L;
+static uint8_t g_44 = 0UL;
+static int16_t g_55 = 1L;
+static int32_t *g_79 = &g_27;
+static uint32_t g_83 = 6UL;
+static const int64_t g_91 = 0x22517922C1E9780CLL;
+static int32_t g_94 = 0x8281B7FFL;
