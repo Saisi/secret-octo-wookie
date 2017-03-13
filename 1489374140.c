492f87c92cd9901142109d35c75ad1efa87da1b5
saisi <saisi@users.noreply.github.com>
2017-03-13 03:02:20 +0000

    1489374140 Bandinelli

---
+++ 1489374140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4001229919
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_6 = 0x78144AD72D76715FLL;
+static const uint8_t g_23 = 0UL;
+static volatile uint64_t g_32 = 0xC415051ACAE7BF19LL;/* VOLATILE GLOBAL g_32 */
+static volatile uint64_t * volatile g_31[4][8][3] = {{{(void*)0,(void*)0,&g_32},{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,(void*)0,&g_32},{&g_32,&g_32,&g_32}},{{&g_32,(void*)0,&g_32},{&g_32,(void*)0,(void*)0},{&g_32,&g_32,&g_32},{&g_32,(void*)0,(void*)0},{(void*)0,&g_32,(void*)0},{&g_32,&g_32,&g_32},{(void*)0,&g_32,(void*)0},{&g_32,&g_32,&g_32}},{{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,(void*)0,&g_32},{&g_32,&g_32,&g_32},{&g_32,&g_32,&g_32},{&g_32,(void*)0,&g_32},{&g_32,&g_32,&g_32},{&g_32,(void*)0,&g_32}},{{(void*)0,(void*)0,&g_32},{(void*)0,(void*)0,&g_32},{&g_32,&g_32,(void*)0},{(void*)0,(void*)0,&g_32},{&g_32,&g_32,(void*)0},{&g_32,&g_32,(void*)0},{&g_32,(void*)0,(void*)0},{&g_32,&g_32,(void*)0}}};
+static uint64_t *g_56 = (void*)0;
+static int64_t g_74 = (-1L);
+static int16_t g_76 = 4L;
+static int16_t g_79 = 0L;
+static int32_t g_83[2] = {1L,1L};
