8b5becbd3d9d149f56d7b818d59455af819effbf
saisi <saisi@users.noreply.github.com>
2017-03-13 08:02:44 +0000

    1489392164 Arctogaean

---
+++ 1489392164.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2923882854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   volatile unsigned f1 : 18;
+   signed f2 : 12;
+   const unsigned f3 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0L;
+static int32_t g_6 = 0x8872FA4AL;
