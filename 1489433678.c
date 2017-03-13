2aac6283521c3fc9578bae4107ba6650bf013c13
saisi <saisi@users.noreply.github.com>
2017-03-13 19:34:39 +0000

    1489433678 aeraria

---
+++ 1489433678.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3038856619
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 0x1F200025260417BELL;
+static uint32_t g_4 = 0xDE6F885EL;
+static uint64_t g_5 = 18446744073709551608UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
