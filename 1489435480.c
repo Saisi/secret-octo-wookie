2b1345d08cd783ac704a32c7d29a78cb899d57f0
saisi <saisi@users.noreply.github.com>
2017-03-13 20:04:40 +0000

    1489435480 arterious

---
+++ 1489435480.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1477594538
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x79E672CAL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xBF90A233L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 9L;
+static volatile int32_t g_6 = 0xD0D658EDL;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0x7A7525D2L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8 = (-6L);/* VOLATILE GLOBAL g_8 */
+static int32_t g_9 = 0x13E13A6CL;
+static int32_t *g_13 = (void*)0;
+static int32_t ** volatile g_12[6] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
