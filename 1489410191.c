075236c76b3cd8737d3ea1940f510a2c9959fb3e
saisi <saisi@users.noreply.github.com>
2017-03-13 13:03:12 +0000

    1489410191 alkanol

---
+++ 1489410191.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1469417769
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile int8_t g_4 = 0xF6L;/* VOLATILE GLOBAL g_4 */
+static const volatile int8_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static int16_t g_6 = 0x0FA5L;
+static volatile int8_t g_7 = 0xC9L;/* VOLATILE GLOBAL g_7 */
+static volatile uint32_t g_10 = 0xEEA53061L;/* VOLATILE GLOBAL g_10 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
