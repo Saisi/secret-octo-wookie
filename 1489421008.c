5b6a967c6f435f2b60cdc7fa940a24848f680c68
saisi <saisi@users.noreply.github.com>
2017-03-13 16:03:29 +0000

    1489421008 abolishing

---
+++ 1489421008.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2206476025
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 27;
+   signed f1 : 6;
+   unsigned f2 : 17;
+   unsigned f3 : 24;
+   volatile signed f4 : 9;
+   signed f5 : 30;
+   unsigned f6 : 5;
