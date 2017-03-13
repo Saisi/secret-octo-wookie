69b35a4a0e44ffd1e8039aa35d1078283e33dac2
saisi <saisi@users.noreply.github.com>
2017-03-13 11:33:01 +0000

    1489404781 albugines

---
+++ 1489404781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      195015710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 29;
+   const signed f1 : 30;
+   const signed f2 : 8;
+   unsigned f3 : 20;
+   unsigned f4 : 19;
+   signed f5 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
