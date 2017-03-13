0c332e7df5e60b1cded5123b7d5b0e418248e1d0
saisi <saisi@users.noreply.github.com>
2017-03-13 07:02:40 +0000

    1489388559 Agoraeus

---
+++ 1489388559.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1110500338
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9 = 0x9DA4D073L;
+static int32_t *g_8 = &g_9;
+static uint8_t g_38 = 0x1EL;
+static int32_t g_70[4] = {(-5L),(-5L),(-5L),(-5L)};
+static uint64_t g_72 = 18446744073709551613UL;
+static uint8_t g_76 = 0xB9L;
+static uint8_t *g_75 = &g_76;
+static int32_t g_78 = 0xA55B90CBL;
+static int32_t * volatile *g_85 = (void*)0;
