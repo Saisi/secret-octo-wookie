93800f678fc3e2e4d4874afaaf9c53b0e9dd41dd
saisi <saisi@users.noreply.github.com>
2017-03-13 07:32:42 +0000

    1489390362 antiputrescent

---
+++ 1489390362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2358067748
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   const int32_t  f1;
+};
+
+union U1 {
+   volatile int8_t * f0;
+   int32_t  f1;
+   volatile int32_t  f2;
+   int16_t  f3;
