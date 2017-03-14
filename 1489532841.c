4b24c7bd5d156b6449e61115f5f326817e8387aa
saisi <saisi@users.noreply.github.com>
2017-03-14 23:07:21 +0000

    1489532841 adenohypophysis

---
+++ 1489532841.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      185204342
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint8_t  f0;
+   int8_t  f1;
+   uint32_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_22[5] = {0x2FD2D1A4E665969ALL,0x2FD2D1A4E665969ALL,0x2FD2D1A4E665969ALL,0x2FD2D1A4E665969ALL,0x2FD2D1A4E665969ALL};
+static int8_t g_34 = (-4L);
