909f3b9f949cf922ab65277b10f4cb81aa3809cd
saisi <saisi@users.noreply.github.com>
2017-03-14 04:35:35 +0000

    1489466135 amphitricha

---
+++ 1489466135.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1047771401
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   uint32_t  f0;
+};
+
+union U5 {
+   int8_t * const  f0;
+   int64_t  f1;
+   int64_t  f2;
+   volatile int64_t  f3;
+   int32_t  f4;
