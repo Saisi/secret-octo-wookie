174f4af41737d8452d304c1fe9e9a50da9e14213
saisi <saisi@users.noreply.github.com>
2017-03-14 02:05:20 +0000

    1489457119 astrologian

---
+++ 1489457119.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3591329761
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 15;
+   signed f1 : 10;
+   unsigned f2 : 5;
+   signed f3 : 28;
+   unsigned f4 : 29;
+   volatile signed f5 : 2;
+   const volatile signed f6 : 25;
