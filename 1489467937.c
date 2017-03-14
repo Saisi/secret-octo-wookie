ee12a2e2ff0099ce485ffb0297fa037bbacd2358
saisi <saisi@users.noreply.github.com>
2017-03-14 05:05:38 +0000

    1489467937 antigravitation

---
+++ 1489467937.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1775571351
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t *g_2 = (void*)0;
+static int8_t g_5 = 0x48L;
+static int8_t * volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+static int32_t g_9 = 7L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
