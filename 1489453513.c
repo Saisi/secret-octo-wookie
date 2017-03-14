ea10b8e50bcb98d83118ea5af7a09fd9fb358521
saisi <saisi@users.noreply.github.com>
2017-03-14 01:05:13 +0000

    1489453513 asseveratory

---
+++ 1489453513.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      106621343
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   volatile unsigned f1 : 13;
+   int8_t  f2;
+   unsigned f3 : 13;
+   unsigned : 0;
+   const signed f4 : 14;
+   volatile unsigned f5 : 25;
+};
+
