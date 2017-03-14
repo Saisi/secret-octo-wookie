14903f5dec7c725e2f1fa547d132fc89bbd7a46d
saisi <saisi@users.noreply.github.com>
2017-03-14 15:06:33 +0000

    1489503993 alcohate

---
+++ 1489503993.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      375352134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x4801897AL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 1L;
+static union U0 g_18 = {0x570A50B5L};
+static uint8_t g_49 = 0xCDL;
+static int8_t g_72 = 0xBCL;
