ff971bbbc73eabe54beac483d3b15233fc8ec60e
saisi <saisi@users.noreply.github.com>
2017-03-14 12:36:20 +0000

    1489494980 algological

---
+++ 1489494980.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3028541133
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1][1] = {{0xEAC8B13EL}};
+static int32_t g_3 = 1L;
+static uint8_t g_37 = 255UL;
+static int64_t g_45 = 0xA3F0AEACB2D505BCLL;
+static uint8_t g_47 = 255UL;
+static uint8_t *g_46 = &g_47;
+static int64_t g_49 = (-5L);
+static int32_t g_62 = 6L;
+static int32_t *g_61 = &g_62;
