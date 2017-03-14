d25f62e1c72171dae071f77028b40a339a16ca34
saisi <saisi@users.noreply.github.com>
2017-03-14 15:36:36 +0000

    1489505796 aircraftsmen

---
+++ 1489505796.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2655892000
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-2L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t * volatile * volatile g_6[10] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static volatile int32_t * volatile * const  volatile g_7[8] = {&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
