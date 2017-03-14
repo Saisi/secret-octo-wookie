a140560ef877e4dc4fb4d679529e09687f3cf16c
saisi <saisi@users.noreply.github.com>
2017-03-14 19:06:59 +0000

    1489518417 arrectary

---
+++ 1489518417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4118609525
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   uint32_t  f1;
+   uint8_t  f2;
+   const volatile int64_t  f3;
+   int32_t  f4;
+   uint8_t  f5;
+   uint16_t  f6;
+   int64_t  f7;
+   uint32_t  f8;
