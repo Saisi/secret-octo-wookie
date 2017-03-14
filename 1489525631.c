63f9e4602b25b7828c95fdc6b62342e33a15edc5
saisi <saisi@users.noreply.github.com>
2017-03-14 21:07:12 +0000

    1489525631 agrodolce

---
+++ 1489525631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4020177861
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 1;
+   int32_t  f1;
+   volatile uint8_t  f2;
+   uint16_t  f3;
+   volatile int8_t * const  f4;
+};
+
+union U1 {
+   uint64_t  f0;
