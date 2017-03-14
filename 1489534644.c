27a683497680e295c930656d6c1cc7cac483c166
saisi <saisi@users.noreply.github.com>
2017-03-14 23:37:24 +0000

    1489534644 antichthon

---
+++ 1489534644.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1762318503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   volatile uint32_t  f1;
+   volatile unsigned f2 : 25;
+   const volatile signed f3 : 23;
+   const uint8_t  f4;
+   signed f5 : 17;
+   volatile signed f6 : 16;
+};
+
