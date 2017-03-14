99a70dab837d0ba6765c4af90b766bc5645183a2
saisi <saisi@users.noreply.github.com>
2017-03-14 17:06:46 +0000

    1489511205 Aceratherium

---
+++ 1489511205.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4222796988
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   uint64_t  f1;
+   unsigned f2 : 4;
+   int64_t  f3;
+   unsigned : 0;
+};
+#pragma pack(pop)
