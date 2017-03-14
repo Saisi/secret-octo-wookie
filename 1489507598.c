7e17dc27c7a87db9ad2e6df7f3461e55ee8df432
saisi <saisi@users.noreply.github.com>
2017-03-14 16:06:39 +0000

    1489507598 augustness

---
+++ 1489507598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2755564658
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 7;
+   signed : 0;
+   signed f1 : 2;
+   unsigned f2 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
