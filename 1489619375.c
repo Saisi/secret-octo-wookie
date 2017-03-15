25ddc2aaee213ae575229201be24957cb6f1bd79
saisi <saisi@users.noreply.github.com>
2017-03-15 23:09:37 +0000

    1489619375 aula

---
+++ 1489619375.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3433801070
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_26 = 0x29EF4A04L;
+static int32_t g_28 = 1L;
+static int32_t *g_27 = &g_28;
+static int32_t *g_32 = &g_28;
+static uint16_t g_41 = 0x32B7L;
+static uint16_t g_55 = 1UL;
+static uint64_t g_102 = 0xDFF61739BB5F37D7LL;
+static int8_t g_110[8] = {0L,(-6L),0L,0L,(-6L),0L,0L,(-6L)};
+static uint32_t g_134 = 0xC44AE9DAL;
