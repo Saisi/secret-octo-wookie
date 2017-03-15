8a88f9178fa0f652913255cd2a91dd3b3ea51faa
saisi <saisi@users.noreply.github.com>
2017-03-15 11:38:32 +0000

    1489577911 autotoxin

---
+++ 1489577911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3034312029
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+};
+
+union U1 {
+   int8_t  f0;
+   const int8_t  f1;
+   volatile uint16_t  f2;
+   int32_t  f3;
+};
