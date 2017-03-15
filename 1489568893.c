b9e7130432c22a0811cfabfe31f93b1b23c280da
saisi <saisi@users.noreply.github.com>
2017-03-15 09:08:15 +0000

    1489568893 Antiarcha

---
+++ 1489568893.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3704517047
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 26;
+   const unsigned f1 : 9;
+   const volatile unsigned f2 : 8;
+   unsigned f3 : 19;
+   const volatile signed f4 : 24;
+   int32_t  f5;
+   signed f6 : 21;
