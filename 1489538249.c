10af1f5971e2b151912a491ed260baabc99696d5
saisi <saisi@users.noreply.github.com>
2017-03-15 00:37:29 +0000

    1489538249 Bahmanid

---
+++ 1489538249.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3799281638
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile uint8_t  f1;
+   uint8_t  f2;
+   uint16_t  f3;
+   const unsigned f4 : 16;
+   uint32_t  f5;
+};
+
+struct S1 {
