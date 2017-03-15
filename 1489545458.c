940d14d211f21a53261caa5283e1d374de957612
saisi <saisi@users.noreply.github.com>
2017-03-15 02:37:38 +0000

    1489545458 annoyance

---
+++ 1489545458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1931118356
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 24;
+   int32_t  f1;
+   unsigned f2 : 5;
+   signed f3 : 19;
+   unsigned f4 : 12;
+   const signed f5 : 8;
+   volatile int32_t  f6;
+};
+
