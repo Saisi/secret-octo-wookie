61db0f7684ed66a1d52454d8f6049015f720b155
saisi <saisi@users.noreply.github.com>
2017-03-15 06:37:58 +0000

    1489559878 antimythic

---
+++ 1489559878.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1404831593
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_7 = 0x57BF5CD5L;
+static int32_t *g_8 = (void*)0;
+static int32_t ** volatile g_9 = &g_8;/* VOLATILE GLOBAL g_9 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+static int32_t * func_2(uint32_t  p_3);
+
