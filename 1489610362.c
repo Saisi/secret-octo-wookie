7a79e2bad247eb10600db5dbb4f395a8c88fa6c7
saisi <saisi@users.noreply.github.com>
2017-03-15 20:39:23 +0000

    1489610362 anapneic

---
+++ 1489610362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4247193566
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   volatile unsigned f1 : 30;
+   signed f2 : 8;
+   signed f3 : 20;
+   signed f4 : 19;
+   unsigned f5 : 30;
+   const signed f6 : 4;
+   const unsigned f7 : 9;
+};
