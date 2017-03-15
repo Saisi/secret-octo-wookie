9e65c35b04333805ea22bf3f85313cec25956a36
saisi <saisi@users.noreply.github.com>
2017-03-15 21:09:25 +0000

    1489612165 aporetic

---
+++ 1489612165.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2766290489
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   unsigned f1 : 6;
+   volatile signed f2 : 17;
+   unsigned f3 : 24;
+   unsigned f4 : 9;
+   const unsigned f5 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
