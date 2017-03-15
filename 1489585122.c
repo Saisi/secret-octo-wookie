d841a8d600b179320afb43161467125864197d49
saisi <saisi@users.noreply.github.com>
2017-03-15 13:38:42 +0000

    1489585122 acone

---
+++ 1489585122.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      920968781
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   signed f1 : 8;
+   const int8_t  f2;
+   volatile uint32_t  f3;
+};
+
+struct S1 {
+   unsigned f0 : 27;
+   volatile unsigned f1 : 19;
