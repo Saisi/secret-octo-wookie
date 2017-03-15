81ad6a7004b6d7db73429e4228284d62e06eec4a
saisi <saisi@users.noreply.github.com>
2017-03-15 01:37:34 +0000

    1489541854 alveary

---
+++ 1489541854.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1321379946
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   int8_t * const  volatile  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_6 = 1UL;
+static int16_t g_8 = (-1L);
+static int8_t g_25[8] = {0x9CL,0x9CL,0x9CL,0x9CL,0x9CL,0x9CL,0x9CL,0x9CL};
+static volatile int8_t * volatile * volatile g_26[4] = {(void*)0,(void*)0,(void*)0,(void*)0};
