b12a74ba4a9f7409dd11101385d325e3217afff9
saisi <saisi@users.noreply.github.com>
2017-03-15 11:08:29 +0000

    1489576109 Anabaena

---
+++ 1489576109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2994873848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   const volatile uint32_t  f1;
+   volatile int8_t  f2;
+   uint64_t  f3;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_10 = 1L;/* VOLATILE GLOBAL g_10 */
