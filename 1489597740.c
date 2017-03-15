b3bd8941364024344f8e6c912c892538c22b0045
saisi <saisi@users.noreply.github.com>
2017-03-15 17:09:00 +0000

    1489597740 arbutin

---
+++ 1489597740.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      826733983
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_3 = 5UL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_5 = 0xC1574781L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
