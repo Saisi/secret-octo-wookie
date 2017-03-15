ec25b16a1bfe49c686d1e65fc98e2dbd3dda3ae7
saisi <saisi@users.noreply.github.com>
2017-03-15 05:07:50 +0000

    1489554470 autocollimate

---
+++ 1489554470.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      400980966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 1;
+   signed f1 : 18;
+   unsigned f2 : 12;
+   const unsigned f3 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_10[8] = {0xE4B9A733L,0xE4B9A733L,0xE4B9A733L,0xE4B9A733L,0xE4B9A733L,0xE4B9A733L,0xE4B9A733L,0xE4B9A733L};
+static volatile int32_t g_13 = 0xBA75A592L;/* VOLATILE GLOBAL g_13 */
