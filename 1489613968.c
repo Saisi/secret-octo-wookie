a64fb5111ccbb40bd8ab3f2471b71f392a0f49dd
saisi <saisi@users.noreply.github.com>
2017-03-15 21:39:28 +0000

    1489613968 anthropolite

---
+++ 1489613968.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      380695802
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xBEAA32B1L;
+static uint16_t g_9 = 0x29C1L;
+static int32_t g_17 = 1L;
+static uint16_t g_20 = 0xCC47L;
+static int32_t g_21 = 0x061A6520L;
