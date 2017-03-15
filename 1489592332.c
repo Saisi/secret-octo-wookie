400171ddf70a4818e53be9da4a02ccfb4ec0d3eb
saisi <saisi@users.noreply.github.com>
2017-03-15 15:38:52 +0000

    1489592332 archmugwump

---
+++ 1489592332.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      943381496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   volatile uint16_t  f0;
+   volatile uint8_t  f1;
+   int32_t  f2;
+   int64_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xE2167FA5L;/* VOLATILE GLOBAL g_2 */
