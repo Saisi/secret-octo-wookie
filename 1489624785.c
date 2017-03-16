5b64864e83fbc73fa31f18a5538d99403bab177f
saisi <saisi@users.noreply.github.com>
2017-03-16 00:39:45 +0000

    1489624785 anthropologically

---
+++ 1489624785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1970491986
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_5 = 9L;
+static int8_t *g_7 = &g_5;
+static int8_t **g_6 = &g_7;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
