a024a1a906baa798d060963f2771c2ab2862df61
saisi <saisi@users.noreply.github.com>
2017-03-16 19:11:26 +0000

    1489691486 Baptista

---
+++ 1489691486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3735328419
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6[5][5] = {{0L,0x81CED8F6L,9L,9L,0x81CED8F6L},{0x23E64F14L,(-1L),0xB7928FBCL,0xB7928FBCL,(-1L)},{0L,0x81CED8F6L,9L,9L,0x81CED8F6L},{0x23E64F14L,(-1L),0xB7928FBCL,0xB7928FBCL,(-1L)},{0L,0x81CED8F6L,9L,9L,0x81CED8F6L}};
+static struct S0 g_50 = {-133};
+static int32_t g_54 = 8L;
+static int32_t g_58 = (-1L);
+static int32_t g_60 = (-6L);
