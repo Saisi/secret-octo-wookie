aea493166f0af8540352355e4d444255f98bc3fe
saisi <saisi@users.noreply.github.com>
2017-03-16 23:11:49 +0000

    1489705907 androphorous

---
+++ 1489705907.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1230560585
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 4L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x15A13695L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x386CCE57L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xCD53BC6EL;/* VOLATILE GLOBAL g_5 */
