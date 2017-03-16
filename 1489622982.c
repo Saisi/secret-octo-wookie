100971d1c1a0a611f3de8ea76883de08e2b6f7e6
saisi <saisi@users.noreply.github.com>
2017-03-16 00:09:43 +0000

    1489622982 amphinucleus

---
+++ 1489622982.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1335148454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 1;
+   const unsigned f1 : 18;
+   signed f2 : 12;
+   unsigned f3 : 8;
+   signed f4 : 23;
+   unsigned f5 : 18;
+};
+
+union U1 {
