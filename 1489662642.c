8344f8162930e069d8159bd79ada19c434e83caa
saisi <saisi@users.noreply.github.com>
2017-03-16 11:10:43 +0000

    1489662642 apiole

---
+++ 1489662642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      625205695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xD3DF75DCL;
+static uint16_t g_7[9][2] = {{0x8144L,8UL},{8UL,0x8144L},{8UL,8UL},{0x8144L,8UL},{8UL,0x8144L},{8UL,8UL},{0x8144L,8UL},{8UL,0x8144L},{8UL,8UL}};
+static uint16_t g_10[3][1] = {{0x0234L},{0x0234L},{0x0234L}};
+static int32_t g_12[3] = {0x0F7E7308L,0x0F7E7308L,0x0F7E7308L};
+static int16_t g_29 = 0x12E7L;
+static uint32_t g_79[7][3][2] = {{{0UL,0x91E25501L},{0UL,0UL},{0UL,0x91E25501L}},{{0UL,0UL},{0UL,0x91E25501L},{0UL,0UL}},{{0UL,0x91E25501L},{0UL,0UL},{0UL,0x91E25501L}},{{0UL,0UL},{0UL,0x91E25501L},{0UL,0UL}},{{0UL,0x91E25501L},{0UL,0UL},{0UL,0x91E25501L}},{{0UL,0UL},{0UL,0x91E25501L},{0UL,0UL}},{{0UL,0x91E25501L},{0UL,0UL},{0UL,0x91E25501L}}};
+static int64_t g_93 = 0x42FB88B70E9AFB84LL;
+static uint32_t g_94 = 0xE8C41CEEL;
+static uint32_t g_108 = 0xE40B14FAL;
