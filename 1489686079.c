351432744d4496ee8aa50400a367bc7d0bccbf23
saisi <saisi@users.noreply.github.com>
2017-03-16 17:41:19 +0000

    1489686079 anaerobiosis

---
+++ 1489686079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2424723258
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   volatile int8_t * volatile  f0;
+   const uint32_t  f1;
+   uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U1 g_2 = {0};/* VOLATILE GLOBAL g_2 */
+
+
