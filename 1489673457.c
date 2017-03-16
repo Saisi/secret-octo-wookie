9439c49beea534dcd87b769b2bdf55bd512d751d
saisi <saisi@users.noreply.github.com>
2017-03-16 14:10:58 +0000

    1489673457 ballpoints

---
+++ 1489673457.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1588950237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2[7] = {1L,1L,1L,1L,1L,1L,1L};
+static int32_t g_3 = 1L;
+static int32_t g_4 = 1L;
+static int32_t g_6 = 0x69EC4279L;
+static uint32_t g_40 = 4294967286UL;
+static uint32_t g_81 = 0x0F33E235L;
+static int32_t *g_95 = &g_6;
+static uint32_t *g_96[10] = {&g_81,&g_81,&g_81,&g_81,&g_81,&g_81,&g_81,&g_81,&g_81,&g_81};
+static uint16_t g_98 = 0xBA02L;
