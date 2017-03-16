26173c8710a8c80179cc7fc1bd9f5a99fbc349b8
saisi <saisi@users.noreply.github.com>
2017-03-16 14:41:00 +0000

    1489675260 alliancer

---
+++ 1489675260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2846325943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+union U1 {
+   volatile int32_t  f0;
+};
+
+union U2 {
+   unsigned f0 : 25;
