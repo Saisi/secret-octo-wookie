b34f725b31175ff64c06070a78c3aa168d095c69
saisi <saisi@users.noreply.github.com>
2017-03-16 17:11:17 +0000

    1489684276 Babara

---
+++ 1489684276.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3744691026
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static uint64_t g_47 = 18446744073709551610UL;
+static uint32_t g_80 = 18446744073709551615UL;
+static int32_t g_85 = 0L;
+static int32_t * const  volatile g_84 = &g_85;/* VOLATILE GLOBAL g_84 */
+static int8_t g_96[4][1] = {{0L},{(-6L)},{0L},{(-6L)}};
+static int16_t g_108[6][3][1] = {{{0x15F7L},{0xFB90L},{0L}},{{0xFB90L},{0x15F7L},{0x902DL}},{{0x902DL},{0x15F7L},{0xFB90L}},{{0L},{0xFB90L},{0x15F7L}},{{0x902DL},{0x902DL},{0x15F7L}},{{0xFB90L},{0L},{0xFB90L}}};
+static uint16_t g_113 = 0xDF6EL;
+static int16_t g_150 = (-5L);
