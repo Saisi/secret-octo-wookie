8c8a9cb9c3a4f707535cbf1fb69f35cad6ab20df
saisi <saisi@users.noreply.github.com>
2017-03-16 10:10:38 +0000

    1489659038 Abuna

---
+++ 1489659038.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3900346614
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   const unsigned f1 : 6;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_11 = 1UL;
+static struct S0 g_15[9] = {{0UL,7},{0UL,7},{0UL,7},{0UL,7},{0UL,7},{0UL,7},{0UL,7},{0UL,7},{0UL,7}};
+static int8_t g_26 = 0L;
+static int64_t g_60 = 0x121ADF561F6604C3LL;
