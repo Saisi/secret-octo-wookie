1309202de1f65801ab94db5dd98f16b15f8c9ad9
saisi <saisi@users.noreply.github.com>
2017-03-16 02:39:55 +0000

    1489631994 atmospherology

---
+++ 1489631994.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3722038054
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = (-6L);
+static uint64_t g_22 = 0x1A1F050678E045DCLL;
+static int32_t g_34 = 0L;
+static int32_t * volatile g_33[5][8] = {{&g_34,&g_34,(void*)0,(void*)0,(void*)0,&g_34,&g_34,&g_34},{&g_34,&g_34,&g_34,&g_34,&g_34,(void*)0,(void*)0,&g_34},{&g_34,&g_34,&g_34,&g_34,&g_34,&g_34,&g_34,(void*)0},{&g_34,&g_34,&g_34,&g_34,(void*)0,&g_34,&g_34,&g_34},{&g_34,&g_34,(void*)0,&g_34,&g_34,&g_34,&g_34,&g_34}};
+static int32_t * volatile g_35 = &g_34;/* VOLATILE GLOBAL g_35 */
+static uint64_t *g_42[7][5] = {{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22},{&g_22,&g_22,&g_22,&g_22,&g_22}};
+static uint32_t g_92[8] = {0xACFA70DAL,1UL,0xACFA70DAL,1UL,0xACFA70DAL,1UL,0xACFA70DAL,1UL};
+static int32_t g_97 = 0x4CB426ABL;
+static int32_t g_105[1] = {0xD35785F4L};
