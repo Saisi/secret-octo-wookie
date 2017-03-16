048f73cdb854f282e64e90f2f88b4ec3acb9a77d
saisi <saisi@users.noreply.github.com>
2017-03-16 19:41:29 +0000

    1489693289 backspacefile

---
+++ 1489693289.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      42121884
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   const volatile unsigned f0 : 26;
+   uint16_t  f1;
+   unsigned f2 : 12;
+   volatile unsigned f3 : 13;
+   const unsigned f4 : 7;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x54C97515L;
