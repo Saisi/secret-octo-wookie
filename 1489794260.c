9d406c1a6aa854fb7e83813db32e33b9dd9afecc
saisi <saisi@users.noreply.github.com>
2017-03-17 23:44:20 +0000

    1489794260 bakestone

---
+++ 1489794260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3811718457
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-6L);
+static volatile uint16_t g_24 = 0x466EL;/* VOLATILE GLOBAL g_24 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
