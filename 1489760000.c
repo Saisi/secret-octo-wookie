e36a74630301ff86fb5fd8390a3e70936d80acf0
saisi <saisi@users.noreply.github.com>
2017-03-17 14:13:21 +0000

    1489760000 Aponogetonaceae

---
+++ 1489760000.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      244937046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 25;
+   const unsigned f1 : 10;
+   const signed f2 : 4;
+   const signed : 0;
+   int32_t  f3;
+   signed f4 : 9;
+   volatile signed f5 : 6;
+   uint32_t  f6;
+};
