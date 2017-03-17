2a613cca6dcb8b5b156088aedecbeb4bc18a34c0
saisi <saisi@users.noreply.github.com>
2017-03-17 07:42:40 +0000

    1489736560 argumentativeness

---
+++ 1489736560.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2472822088
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_17 = 18446744073709551615UL;/* VOLATILE GLOBAL g_17 */
+static int64_t g_42 = 0xDB80EBBD4856CD83LL;
+static uint8_t g_44 = 0UL;
+static int8_t g_48 = 0x81L;
+static int64_t g_51 = (-7L);
+static int32_t g_70 = 2L;
+static int32_t g_107 = 0x6835AC29L;
+static uint8_t g_115 = 0xA8L;
+static int32_t **g_123 = (void*)0;
