55d5cc94e6a11619bcbb8a2aac14c79d77ba4bad
saisi <saisi@users.noreply.github.com>
2017-03-17 03:12:14 +0000

    1489720333 Aias

---
+++ 1489720333.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      380590379
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const signed f0 : 20;
+   int8_t * f1;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
+union U2 {
