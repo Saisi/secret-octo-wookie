e0d39285b22d379186f32127a91bf97ce525c3f3
saisi <saisi@users.noreply.github.com>
2017-03-17 13:13:16 +0000

    1489756395 anaphrodisiac

---
+++ 1489756395.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2676338015
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 14;
+   unsigned f1 : 13;
+   unsigned f2 : 28;
+   unsigned f3 : 23;
+   const volatile unsigned f4 : 12;
+   unsigned f5 : 21;
+   const unsigned : 0;
