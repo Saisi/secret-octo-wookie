fba3cfec0e2c32344a51f8fc2f44b54c9ef9fe3a
saisi <saisi@users.noreply.github.com>
2017-03-17 00:11:53 +0000

    1489709513 Asherites

---
+++ 1489709513.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      722004885
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   const signed f1 : 28;
+   unsigned f2 : 15;
+   signed f3 : 30;
+   volatile signed f4 : 23;
+   volatile unsigned f5 : 20;
+   volatile signed f6 : 3;
+   int32_t  f7;
+};
