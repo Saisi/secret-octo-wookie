3d5e4d407285a498b176e79f5f1d3097743a9fb1
saisi <saisi@users.noreply.github.com>
2017-03-17 17:13:36 +0000

    1489770816 aeroballistic

---
+++ 1489770816.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3284114624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   unsigned f1 : 28;
+   uint16_t  f2;
+   int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = (-1L);
+static int32_t g_33 = 0xC44D9964L;
