cf7ac82317a7c338855107ead7e2a6d3ab987617
saisi <saisi@users.noreply.github.com>
2017-03-17 22:14:12 +0000

    1489788851 abada

---
+++ 1489788851.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2904145477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int16_t  f0;
+   int32_t  f1;
+   volatile int64_t  f2;
+   int16_t  f3;
+   volatile int64_t  f4;
+   const int16_t  f5;
+   int32_t  f6;
