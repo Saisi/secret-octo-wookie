8b956223ced2fddab17bf805c681458b2e0f4fed
saisi <saisi@users.noreply.github.com>
2017-03-17 09:42:57 +0000

    1489743770 Anchusa

---
+++ 1489743770.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3101852383
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   int32_t  f1;
+   uint32_t  f2;
+   int64_t  f3;
+   const unsigned f4 : 9;
+};
+#pragma pack(pop)
