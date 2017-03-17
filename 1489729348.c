a4eb47c99bdf63cdd642a2e9c5d5679a81fbd980
saisi <saisi@users.noreply.github.com>
2017-03-17 05:42:29 +0000

    1489729348 alloxuric

---
+++ 1489729348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3945627230
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   volatile int64_t  f1;
+   const uint32_t  f2;
+   uint32_t  f3;
+   int32_t  f4;
+   uint64_t  f5;
+   uint8_t  f6;
