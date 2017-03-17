6a407dddd200c304a43ac4569dce907cbc65f70c
saisi <saisi@users.noreply.github.com>
2017-03-17 17:43:38 +0000

    1489772618 abdominovesical

---
+++ 1489772618.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3713529414
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x9AC78948L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 1L;
+static int32_t g_8[4] = {(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_36[7][1][4] = {{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}},{{(-1L),(-1L),(-1L),(-1L)}}};
+static int32_t *g_99 = &g_8[1];
+static int32_t **g_98 = &g_99;
+static int32_t g_102 = 0xCF37692AL;
+static uint64_t g_114 = 0x92FD4AE77DDBA47ALL;
