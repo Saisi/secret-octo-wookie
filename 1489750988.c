08b0235e88752b976dca1409d26c5d0aa9ab6250
saisi <saisi@users.noreply.github.com>
2017-03-17 11:43:08 +0000

    1489750988 amylophosphoric

---
+++ 1489750988.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4085102754
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   uint64_t  f1;
+   int64_t  f2;
+   signed f3 : 22;
+   const uint64_t  f4;
+   int16_t  f5;
+   volatile unsigned f6 : 24;
+   const volatile int64_t  f7;
+   int8_t  f8;
