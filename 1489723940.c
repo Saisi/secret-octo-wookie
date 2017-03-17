3088312ebb93ba5d8cc60221e9ae06ff64d148e4
saisi <saisi@users.noreply.github.com>
2017-03-17 04:12:20 +0000

    1489723940 alps

---
+++ 1489723940.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2643333318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 17;
+   const signed f1 : 2;
+   volatile signed f2 : 28;
+   volatile unsigned f3 : 24;
+   signed f4 : 7;
+   const volatile unsigned f5 : 2;
+   volatile signed f6 : 24;
+   unsigned f7 : 13;
+   signed f8 : 29;
