f29db7cbfb25fd96e99ea9ddb224d056b80c0aef
saisi <saisi@users.noreply.github.com>
2017-03-17 08:12:43 +0000

    1489738362 autointoxication

---
+++ 1489738362.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3917786717
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 13;
+   volatile int64_t  f1;
+   signed f2 : 16;
+   signed f3 : 20;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
