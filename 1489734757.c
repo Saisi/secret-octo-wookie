62f4c3a169933ce69eda6fb034588d5b63075133
saisi <saisi@users.noreply.github.com>
2017-03-17 07:12:38 +0000

    1489734757 aphanozygous

---
+++ 1489734757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      300455910
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 1;
+   const unsigned f1 : 18;
+   uint8_t  f2;
+   unsigned f3 : 20;
+};
+#pragma pack(pop)
+
