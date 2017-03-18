42b494e7355ae98bc7e60883285d9a5c10a1af14
saisi <saisi@users.noreply.github.com>
2017-03-18 10:45:30 +0000

    1489833930 Aesir

---
+++ 1489833930.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1562490818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int32_t  f1;
+};
+
+struct S1 {
+   signed f0 : 6;
+   signed f1 : 7;
+   unsigned f2 : 2;
+   unsigned f3 : 27;
