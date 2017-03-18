4a895ecd2af6ab4f9f7dfe51435461559424613a
saisi <saisi@users.noreply.github.com>
2017-03-18 04:44:54 +0000

    1489812293 alectoromachy

---
+++ 1489812293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3168320142
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   volatile unsigned f1 : 22;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile int32_t  f0;
+   int64_t  f1;
