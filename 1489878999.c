0bfdbfccf2bc260c574313610632fe6ec326e76b
saisi <saisi@users.noreply.github.com>
2017-03-18 23:16:40 +0000

    1489878999 appendicious

---
+++ 1489878999.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3766505070
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 5;
+   unsigned f1 : 6;
+   const unsigned f2 : 16;
+   int16_t  f3;
+   const volatile int32_t  f4;
+   signed f5 : 21;
+   const unsigned f6 : 2;
+   volatile unsigned f7 : 19;
+};
