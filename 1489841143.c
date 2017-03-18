ca11b6889c46277d325866239bd3576905a9c9aa
saisi <saisi@users.noreply.github.com>
2017-03-18 12:45:43 +0000

    1489841143 amylene

---
+++ 1489841143.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3646636045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   const int16_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_10 = 0x0D29L;
+static volatile union U0 g_26 = {9UL};/* VOLATILE GLOBAL g_26 */
+static uint64_t g_39 = 18446744073709551614UL;
