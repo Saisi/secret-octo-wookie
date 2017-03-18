23a82d1a24fc758a80d709bb0e8bd74a3b029e06
saisi <saisi@users.noreply.github.com>
2017-03-18 20:16:24 +0000

    1489868183 antisplitting

---
+++ 1489868183.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3583800038
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x897A4A12L;
+static int32_t *g_2 = &g_3;
+static int32_t ** volatile g_4 = (void*)0;/* VOLATILE GLOBAL g_4 */
+static int32_t ** volatile g_5 = &g_2;/* VOLATILE GLOBAL g_5 */
+static uint16_t g_49[7][1] = {{1UL},{0x6232L},{1UL},{0x6232L},{1UL},{0x6232L},{1UL}};
+static uint16_t *g_48[8][1] = {{(void*)0},{(void*)0},{&g_49[6][0]},{(void*)0},{(void*)0},{&g_49[6][0]},{(void*)0},{(void*)0}};
+static int64_t g_92 = (-10L);
+static int32_t g_96 = 0x65AAE70CL;
+static uint32_t g_107 = 0xFACFA70DL;
