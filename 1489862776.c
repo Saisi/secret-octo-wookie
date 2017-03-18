d3f93392244344b250bccdfbdab63c9fea1ac7d6
saisi <saisi@users.noreply.github.com>
2017-03-18 18:46:16 +0000

    1489862776 Ameslan

---
+++ 1489862776.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1584689084
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+   signed f1 : 17;
+   int32_t  f2;
+   signed f3 : 27;
+   volatile signed f4 : 9;
+   unsigned f5 : 14;
+   volatile signed f6 : 2;
+   volatile signed f7 : 31;
+   signed f8 : 1;
