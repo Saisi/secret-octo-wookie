bb82c464562a533e4de6e2a17ae05c195cb97ad9
saisi <saisi@users.noreply.github.com>
2017-03-18 16:46:05 +0000

    1489855565 abstrusity

---
+++ 1489855565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      824419139
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[5][3] = {{4L,(-7L),(-7L)},{0x4A61BF07L,(-7L),0L},{0x68593F9BL,4L,0xEB449523L},{0x4A61BF07L,0x4A61BF07L,0xEB449523L},{4L,0x68593F9BL,0L}};
+static int32_t g_4 = 0x71CB27F0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
