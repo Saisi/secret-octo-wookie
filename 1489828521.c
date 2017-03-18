da4865f544ac3d19ff6dab9ccf3a54df9fb6dad6
saisi <saisi@users.noreply.github.com>
2017-03-18 09:15:22 +0000

    1489828521 awaken

---
+++ 1489828521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      807945033
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 3;
+   signed f1 : 14;
+   unsigned f2 : 25;
+   volatile signed : 0;
+   signed f3 : 17;
+   signed f4 : 6;
+   const unsigned f5 : 13;
