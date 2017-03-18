11565b63a3759ab44c8294d4895c4c76a7ee1631
saisi <saisi@users.noreply.github.com>
2017-03-18 13:45:47 +0000

    1489844747 Angles

---
+++ 1489844747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      655200437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_7 = 0x3FA3A92BL;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
