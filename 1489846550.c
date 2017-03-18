050084a96c786417b9e2b94ad92a4b42fd19df39
saisi <saisi@users.noreply.github.com>
2017-03-18 14:15:50 +0000

    1489846550 Barbabas

---
+++ 1489846550.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      920207922
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const int8_t * f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_6 = 0x0272A72759951EA6LL;
+static int32_t g_8[6] = {2L,2L,2L,2L,2L,2L};
+static int32_t *g_7 = &g_8[4];
