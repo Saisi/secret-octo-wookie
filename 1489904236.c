707e19ca973be560116926e154aa708e93c45a3d
saisi <saisi@users.noreply.github.com>
2017-03-19 06:17:18 +0000

    1489904236 anodes

---
+++ 1489904236.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      594920487
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int16_t  f0;
+   unsigned f1 : 23;
+   unsigned f2 : 17;
+   const volatile signed f3 : 17;
+   volatile unsigned f4 : 20;
+   volatile unsigned f5 : 18;
+   volatile signed f6 : 13;
