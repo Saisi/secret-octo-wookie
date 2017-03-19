fe9b4676966d79e5f81dd1ee32700ef364bf571c
saisi <saisi@users.noreply.github.com>
2017-03-19 04:17:06 +0000

    1489897025 baconer

---
+++ 1489897025.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3099919279
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 22;
+   volatile int32_t  f1;
+   uint32_t  f2;
+   volatile int16_t  f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_14 = 4294967295UL;
