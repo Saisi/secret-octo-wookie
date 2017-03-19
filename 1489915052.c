fb3eb39d15401b5b5698ea41cf9fb0ba64d69a0c
saisi <saisi@users.noreply.github.com>
2017-03-19 09:17:32 +0000

    1489915052 acromial

---
+++ 1489915052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3807239600
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 6;
+   unsigned f1 : 3;
+   const volatile signed f2 : 25;
+   volatile unsigned f3 : 1;
+   signed f4 : 12;
+   volatile signed f5 : 19;
+   volatile unsigned f6 : 5;
