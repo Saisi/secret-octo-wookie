4de2360f3e05af77120af39507250f408e174539
saisi <saisi@users.noreply.github.com>
2017-03-19 04:47:09 +0000

    1489898828 ATI

---
+++ 1489898828.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3261471449
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   const volatile unsigned f1 : 22;
+};
+
+struct S1 {
+   const uint16_t  f0;
+   volatile uint32_t  f1;
+   uint8_t  f2;
+   uint8_t  f3;
