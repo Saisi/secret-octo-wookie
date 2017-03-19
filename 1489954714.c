ce35e979f533a49ea9607e378b775fbcab28dd89
saisi <saisi@users.noreply.github.com>
2017-03-19 20:18:35 +0000

    1489954714 antiferromagnetic

---
+++ 1489954714.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3617449725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 29;
+   uint16_t  f1;
+};
+
+union U1 {
+   int16_t  f0;
+   volatile uint32_t  f1;
+};
+
