460149c643c2fdb07b3f6c38412050fcbd59a227
saisi <saisi@users.noreply.github.com>
2017-03-19 10:17:38 +0000

    1489918657 Argiopoidea

---
+++ 1489918657.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      955733637
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   const int16_t  f1;
+   volatile int32_t  f2;
+   int32_t  f3;
+   volatile uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x96CA2CB5L;
