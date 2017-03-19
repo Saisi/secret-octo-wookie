6ca5d95f3b565a0218152616de8575e5af507cce
saisi <saisi@users.noreply.github.com>
2017-03-19 15:48:10 +0000

    1489938489 awink

---
+++ 1489938489.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2156413365
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 25;
+   signed f1 : 12;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_19 = (-8L);
