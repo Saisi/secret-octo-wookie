cba5fe1196709fac0cfbda3eaadaf84e1fee3061
saisi <saisi@users.noreply.github.com>
2017-03-19 00:16:45 +0000

    1489882604 agraffes

---
+++ 1489882604.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1462015063
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * const  f0;
+   int32_t  f1;
+};
+
+union U1 {
+   int16_t  f0;
+   unsigned f1 : 5;
+   int8_t  f2;
+   signed f3 : 8;
