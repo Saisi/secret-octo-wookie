018e8cd7125158bc7f957fa9c31907f5bf15550c
saisi <saisi@users.noreply.github.com>
2017-03-19 15:18:07 +0000

    1489936687 anablepses

---
+++ 1489936687.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1486103882
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   int32_t  f1;
+   const int32_t  f2;
+   const int16_t  f3;
+   int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_7[10] = {0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL,0xCEL};
