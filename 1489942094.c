a43608feb4b9f81f876271fbbf6cd045b22d70b4
saisi <saisi@users.noreply.github.com>
2017-03-19 16:48:15 +0000

    1489942094 adumbrates

---
+++ 1489942094.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3192419651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 9UL;
+static uint64_t g_19 = 18446744073709551608UL;
+static int32_t g_25 = 9L;
+static uint32_t g_54 = 2UL;
+static uint32_t *g_53 = &g_54;
+static int32_t g_57[9] = {0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L,0x656D4C01L};
+static uint16_t g_67 = 65535UL;
+static uint16_t g_119 = 65530UL;
+static int32_t g_129 = 0x8FEB948CL;
