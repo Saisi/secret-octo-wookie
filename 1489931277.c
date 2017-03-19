788a5b2ed4b0a4914b91b13c68804c7c5f5aa726
saisi <saisi@users.noreply.github.com>
2017-03-19 13:47:58 +0000

    1489931277 backslapper

---
+++ 1489931277.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2188208164
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 9;
+   signed f1 : 30;
+   signed f2 : 2;
+   unsigned f3 : 15;
+   unsigned f4 : 13;
+};
+
+struct S1 {
+   volatile int64_t  f0;
