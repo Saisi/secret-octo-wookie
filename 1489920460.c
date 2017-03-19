e040220e13d46018bf9ee78d1702778a5fa2452c
saisi <saisi@users.noreply.github.com>
2017-03-19 10:47:41 +0000

    1489920460 absurdism

---
+++ 1489920460.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3614722072
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 26;
+   volatile unsigned f1 : 7;
+   volatile unsigned f2 : 13;
+};
+#pragma pack(pop)
+
+struct S1 {
