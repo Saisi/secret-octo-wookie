b592f3392480c8635e4e25eca92a363e438c4ed9
saisi <saisi@users.noreply.github.com>
2017-03-19 12:17:48 +0000

    1489925868 auramine

---
+++ 1489925868.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3959185413
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   signed f1 : 20;
+   uint16_t  f2;
+   const uint8_t  f3;
+   uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2][10] = {{0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL},{0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL,0xB96CA2CBL}};
