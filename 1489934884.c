c0b71127878dbdb6204857563c08176de76466ec
saisi <saisi@users.noreply.github.com>
2017-03-19 14:48:05 +0000

    1489934884 autocombustion

---
+++ 1489934884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4096927331
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5[9][10][2] = {{{(-1L),(-1L)},{0x51814487L,0x51814487L},{(-1L),(-1L)},{(-4L),(-1L)},{(-1L),0x51814487L},{0x51814487L,(-1L)},{(-1L),(-4L)},{(-1L),(-1L)},{0x51814487L,0x51814487L},{(-1L),(-1L)}},{{(-4L),(-1L)},{(-1L),0x51814487L},{0x51814487L,(-1L)},{(-1L),(-4L)},{(-1L),(-1L)},{0x51814487L,0x51814487L},{(-1L),(-1L)},{(-4L),(-1L)},{(-1L),0x51814487L},{0x51814487L,(-1L)}},{{(-1L),(-4L)},{(-1L),(-1L)},{0x51814487L,0x51814487L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L}},{{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L}},{{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)}},{{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)}},{{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL}},{{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)}},{{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)},{(-1L),0xA22F0F5BL},{(-1L),(-4L)},{0x23024A13L,0x23024A13L},{(-4L),(-1L)},{0xA22F0F5BL,(-1L)},{(-4L),0x23024A13L},{0x23024A13L,(-4L)}}};
+static int32_t g_6 = 1L;
+static volatile int32_t g_7[3][4] = {{1L,1L,1L,1L},{1L,1L,1L,1L},{1L,1L,1L,1L}};
+static volatile int32_t g_8 = 1L;/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9 = 0x7DBC8B69L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x9278A491L;
+static int32_t g_15[2] = {0x29E5E26CL,0x29E5E26CL};
