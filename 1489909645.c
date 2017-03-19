a1ea38f51910403fafaf8dde2be4a89d965a748e
saisi <saisi@users.noreply.github.com>
2017-03-19 07:47:25 +0000

    1489909645 arthral

---
+++ 1489909645.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1033752221
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 13;
+   unsigned f1 : 16;
+   volatile unsigned f2 : 19;
+   unsigned f3 : 18;
+   volatile unsigned f4 : 27;
+   const unsigned f5 : 8;
+   signed f6 : 7;
