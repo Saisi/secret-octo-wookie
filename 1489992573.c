49f5500a487a6b201213526c1fd1913acdd30dc6
saisi <saisi@users.noreply.github.com>
2017-03-20 06:49:33 +0000

    1489992573 athlete

---
+++ 1489992573.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3946310920
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile unsigned f1 : 31;
+   const signed f2 : 5;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-2L);
+static uint16_t g_32[1][2] = {{0x1457L,0x1457L}};
