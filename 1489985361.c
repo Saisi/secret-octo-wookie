7d7b572695e08256552d97d61b4237ea5bbfddc7
saisi <saisi@users.noreply.github.com>
2017-03-20 04:49:22 +0000

    1489985361 Alcoran

---
+++ 1489985361.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      572089666
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 15;
+   signed f1 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0x58FB770AL;
+static int32_t g_9 = 0xA9F8D19CL;
+static uint32_t g_61 = 1UL;
+static const uint32_t g_78 = 0xFAADAD2DL;
