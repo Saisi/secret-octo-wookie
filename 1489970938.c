3203febe1a0e0d6f74d225cefe088505e8f3969a
saisi <saisi@users.noreply.github.com>
2017-03-20 00:48:59 +0000

    1489970938 abduced

---
+++ 1489970938.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3717041085
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_2[4] = {0xC7L,0xC7L,0xC7L,0xC7L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
