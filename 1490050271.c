5af2b017d15ad8a3e92c54a8918921e7557fee19
saisi <saisi@users.noreply.github.com>
2017-03-20 22:51:11 +0000

    1490050271 anglian

---
+++ 1490050271.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1067026668
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[9][7][2] = {{{0L,0L},{0xCF7921E1L,0x38DD44ADL},{0L,0xEC752F67L},{0x8BD85027L,0x07B4BC66L},{0xB0D193E0L,0x6F9B6917L},{0x6F9B6917L,(-1L)},{0x41FD9D08L,(-4L)}},{{0x9D9E177CL,0L},{(-1L),0xA38D0672L},{0x07B4BC66L,7L},{(-1L),(-1L)},{0xA38D0672L,(-1L)},{(-1L),7L},{0x07B4BC66L,0xA38D0672L}},{{(-1L),0L},{0x9D9E177CL,(-4L)},{0x41FD9D08L,(-1L)},{0x6F9B6917L,0x6F9B6917L},{0xB0D193E0L,0x07B4BC66L},{0x8BD85027L,0xEC752F67L},{0L,0x38DD44ADL}},{{0xCF7921E1L,0L},{0L,(-6L)},{0xB0D193E0L,(-1L)},{0L,0L},{(-1L),(-1L)},{0x38DD44ADL,(-1L)},{0xA38D0672L,(-8L)}},{{(-8L),8L},{(-7L),(-6L)},{0x6F9B6917L,0xB0D193E0L},{0L,0xEC752F67L},{(-1L),0x9D9E177CL},{8L,(-4L)},{0xEC752F67L,(-4L)}},{{8L,0x9D9E177CL},{(-1L),0xEC752F67L},{0L,0xB0D193E0L},{0x6F9B6917L,(-6L)},{(-7L),8L},{(-8L),(-8L)},{0xA38D0672L,(-1L)}},{{0x38DD44ADL,(-1L)},{(-1L),0L},{0L,(-1L)},{0xB0D193E0L,0xCF7921E1L},{0xB0D193E0L,(-1L)},{0L,0L},{(-1L),(-1L)}},{{0x38DD44ADL,(-1L)},{0xA38D0672L,(-8L)},{(-8L),8L},{(-7L),(-6L)},{0x6F9B6917L,0xB0D193E0L},{0L,0xEC752F67L},{(-1L),0x9D9E177CL}},{{8L,(-4L)},{0xEC752F67L,(-4L)},{8L,0x9D9E177CL},{(-1L),0xEC752F67L},{0L,0xB0D193E0L},{0x6F9B6917L,(-6L)},{(-7L),8L}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
