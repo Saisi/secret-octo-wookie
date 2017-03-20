93032c8abb101e6708e29c82fea625ae902c9364
saisi <saisi@users.noreply.github.com>
2017-03-20 20:20:56 +0000

    1490041256 adventively

---
+++ 1490041256.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3453128479
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x44CACFF8L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t * volatile g_3[7] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static volatile int32_t * volatile g_4 = (void*)0;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t * volatile g_5[9][7][3] = {{{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,(void*)0},{&g_2,&g_2,&g_2}},{{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,(void*)0},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{(void*)0,&g_2,&g_2}},{{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,(void*)0,&g_2},{&g_2,(void*)0,&g_2},{(void*)0,&g_2,(void*)0},{&g_2,&g_2,(void*)0},{&g_2,&g_2,&g_2}},{{&g_2,&g_2,&g_2},{(void*)0,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,(void*)0,&g_2},{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_2}},{{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{(void*)0,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_2,(void*)0,&g_2},{&g_2,&g_2,(void*)0}},{{&g_2,&g_2,(void*)0},{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{(void*)0,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,(void*)0}},{{&g_2,(void*)0,&g_2},{&g_2,&g_2,(void*)0},{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,(void*)0,&g_2},{&g_2,(void*)0,&g_2}},{{(void*)0,&g_2,(void*)0},{&g_2,&g_2,(void*)0},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{(void*)0,(void*)0,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2}},{{&g_2,(void*)0,&g_2},{(void*)0,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2},{(void*)0,(void*)0,&g_2}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
