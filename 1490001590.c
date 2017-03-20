6a816d59c633b0c844b68f2f33c08f93431c34c6
saisi <saisi@users.noreply.github.com>
2017-03-20 09:19:51 +0000

    1490001590 Amblypoda

---
+++ 1490001590.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3812541152
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = (-1L);
+static int32_t *g_4 = &g_5;
+static int32_t g_69 = 0x58A4122EL;
+static uint16_t g_71 = 0xD7D2L;
+static volatile uint8_t g_74 = 0xCEL;/* VOLATILE GLOBAL g_74 */
+static uint8_t g_93 = 0xF0L;
+static int8_t g_98 = (-1L);
+static int32_t g_100[5] = {0x348FB8AAL,0x348FB8AAL,0x348FB8AAL,0x348FB8AAL,0x348FB8AAL};
+static uint32_t g_139 = 18446744073709551609UL;
