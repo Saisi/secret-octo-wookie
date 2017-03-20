545796be22b849c342ba0d77ea0de258ff0202f8
saisi <saisi@users.noreply.github.com>
2017-03-20 00:18:56 +0000

    1489969136 abjudicator

---
+++ 1489969136.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3011279453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 13;
+   signed f1 : 16;
+   signed f2 : 19;
+   signed f3 : 18;
+};
+
+union U1 {
+   volatile signed f0 : 13;
+   int64_t  f1;
