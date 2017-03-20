9befff656dce61cf0e850fe28cbe6f085b6b5793
saisi <saisi@users.noreply.github.com>
2017-03-20 22:21:09 +0000

    1490048469 auriculars

---
+++ 1490048469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      858743771
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6][9] = {{0x5E4B7834L,0x6D6092F6L,0x5E4B7834L,1L,0x6D6092F6L,0x5DEC4F3FL,0x5DEC4F3FL,0x6D6092F6L,1L},{0L,1L,0L,5L,1L,5L,5L,1L,5L},{0x5E4B7834L,0x6D6092F6L,0x5E4B7834L,1L,0x6D6092F6L,0x5DEC4F3FL,0x5DEC4F3FL,0x6D6092F6L,1L},{0L,1L,0L,5L,1L,5L,5L,1L,5L},{0x5E4B7834L,0x6D6092F6L,0x5E4B7834L,1L,0x6D6092F6L,0x5DEC4F3FL,0x5DEC4F3FL,0x6D6092F6L,1L},{0L,5L,0xBD035C57L,(-4L),5L,0x51D52AA0L,0x51D52AA0L,5L,(-4L)}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
