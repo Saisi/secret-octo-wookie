ae292cde859f215840186a423ba54e631d7f3a95
saisi <saisi@users.noreply.github.com>
2017-03-20 16:20:33 +0000

    1490026833 amidofluoride

---
+++ 1490026833.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      776313842
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static int16_t g_40 = 3L;
+static int32_t g_76 = 0xE68AC39FL;
+static int32_t * const  volatile g_75[4][1] = {{&g_76},{&g_76},{&g_76},{&g_76}};
+static uint16_t g_81 = 0x6ABBL;
+static int32_t g_95[1][4][8] = {{{5L,0L,5L,0x58852677L,(-4L),0xF3D9679FL,0L,(-9L)},{0xBA83E1A6L,(-4L),0L,0xF3D9679FL,0xF3D9679FL,0L,(-4L),0xBA83E1A6L},{0xBA83E1A6L,0x58852677L,(-3L),5L,(-4L),0L,0L,0L},{5L,0L,(-9L),0L,5L,0L,0L,(-4L)}}};
+static uint8_t g_127 = 1UL;
+static uint32_t g_154 = 0xE4F1F3A6L;
+static uint8_t g_183[3] = {0xE3L,0xE3L,0xE3L};
