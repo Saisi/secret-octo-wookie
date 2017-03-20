d4b0d7ef8f635838d0fb5cb60fec087bee36ad45
saisi <saisi@users.noreply.github.com>
2017-03-20 10:19:56 +0000

    1490005196 animacule

---
+++ 1490005196.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2047117902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 21;
+   int64_t  f1;
+   unsigned f2 : 30;
+   signed f3 : 8;
+   uint32_t  f4;
+   const signed f5 : 14;
+   uint16_t  f6;
+   signed f7 : 11;
+};
