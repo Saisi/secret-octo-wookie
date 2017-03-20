1f8b68ac7dc343983302bcbc1510f0af20ca8af7
saisi <saisi@users.noreply.github.com>
2017-03-20 09:49:53 +0000

    1490003393 antinucleon

---
+++ 1490003393.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      256966259
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x97632667L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x6BACD795L;
+static int32_t *g_10[6] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static union U0 g_24 = {0x3FL};
+static int32_t g_30 = 0xC795C380L;
