eef5b34c956b47c8bb5e8480a952877725dc4fcc
saisi <saisi@users.noreply.github.com>
2017-03-20 03:19:14 +0000

    1489979953 Augustales

---
+++ 1489979953.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      517498934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 9;
+   unsigned f1 : 26;
+   const unsigned f2 : 20;
+   volatile signed f3 : 16;
+   volatile unsigned f4 : 31;
+   volatile unsigned f5 : 26;
+};
+
+#pragma pack(push)
