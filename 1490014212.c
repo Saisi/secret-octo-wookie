3a5cc587671101bb8f69a068be3429c99ffbd0aa
saisi <saisi@users.noreply.github.com>
2017-03-20 12:50:13 +0000

    1490014212 ataxaphasia

---
+++ 1490014212.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3030888518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 17;
+   const uint32_t  f1;
+   signed f2 : 10;
+   int32_t  f3;
+   volatile signed f4 : 24;
+   volatile unsigned f5 : 26;
+};
