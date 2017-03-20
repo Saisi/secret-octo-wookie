ab7f013e0699639b610948ec7f94efd5b05df6bc
saisi <saisi@users.noreply.github.com>
2017-03-20 08:49:48 +0000

    1489999788 anthropology

---
+++ 1489999788.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3166900651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_7 = 0x260417BEL;
+static uint16_t g_37 = 0x764CL;
+static uint8_t g_70 = 0xBEL;
+static volatile int32_t g_73 = 6L;/* VOLATILE GLOBAL g_73 */
+static volatile int32_t *g_72 = &g_73;
+static int32_t g_90 = 0L;
+static int32_t *g_97 = &g_90;
+static int32_t **g_96 = &g_97;
+static int32_t ***g_106 = &g_96;
