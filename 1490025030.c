d614910e457b28f246b43f4ae5ba0f17f3bba340
saisi <saisi@users.noreply.github.com>
2017-03-20 15:50:31 +0000

    1490025030 antimonide

---
+++ 1490025030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3019955606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   signed f1 : 10;
+   volatile unsigned f2 : 4;
+   signed : 0;
+   volatile int32_t  f3;
+   signed f4 : 9;
+   signed f5 : 6;
