470a7e44ad4fd38651e0d29a0f7542011f9165fd
saisi <saisi@users.noreply.github.com>
2017-03-20 05:49:28 +0000

    1489988967 amberfishes

---
+++ 1489988967.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3436953087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   unsigned f1 : 29;
+   unsigned f2 : 12;
+   signed f3 : 7;
+   const signed f4 : 28;
+   const int32_t  f5;
+   volatile unsigned f6 : 16;
+};
+
