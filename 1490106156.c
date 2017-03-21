0656fda75cc08e6a4b38cb0e6bd6ac79e75978b6
saisi <saisi@users.noreply.github.com>
2017-03-21 14:22:37 +0000

    1490106156 arteriorrhexis

---
+++ 1490106156.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1787209319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[2] = {0x13B91020L,0x13B91020L};
+static volatile int32_t g_5 = 0L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0xE8384627L;
+static int32_t g_10 = 7L;
+static int32_t g_64 = 0L;
+static int32_t *g_63[8] = {&g_64,&g_64,&g_64,&g_64,&g_64,&g_64,&g_64,&g_64};
+static int32_t *g_67 = &g_64;
+static int64_t g_81 = 0L;
+static int8_t g_106 = 0x27L;
