dd28dd188f66e21f8b1b167f60634fd507892ef9
saisi <saisi@users.noreply.github.com>
2017-03-21 01:21:25 +0000

    1490059284 autostoper

---
+++ 1490059284.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3539213917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint32_t  f1;
+   const uint32_t  f2;
+   uint64_t  f3;
+};
+
+struct S1 {
+   struct S0  f0;
+   volatile int32_t  f1;
