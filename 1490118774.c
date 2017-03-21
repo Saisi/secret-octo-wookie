4d1d5738148369474c2626a68c024fd8cf09fb6e
saisi <saisi@users.noreply.github.com>
2017-03-21 17:52:55 +0000

    1490118774 arthropathology

---
+++ 1490118774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      828882604
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+};
+
+struct S1 {
+   const volatile int16_t  f0;
+   const volatile uint16_t  f1;
+   uint16_t  f2;
+   struct S0  f3;
+   uint16_t  f4;
