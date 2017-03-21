4f192e2246f16e46393402cad83a2660b045f9cc
saisi <saisi@users.noreply.github.com>
2017-03-21 04:21:42 +0000

    1490070102 antinome

---
+++ 1490070102.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2433562045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   volatile int64_t  f1;
+   uint8_t  f2;
+   uint16_t  f3;
+};
+
+union U1 {
+   uint8_t  f0;
+   uint32_t  f1;
