c0e0689a70f00aecba63adcfb108b6720e4b7db4
saisi <saisi@users.noreply.github.com>
2017-03-21 19:53:07 +0000

    1490125986 ackmen

---
+++ 1490125986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2591611278
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 21;
+   signed f1 : 22;
+   const unsigned : 0;
+   unsigned f2 : 12;
+   signed f3 : 11;
+   unsigned f4 : 22;
+   volatile unsigned f5 : 28;
+   volatile unsigned f6 : 1;
+};
