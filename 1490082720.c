65dec8b337e2f03960c312480ed91eb78b8b8909
saisi <saisi@users.noreply.github.com>
2017-03-21 07:52:00 +0000

    1490082720 archdukes

---
+++ 1490082720.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2216997684
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_4 = 0UL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_6 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
