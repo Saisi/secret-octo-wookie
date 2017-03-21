1c9aeb6330621dccf71ceefc19132ac92eda8560
saisi <saisi@users.noreply.github.com>
2017-03-21 10:22:15 +0000

    1490091734 accouple

---
+++ 1490091734.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2866641600
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+   unsigned f1 : 11;
+   unsigned f2 : 1;
+};
+
+struct S1 {
+   uint64_t  f0;
+   unsigned f1 : 30;
+   const volatile int8_t  f2;
