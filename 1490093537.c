d75b715adb9c6e7583f27103758f35e332de35b4
saisi <saisi@users.noreply.github.com>
2017-03-21 10:52:18 +0000

    1490093537 alcoate

---
+++ 1490093537.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4078758616
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-3L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xE5596960L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0xA7C20398L;
+static volatile int32_t g_9 = 3L;/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10 = 0L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t g_11[9] = {0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L,0xB95AE649L};
+static int32_t g_12 = 0xE3FF14EBL;
+static int32_t g_16 = 0L;
