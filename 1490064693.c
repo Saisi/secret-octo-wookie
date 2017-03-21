7fba10952234af376b53dcc5f418aca040d000f0
saisi <saisi@users.noreply.github.com>
2017-03-21 02:51:33 +0000

    1490064693 Azophi

---
+++ 1490064693.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4251039507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x44696A48L;
+static uint32_t g_10 = 4294967295UL;
+static uint32_t g_24 = 6UL;
+static int32_t g_47[8] = {1L,1L,1L,1L,1L,1L,1L,1L};
+static uint64_t g_60 = 0xCB11B1996FCD4D33LL;
+static int16_t g_75 = 6L;
+static int32_t g_80 = 0x96D9C50EL;
+static int32_t * volatile g_79 = &g_80;/* VOLATILE GLOBAL g_79 */
+static uint32_t g_81[1] = {0x06156DBBL};
