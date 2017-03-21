f37d771ab00e59e9750b3dbe6d55846392d32c2e
saisi <saisi@users.noreply.github.com>
2017-03-21 13:22:31 +0000

    1490102551 autotransplantation

---
+++ 1490102551.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3945176668
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_13 = 0x70L;
+static uint32_t g_14 = 0x978F3C34L;
+static uint32_t g_24[10] = {8UL,1UL,8UL,8UL,1UL,8UL,8UL,1UL,8UL,8UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+static uint16_t  func_6(int8_t  p_7, int32_t  p_8, uint64_t  p_9, int32_t  p_10, uint32_t  p_11);
+
