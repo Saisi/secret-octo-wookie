b66da5b1198aadac59d083427c72a33dab79d2ac
saisi <saisi@users.noreply.github.com>
2017-03-21 18:22:58 +0000

    1490120577 Arch

---
+++ 1490120577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3048503408
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   unsigned f1 : 19;
+   signed f2 : 27;
+   int64_t  f3;
+   volatile unsigned f4 : 21;
+   const unsigned f5 : 3;
+   unsigned f6 : 28;
