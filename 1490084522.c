5131a6295ba719743cc849064830dbd9de95c82b
saisi <saisi@users.noreply.github.com>
2017-03-21 08:22:03 +0000

    1490084522 backwashes

---
+++ 1490084522.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3338502496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   int16_t  f1;
+   volatile uint8_t  f2;
+   uint32_t  f3;
+   uint16_t  f4;
+   const int16_t  f5;
+   uint32_t  f6;
+};
+
