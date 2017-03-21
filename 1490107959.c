d60750a19092bc175ace5d9b69c9f50857aae1ce
saisi <saisi@users.noreply.github.com>
2017-03-21 14:52:40 +0000

    1490107959 Acorus

---
+++ 1490107959.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4267869024
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   unsigned f1 : 27;
+   signed f2 : 17;
+   unsigned f3 : 25;
+   unsigned f4 : 4;
+   signed f5 : 11;
+   unsigned f6 : 29;
+};
+
