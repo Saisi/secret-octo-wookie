2c2f24a91722cb420d3459b5274e94f575da8092
saisi <saisi@users.noreply.github.com>
2017-03-21 03:51:40 +0000

    1490068298 Adine

---
+++ 1490068298.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1948548028
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 12;
+   const signed f1 : 17;
+   signed f2 : 15;
+   signed f3 : 31;
+   const signed f4 : 18;
+};
+#pragma pack(pop)
