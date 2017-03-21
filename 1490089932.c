ccb0bf1901fcea95d9a03175356cbc0dc3a8d55d
saisi <saisi@users.noreply.github.com>
2017-03-21 09:52:12 +0000

    1490089932 airthed

---
+++ 1490089932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1780981655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 10;
+   volatile int32_t  f1;
+   volatile uint64_t  f2;
+};
+
+union U1 {
+   volatile int64_t  f0;
+};
+
