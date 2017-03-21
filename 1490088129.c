5948989f439568b3db81661d03f77a1cbbd6a09d
saisi <saisi@users.noreply.github.com>
2017-03-21 09:22:10 +0000

    1490088129 Afrikah

---
+++ 1490088129.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1514558998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const unsigned f1 : 22;
+   unsigned f2 : 18;
+   volatile signed f3 : 12;
+   const unsigned f4 : 1;
+   signed f5 : 9;
+};
+
+union U1 {
