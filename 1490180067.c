cb05dea663057e2a63db209b990494e3a0adeaec
saisi <saisi@users.noreply.github.com>
2017-03-22 10:54:28 +0000

    1490180067 Apatela

---
+++ 1490180067.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2327226755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-3L);
+static volatile int32_t g_5 = 0xCCCE1E2CL;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7[4][3][8] = {{{(-1L),0L,(-1L),0L,0L,(-10L),(-10L),0L},{0L,(-10L),(-10L),0L,0L,(-1L),0L,(-1L)},{0L,(-1L),5L,(-1L),0L,5L,1L,1L}},{{(-1L),(-1L),0L,0L,(-1L),(-1L),(-10L),(-1L)},{1L,(-10L),0L,1L,0L,(-10L),1L,(-1L)},{(-1L),0L,5L,1L,1L,5L,(-10L),0L}},{{0xF68593F9L,(-1L),5L,0x208373B2L,(-1L),0x208373B2L,5L,(-1L)},{0L,5L,0xF68593F9L,0L,0x208373B2L,0x208373B2L,0L,0xF68593F9L},{(-1L),(-1L),1L,(-10L),0L,1L,0L,(-10L)}},{{0xF68593F9L,(-10L),0xF68593F9L,0x208373B2L,(-10L),5L,5L,(-10L)},{(-10L),5L,5L,(-10L),0x208373B2L,0xF68593F9L,(-10L),0xF68593F9L},{(-10L),0L,1L,0L,(-10L),1L,(-1L),(-1L)}}};
+static int32_t g_8 = 3L;
+static int32_t g_29 = (-2L);
+static int32_t g_33 = (-4L);
+static int8_t g_38 = (-1L);
