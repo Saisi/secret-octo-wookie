973d84a125e868260f8cb50314286b7f3558b47d
saisi <saisi@users.noreply.github.com>
2017-03-22 18:55:12 +0000

    1490208912 arteriodialysis

---
+++ 1490208912.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3307451031
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 10;
+   const int32_t  f1;
+   volatile int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2[1][2] = {{65527UL,65527UL}};
+static int64_t g_7 = 0x9323D1BCA9D69236LL;
+static int32_t g_12[9] = {5L,5L,5L,5L,5L,5L,5L,5L,5L};
