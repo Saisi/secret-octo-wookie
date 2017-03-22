5df80ce2ca2ce051278545563792483e486105cf
saisi <saisi@users.noreply.github.com>
2017-03-22 06:54:02 +0000

    1490165642 Atbara

---
+++ 1490165642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3623194301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 29;
+   int32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_5 = {5028,0x4279C418L};
