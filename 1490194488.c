d56e5dac90a533dc6ab43659c33f44849d7789dd
saisi <saisi@users.noreply.github.com>
2017-03-22 14:54:48 +0000

    1490194488 angiokeratoma

---
+++ 1490194488.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2953459742
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8] = {0xD453606DL,0xD453606DL,0xD453606DL,0xD453606DL,0xD453606DL,0xD453606DL,0xD453606DL,0xD453606DL};
+static int32_t *g_32 = &g_3[7];
+static int32_t **g_31 = &g_32;
+static int8_t g_98 = 0x4EL;
+static int32_t g_100[6] = {0x35965D04L,0x35965D04L,0x35965D04L,0x35965D04L,0x35965D04L,0x35965D04L};
