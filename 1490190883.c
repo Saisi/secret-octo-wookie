afdd36611e08c720886c94a18ad41d5206dedaf2
saisi <saisi@users.noreply.github.com>
2017-03-22 13:54:43 +0000

    1490190883 Aldus

---
+++ 1490190883.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      387722493
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   unsigned : 0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x871D976FL;
+static volatile uint8_t g_10 = 1UL;/* VOLATILE GLOBAL g_10 */
+static int64_t g_25 = (-4L);
+static int32_t g_61 = 0xF0E26A25L;
