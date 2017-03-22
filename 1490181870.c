83131db4f320f2230678938c409dcf629c693dea
saisi <saisi@users.noreply.github.com>
2017-03-22 11:24:30 +0000

    1490181870 acidophil

---
+++ 1490181870.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3756271200
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   signed f1 : 27;
+   signed f2 : 17;
+   unsigned f3 : 25;
+   volatile signed f4 : 4;
+   unsigned f5 : 11;
+   unsigned f6 : 29;
