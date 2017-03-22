992836a0203d96240a5bcb9ccabd61ddc03f1b10
saisi <saisi@users.noreply.github.com>
2017-03-22 03:53:47 +0000

    1490154827 advection

---
+++ 1490154827.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1276713453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_5 = 0x9753L;/* VOLATILE GLOBAL g_5 */
+static volatile int8_t g_9[1][6] = {{0L,0L,0L,0L,0L,0L}};
+static int8_t g_10 = 1L;
+static volatile uint64_t g_11 = 18446744073709551615UL;/* VOLATILE GLOBAL g_11 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
