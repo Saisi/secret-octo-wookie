0021a767ecd78b5e72be380c739dc5a519c426cf
saisi <saisi@users.noreply.github.com>
2017-03-22 21:55:30 +0000

    1490219729 anthropophysiography

---
+++ 1490219729.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1778404347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 28;
+   unsigned f1 : 3;
+   volatile unsigned f2 : 26;
+   unsigned f3 : 29;
+   const unsigned f4 : 26;
+};
+
+union U1 {
+   const volatile int16_t  f0;
