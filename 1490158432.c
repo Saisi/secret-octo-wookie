5bae8836388acd9f899489a4fad3990a5069f1c1
saisi <saisi@users.noreply.github.com>
2017-03-22 04:53:52 +0000

    1490158432 afterwisdom

---
+++ 1490158432.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3441252542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_7 = (void*)0;
+static int32_t g_10 = 3L;
+static uint32_t g_13 = 0xFAF07207L;
+static int64_t g_47 = 0xD4FE93046AA1DD5CLL;
+static int32_t g_68[3] = {0xD0AE446FL,0xD0AE446FL,0xD0AE446FL};
+static int32_t * volatile g_69 = (void*)0;/* VOLATILE GLOBAL g_69 */
+static int32_t *g_72[3] = {&g_68[2],&g_68[2],&g_68[2]};
+static int32_t ** volatile g_71[5][6][3] = {{{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0}},{{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0}},{{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0}},{{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0}},{{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0},{&g_72[0],&g_72[0],&g_72[0]},{(void*)0,&g_72[1],(void*)0}}};
+static int32_t ** const  volatile g_73[8] = {&g_72[0],&g_72[0],&g_72[0],&g_72[0],&g_72[0],&g_72[0],&g_72[0],&g_72[0]};
