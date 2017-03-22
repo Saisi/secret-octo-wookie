c442b882fa83e026bcbfee780ecbf8214c226910
saisi <saisi@users.noreply.github.com>
2017-03-22 23:55:40 +0000

    1490226940 babylon

---
+++ 1490226940.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2071754892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x70C3099CL;
+static int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static volatile uint32_t g_5[6][5] = {{9UL,0xABFC3938L,0xABFC3938L,9UL,0xD811F07BL},{0xD44AD374L,0x7BEDEDB5L,1UL,1UL,0x7BEDEDB5L},{0xD811F07BL,0xABFC3938L,18446744073709551615UL,0xECAFE8B0L,0xECAFE8B0L},{0x213EC752L,18446744073709551615UL,0x213EC752L,1UL,0UL},{0xE0566A92L,9UL,0xECAFE8B0L,9UL,0xE0566A92L},{0x213EC752L,0xD44AD374L,18446744073709551615UL,0x7BEDEDB5L,18446744073709551615UL}};
