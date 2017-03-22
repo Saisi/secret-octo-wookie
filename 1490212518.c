72c30ed2f22b4f6d940d03fcf105af3b920023a2
saisi <saisi@users.noreply.github.com>
2017-03-22 19:55:19 +0000

    1490212518 aggrievedness

---
+++ 1490212518.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3327920755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 24;
+   const volatile unsigned f1 : 15;
+   const signed f2 : 22;
+   const unsigned f3 : 9;
+   signed f4 : 22;
+   unsigned f5 : 23;
+   const volatile unsigned f6 : 26;
