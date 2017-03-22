8409846cae901ab38b45e3cd2c3ce2e49bc564c1
saisi <saisi@users.noreply.github.com>
2017-03-22 12:24:36 +0000

    1490185475 ballonnes

---
+++ 1490185475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      637221961
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 3;
+   volatile signed f1 : 14;
+   unsigned f2 : 25;
+   signed : 0;
+   volatile unsigned f3 : 17;
+   signed f4 : 6;
+   volatile unsigned f5 : 13;
