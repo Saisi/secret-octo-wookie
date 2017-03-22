b3d08c18de06bb68265bbcf3f735eb1deb8c2f05
saisi <saisi@users.noreply.github.com>
2017-03-22 09:24:20 +0000

    1490174660 asphyxia

---
+++ 1490174660.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3432297582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 4L;
+static int64_t g_27 = (-10L);
+static int32_t g_29 = 6L;
+static uint32_t g_47 = 4294967294UL;
+static uint64_t g_51[5] = {0x5CCFDEF05DE082ADLL,0x5CCFDEF05DE082ADLL,0x5CCFDEF05DE082ADLL,0x5CCFDEF05DE082ADLL,0x5CCFDEF05DE082ADLL};
+static volatile uint64_t g_53 = 18446744073709551613UL;/* VOLATILE GLOBAL g_53 */
+static volatile uint64_t *g_52 = &g_53;
+static uint64_t g_60 = 0UL;
