d295484ef6e7ab60aefcf4177841aacca2dc58ba
saisi <saisi@users.noreply.github.com>
2017-03-22 10:24:25 +0000

    1490178265 antipedal

---
+++ 1490178265.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      813346240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0x47DC4B4EL;
+static volatile union U0 g_40 = {0x9040C3F3L};/* VOLATILE GLOBAL g_40 */
+static uint64_t g_73 = 18446744073709551615UL;
+static int8_t g_76[1] = {0x4CL};
