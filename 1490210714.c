a89c278239cf1290fc18946bdfaa8d83d725930b
saisi <saisi@users.noreply.github.com>
2017-03-22 19:25:16 +0000

    1490210714 acroneurosis

---
+++ 1490210714.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3811578805
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 14;
+   unsigned f2 : 12;
+   volatile unsigned : 0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
