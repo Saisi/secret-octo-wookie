b1c94d56d36cd567883f725053c519f8b864fa7c
saisi <saisi@users.noreply.github.com>
2017-03-22 08:54:17 +0000

    1490172856 arbitrager

---
+++ 1490172856.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      536116022
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 26;
+   volatile signed f2 : 20;
+   unsigned f3 : 16;
+   volatile signed f4 : 31;
+   signed f5 : 26;
+};
+
+#pragma pack(push)
