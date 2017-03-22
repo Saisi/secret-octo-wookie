0f220f60850f08ebf71d66e61db43f4de447c3f5
saisi <saisi@users.noreply.github.com>
2017-03-22 00:53:34 +0000

    1490144013 ankylosaur

---
+++ 1490144013.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2527578742
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint16_t  f1;
+   volatile uint64_t  f2;
+   const int64_t  f3;
+   uint32_t  f4;
+   const int16_t  f5;
+   volatile uint64_t  f6;
+   int16_t  f7;
+   volatile uint16_t  f8;
