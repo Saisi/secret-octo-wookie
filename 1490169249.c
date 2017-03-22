e8ba0c6f7b400064f91fc4a6aed507bb867d3438
saisi <saisi@users.noreply.github.com>
2017-03-22 07:54:12 +0000

    1490169249 Americawards

---
+++ 1490169249.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2237410655
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   const volatile signed f1 : 13;
+   volatile unsigned f2 : 28;
+};
+
+struct S1 {
+   volatile int64_t  f0;
+   volatile signed f1 : 14;
+   const int8_t  f2;
