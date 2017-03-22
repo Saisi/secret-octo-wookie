7db4de80e778242f7a7b0d31277748880d42cdc1
saisi <saisi@users.noreply.github.com>
2017-03-22 17:25:02 +0000

    1490203502 aniconism

---
+++ 1490203502.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3616589270
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 25;
+   volatile uint64_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 17;
+   signed : 0;
