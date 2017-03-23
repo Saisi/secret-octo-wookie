a98784835cae2140571276cf42885fc6cf81cbdb
saisi <saisi@users.noreply.github.com>
2017-03-23 19:27:30 +0000

    1490297250 antinormality

---
+++ 1490297250.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      992951939
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 10;
+   volatile unsigned f1 : 1;
+   unsigned f2 : 24;
+   unsigned f3 : 15;
+   signed f4 : 14;
+   unsigned f5 : 30;
+   signed f6 : 20;
