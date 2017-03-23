bedf6bcfabbaa522af074baa6667e2399befecb4
saisi <saisi@users.noreply.github.com>
2017-03-23 14:57:07 +0000

    1490281026 antiphysicalness

---
+++ 1490281026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2766823229
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 29;
+   volatile uint32_t  f1;
+   signed : 0;
+   unsigned f2 : 4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
