9c41d64be856127ad357e6521f4b58f454f0b420
saisi <saisi@users.noreply.github.com>
2017-03-23 23:27:51 +0000

    1490311670 acaleph

---
+++ 1490311670.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      647893046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[4] = {0xDD3F4672L,0xDD3F4672L,0xDD3F4672L,0xDD3F4672L};
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x449C9FB3L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x4800FA72L;
+static int32_t g_54 = (-1L);
+static int32_t *g_53 = &g_54;
+static int16_t g_64 = (-1L);
+static int16_t g_76 = 0x84C8L;
+static int32_t g_79 = 1L;
