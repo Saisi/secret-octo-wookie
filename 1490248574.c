5b8a582adb2cb34bcef4c6af9bec03d1f898c4c7
saisi <saisi@users.noreply.github.com>
2017-03-23 05:56:15 +0000

    1490248574 baramin

---
+++ 1490248574.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3313042303
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile int8_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0xD490C4D8L;
+static uint8_t g_24 = 0x50L;
+static uint32_t g_27 = 0x196E86FFL;
+static uint32_t g_43[4] = {0x5DFCC9F7L,0x5DFCC9F7L,0x5DFCC9F7L,0x5DFCC9F7L};
+static int32_t g_73 = 0L;
+static uint32_t g_74 = 18446744073709551615UL;
+static uint64_t g_78 = 1UL;
+static uint16_t g_90[3][10] = {{0x81E2L,65534UL,0x81E2L,0xFB84L,0UL,0x3999L,3UL,3UL,0x3999L,0UL},{0x2252L,0x1CEEL,0x1CEEL,0x2252L,0x81E2L,0x70E9L,0xFB84L,3UL,0xFB84L,0x70E9L},{0x3999L,0x1904L,0x81E2L,0x1904L,0x3999L,0xFB88L,0x1CEEL,0xFB84L,0xFB84L,0x1CEEL}};
