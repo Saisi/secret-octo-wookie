15bc6d88edeeed3e81aad5eb2c140ec745770f6c
saisi <saisi@users.noreply.github.com>
2017-03-23 12:56:54 +0000

    1490273814 Ahtena

---
+++ 1490273814.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2829777453
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   unsigned f1 : 24;
+   volatile signed f2 : 27;
+   volatile uint32_t  f3;
+   unsigned f4 : 19;
+   signed f5 : 21;
+   signed f6 : 4;
+   unsigned f7 : 14;
+};
