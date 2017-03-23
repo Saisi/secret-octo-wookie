390e3a6823c345314ee8aa8ccbfb3940c77ece5e
saisi <saisi@users.noreply.github.com>
2017-03-23 04:26:07 +0000

    1490243166 antistaphylococcic

---
+++ 1490243166.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4061670677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   volatile int32_t  f1;
+   int16_t  f2;
+   uint64_t  f3;
+   volatile unsigned f4 : 3;
+   signed f5 : 16;
+};
