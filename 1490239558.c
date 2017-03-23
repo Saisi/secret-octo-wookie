77e52b706b4967f49f76f30deb22d5a6b9f8d5b4
saisi <saisi@users.noreply.github.com>
2017-03-23 03:25:59 +0000

    1490239558 Artemisa

---
+++ 1490239558.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3575732288
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x3E90A624L;
+static int32_t *g_6 = &g_4;
+static int32_t ** const  volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
+static const int32_t *g_19[6][10] = {{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4},{&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4}};
+static const int32_t **g_18 = &g_19[0][2];
+static int64_t g_21 = 0x80A7C6DD3ED23C5ALL;
+static int32_t g_23 = 0L;
+static uint16_t g_79[2][2][10] = {{{0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL},{0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L}},{{0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL},{0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L,0UL,0UL,0x35D6L,0x35D6L}}};
+static int32_t *g_101 = (void*)0;
