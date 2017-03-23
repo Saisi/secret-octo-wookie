a9a4c02465a41ad3c1a71334d37977bdeb9294d8
saisi <saisi@users.noreply.github.com>
2017-03-23 02:25:54 +0000

    1490235953 arginases

---
+++ 1490235953.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      776864724
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 24;
+   volatile unsigned f1 : 13;
+   volatile unsigned f2 : 27;
+   const volatile signed f3 : 27;
+   signed f4 : 30;
+   unsigned f5 : 29;
+   volatile signed f6 : 7;
+};
+
