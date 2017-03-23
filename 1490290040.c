deec851ab2c9217b459e0ee3ecd35b1561dffaee
saisi <saisi@users.noreply.github.com>
2017-03-23 17:27:20 +0000

    1490290040 antipharmic

---
+++ 1490290040.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1323376163
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   volatile uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 0x20FE6E3245F1DBECLL;/* VOLATILE GLOBAL g_2 */
+static uint64_t g_16 = 0xB2C57E71F09A2B44LL;
+static const uint8_t g_25 = 0UL;
+static const uint8_t *g_24 = &g_25;
