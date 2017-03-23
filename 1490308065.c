7743630afcf77ca2266fe01370c63a021f443bda
saisi <saisi@users.noreply.github.com>
2017-03-23 22:27:45 +0000

    1490308065 agitationist

---
+++ 1490308065.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2978692783
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 22;
+   volatile unsigned f1 : 21;
+   const signed f2 : 4;
+   volatile signed f3 : 31;
+   volatile signed f4 : 20;
+};
+
+struct S1 {
+   volatile unsigned f0 : 1;
