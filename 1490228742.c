5e41aa410211520472c71cfa7fb58708d623ff16
saisi <saisi@users.noreply.github.com>
2017-03-23 00:25:42 +0000

    1490228742 amnesias

---
+++ 1490228742.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2850351688
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   signed f1 : 31;
+   signed f2 : 23;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 1;
