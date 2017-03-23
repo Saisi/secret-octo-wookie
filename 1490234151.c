a97b5de395caa687ecac831e0d4832dffdbaf647
saisi <saisi@users.noreply.github.com>
2017-03-23 01:55:51 +0000

    1490234151 adenization

---
+++ 1490234151.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      185429600
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   signed f1 : 27;
+   signed f2 : 17;
+   signed f3 : 25;
+   unsigned f4 : 4;
+   signed f5 : 11;
+   unsigned f6 : 29;
