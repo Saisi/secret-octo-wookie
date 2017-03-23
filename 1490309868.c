718555d1fc331a0cfd5f59deedf318166fa4db6e
saisi <saisi@users.noreply.github.com>
2017-03-23 22:57:48 +0000

    1490309868 allision

---
+++ 1490309868.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      400528570
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U4 {
+   int64_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U4  func_1(void);
