964f052c78da7894ba172b4e1e8b672cc7f39da9
saisi <saisi@users.noreply.github.com>
2017-03-24 08:58:45 +0000

    1490345925 apochromatism

---
+++ 1490345925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2812359622
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0x01897A4AL;/* VOLATILE GLOBAL g_2 */
+static uint32_t g_8[3] = {3UL,3UL,3UL};
+static uint32_t g_55 = 18446744073709551608UL;
+static int64_t g_59 = 0xBF09457720F84B0FLL;
+static int64_t g_61[8] = {0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL,0xBC19AC4E69A4575FLL};
+static int64_t g_65[3] = {(-1L),(-1L),(-1L)};
+static volatile int64_t g_73[2] = {2L,2L};
+static volatile int64_t g_74 = 0L;/* VOLATILE GLOBAL g_74 */
+static volatile int64_t g_75 = 6L;/* VOLATILE GLOBAL g_75 */
