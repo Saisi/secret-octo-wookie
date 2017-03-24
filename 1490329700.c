66d7a4a5c012c810acf4731710bf74303f245c28
saisi <saisi@users.noreply.github.com>
2017-03-24 04:28:20 +0000

    1490329700 auntsary

---
+++ 1490329700.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      332526694
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xE2799AC7L;
+static int32_t g_6[7] = {0xF38C0887L,0x19AC196AL,0xF38C0887L,0xF38C0887L,0x19AC196AL,0xF38C0887L,0xF38C0887L};
+static int32_t g_7 = (-1L);
+static int16_t g_37 = 0x88AFL;
+static int32_t g_42 = 0xD0127420L;
+static int8_t g_67 = 0x76L;
+static int8_t **g_98 = (void*)0;
+static int32_t g_125 = 1L;
+static uint32_t g_139 = 0xC6E4DE62L;
