dfa4500b7d0736b8d843af51cd98821eaf66e2fa
saisi <saisi@users.noreply.github.com>
2017-03-24 15:29:27 +0000

    1490369365 adducent

---
+++ 1490369365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      352696635
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 28;
+};
+
+struct S1 {
+   const volatile uint8_t  f0;
+   volatile uint64_t  f1;
+   struct S0  f2;
+   signed f3 : 26;
+   uint32_t  f4;
