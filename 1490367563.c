739f70607a12e3467b1e6d64635f391b6c342815
saisi <saisi@users.noreply.github.com>
2017-03-24 14:59:23 +0000

    1490367563 antipapist

---
+++ 1490367563.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4271230476
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const int64_t  f1;
+   int8_t  f2;
+   const uint16_t  f3;
+   int8_t * const  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile union U0 g_7 = {1L};/* VOLATILE GLOBAL g_7 */
