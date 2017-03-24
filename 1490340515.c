37c3ca26d4d884df8f5264a3e24155860cf61e6d
saisi <saisi@users.noreply.github.com>
2017-03-24 07:28:37 +0000

    1490340515 appositional

---
+++ 1490340515.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3854379888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 6;
+   signed f1 : 3;
+   const signed f2 : 25;
+   const unsigned f3 : 1;
+   signed f4 : 12;
+};
+
+union U1 {
+   uint8_t  f0;
