0cb6953058169d10ef7a050a7f8cc3e81397ca87
saisi <saisi@users.noreply.github.com>
2017-03-24 16:29:33 +0000

    1490372973 Baeyer

---
+++ 1490372973.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3607536892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   signed f0 : 28;
+   volatile unsigned f1 : 2;
+   signed f2 : 24;
+   signed f3 : 17;
+   unsigned f4 : 28;
+   unsigned f5 : 9;
+   signed f6 : 16;
+   volatile signed f7 : 28;
+};
