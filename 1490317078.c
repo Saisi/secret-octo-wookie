d70fc48253caea65f3f318420c7c99e3622ae41d
saisi <saisi@users.noreply.github.com>
2017-03-24 00:58:00 +0000

    1490317078 anasarcas

---
+++ 1490317078.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1989054013
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   unsigned : 0;
+   const volatile unsigned f1 : 3;
+   int32_t  f2;
+   unsigned f3 : 27;
+   unsigned f4 : 29;
+   const signed f5 : 12;
+   const signed f6 : 22;
+   signed f7 : 13;
