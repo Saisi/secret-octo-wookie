37f7ebc2c709ed4efcfdf19a1966f5eb350a9625
saisi <saisi@users.noreply.github.com>
2017-03-24 22:30:10 +0000

    1490394610 bacciferous

---
+++ 1490394610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1177927209
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-7L);
+static int32_t * volatile g_3[1][10][8] = {{{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,(void*)0,&g_2,&g_2,&g_2,(void*)0,&g_2},{&g_2,(void*)0,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2}}};
+static int32_t * volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+static uint16_t g_18 = 0xBCD7L;
+static int32_t g_69 = 9L;
+static const int32_t *g_92 = &g_2;
+static const int32_t **g_91[4] = {&g_92,&g_92,&g_92,&g_92};
+static const int32_t ***g_90 = &g_91[1];
+static uint16_t g_96 = 1UL;
