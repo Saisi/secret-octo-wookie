6f65931dbabce13240264e8b178f21938192fc0c
saisi <saisi@users.noreply.github.com>
2017-03-24 09:58:50 +0000

    1490349530 arborization

---
+++ 1490349530.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      880507481
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[6] = {(-8L),0xE0648261L,(-8L),(-8L),0xE0648261L,(-8L)};
+static int64_t g_46 = 2L;
+static int32_t g_54[6][6][2] = {{{0x7D757520L,1L},{(-1L),0x7D757520L},{4L,0xD742CC32L},{4L,0x7D757520L},{(-1L),1L},{0x7D757520L,(-6L)}},{{0x4FEF5379L,0x7E10320BL},{0x860C01EFL,5L},{5L,0x0BDE8AFFL},{0xAB883EB9L,(-2L)},{0L,1L},{0x0BDE8AFFL,(-1L)}},{{(-1L),0x4FEF5379L},{(-6L),(-1L)},{(-1L),0L},{(-8L),0xA4EF21DDL},{(-1L),0x734826AAL},{(-1L),0x256FAE7AL}},{{(-2L),0x256FAE7AL},{(-1L),0x734826AAL},{(-1L),(-6L)},{4L,(-1L)},{1L,(-8L)},{0L,0xAB883EB9L}},{{(-1L),(-2L)},{1L,0x7D757520L},{(-1L),0L},{0x0BDE8AFFL,1L},{0x89C1C50DL,0x89C1C50DL},{0xA4EF21DDL,0x860C01EFL}},{{0xAB883EB9L,0x256FAE7AL},{0xD742CC32L,(-1L)},{(-8L),0xD742CC32L},{5L,0x60EC253DL},{5L,0xD742CC32L},{(-8L),(-1L)}}};
+static uint64_t g_68 = 0xC5DE30EA3BBE7568LL;
+static uint8_t g_82 = 0xF5L;
+static int8_t g_93 = 0x18L;
+static int8_t g_110 = 0xB0L;
+static int16_t g_146 = 0xEBDFL;
+static volatile int16_t g_151 = 0x1189L;/* VOLATILE GLOBAL g_151 */
