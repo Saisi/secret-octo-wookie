ca26206860e6d5311ea3373f4d9200a9e851f0c4
saisi <saisi@users.noreply.github.com>
2017-03-24 07:58:40 +0000

    1490342319 aether

---
+++ 1490342319.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2828916792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint32_t  f1;
+   uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_6[5][1] = {{(-4L)},{(-4L)},{(-4L)},{(-4L)},{(-4L)}};
+static int8_t g_17 = 0x9FL;
+static uint16_t g_18[6] = {1UL,3UL,3UL,1UL,3UL,3UL};
