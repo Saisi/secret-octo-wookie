3c4dc60d94c3c5634a53273018d9471b7a5ddf34
saisi <saisi@users.noreply.github.com>
2017-03-24 03:58:17 +0000

    1490327897 Abercromby

---
+++ 1490327897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3464724262
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   signed f1 : 30;
+   unsigned f2 : 26;
+   int16_t  f3;
+};
+#pragma pack(pop)
+
