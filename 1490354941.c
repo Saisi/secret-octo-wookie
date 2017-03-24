1eea41e3bab3b1508e4eac58c0fdcd794e9fdf08
saisi <saisi@users.noreply.github.com>
2017-03-24 11:29:03 +0000

    1490354941 Apogonidae

---
+++ 1490354941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3059581389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   volatile uint32_t  f1;
+   volatile int8_t  f2;
+   int16_t  f3;
+   volatile int32_t  f4;
+   uint8_t  f5;
+   const int64_t  f6;
