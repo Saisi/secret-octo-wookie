8031ada5a95fb66118615f4e468e5e2fc6aebd00
saisi <saisi@users.noreply.github.com>
2017-03-24 15:59:31 +0000

    1490371169 Astropecten

---
+++ 1490371169.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2793319045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int32_t  f1;
+   volatile uint64_t  f2;
+   uint64_t  f3;
+   uint8_t  f4;
+   const int8_t  f5;
+   volatile uint16_t  f6;
+   volatile unsigned f7 : 25;
+   const unsigned f8 : 28;
