c7fa60771c6d9c0cff5df40876945db7e7530997
saisi <saisi@users.noreply.github.com>
2017-03-24 06:28:31 +0000

    1490336910 amantillo

---
+++ 1490336910.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      32116594
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   volatile unsigned f1 : 4;
+   signed f2 : 8;
+   unsigned f3 : 10;
+   volatile unsigned f4 : 7;
+   unsigned f5 : 23;
+   const unsigned f6 : 28;
+   uint32_t  f7;
+   signed f8 : 23;
