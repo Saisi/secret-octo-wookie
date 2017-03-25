82547954a0ef337e60b5d18bd82838fd6f5015fd
saisi <saisi@users.noreply.github.com>
2017-03-25 10:31:22 +0000

    1490437880 backpedaling

---
+++ 1490437880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2222594233
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 27;
+   volatile int32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 0xCDC345FEL;
+static volatile int32_t * volatile g_10[3] = {(void*)0,(void*)0,(void*)0};
+static int32_t g_16 = (-1L);
