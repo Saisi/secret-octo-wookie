15b484daa3f15bfdd45b7a22bf21242fb20f339e
saisi <saisi@users.noreply.github.com>
2017-03-25 07:31:05 +0000

    1490427065 Andros

---
+++ 1490427065.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      978522086
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 1;
+   unsigned f1 : 18;
+   volatile unsigned f2 : 12;
+   signed f3 : 8;
+   int64_t  f4;
+   unsigned f5 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
