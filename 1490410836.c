5d91e62644eaddf0fccd7e0b8d35329169ea3e21
saisi <saisi@users.noreply.github.com>
2017-03-25 03:00:37 +0000

    1490410836 atomisation

---
+++ 1490410836.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2948003942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   int16_t  f1;
+   signed f2 : 25;
+   int32_t  f3;
+};
+#pragma pack(pop)
+
