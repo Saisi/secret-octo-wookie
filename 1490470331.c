1368cd3ae3133042571a7274469cf6403fe78006
saisi <saisi@users.noreply.github.com>
2017-03-25 19:32:12 +0000

    1490470331 babus

---
+++ 1490470331.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      50455679
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   volatile uint8_t  f1;
+   volatile signed f2 : 12;
+   volatile signed f3 : 7;
+};
+
+union U1 {
+   volatile int8_t  f0;
+};
