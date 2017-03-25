36767de7dea8ad39491ddd94612384a6da6c318b
saisi <saisi@users.noreply.github.com>
2017-03-25 23:02:31 +0000

    1490482950 Banquo

---
+++ 1490482950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2044539424
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int16_t  f1;
+};
+
+union U1 {
+   volatile int32_t  f0;
+};
+
+union U2 {
