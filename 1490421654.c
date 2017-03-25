b46e070553dc3d1fb058128b64ede4666aab8d45
saisi <saisi@users.noreply.github.com>
2017-03-25 06:00:56 +0000

    1490421654 Aredale

---
+++ 1490421654.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3022832822
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   volatile signed f1 : 26;
+   signed f2 : 20;
+   volatile signed f3 : 16;
+   unsigned f4 : 31;
+   signed f5 : 26;
+};
+
+#pragma pack(push)
