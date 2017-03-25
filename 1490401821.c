eb7afd297b0962958bb93fabe1b6b383e77145a1
saisi <saisi@users.noreply.github.com>
2017-03-25 00:30:21 +0000

    1490401821 alcoholist

---
+++ 1490401821.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      59709176
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 10;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6] = {0xF57A7C20L,1L,0xF57A7C20L,0xF57A7C20L,1L,0xF57A7C20L};
+static int32_t g_3 = 0x70ADE216L;
+static int8_t g_13 = 0xE6L;
+static int32_t g_20[9] = {(-2L),(-2L),(-2L),(-2L),(-2L),(-2L),(-2L),(-2L),(-2L)};
+static int32_t g_32 = 0x9B996A53L;
