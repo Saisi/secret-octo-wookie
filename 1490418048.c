d6b51e9d26a1e65952a550b87dcbca542a202b60
saisi <saisi@users.noreply.github.com>
2017-03-25 05:00:49 +0000

    1490418048 argle

---
+++ 1490418048.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2588170639
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 6;
+   unsigned f1 : 5;
+   signed f2 : 20;
+   unsigned f3 : 15;
+   uint32_t  f4;
+};
+#pragma pack(pop)
