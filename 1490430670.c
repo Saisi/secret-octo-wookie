6c7d2054888285e996ad34c5603d055efcf37c63
saisi <saisi@users.noreply.github.com>
2017-03-25 08:31:10 +0000

    1490430670 altercate

---
+++ 1490430670.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2069879503
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int64_t  f1;
+   volatile int32_t  f2;
+};
+
+union U1 {
+   const struct S0  f0;
+   volatile int64_t  f1;
+   const volatile int8_t * const  f2;
