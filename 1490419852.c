377bc841720ce82b38aed5c0f70116e410268edc
saisi <saisi@users.noreply.github.com>
2017-03-25 05:30:52 +0000

    1490419852 attitudiniser

---
+++ 1490419852.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1586998804
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_7 = 0x7FL;
+static uint64_t g_19 = 0UL;
+static struct S0 g_28 = {2070};
+static int32_t g_36 = 0xAC1E0F75L;
+static uint8_t g_69[3][5] = {{0UL,0UL,0UL,0UL,0UL},{0x8BL,0x90L,0x8BL,0x90L,0x8BL},{0UL,0UL,0UL,0UL,0UL}};
