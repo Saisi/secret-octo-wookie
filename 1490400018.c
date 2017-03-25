17902abd4c8f36d5e9e634d6e6e6383cfe566962
saisi <saisi@users.noreply.github.com>
2017-03-25 00:00:19 +0000

    1490400018 allocation

---
+++ 1490400018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2101232636
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+   int8_t  f1;
+   const signed f2 : 9;
+   unsigned f3 : 7;
+   unsigned f4 : 16;
+   volatile unsigned f5 : 28;
+   const unsigned f6 : 13;
+};
+
