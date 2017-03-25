b67fd75bdd07ad93655d40d85a6626e7793dce7e
saisi <saisi@users.noreply.github.com>
2017-03-25 14:31:43 +0000

    1490452302 akroasis

---
+++ 1490452302.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1891654821
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   int32_t  f1;
+   int16_t  f2;
+   volatile unsigned f3 : 15;
+   volatile int64_t  f4;
+   int8_t  f5;
+   const volatile int8_t  f6;
+   uint64_t  f7;
+   signed f8 : 18;
