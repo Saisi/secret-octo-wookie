5ef4b32df22d11ed02dce954ccd4de6566934832
saisi <saisi@users.noreply.github.com>
2017-03-25 20:32:18 +0000

    1490473938 baidak

---
+++ 1490473938.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1784435571
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const uint64_t g_2 = 0x6BACD79536444696LL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
