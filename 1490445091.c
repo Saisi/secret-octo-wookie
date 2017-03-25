56c50bdc3d0156271ad200907940a7f6d767379e
saisi <saisi@users.noreply.github.com>
2017-03-25 12:31:31 +0000

    1490445091 awakes

---
+++ 1490445091.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4249903384
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_6 = 2L;/* VOLATILE GLOBAL g_6 */
+static uint64_t g_9 = 18446744073709551606UL;
+static uint32_t g_11 = 9UL;
+static int16_t g_21 = 0xF7B1L;
+static uint16_t g_48 = 0x1F99L;
+static int32_t *g_68 = (void*)0;
+static int32_t g_76 = 0L;
+static int32_t *g_75[6][10] = {{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76},{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76},{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76},{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76},{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76},{&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76,&g_76}};
+static int32_t g_100[5][10] = {{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),0x00741811L,(-1L),(-1L),0x00741811L,(-1L),(-1L),0x00741811L,(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),0x00741811L,(-1L),(-1L),0x00741811L,(-1L),(-1L),0x00741811L,(-1L)}};
