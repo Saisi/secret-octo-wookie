504201c7e43ac64d6b96ce1983be51e2578469cc
saisi <saisi@users.noreply.github.com>
2017-03-26 03:33:05 +0000

    1490499185 antapices

---
+++ 1490499185.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      314440762
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[3] = {0x29C1464FL,0x29C1464FL,0x29C1464FL};
+static int32_t *g_2[1] = {&g_3[0]};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
