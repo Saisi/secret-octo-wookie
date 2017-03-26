52962910016f972b661b1a0070d40c36c5a3a45b
saisi <saisi@users.noreply.github.com>
2017-03-26 06:03:17 +0000

    1490508197 AmerSp

---
+++ 1490508197.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3908328765
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   signed : 0;
+};
+
+union U1 {
+   uint32_t  f0;
+   const int8_t * const  f1;
+   volatile int32_t  f2;
+   int32_t  f3;
