7e0d376cd5a48157dc7d36980332a732c2903187
saisi <saisi@users.noreply.github.com>
2017-03-26 01:02:50 +0000

    1490490164 antapexes

---
+++ 1490490164.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1137144863
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+   uint8_t  f1;
+   int32_t  f2;
+   const int32_t  f3;
+   int8_t  f4;
+   int32_t  f5;
+   signed f6 : 21;
+};
+
