01eabd82abc1f1a49ed4ce25531b08b92aab6943
saisi <saisi@users.noreply.github.com>
2017-03-26 04:03:07 +0000

    1490500987 auditorial

---
+++ 1490500987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      705014840
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   uint64_t  f1;
+   uint64_t  f2;
+   volatile int64_t  f3;
+   volatile unsigned f4 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6[5] = {4294967295UL,4294967295UL,4294967295UL,4294967295UL,4294967295UL};
