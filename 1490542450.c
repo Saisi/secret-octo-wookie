a8766986455eb61de58f56999eb18f198fe1dd4e
saisi <saisi@users.noreply.github.com>
2017-03-26 15:34:10 +0000

    1490542450 Baptornis

---
+++ 1490542450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2777334005
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-5L);
+static int32_t *g_7 = (void*)0;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
