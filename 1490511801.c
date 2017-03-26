5f501e5e1b495a6ccdd8e61cf7369cc53c799013
saisi <saisi@users.noreply.github.com>
2017-03-26 07:03:21 +0000

    1490511801 Animalia

---
+++ 1490511801.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      618894286
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 21;
+   signed f1 : 22;
+   unsigned : 0;
+   volatile unsigned f2 : 12;
+   const signed f3 : 11;
+   signed f4 : 22;
+   const int32_t  f5;
+   unsigned f6 : 3;
+   signed f7 : 8;
