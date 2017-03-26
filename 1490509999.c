7af778ef696c57d8e41762129c21242c959fd8b6
saisi <saisi@users.noreply.github.com>
2017-03-26 06:33:19 +0000

    1490509999 asperated

---
+++ 1490509999.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      404921244
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_2[7] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
