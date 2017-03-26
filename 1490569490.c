d5e8147a2e502c50694d2850b88d92feeefbf2bc
saisi <saisi@users.noreply.github.com>
2017-03-26 23:04:50 +0000

    1490569490 acetylenogen

---
+++ 1490569490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      8231286
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 9;
+   unsigned f1 : 26;
+   const volatile signed f2 : 20;
+   volatile unsigned f3 : 16;
+   unsigned f4 : 31;
+   signed f5 : 26;
+};
+
+#pragma pack(push)
