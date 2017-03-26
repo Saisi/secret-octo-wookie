e1e708f95864d84ca356cac343c9447e34098395
saisi <saisi@users.noreply.github.com>
2017-03-26 10:33:40 +0000

    1490524419 aphanisia

---
+++ 1490524419.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4267619731
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const unsigned f1 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x6E6BACD7L;
+static uint64_t g_10 = 0xD91E66C619A80BFBLL;
