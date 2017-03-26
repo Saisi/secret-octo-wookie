1d05fd3e9e937f9c5566bc2da8bb89c6f2bdb39d
saisi <saisi@users.noreply.github.com>
2017-03-26 14:04:02 +0000

    1490537041 antelopine

---
+++ 1490537041.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3821897470
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x3AE35568L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x53606DE7L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 5L;
+static int32_t *g_46[1][4] = {{(void*)0,(void*)0,(void*)0,(void*)0}};
+static int32_t ** volatile g_45 = &g_46[0][3];/* VOLATILE GLOBAL g_45 */
+static int32_t g_52 = 0x6C56D20AL;
+static const int32_t g_66 = 7L;
+static uint8_t g_77[1] = {7UL};
