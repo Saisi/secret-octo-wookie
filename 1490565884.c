c1bb94402893f3064fd071f606ba965862e9dfcf
saisi <saisi@users.noreply.github.com>
2017-03-26 22:04:45 +0000

    1490565884 agons

---
+++ 1490565884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1540832285
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint8_t  f1;
+   unsigned f2 : 19;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 4294967294UL;
+static union U0 g_16 = {0x6673E7EDL};
