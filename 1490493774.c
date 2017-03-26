f5a1da3fce4fc7e1715db7f1561229edadbdf5b2
saisi <saisi@users.noreply.github.com>
2017-03-26 02:02:55 +0000

    1490493774 alternater

---
+++ 1490493774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3746748856
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+};
+
+struct S1 {
+   signed f0 : 15;
+   signed f1 : 21;
+   unsigned f2 : 6;
+   volatile unsigned f3 : 26;
+   volatile int8_t  f4;
