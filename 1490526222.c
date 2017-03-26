a1d5582a8a02f0d18f7b05b1f37cfcfb6fddf4ef
saisi <saisi@users.noreply.github.com>
2017-03-26 11:03:46 +0000

    1490526222 anodon

---
+++ 1490526222.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1941540403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 24;
+};
+
+struct S1 {
+   int32_t  f0;
+   const int16_t  f1;
+   struct S0  f2;
+   uint16_t  f3;
+   const volatile struct S0  f4;
