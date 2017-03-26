b7d9e5a0bdbba1469113af3e5cdeec349a6b6460
saisi <saisi@users.noreply.github.com>
2017-03-26 09:03:32 +0000

    1490519012 agonium

---
+++ 1490519012.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1475604061
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 13;
+   signed f1 : 16;
+   volatile unsigned f2 : 19;
+   const volatile unsigned f3 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
