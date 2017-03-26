4c7cb7682dff2ffb26351f5e5d3b01718b3b68aa
saisi <saisi@users.noreply.github.com>
2017-03-26 02:32:59 +0000

    1490495577 aspersoria

---
+++ 1490495577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1169702884
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   int64_t  f1;
+   unsigned f2 : 24;
+   uint8_t  f3;
+   const int32_t  f4;
+   const int16_t  f5;
+   volatile uint64_t  f6;
+   const volatile uint16_t  f7;
+   int32_t  f8;
