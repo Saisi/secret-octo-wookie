88ec6864d4daf40fae7c8f835b1ab8c007eb29cb
saisi <saisi@users.noreply.github.com>
2017-03-26 05:33:15 +0000

    1490506394 bagatelle

---
+++ 1490506394.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3668692952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 26;
+   signed f1 : 7;
+   const signed f2 : 13;
+   unsigned f3 : 5;
+   unsigned : 0;
+   volatile unsigned f4 : 23;
+   signed f5 : 25;
