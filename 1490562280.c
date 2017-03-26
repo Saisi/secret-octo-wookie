836069a5cda74551eb506107359f14f733d8c028
saisi <saisi@users.noreply.github.com>
2017-03-26 21:04:40 +0000

    1490562280 Arvad

---
+++ 1490562280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      333015387
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x7A888ADAL;
+static uint32_t g_4 = 0x5735E4B7L;
+static int32_t g_13 = 0xA5DEC4F3L;
+static int32_t * volatile g_12 = &g_13;/* VOLATILE GLOBAL g_12 */
+static int32_t g_20 = 1L;
+static volatile uint32_t g_27 = 0x37828451L;/* VOLATILE GLOBAL g_27 */
+static volatile uint32_t *g_26 = &g_27;
+static uint32_t g_29 = 0xFCC1031CL;
+static int8_t g_43 = (-10L);
