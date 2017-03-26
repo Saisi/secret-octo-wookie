37fba71f02b6a838319bc046e6b6a1cb82428325
saisi <saisi@users.noreply.github.com>
2017-03-26 12:33:54 +0000

    1490531633 avicularian

---
+++ 1490531633.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      385782904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint8_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_11 = 0x63BA20B9L;/* VOLATILE GLOBAL g_11 */
+static volatile int32_t * volatile g_10[3] = {&g_11,&g_11,&g_11};
+static int32_t g_14[5] = {0x36DE0184L,0x36DE0184L,0x36DE0184L,0x36DE0184L,0x36DE0184L};
