824c0f68db99efc3cb1ad2c32701fae33149a79a
saisi <saisi@users.noreply.github.com>
2017-03-26 11:33:49 +0000

    1490528028 acarotoxic

---
+++ 1490528028.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2175103074
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 31;
+   const volatile unsigned f1 : 24;
+   signed f2 : 26;
+   signed f3 : 30;
+   volatile unsigned f4 : 21;
+   const volatile signed f5 : 24;
+   unsigned f6 : 22;
