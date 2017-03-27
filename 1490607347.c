781d8fcc464bf1422ae82503c76ff9ef4594b186
saisi <saisi@users.noreply.github.com>
2017-03-27 09:35:48 +0000

    1490607347 baldheaded

---
+++ 1490607347.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2069135480
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 0x8974B666L;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
+static int16_t g_44[1][6][1] = {{{(-1L)},{5L},{5L},{(-1L)},{5L},{5L}}};
+static uint32_t g_45 = 3UL;
+static int32_t g_54[4] = {0x8071081BL,0x8071081BL,0x8071081BL,0x8071081BL};
+static int32_t g_124 = (-1L);
+static int16_t g_127 = 0x2C60L;
+static uint32_t g_128 = 4294967293UL;
+static uint64_t g_137 = 18446744073709551615UL;
