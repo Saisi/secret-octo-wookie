c961a31bfcd55cb0d6de837769cd9dc9428b5451
saisi <saisi@users.noreply.github.com>
2017-03-27 22:36:59 +0000

    1490654219 acetylizing

---
+++ 1490654219.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      626182440
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   unsigned f1 : 15;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 8L;
+static int16_t g_22 = (-5L);
+static int16_t *g_21 = &g_22;
