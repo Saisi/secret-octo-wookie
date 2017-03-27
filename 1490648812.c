f4b8db5ce17e68b06d9f2980a088707eb01559d7
saisi <saisi@users.noreply.github.com>
2017-03-27 21:06:52 +0000

    1490648812 athenor

---
+++ 1490648812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2552194477
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFDA0E60FL;
+static volatile uint64_t g_8 = 18446744073709551615UL;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
