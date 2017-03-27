eac5f0a2600415e4b601ba3484a376cad865d5bf
saisi <saisi@users.noreply.github.com>
2017-03-27 16:36:28 +0000

    1490632587 Adramelech

---
+++ 1490632587.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2053791235
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 16;
+   signed f1 : 1;
+   volatile unsigned f2 : 24;
+   const signed f3 : 15;
+   const volatile signed f4 : 14;
+   signed f5 : 30;
+   unsigned f6 : 20;
+   volatile signed f7 : 11;
+   signed f8 : 30;
