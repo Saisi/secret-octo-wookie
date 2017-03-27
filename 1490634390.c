2e903698c0a8c46bf7963ae28974c6ca03fb9222
saisi <saisi@users.noreply.github.com>
2017-03-27 17:06:31 +0000

    1490634390 anticonvulsive

---
+++ 1490634390.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3144054593
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xD06CE125L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xDF251D69L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-6L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0x1F782A18L;/* VOLATILE GLOBAL g_6 */
