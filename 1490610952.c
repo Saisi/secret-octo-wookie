7de77d25120776c0e0114ad1aa94d835a23159be
saisi <saisi@users.noreply.github.com>
2017-03-27 10:35:52 +0000

    1490610952 Archaeoceti

---
+++ 1490610952.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3740339017
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   volatile int8_t * f1;
+};
+
+union U1 {
+   const int8_t * f0;
+   int8_t * f1;
+   const volatile int16_t  f2;
+   uint8_t  f3;
