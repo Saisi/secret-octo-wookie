f4721beac062933bdf1ba9a2ddcb2250cbeca249
saisi <saisi@users.noreply.github.com>
2017-03-27 12:06:02 +0000

    1490616361 afterbreast

---
+++ 1490616361.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1795852088
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   signed f1 : 23;
+   unsigned f2 : 15;
+   const uint32_t  f3;
+   unsigned f4 : 6;
+   signed f5 : 26;
+   volatile signed f6 : 19;
+   const volatile signed : 0;
+   volatile unsigned f7 : 17;
