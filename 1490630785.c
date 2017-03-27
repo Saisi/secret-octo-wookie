73adad2f1f1aea96759e3376398481c3bd653bce
saisi <saisi@users.noreply.github.com>
2017-03-27 16:06:25 +0000

    1490630785 archworker

---
+++ 1490630785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1853042844
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 3L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[2] = {(-6L),(-6L)};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
