9d8ee92b3bb0ab38e7582cd9eb19f5c78d27a273
saisi <saisi@users.noreply.github.com>
2017-03-27 03:35:17 +0000

    1490585717 aldamin

---
+++ 1490585717.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2168135732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_15 = 5UL;
+static uint8_t g_50 = 255UL;
+static int16_t g_53 = 0L;
+static int8_t g_54 = (-10L);
+static struct S0 g_94[4] = {{0x851AA8C9L},{0x851AA8C9L},{0x851AA8C9L},{0x851AA8C9L}};
