e2123d8a12dcac5d7f68301ff95a26f59f08911d
saisi <saisi@users.noreply.github.com>
2017-03-27 02:05:09 +0000

    1490580308 antibodies

---
+++ 1490580308.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1192533459
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 8;
+   volatile signed f1 : 31;
+   const volatile signed f2 : 6;
+   unsigned f3 : 25;
+   signed f4 : 6;
+   signed f5 : 7;
+   signed f6 : 10;
+   const unsigned f7 : 21;
+   unsigned f8 : 4;
