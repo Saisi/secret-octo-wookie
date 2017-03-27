3cd70ce1186a9811ae5625a69360a231be8b3c6b
saisi <saisi@users.noreply.github.com>
2017-03-27 08:35:42 +0000

    1490603741 antidemocratical

---
+++ 1490603741.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2373259561
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 19;
+   signed f1 : 30;
+   volatile unsigned f2 : 9;
+   const unsigned f3 : 16;
+};
+#pragma pack(pop)
+
