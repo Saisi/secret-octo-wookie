854a062285979a5da9d0a399035ef730d8ca2444
saisi <saisi@users.noreply.github.com>
2017-03-27 21:36:54 +0000

    1490650614 ammo

---
+++ 1490650614.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2652206621
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_3 = (void*)0;/* VOLATILE GLOBAL g_3 */
+static int32_t g_47[5] = {0L,0L,0L,0L,0L};
+static uint16_t g_53[2][8] = {{65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL},{65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL}};
+static int16_t g_55 = 7L;
+static volatile int16_t g_56 = 0x43A5L;/* VOLATILE GLOBAL g_56 */
+static int32_t g_59 = 0L;
+static int32_t * volatile g_62 = &g_59;/* VOLATILE GLOBAL g_62 */
+static uint16_t g_88 = 0x1B01L;
+static uint16_t *g_87 = &g_88;
