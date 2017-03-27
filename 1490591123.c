f0879e26c0c9a81aac4c98c4f3fa6a0df445b938
saisi <saisi@users.noreply.github.com>
2017-03-27 05:05:24 +0000

    1490591123 aspergilliform

---
+++ 1490591123.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3502417406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 13;
+   signed f1 : 14;
+   const volatile int32_t  f2;
+   unsigned : 0;
+   const unsigned f3 : 5;
+   const signed f4 : 29;
+};
+
+struct S1 {
