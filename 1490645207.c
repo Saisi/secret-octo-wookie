594969876a04e242acf5d2dc07f69a6dd150ca87
saisi <saisi@users.noreply.github.com>
2017-03-27 20:06:47 +0000

    1490645207 anemotaxis

---
+++ 1490645207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2028720854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2 = 0UL;
+static int32_t g_7[9] = {0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L,0xA75C19C5L};
+static int32_t * volatile g_6 = &g_7[2];/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
