7825bda19bc0f0c010e5bcce68e045fd03063930
saisi <saisi@users.noreply.github.com>
2017-03-27 23:37:05 +0000

    1490657825 arthrotomy

---
+++ 1490657825.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      312549529
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint32_t  f1;
+   volatile int64_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   const uint8_t  f0;
+   const int16_t  f1;
