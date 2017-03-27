c4f1353e2d421a96455cae90a0e06d6284620c66
saisi <saisi@users.noreply.github.com>
2017-03-27 07:35:37 +0000

    1490600136 Barahona

---
+++ 1490600136.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      332405423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint32_t  f0;
+   volatile signed f1 : 27;
+   signed f2 : 5;
+   unsigned f3 : 21;
+   unsigned f4 : 8;
+   unsigned f5 : 22;
+   int32_t  f6;
+};
+
