dadd4ab028cf3326f4201d8de3319a9ed248c910
saisi <saisi@users.noreply.github.com>
2017-03-28 13:38:22 +0000

    1490708302 aftercooler

---
+++ 1490708302.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      516207475
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_21 = 0x1EL;
+static int32_t g_34 = 0xDD3610CBL;
+static int32_t g_36 = (-2L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+static int32_t  func_2(uint32_t  p_3, int16_t  p_4, int32_t  p_5, uint64_t  p_6);
+static int32_t  func_18(uint32_t  p_19);
