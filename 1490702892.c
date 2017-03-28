72e9897cd0a9934c67e2590a0d37039bc1a134ce
saisi <saisi@users.noreply.github.com>
2017-03-28 12:08:14 +0000

    1490702892 amylin

---
+++ 1490702892.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      770698857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 19;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   int32_t  f1;
+   signed f2 : 5;
+};
+
