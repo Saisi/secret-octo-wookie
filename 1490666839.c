e20c26cbe72ed178f86a8f1ac52ba35ce9fd297e
saisi <saisi@users.noreply.github.com>
2017-03-28 02:07:20 +0000

    1490666839 bailieries

---
+++ 1490666839.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4093947530
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   int16_t  f1;
+   signed f2 : 11;
+   int32_t  f3;
+   int16_t  f4;
+   volatile int32_t  f5;
+   uint32_t  f6;
+   int64_t  f7;
+};
