6e76e34cb707a8378d0acd70fed087617bf23cd5
saisi <saisi@users.noreply.github.com>
2017-03-28 19:38:52 +0000

    1490729932 Balac

---
+++ 1490729932.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      26026657
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int64_t g_8[4][2] = {{7L,7L},{7L,7L},{7L,7L},{7L,7L}};
+static int32_t *g_15[1] = {(void*)0};
+static uint64_t g_45 = 0xD437F1BD53D2AE45LL;
+static uint64_t g_55 = 0x156BE8236511F0B9LL;
+static uint32_t g_57 = 1UL;
+static uint32_t g_68 = 0x0B913A86L;
+static int32_t *g_79 = (void*)0;
+static int32_t * volatile * volatile g_78[3][4][2] = {{{&g_79,&g_79},{&g_79,&g_79},{(void*)0,&g_79},{&g_79,&g_79}},{{(void*)0,&g_79},{&g_79,&g_79},{&g_79,&g_79},{(void*)0,&g_79}},{{&g_79,&g_79},{(void*)0,&g_79},{&g_79,&g_79},{&g_79,&g_79}}};
+static uint32_t g_82 = 0xF077D7E7L;
