2524dd3210d8ee213c7f6d36ecfd65c5519eba7a
saisi <saisi@users.noreply.github.com>
2017-03-28 03:37:29 +0000

    1490672248 aeroliths

---
+++ 1490672248.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3215928888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const unsigned f0 : 2;
+   volatile unsigned f1 : 30;
+   signed f2 : 25;
