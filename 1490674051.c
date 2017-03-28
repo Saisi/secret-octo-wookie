b449606d14e40112783d9193ce05652dce40e711
saisi <saisi@users.noreply.github.com>
2017-03-28 04:07:31 +0000

    1490674051 antiterrorist

---
+++ 1490674051.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4174458709
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 9;
+   unsigned f1 : 28;
+   unsigned f2 : 15;
+   unsigned f3 : 30;
+   signed f4 : 23;
+   const volatile signed f5 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
