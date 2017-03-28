9c24da4337923310e19526bedf3a88f2cd83c55b
saisi <saisi@users.noreply.github.com>
2017-03-28 00:37:11 +0000

    1490661431 argyranthemous

---
+++ 1490661431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      181548389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 17;
+   uint64_t  f1;
+   const uint8_t  f2;
+   uint8_t  f3;
+};
+
+struct S1 {
+   const volatile signed f0 : 27;
+   signed f1 : 23;
