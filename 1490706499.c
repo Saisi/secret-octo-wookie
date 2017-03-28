8ed10e3f797bc0d9164021ced7fe3f793bf53560
saisi <saisi@users.noreply.github.com>
2017-03-28 13:08:20 +0000

    1490706499 antinationalism

---
+++ 1490706499.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4140084892
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-4L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xFE387FB0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static int32_t g_10 = 0x0C170CD5L;
+static uint16_t g_27 = 1UL;
+static int32_t g_50 = 0x1B8D8131L;
+static int32_t * volatile g_49 = &g_50;/* VOLATILE GLOBAL g_49 */
+static uint64_t g_67 = 0xE54384F3AC16B405LL;
+static uint8_t g_88 = 0x21L;
