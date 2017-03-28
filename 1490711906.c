25faead9be193188fbf1dc2abb9495b64c98f22a
saisi <saisi@users.noreply.github.com>
2017-03-28 14:38:27 +0000

    1490711906 azotemias

---
+++ 1490711906.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1930083279
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const volatile uint32_t  f1;
+   int16_t  f2;
+   const uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4 = 0xF09CC25FL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_25 = 0x6A2A5EA8L;
