4cb3187ad88af41f69cb6ea4134308c2f9baba56
saisi <saisi@users.noreply.github.com>
2017-03-28 20:08:55 +0000

    1490731735 azophenol

---
+++ 1490731735.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      959981045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t  f0;
+   uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static int16_t g_17 = 0xCD32L;
+static uint8_t g_19 = 6UL;
+static uint64_t g_24 = 0x9829CBC21B07FB07LL;
