f05a72ed43b7402c2e0dd9fd4be8ded1b0de6460
saisi <saisi@users.noreply.github.com>
2017-03-29 07:40:08 +0000

    1490773207 aventail

---
+++ 1490773207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2942987565
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   int32_t  f1;
+   int8_t  f2;
+   volatile int64_t  f3;
+};
+
+union U1 {
+   const int32_t  f0;
+};
