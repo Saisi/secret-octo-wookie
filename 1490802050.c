0219f7bafde18a91dfd514450760561641bd66ba
saisi <saisi@users.noreply.github.com>
2017-03-29 15:40:57 +0000

    1490802050 articulationes

---
+++ 1490802050.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3521376320
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   const volatile uint16_t  f1;
+   int64_t  f2;
+};
+
+struct S1 {
+   const volatile unsigned f0 : 29;
+   volatile signed f1 : 3;
+   volatile uint16_t  f2;
