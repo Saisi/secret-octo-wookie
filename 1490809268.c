59bf4e3d741138bd45f5f8a43da061a8a6cb8b13
saisi <saisi@users.noreply.github.com>
2017-03-29 17:41:09 +0000

    1490809268 autonomies

---
+++ 1490809268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1600680792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   int64_t  f1;
+   int64_t  f2;
+   uint32_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-4L);/* VOLATILE GLOBAL g_3 */
