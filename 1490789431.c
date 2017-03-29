83fd43d58e9860e611ea4dd9c995ed78ac129962
saisi <saisi@users.noreply.github.com>
2017-03-29 12:10:31 +0000

    1490789431 angelocracy

---
+++ 1490789431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2271571173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   int16_t  f1;
+   int32_t  f2;
+   unsigned f3 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6] = {(-4L),(-6L),(-4L),(-4L),(-6L),(-4L)};
+static volatile int32_t g_3 = 0x1DBA3399L;/* VOLATILE GLOBAL g_3 */
