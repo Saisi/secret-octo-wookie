a77a1c30f77e38c9c9116a7051a7b9cccf12fef1
saisi <saisi@users.noreply.github.com>
2017-03-29 01:09:24 +0000

    1490749764 anemia

---
+++ 1490749764.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3173589458
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   volatile signed f0 : 16;
+   const unsigned f1 : 8;
+   volatile unsigned : 0;
+   const volatile unsigned f2 : 23;
+   volatile signed f3 : 23;
+   uint8_t  f4;
+   const uint64_t  f5;
+   uint8_t  f6;
+   signed f7 : 26;
