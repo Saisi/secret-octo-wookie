a2ddd4f4b71f7bdb823b61691bd1578f4a548e9e
saisi <saisi@users.noreply.github.com>
2017-03-29 18:41:14 +0000

    1490812873 analemmata

---
+++ 1490812873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      235468834
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int32_t g_5 = 0xB8C3AD3BL;
+static int32_t g_7 = 0xFAFD11D9L;
+static uint64_t g_37[3][1] = {{0x85A06C0F1A0197CBLL},{0x85A06C0F1A0197CBLL},{0x85A06C0F1A0197CBLL}};
+static int32_t g_45 = 3L;
+static int32_t g_72 = 1L;
+static int64_t g_78 = 0L;
+static int64_t g_82 = 0L;
+static int32_t g_84 = 0x2AC96AAEL;
