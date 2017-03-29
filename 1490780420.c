ad4e63145e29f0212b72d800e3cfac20263f7085
saisi <saisi@users.noreply.github.com>
2017-03-29 09:40:20 +0000

    1490780420 Ariosto

---
+++ 1490780420.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      790891396
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   unsigned f1 : 5;
+   int8_t * const  f2;
+   const volatile int8_t * const  f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[4] = {(-8L),(-8L),(-8L),(-8L)};
