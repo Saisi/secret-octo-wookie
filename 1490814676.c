5733f78b968b00d311a9a604064ad442d0701bf3
saisi <saisi@users.noreply.github.com>
2017-03-29 19:11:16 +0000

    1490814676 aquarians

---
+++ 1490814676.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1210639865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 27;
+   signed f1 : 6;
+   signed f2 : 17;
+   signed f3 : 24;
+};
+#pragma pack(pop)
+
