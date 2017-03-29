fe946a45ace1df6c9fa5b925778ebc0a318ca712
saisi <saisi@users.noreply.github.com>
2017-03-29 02:39:31 +0000

    1490755171 baraka

---
+++ 1490755171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1782267034
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 27;
+   volatile signed f1 : 4;
+};
+
+union U1 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
