dc0248f6ce6f340880ca7599baac698541ab195e
saisi <saisi@users.noreply.github.com>
2017-03-29 17:11:06 +0000

    1490807466 barbotine

---
+++ 1490807466.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1290441857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x24A8C6A5L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x6CE125D1L;
+static int32_t g_8 = 9L;
+static uint32_t g_26 = 1UL;
+static uint16_t g_32 = 0x511FL;
+static uint8_t g_117 = 1UL;
+static uint32_t *g_130 = &g_26;
+static uint32_t **g_129 = &g_130;
+static uint32_t ***g_128[3][7] = {{&g_129,&g_129,&g_129,&g_129,&g_129,&g_129,&g_129},{&g_129,(void*)0,&g_129,(void*)0,&g_129,(void*)0,&g_129},{&g_129,&g_129,&g_129,&g_129,&g_129,&g_129,&g_129}};
