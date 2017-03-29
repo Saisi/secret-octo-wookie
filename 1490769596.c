a9b6f3da486520aba1ca9ef05bec6513e23770de
saisi <saisi@users.noreply.github.com>
2017-03-29 06:39:57 +0000

    1490769596 amalgamatize

---
+++ 1490769596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      152998741
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * const  volatile  f0;
+   volatile uint8_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-9L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x7831E42AL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
