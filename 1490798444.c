14af78b38adf4b079e59913c5e8a468d854c69d3
saisi <saisi@users.noreply.github.com>
2017-03-29 14:40:45 +0000

    1490798444 aminosulphonic

---
+++ 1490798444.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      780140052
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[1] = {7L};
+static int32_t g_10 = 0x8F0AA6E6L;
+static int32_t *g_12[1] = {&g_10};
+static int32_t ** volatile g_11 = &g_12[0];/* VOLATILE GLOBAL g_11 */
+static int32_t g_74 = 0xA33FFEA3L;
+static uint8_t g_76 = 0UL;
+static const uint8_t g_77 = 255UL;
+static uint64_t g_86 = 0xEFE450DE0B5DAE8CLL;
+static int8_t g_91 = 0x37L;
