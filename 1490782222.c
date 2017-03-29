771ca751b1523f70ff2f040771ecab161a82b422
saisi <saisi@users.noreply.github.com>
2017-03-29 10:10:23 +0000

    1490782222 agnomens

---
+++ 1490782222.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1892758199
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x9B3198A8L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-10L);
+static int32_t g_6 = 6L;
+static uint8_t g_23[5] = {0xA3L,0xA3L,0xA3L,0xA3L,0xA3L};
+static uint16_t g_25 = 0UL;
+static int8_t g_73 = 0L;
+static int32_t g_95 = (-1L);
+static int16_t g_103 = 0x7DC7L;
+static uint32_t g_104 = 0xF688490AL;
