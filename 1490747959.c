6012d13a10423e818c22478071d10dfd798500f1
saisi <saisi@users.noreply.github.com>
2017-03-29 00:39:20 +0000

    1490747959 ACWA

---
+++ 1490747959.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2440758785
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_11 = 18446744073709551606UL;
+static int64_t g_14 = 0xBE9FB1C03809D010LL;
+static int32_t g_27[5] = {0x7B47E0FAL,0x7B47E0FAL,0x7B47E0FAL,0x7B47E0FAL,0x7B47E0FAL};
+static int32_t g_80 = 0L;
+static int32_t * volatile g_79 = &g_80;/* VOLATILE GLOBAL g_79 */
+static uint8_t g_85 = 0xF4L;
+static uint32_t g_87 = 0x4EDDC8DEL;
+static int32_t g_123 = 0x053DA949L;
+static int64_t g_127 = (-6L);
