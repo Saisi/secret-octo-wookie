68af1424bad89eab0efeac93e3413151e075c9a6
saisi <saisi@users.noreply.github.com>
2017-03-29 05:39:51 +0000

    1490765990 amorphophyte

---
+++ 1490765990.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3789253003
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   const unsigned f1 : 11;
+   const int64_t  f2;
+   const unsigned f3 : 19;
+   volatile uint32_t  f4;
+   signed f5 : 22;
+   unsigned f6 : 2;
+   unsigned f7 : 19;
+   volatile signed f8 : 16;
