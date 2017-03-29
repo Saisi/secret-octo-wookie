fd84d5d812569cb70efa6bc954750d4180201416
saisi <saisi@users.noreply.github.com>
2017-03-29 16:10:59 +0000

    1490803859 Afrikanerdom

---
+++ 1490803859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2556548105
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0xF47AL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_9 = 9L;
+static volatile int8_t g_17[3] = {0xB6L,0xB6L,0xB6L};
+static uint8_t g_18 = 0x62L;
+static int32_t ** const  volatile g_23 = (void*)0;/* VOLATILE GLOBAL g_23 */
+static int32_t *g_25 = &g_9;
+static int32_t ** const  volatile g_24 = &g_25;/* VOLATILE GLOBAL g_24 */
+
+
