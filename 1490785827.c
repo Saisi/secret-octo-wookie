53515d3687aca66f67905f36e90cb7f6acf1dd27
saisi <saisi@users.noreply.github.com>
2017-03-29 11:10:27 +0000

    1490785827 annulose

---
+++ 1490785827.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1751790421
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 30;
+   uint32_t  f2;
+   volatile uint32_t  f3;
+};
+#pragma pack(pop)
+
