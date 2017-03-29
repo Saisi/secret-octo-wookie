f5f71d897912c059af4958b0b59a0d2029feec4c
saisi <saisi@users.noreply.github.com>
2017-03-29 04:39:45 +0000

    1490762385 alforjas

---
+++ 1490762385.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1176647549
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_4 = 0xC70BB743D837BC75LL;/* VOLATILE GLOBAL g_4 */
+static int64_t g_12 = (-1L);
+static int32_t g_32 = 0x5D9C7279L;
+static int16_t g_75[8] = {8L,8L,8L,8L,8L,8L,8L,8L};
+static volatile int16_t g_77 = 0x1510L;/* VOLATILE GLOBAL g_77 */
+static volatile int16_t * const g_76[1] = {&g_77};
+static uint16_t g_83[2] = {65527UL,65527UL};
+static int32_t g_84 = 1L;
+static int32_t g_85[5] = {0L,0L,0L,0L,0L};
