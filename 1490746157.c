60280b581d16cabae98320570500365e38acbf42
saisi <saisi@users.noreply.github.com>
2017-03-29 00:09:17 +0000

    1490746157 anaerobion

---
+++ 1490746157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1135267102
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x3AE35568L;
+static int32_t g_60[10] = {0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L,0xF4EDB986L};
+static volatile int32_t g_66[10] = {7L,7L,7L,7L,7L,7L,7L,7L,7L,7L};
+static int32_t g_67 = 0xF83262CCL;
+static int32_t g_70 = 0x7F612420L;
+static int32_t g_72 = 0x7BDE7BC9L;
+static int32_t * const  volatile g_71 = &g_72;/* VOLATILE GLOBAL g_71 */
+static uint8_t g_77 = 0x9AL;
+static uint32_t g_83 = 0UL;
