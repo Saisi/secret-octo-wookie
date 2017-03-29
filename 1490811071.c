d91509ab1a6c91cae8b32267c5cd2b64d4973f90
saisi <saisi@users.noreply.github.com>
2017-03-29 18:11:12 +0000

    1490811071 bandoleers

---
+++ 1490811071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3821281315
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_16 = 0x9CL;
+static uint8_t g_19 = 8UL;
+static volatile uint8_t g_48 = 7UL;/* VOLATILE GLOBAL g_48 */
+static int32_t g_52[1][4] = {{0x0BB9A67BL,0x0BB9A67BL,0x0BB9A67BL,0x0BB9A67BL}};
+static uint32_t g_61 = 0xDF942F02L;
+static int64_t g_66[7] = {5L,5L,5L,5L,5L,5L,5L};
+static int8_t g_68 = 0x1BL;
+static int8_t g_71 = 0x00L;
+static uint8_t g_73 = 0xAEL;
