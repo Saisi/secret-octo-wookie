7c3c550ab637082cc281b71690fe48be08c53725
saisi <saisi@users.noreply.github.com>
2017-03-30 00:41:43 +0000

    1490834503 albugo

---
+++ 1490834503.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1299662537
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile int32_t  f1;
+   volatile int16_t  f2;
+   uint8_t  f3;
+   const uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_3[2][9][3] = {{{0x5BF2L,0x941FL,0x49B7L},{0x5132L,2UL,6UL},{0x87D0L,0x5132L,0UL},{65526UL,0xAA0DL,0xAA0DL},{65526UL,0UL,0x5132L},{0x87D0L,6UL,2UL},{0x5132L,0x49B7L,0x941FL},{0x5BF2L,1UL,65526UL},{0xAA0DL,0x49B7L,0xA516L}},{{0x941FL,6UL,0x5BF2L},{65532UL,0UL,1UL},{1UL,0xAA0DL,1UL},{0UL,0x5132L,0x5BF2L},{0x9A57L,2UL,0xA516L},{0xCE4AL,0x941FL,65526UL},{2UL,0UL,0x9A57L},{0UL,0x941FL,6UL},{0x5BF2L,0UL,0xAA0DL}}};
