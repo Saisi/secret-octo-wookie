8a829af3a47da26ba252cd1fbf92f3850cb692b6
saisi <saisi@users.noreply.github.com>
2017-03-30 17:13:12 +0000

    1490893991 argenter

---
+++ 1490893991.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      375610347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int16_t  f1;
+};
+
+union U1 {
+   int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
