482973b3a0b70da095ebee743bdb52633c62cdb4
saisi <saisi@users.noreply.github.com>
2017-03-30 14:42:59 +0000

    1490884978 antiphona

---
+++ 1490884978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1699373811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x67A876E6L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xD7953644L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6[1][2] = {{(-8L),(-8L)}};
+static int32_t g_7 = 0xEC79CF18L;
+static volatile int32_t g_8 = (-5L);/* VOLATILE GLOBAL g_8 */
+static volatile int32_t g_9[7] = {0xEB200688L,0xEB200688L,0xEB200688L,0xEB200688L,0xEB200688L,0xEB200688L,0xEB200688L};
+static int32_t g_10 = 4L;
+static int32_t g_12[4][5] = {{(-1L),0x7812A3CCL,(-1L),0x7812A3CCL,(-1L)},{0L,0L,0L,0L,0L},{(-1L),0x7812A3CCL,(-1L),0x7812A3CCL,(-1L)},{0L,0L,0L,0L,0L}};
