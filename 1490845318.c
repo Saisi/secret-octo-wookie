38d399b2d532053a0c49771b233297130829eeae
saisi <saisi@users.noreply.github.com>
2017-03-30 03:41:59 +0000

    1490845318 anoraks

---
+++ 1490845318.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3250122061
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 5;
+   volatile signed f1 : 8;
+   unsigned f2 : 11;
+   unsigned f3 : 10;
+   unsigned f4 : 19;
+   uint32_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
