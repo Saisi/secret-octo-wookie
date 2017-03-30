80531f6ce5555fa6421961df8672a63d8af00fef
saisi <saisi@users.noreply.github.com>
2017-03-30 08:12:25 +0000

    1490861544 articulations

---
+++ 1490861544.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      56413403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_14[6][10] = {{0x2E65L,65529UL,0UL,65529UL,0x2E65L,0UL,0xF7E8L,0xDA58L,8UL,0xEC4FL},{0xF7E8L,0x6076L,0xEC4FL,0UL,7UL,0x700DL,0x700DL,7UL,0UL,0xEC4FL},{0UL,0UL,9UL,0xEC4FL,0x2E65L,65535UL,7UL,0UL,65535UL,0x52AAL},{0UL,0UL,7UL,8UL,65529UL,8UL,7UL,0UL,0UL,0UL},{0x6076L,0UL,65535UL,0x2E65L,0x52AAL,0xCE55L,9UL,0UL,0UL,9UL},{65529UL,65535UL,65535UL,65535UL,65535UL,65529UL,0xCE55L,0xEC4FL,0xDA58L,0x700DL}};
+static int32_t g_17 = 0xEFDB7B24L;
+static int32_t * volatile g_16 = &g_17;/* VOLATILE GLOBAL g_16 */
+static int32_t g_63 = 0x2C90ACEFL;
+static int32_t g_66 = 0L;
