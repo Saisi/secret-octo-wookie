d5834a867acb381658c0da13dfa920a01eff9a8c
saisi <saisi@users.noreply.github.com>
2017-03-30 20:13:25 +0000

    1490904805 backberend

---
+++ 1490904805.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3553391307
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   int8_t  f1;
+   uint32_t  f2;
+   volatile uint32_t  f3;
+   uint32_t  f4;
+   const volatile int8_t  f5;
+   int32_t  f6;
+};
+
