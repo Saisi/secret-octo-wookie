abb543a3c206e509435409a5bac2fd938895a445
saisi <saisi@users.noreply.github.com>
2017-03-30 15:13:01 +0000

    1490886781 adelocodonic

---
+++ 1490886781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3811229230
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_22 = 65527UL;/* VOLATILE GLOBAL g_22 */
+static int32_t g_28 = 3L;
+static uint32_t g_42 = 0UL;
+static int32_t g_77 = (-7L);
+static uint32_t g_85 = 0x30BF04B2L;
+static uint32_t *g_104 = &g_42;
+static uint64_t g_106 = 0x18E2BB11FE8BE9C2LL;
+static uint64_t g_107 = 9UL;
+static uint8_t g_125 = 0xE1L;
