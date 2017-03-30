bd5042224f50e989e9c7fc24f79e21d91ca77252
saisi <saisi@users.noreply.github.com>
2017-03-30 22:43:39 +0000

    1490913818 albuminimeter

---
+++ 1490913818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      853485279
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+   unsigned f1 : 13;
+   signed f2 : 28;
+   unsigned f3 : 23;
+   const int8_t  f4;
+   signed f5 : 14;
+   const signed f6 : 25;
+   signed f7 : 15;
+   signed f8 : 2;
