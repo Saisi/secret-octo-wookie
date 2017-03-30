21554f6b336cae908219b224539cb52ac4b2c516
saisi <saisi@users.noreply.github.com>
2017-03-30 11:42:45 +0000

    1490874163 angustura

---
+++ 1490874163.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1319695849
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 19;
+   signed f1 : 30;
+   volatile signed f2 : 9;
+   signed f3 : 16;
+   signed f4 : 1;
+   unsigned f5 : 22;
+   const signed f6 : 29;
+   signed f7 : 28;
+};
