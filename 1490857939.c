1d49a30003f02f98ad58d8e72564e8c327187d22
saisi <saisi@users.noreply.github.com>
2017-03-30 07:12:20 +0000

    1490857939 anisophyllous

---
+++ 1490857939.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2013460318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   const int32_t  f1;
+   volatile int16_t  f2;
+   int64_t  f3;
+   volatile uint64_t  f4;
+   volatile uint8_t  f5;
+   uint16_t  f6;
+   int8_t  f7;
+   int16_t  f8;
