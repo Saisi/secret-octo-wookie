dbbae0bc9b7d98cdfdc3cafa36eec4c55dc7c3d2
saisi <saisi@users.noreply.github.com>
2017-03-30 10:12:35 +0000

    1490868755 bannition

---
+++ 1490868755.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      161985446
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   volatile uint16_t  f1;
+   int16_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_11 = 0x4D0289A6L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_34 = 0x5B641B44L;
