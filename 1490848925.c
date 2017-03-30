4a11703df408ea563ee90fb707afc9d39756b7b7
saisi <saisi@users.noreply.github.com>
2017-03-30 04:42:07 +0000

    1490848925 ageism

---
+++ 1490848925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2031576976
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   volatile int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_15 = 2L;
+static uint16_t g_33 = 0x4DF8L;
+static int32_t g_48 = 0xCD890F6DL;
+static uint16_t g_49[1][3] = {{0x7DA8L,0x7DA8L,0x7DA8L}};
