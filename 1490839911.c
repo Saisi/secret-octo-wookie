6ee80feb186741b0d2e77b7de26d42d9608761dc
saisi <saisi@users.noreply.github.com>
2017-03-30 02:11:51 +0000

    1490839911 allotrope

---
+++ 1490839911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      732191719
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 18;
+   volatile uint32_t  f1;
+   const signed : 0;
+   const volatile unsigned f2 : 11;
+   const volatile signed f3 : 16;
+   const signed f4 : 9;
+   const unsigned f5 : 4;
