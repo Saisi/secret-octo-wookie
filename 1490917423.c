8e9bca2a868948f1e2effa5596758bca5e8f4cc3
saisi <saisi@users.noreply.github.com>
2017-03-30 23:43:43 +0000

    1490917423 azodisulphonic

---
+++ 1490917423.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1944344243
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile int8_t * f1;
+   int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int32_t g_10 = 0x1F13B01DL;
+static int64_t g_19 = 0L;
