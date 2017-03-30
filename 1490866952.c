5160bd75ea5005e1d8e13a2efad43bb4b12b7f0e
saisi <saisi@users.noreply.github.com>
2017-03-30 09:42:33 +0000

    1490866952 amygdalaceous

---
+++ 1490866952.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3845137547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   volatile int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 4294967295UL;
+static const int32_t g_12 = (-1L);
+static uint8_t g_41 = 8UL;
+static int64_t g_69 = 0x0032DC71837DEF5FLL;
