d65efa34891a34ccd17b27905fa9bf1d147c6e0f
saisi <saisi@users.noreply.github.com>
2017-03-31 01:43:53 +0000

    1490924632 altimetrical

---
+++ 1490924632.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4259112345
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xDF37D45DL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[1] = {0x2887CF2EL};
+static int32_t g_7 = 0x0F5872D9L;
+static uint8_t g_44 = 0UL;
+static int64_t g_78 = (-6L);
+static int16_t g_80 = 2L;
+static int32_t g_84 = 0L;
+static uint32_t g_90 = 0xF4439D3FL;
