2608ce7a065e4a20c9a2ac9fee8de79cb254be2f
saisi <saisi@users.noreply.github.com>
2017-03-31 09:44:35 +0000

    1490953475 Appalachian

---
+++ 1490953475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1376795659
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   volatile int64_t  f1;
+   int16_t  f2;
+   const unsigned f3 : 14;
+   volatile int8_t * f4;
+};
+
+union U1 {
+   volatile int32_t  f0;
