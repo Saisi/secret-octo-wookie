2da38c40a5d7ecd96148023eaea0c192faa7d773
saisi <saisi@users.noreply.github.com>
2017-03-31 17:15:23 +0000

    1490980522 Astrabacus

---
+++ 1490980522.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3476187659
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_17 = 0x0BL;
+static uint8_t g_21 = 246UL;
+static int32_t g_23 = 8L;
+static int32_t * volatile g_22 = &g_23;/* VOLATILE GLOBAL g_22 */
+static const int32_t *g_28 = (void*)0;
+static int32_t g_45 = 1L;
+static uint8_t g_47 = 4UL;
+static volatile uint8_t * const  volatile *g_63 = (void*)0;
+static int16_t g_65 = 0x576DL;
