1e68ba7c1a6c3e31e19ca099daaf8452a6106667
saisi <saisi@users.noreply.github.com>
2017-03-31 01:13:50 +0000

    1490922830 asyzygetic

---
+++ 1490922830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3104322404
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   int64_t  f0;
+   const uint32_t  f1;
+   const volatile uint16_t  f2;
