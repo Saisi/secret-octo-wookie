7bb3865981faa566e2452afa667286827cb398da
saisi <saisi@users.noreply.github.com>
2017-03-31 07:14:24 +0000

    1490944463 acetylator

---
+++ 1490944463.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2408910408
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8] = {(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L)};
+static const int32_t *g_2 = &g_3[6];
+static int32_t g_5 = 0x01FC3EEEL;
+static uint16_t g_24 = 1UL;
+static int32_t **g_38 = (void*)0;
