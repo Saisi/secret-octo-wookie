cc5dd16189f13af2d167ee5411448ea8c1b31379
saisi <saisi@users.noreply.github.com>
2017-03-31 10:44:41 +0000

    1490957080 acropolises

---
+++ 1490957080.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2842540827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 12;
+   uint16_t  f1;
+   uint16_t  f2;
+   volatile int32_t  f3;
+   volatile int32_t  f4;
+   volatile int16_t  f5;
+   int64_t  f6;
+};
+
