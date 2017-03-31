83b5c50f5d21dee74c9770db8e86d96947abd39e
saisi <saisi@users.noreply.github.com>
2017-03-31 18:15:28 +0000

    1490984127 Arundo

---
+++ 1490984127.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2057129214
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 13;
+   int32_t  f1;
+   int64_t  f2;
+   uint64_t  f3;
+};
+#pragma pack(pop)
+
