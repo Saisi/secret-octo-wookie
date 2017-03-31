a9f36f6a3003711b9966f0d5a2a1e83d698f2909
saisi <saisi@users.noreply.github.com>
2017-03-31 16:45:20 +0000

    1490978720 arquebus

---
+++ 1490978720.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3025764763
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_20[5][4] = {{0x52835357L,0xE65251C6L,1L,1L},{2L,2L,0x52835357L,1L},{(-1L),0xE65251C6L,(-1L),0x52835357L},{(-1L),0x52835357L,0x52835357L,(-1L)},{2L,0x52835357L,1L,0x52835357L}};
+static uint32_t g_22 = 0xFDB7B241L;
+static int32_t g_68 = 0xEFCE06DFL;
+static int32_t g_74 = 0xE5A1AE28L;
+static int16_t g_80 = 0L;
+static int32_t *g_85 = &g_74;
+static int32_t * volatile *g_84[5] = {&g_85,&g_85,&g_85,&g_85,&g_85};
+static int8_t g_120[4] = {3L,3L,3L,3L};
+static uint16_t g_123[8] = {65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL,65535UL};
