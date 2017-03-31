c6d46f2823efd8eb28adf7bfba8b71399c1f936c
saisi <saisi@users.noreply.github.com>
2017-03-31 17:45:25 +0000

    1490982325 anaconda

---
+++ 1490982325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      620134226
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int32_t g_6[4] = {0xA772304BL,0xA772304BL,0xA772304BL,0xA772304BL};
+static int32_t g_8 = 0x2FF40FC0L;
+static int32_t * volatile g_7 = &g_8;/* VOLATILE GLOBAL g_7 */
+static uint32_t g_50[4][4] = {{0x13BCF59FL,0x13BCF59FL,4294967293UL,0x13BCF59FL},{0x13BCF59FL,1UL,1UL,0x13BCF59FL},{1UL,0x13BCF59FL,1UL,1UL},{0x13BCF59FL,0x13BCF59FL,4294967293UL,0x13BCF59FL}};
+static int8_t g_60 = 0L;
+static int64_t g_67 = 8L;
+static int16_t g_72 = 0x85DAL;
+static int32_t *g_77 = &g_6[2];
