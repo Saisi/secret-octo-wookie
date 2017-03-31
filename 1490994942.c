4a3065751c62706534cc7259c45ae5cee6df1b32
saisi <saisi@users.noreply.github.com>
2017-03-31 21:15:42 +0000

    1490994942 affrighting

---
+++ 1490994942.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1961491883
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-8L);
+static volatile uint64_t g_6[5] = {0xD672A8358BD4DFBDLL,0xD672A8358BD4DFBDLL,0xD672A8358BD4DFBDLL,0xD672A8358BD4DFBDLL,0xD672A8358BD4DFBDLL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
