e693259be33a54de8befc81bbf93af05fca92d14
saisi <saisi@users.noreply.github.com>
2017-03-31 12:14:48 +0000

    1490962488 agitant

---
+++ 1490962488.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2109753131
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[1][3][3] = {{{0x417BE187L,5L,0x417BE187L},{0x417BE187L,5L,0x417BE187L},{0x417BE187L,5L,0x417BE187L}}};
+static uint32_t g_10[5] = {0x64C7B7F6L,0x64C7B7F6L,0x64C7B7F6L,0x64C7B7F6L,0x64C7B7F6L};
+static int32_t g_40 = 0xEE3199F2L;
+static uint16_t g_53[3][2][1] = {{{0UL},{0x6BCEL}},{{0UL},{0UL}},{{0x6BCEL},{0UL}}};
+static uint32_t g_66 = 0x8833E6FCL;
+static int32_t g_68 = (-10L);
+static uint8_t g_71 = 2UL;
+static int32_t *g_76 = &g_40;
