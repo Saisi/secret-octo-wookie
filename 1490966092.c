e40f943b9a99318ee7cacd8fa72b15c754d0bc72
saisi <saisi@users.noreply.github.com>
2017-03-31 13:14:52 +0000

    1490966092 azafrin

---
+++ 1490966092.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2052301021
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x37BC7528L;
+static int32_t g_7 = 0x6FF55E00L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
