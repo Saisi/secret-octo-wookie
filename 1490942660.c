443717bc69249d362230b5eb617f8f35a35c4ee6
saisi <saisi@users.noreply.github.com>
2017-03-31 06:44:21 +0000

    1490942660 Achaia

---
+++ 1490942660.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3169680936
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   volatile uint64_t  f1;
+   const int8_t  f2;
+   signed f3 : 13;
+   int16_t  f4;
+};
+
+union U1 {
+   uint32_t  f0;
