2b249c45412b2eddaa92a7e97e11aeeb7734df4a
saisi <saisi@users.noreply.github.com>
2017-03-31 00:13:45 +0000

    1490919225 agonistics

---
+++ 1490919225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2063569755
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_32 = 0x7BL;
+static int32_t g_34 = 0xA70051EFL;
+static int16_t g_49 = 9L;
+static uint64_t g_58 = 5UL;
+static int32_t *g_60 = &g_34;
+static int32_t ** const  volatile g_59 = &g_60;/* VOLATILE GLOBAL g_59 */
+static uint32_t g_74 = 0x77628D16L;
+static int16_t *g_77[4] = {&g_49,&g_49,&g_49,&g_49};
+static uint32_t g_79 = 3UL;
