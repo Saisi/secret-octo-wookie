6125e767eb0ca6967094b7cb5da7f97619ef68e5
saisi <saisi@users.noreply.github.com>
2017-04-01 11:16:56 +0000

    1491045416 armours

---
+++ 1491045416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      957358923
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   signed f1 : 11;
+   unsigned f2 : 2;
+   signed f3 : 5;
+   unsigned f4 : 2;
+   unsigned f5 : 19;
+   const volatile unsigned f6 : 6;
+   signed f7 : 1;
+   const unsigned : 0;
