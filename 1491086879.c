83748b1b39d421a83da917dcccc2bf92249067b7
saisi <saisi@users.noreply.github.com>
2017-04-01 22:48:00 +0000

    1491086879 Anacletus

---
+++ 1491086879.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2020832994
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_7 = 0x3152C58FB770A77CLL;
+static int8_t g_9[8][3] = {{(-6L),0xFAL,0xFAL},{0L,7L,0L},{(-6L),(-6L),0xFAL},{0xEEL,7L,0xEEL},{(-6L),0xFAL,0xFAL},{0L,7L,0L},{(-6L),(-6L),0xFAL},{0xEEL,7L,0xEEL}};
+static int32_t g_11 = 0xFF3DE036L;
+static int8_t g_12 = 3L;
+static volatile int32_t g_15 = 1L;/* VOLATILE GLOBAL g_15 */
+static const volatile int32_t * volatile g_20 = &g_15;/* VOLATILE GLOBAL g_20 */
+static const volatile int32_t * volatile * volatile g_19[2] = {&g_20,&g_20};
+static const volatile int32_t * volatile * volatile g_21 = &g_20;/* VOLATILE GLOBAL g_21 */
+static int32_t *g_64[6][4][8] = {{{&g_11,&g_11,(void*)0,(void*)0,(void*)0,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11,(void*)0},{(void*)0,(void*)0,&g_11,(void*)0,&g_11,&g_11,(void*)0,(void*)0},{&g_11,(void*)0,&g_11,&g_11,(void*)0,&g_11,(void*)0,&g_11}},{{&g_11,(void*)0,(void*)0,&g_11,(void*)0,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}},{{(void*)0,&g_11,&g_11,(void*)0,&g_11,&g_11,&g_11,(void*)0},{(void*)0,&g_11,(void*)0,&g_11,(void*)0,&g_11,(void*)0,&g_11},{&g_11,&g_11,(void*)0,(void*)0,(void*)0,&g_11,&g_11,(void*)0},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}},{{&g_11,&g_11,(void*)0,&g_11,&g_11,&g_11,(void*)0,(void*)0},{&g_11,&g_11,(void*)0,(void*)0,&g_11,&g_11,&g_11,(void*)0},{&g_11,(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{(void*)0,(void*)0,&g_11,&g_11,(void*)0,&g_11,&g_11,(void*)0}},{{(void*)0,&g_11,&g_11,(void*)0,&g_11,&g_11,(void*)0,&g_11},{&g_11,(void*)0,&g_11,&g_11,&g_11,(void*)0,&g_11,(void*)0},{&g_11,(void*)0,(void*)0,(void*)0,&g_11,&g_11,(void*)0,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,(void*)0}},{{&g_11,(void*)0,&g_11,&g_11,(void*)0,&g_11,(void*)0,&g_11},{&g_11,(void*)0,(void*)0,&g_11,(void*)0,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0}}};
