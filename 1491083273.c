b360dff46ec50a6b4ce62ed2d00a45afd3401b9f
saisi <saisi@users.noreply.github.com>
2017-04-01 21:47:55 +0000

    1491083273 Arethuse

---
+++ 1491083273.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1527566336
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 14;
+   unsigned f1 : 11;
+   signed f2 : 1;
+   const signed f3 : 9;
+   unsigned f4 : 20;
+   const signed f5 : 27;
+   unsigned f6 : 13;
+};
+
