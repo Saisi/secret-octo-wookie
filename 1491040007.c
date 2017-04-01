a63c17a2c769f9a1aff66c4172cac777608dadfd
saisi <saisi@users.noreply.github.com>
2017-04-01 09:46:48 +0000

    1491040007 Balkar

---
+++ 1491040007.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3457663982
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile uint8_t  f1;
+   unsigned f2 : 14;
+   unsigned f3 : 24;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
