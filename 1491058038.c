5e0124ed469811473b664bcd48dae8adf538e219
saisi <saisi@users.noreply.github.com>
2017-04-01 14:47:18 +0000

    1491058038 anticline

---
+++ 1491058038.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      71609826
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 31;
+   const signed f1 : 24;
+   const volatile signed f2 : 26;
+   signed f3 : 30;
+   volatile signed f4 : 21;
+   const unsigned f5 : 24;
+};
