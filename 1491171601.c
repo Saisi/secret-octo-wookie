0d0bcd6e7e99a86fa179a735f6bb4088094ec337
saisi <saisi@users.noreply.github.com>
2017-04-02 22:20:01 +0000

    1491171601 amanita

---
+++ 1491171601.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1672611139
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[1][8] = {{1L,0xCB363F9EL,1L,1L,0xCB363F9EL,1L,1L,0xCB363F9EL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
