bc31040c2a756ab677ce9e8e8f1c4a325237a694
saisi <saisi@users.noreply.github.com>
2017-04-02 19:49:47 +0000

    1491162586 Argas

---
+++ 1491162586.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      62245429
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 25;
+   unsigned f1 : 12;
+   const signed f2 : 31;
+   unsigned f3 : 14;
+   volatile int16_t  f4;
+   volatile signed f5 : 25;
+   int32_t  f6;
