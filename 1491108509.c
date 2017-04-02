f9d9413b2b5f6618b1b3be622289aeba7b72f974
saisi <saisi@users.noreply.github.com>
2017-04-02 04:48:30 +0000

    1491108509 anthracitic

---
+++ 1491108509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1039097460
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 8;
+   volatile unsigned f1 : 29;
+   volatile unsigned f2 : 11;
+   signed f3 : 11;
+   signed f4 : 14;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
