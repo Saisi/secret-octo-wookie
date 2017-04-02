1ed02316266f5d80a0ba394190a29b67407952a5
saisi <saisi@users.noreply.github.com>
2017-04-02 05:18:32 +0000

    1491110312 amphitokal

---
+++ 1491110312.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1479206194
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 7;
+   const unsigned : 0;
+   const int64_t  f1;
+   signed f2 : 10;
+   unsigned f3 : 7;
+   const volatile signed f4 : 23;
+};
