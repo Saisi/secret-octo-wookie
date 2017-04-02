dc2f2322ba79ce03e37d1938f2cebbc8d8458804
saisi <saisi@users.noreply.github.com>
2017-04-02 03:18:22 +0000

    1491103102 agalma

---
+++ 1491103102.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2041579348
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   unsigned f1 : 5;
+   volatile unsigned f2 : 5;
+   unsigned f3 : 19;
+   unsigned f4 : 12;
+   uint8_t  f5;
+   const volatile unsigned f6 : 14;
+};
+
