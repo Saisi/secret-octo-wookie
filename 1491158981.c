10da0dd401022cdb43c7314d536ca140bca46b74
saisi <saisi@users.noreply.github.com>
2017-04-02 18:49:42 +0000

    1491158981 abyssolith

---
+++ 1491158981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2316703770
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+union U1 {
+   signed f0 : 12;
+   int16_t  f1;
+   int8_t * f2;
+};
+
