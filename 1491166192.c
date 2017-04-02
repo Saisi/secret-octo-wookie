1a9f32b166af554a65a8dfd86c9dfc1ec3921c80
saisi <saisi@users.noreply.github.com>
2017-04-02 20:49:53 +0000

    1491166192 Agacles

---
+++ 1491166192.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3822565737
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_15 = 0x6BL;
+static uint32_t g_20 = 3UL;
+static int32_t g_24 = 1L;
+static volatile int32_t g_29 = 0xEE61780EL;/* VOLATILE GLOBAL g_29 */
+static volatile int32_t *g_28 = &g_29;
+static int8_t g_31 = 0xDCL;
+static int32_t g_80[7] = {(-3L),(-3L),(-3L),(-3L),(-3L),(-3L),(-3L)};
+static int32_t *g_79 = &g_80[2];
+static int16_t g_104 = (-4L);
