ded0f7e63e59535df7d565b0511aff545f0927cc
saisi <saisi@users.noreply.github.com>
2017-04-02 09:48:55 +0000

    1491126535 anthropogeography

---
+++ 1491126535.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4176202724
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S1 {
+   signed f0 : 16;
+   signed f1 : 17;
+   unsigned f2 : 6;
+   signed f3 : 31;
+   unsigned f4 : 11;
+   const int8_t  f5;
+   volatile signed f6 : 2;
+   volatile int8_t  f7;
+   signed f8 : 16;
