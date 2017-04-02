77fd351ce3eb86c2b8f551fb9ec2458a078c9dd7
saisi <saisi@users.noreply.github.com>
2017-04-02 00:48:10 +0000

    1491094090 antaphroditic

---
+++ 1491094090.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3767637501
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   const uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-5L);
+static volatile int64_t g_6 = 0x8067E307B5CA9869LL;/* VOLATILE GLOBAL g_6 */
+static int8_t g_7 = 0x85L;
+static uint32_t g_8 = 0x51CFD1F2L;
