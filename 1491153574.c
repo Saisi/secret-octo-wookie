9174040f606e439227e022d0a75288bfa0101998
saisi <saisi@users.noreply.github.com>
2017-04-02 17:19:35 +0000

    1491153574 Abkhasian

---
+++ 1491153574.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      561447021
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   volatile unsigned f1 : 24;
+   unsigned f2 : 27;
+   const signed f3 : 26;
+   signed f4 : 3;
+   volatile signed f5 : 16;
+};
