a5555dd5bba40199fd64bc7a66d7c583f68c2bd3
saisi <saisi@users.noreply.github.com>
2017-04-02 15:19:25 +0000

    1491146365 adjacency

---
+++ 1491146365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2925247458
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[3][3] = {{0x58D32FFAL,0x58D32FFAL,0x58D32FFAL},{(-6L),0xD3B3152CL,(-6L)},{0x58D32FFAL,0x58D32FFAL,0x58D32FFAL}};
+static int32_t g_4 = (-9L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
