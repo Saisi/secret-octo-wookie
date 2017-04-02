74fc49128ecef85805213c05d24b2a9c412bbc44
saisi <saisi@users.noreply.github.com>
2017-04-02 08:18:45 +0000

    1491121125 antirecruiting

---
+++ 1491121125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3382100635
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint64_t  f1;
+   uint16_t  f2;
+   const signed f3 : 13;
+   uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_8 = {18446744073709551612UL};
