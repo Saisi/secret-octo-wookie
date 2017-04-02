dff618b41486a79f0a6a1fa9648b58fc842089e5
saisi <saisi@users.noreply.github.com>
2017-04-02 22:50:05 +0000

    1491173405 Arabele

---
+++ 1491173405.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1879441159
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   int8_t * f1;
+   uint8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 3UL;
+static int32_t g_7 = 0xE1B61BABL;
+static volatile uint32_t g_10 = 4294967291UL;/* VOLATILE GLOBAL g_10 */
