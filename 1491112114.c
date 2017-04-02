87b7aee8889da2e4c1c4fb0752ac9a49ccdeee5f
saisi <saisi@users.noreply.github.com>
2017-04-02 05:48:34 +0000

    1491112114 amphispermous

---
+++ 1491112114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2041868456
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xEEE2FDE2L;
+static const int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static const int32_t * volatile * volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+static int32_t g_10[6][10] = {{0x5B054DBBL,1L,(-1L),0x5440874BL,(-1L),1L,0x5B054DBBL,0L,0L,0L},{0x5B054DBBL,0L,4L,0L,0L,4L,0L,0x5B054DBBL,0L,0L},{4L,0L,0x5B054DBBL,0L,0L,0L,0x5B054DBBL,0L,4L,0L},{(-1L),1L,0x5B054DBBL,0L,0L,0L,0L,0x5B054DBBL,1L,(-1L)},{1L,0L,4L,0L,0x5F07C70AL,(-1L),0x5440874BL,0L,0L,1L},{0L,0xD373EE76L,4L,1L,0x5440874BL,0x5B054DBBL,0x5B054DBBL,0x5440874BL,1L,4L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
