d63b677ec09fa39f436d8de4e184f2017e06baf6
saisi <saisi@users.noreply.github.com>
2017-04-02 14:49:23 +0000

    1491144563 athletehood

---
+++ 1491144563.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3142614425
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-6L);
+static int32_t *g_5 = &g_4;
+static int32_t g_7 = 0xFB5D1CB6L;
+static uint16_t g_8 = 65526UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
