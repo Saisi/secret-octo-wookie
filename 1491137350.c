045673f07dac4ff39f42bb3d33abe67047bf5832
saisi <saisi@users.noreply.github.com>
2017-04-02 12:49:11 +0000

    1491137350 albarello

---
+++ 1491137350.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1718700117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[6][1] = {{1L},{0xA43D9532L},{1L},{0xA43D9532L},{1L},{0xA43D9532L}};
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 4L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
