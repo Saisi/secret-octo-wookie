4af266f334afefcda376833af26c5c4b9a877dbc
saisi <saisi@users.noreply.github.com>
2017-04-02 10:18:58 +0000

    1491128337 airest

---
+++ 1491128337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4026275721
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_33 = 0xA3FDA37BL;
+static int32_t g_68 = 0xA796FB6CL;
+static volatile int32_t g_70 = 0x34D56B86L;/* VOLATILE GLOBAL g_70 */
+static uint8_t g_71 = 0UL;
+static int64_t g_105[4] = {0xBB1179EF6EAEA544LL,0xBB1179EF6EAEA544LL,0xBB1179EF6EAEA544LL,0xBB1179EF6EAEA544LL};
+static const volatile int32_t g_110 = 0xC366FF5FL;/* VOLATILE GLOBAL g_110 */
+static int32_t *g_113 = &g_68;
+static volatile int32_t g_134 = 7L;/* VOLATILE GLOBAL g_134 */
+static const int8_t g_136[5][1][1] = {{{1L}},{{1L}},{{1L}},{{1L}},{{1L}}};
