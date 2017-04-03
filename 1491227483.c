1816fa3079678e08bf6f7945b1023c43752b4bb1
saisi <saisi@users.noreply.github.com>
2017-04-03 13:51:24 +0000

    1491227483 antiaphrodisiac

---
+++ 1491227483.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      96504421
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 17;
+   volatile unsigned f1 : 4;
+};
+#pragma pack(pop)
+
+struct S1 {
+   volatile uint8_t  f0;
