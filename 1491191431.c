81a7cef088ab2011ef8962904e11ba024225573d
saisi <saisi@users.noreply.github.com>
2017-04-03 03:50:32 +0000

    1491191431 antireaction

---
+++ 1491191431.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1905618725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   int64_t  f1;
+   signed f2 : 24;
+   const volatile signed f3 : 26;
+   volatile int8_t  f4;
+   const int64_t  f5;
+   const volatile signed f6 : 23;
