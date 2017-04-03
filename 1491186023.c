fb8a62a7b321e834ea9d646436e298a81a294002
saisi <saisi@users.noreply.github.com>
2017-04-03 02:20:24 +0000

    1491186023 antinaturally

---
+++ 1491186023.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2311254885
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   unsigned f1 : 4;
+   signed f2 : 23;
+   const volatile signed f3 : 6;
+   signed f4 : 31;
+   unsigned f5 : 28;
+   signed f6 : 11;
+   const volatile unsigned f7 : 18;
+   volatile unsigned f8 : 13;
