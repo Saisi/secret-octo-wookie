30df89339d4d2ab3c3592940b04f58f660a762ea
saisi <saisi@users.noreply.github.com>
2017-04-03 21:52:06 +0000

    1491256325 ballate

---
+++ 1491256325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3713490787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 16;
+   int32_t  f1;
+   int8_t * f2;
+   int8_t * f3;
+   uint32_t  f4;
+};
+
+union U1 {
+   const uint64_t  f0;
