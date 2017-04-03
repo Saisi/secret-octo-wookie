b59fc1ec153ccf59c5c9c5b013d2005b09226cf9
saisi <saisi@users.noreply.github.com>
2017-04-03 04:20:34 +0000

    1491193234 Ampelis

---
+++ 1491193234.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2678839428
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 16;
+   volatile unsigned f1 : 5;
+   const uint32_t  f2;
+   volatile unsigned f3 : 11;
+   const volatile unsigned f4 : 4;
+   volatile unsigned : 0;
+   const volatile unsigned f5 : 1;
+};
+
