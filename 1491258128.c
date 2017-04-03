21ae838904e7cb478cbf45a444acb8709fc7530a
saisi <saisi@users.noreply.github.com>
2017-04-03 22:22:08 +0000

    1491258128 amylodyspepsia

---
+++ 1491258128.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4143394940
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2[4][10] = {{65532UL,0UL,65532UL,0xA39EL,0UL,0x3B3FL,0x3B3FL,0UL,0xA39EL,65532UL},{0xF3C3L,0xF3C3L,65535UL,0UL,1UL,65535UL,1UL,0UL,65535UL,0xF3C3L},{1UL,0x3B3FL,65532UL,1UL,0xA39EL,0xA39EL,1UL,65532UL,0x3B3FL,1UL},{65532UL,0xF3C3L,0x3B3FL,0xA39EL,0xF3C3L,0xA39EL,0x3B3FL,0xF3C3L,65532UL,65532UL}};
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xB9876427L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-1L);
+static volatile struct S0 g_24 = {0x058EL};/* VOLATILE GLOBAL g_24 */
