236614ad2c9743de8cc357cd6308322edd80bf3a
saisi <saisi@users.noreply.github.com>
2017-04-03 09:21:00 +0000

    1491211258 archdefender

---
+++ 1491211258.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3305288579
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+};
+
+struct S1 {
+   volatile signed f0 : 30;
+   volatile unsigned f1 : 17;
+   signed f2 : 30;
+   volatile unsigned f3 : 31;
+   volatile signed f4 : 2;
