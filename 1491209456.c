5fbdae729f5d4abd09e3a17064a3a5a645f0e76c
saisi <saisi@users.noreply.github.com>
2017-04-03 08:50:56 +0000

    1491209456 autoanalytic

---
+++ 1491209456.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1749942067
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 24;
+   int8_t  f1;
+   volatile uint32_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+   const unsigned f1 : 7;
+   int32_t  f2;
