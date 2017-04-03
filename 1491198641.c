d7d840e53731df9438ef201a19ce9ef7bfba7c55
saisi <saisi@users.noreply.github.com>
2017-04-03 05:50:42 +0000

    1491198641 alima

---
+++ 1491198641.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1389471083
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0x00025260L;/* VOLATILE GLOBAL g_2 */
+static uint32_t g_27 = 0x62C5D177L;
+static union U0 g_50 = {5L};/* VOLATILE GLOBAL g_50 */
+static union U0 g_51 = {0x492A4153L};/* VOLATILE GLOBAL g_51 */
