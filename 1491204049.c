44fcd00e28e04514442226d1263cd966e0c991fe
saisi <saisi@users.noreply.github.com>
2017-04-03 07:20:49 +0000

    1491204049 akela

---
+++ 1491204049.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      224629672
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-8L);
+static volatile int32_t g_12 = (-2L);/* VOLATILE GLOBAL g_12 */
+static int32_t g_13 = 0x8F9C25B0L;
+static int8_t g_14[5] = {0x33L,0x33L,0x33L,0x33L,0x33L};
+static volatile uint16_t g_15 = 0x61BEL;/* VOLATILE GLOBAL g_15 */
+static int8_t g_57 = 0xA4L;
+static int32_t g_62 = 0x0B252307L;
+static uint16_t g_103 = 65534UL;
+static volatile int8_t * volatile * volatile g_114 = (void*)0;/* VOLATILE GLOBAL g_114 */
