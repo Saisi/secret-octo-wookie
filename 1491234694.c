d4599355b226f1b243cfae45ff521457a6b0e8f6
saisi <saisi@users.noreply.github.com>
2017-04-03 15:51:35 +0000

    1491234694 aphyllose

---
+++ 1491234694.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1836330775
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   const signed f1 : 25;
+   volatile signed f2 : 24;
+   unsigned f3 : 3;
+   signed f4 : 8;
+   signed f5 : 1;
+   volatile unsigned f6 : 28;
+};
+
