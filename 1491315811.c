470aa23f2018ce4760583645d19ab40e52d0c55a
saisi <saisi@users.noreply.github.com>
2017-04-04 14:23:31 +0000

    1491315811 baiocchi

---
+++ 1491315811.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4036062631
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x1713B910L;
+static int16_t g_12[5][3] = {{1L,0x2515L,1L},{1L,0x2515L,1L},{1L,0x2515L,1L},{1L,0x2515L,1L},{1L,0x2515L,1L}};
+static volatile int32_t g_13 = 0xEBF4D39DL;/* VOLATILE GLOBAL g_13 */
+static volatile int8_t g_15 = 0xC7L;/* VOLATILE GLOBAL g_15 */
+static int8_t g_16[1][7][4] = {{{(-2L),0x0FL,0xD0L,0xD0L},{0xEAL,0xEAL,(-2L),0xD0L},{0L,0x0FL,0L,(-2L)},{0L,(-2L),(-2L),0L},{0xEAL,(-2L),0xD0L,(-2L)},{(-2L),0x0FL,0xD0L,0xD0L},{0xEAL,0xEAL,(-2L),0xD0L}}};
+static volatile uint16_t g_17[3] = {0x33EDL,0x33EDL,0x33EDL};
+static volatile uint8_t g_20 = 0x22L;/* VOLATILE GLOBAL g_20 */
+
+
