d8f9e84efc01c2a07a71d45113c83ca9033095c5
saisi <saisi@users.noreply.github.com>
2017-04-04 00:22:19 +0000

    1491265339 antidivine

---
+++ 1491265339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1788402799
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 18446744073709551613UL;
+static int16_t g_27 = (-10L);
+static volatile int16_t g_41 = 0xFAEAL;/* VOLATILE GLOBAL g_41 */
+static volatile int16_t *g_40 = &g_41;
