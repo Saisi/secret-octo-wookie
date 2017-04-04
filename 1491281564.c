e499f87e697cd7e47b2a6f419e73b235d4dcc1b6
saisi <saisi@users.noreply.github.com>
2017-04-04 04:52:44 +0000

    1491281564 acousmas

---
+++ 1491281564.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4161607966
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int8_t  f0;
+   uint64_t  f1;
+};
+
+struct S1 {
+   const uint64_t  f0;
+   volatile int8_t  f1;
+   int64_t  f2;
+   volatile int64_t  f3;
