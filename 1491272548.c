da9006f6128ef7e3b8717fc53098d040453dd52e
saisi <saisi@users.noreply.github.com>
2017-04-04 02:22:30 +0000

    1491272548 azumbre

---
+++ 1491272548.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      242437111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   uint8_t  f1;
+};
+
+union U1 {
+   const int8_t * f0;
+};
+
+union U2 {
