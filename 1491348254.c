c4af0a8c576d596f476be4bfe9121ebdb31a399b
saisi <saisi@users.noreply.github.com>
2017-04-04 23:24:15 +0000

    1491348254 apostolate

---
+++ 1491348254.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3412280437
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+};
+
+union U1 {
+   uint32_t  f0;
+   const uint16_t  f1;
+   const volatile int16_t  f2;
+   int32_t  f3;
+};
