ca042437e049c76ef2e49cfb9d3da7e2172f78f6
saisi <saisi@users.noreply.github.com>
2017-04-04 08:23:02 +0000

    1491294182 backscatters

---
+++ 1491294182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2576032389
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_11 = 0xAB27L;
+static int32_t g_13[4][10] = {{(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL,(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL},{(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL,(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL},{(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL,(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL},{(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL,(-1L),0xE58BFC4FL,(-1L),(-1L),0xE58BFC4FL}};
+static int32_t g_38[3] = {6L,6L,6L};
+static int8_t g_78 = (-2L);
+static int32_t g_82 = 1L;
+static int16_t g_85 = 0L;
+static int32_t ** volatile g_87 = (void*)0;/* VOLATILE GLOBAL g_87 */
+static uint16_t g_113 = 65535UL;
+static uint8_t g_124 = 1UL;
