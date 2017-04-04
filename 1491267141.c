937ac53fbf2d219b8c1f938b56a09eb8f3d63ecc
saisi <saisi@users.noreply.github.com>
2017-04-04 00:52:22 +0000

    1491267141 balneologist

---
+++ 1491267141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2257435865
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * const  volatile  f0;
+   uint16_t  f1;
+};
+
+union U1 {
+   uint16_t  f0;
+   volatile signed f1 : 14;
+};
+
