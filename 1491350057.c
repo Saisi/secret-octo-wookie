1c83110f8c3623cfff23eb0721f10f9323972b7b
saisi <saisi@users.noreply.github.com>
2017-04-04 23:54:17 +0000

    1491350057 arteriogenesis

---
+++ 1491350057.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      325335346
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   int8_t * volatile  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_7 = 247UL;
+static int32_t g_9[9] = {0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L,0xCB444811L};
+static int8_t g_28[7] = {0x69L,(-1L),0x69L,0x69L,(-1L),0x69L,0x69L};
+static int8_t *g_27 = &g_28[2];
