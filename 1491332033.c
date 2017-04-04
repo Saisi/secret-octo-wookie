057c62e889105be6ab4ea62357f612e5f06321c4
saisi <saisi@users.noreply.github.com>
2017-04-04 18:53:53 +0000

    1491332033 Allstate

---
+++ 1491332033.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2371670516
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 0xB7C7L;
+static int32_t g_9 = 0x0AA6E6F7L;
+static volatile uint64_t g_27 = 0xCB06A9BEBB6F2BA1LL;/* VOLATILE GLOBAL g_27 */
+static int32_t *g_34 = &g_9;
+static int32_t ** const g_33 = &g_34;
+static uint16_t g_76 = 0UL;
+static int64_t g_103[1] = {0L};
+static int64_t g_105 = 0x90B87A386FCCF6B0LL;
+static uint32_t g_107 = 1UL;
