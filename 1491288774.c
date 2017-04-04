e9c2be9001b7b2a186a53ea4ecfc0b31648c3ced
saisi <saisi@users.noreply.github.com>
2017-04-04 06:52:54 +0000

    1491288774 alloxans

---
+++ 1491288774.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3299150101
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 6L;
+static int8_t g_15 = 0xA2L;
+static volatile int8_t g_16 = 0xC1L;/* VOLATILE GLOBAL g_16 */
+static volatile int32_t g_17 = 0L;/* VOLATILE GLOBAL g_17 */
+static uint16_t g_18 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
