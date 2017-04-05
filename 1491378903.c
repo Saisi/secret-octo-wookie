ef58d3dfed3fc4fc67642cc82b4be56c5f430fe7
saisi <saisi@users.noreply.github.com>
2017-04-05 07:55:03 +0000

    1491378903 aviatory

---
+++ 1491378903.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      214118803
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_3[10] = {0x79L,0x79L,0x79L,0x79L,0x79L,0x79L,0x79L,0x79L,0x79L,0x79L};
+static int32_t g_5 = 0L;
+static uint8_t g_50 = 255UL;
+static uint8_t *g_49 = &g_50;
