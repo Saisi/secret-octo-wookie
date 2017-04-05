b382c3d53bfc04974abf6ead0719735bf7d5544f
saisi <saisi@users.noreply.github.com>
2017-04-05 04:24:42 +0000

    1491366282 anticeremonial

---
+++ 1491366282.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3780561505
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int8_t  f0;
+   signed f1 : 4;
+   unsigned f2 : 10;
+   signed f3 : 6;
+   signed f4 : 21;
+   signed f5 : 15;
+};
