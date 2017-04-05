e7fac4cb23c529348ea61b11056bac6b38889079
saisi <saisi@users.noreply.github.com>
2017-04-05 08:25:07 +0000

    1491380706 adaptabilities

---
+++ 1491380706.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3746945018
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[9][2] = {{0L,0xB6748F69L},{0x1CECB457L,0L},{(-1L),(-1L)},{(-1L),0L},{0x1CECB457L,0xB6748F69L},{0L,0xB6748F69L},{0x1CECB457L,0L},{(-1L),(-1L)},{(-1L),0L}};
+static int32_t g_6 = 0xB9DD952EL;
+static int8_t g_49 = 0xAAL;
+static int16_t g_74 = 0L;
+static int8_t *g_77 = (void*)0;
