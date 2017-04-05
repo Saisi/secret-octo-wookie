fcf33f9448503541c751befdbc1781546bdff6a1
saisi <saisi@users.noreply.github.com>
2017-04-05 00:54:23 +0000

    1491353662 arthrotropic

---
+++ 1491353662.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2859402951
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 2;
+   unsigned f1 : 17;
+   volatile unsigned f2 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xC29000D4L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-3L);
+static int32_t g_62 = 0x1FD29CC0L;
