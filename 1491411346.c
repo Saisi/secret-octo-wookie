02ee7c7b7baa049911fff8089438687c21696331
saisi <saisi@users.noreply.github.com>
2017-04-05 16:55:47 +0000

    1491411346 Annellata

---
+++ 1491411346.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1750794070
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   signed f1 : 10;
+   uint32_t  f2;
+   signed f3 : 12;
+   const volatile signed f4 : 1;
+   unsigned f5 : 9;
+   volatile uint32_t  f6;
