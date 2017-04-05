66710a1d8a5c00aa01186448d4befa6ec18a9ce7
saisi <saisi@users.noreply.github.com>
2017-04-05 10:25:17 +0000

    1491387916 annexer

---
+++ 1491387916.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3985230758
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S3 {
+   signed f0 : 24;
+   signed : 0;
+   unsigned f1 : 21;
+   const volatile signed f2 : 16;
+   unsigned f3 : 20;
+};
+#pragma pack(pop)
