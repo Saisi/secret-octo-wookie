142181b81622d82524ed0a667d0e15fa1021505a
saisi <saisi@users.noreply.github.com>
2017-04-05 06:24:52 +0000

    1491373491 allotropous

---
+++ 1491373491.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1595303827
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_6[6] = {0xBAL,0xBAL,0xBAL,0xBAL,0xBAL,0xBAL};
+static volatile int32_t g_11 = 1L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_12 = 0x1A0734F0L;
+static int32_t g_13 = 0x06887E08L;
+static int32_t g_78 = (-1L);
+static int8_t g_79 = 6L;
+static uint8_t g_83 = 0x14L;
+static int32_t g_96 = 9L;
+static uint16_t g_102 = 1UL;
