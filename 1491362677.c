41ffbca815497f7664c9c961403a23e383817849
saisi <saisi@users.noreply.github.com>
2017-04-05 03:24:38 +0000

    1491362677 antithetical

---
+++ 1491362677.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2031655991
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   volatile uint64_t  f1;
+   int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_25 = 4294967293UL;
+static int16_t g_31 = 0x10AFL;
+static int32_t g_51[3] = {0x865E1BFEL,0x865E1BFEL,0x865E1BFEL};
