a4d328413e789539814f973ed428f03ecbe15aec
saisi <saisi@users.noreply.github.com>
2017-04-05 13:55:33 +0000

    1491400533 airsick

---
+++ 1491400533.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3048269888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static int32_t g_5 = 0x100C4307L;
+static int32_t g_6 = 6L;
+static uint64_t g_59 = 0UL;
+static volatile uint64_t g_77 = 0x19C50ED6CA138DBELL;/* VOLATILE GLOBAL g_77 */
+static volatile uint64_t * volatile g_76 = &g_77;/* VOLATILE GLOBAL g_76 */
+static uint8_t g_101 = 0x8EL;
+static int8_t g_104 = 8L;
+static int32_t g_105 = 0x8108B99CL;
