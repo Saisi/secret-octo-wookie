5303475cca1705fa8928823a3b49b2b43ec2d759
saisi <saisi@users.noreply.github.com>
2017-04-05 22:27:06 +0000

    1491431226 antennary

---
+++ 1491431226.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      58313524
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint32_t g_6 = 3UL;/* VOLATILE GLOBAL g_6 */
+static uint64_t g_14 = 0xD7C1188E17B844A7LL;
+static uint64_t g_32 = 0xBEFA1E1C5A3D7F19LL;
+static uint32_t g_38 = 4294967295UL;
+static const uint64_t g_46 = 18446744073709551608UL;
+static uint32_t g_48 = 1UL;
+static uint32_t *g_47 = &g_48;
+static uint64_t g_64[1][1] = {{18446744073709551615UL}};
+static uint8_t g_70[8] = {255UL,255UL,0x8FL,255UL,255UL,0x8FL,255UL,255UL};
