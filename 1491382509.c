ffe9602e8f5112f72cf673d4eedb5cab9c7a9a42
saisi <saisi@users.noreply.github.com>
2017-04-05 08:55:09 +0000

    1491382509 antelopine

---
+++ 1491382509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      308676494
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 21;
+   unsigned f1 : 22;
+   const signed : 0;
+   signed f2 : 12;
+   const unsigned f3 : 11;
+   unsigned f4 : 22;
+};
