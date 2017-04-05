1cbc1570c205173ce90000376cabae1b420f78d9
saisi <saisi@users.noreply.github.com>
2017-04-05 02:24:31 +0000

    1491359071 adherences

---
+++ 1491359071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1362832930
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int16_t  f1;
+   const int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5020F89BL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x158D32FFL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
