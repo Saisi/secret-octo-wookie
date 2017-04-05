d75550535ef996c4f3aa1075f70b7d59ab19a590
saisi <saisi@users.noreply.github.com>
2017-04-05 01:24:26 +0000

    1491355465 analysation

---
+++ 1491355465.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1652963610
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 27;
+   signed f1 : 4;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_4 = (-1L);
