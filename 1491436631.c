974afcefcb01898b6849c5d0a51b1031c199581f
saisi <saisi@users.noreply.github.com>
2017-04-05 23:57:11 +0000

    1491436631 anabaptist

---
+++ 1491436631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      738870494
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 29;
+   volatile unsigned f1 : 30;
+   volatile unsigned f2 : 8;
+   int32_t  f3;
+   unsigned f4 : 21;
+   unsigned f5 : 13;
+   const unsigned f6 : 26;
