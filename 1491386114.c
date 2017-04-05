e2259158634d39166da24924d025c7958f96ea43
saisi <saisi@users.noreply.github.com>
2017-04-05 09:55:14 +0000

    1491386114 andromeda

---
+++ 1491386114.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2150646988
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   uint32_t  f1;
+   int32_t  f2;
+   int64_t  f3;
+   volatile uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-9L);
