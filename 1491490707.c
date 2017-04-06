6cefd0d5d2a29106ee2abd2272b3feaee62473ce
saisi <saisi@users.noreply.github.com>
2017-04-06 14:58:27 +0000

    1491490707 Banon

---
+++ 1491490707.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1298517499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint32_t  f1;
+   unsigned f2 : 3;
+   const volatile int64_t  f3;
+   const int32_t  f4;
+   uint64_t  f5;
+   volatile uint32_t  f6;
+};
+
