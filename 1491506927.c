b30dfff902c13c96fd8bc66eddc1d6dc7a29b68e
saisi <saisi@users.noreply.github.com>
2017-04-06 19:28:47 +0000

    1491506927 anglophobia

---
+++ 1491506927.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1059102237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13[1] = {0x7B5CA986L};
+static int32_t *g_12 = &g_13[0];
+static int32_t g_20[6] = {0xEB59008CL,0xEB59008CL,0xEB59008CL,0xEB59008CL,0xEB59008CL,0xEB59008CL};
+static int32_t * volatile g_19 = &g_20[3];/* VOLATILE GLOBAL g_19 */
+static uint64_t g_26 = 0x68CB667F5AEE0CD9LL;
+static uint8_t g_50 = 0x5AL;
+static volatile uint64_t g_52 = 4UL;/* VOLATILE GLOBAL g_52 */
+static volatile uint64_t g_53 = 2UL;/* VOLATILE GLOBAL g_53 */
+static volatile uint64_t *g_51[6] = {(void*)0,(void*)0,&g_52,(void*)0,(void*)0,&g_52};
