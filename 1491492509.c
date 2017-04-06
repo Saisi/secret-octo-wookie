d36cb46e9b296a3c86d8be1287ff071568eac428
saisi <saisi@users.noreply.github.com>
2017-04-06 15:28:30 +0000

    1491492509 antichymosin

---
+++ 1491492509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2715777185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   volatile int64_t  f1;
+   volatile int8_t  f2;
+   uint32_t  f3;
+   volatile signed f4 : 18;
+   unsigned f5 : 13;
+   volatile uint8_t  f6;
