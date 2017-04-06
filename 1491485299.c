aca5077678d2fa0ee16ead80aa7702fdba7f2d7a
saisi <saisi@users.noreply.github.com>
2017-04-06 13:28:20 +0000

    1491485299 armchaired

---
+++ 1491485299.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2814165686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 9;
+   const unsigned f1 : 26;
+   int32_t  f2;
+   volatile uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xA654FDDDL;
+static uint64_t g_7[7] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL};
