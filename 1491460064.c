53a1946708f5561f8d0d232001503fa50ceceb1c
saisi <saisi@users.noreply.github.com>
2017-04-06 06:27:45 +0000

    1491460064 adsorbable

---
+++ 1491460064.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3063582543
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+   signed f1 : 5;
+   const volatile unsigned f2 : 20;
+   unsigned f3 : 15;
+   signed f4 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_15 = 0xB9L;
