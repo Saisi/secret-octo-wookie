b47570282abe1bcdf51c6a15ef3d397ac293312f
saisi <saisi@users.noreply.github.com>
2017-04-06 00:27:14 +0000

    1491438433 bairnliness

---
+++ 1491438433.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1718860656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[4] = {0xA115252CL,0xA115252CL,0xA115252CL,0xA115252CL};
+static int32_t g_3 = 0x3C69AED2L;
+static volatile int32_t g_4 = 0x263126FFL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5[5] = {0x1A9565E0L,0x1A9565E0L,0x1A9565E0L,0x1A9565E0L,0x1A9565E0L};
+static int32_t g_6[4] = {(-1L),(-1L),(-1L),(-1L)};
