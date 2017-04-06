0c7d6a0b6aa7472e4b387fc5782b5696f85966ed
saisi <saisi@users.noreply.github.com>
2017-04-06 20:28:52 +0000

    1491510532 amphanthium

---
+++ 1491510532.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2793065507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 16;
+   int8_t  f1;
+   signed f2 : 23;
+   unsigned f3 : 31;
+   volatile uint16_t  f4;
+   signed f5 : 18;
+   unsigned f6 : 14;
