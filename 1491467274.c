486abeff62f7f19e9903dc122e91b577b850b793
saisi <saisi@users.noreply.github.com>
2017-04-06 08:27:55 +0000

    1491467274 anoxemias

---
+++ 1491467274.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1693470654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xAE355683L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-10L);
+static int16_t g_40[8] = {0x711AL,(-1L),0x711AL,0x711AL,(-1L),0x711AL,0x711AL,(-1L)};
+static uint64_t g_91 = 18446744073709551607UL;
+static int32_t g_94 = 0x9F19B52DL;
+static int32_t *g_93 = &g_94;
+static const int32_t g_102 = 0x59328DFEL;
+static uint32_t g_111 = 0x192C249DL;
+static uint64_t g_121 = 0x06A33997603614A4LL;
