72ed0dc77e02a6f3e418a20ea0d24db2f5674537
saisi <saisi@users.noreply.github.com>
2017-04-06 16:28:35 +0000

    1491496115 aceologic

---
+++ 1491496115.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1294440741
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x5362DAABL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x21A35CA4L;
+static volatile uint8_t g_7[1] = {0UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
