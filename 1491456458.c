63a8d975747d890b06b962e738d115543ebed194
saisi <saisi@users.noreply.github.com>
2017-04-06 05:27:39 +0000

    1491456458 Barbarian

---
+++ 1491456458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2921807906
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   volatile int32_t  f1;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint32_t  f0;
+   const struct S0  f1;
