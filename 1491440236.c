baab589c917f14a8df6554d5692d3dd578a99c61
saisi <saisi@users.noreply.github.com>
2017-04-06 00:57:17 +0000

    1491440236 antinucleon

---
+++ 1491440236.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2326115848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   const uint64_t  f1;
+   const signed f2 : 10;
+   const uint64_t  f3;
+   volatile int8_t  f4;
+};
+#pragma pack(pop)
