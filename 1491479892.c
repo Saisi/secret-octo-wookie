e52ac0718ea9fcb6cfaeeabd33011806c70b2b44
saisi <saisi@users.noreply.github.com>
2017-04-06 11:58:12 +0000

    1491479892 anorthitite

---
+++ 1491479892.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4241562777
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 11;
+   volatile unsigned f1 : 22;
+};
+
+struct S1 {
+   uint16_t  f0;
+   const volatile unsigned f1 : 14;
+   signed f2 : 25;
+   const signed f3 : 25;
