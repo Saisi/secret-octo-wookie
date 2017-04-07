da0b0ba7520df05f06aca61dabbdd66336725b82
saisi <saisi@users.noreply.github.com>
2017-04-07 11:00:13 +0000

    1491562812 antitemperance

---
+++ 1491562812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1794137012
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   int32_t  f1;
+   uint16_t  f2;
+   const unsigned f3 : 15;
+   volatile uint8_t  f4;
+   uint32_t  f5;
+   uint64_t  f6;
+};
+
