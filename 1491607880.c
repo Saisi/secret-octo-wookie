5379a91e8cd3945a65033d851e061c83a8897486
saisi <saisi@users.noreply.github.com>
2017-04-07 23:31:21 +0000

    1491607880 Alagoz

---
+++ 1491607880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3966886570
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   int8_t * volatile  f1;
+   volatile int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_21 = 4UL;/* VOLATILE GLOBAL g_21 */
+static const uint16_t g_25[8][7] = {{65531UL,0x8009L,0x6488L,0xBDA1L,1UL,65531UL,0xD9E6L},{0UL,0x59ECL,65531UL,65534UL,0xD9E6L,65534UL,65531UL},{0xB560L,0xB560L,65531UL,1UL,0x7E81L,0xE707L,5UL},{65531UL,7UL,0x6488L,0x7E81L,0xB560L,0UL,0x59ECL},{0x8422L,1UL,65534UL,7UL,0x7E81L,0x3488L,1UL},{65531UL,0xD9E6L,1UL,1UL,0xD9E6L,65531UL,1UL},{0x3488L,0x7E81L,7UL,65534UL,65531UL,1UL,0x3488L},{7UL,65531UL,0UL,0x9733L,0x8422L,0x7E81L,0x59ECL}};
+static int32_t g_38 = 0L;
