cb09c38066c58fb6d427bf096e7390d85174c734
saisi <saisi@users.noreply.github.com>
2017-04-07 15:00:32 +0000

    1491577232 arrosion

---
+++ 1491577232.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3626287665
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 8UL;
+static int32_t g_8[7] = {0xF01BA028L,5L,0xF01BA028L,0xF01BA028L,5L,0xF01BA028L,0xF01BA028L};
+static int32_t g_10[3] = {0x0274952FL,0x0274952FL,0x0274952FL};
+static uint16_t g_29 = 0x2762L;
+static uint8_t g_33 = 0x81L;
+static const uint16_t g_90 = 5UL;
+static const uint16_t *g_89 = &g_90;
+static int16_t g_93 = 2L;
+static int16_t g_95[4][6] = {{0x90B5L,0x90B5L,(-1L),0x90B5L,0x90B5L,(-1L)},{0x90B5L,0x90B5L,(-1L),0x90B5L,0x90B5L,(-1L)},{0x90B5L,0x90B5L,(-1L),0x90B5L,0x90B5L,(-1L)},{0x90B5L,0x90B5L,(-1L),0x90B5L,0x90B5L,(-1L)}};
