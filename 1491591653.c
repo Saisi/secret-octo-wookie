1943f828e5562fa532a5dc742f92b2865a0613e8
saisi <saisi@users.noreply.github.com>
2017-04-07 19:00:54 +0000

    1491591653 amelia

---
+++ 1491591653.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1081655639
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 10;
+   signed f1 : 25;
+   unsigned f2 : 24;
+   const signed f3 : 3;
+   volatile unsigned f4 : 8;
+   signed f5 : 1;
+   volatile unsigned f6 : 28;
+   volatile unsigned f7 : 31;
+   signed f8 : 6;
