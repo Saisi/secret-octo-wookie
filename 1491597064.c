632c1a96ddb7df649856ccfe5be37f804fb63d4e
saisi <saisi@users.noreply.github.com>
2017-04-07 20:31:05 +0000

    1491597064 antiparasitical

---
+++ 1491597064.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      224287016
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2 = 0xEA75L;
+static uint16_t g_47 = 0x1FA6L;
+static int32_t g_49 = 0xA4A104ACL;
+static uint32_t g_54 = 18446744073709551611UL;
+static int32_t *g_73 = &g_49;
+static int32_t ** volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static uint16_t *g_87 = &g_47;
+static uint16_t *g_88 = &g_47;
+static int64_t g_109[9] = {0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL,0x45A0AE89175669BALL};
