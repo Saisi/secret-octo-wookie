0521fe65228ab0bc4a5b96e7ddff9172871abbaa
saisi <saisi@users.noreply.github.com>
2017-04-07 01:29:18 +0000

    1491528557 alpestral

---
+++ 1491528557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2599487224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 10;
+   unsigned f1 : 23;
+   unsigned f2 : 29;
+};
+
+union U1 {
+   uint64_t  f0;
+   int16_t  f1;
+   uint32_t  f2;
