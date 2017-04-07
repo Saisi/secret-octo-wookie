33af0cc23205dbb9c2bde39cd6fb047afb926ff0
saisi <saisi@users.noreply.github.com>
2017-04-07 05:59:40 +0000

    1491544780 acontia

---
+++ 1491544780.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      970837147
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile int64_t  f0;
+   signed f1 : 17;
+   signed f2 : 3;
+   const uint64_t  f3;
+   const volatile signed f4 : 23;
+   unsigned f5 : 30;
+   signed f6 : 10;
