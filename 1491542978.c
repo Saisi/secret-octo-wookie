7be3ea0a11bcf0789641bed1db1a9dbc464b5888
saisi <saisi@users.noreply.github.com>
2017-04-07 05:29:38 +0000

    1491542978 archmonarchist

---
+++ 1491542978.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      129361767
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 25;
+   volatile unsigned f1 : 23;
+   const unsigned f2 : 13;
