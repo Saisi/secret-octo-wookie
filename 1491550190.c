dcf28f88a9c7949a886a5c29a634f21a7c0243d5
saisi <saisi@users.noreply.github.com>
2017-04-07 07:29:50 +0000

    1491550190 Algoman

---
+++ 1491550190.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      29975993
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   int64_t  f1;
+   int32_t  f2;
+   const volatile uint64_t  f3;
+   volatile uint8_t  f4;
+   int32_t  f5;
+};
+
+struct S1 {
