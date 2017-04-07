047e33f53b581aa2b347d1935b2c742aac09030a
saisi <saisi@users.noreply.github.com>
2017-04-07 15:30:35 +0000

    1491579034 baniwa

---
+++ 1491579034.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4145249242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 27;
+   const signed f1 : 4;
+   const volatile signed f2 : 22;
+   signed f3 : 10;
+   volatile unsigned f4 : 17;
+   const signed f5 : 4;
+   volatile unsigned f6 : 18;
