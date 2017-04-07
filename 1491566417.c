c14065b8e41089659b87142b0570702baa2a203d
saisi <saisi@users.noreply.github.com>
2017-04-07 12:00:18 +0000

    1491566417 Anglophiliac

---
+++ 1491566417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      36249165
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_8 = 0x35L;
+static uint32_t g_10 = 0UL;
+static uint32_t g_24 = 0UL;
+static const uint32_t *g_23 = &g_24;
+static uint64_t g_39 = 0xC39C9D9403132D68LL;
+static uint16_t g_43 = 0x6E14L;
+static uint16_t g_53 = 0x4A21L;
+static uint16_t * const g_52 = &g_53;
+static int8_t g_69 = (-1L);
