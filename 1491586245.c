1e49890043ede5029129315812b7a97ebd2dfc88
saisi <saisi@users.noreply.github.com>
2017-04-07 17:30:46 +0000

    1491586245 alacrities

---
+++ 1491586245.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2626638432
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+};
+
+struct S1 {
+   int32_t  f0;
+};
+
+#pragma pack(push)
+#pragma pack(1)
