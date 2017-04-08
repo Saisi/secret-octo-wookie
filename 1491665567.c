c924b89f156fff590298c607c5c6ad9c662b44aa
saisi <saisi@users.noreply.github.com>
2017-04-08 15:32:48 +0000

    1491665567 acetylize

---
+++ 1491665567.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      162769275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 28;
+   unsigned f1 : 3;
+   volatile unsigned f2 : 26;
+   volatile signed f3 : 29;
+   volatile signed f4 : 26;
+   volatile unsigned f5 : 11;
+   signed f6 : 30;
+};
+
