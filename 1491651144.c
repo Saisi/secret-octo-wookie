28639d2494175b07d539721a9600a0d83320439c
saisi <saisi@users.noreply.github.com>
2017-04-08 11:32:25 +0000

    1491651144 antlia

---
+++ 1491651144.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2636245646
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static uint8_t g_10[4] = {253UL,253UL,253UL,253UL};
+static uint8_t g_20 = 4UL;
+static uint8_t * const g_19 = &g_20;
+static int32_t *g_45 = &g_2;
+static int32_t ** volatile g_44 = &g_45;/* VOLATILE GLOBAL g_44 */
+static int16_t g_65 = 0L;
+static uint32_t g_98[2][5] = {{0UL,0UL,0UL,0UL,0UL},{0UL,0UL,0UL,0UL,0UL}};
+static int32_t g_100 = 0x6120E7A9L;
