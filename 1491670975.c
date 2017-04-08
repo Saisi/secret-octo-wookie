57f903363daf92cc89ad36bd3c0623e4e1570164
saisi <saisi@users.noreply.github.com>
2017-04-08 17:02:55 +0000

    1491670975 anthophyllitic

---
+++ 1491670975.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4096658943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 30;
+   const unsigned f1 : 29;
+   unsigned f2 : 12;
+   signed f3 : 7;
+   unsigned f4 : 28;
+   signed f5 : 5;
+   volatile int32_t  f6;
