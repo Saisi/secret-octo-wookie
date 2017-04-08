85c16452ce42c0f11e7654bb71a525ced0b13fb0
saisi <saisi@users.noreply.github.com>
2017-04-08 12:32:29 +0000

    1491654749 amathophobia

---
+++ 1491654749.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      97886796
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 20;
+   signed f1 : 25;
+   unsigned f2 : 23;
+   const volatile unsigned f3 : 7;
+   const unsigned f4 : 26;
+};
+#pragma pack(pop)
