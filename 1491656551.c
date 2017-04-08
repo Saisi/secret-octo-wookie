263d44d326e64a0763059d6ec3ddf1412357943b
saisi <saisi@users.noreply.github.com>
2017-04-08 13:02:34 +0000

    1491656551 antiquities

---
+++ 1491656551.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      867101239
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int8_t * volatile  f1;
+   unsigned f2 : 26;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6[8][8] = {{(-8L),1L,(-8L),(-8L),1L,(-8L),(-8L),1L},{1L,(-8L),(-8L),1L,(-8L),(-8L),1L,(-8L)},{1L,1L,(-5L),1L,1L,(-5L),1L,1L},{(-8L),1L,(-8L),(-8L),1L,(-8L),(-8L),1L},{1L,(-8L),(-8L),1L,(-8L),(-8L),1L,(-8L)},{1L,1L,(-5L),1L,1L,(-5L),(-8L),(-8L)},{(-5L),(-8L),(-5L),(-5L),(-8L),(-5L),(-5L),(-8L)},{(-8L),(-5L),(-5L),(-8L),(-5L),(-5L),(-8L),(-5L)}};
+static int8_t g_39[6] = {(-4L),(-4L),(-4L),(-4L),(-4L),(-4L)};
