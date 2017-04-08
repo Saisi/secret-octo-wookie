638a8ade75327adc7196ebb2ce01d9cbd6a7440c
saisi <saisi@users.noreply.github.com>
2017-04-08 06:01:52 +0000

    1491631311 aciculums

---
+++ 1491631311.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1319317121
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   const int8_t  f1;
+   uint32_t  f2;
+   uint16_t  f3;
+   uint32_t  f4;
+   int16_t  f5;
+   const int8_t  f6;
+   uint8_t  f7;
+};
