632555befbaaad5d2d048ba13cc799463f4f3745
saisi <saisi@users.noreply.github.com>
2017-04-08 07:31:59 +0000

    1491636719 ancestress

---
+++ 1491636719.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3620116595
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 7L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 8L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6[9] = {0xD7953644L,(-1L),0xD7953644L,0xD7953644L,(-1L),0xD7953644L,0xD7953644L,(-1L),0xD7953644L};
+static uint64_t g_19 = 0x5A60915BB002891BLL;
+static uint64_t g_35[5] = {0xDD3610CB4E3473EDLL,0xDD3610CB4E3473EDLL,0xDD3610CB4E3473EDLL,0xDD3610CB4E3473EDLL,0xDD3610CB4E3473EDLL};
+static uint64_t *g_34 = &g_35[1];
+static uint8_t g_39 = 0x8AL;
