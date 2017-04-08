26620ba8b7c942d933b6825de0cab2c58ea3cb03
saisi <saisi@users.noreply.github.com>
2017-04-08 17:32:59 +0000

    1491672777 Arabella

---
+++ 1491672777.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      482217945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 11;
+   unsigned f1 : 22;
+   const int64_t  f2;
+   signed f3 : 2;
+   volatile unsigned f4 : 1;
+   uint32_t  f5;
+};
+
+union U1 {
