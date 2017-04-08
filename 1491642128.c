d1b6d12197ae2e8e5b57705aff66526c4c830781
saisi <saisi@users.noreply.github.com>
2017-04-08 09:02:13 +0000

    1491642128 baggies

---
+++ 1491642128.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1699600677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   volatile int8_t * f3;
+   volatile signed f4 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_2[8] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
