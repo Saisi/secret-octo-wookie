d00dd00a1aa14272c2113eb7a35fcf942f59836f
saisi <saisi@users.noreply.github.com>
2017-04-08 20:03:15 +0000

    1491681795 Avera

---
+++ 1491681795.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4216800710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile unsigned f1 : 2;
+   volatile int64_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 4294967295UL;
+static int32_t g_44 = 0xDE6EAB3AL;
