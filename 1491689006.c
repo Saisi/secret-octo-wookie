56733c92be64ac967ad1f8f3da5462e9f30ebb5e
saisi <saisi@users.noreply.github.com>
2017-04-08 22:03:26 +0000

    1491689006 apoblast

---
+++ 1491689006.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1634997928
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 2;
+   const volatile uint8_t  f1;
+   const signed f2 : 7;
+};
+
+union U1 {
+   int8_t * f0;
+   const uint32_t  f1;
+   volatile int64_t  f2;
