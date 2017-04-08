316e1a000f8952d33a296fa16304d98b2567e854
saisi <saisi@users.noreply.github.com>
2017-04-08 23:03:34 +0000

    1491692611 apexes

---
+++ 1491692611.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1263858917
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   signed f1 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile signed f0 : 26;
+   volatile unsigned f1 : 4;
