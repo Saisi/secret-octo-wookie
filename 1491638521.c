d2a45b30fb36001d40b49ccfccc0d0631400fcfa
saisi <saisi@users.noreply.github.com>
2017-04-08 08:02:02 +0000

    1491638521 amphitropous

---
+++ 1491638521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3927270795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 4;
+   signed f1 : 11;
+   unsigned f2 : 2;
+   volatile signed f3 : 5;
+   unsigned f4 : 2;
+};
+
+union U1 {
+   uint32_t  f0;
