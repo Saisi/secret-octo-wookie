e53c600453071699c101ac8c4d35af1b10ef3f65
saisi <saisi@users.noreply.github.com>
2017-04-09 20:05:20 +0000

    1491768320 albuminurophobia

---
+++ 1491768320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2208994263
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_3 = 0x1BF5L;
+static int16_t g_5[7][9] = {{0L,0L,0x8CC3L,0L,(-10L),0xFC4BL,0L,(-8L),1L},{0x56E3L,0x868BL,0xDCEFL,0xAE28L,(-10L),0x89B1L,0x6836L,0L,0x822CL},{0xCEF0L,0L,0x8CC3L,0x8BD9L,0x9393L,(-10L),(-4L),0xDCEFL,(-4L)},{0xAE28L,0x9393L,0xBD13L,0xBD13L,0x9393L,0xAE28L,0x82CAL,0xE637L,0x7BB0L},{0xE637L,(-4L),0x37ACL,1L,(-10L),5L,0x8CC3L,0x56E3L,(-1L)},{0L,5L,0x3E38L,0xAE28L,0x6836L,0x510AL,1L,0xFC4BL,0x9393L},{1L,0x9393L,(-8L),(-1L),(-10L),(-10L),(-1L),(-8L),0x9393L}};
+static int16_t * const  volatile g_4 = &g_5[4][6];/* VOLATILE GLOBAL g_4 */
+static int16_t g_45 = 1L;
+static int32_t g_46 = (-1L);
+static uint64_t g_47 = 4UL;
+static int32_t g_71 = 0x4D5E33ABL;
+static int32_t g_97 = 0x42C7241AL;
+static const int32_t *g_96[9][1][3] = {{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}},{{&g_97,&g_97,&g_97}}};
