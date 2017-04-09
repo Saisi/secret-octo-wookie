3ce3dd75ece2b4232a5e733baf2cb37ff1f4f965
saisi <saisi@users.noreply.github.com>
2017-04-09 13:04:44 +0000

    1491743084 airthing

---
+++ 1491743084.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3191370134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x9C5A654FL;
+static int32_t g_37 = 0x6A47CDEDL;
+static uint16_t g_68 = 0x50D2L;
+static uint32_t g_91 = 0x7B75AE7BL;
+static uint8_t g_94 = 5UL;
+static uint8_t g_96 = 8UL;
+static int8_t g_100 = 0x50L;
+static int8_t g_104 = 9L;
