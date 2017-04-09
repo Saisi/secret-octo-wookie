75c0db6dbefb9e3dceef2f91086b9927fc862d43
saisi <saisi@users.noreply.github.com>
2017-04-09 19:05:15 +0000

    1491764715 bacteriotropin

---
+++ 1491764715.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1558887046
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x8CE9B8A5L;
+static int32_t * volatile g_3[9] = {&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4,&g_4};
+static int32_t * const  volatile g_5[5] = {&g_4,&g_4,&g_4,&g_4,&g_4};
+static int32_t * volatile g_6 = &g_4;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
