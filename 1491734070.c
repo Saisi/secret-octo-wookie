9703e0b524cc620cf64e7d2d56cd06ad6842a86d
saisi <saisi@users.noreply.github.com>
2017-04-09 10:34:30 +0000

    1491734070 androsphinxes

---
+++ 1491734070.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      783232171
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 20;
+   signed f1 : 27;
+   signed f2 : 18;
+   volatile unsigned f3 : 21;
+   unsigned f4 : 18;
+};
+
+union U1 {
+   int16_t  f0;
