3d885e9b4baf9249a76fd073dab60cfeb3158834
saisi <saisi@users.noreply.github.com>
2017-04-09 04:33:58 +0000

    1491712438 aerocartography

---
+++ 1491712438.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1642826645
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_11 = 0x7BL;
+static uint16_t g_13 = 65530UL;
+static uint16_t g_15 = 0xC2C9L;
+static uint16_t g_37 = 0x9535L;
+static uint16_t * const g_36 = &g_37;
+static uint16_t *g_41 = &g_37;
+static const uint16_t *g_45[1][6][9] = {{{&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37},{(void*)0,&g_37,&g_37,&g_37,&g_37,&g_37,(void*)0,&g_37,&g_37},{&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37},{(void*)0,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,(void*)0},{&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37,&g_37},{&g_37,&g_37,&g_37,&g_37,(void*)0,&g_37,(void*)0,&g_37,&g_37}}};
+static int32_t g_71 = (-1L);
+static int32_t * volatile g_72 = &g_71;/* VOLATILE GLOBAL g_72 */
