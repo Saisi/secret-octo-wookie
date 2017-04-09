ff696c1cb7b07fd7a2b2bb023384531b387f51bd
saisi <saisi@users.noreply.github.com>
2017-04-09 07:04:13 +0000

    1491721452 apolaustic

---
+++ 1491721452.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      997938464
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 30;
+   unsigned f1 : 27;
+   const unsigned f2 : 17;
+   unsigned f3 : 25;
+   signed f4 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[1] = {0UL};
