ba5c97c8160a814db57d57ace6c519fa5d9e80f1
saisi <saisi@users.noreply.github.com>
2017-04-09 22:35:32 +0000

    1491777332 arefact

---
+++ 1491777332.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1247101135
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 0xC7L;
+static int8_t *g_6[9] = {&g_7,&g_7,&g_7,&g_7,&g_7,&g_7,&g_7,&g_7,&g_7};
+static int32_t *g_10 = (void*)0;
+static int16_t g_12[1] = {0x06ECL};
+static volatile int32_t g_39 = 7L;/* VOLATILE GLOBAL g_39 */
