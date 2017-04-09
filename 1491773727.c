9141a8cdaf2647540550177a601f49613527eeaf
saisi <saisi@users.noreply.github.com>
2017-04-09 21:35:27 +0000

    1491773727 anabiotic

---
+++ 1491773727.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3703658556
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 12;
+   int32_t  f1;
+   const volatile int64_t  f2;
+};
+
+union U1 {
+   int32_t  f0;
+};
+
