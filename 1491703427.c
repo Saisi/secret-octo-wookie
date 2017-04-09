8c8324d3743d0c984c7698e473b2d795cb6ebd5b
saisi <saisi@users.noreply.github.com>
2017-04-09 02:03:47 +0000

    1491703427 airliners

---
+++ 1491703427.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2991255196
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 28;
+   signed f1 : 1;
+   const signed f2 : 31;
+};
+
+union U1 {
+   int32_t  f0;
+   uint32_t  f1;
+   volatile uint64_t  f2;
