5957588f7faad4ee4567178206cd4945e5bf7245
saisi <saisi@users.noreply.github.com>
2017-04-09 02:33:49 +0000

    1491705229 astringence

---
+++ 1491705229.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3657828656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x36D06583L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[2][6][2] = {{{1L,(-3L)},{0x95A52772L,1L},{0x21EBC5F9L,0x21EBC5F9L},{0x21EBC5F9L,1L},{0x95A52772L,(-3L)},{1L,(-3L)}},{{0x95A52772L,1L},{0x21EBC5F9L,0x21EBC5F9L},{0x21EBC5F9L,1L},{0x95A52772L,(-3L)},{1L,(-3L)},{0x95A52772L,1L}}};
+static struct S0 g_6 = {0xE548L};
+static struct S0 *g_5 = &g_6;
+static struct S0 ** volatile g_7[2] = {(void*)0,(void*)0};
