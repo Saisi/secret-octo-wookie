63d98b43dcfa283408bd18787bfec7d1bca2cf47
saisi <saisi@users.noreply.github.com>
2017-04-09 20:35:22 +0000

    1491770122 Aladdin

---
+++ 1491770122.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2979466768
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   int64_t  f1;
+   uint8_t  f2;
+};
+
+struct S1 {
+   uint64_t  f0;
+   uint32_t  f1;
+   const struct S0  f2;
