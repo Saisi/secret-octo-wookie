aa56691f97f891f83ffdecb4fbacd12ed7fcc569
saisi <saisi@users.noreply.github.com>
2017-04-09 23:05:35 +0000

    1491779134 antimoralism

---
+++ 1491779134.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2420811563
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   const int16_t  f1;
+   uint32_t  f2;
+   uint16_t  f3;
+   volatile uint16_t  f4;
+   const uint64_t  f5;
+   uint32_t  f6;
+   const uint64_t  f7;
+   volatile uint32_t  f8;
