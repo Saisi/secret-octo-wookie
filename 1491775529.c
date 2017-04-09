cec5a239222e2e969df901ad8d7d661ae5da4599
saisi <saisi@users.noreply.github.com>
2017-04-09 22:05:30 +0000

    1491775529 autographist

---
+++ 1491775529.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1124788824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * const  f0;
+   volatile unsigned f1 : 8;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_17[10][7] = {{(-1L),0x71L,0xF8L,0L,0xF5L,0L,(-10L)},{0x3AL,6L,0xF5L,0xF5L,6L,0x3AL,0xF8L},{0x3AL,(-1L),(-9L),0x71L,0L,0x8FL,0x4DL},{(-1L),(-10L),0x4DL,0xF1L,0xADL,(-2L),0L},{0xADL,(-1L),0L,0L,0L,0L,(-1L)},{0xDDL,6L,0L,(-2L),0xFBL,0L,(-9L)},{0xA0L,0x71L,0x4DL,0L,0L,0x71L,9L},{0xF1L,(-9L),0x4DL,0x0DL,6L,6L,0x0DL},{0xFBL,9L,0xFBL,0L,6L,(-1L),0x8FL},{0xADL,0x71L,0x3AL,9L,0L,(-10L),0xF8L}};
+static int8_t *g_16 = &g_17[8][5];
+static int64_t g_22 = 5L;
+static uint8_t g_27 = 0x30L;
