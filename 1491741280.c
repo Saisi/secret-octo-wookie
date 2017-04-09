57611cca0583860063e6d5cf1a1dbb6a5d35c064
saisi <saisi@users.noreply.github.com>
2017-04-09 12:34:42 +0000

    1491741280 arcticwards

---
+++ 1491741280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3826969298
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   uint32_t  f1;
+   const signed f2 : 15;
+   volatile signed f3 : 23;
+   const uint32_t  f4;
+   int32_t  f5;
+   uint32_t  f6;
