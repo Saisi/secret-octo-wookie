9e32155d7bed98152e41a5fb3bcca29ec38570c3
saisi <saisi@users.noreply.github.com>
2017-04-09 12:04:38 +0000

    1491739477 arhythmic

---
+++ 1491739477.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2112633007
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 22;
+   volatile signed f1 : 21;
+   volatile signed f2 : 4;
+   const unsigned f3 : 31;
+   volatile signed f4 : 20;
+   uint32_t  f5;
+   signed f6 : 1;
