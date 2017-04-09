856ae0ebd307cbec165f73e98faa6b8fc659b75c
saisi <saisi@users.noreply.github.com>
2017-04-09 18:35:13 +0000

    1491762913 achroglobin

---
+++ 1491762913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      278515311
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   int8_t * const  f1;
+   volatile uint8_t  f2;
+   const int8_t  f3;
+   const volatile uint32_t  f4;
