c1a3c98d59df4bf1757bdfe3f7f76c92c38aa262
saisi <saisi@users.noreply.github.com>
2017-04-09 03:03:52 +0000

    1491707031 agoniadin

---
+++ 1491707031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      260166572
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const int8_t  f1;
+   uint64_t  f2;
+   volatile int32_t  f3;
+   int32_t  f4;
+   int8_t  f5;
+   int32_t  f6;
+   unsigned f7 : 26;
+   unsigned f8 : 9;
