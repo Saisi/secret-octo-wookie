5b7f6fccad4cdec9ec8005b0e8c6766ce770d546
saisi <saisi@users.noreply.github.com>
2017-04-10 17:07:14 +0000

    1491844033 admirer

---
+++ 1491844033.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3779914990
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 5;
+   signed f1 : 6;
+};
+
+struct S1 {
+   uint16_t  f0;
+   int8_t  f1;
+   uint8_t  f2;
+   int16_t  f3;
