9c023504840854acade196650a015b60f59318ad
saisi <saisi@users.noreply.github.com>
2017-04-10 16:07:09 +0000

    1491840427 Alexas

---
+++ 1491840427.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1801372548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   volatile unsigned f1 : 5;
+   volatile signed f2 : 19;
+   volatile signed f3 : 19;
+   unsigned f4 : 22;
+};
+#pragma pack(pop)
