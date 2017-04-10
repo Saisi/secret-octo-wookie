5b827240cd81d7bdf9622846ddf8db3ce6b6f584
saisi <saisi@users.noreply.github.com>
2017-04-10 11:36:43 +0000

    1491824202 authoritarian

---
+++ 1491824202.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      353706117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 1;
+   volatile unsigned f1 : 20;
+   unsigned f2 : 7;
+   unsigned f3 : 22;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_12 = 1UL;
+static int32_t g_19 = 0xD6B3BFA3L;
