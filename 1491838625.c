ba9be6ea2a38aa389fce743e5507d83d4a2b9f93
saisi <saisi@users.noreply.github.com>
2017-04-10 15:37:05 +0000

    1491838625 acetylative

---
+++ 1491838625.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1737484133
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   signed f0 : 9;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xC4AB274BL;
+static int8_t g_7 = (-1L);
+static int32_t g_9 = 9L;
+static int32_t ** volatile g_10 = (void*)0;/* VOLATILE GLOBAL g_10 */
