26d67caa65590cecb8590f6b0b98eb5f2b0d4bc1
saisi <saisi@users.noreply.github.com>
2017-04-10 20:07:29 +0000

    1491854849 banderilla

---
+++ 1491854849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2256690584
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_16 = (-1L);
+static int8_t g_22[3] = {0x6DL,0x6DL,0x6DL};
+static int8_t g_35[8] = {0x63L,0x63L,0x63L,0x63L,0x63L,0x63L,0x63L,0x63L};
+static int8_t *g_34 = &g_35[0];
+static int32_t g_40 = 0x13C27F35L;
+static uint32_t g_45 = 1UL;
+static int64_t g_83[1] = {0x64A3A207B8A5F036LL};
+static int64_t g_91 = 0x9AB67338D581CA92LL;
+static int64_t * volatile g_90[5][2][10] = {{{&g_91,(void*)0,&g_91,&g_91,(void*)0,&g_91,&g_91,&g_91,(void*)0,&g_91},{&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91,(void*)0,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,(void*)0,(void*)0}},{{&g_91,(void*)0,&g_91,&g_91,&g_91,&g_91,(void*)0,&g_91,(void*)0,&g_91},{&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91}},{{&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91},{&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91,&g_91}},{{(void*)0,&g_91,(void*)0,&g_91,&g_91,&g_91,&g_91,(void*)0,&g_91,(void*)0},{(void*)0,&g_91,&g_91,&g_91,&g_91,&g_91,(void*)0,(void*)0,&g_91,&g_91}}};
