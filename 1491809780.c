b79d1086fbfef6d4d67bc3387438eeeb16d34d5f
saisi <saisi@users.noreply.github.com>
2017-04-10 07:36:21 +0000

    1491809780 Asteropaeus

---
+++ 1491809780.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1465936228
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint16_t  f1;
+   volatile signed f2 : 3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x83C8FC55L;
+static int32_t g_7 = 0xB2391CB4L;
+static volatile int8_t g_8 = (-5L);/* VOLATILE GLOBAL g_8 */
