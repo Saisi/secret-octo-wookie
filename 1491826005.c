b61a4571f67622f32316cc06a98ad499df831264
saisi <saisi@users.noreply.github.com>
2017-04-10 12:06:46 +0000

    1491826005 abondance

---
+++ 1491826005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2333876608
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint64_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_13 = 0x96L;
+static uint16_t g_18 = 65532UL;
