f31b62a0cf5e2eeb31add4a54b60ff32e0a8bcd5
saisi <saisi@users.noreply.github.com>
2017-04-10 05:06:08 +0000

    1491800768 aleurones

---
+++ 1491800768.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1464650127
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x506D4441L;
+static int64_t g_21 = 0xED7C6AE49A2F3817LL;
+static int8_t g_23 = 0xA9L;
+static int32_t g_28 = 0x7790660DL;
+static uint8_t g_31 = 1UL;
+static int8_t *g_69 = (void*)0;
+static int8_t **g_68[8] = {&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69};
+static uint16_t g_72 = 0x372CL;
+static volatile uint16_t g_80 = 0x7300L;/* VOLATILE GLOBAL g_80 */
