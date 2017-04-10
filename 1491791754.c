2dfb4a165fc33db5da948ba2e125d95f4867e99f
saisi <saisi@users.noreply.github.com>
2017-04-10 02:35:55 +0000

    1491791754 amain

---
+++ 1491791754.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2071098330
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = 1L;
+static int16_t g_18 = 0L;
+static uint8_t g_20 = 255UL;
+static int64_t g_23 = (-1L);
+static uint8_t *g_74 = (void*)0;
+static uint8_t g_80[10][4] = {{0xF2L,0UL,1UL,7UL},{0xBCL,0xC3L,0xBCL,7UL},{1UL,0UL,0xF2L,8UL},{0x81L,0x1BL,0UL,0UL},{0xE8L,0xE8L,0UL,0xBCL},{0x81L,250UL,0xF2L,0x1BL},{1UL,0xF2L,0xBCL,0xF2L},{0xBCL,0xF2L,1UL,0x1BL},{0xF2L,250UL,0x81L,0xBCL},{0UL,0xE8L,0xE8L,0UL}};
+static int8_t g_83 = 1L;
+static int32_t *g_88 = &g_12;
+static int32_t ** volatile g_87 = &g_88;/* VOLATILE GLOBAL g_87 */
