7dafb1c7e06f5fd4650270f20a3a04dd7a3d88b0
saisi <saisi@users.noreply.github.com>
2017-04-10 05:36:11 +0000

    1491802570 atropins

---
+++ 1491802570.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2502038704
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 6;
+   unsigned f1 : 3;
+   signed f2 : 25;
+   unsigned f3 : 1;
+   const signed f4 : 12;
+   volatile unsigned f5 : 19;
+   const unsigned f6 : 5;
