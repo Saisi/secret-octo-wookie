e731e3c4182d64020b15fe3e62272d1577b922a6
saisi <saisi@users.noreply.github.com>
2017-04-10 13:36:54 +0000

    1491831413 ambitionless

---
+++ 1491831413.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2790449409
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile signed f0 : 31;
+   unsigned f1 : 26;
+   signed f2 : 21;
+   signed f3 : 12;
+   unsigned f4 : 13;
+   volatile signed f5 : 18;
+};
+
+/* --- GLOBAL VARIABLES --- */
