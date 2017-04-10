10f086426d0c0cfa8ac3d72954bd2253b7b5e00d
saisi <saisi@users.noreply.github.com>
2017-04-10 09:06:29 +0000

    1491815189 Amaleta

---
+++ 1491815189.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2747016699
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4[5] = {0xDBA76AABL,0xDBA76AABL,0xDBA76AABL,0xDBA76AABL,0xDBA76AABL};
+static uint16_t g_9[9] = {0xFA20L,0xFA20L,0xFA20L,0xFA20L,0xFA20L,0xFA20L,0xFA20L,0xFA20L,0xFA20L};
+static int32_t g_19 = 0x3BC4AEE5L;
+static int32_t g_21 = 0x451D77F1L;
+static int32_t *g_20 = &g_21;
+static uint16_t g_57 = 0UL;
+static int32_t g_88 = 3L;
+static int32_t g_92 = 0x9654EA11L;
+static uint16_t g_114 = 0x2D56L;
