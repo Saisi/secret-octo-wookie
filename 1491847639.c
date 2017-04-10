6f189a3ae75912e63567a6a9286d86fadf08bb1d
saisi <saisi@users.noreply.github.com>
2017-04-10 18:07:20 +0000

    1491847639 antiulcer

---
+++ 1491847639.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2709107972
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 16;
+   const volatile signed f1 : 5;
+   volatile signed f2 : 19;
+   int32_t  f3;
+   volatile signed f4 : 4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
