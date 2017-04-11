d406ea2fe9f53c361bc74a8c41de4d6012e37527
saisi <saisi@users.noreply.github.com>
2017-04-11 12:38:58 +0000

    1491914338 aeronautically

---
+++ 1491914338.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1251874710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 25;
+   const uint64_t  f1;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFBB6BC3EL;
+static int32_t g_4[4][1][6] = {{{0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL}},{{0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL}},{{0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL}},{{0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL,0x3773672DL}}};
+static volatile union U0 g_20 = {0x593F6A08L};/* VOLATILE GLOBAL g_20 */
