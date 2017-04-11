7adeb5b703c22663308a56e1d063d5e87f7250e4
saisi <saisi@users.noreply.github.com>
2017-04-11 03:38:08 +0000

    1491881888 balsa

---
+++ 1491881888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2333706718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 29;
+   volatile uint32_t  f1;
+   signed f2 : 6;
+   volatile unsigned f3 : 16;
+   signed f4 : 21;
+   unsigned f5 : 13;
+   int64_t  f6;
