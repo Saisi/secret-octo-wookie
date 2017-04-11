766154b130f3ef63058a055079a1919514e156ea
saisi <saisi@users.noreply.github.com>
2017-04-11 10:08:45 +0000

    1491905324 abortions

---
+++ 1491905324.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2265195918
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 21;
+   volatile signed f1 : 22;
+   signed : 0;
+   int8_t  f2;
+   const volatile unsigned f3 : 11;
+   const volatile unsigned f4 : 22;
+   signed f5 : 28;
+   volatile unsigned f6 : 1;
+};
