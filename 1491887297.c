c9cac8e974cdccf402f248d9596c38df61efb13c
saisi <saisi@users.noreply.github.com>
2017-04-11 05:08:17 +0000

    1491887297 antirusts

---
+++ 1491887297.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      169633109
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_20 = 4UL;
+static uint8_t g_69[10] = {255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL,255UL};
+static int32_t g_71 = 0x8AC507B3L;
+static int32_t *g_73 = &g_71;
+static int32_t ** volatile g_72[2][10][7] = {{{&g_73,&g_73,&g_73,&g_73,&g_73,&g_73,&g_73},{&g_73,(void*)0,&g_73,&g_73,&g_73,&g_73,&g_73},{&g_73,(void*)0,&g_73,&g_73,(void*)0,&g_73,&g_73},{(void*)0,&g_73,&g_73,&g_73,&g_73,&g_73,(void*)0},{&g_73,&g_73,&g_73,(void*)0,(void*)0,&g_73,&g_73},{&g_73,&g_73,&g_73,&g_73,&g_73,(void*)0,&g_73},{&g_73,&g_73,&g_73,&g_73,&g_73,&g_73,(void*)0},{&g_73,(void*)0,(void*)0,&g_73,&g_73,&g_73,(void*)0},{&g_73,(void*)0,&g_73,&g_73,(void*)0,&g_73,&g_73},{&g_73,(void*)0,&g_73,&g_73,&g_73,&g_73,&g_73}},{{&g_73,&g_73,&g_73,&g_73,&g_73,&g_73,(void*)0},{(void*)0,&g_73,&g_73,&g_73,&g_73,&g_73,&g_73},{&g_73,(void*)0,(void*)0,&g_73,&g_73,(void*)0,&g_73},{&g_73,(void*)0,&g_73,&g_73,(void*)0,(void*)0,&g_73},{&g_73,(void*)0,&g_73,(void*)0,&g_73,&g_73,&g_73},{&g_73,&g_73,&g_73,&g_73,&g_73,&g_73,&g_73},{&g_73,&g_73,&g_73,(void*)0,&g_73,&g_73,&g_73},{&g_73,&g_73,(void*)0,&g_73,&g_73,(void*)0,&g_73},{(void*)0,&g_73,&g_73,&g_73,(void*)0,(void*)0,&g_73},{&g_73,(void*)0,&g_73,&g_73,&g_73,&g_73,&g_73}}};
+static int32_t ** volatile g_76 = &g_73;/* VOLATILE GLOBAL g_76 */
+static uint32_t g_83 = 0x5E7E8EE7L;
+static uint64_t g_86[3] = {0x207E8793EB104B5BLL,0x207E8793EB104B5BLL,0x207E8793EB104B5BLL};
+static uint64_t g_119 = 18446744073709551615UL;
