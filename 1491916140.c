4b37624d5020266ec9aa62361b22cda36591872f
saisi <saisi@users.noreply.github.com>
2017-04-11 13:09:02 +0000

    1491916140 Balan

---
+++ 1491916140.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2861949499
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   unsigned f1 : 29;
+   uint16_t  f2;
+   int8_t * volatile  f3;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   int16_t  f1;
