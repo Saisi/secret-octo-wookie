0fd1e2bc36c2e042f69377ef7e4a313d5dd7f349
saisi <saisi@users.noreply.github.com>
2017-04-11 15:39:17 +0000

    1491925156 apolarity

---
+++ 1491925156.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3295597275
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const int16_t  f1;
+   int8_t * volatile  f2;
+   const volatile int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 9UL;
+static volatile int32_t g_18 = 5L;/* VOLATILE GLOBAL g_18 */
