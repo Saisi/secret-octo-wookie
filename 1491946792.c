939d7bb25809ef2b1ef3d9b898eac948b17e8314
saisi <saisi@users.noreply.github.com>
2017-04-11 21:39:53 +0000

    1491946792 Agatharchides

---
+++ 1491946792.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2216735784
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = 0x4A8BL;
+static uint8_t g_34 = 0xA0L;
+static int8_t g_69 = (-1L);
+static uint8_t g_83[1] = {0xF9L};
+static int32_t g_100 = 0x4C0DA393L;
+static int32_t *g_99 = &g_100;
+static int32_t ** volatile g_101[9][3] = {{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99},{&g_99,&g_99,&g_99}};
+static int32_t ** volatile g_102 = &g_99;/* VOLATILE GLOBAL g_102 */
+static int32_t ** volatile g_103 = &g_99;/* VOLATILE GLOBAL g_103 */
