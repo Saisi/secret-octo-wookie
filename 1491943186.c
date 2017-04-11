ed02cc09d227f5f4e128cb05602ef978daa7a0e0
saisi <saisi@users.noreply.github.com>
2017-04-11 20:39:47 +0000

    1491943186 Aviva

---
+++ 1491943186.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2042131061
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   const volatile uint32_t  f1;
+   volatile int8_t  f2;
+   signed f3 : 23;
+   volatile uint32_t  f4;
+   int32_t  f5;
+   const uint64_t  f6;
+   uint64_t  f7;
+};
