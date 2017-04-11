925dff4422ad7eddedce485a25a8b7c62d35db88
saisi <saisi@users.noreply.github.com>
2017-04-11 17:39:27 +0000

    1491932367 arithmetical

---
+++ 1491932367.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      53564783
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile uint64_t  f1;
+   volatile int64_t  f2;
+   volatile uint64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_15 = 65531UL;
+static int32_t g_30 = 0xA74ADF29L;
