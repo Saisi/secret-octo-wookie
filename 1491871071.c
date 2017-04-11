bbeda02a74300a9251726e9457f719cd68079e03
saisi <saisi@users.noreply.github.com>
2017-04-11 00:37:51 +0000

    1491871071 aerify

---
+++ 1491871071.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      732986640
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 22;
+   const volatile uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_4[9] = {0x08CCCDDD1D8F8F57LL,0xED23B6182EAC4530LL,0x08CCCDDD1D8F8F57LL,0x08CCCDDD1D8F8F57LL,0xED23B6182EAC4530LL,0x08CCCDDD1D8F8F57LL,0x08CCCDDD1D8F8F57LL,0xED23B6182EAC4530LL,0x08CCCDDD1D8F8F57LL};
+static uint32_t g_20 = 1UL;
+static int64_t g_24 = 0xF62C4DF8E3BF118CLL;
+static uint32_t *g_42 = &g_20;
