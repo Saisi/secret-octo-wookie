1e71e55e9ed7df6be76e5686690133ce67670022
saisi <saisi@users.noreply.github.com>
2017-04-11 18:39:32 +0000

    1491935972 antipolo

---
+++ 1491935972.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      902906566
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 17;
+   unsigned f1 : 2;
+   const signed f2 : 28;
+   volatile unsigned f3 : 24;
+   volatile signed f4 : 7;
+   unsigned f5 : 2;
+   signed f6 : 24;
