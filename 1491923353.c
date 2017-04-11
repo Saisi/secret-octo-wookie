16741aad00642b9d94e4030ed9d6d5badfe6ce76
saisi <saisi@users.noreply.github.com>
2017-04-11 15:09:14 +0000

    1491923353 airdrome

---
+++ 1491923353.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1284186344
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xCFF0EA75L;
+static volatile int16_t g_5[5] = {0xF825L,0xF825L,0xF825L,0xF825L,0xF825L};
+static int32_t g_7 = 0xFC3EEE2FL;
+static uint16_t g_12[10] = {65531UL,65531UL,65531UL,65531UL,65531UL,65531UL,65531UL,65531UL,65531UL,65531UL};
+static int32_t * volatile g_41 = (void*)0;/* VOLATILE GLOBAL g_41 */
+static int32_t g_43 = 1L;
+static uint32_t g_54 = 0x18073565L;
+static uint32_t g_64[3] = {0x2D863E32L,0x2D863E32L,0x2D863E32L};
+static volatile uint64_t g_79[2] = {0x34BB8EBACA85E45CLL,0x34BB8EBACA85E45CLL};
