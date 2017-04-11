619d54a081fe50244ef323f6fb374523b3f74406
saisi <saisi@users.noreply.github.com>
2017-04-11 08:08:32 +0000

    1491898111 albuminizing

---
+++ 1491898111.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      540322240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_20 = 0x0C43L;
+static uint8_t g_36 = 0xF1L;
+static int32_t g_39 = 1L;
+static int16_t g_88[4][8] = {{0x38DBL,0x9C50L,(-2L),(-2L),0x9C50L,0x38DBL,0L,0x6B13L},{0xC8B2L,0L,0x942AL,0x37FFL,0x38DBL,0x942AL,0x9C50L,0x942AL},{2L,0x37FFL,0xCEC8L,0x37FFL,2L,0xD6CAL,0x37FFL,0x6B13L},{(-1L),2L,0x38DBL,(-2L),0x37FFL,(-1L),(-1L),0x37FFL}};
+static uint16_t g_90 = 0x3780L;
+static uint16_t g_93 = 1UL;
+static int8_t g_103[8] = {0x9AL,0xAEL,0xAEL,0x9AL,0xAEL,0xAEL,0x9AL,0xAEL};
+static volatile uint16_t g_104 = 0x1D51L;/* VOLATILE GLOBAL g_104 */
+static uint16_t *g_125 = &g_90;
