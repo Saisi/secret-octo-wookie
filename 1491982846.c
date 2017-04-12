cad683eadc0066380b35ec6af177ea68895dab79
saisi <saisi@users.noreply.github.com>
2017-04-12 07:40:48 +0000

    1491982846 Aetobatidae

---
+++ 1491982846.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2052710134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 9;
+   const int32_t  f1;
+   volatile uint64_t  f2;
+   uint64_t  f3;
+   volatile uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-3L);/* VOLATILE GLOBAL g_2 */
