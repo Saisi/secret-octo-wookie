7c891662f85332649838381ff93ef3d4a824c75b
saisi <saisi@users.noreply.github.com>
2017-04-12 00:40:11 +0000

    1491957610 atamasco

---
+++ 1491957610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3554820588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4][9] = {{(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L)},{(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L)},{(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L)},{(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L),(-1L),0x176B0F73L,(-1L)}};
+static volatile int16_t g_13 = 9L;/* VOLATILE GLOBAL g_13 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
