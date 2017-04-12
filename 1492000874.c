0a8ffb93c83a63a736247eb93c9ac89116cf8e6e
saisi <saisi@users.noreply.github.com>
2017-04-12 12:41:14 +0000

    1492000874 backcross

---
+++ 1492000874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      89070736
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[10] = {(-1L),0L,0xCB13638EL,0xCB13638EL,0L,(-1L),0L,0xCB13638EL,0xCB13638EL,0L};
+static int32_t g_3 = 0xED23B618L;
+static int32_t g_10 = 1L;
+static uint16_t g_34 = 0x5835L;
+static int8_t g_64 = 0x02L;
