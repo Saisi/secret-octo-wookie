cd82b6a446bba30cd9b7f106840339d2453574f1
saisi <saisi@users.noreply.github.com>
2017-04-12 19:11:46 +0000

    1492024306 acetylate

---
+++ 1492024306.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3271047579
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xA20BD035L;
+static int8_t g_11 = 1L;
+static int16_t g_13[2][6] = {{0x0D62L,0xE813L,0x0D62L,0x0D62L,0xE813L,0x0D62L},{0x0D62L,0xE813L,0x0D62L,0x0D62L,0xE813L,0x0D62L}};
+static volatile int8_t g_15[7] = {0xCAL,1L,0xCAL,0xCAL,1L,0xCAL,0xCAL};
+static int64_t g_18 = 1L;
+static uint16_t g_19 = 0xEC46L;
+
+
+/* --- FORWARD DECLARATIONS --- */
