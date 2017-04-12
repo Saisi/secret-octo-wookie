2aee0666fd1d5a2f6255f9c2c437576f6b37f7e2
saisi <saisi@users.noreply.github.com>
2017-04-12 02:40:20 +0000

    1491964819 amphitropous

---
+++ 1491964819.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      168907329
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-10L);
+static uint8_t g_40 = 0xB3L;
+static int8_t g_44 = 1L;
+static int32_t g_53 = 3L;
+static const int32_t g_81 = 0L;
+static const int32_t *g_80 = &g_81;
+static int16_t g_94 = (-2L);
+static int32_t g_96 = 0x3044B131L;
+static int16_t g_97 = 0x3E25L;
