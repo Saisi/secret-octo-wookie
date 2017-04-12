029a1f1229a31995d198f3fe49382274de733c10
saisi <saisi@users.noreply.github.com>
2017-04-12 02:10:17 +0000

    1491963017 antineutral

---
+++ 1491963017.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2305406
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * volatile  f0;
+   const uint32_t  f1;
+   int8_t * const  volatile  f2;
+   volatile int8_t * const  f3;
+   volatile int8_t * volatile  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[10][1] = {{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL},{0xB905D09EL}};
