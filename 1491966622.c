cfdd10199f5e88abc58a8ce212c7a4e4628358bc
saisi <saisi@users.noreply.github.com>
2017-04-12 03:10:22 +0000

    1491966622 Antiaris

---
+++ 1491966622.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      838819834
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 11;
+   signed f1 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = (-9L);/* VOLATILE GLOBAL g_2 */
+static uint32_t g_4 = 0x43046B6EL;
+static int16_t g_12 = (-5L);
+static uint16_t g_14 = 65527UL;
