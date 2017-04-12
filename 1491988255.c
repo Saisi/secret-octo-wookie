0a758a114c2ab5afe8ce6f1df235c79e62c738f1
saisi <saisi@users.noreply.github.com>
2017-04-12 09:10:55 +0000

    1491988255 abristle

---
+++ 1491988255.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2708294532
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int32_t  f1;
+   int64_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_12 = 8L;
+static int32_t g_64[2] = {0xEEF10DECL,0xEEF10DECL};
