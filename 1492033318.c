e9ffb9f108319454e429382c95ad6f06b230d778
saisi <saisi@users.noreply.github.com>
2017-04-12 21:41:59 +0000

    1492033318 Amphibia

---
+++ 1492033318.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1891113979
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 28;
+   unsigned f1 : 3;
+   signed f2 : 26;
+   volatile signed f3 : 29;
+   const signed f4 : 26;
+};
+
+struct S1 {
+   uint64_t  f0;
