6c321366ffdc1d08f8a22f3be72430c691c75b4c
saisi <saisi@users.noreply.github.com>
2017-04-12 08:40:53 +0000

    1491986452 atherosclerotically

---
+++ 1491986452.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1772503805
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static uint8_t g_15[9] = {0x3EL,0xD1L,0x3EL,0x3EL,0xD1L,0x3EL,0x3EL,0xD1L,0x3EL};
+static uint32_t g_33 = 0UL;
+static int64_t g_36 = 0L;
+static int32_t g_44 = 0L;
+static uint32_t *g_60 = (void*)0;
+static int16_t g_70 = 0x9D40L;
+static volatile int16_t g_78 = 0x235BL;/* VOLATILE GLOBAL g_78 */
+static volatile int64_t g_83 = 1L;/* VOLATILE GLOBAL g_83 */
