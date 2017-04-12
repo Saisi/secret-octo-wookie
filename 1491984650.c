3870a98a69fa1f024e3069beaecc4f09d6ac1fd2
saisi <saisi@users.noreply.github.com>
2017-04-12 08:10:50 +0000

    1491984650 Ardelis

---
+++ 1491984650.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      947215525
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int8_t  f1;
+   int8_t  f2;
+   volatile unsigned f3 : 6;
+   int8_t * f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2][9] = {{0x443250E6L,0x6CA2CB51L,0x443250E6L,0x443250E6L,0x6CA2CB51L,0x443250E6L,0x443250E6L,0x6CA2CB51L,0x443250E6L},{0x443250E6L,0x6CA2CB51L,0x443250E6L,0x443250E6L,0x6CA2CB51L,0x443250E6L,0x443250E6L,0x6CA2CB51L,0x443250E6L}};
