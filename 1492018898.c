0f1e072cbabe3817b54a14687acbdeb20ab5b4e2
saisi <saisi@users.noreply.github.com>
2017-04-12 17:41:39 +0000

    1492018898 acellular

---
+++ 1492018898.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3151005469
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 13;
+   const volatile unsigned f1 : 16;
+   volatile signed f2 : 19;
+   const signed f3 : 18;
+   signed f4 : 27;
+   unsigned f5 : 8;
+   signed f6 : 7;
+   unsigned f7 : 30;
+};
