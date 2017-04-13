1352982e3bef91f055bb393cdbfc92baae6cfe53
saisi <saisi@users.noreply.github.com>
2017-04-13 03:42:29 +0000

    1492054947 acephalism

---
+++ 1492054947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4275507740
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   volatile uint32_t  f1;
+   int8_t  f2;
+   volatile uint64_t  f3;
+   const uint64_t  f4;
+   const uint32_t  f5;
+   const volatile int32_t  f6;
+   int64_t  f7;
+   uint16_t  f8;
