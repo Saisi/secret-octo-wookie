e6041366489ec19b674103afb7479c99defc48e4
saisi <saisi@users.noreply.github.com>
2017-04-13 05:42:39 +0000

    1492062158 Anglicanism

---
+++ 1492062158.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1456772758
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 25;
+   unsigned f1 : 10;
+   unsigned f2 : 4;
+   int8_t  f3;
+   signed f4 : 1;
+   signed f5 : 9;
+   signed f6 : 6;
