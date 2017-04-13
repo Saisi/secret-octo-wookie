b9a72855f57e7fc2894cc6f645740469f427c064
saisi <saisi@users.noreply.github.com>
2017-04-13 21:44:01 +0000

    1492119841 antipriestcraft

---
+++ 1492119841.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      177822801
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   const signed f0 : 9;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[7] = {0x6BAFCB14L,0x6BAFCB14L,0x6BAFCB14L,0x6BAFCB14L,0x6BAFCB14L,0x6BAFCB14L,0x6BAFCB14L};
+static int8_t g_5 = (-6L);
+static int32_t g_7 = 0x515FDC8DL;
+static int32_t * volatile g_6 = &g_7;/* VOLATILE GLOBAL g_6 */
