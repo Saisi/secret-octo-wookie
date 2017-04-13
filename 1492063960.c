e99cc5aeff7f0e8ebfdc358188c832529c40b176
saisi <saisi@users.noreply.github.com>
2017-04-13 06:12:41 +0000

    1492063960 activator

---
+++ 1492063960.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1673028374
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[10] = {0x01F2C29AL,8L,0x01F2C29AL,0x01F2C29AL,8L,0x01F2C29AL,0x01F2C29AL,8L,0x01F2C29AL,0x01F2C29AL};
+static int32_t g_5 = 0L;
+static uint16_t g_61 = 0x1900L;
+static int32_t g_63 = 0L;
+static int32_t * volatile g_62 = &g_63;/* VOLATILE GLOBAL g_62 */
+static int16_t g_87 = 0L;
+static int32_t *g_100 = &g_2[7];
+static int32_t **g_99 = &g_100;
+static uint8_t g_104 = 0xBFL;
