442a0d23ef6a8e2cc84dc5f80563c09a27fcceb7
saisi <saisi@users.noreply.github.com>
2017-04-13 22:44:08 +0000

    1492123447 anocithesia

---
+++ 1492123447.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4167272807
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   unsigned f1 : 27;
+   int8_t * const  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xD4E7937DL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-2L);
