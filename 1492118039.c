abeef948ad021edca00c5134a1ccd31d3dfd5d46
saisi <saisi@users.noreply.github.com>
2017-04-13 21:13:59 +0000

    1492118039 albumin

---
+++ 1492118039.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3644668980
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xD6C05BAFL;
+static uint32_t g_28 = 1UL;
+static volatile int32_t g_56 = 0xC1731D49L;/* VOLATILE GLOBAL g_56 */
+static volatile int32_t g_57 = 0L;/* VOLATILE GLOBAL g_57 */
+static volatile int32_t g_58 = 0L;/* VOLATILE GLOBAL g_58 */
+static int32_t g_59 = (-9L);
+static int64_t g_61 = 0xFA46B4C01F903A79LL;
+static int64_t *g_67 = (void*)0;
+static int64_t **g_66 = &g_67;
