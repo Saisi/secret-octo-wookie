6f8a3ea2af96b753e01149814993867e73d84f22
saisi <saisi@users.noreply.github.com>
2017-04-13 13:13:17 +0000

    1492089196 allochroite

---
+++ 1492089196.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3781082908
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 28;
+   unsigned f1 : 1;
+   signed f2 : 31;
+   signed f3 : 15;
+   volatile unsigned f4 : 2;
+   volatile unsigned f5 : 17;
+   const volatile signed f6 : 11;
