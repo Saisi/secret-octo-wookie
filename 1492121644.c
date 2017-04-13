424e40f0f53fbc241fcf2bfa504a24f17acc9649
saisi <saisi@users.noreply.github.com>
2017-04-13 22:14:05 +0000

    1492121644 Asio

---
+++ 1492121644.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3224385010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int64_t  f0;
+   uint16_t  f1;
+   const uint32_t  f2;
+   int16_t  f3;
+   int8_t  f4;
+   int16_t  f5;
+   int32_t  f6;
+   int8_t  f7;
+};
