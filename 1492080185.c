0a69676fe3babf2fc467ce7b9a25e49920c856b7
saisi <saisi@users.noreply.github.com>
2017-04-13 10:43:05 +0000

    1492080185 anticarnivorous

---
+++ 1492080185.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1951150343
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 2L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t * volatile * volatile g_4 = &g_2;/* VOLATILE GLOBAL g_4 */
+static int32_t g_10 = (-1L);
+static int32_t *g_9 = &g_10;
+static int32_t **g_8 = &g_9;
+static int32_t *** volatile g_7[5] = {&g_8,&g_8,&g_8,&g_8,&g_8};
+static int32_t *** const  volatile g_11 = &g_8;/* VOLATILE GLOBAL g_11 */
+static int32_t g_37[3][3] = {{0L,0L,0L},{0x35DD7582L,0x35DD7582L,0x35DD7582L},{0L,0L,0L}};
