ea43c438138bb295e0b55413db026231077a82cd
saisi <saisi@users.noreply.github.com>
2017-04-13 15:43:28 +0000

    1492098208 antisceptic

---
+++ 1492098208.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3768845555
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-7L);
+static volatile uint32_t g_5[3] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
+static int32_t g_8 = 1L;
+static uint32_t g_9[1] = {0x12A3CC92L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
