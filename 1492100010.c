e3e1497c6132ac0121c15be8cdeb32e5b26d21ae
saisi <saisi@users.noreply.github.com>
2017-04-13 16:13:30 +0000

    1492100010 analecta

---
+++ 1492100010.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3419745186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 31;
+   const volatile signed f1 : 24;
+   const int8_t  f2;
+   signed f3 : 2;
+   unsigned f4 : 31;
+   signed f5 : 15;
+   volatile signed f6 : 20;
+   volatile unsigned f7 : 29;
+   const unsigned f8 : 26;
