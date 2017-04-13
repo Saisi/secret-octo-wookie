072c054ae38b0ab9ef1c483c23c99df633485cb4
saisi <saisi@users.noreply.github.com>
2017-04-13 06:42:44 +0000

    1492065763 autonegation

---
+++ 1492065763.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2719365507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 0x77E8L;
+static volatile int32_t g_10 = 0x4F147909L;/* VOLATILE GLOBAL g_10 */
+static volatile int32_t *g_9 = &g_10;
+static int32_t g_13 = 0L;
+static uint8_t g_30 = 255UL;
+static int8_t g_38 = 1L;
+static uint16_t g_84 = 65535UL;
+static int8_t g_91 = 9L;
+static uint16_t g_92 = 0xADD1L;
