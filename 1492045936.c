25b81d7766915a0e7bb11008059d1219c5f95fcf
saisi <saisi@users.noreply.github.com>
2017-04-13 01:12:16 +0000

    1492045936 adminiculation

---
+++ 1492045936.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1774118352
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 22;
+   const uint32_t  f1;
+   unsigned f2 : 11;
+   volatile unsigned f3 : 1;
+   volatile unsigned f4 : 2;
+};
+#pragma pack(pop)
