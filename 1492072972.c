4f1a2f8aae699d71b4b3e26e01639a3661160db6
saisi <saisi@users.noreply.github.com>
2017-04-13 08:42:53 +0000

    1492072972 archostegnosis

---
+++ 1492072972.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      940280028
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint8_t  f0;
+   volatile uint8_t  f1;
+   const uint16_t  f2;
+   uint32_t  f3;
+   uint32_t  f4;
+   uint8_t  f5;
+   volatile int32_t  f6;
