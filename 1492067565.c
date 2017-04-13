b62d9605e175d7d47e5dd21dee78e94bd8512ea3
saisi <saisi@users.noreply.github.com>
2017-04-13 07:12:46 +0000

    1492067565 algesia

---
+++ 1492067565.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3630388315
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int16_t  f1;
+   unsigned f2 : 27;
+   uint64_t  f3;
+   volatile int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x5A5EFA20L;
