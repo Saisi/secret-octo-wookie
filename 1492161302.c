abe7203b34820c6afd52c200cccebad5d3ab9997
saisi <saisi@users.noreply.github.com>
2017-04-14 09:15:02 +0000

    1492161302 actinomycoma

---
+++ 1492161302.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1172916403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 24;
+   unsigned f1 : 15;
+   uint32_t  f2;
+   volatile signed f3 : 7;
+   unsigned f4 : 18;
+   unsigned f5 : 24;
+   volatile unsigned f6 : 27;
