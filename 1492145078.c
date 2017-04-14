92cdec2ac47884c4d52da71e7937aa8498933808
saisi <saisi@users.noreply.github.com>
2017-04-14 04:44:38 +0000

    1492145078 albuminuric

---
+++ 1492145078.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      439313134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int16_t  f0;
+   uint32_t  f1;
+   uint32_t  f2;
+   uint64_t  f3;
+   volatile signed f4 : 20;
+   uint32_t  f5;
+   volatile unsigned f6 : 6;
+   int16_t  f7;
+};
