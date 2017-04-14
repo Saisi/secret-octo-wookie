092211eb6b3b508881dede6b8db25679afb58c7d
saisi <saisi@users.noreply.github.com>
2017-04-14 11:15:15 +0000

    1492168514 agrypnia

---
+++ 1492168514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1442037495
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_12 = 0xE9L;
+static uint16_t g_29 = 0x136DL;
+static int32_t g_33[3] = {(-1L),(-1L),(-1L)};
+static uint16_t g_67 = 65529UL;
+static int32_t g_69 = 0L;
