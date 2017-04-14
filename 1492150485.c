78797d8e27a694e0a0c68acde6fe48c70fac6f89
saisi <saisi@users.noreply.github.com>
2017-04-14 06:14:46 +0000

    1492150485 arrested

---
+++ 1492150485.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3448129711
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   volatile unsigned f1 : 27;
+   signed f2 : 5;
+   const volatile unsigned f3 : 21;
+   volatile signed f4 : 8;
+};
+#pragma pack(pop)
