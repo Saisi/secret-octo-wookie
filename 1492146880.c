3662ca8640d784a929350f38115b5c2d49a84815
saisi <saisi@users.noreply.github.com>
2017-04-14 05:14:41 +0000

    1492146880 azogreen

---
+++ 1492146880.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1331666192
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 4L;
+static volatile int32_t g_52 = 0xDFEA44E0L;/* VOLATILE GLOBAL g_52 */
+static uint32_t g_54 = 4294967295UL;
+static int16_t g_57 = (-1L);
+static int8_t g_59 = 0x48L;
+static uint32_t g_72 = 1UL;
+static uint32_t g_89 = 0x7944D42BL;
+static int64_t g_92 = 0x0EAEAAC79E06D3C1LL;
+static uint32_t g_93 = 0x36BC8474L;
