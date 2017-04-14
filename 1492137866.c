6f8d1b4ea7a1cd8776750a35f1ad2e068557f061
saisi <saisi@users.noreply.github.com>
2017-04-14 02:44:27 +0000

    1492137866 americanizing

---
+++ 1492137866.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2306987582
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   int32_t  f1;
+   uint16_t  f2;
+   unsigned f3 : 4;
+   int64_t  f4;
+   const volatile uint16_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
