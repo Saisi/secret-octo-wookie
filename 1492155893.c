d6ec3ecaa57498e9812075e51483d0926b3d3436
saisi <saisi@users.noreply.github.com>
2017-04-14 07:44:53 +0000

    1492155893 acroamatics

---
+++ 1492155893.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2921024613
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   unsigned f1 : 4;
+   signed f2 : 23;
+   signed f3 : 6;
+};
+#pragma pack(pop)
+
