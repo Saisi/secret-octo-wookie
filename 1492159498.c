5d160b5adc9a67159795ff623f8167c7e4e3b969
saisi <saisi@users.noreply.github.com>
2017-04-14 08:44:58 +0000

    1492159498 Abigale

---
+++ 1492159498.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4228243970
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-6L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 2L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xF2EE76F5L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[10][2] = {{0x3AD3B315L,(-1L)},{0L,0L},{(-1L),0x3AD3B315L},{9L,0x3AD3B315L},{(-1L),0L},{0L,(-1L)},{0x3AD3B315L,9L},{0x3AD3B315L,(-1L)},{0L,0L},{(-1L),0x3AD3B315L}};
+static volatile int32_t g_9 = (-1L);/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10[5] = {0L,0L,0L,0L,0L};
+static int32_t g_11 = (-1L);
+static int32_t g_24[3][8] = {{4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L},{4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L},{4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L,4L,0x5A8DF044L}};
+static int32_t *g_32 = &g_24[0][3];
