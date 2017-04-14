56f354bd58d6195909bcc0d4a5943c35a8dae9ab
saisi <saisi@users.noreply.github.com>
2017-04-14 15:15:34 +0000

    1492182934 Autrans

---
+++ 1492182934.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4228139656
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 18;
+   unsigned f1 : 31;
+   volatile unsigned f2 : 5;
+   volatile signed f3 : 29;
+   unsigned f4 : 24;
+};
+
+#pragma pack(push)
+#pragma pack(1)
