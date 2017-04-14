dca098049d965039ff157267223c2cdd0d276881
saisi <saisi@users.noreply.github.com>
2017-04-14 12:15:19 +0000

    1492172119 arsonist

---
+++ 1492172119.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3392093474
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-5L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
