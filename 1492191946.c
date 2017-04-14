b4c1d1a992710eceee8fa08251ff29c1f36e4ff5
saisi <saisi@users.noreply.github.com>
2017-04-14 17:45:46 +0000

    1492191946 Amphibiotica

---
+++ 1492191946.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3173179526
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 17;
+   uint8_t  f1;
+   unsigned f2 : 28;
+   volatile signed f3 : 24;
+   signed f4 : 7;
+   volatile unsigned f5 : 2;
+};
+
+union U1 {
