8cbf318da4a9704181177e34f14ac7e657717dfb
saisi <saisi@users.noreply.github.com>
2017-04-14 12:45:22 +0000

    1492173921 anticonstitutionally

---
+++ 1492173921.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3281066727
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   uint64_t  f1;
+   const uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_8[9][2] = {{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)},{(-5L),(-5L)}};
+static int8_t g_23 = 0L;
+static uint64_t g_46 = 0xA3821C760FC5C2F0LL;
