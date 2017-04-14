f9014bd31307163f59a30a485f42efdf2b6d97f1
saisi <saisi@users.noreply.github.com>
2017-04-14 13:45:27 +0000

    1492177527 anaxagorean

---
+++ 1492177527.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1417184659
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   volatile int64_t  f1;
+   volatile int64_t  f2;
+   const unsigned f3 : 22;
+   uint32_t  f4;
+};
+
+struct S1 {
+   unsigned f0 : 7;
