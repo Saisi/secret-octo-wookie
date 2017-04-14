89889916d0a018f10a8a0993cb5898ccccddc806
saisi <saisi@users.noreply.github.com>
2017-04-14 16:45:41 +0000

    1492188341 archistome

---
+++ 1492188341.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      941330550
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 9;
+   const signed f1 : 26;
+   const volatile unsigned f2 : 20;
+   volatile unsigned f3 : 16;
+};
+#pragma pack(pop)
+
