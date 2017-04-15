fac1647fa0820e684ed6c4899057c3dfaff4b573
saisi <saisi@users.noreply.github.com>
2017-04-15 03:46:38 +0000

    1492227996 anorthography

---
+++ 1492227996.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2319989606
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6[7] = {0L,0L,0L,0L,0L,0L,0L};
+static int32_t g_54 = (-1L);
+static uint16_t g_56 = 65526UL;
+static uint64_t g_76 = 0x288A3D85AA7F794FLL;
+static uint8_t g_77 = 0xD9L;
+static uint32_t g_81 = 18446744073709551615UL;
+static uint32_t g_89 = 0x0A4EDB9DL;
+static uint32_t *g_96 = &g_89;
+static uint32_t **g_95 = &g_96;
