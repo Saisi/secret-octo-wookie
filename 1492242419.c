52b7c8fe21d6055542dbd178306d9bf560a6a3e1
saisi <saisi@users.noreply.github.com>
2017-04-15 07:47:01 +0000

    1492242419 apophyges

---
+++ 1492242419.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3644296663
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   volatile int32_t  f1;
+   const unsigned f2 : 9;
+   unsigned f3 : 9;
+   unsigned f4 : 12;
+   unsigned f5 : 10;
+   unsigned f6 : 5;
+   uint16_t  f7;
+   volatile signed f8 : 14;
