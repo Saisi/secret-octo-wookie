fc1f1a36c22d4a3f6572a4edf1e6c55879e52e5a
saisi <saisi@users.noreply.github.com>
2017-04-15 13:47:37 +0000

    1492264056 aldermanity

---
+++ 1492264056.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1894893879
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   int64_t  f1;
+   int32_t  f2;
+   volatile signed f3 : 8;
+   int64_t  f4;
+   int16_t  f5;
+   volatile signed f6 : 17;
+};
+
