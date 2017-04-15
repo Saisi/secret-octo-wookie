0dec9c3a3494b2ffa1ff5a45228c4dd0d8651f98
saisi <saisi@users.noreply.github.com>
2017-04-15 12:17:29 +0000

    1492258649 acapnial

---
+++ 1492258649.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3121380757
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   signed f1 : 28;
+   volatile signed f2 : 15;
+   const unsigned f3 : 30;
+};
+#pragma pack(pop)
+
