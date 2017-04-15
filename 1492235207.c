18eae660f6cb7706b3df88f24b790f52cf70f9eb
saisi <saisi@users.noreply.github.com>
2017-04-15 05:46:48 +0000

    1492235207 ailweed

---
+++ 1492235207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3758699318
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 9;
+   const volatile signed f1 : 26;
+   const unsigned f2 : 20;
+   const unsigned f3 : 16;
+   signed f4 : 31;
+   const volatile int64_t  f5;
+};
+
+struct S1 {
