9efb61d0d8dbff4d0816b56280abde228e8cd4bb
saisi <saisi@users.noreply.github.com>
2017-04-15 21:18:15 +0000

    1492291095 ascarides

---
+++ 1492291095.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2048087909
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   volatile int8_t  f1;
+   const volatile int8_t  f2;
+   uint16_t  f3;
+   const volatile int8_t  f4;
+   const uint16_t  f5;
+   const int64_t  f6;
+   int64_t  f7;
+};
