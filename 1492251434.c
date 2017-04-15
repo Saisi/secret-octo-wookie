1cccaaa70fe6da72befdb220bf431e088aede044
saisi <saisi@users.noreply.github.com>
2017-04-15 10:17:15 +0000

    1492251434 Astropectinidae

---
+++ 1492251434.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2238619386
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int8_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static volatile uint16_t g_8 = 0xBB9CL;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
