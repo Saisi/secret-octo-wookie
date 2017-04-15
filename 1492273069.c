f65b39a0eab40471d0fe0a80af4f1982df7a1185
saisi <saisi@users.noreply.github.com>
2017-04-15 16:17:49 +0000

    1492273069 alchemise

---
+++ 1492273069.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2669519551
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_3 = 0xE5CCL;
+static uint64_t g_5 = 0x94A2A4F402E31D83LL;
+static volatile int32_t g_58 = 2L;/* VOLATILE GLOBAL g_58 */
+static int32_t g_88[4][5][1] = {{{7L},{1L},{0x81E2E8C4L},{(-1L)},{(-1L)}},{{0x0E9AFB84L},{(-1L)},{(-1L)},{0x81E2E8C4L},{1L}},{{7L},{1L},{0x81E2E8C4L},{(-1L)},{(-1L)}},{{0x0E9AFB84L},{(-1L)},{(-1L)},{0x81E2E8C4L},{1L}}};
+static uint8_t g_102 = 1UL;
+static uint8_t g_104 = 0UL;
+static uint64_t g_116 = 7UL;
+static uint16_t g_117 = 0xCD9EL;
+static uint32_t g_129 = 0xADA2C71BL;
