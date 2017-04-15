650707fe40ec7ea9f48e8696fa459009ca32e646
saisi <saisi@users.noreply.github.com>
2017-04-15 20:18:10 +0000

    1492287490 armorwise

---
+++ 1492287490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      633113811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0L;
+static int32_t *g_7 = &g_5;
+static uint16_t g_73 = 65535UL;
+static uint8_t g_83[2][1] = {{0UL},{0UL}};
+static uint32_t g_85 = 1UL;
+static uint32_t g_87 = 0x713981DCL;
+static int32_t g_89 = 0x13DB3B8CL;
+static volatile uint16_t g_91 = 0UL;/* VOLATILE GLOBAL g_91 */
+static uint16_t *g_116 = &g_73;
