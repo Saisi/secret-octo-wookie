ee44938781af233148c65468cb8a6a285131b902
saisi <saisi@users.noreply.github.com>
2017-04-15 04:46:43 +0000

    1492231602 Algarsife

---
+++ 1492231602.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1381732908
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 4;
+   const signed f1 : 9;
+   volatile unsigned f2 : 7;
+   signed f3 : 23;
+   signed f4 : 10;
+   signed f5 : 25;
+   volatile unsigned f6 : 19;
