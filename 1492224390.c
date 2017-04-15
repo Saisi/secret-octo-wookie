8ff920e82d3a2908be089ad5567836102c7ad3e6
saisi <saisi@users.noreply.github.com>
2017-04-15 02:46:31 +0000

    1492224390 antheridia

---
+++ 1492224390.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      994362271
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * volatile  f0;
+   volatile uint8_t  f1;
+   int32_t  f2;
+};
+
+union U1 {
+   uint8_t  f0;
+   int8_t * f1;
+   const signed f2 : 25;
