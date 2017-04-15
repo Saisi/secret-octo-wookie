eea24637a0a7c7e95d5c494ede62c33b4193049b
saisi <saisi@users.noreply.github.com>
2017-04-15 02:16:28 +0000

    1492222588 arpeggio

---
+++ 1492222588.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      860038224
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 0x82EAC453L;
+static int32_t g_6 = (-1L);
+static volatile int32_t g_15 = 0xD069F498L;/* VOLATILE GLOBAL g_15 */
+static volatile uint32_t g_16 = 0xB0B4EF66L;/* VOLATILE GLOBAL g_16 */
