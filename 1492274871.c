177e5da4262ea027b5d782cd780e961a8a676561
saisi <saisi@users.noreply.github.com>
2017-04-15 16:47:52 +0000

    1492274871 archecclesiastic

---
+++ 1492274871.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3399381122
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x76F5020FL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-6L);
+static int16_t g_7 = 0xFF3DL;
+static uint16_t g_32 = 0xCC09L;
+static int16_t g_43 = 4L;
+static int16_t *g_42 = &g_43;
+static int16_t * const *g_41 = &g_42;
+static int32_t g_93[4] = {(-3L),(-3L),(-3L),(-3L)};
+static int32_t *g_92 = &g_93[1];
