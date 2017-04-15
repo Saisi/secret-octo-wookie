7413523537c22c7fec13354b373c42c487b4bb71
saisi <saisi@users.noreply.github.com>
2017-04-15 21:48:18 +0000

    1492292897 accumulativeness

---
+++ 1492292897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2822347506
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile int64_t  f0;
+   int64_t  f1;
+   const uint64_t  f2;
+   volatile int64_t  f3;
+};
+
+struct S1 {
+   int16_t  f0;
+};
