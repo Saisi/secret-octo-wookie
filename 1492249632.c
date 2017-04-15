c7dd1cffcf1489e457ad632a657d461cc8a1995c
saisi <saisi@users.noreply.github.com>
2017-04-15 09:47:12 +0000

    1492249632 antifemale

---
+++ 1492249632.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1375926818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xDC85F70AL;
+static volatile int32_t g_5 = 0x46158D32L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 6L;
+static int32_t g_11 = 1L;
+static int32_t * const  volatile g_10 = &g_11;/* VOLATILE GLOBAL g_10 */
+static int32_t g_51 = 1L;
+static uint8_t g_68 = 0UL;
+static int32_t g_83 = (-10L);
+static int8_t g_87[9] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
