d9a71d620d48e278bc95c2886603e3e29be83cbd
saisi <saisi@users.noreply.github.com>
2017-04-15 07:16:57 +0000

    1492240616 attractableness

---
+++ 1492240616.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1878612366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   unsigned f1 : 22;
+};
+
+union U1 {
+   const int32_t  f0;
+   volatile uint64_t  f1;
+   uint16_t  f2;
+   int32_t  f3;
