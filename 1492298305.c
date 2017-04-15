601665fe34f8d92b758d72c61294c6012686740c
saisi <saisi@users.noreply.github.com>
2017-04-15 23:18:26 +0000

    1492298305 Axa

---
+++ 1492298305.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2559128243
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const volatile signed f1 : 15;
+};
+
+union U1 {
+   int8_t * volatile  f0;
+   volatile unsigned f1 : 3;
+   volatile uint32_t  f2;
+   uint16_t  f3;
