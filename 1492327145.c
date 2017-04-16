85b9342e228b584f709f56ef817c148209bb7100
saisi <saisi@users.noreply.github.com>
2017-04-16 07:19:07 +0000

    1492327145 assiduousnesses

---
+++ 1492327145.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      989342585
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 27;
+   volatile unsigned f1 : 6;
+   const signed f2 : 17;
+   signed f3 : 24;
+   signed f4 : 9;
+   const signed f5 : 30;
+   volatile unsigned f6 : 5;
+   unsigned f7 : 4;
+};
