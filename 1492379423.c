c1e9e4e10f47ff18ebc3e886e97efdb62aacecd7
saisi <saisi@users.noreply.github.com>
2017-04-16 21:50:24 +0000

    1492379423 Archcape

---
+++ 1492379423.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3494440624
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0x30722263L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t *g_2 = &g_3;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
