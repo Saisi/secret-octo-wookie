f1cb6740baea1f5e71dff1dc5fa3675e95cd072d
saisi <saisi@users.noreply.github.com>
2017-04-16 14:19:42 +0000

    1492352382 adiathermancy

---
+++ 1492352382.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1339720938
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 6L;
+static int32_t *g_13 = &g_14;
+static int32_t g_24 = 0x81A82E77L;
+static int32_t g_38[10] = {0xDBC2A41BL,4L,4L,4L,4L,0xDBC2A41BL,4L,4L,4L,4L};
+static int32_t *g_37[7] = {&g_38[5],&g_38[5],&g_38[5],&g_38[5],&g_38[5],&g_38[5],&g_38[5]};
