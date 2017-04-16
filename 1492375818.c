443baf01c1409421cd454075e9f18710d9fd1b59
saisi <saisi@users.noreply.github.com>
2017-04-16 20:50:19 +0000

    1492375818 Alleghany

---
+++ 1492375818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1459601837
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 21;
+   const int32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_4 = 0xA0E6L;
