d4b106cb45b39cfbded212f96f551d5ad1666c89
saisi <saisi@users.noreply.github.com>
2017-04-16 18:50:08 +0000

    1492368607 acetylperoxide

---
+++ 1492368607.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3552716850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint32_t  f1;
+   int16_t  f2;
+   volatile uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static uint32_t g_12 = 0x60CBEB96L;
