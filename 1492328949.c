242a9ecbbd32b87720d58918d537711c2a048418
saisi <saisi@users.noreply.github.com>
2017-04-16 07:49:10 +0000

    1492328949 allotrylic

---
+++ 1492328949.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3629331237
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 17;
+   unsigned f1 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[4] = {1L,1L,1L,1L};
+static int32_t g_9 = 9L;
+static int32_t * volatile g_8 = &g_9;/* VOLATILE GLOBAL g_8 */
+static int32_t **g_14 = (void*)0;
