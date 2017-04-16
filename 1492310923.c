cb0b6d293817adf17f5f708de7f4d93e09d6c2c5
saisi <saisi@users.noreply.github.com>
2017-04-16 02:48:43 +0000

    1492310923 Alidus

---
+++ 1492310923.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      714911434
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   signed f1 : 16;
+   unsigned f2 : 4;
+   const unsigned f3 : 22;
+   uint32_t  f4;
+   signed f5 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
