c2be2c3dfd71e0aef53947ae3e327fc483368255
saisi <saisi@users.noreply.github.com>
2017-04-16 16:19:53 +0000

    1492359592 Astraean

---
+++ 1492359592.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1544878134
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 9;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x1F2C29A3L;
+static uint8_t g_12 = 0xDFL;
+static uint64_t g_13 = 0x83989226C312FBB6LL;
+static uint32_t g_44 = 0x61FB9A31L;
