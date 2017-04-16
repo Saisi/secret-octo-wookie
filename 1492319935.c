5b07a5c17f1bb94eec1fd9adf729dfb38eb7f5e4
saisi <saisi@users.noreply.github.com>
2017-04-16 05:18:55 +0000

    1492319935 ancylostome

---
+++ 1492319935.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      684800918
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   int32_t  f0;
+   signed f1 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U1 g_2 = {0xC5A654FDL};
+
+
+/* --- FORWARD DECLARATIONS --- */
