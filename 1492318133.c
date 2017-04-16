df7dc5ac369221ffcf3b3a689f82af6a2dfcf1a2
saisi <saisi@users.noreply.github.com>
2017-04-16 04:48:53 +0000

    1492318133 baleen

---
+++ 1492318133.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      531625417
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 3;
+   const volatile signed f1 : 14;
+   unsigned f2 : 25;
+   volatile unsigned : 0;
+   signed f3 : 17;
+   unsigned f4 : 6;
+   signed f5 : 13;
