8f8dcc1ccd9049bc557130aaadcaa44c52f9fc8f
saisi <saisi@users.noreply.github.com>
2017-04-16 11:19:28 +0000

    1492341568 adobos

---
+++ 1492341568.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1541568250
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_16 = (-1L);
+static int32_t g_21[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int8_t g_50 = 0L;
+static int32_t g_84 = (-1L);
+static int64_t g_96 = 0x5A1B6BD7A89A43D5LL;
+static int8_t g_98 = (-1L);
+static int8_t g_124 = (-1L);
+static int32_t g_126 = 7L;
+static int32_t * volatile g_125 = &g_126;/* VOLATILE GLOBAL g_125 */
