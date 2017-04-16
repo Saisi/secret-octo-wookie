86dcdc758702d37371dec1a3156f9d2c5d23201d
saisi <saisi@users.noreply.github.com>
2017-04-16 21:20:22 +0000

    1492377621 albe

---
+++ 1492377621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3711479934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 2L;
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static int32_t g_6 = (-1L);
+static int64_t g_7 = 4L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
