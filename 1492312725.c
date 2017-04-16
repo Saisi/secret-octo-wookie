c95bcc23dbb4596f29feef00986c856dd953e484
saisi <saisi@users.noreply.github.com>
2017-04-16 03:18:46 +0000

    1492312725 ammoniac

---
+++ 1492312725.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1550986289
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile int32_t g_21 = 0xE1E01B17L;/* VOLATILE GLOBAL g_21 */
+static uint16_t g_26 = 1UL;
+static int8_t g_33 = 0x0CL;
+static int32_t g_46 = 3L;
+static int32_t g_74 = 0x6B8CC39BL;
+static int8_t g_75 = 0xEBL;
+static int8_t g_76 = 0x91L;
+static uint64_t g_77[1] = {0x3D53AF4B502D4801LL};
+static uint8_t g_135 = 4UL;
