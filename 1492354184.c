41d4e4ae2671a23d3831e0732de4972f175820b5
saisi <saisi@users.noreply.github.com>
2017-04-16 14:49:45 +0000

    1492354184 antiscolic

---
+++ 1492354184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2091647797
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 25;
+   signed f1 : 12;
+   unsigned f2 : 31;
+   volatile signed f3 : 14;
+   unsigned f4 : 7;
+   volatile unsigned f5 : 4;
+};
+
+#pragma pack(push)
