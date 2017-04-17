5cbb2554cea607ccb4fe50051d8e45088e64733d
saisi <saisi@users.noreply.github.com>
2017-04-17 07:51:17 +0000

    1492415476 acutorsion

---
+++ 1492415476.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1960007661
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 21;
+   volatile signed f1 : 24;
+   const volatile unsigned f2 : 27;
+   signed f3 : 26;
+   unsigned f4 : 3;
+   signed f5 : 16;
+   signed f6 : 15;
+   unsigned f7 : 6;
+};
