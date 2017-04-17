2cafbe7a640fe3457a40f7f8711bc431a664dc53
saisi <saisi@users.noreply.github.com>
2017-04-17 18:52:21 +0000

    1492455141 attributor

---
+++ 1492455141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1548372518
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-10L);
+static int32_t g_6[10] = {(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L),(-9L)};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
