ec2c172fd7a497648564979da214470353075a76
saisi <saisi@users.noreply.github.com>
2017-04-17 08:51:22 +0000

    1492419081 atomises

---
+++ 1492419081.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      836542615
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 10;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[2][5] = {{(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L)}};
+static int8_t g_5[3][10] = {{(-1L),7L,0xDDL,0x6FL,3L,3L,0x6FL,0xDDL,7L,(-1L)},{(-5L),7L,0L,3L,0x23L,1L,3L,1L,0x23L,3L},{3L,1L,3L,7L,0x23L,1L,(-1L),(-1L),(-1L),(-1L)}};
+static int32_t g_6 = (-10L);
+static volatile int32_t g_49 = 3L;/* VOLATILE GLOBAL g_49 */
