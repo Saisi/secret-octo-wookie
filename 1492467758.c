6b5c43dbba40931e44e489f05ddbe1bb535ba9b9
saisi <saisi@users.noreply.github.com>
2017-04-17 22:22:39 +0000

    1492467758 abolitionise

---
+++ 1492467758.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2907178217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   int64_t  f1;
+   uint16_t  f2;
+   const uint64_t  f3;
+   volatile int16_t  f4;
+};
+
+union U1 {
+   const signed f0 : 5;
