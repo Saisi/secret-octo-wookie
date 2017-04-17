d2ae2cbe0d1da054318bc1d0517995ef5c07c919
saisi <saisi@users.noreply.github.com>
2017-04-17 12:51:43 +0000

    1492433503 anticosmetic

---
+++ 1492433503.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      324625347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static uint8_t g_14 = 3UL;
+static int64_t g_74 = 1L;
+static uint16_t g_96 = 0x47F6L;
+static uint64_t g_98 = 9UL;
+static volatile uint32_t g_122 = 7UL;/* VOLATILE GLOBAL g_122 */
+static uint8_t g_146[1] = {2UL};
+static int16_t g_174 = 1L;
+static int32_t g_176 = 0x50D549ECL;
