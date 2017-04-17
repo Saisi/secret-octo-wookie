c4176c487b96cd210fe30769ee7e7ae8e37e9754
saisi <saisi@users.noreply.github.com>
2017-04-17 11:51:39 +0000

    1492429897 antianarchist

---
+++ 1492429897.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1326802898
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   volatile signed f1 : 29;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x1A770AACL;
+static uint64_t g_16 = 0x6CF6D70783DB47DCLL;
+static int8_t g_25 = 0x6AL;
