e997da8733d1581ef834594886db7d824bd2c01b
saisi <saisi@users.noreply.github.com>
2017-04-17 07:21:14 +0000

    1492413673 arterioscleroses

---
+++ 1492413673.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1055045591
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 10;
+   int16_t  f1;
+   unsigned f2 : 9;
+};
+
+union U1 {
+   volatile uint8_t  f0;
+   volatile uint8_t  f1;
+   volatile uint32_t  f2;
