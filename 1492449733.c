b7abbc298737db98532251fbc01972791db138e8
saisi <saisi@users.noreply.github.com>
2017-04-17 17:22:15 +0000

    1492449733 band

---
+++ 1492449733.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1354911510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const volatile uint32_t  f1;
+   int8_t  f2;
+   uint8_t  f3;
+   volatile uint8_t  f4;
+   unsigned f5 : 15;
+};
+
+struct S1 {
