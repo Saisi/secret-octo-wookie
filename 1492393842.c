feea9fc14b61270aedbb892fb3113b96c6f85c58
saisi <saisi@users.noreply.github.com>
2017-04-17 01:50:43 +0000

    1492393842 arabesques

---
+++ 1492393842.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4293412403
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const uint16_t  f1;
+   signed f2 : 31;
+   const int16_t  f3;
+   uint32_t  f4;
+   int64_t  f5;
+   int32_t  f6;
+   const volatile uint32_t  f7;
+   int8_t  f8;
