6a9c2eaa6d1ef01ef08f07a1dac8d4cb61531227
saisi <saisi@users.noreply.github.com>
2017-04-17 10:51:31 +0000

    1492426291 aerophagist

---
+++ 1492426291.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      145402588
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const unsigned f1 : 1;
+   signed f2 : 31;
+   const uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6][7] = {{0x27603859L,0xC170CD50L,0xC170CD50L,0x27603859L,1L,0xBFC88EB9L,1L},{0x978F3C34L,1L,1L,0x978F3C34L,0x65653D42L,(-2L),0x65653D42L},{0x27603859L,0xC170CD50L,0xC170CD50L,0x27603859L,1L,0xBFC88EB9L,1L},{0x978F3C34L,1L,1L,0x978F3C34L,0x65653D42L,(-2L),0x65653D42L},{0x27603859L,0xC170CD50L,0xC170CD50L,0x27603859L,1L,0xBFC88EB9L,1L},{0x978F3C34L,1L,1L,0x978F3C34L,0x65653D42L,(-2L),0x65653D42L}};
+static uint8_t g_19 = 1UL;
