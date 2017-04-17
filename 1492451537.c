d87d4cbe5b0275cc0fa27cb70344a3d927c9fa57
saisi <saisi@users.noreply.github.com>
2017-04-17 17:52:17 +0000

    1492451537 alpinisms

---
+++ 1492451537.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      493167336
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[4][4][5] = {{{0UL,0UL,0x28L,0UL,255UL},{1UL,0UL,4UL,0x5CL,0x9DL},{0x5CL,0UL,0UL,0UL,0UL},{255UL,4UL,251UL,0UL,0xB1L}},{{255UL,0xB1L,0xB3L,0x5CL,0UL},{0xA8L,0x28L,0x9DL,0UL,0UL},{255UL,0xA8L,0UL,0xA8L,255UL},{255UL,0x7DL,0UL,0x9DL,4UL}},{{0x5CL,0xF1L,0x9DL,251UL,1UL},{1UL,0xA8L,0x7DL,0x28L,1UL},{0xF1L,4UL,4UL,0xF1L,0xA8L},{1UL,4UL,0UL,1UL,0xF1L}},{{0x28L,0xA8L,1UL,255UL,0xB3L},{0UL,255UL,0x5CL,1UL,1UL},{255UL,0x28L,255UL,0xF1L,0UL},{255UL,251UL,255UL,0x28L,0UL}}};
+static int32_t g_3 = 4L;
+static volatile int32_t g_4 = 0x779C63C4L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
