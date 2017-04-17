2e6dce2843fcd55b010eea183b522120ab11795b
saisi <saisi@users.noreply.github.com>
2017-04-17 20:22:30 +0000

    1492460549 aphacic

---
+++ 1492460549.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1752654433
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   int32_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0UL;
+static uint64_t g_27 = 0x5FA3187110E6B6CDLL;
+static uint16_t g_46 = 0x383DL;
