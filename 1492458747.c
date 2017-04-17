701da8738e7012c21284b633e34284780612f77d
saisi <saisi@users.noreply.github.com>
2017-04-17 19:52:27 +0000

    1492458747 Aveline

---
+++ 1492458747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      480716487
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xB0BF2465L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x21E0BEC0L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0L;
+static volatile int32_t g_7 = 9L;/* VOLATILE GLOBAL g_7 */
