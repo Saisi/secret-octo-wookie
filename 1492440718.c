ad127cc57a4288323fe6644655343b64d1c54943
saisi <saisi@users.noreply.github.com>
2017-04-17 14:51:59 +0000

    1492440718 Albanese

---
+++ 1492440718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2627764525
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   uint8_t  f1;
+   volatile uint16_t  f2;
+   volatile uint16_t  f3;
+   uint16_t  f4;
+   const int8_t  f5;
+   signed f6 : 16;
