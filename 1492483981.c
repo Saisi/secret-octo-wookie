0af0c1799a6aebe4a48ce54d0edf2fe529d0dbd2
saisi <saisi@users.noreply.github.com>
2017-04-18 02:53:02 +0000

    1492483981 Ampere

---
+++ 1492483981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3631886697
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   int32_t  f1;
+   const uint64_t  f2;
+   unsigned f3 : 10;
+   int32_t  f4;
+   const uint32_t  f5;
+   uint32_t  f6;
+   uint32_t  f7;
+};
