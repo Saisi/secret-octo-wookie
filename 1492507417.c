b00c3dbc9aa2efd1f6ec83b7b52e137e2b1ad9bd
saisi <saisi@users.noreply.github.com>
2017-04-18 09:23:38 +0000

    1492507417 Aphroditeum

---
+++ 1492507417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3600594368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 14;
+   const uint8_t  f1;
+   const uint64_t  f2;
+   unsigned f3 : 9;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_15 = 250UL;
