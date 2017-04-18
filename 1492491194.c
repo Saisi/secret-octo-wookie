00b7c72199d281efb2df3d146060990af65bdf24
saisi <saisi@users.noreply.github.com>
2017-04-18 04:53:15 +0000

    1492491194 anoles

---
+++ 1492491194.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1765119676
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   volatile uint32_t  f1;
+   const signed f2 : 4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile unsigned f0 : 17;
