a9b913271590fbdc0a6b78e04362bff10f6ce1e6
saisi <saisi@users.noreply.github.com>
2017-04-18 13:24:00 +0000

    1492521840 bandsaw

---
+++ 1492521840.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3741960637
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_17 = 0xCF5AL;
+static int64_t g_54 = 0xF2B7826243A5D8D3LL;
+static int32_t g_68 = 0x64F89C89L;
+static int32_t *g_79 = (void*)0;
+static int32_t ** const  volatile g_78 = &g_79;/* VOLATILE GLOBAL g_78 */
+static volatile int64_t g_85 = 0x35453F201F9D99D0LL;/* VOLATILE GLOBAL g_85 */
+static volatile int64_t *g_84 = &g_85;
+static uint8_t g_110[8] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL};
+static int8_t g_117 = (-9L);
