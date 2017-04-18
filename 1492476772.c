0bd4d0117da892b717dd2412e542bebd3fcba862
saisi <saisi@users.noreply.github.com>
2017-04-18 00:52:52 +0000

    1492476772 antimachine

---
+++ 1492476772.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      981704195
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   volatile signed f1 : 1;
+   volatile signed f2 : 24;
+   signed f3 : 15;
+   signed f4 : 14;
+   signed f5 : 30;
+   volatile int16_t  f6;
+   volatile signed f7 : 30;
+   signed f8 : 1;
