d8bcde0bf00f5ab40d128ee7556b6d5db4f0e70d
saisi <saisi@users.noreply.github.com>
2017-04-18 11:53:50 +0000

    1492516430 angularly

---
+++ 1492516430.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3659935854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 5;
+   volatile unsigned f1 : 6;
+   unsigned f2 : 16;
+   signed f3 : 28;
+};
+
+union U1 {
+   uint16_t  f0;
+   uint16_t  f1;
