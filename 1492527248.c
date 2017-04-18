8a82c8edd1d1b52b6b5de3f2f2ae61ab24a0385b
saisi <saisi@users.noreply.github.com>
2017-04-18 14:54:09 +0000

    1492527248 Alpujarra

---
+++ 1492527248.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      624274367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 30;
+   unsigned f1 : 29;
+   signed f2 : 12;
+   signed f3 : 7;
+   volatile signed f4 : 28;
+   volatile unsigned f5 : 5;
+   const volatile signed f6 : 16;
+};
+
