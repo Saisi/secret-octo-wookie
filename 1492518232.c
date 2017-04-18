83440733aad7cd158374f6d1b011315a2aaa8792
saisi <saisi@users.noreply.github.com>
2017-04-18 12:23:54 +0000

    1492518232 antambulacral

---
+++ 1492518232.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4228281649
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 23;
+   signed f1 : 18;
+   unsigned f2 : 13;
+   signed f3 : 4;
+   const signed f4 : 5;
+   const unsigned f5 : 10;
+};
+
+struct S1 {
