8ddaf28514d34d793217eaf51d487e823dc90ce1
saisi <saisi@users.noreply.github.com>
2017-04-18 04:23:12 +0000

    1492489392 axometric

---
+++ 1492489392.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1369668402
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_16 = 0x0DL;/* VOLATILE GLOBAL g_16 */
+static int32_t g_17 = 1L;
+static int32_t g_28 = 0x52B4B5A9L;
+static uint64_t g_63 = 0x702B5BDDDD5D57D3LL;
+static int32_t g_65 = 0xC8BB6AF8L;
+static uint32_t g_71 = 0x987F9BD8L;
+static const volatile uint32_t g_109[10] = {4294967295UL,0x0BB484B0L,4294967295UL,4294967295UL,0x0BB484B0L,4294967295UL,4294967295UL,0x0BB484B0L,4294967295UL,4294967295UL};
+static const volatile uint32_t *g_108 = &g_109[0];
+static const volatile uint32_t **g_107 = &g_108;
