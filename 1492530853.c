fb264bc33ca699f9605a49ea75ecac75d433e2a9
saisi <saisi@users.noreply.github.com>
2017-04-18 15:54:13 +0000

    1492530853 Agle

---
+++ 1492530853.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1303563595
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x0F7A73D2L;
+static uint64_t g_13 = 0xE5E81A7ECC2443ACLL;
+static int8_t g_30 = 0L;
+static uint8_t g_42 = 254UL;
