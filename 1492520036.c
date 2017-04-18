141535db232f64105d0caefe09a2935adb059fb2
saisi <saisi@users.noreply.github.com>
2017-04-18 12:53:58 +0000

    1492520036 angiothlipsis

---
+++ 1492520036.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3765223369
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 3;
+   unsigned f1 : 14;
+   const volatile unsigned f2 : 25;
+   unsigned : 0;
+   unsigned f3 : 17;
+   const volatile signed f4 : 6;
+   signed f5 : 13;
+   unsigned f6 : 12;
+   signed f7 : 31;
