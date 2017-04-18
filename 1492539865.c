7216f4bc237ede3b75578acec9f3e33471e0cf59
saisi <saisi@users.noreply.github.com>
2017-04-18 18:24:26 +0000

    1492539865 autoregulatory

---
+++ 1492539865.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2549838695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 18;
+   signed f1 : 1;
+   signed : 0;
+   unsigned f2 : 11;
+   volatile int32_t  f3;
+   volatile unsigned f4 : 18;
+   unsigned f5 : 13;
