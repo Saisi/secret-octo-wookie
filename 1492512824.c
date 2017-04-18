73e7f34948db88de1176b4a391964de1fd53e552
saisi <saisi@users.noreply.github.com>
2017-04-18 10:53:45 +0000

    1492512824 abominator

---
+++ 1492512824.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1078632142
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 21;
+   const unsigned f1 : 22;
+   signed : 0;
+   volatile unsigned f2 : 12;
+   signed f3 : 11;
+   uint32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
