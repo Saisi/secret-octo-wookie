1de20fa2b7880a94f7b0c1f55a39cb52f4202d8e
saisi <saisi@users.noreply.github.com>
2017-04-18 20:54:42 +0000

    1492548882 antibishop

---
+++ 1492548882.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3458576296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   volatile uint32_t  f0;
+   volatile unsigned f1 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x1FC3EEE2L;
+static volatile int32_t g_6 = 0x8F9C25B0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static union U1 g_8 = {18446744073709551610UL};/* VOLATILE GLOBAL g_8 */
