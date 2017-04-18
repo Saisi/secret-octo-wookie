de96e962045df0a24e0a34168c6bc4dd7e4f1bcc
saisi <saisi@users.noreply.github.com>
2017-04-18 20:24:39 +0000

    1492547079 axenically

---
+++ 1492547079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1134713187
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   signed f1 : 7;
+   const signed f2 : 14;
+   const volatile unsigned f3 : 1;
+   signed f4 : 14;
+   signed f5 : 15;
+   unsigned f6 : 18;
