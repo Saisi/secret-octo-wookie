e6e0c0173dad9e2d3c4ab4df32864a0fefe81b7c
saisi <saisi@users.noreply.github.com>
2017-04-18 21:24:44 +0000

    1492550684 Aliacensis

---
+++ 1492550684.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3200993441
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   int32_t  f0;
+   volatile uint64_t  f1;
+   uint64_t  f2;
+   int32_t  f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2 = 0xEEL;
