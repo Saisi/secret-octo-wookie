3c601782c9259955407dc67145d0841531a87e3c
saisi <saisi@users.noreply.github.com>
2017-04-19 16:26:24 +0000

    1492619184 agatizing

---
+++ 1492619184.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4200864929
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_15[2] = {0x27439CB6L,0x27439CB6L};
+static int32_t *g_14 = &g_15[0];
+static volatile int32_t g_16 = 0L;/* VOLATILE GLOBAL g_16 */
+static volatile int32_t g_17[2] = {0xFB1C0380L,0xFB1C0380L};
+static volatile int32_t g_18 = 0x9328A062L;/* VOLATILE GLOBAL g_18 */
+static int32_t g_19[2] = {0x0CD9D754L,0x0CD9D754L};
+static uint16_t g_42 = 0x1BABL;
+static int32_t g_76 = 0xE7D51263L;
+static int8_t g_78 = 0x93L;
