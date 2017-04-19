5e2ba97440f510f8473073c22df111929d34a4ef
saisi <saisi@users.noreply.github.com>
2017-04-19 19:56:40 +0000

    1492631800 apophyges

---
+++ 1492631800.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4155918575
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   const uint32_t  f1;
+   const int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x4C506D44L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[3][7] = {{0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L},{(-4L),(-4L),(-4L),(-4L),(-4L),(-4L),(-4L)},{0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L,0x2BD7B1C2L}};
+static int32_t g_24 = 0x8FB8CEFFL;
