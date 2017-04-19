4747a3c9aa7772c24b53ad7d985b6c789698352e
saisi <saisi@users.noreply.github.com>
2017-04-19 11:26:00 +0000

    1492601159 balanoposthitis

---
+++ 1492601159.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3799935504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 22;
+   volatile unsigned f1 : 19;
+   unsigned f2 : 9;
+   unsigned f3 : 17;
+   signed f4 : 28;
+};
+
+struct S1 {
+   unsigned f0 : 15;
