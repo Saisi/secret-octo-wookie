d6eaeafcfbd64155e2c83652ebf38b612ce690f9
saisi <saisi@users.noreply.github.com>
2017-04-19 07:55:42 +0000

    1492588541 acanthodian

---
+++ 1492588541.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1521880140
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int8_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 0x845DL;
+static const int32_t g_9 = 1L;
