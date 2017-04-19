abb0d464bda0d1faa6ff1fb4ecda29dcf18a51fc
saisi <saisi@users.noreply.github.com>
2017-04-19 02:55:15 +0000

    1492570515 Asterella

---
+++ 1492570515.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1001363510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 9;
+   unsigned f1 : 26;
+   int32_t  f2;
+   int32_t  f3;
+   int32_t  f4;
+};
+
+union U1 {
+   uint64_t  f0;
