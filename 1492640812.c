c70816c5acf7b6426e178ea60e42efc17a7384f0
saisi <saisi@users.noreply.github.com>
2017-04-19 22:26:52 +0000

    1492640812 arteriopalmus

---
+++ 1492640812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2359082100
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_9 = 0xC8L;
+static int32_t g_11 = (-6L);
+static int64_t g_33 = 0L;
+static int32_t g_36 = 0L;
+static int32_t * volatile g_35 = &g_36;/* VOLATILE GLOBAL g_35 */
+static int32_t *g_39 = &g_36;
+static uint8_t g_69 = 247UL;
+static int8_t g_80 = 0x25L;
+static int32_t g_88 = (-1L);
