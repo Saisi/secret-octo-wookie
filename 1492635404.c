f9106f339e633bd8c2c1fe36bba0ff49137d1c72
saisi <saisi@users.noreply.github.com>
2017-04-19 20:56:45 +0000

    1492635404 baggages

---
+++ 1492635404.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4262740963
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 16;
+};
+
+struct S1 {
+   volatile signed f0 : 23;
+   const unsigned f1 : 31;
+};
+
+struct S2 {
