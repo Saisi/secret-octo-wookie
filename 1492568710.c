44988b874ae6efd258bd42e708c74245bc1f4804
saisi <saisi@users.noreply.github.com>
2017-04-19 02:25:13 +0000

    1492568710 apetaly

---
+++ 1492568710.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      8994368
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+union U1 {
+   volatile unsigned f0 : 10;
+};
+
+union U2 {
+   const int32_t  f0;
