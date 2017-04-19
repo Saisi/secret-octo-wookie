d0f779d1852f73eb65b4f19162e1295e8c3ea2e8
saisi <saisi@users.noreply.github.com>
2017-04-19 19:26:38 +0000

    1492629998 Audaean

---
+++ 1492629998.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3597969292
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 20;
+   signed f1 : 25;
+   volatile signed f2 : 23;
+   unsigned f3 : 7;
+   signed f4 : 26;
+};
+#pragma pack(pop)
