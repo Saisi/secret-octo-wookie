4d0147f06ee3e4ef193bd2479bdb9309e1d92e0f
saisi <saisi@users.noreply.github.com>
2017-04-19 16:56:26 +0000

    1492620986 ablins

---
+++ 1492620986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      816623255
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 10;
+   volatile unsigned f1 : 25;
+   volatile unsigned f2 : 24;
+   unsigned f3 : 3;
+   unsigned f4 : 8;
+   unsigned f5 : 1;
+   const unsigned f6 : 28;
+};
+
