581e7cfdf37bf4a4aca30874c3bdc42c39148e34
saisi <saisi@users.noreply.github.com>
2017-04-19 18:56:36 +0000

    1492628195 antiketogenic

---
+++ 1492628195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2562149988
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 22;
+   volatile unsigned f1 : 30;
+   unsigned f2 : 2;
+   volatile uint8_t  f3;
+   unsigned f4 : 13;
+};
+#pragma pack(pop)
