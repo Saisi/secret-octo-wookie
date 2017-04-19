9de559db0b74592755e96aea4558461bace32062
saisi <saisi@users.noreply.github.com>
2017-04-19 15:56:22 +0000

    1492617381 acture

---
+++ 1492617381.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      8851500
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 4;
+   volatile int32_t  f1;
+   uint32_t  f2;
+   unsigned f3 : 23;
+   int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[5][10] = {{0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L},{0L,0L,0x2A2B3B7BL,0L,0L,0x2A2B3B7BL,0L,0L,0x2A2B3B7BL,0L},{0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L},{0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L,0xD44AD374L},{0L,0L,0x2A2B3B7BL,0L,0L,0x2A2B3B7BL,0xD44AD374L,0xD44AD374L,0L,0xD44AD374L}};
