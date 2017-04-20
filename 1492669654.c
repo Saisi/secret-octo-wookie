68a287ca8c66cca9099b6b15eaad504e28296e11
saisi <saisi@users.noreply.github.com>
2017-04-20 06:27:35 +0000

    1492669654 axletree

---
+++ 1492669654.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2127081725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 29;
+   volatile signed : 0;
+   uint32_t  f1;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[5] = {5L,5L,5L,5L,5L};
+static int32_t g_4 = 1L;
