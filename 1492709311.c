7bf127610aa57d77c183c99fbb36e9892ee7369a
saisi <saisi@users.noreply.github.com>
2017-04-20 17:28:31 +0000

    1492709311 antiphysician

---
+++ 1492709311.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2804462187
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[8][3] = {{255UL,0x41L,8UL},{0UL,0x41L,0UL},{0xBEL,255UL,8UL},{0xBEL,0xBEL,255UL},{0UL,255UL,255UL},{255UL,0x41L,8UL},{0UL,0x41L,0UL},{0xBEL,255UL,8UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
