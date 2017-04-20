79abf66ef0fa2f47ff58244d383b23dcb7f52417
saisi <saisi@users.noreply.github.com>
2017-04-20 12:58:09 +0000

    1492693088 agammaglobulinemic

---
+++ 1492693088.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1440519233
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 31;
+   unsigned f1 : 26;
+   uint32_t  f2;
+   unsigned f3 : 22;
+   volatile signed f4 : 5;
+   const signed f5 : 31;
+   signed f6 : 6;
+   const volatile signed f7 : 8;
+};
