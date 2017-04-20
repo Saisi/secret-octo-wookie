35b15dfc4ce5cc69c4d1152555b8b75810bdb9b1
saisi <saisi@users.noreply.github.com>
2017-04-20 11:58:01 +0000

    1492689481 archidome

---
+++ 1492689481.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2984127551
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   unsigned f0 : 5;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
