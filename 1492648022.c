fb638e69e0e00e6bce12590b7eb6edddcb1eb4f5
saisi <saisi@users.noreply.github.com>
2017-04-20 00:27:02 +0000

    1492648022 aflower

---
+++ 1492648022.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1837366953
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 19;
+   unsigned f1 : 30;
+   signed f2 : 9;
+   unsigned f3 : 16;
+   signed f4 : 1;
+   unsigned f5 : 22;
+   signed f6 : 29;
