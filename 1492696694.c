7b0d570a37f8e1c9e22143463fdc285bfffdbfca
saisi <saisi@users.noreply.github.com>
2017-04-20 13:58:15 +0000

    1492696694 autodynes

---
+++ 1492696694.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1390283343
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 6;
+   volatile int16_t  f1;
+   const signed f2 : 20;
+   const int8_t  f3;
+   unsigned f4 : 24;
+};
+#pragma pack(pop)
