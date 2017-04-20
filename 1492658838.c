3c8899193be3489afb579718706e4c05b916a548
saisi <saisi@users.noreply.github.com>
2017-04-20 03:27:19 +0000

    1492658838 acidy

---
+++ 1492658838.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2578089034
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 19;
+   volatile signed f1 : 28;
+   signed f2 : 14;
+   unsigned f3 : 2;
+   signed f4 : 9;
+   volatile signed f5 : 28;
+   unsigned f6 : 10;
