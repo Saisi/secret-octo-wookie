548a115afb8d2a96947aabe8c25df00c1d3851bd
saisi <saisi@users.noreply.github.com>
2017-04-20 07:57:42 +0000

    1492675062 atomistics

---
+++ 1492675062.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      759153481
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   uint64_t  f1;
+   int32_t  f2;
+   volatile uint32_t  f3;
+   volatile int8_t  f4;
+   const uint16_t  f5;
+   const volatile signed f6 : 6;
+   uint8_t  f7;
+   const volatile signed f8 : 12;
