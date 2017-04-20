6c17a28bb040bd2450f9a3a16d532d0e1e8d41e1
saisi <saisi@users.noreply.github.com>
2017-04-20 18:28:36 +0000

    1492712916 anaesthesiant

---
+++ 1492712916.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3458784761
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x61F9C28AL;
+static int32_t g_6 = 9L;
+static int32_t g_10 = 0xE4FC2285L;
+static uint32_t g_30 = 0x87A5EAFBL;
+static int32_t g_54 = 0L;
+static volatile uint32_t g_57 = 0xCC3231A2L;/* VOLATILE GLOBAL g_57 */
+static int32_t * const g_84 = &g_54;
+static int32_t * const * const g_83 = &g_84;
