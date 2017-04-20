cc012fbbf02764801f93f40d111624adf48dda54
saisi <saisi@users.noreply.github.com>
2017-04-20 12:28:06 +0000

    1492691285 bandages

---
+++ 1492691285.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3774279032
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2 = 0x57L;
+static uint32_t g_8 = 0xB666A756L;
+static uint32_t g_43 = 0x67A7BF74L;
+static uint8_t g_98 = 247UL;
+static int8_t g_106[2] = {0x46L,0x46L};
+static int16_t g_110 = 0x7051L;
+static uint64_t g_112 = 0xED797CFCBFA339BCLL;
+static int32_t g_114 = 0x8AFECA80L;
+static int32_t g_116 = 0x0D3BD28FL;
