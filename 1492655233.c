910ba2f44655dccea271eb5e04422c038eb02cc6
saisi <saisi@users.noreply.github.com>
2017-04-20 02:27:13 +0000

    1492655233 Acrilan

---
+++ 1492655233.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      501938942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_19 = 65534UL;
+static volatile int8_t g_20 = 0x47L;/* VOLATILE GLOBAL g_20 */
+static int32_t g_25 = 0x9DADE661L;
+static uint8_t g_39 = 0xA1L;
+static uint16_t g_50 = 0xD786L;
+static uint16_t *g_49[4] = {&g_50,&g_50,&g_50,&g_50};
+static volatile uint64_t g_57 = 0UL;/* VOLATILE GLOBAL g_57 */
+static uint32_t g_59 = 0xC4142545L;
+static int32_t g_72[6] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
