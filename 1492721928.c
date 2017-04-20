5a2c489fc83773a98ac048ae12624310a94ba9fc
saisi <saisi@users.noreply.github.com>
2017-04-20 20:58:48 +0000

    1492721928 Baalbeer

---
+++ 1492721928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3042467118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2[1] = {0xE0BCL};
+static volatile int32_t g_3 = 0xBE8E56F9L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[6] = {0xFB6318D5L,0xFB6318D5L,0x10B999FBL,0xFB6318D5L,0xFB6318D5L,0x10B999FBL};
+static volatile int32_t g_5[3][3] = {{0x0585B7F3L,0x0585B7F3L,0x0585B7F3L},{(-5L),(-5L),(-5L)},{0x0585B7F3L,0x0585B7F3L,0x0585B7F3L}};
+static int32_t g_6[2] = {(-1L),(-1L)};
+static volatile int32_t g_7 = (-2L);/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0xB4E94F93L;
+static volatile int32_t g_11 = 0xA3CFCE29L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_12 = 0x4FFCF33FL;
