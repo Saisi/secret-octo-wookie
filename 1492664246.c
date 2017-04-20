2a65b7a0d54e329a83d78af0b0b9b478c0be5f8d
saisi <saisi@users.noreply.github.com>
2017-04-20 04:57:27 +0000

    1492664246 architrave

---
+++ 1492664246.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1564904361
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   volatile int64_t  f1;
+   volatile uint64_t  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+   int32_t  f1;
+   int32_t  f2;
