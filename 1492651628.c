850946adb08ab98e77109db89e4e26dfa2eccb59
saisi <saisi@users.noreply.github.com>
2017-04-20 01:27:08 +0000

    1492651628 bacteriologists

---
+++ 1492651628.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2523574537
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_11 = 0xF4L;
+static uint8_t g_12 = 0xECL;
+static int64_t g_16 = 0x4D1454BCD76CE4A6LL;
+static const int64_t g_43 = (-1L);
+static const int64_t *g_42 = &g_43;
+static int32_t g_52 = 0L;
+static volatile int32_t g_53 = 9L;/* VOLATILE GLOBAL g_53 */
+static volatile int32_t g_54 = 1L;/* VOLATILE GLOBAL g_54 */
+static volatile int32_t g_55[5][9] = {{0x7E12FD89L,8L,0L,0L,0L,8L,0x7E12FD89L,0x9AA50887L,0x7B35F197L},{0x9AA50887L,(-10L),0L,0x8BBEB927L,0x3ED13222L,8L,0x3ED13222L,0x8BBEB927L,0L},{0L,0L,0xFE2499E6L,6L,0x7B35F197L,(-10L),8L,0x9AA50887L,8L},{0L,0x7E12FD89L,(-10L),(-10L),0x7E12FD89L,0L,6L,0L,0xA81D68C2L},{0x9AA50887L,0x8BBEB927L,0xFE2499E6L,0xA81D68C2L,0x7E12FD89L,0x7E12FD89L,0xA81D68C2L,0xFE2499E6L,0x8BBEB927L}};
