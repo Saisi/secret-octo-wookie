c300b69a2e979ef921dd79d9a50a6a5abab32e2a
saisi <saisi@users.noreply.github.com>
2017-04-20 18:58:39 +0000

    1492714718 autoheterosis

---
+++ 1492714718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      59074427
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+   signed f1 : 3;
+   uint16_t  f2;
+   const volatile int8_t  f3;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_16 = (-5L);
