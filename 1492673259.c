96c1de06301cc9da43165ece6be38faba47de63d
saisi <saisi@users.noreply.github.com>
2017-04-20 07:27:39 +0000

    1492673259 apiararies

---
+++ 1492673259.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4047610633
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 24;
+   signed f2 : 14;
+   const volatile unsigned f3 : 26;
+   unsigned f4 : 25;
+   volatile signed f5 : 3;
+   volatile signed f6 : 26;
+   volatile signed f7 : 12;
+};
