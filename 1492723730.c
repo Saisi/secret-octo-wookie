c4f126db4c33f9f10efc1c953695520cdce15016
saisi <saisi@users.noreply.github.com>
2017-04-20 21:28:51 +0000

    1492723730 ambusher

---
+++ 1492723730.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      64044117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x532BB11AL;
+static int32_t g_11 = 0xD329B3A0L;
+static union U0 g_50 = {0xB974DB9BL};
+static uint64_t g_69 = 0UL;
