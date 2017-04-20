440da1b70c2b6070b471794c647370b336242713
saisi <saisi@users.noreply.github.com>
2017-04-20 20:28:46 +0000

    1492720125 Arvida

---
+++ 1492720125.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2024502632
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 2;
+   const volatile unsigned f1 : 15;
+   uint32_t  f2;
+   signed f3 : 13;
+   signed f4 : 30;
+   unsigned f5 : 18;
+   const volatile signed f6 : 11;
+};
+
