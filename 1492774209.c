6707ca356852ca97348e857b6212ec16afbc19de
saisi <saisi@users.noreply.github.com>
2017-04-21 11:30:10 +0000

    1492774209 atomic

---
+++ 1492774209.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3332956650
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+};
+
+union U1 {
+   unsigned f0 : 19;
+};
+
+union U2 {
+   uint64_t  f0;
