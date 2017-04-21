01913b0972bc74e1d05805bc0289c361ed0ca469
saisi <saisi@users.noreply.github.com>
2017-04-21 14:30:25 +0000

    1492785025 anethols

---
+++ 1492785025.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3750593206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_11 = 7UL;
+static int8_t g_35[5] = {0x35L,0x35L,0x35L,0x35L,0x35L};
+static uint8_t g_58 = 0xA4L;
+static uint8_t *g_61 = &g_11;
+static volatile uint64_t g_113[8] = {0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL,0xA727FAAB159BBC08LL};
+static int32_t g_115 = 0xB872472FL;
+static uint32_t g_117 = 1UL;
+static uint8_t g_129[5][4] = {{1UL,1UL,0x9BL,1UL},{1UL,0x82L,0x82L,1UL},{0x82L,1UL,0x82L,0x82L},{1UL,1UL,0x9BL,1UL},{1UL,0x82L,0x82L,1UL}};
+static int16_t g_156 = 1L;
