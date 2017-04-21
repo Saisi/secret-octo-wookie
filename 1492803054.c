b10454b09dd68803697064d35338f1a401651beb
saisi <saisi@users.noreply.github.com>
2017-04-21 19:30:55 +0000

    1492803054 acalephoid

---
+++ 1492803054.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1803381360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint16_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_9 = 8UL;/* VOLATILE GLOBAL g_9 */
+static uint64_t g_36 = 0UL;
