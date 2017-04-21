d05a29ac6dff165389364fa20924993737378f2c
saisi <saisi@users.noreply.github.com>
2017-04-21 05:29:33 +0000

    1492752572 ballyhack

---
+++ 1492752572.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1314314091
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_12 = 0x21C3A23FL;
+static uint32_t g_20 = 0UL;
+static uint16_t g_56[2] = {0xDDF3L,0xDDF3L};
+static union U0 g_80 = {0xD333L};
