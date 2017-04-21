fdbd43b765316f1976c3f29c276b808190410000
saisi <saisi@users.noreply.github.com>
2017-04-21 11:00:05 +0000

    1492772404 baldricks

---
+++ 1492772404.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1826441637
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int64_t  f1;
+   uint8_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+};
+
