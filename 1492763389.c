e10deb29d78453b9e02572aa7a00b2bf01d5b7f1
saisi <saisi@users.noreply.github.com>
2017-04-21 08:29:50 +0000

    1492763389 albation

---
+++ 1492763389.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      631750128
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 6;
+   signed f1 : 3;
+   const unsigned f2 : 25;
+   unsigned f3 : 1;
+   const unsigned f4 : 12;
+   const signed f5 : 19;
+   const signed f6 : 5;
+};
+
