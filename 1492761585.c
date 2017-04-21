c544e488c1345ff93446a59c57b0b007576fe7a6
saisi <saisi@users.noreply.github.com>
2017-04-21 07:59:46 +0000

    1492761585 Africanization

---
+++ 1492761585.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      181167960
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 26;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_15 = 0xD4A0D461E3FF14EBLL;
+static int32_t g_18 = 0x4AD38DC5L;
+static int32_t g_22 = 5L;
+static uint16_t g_51 = 0UL;
