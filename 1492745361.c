26e7d9ad78d56a3c182100e9696d751ffdbeb493
saisi <saisi@users.noreply.github.com>
2017-04-21 03:29:21 +0000

    1492745361 alpenstocks

---
+++ 1492745361.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3311274523
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xD97A888AL;
