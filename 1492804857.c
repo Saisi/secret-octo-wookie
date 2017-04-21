092487e5261615c0ea4daf7a4cfa925b9cf2e3d9
saisi <saisi@users.noreply.github.com>
2017-04-21 20:00:57 +0000

    1492804857 azophen

---
+++ 1492804857.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2791726654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[1] = {0x3606DE71L};
+static int32_t g_5 = 0xB905D09EL;
+static int32_t g_25[2][2][3] = {{{0x28CAE2FEL,0x28CAE2FEL,0x28CAE2FEL},{0x28CAE2FEL,0x28CAE2FEL,0x28CAE2FEL}},{{0x28CAE2FEL,0x28CAE2FEL,0x28CAE2FEL},{0x28CAE2FEL,0x28CAE2FEL,0x28CAE2FEL}}};
+static int32_t *g_24[3] = {&g_25[1][1][0],&g_25[1][1][0],&g_25[1][1][0]};
+static int16_t g_76[1] = {(-5L)};
+static int32_t g_85 = (-8L);
+static int16_t g_92 = 0x3E41L;
+static uint32_t g_93 = 0xFB7473ACL;
