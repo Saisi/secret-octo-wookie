5fba8e19143cebfbd239f7eaebf90eb920f62502
saisi <saisi@users.noreply.github.com>
2017-04-21 22:31:10 +0000

    1492813869 acclimatable

---
+++ 1492813869.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4121694998
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   volatile unsigned f1 : 10;
+   int32_t  f2;
+   unsigned f3 : 12;
+};
+#pragma pack(pop)
+
