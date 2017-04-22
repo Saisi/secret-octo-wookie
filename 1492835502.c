fb6ee65b89766b9c1f0758908c9d63adebc9a4ef
saisi <saisi@users.noreply.github.com>
2017-04-22 04:31:42 +0000

    1492835502 alpenstocks

---
+++ 1492835502.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4265253391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 6;
+};
+
+struct S1 {
+   struct S0  f0;
+   const volatile signed f1 : 27;
+   unsigned f2 : 1;
+   const unsigned : 0;
+   const unsigned f3 : 4;
