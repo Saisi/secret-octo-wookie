0ee50a835013c6603c2b6c17bc93c3e53213938f
saisi <saisi@users.noreply.github.com>
2017-04-22 17:32:47 +0000

    1492882366 altarpiece

---
+++ 1492882366.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      378768075
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   const uint64_t  f1;
+   const uint32_t  f2;
+   volatile int16_t  f3;
+   const volatile uint32_t  f4;
+   volatile uint64_t  f5;
+   signed f6 : 19;
+};
+
