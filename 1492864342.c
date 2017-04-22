66ef99f448de2da5721a5b791bc6a56929b89940
saisi <saisi@users.noreply.github.com>
2017-04-22 12:32:22 +0000

    1492864342 acetanisidide

---
+++ 1492864342.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1959297496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+   unsigned f2 : 18;
+   const unsigned f3 : 14;
+   uint8_t  f4;
+};
+
+union U1 {
+   uint64_t  f0;
