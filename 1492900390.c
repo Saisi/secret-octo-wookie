f4a316dabf266ab1b9a58a1a514fed47d02fbfde
saisi <saisi@users.noreply.github.com>
2017-04-22 22:33:10 +0000

    1492900390 antiship

---
+++ 1492900390.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3634590111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+   volatile unsigned f1 : 13;
+   volatile signed f2 : 28;
+   signed f3 : 23;
+   const signed f4 : 12;
+};
+
+struct S1 {
+   volatile int8_t  f0;
