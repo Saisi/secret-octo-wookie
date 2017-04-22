487090364c6766254dc38f9fb7aa63b7485e1d61
saisi <saisi@users.noreply.github.com>
2017-04-22 06:01:49 +0000

    1492840909 Abadite

---
+++ 1492840909.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1821777576
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   const int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint8_t g_4 = 248UL;/* VOLATILE GLOBAL g_4 */
+static uint16_t g_9[7] = {0x831DL,0x831DL,0x831DL,0x831DL,0x831DL,0x831DL,0x831DL};
+static int16_t g_24 = 6L;
+static uint16_t g_30 = 0x5741L;
