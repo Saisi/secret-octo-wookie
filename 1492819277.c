37e1eb04d5e4b90247ecba6b4b56304163270b28
saisi <saisi@users.noreply.github.com>
2017-04-22 00:01:18 +0000

    1492819277 acrobatic

---
+++ 1492819277.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1932638374
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 1;
+   uint32_t  f1;
+   volatile unsigned f2 : 26;
+   unsigned f3 : 20;
+   unsigned f4 : 9;
+   unsigned f5 : 17;
+};
