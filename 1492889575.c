2cb0c1d86747979c4a5e60879d89597dfabe13d0
saisi <saisi@users.noreply.github.com>
2017-04-22 19:32:56 +0000

    1492889575 absurdum

---
+++ 1492889575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3674866504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x7DF2F4A8L;
+static uint16_t g_8 = 1UL;
+static uint8_t g_22 = 0UL;
+static uint64_t g_71 = 0x08F98093AF8EF5D3LL;
+static int32_t g_73 = 0x62B132DCL;
+static int32_t * volatile g_74 = &g_73;/* VOLATILE GLOBAL g_74 */
+static uint8_t g_81 = 0UL;
+static volatile uint8_t g_90 = 255UL;/* VOLATILE GLOBAL g_90 */
+static volatile uint8_t *g_89 = &g_90;
