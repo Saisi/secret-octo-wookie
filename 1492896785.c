2157451e6caae41dff66ed5daf2ac00df98e8e2f
saisi <saisi@users.noreply.github.com>
2017-04-22 21:33:06 +0000

    1492896785 ANAC

---
+++ 1492896785.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2988919486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint64_t  f0;
+   uint8_t  f1;
+   volatile uint16_t  f2;
+   uint8_t  f3;
+   uint32_t  f4;
+};
+
+union U1 {
+   volatile uint64_t  f0;
