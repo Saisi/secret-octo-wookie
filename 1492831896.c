f7fd12162a2509fe80c0abfcd89406a025e96722
saisi <saisi@users.noreply.github.com>
2017-04-22 03:31:36 +0000

    1492831896 anglicanism

---
+++ 1492831896.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      774193685
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t * volatile g_2[3] = {(void*)0,(void*)0,(void*)0};
+static int32_t * volatile * volatile g_3 = &g_2[1];/* VOLATILE GLOBAL g_3 */
+static uint32_t g_13 = 1UL;
+static int8_t g_20 = 0L;
+static int16_t g_25 = 0L;
+static int32_t *g_34 = (void*)0;
+static int32_t g_37 = 0xFC22F747L;
+static int32_t ** volatile g_38 = &g_34;/* VOLATILE GLOBAL g_38 */
+static uint8_t g_48 = 0x8BL;
