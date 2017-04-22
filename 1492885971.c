5a8bc232377390ff0c0c7a7c7200a3b5f870a6ba
saisi <saisi@users.noreply.github.com>
2017-04-22 18:32:51 +0000

    1492885971 apotropaic

---
+++ 1492885971.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2274038366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 29;
+   int16_t  f1;
+   const int8_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+   unsigned : 0;
+   uint16_t  f1;
