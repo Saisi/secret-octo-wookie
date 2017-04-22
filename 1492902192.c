18e1976a62d17da8f48044545fcb388a22505bd6
saisi <saisi@users.noreply.github.com>
2017-04-22 23:03:12 +0000

    1492902192 Amfortas

---
+++ 1492902192.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3700675651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x20FE6E32L;
+static int64_t g_12 = 0xF6EFBA1A4D63A88ELL;
+static const int64_t g_40 = 0L;
+static const int64_t *g_39 = &g_40;
+static uint8_t g_49 = 254UL;
+static int32_t g_56[9] = {0x05E60D1FL,0x4C01E07AL,0x05E60D1FL,0x05E60D1FL,0x4C01E07AL,0x05E60D1FL,0x05E60D1FL,0x4C01E07AL,0x05E60D1FL};
+static int64_t g_64[3] = {0L,0L,0L};
+static uint8_t g_68[10][2] = {{1UL,246UL},{1UL,0x69L},{0x5CL,0x5CL},{0x69L,1UL},{246UL,1UL},{0x69L,0x5CL},{0x5CL,0x69L},{1UL,246UL},{1UL,0x69L},{0x5CL,0x5CL}};
+static uint8_t g_82 = 254UL;
