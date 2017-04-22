f173359162e9dcb84db8d87ba481eab4d73e7ad3
saisi <saisi@users.noreply.github.com>
2017-04-22 05:31:47 +0000

    1492839107 aquilegia

---
+++ 1492839107.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1613179247
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   int64_t  f1;
+   volatile signed f2 : 5;
+   int64_t  f3;
+   int8_t  f4;
+};
+
+#pragma pack(push)
+#pragma pack(1)
