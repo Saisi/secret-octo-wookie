6890489e8704094f32e1ef24d9fe4437b3de372e
saisi <saisi@users.noreply.github.com>
2017-04-22 04:01:40 +0000

    1492833698 acyrological

---
+++ 1492833698.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      760853760
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   int8_t  f1;
+   volatile int16_t  f2;
+   const uint32_t  f3;
+};
+
+union U1 {
+   uint32_t  f0;
+   const uint16_t  f1;
