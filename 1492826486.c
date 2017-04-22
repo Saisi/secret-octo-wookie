08434ade95eb734cab5e22ce04cce8a97c60ccd8
saisi <saisi@users.noreply.github.com>
2017-04-22 02:01:27 +0000

    1492826486 Annaba

---
+++ 1492826486.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1048851790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 21;
+   signed f1 : 22;
+   volatile unsigned : 0;
+   unsigned f2 : 12;
+   int16_t  f3;
+   const volatile signed f4 : 5;
+};
