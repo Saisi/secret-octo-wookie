23eb2e6552354268ea7f9f7aa0418822d8c99c01
saisi <saisi@users.noreply.github.com>
2017-04-22 14:32:32 +0000

    1492871552 accadian

---
+++ 1492871552.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1262387686
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const int16_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x4A12B57EL;
+static int32_t g_6 = 0xFA9D19ACL;
+static const int32_t g_18[9] = {(-1L),0xC6EED1E1L,(-1L),(-1L),0xC6EED1E1L,(-1L),(-1L),0xC6EED1E1L,(-1L)};
