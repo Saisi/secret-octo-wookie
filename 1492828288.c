8de5d96b6361d9a92f682f9ed3065241aed4e71f
saisi <saisi@users.noreply.github.com>
2017-04-22 02:31:32 +0000

    1492828288 asthenope

---
+++ 1492828288.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      786081902
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 5;
+   signed f1 : 6;
+   volatile unsigned f2 : 16;
+   volatile signed f3 : 28;
+   volatile uint16_t  f4;
+   unsigned f5 : 21;
+   volatile signed f6 : 2;
+   volatile unsigned f7 : 19;
+};
