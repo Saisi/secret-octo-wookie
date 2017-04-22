ffbff867c4c117bded42c70c0f4e0c1612800c86
saisi <saisi@users.noreply.github.com>
2017-04-22 07:31:57 +0000

    1492846316 Apopka

---
+++ 1492846316.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      333325484
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 4;
+   volatile unsigned f1 : 9;
+   unsigned f2 : 7;
+   signed f3 : 23;
+   const signed f4 : 10;
+   unsigned f5 : 25;
+   unsigned f6 : 19;
