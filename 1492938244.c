e68199ac18358a1a5aff05009b1f973520093495
saisi <saisi@users.noreply.github.com>
2017-04-23 09:04:04 +0000

    1492938244 auxotrophy

---
+++ 1492938244.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1398250248
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_54 = (-9L);/* VOLATILE GLOBAL g_54 */
+static uint8_t g_65 = 0xDDL;
+static union U0 g_66 = {-9L};
+static int32_t g_68 = 0xB2A93F04L;
+static int64_t g_71 = 0x3E3202F603F559CDLL;
