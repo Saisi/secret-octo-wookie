058e4e913ff12e7d437c71e28400eb342f944fd0
saisi <saisi@users.noreply.github.com>
2017-04-23 02:03:25 +0000

    1492913004 antimasque

---
+++ 1492913004.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3668940235
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_9 = 8UL;
+static uint64_t g_63 = 0x1F0CFADBACD6BCE8LL;
+static uint16_t g_65 = 8UL;
+static int32_t g_84 = 0xA90D2634L;
+static uint8_t g_103 = 0x3AL;
+static volatile int32_t **g_124 = (void*)0;
+static int8_t g_150[5][9] = {{0x71L,(-1L),(-1L),(-1L),(-1L),0x71L,(-1L),(-1L),(-1L)},{1L,1L,0x71L,(-1L),0x71L,1L,1L,0x71L,(-1L)},{0x54L,(-1L),0x54L,0x71L,0x71L,0x54L,(-1L),0x54L,0x71L},{0x54L,0x71L,0x71L,0x54L,(-1L),0x54L,0x71L,0x71L,0x54L},{1L,0x71L,(-1L),0x71L,1L,1L,0x71L,(-1L),0x71L}};
+static uint8_t g_152 = 252UL;
+static int64_t g_154 = 1L;
