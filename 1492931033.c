46b888a8337ee6fe73bebf3693f17ebb77f7442e
saisi <saisi@users.noreply.github.com>
2017-04-23 07:03:54 +0000

    1492931033 apetalose

---
+++ 1492931033.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      146892615
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_4 = 0x6DL;
+static uint8_t g_6 = 0x55L;
