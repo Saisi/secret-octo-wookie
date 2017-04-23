85cf4d40758e33b56ac457815fa8ac9779b392c8
saisi <saisi@users.noreply.github.com>
2017-04-23 01:03:20 +0000

    1492909400 baconize

---
+++ 1492909400.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3357076685
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   int8_t * volatile  f0;
+   signed : 0;
+   uint64_t  f1;
+   unsigned f2 : 7;
+   volatile int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0xAF14C90DL;
