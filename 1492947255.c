b268635606fb71ae003d510b4f4be2ef25503a43
saisi <saisi@users.noreply.github.com>
2017-04-23 11:34:16 +0000

    1492947255 abortifacient

---
+++ 1492947255.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4115737472
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+   int16_t  f3;
+   uint64_t  f4;
+   uint16_t  f5;
+   volatile uint8_t  f6;
+   uint32_t  f7;
+   uint16_t  f8;
