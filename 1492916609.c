7a63066ba6d0091483d5fd1fa629f625d6cace18
saisi <saisi@users.noreply.github.com>
2017-04-23 03:03:29 +0000

    1492916609 affected

---
+++ 1492916609.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4206347217
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x5898F01BL;
+static int8_t g_10 = 5L;
+static volatile int32_t g_11 = 0xA6D0811FL;/* VOLATILE GLOBAL g_11 */
+static volatile int64_t g_12 = 0xD0E834AB462A77B6LL;/* VOLATILE GLOBAL g_12 */
+static volatile uint64_t g_13[1][9] = {{0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL,0xAEC51AC06E3268DDLL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
