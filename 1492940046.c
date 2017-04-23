d2f1191d90de36ca83992b5ba2159c0b70456933
saisi <saisi@users.noreply.github.com>
2017-04-23 09:34:06 +0000

    1492940046 alms

---
+++ 1492940046.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1971999631
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   const uint16_t  f1;
+   const volatile uint8_t  f2;
+};
+
+union U1 {
+   uint16_t  f0;
+   unsigned f1 : 5;
+   uint8_t  f2;
