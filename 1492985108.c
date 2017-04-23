6c9b52f941bb42aea6a3826c91c566f91a1ff77f
saisi <saisi@users.noreply.github.com>
2017-04-23 22:05:09 +0000

    1492985108 bacteritic

---
+++ 1492985108.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3813496977
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 7;
+   int8_t * f1;
+   uint64_t  f2;
+   int16_t  f3;
+};
+
+union U1 {
+   const uint8_t  f0;
+   const int8_t  f1;
