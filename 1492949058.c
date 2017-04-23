3c16d4a57d9b876957d22384465d86793515e2c1
saisi <saisi@users.noreply.github.com>
2017-04-23 12:04:18 +0000

    1492949058 Amis

---
+++ 1492949058.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      749073194
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S4 {
+   signed f0 : 26;
+   const signed f1 : 31;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static struct S4 g_2 = {-4149,45314};
+
+
+/* --- FORWARD DECLARATIONS --- */
