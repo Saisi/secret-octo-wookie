c840828e06ab2eb37cf5320737dfea14765ab942
saisi <saisi@users.noreply.github.com>
2017-04-23 10:34:11 +0000

    1492943651 arms

---
+++ 1492943651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      44373161
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x28F6C74AL;
+static int32_t g_9[7][4] = {{1L,0x7C4D1454L,1L,1L},{0x7C4D1454L,0x7C4D1454L,0x849B76EEL,0x7C4D1454L},{0x7C4D1454L,1L,1L,0x7C4D1454L},{1L,0x7C4D1454L,1L,1L},{0x7C4D1454L,0x7C4D1454L,0x849B76EEL,0x7C4D1454L},{0x7C4D1454L,1L,1L,0x7C4D1454L},{1L,0x7C4D1454L,1L,1L}};
+static int32_t * volatile g_8[8] = {&g_9[5][0],&g_9[5][0],&g_4,&g_9[5][0],&g_9[5][0],&g_4,&g_9[5][0],&g_9[5][0]};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
