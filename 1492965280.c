79dbce1322ddd2e2b8275546ae9bb2b7a4b84b4a
saisi <saisi@users.noreply.github.com>
2017-04-23 16:34:40 +0000

    1492965280 autologist

---
+++ 1492965280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      88089738
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 19;
+   unsigned f1 : 28;
+   const volatile unsigned f2 : 14;
+   volatile unsigned f3 : 2;
+   signed f4 : 9;
+   signed f5 : 28;
+   const unsigned f6 : 10;
