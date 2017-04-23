04efbf0873057c073b14cd32adbb6937960cc8fa
saisi <saisi@users.noreply.github.com>
2017-04-23 17:04:42 +0000

    1492967082 airify

---
+++ 1492967082.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      156350185
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U5 {
+   const volatile int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U5 g_2[4][4] = {{{0},{0},{0},{0}},{{0},{0},{0},{0}},{{0},{0},{0},{0}},{{0},{0},{0},{0}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U5  func_1(void);
