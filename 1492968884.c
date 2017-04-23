dbe7420ea7588af148cdd91e189382e9fcd88004
saisi <saisi@users.noreply.github.com>
2017-04-23 17:34:45 +0000

    1492968884 anvilling

---
+++ 1492968884.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4083058740
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 8;
+   unsigned f1 : 29;
+   signed f2 : 11;
+   unsigned f3 : 11;
+   unsigned f4 : 14;
+   unsigned f5 : 13;
+   signed f6 : 23;
