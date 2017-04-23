13f92386893f56f1c8fabe2f1c73f31792c5a27c
saisi <saisi@users.noreply.github.com>
2017-04-23 22:35:11 +0000

    1492986911 aspermia

---
+++ 1492986911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      429839130
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 27;
+   volatile signed f1 : 4;
+   unsigned f2 : 22;
+   const unsigned f3 : 10;
+   const volatile unsigned f4 : 17;
+   unsigned f5 : 4;
+   unsigned f6 : 18;
