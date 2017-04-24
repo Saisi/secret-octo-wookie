7d8185de736bfa439b775915d4665a552922b7f1
saisi <saisi@users.noreply.github.com>
2017-04-24 00:35:20 +0000

    1492994120 acclimatiser

---
+++ 1492994120.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2614959442
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_28 = (-7L);
+static int32_t *g_27 = &g_28;
+static int32_t *g_62 = &g_28;
+static int32_t ** volatile g_61 = &g_62;/* VOLATILE GLOBAL g_61 */
+static int32_t g_64 = 0x4489BE67L;
+static uint64_t g_71 = 0x364560F9103153D5LL;
+static int16_t g_76 = 3L;
+static uint32_t g_88 = 0xCD962F93L;
+static uint8_t g_112 = 255UL;
