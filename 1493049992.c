d342ab45568e64ee4dc8494a73994350fd3e6d92
saisi <saisi@users.noreply.github.com>
2017-04-24 16:06:33 +0000

    1493049992 bandana

---
+++ 1493049992.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2470634083
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 16;
+   const uint32_t  f1;
+   signed f2 : 23;
+   signed f3 : 31;
+   signed f4 : 10;
+};
+#pragma pack(pop)
