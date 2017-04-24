d088f2518cbb27b04d20bd54a2990c8e8aa5cce5
saisi <saisi@users.noreply.github.com>
2017-04-24 20:06:53 +0000

    1493064412 Anasitch

---
+++ 1493064412.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2370370303
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 30;
+   volatile int8_t * volatile  f1;
+   uint64_t  f2;
+   int16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_9 = 3UL;/* VOLATILE GLOBAL g_9 */
+static uint8_t g_16 = 0xD1L;
