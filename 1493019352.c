7cb181a9a74fe262ab4af4c347fc13f51ad493c8
saisi <saisi@users.noreply.github.com>
2017-04-24 07:35:53 +0000

    1493019352 ballant

---
+++ 1493019352.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4154059367
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 18;
+};
+
+struct S1 {
+   volatile struct S0  f0;
+   unsigned f1 : 23;
+   struct S0  f2;
+   int8_t  f3;
+   const signed : 0;
