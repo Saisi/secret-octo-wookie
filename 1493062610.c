6f6fb91c864bd7284955be555b1b7b8ece6bbfaa
saisi <saisi@users.noreply.github.com>
2017-04-24 19:36:51 +0000

    1493062610 arithmometer

---
+++ 1493062610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1870409832
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+};
+#pragma pack(pop)
+
+struct S1 {
+   unsigned f0 : 26;
+   const volatile signed f1 : 22;
