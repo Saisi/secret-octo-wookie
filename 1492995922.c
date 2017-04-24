324e062f8e26c7150f54f7e343eb3aa7d66b989f
saisi <saisi@users.noreply.github.com>
2017-04-24 01:05:23 +0000

    1492995922 antistate

---
+++ 1492995922.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2937899891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_8[4] = {5L,5L,5L,5L};
+static int8_t g_18 = 0xCCL;
+static int32_t g_23 = 0x806AA1C3L;
+static int32_t * const  volatile g_22 = &g_23;/* VOLATILE GLOBAL g_22 */
+static uint32_t g_29 = 9UL;
+static volatile uint32_t g_32 = 0x11B1996FL;/* VOLATILE GLOBAL g_32 */
+static int32_t g_53[1][1] = {{0L}};
+static int32_t * const g_52 = &g_53[0][0];
+static uint64_t g_81 = 18446744073709551615UL;
