b48bb9465b1861bc7196b09b9440e47b332a9534
saisi <saisi@users.noreply.github.com>
2017-04-24 10:06:05 +0000

    1493028365 bannerer

---
+++ 1493028365.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1645519835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xA5DEC4F3L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = (-1L);
+static volatile int64_t g_10 = 0x9117A540F8278B72LL;/* VOLATILE GLOBAL g_10 */
+static const volatile int64_t *g_9 = &g_10;
+static int64_t g_12[9] = {0L,0L,0L,0L,0L,0L,0L,0L,0L};
+static int8_t g_27 = 0L;
+static int8_t *g_36 = (void*)0;
+static int32_t g_40 = 0x71521F00L;
+static int32_t * volatile g_39 = &g_40;/* VOLATILE GLOBAL g_39 */
