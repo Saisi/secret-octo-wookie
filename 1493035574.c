f4e1b7f3df33976e0e5ffb9fc7e80c0bb8b7c25c
saisi <saisi@users.noreply.github.com>
2017-04-24 12:06:15 +0000

    1493035574 asper

---
+++ 1493035574.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3909577983
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 30;
+   signed f1 : 29;
+   volatile signed f2 : 12;
+   signed f3 : 7;
+   const int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static struct S0 g_25[2][8] = {{{21757,-13526,-4,-4,0x91L},{18189,-17379,4,3,0x12L},{21757,-13526,-4,-4,0x91L},{21757,-13526,-4,-4,0x91L},{18189,-17379,4,3,0x12L},{21757,-13526,-4,-4,0x91L},{21757,-13526,-4,-4,0x91L},{18189,-17379,4,3,0x12L}},{{18189,-17379,4,3,0x12L},{21757,-13526,-4,-4,0x91L},{21757,-13526,-4,-4,0x91L},{18189,-17379,4,3,0x12L},{21757,-13526,-4,-4,0x91L},{21757,-13526,-4,-4,0x91L},{18189,-17379,4,3,0x12L},{21757,-13526,-4,-4,0x91L}}};
