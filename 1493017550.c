11a758be20adb4e695d65deac522ab3f471e6b11
saisi <saisi@users.noreply.github.com>
2017-04-24 07:05:50 +0000

    1493017550 angiodermatitis

---
+++ 1493017550.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4035693790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   unsigned f1 : 30;
+   signed f2 : 8;
+   volatile signed f3 : 20;
+   volatile signed f4 : 19;
+   signed f5 : 30;
+   volatile signed f6 : 4;
+   volatile unsigned f7 : 9;
+};
