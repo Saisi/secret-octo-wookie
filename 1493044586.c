34bd27308327c835dcce639704692eb8891fa42d
saisi <saisi@users.noreply.github.com>
2017-04-24 14:36:26 +0000

    1493044586 Adephaga

---
+++ 1493044586.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1731719524
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   int8_t * f1;
+   int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 6L;
+static int64_t g_16[3][8] = {{1L,(-4L),(-4L),1L,1L,(-4L),(-4L),1L},{1L,(-4L),(-4L),1L,1L,(-4L),(-4L),1L},{1L,(-4L),(-4L),1L,1L,(-4L),(-4L),1L}};
+static volatile int64_t g_18 = 0L;/* VOLATILE GLOBAL g_18 */
