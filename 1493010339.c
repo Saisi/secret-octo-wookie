e99525348cbeab8fff9f6455f494d873f34b1db1
saisi <saisi@users.noreply.github.com>
2017-04-24 05:05:40 +0000

    1493010339 antigraphy

---
+++ 1493010339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3668285911
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 10;
+   volatile unsigned f1 : 25;
+   volatile unsigned f2 : 24;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   volatile signed f0 : 12;
