64e4d1e08339f5b1021dd4301af5b63a286469b3
saisi <saisi@users.noreply.github.com>
2017-04-25 17:38:39 +0000

    1493141919 Anaplasma

---
+++ 1493141919.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1952346695
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 2;
+   unsigned f1 : 17;
+   volatile uint16_t  f2;
+   unsigned f3 : 1;
+   const unsigned f4 : 4;
+   const unsigned f5 : 2;
+   signed f6 : 29;
+};
+
