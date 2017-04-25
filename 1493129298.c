2841301f84272111453f61ac8e04e8c446f978fc
saisi <saisi@users.noreply.github.com>
2017-04-25 14:08:18 +0000

    1493129298 adenotomic

---
+++ 1493129298.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4278299384
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 9L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0xC41AC3DCL;
+static uint8_t g_19 = 0x13L;
+static uint8_t g_23 = 255UL;
+static uint8_t *g_22 = &g_23;
+static uint8_t *g_39 = (void*)0;
+static int32_t g_44 = 1L;
+static int32_t *g_43 = &g_44;
+static int64_t g_68 = 8L;
