48ee60eaa3358a6d6845440a85f9c572eac41b2d
saisi <saisi@users.noreply.github.com>
2017-04-25 00:37:16 +0000

    1493080636 anolyte

---
+++ 1493080636.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      993198574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xCC5D9B4EL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x934F545EL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xFDEB7727L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 5L;
+static int32_t g_8 = (-6L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const int16_t  func_1(void);
