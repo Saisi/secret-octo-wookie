fb991ae392be31a2b8448443564d8c91c50e0820
saisi <saisi@users.noreply.github.com>
2017-04-25 19:08:46 +0000

    1493147325 abaser

---
+++ 1493147325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2712610454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   int32_t  f1;
+   uint8_t  f2;
+   uint16_t  f3;
+   uint32_t  f4;
+   uint8_t  f5;
+};
+
+union U1 {
