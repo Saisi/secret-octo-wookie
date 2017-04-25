55b546df3ce1233964d5db69e922a0c64a1f15f3
saisi <saisi@users.noreply.github.com>
2017-04-25 14:38:22 +0000

    1493131100 arithmetical

---
+++ 1493131100.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      566112446
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = 0x6DE7L;
+static int64_t g_6 = (-4L);
+static int32_t g_10 = 0x04F77722L;
+static int32_t g_12 = 0x3AE4C7BAL;
+static int32_t g_14[10][1] = {{0x377D6446L},{0x2A67F6A1L},{0x377D6446L},{0x2A67F6A1L},{0x377D6446L},{0x2A67F6A1L},{0x377D6446L},{0x2A67F6A1L},{0x377D6446L},{0x2A67F6A1L}};
+static int32_t g_16 = 0L;
+static int32_t * const  volatile g_15 = &g_16;/* VOLATILE GLOBAL g_15 */
+static uint8_t g_42 = 0x3CL;
+static uint16_t g_67[9] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL};
