b66714edf673d20f78144f36df7fa76ed3439ad4
saisi <saisi@users.noreply.github.com>
2017-04-25 19:38:49 +0000

    1493149128 Baconianism

---
+++ 1493149128.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3024344958
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 13;
+   int32_t  f1;
+   uint64_t  f2;
+   const int16_t  f3;
+   uint64_t  f4;
+   volatile int32_t  f5;
+};
+
+#pragma pack(push)
