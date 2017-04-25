c525b10546542738c5cb6899448c57b6e6b951ff
saisi <saisi@users.noreply.github.com>
2017-04-25 09:37:57 +0000

    1493113076 ascarids

---
+++ 1493113076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1020690048
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 14;
+   unsigned f1 : 11;
+   signed f2 : 1;
+   unsigned f3 : 9;
+   unsigned f4 : 20;
+};
+
+#pragma pack(push)
+#pragma pack(1)
