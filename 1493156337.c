e7e000639cdcba64b9ea7b031c7954d5b492138a
saisi <saisi@users.noreply.github.com>
2017-04-25 21:38:58 +0000

    1493156337 Angraecum

---
+++ 1493156337.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1812152589
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile int32_t  f1;
+   unsigned f2 : 8;
+   volatile uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_8 = 0xEF6BL;
+static uint32_t g_21 = 3UL;
