8bd5a0a3713532712cc6b9e5d57982136c23bb04
saisi <saisi@users.noreply.github.com>
2017-04-25 07:37:48 +0000

    1493105867 amercements

---
+++ 1493105867.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      33773513
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint64_t  f0;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   volatile uint64_t  f0;
+};
+
+union U2 {
