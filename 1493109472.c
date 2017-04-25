95014c759533b4a12a934f9156ccbae0162e0286
saisi <saisi@users.noreply.github.com>
2017-04-25 08:37:52 +0000

    1493109472 Aptera

---
+++ 1493109472.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      992807800
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = 0x4C00011151C3C39BLL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4[2][10] = {{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)},{(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)}};
+static int8_t g_15 = (-7L);
+static uint16_t g_18 = 0x9FA9L;
+static int32_t *g_22 = &g_4[0][0];
+static int32_t ** volatile g_21 = &g_22;/* VOLATILE GLOBAL g_21 */
+
+
+/* --- FORWARD DECLARATIONS --- */
