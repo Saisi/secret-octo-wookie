76b76c67bf43172b366eedbd8e17b97284884326
saisi <saisi@users.noreply.github.com>
2017-04-26 18:10:54 +0000

    1493230253 Abilyne

---
+++ 1493230253.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      891840246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   volatile int16_t  f1;
+   signed f2 : 2;
+   signed f3 : 28;
+   int32_t  f4;
+   signed f5 : 25;
+   signed f6 : 22;
+   unsigned f7 : 23;
+   signed f8 : 12;
