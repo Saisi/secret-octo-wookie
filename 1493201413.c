4102461cc36d8e3d1659eb855e39052efee8d2e5
saisi <saisi@users.noreply.github.com>
2017-04-26 10:10:13 +0000

    1493201413 ARCM

---
+++ 1493201413.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1921283415
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 10;
+   const signed f1 : 25;
+   signed f2 : 24;
+   signed f3 : 3;
+   unsigned f4 : 8;
+   const volatile signed f5 : 1;
+   volatile unsigned f6 : 28;
+};
+
