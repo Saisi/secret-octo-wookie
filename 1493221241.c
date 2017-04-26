5e8d7c41e905d140edf440dc847fd3cae0333a66
saisi <saisi@users.noreply.github.com>
2017-04-26 15:40:42 +0000

    1493221241 anticapital

---
+++ 1493221241.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      892617767
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   volatile signed f1 : 23;
+   const unsigned f2 : 17;
+   signed f3 : 17;
+   volatile signed f4 : 20;
+   volatile unsigned f5 : 18;
+   signed f6 : 13;
+   unsigned f7 : 19;
+   signed f8 : 22;
