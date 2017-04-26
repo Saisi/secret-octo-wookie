b9e322288efa4fb84da489b1e533ce7441a95874
saisi <saisi@users.noreply.github.com>
2017-04-26 11:10:19 +0000

    1493205018 Antipus

---
+++ 1493205018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3563198120
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int32_t  f1;
+   const uint64_t  f2;
+   const signed f3 : 13;
+   int8_t * volatile  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[10] = {1L,0x1FC3EEE2L,0x72D4EA68L,0x72D4EA68L,0x1FC3EEE2L,1L,0x1FC3EEE2L,0x72D4EA68L,0x72D4EA68L,0x1FC3EEE2L};
