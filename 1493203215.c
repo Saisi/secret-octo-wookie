d27e5a72b94cf0f734441969ee97ca7b12819300
saisi <saisi@users.noreply.github.com>
2017-04-26 10:40:16 +0000

    1493203215 azorite

---
+++ 1493203215.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2292518545
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint32_t  f0;
+   uint64_t  f1;
+   uint64_t  f2;
+   const volatile uint64_t  f3;
+   uint32_t  f4;
+   const int64_t  f5;
+};
+
+#pragma pack(push)
