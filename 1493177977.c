af3003f42899eda3213e1868314ff4643426ba5d
saisi <saisi@users.noreply.github.com>
2017-04-26 03:39:37 +0000

    1493177977 autobuses

---
+++ 1493177977.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2107496206
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   uint32_t  f1;
+   int16_t  f2;
+   volatile int64_t  f3;
+   uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[5][6][3] = {{{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L}},{{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L}},{{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{0x99FB023EL,0xCFB6318DL,0x99FB023EL},{0L,0L,0L},{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L}},{{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L},{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L},{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L}},{{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L},{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L},{(-1L),0x99FB023EL,(-1L)},{0L,0L,0L}}};
