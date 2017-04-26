d05d7071841507e0104c894718268c7095dc9484
saisi <saisi@users.noreply.github.com>
2017-04-26 14:40:37 +0000

    1493217636 ammonite

---
+++ 1493217636.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3803733277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   volatile uint32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x2C70BB74L;/* VOLATILE GLOBAL g_2 */
