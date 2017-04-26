6d2c4ee8e5014f592031b190cc08affa211bce2f
saisi <saisi@users.noreply.github.com>
2017-04-26 08:10:00 +0000

    1493194198 Attalea

---
+++ 1493194198.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4183681386
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint16_t  f0;
+   const uint16_t  f1;
+   const int16_t  f2;
+   volatile uint8_t  f3;
+};
+
+struct S1 {
+   volatile uint16_t  f0;
+   uint32_t  f1;
