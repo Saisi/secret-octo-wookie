5b4a1f76f611c3f924030b1f5834d41b398df8d6
saisi <saisi@users.noreply.github.com>
2017-04-26 09:40:11 +0000

    1493199610 attributive

---
+++ 1493199610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3160929786
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 11;
+   volatile unsigned f1 : 20;
+   const signed f2 : 6;
+   unsigned f3 : 26;
+   unsigned f4 : 1;
+   signed f5 : 20;
+};
