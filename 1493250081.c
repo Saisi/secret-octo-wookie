0355d483dba8035a182e93f542ee0ffa1ef0f1b5
saisi <saisi@users.noreply.github.com>
2017-04-26 23:41:21 +0000

    1493250081 aftermatter

---
+++ 1493250081.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2110846926
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0x18D58162L;
+static volatile uint8_t g_16 = 9UL;/* VOLATILE GLOBAL g_16 */
+static int8_t g_33 = (-10L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+static int8_t  func_28(const int32_t  p_29);
+
