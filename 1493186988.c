62e0ac576c2e0ff3ab616587b906e572089cc218
saisi <saisi@users.noreply.github.com>
2017-04-26 06:09:49 +0000

    1493186988 algol

---
+++ 1493186988.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3983359302
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x7E2799ACL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x4801897AL;
+static int32_t g_7 = 0x7555B7CEL;
+static uint16_t g_10 = 0xB8AEL;
+static int32_t *g_37 = &g_7;
+static int32_t g_67 = 0x6A5D0C9AL;
+static uint64_t g_78[4] = {0x41C5AB2A44BF8E16LL,0x41C5AB2A44BF8E16LL,0x41C5AB2A44BF8E16LL,0x41C5AB2A44BF8E16LL};
+static int8_t g_87[6] = {1L,1L,1L,1L,1L,1L};
+static int64_t g_89 = (-10L);
