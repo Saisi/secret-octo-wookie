e75b3eef82f182ca9f9f8c518a23af5652eb8414
saisi <saisi@users.noreply.github.com>
2017-04-26 15:10:39 +0000

    1493219439 agricolite

---
+++ 1493219439.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      5067178
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 3;
+   const signed f1 : 12;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[4][6] = {{1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL}};
+static int32_t g_4 = (-2L);
+static int32_t *g_7 = &g_4;
+static int32_t **g_6 = &g_7;
