9e7a2053cb16e52eb58e18ec1b7994f9955767c2
saisi <saisi@users.noreply.github.com>
2017-04-26 21:41:12 +0000

    1493242872 aerologists

---
+++ 1493242872.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2440847480
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_6 = 65535UL;/* VOLATILE GLOBAL g_6 */
+static int16_t g_8 = 0x36DEL;
+static int8_t g_9 = 0x8FL;
+static int32_t g_11[3][4][2] = {{{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL},{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL}},{{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL},{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL}},{{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL},{0xB0BEFCDFL,0xAF5BFB66L},{0xB0BEFCDFL,0xB0BEFCDFL}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
