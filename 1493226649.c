2129f1dd0060d5cc74ff1276e058251c86852847
saisi <saisi@users.noreply.github.com>
2017-04-26 17:10:49 +0000

    1493226649 amyotaxia

---
+++ 1493226649.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      110400517
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint32_t g_7 = 0xDBA33992L;
+static uint8_t g_38[6][1] = {{0x5BL},{0x84L},{0x5BL},{0x84L},{0x5BL},{0x84L}};
+static int32_t *g_70 = (void*)0;
+static uint8_t g_72 = 0xE1L;
+static int32_t g_76 = 0x28BF2FEAL;
