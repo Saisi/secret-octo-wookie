091a6e95d7c1f31f2ce52edc3b0474c1c321dbf4
saisi <saisi@users.noreply.github.com>
2017-04-26 07:09:54 +0000

    1493190593 baffeta

---
+++ 1493190593.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2157863575
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   int32_t  f1;
+   int64_t  f2;
+   volatile int8_t * volatile  f3;
+   uint16_t  f4;
+};
+
+union U1 {
+   const uint32_t  f0;
