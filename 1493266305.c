ec98e58690ebbcca05f561c8b5a5d2fe29c13f7f
saisi <saisi@users.noreply.github.com>
2017-04-27 04:11:46 +0000

    1493266305 ballista

---
+++ 1493266305.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2585027787
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 4;
+   uint32_t  f1;
+   const uint32_t  f2;
+   const volatile uint32_t  f3;
+   const uint8_t  f4;
+};
+
+union U1 {
+   uint64_t  f0;
