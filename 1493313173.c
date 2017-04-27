5336beaba67adc4e2905870863b473546e3bc477
saisi <saisi@users.noreply.github.com>
2017-04-27 17:12:54 +0000

    1493313173 Balaenicipites

---
+++ 1493313173.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      939971547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   const unsigned f1 : 13;
+   uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_5 = (-1L);
+static int32_t g_19[9][6][4] = {{{1L,(-1L),(-1L),1L},{(-1L),1L,0L,0xFEC46BC2L},{(-1L),0L,(-1L),0x9FF446CDL},{1L,0xFEC46BC2L,0x9FF446CDL,0x9FF446CDL},{0L,0L,(-1L),0xFEC46BC2L},{0xFEC46BC2L,1L,(-1L),1L}},{{0L,(-1L),0x9FF446CDL,(-1L)},{1L,(-1L),(-1L),1L},{(-1L),1L,0L,0xFEC46BC2L},{(-1L),0L,(-1L),0x9FF446CDL},{1L,0xFEC46BC2L,0x9FF446CDL,0x9FF446CDL},{0L,0L,0L,0x9FF446CDL}},{{0x9FF446CDL,(-1L),0L,(-1L)},{(-6L),0xFEC46BC2L,(-1L),0L},{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)},{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL},{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)},{(-1L),0x9FF446CDL,(-1L),(-1L)}},{{(-6L),(-6L),0L,0x9FF446CDL},{0x9FF446CDL,(-1L),0L,(-1L)},{(-6L),0xFEC46BC2L,(-1L),0L},{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)},{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL},{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)}},{{(-1L),0x9FF446CDL,(-1L),(-1L)},{(-6L),(-6L),0L,0x9FF446CDL},{0x9FF446CDL,(-1L),0L,(-1L)},{(-6L),0xFEC46BC2L,(-1L),0L},{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)},{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL}},{{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)},{(-1L),0x9FF446CDL,(-1L),(-1L)},{(-6L),(-6L),0L,0x9FF446CDL},{0x9FF446CDL,(-1L),0L,(-1L)},{(-6L),0xFEC46BC2L,(-1L),0L},{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)}},{{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL},{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)},{(-1L),0x9FF446CDL,(-1L),(-1L)},{(-6L),(-6L),0L,0x9FF446CDL},{0x9FF446CDL,(-1L),0L,(-1L)},{(-6L),0xFEC46BC2L,(-1L),0L}},{{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)},{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL},{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)},{(-1L),0x9FF446CDL,(-1L),(-1L)},{(-6L),(-6L),0L,0x9FF446CDL},{0x9FF446CDL,(-1L),0L,(-1L)}},{{(-6L),0xFEC46BC2L,(-1L),0L},{(-1L),0xFEC46BC2L,0xFEC46BC2L,(-1L)},{0xFEC46BC2L,(-1L),(-6L),0x9FF446CDL},{0xFEC46BC2L,(-6L),0xFEC46BC2L,(-1L)},{(-1L),0x9FF446CDL,(-1L),(-1L)},{(-6L),(-6L),0L,0x9FF446CDL}}};
+static int32_t *g_18 = &g_19[6][1][1];
