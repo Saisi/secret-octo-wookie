30beecc718403e5d352e50a513f7a5a83d2ad54b
saisi <saisi@users.noreply.github.com>
2017-04-27 21:13:17 +0000

    1493327597 acetylsalol

---
+++ 1493327597.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3951076175
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S2 {
+   const signed f0 : 28;
+   unsigned f1 : 25;
+   volatile signed f2 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_3 = 0xEEL;
+static struct S2 g_4 = {5016,2327,-76};/* VOLATILE GLOBAL g_4 */
+
