aa3c7ffd5fd793b55655c648261c67fcc65bcd29
saisi <saisi@users.noreply.github.com>
2017-04-27 07:42:01 +0000

    1493278921 attenuators

---
+++ 1493278921.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      75759573
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   int8_t * volatile  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int8_t g_4[4][4][2] = {{{0x11L,(-1L)},{(-1L),0x11L},{(-1L),(-1L)},{0x11L,(-1L)}},{{(-1L),0x11L},{(-1L),(-1L)},{0x11L,(-1L)},{(-1L),0x11L}},{{(-1L),(-1L)},{0x11L,(-1L)},{(-1L),0x11L},{(-1L),(-1L)}},{{0x11L,(-1L)},{(-1L),0x11L},{(-1L),(-1L)},{0x11L,(-1L)}}};
+static int8_t g_19[2] = {1L,1L};
+static int8_t *g_18 = &g_19[1];
+static int8_t * volatile *g_17 = &g_18;
