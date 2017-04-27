b3cb86083ef4349c901fddf5c0ca18f5efcb40ca
saisi <saisi@users.noreply.github.com>
2017-04-27 05:41:53 +0000

    1493271712 aerifaction

---
+++ 1493271712.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      555167414
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x24F01C08L;
+static int64_t g_7 = 0x1E29EA41449C9FB3LL;
+static int16_t g_8[7] = {0x8A01L,0x8A01L,0x8A01L,0x8A01L,0x8A01L,0x8A01L,0x8A01L};
+static volatile uint32_t g_9[3] = {0x83D78398L,0x83D78398L,0x83D78398L};
+static uint32_t g_27 = 0xFC3E8CE6L;
+static uint8_t g_31 = 0xD8L;
+static int32_t g_33 = 0xFCA86293L;
+static int32_t *g_32 = &g_33;
+static uint64_t g_38 = 0x6D9918C6A8663C10LL;
