d17e8d5af20744140748af6faac23acc156ee938
saisi <saisi@users.noreply.github.com>
2017-04-27 06:41:57 +0000

    1493275316 amalic

---
+++ 1493275316.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      343791610
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 11;
+   unsigned f1 : 20;
+   signed f2 : 6;
+   unsigned f3 : 26;
+   signed f4 : 1;
+   const signed f5 : 20;
+};
+
+#pragma pack(push)
