6ccfe103dcb0f3083f28c44c6de41b37d20379d8
saisi <saisi@users.noreply.github.com>
2017-04-27 03:11:41 +0000

    1493262701 aneurysmatic

---
+++ 1493262701.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2479269202
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-6L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 7L;
+static int32_t g_34 = (-5L);
