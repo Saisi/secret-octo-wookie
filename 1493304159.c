7bbf53aa3d155301189607b5691978d11aef8696
saisi <saisi@users.noreply.github.com>
2017-04-27 14:42:39 +0000

    1493304159 algaroba

---
+++ 1493304159.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      19591824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0xB8F6C218L;
+static volatile int16_t g_18 = 0x4DF8L;/* VOLATILE GLOBAL g_18 */
+static int64_t g_19 = 0xC1FCC8AF95342363LL;
+static volatile int16_t g_20 = 0x7AE2L;/* VOLATILE GLOBAL g_20 */
+static uint8_t g_21 = 0x08L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
