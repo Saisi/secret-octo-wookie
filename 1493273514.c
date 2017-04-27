6cc402808508c062945d48ebaa6c60e6b89e06b2
saisi <saisi@users.noreply.github.com>
2017-04-27 06:11:54 +0000

    1493273514 arisard

---
+++ 1493273514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      307310790
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_5 = 0x555B7CEFL;
+static const int32_t *g_4[7][6] = {{&g_5,&g_5,&g_5,&g_5,&g_5,&g_5},{&g_5,(void*)0,&g_5,&g_5,(void*)0,&g_5},{(void*)0,&g_5,&g_5,&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5,(void*)0,(void*)0,&g_5},{&g_5,(void*)0,&g_5,&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5,&g_5,&g_5,&g_5},{&g_5,&g_5,&g_5,&g_5,&g_5,&g_5}};
+static const int32_t ** volatile g_3 = &g_4[6][0];/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
