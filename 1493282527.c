47d8f417e43cb1bf9b93de531827fbe5111aad80
saisi <saisi@users.noreply.github.com>
2017-04-27 08:42:07 +0000

    1493282527 Arends

---
+++ 1493282527.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4235605439
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   const uint16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_4 = 0x5F394A2AL;
+
+
+/* --- FORWARD DECLARATIONS --- */
