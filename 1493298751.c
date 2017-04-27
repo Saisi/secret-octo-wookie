90192e15ff09793579d5568a1e83b1c2af5e44d5
saisi <saisi@users.noreply.github.com>
2017-04-27 13:12:32 +0000

    1493298751 adiagnostic

---
+++ 1493298751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1231333909
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 0x4C23AFC3L;
+static int32_t g_12 = 0xA80E3580L;
+static int8_t g_15 = 1L;
+static uint32_t g_17 = 0xBD42CA8CL;
