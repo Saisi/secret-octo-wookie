d9cdb3f0e21ddf68a3ad9b979641a0d2eb3a8796
saisi <saisi@users.noreply.github.com>
2017-04-27 01:41:34 +0000

    1493257292 Artevelde

---
+++ 1493257292.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3834234585
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int32_t  f1;
+   const int64_t  f2;
+};
+
+union U1 {
+   int16_t  f0;
+   const int8_t * volatile  f1;
+};
