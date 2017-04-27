f81db0430c86582da0e68094e8c0b5e9c57d01a7
saisi <saisi@users.noreply.github.com>
2017-04-27 15:12:41 +0000

    1493305961 ammoniuria

---
+++ 1493305961.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4131936407
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static uint32_t g_26[10] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL};
+static int32_t g_56 = (-1L);
+static uint32_t g_72 = 0x9E0D9448L;
+static uint8_t g_77 = 0x8FL;
+static uint8_t *g_89 = &g_77;
+static uint16_t g_98 = 0xAE3AL;
+static int64_t g_102 = 0xA6627841B20306C0LL;
+static uint8_t g_115 = 255UL;
