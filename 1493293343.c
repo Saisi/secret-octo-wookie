cfd6785e8237ddc9b141b43c194d37c2c4e20b7c
saisi <saisi@users.noreply.github.com>
2017-04-27 11:42:24 +0000

    1493293343 agronomical

---
+++ 1493293343.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      900677378
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   uint32_t  f1;
+   const volatile int8_t  f2;
+   int8_t * const  f3;
+   volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_14[3][3][5] = {{{0x4A9BACDEL,0UL,0x4A9BACDEL,0x4A9BACDEL,0UL},{0UL,0x4A9BACDEL,0x4A9BACDEL,0UL,0x4A9BACDEL},{0UL,0UL,0xF8D19C92L,0UL,0UL}},{{0x4A9BACDEL,0UL,0x4A9BACDEL,0x4A9BACDEL,0UL},{0UL,0x4A9BACDEL,0x4A9BACDEL,0UL,0x4A9BACDEL},{0UL,0UL,0xF8D19C92L,0UL,0UL}},{{0x4A9BACDEL,0UL,0x4A9BACDEL,0x4A9BACDEL,0UL},{0UL,0x4A9BACDEL,0x4A9BACDEL,0UL,0x4A9BACDEL},{0UL,0UL,0xF8D19C92L,0UL,0UL}}};
