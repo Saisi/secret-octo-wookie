dfc13f7c19ea1647eb772b3dbcb28428d14c55f3
saisi <saisi@users.noreply.github.com>
2017-04-27 02:41:39 +0000

    1493260899 anatreptic

---
+++ 1493260899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1848052776
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   uint16_t  f1;
+   uint8_t  f2;
+   volatile int64_t  f3;
+   const volatile unsigned f4 : 17;
+   unsigned f5 : 7;
+   const uint8_t  f6;
+};
+
