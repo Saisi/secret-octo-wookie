012d94f2c1e9aa8ad47d81481269463d66d73e83
saisi <saisi@users.noreply.github.com>
2017-04-27 20:43:15 +0000

    1493325794 axial

---
+++ 1493325794.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2850760444
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 4L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xFE387FB0L;
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0x5158018DL;
+static int32_t g_12 = 0xF8716B5BL;
+static int32_t * volatile g_78 = &g_12;/* VOLATILE GLOBAL g_78 */
+static int32_t * volatile g_79 = &g_12;/* VOLATILE GLOBAL g_79 */
+static int32_t * volatile *g_77[9] = {&g_79,&g_78,&g_79,&g_79,&g_78,&g_79,&g_79,&g_78,&g_79};
+static uint16_t g_89 = 0UL;
