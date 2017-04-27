f69aefe8724072c82ddabb4d98a2ef8aec1ed77a
saisi <saisi@users.noreply.github.com>
2017-04-27 12:42:29 +0000

    1493296949 apasttra

---
+++ 1493296949.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      757784712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const uint8_t  f0;
+};
+
+struct S1 {
+   int32_t  f0;
+};
+
+union U2 {
+   volatile int16_t  f0;
