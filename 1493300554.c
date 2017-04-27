da4bb333f36d4bef9d06944ec7bb2e87c1b56b7c
saisi <saisi@users.noreply.github.com>
2017-04-27 13:42:35 +0000

    1493300554 antiluetic

---
+++ 1493300554.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1810485179
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   uint16_t  f1;
+   uint16_t  f2;
+   const uint8_t  f3;
+   const uint32_t  f4;
+   int32_t  f5;
+   int8_t  f6;
+};
+
