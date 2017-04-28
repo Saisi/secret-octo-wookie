006e22640e158997b37a0df79fdbeeab4cf544b6
saisi <saisi@users.noreply.github.com>
2017-04-28 10:14:30 +0000

    1493374469 apolaustic

---
+++ 1493374469.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3666658959
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   int8_t  f1;
+   int32_t  f2;
+   int32_t  f3;
+   uint16_t  f4;
+   unsigned f5 : 4;
+   int8_t  f6;
+};
+
