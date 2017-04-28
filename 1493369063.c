079cb938672d653a7a3579bec6b0354f95e96fd1
saisi <saisi@users.noreply.github.com>
2017-04-28 08:44:23 +0000

    1493369063 Aurantiaceae

---
+++ 1493369063.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3059576658
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x7013DCE9L;
+static int32_t * volatile g_2 = &g_3;/* VOLATILE GLOBAL g_2 */
+static int32_t g_18 = 0x945CB81EL;
+static volatile int8_t *g_21 = (void*)0;
+static int8_t g_24[7] = {0xCDL,0x79L,0xCDL,0xCDL,0x79L,0xCDL,0xCDL};
