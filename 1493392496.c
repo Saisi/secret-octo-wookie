7bb60b39c2bf10e48557e04d559f520f4ec7c8b2
saisi <saisi@users.noreply.github.com>
2017-04-28 15:14:57 +0000

    1493392496 alkaloids

---
+++ 1493392496.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      931743108
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5E48D327L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0L;
+static int32_t g_15 = 0x9F30CC2FL;
+static int8_t g_36 = (-2L);
+static uint16_t g_64[7] = {0x197BL,0x197BL,0x197BL,0x197BL,0x197BL,0x197BL,0x197BL};
+static int32_t g_81 = 0x4961F084L;
+static uint64_t g_89 = 0xA5AF80CEF6D7B3CALL;
+static int32_t g_100[1] = {0xD423D3ABL};
+static uint32_t g_155[8] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL};
