a119da1413f4bd69481cf9babae8a37f0125fdb0
saisi <saisi@users.noreply.github.com>
2017-04-28 09:44:27 +0000

    1493372667 alphabetizers

---
+++ 1493372667.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3603294329
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 27;
+   volatile signed f1 : 6;
+   volatile signed f2 : 17;
+   signed f3 : 24;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S0 g_4 = {262,-4,66,-3924};/* VOLATILE GLOBAL g_4 */
+static uint32_t g_5 = 0x2D9EE1C9L;
