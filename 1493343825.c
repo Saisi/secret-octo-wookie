05c44cb763368860be0e363fb902d76892926764
saisi <saisi@users.noreply.github.com>
2017-04-28 01:43:45 +0000

    1493343825 antiking

---
+++ 1493343825.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3580865076
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 29;
+   signed f2 : 11;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
