f8ec2ec8dda0b416938daf8b2091d9fd6f9de04d
saisi <saisi@users.noreply.github.com>
2017-04-28 22:15:39 +0000

    1493417739 agorot

---
+++ 1493417739.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3123877204
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 24;
+   const signed f1 : 13;
+   signed f2 : 27;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_6 = 0xF307D7C1L;
+static uint32_t g_41[6] = {0x137D6C50L,0x137D6C50L,0x137D6C50L,0x137D6C50L,0x137D6C50L,0x137D6C50L};
+static volatile int32_t g_55 = (-1L);/* VOLATILE GLOBAL g_55 */
