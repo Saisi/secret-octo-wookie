c40c3509904c57b8d078d42e9fe5bb8157406d27
saisi <saisi@users.noreply.github.com>
2017-04-28 19:45:26 +0000

    1493408726 Aceratherium

---
+++ 1493408726.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      584877267
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int32_t *g_21[5][7] = {{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,(void*)0,(void*)0,(void*)0,&g_2,(void*)0,(void*)0},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,(void*)0,&g_2,&g_2,&g_2},{&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2}};
+static int32_t **g_32 = &g_21[1][4];
+static int32_t g_34[5][5][5] = {{{2L,0L,(-10L),(-10L),0L},{0x04A32A0CL,(-1L),(-1L),(-1L),(-1L)},{2L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L}},{{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L}},{{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L}},{{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L}},{{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L},{0L,0x3ADB0B63L,(-1L),(-1L),0x3ADB0B63L},{1L,1L,0L,0L,1L}}};
+static int32_t ** volatile g_35 = &g_21[0][1];/* VOLATILE GLOBAL g_35 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
