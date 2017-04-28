c59ec6b9dda391b28b94d8f5373cebfe0614cfb3
saisi <saisi@users.noreply.github.com>
2017-04-28 09:14:25 +0000

    1493370865 Abramis

---
+++ 1493370865.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3339976555
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 20;
+   volatile unsigned f1 : 27;
+   volatile signed f2 : 18;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
