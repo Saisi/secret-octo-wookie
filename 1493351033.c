7ac7680d380f2966606f7b2527bfc440606d3fc6
saisi <saisi@users.noreply.github.com>
2017-04-28 03:43:56 +0000

    1493351033 antirabic

---
+++ 1493351033.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      428529063
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 18;
+   const uint16_t  f1;
+   signed f2 : 17;
+   unsigned f3 : 17;
+   const signed f4 : 20;
+   volatile unsigned f5 : 18;
+   unsigned f6 : 13;
+};
+
