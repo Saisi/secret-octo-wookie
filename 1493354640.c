ac3d9d96ba3de8e4643359c1092a2ab30d98aaaa
saisi <saisi@users.noreply.github.com>
2017-04-28 04:44:01 +0000

    1493354640 barandos

---
+++ 1493354640.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2886739651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0L;
+static volatile int32_t g_7 = 0L;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = (-9L);
+static int32_t g_12[2][6] = {{0x4F147909L,(-1L),1L,1L,(-1L),0x4F147909L},{0xA1A4D63AL,0x4F147909L,1L,0x4F147909L,0xA1A4D63AL,0xA1A4D63AL}};
+static int32_t * volatile g_11 = &g_12[0][2];/* VOLATILE GLOBAL g_11 */
+static int32_t *g_14 = &g_4;
+static int32_t ** volatile g_13 = &g_14;/* VOLATILE GLOBAL g_13 */
