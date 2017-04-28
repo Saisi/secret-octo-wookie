fa13a40703814f049f71765f749c71596697cc00
saisi <saisi@users.noreply.github.com>
2017-04-28 07:14:16 +0000

    1493363656 acarodermatitis

---
+++ 1493363656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1843433682
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   volatile uint32_t  f1;
+   int32_t  f2;
+   const volatile uint32_t  f3;
+   volatile uint64_t  f4;
+   volatile signed f5 : 8;
+   uint32_t  f6;
+   const volatile int32_t  f7;
+   int16_t  f8;
