b4eba1d253a9bbc5f1ab1f664e0869ccee62f330
saisi <saisi@users.noreply.github.com>
2017-04-28 21:15:34 +0000

    1493414133 acupunctures

---
+++ 1493414133.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3352670835
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 24;
+   unsigned f1 : 15;
+   signed f2 : 22;
+   signed f3 : 9;
+   signed f4 : 22;
+   const signed f5 : 23;
+   unsigned f6 : 26;
