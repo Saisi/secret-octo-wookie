84950f0684113690b5fb959fe452297f66105103
saisi <saisi@users.noreply.github.com>
2017-04-29 23:47:55 +0000

    1493509674 antiseptic

---
+++ 1493509674.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1581412491
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xB9B55BB2L;
+static int32_t g_5[10] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static int32_t g_6 = 0xA73D2D67L;
+static int64_t g_19 = 0x6D8FF8CD3BED162CLL;
+static uint64_t g_38 = 18446744073709551609UL;
+static int16_t g_39 = 0x8F96L;
+static uint16_t g_123 = 0x7F61L;
+static uint64_t g_134 = 1UL;
+static int8_t g_148 = (-1L);
