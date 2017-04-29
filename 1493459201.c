6b192dc202560b058747a61f4591538619f81e08
saisi <saisi@users.noreply.github.com>
2017-04-29 09:46:41 +0000

    1493459201 almonage

---
+++ 1493459201.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1465409533
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x85A1FA14L;
+static volatile uint8_t g_4[2] = {0xFBL,0xFBL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
