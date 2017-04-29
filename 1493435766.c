c5fbb02c89d5b32af766564c703e89f0202abda0
saisi <saisi@users.noreply.github.com>
2017-04-29 03:16:07 +0000

    1493435766 agamoid

---
+++ 1493435766.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      583273144
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[4][2][10] = {{{0x39L,0x5EL,0UL,0x24L,0x7FL,0x21L,0x67L,0x7FL,0x2EL,0x51L},{0UL,5UL,0x9BL,255UL,247UL,0UL,247UL,255UL,0x9BL,5UL}},{{0x96L,0x21L,0x51L,255UL,1UL,0x24L,255UL,0x39L,255UL,0x96L},{0x51L,0UL,0x21L,1UL,5UL,0x24L,0x67L,0UL,0UL,0UL}},{{0x96L,255UL,0UL,0UL,0UL,0UL,255UL,1UL,255UL,0x2EL},{0x51L,1UL,1UL,0x39L,0UL,0UL,0x51L,0UL,4UL,0x51L}},{{4UL,0x24L,1UL,0UL,0x24L,0UL,0UL,1UL,0UL,0UL},{255UL,0x2EL,5UL,0x2EL,255UL,0UL,0x39L,0x51L,0UL,0x21L}}};
+static int32_t g_3 = 0L;
+static volatile int32_t g_4 = 0xB4C8F0D0L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5[9] = {0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L,0xAD38DC53L};
