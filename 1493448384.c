a70c5da8a3efaeb76727dfa3206a9ae187f6c1f1
saisi <saisi@users.noreply.github.com>
2017-04-29 06:46:26 +0000

    1493448384 autoneurotoxin

---
+++ 1493448384.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2964299608
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   int8_t  f1;
+   const uint8_t  f2;
+   int32_t  f3;
+   const uint64_t  f4;
+   volatile int32_t  f5;
+   int64_t  f6;
+   int16_t  f7;
+   volatile uint32_t  f8;
