07d48d518920d884d971034014965e54efcc894f
saisi <saisi@users.noreply.github.com>
2017-04-29 17:47:23 +0000

    1493488043 acrosomes

---
+++ 1493488043.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      161003634
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 7;
+   int8_t  f1;
+   signed f2 : 2;
+   unsigned f3 : 6;
+   volatile unsigned f4 : 29;
+   signed : 0;
+   const int8_t  f5;
+   unsigned f6 : 5;
+};
