22ab68cdf83d9603f98950396f44c495f012053d
saisi <saisi@users.noreply.github.com>
2017-04-29 14:47:09 +0000

    1493477228 Assuerus

---
+++ 1493477228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2979250850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int32_t  f0;
+   signed f1 : 24;
+   uint32_t  f2;
+};
+
+union U1 {
+   uint64_t  f0;
+   signed f1 : 18;
+   int32_t  f2;
