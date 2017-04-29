d18a9d9455f40771eaf8afe1dc96dc3fd893f95d
saisi <saisi@users.noreply.github.com>
2017-04-29 11:46:52 +0000

    1493466412 arrogator

---
+++ 1493466412.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3203031184
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   unsigned f1 : 4;
+   int64_t  f2;
+   const volatile uint8_t  f3;
+   const int8_t * volatile  f4;
+};
+
+union U1 {
+   int64_t  f0;
