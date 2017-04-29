01fb1e6d2466d9fd44a5d1e9635bd5cb549d1a5e
saisi <saisi@users.noreply.github.com>
2017-04-29 00:15:50 +0000

    1493424950 audition

---
+++ 1493424950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4089811423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 14;
+};
+
+struct S1 {
+   uint16_t  f0;
+   uint32_t  f1;
+   volatile int32_t  f2;
+   signed f3 : 4;
+};
