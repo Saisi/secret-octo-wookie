c8a6d00bf09d3512f0fc74b050d1ea434e220eb2
saisi <saisi@users.noreply.github.com>
2017-04-30 17:19:42 +0000

    1493572781 accommodationist

---
+++ 1493572781.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3798921242
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 27;
+   const int8_t  f1;
+};
+
+union U1 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
