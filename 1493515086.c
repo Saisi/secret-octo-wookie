65a960ea9bba3d58bdfb338bdc97ac6d78f30111
saisi <saisi@users.noreply.github.com>
2017-04-30 01:18:06 +0000

    1493515086 antibiont

---
+++ 1493515086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1139575932
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+union U1 {
+   signed f0 : 1;
+   volatile uint32_t  f1;
+};
+
+union U2 {
