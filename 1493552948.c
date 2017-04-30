e8ce48ff499be3919d0f4a1e6540f780d73f9b56
saisi <saisi@users.noreply.github.com>
2017-04-30 11:49:09 +0000

    1493552948 awacs

---
+++ 1493552948.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2263052423
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[2][4][1] = {{{0x18D4081CL},{9L},{9L},{0x18D4081CL}},{{9L},{9L},{0x18D4081CL},{9L}}};
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x7D8E6E83L;
+static uint32_t g_39 = 0x910A83AFL;
+static uint64_t g_42 = 0x56A218F0ED693765LL;
+static int32_t g_49 = 0x9D070FEEL;
+static int8_t g_93 = 0x8DL;
+static volatile int32_t g_124 = 0xAFE12C2FL;/* VOLATILE GLOBAL g_124 */
+static volatile int32_t *g_123[4] = {&g_124,&g_124,&g_124,&g_124};
