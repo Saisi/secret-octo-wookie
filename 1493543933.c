07a2337b0886d0433afe30576df2870a719e20f1
saisi <saisi@users.noreply.github.com>
2017-04-30 09:18:55 +0000

    1493543933 acoustics

---
+++ 1493543933.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3697411116
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 4;
+   const uint32_t  f1;
+   signed f2 : 7;
+   const volatile unsigned f3 : 23;
+   const signed f4 : 10;
+   unsigned f5 : 25;
+   unsigned f6 : 19;
