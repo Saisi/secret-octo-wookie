13dc6b4a23bc8de29dc712ccd11db67aa29304ad
saisi <saisi@users.noreply.github.com>
2017-04-30 08:48:51 +0000

    1493542130 anemonol

---
+++ 1493542130.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1389130094
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int8_t * f1;
+   int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xF9B2CFB6L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[2][9] = {{(-5L),(-1L),(-5L),(-5L),(-1L),(-5L),(-5L),(-1L),(-5L)},{(-5L),(-1L),(-5L),(-5L),(-1L),(-5L),(-5L),(-1L),(-5L)}};
+static int32_t g_7 = (-1L);
