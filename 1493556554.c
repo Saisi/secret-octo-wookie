509729fc5b4f6b6574ecab8e6e9c4d387cd21ffd
saisi <saisi@users.noreply.github.com>
2017-04-30 12:49:15 +0000

    1493556554 aduncated

---
+++ 1493556554.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1322728599
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3[4] = {0L,0L,0L,0L};
+static int16_t g_5 = 0x3607L;
+static uint16_t g_6 = 9UL;
+static int16_t g_12 = 0x0086L;
+static int32_t g_47 = 0L;
+static uint16_t g_62[2][5][8] = {{{65535UL,0xF746L,0xF746L,65535UL,0xAF47L,0x686BL,0xB62EL,6UL},{65535UL,0xF746L,7UL,65535UL,6UL,0x686BL,0x686BL,6UL},{65535UL,0xF746L,0xF746L,65535UL,0xAF47L,0x686BL,0xB62EL,6UL},{65535UL,0xF746L,7UL,65535UL,6UL,0x686BL,0x686BL,6UL},{65535UL,0xF746L,0xF746L,65535UL,0xAF47L,0x686BL,0xB62EL,6UL}},{{65535UL,0xF746L,7UL,65535UL,6UL,0x686BL,0x686BL,6UL},{65535UL,0xF746L,0xF746L,65535UL,0xAF47L,0x686BL,0xB62EL,6UL},{65535UL,0xF746L,7UL,65535UL,6UL,0x686BL,0x686BL,6UL},{65535UL,0xF746L,0xF746L,65535UL,0xAF47L,0x686BL,0xB62EL,6UL},{65535UL,0xF746L,7UL,65535UL,6UL,0x686BL,0x686BL,6UL}}};
+static int64_t g_75 = 0L;
+static int16_t g_87 = 0xE799L;
+static int16_t *g_86[6][1][6] = {{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}},{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}},{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}},{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}},{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}},{{&g_12,&g_87,&g_12,&g_12,&g_87,&g_12}}};
