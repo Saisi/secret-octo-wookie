c47861141c3eacec289b3893a41be6263639ec64
saisi <saisi@users.noreply.github.com>
2017-04-30 16:19:35 +0000

    1493569174 alizarine

---
+++ 1493569174.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2512826845
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint64_t  f0;
+   volatile int8_t * const  volatile  f1;
+   volatile int64_t  f2;
+   const volatile int16_t  f3;
+};
+
+union U1 {
+   const uint16_t  f0;
+};
