1f58323124905b8dd2d7a03ff5d8b5357e4b4ca9
saisi <saisi@users.noreply.github.com>
2017-04-30 06:18:37 +0000

    1493533116 acroamata

---
+++ 1493533116.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2836956269
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   uint32_t  f1;
+   uint16_t  f2;
+   signed f3 : 14;
+   int16_t  f4;
+   unsigned f5 : 28;
+};
+
+struct S1 {
