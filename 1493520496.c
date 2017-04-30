001234ec24844dcd4040f3532ddf8e666451b42d
saisi <saisi@users.noreply.github.com>
2017-04-30 02:48:16 +0000

    1493520496 aplacophorous

---
+++ 1493520496.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1197325681
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 0x57L;
+static uint8_t g_45 = 247UL;
+static uint8_t g_48[8][5] = {{0x2AL,255UL,255UL,0x2AL,255UL},{0UL,1UL,0UL,0UL,1UL},{255UL,255UL,0x3EL,0x94L,0x94L},{0xB4L,3UL,0xB4L,0UL,248UL},{0x34L,0x2AL,0x94L,0x2AL,0x34L},{0xB4L,0UL,3UL,1UL,3UL},{255UL,255UL,0x94L,0x34L,255UL},{0UL,0xB4L,0xB4L,0UL,3UL}};
+static int8_t g_50 = 0xE9L;
+static uint8_t *g_65[7][3] = {{&g_4,&g_4,&g_45},{&g_4,&g_4,&g_45},{&g_48[6][4],&g_48[6][4],&g_4},{&g_48[6][4],&g_48[6][4],&g_4},{&g_48[6][4],&g_48[6][4],&g_4},{&g_48[6][4],&g_48[6][4],&g_4},{&g_48[6][4],&g_48[6][4],&g_4}};
+static uint8_t ** const g_64 = &g_65[2][0];
+static const int8_t *g_72 = &g_50;
+static volatile uint32_t g_73[2] = {3UL,3UL};
+static int32_t g_88 = 0L;
