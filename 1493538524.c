e56af39158d189f15b8788974da2945434f0898e
saisi <saisi@users.noreply.github.com>
2017-04-30 07:48:45 +0000

    1493538524 balaenoid

---
+++ 1493538524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1738737664
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   uint16_t  f1;
+   uint8_t  f2;
+   int16_t  f3;
+   const volatile uint64_t  f4;
+   unsigned f5 : 3;
+   int32_t  f6;
+   unsigned f7 : 13;
+   const int16_t  f8;
