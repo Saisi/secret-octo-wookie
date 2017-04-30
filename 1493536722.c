be7159161b6d555e139d9e2a90dbe0cff3ce6281
saisi <saisi@users.noreply.github.com>
2017-04-30 07:18:42 +0000

    1493536722 anisometropia

---
+++ 1493536722.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      728733063
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x713B9102L;
+static int32_t g_6 = (-1L);
+static int16_t g_30 = 1L;
+static int32_t g_44 = 0xB91300D3L;
+static int32_t g_67 = (-9L);
+static volatile int32_t g_70 = 0L;/* VOLATILE GLOBAL g_70 */
+static volatile int32_t g_71 = 6L;/* VOLATILE GLOBAL g_71 */
+static int32_t g_72 = 0xEC334572L;
+static volatile int32_t g_75[1][9] = {{5L,5L,5L,5L,5L,5L,5L,5L,5L}};
