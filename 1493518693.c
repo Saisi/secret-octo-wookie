9b26978eab8d0ff8f7dfffe6fc005e60a96e58bd
saisi <saisi@users.noreply.github.com>
2017-04-30 02:18:13 +0000

    1493518693 antireactive

---
+++ 1493518693.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3284797864
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_4 = 0x4A8BL;/* VOLATILE GLOBAL g_4 */
+static int16_t g_5 = 1L;
+static int32_t g_23 = 0xFA6F63EEL;
+static uint64_t g_25[1] = {0xC7DF39F3C5A4A104LL};
+static int32_t g_76 = (-5L);
+static volatile int32_t g_77 = (-6L);/* VOLATILE GLOBAL g_77 */
+static int32_t g_78 = 0x1739C36DL;
+static int32_t g_79 = 0xD24BD9F5L;
+static uint16_t g_82 = 65529UL;
