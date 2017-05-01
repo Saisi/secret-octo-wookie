082de15e86d5213dab528dc23143fc89e312e454
saisi <saisi@users.noreply.github.com>
2017-05-01 19:52:08 +0000

    1493668328 armlock

---
+++ 1493668328.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2743191959
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x4A18885CL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = (-7L);
+static volatile int32_t g_6 = 1L;/* VOLATILE GLOBAL g_6 */
