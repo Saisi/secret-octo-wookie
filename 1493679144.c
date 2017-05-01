339925d76e8b09c3abff2dc45e70cbccb5a22290
saisi <saisi@users.noreply.github.com>
2017-05-01 22:52:24 +0000

    1493679144 appendotome

---
+++ 1493679144.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2805445815
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x6E60B0CEL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x94FE5F36L;
+static int32_t g_7 = 0xA9D69236L;
+static volatile uint32_t g_10 = 9UL;/* VOLATILE GLOBAL g_10 */
+static volatile uint32_t g_14[3] = {7UL,7UL,7UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
