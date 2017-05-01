8ca3b827cd237a697cd4a1b975eab473606bfc72
saisi <saisi@users.noreply.github.com>
2017-05-01 12:21:24 +0000

    1493641284 aquamanale

---
+++ 1493641284.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4024020501
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_31 = 0xD9A2B82C4475EE4CLL;
+static uint16_t g_96 = 1UL;
+static int32_t g_107 = 0x587DEF5AL;
+static int32_t * volatile g_106 = &g_107;/* VOLATILE GLOBAL g_106 */
+static int32_t g_118 = 1L;
+static int32_t * volatile g_117 = &g_118;/* VOLATILE GLOBAL g_117 */
+static uint32_t g_158 = 0UL;
+static volatile uint8_t g_166 = 0x91L;/* VOLATILE GLOBAL g_166 */
+static volatile uint8_t *g_165 = &g_166;
