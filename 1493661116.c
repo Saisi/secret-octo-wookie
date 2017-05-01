4474e3150d91dfc7b4a5d3de7d4d5d5b007c691e
saisi <saisi@users.noreply.github.com>
2017-05-01 17:51:56 +0000

    1493661116 arecolin

---
+++ 1493661116.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2673046914
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 1L;
+static volatile uint32_t g_5 = 0xD1C58BDFL;/* VOLATILE GLOBAL g_5 */
+static volatile uint32_t g_8 = 0xD21CD430L;/* VOLATILE GLOBAL g_8 */
+static volatile uint16_t g_11 = 6UL;/* VOLATILE GLOBAL g_11 */
+static const int32_t *g_17 = &g_4;
+static const int32_t ** volatile g_16 = &g_17;/* VOLATILE GLOBAL g_16 */
+
+
+/* --- FORWARD DECLARATIONS --- */
