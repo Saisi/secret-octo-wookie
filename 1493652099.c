dc9b19d79bd373586a44257665f920b9daa167fc
saisi <saisi@users.noreply.github.com>
2017-05-01 15:21:39 +0000

    1493652099 amani

---
+++ 1493652099.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      559561168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6[7][1][7] = {{{0x85589B58L,(-3L),0x85589B58L,0x85589B58L,(-3L),0x85589B58L,0x85589B58L}},{{0x31C587C7L,0x31C587C7L,5L,0x31C587C7L,0x31C587C7L,5L,0x31C587C7L}},{{(-3L),0x85589B58L,0x85589B58L,(-3L),0x85589B58L,0x85589B58L,(-3L)}},{{0x0B168F92L,0x31C587C7L,0x0B168F92L,0x0B168F92L,0x31C587C7L,0x0B168F92L,0x0B168F92L}},{{(-3L),(-3L),0xB89BDD77L,(-3L),(-3L),0xB89BDD77L,(-3L)}},{{0x31C587C7L,0x0B168F92L,0x0B168F92L,0x31C587C7L,0x0B168F92L,0x0B168F92L,0x31C587C7L}},{{0x85589B58L,(-3L),0x85589B58L,0x85589B58L,0x85589B58L,0xB89BDD77L,0xB89BDD77L}}};
+static uint8_t g_11 = 0xFFL;
+static uint8_t g_40 = 0x3AL;
+static uint8_t *g_39[9][1][6] = {{{&g_40,&g_40,&g_40,(void*)0,(void*)0,(void*)0}},{{(void*)0,&g_40,&g_40,(void*)0,&g_40,&g_40}},{{(void*)0,&g_40,&g_40,&g_40,&g_40,(void*)0}},{{(void*)0,&g_40,(void*)0,&g_40,&g_40,(void*)0}},{{&g_40,&g_40,(void*)0,&g_40,&g_40,&g_40}},{{&g_40,&g_40,&g_40,&g_40,(void*)0,&g_40}},{{&g_40,&g_40,(void*)0,&g_40,(void*)0,&g_40}},{{&g_40,(void*)0,&g_40,&g_40,&g_40,&g_40}},{{&g_40,(void*)0,&g_40,&g_40,&g_40,&g_40}}};
+static uint8_t *g_55[1][4][2] = {{{&g_40,&g_40},{&g_40,&g_40},{&g_40,&g_40},{&g_40,&g_40}}};
