55c83cfe98552aec35a5347e0d1eff621a2effc3
saisi <saisi@users.noreply.github.com>
2017-05-01 16:21:46 +0000

    1493655705 anacoenoses

---
+++ 1493655705.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      527322296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   unsigned f1 : 23;
+   uint16_t  f2;
+};
+
+struct S1 {
+   const volatile signed f0 : 6;
+   signed f1 : 5;
+   volatile signed f2 : 18;
