673dcd205fe5c5c06437d5049a72bb69b2ad2b1b
saisi <saisi@users.noreply.github.com>
2017-05-01 05:20:47 +0000

    1493616047 Antiarchi

---
+++ 1493616047.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2130857078
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 9;
+   signed f1 : 26;
+   unsigned f2 : 20;
+   int32_t  f3;
+   unsigned f4 : 17;
+   signed f5 : 25;
+};
+
+struct S1 {
