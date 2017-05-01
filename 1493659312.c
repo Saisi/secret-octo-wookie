45990d7ccb8574e22a9c3ea8f6d81c3f8f6e9736
saisi <saisi@users.noreply.github.com>
2017-05-01 17:21:53 +0000

    1493659312 affere

---
+++ 1493659312.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3389487213
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 21;
+   volatile signed f1 : 24;
+   signed f2 : 27;
+   int32_t  f3;
+   signed f4 : 19;
+   unsigned f5 : 21;
+};
+
+#pragma pack(push)
