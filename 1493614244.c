03f49ccee6e52f3726f849e269ac93bf1cb28ee4
saisi <saisi@users.noreply.github.com>
2017-05-01 04:50:45 +0000

    1493614244 aposelene

---
+++ 1493614244.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3685008950
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint16_t  f0;
+   volatile uint16_t  f1;
+   const int64_t  f2;
+   uint16_t  f3;
+   volatile uint32_t  f4;
+   const uint16_t  f5;
+   const volatile int8_t  f6;
+   unsigned f7 : 15;
+   const uint64_t  f8;
