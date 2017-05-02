16db9e931a82fbb7e7d0a21a70fb275715e8dd80
saisi <saisi@users.noreply.github.com>
2017-05-02 03:22:49 +0000

    1493695368 Agata

---
+++ 1493695368.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3420428391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_14 = 0xF210L;
+static int32_t g_16[5] = {(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t *g_31 = (void*)0;
+static int32_t ** volatile g_30 = &g_31;/* VOLATILE GLOBAL g_30 */
+static uint16_t g_46 = 65535UL;
+static volatile uint16_t g_57 = 0UL;/* VOLATILE GLOBAL g_57 */
+static volatile uint16_t *g_56 = &g_57;
+static uint16_t g_92 = 0x7D67L;
+static int64_t g_105 = 0x84F583EF4C8A73D9LL;
