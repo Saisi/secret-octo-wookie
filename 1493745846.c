75501605b161b8f0859728384122aab093bed0c0
saisi <saisi@users.noreply.github.com>
2017-05-02 17:24:06 +0000

    1493745846 amorosity

---
+++ 1493745846.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      109230758
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[2] = {1L,1L};
+static int32_t *g_5 = (void*)0;
+static int32_t ** volatile g_4[4] = {&g_5,&g_5,&g_5,&g_5};
+static int32_t ** volatile g_6 = &g_5;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
