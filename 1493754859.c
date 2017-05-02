024727052c2f4ebdb8c59fe5b1cfe07f408cb791
saisi <saisi@users.noreply.github.com>
2017-05-02 19:54:19 +0000

    1493754859 Agawam

---
+++ 1493754859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3458854971
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x313DD33AL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3[1] = {0xBDBA76AAL};
+static int32_t g_4 = 0xAFE01BD9L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
