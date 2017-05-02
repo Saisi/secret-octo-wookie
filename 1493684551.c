89f75780600dbbaf5d040de0779ea975a2650101
saisi <saisi@users.noreply.github.com>
2017-05-02 00:22:32 +0000

    1493684551 antipastos

---
+++ 1493684551.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1330973597
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 5L;
+static int64_t g_15[3] = {0x96673E7ED6F1860ELL,0x96673E7ED6F1860ELL,0x96673E7ED6F1860ELL};
+static volatile int16_t g_16 = 2L;/* VOLATILE GLOBAL g_16 */
+static volatile uint16_t g_17 = 0UL;/* VOLATILE GLOBAL g_17 */
+static uint16_t g_44 = 65533UL;
+static int32_t g_50 = 0xCBFB41CEL;
+static int32_t g_54[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t * volatile g_53 = &g_54[5];/* VOLATILE GLOBAL g_53 */
+static int64_t g_56 = 0L;
