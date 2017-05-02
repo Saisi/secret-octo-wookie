18c3e318aad15cf7baf2f68cbf5f5f38e4a06278
saisi <saisi@users.noreply.github.com>
2017-05-02 08:23:20 +0000

    1493713400 anhemolytic

---
+++ 1493713400.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3497168959
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static volatile int8_t g_6 = 0xFFL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0xEF889A27L;
+static uint8_t g_8 = 0x7AL;
+static uint32_t g_16 = 1UL;
+static volatile uint8_t g_19 = 7UL;/* VOLATILE GLOBAL g_19 */
+
+
+/* --- FORWARD DECLARATIONS --- */
