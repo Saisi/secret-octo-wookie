8c7da4528343ed061d9f384a42f53af31dda3cc6
saisi <saisi@users.noreply.github.com>
2017-05-02 18:54:14 +0000

    1493751253 Akili

---
+++ 1493751253.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3139627589
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t * const  f0;
+   const volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x0B4FFB87L;
+static int32_t g_5 = 0xA3399244L;
+static int8_t g_17 = (-1L);
+static int8_t *g_16 = &g_17;
