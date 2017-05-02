b708b36f84788b2aa2c7cd0e1dd26641de5588d8
saisi <saisi@users.noreply.github.com>
2017-05-02 05:23:04 +0000

    1493702584 arghoul

---
+++ 1493702584.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2376791271
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int64_t g_2 = 0x02071B18D4081C29LL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4[7] = {0x37D8E6E8L,0x37D8E6E8L,0x37D8E6E8L,0x37D8E6E8L,0x37D8E6E8L,0x37D8E6E8L,0x37D8E6E8L};
+static volatile int32_t * volatile g_3 = &g_4[3];/* VOLATILE GLOBAL g_3 */
+static int32_t g_10[10] = {1L,1L,1L,1L,1L,1L,1L,1L,1L,1L};
+static int8_t g_15 = 0xFEL;
+static uint32_t g_56 = 0xCDD51B90L;
+static int32_t g_60 = (-3L);
+static const int32_t *g_69 = (void*)0;
+static const int32_t * volatile * const g_68[6][8] = {{(void*)0,&g_69,&g_69,(void*)0,&g_69,&g_69,(void*)0,&g_69},{(void*)0,&g_69,&g_69,&g_69,(void*)0,&g_69,&g_69,&g_69},{&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69},{&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69},{&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69},{&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69,&g_69}};
