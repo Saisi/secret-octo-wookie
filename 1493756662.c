b97b1f68c4ada801bfe4503153c88d5586eb75ff
saisi <saisi@users.noreply.github.com>
2017-05-02 20:24:22 +0000

    1493756662 arendator

---
+++ 1493756662.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      47891718
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 17;
+   volatile signed f1 : 2;
+   const volatile unsigned f2 : 28;
+   const unsigned f3 : 24;
+   unsigned f4 : 7;
+   int16_t  f5;
+};
+
+union U1 {
