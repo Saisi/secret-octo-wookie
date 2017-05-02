b15e15215769f9416523e514967b5a051e0d5a2d
saisi <saisi@users.noreply.github.com>
2017-05-02 23:24:38 +0000

    1493767478 baculus

---
+++ 1493767478.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      897710842
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xB49D585EL;
+static volatile int32_t g_4 = 0xA519D814L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 3L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x4145C174L;
+static int32_t g_8 = 0x652065C8L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
