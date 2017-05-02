b45b32e7572d8aed0e8609aba18750b6a4ff5c6d
saisi <saisi@users.noreply.github.com>
2017-05-02 22:24:34 +0000

    1493763873 Aequiculi

---
+++ 1493763873.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2088955803
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 12;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
+union U2 {
+   const volatile int8_t * const  volatile  f0;
