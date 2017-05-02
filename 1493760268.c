b65aa1871599022917396ea94a882f9ff12a185e
saisi <saisi@users.noreply.github.com>
2017-05-02 21:24:28 +0000

    1493760268 backhoes

---
+++ 1493760268.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      575956243
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_15 = 0L;
+static uint16_t g_16 = 0x3A4DL;
+static int32_t g_32[3][10] = {{0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L},{0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L},{0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L,0xCFD8F271L}};
+static int64_t g_53 = 0x12D56B383239552BLL;
+static volatile uint8_t g_55 = 0xDBL;/* VOLATILE GLOBAL g_55 */
+static volatile int16_t g_62 = 0x3500L;/* VOLATILE GLOBAL g_62 */
+static volatile int16_t * volatile g_61[6] = {&g_62,&g_62,&g_62,&g_62,&g_62,&g_62};
+static int16_t g_64 = 0x3647L;
+static int16_t *g_63 = &g_64;
