ec2499edf79c5fb7beadb52c4b2240cb4de341a2
saisi <saisi@users.noreply.github.com>
2017-05-02 19:24:17 +0000

    1493753056 archpriesthood

---
+++ 1493753056.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      982725440
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   volatile int8_t  f1;
+   const volatile uint32_t  f2;
+   volatile uint32_t  f3;
+   const uint16_t  f4;
+   uint32_t  f5;
+   int8_t  f6;
+   unsigned f7 : 4;
+   uint16_t  f8;
