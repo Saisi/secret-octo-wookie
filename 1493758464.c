013e7afc7ce13af676f9858ad53649418e72278b
saisi <saisi@users.noreply.github.com>
2017-05-02 20:54:26 +0000

    1493758464 ambulancer

---
+++ 1493758464.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1299515524
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 16;
+   unsigned f1 : 5;
+   signed f2 : 19;
+   uint8_t  f3;
+   uint32_t  f4;
+   const unsigned f5 : 21;
+   unsigned f6 : 6;
