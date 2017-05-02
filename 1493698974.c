2e07e498fc2add19835f6c998936f9530fee7e85
saisi <saisi@users.noreply.github.com>
2017-05-02 04:22:54 +0000

    1493698974 ballata

---
+++ 1493698974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2091863593
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[4] = {0L,0L,0L,0L};
+static volatile int32_t g_3 = 0x36951AD3L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5[7][2] = {{0xF5CD53BCL,0xF5CD53BCL},{4L,0xF5CD53BCL},{0xF5CD53BCL,4L},{0xF5CD53BCL,0xF5CD53BCL},{4L,0xF5CD53BCL},{0xF5CD53BCL,4L},{0xF5CD53BCL,0xF5CD53BCL}};
+static volatile int32_t g_6[5] = {0L,0L,0L,0L,0L};
