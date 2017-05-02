cd6e32223317a657891c73f61f0810d2e6e66763
saisi <saisi@users.noreply.github.com>
2017-05-02 10:23:31 +0000

    1493720610 azotine

---
+++ 1493720610.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2307803662
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xDE0BCD8CL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[8] = {0x16210B99L,0xDBE8E56FL,0x16210B99L,0x16210B99L,0xDBE8E56FL,0x16210B99L,0x16210B99L,0xDBE8E56FL};
+static int32_t g_5 = 0x23ED15DEL;
+static int32_t *g_57[1] = {(void*)0};
+static int32_t g_65[2] = {1L,1L};
+static uint64_t g_76 = 0UL;
+static uint32_t g_101 = 0x87E9A703L;
+static uint64_t g_129 = 18446744073709551610UL;
