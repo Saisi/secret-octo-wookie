49021619abc749f69967185b4a4db9de6dc7b273
saisi <saisi@users.noreply.github.com>
2017-05-02 11:23:36 +0000

    1493724215 alabastrums

---
+++ 1493724215.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1174041443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 3L;
+static volatile int32_t g_5[6][2] = {{1L,0x20FE6E32L},{0x20FE6E32L,1L},{0x20FE6E32L,0x20FE6E32L},{1L,0x20FE6E32L},{0x20FE6E32L,1L},{0x20FE6E32L,0x20FE6E32L}};
+static volatile int32_t g_6 = 5L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0xF147909BL;/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = (-1L);
+static uint16_t g_77 = 0UL;
+static int32_t g_93[9] = {0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL,0x4478C2BAL};
+static int64_t g_133 = (-7L);
+static uint16_t g_162 = 0xE04EL;
