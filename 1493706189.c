2cc3736b15eca71f3aa7c63ae6d54fe62084e096
saisi <saisi@users.noreply.github.com>
2017-05-02 06:23:10 +0000

    1493706189 bagpod

---
+++ 1493706189.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1661301643
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 4;
+   const unsigned f1 : 11;
+   signed f2 : 2;
+   signed f3 : 5;
+   const volatile int64_t  f4;
+};
+#pragma pack(pop)
