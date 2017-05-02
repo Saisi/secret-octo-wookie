c3d656c32d07e695b3006ccc8e47d107c6a89435
saisi <saisi@users.noreply.github.com>
2017-05-02 14:23:50 +0000

    1493735030 antievolutional

---
+++ 1493735030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1503757486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   const unsigned f1 : 27;
+};
+
+union U1 {
+   int16_t  f0;
+};
+
+union U2 {
