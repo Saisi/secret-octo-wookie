4b04a69c2644f9dc6162b4087e8620604cd7a731
saisi <saisi@users.noreply.github.com>
2017-05-02 07:53:18 +0000

    1493711598 areological

---
+++ 1493711598.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2383684332
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 4;
+   const volatile unsigned f1 : 9;
+   unsigned f2 : 7;
+   unsigned f3 : 23;
+   const unsigned f4 : 10;
+   volatile unsigned f5 : 25;
+   signed f6 : 19;
