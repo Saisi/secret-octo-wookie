008d424f408a980aed489dc756dc673d71dd4339
saisi <saisi@users.noreply.github.com>
2017-05-02 12:53:43 +0000

    1493729623 ammono

---
+++ 1493729623.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4210369583
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_27 = 0x24L;
+static uint64_t g_38 = 18446744073709551612UL;
+static uint8_t g_50 = 0UL;
+static int32_t g_53 = (-7L);
