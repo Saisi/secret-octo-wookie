fedaec91141a5e69ce36cda451f34baf38ceb8a2
saisi <saisi@users.noreply.github.com>
2017-05-02 09:23:25 +0000

    1493717005 autoserum

---
+++ 1493717005.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      79210127
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[5] = {0x9D786592L,0x9D786592L,0x9D786592L,0x9D786592L,0x9D786592L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
