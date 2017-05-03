ca06e7220f125b58208e7769e35de2ada6baae83
saisi <saisi@users.noreply.github.com>
2017-05-03 14:26:16 +0000

    1493821575 azeotropy

---
+++ 1493821575.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      212714076
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint8_t  f0;
+   volatile int8_t  f1;
+   uint16_t  f2;
+   volatile int32_t  f3;
+   const uint16_t  f4;
+   uint16_t  f5;
+   uint64_t  f6;
+   volatile uint8_t  f7;
+   const int16_t  f8;
