a939f3caa1f96549da504d78de390330dd0af292
saisi <saisi@users.noreply.github.com>
2017-05-03 03:55:07 +0000

    1493783706 aladdin

---
+++ 1493783706.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1438162946
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 15;
+   int64_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_9 = 0L;/* VOLATILE GLOBAL g_9 */
+static int8_t g_13 = 1L;
+static int32_t g_71 = 0x8BFA0855L;
