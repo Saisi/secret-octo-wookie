0dfce89b6677fbc9057f45d4c05dec1113113d90
saisi <saisi@users.noreply.github.com>
2017-05-03 11:25:52 +0000

    1493810752 antineuritic

---
+++ 1493810752.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1523168862
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint16_t  f0;
+   unsigned f1 : 26;
+   signed f2 : 6;
+   volatile unsigned f3 : 16;
+};
+#pragma pack(pop)
+
