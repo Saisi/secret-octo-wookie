6d9366d74784474f4b18d17a322ade25f52b26e8
saisi <saisi@users.noreply.github.com>
2017-05-03 19:26:44 +0000

    1493839603 attempering

---
+++ 1493839603.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3818165611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 20;
+   unsigned f1 : 27;
+   const unsigned f2 : 18;
+   signed f3 : 21;
+   volatile unsigned f4 : 18;
+   unsigned f5 : 3;
+   unsigned f6 : 22;
+   volatile unsigned f7 : 17;
+   const signed f8 : 16;
