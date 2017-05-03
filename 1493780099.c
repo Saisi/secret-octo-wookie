fb05053aafe0ebb28b76e76c27908b10ee77bd77
saisi <saisi@users.noreply.github.com>
2017-05-03 02:55:00 +0000

    1493780099 arginine

---
+++ 1493780099.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3273513548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   unsigned f1 : 1;
+   unsigned f2 : 17;
+   unsigned f3 : 15;
+   volatile unsigned f4 : 24;
+   signed f5 : 4;
+   uint8_t  f6;
+};
+
