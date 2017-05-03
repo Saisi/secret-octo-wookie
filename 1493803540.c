172767e57305ccf07fdd5002582a08996459db77
saisi <saisi@users.noreply.github.com>
2017-05-03 09:25:40 +0000

    1493803540 acquisitive

---
+++ 1493803540.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      847939693
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const int16_t  f0;
+   const int32_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_5 = 1UL;
