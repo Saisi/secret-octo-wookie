52bf250389230b3eaeacea52260cd79d87075fbb
saisi <saisi@users.noreply.github.com>
2017-05-03 06:55:26 +0000

    1493794524 antikings

---
+++ 1493794524.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      964354805
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint8_t  f1;
+   volatile int64_t  f2;
+   int8_t * f3;
+   const int64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = (-8L);
