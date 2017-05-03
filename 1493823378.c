ddcc1d7249edaa3494316698dba44537a8ebeaf1
saisi <saisi@users.noreply.github.com>
2017-05-03 14:56:18 +0000

    1493823378 adroitness

---
+++ 1493823378.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2586516895
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_3 = 9UL;
+static int16_t g_12[4][2][5] = {{{0x23DCL,0L,0x23DCL,0x23DCL,0L},{0L,0x23DCL,0x23DCL,0L,0x23DCL}},{{0L,0L,0x1D11L,0L,0L},{0x23DCL,0L,0x23DCL,0x23DCL,0L}},{{0L,0x23DCL,0x23DCL,0L,0x23DCL},{0L,0L,0x1D11L,0L,0L}},{{0x23DCL,0L,0x23DCL,0x23DCL,0L},{0L,0x23DCL,0x23DCL,0L,0x23DCL}}};
+static int32_t g_14[3] = {0xF42C60EDL,0xF42C60EDL,0xF42C60EDL};
+static uint16_t g_46 = 0xA9ACL;
+static uint16_t g_70 = 0x3274L;
+static volatile uint16_t g_83 = 65526UL;/* VOLATILE GLOBAL g_83 */
+static volatile uint16_t *g_82 = &g_83;
+static volatile uint16_t * volatile *g_81 = &g_82;
+static int32_t g_86 = 3L;
