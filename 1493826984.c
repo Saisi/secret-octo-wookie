c4e918d67885832c1a7ee771bf6d6a2e3a99eabd
saisi <saisi@users.noreply.github.com>
2017-05-03 15:56:24 +0000

    1493826984 Barca

---
+++ 1493826984.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2289759824
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-6L);
+static volatile int64_t g_7 = 0L;/* VOLATILE GLOBAL g_7 */
+static volatile uint32_t g_9[5][1][3] = {{{0x65AB4D25L,1UL,0x65AB4D25L}},{{0xA7AB5BF7L,4294967295UL,0xA7AB5BF7L}},{{0x65AB4D25L,1UL,0x65AB4D25L}},{{0xA7AB5BF7L,4294967295UL,0xA7AB5BF7L}},{{0x65AB4D25L,1UL,0x65AB4D25L}}};
+static volatile int16_t g_12 = 0xB0CEL;/* VOLATILE GLOBAL g_12 */
+static uint8_t g_13 = 5UL;
+static uint8_t g_16 = 0x77L;
+
+
+/* --- FORWARD DECLARATIONS --- */
