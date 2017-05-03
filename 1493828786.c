905983ad5338f0ad4084ed291677b953579ca5a0
saisi <saisi@users.noreply.github.com>
2017-05-03 16:26:27 +0000

    1493828786 Agromyzidae

---
+++ 1493828786.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2869167249
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 7;
+   volatile unsigned f1 : 2;
+   unsigned f2 : 29;
+   const volatile unsigned f3 : 20;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
