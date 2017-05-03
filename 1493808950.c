c758d4fc534427ece175698eaad32a413eb83fc4
saisi <saisi@users.noreply.github.com>
2017-05-03 10:55:50 +0000

    1493808950 aporias

---
+++ 1493808950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1102265066
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint16_t  f1;
+   uint32_t  f2;
+   volatile int8_t * f3;
+   volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_2[8][9] = {{{0xBB4EBB0CL},{0x622A149CL},{0xBB4EBB0CL},{0x3C0BF90AL},{0x622A149CL},{1UL},{1UL},{0x622A149CL},{0x3C0BF90AL}},{{0xBB4EBB0CL},{0x622A149CL},{0xBB4EBB0CL},{0x3C0BF90AL},{0x622A149CL},{1UL},{1UL},{0x622A149CL},{0x3C0BF90AL}},{{0xBB4EBB0CL},{0x622A149CL},{0xBB4EBB0CL},{0x3C0BF90AL},{0x622A149CL},{1UL},{1UL},{0x622A149CL},{0x3C0BF90AL}},{{0xBB4EBB0CL},{0x622A149CL},{0xBB4EBB0CL},{0x3C0BF90AL},{0x622A149CL},{1UL},{1UL},{0x622A149CL},{0x3C0BF90AL}},{{0xBB4EBB0CL},{0x622A149CL},{0xBB4EBB0CL},{0x3C0BF90AL},{0x622A149CL},{1UL},{1UL},{0x622A149CL},{0x3C0BF90AL}},{{0xBB4EBB0CL},{1UL},{1UL},{0xC79E672CL},{1UL},{3UL},{3UL},{1UL},{0xC79E672CL}},{{1UL},{1UL},{1UL},{0xC79E672CL},{1UL},{3UL},{3UL},{1UL},{0xC79E672CL}},{{1UL},{1UL},{1UL},{0xC79E672CL},{1UL},{3UL},{3UL},{1UL},{0xC79E672CL}}};
