0794ce25100d07ec8a7075db09bc97007e15e589
saisi <saisi@users.noreply.github.com>
2017-05-03 11:55:59 +0000

    1493812555 Arimathaean

---
+++ 1493812555.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2358280255
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   int64_t  f1;
+   uint16_t  f2;
+   uint16_t  f3;
+   volatile int32_t  f4;
+   uint8_t  f5;
+   const volatile int32_t  f6;
+   uint8_t  f7;
+   int32_t  f8;
