ea14754c5fd04bf09eb304971b6993af75fccfd8
saisi <saisi@users.noreply.github.com>
2017-05-03 16:56:30 +0000

    1493830589 anhalonine

---
+++ 1493830589.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4010217541
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * volatile  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_17 = 0xFA32L;
+static uint8_t g_21[3] = {0x5DL,0x5DL,0x5DL};
+static int32_t g_28 = 0x4F9DBC9BL;
+static uint16_t g_34 = 0xBEFAL;
