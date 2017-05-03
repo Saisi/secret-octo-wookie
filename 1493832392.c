cae723a20138db486c0178ce3c1c3d6dcd480471
saisi <saisi@users.noreply.github.com>
2017-05-03 17:26:33 +0000

    1493832392 applosive

---
+++ 1493832392.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2829967711
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint16_t g_2 = 1UL;
+static uint32_t g_20 = 0xFF44CACFL;
+static uint32_t g_29[9] = {0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL,0x2C60EDACL};
+static uint32_t *g_28 = &g_29[4];
