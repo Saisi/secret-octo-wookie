c81b88fb994ab4f57efaca8373ab7390e1287b36
saisi <saisi@users.noreply.github.com>
2017-05-03 07:55:32 +0000

    1493798131 apronless

---
+++ 1493798131.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2853810420
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xB7C7CE53L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[3] = {0xA8E97EEDL,0xA8E97EEDL,0xA8E97EEDL};
+static volatile int32_t g_6 = 0xC4C8D3C8L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 1L;
