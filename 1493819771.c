b0c79f81134aab325e4d43b1f5dcf934545828de
saisi <saisi@users.noreply.github.com>
2017-05-03 13:56:13 +0000

    1493819771 archaeostoma

---
+++ 1493819771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      799100598
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   unsigned f1 : 6;
+   volatile int32_t  f2;
+   int8_t  f3;
+};
+
+union U1 {
+   volatile uint16_t  f0;
+   int8_t * volatile  f1;
