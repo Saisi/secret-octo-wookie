90cd7f14ce4c475b1d278e5b2628e480c42cbea2
saisi <saisi@users.noreply.github.com>
2017-05-03 20:56:52 +0000

    1493845012 antipolitics

---
+++ 1493845012.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1642116362
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xE1A3622BL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4 = 0xDF98A59CL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+static int32_t g_5 = 9L;
+static int32_t g_10 = 4L;
+static volatile int64_t g_26 = 0x74DA8628405CEFD0LL;/* VOLATILE GLOBAL g_26 */
+static int32_t g_57 = (-1L);
+static const uint64_t g_83 = 0UL;
+static int32_t g_87 = (-7L);
