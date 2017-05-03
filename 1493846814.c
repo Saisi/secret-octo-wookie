64a4e7672eee54e2b7c40d26faee798f060fbe6b
saisi <saisi@users.noreply.github.com>
2017-05-03 21:26:55 +0000

    1493846814 amboceptor

---
+++ 1493846814.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2746856619
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_9[1] = {1L};
+static int32_t * const g_8 = &g_9[0];
+static uint16_t g_21[6][6][1] = {{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}},{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}},{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}},{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}},{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}},{{5UL},{5UL},{0xE9A0L},{5UL},{5UL},{0xE9A0L}}};
+static int32_t g_23 = 0x764CE34AL;
+static int32_t g_78[1] = {(-3L)};
+static uint8_t g_86 = 0xA6L;
+static int32_t g_91 = 0x53DE9127L;
+static uint64_t g_93[10] = {0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL,0x4C8B43F6205A1655LL};
+static uint8_t g_95 = 0x07L;
