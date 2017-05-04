b45c5eef996378192a2940eeb1a385285575118d
saisi <saisi@users.noreply.github.com>
2017-05-04 21:58:53 +0000

    1493935132 bandoleer

---
+++ 1493935132.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      796680139
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+union U1 {
+   uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_7 = 0x5010F7A7L;
