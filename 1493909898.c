d6f914e4b4d4cf90b7c489281e0025ad087f338a
saisi <saisi@users.noreply.github.com>
2017-05-04 14:58:19 +0000

    1493909898 amethysts

---
+++ 1493909898.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1085003042
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint8_t  f0;
+   const int32_t  f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x35A23E86L;
+static int32_t * volatile g_60 = (void*)0;/* VOLATILE GLOBAL g_60 */
+static int32_t g_62 = 0xE525036FL;
