cc3c32d5b5deee05f907386c8e6f4fd33066bc56
saisi <saisi@users.noreply.github.com>
2017-05-04 12:58:08 +0000

    1493902688 autocarpic

---
+++ 1493902688.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2572789022
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-4L);
+static volatile int32_t g_10[6] = {6L,6L,6L,6L,6L,6L};
+static volatile int32_t * volatile g_9 = &g_10[5];/* VOLATILE GLOBAL g_9 */
+static volatile int32_t * const  volatile *g_8[6][10] = {{(void*)0,&g_9,&g_9,&g_9,(void*)0,(void*)0,(void*)0,(void*)0,&g_9,&g_9},{(void*)0,(void*)0,&g_9,(void*)0,(void*)0,(void*)0,&g_9,(void*)0,(void*)0,&g_9},{&g_9,(void*)0,(void*)0,(void*)0,(void*)0,&g_9,&g_9,&g_9,(void*)0,(void*)0},{&g_9,(void*)0,&g_9,(void*)0,&g_9,&g_9,(void*)0,&g_9,(void*)0,&g_9},{(void*)0,&g_9,(void*)0,(void*)0,(void*)0,&g_9,(void*)0,(void*)0,&g_9,(void*)0},{&g_9,(void*)0,(void*)0,&g_9,(void*)0,(void*)0,(void*)0,&g_9,(void*)0,(void*)0}};
+static volatile int32_t * const  volatile ** volatile g_11 = &g_8[1][6];/* VOLATILE GLOBAL g_11 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
