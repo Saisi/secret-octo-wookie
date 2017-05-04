71a005bc3bb91b298863d5b80edff73b95d936a9
saisi <saisi@users.noreply.github.com>
2017-05-04 16:58:27 +0000

    1493917106 aluminoferric

---
+++ 1493917106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1545644811
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   volatile unsigned f1 : 5;
+   const unsigned f2 : 12;
+   signed f3 : 19;
+   volatile unsigned f4 : 2;
+   int32_t  f5;
+   signed f6 : 6;
+};
+
