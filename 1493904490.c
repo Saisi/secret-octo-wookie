3f14fc630daf46a0f1b1f185c1e7093002bd4553
saisi <saisi@users.noreply.github.com>
2017-05-04 13:28:11 +0000

    1493904490 arachnephobia

---
+++ 1493904490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1753595010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 15;
+   volatile unsigned f1 : 8;
+   const unsigned f2 : 10;
+   const signed f3 : 14;
+   int32_t  f4;
+   volatile signed f5 : 31;
+   volatile uint32_t  f6;
