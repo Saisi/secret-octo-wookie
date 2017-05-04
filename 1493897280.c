aef462833c7824de23d75b0d07093d200fcfc444
saisi <saisi@users.noreply.github.com>
2017-05-04 11:28:00 +0000

    1493897280 analcimes

---
+++ 1493897280.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1887928683
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 20;
+   signed f1 : 27;
+   volatile signed f2 : 18;
+   const volatile signed f3 : 21;
+   signed f4 : 18;
+   unsigned f5 : 3;
+   signed f6 : 22;
+};
+
