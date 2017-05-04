7fc52f4b9483abe76194c93f0954ffe255faed78
saisi <saisi@users.noreply.github.com>
2017-05-04 10:57:58 +0000

    1493895478 Asterolepis

---
+++ 1493895478.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2291675945
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   signed f1 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x6951AD38L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xE57BF5CDL;/* VOLATILE GLOBAL g_5 */
