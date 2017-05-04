abcd55004960daa4a2ad9362adcce9e3782923e3
saisi <saisi@users.noreply.github.com>
2017-05-04 23:28:59 +0000

    1493940539 androcephalum

---
+++ 1493940539.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      271444189
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 65531UL;
+static int8_t g_7 = 0L;
+static int32_t * const  volatile g_8 = (void*)0;/* VOLATILE GLOBAL g_8 */
+static int32_t g_10 = (-1L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
