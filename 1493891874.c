ca7d77adf767d203cefc79b0d2b87f13f149e389
saisi <saisi@users.noreply.github.com>
2017-05-04 09:57:54 +0000

    1493891874 Arthrogastra

---
+++ 1493891874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2858318430
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_13 = 6UL;
+static int32_t g_27 = 0xC1C234ACL;
+static volatile int32_t g_48 = 5L;/* VOLATILE GLOBAL g_48 */
+static volatile int32_t * volatile g_47 = &g_48;/* VOLATILE GLOBAL g_47 */
+static int32_t g_52[3] = {0x60E2EFE8L,0x60E2EFE8L,0x60E2EFE8L};
+static int32_t g_67 = 1L;
+static const int32_t g_87 = 0x1BE33BD0L;
+static int32_t g_99 = 0xAADB6CD3L;
+static uint16_t g_101 = 65533UL;
