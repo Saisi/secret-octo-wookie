78aa5d28d49344b0ca6393b4b94894b07cd893a3
saisi <saisi@users.noreply.github.com>
2017-05-05 02:29:17 +0000

    1493951355 apposiopestic

---
+++ 1493951355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1607760482
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const volatile uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_7 = 0xFBL;
+static int32_t g_18 = 9L;
+static volatile int32_t g_57[2][8] = {{0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L},{0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L,0xFFE61709L}};
+static int32_t g_58 = 0x8A3D3555L;
