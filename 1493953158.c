ab056cb8fe31426f8d6a58043196863f01bcd7de
saisi <saisi@users.noreply.github.com>
2017-05-05 02:59:19 +0000

    1493953158 anaphylactic

---
+++ 1493953158.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3682273683
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   uint64_t  f1;
+   uint8_t  f2;
+   volatile int8_t  f3;
+};
+
+union U1 {
+   int8_t * f0;
+   uint32_t  f1;
