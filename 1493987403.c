09e5d0a16cfda49b589f6df1b168d41bbdf286e0
saisi <saisi@users.noreply.github.com>
2017-05-05 12:30:04 +0000

    1493987403 Ahwaz

---
+++ 1493987403.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3220263496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0xA75CL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_5 = 0xF2F4A8B1L;
+static int32_t *g_29 = (void*)0;
+static int32_t g_41 = (-1L);
+static uint16_t g_56[8][3] = {{0UL,4UL,0UL},{1UL,0x4042L,1UL},{0UL,4UL,0UL},{1UL,0x4042L,1UL},{0UL,4UL,0UL},{1UL,0x4042L,1UL},{0UL,4UL,0UL},{1UL,0x4042L,1UL}};
+static int32_t g_57[2] = {1L,1L};
+static int32_t g_74 = 1L;
+static int16_t g_100 = 0xCB4FL;
+static int32_t *g_129[5][7] = {{&g_57[1],&g_57[0],&g_57[0],&g_57[0],&g_57[0],&g_57[1],&g_41},{&g_57[1],&g_57[0],&g_57[0],&g_57[0],&g_57[0],&g_57[1],&g_41},{&g_57[1],&g_57[0],&g_57[0],&g_57[0],&g_57[0],&g_57[1],&g_5},{&g_57[1],&g_57[0],&g_41,&g_41,&g_57[0],&g_57[1],&g_5},{&g_57[1],&g_57[0],&g_41,&g_41,&g_57[0],&g_57[1],&g_5}};
