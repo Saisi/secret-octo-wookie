8e2b69e08551944233848f7929fc0d739239240d
saisi <saisi@users.noreply.github.com>
2017-05-05 05:59:35 +0000

    1493963974 backwinding

---
+++ 1493963974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2575584142
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 21;
+   unsigned f1 : 22;
+   volatile signed : 0;
+   const unsigned f2 : 12;
+   volatile unsigned f3 : 11;
+   const unsigned f4 : 22;
+};
+
+struct S1 {
