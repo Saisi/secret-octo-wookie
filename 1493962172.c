cf8ac513f5afbb6fe1c1957ce1247be0b5d93165
saisi <saisi@users.noreply.github.com>
2017-05-05 05:29:32 +0000

    1493962172 Arceuthobium

---
+++ 1493962172.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2332130784
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_25 = 0x1BC383C6L;
+static int32_t g_85 = 0L;
+static int8_t g_87 = 0xBFL;
+static int32_t g_127 = 0xFE8BAC28L;
+static int32_t * volatile g_135 = (void*)0;/* VOLATILE GLOBAL g_135 */
+static int32_t * const  volatile g_137 = &g_85;/* VOLATILE GLOBAL g_137 */
+static int32_t *g_139 = &g_25;
+static int32_t ** volatile g_138 = &g_139;/* VOLATILE GLOBAL g_138 */
+static volatile uint8_t g_152[10] = {0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL,0UL};
