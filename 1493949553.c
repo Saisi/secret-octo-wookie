6faf27650c3acfc2f66daa03158527f8881011b9
saisi <saisi@users.noreply.github.com>
2017-05-05 01:59:13 +0000

    1493949553 ballista

---
+++ 1493949553.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1311529699
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   unsigned f1 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_7 = 18446744073709551615UL;
+static int32_t g_9[2][1] = {{0xED8F6EFBL},{0xED8F6EFBL}};
+static int32_t * volatile g_8 = &g_9[1][0];/* VOLATILE GLOBAL g_8 */
+
