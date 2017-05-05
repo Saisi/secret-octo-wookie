9520889d55b01b47507e5689558c6abc85e09429
saisi <saisi@users.noreply.github.com>
2017-05-05 04:59:30 +0000

    1493960368 adonis

---
+++ 1493960368.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3709819935
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 14;
+};
+
+struct S1 {
+   volatile signed f0 : 29;
+   volatile signed f1 : 13;
+   const volatile unsigned : 0;
+   volatile signed f2 : 14;
+   volatile unsigned f3 : 25;
