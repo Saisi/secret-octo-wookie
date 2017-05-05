a2918d14353a42a79c29d6996a54f3500721d911
saisi <saisi@users.noreply.github.com>
2017-05-05 07:29:42 +0000

    1493969381 antistreptococcal

---
+++ 1493969381.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      899757975
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+};
+
+union U1 {
+   int32_t  f0;
+   volatile int8_t  f1;
+   const volatile uint16_t  f2;
+};
+
