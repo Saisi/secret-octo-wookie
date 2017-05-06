7c0aa09151b57cce90cd0dde84287b10976c75c2
saisi <saisi@users.noreply.github.com>
2017-05-06 04:31:23 +0000

    1494045081 babels

---
+++ 1494045081.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2263418448
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 22;
+   signed f1 : 19;
+   uint8_t  f2;
+   const signed f3 : 1;
+   const unsigned f4 : 2;
+};
+
+union U1 {
+   uint8_t  f0;
