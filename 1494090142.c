fcdc22e64c8f867782548bec7b67dd3ca1e3ddcf
saisi <saisi@users.noreply.github.com>
2017-05-06 17:02:22 +0000

    1494090142 ADCCP

---
+++ 1494090142.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1833114269
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 13;
+   int16_t  f1;
+   int16_t  f2;
+   volatile uint32_t  f3;
+};
+
+union U1 {
+   signed f0 : 7;
+   unsigned f1 : 19;
