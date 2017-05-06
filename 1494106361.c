25730f3b6051dde5678cf000522eff82ada80bd2
saisi <saisi@users.noreply.github.com>
2017-05-06 21:32:41 +0000

    1494106361 alchemy

---
+++ 1494106361.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      650591054
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 21;
+   const volatile int64_t  f1;
+   const int32_t  f2;
+   const volatile signed f3 : 29;
+};
+#pragma pack(pop)
+
