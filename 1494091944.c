f384b0d2e9fd6fba88a8cbf972e6639a0b906338
saisi <saisi@users.noreply.github.com>
2017-05-06 17:32:24 +0000

    1494091944 absorbable

---
+++ 1494091944.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1275607248
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[6][9] = {{(-8L),1L,(-8L),(-8L),1L,(-8L),(-8L),1L,(-8L)},{6L,(-4L),6L,0xEAC45307L,0L,0xEAC45307L,6L,(-4L),6L},{(-8L),1L,(-8L),(-8L),1L,(-8L),(-8L),1L,(-8L)},{6L,(-4L),6L,0xEAC45307L,0L,0xEAC45307L,6L,(-4L),6L},{(-8L),1L,(-8L),(-8L),1L,(-8L),(-8L),1L,(-8L)},{6L,0xEAC45307L,9L,0L,6L,0L,9L,0xEAC45307L,9L}};
+static volatile int32_t g_4[4][2][2] = {{{0x6A205B29L,(-1L)},{0x6A205B29L,1L}},{{0x02DAFBBDL,0x02DAFBBDL},{1L,0x6A205B29L}},{{(-1L),0x6A205B29L},{1L,0x02DAFBBDL}},{{0x02DAFBBDL,1L},{0x6A205B29L,(-1L)}}};
+static int32_t g_5 = 0xBC9C3D06L;
+static int32_t * volatile g_15[10] = {&g_5,&g_5,(void*)0,(void*)0,&g_5,&g_5,&g_5,(void*)0,(void*)0,&g_5};
+static int32_t * volatile g_16[10] = {&g_3[1][8],&g_3[4][7],&g_5,&g_5,&g_3[4][7],&g_3[1][8],&g_3[4][7],&g_5,&g_5,&g_3[4][7]};
