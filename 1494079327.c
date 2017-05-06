850f5ae9d72281d427e2b7a2ed96eecd68993663
saisi <saisi@users.noreply.github.com>
2017-05-06 14:02:08 +0000

    1494079327 abscession

---
+++ 1494079327.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3959351443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint8_t  f0;
+   unsigned f1 : 13;
+   unsigned f2 : 15;
+   volatile int16_t  f3;
+   volatile unsigned f4 : 19;
+   signed f5 : 10;
+   uint16_t  f6;
