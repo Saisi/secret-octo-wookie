2fa185bbbe2a5d60097a736868a77508e4e711f8
saisi <saisi@users.noreply.github.com>
2017-05-06 06:31:33 +0000

    1494052293 Angka

---
+++ 1494052293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2987985548
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3[5] = {0x0FECAFE8L,0x0FECAFE8L,0x0FECAFE8L,0x0FECAFE8L,0x0FECAFE8L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
