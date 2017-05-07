382b7c6fea905313323caddb25db7580d6ccb684
saisi <saisi@users.noreply.github.com>
2017-05-07 05:33:15 +0000

    1494135195 bagong

---
+++ 1494135195.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2223197955
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   uint16_t  f0;
+   const uint8_t  f1;
+   const uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
