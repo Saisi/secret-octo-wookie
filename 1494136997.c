5437c364bc506643082992af6abbd9406523d947
saisi <saisi@users.noreply.github.com>
2017-05-07 06:03:17 +0000

    1494136997 barbated

---
+++ 1494136997.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1646804014
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   signed f1 : 6;
+   volatile uint8_t  f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   int8_t  f0;
+   const uint32_t  f1;
