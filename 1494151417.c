f9a0e5da1e34a28fab5d55e8d5139c26505b2470
saisi <saisi@users.noreply.github.com>
2017-05-07 10:03:37 +0000

    1494151417 Acer

---
+++ 1494151417.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1334204821
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 9;
+   const uint8_t  f1;
+   uint8_t  f2;
+   int64_t  f3;
+};
+
+union U1 {
+   const int8_t  f0;
+   const uint32_t  f1;
