b962224b83b890bce22f385c123ac262c69965f1
saisi <saisi@users.noreply.github.com>
2017-05-07 16:34:07 +0000

    1494174847 acephal

---
+++ 1494174847.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3706007590
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x57E8B72FL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x14D2EDC7L;
+static int32_t g_6 = (-1L);
+static int32_t g_11 = 0x0B5F0B10L;
+static int32_t * volatile g_10 = &g_11;/* VOLATILE GLOBAL g_10 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
