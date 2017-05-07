44a7bb38cf0ac17f5858ca1f60f00ada05b52d09
saisi <saisi@users.noreply.github.com>
2017-05-07 13:03:51 +0000

    1494162231 Acroclinium

---
+++ 1494162231.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4213778662
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_4 = 0x57E8B72FD4914D2ELL;
+static uint64_t g_19 = 5UL;
+static uint64_t *g_18[2] = {&g_19,&g_19};
+static int32_t g_46 = 6L;
+static const int32_t g_70 = (-1L);
+static int32_t g_73 = 0x58698962L;
+static int64_t g_79 = 0x40C09C4F4FCF4275LL;
+static volatile uint16_t g_80 = 0xD714L;/* VOLATILE GLOBAL g_80 */
+static int32_t g_99 = 0xA47A58E8L;
