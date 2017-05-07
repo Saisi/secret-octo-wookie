e760880f82528923116bd6f7b5af63d3b616223d
saisi <saisi@users.noreply.github.com>
2017-05-07 11:03:42 +0000

    1494155021 aerobion

---
+++ 1494155021.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1564157628
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   const int64_t  f1;
+   volatile unsigned f2 : 4;
+   uint32_t  f3;
+   int16_t  f4;
+};
+#pragma pack(pop)
