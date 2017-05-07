ad2e883a11a1a7889c6c6aacabd3c03aaba9125c
saisi <saisi@users.noreply.github.com>
2017-05-07 21:34:34 +0000

    1494192874 Agapemonist

---
+++ 1494192874.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1370099818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   int64_t  f1;
+   volatile uint16_t  f2;
+   int64_t  f3;
+};
+#pragma pack(pop)
+
