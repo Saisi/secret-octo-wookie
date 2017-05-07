96b7903c7d00ac939be2302491767cfbd8937f5d
saisi <saisi@users.noreply.github.com>
2017-05-07 03:33:07 +0000

    1494127986 anteconsonantal

---
+++ 1494127986.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2086008608
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 30;
+   const volatile signed f1 : 27;
+   unsigned f2 : 17;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
