380a6e39eb22313b13c3e26439c80a7a0bf90875
saisi <saisi@users.noreply.github.com>
2017-05-07 02:02:59 +0000

    1494122579 Acanthodii

---
+++ 1494122579.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      105880850
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U3 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-7L);
+static int32_t * volatile g_3 = &g_4;/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
