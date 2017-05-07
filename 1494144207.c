2c4de3ec7c88151e677872f2f3752dcc77a84ec6
saisi <saisi@users.noreply.github.com>
2017-05-07 08:03:28 +0000

    1494144207 antiparallel

---
+++ 1494144207.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1158360967
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   volatile uint16_t  f1;
+   int32_t  f2;
+   int64_t  f3;
+   volatile int16_t  f4;
+   int32_t  f5;
+   const volatile unsigned f6 : 2;
+   const uint64_t  f7;
+   const uint32_t  f8;
