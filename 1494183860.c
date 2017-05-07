ddfa1d8c61a00d56fb81fab45e52a6f3af982e12
saisi <saisi@users.noreply.github.com>
2017-05-07 19:04:20 +0000

    1494183860 ancress

---
+++ 1494183860.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      91575019
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile unsigned f0 : 20;
+};
+#pragma pack(pop)
+
+struct S1 {
+   uint8_t  f0;
+   const uint64_t  f1;
