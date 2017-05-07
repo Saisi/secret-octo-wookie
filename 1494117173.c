4c0ec94b56e291032d74bcadb10d07029013c90b
saisi <saisi@users.noreply.github.com>
2017-05-07 00:32:53 +0000

    1494117173 alembicated

---
+++ 1494117173.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4151490939
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 1L;
+static uint64_t g_8 = 0xBD035C5735E4B783LL;
+static uint16_t g_42 = 0xA151L;
+static uint8_t g_46 = 0x00L;
+static int32_t g_66 = 0x5A0A0DE8L;
+static int32_t *g_83 = (void*)0;
+static int32_t ** volatile g_82[9][8][3] = {{{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,(void*)0,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83}},{{(void*)0,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,(void*)0,&g_83},{&g_83,&g_83,&g_83}},{{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{(void*)0,(void*)0,&g_83},{&g_83,&g_83,(void*)0}},{{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,(void*)0,(void*)0}},{{&g_83,&g_83,(void*)0},{(void*)0,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{(void*)0,&g_83,(void*)0},{(void*)0,(void*)0,&g_83}},{{&g_83,&g_83,(void*)0},{&g_83,&g_83,&g_83},{&g_83,(void*)0,&g_83},{&g_83,(void*)0,&g_83},{&g_83,&g_83,&g_83},{(void*)0,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,(void*)0}},{{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,&g_83,(void*)0},{(void*)0,(void*)0,&g_83},{&g_83,(void*)0,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83}},{{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,(void*)0},{&g_83,(void*)0,&g_83},{&g_83,(void*)0,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,(void*)0}},{{&g_83,(void*)0,&g_83},{&g_83,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83},{(void*)0,&g_83,&g_83},{&g_83,&g_83,&g_83},{&g_83,&g_83,&g_83}}};
+static int16_t g_114 = 0x0418L;
+static int8_t g_119 = 0x7DL;
