635f6a3423f0163531b54bfc78e9ea20b7707cb5
saisi <saisi@users.noreply.github.com>
2017-05-08 08:35:29 +0000

    1494232529 anthraxylon

---
+++ 1494232529.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      534635580
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = (-1L);
+static int8_t g_16 = (-1L);
+static int64_t g_18 = 0x9E71B35D56974EDFLL;
+static int64_t *g_42 = (void*)0;
+static int64_t *g_50 = &g_18;
+static int64_t * volatile * volatile g_49 = &g_50;/* VOLATILE GLOBAL g_49 */
+static uint8_t g_54 = 0x4DL;
+static int64_t **g_57[4] = {&g_50,&g_50,&g_50,&g_50};
+static int64_t *** volatile g_56[1][4][2] = {{{&g_57[3],&g_57[3]},{(void*)0,&g_57[3]},{&g_57[3],(void*)0},{&g_57[3],&g_57[3]}}};
