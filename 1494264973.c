7812bde06c1aa3356293f32ba5bcdd2dccf1594c
saisi <saisi@users.noreply.github.com>
2017-05-08 17:36:13 +0000

    1494264973 athrocytosis

---
+++ 1494264973.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3033869079
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 0x094A1888L;
+static int32_t g_19 = (-1L);
+static volatile int32_t g_22 = 0x637C5B15L;/* VOLATILE GLOBAL g_22 */
+static int32_t g_23 = 0xFBD13D8CL;
+static int32_t g_29 = 4L;
+static int32_t * const  volatile g_30 = (void*)0;/* VOLATILE GLOBAL g_30 */
+static int32_t g_32 = 0x686A37BDL;
+static int16_t g_38 = (-1L);
+static uint8_t g_45 = 255UL;
