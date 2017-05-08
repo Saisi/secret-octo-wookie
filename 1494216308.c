68bf52cdc7f1f939ca83e50af7386055e4dc720d
saisi <saisi@users.noreply.github.com>
2017-05-08 04:05:09 +0000

    1494216308 ARCO

---
+++ 1494216308.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      655359001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint64_t  f0;
+   unsigned : 0;
+   volatile signed f1 : 22;
+   volatile signed f2 : 2;
+   volatile signed f3 : 1;
+   const signed f4 : 11;
+   signed f5 : 2;
+   unsigned f6 : 20;
+};
