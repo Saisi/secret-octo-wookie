9d64414bbbae4780949fba3c1eabd86594ba5d39
saisi <saisi@users.noreply.github.com>
2017-05-08 16:36:09 +0000

    1494261369 anthroposcopy

---
+++ 1494261369.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2731974337
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_17 = 0x327439CBL;/* VOLATILE GLOBAL g_17 */
+static uint16_t g_26 = 65534UL;
+static uint16_t g_28 = 0x0573L;
+static const uint16_t g_51 = 65534UL;
+static uint16_t g_71 = 0xA861L;
+static int16_t g_75 = (-10L);
+static int32_t g_79 = 0L;
+static uint8_t g_93 = 0x6EL;
+static int32_t g_96 = 0x089A473FL;
