19b365d83faba5a350758f6c20dbc458ce26d3f2
saisi <saisi@users.noreply.github.com>
2017-05-08 17:06:11 +0000

    1494263171 archabomination

---
+++ 1494263171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2147482952
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int8_t  f1;
+   const int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_11 = 255UL;
+static int32_t g_12[3][2] = {{(-1L),(-1L)},{(-1L),(-1L)},{(-1L),(-1L)}};
+static int32_t g_43 = 0x9C214F16L;
