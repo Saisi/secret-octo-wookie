f39126a1ee56d8aac8d67ff9d6599e825a036d12
saisi <saisi@users.noreply.github.com>
2017-05-08 20:06:25 +0000

    1494273984 ASPEN

---
+++ 1494273984.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      364559728
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   uint32_t  f1;
+   volatile signed f2 : 25;
+   volatile int64_t  f3;
+   uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
