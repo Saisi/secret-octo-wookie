e85db4581b6b964ca8ac2e2ed3bb057460e8e123
saisi <saisi@users.noreply.github.com>
2017-05-08 05:05:13 +0000

    1494219913 Annona

---
+++ 1494219913.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      696197832
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint64_t  f0;
+   volatile uint64_t  f1;
+   uint64_t  f2;
+   uint16_t  f3;
+   const signed f4 : 1;
+};
+#pragma pack(pop)
