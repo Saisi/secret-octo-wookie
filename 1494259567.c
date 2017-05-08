98688ce34dacf7eae78d0dbe620e0f68e74b257d
saisi <saisi@users.noreply.github.com>
2017-05-08 16:06:07 +0000

    1494259567 Bacchanalia

---
+++ 1494259567.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2451607038
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = 0xB6069300L;
+static volatile uint32_t g_13 = 0x61C71BC7L;/* VOLATILE GLOBAL g_13 */
+static int64_t g_18 = 0L;
+static int8_t g_20 = 0x15L;
+static int32_t g_34 = 0x2A04F777L;
+static uint8_t g_47 = 0x1CL;
+static int32_t g_125 = 0x6B0C5DECL;
+static uint64_t g_140 = 0x8D75FB7473AC07ACLL;
+static uint8_t g_148 = 251UL;
