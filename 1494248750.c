7a67759fd94c5b6db19da2965c86abaef571feea
saisi <saisi@users.noreply.github.com>
2017-05-08 13:05:52 +0000

    1494248750 artophophoria

---
+++ 1494248750.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1758064547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int16_t  f0;
+   uint64_t  f1;
+   const uint32_t  f2;
+   uint8_t  f3;
+   volatile int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2[10] = {0L,0L,0L,0L,0L,0L,0L,0L,0L,0L};
