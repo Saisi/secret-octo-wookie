c50cb1369e6199c774c09c63b6e67e3fe20e9ec0
saisi <saisi@users.noreply.github.com>
2017-05-08 20:36:27 +0000

    1494275786 antimilitary

---
+++ 1494275786.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4006365168
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   const volatile uint32_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-1L);
+static int8_t g_17 = 0x40L;
+static int32_t g_21 = 0L;
