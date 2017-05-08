9ef38b25ff2e1a636e4a20038d325e773e0466fa
saisi <saisi@users.noreply.github.com>
2017-05-08 14:35:59 +0000

    1494254158 aphoria

---
+++ 1494254158.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      763783675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   signed f1 : 3;
+   int16_t  f2;
+   const signed f3 : 11;
+   unsigned f4 : 6;
+   const unsigned f5 : 28;
+   signed f6 : 15;
+};
+
