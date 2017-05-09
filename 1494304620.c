20f812f91f14c8b49244880c940d432868b26093
saisi <saisi@users.noreply.github.com>
2017-05-09 04:37:01 +0000

    1494304620 alniresinol

---
+++ 1494304620.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4157050489
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   unsigned f1 : 6;
+   unsigned f2 : 17;
+   const signed f3 : 24;
+};
+
+union U1 {
+   volatile uint32_t  f0;
+   volatile signed f1 : 18;
