f6c76b2c16d805dabe3555ab65766af643dbfc02
saisi <saisi@users.noreply.github.com>
2017-05-09 14:37:52 +0000

    1494340672 AOPA

---
+++ 1494340672.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3432109071
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   unsigned f0 : 17;
+   const uint64_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+
+
+/* --- FORWARD DECLARATIONS --- */
