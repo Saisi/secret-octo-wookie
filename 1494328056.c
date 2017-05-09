b4748bc7518538cf533a87d9c82b99175b41d255
saisi <saisi@users.noreply.github.com>
2017-05-09 11:07:37 +0000

    1494328056 backstopped

---
+++ 1494328056.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4082359056
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+   signed f1 : 19;
+   volatile unsigned f2 : 9;
+   const unsigned f3 : 17;
+   const signed f4 : 28;
+};
+
+union U1 {
+   const int8_t  f0;
