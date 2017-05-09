63d274cedfacfd4b870145fad1bc12081d7ed5f3
saisi <saisi@users.noreply.github.com>
2017-05-09 05:37:07 +0000

    1494308227 aloed

---
+++ 1494308227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1640723809
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 15;
+   unsigned f1 : 10;
+   const signed f2 : 5;
+   const unsigned f3 : 28;
+   signed f4 : 29;
+   unsigned f5 : 2;
+   const volatile signed f6 : 25;
+   signed f7 : 8;
+};
