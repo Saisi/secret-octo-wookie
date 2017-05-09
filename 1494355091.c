020ccbaf0268f4bbf92dea6950876cae0bb8a5db
saisi <saisi@users.noreply.github.com>
2017-05-09 18:38:12 +0000

    1494355091 amphiblastula

---
+++ 1494355091.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4216443895
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 26;
+   signed f1 : 9;
+   volatile signed f2 : 8;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
