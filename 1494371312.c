c6be04c1ac5dc4584ed30218a634f93f67da995d
saisi <saisi@users.noreply.github.com>
2017-05-09 23:08:33 +0000

    1494371312 Archegoniatae

---
+++ 1494371312.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1178505848
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static volatile int64_t g_6 = 0x4AD38DC5336DE018LL;/* VOLATILE GLOBAL g_6 */
+static volatile uint64_t g_7 = 9UL;/* VOLATILE GLOBAL g_7 */
+static int64_t g_14 = (-8L);
+static int32_t g_53 = 1L;
+static int64_t g_56[5] = {0xCA8B1460DEEA39BALL,0xCA8B1460DEEA39BALL,0xCA8B1460DEEA39BALL,0xCA8B1460DEEA39BALL,0xCA8B1460DEEA39BALL};
+static uint32_t g_57 = 18446744073709551614UL;
+static uint64_t g_76 = 0x34CEB0392260F147LL;
+static int64_t * volatile g_82[8] = {&g_56[0],&g_56[0],&g_56[0],&g_56[0],&g_56[0],&g_56[0],&g_56[0],&g_56[0]};
