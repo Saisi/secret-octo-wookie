4dc5533ea377bf7de992fe34b868997e316ed2ce
saisi <saisi@users.noreply.github.com>
2017-05-09 16:38:01 +0000

    1494347881 allophite

---
+++ 1494347881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3786026473
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xBC6E5C7AL;
+static volatile uint64_t g_4 = 0x9B7D6B59229A275FLL;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
