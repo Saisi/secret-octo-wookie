ed7b9a7abcbd30185a358e17d15314d3d1cee5d8
saisi <saisi@users.noreply.github.com>
2017-05-09 11:37:39 +0000

    1494329859 Aguste

---
+++ 1494329859.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2621417112
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 1L;
+static int32_t * volatile g_7[8] = {&g_8,&g_8,&g_8,&g_8,&g_8,&g_8,&g_8,&g_8};
+static int32_t * volatile g_9 = &g_8;/* VOLATILE GLOBAL g_9 */
+static int16_t g_22[5][6] = {{(-10L),(-2L),(-10L),0xBD26L,0x70ADL,0x70ADL},{1L,(-10L),(-10L),1L,(-2L),0x2E83L},{0x2E83L,1L,0x70ADL,1L,0x2E83L,0xBD26L},{1L,0x2E83L,0xBD26L,0xBD26L,0x2E83L,1L},{(-10L),1L,(-2L),0x2E83L,(-2L),1L}};
+static int32_t g_36 = 0xEC2A3D91L;
+static int16_t g_55 = 0xA71DL;
+static int16_t *g_54 = &g_55;
+static uint8_t g_58 = 255UL;
+static int16_t g_60 = 0L;
