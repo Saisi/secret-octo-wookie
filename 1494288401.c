941672057a20f4866370f2617cdc48ee98b2f2c0
saisi <saisi@users.noreply.github.com>
2017-05-09 00:06:41 +0000

    1494288401 abilla

---
+++ 1494288401.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4016936593
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_11[5] = {0xFCB143E9L,0xFCB143E9L,0xFCB143E9L,0xFCB143E9L,0xFCB143E9L};
+static volatile uint32_t g_22 = 4294967287UL;/* VOLATILE GLOBAL g_22 */
+static int32_t g_41 = 0xD000C4EEL;
+static int64_t g_72 = 0xA017C95AB7803ABDLL;
+static int64_t *g_80[1] = {(void*)0};
+static uint8_t g_96 = 0x17L;
+static int16_t g_100 = (-4L);
+static uint8_t g_107 = 0x5CL;
+static uint8_t *g_106[2][4] = {{&g_107,&g_107,&g_107,&g_107},{&g_107,&g_107,&g_107,&g_107}};
