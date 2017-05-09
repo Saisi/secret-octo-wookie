b57bcc329a0fc5be5aae1f5360d0f49820689cfc
saisi <saisi@users.noreply.github.com>
2017-05-09 10:07:32 +0000

    1494324452 ballastage

---
+++ 1494324452.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3954398812
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   int32_t  f1;
+   uint64_t  f2;
+   volatile unsigned f3 : 3;
+   uint16_t  f4;
+   volatile int64_t  f5;
+   int8_t  f6;
+};
+
