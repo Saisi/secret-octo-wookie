1e4140cfc45db94e96d632bfcaf8aad1e0e2186a
saisi <saisi@users.noreply.github.com>
2017-05-09 19:38:17 +0000

    1494358696 achromatization

---
+++ 1494358696.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      850667087
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const int16_t  f1;
+   const uint16_t  f2;
+   int64_t  f3;
+};
+
+union U1 {
+   volatile int64_t  f0;
+   int8_t * volatile  f1;
