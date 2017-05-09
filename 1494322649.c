5a5f686ca73c9ed3ffa7354581924499ee57ff47
saisi <saisi@users.noreply.github.com>
2017-05-09 09:37:30 +0000

    1494322649 badinage

---
+++ 1494322649.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3322793667
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+};
+#pragma pack(pop)
+
+struct S1 {
+   uint16_t  f0;
+   volatile uint16_t  f1;
