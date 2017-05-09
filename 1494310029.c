4f1a83ced68573bcf84d902f27c415471c6ef1bc
saisi <saisi@users.noreply.github.com>
2017-05-09 06:07:10 +0000

    1494310029 ascensive

---
+++ 1494310029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3031751486
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 13;
+   const uint32_t  f1;
+   volatile unsigned f2 : 22;
+   signed : 0;
+   signed f3 : 5;
+   const signed f4 : 29;
+   volatile signed f5 : 10;
+   signed f6 : 27;
+};
