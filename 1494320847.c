a1c08222586b49685c5b68a0d185a09ac6da25fa
saisi <saisi@users.noreply.github.com>
2017-05-09 09:07:28 +0000

    1494320847 athymy

---
+++ 1494320847.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2954986319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 6;
+   unsigned f1 : 5;
+   volatile signed f2 : 20;
+   unsigned f3 : 15;
+   const volatile unsigned f4 : 4;
+   unsigned f5 : 13;
+   signed f6 : 24;
