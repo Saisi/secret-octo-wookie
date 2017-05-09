49aae5eefcd520c647802917048ed0ea9a685562
saisi <saisi@users.noreply.github.com>
2017-05-09 20:38:22 +0000

    1494362301 anthemis

---
+++ 1494362301.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      148810905
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int32_t  f0;
+   int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_2 = 0x29BE1D92L;/* VOLATILE GLOBAL g_2 */
+static int8_t g_9 = 4L;
+static uint64_t g_12 = 18446744073709551612UL;
+static uint64_t g_17 = 0xBA87A5EAFB5D1CB6LL;
