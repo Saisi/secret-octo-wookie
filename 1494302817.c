619b6e89a061de7af687ecf54da45a32f4a0c780
saisi <saisi@users.noreply.github.com>
2017-05-09 04:06:58 +0000

    1494302817 aortism

---
+++ 1494302817.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2068561701
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 17;
+   unsigned f1 : 4;
+   int32_t  f2;
+   volatile unsigned f3 : 24;
+   unsigned f4 : 31;
+   const volatile unsigned f5 : 17;
+   const unsigned f6 : 16;
