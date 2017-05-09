7bbc00cd49dcf2615fa19ac283bc5f2b69a5b684
saisi <saisi@users.noreply.github.com>
2017-05-09 17:38:08 +0000

    1494351487 acrostolion

---
+++ 1494351487.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      803402176
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0xA4L;/* VOLATILE GLOBAL g_2 */
+static uint16_t g_20 = 0xF608L;
+static int32_t g_23 = (-7L);
+static int32_t g_29 = 0L;
+static int16_t g_35 = 0x1B36L;
+static uint64_t g_36 = 0UL;
+static int8_t g_39[8] = {4L,5L,4L,4L,5L,4L,4L,5L};
+static volatile uint64_t g_47 = 0x20A048C84060A3B7LL;/* VOLATILE GLOBAL g_47 */
+static int32_t g_50 = 1L;
