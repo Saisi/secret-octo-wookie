ec23d37f11e38132f0447cef8a670e3ca71d7cb6
saisi <saisi@users.noreply.github.com>
2017-05-10 21:40:39 +0000

    1494452438 alcoholdom

---
+++ 1494452438.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2942611173
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile int8_t  f1;
+   int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x85950CFDL;
+static int8_t g_11 = 0L;
+static int8_t *g_10 = &g_11;
