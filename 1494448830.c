6ed7aa3f4a2cfbe1fbded3431b78913f81d6f797
saisi <saisi@users.noreply.github.com>
2017-05-10 20:40:31 +0000

    1494448830 abichite

---
+++ 1494448830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3079060788
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 0L;
+static int32_t *g_12 = &g_13;
+static uint16_t g_24 = 0x66D2L;
+static uint16_t g_36[6] = {0x91C8L,0x37D6L,0x91C8L,0x91C8L,0x37D6L,0x91C8L};
+static uint16_t *g_35 = &g_36[4];
+static int16_t g_37 = 0L;
+static int64_t g_39 = 0xC1E0F758FC5674FALL;
+static int32_t g_58[8] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int8_t g_94 = (-4L);
