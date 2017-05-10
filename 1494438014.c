942b3a6081640ff0fb9c5f5232073e4eb34576d1
saisi <saisi@users.noreply.github.com>
2017-05-10 17:40:16 +0000

    1494438014 amentum

---
+++ 1494438014.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2436785465
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 27;
+   volatile signed f1 : 6;
+   unsigned f2 : 17;
+   signed f3 : 24;
+   unsigned f4 : 9;
+   signed f5 : 30;
+   unsigned f6 : 5;
