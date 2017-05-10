2d2be58da68ebc11ed28e6c1c75f529c6b672a29
saisi <saisi@users.noreply.github.com>
2017-05-10 12:09:43 +0000

    1494418179 antepalatal

---
+++ 1494418179.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      343524549
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   signed f1 : 20;
+   unsigned f2 : 7;
+   signed f3 : 22;
+   signed f4 : 15;
+   unsigned f5 : 12;
+   unsigned f6 : 27;
