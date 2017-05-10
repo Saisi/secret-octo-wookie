290be2cd506882798687ce36cf072830a538a83d
saisi <saisi@users.noreply.github.com>
2017-05-10 09:39:29 +0000

    1494409168 Agapetidae

---
+++ 1494409168.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3716089904
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint64_t  f0;
+   volatile signed f1 : 19;
+   unsigned f2 : 27;
+   signed f3 : 17;
+   signed f4 : 18;
+};
+#pragma pack(pop)
