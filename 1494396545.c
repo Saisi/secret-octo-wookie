2905d40751077b90d0549f8e2c94d9c30cace3e9
saisi <saisi@users.noreply.github.com>
2017-05-10 06:09:05 +0000

    1494396545 Alexas

---
+++ 1494396545.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      31191614
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 0xB905L;
+static int16_t g_8[3] = {0x659DL,0x659DL,0x659DL};
+static int32_t g_26[3] = {0xF2BA167CL,0xF2BA167CL,0xF2BA167CL};
+static uint32_t g_34 = 0UL;
+static int32_t g_43 = 0xA2A67F6AL;
+static int32_t *g_42 = &g_43;
+static uint32_t g_48 = 0x5E2138B3L;
+static int32_t g_87 = 0xA48B7552L;
+static uint32_t g_99 = 0x906A3399L;
