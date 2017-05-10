1500760f2ecd27ad9e60d78de37eb85278350498
saisi <saisi@users.noreply.github.com>
2017-05-10 18:10:18 +0000

    1494439817 ACOF

---
+++ 1494439817.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2035069160
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   const volatile unsigned f1 : 15;
+   signed f2 : 7;
+   volatile unsigned f3 : 13;
+   signed f4 : 30;
+   signed f5 : 18;
+   signed f6 : 11;
