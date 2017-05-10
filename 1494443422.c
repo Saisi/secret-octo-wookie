a2ede999ffa2b139a809f5b007c771c97fcb3b13
saisi <saisi@users.noreply.github.com>
2017-05-10 19:10:23 +0000

    1494443422 aliases

---
+++ 1494443422.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4025726335
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_5[1] = {0x44AD72D76715F0EDLL};
+static uint32_t g_14 = 0xF44CACFFL;
+static uint32_t *g_13 = &g_14;
+static int16_t g_33 = 0xE85EL;
+static int16_t g_40 = (-1L);
+static int16_t *g_39 = &g_40;
+static uint32_t g_52 = 0xCC9F71D6L;
+static uint32_t g_57 = 0UL;
+static int32_t g_78 = 0x25501822L;
