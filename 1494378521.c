e9bb06095564f868ac673c63316f928520dfb57f
saisi <saisi@users.noreply.github.com>
2017-05-10 01:08:41 +0000

    1494378521 Aegithalos

---
+++ 1494378521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1132398630
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 1;
+   uint32_t  f1;
+   unsigned f2 : 26;
+   volatile signed f3 : 20;
+   const signed f4 : 9;
+   const signed f5 : 17;
+   signed f6 : 14;
