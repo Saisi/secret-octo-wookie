6751bcabe0df3f10be59bd1e44f15a70719f6dcd
saisi <saisi@users.noreply.github.com>
2017-05-10 00:08:37 +0000

    1494374917 alpinesque

---
+++ 1494374917.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1616408203
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   signed f1 : 9;
+   int64_t  f2;
+   uint16_t  f3;
+   volatile unsigned f4 : 2;
+};
+
+union U1 {
+   const int16_t  f0;
