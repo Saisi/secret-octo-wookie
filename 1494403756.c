cba169e45444c0df5938cffe94e093d7b5819ea3
saisi <saisi@users.noreply.github.com>
2017-05-10 08:09:17 +0000

    1494403756 amylosis

---
+++ 1494403756.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2935140896
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   uint32_t  f1;
+   volatile signed f2 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_18[5][5] = {{0xBCL,0x65L,0x65L,0xBCL,0xF4L},{0xBCL,0x97L,8L,8L,0x97L},{0xF4L,0x65L,8L,0x3DL,0x3DL},{0x65L,0xF4L,0x65L,8L,0x3DL},{0x97L,0xBCL,0x3DL,0xBCL,0x97L}};
+static int16_t g_33 = 0x5D63L;
+static uint16_t g_50 = 0x236CL;
