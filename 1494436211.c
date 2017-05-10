f3c516e8b9dc916fa63d3a37da64716f01356768
saisi <saisi@users.noreply.github.com>
2017-05-10 17:10:12 +0000

    1494436211 aeronomical

---
+++ 1494436211.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1567215823
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 6;
+   volatile unsigned f1 : 5;
+   signed f2 : 20;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_20[5][9] = {{0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL},{0xAB5457D6L,0x24770669L,0xAB5457D6L,0xAB5457D6L,0x24770669L,0xAB5457D6L,0xAB5457D6L,0x24770669L,0xAB5457D6L},{0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL},{0xAB5457D6L,0x24770669L,0xAB5457D6L,0xAB5457D6L,0x24770669L,0xAB5457D6L,0xAB5457D6L,0x24770669L,0xAB5457D6L},{0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL,0x96F377CCL,(-1L),0x96F377CCL}};
+static int32_t g_49 = (-2L);
+static int32_t *g_48 = &g_49;
