cddc18a23a150cf1dde843ad4031888996437dcb
saisi <saisi@users.noreply.github.com>
2017-05-11 09:41:38 +0000

    1494495697 anthrahydroquinone

---
+++ 1494495697.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2718323605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 1L;
+static int16_t g_24[2][4] = {{1L,1L,1L,1L},{1L,1L,1L,1L}};
+static int16_t g_30 = 0L;
+static int16_t *g_29 = &g_30;
+static int64_t g_32 = 0xE5034859BB7A1405LL;
+static int32_t g_35 = 0L;
+static uint16_t g_40 = 1UL;
+static uint64_t g_43 = 1UL;
+static int32_t g_46 = (-5L);
