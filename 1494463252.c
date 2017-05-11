447618f1ef0ef289fdc5b859b1789adb197565a5
saisi <saisi@users.noreply.github.com>
2017-05-11 00:40:53 +0000

    1494463252 aerobus

---
+++ 1494463252.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2979000080
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+};
+
+struct S1 {
+   int8_t  f0;
+   const volatile int32_t  f1;
+   int32_t  f2;
+   volatile int32_t  f3;
+   const int32_t  f4;
