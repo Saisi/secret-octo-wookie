05b65cd06d95c8a712ac2ef0abcca8c1c26427c0
saisi <saisi@users.noreply.github.com>
2017-05-11 18:12:21 +0000

    1494526339 anticholinergic

---
+++ 1494526339.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3608753373
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+   volatile uint32_t  f1;
+   int8_t * volatile  f2;
+   const volatile int32_t  f3;
+   int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
