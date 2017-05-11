9b1d261538dd2dd36b3f0d881722063d39e0e56a
saisi <saisi@users.noreply.github.com>
2017-05-11 11:41:46 +0000

    1494502906 antibromic

---
+++ 1494502906.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4145718059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static int32_t *g_6 = &g_3;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
