8d6029adb100323689e6df23e08e3d70c7841dd9
saisi <saisi@users.noreply.github.com>
2017-05-11 10:11:40 +0000

    1494497500 analogicalness

---
+++ 1494497500.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4131778037
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+};
+
+union U1 {
+   int8_t  f0;
+   unsigned f1 : 23;
+};
+
+/* --- GLOBAL VARIABLES --- */
