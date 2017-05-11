6e6dedcc1648c4027e68c29b2469783a25ea0273
saisi <saisi@users.noreply.github.com>
2017-05-11 05:41:17 +0000

    1494481277 Alascan

---
+++ 1494481277.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4005917596
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x9FE387FBL;
+static volatile int8_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static int64_t g_7 = 0x3C34EEA2A3B3F365LL;
+static volatile int64_t g_8[2] = {1L,1L};
+static volatile int64_t g_9 = 0L;/* VOLATILE GLOBAL g_9 */
+static volatile int8_t g_10 = 0x0DL;/* VOLATILE GLOBAL g_10 */
+static uint64_t g_11 = 0xD228D5FEAF86F25DLL;
+
+
