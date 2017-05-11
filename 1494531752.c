0477f1bb3a933215258b281f54b1c0665d0ad3b7
saisi <saisi@users.noreply.github.com>
2017-05-11 19:42:32 +0000

    1494531752 atomics

---
+++ 1494531752.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3416968517
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_5 = 1UL;
+static int32_t g_26[1][10][2] = {{{0xFAAB39E2L,(-6L)},{0xE6BB2F14L,0xE6BB2F14L},{(-6L),0xFAAB39E2L},{0x33F29C8DL,0xFAAB39E2L},{(-6L),0xE6BB2F14L},{0xE6BB2F14L,(-6L)},{0xFAAB39E2L,0x33F29C8DL},{0xFAAB39E2L,(-6L)},{0xE6BB2F14L,0xE6BB2F14L},{(-6L),0xFAAB39E2L}}};
+static uint8_t g_43 = 0xEEL;
+static uint8_t g_66 = 255UL;
+static uint8_t *g_65[4] = {&g_66,&g_66,&g_66,&g_66};
+static int32_t *g_76 = &g_26[0][9][0];
+static int32_t ** volatile g_75 = &g_76;/* VOLATILE GLOBAL g_75 */
+static volatile uint8_t ** volatile g_95 = (void*)0;/* VOLATILE GLOBAL g_95 */
+static int32_t g_97 = 0L;
