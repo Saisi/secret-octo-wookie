19b914101a314f995e45101681c82350c00e7378
saisi <saisi@users.noreply.github.com>
2017-05-11 06:41:22 +0000

    1494484881 antilogarithm

---
+++ 1494484881.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3690117678
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   volatile uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_4 = 9L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_21 = 0x8EA2C39DL;
+static int32_t g_32 = 4L;
+static int32_t g_48 = 0xCFDEF05DL;
