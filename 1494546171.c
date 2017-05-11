72e8d027cd813151e1d84c8e31adc790e4124825
saisi <saisi@users.noreply.github.com>
2017-05-11 23:42:52 +0000

    1494546171 ampelopsin

---
+++ 1494546171.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      315155084
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint32_t  f0;
+   volatile int64_t  f1;
+   const volatile int64_t  f2;
+   volatile signed : 0;
+   volatile uint32_t  f3;
+   uint8_t  f4;
+   volatile int8_t  f5;
