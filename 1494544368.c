c724bc32dcdf9a06e5015c115f68840671c2422a
saisi <saisi@users.noreply.github.com>
2017-05-11 23:12:49 +0000

    1494544368 alorcinic

---
+++ 1494544368.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2460909875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 24;
+   signed f1 : 15;
+   signed f2 : 22;
+};
+
+struct S1 {
+   const volatile uint16_t  f0;
+   const volatile uint64_t  f1;
+};
