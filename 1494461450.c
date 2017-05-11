6cb1d949ed9f5a20d8fd1ee0235eb46a4df6b1a6
saisi <saisi@users.noreply.github.com>
2017-05-11 00:10:51 +0000

    1494461450 anomalism

---
+++ 1494461450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2074957828
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 16;
+   unsigned f1 : 5;
+   volatile int16_t  f2;
+   const signed f3 : 11;
+   volatile signed f4 : 4;
+};
+#pragma pack(pop)
