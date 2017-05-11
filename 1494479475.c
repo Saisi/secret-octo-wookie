755d3f9735165155a8c29a0d53d3cbf14234ebbe
saisi <saisi@users.noreply.github.com>
2017-05-11 05:11:15 +0000

    1494479475 acylamidobenzene

---
+++ 1494479475.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3584125095
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const uint32_t  f1;
+   const signed f2 : 25;
+   const int32_t  f3;
+   const int16_t  f4;
+   uint32_t  f5;
+   volatile signed f6 : 9;
+   volatile uint32_t  f7;
+   int16_t  f8;
