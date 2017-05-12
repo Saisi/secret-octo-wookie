c6184ac307000e672b7a391d6e805796bf37f489
saisi <saisi@users.noreply.github.com>
2017-05-12 21:44:43 +0000

    1494625483 Alfric

---
+++ 1494625483.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3433544340
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[7] = {3L,3L,3L,3L,3L,3L,3L};
+static volatile int32_t g_3[8] = {0L,0L,0L,0L,0L,0L,0L,0L};
+static int32_t g_4 = 0x3D8DA8E9L;
+static int32_t g_8 = 0x4C8D3C89L;
+static volatile int32_t *g_27[5][2] = {{&g_3[5],&g_3[5]},{&g_3[5],&g_3[5]},{&g_3[5],&g_3[5]},{&g_3[5],&g_3[5]},{&g_3[5],&g_3[5]}};
+static volatile int32_t ** volatile g_26[3][1] = {{&g_27[1][1]},{&g_27[1][1]},{&g_27[1][1]}};
+static int32_t *g_30 = &g_8;
+static int32_t **g_29 = &g_30;
+static int32_t g_31 = 1L;
