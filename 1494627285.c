fb2c6bf1e6fda01434aa4dfaaf656c3c633b0dba
saisi <saisi@users.noreply.github.com>
2017-05-12 22:14:45 +0000

    1494627285 backstopped

---
+++ 1494627285.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4262135566
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int16_t  f1;
+   const uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_3 = {0xFB6318D5L};
+static union U0 *g_2 = &g_3;
+static union U0 ** volatile g_5[1][7] = {{(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0}};
