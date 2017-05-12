8b36c52c5c74982b6eb82ef7ebe670901ad7e8a3
saisi <saisi@users.noreply.github.com>
2017-05-12 15:14:12 +0000

    1494602052 autovaccine

---
+++ 1494602052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3059580496
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_9 = (-1L);
+static uint32_t g_24[1] = {0x884CB8F6L};
+static uint64_t g_32 = 0UL;
+static int32_t g_38 = 0x55261FC4L;
+static int16_t g_50 = (-1L);
+static int32_t g_58 = 0L;
+static uint64_t g_61 = 18446744073709551612UL;
+static uint32_t g_64 = 4294967295UL;
+static int64_t g_66 = 1L;
