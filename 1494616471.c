aa2aa6f6dadb06dcd6da742816db126d9c722d5c
saisi <saisi@users.noreply.github.com>
2017-05-12 19:14:32 +0000

    1494616471 azlon

---
+++ 1494616471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1726750243
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   const volatile uint8_t  f1;
+   signed f2 : 23;
+   const volatile uint16_t  f3;
+   const uint16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
