720f9d3d51c9ffb750d8351affaab12d6c50ad0e
saisi <saisi@users.noreply.github.com>
2017-05-12 11:13:51 +0000

    1494587631 aortopathy

---
+++ 1494587631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1279273708
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 4;
+   volatile uint64_t  f1;
+   volatile unsigned f2 : 1;
+   signed f3 : 31;
+   unsigned f4 : 8;
+   volatile unsigned f5 : 20;
+   signed f6 : 5;
