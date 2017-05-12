f7b69d61967da29b6beb87816ded4a6a0efd68f2
saisi <saisi@users.noreply.github.com>
2017-05-12 01:43:02 +0000

    1494553381 Amara

---
+++ 1494553381.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      542288978
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 4L;
+static volatile uint32_t g_14 = 0x39D20578L;/* VOLATILE GLOBAL g_14 */
+static int32_t g_51 = 0L;
+static int32_t g_52 = 0L;
+static int32_t g_53 = 3L;
+static int32_t g_54 = (-3L);
+static uint16_t g_76 = 0x3BCEL;
+static int64_t g_98 = 1L;
+static uint16_t g_99 = 0x467BL;
