c2662cec74a5ac3a6317496f22db46edcb1bfdd8
saisi <saisi@users.noreply.github.com>
2017-05-12 20:14:36 +0000

    1494620076 antinuke

---
+++ 1494620076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2173584186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_16 = (-3L);/* VOLATILE GLOBAL g_16 */
+static volatile int32_t g_17 = 0xB4837182L;/* VOLATILE GLOBAL g_17 */
+static volatile int32_t g_18 = 0xF9ACF257L;/* VOLATILE GLOBAL g_18 */
+static volatile int32_t g_19 = 0L;/* VOLATILE GLOBAL g_19 */
+static int32_t g_20 = 0x865B49D5L;
+static volatile int32_t g_24 = 0x4145C174L;/* VOLATILE GLOBAL g_24 */
+static int32_t g_25 = 0x68807383L;
+static int32_t g_31 = 5L;
+static volatile int32_t * volatile g_47 = &g_17;/* VOLATILE GLOBAL g_47 */
