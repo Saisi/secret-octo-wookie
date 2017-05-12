5d71610e354a5e7dce046d64d275293a5714ff60
saisi <saisi@users.noreply.github.com>
2017-05-12 08:13:37 +0000

    1494576816 Albuginaceae

---
+++ 1494576816.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1457649440
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_4 = 0L;
+static int8_t g_24[1][10] = {{0x39L,0x39L,0x39L,0x39L,0x39L,0x39L,0x39L,0x39L,0x39L,0x39L}};
+static int32_t g_33 = 0x99649744L;
+static uint8_t g_45[3][6][4] = {{{0x27L,0x98L,0x27L,0x36L},{0UL,3UL,0x46L,0UL},{255UL,0xDCL,0UL,3UL},{0x93L,0x26L,0UL,0x39L},{255UL,0x93L,0x46L,248UL},{0UL,0x39L,0x27L,0x27L}},{{0x27L,0x27L,0x39L,0UL},{248UL,0x46L,0x93L,255UL},{0x39L,0UL,0x26L,0x93L},{0x36L,254UL,0UL,0x98L},{254UL,0x26L,0x36L,0x27L},{0x93L,1UL,3UL,1UL}},{{0x60L,0x3BL,0x98L,9UL},{3UL,0x46L,0UL,247UL},{3UL,0x39L,0x39L,0x36L},{3UL,0UL,0UL,254UL},{3UL,0x36L,0x98L,0x93L},{0x60L,3UL,3UL,0x60L}}};
+static int32_t g_47 = 0x93F8DE90L;
+static int32_t *g_46 = &g_47;
+static uint32_t g_76[6] = {0x4FE37806L,0x4FE37806L,0x4FE37806L,0x4FE37806L,0x4FE37806L,0x4FE37806L};
+static int8_t g_81[1][6][7] = {{{0L,0L,0L,0L,0L,0L,0L},{1L,1L,1L,1L,1L,1L,1L},{0L,0L,0L,0L,0L,0L,0L},{1L,1L,1L,1L,1L,1L,1L},{0L,0L,0L,0L,0L,0L,0L},{1L,1L,1L,1L,1L,1L,1L}}};
+static int64_t g_83[4] = {0L,0L,0L,0L};
