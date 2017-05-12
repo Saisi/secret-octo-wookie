6b8fd0fd1a4f1c3bd1c2bf03f6addcf45b360d6f
saisi <saisi@users.noreply.github.com>
2017-05-12 18:14:27 +0000

    1494612866 anabolite

---
+++ 1494612866.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3994302138
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint8_t  f0;
+   const volatile int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 1L;
+static const int32_t *g_22 = &g_13;
+static const int32_t **g_21 = &g_22;
+static int64_t g_44 = 1L;
