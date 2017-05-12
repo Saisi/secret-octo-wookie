13d1512111efa12e8b6fc1b24e6fbbe6ebbbe9fa
saisi <saisi@users.noreply.github.com>
2017-05-12 11:43:54 +0000

    1494589433 bacteriaemia

---
+++ 1494589433.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2607496646
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   const signed f1 : 2;
+   uint32_t  f2;
+   int16_t  f3;
+   signed f4 : 24;
+   const signed f5 : 26;
+   volatile unsigned f6 : 23;
+   volatile int32_t  f7;
+   signed f8 : 9;
