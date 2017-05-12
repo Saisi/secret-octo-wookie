818a24c5a9386c8deb57bfd2dda62e1a802e338f
saisi <saisi@users.noreply.github.com>
2017-05-12 13:14:02 +0000

    1494594840 axiolite

---
+++ 1494594840.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2951022650
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 11;
+   unsigned f1 : 20;
+   signed f2 : 6;
+   unsigned f3 : 26;
+};
+
+struct S1 {
+   const unsigned f0 : 27;
+   int32_t  f1;
