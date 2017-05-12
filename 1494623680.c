c06abe11c7fab8cf8ca013f87b1d703df28ff28b
saisi <saisi@users.noreply.github.com>
2017-05-12 21:14:41 +0000

    1494623680 Baalism

---
+++ 1494623680.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1630849085
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * const  f0;
+   int16_t  f1;
+   uint16_t  f2;
+};
+
+union U1 {
+   int8_t  f0;
+   int8_t  f1;
+   volatile int32_t  f2;
