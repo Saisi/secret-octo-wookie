95bd233544c0ee3de36a081dfad1073a9e1d3da1
saisi <saisi@users.noreply.github.com>
2017-05-13 02:45:07 +0000

    1494643506 antibreakage

---
+++ 1494643506.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      730860317
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 13;
+   const unsigned f1 : 16;
+   signed f2 : 19;
+   const signed f3 : 18;
+   signed f4 : 27;
+   signed f5 : 8;
+   const volatile unsigned f6 : 7;
