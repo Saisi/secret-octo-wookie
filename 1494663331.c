f09412e261e6f48ca3dc36524d3adb6773112c8a
saisi <saisi@users.noreply.github.com>
2017-05-13 08:15:33 +0000

    1494663331 ascendance

---
+++ 1494663331.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1880925285
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   unsigned f1 : 6;
+   unsigned f2 : 4;
+   const unsigned f3 : 24;
+};
+
+union U1 {
+   uint8_t  f0;
+   unsigned f1 : 31;
