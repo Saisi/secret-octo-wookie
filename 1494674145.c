e0a03aafaf108bfd0957a4438081d0136e37e3c9
saisi <saisi@users.noreply.github.com>
2017-05-13 11:15:45 +0000

    1494674145 Archaeozoic

---
+++ 1494674145.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      59531039
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 0x2E31D832L;
+static union U0 g_25 = {0x60EDAC7AL};
+static int32_t g_31 = 0x86FF18EEL;
+static volatile int32_t g_37 = 0L;/* VOLATILE GLOBAL g_37 */
