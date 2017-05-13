40ebca90f848d259bde1643c234b2e8bec23d026
saisi <saisi@users.noreply.github.com>
2017-05-13 13:15:55 +0000

    1494681355 aquacultural

---
+++ 1494681355.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3323891142
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint8_t  f0;
+   uint64_t  f1;
+   const int32_t  f2;
+   uint32_t  f3;
+   volatile int32_t  f4;
+   uint16_t  f5;
+   int16_t  f6;
+   uint32_t  f7;
+};
