7fc96c97ecb00bf65ec17f42e616c8bc99344987
saisi <saisi@users.noreply.github.com>
2017-05-13 20:46:32 +0000

    1494708392 archcharlatan

---
+++ 1494708392.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3864970182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   volatile unsigned f1 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile struct S0 g_2 = {250,1};/* VOLATILE GLOBAL g_2 */
+static volatile struct S0 * volatile g_3[9] = {&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2,&g_2};
+static volatile struct S0 g_5[8][5] = {{{212,0},{208,0},{212,0},{60,0},{206,0}},{{356,0},{268,1},{268,1},{356,0},{268,1}},{{206,0},{208,0},{223,1},{208,0},{206,0}},{{268,1},{356,0},{268,1},{268,1},{356,0}},{{206,0},{60,0},{212,0},{208,0},{212,0}},{{356,0},{356,0},{350,1},{356,0},{356,0}},{{212,0},{208,0},{212,0},{60,0},{206,0}},{{356,0},{268,1},{350,1},{268,1},{350,1}}};
+
