ec7198a76758e6a4cf22c8a594773d69a7a68f69
saisi <saisi@users.noreply.github.com>
2017-05-13 07:15:28 +0000

    1494659727 aneuploid

---
+++ 1494659727.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2273628997
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2[1][7][1] = {{{0xA3L},{0x96L},{0x96L},{0xA3L},{0x96L},{0x96L},{0xA3L}}};
+static volatile int32_t g_3 = 0x2443250EL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4[2] = {0x5043C4ABL,0x5043C4ABL};
+static volatile int32_t g_5[1] = {1L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
