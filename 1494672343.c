219039f43f96e7abedd6d0920a4df054739567b2
saisi <saisi@users.noreply.github.com>
2017-05-13 10:45:43 +0000

    1494672343 absinthiating

---
+++ 1494672343.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      452142382
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   unsigned f0 : 21;
+   int64_t  f1;
+   int64_t  f2;
+   const volatile signed f3 : 24;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x05DAE5A6L;
