b41fb024c32aa192c4d9faa0eb8c01a24249c78e
saisi <saisi@users.noreply.github.com>
2017-05-13 13:45:57 +0000

    1494683157 autotuberculin

---
+++ 1494683157.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2843940949
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_10[2] = {0x86E5L,0x86E5L};
+static int32_t g_12 = 1L;
+static int16_t g_17[8][8] = {{(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L},{(-8L),(-8L),9L,(-8L),(-8L),9L,(-8L),(-8L)},{0xBDB9L,(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L,0xBDB9L,(-8L)},{(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L},{(-8L),(-8L),9L,(-8L),(-8L),9L,(-8L),(-8L)},{0xBDB9L,(-8L),0xBDB9L,0xBDB9L,0xBDB9L,9L,9L,0xBDB9L},{0xBDB9L,9L,9L,0xBDB9L,9L,9L,0xBDB9L,9L},{0xBDB9L,0xBDB9L,(-8L),0xBDB9L,0xBDB9L,(-8L),0xBDB9L,0xBDB9L}};
+static uint16_t g_19 = 1UL;
+static int32_t g_27 = 0x4E2E0F61L;
+static int32_t * volatile g_26 = &g_27;/* VOLATILE GLOBAL g_26 */
+static int32_t *g_31 = &g_12;
+static int32_t ** volatile g_30[10] = {&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31,&g_31};
+static int32_t ** volatile g_32 = &g_31;/* VOLATILE GLOBAL g_32 */
