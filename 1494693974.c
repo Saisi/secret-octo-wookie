b72f0ba45925b6014ae407cc72534ae2ba150fda
saisi <saisi@users.noreply.github.com>
2017-05-13 16:46:15 +0000

    1494693974 apophthegmatist

---
+++ 1494693974.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1648478377
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int64_t  f0;
+   const signed f1 : 8;
+   unsigned f2 : 30;
+   uint16_t  f3;
+   const int8_t  f4;
+   signed f5 : 10;
+   signed f6 : 5;
