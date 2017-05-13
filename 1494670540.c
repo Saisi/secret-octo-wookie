7c80c928bdd0247e54ae628c8b2970cba6f5bde0
saisi <saisi@users.noreply.github.com>
2017-05-13 10:15:41 +0000

    1494670540 alanyls

---
+++ 1494670540.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2918114476
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint64_t  f0;
+   int32_t  f1;
+   volatile int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4 = (-10L);
+static int32_t g_18 = 1L;
+static int32_t *g_17 = &g_18;
