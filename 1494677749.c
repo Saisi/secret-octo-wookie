d8ca7885b4092199472a215dcccb0eb388bb3a6b
saisi <saisi@users.noreply.github.com>
2017-05-13 12:15:51 +0000

    1494677749 apometaboly

---
+++ 1494677749.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4085465396
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 8;
+   int64_t  f1;
+   uint32_t  f2;
+   int64_t  f3;
+   int16_t  f4;
+   uint64_t  f5;
+   volatile uint8_t  f6;
+   const int32_t  f7;
+   int16_t  f8;
