c4518535476e59dea713f44b464c094c46fb1664
saisi <saisi@users.noreply.github.com>
2017-05-13 02:15:02 +0000

    1494641702 antilactase

---
+++ 1494641702.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3527735829
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * const  f0;
+};
+
+union U1 {
+   const volatile int8_t * volatile  f0;
+   volatile uint16_t  f1;
+   uint16_t  f2;
+   uint32_t  f3;
+};
