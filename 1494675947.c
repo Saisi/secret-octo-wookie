cb26d2615bae503fc4eabee579568760108686d7
saisi <saisi@users.noreply.github.com>
2017-05-13 11:45:47 +0000

    1494675947 ankylopodia

---
+++ 1494675947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4243488010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 28;
+   unsigned f2 : 14;
+   volatile uint8_t  f3;
+   unsigned f4 : 3;
+   unsigned f5 : 3;
+   signed f6 : 24;
+   signed f7 : 17;
+   signed f8 : 30;
