c05dfb336fe834b467581fb5661dc264aec09293
saisi <saisi@users.noreply.github.com>
2017-05-13 03:15:09 +0000

    1494645308 asseverating

---
+++ 1494645308.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1134488379
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint64_t  f1;
+   unsigned f2 : 26;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_8 = 0xFAL;
+static uint64_t g_10 = 0x35C5735E4B7834D8LL;
+static uint16_t g_23 = 0x5240L;
