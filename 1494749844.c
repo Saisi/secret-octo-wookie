0ae0bf5673afefe90c77949264a28bd651581c02
saisi <saisi@users.noreply.github.com>
2017-05-14 08:17:24 +0000

    1494749844 Auburta

---
+++ 1494749844.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3351300711
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-6L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
