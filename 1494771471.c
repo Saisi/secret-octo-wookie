32a4ee5c087c84352df1c05d5447f1d952e517ca
saisi <saisi@users.noreply.github.com>
2017-05-14 14:17:51 +0000

    1494771471 Australianism

---
+++ 1494771471.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3910119857
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   unsigned f1 : 18;
+   volatile int16_t  f2;
+};
+
+union U1 {
+   uint32_t  f0;
+   const int16_t  f1;
+   volatile uint32_t  f2;
