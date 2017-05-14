931e961869fab3ac0106a3bfd79ed43d1b5cbae3
saisi <saisi@users.noreply.github.com>
2017-05-14 04:47:09 +0000

    1494737228 awols

---
+++ 1494737228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      281699363
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 16;
+   uint32_t  f1;
+   uint16_t  f2;
+   uint16_t  f3;
+   unsigned f4 : 14;
+};
+
+union U1 {
+   volatile uint32_t  f0;
