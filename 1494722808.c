ea6cbb83745d7dcca96df92c1d9d8bf7f07d6514
saisi <saisi@users.noreply.github.com>
2017-05-14 00:46:48 +0000

    1494722808 adsorption

---
+++ 1494722808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2541063872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 14;
+   const signed f1 : 11;
+   unsigned f2 : 1;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
