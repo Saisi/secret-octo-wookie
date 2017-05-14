276815893147203f25b365c2824b4cae6b102c56
saisi <saisi@users.noreply.github.com>
2017-05-14 13:47:49 +0000

    1494769669 aggros

---
+++ 1494769669.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      302984187
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x987F2FB5L;
+static int32_t g_6 = 0x4D8A94B2L;
+static uint8_t g_8[7][5] = {{8UL,8UL,8UL,8UL,8UL},{249UL,251UL,249UL,251UL,249UL},{8UL,8UL,8UL,8UL,8UL},{249UL,251UL,249UL,251UL,249UL},{8UL,8UL,8UL,8UL,8UL},{249UL,251UL,249UL,251UL,249UL},{8UL,8UL,8UL,8UL,8UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
