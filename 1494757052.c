6543810526aa5ff9a027fb872fd5d70ba6224345
saisi <saisi@users.noreply.github.com>
2017-05-14 10:17:34 +0000

    1494757052 alcoholicity

---
+++ 1494757052.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3094536118
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 9;
+   signed f1 : 26;
+   unsigned f2 : 20;
+   volatile int32_t  f3;
+   unsigned f4 : 17;
+   signed f5 : 25;
+   unsigned f6 : 22;
+   unsigned f7 : 23;
+   volatile signed f8 : 12;
