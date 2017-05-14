86fcae00072dd30aea7aef3b8f480539e1ce1677
saisi <saisi@users.noreply.github.com>
2017-05-14 12:47:45 +0000

    1494766064 atrabiliary

---
+++ 1494766064.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3638976614
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 1;
+   signed f1 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 20;
+   signed f1 : 9;
