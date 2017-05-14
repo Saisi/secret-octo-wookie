62c20753ea9a3ff87a82031b09c834bc884e88f8
saisi <saisi@users.noreply.github.com>
2017-05-14 02:46:57 +0000

    1494730016 acquaintanceship

---
+++ 1494730016.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2094159213
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static uint8_t g_10 = 0x30L;
+static uint16_t g_12 = 0UL;
+static int32_t g_44 = (-2L);
+static volatile int64_t g_54[10][1] = {{(-10L)},{1L},{(-10L)},{1L},{(-10L)},{1L},{(-10L)},{1L},{(-10L)},{1L}};
+static int32_t g_55 = 4L;
+static volatile int8_t g_57 = 0x8CL;/* VOLATILE GLOBAL g_57 */
+static uint64_t g_58[4] = {0xB476018F8C21E6DBLL,0xB476018F8C21E6DBLL,0xB476018F8C21E6DBLL,0xB476018F8C21E6DBLL};
+static volatile int8_t g_88[7][3] = {{6L,(-8L),(-8L)},{0x31L,0xCCL,0xCCL},{6L,(-8L),(-8L)},{0x31L,0xCCL,0xCCL},{6L,(-8L),(-8L)},{0x31L,0xCCL,0xCCL},{6L,(-8L),(-8L)}};
