fdf691076f34186c6204144499427e7efa833113
saisi <saisi@users.noreply.github.com>
2017-05-14 05:47:13 +0000

    1494740832 antiscrofulous

---
+++ 1494740832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      954880225
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   volatile uint8_t  f1;
+   const int8_t * f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 0x10L;
+static int8_t *g_8 = &g_9;
+static int8_t g_47 = 0L;
