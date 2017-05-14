476ec48b4edc8dc4be1d3a59cd368587e0316166
saisi <saisi@users.noreply.github.com>
2017-05-14 17:48:14 +0000

    1494784093 Asclepi

---
+++ 1494784093.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4197567386
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2 = 18446744073709551612UL;
+static uint8_t g_3 = 249UL;
+static int8_t g_42 = 0x09L;
+static const int8_t g_65 = 3L;
+static const int8_t *g_64[3] = {&g_65,&g_65,&g_65};
+static int8_t g_80[1] = {0x19L};
+static int16_t g_94 = (-1L);
+static uint64_t g_96 = 0xC0C20EB8FC401265LL;
+static int32_t g_98 = (-1L);
