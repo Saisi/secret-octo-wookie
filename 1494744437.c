1eba719dcaa909187d08d6eccd555087254b1110
saisi <saisi@users.noreply.github.com>
2017-05-14 06:47:17 +0000

    1494744437 attender

---
+++ 1494744437.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2002401744
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_8[9][1][2] = {{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}},{{0x182EAC4530722263LL,0x182EAC4530722263LL}}};
+static int32_t g_42 = 0x1FCC8AF9L;
+static uint8_t g_44 = 0x1EL;
+static union U0 g_58 = {0x5FE9L};
+static volatile int32_t *g_63 = (void*)0;
