b8b4327ec29a4485272a417714517cb9ba3d0c8e
saisi <saisi@users.noreply.github.com>
2017-05-14 21:48:34 +0000

    1494798513 asterospondylic

---
+++ 1494798513.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      323731276
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint32_t  f0;
+   int8_t  f1;
+   volatile int16_t  f2;
+   const uint8_t  f3;
+   uint16_t  f4;
+   const uint32_t  f5;
+   unsigned f6 : 4;
+   int32_t  f7;
+   const volatile int32_t  f8;
