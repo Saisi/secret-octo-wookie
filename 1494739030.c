b844faed93ff7288536e29016bf1e55e471d96ff
saisi <saisi@users.noreply.github.com>
2017-05-14 05:17:10 +0000

    1494739030 Almohade

---
+++ 1494739030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1594372476
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   uint8_t  f0;
+   uint64_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U1 g_4 = {0x8FL};
+
+
