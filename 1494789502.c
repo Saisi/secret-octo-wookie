e745d306c1c45536576a9d1333d9c71dc3c0e047
saisi <saisi@users.noreply.github.com>
2017-05-14 19:18:22 +0000

    1494789502 aboriginality

---
+++ 1494789502.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1879686240
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-1L);
+static int64_t g_7 = 0L;
+static int32_t g_8 = 1L;
+static volatile uint32_t g_9 = 0xF6085E07L;/* VOLATILE GLOBAL g_9 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
