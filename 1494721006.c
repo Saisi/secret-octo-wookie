b97fecdb563e4dbbfe11411fb9a9c1718de8fdc7
saisi <saisi@users.noreply.github.com>
2017-05-14 00:16:47 +0000

    1494721006 bacteriologies

---
+++ 1494721006.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2789791144
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[3][10] = {{1L,(-4L),(-7L),(-7L),(-4L),1L,(-1L),0xD65121ACL,(-7L),(-1L)},{0xFF0EA75CL,(-4L),0xF72D4EA6L,0xD65121ACL,(-4L),0xEE2FDE2BL,(-4L),0xD65121ACL,0xF72D4EA6L,(-4L)},{0xFF0EA75CL,(-1L),(-7L),0xD65121ACL,(-1L),1L,(-4L),(-7L),(-7L),(-4L)}};
+static int32_t g_35 = 8L;
+static int32_t * volatile g_34 = &g_35;/* VOLATILE GLOBAL g_34 */
+static uint64_t g_46 = 0UL;
+static int32_t *g_63 = &g_35;
+static int32_t **g_62 = &g_63;
+static uint32_t g_95 = 0xCD8B2F8DL;
+static uint16_t g_97 = 65532UL;
+static const int32_t **g_100 = (void*)0;
