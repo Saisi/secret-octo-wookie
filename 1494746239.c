95bb2331f190f358c14570263fc277ae748597a9
saisi <saisi@users.noreply.github.com>
2017-05-14 07:17:19 +0000

    1494746239 alkarsine

---
+++ 1494746239.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3025316098
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 15;
+   const signed f1 : 8;
+   volatile signed f2 : 10;
+   const volatile unsigned f3 : 14;
+   unsigned f4 : 5;
+   unsigned f5 : 8;
+   const signed f6 : 6;
