7e49370db8ab900778cea7b630eeae9f341b4001
saisi <saisi@users.noreply.github.com>
2017-05-14 20:18:27 +0000

    1494793106 antiwaste

---
+++ 1494793106.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1602044234
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 28;
+   unsigned f2 : 14;
+   volatile unsigned f3 : 2;
+   signed f4 : 9;
+   unsigned f5 : 28;
+   volatile signed f6 : 10;
+   unsigned f7 : 23;
+   volatile signed f8 : 31;
