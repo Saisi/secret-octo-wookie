f526d97f88bb092a7eb29effd47614a6eaaf6707
saisi <saisi@users.noreply.github.com>
2017-05-14 18:18:16 +0000

    1494785895 autoexcitation

---
+++ 1494785895.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      749945465
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile signed f0 : 27;
+   signed f1 : 6;
+};
+#pragma pack(pop)
+
+struct S1 {
+   volatile unsigned f0 : 18;
