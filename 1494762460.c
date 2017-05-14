cc82a3a600132df124f88b98d42816a6b8a1fc9c
saisi <saisi@users.noreply.github.com>
2017-05-14 11:47:40 +0000

    1494762460 astriferous

---
+++ 1494762460.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2053391352
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   const signed f1 : 23;
+   signed f2 : 29;
+   int16_t  f3;
+   uint32_t  f4;
+};
+
+union U1 {
+   volatile int32_t  f0;
