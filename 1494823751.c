e9862f35e9996d318ca3d8a9c861bc394bcc0d6d
saisi <saisi@users.noreply.github.com>
2017-05-15 04:49:12 +0000

    1494823751 antelabium

---
+++ 1494823751.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3899714384
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_10 = (-1L);
+static int32_t *g_9 = &g_10;
+static uint32_t g_15[6] = {0UL,0x5E08CCCDL,0UL,0UL,0x5E08CCCDL,0UL};
+static uint16_t g_22 = 0xFF29L;
+static int32_t g_36 = 1L;
+static volatile uint16_t g_41 = 65535UL;/* VOLATILE GLOBAL g_41 */
+static uint32_t g_67 = 0UL;
+static uint8_t g_73 = 252UL;
+static int64_t g_76 = 0x00DD038CF49CF94ALL;
