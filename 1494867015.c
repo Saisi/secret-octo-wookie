2e6687e31ca9bb4ab9a499f2a113c8989e2a73a0
saisi <saisi@users.noreply.github.com>
2017-05-15 16:50:16 +0000

    1494867015 aphasiology

---
+++ 1494867015.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1437249147
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 28;
+   const volatile signed f1 : 3;
+   signed f2 : 26;
+};
+#pragma pack(pop)
+
+struct S1 {
