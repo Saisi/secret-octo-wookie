07c79b8d2493e6ed3acfbd3cde3703bb5952d0c0
saisi <saisi@users.noreply.github.com>
2017-05-15 13:49:53 +0000

    1494856193 Alocasia

---
+++ 1494856193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      883970054
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static int32_t g_5 = 8L;
+static const int32_t *g_8[1] = {(void*)0};
+static const int32_t ** const  volatile g_7 = &g_8[0];/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_11 = 0x56574B60L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_12 = 0x467BB1A8L;
+static uint8_t g_13 = 1UL;
+
+
