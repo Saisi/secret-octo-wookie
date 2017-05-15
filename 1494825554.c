7b851150c518c871375f15340def3fd34ee70ac4
saisi <saisi@users.noreply.github.com>
2017-05-15 05:19:14 +0000

    1494825554 Atalanta

---
+++ 1494825554.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4061189475
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const uint32_t  f0;
+   int16_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t *g_5 = (void*)0;
+static volatile uint16_t g_8 = 0UL;/* VOLATILE GLOBAL g_8 */
+static int32_t g_11 = 6L;
+static union U1 g_12 = {0x495231FBL};
