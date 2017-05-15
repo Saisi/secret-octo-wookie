a59119d526e9dddfa193a2a1e61f30265aa3953b
saisi <saisi@users.noreply.github.com>
2017-05-15 11:19:42 +0000

    1494847182 alcoholomania

---
+++ 1494847182.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3347545159
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[5][3] = {{0xC2A82A10L,9L,(-10L)},{0x2B74C38BL,9L,0x2B74C38BL},{0xB552BF33L,0xC2A82A10L,(-10L)},{0xB552BF33L,0xB552BF33L,0xC2A82A10L},{0x2B74C38BL,0xC2A82A10L,0xC2A82A10L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
