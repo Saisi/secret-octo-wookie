02d7dd52e0985b0d21846962ea5f5d9cc639a73d
saisi <saisi@users.noreply.github.com>
2017-05-15 23:20:47 +0000

    1494890447 ablator

---
+++ 1494890447.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3819840464
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int8_t * f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_8 = 0x6312L;
+static int8_t g_20[4][10] = {{0x6BL,0L,0x89L,0x89L,0L,0x6BL,0L,0x89L,0x89L,0L},{0x6BL,0L,0x89L,0x89L,0L,0x6BL,0L,0x89L,0x89L,0L},{0x6BL,0L,0x89L,0x89L,0L,0x6BL,0L,0x89L,0x89L,0L},{0x6BL,0L,0x89L,0x89L,0L,0x6BL,0L,0x89L,0x89L,0L}};
+static uint8_t g_23 = 0x55L;
+static int8_t g_30 = 0x8CL;
