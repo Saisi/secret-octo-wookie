cbab40055bb6fd3f9a97cb7302a63e2aa9cfb82c
saisi <saisi@users.noreply.github.com>
2017-05-15 03:49:08 +0000

    1494820147 autecology

---
+++ 1494820147.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      75422285
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0xC8B1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xF833FCB5L;
+static volatile int32_t g_6 = 0L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0x948708EFL;
+static int32_t g_13 = 0x6B16B471L;
+static uint64_t g_82 = 0xA79815C87E177106LL;
+static uint8_t g_84 = 5UL;
+static uint8_t *g_83 = &g_84;
