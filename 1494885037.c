54ecdf21df70c70d9b1c2ca4b1a2309878330c6d
saisi <saisi@users.noreply.github.com>
2017-05-15 21:50:39 +0000

    1494885037 angiology

---
+++ 1494885037.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3501687616
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 14;
+   uint8_t  f1;
+   uint64_t  f2;
+   const volatile uint32_t  f3;
+   volatile int32_t  f4;
+};
+#pragma pack(pop)
