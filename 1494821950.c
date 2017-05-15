1e3ce06bc105313336ee9244270a93380ef0cdb4
saisi <saisi@users.noreply.github.com>
2017-05-15 04:19:10 +0000

    1494821950 australite

---
+++ 1494821950.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      168219507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U2 {
+   const int64_t  f0;
+   volatile int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U2 g_2 = {9L};/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
