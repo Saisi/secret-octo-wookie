994f6bc94df515f811f0ff7e8e5f5e754c403dda
saisi <saisi@users.noreply.github.com>
2017-05-15 00:18:52 +0000

    1494807531 baetuli

---
+++ 1494807531.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4289655804
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int64_t  f1;
+   signed f2 : 15;
+};
+
+union U1 {
+   signed f0 : 10;
+};
+
