967707efa39e8f6843115a832c0b425221e46111
saisi <saisi@users.noreply.github.com>
2017-05-15 14:50:04 +0000

    1494859803 Australiana

---
+++ 1494859803.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1586519794
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_18 = 0xF0842170L;
+static int64_t g_43[5] = {0x919C44D4E2B79DC8LL,0x919C44D4E2B79DC8LL,0x919C44D4E2B79DC8LL,0x919C44D4E2B79DC8LL,0x919C44D4E2B79DC8LL};
+static int32_t g_95[10][3][1] = {{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}},{{0x042358CEL},{0x042358CEL},{0x042358CEL}}};
+static uint32_t g_99 = 1UL;
+static int64_t *g_106 = &g_43[0];
+static int8_t g_126 = 0x1AL;
+static int8_t g_132 = 0xD0L;
+static int8_t *g_135 = &g_132;
+static const int32_t *g_145 = &g_95[8][1][0];
