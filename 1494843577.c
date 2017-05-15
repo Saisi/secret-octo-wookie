5ce066595ed2456289e70e327ac28aae69a30fdd
saisi <saisi@users.noreply.github.com>
2017-05-15 10:19:38 +0000

    1494843577 architect

---
+++ 1494843577.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1950582281
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_3 = 0x56915A13L;
+static int8_t g_15 = 0x9AL;
+static int32_t g_19 = 0x44401D1BL;
+static uint32_t g_26 = 4294967291UL;
+static volatile uint16_t g_57 = 65530UL;/* VOLATILE GLOBAL g_57 */
+static volatile int32_t g_67 = 0x48320013L;/* VOLATILE GLOBAL g_67 */
+static int32_t g_68 = 0L;
+static int8_t g_77 = 0x8FL;
+static uint64_t g_93[5] = {18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL,18446744073709551615UL};
