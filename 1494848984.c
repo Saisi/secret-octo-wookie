25873d60c350ac8d7297d81dbc0cc4965fa04fe8
saisi <saisi@users.noreply.github.com>
2017-05-15 11:49:45 +0000

    1494848984 autopilot

---
+++ 1494848984.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3144357605
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-10L);
+static int16_t g_13 = (-1L);
+static int16_t g_15 = 0xB21AL;
+static int16_t g_22[2] = {0xD0B0L,0xD0B0L};
+static int32_t g_24[9] = {1L,1L,1L,1L,1L,1L,1L,1L,1L};
+static volatile int32_t g_39 = (-4L);/* VOLATILE GLOBAL g_39 */
+static int32_t g_40 = 0xD5C0F4FDL;
