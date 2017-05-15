9549e83cc900cfa56b238e3305476785c80c5736
saisi <saisi@users.noreply.github.com>
2017-05-15 17:50:20 +0000

    1494870620 agglomerant

---
+++ 1494870620.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3094531550
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const int16_t  f1;
+   const uint8_t  f2;
+   unsigned f3 : 20;
+   const int8_t  f4;
+   uint8_t  f5;
+   const unsigned f6 : 7;
+   const volatile int16_t  f7;
+   int64_t  f8;
