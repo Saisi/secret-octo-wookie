5b2f244409a0dcf8b24c791dccd9e3264d6e3476
saisi <saisi@users.noreply.github.com>
2017-05-15 20:50:33 +0000

    1494881432 antitheism

---
+++ 1494881432.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2591379716
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_6 = 0xA9A971867B8F5F49LL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_42[8][8] = {{0x87FD074DL,9L,0xF58CDC14L,0x87FD074DL,(-8L),0xAB8A87EAL,(-1L),0x38D679BDL},{9L,(-1L),0xF58CDC14L,(-10L),0xC57511ECL,0xC57511ECL,(-10L),0xF58CDC14L},{(-8L),(-8L),0xC9BA5B04L,(-1L),0xFC72FA5DL,0x419C2B2EL,(-1L),(-8L)},{(-1L),9L,(-2L),0xC57511ECL,(-1L),(-2L),0xF58CDC14L,(-8L)},{9L,0x87FD074DL,(-1L),(-1L),(-1L),0x87FD074DL,9L,0xF58CDC14L},{0x38D679BDL,(-1L),0x5ADB51FDL,(-10L),(-1L),0x90638325L,(-1L),0xF58CDC14L},{0x5ADB51FDL,0xF58CDC14L,0x720725FCL,(-1L),(-2L),0xC9BA5B04L,0xC9BA5B04L,(-2L)},{0xF58CDC14L,0x419C2B2EL,0x419C2B2EL,0xF58CDC14L,0x86BDAE89L,0x5ADB51FDL,1L,(-1L)}};
+static int32_t g_43 = (-1L);
+static int32_t g_44 = 0x766593C3L;
