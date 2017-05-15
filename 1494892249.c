ac5bcf5254d8f970af3191ff4c9ead348fa44efa
saisi <saisi@users.noreply.github.com>
2017-05-15 23:50:49 +0000

    1494892249 acutonodose

---
+++ 1494892249.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3306901818
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-8L);
+static uint32_t g_10 = 0xFF609D70L;
+static uint32_t g_50 = 0xE719C29AL;
+static uint32_t *g_49 = &g_50;
+static int64_t g_62 = 0xC71F8B108A6652F5LL;
+static int64_t g_64 = 1L;
+static int32_t g_70 = 0L;
+static const int32_t g_107 = 4L;
+static const int32_t *g_106[9] = {&g_107,&g_107,&g_107,&g_107,&g_107,&g_107,&g_107,&g_107,&g_107};
