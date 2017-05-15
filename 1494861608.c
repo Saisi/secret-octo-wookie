325d02e4c28af40dbbd4dfc5039773225acec088
saisi <saisi@users.noreply.github.com>
2017-05-15 15:20:08 +0000

    1494861608 antisensuality

---
+++ 1494861608.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4267155733
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int64_t  f0;
+   const int8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int16_t g_2 = 1L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0x31E42A43L;
+static volatile int32_t g_7[4] = {(-1L),(-1L),(-1L),(-1L)};
+static volatile int32_t g_8 = 0x80D416DBL;/* VOLATILE GLOBAL g_8 */
