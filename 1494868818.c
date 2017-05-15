e77625385dca6bfb8f61f93b3d16592efd738e1f
saisi <saisi@users.noreply.github.com>
2017-05-15 17:20:18 +0000

    1494868818 antipestilence

---
+++ 1494868818.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2570060725
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_2[2] = {0xE83C7730L,0xE83C7730L};
+static int32_t g_4 = (-1L);
+static uint16_t g_10 = 5UL;
+static int16_t g_35 = 0L;
+static volatile uint16_t g_44 = 0x4B71L;/* VOLATILE GLOBAL g_44 */
+static volatile uint16_t *g_43 = &g_44;
+static uint8_t g_66[3][9] = {{0xE7L,0UL,0UL,0xE7L,248UL,0xE7L,0UL,0UL,0xE7L},{255UL,0x57L,0x8EL,0x57L,255UL,255UL,0x57L,0x8EL,0x57L},{0UL,248UL,0xD8L,0xD8L,248UL,0UL,248UL,0xD8L,0xD8L}};
+static uint16_t g_72[4][5] = {{65530UL,65535UL,65535UL,65530UL,65535UL},{65530UL,65530UL,0xDD00L,65530UL,65530UL},{65535UL,65530UL,65535UL,65535UL,65530UL},{65530UL,65535UL,0xDD00L,65535UL,0xDD00L}};
+static uint8_t g_80 = 0x2BL;
