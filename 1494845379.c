d97913e6f219707e2ab175de49aa12adc26cddba
saisi <saisi@users.noreply.github.com>
2017-05-15 10:49:40 +0000

    1494845379 archarchitect

---
+++ 1494845379.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2038957833
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 14;
+   signed f2 : 25;
+   const signed : 0;
+   unsigned f3 : 17;
+   unsigned f4 : 6;
+   volatile signed f5 : 13;
+   const unsigned f6 : 12;
+   signed f7 : 31;
