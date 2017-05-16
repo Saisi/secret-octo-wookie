1880baf54afbf3bf39bd0552f44b38b6c8096998
saisi <saisi@users.noreply.github.com>
2017-05-16 07:51:28 +0000

    1494921088 antilegalist

---
+++ 1494921088.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2793819277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   uint64_t  f1;
+   const int64_t  f2;
+};
+
+union U1 {
+   volatile int16_t  f0;
+   int8_t  f1;
+   int32_t  f2;
