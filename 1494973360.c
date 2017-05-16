58b1670d2adee08737eca20405e7c1d0c3643d33
saisi <saisi@users.noreply.github.com>
2017-05-16 22:22:40 +0000

    1494973360 anticonservative

---
+++ 1494973360.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3546120174
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 5;
+   unsigned f1 : 6;
+   unsigned f2 : 16;
+   unsigned f3 : 28;
+   volatile signed f4 : 27;
+   volatile unsigned f5 : 6;
+   unsigned f6 : 12;
