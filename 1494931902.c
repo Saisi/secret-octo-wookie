aa83debeceabb201b09f09e06f62d2083fa2cfb3
saisi <saisi@users.noreply.github.com>
2017-05-16 10:51:42 +0000

    1494931902 antidotism

---
+++ 1494931902.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2534626860
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 4;
+   unsigned f1 : 9;
+   signed f2 : 7;
+};
+
+struct S1 {
+   struct S0  f0;
+   uint64_t  f1;
+   volatile int8_t  f2;
