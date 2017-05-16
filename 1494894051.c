4373b4cf881363e9118c91dca9736d2200f55182
saisi <saisi@users.noreply.github.com>
2017-05-16 00:20:52 +0000

    1494894051 anhydrite

---
+++ 1494894051.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3568868353
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   const uint32_t  f1;
+   uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_2 = 1L;
+static int8_t g_15 = 6L;
+static const int8_t g_75 = (-1L);
