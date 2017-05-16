d02f3679050039ae7cc14089f1ada1713bca4853
saisi <saisi@users.noreply.github.com>
2017-05-16 04:51:14 +0000

    1494910274 bannut

---
+++ 1494910274.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4269172256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_24[6][7] = {{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L},{1L,1L,1L,1L,1L,1L,1L}};
+static int16_t g_26 = 0x9491L;
+static int32_t g_45 = (-3L);
+static uint8_t g_47 = 0xFBL;
+static int16_t g_54 = (-7L);
+static int16_t *g_53[5] = {&g_54,&g_54,&g_54,&g_54,&g_54};
+static uint32_t g_77 = 4294967295UL;
+static int16_t g_93[10] = {0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L,0x3B14L};
+static uint8_t g_110[10] = {0xACL,255UL,255UL,0xACL,255UL,0xACL,255UL,255UL,0xACL,255UL};
