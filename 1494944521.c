de2a64a07a6e4221c551ca0cb123783929abfb6a
saisi <saisi@users.noreply.github.com>
2017-05-16 14:22:02 +0000

    1494944521 adradially

---
+++ 1494944521.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1161865513
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_8 = 1UL;
+static int32_t g_68 = 0xC30F106BL;
+static int32_t *g_76[10] = {&g_68,&g_68,&g_68,&g_68,&g_68,&g_68,&g_68,&g_68,&g_68,&g_68};
+static int32_t **g_75 = &g_76[5];
+static int32_t g_80 = 0x045D781FL;
+static int8_t g_93 = (-1L);
+static volatile uint32_t g_98 = 0x479C51A5L;/* VOLATILE GLOBAL g_98 */
+static int32_t g_100[2] = {0L,0L};
+static uint8_t g_114 = 1UL;
