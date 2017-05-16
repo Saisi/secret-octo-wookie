4b9fb84385c509ec845bef6e7ba9be3f09594a0c
saisi <saisi@users.noreply.github.com>
2017-05-16 18:22:22 +0000

    1494958941 aeroscopic

---
+++ 1494958941.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3758153063
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x13B91020L;
+static int32_t * volatile *g_9 = (void*)0;
+static int16_t g_19 = 1L;
+static int32_t * const g_35 = &g_2;
+static int32_t * const *g_34 = &g_35;
+static int8_t g_62 = 0x2BL;
+static int32_t g_64 = 7L;
+static int16_t g_78 = (-1L);
+static int16_t *g_77 = &g_78;
