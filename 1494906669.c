45448c00e71311d97a8b0c8159feca1192ab723a
saisi <saisi@users.noreply.github.com>
2017-05-16 03:51:10 +0000

    1494906669 axofugal

---
+++ 1494906669.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2184722166
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_5 = 1L;
+static int16_t g_7 = (-7L);
+static int8_t g_16 = 0x6CL;
+static const int16_t *g_17 = (void*)0;
+static uint8_t g_34 = 0xDEL;
+static uint8_t g_62 = 0xC6L;
+static int32_t g_66[1][10] = {{0x0461302DL,0x18C6A866L,0x0461302DL,0x0461302DL,0x18C6A866L,0x0461302DL,0x0461302DL,0x18C6A866L,0x0461302DL,0x0461302DL}};
+static int16_t g_70 = 0xE883L;
+static int16_t *g_69 = &g_70;
