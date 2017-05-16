f9b256845885e90b534be36a32ee439f56fb1d86
saisi <saisi@users.noreply.github.com>
2017-05-16 23:22:45 +0000

    1494976964 Anguillulidae

---
+++ 1494976964.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      575765692
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 12;
+   int8_t  f1;
+   signed f2 : 9;
+   const volatile uint16_t  f3;
+   const volatile uint32_t  f4;
+   uint32_t  f5;
+   const uint16_t  f6;
