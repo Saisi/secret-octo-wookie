818582e3bbceda73dc34afe56931e229cd04e8b8
saisi <saisi@users.noreply.github.com>
2017-05-16 01:20:56 +0000

    1494897656 acanthial

---
+++ 1494897656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      977773991
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 18;
+   int8_t  f1;
+   signed f2 : 17;
+   signed f3 : 17;
+   volatile unsigned f4 : 20;
+   volatile signed f5 : 18;
+   uint64_t  f6;
+};
+
