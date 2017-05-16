16ce96d4bf5f618193f6cad6b58523eb4ede9681
saisi <saisi@users.noreply.github.com>
2017-05-16 21:22:35 +0000

    1494969755 arcform

---
+++ 1494969755.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1660636528
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   const volatile uint32_t  f1;
+   volatile signed f2 : 30;
+   uint32_t  f3;
+   const int32_t  f4;
+   const int32_t  f5;
+   const volatile int32_t  f6;
+   const uint32_t  f7;
+   volatile uint32_t  f8;
