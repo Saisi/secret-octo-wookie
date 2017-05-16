0e074f0a7d618cb0fc4c5c91924c7c1343f5a485
saisi <saisi@users.noreply.github.com>
2017-05-16 09:21:36 +0000

    1494926495 astromancer

---
+++ 1494926495.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1976638215
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   const volatile int16_t  f1;
+   volatile int8_t * f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x00D4E793L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 4L;
