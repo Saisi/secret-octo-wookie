0d83800fc88dc2ffffa1677cf775b8a3d5699813
saisi <saisi@users.noreply.github.com>
2017-05-16 06:21:21 +0000

    1494915680 amniorrhea

---
+++ 1494915680.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3583797296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   volatile int32_t  f1;
+   volatile uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 0x54C3C69AED23B618LL;/* VOLATILE GLOBAL g_2 */
+static uint64_t g_19 = 1UL;
+static uint64_t g_53 = 18446744073709551614UL;
