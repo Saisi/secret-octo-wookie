7efe13667111d569d7c96d1422d323ab95b31954
saisi <saisi@users.noreply.github.com>
2017-05-16 19:22:26 +0000

    1494962546 achroite

---
+++ 1494962546.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      113429064
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 0x88999D94L;
+static int32_t g_17 = 0x26EC17E5L;
+static int32_t * const  volatile g_16[5][1] = {{&g_17},{&g_17},{&g_17},{&g_17},{&g_17}};
+static int32_t * volatile g_18 = &g_17;/* VOLATILE GLOBAL g_18 */
+static uint16_t g_72 = 65531UL;
+static int32_t *g_77 = &g_17;
+static int32_t ** volatile g_76[9][7] = {{&g_77,&g_77,&g_77,&g_77,&g_77,(void*)0,&g_77},{(void*)0,&g_77,&g_77,&g_77,&g_77,(void*)0,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,&g_77,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,(void*)0,&g_77},{(void*)0,&g_77,&g_77,&g_77,&g_77,(void*)0,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,&g_77,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,&g_77,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,&g_77,&g_77},{&g_77,&g_77,&g_77,&g_77,&g_77,(void*)0,&g_77}};
+static int32_t ** volatile g_78 = &g_77;/* VOLATILE GLOBAL g_78 */
+static uint32_t g_80[9] = {0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L,0xEEEC8559L};
