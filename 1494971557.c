0759e3d4a7a82e2ae71f07e88a7a097aff38594b
saisi <saisi@users.noreply.github.com>
2017-05-16 21:52:38 +0000

    1494971557 anticorrosive

---
+++ 1494971557.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2776325987
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0L;
+static int32_t g_5 = (-8L);
+static int64_t g_51 = 0x3019C36C1D66BD5DLL;
+static int64_t g_53 = 0x8718302CCA7A22F0LL;
+static int32_t g_71 = 0x23A55A41L;
+static int32_t * volatile g_70 = &g_71;/* VOLATILE GLOBAL g_70 */
+static int32_t g_91 = 0xE514D861L;
+static int8_t g_98[8] = {0xDAL,0xDAL,0xDAL,0xDAL,0xDAL,0xDAL,0xDAL,0xDAL};
+static uint16_t g_105 = 0x3A51L;
