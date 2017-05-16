92ad4bec16bbcb24201dffd4866256e12df0c585
saisi <saisi@users.noreply.github.com>
2017-05-16 19:52:28 +0000

    1494964348 adaptometer

---
+++ 1494964348.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      63462175
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 14;
+   const uint32_t  f1;
+   uint32_t  f2;
+   signed f3 : 23;
+   int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x35F394A2L;
