c225a11783b490e004210821da9f17444609df14
saisi <saisi@users.noreply.github.com>
2017-05-17 06:53:23 +0000

    1495004003 abditory

---
+++ 1495004003.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4273532782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   uint64_t  f1;
+   uint16_t  f2;
+   int32_t  f3;
+   volatile uint16_t  f4;
+   int8_t  f5;
+};
+
+#pragma pack(push)
