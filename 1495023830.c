a5cb2c1604690cb9c0b0dfb407ebf01c699d6f7d
saisi <saisi@users.noreply.github.com>
2017-05-17 12:23:50 +0000

    1495023830 anergic

---
+++ 1495023830.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      523401594
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   uint32_t  f1;
+};
+
+struct S1 {
+   const volatile int64_t  f0;
+   int32_t  f1;
+   const volatile signed f2 : 1;
+   const uint64_t  f3;
