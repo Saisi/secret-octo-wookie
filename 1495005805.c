196c5efb2a8ddf6d190b8453c4e39d1ae63c26ce
saisi <saisi@users.noreply.github.com>
2017-05-17 07:23:25 +0000

    1495005805 bandaite

---
+++ 1495005805.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      202552888
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFD41E559L;
+static volatile uint64_t g_6 = 0xEB64D07DDBF4DF7BLL;/* VOLATILE GLOBAL g_6 */
+static uint32_t g_46 = 0x147C038DL;
+static uint64_t g_91 = 0xCBFA339BCA7AF0C8LL;
+static int8_t g_93 = 1L;
+static uint8_t g_101[1] = {253UL};
+static int32_t g_108[8] = {0x054D2AC3L,0x358B7845L,0x054D2AC3L,0x358B7845L,0x054D2AC3L,0x358B7845L,0x054D2AC3L,0x358B7845L};
+static volatile int64_t g_109 = (-1L);/* VOLATILE GLOBAL g_109 */
+static volatile int32_t g_115 = 0xBBE26AF1L;/* VOLATILE GLOBAL g_115 */
