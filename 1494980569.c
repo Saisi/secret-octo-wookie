f72b85b57dd3f19d7f631bed1f78218021885925
saisi <saisi@users.noreply.github.com>
2017-05-17 00:22:49 +0000

    1494980569 amblotic

---
+++ 1494980569.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1553583531
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 20;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[10] = {(-1L),0x6432A276L,(-1L),(-1L),0x6432A276L,(-1L),(-1L),0x6432A276L,(-1L),(-1L)};
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
