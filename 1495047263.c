3f2958877cb362583920c609d3c2e0a0143d58e7
saisi <saisi@users.noreply.github.com>
2017-05-17 18:54:23 +0000

    1495047263 achar

---
+++ 1495047263.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1593360967
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 2;
+   signed f1 : 17;
+   const volatile signed f2 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 2L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x37D8E6E8L;
