692b0866c49dd9d14fc30cdb9e6430eb03326047
saisi <saisi@users.noreply.github.com>
2017-05-17 17:24:17 +0000

    1495041856 auditioning

---
+++ 1495041856.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      832414882
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = (-2L);
+static volatile int32_t g_13 = 0x8B78ACC4L;/* VOLATILE GLOBAL g_13 */
+static volatile int32_t g_14[3] = {(-10L),(-10L),(-10L)};
+static volatile int32_t g_15 = 0x14A5EA96L;/* VOLATILE GLOBAL g_15 */
+static int32_t g_16 = 0L;
+static int8_t g_42 = (-2L);
+static int32_t g_53 = 0xAC12ABB1L;
+static int32_t g_63 = (-1L);
+static int64_t g_70 = 4L;
