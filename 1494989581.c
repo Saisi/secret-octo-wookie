fef31a9d06c0bada8e3139e66adc0a28e4830388
saisi <saisi@users.noreply.github.com>
2017-05-17 02:53:02 +0000

    1494989581 aligners

---
+++ 1494989581.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2111328208
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 22;
+   unsigned f1 : 19;
+   unsigned f2 : 9;
+   signed f3 : 17;
+   const unsigned f4 : 28;
+   const signed f5 : 3;
+   volatile signed f6 : 21;
+   const signed f7 : 22;
+   volatile unsigned f8 : 2;
