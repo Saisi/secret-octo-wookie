3242ddbb58a0d5727a0f9d1ed29737f80beabdeb
saisi <saisi@users.noreply.github.com>
2017-05-17 14:54:03 +0000

    1495032843 atropisms

---
+++ 1495032843.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2395248307
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_24 = 0xC32B96CEL;
+static uint64_t g_30 = 0UL;
+static uint8_t g_42 = 0x07L;
+static uint32_t g_46 = 0x63F6D1D8L;
+static int8_t g_58 = 1L;
+static int8_t g_69 = 0L;
+static int32_t g_78 = (-9L);
+static volatile int8_t g_88[3] = {6L,6L,6L};
+static volatile int32_t g_89 = 0xF7348F3BL;/* VOLATILE GLOBAL g_89 */
