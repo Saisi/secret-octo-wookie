a31dc813dc732b5b7e53ce67faf8fb619d64605d
saisi <saisi@users.noreply.github.com>
2017-05-17 21:54:40 +0000

    1495058079 aggressions

---
+++ 1495058079.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2027433914
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint64_t  f0;
+   int64_t  f1;
+   volatile uint64_t  f2;
+   volatile int32_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
