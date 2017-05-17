edbfa3328e7bf80035ebef0abb8412b9b66e3228
saisi <saisi@users.noreply.github.com>
2017-05-17 05:23:16 +0000

    1494998596 angelico

---
+++ 1494998596.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3141080095
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   const int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_12 = 0x4AD72D76L;
+static union U0 g_14 = {0xA4BEDCC590F02349LL};
+static union U0 g_18 = {5L};
+static union U0 *g_17 = &g_18;
