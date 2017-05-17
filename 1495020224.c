f815658ff69be3987b2c8ad6a1275f27af55c1cb
saisi <saisi@users.noreply.github.com>
2017-05-17 11:23:46 +0000

    1495020224 aitchpiece

---
+++ 1495020224.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      647422556
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 28;
+   uint16_t  f1;
+   uint32_t  f2;
+   const int32_t  f3;
+   const signed f4 : 16;
+   unsigned f5 : 7;
+   int8_t  f6;
+   uint64_t  f7;
+   signed f8 : 20;
