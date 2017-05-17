f8fa60018faf6eb0238c70784538c4ed7c7aa652
saisi <saisi@users.noreply.github.com>
2017-05-17 16:24:12 +0000

    1495038251 asynaptic

---
+++ 1495038251.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3614309541
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   const volatile int32_t  f1;
+   const volatile uint32_t  f2;
+   const uint8_t  f3;
+   const signed : 0;
+   uint16_t  f4;
+   uint32_t  f5;
+   uint64_t  f6;
+};
