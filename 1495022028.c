f4533645a05396add61b9e9ec3f34634c3746079
saisi <saisi@users.noreply.github.com>
2017-05-17 11:53:48 +0000

    1495022028 anencephalus

---
+++ 1495022028.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      420502724
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 16;
+   uint64_t  f1;
+   unsigned f2 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xDB505206L;
+static int32_t g_6 = (-10L);
+static int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
