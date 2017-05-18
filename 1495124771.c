92699ff5c7c196891ab105346e5fcd7bd87e8c50
saisi <saisi@users.noreply.github.com>
2017-05-18 16:26:11 +0000

    1495124771 agileness

---
+++ 1495124771.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      543154057
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xA0256915L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 3L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xD386CCE5L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = (-6L);
+static volatile int32_t g_9 = 1L;/* VOLATILE GLOBAL g_9 */
+static int32_t g_10 = 0x2276B87DL;
+static int32_t g_47 = 1L;
+static int64_t g_62 = 9L;
+static int64_t g_63 = 1L;
