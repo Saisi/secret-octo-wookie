17f1dd1b7d3c1d320c5286d0cadeb2a3d32a26c5
saisi <saisi@users.noreply.github.com>
2017-05-18 02:55:01 +0000

    1495076101 anisotrope

---
+++ 1495076101.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3417522747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile int32_t  f0;
+   volatile signed f1 : 1;
+   volatile int8_t  f2;
+};
+#pragma pack(pop)
+
+union U1 {
