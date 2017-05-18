1eff6e86fb76a7f3cfb72f9c26ab4e93d8226732
saisi <saisi@users.noreply.github.com>
2017-05-18 13:55:57 +0000

    1495115757 absorptance

---
+++ 1495115757.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      664331470
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 21;
+   unsigned f1 : 22;
+   signed : 0;
+   const volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[9] = {0L,0L,0L,0L,0L,0L,0L,0L,0L};
+static volatile int32_t g_3 = 0x18D58162L;/* VOLATILE GLOBAL g_3 */
