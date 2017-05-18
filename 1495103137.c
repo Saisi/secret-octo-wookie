5e77f7be5fc39bf06c3d2c09937566ebf6469bee
saisi <saisi@users.noreply.github.com>
2017-05-18 10:25:38 +0000

    1495103137 Axa

---
+++ 1495103137.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      941134257
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_2 = 0x58E4L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0L;
+static int32_t g_8 = 0x4438BEA1L;
+static volatile int16_t g_12 = 0x8B08L;/* VOLATILE GLOBAL g_12 */
+static volatile uint16_t g_14 = 0UL;/* VOLATILE GLOBAL g_14 */
+static uint64_t g_27 = 0x68DD2AC30BCA0BECLL;
+static int16_t g_30 = 0x31CDL;
+static int32_t *g_34 = &g_8;
+static int32_t ** volatile g_33 = &g_34;/* VOLATILE GLOBAL g_33 */
