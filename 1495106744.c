7eb4a50ee85c5b3e967db9c7d98722f8361e9277
saisi <saisi@users.noreply.github.com>
2017-05-18 11:25:45 +0000

    1495106744 attorneyism

---
+++ 1495106744.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3082619086
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 21;
+   unsigned f1 : 22;
+   unsigned : 0;
+   signed f2 : 12;
+   signed f3 : 11;
+   unsigned f4 : 22;
+   signed f5 : 28;
