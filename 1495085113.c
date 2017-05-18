9d925cabf2a9777cd134afa6d0e48d81c4f5b642
saisi <saisi@users.noreply.github.com>
2017-05-18 05:25:13 +0000

    1495085113 backhatch

---
+++ 1495085113.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3849976468
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int16_t  f0;
+   int32_t  f1;
+   signed f2 : 27;
+   volatile uint32_t  f3;
+   const volatile int16_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
