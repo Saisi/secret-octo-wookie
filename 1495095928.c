064a493a5ca76abc9b39f1f92df508a59c9f30cd
saisi <saisi@users.noreply.github.com>
2017-05-18 08:25:28 +0000

    1495095928 bankroller

---
+++ 1495095928.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3538333500
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   const int64_t  f1;
+   volatile uint64_t  f2;
+   volatile int64_t  f3;
+   uint8_t  f4;
+   uint16_t  f5;
+   int32_t  f6;
+   volatile unsigned f7 : 13;
+   int32_t  f8;
