94d324235b293b9b1f843b8b43d48f8917df86f4
saisi <saisi@users.noreply.github.com>
2017-05-18 02:24:59 +0000

    1495074299 axiomatizing

---
+++ 1495074299.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3943941266
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_2 = 0xA4D0L;
+static uint32_t g_5 = 0xA66E58D0L;
+static int32_t g_8 = 0x9141A9C2L;
+static volatile uint32_t g_17[7] = {4294967288UL,5UL,4294967288UL,4294967288UL,5UL,4294967288UL,4294967288UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
