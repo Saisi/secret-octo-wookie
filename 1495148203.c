1115491a7a80ba2408ff4c4c88277f1a7a84d324
saisi <saisi@users.noreply.github.com>
2017-05-18 22:56:44 +0000

    1495148203 amygdalotome

---
+++ 1495148203.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      659476451
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_18 = 0L;/* VOLATILE GLOBAL g_18 */
+static uint8_t g_20[1][9][6] = {{{250UL,0x19L,0x89L,0x89L,0x19L,250UL},{8UL,250UL,0x89L,250UL,8UL,8UL},{255UL,250UL,250UL,255UL,0x19L,255UL},{255UL,0x19L,255UL,250UL,250UL,255UL},{8UL,8UL,250UL,0x89L,250UL,8UL},{250UL,0x19L,0x89L,0x89L,0x19L,250UL},{8UL,250UL,0x89L,250UL,8UL,8UL},{255UL,250UL,250UL,255UL,8UL,0x89L},{0x89L,8UL,0x89L,255UL,255UL,0x89L}}};
+static int16_t g_46 = (-2L);
+static int32_t g_54 = (-1L);
+static uint64_t g_88 = 0x0730A04A8F9B1095LL;
+static uint64_t g_92 = 3UL;
+static uint16_t g_109 = 0x52FFL;
+static uint32_t g_130 = 9UL;
+static int32_t g_131 = 0x2C1609A6L;
