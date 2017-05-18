ffe69a64918e3628163ae48489985f8c22df9aee
saisi <saisi@users.noreply.github.com>
2017-05-18 14:56:02 +0000

    1495119361 aubin

---
+++ 1495119361.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1499357187
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile unsigned f0 : 25;
+};
+
+union U1 {
+   const int64_t  f0;
+};
+
+union U2 {
+   const int16_t  f0;
