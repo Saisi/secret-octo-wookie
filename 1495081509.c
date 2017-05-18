f8cade73355369c2fb5cb56d9324619103cba86a
saisi <saisi@users.noreply.github.com>
2017-05-18 04:25:09 +0000

    1495081509 Aristophanes

---
+++ 1495081509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3670029182
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   int64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_12 = 0xDCL;
+static int32_t g_22 = 0xB79AE4F7L;
+static int32_t *g_21 = &g_22;
+static int32_t g_25[5] = {0L,0L,0L,0L,0L};
