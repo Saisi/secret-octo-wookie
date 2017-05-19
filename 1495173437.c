99301e5689fbe93fee91bfd0103477e14d3c3e51
saisi <saisi@users.noreply.github.com>
2017-05-19 05:57:17 +0000

    1495173437 anthropophagi

---
+++ 1495173437.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1122140636
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = (-4L);
+static volatile uint16_t g_4 = 65535UL;/* VOLATILE GLOBAL g_4 */
+static int32_t *g_11 = (void*)0;
+static int32_t ** volatile g_10[4][5][6] = {{{(void*)0,&g_11,&g_11,&g_11,&g_11,(void*)0},{&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,(void*)0,&g_11}},{{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,(void*)0,&g_11,(void*)0,&g_11,(void*)0},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,&g_11,(void*)0,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,(void*)0,&g_11}},{{&g_11,&g_11,(void*)0,&g_11,&g_11,(void*)0},{&g_11,&g_11,(void*)0,(void*)0,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,(void*)0,(void*)0,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}},{{&g_11,(void*)0,&g_11,&g_11,&g_11,&g_11},{&g_11,(void*)0,&g_11,(void*)0,(void*)0,&g_11},{&g_11,&g_11,(void*)0,&g_11,&g_11,(void*)0},{(void*)0,&g_11,(void*)0,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,(void*)0,&g_11,&g_11}}};
+static int32_t ** volatile g_12[9][2][8] = {{{(void*)0,&g_11,&g_11,&g_11,&g_11,(void*)0,(void*)0,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0}},{{&g_11,(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,(void*)0,(void*)0,&g_11,(void*)0,(void*)0,&g_11,(void*)0}},{{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}},{{&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,(void*)0,&g_11}},{{&g_11,&g_11,&g_11,(void*)0,&g_11,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,(void*)0}},{{(void*)0,(void*)0,(void*)0,(void*)0,&g_11,&g_11,&g_11,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}},{{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11},{&g_11,(void*)0,(void*)0,&g_11,(void*)0,&g_11,&g_11,&g_11}},{{(void*)0,&g_11,(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11},{(void*)0,&g_11,&g_11,&g_11,&g_11,&g_11,(void*)0,&g_11}},{{&g_11,&g_11,&g_11,(void*)0,&g_11,&g_11,(void*)0,&g_11},{&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11,&g_11}}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
