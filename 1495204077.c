c9cc8327f40e00fd477863ff50cef7b9633bd173
saisi <saisi@users.noreply.github.com>
2017-05-19 14:27:57 +0000

    1495204077 archeol

---
+++ 1495204077.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3535156079
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 0xAF425F5FL;
+static volatile uint32_t g_18 = 0x5A63A74AL;/* VOLATILE GLOBAL g_18 */
+static uint64_t g_24 = 0UL;
+static int32_t g_30 = 0xBE74FD57L;
+static int32_t *g_29 = &g_30;
+static int64_t g_39[3][3] = {{4L,4L,4L},{0xD14B4322E7C650BALL,0xD14B4322E7C650BALL,0xD14B4322E7C650BALL},{4L,4L,4L}};
+static int32_t g_49 = (-4L);
+static const int32_t g_80 = 9L;
+static const int32_t *g_79 = &g_80;
