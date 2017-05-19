ab63765415c59b662a4e7ba3362b65c527cb9d6e
saisi <saisi@users.noreply.github.com>
2017-05-19 10:27:36 +0000

    1495189656 actus

---
+++ 1495189656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1880505560
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint64_t g_2 = 7UL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_4 = 0x6960F57AL;
+static int32_t *g_51 = (void*)0;
+static int32_t g_57 = 0xFF74F57BL;
+static int32_t g_83 = (-10L);
+static int32_t g_87 = 0x4FF24264L;
+static int32_t g_88[8] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static uint8_t g_90 = 0xADL;
+static int32_t * const g_101[6][9][4] = {{{&g_57,&g_4,(void*)0,&g_57},{&g_4,(void*)0,&g_4,&g_57},{&g_83,&g_4,(void*)0,&g_4},{(void*)0,&g_57,&g_4,&g_83},{&g_57,(void*)0,&g_57,&g_57},{(void*)0,&g_83,&g_57,&g_83},{(void*)0,&g_4,&g_57,(void*)0},{&g_57,&g_83,&g_4,&g_83},{(void*)0,&g_83,(void*)0,&g_83}},{{&g_83,&g_4,&g_4,(void*)0},{&g_4,&g_57,&g_57,&g_83},{&g_4,&g_83,(void*)0,&g_57},{&g_57,(void*)0,&g_83,&g_57},{&g_4,&g_83,(void*)0,&g_83},{&g_4,&g_57,&g_83,(void*)0},{&g_57,&g_4,&g_83,&g_4},{&g_4,(void*)0,&g_4,(void*)0},{&g_83,&g_57,(void*)0,&g_57}},{{&g_57,&g_4,(void*)0,&g_57},{&g_4,&g_4,(void*)0,&g_83},{&g_57,&g_57,(void*)0,&g_83},{&g_83,(void*)0,&g_4,&g_57},{&g_4,&g_57,&g_83,&g_83},{&g_57,&g_57,&g_83,&g_4},{&g_4,&g_4,(void*)0,&g_57},{&g_4,&g_4,&g_83,(void*)0},{&g_57,&g_4,(void*)0,&g_57}},{{&g_4,&g_4,&g_57,&g_4},{&g_4,&g_57,&g_4,&g_83},{(void*)0,&g_57,(void*)0,&g_57},{&g_57,(void*)0,&g_4,&g_83},{&g_4,&g_57,(void*)0,&g_83},{&g_57,&g_4,&g_83,&g_57},{&g_57,&g_4,(void*)0,&g_57},{&g_4,&g_57,&g_4,(void*)0},{&g_57,(void*)0,(void*)0,&g_4}},{{(void*)0,&g_4,&g_4,(void*)0},{&g_4,&g_57,&g_57,&g_83},{&g_4,&g_83,(void*)0,&g_57},{&g_57,(void*)0,&g_83,&g_57},{&g_4,&g_83,(void*)0,&g_83},{&g_4,&g_57,&g_83,(void*)0},{&g_57,&g_4,&g_83,&g_4},{&g_4,(void*)0,&g_4,(void*)0},{&g_83,&g_57,(void*)0,&g_57}},{{&g_57,&g_4,(void*)0,&g_57},{&g_4,&g_4,(void*)0,&g_83},{&g_57,&g_57,(void*)0,&g_83},{&g_83,(void*)0,&g_4,&g_57},{&g_4,&g_57,&g_83,&g_83},{&g_57,&g_57,&g_83,&g_4},{&g_4,&g_4,(void*)0,&g_57},{&g_4,&g_4,&g_83,(void*)0},{&g_57,&g_4,(void*)0,&g_57}}};
