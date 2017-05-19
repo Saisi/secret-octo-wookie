0c8806ba0eaaf8b700c5b2301d96194e87374ad0
saisi <saisi@users.noreply.github.com>
2017-05-19 22:28:34 +0000

    1495232914 acidophilic

---
+++ 1495232914.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3335215819
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_12 = 0xE5E81A7ECC2443ACLL;
+static int32_t g_37 = 0xCE34AAFBL;
+static int16_t g_43[5][6] = {{0x321AL,0L,0L,0x321AL,0x321AL,0L},{0x321AL,0x321AL,0L,0L,0x321AL,0x321AL},{0x321AL,0L,0L,0x321AL,0x321AL,0L},{0x321AL,0x321AL,0L,0L,0x321AL,0x321AL},{0x321AL,0L,0L,0x321AL,0x321AL,0L}};
+static int64_t g_70[8] = {0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL,0x8D550032DC71837DLL};
