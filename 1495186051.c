7c1cae0972f213e4560ff675c973f6cd47f7f801
saisi <saisi@users.noreply.github.com>
2017-05-19 09:27:32 +0000

    1495186051 autobus

---
+++ 1495186051.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4270021942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   const volatile unsigned f1 : 6;
+   signed f2 : 2;
+   signed f3 : 28;
+   unsigned f4 : 17;
+   signed f5 : 25;
+   const unsigned f6 : 22;
+   signed f7 : 23;
+};
