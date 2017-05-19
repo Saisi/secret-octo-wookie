49f9d25df75536459d0e2fa86103640a1305bb82
saisi <saisi@users.noreply.github.com>
2017-05-19 15:28:02 +0000

    1495207682 alluviation

---
+++ 1495207682.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      405131700
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   volatile uint64_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[4][1][10] = {{{2L,(-1L),0x0EF307D7L,0x8D3C891CL,0xE97EEDE0L,(-8L),0x15D7249AL,0x320E32CFL,0x320E32CFL,0x15D7249AL}},{{(-4L),(-1L),0x15D7249AL,0x15D7249AL,(-1L),(-4L),0x4074DD03L,0x534BF8F0L,0x1188E17BL,(-8L)}},{{0x534BF8F0L,0xFBF2121FL,0L,(-4L),0x8D3C891CL,1L,0xE97EEDE0L,0x0EF307D7L,0xE97EEDE0L,1L}},{{0x534BF8F0L,0x320E32CFL,2L,0x320E32CFL,0x534BF8F0L,(-4L),1L,0L,0x48C66F5AL,1L}}};
+static uint64_t g_13 = 0x912530A9BCA7B5CFLL;
+static uint64_t g_17 = 3UL;
