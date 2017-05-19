e0d42afac9d08ba2fe8ed884132bcaeba0dbe50b
saisi <saisi@users.noreply.github.com>
2017-05-19 04:27:10 +0000

    1495168029 animikite

---
+++ 1495168029.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2519790951
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   int64_t  f1;
+   const int8_t  f2;
+   uint64_t  f3;
+   int32_t  f4;
+   uint64_t  f5;
+   int8_t  f6;
