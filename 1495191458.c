7e9d8359d3b769e6a1e7b00218557fe3bbba8d0c
saisi <saisi@users.noreply.github.com>
2017-05-19 10:57:39 +0000

    1495191458 antennal

---
+++ 1495191458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1645088937
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const uint32_t  f0;
+   unsigned f1 : 8;
+   signed f2 : 30;
+   int64_t  f3;
+   volatile unsigned f4 : 22;
+   signed f5 : 1;
+};
