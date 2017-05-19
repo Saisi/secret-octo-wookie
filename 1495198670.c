8872f2e9b6f8104e12018dfc408d57e12dd79ca8
saisi <saisi@users.noreply.github.com>
2017-05-19 12:57:50 +0000

    1495198670 acetone

---
+++ 1495198670.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1199013167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   unsigned f1 : 21;
+   volatile uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_25 = 0x6F37L;
+static uint16_t g_28 = 65535UL;
+static uint64_t g_36 = 18446744073709551611UL;
