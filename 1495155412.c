427670f4a05733c3435fa8c0f0991040719d3c8f
saisi <saisi@users.noreply.github.com>
2017-05-19 00:56:53 +0000

    1495155412 Amasta

---
+++ 1495155412.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1010389647
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 6;
+   signed f1 : 5;
+   unsigned f2 : 20;
+   unsigned f3 : 15;
+   signed f4 : 4;
+   volatile unsigned f5 : 13;
+   unsigned f6 : 24;
+};
+
