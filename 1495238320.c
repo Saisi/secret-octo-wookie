8a903c6d6644b7bc79ec6cf6a94d8a85ae1bc2a8
saisi <saisi@users.noreply.github.com>
2017-05-19 23:58:40 +0000

    1495238320 Amethyst

---
+++ 1495238320.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2287088929
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-2L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x3D06CE12L;
+static uint32_t g_14[1][6] = {{4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL,4294967287UL}};
+static uint32_t *g_27 = (void*)0;
+static int32_t g_34 = 5L;
+static uint32_t g_35[6] = {0x1B9B9640L,0x1F0B9F5AL,0x1B9B9640L,0x1B9B9640L,0x1F0B9F5AL,0x1B9B9640L};
+static int16_t g_70 = (-3L);
+static int64_t g_73 = 0x7D512630673EEFD7LL;
+static int32_t g_83[1] = {0x2CB0EA5CL};
