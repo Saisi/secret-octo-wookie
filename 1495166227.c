2a24c7db6266f6d89c4b996b482d32053a3f5e8a
saisi <saisi@users.noreply.github.com>
2017-05-19 03:57:07 +0000

    1495166227 agonic

---
+++ 1495166227.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3042802238
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 13;
+   uint64_t  f1;
+   signed f2 : 24;
+   volatile int32_t  f3;
+};
+
+union U1 {
+   const int32_t  f0;
+};
