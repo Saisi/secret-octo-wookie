e4c7ec730124217f7d7672fc8c706f9f82c22c3b
saisi <saisi@users.noreply.github.com>
2017-05-19 01:56:57 +0000

    1495159017 atomy

---
+++ 1495159017.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2683240547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 16;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_43 = 4294967287UL;
+static volatile int32_t g_44 = 0x27F08E22L;/* VOLATILE GLOBAL g_44 */
+static int32_t g_50 = 0xB35B6890L;
+static int32_t g_51 = 0xEAA3E8F0L;
+static int32_t g_52 = 0x16D21B18L;
