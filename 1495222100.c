ec20fa35b0959f2bfe4be61556d631f2598a82af
saisi <saisi@users.noreply.github.com>
2017-05-19 19:28:21 +0000

    1495222100 anatomising

---
+++ 1495222100.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3920036942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   unsigned f1 : 18;
+   signed f2 : 30;
+   volatile signed f3 : 8;
+   const volatile unsigned f4 : 13;
+   signed f5 : 5;
+   volatile int16_t  f6;
+   volatile unsigned f7 : 6;
+   unsigned f8 : 21;
