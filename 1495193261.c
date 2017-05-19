c3162d9551bb4b2c3baf44a2966590ad4eb778c3
saisi <saisi@users.noreply.github.com>
2017-05-19 11:27:41 +0000

    1495193261 antitorpedo

---
+++ 1495193261.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      16604277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int32_t  f0;
+   uint32_t  f1;
+   uint32_t  f2;
+   int8_t  f3;
+   uint32_t  f4;
+   volatile uint16_t  f5;
+   int64_t  f6;
+   int32_t  f7;
+   int8_t  f8;
