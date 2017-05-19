3bdf3e26724d031543707bd7adf797ce874c5714
saisi <saisi@users.noreply.github.com>
2017-05-19 08:27:27 +0000

    1495182446 afterguard

---
+++ 1495182446.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3442098476
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_13 = 0xBFC3938DD44AD374LL;
+static uint32_t g_28 = 0xD96711DBL;
+static int8_t g_39 = 0L;
+static int32_t g_52 = (-3L);
+static volatile int64_t g_56 = 0x3547A0585C2C102DLL;/* VOLATILE GLOBAL g_56 */
+static volatile uint32_t g_57 = 9UL;/* VOLATILE GLOBAL g_57 */
+static int8_t g_82[4][5] = {{0x4FL,1L,0x4FL,0x9AL,1L},{0x2BL,0x5FL,0x9AL,0x2BL,0x9AL},{0x2BL,0x2BL,0L,1L,0x79L},{0x4FL,0x79L,0x9AL,0x9AL,0x79L}};
+static int32_t ** volatile g_90 = (void*)0;/* VOLATILE GLOBAL g_90 */
+static int32_t g_92 = (-9L);
