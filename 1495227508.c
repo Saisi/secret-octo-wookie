c89610689ee37cf344da81ff5cc4d07bd6eedfd2
saisi <saisi@users.noreply.github.com>
2017-05-19 20:58:28 +0000

    1495227508 alhambra

---
+++ 1495227508.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2845657942
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x12E1DFE0L;
+static int32_t * const  volatile g_4[4] = {&g_5,&g_5,&g_5,&g_5};
+static int32_t * volatile g_6 = &g_5;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
