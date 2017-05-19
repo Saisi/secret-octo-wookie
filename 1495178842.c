59e38f6149eaf960d4b9f08417a6ff4ab439e81e
saisi <saisi@users.noreply.github.com>
2017-05-19 07:27:22 +0000

    1495178842 Ammonitess

---
+++ 1495178842.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3916319574
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_5 = 18446744073709551607UL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 0x1449C9FBL;
+static int32_t *g_9 = &g_6;
+static int32_t * volatile * volatile g_8 = &g_9;/* VOLATILE GLOBAL g_8 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+
