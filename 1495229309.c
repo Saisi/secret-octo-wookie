afe42efc5bcee0bdbb518c29c0f36bd4ce26eb02
saisi <saisi@users.noreply.github.com>
2017-05-19 21:28:30 +0000

    1495229309 Ancerata

---
+++ 1495229309.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2216924504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x7A7C2039L;
+static union U0 g_11 = {0x4C8F0D0714AD38DCLL};
+static uint32_t g_60 = 0xF093D7F2L;
+static int8_t g_78 = 0x91L;
+static uint32_t g_80[1] = {4294967295UL};
