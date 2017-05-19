1c78aadf8f16a5d924c7075775bda0c3532d0309
saisi <saisi@users.noreply.github.com>
2017-05-19 18:28:16 +0000

    1495218495 adoptionism

---
+++ 1495218495.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3380679198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_11 = 0x6D2E8588L;
+static int8_t g_22 = 0x17L;
+static uint8_t g_26 = 248UL;
+static uint8_t *g_45 = (void*)0;
+static int8_t g_47[10][9] = {{(-3L),0L,0x0AL,(-8L),0x60L,0xD0L,0xD0L,0x60L,(-8L)},{(-6L),0xE8L,(-6L),6L,1L,(-3L),1L,0x93L,0L},{0x6CL,1L,0xA1L,0xF6L,(-8L),0L,0xDDL,(-9L),0L},{0xF5L,(-6L),(-3L),6L,0xD0L,0L,(-3L),1L,0x3CL},{(-1L),0L,4L,(-8L),0x3CL,1L,0x5EL,1L,0x3CL},{5L,0x2FL,0x2FL,0L,0x45L,(-3L),0x3CL,(-6L),(-9L)},{0x6CL,(-8L),(-3L),(-3L),0x60L,0xA1L,0x5EL,1L,0xD0L},{0L,0x3CL,1L,0x60L,0x45L,(-6L),0xD0L,(-9L),0xD5L},{(-3L),0xDDL,(-3L),0L,1L,0x0AL,0xE8L,0xE8L,0x0AL},{(-3L),0x0AL,(-6L),0x0AL,(-3L),0L,0x86L,0x6CL,0xE8L}};
+static volatile int32_t *g_48[7] = {(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0,(void*)0};
+static uint32_t g_101 = 4294967286UL;
+static uint8_t g_106 = 4UL;
+static uint32_t g_111 = 0x39976036L;
