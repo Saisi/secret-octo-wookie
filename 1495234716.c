7a6b48c9a88a15df4aff2cefe44998e3c138424f
saisi <saisi@users.noreply.github.com>
2017-05-19 22:58:36 +0000

    1495234716 agism

---
+++ 1495234716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3409930100
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x1D14CD49L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xAAF0A9B2L;
+static uint16_t g_10[2] = {1UL,1UL};
+static uint32_t g_14 = 0x6B42BE19L;
+static int32_t g_16 = 0x3B1DAB55L;
+static int8_t g_19 = 0L;
+static int32_t g_21 = (-9L);
+static int32_t * volatile g_20 = &g_21;/* VOLATILE GLOBAL g_20 */
+
