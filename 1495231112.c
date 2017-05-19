28859134932c34922ce7412f0a99581da641fe64
saisi <saisi@users.noreply.github.com>
2017-05-19 21:58:32 +0000

    1495231112 arrestees

---
+++ 1495231112.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3093264447
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5[3] = {(-8L),(-8L),(-8L)};
+static int32_t g_7 = (-1L);
+static uint8_t g_24 = 0x4EL;
+static uint32_t g_27 = 4294967295UL;
+static uint32_t g_89 = 1UL;
+static int8_t g_92[5] = {(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_97 = 1L;
+static int32_t * volatile g_96 = &g_97;/* VOLATILE GLOBAL g_96 */
+static uint8_t g_106 = 0x15L;
