6ab4d799414e4bbf36ec278800ab804a231dc548
saisi <saisi@users.noreply.github.com>
2017-05-20 21:00:19 +0000

    1495314018 Alghero

---
+++ 1495314018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      605264101
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 17;
+   signed f1 : 4;
+   unsigned f2 : 23;
+   unsigned f3 : 6;
+   volatile unsigned f4 : 31;
+   signed f5 : 28;
+   unsigned f6 : 11;
+   unsigned f7 : 18;
+   volatile unsigned f8 : 13;
