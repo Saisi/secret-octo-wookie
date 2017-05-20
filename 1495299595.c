fa4581cf73b5cb22aa1e0dae048c2a2523eb58a6
saisi <saisi@users.noreply.github.com>
2017-05-20 16:59:56 +0000

    1495299595 apperceptionist

---
+++ 1495299595.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3383213901
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   uint64_t  f1;
+   uint64_t  f2;
+   volatile int32_t  f3;
+   const uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 4L;
