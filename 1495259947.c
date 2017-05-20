c12ea428edc436936f72e1c04d4be70431e33908
saisi <saisi@users.noreply.github.com>
2017-05-20 05:59:08 +0000

    1495259947 apyretic

---
+++ 1495259947.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3361015782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_18 = (-1L);
+static int32_t g_56[9] = {0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L,0x3C3AE507L};
+static int64_t g_58[1] = {0xEA1BCA3396D56A42LL};
+static int32_t g_61 = (-1L);
+static int32_t *g_60[1] = {&g_61};
+static uint16_t g_64 = 0x42D6L;
+static uint8_t g_72 = 1UL;
+static volatile uint8_t g_74 = 1UL;/* VOLATILE GLOBAL g_74 */
+static volatile uint8_t *g_73 = &g_74;
