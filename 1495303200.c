02dbd0f1bdc221a46f8d69acc26f29c089002420
saisi <saisi@users.noreply.github.com>
2017-05-20 18:00:00 +0000

    1495303200 anthobiology

---
+++ 1495303200.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3692176361
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 19;
+   unsigned f1 : 30;
+   signed f2 : 9;
+   unsigned f3 : 16;
+   signed f4 : 1;
+   const unsigned f5 : 22;
+};
