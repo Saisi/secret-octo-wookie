c08eb1b21a1939ebf2ee761b984282af5ee22fe9
saisi <saisi@users.noreply.github.com>
2017-05-20 16:29:54 +0000

    1495297793 ardurous

---
+++ 1495297793.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3560035712
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x07EA4C57L;
+static uint64_t g_18 = 1UL;
+static int32_t g_34 = 0x2F520A04L;
+static int32_t * volatile g_33 = &g_34;/* VOLATILE GLOBAL g_33 */
+static volatile int8_t g_39 = 0L;/* VOLATILE GLOBAL g_39 */
+static volatile int16_t g_40 = 0x8C68L;/* VOLATILE GLOBAL g_40 */
+static volatile int32_t g_41 = 0xD23037D9L;/* VOLATILE GLOBAL g_41 */
+static uint32_t g_42 = 0x6C217795L;
+static int64_t g_74 = 0x9E236B79BA4D29FBLL;
