abfe9cdf4c14b138c4802ca805aa7edff1456f70
saisi <saisi@users.noreply.github.com>
2017-05-20 17:29:58 +0000

    1495301397 autocab

---
+++ 1495301397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3356036875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 4;
+   volatile unsigned f1 : 11;
+   volatile unsigned f2 : 2;
+   const int64_t  f3;
+   signed f4 : 14;
+   signed f5 : 4;
+   unsigned f6 : 23;
+   unsigned f7 : 21;
+   signed f8 : 16;
