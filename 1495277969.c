161db29341a510627cbffd21d93a8b5e36c7ab83
saisi <saisi@users.noreply.github.com>
2017-05-20 10:59:30 +0000

    1495277969 acroterion

---
+++ 1495277969.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1259837031
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 18;
+   unsigned f1 : 1;
+   const unsigned : 0;
+   unsigned f2 : 11;
+   volatile signed f3 : 16;
+};
+
+#pragma pack(push)
+#pragma pack(1)
