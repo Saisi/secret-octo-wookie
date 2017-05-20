aa5c2c2a154bd342650bf54c383f796db60db139
saisi <saisi@users.noreply.github.com>
2017-05-20 01:58:50 +0000

    1495245529 acidic

---
+++ 1495245529.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      70455877
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 1;
+   int32_t  f1;
+   const uint32_t  f2;
+   const int8_t  f3;
+   volatile uint16_t  f4;
+   int8_t  f5;
+   const int32_t  f6;
