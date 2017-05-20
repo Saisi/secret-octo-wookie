a4212f23cec4352e4af6866bf28dda424241c469
saisi <saisi@users.noreply.github.com>
2017-05-20 15:29:49 +0000

    1495294189 bairn

---
+++ 1495294189.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3265872978
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const volatile unsigned f0 : 23;
+   volatile signed f1 : 16;
+   volatile unsigned f2 : 18;
+   volatile unsigned f3 : 22;
+   signed f4 : 13;
+   volatile uint16_t  f5;
+};
+
+#pragma pack(push)
