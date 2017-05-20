e634ad38d37df4bef9f0032ce4708edd1cfb78b7
saisi <saisi@users.noreply.github.com>
2017-05-20 07:29:14 +0000

    1495265354 backdrop

---
+++ 1495265354.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      62627456
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const unsigned f0 : 14;
+   unsigned f1 : 11;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
