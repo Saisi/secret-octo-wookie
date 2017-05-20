ab53e26a26c8ef4bf671a7e4893021f21160c821
saisi <saisi@users.noreply.github.com>
2017-05-20 13:29:41 +0000

    1495286981 antischolastic

---
+++ 1495286981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4079717456
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x03342EB9L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 2L;
+static uint32_t g_92 = 7UL;
+static int64_t g_99 = 0x8EAF535935178915LL;
+static uint16_t g_104 = 0xF5EEL;
+static uint32_t g_105 = 1UL;
+static uint64_t g_127 = 18446744073709551615UL;
+static int32_t g_131 = 6L;
