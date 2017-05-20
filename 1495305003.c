f8cdfc5390a1f02178209ab843ffd8355d2d412a
saisi <saisi@users.noreply.github.com>
2017-05-20 18:30:04 +0000

    1495305003 anemogram

---
+++ 1495305003.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2853465323
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const uint8_t  f1;
+   const unsigned f2 : 27;
+   const int64_t  f3;
+   const uint16_t  f4;
+   signed f5 : 18;
+   int16_t  f6;
