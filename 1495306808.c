3812d660996e27fc20972885e7faaacbd693b96c
saisi <saisi@users.noreply.github.com>
2017-05-20 19:00:09 +0000

    1495306808 aleyard

---
+++ 1495306808.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      238578922
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   unsigned f1 : 12;
+   signed f2 : 30;
+   volatile unsigned f3 : 18;
+   const unsigned f4 : 25;
+   signed f5 : 12;
+   signed f6 : 26;
+   signed f7 : 7;
+   signed f8 : 15;
