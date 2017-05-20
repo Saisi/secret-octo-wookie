b87f6385dd4c8a35365b80ce7795c8985261bce6
saisi <saisi@users.noreply.github.com>
2017-05-20 03:28:56 +0000

    1495250935 actinopraxis

---
+++ 1495250935.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3743443958
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = (-1L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0x21591249L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0L;
+static int32_t g_6 = 1L;
+static volatile int32_t g_7 = (-1L);/* VOLATILE GLOBAL g_7 */
+static int32_t g_8 = 0xF82D83D7L;
+static volatile int32_t *g_16[3] = {&g_3,&g_3,&g_3};
+static volatile int32_t g_19 = (-1L);/* VOLATILE GLOBAL g_19 */
+static int32_t g_20 = 0L;
