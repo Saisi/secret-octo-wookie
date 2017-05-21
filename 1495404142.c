f7880f825f2c8fde852bb91a422d7e9ef6006ddd
saisi <saisi@users.noreply.github.com>
2017-05-21 22:02:23 +0000

    1495404142 aversion

---
+++ 1495404142.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2286984587
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint64_t g_34 = 2UL;
+static int32_t g_36 = (-6L);
+static int32_t * volatile g_35[6] = {&g_36,&g_36,&g_36,&g_36,&g_36,&g_36};
+static int16_t g_64 = 0x9781L;
+static uint8_t g_68 = 0UL;
+static int16_t g_71[3] = {0x18D7L,0x18D7L,0x18D7L};
+static uint32_t g_81 = 4294967288UL;
+static int64_t g_105[7] = {(-1L),0x794862D36C47714FLL,(-1L),(-1L),0x794862D36C47714FLL,(-1L),(-1L)};
+static volatile uint16_t g_125 = 65528UL;/* VOLATILE GLOBAL g_125 */
