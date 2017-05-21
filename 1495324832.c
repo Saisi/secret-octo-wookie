23742f1e80c4869813c97fdd6b4cc0f32df16e08
saisi <saisi@users.noreply.github.com>
2017-05-21 00:00:34 +0000

    1495324832 aroxyl

---
+++ 1495324832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3704977591
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   const volatile int32_t  f1;
+   uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x094A1888L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xF1BF506EL;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4 = 0x0CEAFD94L;
