978c774dd33afc9cfc4852ac1fa72615e75d7de3
saisi <saisi@users.noreply.github.com>
2017-05-21 21:02:18 +0000

    1495400538 acrylate

---
+++ 1495400538.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2942763504
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 6;
+   volatile uint8_t  f1;
+   unsigned f2 : 25;
+   const uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_5[7] = {1UL,18446744073709551615UL,1UL,1UL,18446744073709551615UL,1UL,1UL};
+static uint16_t g_8[2] = {1UL,1UL};
