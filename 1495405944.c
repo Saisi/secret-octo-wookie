b519f8de984744ab6367752e9800ce85b170f8bc
saisi <saisi@users.noreply.github.com>
2017-05-21 22:32:25 +0000

    1495405944 actinology

---
+++ 1495405944.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3660078465
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int32_t  f0;
+   uint8_t  f1;
+   volatile int32_t  f2;
+   const int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_16 = 3L;/* VOLATILE GLOBAL g_16 */
+static int16_t g_26 = 0x3F91L;
