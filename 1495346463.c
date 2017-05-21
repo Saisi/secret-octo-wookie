cc87122223d32558bb15b09a5effa390de6da746
saisi <saisi@users.noreply.github.com>
2017-05-21 06:01:03 +0000

    1495346463 auriculo

---
+++ 1495346463.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4157859391
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_13 = 0xC7L;
+static int16_t g_15 = 0x09F5L;
+static int32_t g_24 = 0x14E76493L;
+static int32_t * const  volatile g_23 = &g_24;/* VOLATILE GLOBAL g_23 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint8_t  func_1(void);
+static int16_t  func_6(int8_t * p_7, int8_t * p_8, int8_t * p_9, int8_t * p_10, int16_t  p_11);
