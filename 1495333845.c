98b99dc336d336c1b34896fce6bc427dbee75864
saisi <saisi@users.noreply.github.com>
2017-05-21 02:30:46 +0000

    1495333845 aminodiphenyl

---
+++ 1495333845.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      893503360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 6L;
+static int32_t g_4 = 4L;
+static int32_t g_5 = (-5L);
+static volatile int32_t g_6 = (-1L);/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = (-2L);
+static uint64_t g_40[10] = {0x0BB265D63260039CLL,0x7E942FAFECEB2CFBLL,0xCF58A4122E7DFE98LL,0xCF58A4122E7DFE98LL,0x7E942FAFECEB2CFBLL,0x0BB265D63260039CLL,0x7E942FAFECEB2CFBLL,0xCF58A4122E7DFE98LL,0xCF58A4122E7DFE98LL,0x7E942FAFECEB2CFBLL};
+static int32_t g_55[7] = {8L,8L,8L,8L,8L,8L,8L};
+static int16_t g_68 = 0xDC3DL;
+static uint64_t g_85 = 0xBDFF1065C771C756LL;
