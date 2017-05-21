019c30e7eb8fbb6555169cb512b127d3254140ad
saisi <saisi@users.noreply.github.com>
2017-05-21 18:02:02 +0000

    1495389721 atelopodia

---
+++ 1495389721.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1494119246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xB26BDDE0L;
+static int32_t g_26[1][7] = {{2L,2L,2L,2L,2L,2L,2L}};
+static uint64_t g_29 = 0x0389A9B8FA48A2B8LL;
+static uint32_t g_33 = 0xCCE35A12L;
+static volatile int32_t g_57 = 0x871D1BA0L;/* VOLATILE GLOBAL g_57 */
+static int32_t g_58 = 0x189EFA4EL;
+static int32_t g_60 = (-10L);
+static int32_t g_61 = 3L;
+static int32_t g_64 = 1L;
