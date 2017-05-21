c323e9f6980c01820f4ef1b89549c2e7a06004a3
saisi <saisi@users.noreply.github.com>
2017-05-21 12:31:35 +0000

    1495369894 attritive

---
+++ 1495369894.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1473273986
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int64_t  f0;
+   int64_t  f1;
+   unsigned f2 : 7;
+   volatile int16_t  f3;
+   uint64_t  f4;
+   const uint32_t  f5;
+   uint16_t  f6;
+   int32_t  f7;
+};
