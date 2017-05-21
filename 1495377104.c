3fcf9bef33f511c0932bbed4a33d1ab8eeebfd3c
saisi <saisi@users.noreply.github.com>
2017-05-21 14:31:45 +0000

    1495377104 Ascalabota

---
+++ 1495377104.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      285041366
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   volatile uint64_t  f1;
+   int32_t  f2;
+   int8_t * volatile  f3;
+   volatile uint8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
