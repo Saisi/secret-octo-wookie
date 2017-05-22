e92ccb5778020407a23b42fced80d9df6957792b
saisi <saisi@users.noreply.github.com>
2017-05-22 03:02:47 +0000

    1495422166 anomodont

---
+++ 1495422166.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1950886438
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 1;
+   signed f1 : 18;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
