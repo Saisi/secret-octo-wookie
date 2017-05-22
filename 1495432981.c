32ea883a949137b14260a415a982aa4ff3d78b94
saisi <saisi@users.noreply.github.com>
2017-05-22 06:03:04 +0000

    1495432981 avgases

---
+++ 1495432981.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3929680854
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 25;
+   volatile int32_t  f1;
+};
+
+union U1 {
+   int32_t  f0;
+   volatile int16_t  f1;
+};
+
