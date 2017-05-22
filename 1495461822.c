d6b8b9c40faf018d7ff181ef75c3f26e553e2c5d
saisi <saisi@users.noreply.github.com>
2017-05-22 14:03:43 +0000

    1495461822 albardine

---
+++ 1495461822.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      169229552
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint32_t  f0;
+   uint16_t  f1;
+   int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xCB13638EL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x4C3C69AEL;
+static int32_t g_45 = (-1L);
