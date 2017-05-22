a2fe2c96a1fcfb29227a93b96b370d22c4255f39
saisi <saisi@users.noreply.github.com>
2017-05-22 02:02:41 +0000

    1495418561 balladical

---
+++ 1495418561.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3045320256
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int8_t  f0;
+   unsigned f1 : 27;
+   unsigned f2 : 3;
+   signed f3 : 25;
+   volatile signed f4 : 26;
+};
+#pragma pack(pop)
