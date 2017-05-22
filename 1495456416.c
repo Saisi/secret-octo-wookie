293a657f07173f7d8929d90748eb2693eacfcfd0
saisi <saisi@users.noreply.github.com>
2017-05-22 12:33:36 +0000

    1495456416 balladist

---
+++ 1495456416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3701910234
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   uint64_t  f1;
+};
+
+union U1 {
+   uint32_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
