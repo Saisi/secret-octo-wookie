245c104b9a44909dde7f57496395a0d62ff98909
saisi <saisi@users.noreply.github.com>
2017-05-22 15:33:51 +0000

    1495467230 anoterite

---
+++ 1495467230.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      22589308
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 12;
+   const signed f1 : 17;
+   volatile unsigned f2 : 15;
+   const volatile unsigned f3 : 31;
+   signed f4 : 18;
+   volatile uint16_t  f5;
+   signed f6 : 13;
+};
+
