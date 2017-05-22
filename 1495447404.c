e295c14800b3d4ddee11b7ea7536cb04c5995252
saisi <saisi@users.noreply.github.com>
2017-05-22 10:03:24 +0000

    1495447404 Allina

---
+++ 1495447404.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      548377807
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x5B57FD54L;
+static int32_t g_33 = 0x1F3403CAL;
+static uint64_t g_35 = 0x329BFAEAD2214C25LL;
+static volatile int16_t g_40 = 0x8E01L;/* VOLATILE GLOBAL g_40 */
+static uint16_t g_43 = 0xD602L;
+static int32_t * volatile g_57 = &g_33;/* VOLATILE GLOBAL g_57 */
+static uint16_t g_87 = 0x800BL;
+static int8_t g_89 = 0xF0L;
+static int64_t g_102 = 0x9FEC4F7CDB24405CLL;
