3db183f6214357f6e0d5ac215669ed5358388f9a
saisi <saisi@users.noreply.github.com>
2017-05-22 19:34:13 +0000

    1495481651 Ajo

---
+++ 1495481651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1142352094
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 29;
+   unsigned f1 : 30;
+};
+#pragma pack(pop)
+
+union U1 {
+   const int32_t  f0;
