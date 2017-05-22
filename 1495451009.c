50ed2120a64fb0f84c2b3075f5a3b5cba4ddceac
saisi <saisi@users.noreply.github.com>
2017-05-22 11:03:30 +0000

    1495451009 archbishoprics

---
+++ 1495451009.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2732959839
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int64_t  f0;
+   int32_t  f1;
+   uint64_t  f2;
+   uint16_t  f3;
+   int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xA35F394AL;/* VOLATILE GLOBAL g_2 */
