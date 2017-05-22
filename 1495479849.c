651b60b2d4c57d1730b90a456855fcc44848a91d
saisi <saisi@users.noreply.github.com>
2017-05-22 19:04:10 +0000

    1495479849 axisymmetric

---
+++ 1495479849.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      714066186
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0x3EFC07E1L;
+static int32_t *g_7 = &g_8;
+static int16_t g_21 = 2L;
+static int32_t g_31 = (-1L);
+static int32_t * volatile g_30 = &g_31;/* VOLATILE GLOBAL g_30 */
+static int32_t *g_33 = &g_8;
+static int32_t ** volatile g_32 = &g_33;/* VOLATILE GLOBAL g_32 */
+static int16_t *g_37 = &g_21;
+static int8_t g_49 = (-2L);
