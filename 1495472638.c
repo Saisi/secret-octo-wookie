de03931c8912f71da4ace3e72be401cf8eb745f8
saisi <saisi@users.noreply.github.com>
2017-05-22 17:03:58 +0000

    1495472638 articulatory

---
+++ 1495472638.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2875335868
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   volatile uint32_t  f1;
+   volatile uint16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 5L;
+static volatile union U0 g_30 = {0};/* VOLATILE GLOBAL g_30 */
+static int32_t g_39 = 1L;
