df8034e0d23e25319c7a239e0eee4a25518537b8
saisi <saisi@users.noreply.github.com>
2017-05-22 07:03:09 +0000

    1495436588 anacathartic

---
+++ 1495436588.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2132077056
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   signed f1 : 11;
+   int16_t  f2;
+   volatile uint32_t  f3;
+   const uint16_t  f4;
+};
+
+union U1 {
+   const signed f0 : 7;
