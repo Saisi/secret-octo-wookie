209d26a2b4ea5369cfec696f3535611b84d3a72d
saisi <saisi@users.noreply.github.com>
2017-05-23 13:35:40 +0000

    1495546539 Australopithecinae

---
+++ 1495546539.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3597894319
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 22;
+   volatile unsigned f1 : 21;
+   volatile signed f2 : 4;
+   unsigned f3 : 31;
+   unsigned f4 : 20;
+   volatile signed f5 : 29;
+   const unsigned f6 : 8;
+   unsigned f7 : 27;
+   volatile uint32_t  f8;
