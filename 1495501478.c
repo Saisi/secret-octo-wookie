f95a890dd8d127c8a68afcbf612d4658ef9882c4
saisi <saisi@users.noreply.github.com>
2017-05-23 01:04:38 +0000

    1495501478 amphimictical

---
+++ 1495501478.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1164176137
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 3;
+   unsigned f1 : 14;
+   volatile unsigned f2 : 25;
+   const volatile signed : 0;
+   volatile unsigned f3 : 17;
+   unsigned f4 : 6;
+   unsigned f5 : 13;
+   signed f6 : 12;
+   signed f7 : 31;
