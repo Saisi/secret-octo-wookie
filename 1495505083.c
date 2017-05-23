b1af20ca228ec5a74ee7ec67ad3751cb9c7ddd89
saisi <saisi@users.noreply.github.com>
2017-05-23 02:04:43 +0000

    1495505083 balize

---
+++ 1495505083.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3846672735
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[7][7] = {{0xF44CACFFL,0x42C60EDAL,0L,0L,0x42C60EDAL,0xF44CACFFL,0x42C60EDAL},{3L,(-2L),(-2L),3L,3L,3L,(-2L)},{0x5EF889A2L,0x5EF889A2L,0xF44CACFFL,0L,0xF44CACFFL,0x5EF889A2L,0x5EF889A2L},{0x7A8CF1D6L,(-2L),1L,(-2L),0x7A8CF1D6L,0x7A8CF1D6L,(-2L)},{0x07C7BE02L,0x42C60EDAL,0x07C7BE02L,0xF44CACFFL,0xF44CACFFL,0x07C7BE02L,0x42C60EDAL},{(-2L),3L,1L,1L,3L,(-2L),3L},{0x07C7BE02L,0xF44CACFFL,0xF44CACFFL,0x07C7BE02L,0x5EF889A2L,0L,0x07C7BE02L}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
