ff9d7b76b81a92ece82daf6143b3415df2b420d0
saisi <saisi@users.noreply.github.com>
2017-05-23 00:34:36 +0000

    1495499675 aquadag

---
+++ 1495499675.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3754790326
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 9;
+   volatile signed f1 : 26;
+};
+
+struct S1 {
+   const volatile signed f0 : 28;
+   uint32_t  f1;
+   unsigned f2 : 18;
+   int16_t  f3;
