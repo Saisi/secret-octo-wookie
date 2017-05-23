9766477af99fa8e6ad5e672d525249d8c582a449
saisi <saisi@users.noreply.github.com>
2017-05-23 19:36:11 +0000

    1495568170 antirape

---
+++ 1495568170.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1455680103
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 18;
+   unsigned f1 : 1;
+   signed : 0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_9 = (-6L);/* VOLATILE GLOBAL g_9 */
+static uint32_t g_12 = 2UL;
+static uint32_t g_20 = 5UL;
