36a5c64b00a92eacef32d5285a30ebab998cce53
saisi <saisi@users.noreply.github.com>
2017-05-23 18:06:03 +0000

    1495562762 astrobiological

---
+++ 1495562762.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1309906594
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_2 = 0x70ADL;
+static int32_t g_11 = 0L;
+static int32_t g_12 = 5L;
+static uint16_t g_23 = 0xD402L;
+static int16_t g_27 = 0x03B1L;
+static int32_t g_55[9] = {0L,0L,0L,0L,0L,0L,0L,0L,0L};
+static uint32_t g_89 = 0xDCDEDC29L;
+static int32_t g_102 = 0x6FD7422EL;
+static int8_t g_104[3] = {0x2DL,0x2DL,0x2DL};
