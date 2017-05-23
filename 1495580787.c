36b2c7a369783b69f32f65328538e7cca05bb85f
saisi <saisi@users.noreply.github.com>
2017-05-23 23:06:28 +0000

    1495580787 aortoclasia

---
+++ 1495580787.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2665021560
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 1L;
+static int16_t g_11 = 0L;
+static uint8_t g_19 = 0x11L;
+static int32_t * volatile g_22 = &g_2;/* VOLATILE GLOBAL g_22 */
+static uint64_t g_28 = 18446744073709551615UL;
+static volatile int64_t g_32 = 1L;/* VOLATILE GLOBAL g_32 */
+static volatile int8_t g_36 = (-1L);/* VOLATILE GLOBAL g_36 */
+static volatile uint32_t g_37 = 18446744073709551615UL;/* VOLATILE GLOBAL g_37 */
+static uint32_t g_45 = 0x9FB3884EL;
