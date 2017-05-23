f9be9b6459c8fc574903a616d7e459949200e33f
saisi <saisi@users.noreply.github.com>
2017-05-23 10:35:27 +0000

    1495535725 autodiffusion

---
+++ 1495535725.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4145797844
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 24;
+   unsigned f1 : 13;
+   signed f2 : 27;
+   unsigned f3 : 27;
+   unsigned f4 : 30;
+   signed f5 : 29;
+   signed f6 : 7;
+   signed : 0;
+   volatile unsigned f7 : 4;
