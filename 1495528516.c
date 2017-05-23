6a6f65550f47bc96f586bed03feaeb7e651189f1
saisi <saisi@users.noreply.github.com>
2017-05-23 08:35:16 +0000

    1495528516 anisotropic

---
+++ 1495528516.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2942525056
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_15 = 0x18E418A5L;/* VOLATILE GLOBAL g_15 */
+static uint16_t g_22[8] = {9UL,9UL,9UL,9UL,9UL,9UL,9UL,9UL};
+static uint8_t g_31 = 0UL;
+static uint8_t g_48 = 0xFAL;
+static int32_t g_65 = 0x121F2059L;
+static int32_t *g_99 = &g_65;
+static int32_t **g_98[6] = {&g_99,&g_99,&g_99,&g_99,&g_99,&g_99};
+static uint32_t g_105 = 4294967287UL;
+static int64_t g_108 = 0x02F47D66A4358B9ELL;
