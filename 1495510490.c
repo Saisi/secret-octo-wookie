13cbfa2d3b072647e120bec752785d0cabdec128
saisi <saisi@users.noreply.github.com>
2017-05-23 03:34:51 +0000

    1495510490 atweel

---
+++ 1495510490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      708256934
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[9][2] = {{1L,1L},{1L,1L},{1L,1L},{1L,1L},{1L,1L},{1L,1L},{1L,1L},{1L,1L},{1L,1L}};
+static volatile int32_t g_5[9] = {0x73BF38C0L,0xA9D19AC1L,0x73BF38C0L,0x73BF38C0L,0xA9D19AC1L,0x73BF38C0L,0x73BF38C0L,0xA9D19AC1L,0x73BF38C0L};
+static volatile int32_t g_6 = 0xFA4AD744L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = (-1L);
+static int32_t g_10 = 0xE4B9A733L;
+static volatile int32_t g_11 = 1L;/* VOLATILE GLOBAL g_11 */
+static int32_t g_12 = 0L;
+static volatile int32_t g_13 = 0xA75A592CL;/* VOLATILE GLOBAL g_13 */
+static volatile int32_t g_14 = 1L;/* VOLATILE GLOBAL g_14 */
