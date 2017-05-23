804337d8c793e7b8cb48583758adbd3c62f229a7
saisi <saisi@users.noreply.github.com>
2017-05-23 12:35:35 +0000

    1495542935 albuminiferous

---
+++ 1495542935.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4200651308
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[1] = {0xC1L};
+static int32_t g_4[10][3] = {{(-2L),(-2L),0L},{0x1F94964AL,0x1F94964AL,0L},{(-2L),(-2L),0L},{0x1F94964AL,0x1F94964AL,0L},{(-2L),(-2L),0L},{0x1F94964AL,0x1F94964AL,0L},{(-2L),(-2L),0L},{0x1F94964AL,0x1F94964AL,0L},{(-2L),(-2L),0L},{0x1F94964AL,0x1F94964AL,0L}};
+static int32_t * volatile g_3 = &g_4[7][2];/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint16_t  func_1(void);
+
+
