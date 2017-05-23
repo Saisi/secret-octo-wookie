87a265fb2c9659d8471733d6ca5ed4397ab1f4fd
saisi <saisi@users.noreply.github.com>
2017-05-23 14:05:42 +0000

    1495548341 alophas

---
+++ 1495548341.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      740940901
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 17;
+   volatile unsigned f1 : 4;
+   signed f2 : 23;
+   unsigned f3 : 6;
+   unsigned f4 : 31;
+   volatile unsigned f5 : 28;
+   unsigned f6 : 11;
