c9b5cf3fc50460d14fa878e90dab95247a3288fe
saisi <saisi@users.noreply.github.com>
2017-05-23 06:35:06 +0000

    1495521306 accouplement

---
+++ 1495521306.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4091179660
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   const unsigned f1 : 25;
+   uint32_t  f2;
+   uint32_t  f3;
+   const int32_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x70C3099CL;
