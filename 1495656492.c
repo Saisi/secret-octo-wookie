13ec4e12b3ff6aa0a67d2185d63fc22509935c1e
saisi <saisi@users.noreply.github.com>
2017-05-24 20:08:13 +0000

    1495656492 affying

---
+++ 1495656492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4281243891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 24;
+   unsigned f1 : 15;
+   volatile unsigned f2 : 22;
+   unsigned f3 : 9;
+   const unsigned f4 : 22;
+   signed f5 : 23;
+   unsigned f6 : 26;
