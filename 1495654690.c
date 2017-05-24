484a741e8f59e850e37295a922119a304ed94bc6
saisi <saisi@users.noreply.github.com>
2017-05-24 19:38:10 +0000

    1495654690 Abbevilean

---
+++ 1495654690.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3982826726
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = (-1L);
+static int8_t g_21 = 0L;
+static uint16_t g_46[1][10] = {{2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL,2UL}};
+static uint32_t g_68 = 6UL;
+static volatile int8_t g_81 = 0xFBL;/* VOLATILE GLOBAL g_81 */
+static volatile int8_t * volatile g_80[2] = {&g_81,&g_81};
+static int64_t g_89 = 0x3986B299B5F277F1LL;
+static uint32_t g_113 = 0x2777EF5EL;
+static uint16_t g_125 = 0x8768L;
