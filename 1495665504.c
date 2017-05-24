9237e8e429c8dbc685f041694582d7c3f842a75b
saisi <saisi@users.noreply.github.com>
2017-05-24 22:38:25 +0000

    1495665504 Aratus

---
+++ 1495665504.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3042066157
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   uint32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_13 = 0x729CF308L;
+static int64_t g_32 = 0x3CC9F196E6C8CAC3LL;
+static int16_t g_64 = 2L;
+static const union U0 g_70 = {0x98L};
