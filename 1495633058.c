11335ba78d5564a2d827f0ae2a715e1aacc8f9a6
saisi <saisi@users.noreply.github.com>
2017-05-24 13:37:39 +0000

    1495633058 ammonic

---
+++ 1495633058.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1105690814
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 13;
+   unsigned f1 : 14;
+   uint8_t  f2;
+   volatile signed : 0;
+   signed f3 : 5;
+   unsigned f4 : 29;
+};
+
+#pragma pack(push)
