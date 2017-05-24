68faaa75658de377f211feb856227f80a63fdb1f
saisi <saisi@users.noreply.github.com>
2017-05-24 08:37:11 +0000

    1495615031 Alhagi

---
+++ 1495615031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3192702296
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   int8_t  f1;
+   int64_t  f2;
+   unsigned f3 : 5;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_15 = 1UL;
+static volatile int32_t g_34[1][3][10] = {{{0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL,0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL},{0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL,0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL},{0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL,0xDAA08BBDL,0x638299FAL,0x5205300CL,0x5205300CL,0x638299FAL}}};
