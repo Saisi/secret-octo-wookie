f71dae78049f53d6e07ff4114bd2a4a806159373
saisi <saisi@users.noreply.github.com>
2017-05-24 18:38:06 +0000

    1495651086 archaeographic

---
+++ 1495651086.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3992883570
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_14 = 0x62BB2C59L;
+static uint8_t g_77 = 0x2AL;
+static int32_t g_80 = 1L;
+static int32_t **g_82 = (void*)0;
+static int32_t *** volatile g_81 = &g_82;/* VOLATILE GLOBAL g_81 */
+static int16_t g_87 = (-2L);
+static uint8_t g_98 = 0UL;
+static uint8_t *g_97 = &g_98;
+static int64_t g_105[2][9][2] = {{{1L,1L},{6L,1L},{0xEDF6E77A780BBC09LL,0x65742D117B57B06ELL},{0xEDF6E77A780BBC09LL,1L},{6L,1L},{1L,(-8L)},{0x79FF52682FBA83E1LL,0x534F93D1D1CE175ELL},{1L,7L},{7L,7L}},{{1L,0x534F93D1D1CE175ELL},{0x79FF52682FBA83E1LL,(-8L)},{1L,1L},{6L,1L},{0xEDF6E77A780BBC09LL,0x65742D117B57B06ELL},{0xEDF6E77A780BBC09LL,1L},{6L,1L},{1L,(-8L)},{0x79FF52682FBA83E1LL,0x534F93D1D1CE175ELL}}};
