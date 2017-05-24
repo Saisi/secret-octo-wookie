5a3d8080aba927a2a5dd359dd3a073c2fdc799d7
saisi <saisi@users.noreply.github.com>
2017-05-24 06:06:58 +0000

    1495606018 adenotomy

---
+++ 1495606018.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2599940254
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S3 {
+   signed f0 : 16;
+   const volatile unsigned f1 : 21;
+   const unsigned f2 : 3;
+   signed f3 : 21;
+   const signed f4 : 17;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 5L;
