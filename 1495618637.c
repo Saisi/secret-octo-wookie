b519da23f963f08cbe1760b26c250665504a19d6
saisi <saisi@users.noreply.github.com>
2017-05-24 09:37:17 +0000

    1495618637 abdominalian

---
+++ 1495618637.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3817758828
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile unsigned f0 : 4;
+   volatile signed f1 : 9;
+   uint16_t  f2;
+   int32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x964A53AAL;
+static volatile int16_t g_34 = 0xB64BL;/* VOLATILE GLOBAL g_34 */
