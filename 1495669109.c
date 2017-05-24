53b0a33b9ade83c9ec6c690a213b4283ea16d5fa
saisi <saisi@users.noreply.github.com>
2017-05-24 23:38:29 +0000

    1495669109 astigmatism

---
+++ 1495669109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      190941203
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 8;
+   unsigned f1 : 31;
+   signed f2 : 6;
+   int32_t  f3;
+   int8_t  f4;
+};
+
+union U1 {
+   volatile int64_t  f0;
