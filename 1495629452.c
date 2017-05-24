1e84304f8dc2ddc69b455aa065839894dcd9ff57
saisi <saisi@users.noreply.github.com>
2017-05-24 12:37:33 +0000

    1495629452 autogenetically

---
+++ 1495629452.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4159735784
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   const uint32_t  f1;
+   const volatile uint32_t  f2;
+   const volatile uint64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const uint8_t g_2 = 6UL;
+static int8_t g_15 = 0x54L;
