67d1697e8c2d8ec2125cbbcc71d15611559154bc
saisi <saisi@users.noreply.github.com>
2017-05-24 07:07:04 +0000

    1495609624 Algonquian

---
+++ 1495609624.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      699433223
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 2;
+   unsigned f1 : 17;
+   const volatile signed f2 : 16;
+};
+#pragma pack(pop)
+
+union U1 {
