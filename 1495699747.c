f73e9398801e95103f24f22989d038757a68522a
saisi <saisi@users.noreply.github.com>
2017-05-25 08:09:07 +0000

    1495699747 amygdal

---
+++ 1495699747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2386291254
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 9;
+   const uint32_t  f1;
+   const volatile uint32_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = (-1L);
+static uint32_t g_18[2] = {4294967287UL,4294967287UL};
