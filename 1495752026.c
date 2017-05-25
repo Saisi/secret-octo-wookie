daa25f55ef6c9add5ea556bd33471fe1146c5a69
saisi <saisi@users.noreply.github.com>
2017-05-25 22:40:27 +0000

    1495752026 acroataxia

---
+++ 1495752026.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3011086576
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x69AED23BL;
+static int64_t g_8[8] = {0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL,0x084BC9C3D069F498LL};
+static uint64_t g_9 = 4UL;
+static int16_t g_19 = 0xC84BL;
+static volatile uint16_t g_20 = 0UL;/* VOLATILE GLOBAL g_20 */
+static uint8_t g_49 = 0xE2L;
+static uint16_t g_62[3] = {0x9BABL,0x9BABL,0x9BABL};
+static int32_t g_88[6] = {1L,1L,1L,1L,1L,1L};
+static int32_t * volatile g_87[1][5][5] = {{{&g_88[0],&g_6,&g_6,&g_88[0],&g_6},{&g_88[0],&g_88[0],&g_6,&g_88[0],&g_88[0]},{&g_6,&g_88[0],&g_6,&g_6,&g_88[0]},{&g_88[0],&g_6,&g_6,&g_88[0],&g_6},{&g_88[0],&g_88[0],&g_6,&g_88[0],&g_88[0]}}};
