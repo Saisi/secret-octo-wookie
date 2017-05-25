8e3d63cf92365b47c14971bb5035f79d35148abc
saisi <saisi@users.noreply.github.com>
2017-05-25 23:40:33 +0000

    1495755631 Bandjarmasin

---
+++ 1495755631.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      382894313
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint16_t g_10[10] = {0x9849L,0x9849L,0x9849L,0x9849L,0x9849L,0x9849L,0x9849L,0x9849L,0x9849L,0x9849L};
+static uint32_t g_19 = 0UL;
+static int8_t g_41 = 0xEEL;
+static int32_t g_43 = (-1L);
+static uint64_t g_47 = 0xFB6C717898A80146LL;
+static uint64_t g_73 = 0x1D73CB4EA99669D5LL;
+static int64_t g_86 = (-1L);
+static int32_t *g_94 = &g_43;
+static int32_t **g_93 = &g_94;
