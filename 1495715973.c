ca08cd9d5de275a53c0ddeec9d00359cf48d176d
saisi <saisi@users.noreply.github.com>
2017-05-25 12:39:34 +0000

    1495715973 aeneus

---
+++ 1495715973.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2202226106
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_14 = 0xAAL;
+static int8_t g_27[3] = {0x19L,0x19L,0x19L};
+static int16_t g_62 = (-1L);
+static uint16_t g_68 = 0x32E0L;
+static uint16_t g_84 = 65533UL;
+static uint16_t *g_83 = &g_84;
+static int32_t g_87 = 1L;
+static int32_t *g_86 = &g_87;
+static int64_t g_100[6][3] = {{5L,0L,5L},{0x1B6BEFC5446ED9A8LL,0xFD40FD77138BF9CCLL,0L},{0x1B6BEFC5446ED9A8LL,0x1B6BEFC5446ED9A8LL,0xFD40FD77138BF9CCLL},{5L,0xFD40FD77138BF9CCLL,0xFD40FD77138BF9CCLL},{0xFD40FD77138BF9CCLL,0L,0L},{5L,0L,5L}};
