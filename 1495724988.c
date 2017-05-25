45be1d20d2a427f5fe1817e8ff957c955c3847f5
saisi <saisi@users.noreply.github.com>
2017-05-25 15:09:48 +0000

    1495724988 airometer

---
+++ 1495724988.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2911473143
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 26;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_8 = 18446744073709551606UL;/* VOLATILE GLOBAL g_8 */
+static int64_t g_24 = 0L;
