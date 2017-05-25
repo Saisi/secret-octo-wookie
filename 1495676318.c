40558b8841eb3b928c0bba9cbd97c9eff29e0d04
saisi <saisi@users.noreply.github.com>
2017-05-25 01:38:38 +0000

    1495676318 anoia

---
+++ 1495676318.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      973977510
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   signed f1 : 18;
+   const int8_t * volatile  f2;
+   uint32_t  f3;
+   int8_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_6 = 5L;
