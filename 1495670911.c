139637c24ab010f31b4c0238ee8c7e0351943d0d
saisi <saisi@users.noreply.github.com>
2017-05-25 00:08:31 +0000

    1495670911 antiracing

---
+++ 1495670911.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      374085327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-5L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x4C506D44L;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
