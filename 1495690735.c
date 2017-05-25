e20b449e975cb9d062290f7433d3c8e79b98e7af
saisi <saisi@users.noreply.github.com>
2017-05-25 05:38:55 +0000

    1495690735 baaskaaps

---
+++ 1495690735.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2220362723
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 16;
+   volatile signed f1 : 7;
+   signed f2 : 14;
+   volatile unsigned f3 : 1;
+   const signed f4 : 14;
+   unsigned f5 : 15;
+   const unsigned f6 : 18;
+};
+
