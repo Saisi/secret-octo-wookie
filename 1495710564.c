3034a967a748ca757f381b8e92de66e22bd884a8
saisi <saisi@users.noreply.github.com>
2017-05-25 11:09:26 +0000

    1495710564 areologic

---
+++ 1495710564.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      520185360
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x52CB1363L;
+static volatile int32_t g_4[10] = {(-1L),0xB6182EACL,(-1L),(-1L),0xB6182EACL,(-1L),(-1L),0xB6182EACL,(-1L),(-1L)};
+static int32_t g_5 = 0xFF23B1A9L;
+static volatile int32_t g_6 = 9L;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x76C51653L;
+static int8_t g_15 = (-8L);
+static int8_t g_38 = 0xA3L;
+static int8_t *g_37 = &g_38;
+static uint8_t g_47 = 1UL;
