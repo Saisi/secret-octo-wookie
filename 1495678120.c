1c0962b2835fc867ad1f9fa916d6098351215c6e
saisi <saisi@users.noreply.github.com>
2017-05-25 02:08:40 +0000

    1495678120 armlock

---
+++ 1495678120.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1257654664
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 18;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   uint32_t  f0;
+   const volatile uint8_t  f1;
+   int32_t  f2;
