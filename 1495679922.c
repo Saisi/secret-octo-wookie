40164e82cb0913a2e92588f2104f97f6673d8d01
saisi <saisi@users.noreply.github.com>
2017-05-25 02:38:42 +0000

    1495679922 Balough

---
+++ 1495679922.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1150070801
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 7;
+   const signed f1 : 2;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 30;
+   signed f1 : 13;
