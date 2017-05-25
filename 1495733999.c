0527b1fdbb0755ac6da44bbe155e442b97749f47
saisi <saisi@users.noreply.github.com>
2017-05-25 17:39:59 +0000

    1495733999 alliterating

---
+++ 1495733999.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4167242875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_3 = 0L;
+static int32_t g_5 = 0x251C6076L;
+static int32_t * volatile g_4 = &g_5;/* VOLATILE GLOBAL g_4 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
+
