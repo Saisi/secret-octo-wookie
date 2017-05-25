d88dcf325c31615495fbc84787f0b945a15580b0
saisi <saisi@users.noreply.github.com>
2017-05-25 10:09:20 +0000

    1495706959 acloud

---
+++ 1495706959.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1190315675
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * const  volatile  f0;
+   uint64_t  f1;
+   volatile int8_t  f2;
+   const int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0xAFE01BD9L;
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
