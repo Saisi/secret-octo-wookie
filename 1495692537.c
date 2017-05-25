1ec26e7469cc6b18bf2bd78c47a7394f64c62796
saisi <saisi@users.noreply.github.com>
2017-05-25 06:08:57 +0000

    1495692537 Aberglaube

---
+++ 1495692537.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2464403943
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+   uint64_t  f1;
+   int16_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_8 = 4294967295UL;
+static uint8_t g_51 = 0x38L;
