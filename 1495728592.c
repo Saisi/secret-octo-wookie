82a5c586201e9cbed949eeed203f289766c2c07d
saisi <saisi@users.noreply.github.com>
2017-05-25 16:09:52 +0000

    1495728592 Analomink

---
+++ 1495728592.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3971320792
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t * f0;
+   const uint32_t  f1;
+   const int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_12 = 0xB9L;
+static int8_t *g_23 = (void*)0;
+static union U0 g_31 = {0};
