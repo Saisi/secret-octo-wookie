7ccbee7cd35466fa69f291f9f151552db8a781c8
saisi <saisi@users.noreply.github.com>
2017-05-25 14:09:43 +0000

    1495721381 abusage

---
+++ 1495721381.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      758234677
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 25;
+   volatile signed f1 : 12;
+   unsigned f2 : 31;
+   unsigned f3 : 14;
+   const unsigned f4 : 7;
+   const int8_t  f5;
+};
