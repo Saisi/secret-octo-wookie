dbb4ddf170ea6a64d3d482450ca8f85033e6f646
saisi <saisi@users.noreply.github.com>
2017-05-25 03:08:44 +0000

    1495681724 alkanal

---
+++ 1495681724.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1642515183
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U1 {
+   const int32_t  f0;
+   const uint32_t  f1;
+   uint8_t  f2;
+   volatile int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3[8] = {0x878ABC10L,0xF9D78659L,0x878ABC10L,0x878ABC10L,0xF9D78659L,0x878ABC10L,0x878ABC10L,0xF9D78659L};
+static int32_t *g_2[3] = {&g_3[6],&g_3[6],&g_3[6]};
