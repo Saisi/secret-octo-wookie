21f1bcb7f2c6ca9abaa5e0527faba9206b64cff0
saisi <saisi@users.noreply.github.com>
2017-05-25 23:10:29 +0000

    1495753829 balkan

---
+++ 1495753829.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3533539562
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 3;
+   signed f1 : 12;
+   unsigned f2 : 30;
+   unsigned f3 : 18;
+};
+#pragma pack(pop)
+
