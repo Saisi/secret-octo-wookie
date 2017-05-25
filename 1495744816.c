16ec482d4fe024f640d3ca054b881339fab321cb
saisi <saisi@users.noreply.github.com>
2017-05-25 20:40:16 +0000

    1495744816 ACHEFT

---
+++ 1495744816.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2057284021
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 25;
+   const volatile unsigned f1 : 12;
+   unsigned f2 : 31;
+   const volatile signed f3 : 14;
+   volatile signed f4 : 7;
+   uint16_t  f5;
+   unsigned f6 : 24;
