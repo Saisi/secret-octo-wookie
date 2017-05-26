79d77a3ff47f23e163c783cf072d1113ce3a1917
saisi <saisi@users.noreply.github.com>
2017-05-26 22:12:27 +0000

    1495836747 acouasm

---
+++ 1495836747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4011258246
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint32_t g_10[5] = {1UL,1UL,1UL,1UL,1UL};
+static int32_t g_11 = 0x3BF38C08L;
+static int8_t g_14 = 0x44L;
+static uint32_t g_18[3][7] = {{0UL,7UL,0xFBEFA5F9L,0xFBEFA5F9L,7UL,0UL,0x05B0BB8BL},{0UL,7UL,0xFBEFA5F9L,0xFBEFA5F9L,7UL,0UL,0x05B0BB8BL},{0UL,7UL,0xFBEFA5F9L,0xFBEFA5F9L,7UL,0UL,0x05B0BB8BL}};
+static uint16_t g_37[10] = {1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL};
+static uint32_t *g_38 = (void*)0;
+static int8_t g_40 = 0xF7L;
+static int8_t * const g_39 = &g_40;
+static uint16_t g_51 = 0x5869L;
