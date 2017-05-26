2ddb014550eb8eee94c02de41993d2c27f68c296
saisi <saisi@users.noreply.github.com>
2017-05-26 23:12:32 +0000

    1495840351 Amalfitan

---
+++ 1495840351.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      391751698
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   unsigned f1 : 16;
+};
+
+struct S1 {
+   unsigned f0 : 26;
+   unsigned f1 : 23;
+   const unsigned f2 : 7;
+   unsigned f3 : 12;
