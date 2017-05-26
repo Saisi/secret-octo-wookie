b5d03f66c7a8bcd1efa3785a347b4c3c04366af8
saisi <saisi@users.noreply.github.com>
2017-05-26 12:11:36 +0000

    1495800696 anakinesis

---
+++ 1495800696.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3020029041
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 23;
+   signed f1 : 18;
+   volatile signed f2 : 13;
+   unsigned f3 : 4;
+   int64_t  f4;
+   const volatile signed f5 : 23;
+   volatile signed f6 : 30;
+   volatile unsigned : 0;
+   volatile unsigned f7 : 15;
