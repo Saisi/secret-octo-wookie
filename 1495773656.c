bffd01e63f2cfa55cff470692a7cf25e4b15e768
saisi <saisi@users.noreply.github.com>
2017-05-26 04:40:56 +0000

    1495773656 antiflash

---
+++ 1495773656.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4208396891
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_36[4] = {254UL,254UL,254UL,254UL};
+static int16_t g_47 = 0x6378L;
+static uint32_t g_67 = 18446744073709551606UL;
+static int16_t g_85 = (-1L);
+static int32_t g_87 = 0L;
+static int8_t g_106 = (-1L);
+static uint8_t g_108[9] = {3UL,3UL,3UL,3UL,3UL,3UL,3UL,3UL,3UL};
+static int64_t g_141[10] = {1L,0L,1L,0L,1L,0L,1L,0L,1L,0L};
+static int32_t * volatile g_148 = &g_87;/* VOLATILE GLOBAL g_148 */
