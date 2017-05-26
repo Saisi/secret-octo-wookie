1043d7b32fc0842fd33c51be34496418ffd5693a
saisi <saisi@users.noreply.github.com>
2017-05-26 08:41:18 +0000

    1495788077 abolishes

---
+++ 1495788077.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1246580536
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 10;
+   signed f1 : 23;
+   volatile int32_t  f2;
+   int64_t  f3;
+   volatile signed f4 : 25;
+   volatile unsigned f5 : 23;
+   const signed : 0;
+};
+
