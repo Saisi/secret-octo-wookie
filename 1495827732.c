7f2ce0c68eb9869aae70fc3c9ccb3da3f6063537
saisi <saisi@users.noreply.github.com>
2017-05-26 19:42:12 +0000

    1495827732 apivorous

---
+++ 1495827732.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1926339075
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   signed f0 : 23;
+   uint16_t  f1;
+   uint16_t  f2;
+   const int16_t  f3;
+   int16_t  f4;
+};
+
+union U1 {
+   signed f0 : 30;
