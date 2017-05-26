4a548f78475c5c699df139e406dd6ac493777059
saisi <saisi@users.noreply.github.com>
2017-05-26 20:12:16 +0000

    1495829534 Araneiformia

---
+++ 1495829534.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1771009651
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+};
+#pragma pack(pop)
+
+union U1 {
+   const uint8_t  f0;
+   signed f1 : 7;
