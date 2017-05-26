ba0b011f5fe9367542eabb11d66c8e5231f4e63d
saisi <saisi@users.noreply.github.com>
2017-05-26 13:41:43 +0000

    1495806102 Addressograph

---
+++ 1495806102.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      128128814
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_9 = 0xFBL;
+static int8_t g_76[2][7] = {{0x91L,0x91L,0x91L,0x91L,0x91L,0x91L,0x91L},{0xAFL,0x3EL,0xAFL,0x3EL,0xAFL,0x3EL,0xAFL}};
+static uint8_t g_78 = 0x26L;
+static int8_t g_81 = 0xD3L;
+static int32_t g_83 = (-5L);
+static uint16_t g_88 = 0UL;
+static uint16_t g_91 = 1UL;
+static volatile uint16_t g_96 = 0xF2A7L;/* VOLATILE GLOBAL g_96 */
+static int32_t *g_101 = &g_83;
