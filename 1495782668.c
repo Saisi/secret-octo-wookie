5517df6077d12ca03a44c0c0271b8eb4d2fe9671
saisi <saisi@users.noreply.github.com>
2017-05-26 07:11:08 +0000

    1495782668 adsessor

---
+++ 1495782668.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3496121265
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 23;
+   const signed f1 : 18;
+   unsigned f2 : 13;
+   volatile signed f3 : 4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2 = 0x95E17CA0L;
+static uint64_t g_30 = 18446744073709551615UL;
