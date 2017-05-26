6a347247d0c3d142c4afd8bf8784859837964f6e
saisi <saisi@users.noreply.github.com>
2017-05-26 18:12:06 +0000

    1495822325 astringer

---
+++ 1495822325.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      564636492
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   uint32_t  f1;
+   volatile uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFE04822BL;
+static volatile int32_t g_4 = 0x1F07B4BCL;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = 0L;
