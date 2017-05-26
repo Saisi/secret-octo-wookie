5f3beeda8204c5aa9294ab6cf7836cb3913d4af3
saisi <saisi@users.noreply.github.com>
2017-05-26 17:11:58 +0000

    1495818718 animalization

---
+++ 1495818718.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1516992843
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 4;
+   const volatile signed f1 : 11;
+   const int8_t  f2;
+   volatile signed f3 : 19;
+   const volatile unsigned f4 : 14;
+   unsigned f5 : 4;
+   const signed f6 : 23;
+};
+
