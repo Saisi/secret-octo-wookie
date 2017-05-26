22fea4388e49e1c9b5378a565ed394d5a60bd0d7
saisi <saisi@users.noreply.github.com>
2017-05-26 15:11:49 +0000

    1495811509 afterchrome

---
+++ 1495811509.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3923305434
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int32_t  f0;
+   uint32_t  f1;
+   const volatile int64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xFEBB6748L;
+static int32_t g_6 = 0L;
+static int32_t * volatile g_5 = &g_6;/* VOLATILE GLOBAL g_5 */
