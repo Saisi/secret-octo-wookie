1f34561c71c6f10a0512f48c22f4ff692b5c4781
saisi <saisi@users.noreply.github.com>
2017-05-26 16:41:56 +0000

    1495816915 Ancius

---
+++ 1495816915.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1120939823
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   signed f1 : 21;
+   volatile uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static const int32_t g_5 = 0xA6B950BAL;
+static int32_t g_7 = (-1L);
+static int32_t g_11 = 1L;
