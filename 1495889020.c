7bd39b351159cbb5237b9816ed9bc564d00a604a
saisi <saisi@users.noreply.github.com>
2017-05-27 12:43:40 +0000

    1495889020 anemobiagraph

---
+++ 1495889020.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2884130782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   uint32_t  f0;
+   volatile unsigned f1 : 26;
+   volatile uint64_t  f2;
+   uint32_t  f3;
+   volatile int32_t  f4;
+   uint8_t  f5;
+};
+
+/* --- GLOBAL VARIABLES --- */
