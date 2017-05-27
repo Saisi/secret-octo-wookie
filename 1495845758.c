fd24c54c17c66d0e80feb84518095ba2f621a713
saisi <saisi@users.noreply.github.com>
2017-05-27 00:42:39 +0000

    1495845758 archchemic

---
+++ 1495845758.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1936941523
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 0UL;/* VOLATILE GLOBAL g_2 */
+static uint16_t g_8 = 0UL;
+static uint16_t g_16 = 8UL;
+static uint32_t g_22 = 4294967286UL;
+static int32_t g_54 = 0xD3610CB4L;
