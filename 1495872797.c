fde6bde977baec49b97f3f5c74dac76d05bef0b8
saisi <saisi@users.noreply.github.com>
2017-05-27 08:13:17 +0000

    1495872797 amylocellulose

---
+++ 1495872797.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1517479170
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_15[9] = {8L,8L,8L,8L,8L,8L,8L,8L,8L};
+static int32_t g_16 = 0x15E314A5L;
+static int32_t g_50 = 0xA72CB566L;
+static uint32_t g_52 = 0xD4033E27L;
+static volatile int32_t g_60 = 0xFAADAD2DL;/* VOLATILE GLOBAL g_60 */
+static volatile int32_t *g_59 = &g_60;
+static int32_t g_70 = 0L;
+static int32_t g_74 = (-1L);
+static int64_t g_83 = 1L;
