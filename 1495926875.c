87e118a2ff1116325e49ed5726ae8de927d059e7
saisi <saisi@users.noreply.github.com>
2017-05-27 23:14:35 +0000

    1495926875 areel

---
+++ 1495926875.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3126362497
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint32_t  f0;
+   uint8_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[3][1][2] = {{{1L,1L}},{{1L,1L}},{{1L,1L}}};
+static int32_t g_12[7] = {0x900FDB69L,(-1L),0x900FDB69L,0x900FDB69L,(-1L),0x900FDB69L,0x900FDB69L};
+static int32_t * volatile g_11 = &g_12[4];/* VOLATILE GLOBAL g_11 */
