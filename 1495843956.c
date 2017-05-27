988caf770ef5b4c1af677ccf7b9881190b5e7c58
saisi <saisi@users.noreply.github.com>
2017-05-27 00:12:37 +0000

    1495843956 amoebian

---
+++ 1495843956.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2274189198
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_4 = {4UL};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static union U0  func_1(void);
