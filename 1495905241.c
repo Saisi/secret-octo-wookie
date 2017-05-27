2f7a4a117959598fff6e0a9b6a2eb5101ad84f45
saisi <saisi@users.noreply.github.com>
2017-05-27 17:14:01 +0000

    1495905241 animate

---
+++ 1495905241.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3375143766
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[9] = {0x3EC3D4E7L,(-1L),0x3EC3D4E7L,0x3EC3D4E7L,(-1L),0x3EC3D4E7L,0x3EC3D4E7L,(-1L),0x3EC3D4E7L};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
