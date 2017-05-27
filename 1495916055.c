01288da2dae0ca237fbe3f1fa2848d782e2040aa
saisi <saisi@users.noreply.github.com>
2017-05-27 20:14:15 +0000

    1495916055 autoharp

---
+++ 1495916055.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1909344443
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   volatile int8_t * volatile  f1;
+   volatile int32_t  f2;
+   uint16_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xA2AFE01BL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xA888ADAEL;/* VOLATILE GLOBAL g_3 */
