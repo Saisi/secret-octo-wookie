71eacc4311f5e3e5e8bf99565541839f922f0753
saisi <saisi@users.noreply.github.com>
2017-05-27 11:43:35 +0000

    1495885414 aetiogenic

---
+++ 1495885414.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4141711905
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   unsigned f0 : 15;
+   volatile uint64_t  f1;
+};
+#pragma pack(pop)
+
+#pragma pack(push)
+#pragma pack(1)
