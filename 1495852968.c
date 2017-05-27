63a103800ceccbadd0228c0fee085ef0c3f878e8
saisi <saisi@users.noreply.github.com>
2017-05-27 02:42:48 +0000

    1495852968 autocoid

---
+++ 1495852968.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      752043301
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile signed f0 : 17;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_7 = 0x50E6L;
+static int32_t g_27[8] = {0x35492233L,0x35492233L,0x35492233L,0x35492233L,0x35492233L,0x35492233L,0x35492233L,0x35492233L};
+static int16_t g_30 = (-1L);
+static uint32_t g_49 = 4294967290UL;
