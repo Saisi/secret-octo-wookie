6215c30edd1eb5fb4d4c544c2c7f13a401707077
saisi <saisi@users.noreply.github.com>
2017-05-27 04:12:56 +0000

    1495858376 Babbitt

---
+++ 1495858376.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3839723905
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile uint16_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_14 = 18446744073709551615UL;/* VOLATILE GLOBAL g_14 */
+static uint64_t g_19 = 18446744073709551610UL;
+static uint8_t g_32 = 0xE3L;
+static int32_t g_34[6][1] = {{(-1L)},{0xBC23CEA2L},{(-1L)},{0xBC23CEA2L},{(-1L)},{0xBC23CEA2L}};
+static int8_t g_59[1][6][10] = {{{0x43L,0x86L,0x44L,0x86L,0x43L,0xCEL,0xCEL,0x43L,0x86L,0x44L},{(-6L),(-6L),0x44L,0x43L,0x9DL,0x43L,0x44L,(-6L),(-6L),0x44L},{0x86L,0x43L,0xCEL,0xCEL,0x43L,0x86L,0x44L,0x86L,0x43L,0xCEL},{5L,(-6L),5L,0xCEL,0x44L,0x44L,0xCEL,5L,(-6L),5L},{0x43L,5L,0x9DL,0xCEL,0x9DL,5L,0x43L,0x43L,5L,0x9DL},{5L,0x43L,0x43L,5L,0x9DL,0xCEL,0x9DL,5L,0x43L,0x43L}}};
