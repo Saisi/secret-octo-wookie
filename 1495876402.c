3dd2d30fa6179e116b84ba62510874b285666052
saisi <saisi@users.noreply.github.com>
2017-05-27 09:13:22 +0000

    1495876402 arnica

---
+++ 1495876402.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2918739485
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x00FE1CA2L;
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0xEC4279C4L;/* VOLATILE GLOBAL g_5 */
+static volatile int32_t g_6 = 3L;/* VOLATILE GLOBAL g_6 */
+static volatile int32_t g_7 = 0x47B80BE3L;/* VOLATILE GLOBAL g_7 */
+static volatile int32_t g_8[8] = {0xCF95751CL,0xCF95751CL,0xCF95751CL,0xCF95751CL,0xCF95751CL,0xCF95751CL,0xCF95751CL,0xCF95751CL};
+static volatile int32_t g_9 = 0x3E7ED6F1L;/* VOLATILE GLOBAL g_9 */
+static volatile int32_t g_10 = (-10L);/* VOLATILE GLOBAL g_10 */
+static int32_t g_11 = (-1L);
