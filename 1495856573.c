55216e64eba4ed10a35272eda26e2e16021d4327
saisi <saisi@users.noreply.github.com>
2017-05-27 03:42:54 +0000

    1495856573 antherozoid

---
+++ 1495856573.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2210823732
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int16_t  f1;
+   int8_t * f2;
+   uint32_t  f3;
+};
+
+union U1 {
+   volatile int32_t  f0;
+   const volatile uint32_t  f1;
