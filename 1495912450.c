305af7bd92f1ca8149d73eb8f9e4af4fef19797a
saisi <saisi@users.noreply.github.com>
2017-05-27 19:14:11 +0000

    1495912450 axonal

---
+++ 1495912450.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1899626040
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int64_t  f0;
+   int64_t  f1;
+   signed f2 : 29;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_2[4] = {0xA7C20398L,0xA7C20398L,0xA7C20398L,0xA7C20398L};
+static volatile int32_t g_3 = (-7L);/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0L;/* VOLATILE GLOBAL g_4 */
