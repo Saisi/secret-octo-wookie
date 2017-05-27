bf4a4de3534688e2b30622a0983ac23ac206fab8
saisi <saisi@users.noreply.github.com>
2017-05-27 23:44:37 +0000

    1495928677 acrosarcum

---
+++ 1495928677.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2668969568
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x3185299DL;
+static volatile uint32_t g_6[7] = {5UL,5UL,5UL,5UL,5UL,5UL,5UL};
+static uint16_t g_37 = 0x0B91L;
+static uint16_t *g_36 = &g_37;
+static uint16_t g_59 = 0x53DDL;
+static uint16_t g_61 = 0xD4C3L;
+static int32_t g_64[8] = {(-10L),(-10L),(-10L),(-10L),(-10L),(-10L),(-10L),(-10L)};
+static const uint16_t **g_85 = (void*)0;
+static int8_t g_97 = 0x6DL;
