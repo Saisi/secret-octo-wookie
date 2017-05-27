7afbec00adc6f8b247a20a049617f0c06387b494
saisi <saisi@users.noreply.github.com>
2017-05-27 15:13:51 +0000

    1495898031 ballock

---
+++ 1495898031.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      561681116
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x1A049FE3L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xB0B02971L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = 0xC2DB2212L;/* VOLATILE GLOBAL g_4 */
+static int32_t g_5 = (-1L);
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
