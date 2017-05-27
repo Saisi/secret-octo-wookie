07d9de7907d779f4095cda33dceeb05b1f5a6abe
saisi <saisi@users.noreply.github.com>
2017-05-27 06:13:07 +0000

    1495865587 alienicola

---
+++ 1495865587.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1387915347
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 8;
+   unsigned f1 : 31;
+   const unsigned f2 : 6;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   struct S0  f0;
