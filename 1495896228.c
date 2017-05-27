229bc6461e6f1ccaf2fbf7fd4b7018bd3220ca5d
saisi <saisi@users.noreply.github.com>
2017-05-27 14:43:50 +0000

    1495896228 aeronomics

---
+++ 1495896228.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2961429338
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4 = 1UL;
+static int32_t g_9 = (-1L);
+static uint64_t g_27 = 18446744073709551610UL;
+static int32_t g_85 = (-1L);
+static volatile int64_t g_87[8][1] = {{0L},{0x98E57B790EB5CFE7LL},{0L},{0x98E57B790EB5CFE7LL},{0L},{0x98E57B790EB5CFE7LL},{0L},{0x98E57B790EB5CFE7LL}};
+static volatile int32_t g_88[9] = {0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L,0xC0C20EB8L};
+static int8_t g_89 = 0x8AL;
+static volatile uint32_t g_90 = 0x9823142AL;/* VOLATILE GLOBAL g_90 */
+static int8_t g_101 = 0xE4L;
