8de5d9eb518775f9c751007277278b8f881c4aef
saisi <saisi@users.noreply.github.com>
2017-05-28 19:16:09 +0000

    1495998969 apoop

---
+++ 1495998969.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      556131803
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int8_t g_2 = 0xA9L;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4[3] = {0xBECBA2AFL,0xBECBA2AFL,0xBECBA2AFL};
+static volatile int32_t * volatile g_3 = &g_4[0];/* VOLATILE GLOBAL g_3 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
+
+
