0aac9c4f92327b13f8e5ddd94eee76bdfaf2c1f0
saisi <saisi@users.noreply.github.com>
2017-05-28 08:15:20 +0000

    1495959319 anthratetrol

---
+++ 1495959319.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      885941871
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t *g_8 = (void*)0;
+static const int32_t g_21 = 0L;
+static int16_t g_23[8] = {0L,0L,0L,0L,0L,0L,0L,0L};
+static int16_t g_25[10] = {0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L,0x88F4L};
+static const int32_t *g_35 = &g_21;
+static const int32_t **g_34 = &g_35;
+static uint16_t g_49 = 0UL;
+static int32_t g_52 = 1L;
+static int32_t * volatile g_54 = (void*)0;/* VOLATILE GLOBAL g_54 */
