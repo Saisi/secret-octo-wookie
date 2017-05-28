9eb1760f00cda875e2064456a7bba45b3eca2bc7
saisi <saisi@users.noreply.github.com>
2017-05-28 07:15:14 +0000

    1495955714 Amycus

---
+++ 1495955714.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2846025920
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   uint32_t  f1;
+};
+
+union U1 {
+   const volatile signed f0 : 21;
+};
+
+union U2 {
