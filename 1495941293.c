f10fb62d4aff76da530e81f5b8b15b3b3bd5515a
saisi <saisi@users.noreply.github.com>
2017-05-28 03:14:54 +0000

    1495941293 arthrotyphoid

---
+++ 1495941293.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2325128357
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const volatile int8_t  f0;
+   uint8_t  f1;
+   const volatile uint32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_8 = 0xAD2086BDL;
+static uint16_t g_19 = 0x13B2L;
+static int16_t g_25 = 0x0D13L;
