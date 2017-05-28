fbcd7a5cb3ff03716868b09a0a1f713d0a45fbc5
saisi <saisi@users.noreply.github.com>
2017-05-28 18:16:05 +0000

    1495995364 aircrafts

---
+++ 1495995364.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      805261668
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   uint32_t  f1;
+   volatile int8_t * f2;
+   volatile unsigned f3 : 15;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_6 = 4294967295UL;/* VOLATILE GLOBAL g_6 */
+static uint16_t g_16 = 65529UL;
