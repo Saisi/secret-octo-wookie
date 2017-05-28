74a52ff61541699070bcb40693580b0f0752e23e
saisi <saisi@users.noreply.github.com>
2017-05-28 12:15:37 +0000

    1495973737 Apneumona

---
+++ 1495973737.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3045130910
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile uint32_t  f0;
+   const int64_t  f1;
+   const volatile unsigned f2 : 21;
+   int8_t  f3;
+   uint8_t  f4;
+   unsigned f5 : 22;
+   volatile int64_t  f6;
