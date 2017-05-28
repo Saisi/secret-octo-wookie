0d2e95522449473fe5ab81ddcdeaa9b108a587b3
saisi <saisi@users.noreply.github.com>
2017-05-28 14:45:48 +0000

    1495982747 acetylaniline

---
+++ 1495982747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3353872702
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int8_t * const  volatile  f1;
+   uint64_t  f2;
+   volatile int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_20 = 2L;
+static volatile int32_t g_34 = 5L;/* VOLATILE GLOBAL g_34 */
