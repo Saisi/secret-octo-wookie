995d7dccd1766655bc05f239ec8cca42e97c6e6e
saisi <saisi@users.noreply.github.com>
2017-05-28 09:45:26 +0000

    1495964726 aerophagist

---
+++ 1495964726.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2329733418
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint16_t g_5 = 0xBB0CL;
+static uint16_t g_20 = 65535UL;
+static uint16_t *g_19[6][1] = {{&g_20},{&g_20},{&g_20},{&g_20},{&g_20},{&g_20}};
+static volatile int32_t g_23 = 1L;/* VOLATILE GLOBAL g_23 */
+static int32_t g_25 = 0x18461E70L;
+static uint32_t g_27[1] = {4294967287UL};
+static int32_t * volatile g_31 = &g_25;/* VOLATILE GLOBAL g_31 */
+static volatile uint8_t g_35[8] = {0x2CL,0x2CL,0x2CL,0x2CL,0x2CL,0x2CL,0x2CL,0x2CL};
+
