c050f54e87858899a6bfa065e8d859d52116502c
saisi <saisi@users.noreply.github.com>
2017-05-28 19:46:11 +0000

    1496000770 ARCNET

---
+++ 1496000770.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4169081209
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 27;
+   const signed f1 : 6;
+   volatile unsigned f2 : 17;
+   unsigned f3 : 24;
+   unsigned f4 : 9;
+   signed f5 : 30;
+};
+
+/* --- GLOBAL VARIABLES --- */
