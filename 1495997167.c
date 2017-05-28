c7dec78cf64175a304c9922a895800499e139a4f
saisi <saisi@users.noreply.github.com>
2017-05-28 18:46:07 +0000

    1495997167 Adamek

---
+++ 1495997167.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      982170073
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xE446DF37L;
+static uint8_t g_5 = 0UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static const uint64_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
