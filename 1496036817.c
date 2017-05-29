3a905455340dc7acd217ef025ce3afac98580a9e
saisi <saisi@users.noreply.github.com>
2017-05-29 05:46:57 +0000

    1496036817 airlock

---
+++ 1496036817.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3118340478
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x3AE35568L;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0x04CD4536L;
+static int32_t g_10 = (-1L);
+static int32_t * volatile g_9 = &g_10;/* VOLATILE GLOBAL g_9 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int16_t  func_1(void);
+
