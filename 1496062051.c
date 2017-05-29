0e38e9216435a04cad4a5f3b135ea578aa6219c9
saisi <saisi@users.noreply.github.com>
2017-05-29 12:47:31 +0000

    1496062051 agelast

---
+++ 1496062051.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3918625665
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_3 = 0xCE125D16L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t * const g_2[4][9][6] = {{{&g_3,&g_3,(void*)0,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_3,(void*)0,&g_3},{(void*)0,(void*)0,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0},{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3}},{{(void*)0,&g_3,&g_3,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3}},{{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,&g_3,&g_3,(void*)0,&g_3,(void*)0},{&g_3,&g_3,(void*)0,(void*)0,(void*)0,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,(void*)0,&g_3,&g_3},{&g_3,(void*)0,&g_3,&g_3,&g_3,(void*)0},{&g_3,&g_3,(void*)0,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3}},{{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,(void*)0,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3},{(void*)0,&g_3,&g_3,&g_3,(void*)0,&g_3},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0},{&g_3,&g_3,&g_3,&g_3,&g_3,(void*)0},{(void*)0,&g_3,&g_3,&g_3,&g_3,&g_3}}};
+static volatile int32_t *g_5 = &g_3;
+static volatile int32_t ** volatile g_4[1] = {&g_5};
+static volatile int32_t ** volatile g_8 = &g_5;/* VOLATILE GLOBAL g_8 */
+static int32_t g_10 = 0x405732AAL;
+static int32_t g_14 = 0xA67F8B4DL;
+static int32_t * volatile g_13[7][1] = {{&g_14},{&g_14},{&g_14},{&g_14},{&g_14},{&g_14},{&g_14}};
+static int32_t * volatile g_15 = (void*)0;/* VOLATILE GLOBAL g_15 */
