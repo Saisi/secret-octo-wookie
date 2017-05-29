e1083071d3e97bf740cb07477427b624ee746588
saisi <saisi@users.noreply.github.com>
2017-05-29 20:48:09 +0000

    1496090888 aphanesite

---
+++ 1496090888.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1087985272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const unsigned f0 : 10;
+   unsigned f1 : 23;
+   const unsigned f2 : 29;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
