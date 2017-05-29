46d69bc299833939db08d41d2ca8d1ca0822eba8
saisi <saisi@users.noreply.github.com>
2017-05-29 13:17:33 +0000

    1496063853 anticosine

---
+++ 1496063853.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4154108542
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint32_t  f0;
+   uint8_t  f1;
+   uint16_t  f2;
+   int8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x05D09EA9L;
+static int32_t *g_2 = &g_3;
