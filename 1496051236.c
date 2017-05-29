9f81df50237ab6170732c7262cf2028e32319cb8
saisi <saisi@users.noreply.github.com>
2017-05-29 09:47:16 +0000

    1496051236 anteambulation

---
+++ 1496051236.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2232314962
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_12 = (-1L);
+static int16_t g_13 = 0x5995L;
+static uint8_t g_58 = 0x70L;
+static uint32_t g_70 = 4294967295UL;
+static uint32_t g_75 = 0x38C46158L;
+static int8_t g_96 = 0L;
+static uint8_t g_104 = 252UL;
+static uint16_t g_129 = 0UL;
+static uint32_t g_139[4][10] = {{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL},{1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL,1UL}};
