0d625f68e09a5b20d5c370cf00c85eb3fe2e8774
saisi <saisi@users.noreply.github.com>
2017-05-29 14:17:38 +0000

    1496067458 anableps

---
+++ 1496067458.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      985606994
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 23;
+   const volatile unsigned f1 : 16;
+   const volatile int16_t  f2;
+   const volatile int32_t  f3;
+   unsigned f4 : 23;
+   signed f5 : 7;
+   volatile signed f6 : 12;
