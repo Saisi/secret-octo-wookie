004399ac50aebb4baa15fa9105dafef079ec71c3
saisi <saisi@users.noreply.github.com>
2017-05-29 17:47:54 +0000

    1496080073 anoplotherioid

---
+++ 1496080073.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3160783674
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 11;
+   signed f1 : 20;
+   uint16_t  f2;
+   unsigned f3 : 14;
+   signed f4 : 27;
+   volatile signed f5 : 27;
+   unsigned f6 : 16;
+   signed f7 : 9;
+   signed f8 : 22;
