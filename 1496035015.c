f3b642dbf054029ee96bde32b446332c2b70ade8
saisi <saisi@users.noreply.github.com>
2017-05-29 05:16:55 +0000

    1496035015 acidoses

---
+++ 1496035015.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2481866507
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static const volatile uint32_t g_12[2][9] = {{7UL,0xF7A73D2DL,7UL,7UL,0xF7A73D2DL,7UL,7UL,0xF7A73D2DL,7UL},{0x3F0BC502L,18446744073709551615UL,0x3F0BC502L,0x3F0BC502L,18446744073709551615UL,0x3F0BC502L,0x3F0BC502L,18446744073709551615UL,0x3F0BC502L}};
+static int8_t g_14 = 0xCCL;
+static uint8_t g_16 = 1UL;
+static volatile uint16_t *g_28 = (void*)0;
+static uint16_t g_53 = 0x4153L;
+static uint32_t g_60 = 4294967286UL;
+static int32_t g_66 = 0x265D2828L;
+static const uint16_t **g_68 = (void*)0;
+static const int32_t *g_137 = (void*)0;
