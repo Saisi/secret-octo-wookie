c5e3bc812b02668cae35b446d09af0f7687d4d58
saisi <saisi@users.noreply.github.com>
2017-05-29 01:46:38 +0000

    1496022397 autocatheterism

---
+++ 1496022397.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3303405597
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_5 = 0x837BC752L;
+static int32_t *g_4 = &g_5;
+static int64_t g_11 = 0L;
+static uint32_t g_14 = 0x8CF5A8B0L;
+static int32_t g_16 = 0x927F7B6FL;
+static uint32_t g_33[9][4][1] = {{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}},{{0xC7279C90L},{0xBB1300CAL},{0xC7279C90L},{0xBB1300CAL}}};
+static uint64_t g_55 = 0x717C264F89C89462LL;
+static uint8_t g_59 = 0x57L;
+static uint16_t g_68[5] = {0x4437L,0x4437L,0x4437L,0x4437L,0x4437L};
