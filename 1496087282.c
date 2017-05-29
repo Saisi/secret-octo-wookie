d92945f194004d49eedccd90bb2496fd27b1e71d
saisi <saisi@users.noreply.github.com>
2017-05-29 19:48:03 +0000

    1496087282 Auberta

---
+++ 1496087282.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      281051010
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int8_t  f0;
+   const volatile int8_t  f1;
+   const volatile uint16_t  f2;
+   volatile uint8_t  f3;
+   signed f4 : 30;
+   const int32_t  f5;
+   uint32_t  f6;
+   signed f7 : 6;
+   const int8_t  f8;
