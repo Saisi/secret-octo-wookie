616e7c903ecef6d9924cb95aee8e8805d4a366b7
saisi <saisi@users.noreply.github.com>
2017-05-29 07:17:06 +0000

    1496042225 asperulous

---
+++ 1496042225.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2677494284
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int64_t  f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_9 = 0L;
+static int32_t g_11 = (-1L);
+static int32_t g_17 = 0x9D089051L;
+static uint32_t g_18[2] = {0x7E3B6F9BL,0x7E3B6F9BL};
+static uint16_t g_22 = 0x3718L;
