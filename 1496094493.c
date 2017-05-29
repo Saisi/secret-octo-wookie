6424dcbd36bc3f247407df867562e4245fb76cbf
saisi <saisi@users.noreply.github.com>
2017-05-29 21:48:13 +0000

    1496094493 archaeus

---
+++ 1496094493.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1527225972
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   signed f0 : 5;
+   signed f1 : 3;
+   volatile unsigned f2 : 28;
+   const unsigned f3 : 24;
+   signed f4 : 25;
+   unsigned f5 : 14;
+   volatile signed f6 : 7;
