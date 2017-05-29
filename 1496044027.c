23f0ddd799580eb08093a036fe604596d19d4d44
saisi <saisi@users.noreply.github.com>
2017-05-29 07:47:08 +0000

    1496044027 arrie

---
+++ 1496044027.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2546504142
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int32_t  f0;
+   uint16_t  f1;
+   const int64_t  f2;
+   int32_t  f3;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
