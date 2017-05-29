9b5deb0c0fc275aab1cbe8ac83dae43a2120ef13
saisi <saisi@users.noreply.github.com>
2017-05-29 13:47:36 +0000

    1496065655 abondance

---
+++ 1496065655.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      335887051
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile uint16_t  f0;
+   unsigned f1 : 9;
+   const int16_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_4[10][5] = {{1L,0xDFBDL,1L,0x0BC5L,0xDFBDL},{0x2D67L,0x58BDL,3L,0x2D67L,3L},{(-1L),(-1L),1L,0xDFBDL,(-7L)},{(-1L),1L,3L,3L,1L},{(-7L),(-8L),1L,(-7L),0x0BC5L},{(-7L),1L,0x1A7EL,1L,(-7L)},{1L,(-1L),(-8L),0x0BC5L,(-1L)},{(-7L),0x58BDL,0x58BDL,(-7L),3L},{(-7L),0xDFBDL,1L,(-1L),(-1L)},{(-1L),0x58BDL,(-3L),1L,0x58BDL}};
+static int32_t g_6 = (-6L);
+static int32_t g_8 = 9L;
