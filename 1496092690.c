56879484c11a1d22de3ad614cabac143157e4bb1
saisi <saisi@users.noreply.github.com>
2017-05-29 21:18:11 +0000

    1496092690 actinoelectric

---
+++ 1496092690.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      658023872
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 14;
+   volatile uint32_t  f1;
+   volatile signed f2 : 3;
+   unsigned f3 : 25;
+   volatile signed f4 : 26;
+   unsigned f5 : 30;
+   int32_t  f6;
+   unsigned f7 : 11;
+   unsigned f8 : 6;
