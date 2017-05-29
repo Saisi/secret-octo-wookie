55082e9fb02db7caefcabd8e81e900c2b549241f
saisi <saisi@users.noreply.github.com>
2017-05-29 14:47:40 +0000

    1496069260 apollo

---
+++ 1496069260.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1340013021
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int64_t g_22 = 0x1860EC01E6E2BFBDLL;
+static uint8_t g_35 = 4UL;
+static int32_t g_38 = 0x9796A0B5L;
+static int16_t g_69 = 8L;
+static int32_t g_73[5] = {0xF27779E4L,0xF27779E4L,0xF27779E4L,0xF27779E4L,0xF27779E4L};
+static int32_t g_82 = 0x72F548F9L;
+static uint16_t g_198 = 0x6C0DL;
+static uint32_t g_202 = 0x0D3CD378L;
+static int32_t *g_210[6] = {&g_73[4],&g_73[3],&g_73[3],&g_73[4],&g_73[3],&g_73[3]};
