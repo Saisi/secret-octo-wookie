75bc6d1e5edbbbcb17ba5e88079d88c337e972df
saisi <saisi@users.noreply.github.com>
2017-05-29 23:18:20 +0000

    1496099899 allogenic

---
+++ 1496099899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      819622956
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 4;
+   volatile uint32_t  f1;
+   volatile signed f2 : 1;
+   signed f3 : 31;
+   const unsigned f4 : 8;
+   volatile uint16_t  f5;
+   const unsigned f6 : 26;
+};
+
