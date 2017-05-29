2d4a95d9566643fc3983e750f8bdd9ea85c1112e
saisi <saisi@users.noreply.github.com>
2017-05-29 11:17:24 +0000

    1496056642 babka

---
+++ 1496056642.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3333520471
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t  f0;
+   int8_t * const  f1;
+   const int16_t  f2;
+   uint8_t  f3;
+};
+
+union U1 {
+   unsigned f0 : 20;
+   const uint32_t  f1;
