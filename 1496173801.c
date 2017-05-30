69519f47a7d0ba86b454eea1575fcde04797ede6
saisi <saisi@users.noreply.github.com>
2017-05-30 19:50:01 +0000

    1496173801 anemochoric

---
+++ 1496173801.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3600707976
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile signed f0 : 18;
+   unsigned f1 : 31;
+   int32_t  f2;
+   unsigned f3 : 29;
+   const signed f4 : 14;
+   volatile signed f5 : 2;
+   unsigned f6 : 27;
