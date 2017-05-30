fe0f1b2a3dd5ff72ca39f322253cd0ef9c0ecbe2
saisi <saisi@users.noreply.github.com>
2017-05-30 11:19:21 +0000

    1496143159 algarrobin

---
+++ 1496143159.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2231681719
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x3905C809L;
+static uint16_t g_12 = 0xD686L;
+static volatile uint16_t g_55 = 0xB430L;/* VOLATILE GLOBAL g_55 */
+static volatile uint16_t * volatile g_54 = &g_55;/* VOLATILE GLOBAL g_54 */
+static uint16_t g_73 = 0xB464L;
+static uint64_t g_87 = 18446744073709551615UL;
+static int16_t g_111 = 7L;
+static int16_t g_113[8] = {0x7CD6L,0x7CD6L,0x7CD6L,0x7CD6L,0x7CD6L,0x7CD6L,0x7CD6L,0x7CD6L};
+static uint16_t g_125 = 0xDF47L;
