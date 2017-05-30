807b95f4f9a3dc2842402859314e819f268ef55c
saisi <saisi@users.noreply.github.com>
2017-05-30 16:49:48 +0000

    1496162987 aegilops

---
+++ 1496162987.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1857525300
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const uint16_t  f0;
+   const uint64_t  f1;
+   volatile int32_t  f2;
+   uint16_t  f3;
+   volatile uint64_t  f4;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_5 = 0x7FL;
