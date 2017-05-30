5bd40b99bd996c68bee6b300cf7af74236ac8c69
saisi <saisi@users.noreply.github.com>
2017-05-30 12:19:26 +0000

    1496146765 Albruna

---
+++ 1496146765.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2226566327
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   unsigned f1 : 9;
+   const volatile int8_t  f2;
+   uint16_t  f3;
+   int64_t  f4;
+};
+
+union U1 {
+   int8_t  f0;
