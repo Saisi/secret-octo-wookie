bd5c65e2b6371d8e2776d19ab3f5ba30115fa672
saisi <saisi@users.noreply.github.com>
2017-05-30 05:48:52 +0000

    1496123332 Achorn

---
+++ 1496123332.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      736399081
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile uint32_t g_10 = 1UL;/* VOLATILE GLOBAL g_10 */
+static int64_t g_17 = (-1L);
+static int64_t g_48 = 0x35211521FAFBF261LL;
+static int8_t g_63 = 7L;
+static uint32_t g_85 = 4294967295UL;
+static uint32_t g_92 = 1UL;
+static int32_t g_104 = 0L;
+static int8_t g_107 = 1L;
+static int8_t *g_106 = &g_107;
