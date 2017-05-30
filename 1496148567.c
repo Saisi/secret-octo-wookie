2ffb470d0000ddf0b3c9e9c0281f88be2fff523f
saisi <saisi@users.noreply.github.com>
2017-05-30 12:49:29 +0000

    1496148567 apronlike

---
+++ 1496148567.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3229093782
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 25;
+   volatile uint8_t  f1;
+   signed f2 : 18;
+   volatile signed f3 : 12;
+   const signed f4 : 1;
+   signed f5 : 9;
+};
+
+union U1 {
