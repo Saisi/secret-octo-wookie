f42ea1be31748d79560958ebee7781590213c16e
saisi <saisi@users.noreply.github.com>
2017-05-30 22:20:19 +0000

    1496182815 allometric

---
+++ 1496182815.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4157891283
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int64_t  f0;
+   const volatile int32_t  f1;
+   unsigned f2 : 15;
+};
+#pragma pack(pop)
+
+struct S1 {
