dd3d5112d5945b905e64fef8710efac1186e3227
saisi <saisi@users.noreply.github.com>
2017-05-30 06:18:54 +0000

    1496125134 ambiguous

---
+++ 1496125134.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      842107538
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   volatile unsigned f0 : 23;
+   volatile signed f1 : 16;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xE7D8A870L;/* VOLATILE GLOBAL g_2 */
