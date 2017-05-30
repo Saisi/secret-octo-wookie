e9d62aa005506fcb80698ef0657404dd0e7ee2f4
saisi <saisi@users.noreply.github.com>
2017-05-30 13:19:31 +0000

    1496150371 avaritia

---
+++ 1496150371.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2134880277
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2[7][3] = {{0x30L,8UL,8UL},{255UL,0x72L,255UL},{0x30L,0x30L,8UL},{0xC7L,0x72L,0xC7L},{0x30L,8UL,8UL},{255UL,0x72L,255UL},{0x30L,0x30L,8UL}};
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int8_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
+/* ------------------------------------------ */
