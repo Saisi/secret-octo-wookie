01653d888cfc4a7661bd8da4845341b84645545c
saisi <saisi@users.noreply.github.com>
2017-05-30 08:49:07 +0000

    1496134147 Arva

---
+++ 1496134147.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3399504117
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_2[7] = {1L,4L,1L,1L,4L,1L,1L};
+static int32_t g_3 = 7L;
+static uint16_t g_8 = 65534UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint64_t  func_1(void);
+
+
