fb7d55232ad3444917828764cec950c1eb516ad5
saisi <saisi@users.noreply.github.com>
2017-05-30 04:18:45 +0000

    1496117925 backpacker

---
+++ 1496117925.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3256218944
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 14;
+   const unsigned f1 : 11;
+   volatile unsigned f2 : 1;
+   unsigned f3 : 9;
+   unsigned f4 : 20;
+   unsigned f5 : 27;
+   signed f6 : 13;
