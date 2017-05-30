ead4eb5f8ef4f1be9aab2fc2bc2c8b03b71a5364
saisi <saisi@users.noreply.github.com>
2017-05-30 22:50:21 +0000

    1496184621 antisialic

---
+++ 1496184621.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4226829416
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   const int8_t  f0;
+   volatile int64_t  f1;
+   int32_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6][3][3] = {{{1L,0L,(-1L)},{0L,(-2L),(-2L)},{0xFDE2BBEFL,1L,(-1L)}},{{1L,1L,1L},{1L,0xD4EA6801L,0L},{0xFDE2BBEFL,0xD94B4B13L,0xFDE2BBEFL}},{{0L,0xD4EA6801L,1L},{1L,1L,1L},{(-1L),1L,0xFDE2BBEFL}},{{(-2L),(-2L),0L},{(-1L),0L,1L},{1L,0L,(-1L)}},{{0L,(-2L),(-2L)},{0xFDE2BBEFL,1L,(-1L)},{1L,1L,1L}},{{1L,0L,(-2L)},{0xD4EA6801L,0xD65121ACL,0xD4EA6801L},{(-2L),0L,0xFDE2BBEFL}}};
+static uint8_t g_3 = 0xA8L;
+static int32_t g_5 = 0xC285CE96L;
