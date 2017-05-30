ab19d6cdb41be7f0cb04390e0022429ebdc0b338
saisi <saisi@users.noreply.github.com>
2017-05-30 01:18:30 +0000

    1496107109 balconet

---
+++ 1496107109.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1406515530
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0L;
+static uint8_t g_9 = 0xBBL;
+static uint32_t g_55[10] = {0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL,0xC875FF9BL};
+static int32_t g_58 = (-2L);
+static uint32_t g_65 = 0UL;
+static int32_t * const  volatile g_66 = (void*)0;/* VOLATILE GLOBAL g_66 */
+static int32_t g_68 = 0x98A7B986L;
+static int32_t * volatile g_67 = &g_68;/* VOLATILE GLOBAL g_67 */
+static int32_t *g_110 = &g_68;
