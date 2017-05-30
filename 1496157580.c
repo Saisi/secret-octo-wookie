dbadd67f59468350b6e79eecbbec9960e87c16af
saisi <saisi@users.noreply.github.com>
2017-05-30 15:19:41 +0000

    1496157580 aphonias

---
+++ 1496157580.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      108168654
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-8L);/* VOLATILE GLOBAL g_2 */
+static int32_t g_3 = 0xDE0BCD8CL;
+static int32_t g_6 = 5L;
+static uint32_t g_11 = 0xF37C04CCL;
+static uint16_t g_17 = 0x34F5L;
+static uint32_t g_26 = 0UL;
+static const uint32_t *g_25 = &g_26;
+static uint8_t g_36[10] = {0xEFL,0xCEL,0xEFL,0xEFL,0xCEL,0xEFL,0xEFL,0xCEL,0xEFL,0xEFL};
+static int64_t g_82 = 0x830386B9BDD1892FLL;
