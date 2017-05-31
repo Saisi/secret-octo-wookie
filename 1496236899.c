453a819dbf41d539f1b09498263b7977d09d5a66
saisi <saisi@users.noreply.github.com>
2017-05-31 13:21:40 +0000

    1496236899 Azarcon

---
+++ 1496236899.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3918827415
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   unsigned f0 : 10;
+   int32_t  f1;
+   const uint32_t  f2;
+   volatile uint32_t  f3;
+   volatile int32_t  f4;
+};
+
+union U1 {
+   const int64_t  f0;
