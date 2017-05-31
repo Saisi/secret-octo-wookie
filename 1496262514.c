b7ee0965e31c8563a89fcab67116fe2ee7436809
saisi <saisi@users.noreply.github.com>
2017-05-31 20:28:34 +0000

    1496262514 adjuncts

---
+++ 1496262514.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1143261710
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const int8_t  f0;
+   volatile int32_t  f1;
+   int64_t  f2;
+   signed f3 : 8;
+};
+
+union U1 {
+   signed f0 : 14;
+   unsigned f1 : 27;
