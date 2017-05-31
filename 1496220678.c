fa3650b1772ba501a01abef1f8e1a3de955233c5
saisi <saisi@users.noreply.github.com>
2017-05-31 08:51:18 +0000

    1496220678 apostolian

---
+++ 1496220678.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2356971001
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint64_t  f0;
+   int32_t  f1;
+   int8_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x023703ACL;
+static volatile int32_t g_46[7] = {(-6L),(-6L),(-6L),(-6L),(-6L),(-6L),(-6L)};
+static volatile int32_t g_47 = 0xAF09D89CL;/* VOLATILE GLOBAL g_47 */
