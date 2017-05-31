345d8b785583b9bf0c1b03034f46497b767909fe
saisi <saisi@users.noreply.github.com>
2017-05-31 13:53:51 +0000

    1496238829 anticyclogenesis

---
+++ 1496238829.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1923614227
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 8;
+   unsigned f1 : 31;
+   volatile unsigned f2 : 6;
+   volatile unsigned f3 : 25;
+   const volatile unsigned f4 : 6;
+   int32_t  f5;
+   unsigned f6 : 11;
+};
+
