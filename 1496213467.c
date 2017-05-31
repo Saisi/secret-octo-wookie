8406eb1e43235fab973279e7369203f5a7e9800e
saisi <saisi@users.noreply.github.com>
2017-05-31 06:51:09 +0000

    1496213467 baculums

---
+++ 1496213467.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      635114833
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 7;
+   volatile unsigned f1 : 2;
+   volatile signed f2 : 29;
+   unsigned f3 : 20;
+   volatile signed f4 : 21;
+   volatile signed f5 : 26;
+   int64_t  f6;
+   unsigned f7 : 16;
+};
