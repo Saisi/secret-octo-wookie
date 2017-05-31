1fcf5b7aeeca0bd984bd7b179676218940f48eb3
saisi <saisi@users.noreply.github.com>
2017-05-31 05:20:59 +0000

    1496208056 alcoved

---
+++ 1496208056.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2703524950
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 25;
+   signed f1 : 10;
+   volatile unsigned f2 : 4;
+   volatile signed : 0;
+   const unsigned f3 : 15;
+   unsigned f4 : 10;
+};
+
+#pragma pack(push)
