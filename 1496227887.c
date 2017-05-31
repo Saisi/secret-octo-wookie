82aaa951cfca86373610dd8b4e7017198dfa3e30
saisi <saisi@users.noreply.github.com>
2017-05-31 10:51:27 +0000

    1496227887 backwraps

---
+++ 1496227887.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4114973795
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_6 = 0x1DBECCCEL;
+static int32_t g_17 = 0L;
+static uint32_t g_33 = 0x37FC0549L;
+static int16_t g_56 = 0xC8B6L;
+static int32_t g_62 = 0xC6BF2A6BL;
+static int32_t * volatile g_61 = &g_62;/* VOLATILE GLOBAL g_61 */
+static int32_t * volatile g_63[6] = {&g_17,&g_62,&g_17,&g_17,&g_62,&g_17};
+static int32_t *g_69[1][7][5] = {{{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62},{&g_62,&g_62,&g_62,&g_62,&g_62}}};
+static int32_t **g_68 = &g_69[0][4][2];
