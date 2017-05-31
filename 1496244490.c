b4b0d60ea69a3f06ae8924da401cb3991ce579e7
saisi <saisi@users.noreply.github.com>
2017-05-31 15:28:10 +0000

    1496244490 Aphodius

---
+++ 1496244490.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1500120111
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint16_t  f0;
+   const uint8_t  f1;
+   volatile uint8_t  f2;
+   volatile uint8_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 1L;
+static uint64_t g_4 = 0x6544D1878ABC106ELL;
