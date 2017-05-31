35f5137981c946832d9d12cf126682e7e38e36eb
saisi <saisi@users.noreply.github.com>
2017-05-31 00:50:33 +0000

    1496191832 adipyl

---
+++ 1496191832.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2898894733
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   int32_t  f1;
+   int8_t  f2;
+   int64_t  f3;
+   const volatile uint8_t  f4;
+   volatile uint32_t  f5;
+   int64_t  f6;
+   volatile int32_t  f7;
+   uint16_t  f8;
