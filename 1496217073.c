f76b01f9f652a1d100c60358fa33c3be02b71add
saisi <saisi@users.noreply.github.com>
2017-05-31 07:51:14 +0000

    1496217073 accrescendi

---
+++ 1496217073.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4002433989
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 1;
+   volatile uint16_t  f1;
+};
+
+union U1 {
+   int64_t  f0;
+   const int8_t * f1;
+   volatile uint64_t  f2;
+   int64_t  f3;
