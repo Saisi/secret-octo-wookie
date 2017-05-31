367d17e60fd5d74ef95b4d8e1750b3d264fb509e
saisi <saisi@users.noreply.github.com>
2017-05-31 15:58:13 +0000

    1496246292 antimallein

---
+++ 1496246292.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1628363743
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t * f0;
+   int32_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_24 = 9UL;
+static uint32_t g_26 = 1UL;
+static int8_t g_28 = 1L;
+static int8_t *g_27 = &g_28;
