d62309f644badef96ad8c8fd1133e35b7237ad7f
saisi <saisi@users.noreply.github.com>
2017-05-31 23:28:48 +0000

    1496273328 albopannin

---
+++ 1496273328.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3780608459
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4 = 0L;
+static volatile uint32_t g_6 = 0xA7ECC244L;/* VOLATILE GLOBAL g_6 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
