bca751cc9663cbda7b40641baed6388402e52a75
saisi <saisi@users.noreply.github.com>
2017-05-31 06:21:05 +0000

    1496211665 Bacteroides

---
+++ 1496211665.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      316368669
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_2 = 253UL;
+static int16_t g_63 = (-8L);
+static int32_t g_66[2][1] = {{0x7D93CBFBL},{0x7D93CBFBL}};
+static int32_t *g_65 = &g_66[0][0];
+static int8_t g_68 = 0xF2L;
+static const int16_t *g_73 = &g_63;
+static const int16_t * volatile * volatile g_72 = &g_73;/* VOLATILE GLOBAL g_72 */
+static int16_t ***g_90 = (void*)0;
+static int8_t g_92[1] = {(-3L)};
