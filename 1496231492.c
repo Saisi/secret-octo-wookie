8cd0539d532bef55b2885f6a957988654134476b
saisi <saisi@users.noreply.github.com>
2017-05-31 11:51:33 +0000

    1496231492 alveolodental

---
+++ 1496231492.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2150671611
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int64_t  f0;
+   const volatile int64_t  f1;
+   const unsigned f2 : 3;
+   int8_t  f3;
+   int64_t  f4;
+};
+
+union U1 {
+   volatile int32_t  f0;
