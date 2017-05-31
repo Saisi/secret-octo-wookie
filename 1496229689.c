8bdce3802fbca583305b7e6fba4493542033275b
saisi <saisi@users.noreply.github.com>
2017-05-31 11:21:30 +0000

    1496229689 Agram

---
+++ 1496229689.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3993868167
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const signed f0 : 2;
+   volatile unsigned f1 : 17;
+   unsigned f2 : 16;
+   signed f3 : 27;
+   signed : 0;
+   signed f4 : 25;
+   signed f5 : 20;
