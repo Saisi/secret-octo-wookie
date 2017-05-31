1bd472bf0b08c0ff1b80b23e989e827f5378969b
saisi <saisi@users.noreply.github.com>
2017-05-31 09:21:21 +0000

    1496222480 Barco

---
+++ 1496222480.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2828026454
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   unsigned f0 : 25;
+   uint8_t  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x08901F2CL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0x3DF603FAL;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-9L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = (-1L);/* VOLATILE GLOBAL g_5 */
