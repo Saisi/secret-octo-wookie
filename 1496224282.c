299bccaf36e72b061a17cb5b2d1733cb8c8a1d1c
saisi <saisi@users.noreply.github.com>
2017-05-31 09:51:23 +0000

    1496224282 ARAMIS

---
+++ 1496224282.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3315528192
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x85299DADL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 1L;/* VOLATILE GLOBAL g_3 */
+static volatile int32_t g_4 = (-6L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 0x4B4E1265L;/* VOLATILE GLOBAL g_5 */
+static int32_t g_6 = 1L;
