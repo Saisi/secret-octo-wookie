f54b834c0ea722878cedc2d2720b487ca661ecbe
saisi <saisi@users.noreply.github.com>
2017-05-31 00:20:30 +0000

    1496190030 attame

---
+++ 1496190030.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2110443261
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int16_t  f0;
+   unsigned : 0;
+   int8_t * f1;
+};
+
+union U1 {
+   int64_t  f0;
+};
+
