2f5bfafcb417ec020e194f13ffecefddc45529c3
saisi <saisi@users.noreply.github.com>
2017-06-01 03:59:10 +0000

    1496289550 Astakiwi

---
+++ 1496289550.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2354595939
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint32_t  f0;
+   int16_t  f1;
+   int64_t  f2;
+   const int8_t * const  volatile  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = (-1L);/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xF1DBECCCL;/* VOLATILE GLOBAL g_3 */
