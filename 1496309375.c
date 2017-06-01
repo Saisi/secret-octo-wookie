e4889c72c1e8d0b4abe55ea8596413d62517435c
saisi <saisi@users.noreply.github.com>
2017-06-01 09:29:36 +0000

    1496309375 astragalocalcaneal

---
+++ 1496309375.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      187264059
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   int32_t  f0;
+   const int32_t  f1;
+   const int64_t  f2;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
