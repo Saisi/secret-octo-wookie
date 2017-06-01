92f4a2832240ce83ca92a861b1db5aad9ce0a95f
saisi <saisi@users.noreply.github.com>
2017-06-01 17:00:16 +0000

    1496336416 ageisms

---
+++ 1496336416.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4066954397
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const signed f0 : 13;
+   const unsigned f1 : 16;
+   unsigned f2 : 19;
+   signed f3 : 18;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int16_t g_14 = (-8L);
+static int32_t g_29 = 0L;
