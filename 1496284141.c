55a6cc32a334eac7fb066387b9fdc144f0ff4bd0
saisi <saisi@users.noreply.github.com>
2017-06-01 02:29:02 +0000

    1496284141 Abipon

---
+++ 1496284141.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      239016547
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   const unsigned f0 : 16;
+   signed f1 : 7;
+   volatile signed f2 : 14;
+   const unsigned f3 : 1;
+   const signed f4 : 14;
+};
+
+struct S1 {
+   unsigned f0 : 19;
