1b370833a3948b05b14a8f7d0861cd60aeb5974a
saisi <saisi@users.noreply.github.com>
2017-06-01 16:00:12 +0000

    1496332812 ammocete

---
+++ 1496332812.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4257353127
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile uint16_t  f0;
+   int8_t * const  f1;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static union U0 g_2 = {0x7937L};/* VOLATILE GLOBAL g_2 */
+
+
+/* --- FORWARD DECLARATIONS --- */
