1805ea74e18d138200b5913036f71c9d0567a66d
saisi <saisi@users.noreply.github.com>
2017-06-01 10:29:40 +0000

    1496312979 anisotropism

---
+++ 1496312979.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3726419112
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int32_t  f0;
+   volatile unsigned f1 : 15;
+   unsigned f2 : 21;
+   int16_t  f3;
+   const volatile int8_t * f4;
+};
+
+union U1 {
+   int64_t  f0;
