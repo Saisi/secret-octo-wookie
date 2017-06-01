66fe8015dae8be96c8edbb3e6a3d46f3e2f3c607
saisi <saisi@users.noreply.github.com>
2017-06-01 08:29:31 +0000

    1496305770 acupunctuate

---
+++ 1496305770.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1868185337
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 3L;
+static volatile int32_t g_4 = 1L;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5[6] = {0x7CF2E566L,2L,0x7CF2E566L,0x7CF2E566L,2L,0x7CF2E566L};
+static volatile int32_t g_6 = 0xED9BA07EL;/* VOLATILE GLOBAL g_6 */
+static int32_t g_7 = 0x2285FAAFL;
+static int32_t g_24 = 7L;
+static uint8_t g_55[5] = {9UL,9UL,9UL,9UL,9UL};
+static int64_t g_60 = 1L;
+static int32_t g_65[9] = {0L,0x0C256FAEL,0x0C256FAEL,0L,0x0C256FAEL,0x0C256FAEL,0L,0x0C256FAEL,0x0C256FAEL};
