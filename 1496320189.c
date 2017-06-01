7fc01defc21b9d3f19d112925118d411bbd5b15b
saisi <saisi@users.noreply.github.com>
2017-06-01 12:29:49 +0000

    1496320189 agallochum

---
+++ 1496320189.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1651027875
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile int16_t  f0;
+   uint64_t  f1;
+   uint64_t  f2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile uint64_t g_2 = 0x6E3245F1DBECCCE1LL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_4 = (-1L);/* VOLATILE GLOBAL g_4 */
+static volatile int32_t g_5 = 1L;/* VOLATILE GLOBAL g_5 */
