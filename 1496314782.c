969efe2c88326904f0382b5e2cf3ed2dd6fa034c
saisi <saisi@users.noreply.github.com>
2017-06-01 10:59:42 +0000

    1496314782 atomy

---
+++ 1496314782.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      546753597
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile unsigned f0 : 29;
+   signed : 0;
+};
+
+union U1 {
+   const uint8_t  f0;
+   const volatile int32_t  f1;
+   volatile int32_t  f2;
+};
