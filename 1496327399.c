6fb47b70f001fa237bf5e60afc98ef7dc0560d43
saisi <saisi@users.noreply.github.com>
2017-06-01 14:30:00 +0000

    1496327399 attractivenesses

---
+++ 1496327399.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2896063491
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int8_t  f0;
+   uint32_t  f1;
+   int8_t  f2;
+   volatile uint8_t  f3;
+   volatile uint32_t  f4;
+   int8_t  f5;
+   const volatile signed f6 : 30;
+};
+
