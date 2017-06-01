f137a9d52c86eb90e809be645491dc85260817c6
saisi <saisi@users.noreply.github.com>
2017-06-01 23:00:46 +0000

    1496358046 austerities

---
+++ 1496358046.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2920558478
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6] = {0x99FB023EL,0x99FB023EL,0x99FB023EL,0x99FB023EL,0x99FB023EL,0x99FB023EL};
+static int32_t g_3 = 0xDAE5A60DL;
+static int32_t g_8 = 0xE94F934FL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int64_t  func_1(void);
+
+
