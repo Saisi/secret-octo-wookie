b54615527ffee3e15b45e108a98ccb0432037ed2
saisi <saisi@users.noreply.github.com>
2017-06-01 03:29:08 +0000

    1496287747 Aquilae

---
+++ 1496287747.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      873926372
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 3;
+   const volatile unsigned f1 : 30;
+   signed f2 : 2;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0x5052061FL;/* VOLATILE GLOBAL g_2 */
+static int32_t g_3[7] = {(-1L),(-1L),(-1L),(-1L),(-1L),(-1L),(-1L)};
+static int32_t g_6 = 0L;
