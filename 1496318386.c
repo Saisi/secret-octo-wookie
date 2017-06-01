2b3dd21664462963616e8c054e51d9848a355177
saisi <saisi@users.noreply.github.com>
2017-06-01 11:59:47 +0000

    1496318386 Avesta

---
+++ 1496318386.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      907597559
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 26;
+   signed f1 : 9;
+   signed f2 : 8;
+};
+
+#pragma pack(push)
+#pragma pack(1)
+struct S1 {
+   const uint8_t  f0;
