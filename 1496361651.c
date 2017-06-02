7b5bb6a3689b4782eb2170d72aa89b79807d53ef
saisi <saisi@users.noreply.github.com>
2017-06-02 00:00:51 +0000

    1496361651 Ametabola

---
+++ 1496361651.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      364401252
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_4[1][10] = {{1L,1L,1L,1L,1L,1L,1L,1L,1L,1L}};
+static uint64_t g_5 = 1UL;
+
+
+/* --- FORWARD DECLARATIONS --- */
+static uint32_t  func_1(void);
+
+
+/* --- FUNCTIONS --- */
