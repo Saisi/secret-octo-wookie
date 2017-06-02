0adb6c665d837139531fca212d3149a63d6ef8d7
saisi <saisi@users.noreply.github.com>
2017-06-02 10:01:45 +0000

    1496397704 antihelices

---
+++ 1496397704.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1710681284
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   signed f0 : 16;
+   const unsigned f1 : 5;
+   volatile unsigned f2 : 19;
+};
+#pragma pack(pop)
+
+struct S1 {
