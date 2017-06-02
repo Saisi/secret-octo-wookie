6f4888a5024e231328bccdffbc87a70b2391929e
saisi <saisi@users.noreply.github.com>
2017-06-02 16:32:23 +0000

    1496421143 anhydraemia

---
+++ 1496421143.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3802732497
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   uint64_t  f0;
+   const uint16_t  f1;
+};
+#pragma pack(pop)
+
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2[6] = {0x17CA0029L,0x469AD59EL,0x17CA0029L,0x17CA0029L,0x469AD59EL,0x17CA0029L};
