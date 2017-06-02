e2451105d9d5b8438d225c901ac9ecd1b41e8cdb
saisi <saisi@users.noreply.github.com>
2017-06-02 21:32:48 +0000

    1496439167 autoimmunity

---
+++ 1496439167.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3429757781
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   int8_t  f0;
+   uint64_t  f1;
+   volatile uint32_t  f2;
+   uint32_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0xACCBAD86L;
+static int32_t g_6 = (-4L);
