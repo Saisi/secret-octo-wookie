c11d2ff025c1beeb2053772dbdd8071581e1dc1f
saisi <saisi@users.noreply.github.com>
2017-06-02 04:01:17 +0000

    1496376076 Antipus

---
+++ 1496376076.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3983825272
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 10;
+   volatile unsigned f1 : 23;
+   const unsigned f2 : 29;
+};
+
+struct S1 {
+   signed f0 : 6;
+};
+
