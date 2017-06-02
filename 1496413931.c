37719c607723e31c7345b1abef04c7942eba38ee
saisi <saisi@users.noreply.github.com>
2017-06-02 14:32:14 +0000

    1496413931 Akutan

---
+++ 1496413931.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3448076434
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 23;
+   const signed f1 : 16;
+   volatile int16_t  f2;
+   signed f3 : 22;
+   volatile signed f4 : 13;
+   volatile int32_t  f5;
+   unsigned f6 : 12;
+   volatile signed f7 : 21;
+   unsigned f8 : 18;
