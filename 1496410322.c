d48238e580be1c08372e3e28e2678cd7a7935ead
saisi <saisi@users.noreply.github.com>
2017-06-02 13:32:03 +0000

    1496410322 artistdom

---
+++ 1496410322.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4195513187
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   volatile int8_t * f0;
+   uint64_t  f1;
+   int64_t  f2;
+   int64_t  f3;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static int8_t g_11 = 1L;
+static int8_t *g_10 = &g_11;
