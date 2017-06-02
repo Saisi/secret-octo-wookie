7c58f322ca2db5e7fc7aa6d5be19e8b20366df5a
saisi <saisi@users.noreply.github.com>
2017-06-02 08:31:38 +0000

    1496392298 angulometer

---
+++ 1496392298.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1450391045
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static int32_t g_3 = 0x47DFA860L;
+static volatile uint8_t g_5 = 0x5CL;/* VOLATILE GLOBAL g_5 */
+static int32_t g_8 = 0xFA32A17DL;
+static int64_t g_9 = 0x84868FDADF860956LL;
+static volatile uint64_t g_10 = 18446744073709551615UL;/* VOLATILE GLOBAL g_10 */
+
+
+/* --- FORWARD DECLARATIONS --- */
+static int32_t  func_1(void);
