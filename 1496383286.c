0c4e2d2494945ce36a8150861677cefdeb4c5bdb
saisi <saisi@users.noreply.github.com>
2017-06-02 06:01:27 +0000

    1496383286 abstersiveness

---
+++ 1496383286.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      2667027679
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_2 = 0xD3DF75DCL;/* VOLATILE GLOBAL g_2 */
+static volatile int32_t g_3 = 0xD7D490C4L;/* VOLATILE GLOBAL g_3 */
+static int32_t g_4[10] = {0x5CCC90A3L,0L,(-1L),(-1L),0L,0x5CCC90A3L,0L,(-1L),(-1L),0L};
+static uint16_t g_27 = 1UL;
+static uint16_t g_30 = 0x6408L;
+static uint16_t *g_81[6] = {&g_30,&g_30,&g_30,&g_30,&g_30,&g_30};
+static volatile uint16_t g_94 = 65535UL;/* VOLATILE GLOBAL g_94 */
+static volatile uint16_t * volatile g_93 = &g_94;/* VOLATILE GLOBAL g_93 */
+static volatile uint16_t * volatile *g_92[7] = {&g_93,&g_93,&g_93,&g_93,&g_93,&g_93,&g_93};
