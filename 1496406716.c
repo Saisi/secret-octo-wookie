5b01b40df7e865a01e5c35c06483825ee2781818
saisi <saisi@users.noreply.github.com>
2017-06-02 12:31:56 +0000

    1496406716 androclus

---
+++ 1496406716.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3983862013
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+#pragma pack(push)
+#pragma pack(1)
+struct S0 {
+   const volatile uint64_t  f0;
+   int64_t  f1;
+   unsigned f2 : 4;
+   uint64_t  f3;
+   volatile int32_t  f4;
+   volatile int32_t  f5;
+   const uint64_t  f6;
