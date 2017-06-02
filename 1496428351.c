09a768433a07bd4184cca349217ed63021b752d2
saisi <saisi@users.noreply.github.com>
2017-06-02 18:32:32 +0000

    1496428351 balladising

---
+++ 1496428351.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3768523663
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+/* --- GLOBAL VARIABLES --- */
+static volatile int32_t g_4 = 0x2A2C7C8AL;/* VOLATILE GLOBAL g_4 */
+static volatile int32_t *g_3 = &g_4;
+static int32_t g_19 = 1L;
+static uint16_t g_23 = 0x3817L;
+static volatile int8_t g_33 = 0xE8L;/* VOLATILE GLOBAL g_33 */
+static volatile int64_t g_34 = 0x45A3231966BA0539LL;/* VOLATILE GLOBAL g_34 */
+static volatile uint8_t g_35[3] = {247UL,247UL,247UL};
+static int32_t *g_71 = (void*)0;
+static int32_t **g_70 = &g_71;
