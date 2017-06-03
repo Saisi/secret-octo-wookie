5c05f3bb5c202d8e0eeb12bf2449928ec2b223a9
saisi <saisi@users.noreply.github.com>
2017-06-03 00:03:01 +0000

    1496448180 alliable

---
+++ 1496448180.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      537906893
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+union U0 {
+   uint8_t  f0;
+   const int8_t  f1;
+   unsigned f2 : 11;
+};
+
+/* --- GLOBAL VARIABLES --- */
+static uint8_t g_4[9] = {0x94L,0x94L,0x94L,0x94L,0x94L,0x94L,0x94L,0x94L,0x94L};
+static int32_t g_18[6][8] = {{7L,6L,(-5L),0x21DD8E18L,0x879096A9L,7L,7L,0x879096A9L},{0x37F5E175L,(-5L),(-5L),0x37F5E175L,0x0E56979FL,1L,0x879096A9L,(-5L)},{0x879096A9L,0xF51B4373L,0x09757A83L,(-1L),0xF51B4373L,(-6L),0xF51B4373L,(-1L)},{1L,0xF51B4373L,1L,(-5L),0x879096A9L,1L,0x0E56979FL,0x37F5E175L},{6L,(-5L),0x21DD8E18L,0x879096A9L,7L,(-6L),1L,0xEA0262B0L},{(-5L),(-5L),0x37F5E175L,0x0E56979FL,1L,0x879096A9L,(-5L),1L}};
+static union U0 g_21[8][1] = {{{255UL}},{{0x04L}},{{0x04L}},{{255UL}},{{0x04L}},{{0x04L}},{{255UL}},{{0x04L}}};
