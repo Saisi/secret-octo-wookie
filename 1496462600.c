e9802d771a90a049ef3d629fe8703173c905d08c
saisi <saisi@users.noreply.github.com>
2017-06-03 04:03:21 +0000

    1496462600 atmologist

---
+++ 1496462600.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      3717053565
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   volatile signed f0 : 29;
+   unsigned : 0;
+   volatile uint32_t  f1;
+   volatile signed f2 : 2;
+   const unsigned f3 : 11;
+   signed f4 : 24;
+   signed f5 : 23;
+   const signed f6 : 14;
+};
