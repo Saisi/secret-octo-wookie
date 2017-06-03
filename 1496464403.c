3af70b57d84d75fc719f8957e5c232afa57f01e4
saisi <saisi@users.noreply.github.com>
2017-06-03 04:33:23 +0000

    1496464403 acrogynae

---
+++ 1496464403.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      644503419
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   signed f0 : 28;
+   const volatile signed f1 : 3;
+   volatile signed f2 : 26;
+   const signed f3 : 29;
+   volatile signed f4 : 26;
+   volatile unsigned f5 : 11;
+   unsigned f6 : 30;
+};
+
