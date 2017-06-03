3faa9d046e51c26e563f90e53a5c9e673458e443
saisi <saisi@users.noreply.github.com>
2017-06-03 03:03:16 +0000

    1496458996 babyism

---
+++ 1496458996.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      4230681307
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S2 {
+   volatile int32_t  f0;
+   int64_t  f1;
+   const volatile uint64_t  f2;
+   uint32_t  f3;
+   const uint32_t  f4;
+   int32_t  f5;
+   int32_t  f6;
+   const volatile int32_t  f7;
+};
