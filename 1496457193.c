2313b94fc0c3b2549c5dba90b4c1811abef47eaf
saisi <saisi@users.noreply.github.com>
2017-06-03 02:33:14 +0000

    1496457193 Amalrician

---
+++ 1496457193.c
@@ -0,0 +1,25 @@
+/*
+ * This is a RANDOMLY GENERATED PROGRAM.
+ *
+ * Generator: csmith 2.2.0
+ * Git version: dcef523
+ * Options:   (none)
+ * Seed:      1004517747
+ */
+
+#include "csmith.h"
+
+
+static long __undefined;
+
+/* --- Struct/Union Declarations --- */
+struct S0 {
+   int16_t  f0;
+   volatile int64_t  f1;
+   volatile int16_t  f2;
+   volatile int64_t  f3;
+   volatile int32_t  f4;
+   int16_t  f5;
+   volatile uint16_t  f6;
+};
+
